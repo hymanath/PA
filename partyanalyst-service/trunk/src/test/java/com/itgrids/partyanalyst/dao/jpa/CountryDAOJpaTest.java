76db4f89c29c31346ac6cc438a22f4d0731ccba3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-07-14 17:08:56 +0000

    Unit test case for country dao...

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/jpa/CountryDAOJpaTest.java
@@ -0,0 +1,26 @@
+package com.itgrids.partyanalyst.dao.jpa;
+
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.appfuse.dao.BaseDaoTestCase;
+import org.junit.Test;
+
+import com.itgrids.partyanalyst.model.State;
+
+public class CountryDAOJpaTest extends BaseDaoTestCase {
+	
+	private ICountryDAO countryDAO;
+
+	public void setCountryDAO(ICountryDAO countryDAO) {
+		this.countryDAO = countryDAO;
+	}
+	
+	@Test
+	public void testFindStates() {
+		Set<State> states = countryDAO.get(new Long(1)).getStates();
+		Assert.assertEquals(2, states.size());
+	}
+	
+}
