000a4e57108b49ebcbe660dcfaab01d74fca3a87
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-10-13 02:55:22 +0000

    Deleted to integrate our code base

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/IPartyDao.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.dao.hibernate;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.partyanalyst.model.Party;
-
-public interface IPartyDao extends GenericDao<Party, Long> {
-
-}
