9ba689554dc968d4c13e39e29922c04b3dcac4a7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-10-21 10:54:28 +0000

    initial version

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ElectionAllianceDAOHibernateTest.java
@@ -0,0 +1,39 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IElectionAllianceDAO;
+import com.itgrids.partyanalyst.model.ElectionAlliance;
+
+
+public class ElectionAllianceDAOHibernateTest extends BaseDaoTestCase {
+	
+	private IElectionAllianceDAO electionAllianceDAO;
+
+	public void setElectionAllianceDAO(IElectionAllianceDAO electionAllianceDAO){
+		this.electionAllianceDAO = electionAllianceDAO;
+	}
+	
+	//@Test
+	public void testFindByElectionYear(){
+		List<ElectionAlliance> alliances = electionAllianceDAO.findByElectionYear("2009");
+		Assert.assertEquals(4, alliances.size());
+	}
+
+	//@Test
+	public void testHasAlliances(){
+		List<ElectionAlliance> allianceList = electionAllianceDAO.findByElectionYear("2009");
+		for(ElectionAlliance alliance: allianceList){
+			Long partyId = alliance.getParty().getPartyId();
+			if(partyId.equals(new Long(11))){
+				System.out.println("true");
+			} 
+		}
+	
+	}
+}
