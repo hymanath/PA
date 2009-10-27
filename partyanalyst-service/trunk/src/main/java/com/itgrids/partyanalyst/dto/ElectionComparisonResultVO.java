4d28c52d240903bb840c7cf1580173ac7c4633b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-10-27 09:00:26 +0000

    ElectionComparisonResultVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ElectionComparisonResultVO.java
@@ -0,0 +1,53 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 12, 2009
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class ElectionComparisonResultVO {
+
+	private Long districtId;
+	private Long stateId;
+	private String districtName;
+	private List<PartyElectionResultsVO> partyElectionResultsVO;
+	private List<CandidateOppositionVO> candidateOppositionVO;
+	
+	//getters and setters
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public Long getStateId() {
+		return stateId;
+	}
+	public void setStateId(Long stateId) {
+		this.stateId = stateId;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public List<PartyElectionResultsVO> getPartyElectionResultsVO() {
+		return partyElectionResultsVO;
+	}
+	public void setPartyElectionResultsVO(
+			List<PartyElectionResultsVO> partyElectionResultsVO) {
+		this.partyElectionResultsVO = partyElectionResultsVO;
+	}
+	public List<CandidateOppositionVO> getCandidateOppositionVO() {
+		return candidateOppositionVO;
+	}
+	public void setCandidateOppositionVO(
+			List<CandidateOppositionVO> candidateOppositionVO) {
+		this.candidateOppositionVO = candidateOppositionVO;
+	}
+}
