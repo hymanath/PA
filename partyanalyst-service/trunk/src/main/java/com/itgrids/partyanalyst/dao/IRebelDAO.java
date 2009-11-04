f7a1c9457913719db03b4b4eaa51202908dab9be
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-11-04 22:50:55 +0000

    rebels initial version

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IRebelDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AllianceGroup;
+import com.itgrids.partyanalyst.model.PartyRebel;
+
+public interface IRebelDAO extends GenericDao<PartyRebel, Long> {
+
+}
