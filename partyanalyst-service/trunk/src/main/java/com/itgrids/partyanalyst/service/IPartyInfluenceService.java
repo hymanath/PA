4dc3695a6f60f49e8e4405042de83c8ad3bcc779
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-11-08 18:05:39 +0000

    IPartyInfluenceService - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyInfluenceService.java
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on November 3, 2009
+ */
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.DistrictWiseConstituencyElectionResultsVO;
+import com.itgrids.partyanalyst.model.Election;
+import com.itgrids.partyanalyst.model.Nomination;
+
+public interface IPartyInfluenceService {
+
+	public List<DistrictWiseConstituencyElectionResultsVO> getPartyInfluenceReportResults(Long electionTypeId,Long partyId,Long newPartyId,String electionYear,Boolean includeAlliance);
+	
+	public Election getElection(Long electionScopeId,String electionYear);
+	
+	public List<Long> getHasAllianceParties(String electionYear,Long electionType,Long partyId);
+	
+	public List<Nomination> getNominationsForAPartyForAnElection(Long electionTypeId,Long partyId,String electionYear,Long electionId,Boolean includeAlliance);
+}
