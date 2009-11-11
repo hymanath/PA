eb546798e220c264ca47cf2c12342a116b5b7a8b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-11-11 12:30:23 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICrossVotingEstimationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICrossVotingEstimationService.java
@@ -1,8 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.CrossVotingConsolidateVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface ICrossVotingEstimationService {
 	
 	public CrossVotingConsolidateVO getConsolidatedCrossVotingDetails(String electionYear, Long partyId, Long acId, Long pcId);
+	
+	public List<SelectOptionVO> getAssembliesForParliament(Long parliamentId, Long electionYear);
 }
