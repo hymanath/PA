0e83bc2aa894d8903b1fb70473c07766cf5ea7da
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-11-12 11:34:40 +0000

    Created By Mohan

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CrossVotingReportAjaxAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CrossVotingReportAjaxAction.java
@@ -2,32 +2,22 @@ package com.itgrids.partyanalyst.web.action;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
-import org.apache.struts2.util.ServletContextAware;
+
+import com.itgrids.partyanalyst.dto.CrossVotingConsolidateVO;
+import com.itgrids.partyanalyst.dto.ResultObjectVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.ICrossVotingEstimationService;
+import com.itgrids.partyanalyst.service.IPartyBoothWiseResultsService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
-import com.itgrids.partyanalyst.dto.CrossVotingConsolidateVO;
-import com.itgrids.partyanalyst.dto.CrossVotedCandidateVO;
-import com.itgrids.partyanalyst.dto.CrossVotedMandalVO;
-import com.itgrids.partyanalyst.dto.CrossVotedBoothVO;
-import com.itgrids.partyanalyst.dto.ResultObjectVO;
 
+public class CrossVotingReportAjaxAction extends ActionSupport implements ServletRequestAware{
 
-public class CrossVotingReportAjaxAction extends ActionSupport implements ServletRequestAware,ServletContextAware{
-
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String electionYear;
 	private String party;
@@ -36,21 +26,32 @@ public class CrossVotingReportAjaxAction extends ActionSupport implements Servle
 	private HttpServletRequest request;
 	private List<SelectOptionVO> parliamentList;
 	private CrossVotingConsolidateVO crossVotingConsolidateVO;
-	private CrossVotedCandidateVO pcCrossVotedCandidateVO;
-	private CrossVotedCandidateVO acCrossVotedCandidateVO;
-	private List<CrossVotedMandalVO> crossVotedMandalVO;
-	private List<CrossVotedBoothVO> crossVotedBoothVO;
 	private ResultObjectVO resultObjectVO; 
-	
+	private IPartyBoothWiseResultsService partyBoothWiseResultsService;
+	private ICrossVotingEstimationService crossVotingEstimationService;
 
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;		
 	}
-
-	public void setServletContext(ServletContext arg0) {
-		// TODO Auto-generated method stub
-		
+	
+	public IPartyBoothWiseResultsService getPartyBoothWiseResultsService() {
+		return partyBoothWiseResultsService;
 	}
+
+	public void setPartyBoothWiseResultsService(
+			IPartyBoothWiseResultsService partyBoothWiseResultsService) {
+		this.partyBoothWiseResultsService = partyBoothWiseResultsService;
+	}
+
+	public ICrossVotingEstimationService getCrossVotingEstimationService() {
+		return crossVotingEstimationService;
+	}
+
+	public void setCrossVotingEstimationService(
+			ICrossVotingEstimationService crossVotingEstimationService) {
+		this.crossVotingEstimationService = crossVotingEstimationService;
+	}
+
 	
 	public String getElectionYear() {
 		return electionYear;
@@ -129,16 +130,14 @@ public class CrossVotingReportAjaxAction extends ActionSupport implements Servle
 			
 			
 			SelectOptionVO pList1 = new SelectOptionVO();
-			pList1.setId(new Long(1));
-			pList1.setName("Nellore");
+			pList1.setId(new Long(227));
+			pList1.setName("Ongole");			
 			
-			SelectOptionVO pList2 = new SelectOptionVO();
-			pList2.setId(new Long(2));
-			pList2.setName("Rangareddy");
+			parliamentList.add(pList1);			
 			
-			parliamentList.add(pList1);
-			parliamentList.add(pList2);
-				
+			/*parliamentList = partyBoothWiseResultsService.getConstituenciesForParty(new Long(party.trim()), new Long(1), electionYear);
+			System.out.println("Parliament Consties List Size $$$$$$$$$"+parliamentList.size());*/
+			
 			resultObjectVO.setDataList(parliamentList);	
 			
 			
@@ -147,113 +146,36 @@ public class CrossVotingReportAjaxAction extends ActionSupport implements Servle
 		{
 			System.out.println("IN parliament value");
 			
-			System.out.println("IN election year");
-			
 			parliamentList = new ArrayList<SelectOptionVO>();
 			
 			SelectOptionVO pList1 = new SelectOptionVO();
-			pList1.setId(new Long(1));
-			pList1.setName("Nellore");
-			
-			SelectOptionVO pList2 = new SelectOptionVO();
-			pList2.setId(new Long(2));
-			pList2.setName("Rangareddy");
-			
+			pList1.setId(new Long(232));			
+			pList1.setName("Kavali");
 			parliamentList.add(pList1);
-			parliamentList.add(pList2);
+			/*System.out.println("IN election year");
+			
+			parliamentList = crossVotingEstimationService.getAssembliesForParliament(new Long(parliamentValue.trim()), new Long(electionYear.trim()));
+			
+			System.out.println("Parliament Consties List Size $$$$$$$$$"+parliamentList.size());*/
 			
 			resultObjectVO.setDataList(parliamentList);	
 			
 		}
-		else if(electionYear == null && party == null && parliamentValue == null && assemblyValue != null)
+		else if(electionYear != null && party != null && parliamentValue != null && assemblyValue != null)
 		{
-			System.out.println("IN assembly value");
-			crossVotedBoothVO = new ArrayList<CrossVotedBoothVO>();
-			crossVotedMandalVO = new ArrayList<CrossVotedMandalVO>();
+			System.out.println("IN assembly values = "+electionYear+"**"+party+"**"+parliamentValue+"**"+assemblyValue);
 			
-			//
+			System.out.println("Before service execution");			
+			crossVotingConsolidateVO = crossVotingEstimationService.getConsolidatedCrossVotingDetails(electionYear, new Long(party.trim()), new Long(assemblyValue.trim()), new Long(parliamentValue.trim()));
+			System.out.println("Size = "+crossVotingConsolidateVO.getMandals().size());
+			System.out.println("After service execution");
 			
-			CrossVotedBoothVO booth1 =  new CrossVotedBoothVO();
-			booth1.setAcPercentage("50");
-			booth1.setAcValidVotes(new Long(15000));
-			booth1.setPartNO("2");
-			booth1.setPcPercentage("20");
-			booth1.setPcValidVotes(new Long(14000));
-			booth1.setPercentageDifference("20");
-			booth1.setVillagesCovered("Nellore,Krishna");
-			
-			CrossVotedBoothVO booth2 =  new CrossVotedBoothVO();
-			booth2.setAcPercentage("40");
-			booth2.setAcValidVotes(new Long(20000));
-			booth2.setPartNO("3");
-			booth2.setPcPercentage("30");
-			booth2.setPcValidVotes(new Long(10000));
-			booth2.setPercentageDifference("25");
-			booth2.setVillagesCovered("Rangareddy,Adilabad");
-			
-			crossVotedBoothVO.add(booth1);
-			crossVotedBoothVO.add(booth2);			
-			
-			//
-			
-			CrossVotedMandalVO mandal1 = new CrossVotedMandalVO();
-			mandal1.setAcEarnedVotesInMandal(new Long(20000));
-			mandal1.setAcPercentageInMandal("40");
-			mandal1.setAcValidVotesInMandal(new Long(18000));
-			mandal1.setMandalName("Allur");
-			mandal1.setPcEarnedVotesInMandal(new Long(25000));
-			mandal1.setPcPercentageInMandal("25");
-			mandal1.setPcValidVotesInMandal(new Long(22000));
-			mandal1.setPercentageDifferenceInMandal("20");
-			mandal1.setCrossVotedBooths(crossVotedBoothVO);
-			
-			CrossVotedMandalVO mandal2 = new CrossVotedMandalVO();
-			mandal2.setAcEarnedVotesInMandal(new Long(35000));
-			mandal2.setAcPercentageInMandal("60");
-			mandal2.setAcValidVotesInMandal(new Long(32000));
-			mandal2.setMandalName("Sullurupeta");
-			mandal2.setPcEarnedVotesInMandal(new Long(60000));
-			mandal2.setPcPercentageInMandal("52");
-			mandal2.setPcValidVotesInMandal(new Long(55000));
-			mandal2.setPercentageDifferenceInMandal("40");
-			mandal2.setCrossVotedBooths(crossVotedBoothVO);
-
-			crossVotedMandalVO.add(mandal1);
-			crossVotedMandalVO.add(mandal2);
-			
-			
-			//---
-			
-			pcCrossVotedCandidateVO = new CrossVotedCandidateVO();
-			pcCrossVotedCandidateVO.setCandidateId(new Long(1));
-			pcCrossVotedCandidateVO.setCandidateName("Siva Kumar");
-			pcCrossVotedCandidateVO.setImage("image");
-			pcCrossVotedCandidateVO.setRank("1");
-			pcCrossVotedCandidateVO.setVotesPercentage("20");		
-			
-			acCrossVotedCandidateVO = new CrossVotedCandidateVO();
-			acCrossVotedCandidateVO.setCandidateId(new Long(2));
-			acCrossVotedCandidateVO.setCandidateName("Sai Krishna");
-			acCrossVotedCandidateVO.setImage("image");
-			acCrossVotedCandidateVO.setRank("1");
-			acCrossVotedCandidateVO.setVotesPercentage("25");	 
-			
-			//--
-			
-			crossVotingConsolidateVO = new CrossVotingConsolidateVO();
-			crossVotingConsolidateVO.setAcCandidateData(pcCrossVotedCandidateVO);
-			crossVotingConsolidateVO.setPcCandidateData(acCrossVotedCandidateVO);
-			crossVotingConsolidateVO.setMandals(crossVotedMandalVO);
-			
-			setCrossVotingConsolidateVO(crossVotingConsolidateVO);	
-				
 			resultObjectVO.setCrossVotingConsolidateVO(crossVotingConsolidateVO);	
-		}
-		
-				
+		}		
 				
 		return Action.SUCCESS;
 	}
+
 
 	
 }
--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CrossVotingReportInputAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CrossVotingReportInputAction.java
@@ -14,21 +14,18 @@ import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IPartyBoothWiseResultsService;
 
 
-public class CrossVotingReportInputAction extends ActionSupport implements ServletContextAware,ServletRequestAware {
+public class CrossVotingReportInputAction extends ActionSupport implements ServletRequestAware {
 
 	private List<SelectOptionVO> electionYearList;
 	private List<SelectOptionVO> partyList;
-	
-	public void setServletContext(ServletContext arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	private IPartyBoothWiseResultsService partyBoothWiseResultsService;
+	private HttpServletRequest request;
 
-	public void setServletRequest(HttpServletRequest arg0) {
-		// TODO Auto-generated method stub
-		
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;	
 	}
 	
 	public List<SelectOptionVO> getElectionYearList() {
@@ -46,63 +43,41 @@ public class CrossVotingReportInputAction extends ActionSupport implements Servl
 	public void setPartyList(List<SelectOptionVO> partyList) {
 		this.partyList = partyList;
 	}
+	
+	public IPartyBoothWiseResultsService getPartyBoothWiseResultsService() {
+		return partyBoothWiseResultsService;
+	}
+
+	public void setPartyBoothWiseResultsService(
+			IPartyBoothWiseResultsService partyBoothWiseResultsService) {
+		this.partyBoothWiseResultsService = partyBoothWiseResultsService;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
 
 	public String execute(){
 		
 		electionYearList = new ArrayList<SelectOptionVO>();
-		partyList =  new ArrayList<SelectOptionVO>();
-		
+		partyList =  partyBoothWiseResultsService.getParties();
+				
 		SelectOptionVO eList1 = new SelectOptionVO();
-		eList1.setId(new Long(2009));
-		eList1.setName("2009");
-		
-		SelectOptionVO eList2 = new SelectOptionVO();
-		eList2.setId(new Long(2004));
-		eList2.setName("2004");
+		eList1.setId(new Long(2004));
+		eList1.setName("2004");
 		
 		electionYearList.add(eList1);
-		electionYearList.add(eList2);
+		
 		setElectionYearList(electionYearList);
-		
-		SelectOptionVO pList1 =  new SelectOptionVO();
-		pList1.setId(new Long(1));
-		pList1.setName("INC");
-		
-		SelectOptionVO pList2 =  new SelectOptionVO();
-		pList2.setId(new Long(2));
-		pList2.setName("BJP");
-		
-		SelectOptionVO pList3 =  new SelectOptionVO();
-		pList3.setId(new Long(4));
-		pList3.setName("CPI");
-		
-		SelectOptionVO pList4 =  new SelectOptionVO();
-		pList4.setId(new Long(5));
-		pList4.setName("CPM");
-		
-		SelectOptionVO pList5 =  new SelectOptionVO();
-		pList5.setId(new Long(9));
-		pList5.setName("PRP");
-		
-		SelectOptionVO pList6 =  new SelectOptionVO();
-		pList6.setId(new Long(10));
-		pList6.setName("TRS");
-		
-		SelectOptionVO pList7 =  new SelectOptionVO();
-		pList6.setId(new Long(11));
-		pList6.setName("TDP");
-		
-		partyList.add(pList1);
-		partyList.add(pList2);
-		partyList.add(pList3);
-		partyList.add(pList4);
-		partyList.add(pList5);
-		partyList.add(pList6);
-		partyList.add(pList7);
-		setPartyList(partyList);
 		
 		return Action.SUCCESS;
 	}
+
+
 	
 
 }
