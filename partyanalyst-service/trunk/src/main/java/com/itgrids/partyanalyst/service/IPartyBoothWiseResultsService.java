3729b25a41f1e27a2ec811a77f0a06fa8884aebf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-03 16:08:50 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyBoothWiseResultsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyBoothWiseResultsService.java
@@ -2,12 +2,9 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
-import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.excel.booth.PartyBoothPerformanceVO;
 
 public interface IPartyBoothWiseResultsService {
-
-	public List<SelectOptionVO> getConstituenciesForElectionScopeAndYear(Long electionScopeId, Long electionYear);
 	
 	public List<PartyBoothPerformanceVO> getBoothWiseResultsForParty(Long partyId, Long constituencyId, String electionYear);
 }
