dd1dd47e9403cc5bce4408c42109ccbfe89665e4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-03 15:55:30 +0000

    IExcelToDBService - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IExcelToDBService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IExcelToDBService.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.service;
 
+import java.io.File;
+
 import com.itgrids.partyanalyst.excel.upload.vo.UploadFormVo;
 
 public interface IExcelToDBService {
 
-	public void readCSVFileAndStoreIntoDB(UploadFormVo uploadFormValueObject) throws Exception;
+	public void readCSVFileAndStoreIntoDB(UploadFormVo uploadFormValueObject,String fileName,File fileToUpload) throws Exception;
 }
