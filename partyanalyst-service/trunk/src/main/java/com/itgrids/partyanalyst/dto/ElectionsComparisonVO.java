6a87ba3182c5ec01a53f29346b84e3298f230dc5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-09 17:23:55 +0000

    ElectionsComparisonVO - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ElectionsComparisonVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ElectionsComparisonVO.java
@@ -17,6 +17,8 @@ public class ElectionsComparisonVO {
 	private int votesGainedCount;
 	private int votesLostCount;
 	private int count;
+	private int constiNotConstiCountForYearOne;
+	private int constiNotConstiCountForYearTwo;
 	private int seatsWonInFirstYearForVotesGained;
 	private int seatsWonInSecondYearForVotesGained;
 	private int seatsLostInFirstYearForVotesGained;
@@ -26,8 +28,19 @@ public class ElectionsComparisonVO {
 	private int seatsWonInSecondYearForVotesLost;
 	private int seatsLostInFirstYearForVotesLost;
 	private int seatsLostInSecondYearForVotesLost;
+	private int constiNotConsideredForYearOneSeatsWon = 0;
+	private int constiNotConsideredForYearOneSeatsLost = 0;
+	private int constiNotConsideredForYearTwoSeatsWon = 0;
+	private int constiNotConsideredForYearTwoSeatsLost = 0;
 	private List<ElectionComparisonResultVO> votesLost;
 	private List<ElectionComparisonResultVO> constituenciesNotConsidered;
+	private List<ElectionComparisonResultVO> constituenciesNotConsideredForYearOne;
+	private List<ElectionComparisonResultVO> constituenciesNotConsideredForYearTwo;
+	private List<PartyElectionResultsVO> constituenciesNotConsideredInFirstYear;
+	private List<PartyElectionResultsVO> constituenciesNotConsideredInSecondYear;
+	private int constiNotParticipatedForYearOne;
+	private int constiNotParticipatedForYearTwo;
+	
 	public int getVotesGainedCount() {
 		return votesGainedCount;
 	}
@@ -142,5 +155,87 @@ public class ElectionsComparisonVO {
 	public void setSeatsLostInSecondYearForVotesLost(
 			int seatsLostInSecondYearForVotesLost) {
 		this.seatsLostInSecondYearForVotesLost = seatsLostInSecondYearForVotesLost;
+	}
+	public List<PartyElectionResultsVO> getConstituenciesNotConsideredInFirstYear() {
+		return constituenciesNotConsideredInFirstYear;
+	}
+	public void setConstituenciesNotConsideredInFirstYear(
+			List<PartyElectionResultsVO> constituenciesNotConsideredInFirstYear) {
+		this.constituenciesNotConsideredInFirstYear = constituenciesNotConsideredInFirstYear;
+	}
+	public List<PartyElectionResultsVO> getConstituenciesNotConsideredInSecondYear() {
+		return constituenciesNotConsideredInSecondYear;
+	}
+	public void setConstituenciesNotConsideredInSecondYear(
+			List<PartyElectionResultsVO> constituenciesNotConsideredInSecondYear) {
+		this.constituenciesNotConsideredInSecondYear = constituenciesNotConsideredInSecondYear;
+	}
+	public List<ElectionComparisonResultVO> getConstituenciesNotConsideredForYearOne() {
+		return constituenciesNotConsideredForYearOne;
+	}
+	public void setConstituenciesNotConsideredForYearOne(
+			List<ElectionComparisonResultVO> constituenciesNotConsideredForYearOne) {
+		this.constituenciesNotConsideredForYearOne = constituenciesNotConsideredForYearOne;
+	}
+	public List<ElectionComparisonResultVO> getConstituenciesNotConsideredForYearTwo() {
+		return constituenciesNotConsideredForYearTwo;
+	}
+	public void setConstituenciesNotConsideredForYearTwo(
+			List<ElectionComparisonResultVO> constituenciesNotConsideredForYearTwo) {
+		this.constituenciesNotConsideredForYearTwo = constituenciesNotConsideredForYearTwo;
+	}
+	public int getConstiNotConstiCountForYearOne() {
+		return constiNotConstiCountForYearOne;
+	}
+	public void setConstiNotConstiCountForYearOne(int constiNotConstiCountForYearOne) {
+		this.constiNotConstiCountForYearOne = constiNotConstiCountForYearOne;
+	}
+	public int getConstiNotConstiCountForYearTwo() {
+		return constiNotConstiCountForYearTwo;
+	}
+	public void setConstiNotConstiCountForYearTwo(int constiNotConstiCountForYearTwo) {
+		this.constiNotConstiCountForYearTwo = constiNotConstiCountForYearTwo;
+	}
+	public int getConstiNotParticipatedForYearOne() {
+		return constiNotParticipatedForYearOne;
+	}
+	public void setConstiNotParticipatedForYearOne(
+			int constiNotParticipatedForYearOne) {
+		this.constiNotParticipatedForYearOne = constiNotParticipatedForYearOne;
+	}
+	public int getConstiNotParticipatedForYearTwo() {
+		return constiNotParticipatedForYearTwo;
+	}
+	public void setConstiNotParticipatedForYearTwo(
+			int constiNotParticipatedForYearTwo) {
+		this.constiNotParticipatedForYearTwo = constiNotParticipatedForYearTwo;
+	}
+	public int getConstiNotConsideredForYearOneSeatsWon() {
+		return constiNotConsideredForYearOneSeatsWon;
+	}
+	public void setConstiNotConsideredForYearOneSeatsWon(
+			int constiNotConsideredForYearOneSeatsWon) {
+		this.constiNotConsideredForYearOneSeatsWon = constiNotConsideredForYearOneSeatsWon;
+	}
+	public int getConstiNotConsideredForYearOneSeatsLost() {
+		return constiNotConsideredForYearOneSeatsLost;
+	}
+	public void setConstiNotConsideredForYearOneSeatsLost(
+			int constiNotConsideredForYearOneSeatsLost) {
+		this.constiNotConsideredForYearOneSeatsLost = constiNotConsideredForYearOneSeatsLost;
+	}
+	public int getConstiNotConsideredForYearTwoSeatsWon() {
+		return constiNotConsideredForYearTwoSeatsWon;
+	}
+	public void setConstiNotConsideredForYearTwoSeatsWon(
+			int constiNotConsideredForYearTwoSeatsWon) {
+		this.constiNotConsideredForYearTwoSeatsWon = constiNotConsideredForYearTwoSeatsWon;
+	}
+	public int getConstiNotConsideredForYearTwoSeatsLost() {
+		return constiNotConsideredForYearTwoSeatsLost;
+	}
+	public void setConstiNotConsideredForYearTwoSeatsLost(
+			int constiNotConsideredForYearTwoSeatsLost) {
+		this.constiNotConsideredForYearTwoSeatsLost = constiNotConsideredForYearTwoSeatsLost;
 	}
 }
