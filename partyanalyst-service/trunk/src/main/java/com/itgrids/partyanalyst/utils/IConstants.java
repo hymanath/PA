0a3b6e6808dbe2af5932b18a321b43507a5c1f0d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-11 09:18:33 +0000

    Added BJP party in static parties - Narender

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -3,6 +3,6 @@ package com.itgrids.partyanalyst.utils;
 public interface IConstants {
 	public static final String DATE_PATTERN = "dd/MM/yyyy";
 	public static final Long CENSUS_YEAR = 2001L;
-	public static final String STATIC_PARTIES = "'INC','PRP','TDP','TRS','CPI','CPM','AIMIM'";
+	public static final String STATIC_PARTIES = "'INC','PRP','TDP','TRS','CPI','CPM','AIMIM','BJP'";
 	public static final int LOOSING_BY_DROPPING_VOTES_CONSTANTS = 5;
 }
