8ee8b37ab922edc7df4b0731a7f58a3dd9a772ac
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-15 16:41:54 +0000

    Modified By Sivakumar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AjaxSearchAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AjaxSearchAction.java
@@ -19,23 +19,12 @@ import com.opensymphony.xwork2.Action;
 public class AjaxSearchAction implements ServletRequestAware{
 
 	private List<String> namesList;
-	private List<SelectOptionVO> candidatesList;
 	JSONObject jObj = null,respObj=null;
 	private String task = null;
 	private IConstituencySearchService constituencySearchService;
 	private ICandidateSearchService candidateSearchService;
 	private HttpServletRequest request;
 	private HttpSession session;
-	
-	
-	
-	public List<SelectOptionVO> getCandidatesList() {
-		return candidatesList;
-	}
-
-	public void setCandidatesList(List<SelectOptionVO> candidatesList) {
-		this.candidatesList = candidatesList;
-	}
 
 	public ICandidateSearchService getCandidateSearchService() {
 		return candidateSearchService;
@@ -56,22 +45,18 @@ public class AjaxSearchAction implements ServletRequestAware{
 	}
 	
 	public List<String> getNamesList() {
-		System.out.println("In getnameslist");
 		return namesList;
 	}
 
 	public void setNamesList(List<String> namesList) {
-		System.out.println("In setnameslist");
 		this.namesList = namesList;
 	}
 
 	public String getTask() {
-		System.out.println("In getTask");
 		return task;
 	}
 
 	public void setTask(String task) {
-		System.out.println("In setTask");
 		this.task = task;
 	}
 	
@@ -80,7 +65,6 @@ public class AjaxSearchAction implements ServletRequestAware{
 	public String execute() 
 	{
 		session = request.getSession();
-		System.out.println("In execute method + AjaxSearchAction");				
 		String param=null;
 		
 		param=getTask();
@@ -97,29 +81,20 @@ public class AjaxSearchAction implements ServletRequestAware{
 		
 		if(jObj.getString("searchCriteria").equalsIgnoreCase("Candidate"))
 		{
-			System.out.println("Search criteria = candidate");			
 			List<SelectOptionVO> candidateNamesAndIds = candidateSearchService.getCandidateNamesAndIds();
 			setNamesList(getNamesFromSelectOptionVos(candidateNamesAndIds));	
-			setCandidatesList(candidateNamesAndIds);
 			session.setAttribute("candidateNamesAndIds", candidateNamesAndIds);
 		}
 		else if (jObj.getString("searchCriteria").equalsIgnoreCase("Constituency"))
 		{
-				System.out.println("Search criteria = Constituency");
 				List<SelectOptionVO> constituencyNamesAndIds = constituencySearchService.getConstituencyNamesAndIds();
 				setNamesList(getNamesFromSelectOptionVos(constituencyNamesAndIds));					
 		}	
 		
-		System.out.println("param = "+param);	
 		 return Action.SUCCESS;
 	}
 	
-	  public String getJSON(){
-		  System.out.println("In getjson");
-	    	return execute();
-	    }
-	  
-	  private List<String> getNamesFromSelectOptionVos(List<SelectOptionVO> namesAndIds){
+	private List<String> getNamesFromSelectOptionVos(List<SelectOptionVO> namesAndIds){
 			List<String> names = new ArrayList<String>();
 			for(SelectOptionVO nameAndId:namesAndIds)
 				names.add(nameAndId.getName());
