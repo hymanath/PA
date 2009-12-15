1c38afa17ae3d461d718d9c3bf40fb00aea0a9d5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-15 12:21:30 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CrossVotingConsolidateVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CrossVotingConsolidateVO.java
@@ -7,6 +7,12 @@ public class CrossVotingConsolidateVO {
 	private CrossVotedCandidateVO acCandidateData;
 	private CrossVotedCandidateVO pcCandidateData;
 	private List<CrossVotedMandalVO> mandals;
+	private Long totalVotersInAC;
+	private Long totalVotersInPC;
+	private Long totalACPolledVotesInConstituency;
+	private Long totalPCPolledVotesInConstituency;
+	private String differenceInACAndPC;
+	private String impactOfAssemblyOnParliament;
 	boolean partyPartisipated = true;
 	boolean hasAlliance = true;
 	
@@ -16,10 +22,22 @@ public class CrossVotingConsolidateVO {
 
 	public CrossVotingConsolidateVO(CrossVotedCandidateVO acCandidateData,
 			CrossVotedCandidateVO pcCandidateData,
-			List<CrossVotedMandalVO> mandals) {
+			List<CrossVotedMandalVO> mandals, Long totalVotersInAC,
+			Long totalVotersInPC, Long totalACPolledVotesInConstituency,
+			Long totalPCPolledVotesInConstituency, String differenceInACAndPC,
+			String impactOfAssemblyOnParliament, boolean partyPartisipated,
+			boolean hasAlliance) {
 		this.acCandidateData = acCandidateData;
 		this.pcCandidateData = pcCandidateData;
 		this.mandals = mandals;
+		this.totalVotersInAC = totalVotersInAC;
+		this.totalVotersInPC = totalVotersInPC;
+		this.totalACPolledVotesInConstituency = totalACPolledVotesInConstituency;
+		this.totalPCPolledVotesInConstituency = totalPCPolledVotesInConstituency;
+		this.differenceInACAndPC = differenceInACAndPC;
+		this.impactOfAssemblyOnParliament = impactOfAssemblyOnParliament;
+		this.partyPartisipated = partyPartisipated;
+		this.hasAlliance = hasAlliance;
 	}
 
 	public CrossVotedCandidateVO getAcCandidateData() {
@@ -46,7 +64,49 @@ public class CrossVotingConsolidateVO {
 		this.mandals = mandals;
 	}
 
-	public boolean getPartyPartisipated() {
+	public Long getTotalVotersInAC() {
+		return totalVotersInAC;
+	}
+
+	public void setTotalVotersInAC(Long totalVotersInAC) {
+		this.totalVotersInAC = totalVotersInAC;
+	}
+
+	public Long getTotalVotersInPC() {
+		return totalVotersInPC;
+	}
+
+	public void setTotalVotersInPC(Long totalVotersInPC) {
+		this.totalVotersInPC = totalVotersInPC;
+	}
+
+	public Long getTotalACPolledVotesInConstituency() {
+		return totalACPolledVotesInConstituency;
+	}
+
+	public void setTotalACPolledVotesInConstituency(
+			Long totalACPolledVotesInConstituency) {
+		this.totalACPolledVotesInConstituency = totalACPolledVotesInConstituency;
+	}
+
+	public Long getTotalPCPolledVotesInConstituency() {
+		return totalPCPolledVotesInConstituency;
+	}
+
+	public void setTotalPCPolledVotesInConstituency(
+			Long totalPCPolledVotesInConstituency) {
+		this.totalPCPolledVotesInConstituency = totalPCPolledVotesInConstituency;
+	}
+
+	public String getImpactOfAssemblyOnParliament() {
+		return impactOfAssemblyOnParliament;
+	}
+
+	public void setImpactOfAssemblyOnParliament(String impactOfAssemblyOnParliament) {
+		this.impactOfAssemblyOnParliament = impactOfAssemblyOnParliament;
+	}
+
+	public boolean isPartyPartisipated() {
 		return partyPartisipated;
 	}
 
@@ -54,12 +114,21 @@ public class CrossVotingConsolidateVO {
 		this.partyPartisipated = partyPartisipated;
 	}
 
-	public boolean getHasAlliance() {
+	public boolean isHasAlliance() {
 		return hasAlliance;
 	}
 
 	public void setHasAlliance(boolean hasAlliance) {
 		this.hasAlliance = hasAlliance;
 	}
+
+	public String getDifferenceInACAndPC() {
+		return differenceInACAndPC;
+	}
+
+	public void setDifferenceInACAndPC(String differenceInACAndPC) {
+		this.differenceInACAndPC = differenceInACAndPC;
+	}
+	
 	
 }
