3c52d5f86c5bae6a823af2cb97469660fad7922e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-16 05:58:26 +0000

    Update for Party Important Dates -Raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreManagementVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreManagementVO.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public class CadreManagementVO extends ResultStatus{
 	private List<UserEventVO>  UserEvents;
-
+	List<PartyImportantDatesVO> userImpDates;
+	
 	public List<UserEventVO> getUserEvents() {
 		return UserEvents;
 	}
 
 	public void setUserEvents(List<UserEventVO> userEvents) {
 		UserEvents = userEvents;
+	}
+
+	public List<PartyImportantDatesVO> getUserImpDates() {
+		return userImpDates;
+	}
+
+	public void setUserImpDates(List<PartyImportantDatesVO> userImpDates) {
+		this.userImpDates = userImpDates;
 	}
 	
 	
