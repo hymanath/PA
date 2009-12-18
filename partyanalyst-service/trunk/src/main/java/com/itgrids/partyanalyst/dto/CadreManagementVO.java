b0e88c5a730fc830a34cf20d5fe156aba6a95d56
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-18 16:17:12 +0000

    cadre Management VO -Mohan

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreManagementVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreManagementVO.java
@@ -3,15 +3,17 @@ package com.itgrids.partyanalyst.dto;
 import java.util.List;
 
 public class CadreManagementVO extends ResultStatus{
-	private List<UserEventVO>  UserEvents;
-	List<ImportantDatesVO> userImpDates;
+	private List<UserEventVO>  userEvents;
+	private List<ImportantDatesVO> userImpDates;
 	
+	
+
 	public List<UserEventVO> getUserEvents() {
-		return UserEvents;
+		return userEvents;
 	}
 
 	public void setUserEvents(List<UserEventVO> userEvents) {
-		UserEvents = userEvents;
+		this.userEvents = userEvents;
 	}
 
 	public List<ImportantDatesVO> getUserImpDates() {
