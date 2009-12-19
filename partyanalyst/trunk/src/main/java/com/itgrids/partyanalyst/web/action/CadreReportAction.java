dfae59b2863ef15fd8a2bdffa14636a3de4945ae
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-19 11:13:24 +0000

    Action for cadre management - Mohan

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CadreReportAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CadreReportAction.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,8 +12,11 @@ import org.apache.struts2.util.ServletContextAware;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.itgrids.partyanalyst.dto.CadreManagementVO;
+import com.itgrids.partyanalyst.dto.ImportantDatesVO;
 import com.itgrids.partyanalyst.dto.UserCadresInfoVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.UserEventVO;
 import com.itgrids.partyanalyst.service.impl.CadreManagementService;
 
 public class CadreReportAction extends ActionSupport implements ServletContextAware,ServletRequestAware{
@@ -20,6 +26,13 @@ public class CadreReportAction extends ActionSupport implements ServletContextAw
 	private UserCadresInfoVO userCadresInfoVO = new UserCadresInfoVO();
 	private HttpServletRequest request;
 	private HttpSession session;
+	
+	private List<UserEventVO> userEventList = new ArrayList<UserEventVO>();
+	private List<ImportantDatesVO> ImpDatesList = new ArrayList<ImportantDatesVO>();
+	private CadreManagementVO cadreManagementVO = new CadreManagementVO();
+	
+	
+	
 	public void setCadreManagementService(
 			CadreManagementService cadreManagementService) {
 		this.cadreManagementService = cadreManagementService;
@@ -34,15 +47,26 @@ public class CadreReportAction extends ActionSupport implements ServletContextAw
 		this.userCadresInfoVO = userCadresInfoVO;
 	}
 
+	public CadreManagementVO getCadreManagementVO() {
+		return cadreManagementVO;
+	}
+
+	public void setCadreManagementVO(CadreManagementVO cadreManagementVO) {
+		this.cadreManagementVO = cadreManagementVO;
+	}
+
 	
 	public String execute() throws Exception{
 		session=request.getSession();
 		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
 		if(user==null)
 			return ERROR;
-		if(user==null){
+		if(user==null)
+		{
 			userCadresInfoVO = new UserCadresInfoVO();
-		}else{
+		}
+		else
+		{
 			userCadresInfoVO.setUserID(user.getRegistrationID()); 
 			userCadresInfoVO.setUserAccessType(user.getAccessType());
 			userCadresInfoVO.setUserAccessValue(user.getAccessValue());
@@ -64,6 +88,44 @@ public class CadreReportAction extends ActionSupport implements ServletContextAw
 				userCadresInfoVO.setUserAccessDisplayValue(mandalName);
 			}
 			userCadresInfoVO = cadreManagementService.getUserCadresInfo(userCadresInfoVO);
+			
+			/*
+			 * User Event VO Mock Data
+			 */
+			UserEventVO userEventVO = new UserEventVO();			
+			userEventVO.setTitle("Party Meeting");
+			userEventVO.setStartDate("20/12/2009 10:15:00");
+			userEventVO.setEndDate("22/12/2009 04:15:00");
+			userEventVO.setDescription("Party Meeting on 20-Dec");
+			
+			UserEventVO userEventVO1 = new UserEventVO();
+			userEventVO1.setTitle("New Event");
+			userEventVO1.setStartDate("24/12/2009 10:15:00");
+			userEventVO1.setEndDate("25/12/2009 04:15:00");
+			userEventVO1.setDescription("New Event on 24-Dec");
+			
+			userEventList.add(userEventVO);
+			userEventList.add(userEventVO1);
+			
+			ImportantDatesVO importantDatesVO = new ImportantDatesVO();
+			importantDatesVO.setTitle("Party Formation Day");
+			importantDatesVO.setStartDate("28/12/2009");
+			importantDatesVO.setImportance("Party formed");
+			importantDatesVO.setEventType("Yearly");
+			
+			ImportantDatesVO importantDatesVO1 = new ImportantDatesVO();
+			importantDatesVO1.setTitle("Founder Birthday");
+			importantDatesVO1.setStartDate("12/01/2010");
+			importantDatesVO1.setImportance("Birthday ");
+			importantDatesVO1.setEventType("Yearly");
+			
+			ImpDatesList.add(importantDatesVO);
+			ImpDatesList.add(importantDatesVO1);		
+						
+			cadreManagementVO.setUserImpDates(ImpDatesList);
+			cadreManagementVO.setUserEvents(userEventList);	
+			
+			this.setCadreManagementVO(cadreManagementVO);
 		}
 		
 		session.setAttribute("USERCADRESINFOVO", userCadresInfoVO);
