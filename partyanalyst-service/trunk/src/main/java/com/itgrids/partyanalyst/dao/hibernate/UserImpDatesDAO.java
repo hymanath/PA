61a6d1361a31635f37c2800eb058674588a3146b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-22 10:19:22 +0000

    updated files related to User Calender - Narender

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserImpDatesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserImpDatesDAO.java
@@ -1,8 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -22,43 +20,11 @@ public class UserImpDatesDAO extends GenericDaoHibernate<UserImpDate, Long> impl
 
 	public List<UserImpDate> findByUsedrId(Long userID) {
 		//jan-0, feb-1, mar-2.... dec-11
-		try{
-
-			Calendar calendar = Calendar.getInstance();
-			int currentMonth = calendar.get(Calendar.MONTH) + 1;
+		Calendar calendar = Calendar.getInstance();
+		int currentMonth = calendar.get(Calendar.MONTH) + 1;
 			
-			return getHibernateTemplate().find(" from UserImpDate model where model.user.registrationId=? and sysdate() <= model.tillDate and " +
+		return getHibernateTemplate().find(" from UserImpDate model where model.user.registrationId=? and sysdate() <= model.tillDate and " +
 					"Month(model.effectiveDate) - " + currentMonth + " in (0,1)" , userID);
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-		return null;
-	}
-	public static void main(String[] sr) throws Exception{
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		Date d = new Date("12/12/2009 "+20 +":13:00");System.out.println(sdf.format(d));
-		Calendar cal = Calendar.getInstance();/*cal.add(Calendar.DATE, 2);*/cal.set(Calendar.YEAR, 2010);
-		System.out.println(cal.get(Calendar.DAY_OF_WEEK));
-		System.out.println(cal.getTime());
-		Calendar cal1 = Calendar.getInstance(); cal1.add(Calendar.MONTH, 2);
-		Date date = new Date();  
-		Date startDate = date;
-		int month = startDate.getMonth();
-		int day = startDate.getDate();
-		int hrs = startDate.getHours();
-		int mins = startDate.getMinutes();
-		int year =Calendar.getInstance().get(Calendar.YEAR);
-		//Calendar cal = Calendar.getInstance();
-		cal.set(2000,13,32);
-		System.out.println("Narender");
-		//date.setYear(1);
-		/*
-		System.out.println(date);
-		cal.add(Calendar.DATE, 30);
-		System.out.println(sdf.format(cal.getTime()));
-
-		Date d = sdf.parse("24-12-2003 12:30:44");
-		System.out.println(sdf.format(d));*/
+		
 	}
 }
