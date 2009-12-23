bd5c8302a420df2b8634a749bce4e10e7c5eff79
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-23 13:05:03 +0000

    VO's for Constituency Management

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ConstituencyManagementVO.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.dto;
+
+public class ConstituencyManagementVO {
+
+	private ProblemManagementVO problemManagementVO;
+
+	public ProblemManagementVO getProblemManagementVO() {
+		return problemManagementVO;
+	}
+
+	public void setProblemManagementVO(ProblemManagementVO problemManagementVO) {
+		this.problemManagementVO = problemManagementVO;
+	}
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ProblemDetailsVO.java
@@ -0,0 +1,49 @@
+package com.itgrids.partyanalyst.dto;
+
+public class ProblemDetailsVO {
+
+	private Long problemID;
+	private String definition;
+	private String description;
+	private String identifiedDate;
+	private String Location;	
+	private String source;
+	
+	public Long getProblemID() {
+		return problemID;
+	}
+	public void setProblemID(Long problemID) {
+		this.problemID = problemID;
+	}
+	public String getDefinition() {
+		return definition;
+	}
+	public void setDefinition(String definition) {
+		this.definition = definition;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getIdentifiedDate() {
+		return identifiedDate;
+	}
+	public void setIdentifiedDate(String identifiedDate) {
+		this.identifiedDate = identifiedDate;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+}
