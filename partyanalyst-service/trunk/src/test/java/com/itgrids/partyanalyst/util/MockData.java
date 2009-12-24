fe6539a2ed1d2f6b4b61dea2de80dae6a4d19fc3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-24 06:23:44 +0000

    updated files related to User Calender - Narender

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/util/MockData.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/util/MockData.java
@@ -1986,8 +1986,8 @@ public class MockData {
 		userPlannedEvents.setDescription("Helping poor of Hyderabad city.....");
 		userPlannedEvents.setLocationType("YAKUTPURA");
 		userPlannedEvents.setLocationId(1L);
-		userPlannedEvents.setStartDate(Calendar.getInstance().getTime().toString());
-		userPlannedEvents.setEndDate(Calendar.getInstance().getTime().toString());
+		userPlannedEvents.setStartDate(Calendar.getInstance().getTime());
+		userPlannedEvents.setEndDate(Calendar.getInstance().getTime());
 		List <SelectOptionVO> organizers = new ArrayList<SelectOptionVO>();
 		SelectOptionVO obj = new SelectOptionVO(1L, "");
 		organizers.add(obj);
