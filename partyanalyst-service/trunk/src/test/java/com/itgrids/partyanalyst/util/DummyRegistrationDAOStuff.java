5c33a915640444c22207f29a6adfaa08ad34e0d5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2009-12-28 12:26:36 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/util/DummyRegistrationDAOStuff.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/util/DummyRegistrationDAOStuff.java
@@ -10,10 +10,10 @@ public class DummyRegistrationDAOStuff {
 
 	public static List<Registration> getRegistrations(){
 			registrations = new ArrayList<Registration>();
-			Registration reg1 = new Registration("aaa","middleName","lastName","itgrids1","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","");
-			Registration reg2 = new Registration("aaa1","middleName","lastName1","itgrids2","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","");
-			Registration reg3 = new Registration("aaa2","middleName","lastName2","itgrids3","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","");
-			Registration reg4 = new Registration("aaa3","middleName","lastName3","itgrids4","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","");
+			Registration reg1 = new Registration("aaa","middleName","lastName","itgrids1","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","", null);
+			Registration reg2 = new Registration("aaa1","middleName","lastName1","itgrids2","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","", null);
+			Registration reg3 = new Registration("aaa2","middleName","lastName2","itgrids3","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","", null);
+			Registration reg4 = new Registration("aaa3","middleName","lastName3","itgrids4","password",java.sql.Date.valueOf("2009-08-06"),"email@gmail.com","99655652","5846526","address","gender","India","517325","","", null);
 			
 			registrations.add(reg1);
 			registrations.add(reg2);
