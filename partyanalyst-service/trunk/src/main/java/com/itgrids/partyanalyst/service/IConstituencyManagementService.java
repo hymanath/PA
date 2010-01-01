f9ff94e618392a7550909cc8499ca0c52601657d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-01 13:43:43 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyManagementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyManagementService.java
@@ -1,5 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.excel.booth.VoterVO;
+
 public interface IConstituencyManagementService {
+	
+	public List<VoterVO> createProblem(Long hamletId, String year);
 	
 }
