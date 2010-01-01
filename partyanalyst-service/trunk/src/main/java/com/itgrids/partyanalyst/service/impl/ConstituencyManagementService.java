d64b30597bb9e0209eaf8275a170d0da10eca177
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-01 13:44:16 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ConstituencyManagementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ConstituencyManagementService.java
@@ -1,15 +1,31 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.itgrids.partyanalyst.dao.IBoothConstituencyElectionVoterDAO;
 import com.itgrids.partyanalyst.dao.IProblemHistoryDAO;
 import com.itgrids.partyanalyst.dao.IProblemLocationDAO;
+import com.itgrids.partyanalyst.excel.booth.VoterVO;
+import com.itgrids.partyanalyst.model.Voter;
 import com.itgrids.partyanalyst.service.IConstituencyManagementService;
 
 public class ConstituencyManagementService implements IConstituencyManagementService{
 	
-	private IProblemLocationDAO problemLocationDAO;
-	private IProblemHistoryDAO problemHistoryDAO;
+	/*private IProblemLocationDAO problemLocationDAO;
+	private IProblemHistoryDAO problemHistoryDAO;*/
+	private IBoothConstituencyElectionVoterDAO boothConstituencyElectionVoterDAO;
 	
-	public IProblemLocationDAO getProblemLocationDAO() {
+	public IBoothConstituencyElectionVoterDAO getBoothConstituencyElectionVoterDAO() {
+		return boothConstituencyElectionVoterDAO;
+	}
+
+	public void setBoothConstituencyElectionVoterDAO(
+			IBoothConstituencyElectionVoterDAO boothConstituencyElectionVoterDAO) {
+		this.boothConstituencyElectionVoterDAO = boothConstituencyElectionVoterDAO;
+	}
+
+	/*public IProblemLocationDAO getProblemLocationDAO() {
 		return problemLocationDAO;
 	}
 	
@@ -23,10 +39,26 @@ public class ConstituencyManagementService implements IConstituencyManagementSer
 	
 	public void setProblemHistoryDAO(IProblemHistoryDAO problemHistoryDAO) {
 		this.problemHistoryDAO = problemHistoryDAO;
-	}
+	}*/
 	
-	public void createProblem(){
+	public List<VoterVO> createProblem(Long hamletId, String year){
+		List<Voter> voters = boothConstituencyElectionVoterDAO.findVotersByHamletAndElectiuonYear(hamletId, year);
+		List<VoterVO> voterVOs = new ArrayList<VoterVO>();
+		VoterVO voterVO = null;
+		for(Voter voter:voters){
+			voterVO = new VoterVO();
+			voterVO.setVoterFirstName(voter.getFirstName()+ voter.getLastName());
+			voterVO.setHouseNo(voter.getHouseNo());
+			voterVO.setAge(voter.getAge());
+			voterVO.setCast(voter.getCast());
+			voterVO.setCastCatagery(voter.getCastCatagery()+" "+voter.getCastSubCatagery());
+			voterVO.setGender(voter.getGender());
+			voterVO.setRelativeFirstName(voter.getRelativeFirstName()+" "+voter.getRelativeLastName());
+			voterVO.setRelationshipType(voter.getRelationshipType());
+			voterVOs.add(voterVO);
+		}
 		
+		return voterVOs;
 	}
 	
 
