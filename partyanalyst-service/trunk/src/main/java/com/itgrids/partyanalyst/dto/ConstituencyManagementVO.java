086bb3216b8f2b9dd85f7fc5ff0bed184951d26d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-06 12:20:55 +0000

    modified By Raghu Gadam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ConstituencyManagementVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ConstituencyManagementVO.java
@@ -1,8 +1,16 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.excel.booth.VoterVO;
+
 public class ConstituencyManagementVO {
 
 	private ProblemManagementVO problemManagementVO;
+	private List<VoterVO> voterDetails;
+	private VoterCastInfoVO VoterCastInfodetails;
+	private List<VoterHouseInfoVO> votersByHouseNos;
+	private List<LocalLeadersVO> localLeaders;
 
 	public ProblemManagementVO getProblemManagementVO() {
 		return problemManagementVO;
@@ -11,4 +19,40 @@ public class ConstituencyManagementVO {
 	public void setProblemManagementVO(ProblemManagementVO problemManagementVO) {
 		this.problemManagementVO = problemManagementVO;
 	}
+	
+	public List<VoterVO> getVoterDetails() {
+		return voterDetails;
+	}
+
+	public void setVoterDetails(List<VoterVO> voterDetails) {
+		this.voterDetails = voterDetails;
+	}
+
+	public VoterCastInfoVO getVoterCastInfodetails() {
+		return VoterCastInfodetails;
+	}
+	
+	public void setVoterCastInfodetails(VoterCastInfoVO voterCastInfodetails) {
+		VoterCastInfodetails = voterCastInfodetails;
+	}
+
+	public List<VoterHouseInfoVO> getVotersByHouseNos() {
+		return votersByHouseNos;
+	}
+
+	public void setVotersByHouseNos(List<VoterHouseInfoVO> votersByHouseNos) {
+		this.votersByHouseNos = votersByHouseNos;
+	}
+
+	public List<LocalLeadersVO> getLocalLeaders() {
+		return localLeaders;
+	}
+
+	public void setLocalLeaders(List<LocalLeadersVO> localLeaders) {
+		this.localLeaders = localLeaders;
+	}
+	
+	
+
+	
 }
