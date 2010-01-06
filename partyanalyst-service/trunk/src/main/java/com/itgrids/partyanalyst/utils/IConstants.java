2332ab4f2b0b409a01a3deecd0ab81c8ce7cf9b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-06 11:34:06 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -9,4 +9,5 @@ public interface IConstants {
 	public static final String DATE_TIME_PATTERN = "dd/MM/yyyy HH:mm:ss";
 	public static final String HAMLET_TYPE = "1";
 	public static final String TOWNSHIP_TYPE = "2";
+	public static final String INDIPENDENT = "IND";
 }
