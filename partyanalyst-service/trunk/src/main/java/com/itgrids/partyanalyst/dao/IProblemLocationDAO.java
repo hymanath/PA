d5c3c8ebda10cd1f607403b1d67740ecd9eaad4c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-06 11:55:06 +0000

    Modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLocationDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ProblemLocation;
 
 public interface IProblemLocationDAO extends GenericDao<ProblemLocation, Long>{
 
+	public List<ProblemLocation> findByHamletId(Long hamletId);
+	
 }
