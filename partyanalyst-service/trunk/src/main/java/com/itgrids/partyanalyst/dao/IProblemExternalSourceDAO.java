f0116673b6ba9d5c4e8059b0ca8e581c4730041b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-08 16:32:54 +0000

    IProblemExternalSourceDAO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemExternalSourceDAO.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (c) 2009 IT Grids India LTD.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on January 2, 2010
+ */
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ProblemExternalSource;
+
+public interface IProblemExternalSourceDAO extends GenericDao<ProblemExternalSource, Long> {
+
+	public List<ProblemExternalSource> findByProblemExternalSourceName(String name);
+}
