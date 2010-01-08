68cdced87717d52b80c697ab9423f2b76806a94d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-08 17:26:06 +0000

    modified By Raghu Gadam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PoliticalChangesVO.java
@@ -0,0 +1,30 @@
+package com.itgrids.partyanalyst.dto;
+
+
+
+public class PoliticalChangesVO {
+
+	private String description;
+	private String date;
+	private String impact;
+
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+		public String getImpact() {
+			return impact;
+		}
+		public void setImpact(String impact) {
+			this.impact = impact;
+		}
+ 
+ 	}
