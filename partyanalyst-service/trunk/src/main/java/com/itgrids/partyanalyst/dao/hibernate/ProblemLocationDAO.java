e4aa0c1128c3763ad9b7dbe3741b1b023e342538
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-10 12:14:30 +0000

    ProblemLocationDAO - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemLocationDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IProblemLocationDAO;
+import com.itgrids.partyanalyst.model.Problem;
 import com.itgrids.partyanalyst.model.ProblemLocation;
 
 public class ProblemLocationDAO extends GenericDaoHibernate<ProblemLocation, Long> implements IProblemLocationDAO{
@@ -18,4 +19,21 @@ public class ProblemLocationDAO extends GenericDaoHibernate<ProblemLocation, Lon
 		return getHibernateTemplate().find("from ProblemLocation model where model.hamlet.hamletId = ?", hamletId);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<Problem> findByHamletIdAndYear(Long hamletId,String year){
+		Object[] params ={hamletId,year};
+		return getHibernateTemplate().find("select model.problemSource.problem  from ProblemLocation model where model.hamlet.hamletId = ? and model.problemSource.problem.year = ?",params);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List findByHamletandYear(Long hamletId,String year){
+		Object[] params ={hamletId,year};
+		return getHibernateTemplate().find("select model.problemSource.problem,model.problemSource,model.problemSource.problemExternalSource  from ProblemLocation model where model.hamlet.hamletId = ? and model.problemSource.problem.year = ?",params);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ProblemLocation> findByHamletIdandYear(Long hamletId,String year){
+		Object[] params ={hamletId,year};
+		return getHibernateTemplate().find("from ProblemLocation model where model.hamlet.hamletId = ? and model.problemSource.problem.year = ?",params);
+	}
 }
