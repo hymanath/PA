f40d9b2f8f35adbeb3b179f89252c59d48642a72
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-10 12:14:04 +0000

    IProblemLocationDAO - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLocationDAO.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.model.Problem;
 import com.itgrids.partyanalyst.model.ProblemLocation;
 
 public interface IProblemLocationDAO extends GenericDao<ProblemLocation, Long>{
 
 	public List<ProblemLocation> findByHamletId(Long hamletId);
+	
+	public List<Problem> findByHamletIdAndYear(Long hamletId,String year);
+	
+	@SuppressWarnings("unchecked")
+	public List findByHamletandYear(Long hamletId,String year);
+	
+	public List<ProblemLocation> findByHamletIdandYear(Long hamletId,String year);
 	
 }
