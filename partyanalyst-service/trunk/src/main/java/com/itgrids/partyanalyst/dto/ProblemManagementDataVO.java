cc19adad16839f920b752b35fa1d87cb1b6f6759
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-23 09:42:50 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ProblemManagementDataVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ProblemManagementDataVO.java
@@ -13,7 +13,15 @@ public class ProblemManagementDataVO {
 
 	private List<HamletProblemVO> hamletProblems;
 	private ResultStatus resultStatus;
+	private List<ProblemBeanVO> newProblems;
+	private ProblemBeanVO problemBeanVO;
 	
+	public ProblemBeanVO getProblemBeanVO() {
+		return problemBeanVO;
+	}
+	public void setProblemBeanVO(ProblemBeanVO problemBeanVO) {
+		this.problemBeanVO = problemBeanVO;
+	}
 	public List<HamletProblemVO> getHamletProblems() {
 		return hamletProblems;
 	}
@@ -26,4 +34,11 @@ public class ProblemManagementDataVO {
 	public void setResultStatus(ResultStatus resultStatus) {
 		this.resultStatus = resultStatus;
 	}
+	public List<ProblemBeanVO> getNewProblems() {
+		return newProblems;
+	}
+	public void setNewProblems(List<ProblemBeanVO> newProblems) {
+		this.newProblems = newProblems;
+	}
+	
 }
