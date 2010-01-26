3a5b7c7d3cab4b54b6421682a29ae8086b7edbd9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-26 10:50:04 +0000

    Created by raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IEPaperService.java
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on January 20,2010
+ */
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.EPaperVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.model.Constituency;
+
+public interface IEPaperService {
+	
+	public List<EPaperVO> getEPapers(String accessType , Long accessValue);
+
+	public List<EPaperVO> getEPapersForDistrict(Long districtId);
+	
+	public List<SelectOptionVO> getDistrictsForState(Long stateId);
+	public Long getStateIdFromConstitunecyByAccessValue(Long accessValue);
+	public Long getStateIdFromDistrictByDistrictId(Long districtId);
+}
