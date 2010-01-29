f15055d7771f5d7194cc5529aa7fb1e9e60ed3df
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-29 08:59:55 +0000

    modified by Ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEPaperDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEPaperDAO.java
@@ -13,17 +13,28 @@ import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.District;
 import com.itgrids.partyanalyst.model.EPaper;
+import com.itgrids.partyanalyst.model.EPaperUrl;
 
 public interface IEPaperDAO extends GenericDao<EPaper,Long> {
 	
 	public List<EPaper> findByEpaperId(Long epaperId);
 	
-	public List<EPaper> findByMainUrl(String mainUrl);
+	public List<EPaper> findByName(String name);
 	
-	public List<EPaper> findByEpaperUrl(String epaperUrl);
+	public List<EPaper> findByDescription(String description);
 	
-	public List findEPapersForDistrictByDistrictId(Long districtId);
+	public List<EPaper> findByClassification(String classification);
 	
-	public List findMainEPapersForStateByStateId(Long districtId);
+	public List<EPaper> findByStateId(Long stateId);
+	
+	public List<EPaper> findByCountryId(Long countryId);
+	
+	public List<EPaper> findByStateUrl(String stateUrl);
+	
+	public List<EPaper> findBycountryUrl(String countryUrl);
+	
+	public List<EPaper> findByLanguage(String language);
+	
+	public List<EPaper> findByImage(String image);
 
 }
