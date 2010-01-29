7a4bdb13ce1726eb024623e32752149c3409e00a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-29 09:24:11 +0000

    modified by Ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/EPaperVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/EPaperVO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dto;
 
-public class EPaperVO {
+public class EPaperVO implements java.io.Serializable{
 	
 	private String paperName;
+	private String description;
+	private String classification;
 	private String epaperUrl;
 	private String mainUrl;
 	private String districtName;
@@ -25,6 +27,40 @@ public class EPaperVO {
 		this.image = image;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public String getClassification() {
+		return classification;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+
+	public String getEpaperUrl() {
+		return epaperUrl;
+	}
+
+	public String getMainUrl() {
+		return mainUrl;
+	}
+
+	public void setEpaperUrl(String epaperUrl) {
+		this.epaperUrl = epaperUrl;
+	}
+
+	public void setMainUrl(String mainUrl) {
+		this.mainUrl = mainUrl;
+	}
+
 	public String getDistrictName() {
 		return districtName;
 	}
@@ -40,20 +76,5 @@ public class EPaperVO {
 	public void setPaperName(String paperName) {
 		this.paperName = paperName;
 	}
-	public String getMainUrl() {
-		return mainUrl;
-	}
-
-	public void setMainUrl(String mainUrl) {
-		this.mainUrl = mainUrl;
-	}
-
-	public String getEpaperUrl() {
-		return epaperUrl;
-	}
-
-	public void setEpaperUrl(String epaperUrl) {
-		this.epaperUrl = epaperUrl;
-	}
-
+	
 }
