203b364889ac9520c9be6452dd32b5f2da6b88b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-29 15:18:28 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupsVO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class UserGroupsVO {
 	private List<UserGroupDetailsVO> groupsCreatedByUser;
+	private List<UserGroupMembersVO> groupsMembers;
 
 	public List<UserGroupDetailsVO> getGroupsCreatedByUser() {
 		return groupsCreatedByUser;
@@ -11,5 +12,13 @@ public class UserGroupsVO {
 
 	public void setGroupsCreatedByUser(List<UserGroupDetailsVO> groupsCreatedByUser) {
 		this.groupsCreatedByUser = groupsCreatedByUser;
+	}
+	public List<UserGroupMembersVO> getGroupsMembers() {
+		return groupsMembers;
+	}
+
+	public void setGroupsMembers(List<UserGroupMembersVO> groupsMembers) {
+		this.groupsMembers = groupsMembers;
 	} 	
+	
 }
