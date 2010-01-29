e0fcaf81632e3a4bdc1f902962b6510b10b6d310
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-01-29 15:18:02 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupMembersVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupMembersVO.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 
 public class UserGroupMembersVO implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String userName;
 	private Long userId;
 	private String address;
 	private String phoneNumber;
 	private String mobileNumber;
+	private String location;
+	private String groupName;
+	private String designation;
+	
 	
 	public String getUserName() {
 		return userName;
@@ -40,6 +48,28 @@ public class UserGroupMembersVO implements Serializable {
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+	
+	
 
 
 }
