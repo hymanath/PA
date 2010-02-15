632add3e11b5189d1229d59b4c0659903672c8b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-15 12:12:35 +0000

    modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ConstituencyPageService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ConstituencyPageService.java
@@ -8,21 +8,35 @@
 package com.itgrids.partyanalyst.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import org.apache.velocity.util.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.util.StringUtils;
 
+import com.itgrids.partyanalyst.dao.IBoothConstituencyElectionDAO;
+import com.itgrids.partyanalyst.dao.IBoothConstituencyElectionVoterDAO;
 import com.itgrids.partyanalyst.dao.IConstituencyDAO;
 import com.itgrids.partyanalyst.dao.IConstituencyElectionResultObjectsDAO;
+import com.itgrids.partyanalyst.dao.IHamletBoothElectionDAO;
+import com.itgrids.partyanalyst.dao.IHamletDAO;
+import com.itgrids.partyanalyst.dao.INominationDAO;
 import com.itgrids.partyanalyst.dto.CandidateOppositionVO;
 import com.itgrids.partyanalyst.dto.CandidateWonVO;
 import com.itgrids.partyanalyst.dto.ConstituencyElectionResultsVO;
 import com.itgrids.partyanalyst.dto.ConstituencyInfoVO;
+import com.itgrids.partyanalyst.dto.HamletAndBoothVO;
+import com.itgrids.partyanalyst.dto.InfluencingPeopleVO;
+import com.itgrids.partyanalyst.dto.LocationWiseBoothDetailsVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.ResultWithExceptionVO;
 import com.itgrids.partyanalyst.model.Candidate;
 import com.itgrids.partyanalyst.model.CandidateResult;
 import com.itgrids.partyanalyst.model.Constituency;
 import com.itgrids.partyanalyst.model.ConstituencyElectionResult;
 import com.itgrids.partyanalyst.model.Election;
+import com.itgrids.partyanalyst.model.HamletBoothElection;
 import com.itgrids.partyanalyst.model.Nomination;
 import com.itgrids.partyanalyst.service.IConstituencyPageService;
 
@@ -32,9 +46,49 @@ public class ConstituencyPageService implements IConstituencyPageService {
 	 * doc
 	 * ConstituencyElectionResults
 	 */
+	private static final Logger log = Logger.getLogger(CrossVotingEstimationService.class);
 	private IConstituencyElectionResultObjectsDAO constituencyElectionResultObjectsDAO;
 	private IConstituencyDAO constituencyDAO;
-	
+	private INominationDAO nominationDAO;
+	private IHamletDAO hamletDAO;
+	private IBoothConstituencyElectionDAO boothConstituencyElectionDAO;
+	private IBoothConstituencyElectionVoterDAO boothConstituencyElectionVoterDAO;
+	private IHamletBoothElectionDAO hamletBoothElectionDAO;
+		
+	public IHamletBoothElectionDAO getHamletBoothElectionDAO() {
+		return hamletBoothElectionDAO;
+	}
+
+	public void setHamletBoothElectionDAO(
+			IHamletBoothElectionDAO hamletBoothElectionDAO) {
+		this.hamletBoothElectionDAO = hamletBoothElectionDAO;
+	}
+
+	public IHamletDAO getHamletDAO() {
+		return hamletDAO;
+	}
+
+	public void setHamletDAO(IHamletDAO hamletDAO) {
+		this.hamletDAO = hamletDAO;
+	}
+
+	public IBoothConstituencyElectionVoterDAO getBoothConstituencyElectionVoterDAO() {
+		return boothConstituencyElectionVoterDAO;
+	}
+
+	public void setBoothConstituencyElectionVoterDAO(
+			IBoothConstituencyElectionVoterDAO boothConstituencyElectionVoterDAO) {
+		this.boothConstituencyElectionVoterDAO = boothConstituencyElectionVoterDAO;
+	}
+
+	public INominationDAO getNominationDAO() {
+		return nominationDAO;
+	}
+
+	public void setNominationDAO(INominationDAO nominationDAO) {
+		this.nominationDAO = nominationDAO;
+	}
+
 	public void setConstituencyElectionResultObjectsDAO(IConstituencyElectionResultObjectsDAO constituencyElectionResultObjectsDAO) {
 		this.constituencyElectionResultObjectsDAO = constituencyElectionResultObjectsDAO;
 	}
@@ -167,5 +221,120 @@ public class ConstituencyPageService implements IConstituencyPageService {
 		else
 			return null;
 	}
-
+	
+	public ResultWithExceptionVO getAllMandalLevelLeaders(Long tehsilId){
+		ResultWithExceptionVO result = new ResultWithExceptionVO();
+		ResultStatus resultStatus = new ResultStatus();
+		List<InfluencingPeopleVO> influencingPeopleVOs = new ArrayList<InfluencingPeopleVO>();
+		InfluencingPeopleVO influencingPeopleVO = null;
+		try{
+			List mandalLeadersInfo = nominationDAO.findLocalLeadersOfMandal(tehsilId);
+			for(int i=0; i<mandalLeadersInfo.size(); i++){
+				influencingPeopleVO = new InfluencingPeopleVO();
+				Object[] values = (Object[])mandalLeadersInfo.get(i);
+				String name = getCandidateFullName((Candidate)values[0]);
+				String designation = (String)values[1];
+				String party = (String)values[2];
+				String year = (String)values[3];
+				String constituencyName = (String)values[4];
+				
+				influencingPeopleVO.setPersonName(name);
+				influencingPeopleVO.setDesignation(designation);
+				influencingPeopleVO.setLocalArea(constituencyName);
+				influencingPeopleVO.setYear(year);
+				influencingPeopleVO.setParty(party);
+				
+				influencingPeopleVOs.add(influencingPeopleVO);
+			}
+		}catch(Exception e){
+			resultStatus.setExceptionEncountered(e);
+			if(log.isDebugEnabled()){
+				log.debug("Exception Raised In getAllMandalLevelLeaders", e);
+			}
+		}
+		result.setFinalResult(influencingPeopleVOs);
+		result.setResultStatus(resultStatus);
+		return result;
+	}
+	
+	public String getCandidateFullName(Candidate candidate){
+		String name = " ";
+		if(StringUtils.hasText(candidate.getFirstname())){
+			name = name + candidate.getFirstname() + " ";
+		}
+		if(StringUtils.hasText(candidate.getMiddlename())){
+			name = name + candidate.getMiddlename() + " ";
+		}
+		if(StringUtils.hasText(candidate.getLastname())){
+			name = name + candidate.getLastname() + " ";
+		}
+		return StringUtils.trimWhitespace(name);
+	}
+	
+	public ResultWithExceptionVO getTownshipWiseBoothDetailsForTehsil(Long tehsilId){
+		ResultStatus resultStatus = new ResultStatus();
+		List<LocationWiseBoothDetailsVO> boothDataFromDB = new ArrayList<LocationWiseBoothDetailsVO>();
+		LocationWiseBoothDetailsVO locationWiseBoothDetailsVO = null;
+		Set<String> hamlets = null;
+		Set<String> boothNos = null;
+		String townshipName = "";
+		Long townshipId = null;
+		Long totalVoters = new Long(0);
+		
+		try{
+			List boothDetails = boothConstituencyElectionVoterDAO.findTownshipWiseBoothDetailsForTehsil(tehsilId);
+			
+			for(int i=0; i<boothDetails.size(); i++){
+				locationWiseBoothDetailsVO = new LocationWiseBoothDetailsVO();
+				Object[] values = (Object[])boothDetails.get(i);
+				
+				if((townshipName).equals(values[0])){
+					if(boothNos.add((String)values[1]))
+						totalVoters = totalVoters + (Long)values[2];
+					hamlets.add((String)values[4]);
+				}else{
+					locationWiseBoothDetailsVO.setLocationName(townshipName);
+					locationWiseBoothDetailsVO.setBooths(boothNos);
+					locationWiseBoothDetailsVO.setPopulation(totalVoters);
+					locationWiseBoothDetailsVO.setSubLocations(hamlets);
+					locationWiseBoothDetailsVO.setHamletsOfTownship(hamletDAO.findHamletNamesByTownshipId(townshipId));
+					boothDataFromDB.add(locationWiseBoothDetailsVO);
+					
+					hamlets = new HashSet<String>(0);
+					boothNos = new HashSet<String>(0);
+					townshipName = (String)values[0];
+					boothNos.add((String)values[1]);
+					totalVoters = (Long)values[2];
+					hamlets.add((String)values[4]);
+					townshipId = (Long)values[5];
+				}
+				
+				if(i == boothDetails.size()-1){
+					locationWiseBoothDetailsVO.setLocationName(townshipName);
+					locationWiseBoothDetailsVO.setBooths(boothNos);
+					locationWiseBoothDetailsVO.setPopulation(totalVoters);
+					locationWiseBoothDetailsVO.setSubLocations(hamlets);
+					locationWiseBoothDetailsVO.setHamletsOfTownship(hamletDAO.findHamletNamesByTownshipId(townshipId));
+					boothDataFromDB.add(locationWiseBoothDetailsVO);
+				}
+					
+			}
+			boothDataFromDB.remove(0);
+		}catch(Exception e){
+			resultStatus.setExceptionEncountered(e);
+			if(log.isDebugEnabled()){
+				log.debug("Exception Occured Due To ", e);
+			}
+		}		
+		return new ResultWithExceptionVO(boothDataFromDB, resultStatus);
+	}
+	
+	public void saveAndUpdateHamletAndBoothInfo(List<HamletAndBoothVO> hamletBooths){
+		for(HamletAndBoothVO hamletAndBooth:hamletBooths){
+			for(Long boothConstituencyElectionId:hamletAndBooth.getBoothConstituencyElectionIds())
+				hamletBoothElectionDAO.save(new HamletBoothElection(boothConstituencyElectionDAO.get(boothConstituencyElectionId), hamletDAO.get(hamletAndBooth.getHamletId())));
+		}
+	}
+	
+	
 }
