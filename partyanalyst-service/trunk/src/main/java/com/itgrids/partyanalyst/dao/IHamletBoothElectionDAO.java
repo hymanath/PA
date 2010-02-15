92b89659fa1ee6332ed573572c543d3b834b17fe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-15 11:35:26 +0000

    modified By Sivakumar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IHamletBoothElectionDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.HamletBoothElection;
+
+public interface IHamletBoothElectionDAO extends GenericDao<HamletBoothElection, Long>{
+
+	
+	
+}
