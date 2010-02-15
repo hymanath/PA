47a1e1749af3f794be95b3ef8355fed4478b25fd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-15 07:59:40 +0000

    modified By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IProblemManagementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IProblemManagementService.java
@@ -11,11 +11,26 @@ import java.util.List;
 
 import com.itgrids.partyanalyst.dto.ProblemBeanVO;
 import com.itgrids.partyanalyst.dto.ProblemManagementDataVO;
-import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.ProblemsOfUserVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface IProblemManagementService {
 
+	public List<SelectOptionVO> getAllTypesOfProblemSources();
 	public ProblemManagementDataVO getProblemsForAHamlet(Long hamletId,String year);
-	public ProblemBeanVO saveProblemData(ProblemBeanVO problemBeanVO);
-	public List<ProblemBeanVO> getProblemsForUser(Long registrationId);
+	public ProblemBeanVO saveNewProblemData(ProblemBeanVO problemBeanVOToSave);
+	public ProblemsOfUserVO getNewProblemsForUser(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> getClassifiedProblemsOfUser(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> updateAndGetClassifiedProblemDataIntoDB(List<ProblemBeanVO> problemsToUpdate);
+	public List<ProblemBeanVO> updateAndGetAssignedProblems(List<ProblemBeanVO> problemsToAssign);
+	public List<ProblemBeanVO> updateAndGetProblemsUnderProgress(List<ProblemBeanVO> problemsInProgress);
+	public List<SelectOptionVO> getDepartmentsForProblemScope(String problemScope);
+	public List<ProblemBeanVO> getAssignedProblems(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> getProblemsUnderProgress(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> getPendingProblemsForAnUser(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> updateAndGetProblemsUnderFixed(List<ProblemBeanVO> problemsFixed);
+	public List<ProblemBeanVO> getFixedProblemsForUser(Long registrationId, Long statusId);
+	public List<ProblemBeanVO> updateAndGetProblemsUnderPending(List<ProblemBeanVO> pendingProblems);
+	
+	
 }
