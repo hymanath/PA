32b5b91f287e6f593052ac9da44d9e716d82bf59
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-22 16:07:47 +0000

    ComparedElectionResultVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ComparedElectionResultVO.java
@@ -0,0 +1,82 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on February 18,2010.
+ */
+package com.itgrids.partyanalyst.dto;
+
+public class ComparedElectionResultVO implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4969671778993399297L;
+    
+	private String candName;
+	private String constiName;
+	private Long votesEarned;
+	private String votesPercent;
+	private String secndCandName;
+	private String secndConstiName;
+	private Long votesEarnedBySecnd;
+	private String secndVotesPercent;
+	private String votesPercentDiff;
+	
+	
+	public String getCandName() {
+		return candName;
+	}
+	public void setCandName(String candName) {
+		this.candName = candName;
+	}
+	public String getConstiName() {
+		return constiName;
+	}
+	public void setConstiName(String constiName) {
+		this.constiName = constiName;
+	}
+	public Long getVotesEarned() {
+		return votesEarned;
+	}
+	public void setVotesEarned(Long votesEarned) {
+		this.votesEarned = votesEarned;
+	}
+	public String getVotesPercent() {
+		return votesPercent;
+	}
+	public void setVotesPercent(String votesPercent) {
+		this.votesPercent = votesPercent;
+	}
+	public String getSecndCandName() {
+		return secndCandName;
+	}
+	public void setSecndCandName(String secndCandName) {
+		this.secndCandName = secndCandName;
+	}
+	public String getSecndConstiName() {
+		return secndConstiName;
+	}
+	public void setSecndConstiName(String secndConstiName) {
+		this.secndConstiName = secndConstiName;
+	}
+	public Long getVotesEarnedBySecnd() {
+		return votesEarnedBySecnd;
+	}
+	public void setVotesEarnedBySecnd(Long votesEarnedBySecnd) {
+		this.votesEarnedBySecnd = votesEarnedBySecnd;
+	}
+	public String getSecndVotesPercent() {
+		return secndVotesPercent;
+	}
+	public void setSecndVotesPercent(String secndVotesPercent) {
+		this.secndVotesPercent = secndVotesPercent;
+	}
+	public String getVotesPercentDiff() {
+		return votesPercentDiff;
+	}
+	public void setVotesPercentDiff(String votesPercentDiff) {
+		this.votesPercentDiff = votesPercentDiff;
+	}
+}
