db21a5904767c7d842e098fbaf3110dd6e953398
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-25 16:23:25 +0000

    other methods to generate problem report-ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IRegistrationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IRegistrationDAO.java
@@ -39,5 +39,7 @@ public interface IRegistrationDAO extends GenericDao<Registration, Long>{
 	public List<String> findPasswords();
 	
 	public List<Registration> findByUserNameAndPassword(String userName, String password);
+	
+	public List<Registration> findByUserRegistrationId(Long registrationId);
 
 }
