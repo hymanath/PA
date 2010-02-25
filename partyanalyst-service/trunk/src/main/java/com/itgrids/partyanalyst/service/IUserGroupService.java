46923cb6711e7bd3a13d12bec1524e71cb54bc45
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-25 16:32:17 +0000

    Created by raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IUserGroupService.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright (c) 2009 IT Grids India LTD.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on February 17, 2010
+ * Author Saikrishna.g
+ */
+
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.dto.UserGroupDetailsVO;
+import com.itgrids.partyanalyst.dto.UserGroupMembersVO;
+
+public interface IUserGroupService {
+
+	public List<SelectOptionVO> getAllStaticGroupNames();
+    public List<UserGroupDetailsVO> getAllSubGroupNames();
+    public List<UserGroupMembersVO> getMembersNames();
+    public UserGroupMembersVO addMemberToGroup(Long groupId, UserGroupMembersVO userGroupMembersToSave);
+    public UserGroupDetailsVO createGroupForUser(Long userId, UserGroupDetailsVO userGroupDetailsToSave);
+  
+}
