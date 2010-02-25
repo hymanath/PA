2bb81d3ff4a5d0e0767d3aae415f1a48e8c059cf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-25 16:22:58 +0000

    other methods to generate problem report-ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemHistoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemHistoryDAO.java
@@ -8,6 +8,17 @@ import com.itgrids.partyanalyst.model.ProblemHistory;
 
 public interface IProblemHistoryDAO extends GenericDao<ProblemHistory, Long>{
 	
-	public List<ProblemHistory> findProblemLocationsByUserId(Long registrationId, Long statusId);
+	public List findProblemsForALocationsByHamletId(Long hamletId);
 	
+	public List findProblemsForALocationsByTehsilId(Long tehsilId);
+	
+	public List findProblemsForALocationsByConstituencyId(String constituencyIds);
+	
+	public List findProblemsByStatusForALocationsByHamletId(Long hamletId,String status);
+	
+	public List findProblemsByStatusForALocationsByTehsilId(Long tehsilId,String status);
+	
+	public List findProblemsByStatusForALocationsByConstituencyId(String constituencyIds,String status);
+	
+	public List<ProblemHistory> findProblemLocationsByUserId(Long registrationId, Long statusId);
 }
