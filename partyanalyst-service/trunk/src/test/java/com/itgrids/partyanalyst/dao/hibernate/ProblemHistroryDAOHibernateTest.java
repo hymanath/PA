02a636db0e5721d6f1de6e1697be94e679c48c67
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-25 16:34:25 +0000

    updated test case for problemHistory--ravi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemHistroryDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemHistroryDAOHibernateTest.java
@@ -4,14 +4,26 @@ import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
 
+import com.itgrids.partyanalyst.dao.IHamletDAO;
 import com.itgrids.partyanalyst.dao.IProblemHistoryDAO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.model.ProblemHistory;
-import com.itgrids.partyanalyst.model.ProblemLocation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
 public class ProblemHistroryDAOHibernateTest extends BaseDaoTestCase {
 
 	private IProblemHistoryDAO problemHistoryDAO;
+	private IHamletDAO hamletDAO;
+	
+	public IHamletDAO getHamletDAO() {
+		return hamletDAO;
+	}
 
+	public void setHamletDAO(IHamletDAO hamletDAO) {
+		this.hamletDAO = hamletDAO;
+	}
 	public IProblemHistoryDAO getProblemHistoryDAO() {
 		return problemHistoryDAO;
 	}
@@ -19,7 +31,7 @@ public class ProblemHistroryDAOHibernateTest extends BaseDaoTestCase {
 	public void setProblemHistoryDAO(IProblemHistoryDAO problemHistoryDAO) {
 		this.problemHistoryDAO = problemHistoryDAO;
 	}
-	
+
 	public void testSave(){
 		ProblemHistory obj = new ProblemHistory();
 		problemHistoryDAO.save(obj);
@@ -34,5 +46,10 @@ public class ProblemHistroryDAOHibernateTest extends BaseDaoTestCase {
 	public void testGetByUserId(){
 		List<ProblemHistory> list = problemHistoryDAO.findProblemLocationsByUserId(new Long(5), new Long(2));
 		assertEquals(1, list.size());
+	}
+	
+	public void testByHamletLocationId(){			
+		List result = problemHistoryDAO.findProblemsForALocationsByHamletId(new Long(66));
+		assertEquals(1, result.size());				
 	}
 }
