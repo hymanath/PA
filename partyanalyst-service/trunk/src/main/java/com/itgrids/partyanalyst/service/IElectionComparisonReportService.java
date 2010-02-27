a09ac3a99faa03486538210a3a29966f6ac8ebda
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-27 06:33:18 +0000

    IElectionComparisonReportService - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionComparisonReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionComparisonReportService.java
@@ -9,6 +9,7 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.CandidateDetailsVO;
 import com.itgrids.partyanalyst.dto.ComparedReportVO;
 import com.itgrids.partyanalyst.dto.DistrictWisePartyResultVO;
 import com.itgrids.partyanalyst.dto.ElectionComparisonReportVO;
@@ -18,4 +19,5 @@ public interface IElectionComparisonReportService {
 	public ElectionComparisonReportVO getDistrictWiseElectionResultsForAParty(Long electionType,Long partyId,Long stateId,String firstYear,String secondYear,Boolean hasAlliances);
 	
 	public ComparedReportVO getComparedElectionResults(Long electionType,Long stateId,Long partyId,String yearOne,String yearTwo,Long districtId,Boolean hasAlliances);
+	
 }
