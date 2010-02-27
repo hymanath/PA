4f290cac1f284b41d90c864356f4bae9e175fd08
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-27 17:21:30 +0000

    Created by raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/GroupsDetailsForUserVO.java
@@ -0,0 +1,71 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class GroupsDetailsForUserVO {
+	private Long userId;
+	private Long staticGroupId;
+	private String staticGroupName;
+	private Long numberOfGroups;
+	private List<UserGroupDetailsVO> subGroupsCreatedByUser;
+	private List<UserGroupMembersVO> groupsMembers;
+	
+	/*public GroupsDetailsForUserVO(){
+		
+	}
+	public GroupsDetailsForUserVO(Long staticGroupId,String staticGroupName,Long numberOfGroups){
+		this.staticGroupId = staticGroupId;
+		this.staticGroupName = staticGroupName;
+		this.numberOfGroups = numberOfGroups;
+	}*/
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getStaticGroupId() {
+		return staticGroupId;
+	}
+
+	public void setStaticGroupId(Long staticGroupId) {
+		this.staticGroupId = staticGroupId;
+	}
+
+	public String getStaticGroupName() {
+		return staticGroupName;
+	}
+
+	public void setStaticGroupName(String staticGroupName) {
+		this.staticGroupName = staticGroupName;
+	}
+
+	public List<UserGroupDetailsVO> getSubGroupsCreatedByUser() {
+		return subGroupsCreatedByUser;
+	}
+
+	public void setSubGroupsCreatedByUser(
+			List<UserGroupDetailsVO> subGroupsCreatedByUser) {
+		this.subGroupsCreatedByUser = subGroupsCreatedByUser;
+	}
+
+	public List<UserGroupMembersVO> getGroupsMembers() {
+		return groupsMembers;
+	}
+
+	public void setGroupsMembers(List<UserGroupMembersVO> groupsMembers) {
+		this.groupsMembers = groupsMembers;
+	}
+
+	public Long getNumberOfGroups() {
+		return numberOfGroups;
+	}
+
+	public void setNumberOfGroups(Long numberOfGroups) {
+		this.numberOfGroups = numberOfGroups;
+	}
+
+	
+}
