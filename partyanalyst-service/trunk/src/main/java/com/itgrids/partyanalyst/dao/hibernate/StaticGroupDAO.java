f8fd7d0c81a7fc7eee1fdc323cc168c2455beee2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-02-27 17:20:14 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticGroupDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticGroupDAO.java
@@ -12,7 +12,6 @@ public class StaticGroupDAO extends GenericDaoHibernate<StaticGroup, Long> imple
 
 	public StaticGroupDAO() {
 		super(StaticGroup.class);
-		// TODO Auto-generated constructor stub
 	}
 
 	@SuppressWarnings("unchecked")
@@ -20,37 +19,5 @@ public class StaticGroupDAO extends GenericDaoHibernate<StaticGroup, Long> imple
 		List<StaticGroup>staticGroup= getHibernateTemplate().find("from  StaticGroup model where model.groupDescription = ?",groupDescription);
 	return staticGroup;
 	}
-
-	@Override
-	public boolean exists(Long arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public StaticGroup get(Long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<StaticGroup> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void remove(Long arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public StaticGroup save(StaticGroup arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
 
 }
