311b0ed6a30d9ff9fca971180efa38a673a913dd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-02 16:11:17 +0000

    bug fixing for user imp date task - Narender

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserImpDatesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserImpDatesDAO.java
@@ -24,8 +24,10 @@ public class UserImpDatesDAO extends GenericDaoHibernate<UserImpDate, Long> impl
 	@SuppressWarnings("unchecked")
 	public List<UserImpDate> findByUserId(Long userID, Calendar inputDate) {
 		log.debug("UserImpDatesDAO.findByuser() ...Start");
-		Object[] params = {userID, inputDate.getTime(),"NO"};
-		List<UserImpDate> result = getHibernateTemplate().find(" from UserImpDate model where model.user.registrationId=? and ? <= model.tillDate and model.isDeleted=?",params);// and " +
+		Calendar end = (Calendar)inputDate.clone();
+		end.add(Calendar.DAY_OF_MONTH, 60);
+		Object[] params = {userID, inputDate.getTime(),"NO",end.getTime()};
+		List<UserImpDate> result = getHibernateTemplate().find(" from UserImpDate model where model.user.registrationId=? and ? <= model.tillDate and model.isDeleted=? and ?>=model.effectiveDate ",params);// and " +
 					//"Month(model.effectiveDate) - " + currentMonth + " in (0,1)" , userID);
 		log.debug("UserImpDatesDAO.findByuser() result.size()"+result.size());
 		return  result;
