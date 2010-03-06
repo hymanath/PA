76de6e30e116e1072703bf4903fcf013331046f4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-06 10:11:01 +0000

    Created by raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/MyGroup.java
@@ -0,0 +1,99 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table(name = "my_group")
+public class MyGroup implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private  Long myGroupId;
+	private String groupName;
+	private String groupDescription;
+	private Date createdDate;
+	private Date updatedDate;
+	private Set <PersonalUserGroup> personalUserGroups = new HashSet<PersonalUserGroup>(0);
+	
+	
+	public MyGroup() {		
+	}	
+	
+	public MyGroup(Long myGroupId, String groupName, String groupDescription,
+			Date createdDate, Date updatedDate, Set <PersonalUserGroup> personalUserGroups) {
+		super();
+		this.myGroupId = myGroupId;
+		this.groupName = groupName;
+		this.groupDescription = groupDescription;
+		this.createdDate = createdDate;
+		this.updatedDate = updatedDate;
+		this.personalUserGroups = personalUserGroups;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "my_group_id", unique = true, nullable = false)
+	public Long getMyGroupId() {
+		return myGroupId;
+	}
+	public void setMyGroupId(Long myGroupId) {
+		this.myGroupId = myGroupId;
+	}
+	@Column(name = "group_name", length = 100)
+	public String getGroupName() {
+		return groupName;
+	}	
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	@Column(name = "group_desc", length = 250)
+	public String getGroupDescription() {
+		return groupDescription;
+	}
+	public void setGroupDescription(String groupDescription) {
+		this.groupDescription = groupDescription;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_date")
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_date")
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "myGroup")
+	public Set<PersonalUserGroup> getPersonalUserGroups() {
+		return personalUserGroups;
+	}
+
+	public void setPersonalUserGroups(Set<PersonalUserGroup> personalUserGroups) {
+		this.personalUserGroups = personalUserGroups;
+	}
+	
+	
+	
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/StaticUserGroup.java
@@ -0,0 +1,88 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+@Entity
+@Table(name = "static_user_group")
+public class StaticUserGroup implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long staticUserGroupId;
+	private Date updatedDate;
+	private StaticUsers staticUser;
+	private PersonalUserGroup personalUserGroup;
+	
+	public StaticUserGroup() {
+		
+	}
+
+	public StaticUserGroup(Long staticUserGroupId, Date updatedDate,
+			StaticUsers staticUser, PersonalUserGroup personalUserGroup) {
+		this.staticUserGroupId = staticUserGroupId;
+		this.updatedDate = updatedDate;
+		this.staticUser = staticUser;
+		this.personalUserGroup = personalUserGroup;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "static_user_group_id", unique = true, nullable = false)
+	public Long getStaticUserGroupId() {
+		return staticUserGroupId;
+	}
+
+	public void setStaticUserGroupId(Long staticUserGroupId) {
+		this.staticUserGroupId = staticUserGroupId;
+	}
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_date")
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "static_user_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public StaticUsers getStaticUser() {
+		return staticUser;
+	}
+
+	public void setStaticUser(StaticUsers staticUser) {
+		this.staticUser = staticUser;
+	}
+	
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "personal_user_group_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PersonalUserGroup getPersonalUserGroup() {
+		return personalUserGroup;
+	}
+
+	public void setPersonalUserGroup(PersonalUserGroup personalUserGroup) {
+		this.personalUserGroup = personalUserGroup;
+	}	
+	
+}
