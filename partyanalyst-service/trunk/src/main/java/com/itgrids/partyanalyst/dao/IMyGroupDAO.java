ee1560439c669f4310ce4ac469305dadd4cb89c1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-06 10:07:22 +0000

    modified by raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMyGroupDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.MyGroup;
+
+public interface IMyGroupDAO extends GenericDao<MyGroup, Long> {
+
+}
