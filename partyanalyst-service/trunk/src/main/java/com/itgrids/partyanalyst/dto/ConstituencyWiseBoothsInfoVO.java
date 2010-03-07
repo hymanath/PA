b4753cc3ba209692363fca6853dc8f1dd15bb42b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-07 10:46:58 +0000

    ConstituencyWiseBoothsInfoVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ConstituencyWiseBoothsInfoVO.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 06,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ConstituencyWiseBoothsInfoVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8573509938318844597L;
+	
+	private Long constituencyId;
+	private Long constiName;
+	private List<BoothTotalVotesVO> boothsDetails;
+	
+	//getters and setters
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public Long getConstiName() {
+		return constiName;
+	}
+	public void setConstiName(Long constiName) {
+		this.constiName = constiName;
+	}
+	public List<BoothTotalVotesVO> getBoothsDetails() {
+		return boothsDetails;
+	}
+	public void setBoothsDetails(List<BoothTotalVotesVO> boothsDetails) {
+		this.boothsDetails = boothsDetails;
+	}
+
+}
