58c2640c4cfeeb203a935c57025a129d30cb1d98
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-07 10:49:33 +0000

    PartyElectionResultVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PartyElectionResultVO.java
@@ -0,0 +1,98 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 06,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class PartyElectionResultVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1689182456480682223L;
+
+	private Long partyId;
+	private String partyName;
+	private String partyLogo;
+	private String partyFlag;
+	private Long candidateId;
+	private String candidateName;
+	private Long votesEarned;
+	private Long validVotes;
+	private Long totalVoters;
+	private String votesPercentage;
+	private Long rank;
+	
+	//getters and setters
+	public Long getPartyId() {
+		return partyId;
+	}
+	public void setPartyId(Long partyId) {
+		this.partyId = partyId;
+	}
+	public String getPartyName() {
+		return partyName;
+	}
+	public void setPartyName(String partyName) {
+		this.partyName = partyName;
+	}
+	public Long getCandidateId() {
+		return candidateId;
+	}
+	public void setCandidateId(Long candidateId) {
+		this.candidateId = candidateId;
+	}
+	public String getCandidateName() {
+		return candidateName;
+	}
+	public void setCandidateName(String candidateName) {
+		this.candidateName = candidateName;
+	}
+	public Long getVotesEarned() {
+		return votesEarned;
+	}
+	public void setVotesEarned(Long votesEarned) {
+		this.votesEarned = votesEarned;
+	}
+	public Long getValidVotes() {
+		return validVotes;
+	}
+	public void setValidVotes(Long validVotes) {
+		this.validVotes = validVotes;
+	}
+	public Long getTotalVoters() {
+		return totalVoters;
+	}
+	public void setTotalVoters(Long totalVoters) {
+		this.totalVoters = totalVoters;
+	}
+	public String getVotesPercentage() {
+		return votesPercentage;
+	}
+	public void setVotesPercentage(String votesPercentage) {
+		this.votesPercentage = votesPercentage;
+	}
+	public Long getRank() {
+		return rank;
+	}
+	public void setRank(Long rank) {
+		this.rank = rank;
+	}
+	public String getPartyLogo() {
+		return partyLogo;
+	}
+	public void setPartyLogo(String partyLogo) {
+		this.partyLogo = partyLogo;
+	}
+	public String getPartyFlag() {
+		return partyFlag;
+	}
+	public void setPartyFlag(String partyFlag) {
+		this.partyFlag = partyFlag;
+	}
+}
