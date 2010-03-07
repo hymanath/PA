8943dd75d364ef06ba9ee16867d6a5405aeb809b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-07 10:45:45 +0000

    BoothResultsForPartyVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BoothResultsForPartyVO.java
@@ -0,0 +1,23 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 06,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+public class BoothResultsForPartyVO implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5347409334048422795L;
+	
+	private Long partyId;
+	private String partyName;
+	private String partySymbol;
+	
+	
+
+}
