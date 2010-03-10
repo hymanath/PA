95d88846ba96ebb7452ae1521968287a379a9b85
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-10 05:42:50 +0000

    ElectionTrendzReportVO - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ElectionTrendzReportVO.java
@@ -0,0 +1,133 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 08,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ElectionTrendzReportVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -255839104302126865L;
+	
+	private String state;
+	private String electionType;
+	private String electionYear;
+	private Long constituencyId;
+	private String constituencyName;
+	private List<String> otherElectionYears;
+	private List<SelectOptionVO> mandalsInfo;
+	
+	private ElectionTrendzInfoVO completeTrendz;
+	private ElectionTrendzInfoVO maleTrendz;
+	private ElectionTrendzInfoVO femaleTrendz;
+	private ElectionTrendzInfoVO maleAndFemaleTrendz;
+	
+	private ResultStatus resultStatus;
+	
+    //getters and setters
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getElectionType() {
+		return electionType;
+	}
+
+	public void setElectionType(String electionType) {
+		this.electionType = electionType;
+	}
+
+	public String getElectionYear() {
+		return electionYear;
+	}
+
+	public void setElectionYear(String electionYear) {
+		this.electionYear = electionYear;
+	}
+
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+
+	public String getConstituencyName() {
+		return constituencyName;
+	}
+
+	public void setConstituencyName(String constituencyName) {
+		this.constituencyName = constituencyName;
+	}
+
+	public List<String> getOtherElectionYears() {
+		return otherElectionYears;
+	}
+
+	public void setOtherElectionYears(List<String> otherElectionYears) {
+		this.otherElectionYears = otherElectionYears;
+	}
+
+	public List<SelectOptionVO> getMandalsInfo() {
+		return mandalsInfo;
+	}
+
+	public void setMandalsInfo(List<SelectOptionVO> mandalsInfo) {
+		this.mandalsInfo = mandalsInfo;
+	}
+
+	public ElectionTrendzInfoVO getCompleteTrendz() {
+		return completeTrendz;
+	}
+
+	public void setCompleteTrendz(ElectionTrendzInfoVO completeTrendz) {
+		this.completeTrendz = completeTrendz;
+	}
+
+	public ElectionTrendzInfoVO getMaleTrendz() {
+		return maleTrendz;
+	}
+
+	public void setMaleTrendz(ElectionTrendzInfoVO maleTrendz) {
+		this.maleTrendz = maleTrendz;
+	}
+
+	public ElectionTrendzInfoVO getFemaleTrendz() {
+		return femaleTrendz;
+	}
+
+	public void setFemaleTrendz(ElectionTrendzInfoVO femaleTrendz) {
+		this.femaleTrendz = femaleTrendz;
+	}
+
+	public ElectionTrendzInfoVO getMaleAndFemaleTrendz() {
+		return maleAndFemaleTrendz;
+	}
+
+	public void setMaleAndFemaleTrendz(ElectionTrendzInfoVO maleAndFemaleTrendz) {
+		this.maleAndFemaleTrendz = maleAndFemaleTrendz;
+	}
+
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+	
+
+}
