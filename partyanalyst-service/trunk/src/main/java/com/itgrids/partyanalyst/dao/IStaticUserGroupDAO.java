e01900181a9c9849a3aec64077d99956b7b2e6cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-11 15:44:58 +0000

    IStaticUserGroupDAO - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStaticUserGroupDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStaticUserGroupDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.StaticUserGroup;
 
 public interface IStaticUserGroupDAO extends GenericDao<StaticUserGroup ,Long> {
 
+	@SuppressWarnings("unchecked")
+	public List getGroupMembersCountForAGroup(Long groupId,Long userId);
 }
