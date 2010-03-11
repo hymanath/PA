2d2a8a11600a7a5108067b38153eb291bbf0115e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-11 15:29:51 +0000

    UserGroupBasicDetails - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupBasicDetails.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 08,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class UserGroupBasicDetails implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6231050197456486604L;
+	
+	private GroupsBasicInfoVO groupBasicDetails;
+	private List<GroupsBasicInfoVO> subGroupDetails;
+	
+    //getters and setters
+	public GroupsBasicInfoVO getGroupBasicDetails() {
+		return groupBasicDetails;
+	}
+
+	public void setGroupBasicDetails(GroupsBasicInfoVO groupBasicDetails) {
+		this.groupBasicDetails = groupBasicDetails;
+	}
+
+	public List<GroupsBasicInfoVO> getSubGroupDetails() {
+		return subGroupDetails;
+	}
+
+	public void setSubGroupDetails(List<GroupsBasicInfoVO> subGroupDetails) {
+		this.subGroupDetails = subGroupDetails;
+	}
+
+}
