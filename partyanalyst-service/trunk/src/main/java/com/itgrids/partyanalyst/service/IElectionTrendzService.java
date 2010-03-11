5e9b3be56c3bfecf4730108678ba6f90872942d2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-11 15:30:39 +0000

    IElectionTrendzService - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionTrendzService.java
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 06,2010
+ */
+package com.itgrids.partyanalyst.service;
+
+import com.itgrids.partyanalyst.dto.ElectionTrendzReportVO;
+import com.itgrids.partyanalyst.dto.MandalCompleteElectionTrendzVO;
+
+public interface IElectionTrendzService {
+
+	public MandalCompleteElectionTrendzVO getCompleteVotingTrendzForAMandal(Long electionType,String electionYear,Long stateId,Long districtId,Long countryId,Long mandalId,Long maleTrendz,Long femaleTrendz);
+	
+	public ElectionTrendzReportVO getVotingTrendzForAnElection(Long electionId,Long elecType,String elecYear,Long stateId,Long countryId,Long constituencyId,Long maleTrendz,Long femaleTrendz);
+	
+	public ElectionTrendzReportVO getVotingTrendzForAConstituency(Long electionId,Long elecType,String elecYear,Long constituencyId,Long maleTrendz,Long femaleTrendz) throws Exception;
+}
