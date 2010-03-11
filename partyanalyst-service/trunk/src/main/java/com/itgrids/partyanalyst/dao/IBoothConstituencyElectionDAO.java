93e7ad0019bc87c641604e159d0e1ed5560dbc7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-11 15:17:41 +0000

    IBoothConstituencyElectionDAO - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothConstituencyElectionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothConstituencyElectionDAO.java
@@ -45,4 +45,35 @@ public interface IBoothConstituencyElectionDAO extends GenericDao<BoothConstitue
 			String assemblyElectionType, String electionYear);
 	
 	public List findVoterInformationByMandalIdsAndDelimitationYear (String mandalsIds,String year, Long constituencyId);
+	
+	@SuppressWarnings("unchecked")
+	public List findMandalWiseMaleBoothsVotingTrendsInAnElection(Long electionId,Long tehsilId,Long trendValue);
+	
+	@SuppressWarnings("unchecked")
+	public List findMandalWiseFemaleBoothsVotingTrendsInAnElection(Long electionId,Long tehsilId,Long trendValue);
+	
+	@SuppressWarnings("unchecked")
+	public List findMandalWiseMaleAndFemaleBoothsVotingTrendsInAnElection(Long electionId,Long tehsilId,Long maleTrend,Long femaleTrend);
+	
+	@SuppressWarnings("unchecked")
+	public List findMandalWiseAllBoothsVotingTrendsInAnElection(Long electionId,Long tehsilId);
+	
+	@SuppressWarnings("unchecked")
+	public List findConstituencyWiseAllBoothsVotingTrendsInAnElection(Long electionId,Long constituencyId);
+	
+	@SuppressWarnings("unchecked")
+	public List findConstituencyWiseMaleBoothsVotingTrendsInAnElection(Long electionId,Long constituencyId,Long trendValue);
+	
+	@SuppressWarnings("unchecked")
+	public List findConstituencyWiseFemaleBoothsVotingTrendsInAnElection(Long electionId,Long constituencyId,Long trendValue);
+	
+	@SuppressWarnings("unchecked")
+	public List findConstituencyWiseMaleAndFemaleBoothsVotingTrendsInAnElection(Long electionId,Long constituencyId,Long maleTrend,Long femaleTrend);	
+	
+	@SuppressWarnings("unchecked")
+	public List findMandalDetailsForAParticularElectionYearForAParticularConstituency(Long constituencyId,Long electionId);
+	
+	@SuppressWarnings("unchecked")
+	public List findAllElectionsForAConstituency(Long constituencyId);
+
 }
