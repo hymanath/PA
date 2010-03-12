3090c280929b5582fdba290a48552d8761e68b8f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-12 18:24:57 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticUserGroupDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticUserGroupDAO.java
@@ -19,10 +19,22 @@ public class StaticUserGroupDAO extends GenericDaoHibernate<StaticUserGroup, Lon
 		return getHibernateTemplate().find("select count(model.staticUserGroupId) from StaticUserGroup model where model.personalUserGroup.personalUserGroupId = ? and model.staticUser.staticUserId = ?",params);
 	}
 	
+
+	@SuppressWarnings("unchecked")
+	public List findMembersByUserId(Long registrationId, Long groupId){
+		Object [] params = {registrationId, groupId};
+		return getHibernateTemplate().find("select model.staticUser.name,model.staticUser.mobileNumber,model.staticUser.emailId," +
+				" model.staticUser.address,model.staticUser.location,model.staticUser.designation,model.staticUser.phoneNumber" +
+				" from StaticUserGroup model where model.personalUserGroup.createdUserId.registrationId= ? " +
+				" and model.personalUserGroup.personalUserGroupId = ?",params);
+		}
+	
+
 	@SuppressWarnings("unchecked")
 	public List getGroupMembersMobileNoForAGroup(Long groupId,Long userId){
 		Object[] params = {groupId,userId};
 		return getHibernateTemplate().find("select model.staticUser.mobileNumber from StaticUserGroup model where model.personalUserGroup.personalUserGroupId = ? and model.staticUser.staticUserId = ?",params);
 	}
 	
+
 }
