5d1bd1441764691815a7c6342eb40304702a8f5d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-15 11:48:00 +0000

    updated for electionVotingTrendz - Mohan

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ElectionVotingTrendzAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ElectionVotingTrendzAction.java
@@ -1,19 +1,147 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.jfree.util.Log;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IStaticDataService;
+import com.itgrids.partyanalyst.service.IElectionTrendzService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.itgrids.partyanalyst.dto.BoothTotalVotesVO;
+import com.itgrids.partyanalyst.dto.ConstituencyWiseBoothsInfoVO;
+import com.itgrids.partyanalyst.dto.ConstituencyWisePartyResultsForMandal;
+import com.itgrids.partyanalyst.dto.MandalVO;
+import com.itgrids.partyanalyst.dto.MandalCompleteElectionTrendzVO;
+import com.itgrids.partyanalyst.dto.MandalElectionTrendzVO;
+import com.itgrids.partyanalyst.dto.PartyElectionResultVO;
+import com.itgrids.partyanalyst.dto.ElectionTrendzReportVO;
+import com.itgrids.partyanalyst.dto.ElectionTrendzInfoVO;
+
+
 
 public class ElectionVotingTrendzAction extends ActionSupport implements ServletRequestAware{
 	
+	
 	private HttpServletRequest request;
+	private List<SelectOptionVO> stateList;
+	private List<SelectOptionVO> districtList;
+	private List<SelectOptionVO> mandalList;
+	private List<SelectOptionVO> constituenciesList;
+	private List<SelectOptionVO> electionType;
+	private List<String> electionYears;
+	private List<MandalVO> mandalVO;
+	
+	private IStaticDataService staticDataService;
+	private IElectionTrendzService electionTrendzService; 
+	
+	private String task = null;
+	JSONObject jObj = null;
+	private MandalCompleteElectionTrendzVO mandalCompleteElectionTrendzVO;
+	private ElectionTrendzReportVO electionTrendzReportVO;
 	
 	
 	
+	public IElectionTrendzService getElectionTrendzService() {
+		return electionTrendzService;
+	}
+
+	public void setElectionTrendzService(
+			IElectionTrendzService electionTrendzService) {
+		this.electionTrendzService = electionTrendzService;
+	}
+	public List<SelectOptionVO> getConstituenciesList() {
+		return constituenciesList;
+	}
+
+	public void setConstituenciesList(List<SelectOptionVO> constituenciesList) {
+		this.constituenciesList = constituenciesList;
+	}
+	public ElectionTrendzReportVO getElectionTrendzReportVO() {
+		return electionTrendzReportVO;
+	}
+
+	public void setElectionTrendzReportVO(
+			ElectionTrendzReportVO electionTrendzReportVO) {
+		this.electionTrendzReportVO = electionTrendzReportVO;
+	}
+	public MandalCompleteElectionTrendzVO getMandalCompleteElectionTrendzVO() {
+		return mandalCompleteElectionTrendzVO;
+	}
+
+	public void setMandalCompleteElectionTrendzVO(
+			MandalCompleteElectionTrendzVO mandalCompleteElectionTrendzVO) {
+		this.mandalCompleteElectionTrendzVO = mandalCompleteElectionTrendzVO;
+	}
+
+	public List<SelectOptionVO> getElectionType() {
+		return electionType;
+	}
+
+	public void setElectionType(List<SelectOptionVO> electionType) {
+		this.electionType = electionType;
+	}
+	public List<MandalVO> getMandalVO() {
+		return mandalVO;
+	}
+
+	public void setMandalVO(List<MandalVO> mandalVO) {
+		this.mandalVO = mandalVO;
+	}
+	public List<String> getElectionYears() {
+		return electionYears;
+	}
+
+	public void setElectionYears(List<String> electionYears) {
+		this.electionYears = electionYears;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public IStaticDataService getStaticDataService() {
+		return staticDataService;
+	}
+
+	public void setStaticDataService(IStaticDataService staticDataService) {
+		this.staticDataService = staticDataService;
+	}
+	
+	public List<SelectOptionVO> getStateList() {
+		return stateList;
+	}
+
+	public void setStateList(List<SelectOptionVO> stateList) {
+		this.stateList = stateList;
+	}
+
+	public List<SelectOptionVO> getDistrictList() {
+		return districtList;
+	}
+
+	public void setDistrictList(List<SelectOptionVO> districtList) {
+		this.districtList = districtList;
+	}
+
+	public List<SelectOptionVO> getMandalList() {
+		return mandalList;
+	}
+
+	public void setMandalList(List<SelectOptionVO> mandalList) {
+		this.mandalList = mandalList;
+	}
+		
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 		
@@ -23,9 +151,140 @@ public class ElectionVotingTrendzAction extends ActionSupport implements Servlet
 		
 		Log.debug("Entered into election voting trendz action");
 		
+		stateList = new ArrayList<SelectOptionVO>(2);
+		stateList.add(0,new SelectOptionVO(0l,"Select State"));
+		stateList.add(1,new SelectOptionVO(1l,"Andra Pradesh"));
+		
+		districtList = new ArrayList<SelectOptionVO>();
+		districtList.add(0,new SelectOptionVO(0l,"Select District"));	
+		
+		mandalList = new ArrayList<SelectOptionVO>();
+		mandalList.add(0,new SelectOptionVO(0l,"Select Mandal"));	
+		
+		constituenciesList = new ArrayList<SelectOptionVO>();
+		constituenciesList.add(0,new SelectOptionVO(0l,"Select Constituency"));
+				
+		electionType = new ArrayList<SelectOptionVO>();
+		electionType.add(0,new SelectOptionVO(0L,"Select"));
+		electionType.add(1,new SelectOptionVO(1L,"Parliament"));
+		electionType.add(2,new SelectOptionVO(2L,"Assembly"));		
+		
+		electionYears = new ArrayList<String>();
+		electionYears.add("Select");
+		electionYears.add("2009");
+		electionYears.add("2004");
+		
 		return Action.SUCCESS;
 	}
 	
+	public String getElectionYearsByElectionId() throws Exception
+	{
+		String param = null;
+		String electionTypeId = "";
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(jObj.getString("electionType") != null && jObj.getString("electionType").length() != 0)
+			electionTypeId = jObj.getString("electionType");
+		
+		//staticDataService.getElection
+		return Action.SUCCESS;
+	}
 	
-
+	public String getDistrictsByStateId() throws Exception
+	{
+		String param = null;
+		String stateId = "";
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+			System.out.println(jObj);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(jObj.getString("stateId") != null && jObj.getString("task").equalsIgnoreCase("getDistricts"))
+		{
+			stateId = jObj.getString("stateId");		
+			districtList = staticDataService.getDistricts(new Long(stateId));
+		}
+		return Action.SUCCESS;
+	}
+	
+	public String getConstituenciesByElectionTypeIdAndStateId() throws Exception
+	{
+		String param = null;
+		String electionTypeId = "";
+		String stateId = "";
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(jObj.getString("task").equalsIgnoreCase("getConstituencies"))
+		{
+			electionTypeId = jObj.getString("electionTypeId");
+			stateId = jObj.getString("stateId");
+		}
+		
+		constituenciesList = staticDataService.getConstituenciesByElectionTypeAndStateId(new Long(electionTypeId), new Long(stateId));
+		return Action.SUCCESS;
+	}
+	
+	public String getMandalsByDistrictId() throws Exception
+	{
+		String param = null;
+		String districtId = "";
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(jObj.getString("districtId") != null && jObj.getString("task").equalsIgnoreCase("getMandals"))
+			districtId = jObj.getString("districtId");
+		
+		mandalVO = staticDataService.getMandalsForDistrict(new Long(districtId));
+		return Action.SUCCESS;
+	}
+	
+	public String getVotingTrendz() throws Exception
+	{
+		String param = null;		
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);	
+			System.out.println("jObj================"+jObj);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String stateId = jObj.getString("stateValue");
+		String constituencyId = jObj.getString("constituencyVal");
+		String electionTypeId = jObj.getString("electionTypeId");	
+		
+		electionTrendzReportVO = new ElectionTrendzReportVO();
+		electionTrendzReportVO = electionTrendzService.getVotingTrendzForAnElection(new Long(0), new Long(electionTypeId), "0", new Long(stateId), new Long(1), new Long(constituencyId), new Long(0), new Long(0));
+		
+		
+		return Action.SUCCESS;
+	}
+	
 }
--- partyanalyst/trunk/src/main/webapp/jsp/electionVotingTrendz.jsp
+++ partyanalyst/trunk/src/main/webapp/jsp/electionVotingTrendz.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+	
+<%@taglib prefix="s" uri="/struts-tags" %>
+<%@taglib uri="http://displaytag.sf.net" prefix="display" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -53,13 +58,196 @@
 
 <!-- YUI Dependency files (End) -->
 
+<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAmy8d-PXO6ktmh6sCNFXdwRSqcWSqDo-rwCiW8VjO_0U_k7HAuxQBSweyAZ1v5ozDSPMDKAFtPwSrGw&sensor=true"
+            type="text/javascript"></script>
+
+<script type="text/javascript" src="js/commonUtilityScript/commonUtilityScript.js"></script>
 <script type="text/javascript" src="js/electionVotingTrendz/electionVotingTrendz.js"></script>
 <link rel="stylesheet" type="text/css"  href="styles/electionVotingTrendz/electionVotingTrendz.css"></link>
 
+<script type="text/javascript">
+	
+
+
+function callAjax(jsObj,url)
+	{			
+		
+ 		var callback = {			
+ 		               success : function( o ) {
+							try {
+								myResults = YAHOO.lang.JSON.parse(o.responseText);	
+								console.log(myResults);
+								
+								if(jsObj.task == "getDistricts")
+								{
+									fillDistrictSelect(myResults);
+								}
+								if(jsObj.task == "getMandals")
+								{
+									fillMandalSelect(myResults);
+								}
+								if(jsObj.task == "getConstituencies")
+								{
+									fillConstituencySelect(myResults);
+								}
+								if(jsObj.task == "getVotingTrendz")
+								{
+									showElectionVotingTrendz(myResults,"votingTrendzReportContentOuter");
+								}						
+										
+							}catch (e) {   
+							   	alert("Invalid JSON result" + e);   
+							}  
+ 		               },
+ 		               scope : this,
+ 		               failure : function( o ) {
+ 		                			alert( "Failed to load result" + o.status + " " + o.statusText);
+ 		                         }
+ 		               };
+
+ 		YAHOO.util.Connect.asyncRequest('GET', url, callback);
+	}
+
+function getConstituenciesForElectionType()
+{
+	var stateSelectElement = document.getElementById("stateSelect");	
+	var stateSelectValue = stateSelectElement.options[stateSelectElement.selectedIndex].value;
+	if(stateSelectValue == 0)
+		return;
+
+
+
+	var elements = document.getElementsByTagName('input'); 
+	var electionTypeId='';
+
+		for(var i=0;i<elements.length;i++)
+		{
+			if(elements[i].type=="radio" && elements[i].name=="electionType" && elements[i].checked==true)
+			{
+				electionTypeId = elements[i].value;				
+			}
+		}	
+		
+	
+
+	var jsObj=	{
+					electionTypeId:electionTypeId,
+					stateId:stateSelectValue,
+					task:"getConstituencies"
+				}
+
+	var rparam ="task="+YAHOO.lang.JSON.stringify(jsObj);				
+	var url = "<%=request.getContextPath()%>/getConstituenciesAjaxAction.action?"+rparam;
+
+	callAjax(jsObj, url);
+}
+
+function getVotingTrendz()
+{
+	var stateSelectElmt = document.getElementById("stateSelect");		
+	var constituencySelectElmt = document.getElementById("constituencySelect");
+
+	var stateVal = stateSelectElmt.options[stateSelectElmt.selectedIndex].value;	
+	var constituencyVal = constituencySelectElmt.options[constituencySelectElmt.selectedIndex].value;
+
+	var elements = document.getElementsByTagName('input'); 
+	var electionTypeId='';
+
+	for(var i=0;i<elements.length;i++)
+	{
+		if(elements[i].type=="radio" && elements[i].name=="electionType" && elements[i].checked==true)
+		{
+			electionTypeId = elements[i].value;				
+		}
+	}	
+	
+	var jsObj=	{
+					stateValue:stateVal,					
+					electionTypeId : electionTypeId,
+					constituencyVal:constituencyVal,
+					task:"getVotingTrendz"
+				}
+
+	var rparam ="task="+YAHOO.lang.JSON.stringify(jsObj);				
+	var url = "<%=request.getContextPath()%>/getVotingTrendzAction.action?"+rparam;
+
+	callAjax(jsObj, url);
+}
+
+</script>
 </head>
-<body>
+<body>	
 	<div id="votingTrendzmain">
-			Welcome To Election Voting Trends.
+		<div id="votingTrendzHeadingContentOuter" class="rounded"> 						
+				<div class="corner topLeft"></div>
+				<div class="corner topRight"></div>
+				<div class="corner bottomLeft"></div>
+				<div class="corner bottomRight"></div>
+				
+				<div id="electionVotingpageHead"> Election Voting Trendz </div>
+		</div>
+		
+		<div id="votingTrendzInputContentOuter" class="rounded"> 						
+			<div class="corner topLeft"></div>
+			<div class="corner topRight"></div>
+			<div class="corner bottomLeft"></div>
+			<div class="corner bottomRight"></div>
+				<div id="votingTrendsInputMain">
+					<div id="votingTrendzInputData">
+						<table width="100%" id="votingTrendzInputTable">
+							<tr>
+								<th> Select Election Type</th>
+								<td colspan="4"> 
+									<div id="electionType">
+										<input type="radio" name="electionType" value="2">Assembly
+										<input type="radio" name="electionType" value="1">Parliament
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<th>Select State</th>
+								<td> 
+									<div id="stateSelectDiv">
+										<s:select theme="simple" id="stateSelect" cssClass="selectBoxClass" name="stateSelect" list="stateList" listKey="id" listValue="name" onchange="getConstituenciesForElectionType(this.id)"></s:select>
+									</div>
+								</td>
+								
+
+								<th>Select Constituency</th>
+								<td>
+									<div id="constituencySelectDiv">
+										<s:select theme="simple" id="constituencySelect" cssClass="selectBoxClass" name="constituencySelect" list="constituenciesList" listKey="id" listValue="name"></s:select>
+									</div>
+								</td>
+
+							</tr>							
+							<tr>
+								<td colspan="6">
+									<div style="text-align:center">
+										<input type="button" value="Get Voting Trendz" onclick="getVotingTrendz()"></input>
+									</div>
+								</td>
+							</tr>
+						</table>
+					</div>
+					<div id="votingTrendzProcessingImg">
+						
+					</div>
+				</div>
+			</div>
+			
+
+			<div id="votingTrendzReportContentOuter" class="rounded"> 						
+				<div class="corner topLeft"></div>
+				<div class="corner topRight"></div>
+				<div class="corner bottomLeft"></div>
+				<div class="corner bottomRight"></div>					
+			</div>
 	</div>
+
+	<script type="text/javascript">
+		intializeVotingTrendzReport();
+			
+	</script>
 </body>
 </html>
