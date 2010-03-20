4f1e50ba3871122de3bcd341c5e93e5816007061
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-20 18:04:23 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupsVO.java
@@ -1,11 +1,18 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class UserGroupsVO {
 	
 	private List <GroupsDetailsForUserVO> groupsDetailsForUser;
 	private List<UserGroupMembersVO> userGroupMembersList;
+	private List<SelectOptionVO> systemGroupsList;
+	private List<SelectOptionVO> myGroupsList;
+	private List<SelectOptionVO> breadCrumb;
+	private Map<Long, String> BMap;
+	private Set<SelectOptionVO> breadCrumbList;
 	 
 	public List<GroupsDetailsForUserVO> getGroupsDetailsForUser() {
 		return groupsDetailsForUser;
@@ -25,5 +32,46 @@ public class UserGroupsVO {
 		this.userGroupMembersList = userGroupMembersList;
 	}
 
- 	
+	public List<SelectOptionVO> getSystemGroupsList() {
+		return systemGroupsList;
+	}
+
+	public void setSystemGroupsList(List<SelectOptionVO> systemGroupsList) {
+		this.systemGroupsList = systemGroupsList;
+	}
+
+	public List<SelectOptionVO> getMyGroupsList() {
+		return myGroupsList;
+	}
+
+	public void setMyGroupsList(List<SelectOptionVO> myGroupsList) {
+		this.myGroupsList = myGroupsList;
+	}
+
+	public List<SelectOptionVO> getBreadCrumb() {
+		return breadCrumb;
+	}
+
+	public void setBreadCrumb(List<SelectOptionVO> breadCrumb) {
+		this.breadCrumb = breadCrumb;
+	}
+
+	public Map<Long, String> getBMap() {
+		return BMap;
+	}
+
+	public void setBMap(Map<Long, String> map) {
+		BMap = map;
+	}
+
+	public Set<SelectOptionVO> getBreadCrumbList() {
+		return breadCrumbList;
+	}
+
+	public void setBreadCrumbList(Set<SelectOptionVO> breadCrumbList) {
+		this.breadCrumbList = breadCrumbList;
+	} 
+	
+	
+	
 }
