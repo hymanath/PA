44cd97cfbd02b8608346f9201c2bde393ddcf80c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-20 09:49:25 +0000

    CandidateVotingTrendzCharts - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CandidateVotingTrendzCharts.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CandidateVotingTrendzCharts.java
@@ -9,12 +9,26 @@ public class CandidateVotingTrendzCharts implements Serializable {
 	 */
 	private static final long serialVersionUID = 4765301587243158318L;
 	
+	private String pollingDetailsChart;
+	private String votingTrendzMainChart;
 	private String candOverallVotesPercent;
 	private String candVotingTrendz;
 	
 	
 	public String getCandOverallVotesPercent() {
 		return candOverallVotesPercent;
+	}
+	public String getPollingDetailsChart() {
+		return pollingDetailsChart;
+	}
+	public void setPollingDetailsChart(String pollingDetailsChart) {
+		this.pollingDetailsChart = pollingDetailsChart;
+	}
+	public String getVotingTrendzMainChart() {
+		return votingTrendzMainChart;
+	}
+	public void setVotingTrendzMainChart(String votingTrendzMainChart) {
+		this.votingTrendzMainChart = votingTrendzMainChart;
 	}
 	public void setCandOverallVotesPercent(String candOverallVotesPercent) {
 		this.candOverallVotesPercent = candOverallVotesPercent;
