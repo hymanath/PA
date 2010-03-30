54a5838c43fa3e15b8c85f5616d9b611aded64d9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-03-30 08:47:54 +0000

    IBasePartyResultsService - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBasePartyResultsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBasePartyResultsService.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.PartyInfoVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.model.Election;
 import com.itgrids.partyanalyst.model.ElectionScope;
 import com.itgrids.partyanalyst.utils.ElectionScopeLevelEnum;
@@ -15,5 +16,5 @@ public interface IBasePartyResultsService {
 	public ElectionScope getElectionScope(Long typeId, Long countryID, Long stateID);
 	public List<Election> getElections(ElectionScope scope);
 	public List<PartyInfoVO> getPartyAndCompetetorsInfo(Election election,
-			String partyShortName,Long stateID, Long districtID, Long constituencyID, int competetorSize, ElectionScopeLevelEnum level);
+			String partyShortName,Long stateID, Long districtID, Long constituencyID, int competetorSize, ElectionScopeLevelEnum level,Boolean hasAlliance,List<SelectOptionVO> allianceParties);
 }
