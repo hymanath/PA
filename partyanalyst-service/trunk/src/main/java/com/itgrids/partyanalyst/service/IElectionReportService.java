7a014f18fda7d1077070e4f010d2568043f0d05a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-01 11:27:52 +0000

    Election Report changes - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionReportService.java
@@ -0,0 +1,15 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on March 31,2010
+ */
+package com.itgrids.partyanalyst.service;
+
+import com.itgrids.partyanalyst.dto.ElectionResultsReportVO;
+
+public interface IElectionReportService {
+
+	public ElectionResultsReportVO getBasicResultsForAnElection(String electionType,String electionYear,Long stateId);
+}
