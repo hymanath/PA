4467ef8767681ea0077d2ebc5bef7b978cec55b4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-08 16:36:16 +0000

    modified by raghav

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CandidateDetailsForElectionDetailsReportAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/CandidateDetailsForElectionDetailsReportAction.java
@@ -1,10 +1,21 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.CandidateDetailsVO;
+import com.itgrids.partyanalyst.dto.ElectionResultsReportVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IStaticDataService;
+import com.itgrids.partyanalyst.service.impl.StaticDataService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -21,6 +32,15 @@ public class CandidateDetailsForElectionDetailsReportAction extends ActionSuppor
 	private String electionType;
 	private String stateName;
 	private String year;
+	private String task = null;
+	JSONObject jObj = null;
+	private CandidateDetailsVO candidateDetailsVO;
+	private CandidateDetailsVO statesListObj;
+	private IStaticDataService staticDataService; 
+	private List<SelectOptionVO> partiesList;
+	private List<SelectOptionVO> districtsList;
+	
+	
 	
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;		
@@ -77,13 +97,140 @@ public class CandidateDetailsForElectionDetailsReportAction extends ActionSuppor
 	public static Logger getLog() {
 		return log;
 	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public String getTask() {
+		return task;
+	}	
 	
+	public JSONObject getJObj() {
+		return jObj;
+	}
+
+	public void setJObj(JSONObject obj) {
+		jObj = obj;
+	}
+
+	public void setCandidateDetailsVO(CandidateDetailsVO candidateDetailsVO) {
+		this.candidateDetailsVO = candidateDetailsVO;
+	}
+
+	public CandidateDetailsVO getCandidateDetailsVO() {
+		return candidateDetailsVO;
+	}
+
+	public void setStaticDataService(IStaticDataService staticDataService) {
+		this.staticDataService = staticDataService;
+	}
+
+	public IStaticDataService getStaticDataService() {
+		return staticDataService;
+	}
+
+	public List<SelectOptionVO> getPartiesList() {
+		return partiesList;
+	}
+
+	public void setPartiesList(List<SelectOptionVO> partiesList) {
+		this.partiesList = partiesList;
+	}
+
+	public List<SelectOptionVO> getDistrictsList() {
+		return districtsList;
+	}
+
+	public void setDistrictsList(List<SelectOptionVO> districtsList) {
+		this.districtsList = districtsList;
+	}
+		
+	public CandidateDetailsVO getStatesListObj() {
+		return statesListObj;
+	}
+
+	public void setStatesListObj(CandidateDetailsVO statesListObj) {
+		this.statesListObj = statesListObj;
+	}
+
 	public String execute () throws Exception 
 	{
+		statesListObj = new CandidateDetailsVO();		
+		statesListObj = staticDataService.getAllStatesInCountry();
+		districtsList = new ArrayList<SelectOptionVO>();
+		districtsList = staticDataService.getDistricts(new Long(stateID));
+		partiesList = new ArrayList<SelectOptionVO>();
+		if(electionType.equals(IConstants.ZPTC) || electionType.equals(IConstants.MPTC))
+		{
+			partiesList = staticDataService.getAllPartiesForAnElectionYear(year, electionType);			
+		} else if (electionType.equals(IConstants.ASSEMBLY_ELECTION_TYPE) || electionType.equals(IConstants.PARLIAMENT_ELECTION_TYPE))
+		{
+			partiesList = staticDataService.getStaticParties();
+		}
+		
 		return Action.SUCCESS;
 		
 	}
-	
-	
+
+	public String ajaxCallHandler()
+	{
+		String param = null;
+		param = getTask();
+		try {
+			jObj = new JSONObject(param);
+			if(log.isDebugEnabled())
+				log.debug(jObj);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(jObj.getString("task").equalsIgnoreCase("getAllCandidates"))
+		{
+			String resultsCategory =jObj.getString("resultsCategory");
+			String resultsCategoryVal = null, electionLevelVal = null;
+			String electionLevel = jObj.getString("electionLevel");
+			Long locationId = new Long(jObj.getString("locationId"));
+			Long partyId = new Long(jObj.getString("partyId"));
+			String year = jObj.getString("year");
+			String electionType = jObj.getString("electionType");
+			String stateID = jObj.getString("stateID");
+			candidateDetailsVO = new CandidateDetailsVO();
+			if(resultsCategory.equals("allCandidates"))
+			{
+				resultsCategoryVal = IConstants.ALL_CANDIDATES; 
+			}
+			if(resultsCategory.equals("wonCandidatesOnly"))
+			{
+				resultsCategoryVal = IConstants.WINNER_CANDIDATES; 
+			}
+			if(electionLevel.equals("countrywiseParliament"))
+			{
+				electionLevelVal = IConstants.COUNTRY_LEVEL; 
+			}
+			if(electionLevel.equals("statewiseParliament"))
+			{
+				electionLevelVal = IConstants.STATE_LEVEL; 
+			}
+			if(electionLevel.equals("statewiseAssembly"))
+			{
+				electionLevelVal = IConstants.STATE_LEVEL; 
+			}
+			if(electionLevel.equals("districtwiseAssembly"))
+			{
+				electionLevelVal = IConstants.DISTRICT_LEVEL; 
+			}
+			log.debug(electionType);
+			log.debug(year);
+			log.debug(resultsCategoryVal);
+			log.debug(electionLevelVal);
+			log.debug(locationId);
+			log.debug(partyId);
+			log.debug(stateID);
+			
+			candidateDetailsVO = staticDataService.getCandidatesPartyInfoForAnElectionType(electionType,year,resultsCategoryVal,electionLevelVal,locationId,partyId,new Long(stateID));
+			}	
+		return Action.SUCCESS;
+	}
 
 }
