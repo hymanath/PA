4b29fd1541ce692c8643a643ad79026be83eb56c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-19 14:46:03 +0000

    Parliament statewise results code - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.utils;
 public interface IConstants {
 	public static final String DATE_PATTERN = "dd/MM/yyyy";
 	public static final Long CENSUS_YEAR = 2001L;
+	public static final Long COUNTRY_INDIA_ID = 1L;
 	public static final String STATIC_PARTIES = "'INC','PRP','TDP','TRS','CPI','CPM','AIMIM','BJP'";
 	public static final int LOOSING_BY_DROPPING_VOTES_CONSTANTS = 5;
 	public static final String SPACE =" ";
