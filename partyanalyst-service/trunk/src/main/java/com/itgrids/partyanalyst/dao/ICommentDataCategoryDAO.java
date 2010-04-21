dd1dd0f96e145cf0e210880df82374e557900e49
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-21 06:40:32 +0000

    user comments code release - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICommentDataCategoryDAO.java
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on April 20,2010
+ */
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CommentDataCategory;
+
+public interface ICommentDataCategoryDAO extends GenericDao<CommentDataCategory, Long> {
+	
+}
