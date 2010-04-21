e82272344f11efa4ba6d0a128f2e802b87dc89a0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-21 10:08:06 +0000

    user comments code release - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICommentDataCategoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICommentDataCategoryDAO.java
@@ -7,10 +7,15 @@
  */
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CommentDataCategory;
 
 public interface ICommentDataCategoryDAO extends GenericDao<CommentDataCategory, Long> {
+
+	@SuppressWarnings("unchecked")
+	public List findCommentDataCategoryByType(String type);
 	
 }
