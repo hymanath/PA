e350bd78bafbc776a25701d20b7450d25037d78a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-04-22 16:19:22 +0000

    Created by raghav

---
+++ partyanalyst/trunk/src/main/webapp/js/CommentsDialog/commentsDialog.js
@@ -0,0 +1,77 @@
+var addCommentsDialog;
+
+function showCommentsDialog(id,candidateName,category, rank,constituencyId,constituencyName,partyName)
+{
+	var elmt = document.getElementById('commentsDialogDiv');
+	var divChild = document.createElement('div');
+	divChild.setAttribute('id','addCommentDiv');
+	var contentStr='';
+	contentStr+='<div class="hd" align="left">Add/View Your Comments</div>';
+	contentStr+='<div class="bd" align="left">';
+	contentStr+='<FIELDSET align="center">';
+	contentStr+='<LEGEND><B>Comments Details</B></LEGEND>';
+	contentStr+='<div id="candidateInfo" align="left"></div>';
+	contentStr+='<div id="previousComments"></div>';
+	contentStr+='</FIELDSET>';
+	contentStr+='<DIV id="alertMessage" style"margin-top:5px;margin-bottom:5px;">Fields marked with * are Mandatory</DIV>';
+	contentStr+='<div id="commentsDetailsDivBody">';
+	contentStr+='<FIELDSET align="center">';
+	contentStr+='<LEGEND><B>Add New Comment</B></LEGEND>';
+	contentStr+='<DIV>';
+	contentStr+='<TABLE width="100%" class="commentsInputTable" border="1" cellspacing="0" cellpadding="0">';
+	if(rank != 'null' && category == 'candidate')
+	{
+		contentStr+='<TR>';
+		contentStr+='<TD align="left" class="commentsInputTd">Classify Your Comment*</TD>';	
+		contentStr+='<TD class="commentsInputTd" align="left"><SELECT class="selectBoxStyle" id="commentsClassificaitonSelectBox"  name="selectBox" style="display:block;">';
+		contentStr+='<OPTION id="0" >Select Classification</OPTION>';
+		contentStr+='</SELECT></TD>';
+		contentStr+='</TR>';
+		getCommentsClassifications(rank);
+		showExistingComments(id,candidateName,category,constituencyId,constituencyName,partyName);
+	}	
+	contentStr+='<TR>';
+	contentStr+='<TD align="left" valign="top" class="commentsInputTd">Comment*</TD>';	
+	contentStr+='<TD class="commentsInputTd" valign="top" align="left"><TEXTAREA style="width:300px;" id="commentText" name="commentText"></TEXTAREA></TD>';
+	contentStr+='</TR>';
+	contentStr+='<TR>';
+	contentStr+='<TD align="left" class="commentsInputTd" valign="top">Posted By*</TD>';	
+	contentStr+='<TD class="commentsInputTd" valign="top" align="left"><input type="text" style="width:300px;" id="commentPostedByText" name="commentPostedByText"/></TD>';
+	contentStr+='</TR>';
+	contentStr+='</TABLE>';
+	contentStr+='</DIV>';
+	contentStr+='</FIELDSET>';	
+	contentStr+='<DIV style="text-align:right;"><INPUT type="button" class="button" id="addCommentsButton" style="width:50px;" onclick="handleAddCommentsSubmit('+id+',\''+category+'\','+constituencyId+')" value="Save"/>';
+	contentStr+='<INPUT type="button" id="addCommentsButton" style="width:50px;" class="button" onclick="handleAddCommentsCancel()" value="Exit"/></DIV>';
+	contentStr+='</div>';
+	contentStr+='</div>';
+	divChild.innerHTML=contentStr;
+	elmt.appendChild(divChild);
+	
+	if(addCommentsDialog)
+		addCommentsDialog.destroy();
+	addCommentsDialog = new YAHOO.widget.Dialog("addCommentDiv",
+			{ 
+			  width : "800px", 
+              fixedcenter : false, 
+              visible : true,  
+              constraintoviewport : true, 
+			  iframe :true,
+			  modal :true,
+			  hideaftersubmit:true,
+			  close:true,
+			  x:400,
+			  y:400,				  
+			  buttons : []
+             } ); 
+	addCommentsDialog.render();
+	//setFocus();
+	var commentsEl = document.getElementById("commentText").focus();
+	
+	//showExistingComments('+id+',\''+category+'\','+constituencyId+');
+}
+
+function handleAddCommentsCancel()
+{
+	addCommentsDialog.cancel();
+}
