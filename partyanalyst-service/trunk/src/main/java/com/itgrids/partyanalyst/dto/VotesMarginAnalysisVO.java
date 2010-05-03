08d52f4b58002387ec36879fedb6456f040a96f3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-05-03 13:15:00 +0000

    Analysis report code release - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/VotesMarginAnalysisVO.java
@@ -0,0 +1,85 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on May 1,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class VotesMarginAnalysisVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8394010146351006404L;
+	
+	private Long marginValueOne;
+	private Long marginValueTwo;
+	private String marginRange;
+	private Long candidatesCount;
+	private Long analyzedCount;
+	
+	private List<Long> nominationIds;
+	private ResultStatus resultStatus;
+
+	public Long getMarginValueOne() {
+		return marginValueOne;
+	}
+
+	public void setMarginValueOne(Long marginValueOne) {
+		this.marginValueOne = marginValueOne;
+	}
+
+	public Long getMarginValueTwo() {
+		return marginValueTwo;
+	}
+
+	public void setMarginValueTwo(Long marginValueTwo) {
+		this.marginValueTwo = marginValueTwo;
+	}
+
+	public String getMarginRange() {
+		return marginRange;
+	}
+
+	public void setMarginRange(String marginRange) {
+		this.marginRange = marginRange;
+	}
+
+	public Long getCandidatesCount() {
+		return candidatesCount;
+	}
+
+	public void setCandidatesCount(Long candidatesCount) {
+		this.candidatesCount = candidatesCount;
+	}
+
+	public Long getAnalyzedCount() {
+		return analyzedCount;
+	}
+
+	public void setAnalyzedCount(Long analyzedCount) {
+		this.analyzedCount = analyzedCount;
+	}
+
+	public List<Long> getNominationIds() {
+		return nominationIds;
+	}
+
+	public void setNominationIds(List<Long> nominationIds) {
+		this.nominationIds = nominationIds;
+	}
+
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+
+}
