b4f3ebc3e7c53751473fa66d23ab6b74420daaee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-05-14 06:24:38 +0000

    Comparator for sorting election years in Constituency Page--ravi

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ElectionDetailsVOComparator.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.utils;
+
+import java.util.Comparator;
+
+import com.itgrids.partyanalyst.dto.ConstituencyElectionResultsVO;
+
+public class ElectionDetailsVOComparator implements Comparator<ConstituencyElectionResultsVO> {
+
+	
+
+	@Override
+	public int compare(ConstituencyElectionResultsVO a,ConstituencyElectionResultsVO b) {
+		return b.getElectionYear().compareTo(a.getElectionYear());
+	}
+
+}
