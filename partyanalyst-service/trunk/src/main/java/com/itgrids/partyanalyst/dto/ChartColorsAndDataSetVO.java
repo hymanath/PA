e5a2955aaf99812e457721d296eb60c48f1aa404
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-05-28 11:13:54 +0000

    Created a new vo for maintaining dataset and colors list for graphs-raghav

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ChartColorsAndDataSetVO.java
@@ -0,0 +1,27 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.awt.Color;
+import java.util.Set;
+
+public class ChartColorsAndDataSetVO {
+
+	private Object dataSet;
+	private Set<Color> colorsSet;
+	
+	public Object getDataSet() {
+		return dataSet;
+	}
+	public void setDataSet(Object dataSet) {
+		this.dataSet = dataSet;
+	}
+	public Set<Color> getColorsSet() {
+		return colorsSet;
+	}
+	public void setColorsSet(Set<Color> colorsSet) {
+		this.colorsSet = colorsSet;
+	}
+	
+	
+	
+	
+}
