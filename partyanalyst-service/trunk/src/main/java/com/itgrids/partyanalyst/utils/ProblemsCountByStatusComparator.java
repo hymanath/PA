70e67556e9fc690d55224397eae7832c00f8c4cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-04 19:59:41 +0000

    Date Issue Resolved In Constituency Management Page By Sivakumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ProblemsCountByStatusComparator.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ProblemsCountByStatusComparator.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
 
 import com.itgrids.partyanalyst.dto.ProblemsCountByStatus;
@@ -7,7 +9,13 @@ import com.itgrids.partyanalyst.dto.ProblemsCountByStatus;
 public class ProblemsCountByStatusComparator implements
 		Comparator<ProblemsCountByStatus> {
 	public int compare(ProblemsCountByStatus a, ProblemsCountByStatus b) {
-		return a.getDate().compareTo(b.getDate());
+		 DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		 try{
+			 return formatter.parse(b.getDate()).compareTo(formatter.parse(a.getDate()));	 
+		 }catch(Exception ex){
+			 ex.printStackTrace();
+		 }
+		return 0;
 	}
 
 }
