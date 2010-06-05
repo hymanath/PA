fb2ed7516fdbdbfe7b8bde20dde23f98d33c484c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-05 14:09:11 +0000

    Models for political change -- ravi

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PoliticalChanges.java
@@ -0,0 +1,202 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "political_changes")
+public class PoliticalChanges extends BaseModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	// Fields
+	private Long politicalChangesId;
+	private Party party;
+	private Registration registration;
+	private ProblemExternalSource externalSource;
+	private PoliticalChangesInformationSource politicalChangesInformationSource;
+	private String title;
+	private String description;
+	private Date identifiedDate;
+	private Date occuredDate;
+	private Date updatedDate;	
+	private String sourceOfInformation;
+	private String isDelete;
+	
+	// Constructors
+
+	/** default constructor */
+	public PoliticalChanges() {
+	}
+
+	/** minimal constructor */
+	public PoliticalChanges(Long politicalChangesId) {
+		this.politicalChangesId = politicalChangesId;
+	}
+
+	/** full constructor */
+	public PoliticalChanges(Long politicalChangesId,String sourceOfInformation,
+			                     Party party,Registration registration,ProblemExternalSource externalSource,
+			                     Date identifiedDate,Date occuredDate,
+			                     Date updatedDate,String description,PoliticalChangesInformationSource politicalChangesInformationSource
+								 ,String title,String isDelete) {
+		this.politicalChangesId = politicalChangesId;
+		this.party = party;
+		this.registration = registration;
+		this.externalSource = externalSource;
+		this.politicalChangesInformationSource = politicalChangesInformationSource;
+		this.sourceOfInformation = sourceOfInformation;
+		this.title = title;
+		this.identifiedDate = identifiedDate;
+		this.occuredDate = occuredDate;
+		this.updatedDate = updatedDate;
+		this.description = description;
+		this.isDelete = isDelete;
+	}
+
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "political_changes_id", unique = true, nullable = false)
+	public Long getPoliticalChangesId() {
+		return politicalChangesId;
+	}
+
+	public void setPoliticalChangesId(Long politicalChangesId) {
+		this.politicalChangesId = politicalChangesId;
+	}
+
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "party_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Party getParty() {
+		return party;
+	}
+
+	public void setParty(Party party) {
+		this.party = party;
+	}
+	
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Registration getRegistration() {
+		return registration;
+	}
+	
+	public void setRegistration(Registration registration) {
+		this.registration = registration;
+	}	 
+	
+	@ManyToOne(cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "external_source_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public ProblemExternalSource getExternalSource() {
+		return externalSource;
+	}
+
+	public void setExternalSource(ProblemExternalSource externalSource) {
+		this.externalSource = externalSource;
+	}
+
+
+	@ManyToOne(cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "political_changes_information_source_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PoliticalChangesInformationSource getPoliticalChangesInformationSource() {
+		return politicalChangesInformationSource;
+	}
+
+	public void setPoliticalChangesInformationSource(
+			PoliticalChangesInformationSource politicalChangesInformationSource) {
+		this.politicalChangesInformationSource = politicalChangesInformationSource;
+	}
+
+	@Column(name = "title", length = 200)
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Column(name = "identified_date", length = 25)
+	public Date getIdentifiedDate() {
+		return identifiedDate;
+	}
+
+	public void setIdentifiedDate(Date identifiedDate) {
+		this.identifiedDate = identifiedDate;
+	}
+
+	@Column(name = "occured_date", length = 25)
+	public Date getOccuredDate() {
+		return occuredDate;
+	}
+
+	public void setOccuredDate(Date occuredDate) {
+		this.occuredDate = occuredDate;
+	}
+
+	@Column(name = "updated_date", length = 25)
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Column(name = "description", length = 500)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	} 
+
+	@Column(name = "source_of_information", length = 200)
+	public String getSourceOfInformation() {
+		return sourceOfInformation;
+	}
+
+	public void setSourceOfInformation(String sourceOfInformation) {
+		this.sourceOfInformation = sourceOfInformation;
+	}
+
+	@Column(name = "is_delete", length = 25)
+	public String getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(String isDelete) {
+		this.isDelete = isDelete;
+	}		
+	
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PoliticalChangesInformationSource.java
@@ -0,0 +1,97 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "political_changes_information_source")
+public class PoliticalChangesInformationSource extends BaseModel implements
+		Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//Fields
+	private Long sourceId;
+	private String sourceName;
+	private String description;
+	private Set<PoliticalChanges> localPoliticalChanges = new HashSet<PoliticalChanges>(0);
+	// Constructors
+
+	/** default constructor */
+	public PoliticalChangesInformationSource() {
+	}
+
+	/** minimal constructor */
+	public PoliticalChangesInformationSource(Long sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	//Full Constructor
+	public PoliticalChangesInformationSource(Long sourceId,String sourceName,String description,Set<PoliticalChanges> localPoliticalChanges) {
+		this.sourceId = sourceId;
+		this.sourceName = sourceName;
+		this.description = description;
+		this.localPoliticalChanges = localPoliticalChanges;
+	}
+
+	//Getters and Setters
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "source_id", unique = true, nullable = false)
+	public Long getSourceId() {
+		return sourceId;
+	}
+
+	public void setSourceId(Long sourceId) {
+		this.sourceId = sourceId;
+	}
+
+	@Column(name = "source_name", length = 25)
+	public String getSourceName() {
+		return sourceName;
+	}
+
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+
+	@Column(name = "description", length = 25)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "politicalChangesInformationSource")
+	public Set<PoliticalChanges> getLocalPoliticalChanges() {
+		return localPoliticalChanges;
+	}
+
+	public void setLocalPoliticalChanges(
+			Set<PoliticalChanges> localPoliticalChanges) {
+		this.localPoliticalChanges = localPoliticalChanges;
+	}
+	
+}
