c29ef3c458fed8a14c361bb2efb559f6b0426507
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-10 14:02:43 +0000

    updated Service for political change -- ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPoliticalChangesService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPoliticalChangesService.java
@@ -3,17 +3,20 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.PoliticalChangesVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface IPoliticalChangesService {
 	
 	public List<SelectOptionVO> getAllPoliticalInformationSources();
 	
-	public void savePoliticalChangeDataReceivedFromUser(PoliticalChangesVO politicalChangesVO);
+	public ResultStatus savePoliticalChangeDataReceivedFromUser(PoliticalChangesVO politicalChangesVo);	
 	
 	public List<PoliticalChangesVO> getAllPoliticalChanges(Long userId);
 	
 	public void deltePoliticalChangeBasedOnUser(Long politicalChangeId);
 	
 	public PoliticalChangesVO getExternalPersonDetails(Long politicalChangeId);
+	
+	public PoliticalChangesVO getDetailsBylocalPoliticalChangeId(Long politicalChangeId);
 }
