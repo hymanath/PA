60e13de685ba7fc039a1b6560b53d9a9ae2bbcb5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-10 13:59:52 +0000

    updated DAO for political change -- ravi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPoliticalChangesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPoliticalChangesDAO.java
@@ -19,4 +19,6 @@ public interface IPoliticalChangesDAO extends GenericDao<PoliticalChanges, Long>
 	public List<PoliticalChanges> findBySourceOfInformation(String sourceOfInformation);
 
 	public List getAllPoliticalChangesByUserId(Long userId);
+	
+	public List getAllPoliticalChangesByPoliticalChangeId(Long politicalChangeId);
 }
