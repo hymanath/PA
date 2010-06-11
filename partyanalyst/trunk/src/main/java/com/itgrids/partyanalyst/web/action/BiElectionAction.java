a50ace3621f182313de5f8234f01111da248b8b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-11 15:31:01 +0000

    UI for bi elections Page - Mohan

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/BiElectionAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/BiElectionAction.java
@@ -1,16 +1,22 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
+import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.BiElectionDistrictVO;
+import com.itgrids.partyanalyst.dto.MandalAllElectionResultsVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.service.IBiElectionPageService;
 import com.opensymphony.xwork2.Action;
@@ -32,7 +38,28 @@ public class BiElectionAction extends ActionSupport implements
 	HttpServletRequest request;
 	HttpServletResponse response;
 	HttpSession session;
+	private String task;
+	org.json.JSONObject jObj;
+	List<MandalAllElectionResultsVO> mandalAllElectionResultsVO;
 	
+	
+	public List<MandalAllElectionResultsVO> getMandalAllElectionResultsVO() {
+		return mandalAllElectionResultsVO;
+	}
+
+	public void setMandalAllElectionResultsVO(
+			List<MandalAllElectionResultsVO> mandalAllElectionResultsVO) {
+		this.mandalAllElectionResultsVO = mandalAllElectionResultsVO;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
 	public List<BiElectionDistrictVO> getDistrictsAndConsts() {
 		return districtsAndConsts;
 	}
@@ -63,9 +90,28 @@ public class BiElectionAction extends ActionSupport implements
 		
 		districtsAndConsts = biElectionPageService.getBiElectionConstituenciesDistrictWise();
 		
-		
-				
 		return Action.SUCCESS;
 	}
+	
+	public String getMandalsVotingTrendz() throws Exception
+	{
+		String param=null;			    
+		param = getTask();
+		
+		try {
+			jObj=new JSONObject(param);
+			System.out.println("jObj = "+jObj);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}	
+		
+		Long districtId = new Long(jObj.getString("districtId"));
+		Long constiId =  new Long(jObj.getString("constituencyId"));
+		
+		mandalAllElectionResultsVO = biElectionPageService.getAllPartysElectionResultsInAConstituencyMandalWise(new Long(232));
+		
+		return Action.SUCCESS;
+	}
+
 
 }
