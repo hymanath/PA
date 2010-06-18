3a804ea353bf1b9ded0098efc8cfbb790b85c5e9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-18 16:27:49 +0000

    Bi Election Page Booths wise Results code release - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBiElectionPageService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBiElectionPageService.java
@@ -8,13 +8,17 @@
 package com.itgrids.partyanalyst.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.itgrids.partyanalyst.dto.BiElectionDistrictVO;
 import com.itgrids.partyanalyst.dto.BiElectionResultsVO;
 import com.itgrids.partyanalyst.dto.ElectionResultsForMandalVO;
 import com.itgrids.partyanalyst.dto.MandalAllElectionResultsVO;
 import com.itgrids.partyanalyst.dto.MandalElectionResultVO;
+import com.itgrids.partyanalyst.dto.PartyVotesMarginResultsInMandal;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.dto.VotesMarginResultsMainVO;
+import com.itgrids.partyanalyst.excel.booth.BoothResultVO;
 
 public interface IBiElectionPageService {
 
@@ -25,4 +29,12 @@ public interface IBiElectionPageService {
 	public ElectionResultsForMandalVO getElectionResultsForMandalsInAConstituencyForAElection(List<Long> mandalIds,String electionType,String electionYear);
 	
 	public List<BiElectionResultsVO> getMandalWiseResultsForAConstituency(Long constituencyId);
+	
+	public Map<String,Map<Long,List<BoothResultVO>>> getPartyMarginResultsInAMandalForAllElections(Long tehsilId,Long partyId,String electionYear,String electionType,String partyType,Long rank) throws Exception;
+	
+	public Map<String,Map<String,Map<Long,List<BoothResultVO>>>> getPartyVotesMarginResults(Long constituencyId,Long mandalId,Long partyId) throws Exception;
+	
+	public PartyVotesMarginResultsInMandal getBoothWisePartyResultsForAMandal(Long constituencyId,Long mandalId,Long partyId);
+	
+	public VotesMarginResultsMainVO getVotesMarginResultsCompleteDetails(Long constituencyId,Long partyId);
 }
