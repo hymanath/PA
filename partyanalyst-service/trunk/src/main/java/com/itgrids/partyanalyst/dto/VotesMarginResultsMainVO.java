d0a3353bf418fefc98dfb342f2cd9e9de7095199
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-18 09:17:38 +0000

    Bi Election Page Booths wise Result VO's - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/VotesMarginResultsMainVO.java
@@ -0,0 +1,61 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on Jun 17,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class VotesMarginResultsMainVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long constituencyId;
+	private String constituencyName;
+	
+	private List<PartyVotesMarginResultsInMandal> partyVotesMarginResultsInMandal;
+    private ResultStatus resultStatus;
+	
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+
+	public String getConstituencyName() {
+		return constituencyName;
+	}
+
+	public void setConstituencyName(String constituencyName) {
+		this.constituencyName = constituencyName;
+	}
+
+	public List<PartyVotesMarginResultsInMandal> getPartyVotesMarginResultsInMandal() {
+		return partyVotesMarginResultsInMandal;
+	}
+
+	public void setPartyVotesMarginResultsInMandal(
+			List<PartyVotesMarginResultsInMandal> partyVotesMarginResultsInMandal) {
+		this.partyVotesMarginResultsInMandal = partyVotesMarginResultsInMandal;
+	}
+
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+	
+	
+
+}
