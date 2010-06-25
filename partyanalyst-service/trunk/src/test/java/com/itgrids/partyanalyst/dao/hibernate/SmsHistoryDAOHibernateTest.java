9287617a9fc5b63895405570089590e0cb2f4268
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-06-25 14:16:41 +0000

    Updated DAO Test Case for SMS Tracking -- ravi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SmsHistoryDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SmsHistoryDAOHibernateTest.java
@@ -1,25 +1,41 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ISmsHistoryDAO;
+import com.itgrids.partyanalyst.dao.ISmsTrackDAO;
 import com.itgrids.partyanalyst.model.SmsHistory;
+import com.itgrids.partyanalyst.model.SmsTrack;
 
 public class SmsHistoryDAOHibernateTest extends BaseDaoTestCase {
 	
-	private ISmsHistoryDAO SmsHistoryDAO;
+	private ISmsHistoryDAO smsHistoryDAO;
+	private ISmsTrackDAO smsTrackDAO;
+	
+	public ISmsTrackDAO getSmsTrackDAO() {
+		return smsTrackDAO;
+	}
 
+	public void setSmsTrackDAO(ISmsTrackDAO smsTrackDAO) {
+		this.smsTrackDAO = smsTrackDAO;
+	}
+
+		 
+	
+	public void testGetDataByHistoryId(){
+		java.util.List<SmsHistory> result = smsHistoryDAO.findByHistoryId(1l);
+		System.out.println(result.size());
+	}
+	
+	
 	public ISmsHistoryDAO getSmsHistoryDAO() {
-		return SmsHistoryDAO;
+		return smsHistoryDAO;
 	}
 
 	public void setSmsHistoryDAO(ISmsHistoryDAO smsHistoryDAO) {
-		SmsHistoryDAO = smsHistoryDAO;
+		this.smsHistoryDAO = smsHistoryDAO;
 	}
-	 
-	
-	public void testGetDataByHistoryId(){
-		java.util.List<SmsHistory> result = SmsHistoryDAO.findByHistoryId(1l);
-		System.out.println(result.size());
-	}
+
 }
