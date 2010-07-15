36909d95ce1b507bb4647bf4975a729fa42c4496
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-07-15 17:25:51 +0000

    BiElection Page changes in View - sai

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/helper/Constants.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/helper/Constants.java
@@ -11,4 +11,66 @@ public interface Constants {
 
 	public static final int MAJAR_BRAND = 10;
 	public static final int MINOR_BRAND = 2;
+	
+	public static final String SIRCILLA = "Sircilla";
+	public static final String SIRCILLA_TDP = "Putta Kishore Kumar";
+	public static final String SIRCILLA_TRS = "K.Taraka Ramarao (KTR)";
+	public static final String SIRCILLA_INC = "K.K.Mahender Reddy";
+	
+	public static final String SIRPUR = "Sirpur";
+	public static final String SIRPUR_TDP = "Palvai Rajyalaxmi";
+	public static final String SIRPUR_TRS = "Kaveti Sammaiah";
+	public static final String SIRPUR_INC = "Allola Indrakaran Reddy";
+	
+	public static final String CHENNUR = "Chennur";
+	public static final String CHENNUR_TDP = "Boda Janardhan";
+	public static final String CHENNUR_TRS = "Nallala Odelu";
+	public static final String CHENNUR_INC = "G.Vinod";
+	
+	public static final String MANCHERIAL = "Mancherial";
+	public static final String MANCHERIAL_TDP = "Gone Hanmanth Rao";
+	public static final String MANCHERIAL_TRS = "Aravind Reddy Gaddam";
+	public static final String MANCHERIAL_INC = "Rachakonda Krishna Rao";
+	
+	public static final String YELLAREDDY = "Yellareddy";
+	public static final String YELLAREDDY_TDP = "Surender Jajala";
+	public static final String YELLAREDDY_TRS = "Eanugu Ravinder Reddy";
+	public static final String YELLAREDDY_INC = "Ali Shabber Mohammed";
+	
+	public static final String NIZAMABAD_URBAN = "Nizamabad Urban";
+	public static final String NIZAMABAD_URBAN_TDP = "Arikela Narsa Reddy";
+	public static final String NIZAMABAD_URBAN_BJP = "Endala Lakshminarayana";
+	public static final String NIZAMABAD_URBAN_INC = "Dharmapuri Srinivas";
+	
+	public static final String KORATLA = "Koratla";
+	public static final String KORATLA_TDP = "Shikari Vishwanatham";
+	public static final String KORATLA_TRS = "Kalvakuntla Vidyasagar Rao";
+	public static final String KORATLA_INC = "Juvvadi Rathnakar Rao";
+	
+	public static final String DHARMAPURI = "Dharmapuri";
+	public static final String DHARMAPURI_TDP = "Nakka Narayana";
+	public static final String DHARMAPURI_TRS = "Eshwar Koppula";
+	public static final String DHARMAPURI_INC = "Adluri Laxman Kumar";
+	
+	public static final String VEMULAWADA = "Vemulawada";
+	public static final String VEMULAWADA_TDP = "Gandra Venkateshwar Rao";
+	public static final String VEMULAWADA_TRS = "Ramesh Chennamaneni";
+	public static final String VEMULAWADA_INC = "Aadi Srinivas";
+	
+	public static final String HUZURABAD = "Huzurabad";
+	public static final String HUZURABAD_TDP = "Damodar Reddy Muddasani";
+	public static final String HUZURABAD_TRS = "Eatela Rajender";
+	public static final String HUZURABAD_INC = "Vakulabharanam Krishna Mohan Rao";
+	
+	public static final String SIDDIPET = "Siddipet";
+	public static final String SIDDIPET_TDP = "P.Babumohan";
+	public static final String SIDDIPET_TRS = "Thanneeru Harish Rao";
+	public static final String SIDDIPET_INC = "Taduri Srinivas Goud";
+	
+	public static final String WARANGAL_WEST = "Warangal West";
+	public static final String WARANGAL_WEST_TDP = "Vem Narender Reddy";
+	public static final String WARANGAL_WEST_TRS = "Dasyam Vinay Bhaskar";
+	public static final String WARANGAL_WEST_INC = "Kondapally Dayasagar Rao";
+	
+	
 }
