b653a37d044f6574955329a2bff00cbca17388e5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-07-17 11:42:53 +0000

    BiElection Page changes  - sai

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/service/ConstituencyPageServiceTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/service/ConstituencyPageServiceTest.java
@@ -66,7 +66,7 @@ public class ConstituencyPageServiceTest extends BaseDaoTestCase{
 	}*/
 	
 	public void testGetConstituencyElecResults(){
-		constituencyPageService.getConstituencyElecResults(403l, "2004");
+		constituencyPageService.getConstituencyElecResults(403l, "2004",true);
 	}
 
 }
