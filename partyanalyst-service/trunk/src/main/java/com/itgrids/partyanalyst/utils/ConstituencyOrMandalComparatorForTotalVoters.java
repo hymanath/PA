c2405dae7c9f336c6cc1a767b38e806394385fad
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-07-18 06:45:43 +0000

    Mandalwise results chart based on strength of mandal - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ConstituencyOrMandalComparatorForTotalVoters.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.utils;
+
+import java.util.Comparator;
+
+import com.itgrids.partyanalyst.dto.ConstituencyOrMandalWiseElectionVO;
+
+public class ConstituencyOrMandalComparatorForTotalVoters implements
+		Comparator<ConstituencyOrMandalWiseElectionVO> {
+
+	public int compare(ConstituencyOrMandalWiseElectionVO a,ConstituencyOrMandalWiseElectionVO b) {
+		return b.getTotalVoters().compareTo(a.getTotalVoters());
+	}
+
+}
