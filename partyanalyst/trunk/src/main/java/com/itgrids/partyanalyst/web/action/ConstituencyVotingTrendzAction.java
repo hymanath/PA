95df6c09c172d5d3cdc4f06962b43dd7fff98ec8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-07-20 07:23:15 +0000

    BiElection Page chart property changes - sai

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ConstituencyVotingTrendzAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ConstituencyVotingTrendzAction.java
@@ -614,7 +614,7 @@ implements ServletRequestAware, ServletResponseAware, ServletContextAware{
 			  String chartPath = context.getRealPath("/")+ "charts\\" + chartName;
 			  
 			  chartColorsAndDataSetVO = createDatasetForChart(electionResList,selectdElections);
-			  ChartProducer.createLineChart(chartTitle, "Election", "Percentages", (DefaultCategoryDataset)chartColorsAndDataSetVO.getDataSet(), chartPath,400,950,new ArrayList<Color>(chartColorsAndDataSetVO.getColorsSet()),true);
+			  ChartProducer.createLineChartWithThickness(chartTitle, "Election", "Percentages", (DefaultCategoryDataset)chartColorsAndDataSetVO.getDataSet(), chartPath,500,1000,new ArrayList<Color>(chartColorsAndDataSetVO.getColorsSet()),true);
 			  
 			  chartColorsAndDataSetVO.setChartName(chartName);
 			  if(votesSharing != null && votesSharing.size() > 0)
@@ -1377,7 +1377,7 @@ implements ServletRequestAware, ServletResponseAware, ServletContextAware{
 			  String chartName = "constituencyElectionsResults"+"_"+constiName+"_"+constiId+"_"+chartNam+".png";
 			  String chartPath = context.getRealPath("/")+ "charts\\" + chartName;
 			  chartColorsAndDataSetVO = createDatasetForChart(electnDataList,partys,electionResList,new Boolean(includeAlliance));
-			  ChartProducer.createLineChart(chartTitle, "Election", "Percentages", (DefaultCategoryDataset)chartColorsAndDataSetVO.getDataSet(), chartPath,400,950,new ArrayList<Color>(chartColorsAndDataSetVO.getColorsSet()),true);
+			  ChartProducer.createLineChartWithThickness(chartTitle, "Election", "Percentages", (DefaultCategoryDataset)chartColorsAndDataSetVO.getDataSet(), chartPath,500,950,new ArrayList<Color>(chartColorsAndDataSetVO.getColorsSet()),true);
 			  
 			  chartColorsAndDataSetVO.setChartName(chartName);
 		}
