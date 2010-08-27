93d9eb2a0f54b4302d32c01d483626c8cff0f347
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-08-27 06:34:20 +0000

    modified by raghav

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EducationalQualificationsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EducationalQualificationsDAO.java
@@ -3,10 +3,10 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import org.appfuse.dao.GenericDao;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
-import com.itgrids.partyanalyst.dao.IEducationalQualifications;
+import com.itgrids.partyanalyst.dao.IEducationalQualificationsDAO;
 import com.itgrids.partyanalyst.model.EducationalQualifications;
 
-public class EducationalQualificationsDAO extends GenericDaoHibernate<EducationalQualifications, Long> implements IEducationalQualifications  {
+public class EducationalQualificationsDAO extends GenericDaoHibernate<EducationalQualifications, Long> implements IEducationalQualificationsDAO  {
 
 	public EducationalQualificationsDAO() {
 		super(EducationalQualifications.class);
