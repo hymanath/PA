e09bec5201b5f4c86fba98d2a9200cce83b1602f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-03 10:25:13 +0000

    Cadre Management Search/Sms service for multiselct options - sai

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/helper/CadreSMSVO.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/helper/CadreSMSVO.java
@@ -9,12 +9,19 @@ import com.itgrids.partyanalyst.dto.SelectOptionVO;
 public class CadreSMSVO implements Serializable{
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private List<SelectOptionVO> states = new ArrayList<SelectOptionVO>();
 	private List<SelectOptionVO> districts = new ArrayList<SelectOptionVO>();
 	private List<SelectOptionVO> constituencies = new ArrayList<SelectOptionVO>();
 	private List<SelectOptionVO> mandals = new ArrayList<SelectOptionVO>();
 	private List<SelectOptionVO> villages = new ArrayList<SelectOptionVO>();
 	private List<SelectOptionVO> regions = new ArrayList<SelectOptionVO>();
+	private List<SelectOptionVO> partyCommitee = new ArrayList<SelectOptionVO>();
+	private List<SelectOptionVO> commiteeDesignation = new ArrayList<SelectOptionVO>();
 	
 	public List<SelectOptionVO> getStates() {
 		return states;
@@ -51,6 +58,18 @@ public class CadreSMSVO implements Serializable{
 	}
 	public void setRegions(List<SelectOptionVO> regions) {
 		this.regions = regions;
+	}
+	public List<SelectOptionVO> getPartyCommitee() {
+		return partyCommitee;
+	}
+	public void setPartyCommitee(List<SelectOptionVO> partyCommitee) {
+		this.partyCommitee = partyCommitee;
+	}
+	public List<SelectOptionVO> getCommiteeDesignation() {
+		return commiteeDesignation;
+	}
+	public void setCommiteeDesignation(List<SelectOptionVO> commiteeDesignation) {
+		this.commiteeDesignation = commiteeDesignation;
 	}
 	
 	
