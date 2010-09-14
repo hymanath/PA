b690920f4869854bd75401c66fff466fd22cdf91
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-14 15:17:25 +0000

    District Page Changes for Multi states - Sai

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyDAOHibernateTest.java
@@ -1,11 +1,14 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
 import com.itgrids.partyanalyst.dao.IPartyDAO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.model.Party;
 import com.itgrids.partyanalyst.utils.IConstants;
 
@@ -21,8 +24,34 @@ public class PartyDAOHibernateTest extends BaseDaoTestCase{
 		this.partyDAO = partyDAO;
 	}
 	
+	/*
 	public void testGetAllParties(){
 		List<Party> parties = partyDAO.findByShortNames(IConstants.STATIC_PARTIES);
 		Assert.assertEquals(2, parties.size());
+	}*/
+	
+	@Test
+	public void testFindPartysInState(){
+		
+		List<SelectOptionVO> partiesLst = new ArrayList<SelectOptionVO>();
+		List<Party> party = partyDAO.findStaticPartiesByRecognization("NP",IConstants.NATIONAL_STATIC_PARTIES);
+		for(Party part:party){
+			//System.out.println(" Party Id :" + part.getPartyId() + "  " + " Party :" + part.getLongName() + " Short Name :" + part.getShortName());
+			
+			SelectOptionVO option = new SelectOptionVO();
+			option.setId(part.getPartyId());
+			option.setName(part.getLongName());
+			
+			partiesLst.add(option);
+		}
+		List<SelectOptionVO> partiesLst1 = new ArrayList<SelectOptionVO>();
+		partiesLst1.add(new SelectOptionVO(1L,"a"));
+		partiesLst1.add(new SelectOptionVO(2L,"b"));
+		
+		partiesLst.addAll(partiesLst1);
+		
+		for(SelectOptionVO optn:partiesLst){
+			System.out.println(" ID :" + optn.getId());
+		}
 	}
 }
