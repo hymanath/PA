e5aa1d5512e25d8cff579ea9e4dc04cf7a35f03d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-23 15:03:15 +0000

    Local Body Elections Service Implementation - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ILocalBodyElectionService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ILocalBodyElectionService.java
@@ -7,7 +7,10 @@
  */
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.LocalBodyElectionResultsVO;
+import com.itgrids.partyanalyst.dto.PartyElectionResultsInConstituencyVO;
 
 /*
  * Interface for Local Body Elections 
@@ -15,4 +18,8 @@ import com.itgrids.partyanalyst.dto.LocalBodyElectionResultsVO;
 public interface ILocalBodyElectionService {
 
 	public LocalBodyElectionResultsVO getLocalBodyElectionResultsByLocalBodyTypeAndYear(Long localBodyId,Long stateId);
+	
+	public List<PartyElectionResultsInConstituencyVO> getLocalBodyElectionResultsInAnElection(Long localBodyId,Long stateId,Long electionId);
+	
+	public List<PartyElectionResultsInConstituencyVO> getLocalBodyElectionResultsForAPartyInAnElection(Long localBodyId,Long stateId,Long electionId,Long partyId);
 }
