36dab86f488abb3f57d1ba1540a1005e76e25d21
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-27 05:54:32 +0000

    Local Body Elections isuues fix - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/TehsilPartyInfoVOBySeatsWonComparator.java
@@ -0,0 +1,13 @@
+package com.itgrids.partyanalyst.utils;
+
+import java.util.Comparator;
+
+import com.itgrids.partyanalyst.dto.TeshilPartyInfoVO;
+
+public class TehsilPartyInfoVOBySeatsWonComparator implements Comparator<TeshilPartyInfoVO> {
+
+	public int compare(TeshilPartyInfoVO a, TeshilPartyInfoVO b) {
+		return b.getPartyWonSeats().compareTo(a.getPartyWonSeats());
+	}
+
+}
