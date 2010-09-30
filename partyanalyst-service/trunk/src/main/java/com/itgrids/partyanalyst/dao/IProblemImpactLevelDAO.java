b465633fc6316450931c7325bb2fab37b703246c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-30 08:30:59 +0000

    Problem Management Module Updates - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemImpactLevelDAO.java
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (c) 2009 IT Grids India LTD.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on September 29, 2010
+ */
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ProblemImpactLevel;
+
+public interface IProblemImpactLevelDAO extends GenericDao<ProblemImpactLevel, Long> {
+
+}
