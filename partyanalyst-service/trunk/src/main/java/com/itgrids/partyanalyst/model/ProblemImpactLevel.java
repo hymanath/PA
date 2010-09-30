7353f40432a7bac65029e6031fac17494fe8a9db
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-09-30 08:31:17 +0000

    Problem Management Module Updates - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ProblemImpactLevel.java
@@ -0,0 +1,94 @@
+/* 
+ * Copyright (c) 2009 IT Grids India LTD.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on September 29, 2010
+ */
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Sai Krishna Basetti
+ *
+ */
+@Entity
+@Table(name="problem_impact_level")
+public class ProblemImpactLevel extends BaseModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long problemImpactLevelId;
+	private String problemImpactLevel;
+	private String description;
+	
+	private Set<ProblemLocation> problemHistory = new HashSet<ProblemLocation>(0);
+	
+	//Default Constructor
+	public ProblemImpactLevel(){
+		
+	}
+
+	//Parameterized Constructor
+	public ProblemImpactLevel(String problemImpactLevel, String description,
+			Set<ProblemLocation> problemHistory) {
+		this.problemImpactLevel = problemImpactLevel;
+		this.description = description;
+		this.problemHistory = problemHistory;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="problem_impact_level_id", unique=true, nullable=false)
+	public Long getProblemImpactLevelId() {
+		return problemImpactLevelId;
+	}
+
+	public void setProblemImpactLevelId(Long problemImpactLevelId) {
+		this.problemImpactLevelId = problemImpactLevelId;
+	}
+
+	@Column(name="problem_impact_level",length=25)
+	public String getProblemImpactLevel() {
+		return problemImpactLevel;
+	}
+
+	public void setProblemImpactLevel(String problemImpactLevel) {
+		this.problemImpactLevel = problemImpactLevel;
+	}
+
+	@Column(name="description",length=25)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "problemImpactLevel")
+	public Set<ProblemLocation> getProblemHistory() {
+		return problemHistory;
+	}
+
+	public void setProblemHistory(Set<ProblemLocation> problemHistory) {
+		this.problemHistory = problemHistory;
+	}
+
+}
