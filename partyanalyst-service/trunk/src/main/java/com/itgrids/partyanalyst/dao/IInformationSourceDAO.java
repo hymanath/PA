c01305455e13bd3f0d257767152ecf3f9851d09d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-10-05 18:43:40 +0000

    Problem Management Module Updates - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInformationSourceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInformationSourceDAO.java
@@ -7,11 +7,13 @@
  */
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.InformationSource;
 
 public interface IInformationSourceDAO extends GenericDao<InformationSource, Long> {
 
-	
+	public List<InformationSource> getInformationSourceByType(String sourceType);
 }
