a17b00b681d788ab585541e29d26a8bfafb58907
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-10-12 15:10:07 +0000

    Rectified bug, by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/InfluencingPeopleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/InfluencingPeopleDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IInfluencingPeopleDAO;
 import com.itgrids.partyanalyst.model.InfluencingPeople;
@@ -38,10 +39,21 @@ public class InfluencingPeopleDAO extends GenericDaoHibernate<InfluencingPeople,
 		return getHibernateTemplate().find("from InfluencingPeople model where model.userAddress.constituency.constituencyId = ?",constituencyId);
 	}
 	
+	@SuppressWarnings("unchecked")
+	/*public List<Object[]> getDetailsByInfluencingPersonId(Long influencingPersonId){
+		return getHibernateTemplate().find("select model.firstName,model.userAddress.district.districtName from InfluencingPeople model where model.influencingPeopleId = ?", influencingPersonId);
+	}*/
+	public List<Object[]> getDetailsByInfluencingPersonId(Long influencingPersonId){
+		return getHibernateTemplate().find("select model.firstName, model.middleName, model.lastName, model.fatherOrSpouseName, model.gender,"+
+					"model.phoneNo, model.email, model.occupation, model.party.partyId, model.caste, model.influencingPeoplePosition.influencingPeoplePositionId, " +
+					"model.influencingScope,model.userAddress.userAddressId from "+
+					"InfluencingPeople model where model.influencingPeopleId = ?", influencingPersonId);
+	}
 	
-	
-	
-	
-	
-	
+	public Integer deleteInfluencingPeopleById(Long influencingPeopleId){
+		Query queryObject = getSession().createQuery("delete from InfluencingPeople model where model.influencingPeopleId = ?");
+		queryObject.setParameter(0, influencingPeopleId);
+		return queryObject.executeUpdate();
+		
+	}
 }
