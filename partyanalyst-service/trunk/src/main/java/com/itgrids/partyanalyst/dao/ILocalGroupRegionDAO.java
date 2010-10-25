b8722a39fba91b57c921628531cd181bd0ce9d73
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-10-25 14:42:32 +0000

    Local User Groups Implementation - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocalGroupRegionDAO.java
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 25, 2010
+ */
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.LocalGroupRegion;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+public interface ILocalGroupRegionDAO extends GenericDao<LocalGroupRegion, Long> {
+
+}
