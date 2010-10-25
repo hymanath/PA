86ca26110d74c68d0185bc386105a20a36512584
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-10-25 14:46:43 +0000

    Local User Groups Implementation - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/LocalGroupRegion.java
@@ -0,0 +1,215 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 25, 2010
+ */
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+@Entity
+@Table(name = "local_group_region")
+public class LocalGroupRegion extends BaseModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long localGroupRegionId;
+	private String description;
+	private Date updatedDate;
+	private String groupRegionScope;
+	private State state;
+	private District district;
+	private Constituency constituency;
+	private Tehsil tehsil;
+	private Township township;
+	private LocalElectionBody localBody;
+	private Constituency ward;
+	private Hamlet hamlet;
+	
+	//Default Constructor
+	public LocalGroupRegion(){
+		
+	}
+	
+	//Parameterized Constructor
+	public LocalGroupRegion(String description, Date updatedDate,
+			String groupRegionScope, State state, District district,
+			Constituency constituency, Tehsil tehsil, Township township,
+			LocalElectionBody localBody, Constituency ward, Hamlet hamlet) {
+		super();
+		this.description = description;
+		this.updatedDate = updatedDate;
+		this.groupRegionScope = groupRegionScope;
+		this.state = state;
+		this.district = district;
+		this.constituency = constituency;
+		this.tehsil = tehsil;
+		this.township = township;
+		this.localBody = localBody;
+		this.ward = ward;
+		this.hamlet = hamlet;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "local_group_region_id", unique = true, nullable = false)
+	public Long getLocalGroupRegionId() {
+		return localGroupRegionId;
+	}
+
+	public void setLocalGroupRegionId(Long localGroupRegionId) {
+		this.localGroupRegionId = localGroupRegionId;
+	}
+
+	@Column(name = "description", length = 300)
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "updated_date")
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Column(name = "group_region_scope", length = 50)
+	public String getGroupRegionScope() {
+		return groupRegionScope;
+	}
+
+	public void setGroupRegionScope(String groupRegionScope) {
+		this.groupRegionScope = groupRegionScope;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="state_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="district_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public District getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(District district) {
+		this.district = district;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="constituency_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Constituency getConstituency() {
+		return constituency;
+	}
+
+	public void setConstituency(Constituency constituency) {
+		this.constituency = constituency;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="tehsil_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Tehsil getTehsil() {
+		return tehsil;
+	}
+
+	public void setTehsil(Tehsil tehsil) {
+		this.tehsil = tehsil;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="township_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Township getTownship() {
+		return township;
+	}
+
+	public void setTownship(Township township) {
+		this.township = township;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="local_body_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public LocalElectionBody getLocalBody() {
+		return localBody;
+	}
+
+	public void setLocalBody(LocalElectionBody localBody) {
+		this.localBody = localBody;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="ward_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Constituency getWard() {
+		return ward;
+	}
+
+	public void setWard(Constituency ward) {
+		this.ward = ward;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="hamlet_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Hamlet getHamlet() {
+		return hamlet;
+	}
+
+	public void setHamlet(Hamlet hamlet) {
+		this.hamlet = hamlet;
+	}
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/StaticUserDesignation.java
@@ -0,0 +1,111 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 25, 2010
+ */
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+@Entity
+@Table(name = "static_user_designation")
+public class StaticUserDesignation extends BaseModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long staticUserDesignationId;
+	private String designationType;
+	private String description;
+	private Date updatedDate;
+	private StaticLocalGroup staticLocalGroup;
+	
+	//Default Constructor
+	public StaticUserDesignation(){
+		
+	}
+	
+	//parameterized constructor
+	public StaticUserDesignation(String designationType, String description,
+			Date updatedDate, StaticLocalGroup staticLocalGroup) {
+		super();
+		this.designationType = designationType;
+		this.description = description;
+		this.updatedDate = updatedDate;
+		this.staticLocalGroup = staticLocalGroup;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "static_user_designation_id", unique = true, nullable = false)
+	public Long getStaticUserDesignationId() {
+		return staticUserDesignationId;
+	}
+	public void setStaticUserDesignationId(Long staticUserDesignationId) {
+		this.staticUserDesignationId = staticUserDesignationId;
+	}
+	
+	@Column(name = "designation_type", length = 50)
+	public String getDesignationType() {
+		return designationType;
+	}
+	public void setDesignationType(String designationType) {
+		this.designationType = designationType;
+	}
+	
+	@Column(name = "description", length = 50)
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "updated_date")
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="static_local_group_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public StaticLocalGroup getStaticLocalGroup() {
+		return staticLocalGroup;
+	}
+	public void setStaticLocalGroup(StaticLocalGroup staticLocalGroup) {
+		this.staticLocalGroup = staticLocalGroup;
+	}
+	
+
+}
