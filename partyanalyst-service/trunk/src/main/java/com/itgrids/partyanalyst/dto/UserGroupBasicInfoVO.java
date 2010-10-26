2cadbf53e81dfa244cf43f6f4e8f4e91a230acc4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-10-26 12:00:01 +0000

    Local User Groups Info By Sivakumar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserGroupBasicInfoVO.java
@@ -0,0 +1,30 @@
+package com.itgrids.partyanalyst.dto;
+
+public class UserGroupBasicInfoVO {
+
+	private Long locationsCount;
+	private Long groupsCount;
+	private String areaType;
+	
+	public Long getLocationsCount() {
+		return locationsCount;
+	}
+	public void setLocationsCount(Long locationsCount) {
+		this.locationsCount = locationsCount;
+	}
+	public Long getGroupsCount() {
+		return groupsCount;
+	}
+	public void setGroupsCount(Long groupsCount) {
+		this.groupsCount = groupsCount;
+	}
+	public String getAreaType() {
+		return areaType;
+	}
+	public void setAreaType(String areaType) {
+		this.areaType = areaType;
+	}
+	
+	
+	
+}
