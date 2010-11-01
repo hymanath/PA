27ac03327c74b46327872b9d8903c11096c6f120
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-01 16:34:56 +0000

    Influencing People Service - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/InfluencingPeopleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/InfluencingPeopleDAO.java
@@ -59,4 +59,170 @@ public class InfluencingPeopleDAO extends GenericDaoHibernate<InfluencingPeople,
 		return queryObject.executeUpdate();
 		
 	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInConstituency(Long userId,
+			Long constituencyId) {
+		Object[] params = {userId,constituencyId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.constituency.constituencyId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInDistrict(Long userId,
+			Long districtId) {
+		Object[] params = {userId,districtId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.district.districtId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInLocalBodys(Long userId,
+			Long localBodyId) {
+		Object[] params = {userId,localBodyId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.localElectionBody.localElectionBodyId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInState(Long userId,
+			Long stateId) {
+		Object[] params = {userId,stateId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.state.stateId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInTehsil(Long userId,
+			Long tehsilId) {
+		Object[] params = {userId,tehsilId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.tehsil.tehsilId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInVillage(Long userId,
+			Long hamletId) {
+		Object[] params = {userId,hamletId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.hamlet.hamletId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInWard(Long userId, Long wardId) {
+		Object[] params = {userId,wardId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId) from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.ward.constituencyId = ?",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInConstituenciesByDistrict(
+			Long userId, Long districtId) {
+		Object[] params = {userId,districtId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.constituency.constituencyId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.district.districtId = ? and "+
+				"model.userAddress.constituency is not null group by model.userAddress.constituency.constituencyId",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInDistrictsByState(Long userId,
+			Long stateId) {
+		Object[] params = {userId,stateId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.district.districtId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.state.stateId = ? and "+
+				"model.userAddress.district is not null group by model.userAddress.district.districtId",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInLocalBodysByConstituency(
+			Long userId, Long constituencyId) {
+		Object[] params = {userId,constituencyId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.localElectionBody.localElectionBodyId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.constituency.constituencyId = ? and "+
+				"model.userAddress.localElectionBody is not null group by model.userAddress.localElectionBody.localElectionBodyId",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInTehsilsByConstituency(
+			Long userId, Long constituencyId) {
+		Object[] params = {userId,constituencyId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.tehsil.tehsilId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.constituency.constituencyId = ? and "+
+				"model.userAddress.tehsil is not null group by model.userAddress.tehsil.tehsilId",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInVillagesByTehsil(Long userId,
+			Long tehsilId) {
+		Object[] params = {userId,tehsilId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.hamlet.hamletId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.tehsil.tehsilId = ? and "+
+				"model.userAddress.hamlet is not null group by model.userAddress.hamlet.hamletId",params);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfInfluencingPeopleInWardsByLocalBody(Long userId,
+			Long localBodyId) {
+		Object[] params = {userId,localBodyId};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.userAddress.ward.constituencyId from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.userAddress.localElectionBody.localElectionBodyId = ? and "+
+				"model.userAddress.ward is not null group by model.userAddress.ward.constituencyId",params);
+	}
+
+	public List getTotalInfluencingPeopleDetailsInConstituency(Long userId,
+			Long constituencyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInDistrict(Long userId,
+			Long districtId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInLocalBodys(Long userId,
+			Long localBodyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInState(Long userId,
+			Long stateId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInTehsil(Long userId,
+			Long tehsilId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInVillage(Long userId,
+			Long hamletId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getTotalInfluencingPeopleDetailsInWard(Long userId, Long wardId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalInfluencingPeopleCountByInfluencingScope(Long userId) {
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.influencingScope from InfluencingPeople model where "+
+				"model.registration.registrationId = ? group by model.influencingScope",userId);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List getTotalInfluencingPeopleCountByInfluencingScope(Long userId,
+			String influencingScope) {
+		Object[] params = {userId,influencingScope};
+		return getHibernateTemplate().find("select count(model.influencingPeopleId),model.influencingScopeValue from InfluencingPeople model where "+
+				"model.registration.registrationId = ? and model.influencingScope = ? group by model.influencingScopeValue",params);
+	}
+
+	
 }
