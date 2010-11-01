f34381d7d07bffde98530f99c8e749f4e1ea5005
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-01 16:35:35 +0000

    Influencing People Service - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IInfluencingPeopleService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IInfluencingPeopleService.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+import com.itgrids.partyanalyst.dto.ConstituencyManagementDataVO;
+import com.itgrids.partyanalyst.dto.ConstituencyManagementInfluenceScopeOverviewVO;
 import com.itgrids.partyanalyst.dto.InfluencingPeopleBeanVO;
 import com.itgrids.partyanalyst.dto.InfluencingPeopleVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
@@ -23,4 +25,11 @@ public interface IInfluencingPeopleService {
 			Map<String, Long> influRangeAndValueMap1);
 	public InfluencingPeopleBeanVO getDetailsByInfluencingPersonId(Long influencingPersonId);
 	public Integer deleteInfluencingPeople(Long influencingPeopleId);
+	
+	public ConstituencyManagementDataVO getInfluencingPeopleOverviewDetails(Long userId);
+	
+	public List<ConstituencyManagementInfluenceScopeOverviewVO> getInfluencingPeopleByInfluenceScope(Long userId);
+	public ConstituencyManagementDataVO getStateRegionAndSubRegionsInfluencingPeopleByUserAndLocation(Long userId,Long stateId,Boolean isScopeData);
+	public ConstituencyManagementDataVO getDistrictRegionAndSubRegionsInfluencingPeopleByUserAndLocation(Long userId,Long districtId,Boolean isScopeData);
+	public ConstituencyManagementDataVO getConstituencyRegionAndSubRegionsInfluencingPeopleByUserAndLocation(Long userId,Long constituencyId,Boolean isScopeData);
 }
