a8332562f098929dbea75a85c0cfb6791d083f63
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-02 15:21:26 +0000

    Influencing People Service impl - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/InfluencingPeopleDetailsVO.java
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 29, 2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+public class InfluencingPeopleDetailsVO extends ResultStatus implements
+		Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long regionId;
+	private String regionName;
+	private String regionType;
+	
+	private List<InfluencingPeopleBeanVO> influencingPeopleDetails;
+
+	public Long getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(Long regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public String getRegionType() {
+		return regionType;
+	}
+
+	public void setRegionType(String regionType) {
+		this.regionType = regionType;
+	}
+
+	public List<InfluencingPeopleBeanVO> getInfluencingPeopleDetails() {
+		return influencingPeopleDetails;
+	}
+
+	public void setInfluencingPeopleDetails(
+			List<InfluencingPeopleBeanVO> influencingPeopleDetails) {
+		this.influencingPeopleDetails = influencingPeopleDetails;
+	}
+
+}
