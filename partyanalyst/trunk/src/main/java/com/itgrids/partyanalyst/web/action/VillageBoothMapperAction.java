469c8850bad52af6e1d6ab4d76ed5c486ab15855
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-02 12:19:11 +0000

    village's covered under a booth -- Ravi Kiran.Y

---
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VillageBoothMapperAction.java
@@ -0,0 +1,108 @@
+package com.itgrids.partyanalyst.web.action;
+
+import java.text.ParseException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.util.ServletContextAware;
+import org.json.JSONObject;
+
+import com.itgrids.partyanalyst.service.IBoothMapperService;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class VillageBoothMapperAction extends ActionSupport implements
+ServletContextAware, ServletResponseAware, ServletRequestAware{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5313524113544783868L;
+	
+	private ServletContext context;
+	HttpServletRequest request;
+	HttpServletResponse response;
+	HttpSession session;
+	private String task;
+	org.json.JSONObject jObj;
+	
+	private IBoothMapperService boothMapperService;
+	
+	
+	public IBoothMapperService getBoothMapperService() {
+		return boothMapperService;
+	}
+	public void setBoothMapperService(IBoothMapperService boothMapperService) {
+		this.boothMapperService = boothMapperService;
+	}
+	public ServletContext getContext() {
+		return context;
+	}
+	public void setContext(ServletContext context) {
+		this.context = context;
+	}
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+	public HttpSession getSession() {
+		return session;
+	}
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public void setServletResponse(HttpServletResponse response) {
+        this.response = response;
+	}
+	public void setServletContext(ServletContext context) {
+		this.context = context;		
+	}	
+	
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public org.json.JSONObject getJObj() {
+		return jObj;
+	}
+	public void setJObj(org.json.JSONObject obj) {
+		jObj = obj;
+	}
+	
+	public String execute(){
+		
+		return SUCCESS;
+	}
+	
+	public String setDataForVillageBoothRelation(){
+		try {
+			jObj = new JSONObject(getTask());
+			System.out.println(jObj);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		Long districtID = new Long(jObj.getString("districtId"));
+		Long electionYear =  new Long(jObj.getString("electionYear"));
+		boothMapperService.setDataForVillageBoothRelation(districtID,electionYear);
+		return SUCCESS;
+	}
+}
