3d0bc7a9f6a17381e8b32fac4594cab470190e42
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-03 15:46:52 +0000

    Influencing People Service impl - Sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ConstituencyManagementRegionWiseCompleteDataVO.java
@@ -0,0 +1,42 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 29, 2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+public class ConstituencyManagementRegionWiseCompleteDataVO implements
+		Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<SelectOptionVO> regionsList;
+	private List<ConstituencyManagementRegionWiseOverviewVO> regionWiseOverview;
+	
+	public List<SelectOptionVO> getRegionsList() {
+		return regionsList;
+	}
+	public void setRegionsList(List<SelectOptionVO> regionsList) {
+		this.regionsList = regionsList;
+	}
+	public List<ConstituencyManagementRegionWiseOverviewVO> getRegionWiseOverview() {
+		return regionWiseOverview;
+	}
+	public void setRegionWiseOverview(
+			List<ConstituencyManagementRegionWiseOverviewVO> regionWiseOverview) {
+		this.regionWiseOverview = regionWiseOverview;
+	}
+
+}
