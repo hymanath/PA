1649576d5414b21cd7b01bfbb4f7b5b9a20eae12
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-09 14:20:26 +0000

    Influencing People Service impl - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PersonalUserGroupDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PersonalUserGroupDAO.java
@@ -91,6 +91,17 @@ public class PersonalUserGroupDAO extends GenericDaoHibernate<PersonalUserGroup,
 	}
 	
 	@SuppressWarnings("unchecked")
+	public List getCompleteUserGroupDetailsForAGroup(Long groupId) {
+		return getHibernateTemplate().find("select model.personalUserGroupId,model.groupName,model.description,model.createdDate,"+
+				"model.staticLocalGroup.staticLocalGroupId,model.staticLocalGroup.groupType,model.staticLocalGroup.description,"+
+				"model.localGroupRegion.groupRegionScope,model.localGroupRegion.state,model.localGroupRegion.district,"+
+				"model.localGroupRegion.constituency,model.localGroupRegion.tehsil,model.localGroupRegion.township,"+
+				"model.localGroupRegion.localBody,model.localGroupRegion.ward,model.localGroupRegion.hamlet "+
+				"from PersonalUserGroup model where model.staticGroup is null and model.myGroup is null and "+
+				"model.personalUserGroupId = ?",groupId);
+	}
+	
+	@SuppressWarnings("unchecked")
 	public List getSubGroupsCompleteDetailsForMyGroup(Long groupId,Long userId){
 		Object[] params = {groupId,userId};
 		return getHibernateTemplate().find("select model.personalUserGroupId,model.groupName,model.description,model.createdDate from PersonalUserGroup model where model.staticGroup is null and model.myGroup is not null and model.parentGroupId.personalUserGroupId = ? and model.createdUserId.registrationId = ?",params);
@@ -101,8 +112,8 @@ public class PersonalUserGroupDAO extends GenericDaoHibernate<PersonalUserGroup,
 		Object[] params = {userId,groupName};
 		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where model.createdUserId.registrationId = ? and  model.groupName = ?",params);
 	}
-	
-	public List findAllGroupCategoriesInfoAndCountsOfLocationsByLocation(String countLocationInfo, Long userId, Long locationId, String compareLocationInfo ){
+    
+    public List findAllGroupCategoriesInfoAndCountsOfLocationsByLocation(String countLocationInfo, Long userId, Long locationId, String compareLocationInfo ){
 		Object[] params = {userId, locationId};
 		return getHibernateTemplate().find("select model.staticLocalGroup.staticLocalGroupId, model.staticLocalGroup.groupType, " +
 				"count(distinct "+countLocationInfo+"), count(model.personalUserGroupId) from PersonalUserGroup model " +
@@ -122,6 +133,186 @@ public class PersonalUserGroupDAO extends GenericDaoHibernate<PersonalUserGroup,
 		Object[] params = {userId, categoryId};
 		return getHibernateTemplate().find("from PersonalUserGroup model where model.createdUserId.registrationId = ? and " +
 				"model.staticLocalGroup.staticLocalGroupId = ? ",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInConstituency(Long userId,
+			Long constituencyId,Long categoryId) {
+		Object[] params = {userId, constituencyId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.constituency is not null and model.localGroupRegion.constituency.constituencyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInConstituencysByDistrict(
+			Long userId, Long districtId,Long categoryId) {
+		Object[] params = {userId, districtId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.district is not null and model.localGroupRegion.district.districtId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.constituency is not null "+
+				"group by model.localGroupRegion.constituency.constituencyId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInDistrict(Long userId,
+			Long districtId,Long categoryId) {
+		Object[] params = {userId, districtId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.district is not null and model.localGroupRegion.district.districtId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInDistrictsByState(Long userId,
+			Long stateId,Long categoryId) {
+		Object[] params = {userId, stateId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.state is not null and model.localGroupRegion.state.stateId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.district is not null "+
+				"group by model.localGroupRegion.district.districtId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInLocalBodys(Long userId,
+			Long localBodyId,Long categoryId) {
+		Object[] params = {userId, localBodyId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.localBody is not null and model.localGroupRegion.localBody.localElectionBodyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInLocalBodysByConstituency(
+			Long userId, Long constituencyId,Long categoryId) {
+		Object[] params = {userId, constituencyId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.constituency is not null and model.localGroupRegion.constituency.constituencyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.localBody is not null "+
+				"group by model.localGroupRegion.localBody.localElectionBodyId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInState(Long userId, Long stateId,Long categoryId) {
+		
+		Object[] params = {userId, stateId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.state is not null and model.localGroupRegion.state.stateId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInTehsil(Long userId, Long tehsilId,Long categoryId) {
+		
+		Object[] params = {userId, tehsilId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.tehsil is not null and model.localGroupRegion.tehsil.tehsilId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInTehsilsByConstituency(Long userId,
+			Long constituencyId,Long categoryId) {
+
+		Object[] params = {userId, constituencyId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.constituency is not null and model.localGroupRegion.constituency.constituencyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.tehsil is not null "+
+				"group by model.localGroupRegion.tehsil.tehsilId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInVillage(Long userId, Long villageId,Long categoryId) {
+		
+		Object[] params = {userId, villageId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.hamlet is not null and model.localGroupRegion.hamlet.hamletId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInVillagesByTehsil(Long userId,
+			Long tehsilId,Long categoryId) {
+
+		Object[] params = {userId, tehsilId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.tehsil is not null and model.localGroupRegion.tehsil.tehsilId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.hamlet is not null "+
+				"group by model.localGroupRegion.hamlet.hamletId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInWard(Long userId, Long wardId,Long categoryId) {
+		
+		Object[] params = {userId, wardId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.ward is not null and model.localGroupRegion.ward.constituencyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalCountOfLocalGroupsInWardsByLocalBodys(Long userId,
+			Long localBodyId,Long categoryId) {
+		
+		Object[] params = {userId, localBodyId,categoryId};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.staticLocalGroup is not null and model.localGroupRegion "+
+				"is not null and model.localGroupRegion.localBody is not null and model.localGroupRegion.localBody.localElectionBodyId = ? "+
+				"and model.staticLocalGroup.staticLocalGroupId = ? and model.localGroupRegion.ward is not null "+
+				"group by model.localGroupRegion.ward.constituencyId",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalLocalUserGroupsByScope(Long userId) {
+		
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.localGroupRegion is not null group by model.localGroupRegion."+
+				"groupRegionScope order by model.localGroupRegion.groupRegionScope",userId);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getTotalLocalUserGroupsByScope(Long userId, String scope) {
+		
+		Object[] params = {userId,scope};
+		return getHibernateTemplate().find("select count(model.personalUserGroupId) from PersonalUserGroup model where "+
+				"model.createdUserId.registrationId = ? and model.localGroupRegion is not null group by model.localGroupRegion."+
+				"groupRegionScope = ?",params);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public List getDistinctLocalGroupCategorysForAUser(Long userId) {
+		
+		return getHibernateTemplate().find("select distinct model.staticLocalGroup.staticLocalGroupId,model.staticLocalGroup.groupType "+
+				"from PersonalUserGroup model where model.createdUserId.registrationId = ? and model.staticLocalGroup is not null order by "+
+				"model.staticLocalGroup.groupType",userId);
 	}
 	
 }
