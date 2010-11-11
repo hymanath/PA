c2709226c73046d25204ccafa2ca04a5bfbc8bf9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-11 11:04:27 +0000

    Local User Groups Service For SMS - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticUsersDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/StaticUsersDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IStaticUsersDAO;
 import com.itgrids.partyanalyst.model.StaticUsers;
@@ -17,8 +18,27 @@ public class StaticUsersDAO extends GenericDaoHibernate<StaticUsers, Long> imple
 	@SuppressWarnings("unchecked")
 	public List<StaticUsers> findByMobileNo(String mobileNumber) {
 		
-		System.out.println("inside dAO...");
 		return getHibernateTemplate().find("from StaticUsers model where model.mobileNumber= ?",mobileNumber);
+	}
+	@SuppressWarnings("unchecked")
+	public List findGroupMembersMobileNosByMemberIds(List<Long> memberIds) {
+		Query queryObject = getSession().createQuery("select model.mobileNumber from StaticUsers model where model.staticUserId in (:memberIds)");
+		queryObject.setParameterList("memberIds", memberIds);
+		return queryObject.list();
+	}
+	@SuppressWarnings("unchecked")
+	public List findGroupMembersMobileNosByMemberIds(String memberIds) {
+		return getHibernateTemplate().find("select model.mobileNumber from StaticUsers model where model.staticUserId in ("+memberIds+")");
+	}
+	@SuppressWarnings("unchecked")
+	public List findGroupMembersNameAndMobileNosByMemberIds(List<Long> memberIds) {
+		Query queryObject = getSession().createQuery("select model.name,model.mobileNumber from StaticUsers model where model.staticUserId in (:memberIds)");
+		queryObject.setParameterList("memberIds", memberIds);
+		return queryObject.list();
+	}
+	@SuppressWarnings("unchecked")
+	public List findGroupMembersNameAndMobileNosByMemberIds(String memberIds) {
+		return getHibernateTemplate().find("select model.name,model.mobileNumber from StaticUsers model where model.staticUserId in ("+memberIds+")");
 	}
 
 
