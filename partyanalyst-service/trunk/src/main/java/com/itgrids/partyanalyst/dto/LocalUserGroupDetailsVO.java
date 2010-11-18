9fb7c832646fca61e33a93dc252f811e083fe847
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-18 08:41:57 +0000

    Local User Groups Locations Transfer Object update - Sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/LocalUserGroupDetailsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/LocalUserGroupDetailsVO.java
@@ -34,6 +34,20 @@ public class LocalUserGroupDetailsVO implements Serializable {
 	private String groupScopeId;
 	private String groupScopeValueId;
 	
+	//group location details
+	private String state;
+	private String stateName;
+	private String district;
+	private String districtName;
+	private String constituency;
+	private String constituencyName;
+	private String mandal;
+	private String mandalName;
+	private String villageOrWard;
+	private String villageOrWardName;
+	private String booth;
+	private String boothName;
+	
 	private List<UserGroupMembersVO> groupMemberDetails;
 	
 	private ResultStatus resultStatus;
@@ -121,6 +135,78 @@ public class LocalUserGroupDetailsVO implements Serializable {
 	}
 	public void setGroupScopeValueId(String groupScopeValueId) {
 		this.groupScopeValueId = groupScopeValueId;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public String getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(String constituency) {
+		this.constituency = constituency;
+	}
+	public String getConstituencyName() {
+		return constituencyName;
+	}
+	public void setConstituencyName(String constituencyName) {
+		this.constituencyName = constituencyName;
+	}
+	public String getMandal() {
+		return mandal;
+	}
+	public void setMandal(String mandal) {
+		this.mandal = mandal;
+	}
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	public String getVillageOrWard() {
+		return villageOrWard;
+	}
+	public void setVillageOrWard(String villageOrWard) {
+		this.villageOrWard = villageOrWard;
+	}
+	public String getVillageOrWardName() {
+		return villageOrWardName;
+	}
+	public void setVillageOrWardName(String villageOrWardName) {
+		this.villageOrWardName = villageOrWardName;
+	}
+	public String getBooth() {
+		return booth;
+	}
+	public void setBooth(String booth) {
+		this.booth = booth;
+	}
+	public String getBoothName() {
+		return boothName;
+	}
+	public void setBoothName(String boothName) {
+		this.boothName = boothName;
 	}
 	
 
