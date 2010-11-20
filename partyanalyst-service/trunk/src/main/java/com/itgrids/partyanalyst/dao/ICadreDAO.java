6a109ebd5f116c2db5d1ad80be715c6c955e4a46
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-20 11:04:29 +0000

    cadre bugs

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreDAO.java
@@ -142,6 +142,7 @@ public interface ICadreDAO extends GenericDao<Cadre, Long>{
 	public List<Cadre> findCadreDetailsNotAssignedToBooth(Long tehsilId,Long userId, String cadreType);
 	public List<Cadre> findCadreDetailsNotAssignedToBoothInLocalBody(Long localBodyId,Long userId, String cadreType);
 	public List<Cadre> findCadreDetailsNotAssignedToBoothInWard(Long wardId,Long userId, String cadreType);
+	public List<Cadre> findCadreDetailsAssignedToBoothInWard(Long wardId, Long userId, String cadreType);
 	public List findCadresByBoothInWard(Long wardId, Long userId, String cadreType);
 	
 	
