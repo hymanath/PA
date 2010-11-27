e6f990c340917e63931e0867bd3434915bc05915
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-11-27 14:13:43 +0000

    updated by siva

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBoothDataValidationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBoothDataValidationService.java
@@ -1,18 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
 import java.io.File;
-import java.util.List;
 
 import com.itgrids.partyanalyst.dto.UploadDataErrorMessageVO;
 import com.itgrids.partyanalyst.excel.CsvException;
 
 public interface IBoothDataValidationService {
 
-	public List<String> readBoothDataExcelFileAndPolpulate(File filePath, String electionYear, Long electionScopeId) throws CsvException;
-	
-	public List<String> readAssemblyBoothResultExcelAndPopulate(File filePath, String electionYear, Long electionScopeId)throws CsvException;
-	
-	public List<String> readParliamentBoothResultExcelAndPopulate(File filePath, String electionYear, Long electionScopeId) throws CsvException;
-	
 	public UploadDataErrorMessageVO readVoterExcelDataAndValidate(File filePath, String electionYear, Long stateId, Long electionTypeId)throws CsvException;
 }
