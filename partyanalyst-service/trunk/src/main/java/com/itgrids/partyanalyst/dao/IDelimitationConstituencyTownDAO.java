2fde9aee1ebb7c37a90e0c2fcb05c50a18ff4e19
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-15 12:50:13 +0000

    To map delemitation tables, by kamalakar & sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDelimitationConstituencyTownDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.DelimitationConstituencyTown;
+
+public interface IDelimitationConstituencyTownDAO extends GenericDao<DelimitationConstituencyTown, Long>{
+
+}
