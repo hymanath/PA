80fbd453c2c8eb6ae62eb25f7990788c0d401f20
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-15 12:50:13 +0000

    To map delemitation tables, by kamalakar & sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDelimitationWardDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.DelimitationWard;
+
+public interface IDelimitationWardDAO extends GenericDao<DelimitationWard, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDelimitationYearDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.DelimitationYear;
+
+public interface IDelimitationYearDAO extends GenericDao<DelimitationYear, Long>{
+
+}
