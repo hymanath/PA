b27cdff166e99644992fa0f9bb70c31cb7c37cf8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-17 12:38:06 +0000

    Delimitation Mappings Upload service - sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/DelimitationMappingUploadVO.java
@@ -0,0 +1,105 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on December 13,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+public class DelimitationMappingUploadVO extends ResultStatus implements
+		Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String delimitationYear;
+	private Set<String> mappedConstituencies;
+	private Integer totalConstiMappedCount = 0;
+	private Integer currentSheet = 0;
+	private Integer currentRow = 0;
+	private Boolean exceptionEncounterStatus;
+	/**
+	 * @return the delimitationYear
+	 */
+	public String getDelimitationYear() {
+		return delimitationYear;
+	}
+	/**
+	 * @param delimitationYear the delimitationYear to set
+	 */
+	public void setDelimitationYear(String delimitationYear) {
+		this.delimitationYear = delimitationYear;
+	}
+	/**
+	 * @return the mappedConstituencies
+	 */
+	public Set<String> getMappedConstituencies() {
+		return mappedConstituencies;
+	}
+	/**
+	 * @param mappedConstituencies the mappedConstituencies to set
+	 */
+	public void setMappedConstituencies(Set<String> mappedConstituencies) {
+		this.mappedConstituencies = mappedConstituencies;
+	}
+	/**
+	 * @return the totalConstiMappedCount
+	 */
+	public Integer getTotalConstiMappedCount() {
+		return totalConstiMappedCount;
+	}
+	/**
+	 * @param totalConstiMappedCount the totalConstiMappedCount to set
+	 */
+	public void setTotalConstiMappedCount(Integer totalConstiMappedCount) {
+		this.totalConstiMappedCount = totalConstiMappedCount;
+	}
+	/**
+	 * @return the currentSheet
+	 */
+	public Integer getCurrentSheet() {
+		return currentSheet;
+	}
+	/**
+	 * @param currentSheet the currentSheet to set
+	 */
+	public void setCurrentSheet(Integer currentSheet) {
+		this.currentSheet = currentSheet;
+	}
+	/**
+	 * @return the currentRow
+	 */
+	public Integer getCurrentRow() {
+		return currentRow;
+	}
+	/**
+	 * @param currentRow the currentRow to set
+	 */
+	public void setCurrentRow(Integer currentRow) {
+		this.currentRow = currentRow;
+	}
+	/**
+	 * @return the exceptionEncounterStatus
+	 */
+	public Boolean getExceptionEncounterStatus() {
+		return exceptionEncounterStatus;
+	}
+	/**
+	 * @param exceptionEncounterStatus the exceptionEncounterStatus to set
+	 */
+	public void setExceptionEncounterStatus(Boolean exceptionEncounterStatus) {
+		this.exceptionEncounterStatus = exceptionEncounterStatus;
+	}
+	
+    
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MandalSubRegionsVO.java
@@ -0,0 +1,86 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on December 13,2010
+ */
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Sai Krishna
+ *
+ */
+public class MandalSubRegionsVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long mandalId;
+	private String mandalName;
+	private Boolean isPartial;
+	
+	private List<SelectOptionVO> villagesList;
+
+	/**
+	 * @return the mandalId
+	 */
+	public Long getMandalId() {
+		return mandalId;
+	}
+
+	/**
+	 * @param mandalId the mandalId to set
+	 */
+	public void setMandalId(Long mandalId) {
+		this.mandalId = mandalId;
+	}
+
+	/**
+	 * @return the mandalName
+	 */
+	public String getMandalName() {
+		return mandalName;
+	}
+
+	/**
+	 * @param mandalName the mandalName to set
+	 */
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+
+	/**
+	 * @return the isPartial
+	 */
+	public Boolean getIsPartial() {
+		return isPartial;
+	}
+
+	/**
+	 * @param isPartial the isPartial to set
+	 */
+	public void setIsPartial(Boolean isPartial) {
+		this.isPartial = isPartial;
+	}
+
+	/**
+	 * @return the villagesList
+	 */
+	public List<SelectOptionVO> getVillagesList() {
+		return villagesList;
+	}
+
+	/**
+	 * @param villagesList the villagesList to set
+	 */
+	public void setVillagesList(List<SelectOptionVO> villagesList) {
+		this.villagesList = villagesList;
+	} 
+
+}
