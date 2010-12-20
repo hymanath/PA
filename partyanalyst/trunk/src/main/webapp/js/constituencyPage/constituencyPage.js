bf8b7cfd89509933928f2f08df6fe19acedf04f8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-20 16:35:29 +0000

    modified by raghav to display localbody problems in dist, const in dist page and const page, municipal page

--- partyanalyst/trunk/src/main/webapp/js/constituencyPage/constituencyPage.js
+++ partyanalyst/trunk/src/main/webapp/js/constituencyPage/constituencyPage.js
@@ -666,7 +666,7 @@ function buildProblemViewingWindow()
 			//onclick="getProblemCompleteDetails('+data.problemHistoryId+')"
 			str+='<div class="problemDataDivClass">';
 			str+='<span><img height="10" width="10" src="/PartyAnalyst/images/icons/constituencyPage/bullet_blue.png"></img></span>';
-			str+='<span><a href="problemCompleteDetailsAction.action?problemHistoryId='+data.problemHistoryId+'"> '+data.problem+'</a></span>';
+			str+='<span><a class="districtAnc" href="problemCompleteDetailsAction.action?problemHistoryId='+data.problemHistoryId+'"> '+data.problem+'</a></span>';
 			str+='</div>';
 			str+='<div id="constituencyMgmtBodyDiv" class="yui-skin-sam"><div id="moreDetailsPanelDiv"></div></div>';
 		}
