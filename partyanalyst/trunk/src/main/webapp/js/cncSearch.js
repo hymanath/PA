259d3c0618ba3defbc04fcced09e3fcd26ed87bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-23 11:36:43 +0000

    removed json error - Venkat

--- partyanalyst/trunk/src/main/webapp/js/cncSearch.js
+++ partyanalyst/trunk/src/main/webapp/js/cncSearch.js
@@ -234,7 +234,7 @@ function callAjaxForStates(jsObj,url)
 				   },
 				   scope : this,
 				   failure : function( o ) {
-								alert( "Failed to load result" + o.status + " " + o.statusText);
+								//alert( "Failed to load result" + o.status + " " + o.statusText);
 							 }
 				   };
 
