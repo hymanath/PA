50291906b35dcd9e2c1f4ef279d0dfda6b0cb65f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-23 11:36:07 +0000

    removed json error - Venkat

--- partyanalyst/trunk/src/main/webapp/js/LocationHierarchy/locationHierarchy.js
+++ partyanalyst/trunk/src/main/webapp/js/LocationHierarchy/locationHierarchy.js
@@ -56,7 +56,7 @@ function callAjaxForLocations(jsObj,url)
 				   },
 				   scope : this,
 				   failure : function( o ) {
-								alert( "Failed to load result" + o.status + " " + o.statusText);
+								//alert( "Failed to load result" + o.status + " " + o.statusText);
 							 }
 				   };
 
