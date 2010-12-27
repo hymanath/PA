30d4d9883f6e860b104e24f8f0b67eba5cf9cc7b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-27 08:35:15 +0000

    Adding static colors to Census chart,kamalakar

--- partyanalyst/trunk/src/main/webapp/js/googleAnalytics/googleAnalytics.js
+++ partyanalyst/trunk/src/main/webapp/js/googleAnalytics/googleAnalytics.js
@@ -77,3 +77,49 @@
    return colorArray;
     
   }
+
+  function setStaticColorsForInteractiveChartsForCensusAndPartiesArray(parties)
+  {
+    var colorStatic = new Array('#088A85','#81BEF7','#B45F04','#585858','#AEB404','#ADE8E0','#C4D296','#E5C55A','#F7C37E','#9C8AE2','#B4045F','#D0A9F5','#CBBEAB','#BCE5BF','#FAE2BD','#95B8E5','#120B0B','#800B0B','#1FCB9D','#90446A','#A9F5A9','#F8E0F7','#CECEF6','#A9F5D0','#8000FF','#AEB404','#E3F6CE','#CEE3F6','#D0A9F5','#E0F8E0','#5E610B',
+		'#088A85','#81BEF7','#B45F04','#585858','#AEB404','#ADE8E0','#C4D296','#E5C55A','#F7C37E','#9C8AE2','#B4045F','#D0A9F5','#CBBEAB','#BCE5BF','#FAE2BD','#95B8E5','#120B0B','#800B0B','#1FCB9D','#90446A','#A9F5A9','#F8E0F7','#CECEF6','#A9F5D0','#8000FF','#AEB404','#E3F6CE','#CEE3F6','#D0A9F5','#E0F8E0','#5E610B',
+		'#088A85','#81BEF7','#B45F04','#585858','#AEB404','#ADE8E0','#C4D296','#E5C55A','#F7C37E','#9C8AE2','#B4045F','#D0A9F5','#CBBEAB','#BCE5BF','#FAE2BD','#95B8E5','#120B0B','#800B0B','#1FCB9D','#90446A','#A9F5A9','#F8E0F7','#CECEF6','#A9F5D0','#8000FF','#AEB404','#E3F6CE','#CEE3F6','#D0A9F5','#E0F8E0','#5E610B');
+
+	var colorArray = new Array();
+	 
+	 //Iterate Parties and Set Colors 
+	 var colorCount = 0;
+	 for(var i=0; i<parties.length; i++)
+	 {
+		 if(parties[i] == 'Total Population')
+			 colorArray.push('#82CAFF');
+		 else if(parties[i] == 'Male Population')
+             colorArray.push('#82CAFF');
+		 else if(parties[i] == 'Female Polpultion')
+             colorArray.push('#82CAFF');
+		 else if(parties[i] == 'SC Population')
+             colorArray.push('#82CAFF');
+		 else if(parties[i] == 'ST Population')
+             colorArray.push('#82CAFF');
+		 else if(parties[i] == 'TDP')
+			 colorArray.push('#C7005D');
+		 else if(parties[i] == 'TRS')
+             colorArray.push('#F5A9F2');
+		 else if(parties[i] == 'INC')
+             colorArray.push('#2A00D2');
+		 else if(parties[i] == 'BJP')
+             colorArray.push('#FE9A2E');
+		 else if(parties[i] == 'PRP')
+             colorArray.push('#74DF00');
+		 else if(parties[i] == 'CPI')
+             colorArray.push('#FA5858');
+		 else if(parties[i] == 'CPM')
+             colorArray.push('gray');
+		 else{
+             colorArray.push(colorStatic[colorCount]);
+             colorCount++;
+		 }
+	 }
+
+   return colorArray;
+    
+  }
