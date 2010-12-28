b90df68b7ef1dca1138c9256f795bb649834203c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-28 14:23:38 +0000

    modified by raghav to implement comments for problems

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserApprovalDetailsDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.UserApprovalDetails;
+
+public interface IUserApprovalDetailsDAO extends GenericDao<UserApprovalDetails, Long> {
+	
+}
