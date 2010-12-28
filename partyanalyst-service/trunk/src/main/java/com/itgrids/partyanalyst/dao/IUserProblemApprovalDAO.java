48ffc8ed425862892b3a2d931995bb8e44ca962b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-28 14:23:38 +0000

    modified by raghav to implement comments for problems

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserProblemApprovalDAO.java
@@ -0,0 +1,13 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.UserProblemApproval;
+
+public interface IUserProblemApprovalDAO extends GenericDao<UserProblemApproval, Long> {
+	@SuppressWarnings("unchecked")
+	List findProblemApprovalsByUser(Long userId, Long problemHistoryId);
+	@SuppressWarnings("unchecked")
+	List findApprovalInfoForProblem(Long problemHistoryId);
+}
