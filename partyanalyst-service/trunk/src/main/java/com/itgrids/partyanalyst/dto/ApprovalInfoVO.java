5755ff41f79418e8bcb2a01cad684f56dbf01291
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2010-12-28 14:22:33 +0000

    modified by raghav to implement comments for problems

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ApprovalInfoVO.java
@@ -0,0 +1,82 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ApprovalInfoVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long userId;
+	private String reason;
+	private String isApproved;
+	private String module;
+	// if used for problems , this variable refers which problem this approval is posted
+	private Long problemHistoryId;
+	private Long totalNoAcceptance;
+	private Long totalNoRejectance;
+	private String lastUpdate;
+	private String userName;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getIsApproved() {
+		return isApproved;
+	}
+	public void setIsApproved(String isApproved) {
+		this.isApproved = isApproved;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+	public Long getProblemHistoryId() {
+		return problemHistoryId;
+	}
+	public void setProblemHistoryId(Long problemHistoryId) {
+		this.problemHistoryId = problemHistoryId;
+	}
+	public Long getTotalNoAcceptance() {
+		return totalNoAcceptance;
+	}
+	public void setTotalNoAcceptance(Long totalNoAcceptance) {
+		this.totalNoAcceptance = totalNoAcceptance;
+	}
+	public Long getTotalNoRejectance() {
+		return totalNoRejectance;
+	}
+	public void setTotalNoRejectance(Long totalNoRejectance) {
+		this.totalNoRejectance = totalNoRejectance;
+	}
+	public String getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+
+}
