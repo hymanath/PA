87e68f6937d40347be1fe6833fd1bdb31322df0f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-01-05 15:33:55 +0000

    To implement Census in parliament Constituency - kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyCensusDetailsDAO.java
@@ -0,0 +1,15 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ConstituencyCensusDetails;
+
+public interface IConstituencyCensusDetailsDAO extends GenericDao<ConstituencyCensusDetails,Long>{
+	
+	public List<Object[]> findConstituencyWiseCensusDetails(Long stateId,Long constituencyId,Long year);
+	
+	public List<Long> checkForConstituencyExistance(Long constituencyId);
+
+}
