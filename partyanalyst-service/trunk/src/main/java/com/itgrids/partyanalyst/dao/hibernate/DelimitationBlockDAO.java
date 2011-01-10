9bd64a84ac00ec3b07dd6a1d1f76470b97e33865
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-01-10 11:12:05 +0000

    Delimitation Mappings Blocks Update - sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/DelimitationBlockDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/DelimitationBlockDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IDelimitationBlockDAO;
@@ -9,5 +11,12 @@ public class DelimitationBlockDAO extends GenericDaoHibernate<DelimitationBlock,
 
 	public DelimitationBlockDAO(){
 		super(DelimitationBlock.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<DelimitationBlock> findByDelimitationWardIdAndBlock(
+			Long delimWardId, Long blockId) {
+		Object[] params = {delimWardId,blockId};
+		return getHibernateTemplate().find("from DelimitationBlock model where model.delimitationWard.delimitationWardId = ? and model.block.blockId = ?",params);
 	}
 }
