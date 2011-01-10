e3984f2cbb5b1d0b4d46d3c4b88d743abcdaddef
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-01-10 11:11:51 +0000

    Delimitation Mappings Blocks Update - sai

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDelimitationBlockDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDelimitationBlockDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.DelimitationBlock;
 
 public interface IDelimitationBlockDAO extends GenericDao<DelimitationBlock, Long>{
 
+	public List<DelimitationBlock> findByDelimitationWardIdAndBlock(Long delimWardId,Long blockId);
 }
