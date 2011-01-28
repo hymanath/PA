6511e3e0c93c53695d0a9314af5c0d7afa999573
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-01-28 12:43:40 +0000

    ADDING BSP PARTY IN CENSUS REPORT - KAMALAKAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -12,7 +12,7 @@ public interface IConstants {
 	public static final String ELECTION_SUBTYPE_BYE = "BYE";
 	public static final Long COUNTRY_INDIA_ID = 1L;
 	public static final String STATIC_PARTIES = "'INC','PRP','TDP','TRS','CPI','CPM','AIMIM','BJP'";
-	public static final String NATIONAL_STATIC_PARTIES = "'INC','CPI','CPM','BJP'";
+	public static final String NATIONAL_STATIC_PARTIES = "'INC','CPI','CPM','BJP','BSP'";
 	public static final String NATIONAL_PARTY_TYPE = "NP";
 	public static final String STATE_PARTY_TYPE = "SP";
 	public static final int LOOSING_BY_DROPPING_VOTES_CONSTANTS = 5;
