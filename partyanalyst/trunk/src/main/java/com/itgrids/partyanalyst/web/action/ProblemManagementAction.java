66628e6094b272923a65457d29cf98d7cff7ef3f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-02-22 17:06:58 +0000

    Problem Management - sai

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ProblemManagementAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ProblemManagementAction.java
@@ -1,7 +1,10 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,12 +13,19 @@ import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import com.itgrids.partyanalyst.dto.ProblemBeanVO;
+import com.itgrids.partyanalyst.dto.ProblemCompleteDetailsVO;
+import com.itgrids.partyanalyst.dto.ProblemManagementChartVO;
 import com.itgrids.partyanalyst.dto.ProblemManagementDataVO;
+import com.itgrids.partyanalyst.dto.ProblemStatusDataVO;
 import com.itgrids.partyanalyst.dto.ProblemsOfUserVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.service.IProblemManagementService;
+import com.itgrids.partyanalyst.service.IStaticDataService;
+import com.itgrids.partyanalyst.service.impl.CadreManagementService;
+import com.itgrids.partyanalyst.service.impl.RegionServiceDataImp;
 import com.itgrids.partyanalyst.utils.IConstants;
+import com.itgrids.partyanalyst.utils.ISessionConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -31,8 +41,39 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 	private HttpSession session;
 	private String task = null;
 	JSONObject jObj = null;
+	private Long pHistoryId; 
+	private List<SelectOptionVO> stateListForProb;
+	private List<SelectOptionVO> districtListForProb;
+	private List<SelectOptionVO> constituencyListForProb;
+	private List<SelectOptionVO> mandalListForProb;
+	private List<SelectOptionVO> villagesListForProb;
+	private RegionServiceDataImp regionServiceDataImp;
+	private CadreManagementService cadreManagementService;
+	private IStaticDataService staticDataService;
 	private IProblemManagementService problemManagementService;
 	private ProblemManagementDataVO problemManagementDataVO;
+	private ProblemManagementChartVO problemManagementChartVO;
+	private ProblemCompleteDetailsVO problemCompleteDetailsVO;
+	private ProblemStatusDataVO problemStatusDataVO;
+	private List<ProblemStatusDataVO> problemStatusDataVOList;
+	
+	
+	public ProblemStatusDataVO getProblemStatusDataVO() {
+		return problemStatusDataVO;
+	}
+
+	public void setProblemStatusDataVO(ProblemStatusDataVO problemStatusDataVO) {
+		this.problemStatusDataVO = problemStatusDataVO;
+	}
+
+	public List<ProblemStatusDataVO> getProblemStatusDataVOList() {
+		return problemStatusDataVOList;
+	}
+
+	public void setProblemStatusDataVOList(
+			List<ProblemStatusDataVO> problemStatusDataVOList) {
+		this.problemStatusDataVOList = problemStatusDataVOList;
+	}
 	
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
@@ -55,6 +96,15 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 		this.task = task;
 	}
 	
+	public ProblemCompleteDetailsVO getProblemCompleteDetailsVO() {
+		return problemCompleteDetailsVO;
+	}
+
+	public void setProblemCompleteDetailsVO(
+			ProblemCompleteDetailsVO problemCompleteDetailsVO) {
+		this.problemCompleteDetailsVO = problemCompleteDetailsVO;
+	}
+
 	public IProblemManagementService getProblemManagementService() {
 		return problemManagementService;
 	}
@@ -73,6 +123,89 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 		this.problemManagementDataVO = problemManagementDataVO;
 	}	
 	
+	public ProblemManagementChartVO getProblemManagementChartVO() {
+		return problemManagementChartVO;
+	}
+
+	public void setProblemManagementChartVO(
+			ProblemManagementChartVO problemManagementChartVO) {
+		this.problemManagementChartVO = problemManagementChartVO;
+	}
+
+	public List<SelectOptionVO> getStateListForProb() {
+		return stateListForProb;
+	}
+
+	public void setStateListForProb(List<SelectOptionVO> stateListForProb) {
+		this.stateListForProb = stateListForProb;
+	}
+
+	public List<SelectOptionVO> getDistrictListForProb() {
+		return districtListForProb;
+	}
+
+	public void setDistrictListForProb(List<SelectOptionVO> districtListForProb) {
+		this.districtListForProb = districtListForProb;
+	}
+
+	public List<SelectOptionVO> getConstituencyListForProb() {
+		return constituencyListForProb;
+	}
+
+	public void setConstituencyListForProb(
+			List<SelectOptionVO> constituencyListForProb) {
+		this.constituencyListForProb = constituencyListForProb;
+	}
+
+	public List<SelectOptionVO> getMandalListForProb() {
+		return mandalListForProb;
+	}
+
+	public void setMandalListForProb(List<SelectOptionVO> mandalListForProb) {
+		this.mandalListForProb = mandalListForProb;
+	}
+
+	public List<SelectOptionVO> getVillagesListForProb() {
+		return villagesListForProb;
+	}
+
+	public void setVillagesListForProb(List<SelectOptionVO> villagesListForProb) {
+		this.villagesListForProb = villagesListForProb;
+	}
+
+	public RegionServiceDataImp getRegionServiceDataImp() {
+		return regionServiceDataImp;
+	}
+
+	public void setRegionServiceDataImp(RegionServiceDataImp regionServiceDataImp) {
+		this.regionServiceDataImp = regionServiceDataImp;
+	}
+
+	public CadreManagementService getCadreManagementService() {
+		return cadreManagementService;
+	}
+
+	public void setCadreManagementService(
+			CadreManagementService cadreManagementService) {
+		this.cadreManagementService = cadreManagementService;
+	}
+
+	public IStaticDataService getStaticDataService() {
+		return staticDataService;
+	}
+
+	public void setStaticDataService(IStaticDataService staticDataService) {
+		this.staticDataService = staticDataService;
+	}
+
+	public Long getPHistoryId() {
+		return pHistoryId;
+	}
+
+	public void setPHistoryId(Long historyId) {
+		pHistoryId = historyId;
+	}
+
 	public String execute() throws Exception{
 			problemManagementDataVO = new ProblemManagementDataVO();
 			ProblemBeanVO problemBeanVO = new ProblemBeanVO();
@@ -137,8 +270,15 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 			} 
 			if(jObj.getString("task").equalsIgnoreCase("newProblemsByUserID"))
 			{
-				ProblemsOfUserVO obj = problemManagementService.getNewProblemsForUser(user.getRegistrationID(), new Long(1));
-				log.debug("New Problems By User ID>>>>>>>>>>>>>>>>>>>>>>"+obj.getProblemRegionScopes().size()+"<<<<<<<<<<-------------------"+obj.getProblemTypes().size());
+				//ProblemsOfUserVO obj = problemManagementService.getNewProblemsForUser(user.getRegistrationID(), new Long(1));
+				
+				String startIndex    = request.getParameter("startIndex");
+				String resultsCount  = request.getParameter("resultsCount");
+				
+								
+				ProblemsOfUserVO obj = problemManagementService.getUserProblemsInDifferentStagesByDate(user.getRegistrationID(), Integer.parseInt(startIndex), Integer.parseInt(resultsCount));
+				
+				problemManagementDataVO.setTotalResultsCount(obj.getTotalResultsCount());
 				problemManagementDataVO.setProblemsOfUserVO(obj);
 			}
 			if(jObj.getString("task").equalsIgnoreCase("classifyProblem"))
@@ -404,6 +544,144 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 	 	return Action.SUCCESS;
 	}	
 	
+	public String getProblemsByFilterView()
+	{
+		
+		problemManagementDataVO = new ProblemManagementDataVO();
+		session = request.getSession();
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		
+		if(user==null)
+			return ERROR;
+		
+		String param = null;
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);
+			System.out.println(jObj);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		String sDate = jObj.getString("startText");
+		String eDate = jObj.getString("endText");
+		Long statusId = jObj.getLong("statusValue");
+		
+		String startIndex = request.getParameter("startIndex");
+		String resultsCount = request.getParameter("resultsCount");
+		
+		DateFormat format = 
+			 new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			
+			Date startDate = null;
+			Date endDate = null;
+			if(sDate != null && !sDate.equalsIgnoreCase(""))
+				startDate = (Date)format.parse(sDate);
+			if(eDate != null && !eDate.equalsIgnoreCase(""))
+				endDate =  (Date)format.parse(eDate);
+			
+			ProblemsOfUserVO obj = problemManagementService.getUserProblemsInDifferentStagesByFilters(user.getRegistrationID(), statusId, startDate, endDate, Integer.parseInt(startIndex), Integer.parseInt(resultsCount));
+			
+			problemManagementDataVO.setTotalResultsCount(obj.getTotalResultsCount());
+			problemManagementDataVO.setProblemsOfUserVO(obj);
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return Action.SUCCESS;
+	}
+	
+	public String getProblemDetailsAndStatus()
+	{
+		session = request.getSession();
+		pHistoryId = Long.parseLong(request.getParameter("pHistoryId"));
+		
+		RegistrationVO regVO = (RegistrationVO) session.getAttribute("USER");
+		
+		if(regVO==null)
+			return ERROR;
+		
+		String accessType =regVO.getAccessType();
+		Long accessValue= new Long(regVO.getAccessValue());
+		
+		stateListForProb        = new ArrayList<SelectOptionVO>(0);
+		districtListForProb     = new ArrayList<SelectOptionVO>(0);
+		constituencyListForProb = new ArrayList<SelectOptionVO>(0);
+		mandalListForProb       = new ArrayList<SelectOptionVO>(0);
+		villagesListForProb     = new ArrayList<SelectOptionVO>(0);
+		
+		if("MLA".equals(accessType))
+		{
+			List<SelectOptionVO> list = regionServiceDataImp.getStateDistrictByConstituencyID(accessValue);
+			stateListForProb.add(list.get(0));			
+			districtListForProb.add(list.get(1));
+			constituencyListForProb.add(list.get(2));
+			mandalListForProb = regionServiceDataImp.getSubRegionsInConstituency(accessValue, IConstants.PRESENT_YEAR, null);
+			mandalListForProb.add(0,new SelectOptionVO(0l,"Select Location"));
+		}
+		else if("DISTRICT".equals(accessType))
+		{
+			List<SelectOptionVO> list = regionServiceDataImp.getStateDistrictByDistrictID(accessValue);
+			
+			stateListForProb.add(list.get(0));
+			districtListForProb.add(list.get(1));
+			
+			constituencyListForProb = regionServiceDataImp.getConstituenciesByDistrictID(accessValue);
+			constituencyListForProb.add(0,new SelectOptionVO(0l,"Select Constituency"));
+		}
+		else if("STATE".equals(accessType))
+		{
+			String name = cadreManagementService.getStateName(accessValue);
+			SelectOptionVO selectOptionVO = new SelectOptionVO();
+			selectOptionVO.setId(accessValue);
+			selectOptionVO.setName(name);
+			stateListForProb.add(selectOptionVO);
+			districtListForProb = staticDataService.getDistricts(accessValue);
+			districtListForProb.add(0,new SelectOptionVO(0l,"Select District"));
+		}
+		
+		session.setAttribute(ISessionConstants.STATES_PROB, stateListForProb);
+		session.setAttribute(ISessionConstants.DISTRICTS_PROB,districtListForProb);
+		session.setAttribute(ISessionConstants.CONSTITUENCIES_PROB,constituencyListForProb);
+		session.setAttribute(ISessionConstants.MANDALS_PROB,mandalListForProb);	
+		session.setAttribute(ISessionConstants.VILLAGES_PROB,villagesListForProb);
+		
+		problemCompleteDetailsVO = problemManagementService.getProblemCompleteInformationByProblemHistory(pHistoryId);
+		/*problemCompleteDetailsVO = new ProblemCompleteDetailsVO();
+		
+		ProblemBeanVO problemBean = new ProblemBeanVO();
+		problemBean.setProblem("Z.P High School Building is in worst stage");
+		problemBean.setDescription("Z.P High School Building is to be repaired, Students facing lot of problems.");
+		problemBean.setExistingFrom("2011-02-07");
+		problemBean.setReportedDate("2011-01-01");
+		problemBean.setProbSource("User");
+		problemBean.setProblemLocation("Andhra Pradesh ,Nellore,Kavali.");
+		problemBean.setProblemStatus("PROGRESS");
+		
+		problemCompleteDetailsVO.setProblemBasicDetails(problemBean);
+		
+		List<ProblemStatusDataVO> problemLifeCycleData = new ArrayList<ProblemStatusDataVO>();
+		ProblemStatusDataVO statusData = new ProblemStatusDataVO();
+		statusData.setDepartment("A.P.S.C.H.E Department");
+		statusData.setComments("Progress");
+		statusData.setDeptLocation("Nellore,AP");
+		statusData.setCadre("Murali Manohar");
+		statusData.setOfficialName("Ramana Murthy");
+		statusData.setProblemStatus("NEW");
+		statusData.setPhoneNo("9243222321");
+		problemLifeCycleData.add(statusData);
+		
+		
+		problemCompleteDetailsVO.setProblemLifeCycleData(problemLifeCycleData);*/
+		
+		return Action.SUCCESS;
+	}
+	
 	public String getProblemCompleteDetails(){
 		problemManagementDataVO = new ProblemManagementDataVO();
 		ProblemBeanVO problemBeanVO = new ProblemBeanVO();
@@ -427,4 +705,90 @@ public class ProblemManagementAction extends ActionSupport implements ServletReq
 	}
 	
 	
+	public String getProblemsPostedByUserToBuildCharts(){
+		
+		session = request.getSession();
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		
+		if(user==null)
+			return ERROR;
+		
+		String param = null;
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);
+			System.out.println(jObj);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		Long problemStatusId = jObj.getLong("statusId");
+		String problemStatus = jObj.getString("statusName");
+		String problemsBy    = jObj.getString("dateType");
+		String startIndex    = jObj.getString("startIndex");
+		String maxResults    = jObj.getString("maxResults");
+		
+		String getProblemsBy = "";
+		
+		if(problemsBy.equalsIgnoreCase("date"))
+			getProblemsBy = IConstants.PROBLEMS_BY_DATE;
+		else if(problemsBy.equalsIgnoreCase("month"))
+			getProblemsBy = IConstants.PROBLEMS_BY_MONTH;
+			
+		
+		if(problemStatusId == null || problemStatusId.equals(0L))
+			problemManagementChartVO = problemManagementService.getProblemsDataForAUserToBuildChart(user.getRegistrationID(), Integer.parseInt(startIndex), Integer.parseInt(maxResults), getProblemsBy);
+		else
+			problemManagementChartVO = problemManagementService.getProblemsDataForAUserToBuildChart(user.getRegistrationID(), problemStatusId, Integer.parseInt(startIndex), Integer.parseInt(maxResults), getProblemsBy);
+		
+	 return Action.SUCCESS;
+	}
+	
+	public String getProblemsPostedDataByUserToBuildCharts()
+	{
+		session = request.getSession();
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		
+		if(user==null)
+			return ERROR;
+		
+		problemManagementChartVO = problemManagementService.getOverallProblemsCountInDifferentLifeCycleStagesPostedByUser(user.getRegistrationID());
+		return Action.SUCCESS;
+	}
+	
+	public String getProblemPresentStatus()
+	{
+		String param = null;
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		problemStatusDataVO = problemManagementService.getProblemRecentDetailsByProblemHistoryId(jObj.getLong("pHistoryId"));
+		
+		return Action.SUCCESS;
+	}
+	
+	public String getProblemRecentActivities()
+	{
+		String param = null;
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		problemStatusDataVOList = problemManagementService.getAllProblemRecentActivityDetails(jObj.getLong("pHistoryId"));
+		
+		return Action.SUCCESS;
+	}
 }
