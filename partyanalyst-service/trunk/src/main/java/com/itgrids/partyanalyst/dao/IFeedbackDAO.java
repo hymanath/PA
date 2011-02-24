cfa6ca8f804394426e8b0745288d151a5700cf91
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-02-24 06:20:40 +0000

    User Feedback code by Swathi

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFeedbackDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.FeedBack;
+
+public interface IFeedbackDAO extends GenericDao<FeedBack, Long>{
+
+}
