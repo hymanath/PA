3477bad2700904c43ecf1b76b65f0d0589df5f71
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-02-25 11:13:32 +0000

    User Feedback by Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserFeedbackVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserFeedbackVO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 public class UserFeedbackVO extends ResultStatus implements Serializable{
@@ -13,7 +14,11 @@ public class UserFeedbackVO extends ResultStatus implements Serializable{
 	private Long taskName;
 	private String comment;
 	private Long userId;
+	private String userType;
+	private String userName;
 	private String responseCategory;
+	private Date posteddate;
+	private String status;
 	
 	public Long getCommentType() {
 		return commentType;
@@ -44,6 +49,30 @@ public class UserFeedbackVO extends ResultStatus implements Serializable{
 	}
 	public void setResponseCategory(String responseCategory) {
 		this.responseCategory = responseCategory;
+	}
+	public Date getPosteddate() {
+		return posteddate;
+	}
+	public void setPosteddate(Date posteddate) {
+		this.posteddate = posteddate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getUserType() {
+		return userType;
 	}
 	
 		
