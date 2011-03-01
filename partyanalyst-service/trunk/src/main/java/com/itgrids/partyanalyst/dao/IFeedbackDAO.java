6ebc494c14a66cf32ae095fea55a629716d59a21
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-03-01 10:25:38 +0000

    User Feedback code by Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFeedbackDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFeedbackDAO.java
@@ -1,9 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.FeedBack;
 
 public interface IFeedbackDAO extends GenericDao<FeedBack, Long>{
 
+	public Integer updateStatusToApproveOrReject(List<Long> commentIdList,String status);
+
 }
+
