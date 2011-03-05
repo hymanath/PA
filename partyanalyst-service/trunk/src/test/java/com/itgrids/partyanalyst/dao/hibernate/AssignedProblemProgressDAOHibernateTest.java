628dba8338b784bc8ed8ad8c8005e436318325af
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-03-05 12:20:44 +0000

    Problem Management Report - kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AssignedProblemProgressDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AssignedProblemProgressDAOHibernateTest.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.junit.Test;
 
 import com.itgrids.partyanalyst.dao.IAssignedProblemProgressDAO;
 import com.itgrids.partyanalyst.model.AssignedProblemProgress;
+import com.itgrids.partyanalyst.model.ProblemHistory;
 
 public class AssignedProblemProgressDAOHibernateTest extends BaseDaoTestCase{
 
@@ -52,7 +54,7 @@ public class AssignedProblemProgressDAOHibernateTest extends BaseDaoTestCase{
 		System.out.println(list.size());
 	}*/
 	
-	@SuppressWarnings("unchecked")
+	/*@SuppressWarnings("unchecked")
 	@Test
 	public void testGetAssignedProblemProgressByRecentUpdate(){
 		
@@ -75,5 +77,90 @@ public class AssignedProblemProgressDAOHibernateTest extends BaseDaoTestCase{
 		}
 		
 		
+	}*/
+	
+	/*public void testGetProblemsByDepartmentScope()
+	{
+		List<AssignedProblemProgress> list = assignedProblemProgressDAO.getProblemsByDepartmentScope(1l,1l);
+		System.out.println(list.size());
+	}*/
+	
+	/*public void testGetAssignedCadreProblemsCountInARegion()
+	{
+		List<Object> list = assignedProblemProgressDAO.getAssignedCadreProblemsCountInARegion(1l,3l,19l);
+		System.out.println(list.size());
+		for(Object obj:list)
+		System.out.println(obj.toString());
+	}*/
+	
+	/*public void testGetAssignedCadreProblemsCountForAnUser()
+	{
+		List<ProblemHistory> list = assignedProblemProgressDAO.getAssignedCadreProblemsInARegion(1l,3l,19l);
+		System.out.println(list.size());
+		for(Object obj:list)
+		System.out.println(obj.toString());
+	}*/
+	
+	/*public void testGetDepartmentWiseProblemStatus()
+	{
+		List<Object> list = assignedProblemProgressDAO.getDepartmentWiseProblemStatus(1l,2l);
+		for(Object obj:list)
+			System.out.println(obj.toString());
+	}*/
+	
+	/*public void testGetDepartmentWiseProblemsBasedOnStatus()
+	{
+		String statusStr = "";
+		List<ProblemHistory> list = assignedProblemProgressDAO.getDepartmentWiseProblemsBasedOnStatus(1l,2l,statusStr);
+		for(ProblemHistory problemHistory : list)
+		System.out.println(problemHistory.getProblemHistoryId());
+		
+	}*/
+	/*public void testGetDepartmentWiseProblemStatusInARegion()
+	{
+		String deptLocationStr = " and model.departmentLocation.district.districtId = 19";
+		List<Long> list = assignedProblemProgressDAO.getDeptWiseAssignedProblemProgressIds(1l,deptLocationStr);
+		System.out.println(list.size());
+		for(Long obj : list)
+		{
+			System.out.println(obj);
+			//System.out.println("---"+obj[1].toString());
+			//System.out.println("---"+obj[2].toString());
+		}
+	}*/
+	
+	/*public void testGetProblemsStatusBasedOnAssignedProblemProgressId()
+	{
+		List<Long> asPList = new ArrayList<Long>(0);
+		asPList.add(77l);
+		asPList.add(75l);
+		asPList.add(78l);
+		
+		List<Object[]> list = assignedProblemProgressDAO.getProblemsStatusBasedOnAssignedProblemProgressId(1l,asPList);
+		System.out.println(list.size());
+		
+		for(Object[] obj : list)
+			for(Object b : obj)
+			System.out.println(b.toString());
+	}*/
+	
+	public void testGetProblemsBasedOnAssignedProblemProgressIdAndStatus()
+	{
+		List<Long> asPList = new ArrayList<Long>(0);
+		asPList.add(77l);
+		asPList.add(75l);
+		asPList.add(78l);
+		
+		String deptStr = " and model.departmentOrganisation.departmentOrganisationId = 2";
+		String status = "NEW";
+		String statusStr = " and model.problemHistory.problemStatus.status = '"+status+"'";
+		
+		List<ProblemHistory> list = assignedProblemProgressDAO.getProblemsBasedOnAssignedProblemProgressIdAndStatus(1l,asPList,deptStr,statusStr);
+		
+		System.out.println(list.size());
+		
+		for(ProblemHistory problemHistory : list)
+			System.out.println(problemHistory.getProblemStatus().getStatus());
 	}
+	
 }
