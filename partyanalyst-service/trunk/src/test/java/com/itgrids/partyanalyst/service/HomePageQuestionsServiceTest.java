b803a302dbe4406f9d8bd1464ac6032d4c33c3cd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-04-27 14:01:22 +0000

    HomePageQuestions implementation in service by hasa

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/service/HomePageQuestionsServiceTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.service;
+
+import com.itgrids.partyanalyst.dao.IHomePageQuestionAnswersDAO;
+import com.itgrids.partyanalyst.dao.IHomePageQuestionDAO;
+import com.itgrids.partyanalyst.dao.IRegistrationDAO;
+
+public class HomePageQuestionsServiceTest {
+	
+	
+	private IRegistrationDAO registrationDAO;
+	private IHomePageQuestionDAO homePageQuestionDAO;
+	private IHomePageQuestionAnswersDAO homePageQuestionAnswersDAO;
+	
+	
+
+}
