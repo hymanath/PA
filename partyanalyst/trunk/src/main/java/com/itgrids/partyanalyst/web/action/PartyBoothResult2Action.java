cf5575680ab51f85ae5022405b56fd946a6c3ce4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-05-04 19:44:46 +0000

    Setting Entitlements To Party Booth Results - kamalakar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/PartyBoothResult2Action.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/PartyBoothResult2Action.java
@@ -3,11 +3,15 @@ package com.itgrids.partyanalyst.web.action;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 
+import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.excel.booth.PartyBoothPerformanceVO;
+import com.itgrids.partyanalyst.helper.EntitlementsHelper;
 import com.itgrids.partyanalyst.service.IPartyBoothWiseResultsService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class PartyBoothResult2Action extends ActionSupport implements ServletRequestAware{
@@ -18,6 +22,7 @@ public class PartyBoothResult2Action extends ActionSupport implements ServletReq
 	private String electionYear;
 	private String electionType;
 	private IPartyBoothWiseResultsService partyBoothWiseResultsService;
+	private EntitlementsHelper entitlementsHelper;
 	private PartyBoothPerformanceVO boothResult;
 
 	
@@ -93,13 +98,32 @@ public class PartyBoothResult2Action extends ActionSupport implements ServletReq
 		this.electionType = electionType;
 	}
 	
+	public EntitlementsHelper getEntitlementsHelper() {
+		return entitlementsHelper;
+	}
+
+
+	public void setEntitlementsHelper(EntitlementsHelper entitlementsHelper) {
+		this.entitlementsHelper = entitlementsHelper;
+	}
+
+
 	public String execute(){
+		
+		HttpSession session = request.getSession();
 		
 		partyName = request.getParameter("partyName");
 		electionYear = request.getParameter("electionYear");
 		constituencyName = request.getParameter("constituencyName");
 		
-			
+		if(session.getAttribute(IConstants.USER) == null && 
+				!entitlementsHelper.checkForEntitlementToViewReport(null, IConstants.PARTY_BOOTHWISE_RESULTS_REPORT))
+			return INPUT;
+		
+		if(!entitlementsHelper.checkForEntitlementToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.PARTY_BOOTHWISE_RESULTS_REPORT) 
+			|| !entitlementsHelper.checkForRegionToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.CONSTITUENCY_LEVEL, new Long(constituencyName)))
+			return ERROR;
+				
 		System.out.println(" values from ajax -------- partyName:"+partyName+" constituencyName:"+constituencyName+" electionYear:"+electionYear);
 		List<PartyBoothPerformanceVO> boothResults = partyBoothWiseResultsService.getBoothWiseResultsForParty(new Long(partyName), new Long(constituencyName), electionYear);
 		boothResult = partyBoothWiseResultsService.getVotingPercentageWiseBoothResult(boothResults.get(0));
