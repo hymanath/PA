43c786f3480e68ed25db1b08edd780d796e839e0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-05-06 13:57:31 +0000

    For Mailing

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailService.java
@@ -1,10 +1,22 @@
 package com.itgrids.partyanalyst.service;
 
 import com.itgrids.partyanalyst.dto.QuickRequestVO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface IMailService {
 
-	 public ResultStatus sendQuickRequestEmailToAdmin(QuickRequestVO quickRequestVO);
+	 public ResultStatus sendQuickRequestEmailToAdmin(QuickRequestVO quickRequestVO,String requestFrom);
+	 
+	 public ResultStatus sendMailToUserToRecoverPassword(RegistrationVO registrationVO,String requestFrom);
+	 
+	  public  ResultStatus sendMailFromLocalHost(QuickRequestVO quickRequestVO);
+	  
+	  public ResultStatus sendRegistrationNotification(RegistrationVO registrationVO,String requestFrom);
+	  
+	  public  ResultStatus sendMailFromServer(QuickRequestVO quickRequestVO);
+	
+		 
+	 
 
 }
