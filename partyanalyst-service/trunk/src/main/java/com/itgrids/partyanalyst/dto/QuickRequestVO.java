9df25662e20fd0e8fe84ba02d9469df89348d0a2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-05-06 13:56:41 +0000

    For Mailing

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/QuickRequestVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/QuickRequestVO.java
@@ -7,8 +7,17 @@ public class QuickRequestVO {
     private String emailId;
     private String mobileNumber;
     private String userRequirement;
-   	
+   	private String toEmailId;
+   	private String text;
+   	private String subject;
+   	private String fromEmailId;
 	
+	public String getFromEmailId() {
+		return fromEmailId;
+	}
+	public void setFromEmailId(String fromEmailId) {
+		this.fromEmailId = fromEmailId;
+	}
 	public String getUserName() {
 		return userName;
 	}
@@ -32,6 +41,24 @@ public class QuickRequestVO {
 	}
 	public void setUserRequirement(String userRequirement) {
 		this.userRequirement = userRequirement;
+	}
+	public String getToEmailId() {
+		return toEmailId;
+	}
+	public void setToEmailId(String toEmailId) {
+		this.toEmailId = toEmailId;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
 	
 }
