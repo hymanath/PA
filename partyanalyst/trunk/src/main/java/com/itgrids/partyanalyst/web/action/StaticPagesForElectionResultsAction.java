57c6bbdae8998a9020d10f8e8b529d8520ecec03
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-05-12 22:47:31 +0000

    Exit Polls -- kamalakar

---
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/StaticPagesForElectionResultsAction.java
@@ -0,0 +1,18 @@
+package com.itgrids.partyanalyst.web.action;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StaticPagesForElectionResultsAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String execute(){
+		
+		return SUCCESS;
+	}
+
+}
