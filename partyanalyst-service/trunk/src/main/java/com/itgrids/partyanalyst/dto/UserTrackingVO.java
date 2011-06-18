3ab404f60add430529dc38b54ee038e5b58bdb8f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-06-18 08:55:45 +0000

    user tracking implementation - hari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserTrackingVO.java
@@ -0,0 +1,38 @@
+package com.itgrids.partyanalyst.dto;
+
+public class UserTrackingVO {
+
+	private String remoteAddress;
+	private String requestURI;
+	private String userType;
+	private Long userId;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getRemoteAddress() {
+		return remoteAddress;
+	}
+	public void setRemoteAddress(String remoteAddress) {
+		this.remoteAddress = remoteAddress;
+	}
+	public String getRequestURI() {
+		return requestURI;
+	}
+	public void setRequestURI(String requestURI) {
+		this.requestURI = requestURI;
+	}
+	
+	
+	
+	
+}
