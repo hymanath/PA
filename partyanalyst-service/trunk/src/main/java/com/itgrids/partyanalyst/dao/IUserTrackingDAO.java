4d810b7a318a30c01005d98e5cd7e8b7157b3773
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-06-18 08:55:17 +0000

    user tracking implementation - hari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserTrackingDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.UserTracking;
+
+
+public interface IUserTrackingDAO extends GenericDao<UserTracking, Long>{
+
+}
