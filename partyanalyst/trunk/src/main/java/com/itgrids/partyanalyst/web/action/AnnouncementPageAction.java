0a740dbcfd37df4af2ddf9ee390d8c70a3c19005
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-26 07:29:16 +0000

    Updated by Kamalakar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AnnouncementPageAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AnnouncementPageAction.java
@@ -1,9 +1,7 @@
 package com.itgrids.partyanalyst.web.action;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -13,35 +11,59 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.util.ServletContextAware;
 import org.json.JSONObject;
-
 import com.itgrids.partyanalyst.dto.AnnouncementResultsVO;
+import com.itgrids.partyanalyst.dto.AnnouncementVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
-import com.itgrids.partyanalyst.service.IAnnocementsService;
+import com.itgrids.partyanalyst.service.IAnnouncementService;
 import com.itgrids.partyanalyst.service.IStaticDataService;
 import com.itgrids.partyanalyst.utils.IConstants;
+import com.itgrids.partyanalyst.utils.ISessionConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import com.opensymphony.xwork2.Preparable;
 
 
-public class AnnouncementPageAction extends ActionSupport implements ServletRequestAware,ServletContextAware {
+public class AnnouncementPageAction extends ActionSupport implements ServletRequestAware,ServletContextAware,ModelDriven, Preparable{
+	
+	private static final long serialVersionUID = 1L;
 	private List<SelectOptionVO> statesList;
 	private IStaticDataService staticDataService;
 	JSONObject jObj = null;
     private List<AnnouncementResultsVO> announcementInfo;
     private String task;
-	public HttpServletRequest request;
-    public ServletContext context;
-    private IAnnocementsService annocementsService;
+	private HttpServletRequest request;
+    private ServletContext context;
+    private IAnnouncementService announcementService;
     private HttpSession session;
-    private long userId;
+    private Long userId;
     private String windowTask;
     private int updated;
-    
+    private List<SelectOptionVO> constituenciesList;
+    private AnnouncementVO announcementVO; 
+    private String announcementId; 
 
-    public int getUpdated() {
+    public IAnnouncementService getAnnouncementService() {
+		return announcementService;
+	}
+	public void setAnnouncementService(IAnnouncementService announcementService) {
+		this.announcementService = announcementService;
+	}
+	
+    public String getAnnouncementId() {
+		return announcementId;
+	}
+	public void setAnnouncementId(String announcementId) {
+		this.announcementId = announcementId;
+	}
+	public AnnouncementVO getAnnouncementVO() {
+		return announcementVO;
+	}
+	public void setAnnouncementVO(AnnouncementVO announcementVO) {
+		this.announcementVO = announcementVO;
+	}
+	public int getUpdated() {
     	return updated;
     }
     public void setUpdated(int updated) {
@@ -56,33 +78,9 @@ public class AnnouncementPageAction extends ActionSupport implements ServletRequ
 		this.constituenciesList = constituenciesList;
 	}
 
-
-	private List<SelectOptionVO> constituenciesList;
- //   private long success;
-    
-    
-    
-//    
-//    public List<AnnouncementInfo> getAnnouncementInf() {
-//		return announcementInf;
-//	}
-//
-//	public void setAnnouncementInf(List<AnnouncementInfo> announcementInf) {
-//		this.announcementInf = announcementInf;
-//	}
-
-     
 	public long getUserId() {
 		return userId;
 	}
-
-//	public long getSuccess() {
-//		return success;
-//	}
-//
-//	public void setSuccess(long success) {
-//		this.success = success;
-//	}
 
 	public String getWindowTask() {
 		return windowTask;
@@ -92,16 +90,8 @@ public class AnnouncementPageAction extends ActionSupport implements ServletRequ
 		this.windowTask = windowTask;
 	}
 
-	public void setUserId(long userId) {
+	public void setUserId(Long userId) {
 		this.userId = userId;
-	}
-
-	public IAnnocementsService getAnnocementsService() {
-		return annocementsService;
-	}
-
-	public void setAnnocementsService(IAnnocementsService annocementsService) {
-		this.annocementsService = annocementsService;
 	}
 
 	public String getTask() {
@@ -136,16 +126,26 @@ public class AnnouncementPageAction extends ActionSupport implements ServletRequ
 		this.statesList = statesList;
 	}
 
-	
+	public void setServletRequest(HttpServletRequest request) {
+		
+		this.request = request;
+	}
+
+	public void setServletContext(ServletContext context) {
+		
+		this.context = context;
+	}
 
 	public String execute(){	
 	
-		System.out.println(windowTask);
+		session = request.getSession();
 		statesList = staticDataService.getParticipatedStatesForAnElectionType(new Long(2));
 		constituenciesList = new ArrayList<SelectOptionVO>();
 		SelectOptionVO selectOptionVO = new SelectOptionVO(0L,"Select Constituency");
 		constituenciesList.add(selectOptionVO);
-		request.setAttribute("windowTask", windowTask);
+		session.setAttribute(ISessionConstants.STATES, statesList);
+		session.setAttribute(ISessionConstants.CONSTITUENCIES, constituenciesList);
+		
 		return "success";
 	}
 	
@@ -156,12 +156,11 @@ public class AnnouncementPageAction extends ActionSupport implements ServletRequ
 		
 		try {
 			jObj=new JSONObject(param);
-	//		System.out.println("jObj = "+jObj);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		Long constituencyId = jObj.getLong("constituencyId");
-		announcementInfo = annocementsService.getAllAnnouncementsInConstituency(constituencyId);
+		announcementInfo = announcementService.getAllAnnouncementsInConstituency(constituencyId);
 	
 	     return Action.SUCCESS;
 	}
@@ -182,86 +181,32 @@ public class AnnouncementPageAction extends ActionSupport implements ServletRequ
 		RegistrationVO regVO = (RegistrationVO) session.getAttribute("USER");
 		userId = regVO.getRegistrationID();
 		System.out.println(userId);
-		announcementInfo = annocementsService.getAllAnnouncementsByUserId(userId);
+		announcementInfo = announcementService.getAllAnnouncementsByUserId(userId);
 		System.out.println(announcementInfo);
 		 return Action.SUCCESS;
 	}
 	
-//	public String deleteAnnouncement(){
-//
-//		String param=null;		
-//		param=request.getParameter("task");
-//		System.out.println(getTask());
-//		
-//		try {
-//			jObj=new JSONObject(param);
-//			System.out.println("jObj = "+jObj);
-//		} catch (ParseException e) {
-//			e.printStackTrace();
-//		}
-//		long announcementId =jObj.getLong("announcementId");
-//		
-//		System.out.println(announcementId);
-//		annocementsService.deleteAnnouncement(announcementId);
-//		success = 1L;
-//		 return Action.SUCCESS;
-//	}
 	
-	
-	public void setServletRequest(HttpServletRequest request) {
-		
-		this.request = request;
+	public Object getModel() {
+		return announcementVO;
 	}
-
 	
-	public void setServletContext(ServletContext context) {
+	public void prepare() throws Exception {
 		
-		this.context = context;
-	}
-
-	public String updateAnnouncementDetails(){
-		
-		String param=null;		
-		param=request.getParameter("task");
-		
-		try {
-			jObj=new JSONObject(param);
-			System.out.println("jObj = "+jObj);
-		} catch (ParseException e) {
-			e.printStackTrace();
+		windowTask = request.getParameter("windowTask");
+		if(windowTask.equalsIgnoreCase(IConstants.NEW))
+		{
+			announcementVO = new AnnouncementVO();
 		}
-		Long announcementId = jObj.getLong("announcementId");
-		Long constituencyId = jObj.getLong("constituencyId");
-		String title = jObj.getString("title");
-		String announcement = jObj.getString("announcement");
-		String fromDate = jObj.getString("fromDate");
-		String toDate = jObj.getString("toDate");
-		AnnouncementResultsVO announcementResultsVO = new AnnouncementResultsVO();
-		announcementResultsVO.setAnnouncementsId(announcementId);
-		announcementResultsVO.setConstituencyId(constituencyId);
-		announcementResultsVO.setTitle(title);
-		announcementResultsVO.setDiscription(announcement);
-		SimpleDateFormat format = new SimpleDateFormat(IConstants.DATE_PATTERN);
-		Date FromDate = null;
-		Date ToDate = null;
-		try {
-			
-			   if(fromDate!=null)
-				   
-			 FromDate = format.parse(fromDate);
-			  
-			   if(toDate!=null)
-			 ToDate = format.parse(toDate);
-			   
-		} catch (ParseException e) {
-			e.printStackTrace();
+		else if(windowTask.equalsIgnoreCase(IConstants.UPDATE_EXISTING))
+		{
+			if(Long.parseLong(announcementId) > 0l)
+			{
+				announcementVO = announcementService.getAnnouncementDetailsByAnnouncementId(Long.parseLong(announcementId));
+			}
+			else
+				announcementVO = new AnnouncementVO();	
 		}
-		announcementResultsVO.setFromDate(FromDate);
-		announcementResultsVO.setToDate(ToDate);
-		annocementsService.updateAnnouncementDetails(announcementResultsVO);
-		updated=1;
-		 return Action.SUCCESS;
 	}
 	
-
 }
