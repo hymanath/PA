acbc7309002d8584ffeec37d121d6c8f10faf74a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-26 07:19:51 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserConstituencyScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserConstituencyScopeDAO.java
@@ -1,19 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Calendar;
-import java.util.Iterator;
 import java.util.List;
-
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-import org.hibernate.Query;
-
 import com.itgrids.partyanalyst.dao.IUserConstituencyScopeDAO;
-import com.itgrids.partyanalyst.model.UserAnnouncement;
 import com.itgrids.partyanalyst.model.UserConstituencyScope;
-import com.itgrids.partyanalyst.utils.IConstants;
 
 public class UserConstituencyScopeDAO extends GenericDaoHibernate<UserConstituencyScope, Long> implements IUserConstituencyScopeDAO{
         
@@ -22,10 +13,10 @@ public class UserConstituencyScopeDAO extends GenericDaoHibernate<UserConstituen
 	}
 
 	
-	public List findAnnouncementsByConstituencyId(Long constituencyId,Date date) {
-		
+	@SuppressWarnings("unchecked")
+	public List<Object[]> findAnnouncementsByConstituencyId(Long constituencyId,Date date)
+	{
 		Object[] parameters = {constituencyId,date,date};
-		
 		return getHibernateTemplate().find("select model.usersAnnouncement.announcement.title," +
 				"model.usersAnnouncement.announcement.discription," +
 				"model.usersAnnouncement.user.firstName,model.usersAnnouncement.user.middleName," +
@@ -34,19 +25,18 @@ public class UserConstituencyScopeDAO extends GenericDaoHibernate<UserConstituen
 				" and date(model.usersAnnouncement.announcement.toDate) >= ?  " +
 				"and date(model.usersAnnouncement.announcement.fromDate) <= ? "+
 						"order by model.usersAnnouncement.announcement.fromDate desc",parameters);
-
-		
 	}
-	public List getConstituencyId(long announcementId){
-	List assembly =	getHibernateTemplate().find("select model.constituency.constituencyId,model.constituency.name,model.constituency.electionScope.electionType.electionType,model.constituency.electionScope.state.stateId FROM UserConstituencyScope model " +
-				" where model.usersAnnouncement.announcement.announcementsId = ?",announcementId);
-	if(assembly.size()>0){
-		return assembly;
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getConstituencyId(long announcementId){
+	return getHibernateTemplate().find("select model.constituency.constituencyId,model.constituency.name,model.constituency.electionScope.electionType.electionType FROM UserConstituencyScope model " +
+				" where model.usersAnnouncement.announcement.announcementsId = ? ",announcementId);
 	}
-	else {
-		return getHibernateTemplate().find("select model.constituency.constituencyId,model.constituency.name,model.constituency.electionScope.electionType.electionType FROM UserConstituencyScope model " +
-				" where model.usersAnnouncement.announcement.announcementsId = ?",announcementId);
-	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UserConstituencyScope> getUserConstituencyScopeByAnnouncementId(Long announcementId)
+	{
+		return getHibernateTemplate().find("select model from UserConstituencyScope model where model.userAnnouncement.announcement.announcementId = ? ",announcementId);
 	}
 	
 }
