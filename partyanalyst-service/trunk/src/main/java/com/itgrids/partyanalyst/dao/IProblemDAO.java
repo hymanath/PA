0e85d4409317adbfe5bd20136ca98ec606e8ecdd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-29 11:05:21 +0000

    adding prob refNo

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Problem;
 
 public interface IProblemDAO extends GenericDao<Problem, Long>{
+	
+	public List<String> getProblemReferenceNo(String refNo);
 
 }
