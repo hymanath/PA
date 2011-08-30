2c7bac9e90214d488f6716d994fe2f5b9e05c659
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-30 10:13:38 +0000

    Updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAnnouncementDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAnnouncementDAO.java
@@ -1,13 +1,8 @@
 package com.itgrids.partyanalyst.dao;
 
-import java.util.List;
-
 import org.appfuse.dao.GenericDao;
-
-
 import com.itgrids.partyanalyst.model.Announcement;
 
 public interface IAnnouncementDAO extends GenericDao<Announcement, Long>{
 
-	public List findAnnouncementsByConstituencyId(Long constituencyId);
 }
