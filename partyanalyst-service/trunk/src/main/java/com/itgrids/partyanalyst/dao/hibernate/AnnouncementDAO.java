a5852415a6cd74fcd3f6d3974bdc767c1cf690b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-30 10:17:33 +0000

    Updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AnnouncementDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AnnouncementDAO.java
@@ -1,22 +1,8 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Calendar;
-
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-import org.hibernate.Query;
-
 import com.itgrids.partyanalyst.dao.IAnnouncementDAO;
-import com.itgrids.partyanalyst.dto.AnnouncementResultsVO;
-import com.itgrids.partyanalyst.model.AnanymousUser;
 import com.itgrids.partyanalyst.model.Announcement;
-import com.itgrids.partyanalyst.model.Booth;
-import com.itgrids.partyanalyst.model.UserConstituencyScope;
-import com.itgrids.partyanalyst.utils.IConstants;
-
-import java.text.SimpleDateFormat;
 
 public class AnnouncementDAO extends GenericDaoHibernate<Announcement, Long> implements IAnnouncementDAO {
 
@@ -24,28 +10,4 @@ public class AnnouncementDAO extends GenericDaoHibernate<Announcement, Long> imp
 		super(Announcement.class);
 	}
 	
-	
-	public List findAnnouncementsByConstituencyId(Long constituencyId) {
-		
-		Calendar currentDate = Calendar.getInstance();
-		System.out.println(constituencyId);
-		SimpleDateFormat formatter=  new SimpleDateFormat(IConstants.DATE_PATTERN);
-		String dateNow = formatter.format(currentDate.getTime());
-		Object[] parameters = {constituencyId,dateNow,dateNow};
-		
-			
-		return getHibernateTemplate().find("select model.title,model.discription," +
-				"model.userAnnouncement.user.firstName,model.userAnnouncement.user.middleName," +
-				"model.userAnnouncement.user.lastName FROM Announcement model WHERE " +
-				"model.userAnnouncement.userConstituencyScope.constituency.constituencyId ="+constituencyId);
-			
-	}
-	
-//	public void deleteAnnouncement(long announcementId){
-//		Announcement announcement = new Announcement();
-//		announcement.setAnnouncementsId(announcementId);
-//	     getHibernateTemplate().delete(announcement);
-//		getHibernateTemplate().flush();
-//		
-//	}
 }
