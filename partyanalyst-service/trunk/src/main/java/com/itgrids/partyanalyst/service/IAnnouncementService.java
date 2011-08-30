3f2ee55ee83f818baba39375ced29cb641662fda
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-30 10:22:53 +0000

    Updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAnnouncementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAnnouncementService.java
@@ -1,9 +1,7 @@
 package com.itgrids.partyanalyst.service;
 import java.util.List;
 
-import com.itgrids.partyanalyst.dto.AnnouncementResultsVO;
 import com.itgrids.partyanalyst.dto.AnnouncementVO;
-import com.itgrids.partyanalyst.dto.ResultStatus;
 public interface IAnnouncementService {
 	
 	public AnnouncementVO saveAnnouncement(AnnouncementVO announcementVO);
@@ -11,8 +9,6 @@ public interface IAnnouncementService {
     public List getAllAnnouncementsInConstituency(Long constituencyId);
     
     public List<AnnouncementVO> getAllAnnouncementsByUserId(Long userId);
-    
-    public void updateAnnouncementDetails(final AnnouncementResultsVO announcementResultsVO);
     
     public AnnouncementVO getAnnouncementDetailsByAnnouncementId(long announcementId);
 }
