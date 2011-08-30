fbc89939b4a074c6e563ea13e634546a944cd489
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-08-30 12:37:30 +0000

    Updated by Uma mahesh

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserConstituencyScopeDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserConstituencyScopeDAOHibernateTest.java
@@ -1,9 +1,15 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import org.appfuse.dao.BaseDaoTestCase;
+import org.junit.Test;
 import com.itgrids.partyanalyst.dao.IUserConstituencyScopeDAO;
+import com.itgrids.partyanalyst.model.UserConstituencyScope;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class UserConstituencyScopeDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -37,13 +43,13 @@ public class UserConstituencyScopeDAOHibernateTest extends BaseDaoTestCase{
     			}
 }
 	
-	public void testGetUserConstituencyScopeByAnnouncementId()
+	/*public void testGetUserConstituencyScopeByAnnouncementId()
 	{
 		List<UserConstituencyScope> list = userConstituencyScopeDAO.getUserConstituencyScopeByAnnouncementId(2l);
 		System.out.println(list.size());
-	}
+	}*/
 	
-	public void testGetAnnouncementDetailsByUserId()
+	/*public void testGetAnnouncementDetailsByUserId()
 	{
 		List<Object[]> list = userConstituencyScopeDAO.getAnnouncementDetailsByUserId(1l);
 		System.out.println(list.size());
@@ -70,4 +76,37 @@ public class UserConstituencyScopeDAOHibernateTest extends BaseDaoTestCase{
 				System.out.print("--"+object.toString());
 		}
 	}*/
+	
+		@Test
+	public void testFindAnnouncementDetailsByUserIdAndDate()
+	{
+		Date fromDate = null;
+		Date toDate = null;
+		Calendar currentDate = Calendar.getInstance();
+		SimpleDateFormat formatter=  new SimpleDateFormat(IConstants.DATE_PATTERN);
+		String dateNow = formatter.format(currentDate.getTime());
+		System.out.println(dateNow);
+		
+		try {
+			fromDate = formatter.parse("12/08/2011");
+			toDate =  formatter.parse("25/08/2011");
+			System.out.println(fromDate);
+			System.out.println(toDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		List<Object[]> list = userConstituencyScopeDAO.findAnnouncementDetailsByUserIdDateConstId(1l,fromDate,toDate,0L);
+		System.out.println(list.size());
+		
+		for(Object[] objects : list)
+		{
+			System.out.println();
+			for(Object object : objects)
+			{
+				System.out.print("--- "+object.toString());
+			}
+		}
+	}
+	
 }
