0eb3269aecd20ba6bb350d69620b93f54565914a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-02 08:09:49 +0000

    code to registration- hari

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ElectionVotingTrendzAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ElectionVotingTrendzAction.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.jfree.util.Log;
 import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IMailService;
+import com.itgrids.partyanalyst.service.IRegistrationService;
 import com.itgrids.partyanalyst.service.IStaticDataService;
 import com.itgrids.partyanalyst.service.IElectionTrendzService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 import com.itgrids.partyanalyst.dto.BoothTotalVotesVO;
@@ -24,6 +28,8 @@ import com.itgrids.partyanalyst.dto.MandalElectionTrendzVO;
 import com.itgrids.partyanalyst.dto.PartyElectionResultVO;
 import com.itgrids.partyanalyst.dto.ElectionTrendzReportVO;
 import com.itgrids.partyanalyst.dto.ElectionTrendzInfoVO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 
 
 
@@ -38,17 +44,76 @@ public class ElectionVotingTrendzAction extends ActionSupport implements Servlet
 	private List<SelectOptionVO> electionType;
 	private List<String> electionYears;
 	private List<MandalVO> mandalVO;
-	
+	private RegistrationVO regVO = null;
 	private IStaticDataService staticDataService;
 	private IElectionTrendzService electionTrendzService; 
-	
+	private IRegistrationService registrationService;
 	private String task = null;
 	JSONObject jObj = null;
 	private MandalCompleteElectionTrendzVO mandalCompleteElectionTrendzVO;
 	private ElectionTrendzReportVO electionTrendzReportVO;
+	private IMailService mailService;
+	private HttpSession session;
+	private Long resultStr;
+	private Integer resultValue;
+		
 	
 	
+	public IMailService getMailService() {
+		return mailService;
+	}
+
+	public void setMailService(IMailService mailService) {
+		this.mailService = mailService;
+	}
+
+	public HttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+
+	public RegistrationVO getRegVO() {
+		return regVO;
+	}
+
+	public void setRegVO(RegistrationVO regVO) {
+		this.regVO = regVO;
+	}
+
+	public Integer getResultValue() {
+		return resultValue;
+	}
+
+	public void setResultValue(Integer resultValue) {
+		this.resultValue = resultValue;
+	}
+
+	public org.json.JSONObject getJObj() {
+		return jObj;
+	}
+	public void setJObj(org.json.JSONObject obj) {
+		jObj = obj;
+	}
 	
+	public Long getResultStr() {
+		return resultStr;
+	}
+
+	public void setResultStr(Long resultStr) {
+		this.resultStr = resultStr;
+	}
+
+	public IRegistrationService getRegistrationService() {
+		return registrationService;
+	}
+
+	public void setRegistrationService(IRegistrationService registrationService) {
+		this.registrationService = registrationService;
+	}
+
 	public IElectionTrendzService getElectionTrendzService() {
 		return electionTrendzService;
 	}
@@ -248,6 +313,57 @@ public class ElectionVotingTrendzAction extends ActionSupport implements Servlet
 		return Action.SUCCESS;
 	}
 	
+	public String checkForRegisteredUserNameAvailability()
+	{
+		String param = null;
+		param = getTask();
+		
+		try {
+			jObj = new JSONObject(param);			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		resultStr = new Long(registrationService.checkForUserNameAvalilability(jObj.getString("userName")).getResultCode());
+		return SUCCESS;
+	}
+	
+	public String changeRegisteredUserNameToEmail(){
+		session = request.getSession();
+		regVO = new RegistrationVO();
+		regVO=(RegistrationVO)session.getAttribute("USER");
+		Long userId=regVO.getRegistrationID();
+		
+		try {
+			jObj = new JSONObject(getTask());
+			if(jObj.getString("task").equalsIgnoreCase("changeRegisteredUserNameToEmail")){
+				
+				resultValue=registrationService.updateRegisteredUserDetailsToUserNameToEmail(userId,jObj.getString("userName"));	
+				return SUCCESS;
+			}
+			/*		String requestURL= request.getRequestURL().toString();
+					String requestFrom = "";
+					if(requestURL.contains("www.partyanalyst.com"))
+						requestFrom = IConstants.SERVER;
+					else
+						requestFrom = IConstants.LOCALHOST;
+					
+					ResultStatus rs = mailService.sendMailToUserToRecoverPassword(regVO,requestFrom);
+				 
+				 if(rs.getResultCode() == 1){
+					 regVO = null;
+				 }
+				return SUCCESS;
+			}*/
+			
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return SUCCESS;
+	
+	}
+	
 	public String getMandalsByDistrictId() throws Exception
 	{
 		String param = null;
@@ -290,5 +406,7 @@ public class ElectionVotingTrendzAction extends ActionSupport implements Servlet
 		
 		return Action.SUCCESS;
 	}
+
+	
 	
 }
