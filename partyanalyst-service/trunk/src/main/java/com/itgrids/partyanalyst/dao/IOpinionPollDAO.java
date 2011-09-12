86e5ef66e37bd08d1643b95cff4b005694af2744
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-12 06:43:41 +0000

    Removed unused imports - kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IOpinionPollDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IOpinionPollDAO.java
@@ -1,12 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
-import java.util.List;
-
 import org.appfuse.dao.GenericDao;
-
 import com.itgrids.partyanalyst.model.OpinionPoll;
-import com.itgrids.partyanalyst.model.Registration;
-import com.itgrids.partyanalyst.model.OpinionPollQuestions;
 
 public interface IOpinionPollDAO extends GenericDao<OpinionPoll, Long>{
 
