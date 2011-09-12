28e3319f447fadc04f527fda830697513d27cd83
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-12 06:54:20 +0000

    Removed unused imports - kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/OpinionPollDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/OpinionPollDAOHibernateTest.java
@@ -1,19 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.util.ArrayList;
 import org.appfuse.dao.BaseDaoTestCase;
-import java.sql.SQLException;
-import java.util.List;
-
-import com.itgrids.partyanalyst.model.Registration;
-import com.itgrids.partyanalyst.model.OpinionPollQuestions;
-import com.itgrids.partyanalyst.model.OpinionPollQuestionOptions;
-import com.itgrids.partyanalyst.model.OpinionPollResult;
-import com.itgrids.partyanalyst.model.OpinionPoll;
-import com.itgrids.partyanalyst.model.QuestionsRepository;
-
 import com.itgrids.partyanalyst.dao.IOpinionPollDAO;
-
 
 public class OpinionPollDAOHibernateTest extends BaseDaoTestCase {
 	
