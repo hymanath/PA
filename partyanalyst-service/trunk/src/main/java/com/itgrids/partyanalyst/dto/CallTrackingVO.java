31d801ac41b113ba631485e0c3313fcad7f32387
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-19 11:45:30 +0000

    Updated by  - Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CallTrackingVO.java
@@ -0,0 +1,81 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CallTrackingVO implements Serializable {
+	
+	private Long problemId;
+	private String problemPurpose;
+	private String referenceNo;
+	private Long callTrackingDetailId;
+	private String name;
+	private String mobile;
+	private String problemAddedDate;
+	private String villageOrTown;
+	private Long count;
+	private Long status;
+	
+	
+	public Long getProblemId() {
+		return problemId;
+	}
+	public void setProblemId(Long problemId) {
+		this.problemId = problemId;
+	}
+	public String getProblemPurpose() {
+		return problemPurpose;
+	}
+	public void setProblemPurpose(String problemPurpose) {
+		this.problemPurpose = problemPurpose;
+	}
+	public Long getCallTrackingDetailId() {
+		return callTrackingDetailId;
+	}
+	public void setCallTrackingDetailId(Long callTrackingDetailId) {
+		this.callTrackingDetailId = callTrackingDetailId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getProblemAddedDate() {
+		return problemAddedDate;
+	}
+	public void setProblemAddedDate(String problemAddedDate) {
+		this.problemAddedDate = problemAddedDate;
+	}
+	public String getVillageOrTown() {
+		return villageOrTown;
+	}
+	public void setVillageOrTown(String villageOrTown) {
+		this.villageOrTown = villageOrTown;
+	}
+	public Long getCount() {
+		return count;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	public Long getStatus() {
+		return status;
+	}
+	public void setStatus(Long status) {
+		this.status = status;
+	}
+	public String getReferenceNo() {
+		return referenceNo;
+	}
+	public void setReferenceNo(String referenceNo) {
+		this.referenceNo = referenceNo;
+	}
+			
+}
