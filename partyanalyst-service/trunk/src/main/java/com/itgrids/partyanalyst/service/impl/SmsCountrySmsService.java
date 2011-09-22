f414618600f1c6088aa9d9eb98e639b52e54fd6f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-22 14:09:48 +0000

    New Sms functionality Implementation - kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SmsCountrySmsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SmsCountrySmsService.java
@@ -11,21 +11,18 @@ import org.apache.log4j.Logger;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
-
 import com.itgrids.partyanalyst.dao.IRegistrationDAO;
 import com.itgrids.partyanalyst.dao.ISmsHistoryDAO;
 import com.itgrids.partyanalyst.dao.ISmsModuleDAO;
 import com.itgrids.partyanalyst.dao.ISmsTrackDAO;
+import com.itgrids.partyanalyst.dto.ResultCodeMapper;
+import com.itgrids.partyanalyst.model.Registration;
 import com.itgrids.partyanalyst.model.SmsHistory;
 import com.itgrids.partyanalyst.model.SmsModule;
 import com.itgrids.partyanalyst.model.SmsTrack;
 import com.itgrids.partyanalyst.service.IPartyAnalystPropertyService;
 import com.itgrids.partyanalyst.service.ISmsService;
 
-/*
- * Username: dakavaram
- * Password: 19716044
- */
 public class SmsCountrySmsService implements ISmsService {
 
 	private static final Logger log = Logger
@@ -86,97 +83,89 @@ public class SmsCountrySmsService implements ISmsService {
 		this.propertyService = propertyService;
 	}
 	
+	/**
+	 *  This Method will Send SMS to given Mobile Numbers
+	 *  @param String message
+	 *  @param boolean isEnglish
+	 *  @param Long userId
+	 *  @param String moduleName
+	 *  @param String[] phoneNumbers
+	 *  @author Kamalakar Dandu
+	 *  @return Long resultCode
+	 *  
+	 */
+	
 	public Long sendSms(String message, boolean isEnglish,Long userId,String moduleName,
-			String... phoneNumbers) {
+			String... phoneNumbers) 
+	{
 		Long smsResultCode;
 		HttpClient client = null;
 		PostMethod post = null;
+		
+		Long count = getRemainingSmsLeftForUser(userId) - phoneNumbers.length;
+		
+		if(count < 0)
+			return (long)ResultCodeMapper.FAILURE;
+		
+		List<Object[]> smsDetails = smsTrackDAO.getUserSmsDetails(userId);
+		
+		if(smsDetails == null || smsDetails.size() == 0)
+			return (long)ResultCodeMapper.FAILURE;
+		
 		smsResultCode = saveSmsData(message,userId,moduleName,phoneNumbers);
-		if(smsResultCode==1){
-			//Failure
-		}else if(smsResultCode==0){
-			client = new HttpClient(new MultiThreadedHttpConnectionManager());
+		
+		client = new HttpClient(new MultiThreadedHttpConnectionManager());
 
-			/* SETUP PROXY */
-			/*if (propertyService.getProperty(PropertyKeys.SERVER_PROXY_HOST) != null && propertyService.getProperty(PropertyKeys.SERVER_PROXY_HOST).trim().length() > 0) {
-				int port = 8080;
-				try {
-					port = Integer.parseInt(propertyService
-							.getProperty(PropertyKeys.SERVER_PROXY_PORT));
-				} catch (NumberFormatException nfe) {
-					log.error(nfe);
-				}
-				client.getHostConfiguration().setProxy(propertyService.getProperty(PropertyKeys.SERVER_PROXY_HOST), port);
-			}*/
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(
+				Integer.parseInt("30000"));
 
-			/*client.getHttpConnectionManager().getParams().setConnectionTimeout(
-					Integer.parseInt(propertyService.getProperty(
-									PropertyKeys.SMS_SMSCOUNTRY_CONNECTION_TIMEOUT,"30000")));*/
-			client.getHttpConnectionManager().getParams().setConnectionTimeout(
-					Integer.parseInt("30000"));
-
-			/*post = new PostMethod(propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_SERVICE_URL));*/
-			post = new PostMethod("http://smscountry.com/SMSCwebservice.asp");
-			// give all in string
-			/*post.addParameter("User", propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_USER));
-			post.addParameter("passwd", propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_PASSWORD));*/
-			
-			post.addParameter("User", "dakavaram");
-			post.addParameter("passwd", "1tGridsINDIA");
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < phoneNumbers.length; i++) {
-				sb.append("91");
-				sb.append(phoneNumbers[i]);
-				System.out.println("Phone Numbers--->"+phoneNumbers[i]);
-				if (i < (phoneNumbers.length-1))
-					sb.append(",");
-			}
-			
-		    System.out.println("Mobile Nos :" + sb.toString());
-		/*	System.out.println("Using "+propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_USER)+propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_PASSWORD)+" for "+sb);*/
-			post.addParameter("mobilenumber", sb.toString());
-			post.addParameter("message", message);/*
-			post.addParameter("sid", propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_SID));*/
-			post.addParameter("sid", "smscntry");
-			post.addParameter("mtype", isEnglish ? "N" : "OL");
-			/*post.addParameter("DR", propertyService
-					.getProperty(PropertyKeys.SMS_SMSCOUNTRY_DR));*/
-			post.addParameter("DR", "Y");
-			
-			System.out.println(" Query String :" + post.getQueryString());
-
-			/* PUSH the URL */
-			try {
-				int statusCode = client.executeMethod(post);
-				if(log.isInfoEnabled()){
-					log.info(post.getStatusLine().toString());
-					System.out.println(post.getStatusLine().toString()+"***"+statusCode+"*****"+post.getQueryString());
-				}
-				System.out.println(post.getStatusLine().toString()+"***"+statusCode+"*****"+post.getQueryString());
-				if (statusCode != HttpStatus.SC_OK) {
-					log.error("SmsCountrySmsService.sendSMS failed: "
-							+ post.getStatusLine());
-					System.out.println("SmsCountrySmsService.sendSMS failed: "
-							+ post.getStatusLine());
-				}
-				if(log.isInfoEnabled()){
-					log.info(post.getResponseBodyAsString());
-					System.out.println(post.getResponseBodyAsString());
-				}} catch (Exception e) {
-					log.error(e);
-					System.out.println(e);
-				} finally {
-					post.releaseConnection();
-				}
-
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < phoneNumbers.length; i++) {
+			sb.append("91");
+			sb.append(phoneNumbers[i]);
+			if (i < (phoneNumbers.length-1))
+				sb.append(",");
 		}
-			return smsResultCode;	
+			
+	    log.debug("Mobile Nos :" + sb.toString());
+	    
+	    post = new PostMethod("http://sms.partyanalyst.com/WebserviceSMS.aspx");
+		
+		post.addParameter("User", smsDetails.get(0)[0].toString());
+		post.addParameter("passwd", smsDetails.get(0)[1].toString());
+		post.addParameter("sid", smsDetails.get(0)[2].toString());
+		
+	    post.addParameter("mobilenumber", sb.toString());
+		post.addParameter("message", message);
+		post.addParameter("mtype", isEnglish ? "N" : "OL");
+		post.addParameter("DR", "Y");
+		
+		log.debug(" Query String :" + post.getQueryString());
+
+		/* PUSH the URL */
+		try 
+		{
+			int statusCode = client.executeMethod(post);
+			
+			log.debug(post.getStatusLine().toString()+"***"+statusCode+"*****"+post.getQueryString());
+			
+			if (statusCode != HttpStatus.SC_OK) {
+				log.error("SmsCountrySmsService.sendSMS failed: "+ post.getStatusLine());
+				log.debug("SmsCountrySmsService.sendSMS failed: "+ post.getStatusLine());
+			}
+			else
+				smsTrackDAO.updateRemainingSmsLeftForUser(userId,count);
+			
+			log.debug(post.getResponseBodyAsString());
+		}catch (Exception e) {
+				log.error(e);
+				System.out.println(e);
+		} finally {
+				post.releaseConnection();
+		}
+		
+		
+		return smsResultCode;	
 	}
 	
 	/**
@@ -186,54 +175,34 @@ public class SmsCountrySmsService implements ISmsService {
 	 * @param moduleName
 	 * @param phoneNumbers
 	 */
-	public Long saveSmsData(final String message,final Long userId,final String moduleName,final String... phoneNumbers){
-		Long smsResultCode=0l;
+	
+	public Long  saveSmsData(final String message,final Long userId,final String moduleName,final String... phoneNumbers)
+	{
 		try{
+			Registration registration = registrationDAO.get(userId);
+			SmsModule smsModule = null;
 			
-			setDefualtCredentialForUser(userId);			
+			List<SmsModule> list = smsModuleDAO.findByModuleName(moduleName);
 			
-			Long leftSMSCount = getRemainingSmsLeftForUser(userId)-phoneNumbers.length;
-			if(leftSMSCount<0){
-				smsResultCode = 1l;  		//Failure
-			}else{
-				smsResultCode = 0l;			//Success
-				transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-					public void doInTransactionWithoutResult(TransactionStatus status) {	
-						SmsHistory history = new SmsHistory();
-						Long moduleId=0l;
-						int counter = 1;
-						Long latestSmsTrackIdForUser = 0l;
-						for (int i = 0; i < phoneNumbers.length; i++) {
-							history.setMobileNumber(phoneNumbers[i]);
-							history.setSmsContent(message);							
-							history.setSentDate(getCurrentDate());
-							
-							List<SmsTrack> result = smsTrackDAO.findLatestRenewalDate(userId);
-							for(SmsTrack latestDate : result){
-								if(counter == 1){
-									latestSmsTrackIdForUser = latestDate.getSmsTrackId();
-								}
-								counter++;
-							}				
-							
-							history.setSmsTrack(smsTrackDAO.get(latestSmsTrackIdForUser));
-							for(SmsModule module : smsModuleDAO.getAll()){
-								if(module.getModuleName().equalsIgnoreCase(moduleName)){
-									moduleId = module.getSmsModuleId();
-								}
-							}
-							history.setSmsModule(smsModuleDAO.get(moduleId));
-							history = smsHistoryDAO.save(history);
-						}
-					}
-				});
+			if(list != null && list.size() > 0)
+				smsModule = list.get(0);
+			
+			for(String mobileNo : phoneNumbers)
+			{
+				SmsHistory smsHistory = new SmsHistory();
+				smsHistory.setRegistration(registration);
+				smsHistory.setMobileNumber(mobileNo);
+				smsHistory.setSmsContent(message);
+				smsHistory.setSentDate(getCurrentDate());
+				smsHistory.setSmsModule(smsModule);
+				smsHistoryDAO.save(smsHistory);
 			}
-			return smsResultCode;
-		}catch(Exception e){
-			e.printStackTrace();
-			log.debug("Exception Raised--->"+e);
-		}		
-		return smsResultCode;
+			
+			return (long)ResultCodeMapper.SUCCESS;
+		}catch (Exception e) {
+			log.error("Error Occured in Saving SMS Data, Exception is - "+e);
+			return (long)ResultCodeMapper.FAILURE;
+		}
 	}
 	
 	public void setDefualtCredentialForUser(final Long userId){
@@ -273,36 +242,17 @@ public class SmsCountrySmsService implements ISmsService {
 	 * This method return total sms left for the user.
 	 * 
 	 * @param userId
-	 * @return
+	 * @return Long Remaining SMS
 	 */
-	public Long getRemainingSmsLeftForUser(Long userId){		
-		int counter = 1;
-		Long latestSmsCount = 0l;
-		Long latestSmsTrackIdForUser = 0l;
-		Long totalSmsLeft = 0l;
+	
+	public Long getRemainingSmsLeftForUser(Long userId)
+	{
 		try{
-			setDefualtCredentialForUser(userId);
-			
-			List<SmsTrack> result = smsTrackDAO.findLatestRenewalDate(userId);
-			if(result!=null){
-				for(SmsTrack track : result){
-					if(counter == 1){
-						latestSmsCount = track.getRenewalSmsCount();
-						latestSmsTrackIdForUser = track.getSmsTrackId();
-					}
-					counter++;
-				}
-			}
-						
-			List<Long> totalSmsAfterRenewal = smsHistoryDAO.getTotalSmsSentByUserAfterRenewal(latestSmsTrackIdForUser);
-			if(totalSmsAfterRenewal!=null){
-				totalSmsLeft = latestSmsCount - Long.parseLong(totalSmsAfterRenewal.get(0).toString());
-			}
-			return totalSmsLeft;			
-			
-		}catch(Exception e){
-			e.printStackTrace();
+			Long count = (Long)smsTrackDAO.getRemainingSMSCountForAUser(userId);
+			return count != null ? count : 0L;
+		}catch (Exception e) {
+			log.error("Error Occured - "+e);
+			return 0L;
 		}
-		return totalSmsLeft;		
 	}
 }
