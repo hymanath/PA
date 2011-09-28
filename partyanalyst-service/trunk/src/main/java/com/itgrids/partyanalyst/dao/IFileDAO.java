9c2445019284cadd8f7eecbb88017779d5809763
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-28 07:51:28 +0000

    File Uploading - sachin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFileDAO.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.File;
+import com.itgrids.partyanalyst.model.FileType;
+
+public interface IFileDAO extends GenericDao<File, Long> {
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFileDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ProblemFile;
+
+public interface IProblemFileDAO extends GenericDao<ProblemFile, Long> {
+
+}
