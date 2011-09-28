8e81b850d8c2aaba3f11b5ad48ae5c199bf49676
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-28 07:55:01 +0000

    File Uploading - sachin

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FileDAOHibernateTest.java
@@ -0,0 +1,24 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IFileDAO;
+
+public class FileDAOHibernateTest extends BaseDaoTestCase{
+	
+	private IFileDAO fileDAO;
+
+	
+
+	public void setFileDAO(IFileDAO fileDAO) {
+		this.fileDAO = fileDAO;
+	}
+	
+	/*public void test()
+	{
+		fileDAO.getAll();
+		
+	}*/
+
+	
+}
