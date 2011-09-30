533ea0b145b127def336b646aa8923764d77fb0e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-09-30 12:59:55 +0000

    Gallaries Integration - kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGallaryDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.Gallary;
+
+public interface IGallaryDAO extends GenericDao<Gallary, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGallaryTypeDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.GallaryType;
+
+public interface IGallaryTypeDAO extends GenericDao<GallaryType, Long>{
+
+}
