cd517766327bdb6f31c42fb10acf4ddd91e52e89
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-01 14:30:34 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFileDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFileDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ProblemFile;
 
 public interface IProblemFileDAO extends GenericDao<ProblemFile, Long> {
-
+ 
+	
+	public List<Object[]> getProblemImagesBasedHistoryId(Long probHistoryId);
 }
