eb8af066cb5bc54f05b65e446b37d617f078fb43
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-01 07:34:34 +0000

    Upadted by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserGallaryDAO.java
+++
@@ -1,8 +0,0 @@
-package com.itgrids.partyanalyst.dao;
-
-import org.appfuse.dao.GenericDao;
-import com.itgrids.partyanalyst.model.UserGallary;
-
-public interface IUserGallaryDAO extends GenericDao<UserGallary, Long>{
-
-}
