42c6e46fae855102c2f5d0ffa692a870244e1a92
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-04 09:40:05 +0000

    Problem File approval - sachin

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFileDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFileDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IProblemFileDAO;
@@ -13,9 +15,28 @@ public class ProblemFileDAOHibernateTest extends BaseDaoTestCase{
 		this.problemFileDAO = problemFileDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		problemFileDAO.getAll();
 		
+	}*/
+	/*public void testGetAllNonApprovedImagesBetweenDatesWithCompleteData(){
+		
+		List<Object[]> result = problemFileDAO.getAllNonApprovedImagesBetweenDatesWithCompleteData();
+		System.out.println(result.size());
+		}*/
+	
+	public void testGetAllNonApprovedFilesAndProblemDetails()
+	{
+		List<Object[]> list = problemFileDAO.getAllNonApprovedFilesAndProblemDetails();
+		
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println();
+			for(Object obj: params)
+				System.out.print("----"+obj.toString());
+		}
 	}
 }
