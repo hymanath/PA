c2cef29893fed311d1ee4a1e1d3575a1b9a41749
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-11 17:33:40 +0000

    Updated by Kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserSubuserRelationDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.UserSubuserRelation;
+
+public interface IUserSubuserRelationDAO extends GenericDao<UserSubuserRelation, Long>{
+
+}
