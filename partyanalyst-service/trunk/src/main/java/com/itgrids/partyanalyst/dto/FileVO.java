8e90fc1471a62445983892b41a62fb476ea6851c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-13 13:58:26 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/FileVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/FileVO.java
@@ -12,10 +12,14 @@ public class FileVO extends ResultStatus implements Serializable {
 	private List<String> fileName = new ArrayList<String>();
 	private List<String> fileTitle;
 	private List<String> fileDescription;
-	private Long fileTypeId;
+	private Long fileTypeId,gallaryId;
 	private Long fileId;
 	private Long problemFileId;
 	private List<String> filePath;
+	private String file,gallaryName,gallaryDescription,gallaryCreatedDate;
+	private String title,gallaryUpdatedDate,name,path;
+	private String description;
+	private String pathOfFile;
 	private String fileName1;
 	private String fileTitle1;
 	private String fileDescription1;
@@ -24,52 +28,36 @@ public class FileVO extends ResultStatus implements Serializable {
 	private String scope;
 	private Date existingFrom;
 	private Date identifiedOn;
-	private String name;
-	private String lastName;
-	private String fullName;
-	private String file;
-	private String title;
-	private String description;
-	private String pathOfFile;
-	
-	public String getFile() {
-		return file;
+	public String getFileName1() {
+		return fileName1;
 	}
 
-	public void setFile(String file) {
-		this.file = file;
+	public void setFileName1(String fileName1) {
+		this.fileName1 = fileName1;
 	}
 
-	public String getTitle() {
-		return title;
+	public String getFileTitle1() {
+		return fileTitle1;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setFileTitle1(String fileTitle1) {
+		this.fileTitle1 = fileTitle1;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getFileDescription1() {
+		return fileDescription1;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setFileDescription1(String fileDescription1) {
+		this.fileDescription1 = fileDescription1;
 	}
 
-	public String getPathOfFile() {
-		return pathOfFile;
+	public String getFilePath1() {
+		return filePath1;
 	}
 
-	public void setPathOfFile(String pathOfFile) {
-		this.pathOfFile = pathOfFile;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setFilePath1(String filePath1) {
+		this.filePath1 = filePath1;
 	}
 
 	public String getProblem() {
@@ -104,14 +92,6 @@ public class FileVO extends ResultStatus implements Serializable {
 		this.identifiedOn = identifiedOn;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getLastName() {
 		return lastName;
 	}
@@ -120,38 +100,46 @@ public class FileVO extends ResultStatus implements Serializable {
 		this.lastName = lastName;
 	}
 
-	public String getFileName1() {
-		return fileName1;
+	public String getFullName() {
+		return fullName;
 	}
 
-	public void setFileName1(String fileName1) {
-		this.fileName1 = fileName1;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
 	}
 
-	public String getFileTitle1() {
-		return fileTitle1;
+	private String lastName;
+	private String fullName;
+
+	public void setFile(String file) {
+		this.file = file;
 	}
 
-	public void setFileTitle1(String fileTitle1) {
-		this.fileTitle1 = fileTitle1;
+	public String getFile() {
+		return file;
+	}
+	public String getTitle() {
+		return title;
 	}
 
-	public String getFileDescription1() {
-		return fileDescription1;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
-	public void setFileDescription1(String fileDescription1) {
-		this.fileDescription1 = fileDescription1;
+	public String getDescription() {
+		return description;
 	}
 
-	public String getFilePath1() {
-		return filePath1;
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPathOfFile() {
+		return pathOfFile;
 	}
 
-	public void setFilePath1(String filePath1) {
-		this.filePath1 = filePath1;
+	public void setPathOfFile(String pathOfFile) {
+		this.pathOfFile = pathOfFile;
 	}
-
 	public List<String> getFileContentType() {
 		return fileContentType;
 	}
@@ -216,4 +204,61 @@ public class FileVO extends ResultStatus implements Serializable {
 		this.filePath = filePath;
 	}
 
+	public Long getGallaryId() {
+		return gallaryId;
+	}
+
+	public String getGallaryName() {
+		return gallaryName;
+	}
+
+	public String getGallaryDescription() {
+		return gallaryDescription;
+	}
+
+	public String getGallaryCreatedDate() {
+		return gallaryCreatedDate;
+	}
+
+	public String getGallaryUpdatedDate() {
+		return gallaryUpdatedDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setGallaryId(Long gallaryId) {
+		this.gallaryId = gallaryId;
+	}
+
+	public void setGallaryName(String gallaryName) {
+		this.gallaryName = gallaryName;
+	}
+
+	public void setGallaryDescription(String gallaryDescription) {
+		this.gallaryDescription = gallaryDescription;
+	}
+
+	public void setGallaryCreatedDate(String gallaryCreatedDate) {
+		this.gallaryCreatedDate = gallaryCreatedDate;
+	}
+
+	public void setGallaryUpdatedDate(String gallaryUpdatedDate) {
+		this.gallaryUpdatedDate = gallaryUpdatedDate;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	
 }
