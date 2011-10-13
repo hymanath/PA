c0b93aaa7ce59340f8714d5317f0d4d0f40e989e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-13 13:56:20 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGallaryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGallaryDAO.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.Gallary;
 
 public interface IGallaryDAO extends GenericDao<Gallary, Long>{
+	
+	public List<Object[]> getCandidateGallaryDetail(Long candidateId,String type,String isDelete);
 
 }
