dedb7baef36c988067bf240e5c72a8a3e77b95b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-13 13:55:49 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFileGallaryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFileGallaryDAO.java
@@ -1,8 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.FileGallary;
 
 public interface IFileGallaryDAO extends GenericDao<FileGallary, Long>{
+	
+	public List<Object[]> getStartingRecordInGallary(Long gallaryId);
+	
+	public List<Object[]> getAllRecordInGallary(Long gallaryId);
 
 }
