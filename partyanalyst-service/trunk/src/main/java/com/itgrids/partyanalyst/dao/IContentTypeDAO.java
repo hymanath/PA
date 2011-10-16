64a29661792e3e59249de9ce88863e5b57cd61af
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-16 07:56:25 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IContentTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IContentTypeDAO.java
@@ -4,5 +4,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.ContentType;
 
 public interface IContentTypeDAO extends GenericDao<ContentType,Long>{
+	
+	public Object getContentTypeByType(String contentType);
 
 }
