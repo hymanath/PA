b13f0028b2e5a57fdef4229facee5a0ac7db512d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-17 15:47:14 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAOHibernateTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IFileGallaryDAO;
+import com.itgrids.partyanalyst.dto.FileVO;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class FileGallaryDAOHibernateTest extends BaseDaoTestCase{
 
@@ -18,9 +20,17 @@ public class FileGallaryDAOHibernateTest extends BaseDaoTestCase{
 	{
 		fileGallaryDAO.getAll();
 	}*/
-	public void testGetAllRecordInGallary()
+	/*public void testGetAllRecordInGallary()
 	{
 		List<Object[]> results = fileGallaryDAO.getAllRecordInGallary(300L);
+		System.out.println(results.size());
+	}*/
+	public void testGetNewsRecordsBySearchCriteria()
+	{
+		FileVO  fileVO = new FileVO();
+		fileVO.setCandidateId(13722L);
+		fileVO.setKeywords("news");
+		List<Object[]> results = fileGallaryDAO.getNewsRecordsBySearchCriteria(fileVO,IConstants.NEWS_GALLARY);
 		System.out.println(results.size());
 	}
 	
