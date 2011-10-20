f0abde016193bdc2bb35bd6f23108a42ded0aeb5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-20 13:20:14 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FileDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FileDAO.java
@@ -1,13 +1,24 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IFileDAO;
 import com.itgrids.partyanalyst.model.File;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class FileDAO extends GenericDaoHibernate<File, Long> implements
 		IFileDAO {
 	public FileDAO() {
 		super(File.class);
+	}
+	public List<Object[]> getFileByFileId(Long fileId)
+	{
+		return getHibernateTemplate().find("select model.fileId,model.fileName,model.filePath,model.fileTitle,model.fileDescription , " +
+				" model.source ,model.fileDate from File model where  model.fileId=?",fileId); 
+		
+		
 	}
 }
