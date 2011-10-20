f43fdc9d7940e3563c3ec37ac5831df86ca45d56
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-20 13:23:20 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
@@ -25,13 +27,45 @@ public class FileGallaryDAOHibernateTest extends BaseDaoTestCase{
 		List<Object[]> results = fileGallaryDAO.getAllRecordInGallary(300L);
 		System.out.println(results.size());
 	}*/
-	public void testGetNewsRecordsBySearchCriteria()
+	/*public void testGetNewsRecordsBySearchCriteria()
 	{
 		FileVO  fileVO = new FileVO();
 		fileVO.setCandidateId(13722L);
 		fileVO.setKeywords("news");
 		List<Object[]> results = fileGallaryDAO.getNewsRecordsBySearchCriteria(fileVO,IConstants.NEWS_GALLARY);
 		System.out.println(results.size());
+	}*/
+	public void testGetNewsToDisplay()
+	{
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		List<Object[]> results = fileGallaryDAO.getFirstFourNewsToDisplay(13722L);
+		System.out.println(results.size());
+		for(Object[] newsDetails: results){
+		    
+		    System.out.println((Long)newsDetails[0]);
+		    System.out.println( newsDetails[1].toString());		    			    	
+		    System.out.println((IConstants.UPLOADED_FILES+"/"+newsDetails[1].toString()));
+		    System.out.println((newsDetails[3].toString()));
+	   	    String desc=""; 
+	   	 if(newsDetails[4] != null && newsDetails[4].toString().length()>=55)
+	   	    {
+	   	    desc = newsDetails[4].toString().substring(0, 50);
+	   	    desc = desc+"...";
+	   	    }
+	   	    else
+	   	    {
+	   	     desc = newsDetails[4].toString();
+	   	     desc = desc+"...";
+	   	    }
+	   	 System.out.println(desc);
+	   	 if(newsDetails[5]!= null)
+	   	System.out.println((newsDetails[5].toString()));
+	   	 if(newsDetails[6]!= null)
+	   	System.out.println((sdf.format((Date)newsDetails[6])) );
+	   	 if(newsDetails[7]!= null)
+	   	System.out.println(((Long)newsDetails[7]));
+	 }
+	 
 	}
 	
 }
