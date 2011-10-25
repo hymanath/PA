38e64e152f1ae6259fa9cf5fbfa65892992e79b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-25 06:56:06 +0000

    updated by kamalakar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/util/IWebConstants.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/util/IWebConstants.java
@@ -4,4 +4,5 @@ public interface IWebConstants {
 
 	public static final String USER = "USER";
 	public static final long FILE_MAX_LENGTH = 2097152;
+	public static final int FILE_RANDOM_NO = 1000000000;
 }
