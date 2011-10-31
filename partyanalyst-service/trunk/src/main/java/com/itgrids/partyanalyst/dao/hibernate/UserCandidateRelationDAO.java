e61ed555bc7229d51da553910e1b248c84335334
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-31 16:09:51 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserCandidateRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserCandidateRelationDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IUserCandidateRelationDAO;
 import com.itgrids.partyanalyst.model.UserCandidateRelation;
@@ -11,4 +14,19 @@ public class UserCandidateRelationDAO extends GenericDaoHibernate<UserCandidateR
 	{
 		super(UserCandidateRelation.class);
 	}
+	
+	public void deleteUserCandidateRelation(Long userCandidateRelationId)
+	{
+		StringBuilder query = new StringBuilder();
+		query.append("delete from UserCandidateRelation model where model.userCandidateRelationId=:userCandidateRelationId");
+		Query queryObject = getSession().createQuery(query.toString());
+		queryObject.setLong("userCandidateRelationId", userCandidateRelationId);
+		queryObject.executeUpdate();
+	}
+	public List<Object[]> getUserCandidateRelationDetails(Long userId)
+	{
+		return getHibernateTemplate().find("select model.userCandidateRelationId,model.candidate.candidateId," +
+				" model.candidate.lastname from UserCandidateRelation model where model.registration.registrationId=?",userId);
+	}
 }
+
