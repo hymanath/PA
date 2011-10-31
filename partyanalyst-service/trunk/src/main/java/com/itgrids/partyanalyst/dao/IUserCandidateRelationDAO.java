4b4039e72d119e46cb6912399f86649b3ebb52b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-10-31 16:08:47 +0000

    Updated By Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserCandidateRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserCandidateRelationDAO.java
@@ -1,8 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.UserCandidateRelation;
 
 public interface IUserCandidateRelationDAO extends GenericDao<UserCandidateRelation,Long>{
+	
+	public void deleteUserCandidateRelation(Long userCandidateRelationId);
 
+	public List<Object[]> getUserCandidateRelationDetails(Long userId);
 }
