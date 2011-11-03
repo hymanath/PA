d62c31a0e0e6289a8fa35f75c1ace980f47b9aa5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-03 11:09:06 +0000

    Sending message functionality in candidate page -- Sachin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMessageToCandidateDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.MessageToCandidate;
+
+public interface IMessageToCandidateDAO extends GenericDao<MessageToCandidate, Long>{
+
+}
