3f53a036eda380a6f25a462e6a33847d83e3af4d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-03 11:10:06 +0000

    Sending message functionality in candidate page -- Sachin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MessageToCandidateDAO.java
@@ -0,0 +1,15 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.dao.IMessageToCandidateDAO;
+import com.itgrids.partyanalyst.model.MessageToCandidate;
+
+public class MessageToCandidateDAO extends
+		GenericDaoHibernate<MessageToCandidate, Long> implements
+		IMessageToCandidateDAO {
+	public MessageToCandidateDAO() {
+		super(MessageToCandidate.class);
+	}
+
+}
