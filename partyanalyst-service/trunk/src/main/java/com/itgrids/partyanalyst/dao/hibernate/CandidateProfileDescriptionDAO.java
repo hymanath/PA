785e576c47688eae2f53fe9eaff96bdb0824dc9d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-03 11:09:23 +0000

    Updated by Sachin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateProfileDescriptionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateProfileDescriptionDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICandidateProfileDescriptionDAO;
 import com.itgrids.partyanalyst.model.CandidateProfileDescription;
@@ -24,4 +25,22 @@ public class CandidateProfileDescriptionDAO extends
 		
 		return getHibernateTemplate().find("select max(model.orderNo) from CandidateProfileDescription model where model.candidate.candidateId=?",candidateId);
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getCandidateProfileInfo(Long candidateId){		
+		return getHibernateTemplate().find("select model.orderNo , model.description , model.candidateProfileDescriptionId from CandidateProfileDescription model where model.candidate.candidateId=?",candidateId);
+	}
+	
+	/*@SuppressWarnings("unchecked")
+	public List<Object> deleteCandidateProfileDescriptionById(Long profDescId){		
+		return getHibernateTemplate().find("DELETE  FROM CandidateProfileDescription model where model.candidateProfileDescriptionId = ?",profDescId);
+	}     */       
+	
+	public Integer deleteCandidateProfileDescriptionById(Long profDescId) {
+		Query queryObject = getSession().createQuery("DELETE  FROM CandidateProfileDescription model where model.candidateProfileDescriptionId = ?");
+		queryObject.setParameter(0, profDescId);
+		return queryObject.executeUpdate();
+		
+	}
+	
 }
