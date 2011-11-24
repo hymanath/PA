b059974aaa9013c32a420e7313621f8a0e96b19f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-24 06:22:12 +0000

    IPartyGalleryDAO - anu & nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyGalleryDAO.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.dao;
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.PartyGallery;
+
+public interface IPartyGalleryDAO extends GenericDao<PartyGallery,Long>{
+
+}
