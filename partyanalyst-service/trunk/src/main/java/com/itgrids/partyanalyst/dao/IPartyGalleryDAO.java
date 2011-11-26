a55147a382009aeba88989c067a7b56d549b45f2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-26 08:16:57 +0000

    Updated by Sachin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyGalleryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyGalleryDAO.java
@@ -1,7 +1,9 @@
 package com.itgrids.partyanalyst.dao;
+import java.util.List;
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyGallery;
 
 public interface IPartyGalleryDAO extends GenericDao<PartyGallery,Long>{
 
+	public List<Object[]> getPartyGallaryDetail(Long partyId,int firstResult,int maxResult,String type) ;
 }
