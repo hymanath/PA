dec2e49656d84a58823bffb35ac3fefa2c3de127
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-11-29 12:36:04 +0000

    Updated by Sachin

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/service/CandidateDetailsServiceTest.java
@@ -0,0 +1,41 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.itgrids.partyanalyst.dao.ICandidateProfileDescriptionDAO;
+import com.itgrids.partyanalyst.service.impl.CandidateDetailsService;
+
+public class CandidateDetailsServiceTest {
+
+	private CandidateDetailsService candidateDetailsService;
+	private ICandidateProfileDescriptionDAO candidateProfileDescriptionDAO;
+	
+	@Before
+	public void init(){
+		candidateProfileDescriptionDAO = EasyMock.createMock(ICandidateProfileDescriptionDAO.class);
+		candidateDetailsService = new CandidateDetailsService();
+	}
+	
+	@Test
+	public void checkGetCandidateProfileDescriptionByCandidateID(){
+		List<Object> results = new ArrayList<Object>(0);
+		results.add("Ram");
+		results.add("Ramesh");
+		EasyMock.expect(candidateProfileDescriptionDAO.getCandidateProfileDescription(10L)).andReturn(results);
+		EasyMock.replay(candidateProfileDescriptionDAO);
+		candidateDetailsService.setCandidateProfileDescriptionDAO(candidateProfileDescriptionDAO);
+		
+		List<String> returnResults = candidateDetailsService.getCandidateProfileDescriptionByCandidateID(10L);
+		
+		Assert.assertEquals(results, returnResults);
+		EasyMock.verify(candidateProfileDescriptionDAO);
+		
+	}
+	
+}
