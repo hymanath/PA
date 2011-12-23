e5fbfd1391ea3ae8b5b662634351a594ca833032
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-23 08:12:32 +0000

    Pancahayat wise voting info implementation- kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HamletBoothElectionDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HamletBoothElectionDAOHibernateTest.java
@@ -31,10 +31,22 @@ public class HamletBoothElectionDAOHibernateTest extends BaseDaoTestCase{
 			System.out.println(values[0]+"\t"+values[1]+"\t"+values[2]+"\t"+values[3]+"\t"+values[4]+"\t"+values[5]);
 	}*/
 	
-	public void testFindPanchayathBoothIdsInTehsilForElection(){
+	/*public void testFindPanchayathBoothIdsInTehsilForElection(){
 		List list = hamletBoothElectionDAO.findPanchayathBoothIdsInTehsilForElection(844l, 2l);
 		for(Object[] values:(List<Object[]>)list)
 			System.out.println(values[0]+"\t"+values[1]);
+	}*/
+	
+	public void testgetPanchayatWiseBoothDetails()
+	{
+		List<Object[]> list = hamletBoothElectionDAO.getPanchayatWiseBoothDetails(844l,38l);
+		
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println("Constituency Id - "+params[0]+"  Name - "+params[1]+" Panchyat Id - "+params[2]+"  Panchayat name - "+params[3]+"hamlet Id - "+params[4]+"   Hamlet Name -  "+params[5]+"   Booth Id -  "+params[6]+"   Booth Name - "+params[7]+"  Total Voters - "+params[8]+"  Valid Votes - "+params[9]);
+		}
 	}
 	
 }
