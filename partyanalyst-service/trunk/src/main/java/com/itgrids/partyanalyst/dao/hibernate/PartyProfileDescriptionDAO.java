72378d6a2eb17f6cd853d0000a038336c23dc050
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-24 07:00:54 +0000

    Updated by anu & nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyProfileDescriptionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyProfileDescriptionDAO.java
@@ -2,6 +2,8 @@ package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.util.List;
 
+import org.hibernate.Query;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IPartyProfileDescriptionDAO;
@@ -13,19 +15,29 @@ import com.itgrids.partyanalyst.model.PartyProfileDescription;
   public PartyProfileDescriptionDAO() {
      super(PartyProfileDescription.class);
       }
+  
   @SuppressWarnings("unchecked")
 	public List<Object> getPartyProfileDescription(Long partyId){		
 		return getHibernateTemplate().find("select model.description from PartyProfileDescription model where model.party.partyId=? order by model.orderNo asc",partyId);
 	}
+  
   public List<Object> getMaxOrderNo(Long partyId){
 		
 		return getHibernateTemplate().find("select max(model.orderNo) from PartyProfileDescription model where model.party.partyId=?",partyId);
 	}
   
   @SuppressWarnings("unchecked")
-	public List<Object[]> getPartyProfileInfo(Long partyId){		
+  public List<Object[]> getPartyProfileInfo(Long partyId){		
 		return getHibernateTemplate().find("select model.orderNo , model.description , model.partyProfileDescriptionId from PartyProfileDescription model where model.party.partyId=?",partyId);
 	}
+  
+  public Integer deletePartyProfileDescriptionById(Long profDescId) {
+		Query queryObject=getSession().createQuery("DELETE FROM PartyProfileDescription model where model.partyProfileDescriptionId=?");
+		queryObject.setParameter(0, profDescId);
+		return queryObject.executeUpdate();
+	}
+	
+	
 }
 
 
