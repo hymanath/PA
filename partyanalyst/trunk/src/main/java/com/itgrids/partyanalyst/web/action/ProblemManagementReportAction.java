2dc2db97efc2fccc4b478fd7d28bd55db713514f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-24 07:01:06 +0000

    Updated by  Swathi

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ProblemManagementReportAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ProblemManagementReportAction.java
@@ -674,8 +674,8 @@ public class ProblemManagementReportAction extends ActionSupport implements
 		String fromDate = jObj.getString("fromDate");
 		String toDate =  jObj.getString("toDate");
 		Long statusId = jObj.getLong("status");
-		String accessType = jObj.getString("accessType");
-		Long accessValue = jObj.getLong("accessValue");
+		//String accessType = jObj.getString("accessType");
+		//Long accessValue = jObj.getLong("accessValue");
 		
 		problemBean = problemManagementReportService.getProblemsPostedByStatusAndDates(user.getRegistrationID(),fromDate, toDate, statusId, accessType, accessValue);
 		return SUCCESS;
