340c2fa48e917728e78080e20834548838e2c8ae
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-26 15:16:46 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyManifestoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyManifestoDAO.java
@@ -4,10 +4,20 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.model.File;
 import com.itgrids.partyanalyst.model.PartyManifesto;
 
 public interface IPartyManifestoDAO extends GenericDao<PartyManifesto, Long> {
 	public List<Object[]> getPartyManifestoInfo(Long partyId);
 	
 	public List<Object[]> getSelectedState(Long partyId);
+
+	public List<Object[]> getElectionTypes(Long partyId, Long stateId);
+	
+	public List<Object[]> getElectionYearsBasedOnElectionTypeId(Long electionTypeId,Long partyId,Long stateId);
+
+	public List<File> getPartyManifestoBasedOnElectionYear(Long electionId,
+			Long partyId, Long stateId);
+
+	public List<Object[]> getPartyManifestoInfo(Long partyId, String queryStr);
 }
