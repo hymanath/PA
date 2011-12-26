d2706d613140df8baf14ac0cc734a2fa96e64224
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-26 13:34:26 +0000

    updated by Sri Hari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageGalleryDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.SpecialPageGallery;
+
+public interface ISpecialPageGalleryDAO extends GenericDao<SpecialPageGallery, Long> {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageUpdatesEmailDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.SpecialPageUpdatesEmail;
+
+public interface ISpecialPageUpdatesEmailDAO extends GenericDao<SpecialPageUpdatesEmail, Long>{
+
+}
