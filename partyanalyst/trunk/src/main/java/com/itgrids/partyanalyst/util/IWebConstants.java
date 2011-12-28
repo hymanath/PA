9cf18fc4da00e6fc75ae613f8599c3fe2cd0044e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-28 16:00:46 +0000

    updated by kamalakar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/util/IWebConstants.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/util/IWebConstants.java
@@ -5,4 +5,11 @@ public interface IWebConstants {
 	public static final String USER = "USER";
 	public static final long FILE_MAX_LENGTH = 2097152;
 	public static final int FILE_RANDOM_NO = 1000000000;
+	
+	public static final String UPLOADED_FILES = "uploaded_files";
+	public static final String PARTY_PROFILE = "party_profile";
+	public static final String CANDIDATE_PROFILE = "candidate_profile";
+	public static final String SPECIAL_PAGE = "special_page";
+	public static final String NEWS_GALLERY = "news_gallery";
+	public static final String PHOTO_GALLERY = "photo_gallery";
 }
