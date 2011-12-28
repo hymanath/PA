04bf8aad7545da438d64c4bdd8c3fa821eb60a76
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2011-12-28 06:46:30 +0000

    Updated by kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISpecialPageService.java
@@ -0,0 +1,5 @@
+package com.itgrids.partyanalyst.service;
+
+public interface ISpecialPageService {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SpecialPageService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ISpecialPageService;
+
+public class SpecialPageService implements ISpecialPageService{
+
+}
