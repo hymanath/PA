88258581dcb0c38477fd52635771c8df61670b08
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:14:28 +0000

    Updated by anu & nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDescriptionDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDescriptionDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 import com.itgrids.partyanalyst.dao.ISpecialPageDescriptionDAO;
 
@@ -13,9 +15,29 @@ public class SpecialPageDescriptionDAOHibernateTest extends BaseDaoTestCase{
 	}
 
 
-	public void test()
+	/*public void test()
 	{
 		specialPageDescriptionDAO.getAll();
+	}*/
+	/*public void testGetEventDescription()
+	{
+		List<Object[]> list=specialPageDescriptionDAO.getEventDescription(1l);
+		for(Object[] params:list)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1].toString());
+			System.out.println(params[2]);
+		}
+	}*/
+	
+	/*public void testdeleteEventProfileDescriptionById(){
+		Integer eventId=specialPageDescriptionDAO.deleteEventProfileDescriptionById(1l);
+		System.out.println(eventId);
+	}*/
+	public void testgetMaxOrderNo()
+	{
+		Object orderNo=specialPageDescriptionDAO.getMaxOrderNo(1l);
+		System.out.println(orderNo);
 	}
 
 }
