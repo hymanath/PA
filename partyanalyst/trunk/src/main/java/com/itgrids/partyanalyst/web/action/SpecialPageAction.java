3f74941358e0b71eb1b38f5a6683537d7bf27c7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:23:16 +0000

    Updated by anu & nagamani

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SpecialPageAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SpecialPageAction.java
@@ -1,27 +1,40 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
 import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.util.ServletContextAware;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.service.ISpecialPageService;
+import com.itgrids.partyanalyst.service.impl.SpecialPageService;
+import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.itgrids.partyanalyst.dto.SpecialPageVO;
 
 public class SpecialPageAction extends ActionSupport implements
-ServletRequestAware, ServletResponseAware,ServletContextAware{
+		ServletRequestAware, ServletResponseAware, ServletContextAware {
 
 	private static final long serialVersionUID = -957791701984246754L;
 	private ServletContext context;
 	private HttpServletRequest request;
 	private HttpServletResponse response;
-	private List<String> descriptions;
+	private ResultStatus result;
+	private HttpSession session;
+	private List<SpecialPageVO> specialPageList;
+	private JSONObject jobj;
+	private String task = null;
 	private Long specialPageId;
-	private ISpecialPageService specialPageService;
+	private List<String> descriptions;
 	
 	public Long getSpecialPageId() {
 		return specialPageId;
@@ -30,8 +43,25 @@ ServletRequestAware, ServletResponseAware,ServletContextAware{
 	public void setSpecialPageId(Long specialPageId) {
 		this.specialPageId = specialPageId;
 	}
-
 	
+	public List<String> getDescriptions() {
+		return descriptions;
+	}
+
+	public void setDescriptions(List<String> descriptions) {
+		this.descriptions = descriptions;
+	}
+	
+	public List<SpecialPageVO> getSpecialPageList() {
+		return specialPageList;
+	}
+
+	public void setSpecialPageList(List<SpecialPageVO> specialPageList) {
+		this.specialPageList = specialPageList;
+	}
+
+	private ISpecialPageService specialPageService;
+
 	public ISpecialPageService getSpecialPageService() {
 		return specialPageService;
 	}
@@ -40,16 +70,33 @@ ServletRequestAware, ServletResponseAware,ServletContextAware{
 		this.specialPageService = specialPageService;
 	}
 
-	public List<String> getDescriptions() {
-		return descriptions;
+	public JSONObject getJobj() {
+		return jobj;
 	}
 
-	public void setDescriptions(List<String> descriptions) {
-		this.descriptions = descriptions;
+	public void setJobj(JSONObject jobj) {
+		this.jobj = jobj;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public ResultStatus getResult() {
+		return result;
+	}
+
+	public void setResult(ResultStatus result) {
+		this.result = result;
 	}
 
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
+		this.session = request.getSession();
 	}
 
 	public void setServletResponse(HttpServletResponse response) {
@@ -59,11 +106,27 @@ ServletRequestAware, ServletResponseAware,ServletContextAware{
 	public void setServletContext(ServletContext context) {
 		this.context = context;
 	}
+
+	public String execute() {
 	
-	public String execute()
-	{
 		descriptions = specialPageService.getSpecialPageDescription(specialPageId);
-		return SUCCESS;
+	return SUCCESS;
 	}
 
+	public String setEmailAlertsForEvent() {
+		try {
+			jobj = new JSONObject(getTask());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		/*session = request.getSession();
+		RegistrationVO registrationVO = (RegistrationVO) session
+				.getAttribute("USER");*/
+		String email = jobj.getString("emailId");
+
+		result = specialPageService.subScribeEmailAlertForAEvent(email, jobj
+				.getLong("specialPageId"));
+		return Action.SUCCESS;
+	}
+	
 }
