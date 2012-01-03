7254e63913f1cb54e7819e9fc980da5c1c892394
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:56:18 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageGalleryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageGalleryDAO.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.model.File;
 import com.itgrids.partyanalyst.model.SpecialPageGallery;
 
 public interface ISpecialPageGalleryDAO extends GenericDao<SpecialPageGallery, Long> {
+	
 	public List<Object[]> getGallariesBySpecialPageId(Long specialPageId, String contentType);
+	
+	public List<Object[]> getSpecialPageGallaryDetails(Long specialPageId,String contentType);
+
+	public List<Object[]> getSpecialPageGallaryDetail(Long specialPageId,
+			int firstRecord, int maxRecord, String type);
+	
+	public List<File> getGalleryBasedOnSpecialPageId(Long specialPageId,int firstRecord, int maxRecord, String contentType);
+	
+	
 
 }
