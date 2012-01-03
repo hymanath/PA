0ccde9be33b147f269fc04e6364356e7063398c6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 15:00:08 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDAOHibernateTest.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
+import org.junit.Test;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageDAO;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class SpecialPageDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -12,9 +16,23 @@ public class SpecialPageDAOHibernateTest extends BaseDaoTestCase{
 		this.specialPageDAO = specialPageDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		specialPageDAO.getAll();
+	}*/
+	
+	
+	public void testGetSpecialPageDetails(){
+		List<Object[]> result = specialPageDAO.getSpecialPageDetails(1l);
+		System.out.println(result.size());
+		for(Object[] resultObj : result){
+			
+			System.out.println("Title --------->" +resultObj[0].toString());
+			System.out.println("Heading --------->" +resultObj[1].toString());
+			System.out.println("Image Path --------->" +resultObj[2].toString());
+			System.out.println("Title --------->" +resultObj[3].toString());
+	
+		}
 	}
 
 }
