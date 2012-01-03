4ab70c7fe87c16db17b2da4a66e7b4f176c4afc0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:17:41 +0000

    Updated by anu & nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageGalleryDAO;
 import com.itgrids.partyanalyst.model.SpecialPageGallery;
@@ -9,6 +12,11 @@ public class SpecialPageGalleryDAO extends GenericDaoHibernate<SpecialPageGaller
 
 	public SpecialPageGalleryDAO(){
 		super(SpecialPageGallery.class);
+	}
+	
+	public List<Object[]> getGallariesBySpecialPageId(Long specialPageId, String contentType){
+		Object[] params = {specialPageId , contentType};
+		return getHibernateTemplate().find("select model.gallary.gallaryId , model.gallary.name from SpecialPageGallery model where model.specialPage.specialPageId=? and model.gallary.contentType.contentType=? and model.isDelect='false' order by model.gallary.name asc",params);
 	}
 	
 }
