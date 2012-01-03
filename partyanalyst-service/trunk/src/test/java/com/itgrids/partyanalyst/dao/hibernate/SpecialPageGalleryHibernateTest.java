792cdf26c77f18273a9806b4802176a7e3b0a0d8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 15:00:48 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryHibernateTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageGalleryDAO;
+import com.itgrids.partyanalyst.model.File;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class SpecialPageGalleryHibernateTest extends BaseDaoTestCase {
 	ISpecialPageGalleryDAO specialPageGalleryDAO;
@@ -13,8 +15,9 @@ public class SpecialPageGalleryHibernateTest extends BaseDaoTestCase {
 			ISpecialPageGalleryDAO specialPageGalleryDAO) {
 		this.specialPageGalleryDAO = specialPageGalleryDAO;
 	}
-
-	/*public void test() {
+	
+	/*public void test()
+	{
 		specialPageGalleryDAO.getAll();
 	}*/
 
@@ -26,5 +29,23 @@ public class SpecialPageGalleryHibernateTest extends BaseDaoTestCase {
 			System.out.println(params[1].toString());
 
 		}
+	}
+	
+	public void testGetSpecialPageGallaryDetails(){
+		
+		List<Object[]> list = specialPageGalleryDAO.getSpecialPageGallaryDetails(1l, IConstants.PHOTO_GALLARY);
+		System.out.println(list.size());
+		for(Object[] listObj : list){
+			System.out.println((Long)listObj[0]);
+			System.out.println(listObj[1].toString());
+			System.out.println(listObj[2].toString());
+			//System.out.println(listObj[3].toString());
+		}
+	}
+	
+	public void testgetGalleryBasedOnSpecialPageId(){
+		List<File> result = specialPageGalleryDAO.getGalleryBasedOnSpecialPageId(1l,1, 2, IConstants.VIDEO_GALLARY);
+		
+		System.out.println(result.size());
 	}
 }
