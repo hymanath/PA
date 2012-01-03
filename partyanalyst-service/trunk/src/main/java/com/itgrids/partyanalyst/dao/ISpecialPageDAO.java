0c52b602aacf8306deb694c8d21d3f22f05a6e0f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:55:58 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.SpecialPage;
 
 public interface ISpecialPageDAO extends GenericDao<SpecialPage,Long>{
-
+	
+	public List<Object[]> getSpecialPageDetails(Long specialPageId);
 }
