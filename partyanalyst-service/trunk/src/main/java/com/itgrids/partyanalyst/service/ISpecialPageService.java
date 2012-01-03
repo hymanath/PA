8f156334edcc0575f8caf1c7d28d62b5a7d68720
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:15:19 +0000

    Updated by anu & nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISpecialPageService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISpecialPageService.java
@@ -2,8 +2,30 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.FileVO;
+import com.itgrids.partyanalyst.dto.GallaryVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.dto.SpecialPageVO;
+
 public interface ISpecialPageService {
 	
 	public List<String> getSpecialPageDescription(Long specialPageId);
+	
+	public ResultStatus subScribeEmailAlertForAEvent(String emailId,Long specialPageId) ;
 
+	public List<SpecialPageVO> getEventProfileInfo(Long specialPageId);
+	public ResultStatus deleteEventProfileDescById(Long specialPageDescriptionId);
+	
+	public ResultStatus saveDescription(SpecialPageVO specialPageVO);
+	public ResultStatus updateEventProfileDescripton(List<SpecialPageVO> specialPageVO,Long specialPageId);
+	public ResultStatus createNewGallaryOrUpdateGallary(GallaryVO gallaryVO,String createOrUpdate);
+	public List<SelectOptionVO> getSource();
+	public List<SelectOptionVO> getLanguage();
+	public ResultStatus uploadAFile(FileVO fileVO);
+	public Long getLocationScopeValue(Long scope,String locationValue);
+	public List<SelectOptionVO> getEventGallarySelectList(Long specialPageId,
+			String contentType);
+	public List<SelectOptionVO> getPartyGallarySelectList(Long specialPageId,
+			String contentType);
 }
