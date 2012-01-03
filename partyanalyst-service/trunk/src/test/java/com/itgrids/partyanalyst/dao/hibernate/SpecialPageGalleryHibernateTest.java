e3813ad1a5ef8f001a9b623d093ed72247064114
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:14:07 +0000

    Modified for PartyGallery model - anu & nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryHibernateTest.java
@@ -1,20 +1,30 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageGalleryDAO;
 
 public class SpecialPageGalleryHibernateTest extends BaseDaoTestCase {
 	ISpecialPageGalleryDAO specialPageGalleryDAO;
-	
+
 	public void setSpecialPageGalleryDAO(
 			ISpecialPageGalleryDAO specialPageGalleryDAO) {
 		this.specialPageGalleryDAO = specialPageGalleryDAO;
 	}
-	
-	public void test()
-	{
-		specialPageGalleryDAO.getAll();
-	}
 
+	/*public void test() {
+		specialPageGalleryDAO.getAll();
+	}*/
+
+	public void testgetGallariesBySpecialPageId() {
+		List<Object[]> list = specialPageGalleryDAO
+				.getGallariesBySpecialPageId(1l, "videoGallary");
+		for (Object[] params : list) {
+			System.out.println(params[0]);
+			System.out.println(params[1].toString());
+
+		}
+	}
 }
