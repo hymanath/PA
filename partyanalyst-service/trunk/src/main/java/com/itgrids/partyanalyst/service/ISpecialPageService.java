c9c74d3da9b91a2527ec55a2c9685ca9ed05b87d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:58:58 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISpecialPageService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISpecialPageService.java
@@ -15,17 +15,39 @@ public interface ISpecialPageService {
 	public ResultStatus subScribeEmailAlertForAEvent(String emailId,Long specialPageId) ;
 
 	public List<SpecialPageVO> getEventProfileInfo(Long specialPageId);
+	
 	public ResultStatus deleteEventProfileDescById(Long specialPageDescriptionId);
 	
 	public ResultStatus saveDescription(SpecialPageVO specialPageVO);
+	
 	public ResultStatus updateEventProfileDescripton(List<SpecialPageVO> specialPageVO,Long specialPageId);
+	
 	public ResultStatus createNewGallaryOrUpdateGallary(GallaryVO gallaryVO,String createOrUpdate);
+	
 	public List<SelectOptionVO> getSource();
+	
 	public List<SelectOptionVO> getLanguage();
+	
 	public ResultStatus uploadAFile(FileVO fileVO);
+	
 	public Long getLocationScopeValue(Long scope,String locationValue);
+	
 	public List<SelectOptionVO> getEventGallarySelectList(Long specialPageId,
-			String contentType);
+				String contentType);
+				
 	public List<SelectOptionVO> getPartyGallarySelectList(Long specialPageId,
 			String contentType);
+			
+	public SpecialPageVO getSpecialPageBasicDetails(Long specialPageId);
+
+	public List<FileVO> getPhotoGalleryBasedOnSpecialPageId(Long specialPageId);
+
+	public List<FileVO> getNewsGalleryBasedOnSpecialPageId(Long specialPageId,int startingRecord,int maxRecord,String type);
+
+	public List<FileVO> getVideoGalleryBasedOnSpecialPageId(Long specialPageId ,int startingRecord,int maxRecord);
+
+	public List<FileVO> setGallaryObjectToFileVO(List<Object[]> records);
+
+	public List<FileVO> getSpecialPageGallaryDetailWithOutGallerySizeZero(
+			Long specialPageId, int firstRecord, int maxRecord, String type);
 }
