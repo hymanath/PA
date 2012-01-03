a0e383d832e0187fac5dcedf8e4e0c717c5b2561
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:17:19 +0000

    Updated by anu & nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDescriptionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDescriptionDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.util.List;
+
+import org.hibernate.Query;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageDescriptionDAO;
@@ -11,9 +14,24 @@ public class SpecialPageDescriptionDAO extends GenericDaoHibernate<SpecialPageDe
 	public SpecialPageDescriptionDAO()
 	{
 		super(SpecialPageDescription.class);
-	} 
+	}
+	
 	@SuppressWarnings("unchecked")
 	public List<Object> getSpecialPageDescription(Long specialPageId){		
 		return getHibernateTemplate().find("select model.description from SpecialPageDescription model where model.specialPageId=? order by model.orderNo asc",specialPageId);
+	}
+	
+	public List<Object[]> getEventDescription(Long specialPageId){
+		return getHibernateTemplate().find("select model.orderNo,model.description,model.specialPageDescriptionId from SpecialPageDescription model where model.specialPage.specialPageId=?",specialPageId);
+	}
+	public Integer deleteEventProfileDescriptionById(Long specialPageDescriptionId)
+	{
+		Query queryObject=getSession().createQuery("delete from SpecialPageDescription model where model.specialPageDescriptionId=?");
+		queryObject.setParameter(0,specialPageDescriptionId);
+		return queryObject.executeUpdate();
+	}
+	public List<Object> getMaxOrderNo(Long specialPageId)
+	{
+		return getHibernateTemplate().find("select max(model.orderNo) from SpecialPageDescription model where model.specialPage.specialPageId=?",specialPageId);
 	}
 }
