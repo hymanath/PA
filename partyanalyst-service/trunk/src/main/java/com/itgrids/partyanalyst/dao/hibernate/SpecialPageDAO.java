5826a718d8de4b35b96f134c3ee6b4eda7774254
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-03 14:56:50 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageDAO.java
@@ -1,15 +1,30 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageDAO;
 import com.itgrids.partyanalyst.model.SpecialPage;
 
-public class SpecialPageDAO extends GenericDaoHibernate<SpecialPage,Long> implements ISpecialPageDAO{
+public class SpecialPageDAO extends GenericDaoHibernate<SpecialPage,Long> implements ISpecialPageDAO {
 	
 	public SpecialPageDAO()
 	{
 		super(SpecialPage.class);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getSpecialPageDetails(Long specialPageId){
+		
+		Query query = getSession().createQuery("SELECT model.title,model.heading,model.profileImgPath ,model.specialPageId FROM SpecialPage model where model.specialPageId =:specialPageId and model.isDelete =:isDelete");
+			
+			query.setParameter("specialPageId", specialPageId);
+			query.setParameter("isDelete", "false");
+			
+		return query.list();
 	}
 
 }
