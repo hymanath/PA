d59cd5944edaebb593420dae34b5478fcf198006
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-04 14:32:53 +0000

    Updated by Sri Hari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMessageToPartyDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.MessageToParty;
+
+public interface IMessageToPartyDAO extends GenericDao<MessageToParty, Long>{
+
+}
