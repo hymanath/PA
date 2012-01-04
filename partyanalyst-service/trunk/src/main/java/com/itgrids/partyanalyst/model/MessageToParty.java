e34945361d42d2e8bfa218fddb2da5a5bc9a42f2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-04 14:36:33 +0000

    Updated by Sri Hari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/MessageToParty.java
@@ -0,0 +1,121 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name="message_to_party")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class MessageToParty extends BaseModel implements java.io.Serializable {
+	
+	private static final long serialVersionUID = 3030367834367449209L;
+
+	private Long messageToPartyId;
+	private String name;
+	private Constituency constituency;
+	private String message;
+	private Party party;
+	private Date sentTime;
+	private String isDelete;
+	
+	public MessageToParty()
+	{}
+	
+	public MessageToParty(String name,Constituency constituency,String message,Party party,Date sentTime,String isDelect)
+	{
+		this.isDelete = isDelect;
+		this.name = name;
+		this.constituency = constituency;
+		this.message=message;
+		this.party = party;
+		this.sentTime=sentTime;
+	}
+
+	@Id
+	@Column(name="message_to_party_id ",unique=true,nullable=false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getMessageToPartyId() {
+		return messageToPartyId;
+	}
+
+	public void setMessageToPartyId(Long messageToPartyId) {
+		this.messageToPartyId = messageToPartyId;
+	}
+	
+	@Column(name="name",length=100)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="party_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Party getParty() {
+		return party;
+	}
+
+	public void setParty(Party party) {
+		this.party = party;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="location")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Constituency getConstituency() {
+		return constituency;
+	}
+
+	public void setConstituency(Constituency constituency) {
+		this.constituency = constituency;
+	}
+
+	@Column(name="message",length=500)
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Column(name="sent_time")
+
+	public Date getSentTime() {
+		return sentTime;
+	}
+
+	public void setSentTime(Date sentTime) {
+		this.sentTime = sentTime;
+	}
+	@Column(name="is_delete",length=10)
+	public String getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(String isDelete) {
+		this.isDelete = isDelete;
+	}
+	
+}
+
