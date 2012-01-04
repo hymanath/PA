a3843d3460c8516437adaa3ad9ed328636184baf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-04 14:33:22 +0000

    Updated by Sri Hari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MessageToCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MessageToCandidateDAO.java
@@ -5,9 +5,7 @@ import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import com.itgrids.partyanalyst.dao.IMessageToCandidateDAO;
 import com.itgrids.partyanalyst.model.MessageToCandidate;
 
-public class MessageToCandidateDAO extends
-		GenericDaoHibernate<MessageToCandidate, Long> implements
-		IMessageToCandidateDAO {
+public class MessageToCandidateDAO extends GenericDaoHibernate<MessageToCandidate, Long> implements	IMessageToCandidateDAO {
 	public MessageToCandidateDAO() {
 		super(MessageToCandidate.class);
 	}
