44956e5165445f8c5ef8b4e1812ede6ace1a6655
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-05 07:27:37 +0000

    Updated by  Swathi

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SpecialPageAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SpecialPageAction.java
@@ -13,10 +13,12 @@ import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.util.ServletContextAware;
 import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.FileVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.service.ISpecialPageService;
 import com.itgrids.partyanalyst.service.impl.SpecialPageService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 import com.itgrids.partyanalyst.dto.SpecialPageVO;
@@ -31,11 +33,30 @@ public class SpecialPageAction extends ActionSupport implements
 	private ResultStatus result;
 	private HttpSession session;
 	private List<SpecialPageVO> specialPageList;
-	private JSONObject jobj;
+	private JSONObject jObj;
 	private String task = null;
 	private Long specialPageId;
 	private List<String> descriptions;
+	private SpecialPageVO specialPageVO;
+	private List<FileVO> fileVOList;
+	private ISpecialPageService specialPageService;
 	
+	public void setFileVOList(List<FileVO> fileVOList) {
+		this.fileVOList = fileVOList;
+	}
+
+	public List<FileVO> getFileVOList() {
+		return fileVOList;
+	}
+
+	public void setSpecialPageVO(SpecialPageVO specialPageVO) {
+		this.specialPageVO = specialPageVO;
+	}
+
+	public SpecialPageVO getSpecialPageVO() {
+		return specialPageVO;
+	}
+
 	public Long getSpecialPageId() {
 		return specialPageId;
 	}
@@ -60,22 +81,12 @@ public class SpecialPageAction extends ActionSupport implements
 		this.specialPageList = specialPageList;
 	}
 
-	private ISpecialPageService specialPageService;
-
 	public ISpecialPageService getSpecialPageService() {
 		return specialPageService;
 	}
 
 	public void setSpecialPageService(ISpecialPageService specialPageService) {
 		this.specialPageService = specialPageService;
-	}
-
-	public JSONObject getJobj() {
-		return jobj;
-	}
-
-	public void setJobj(JSONObject jobj) {
-		this.jobj = jobj;
 	}
 
 	public String getTask() {
@@ -109,24 +120,66 @@ public class SpecialPageAction extends ActionSupport implements
 
 	public String execute() {
 	
+		Long specialPageId = new Long(request.getParameter("specialPageId"));
 		descriptions = specialPageService.getSpecialPageDescription(specialPageId);
+		setSpecialPageVO(specialPageService.getSpecialPageBasicDetails(specialPageId));
+		setFileVOList(specialPageService.getVideoGalleryBasedOnSpecialPageId(specialPageId, 0, 20));
 	return SUCCESS;
 	}
 
 	public String setEmailAlertsForEvent() {
 		try {
-			jobj = new JSONObject(getTask());
+			jObj = new JSONObject(getTask());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		/*session = request.getSession();
 		RegistrationVO registrationVO = (RegistrationVO) session
 				.getAttribute("USER");*/
-		String email = jobj.getString("emailId");
+		String email = jObj.getString("emailId");
 
-		result = specialPageService.subScribeEmailAlertForAEvent(email, jobj
+		result = specialPageService.subScribeEmailAlertForAEvent(email, jObj
 				.getLong("specialPageId"));
 		return Action.SUCCESS;
 	}
 	
+	
+	public String ajaxCallHandlerForSpecialPage(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			
+			if(jObj.getString("task").equalsIgnoreCase("getNewsToDisplay"))
+			{
+				fileVOList = specialPageService.getNewsGalleryBasedOnSpecialPageId(jObj.getLong("specialPageId"),jObj.getInt("startRecord"),jObj.getInt("maxRecord"),jObj.getString("queryType"));
+			}
+			else if(jObj.getString("task").equalsIgnoreCase("getFirstThreePhotoGallaryDetail"))
+			{
+				fileVOList = specialPageService.getPhotoGalleryBasedOnSpecialPageId(jObj.getLong("specialPageId"));
+			}
+			else if(jObj.getString("task").equalsIgnoreCase("getPhotoGallaryWithOutGallerySizeZero"))
+			{
+				fileVOList = specialPageService.getSpecialPageGallaryDetailWithOutGallerySizeZero(jObj.getLong("specialPageId"),jObj.getInt("startRecord"),jObj.getInt("maxRecord"),IConstants.PHOTO_GALLARY);
+			}
+			else if(jObj.getString("task").equalsIgnoreCase("videoGalleriesForASpecialPage")){
+				
+				fileVOList = specialPageService.getVideoGalleryBasedOnSpecialPageId(jObj.getLong("specialPageId"),jObj.getInt("startRecord"),jObj.getInt("maxRecord"));
+			}
+	        else if(jObj.getString("task").equalsIgnoreCase("getPartyPhotoGallaryDetail")){
+				
+	        	//fileVOList = specialPageService.getPartyPhotoGallaryDetail(jObj.getLong("partyId"),jObj.getInt("startRecord"),jObj.getInt("maxRecord"),IConstants.PHOTO_GALLARY);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return Action.SUCCESS;
+		
+	}
+
+	public String getLatestVideosForSpecialPage(){
+		
+		fileVOList = specialPageService.getVideoGalleryBasedOnSpecialPageId(jObj.getLong("specialPageId"),jObj.getInt("startIndex"),jObj.getInt("maxRecords"));
+		
+		return Action.SUCCESS;
+	}
 }
