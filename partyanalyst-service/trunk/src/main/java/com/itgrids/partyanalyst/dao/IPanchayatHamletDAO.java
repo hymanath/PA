de7728386c223ae28e3bd236446143566a54b5c1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-10 12:01:07 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPanchayatHamletDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPanchayatHamletDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PanchayatHamlet;
 
 public interface IPanchayatHamletDAO extends GenericDao<PanchayatHamlet,Long>{
-
+	
+	public List<Object[]> getHamletsOfAPanchayat(Long panchayatId);
 }
