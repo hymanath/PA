2e46e404b567984c589e2fd549bf0fc660459730
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-10 12:02:24 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PanchayatHamletDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PanchayatHamletDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IPanchayatHamletDAO;
@@ -10,5 +12,11 @@ public class PanchayatHamletDAO extends GenericDaoHibernate<PanchayatHamlet,Long
 	public PanchayatHamletDAO()
 	{
 		super(PanchayatHamlet.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getHamletsOfAPanchayat(Long panchayatId)
+	{
+		return getHibernateTemplate().find("select model.hamlet.hamletId,model.hamlet.hamletName from PanchayatHamlet model where model.panchayat.panchayatId = ? order by model.hamlet.hamletName",panchayatId);
 	}
 }
