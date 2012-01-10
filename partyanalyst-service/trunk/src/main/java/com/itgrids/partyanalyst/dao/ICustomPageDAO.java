5fcbf2e2c3d0713e50f8d6a32d3c62bb00de5009
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-10 12:00:32 +0000

    Created by kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICustomPageDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.CustomPage;
+
+public interface ICustomPageDAO extends GenericDao<CustomPage, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICustomPageTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CustomPageType;
+
+public interface ICustomPageTypeDAO extends GenericDao<CustomPageType,Long>{
+
+}
