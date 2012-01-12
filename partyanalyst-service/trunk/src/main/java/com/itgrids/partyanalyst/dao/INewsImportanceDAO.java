530742b08ab3248f54f64a4de9bdbcb9f6511b35
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-12 07:44:00 +0000

    Created by Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INewsImportanceDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.NewsImportance;
+
+public interface INewsImportanceDAO extends GenericDao<NewsImportance, Long>{
+
+}
