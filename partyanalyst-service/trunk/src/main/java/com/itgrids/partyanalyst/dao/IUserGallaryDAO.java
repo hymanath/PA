ebb7be1704e1f1f78375d91f11982000e9625718
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-13 15:19:20 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserGallaryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserGallaryDAO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.UserGallary;
 
 public interface IUserGallaryDAO extends GenericDao<UserGallary,Long>{
-
+	public List<Object[]> getAllNewsGallaryBasedOnUserId(Long userId);
 }
