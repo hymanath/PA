62dd38f121bb47c9a87250f53b7c681b24cf9fee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:25:49 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INewsImportanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INewsImportanceDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.NewsImportance;
 
 public interface INewsImportanceDAO extends GenericDao<NewsImportance, Long>{
-
+	public List<Object[]> getNewsImportanceDetails();
 }
