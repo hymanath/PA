9a68fea0749c487760218560d3c752313655e7d1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:28:00 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsMonitoringService.java
@@ -8,4 +8,8 @@ import com.itgrids.partyanalyst.dto.FileVO;
 public interface INewsMonitoringService {
 	public List<FileVO> getNewsForRegisterUsers(FileVO inputs);
 	public List<FileVO> getAllCountDetails(Date fromDate,Date toDate,String fileType,Long regId);
+	public List<FileVO> getAllSourceDetails();
+	public List<FileVO> getAllCategoryDetails();
+	public List<FileVO> getAllSourceLanguageDetails();
+	public List<FileVO> getAllNewsImportanceDetails();
 }
