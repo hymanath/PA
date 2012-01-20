7dd22c966363ede9323999c93960b4f19abe2259
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:26:23 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceLanguageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceLanguageDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.SourceLanguage;
 
 public interface ISourceLanguageDAO extends GenericDao<SourceLanguage, Long> {
-
+	public List<Object[]> getSourceLanguageDetails();
 	
 }
