1a5ee7a431aaea976f72b68c43e79f3847891d13
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:26:43 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CategoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CategoryDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.ICategoryDAO;
@@ -10,6 +12,8 @@ public class CategoryDAO extends GenericDaoHibernate<Category, Long> implements 
 	public CategoryDAO() {
 		super(Category.class);
 	}
-	
+	public List<Object[]> getCategoryDetails(){
+		   return getHibernateTemplate().find("select model.categoryId,model.categoryType from Category  model order by model.categoryType ");
+	   }
 
 }
