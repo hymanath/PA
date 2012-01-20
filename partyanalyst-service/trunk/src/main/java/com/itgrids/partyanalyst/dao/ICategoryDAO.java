18ff19fc9b3d5f2e4fac935c82173419c6ffcaec
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:25:28 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICategoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICategoryDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Category;
 
 public interface ICategoryDAO extends GenericDao<Category, Long>{
-
+	public List<Object[]> getCategoryDetails();
 }
