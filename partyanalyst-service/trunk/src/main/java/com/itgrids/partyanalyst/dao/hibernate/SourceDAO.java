e0bdb2fa609a57d5cffe847469ff4421526e68fe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:27:18 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SourceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SourceDAO.java
@@ -1,15 +1,18 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.ISourceDAO;
 import com.itgrids.partyanalyst.model.Source;
-import com.itgrids.partyanalyst.model.SourceLanguage;
 
 public class SourceDAO extends GenericDaoHibernate<Source, Long> implements
 		ISourceDAO {
 	public SourceDAO() {
 		super(Source.class);
 	}
-
+   public List<Object[]> getSourceDetails(){
+	   return getHibernateTemplate().find("select model.sourceId,model.source from Source  model order by model.source ");
+   }
 }
