74aeb809a93d918cdff52ff5660ff10a9e8e8f31
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-20 14:26:07 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Source;
 
 public interface ISourceDAO extends GenericDao<Source, Long>{
-
+	public List<Object[]> getSourceDetails();
 }
