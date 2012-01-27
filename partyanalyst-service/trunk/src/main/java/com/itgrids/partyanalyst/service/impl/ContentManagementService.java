b3addeb9537da5373a970343ab922b23c774abd1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-01-27 13:15:50 +0000

    Created by Kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ContentManagementService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.IContentManagementService;
+
+public class ContentManagementService implements IContentManagementService{
+
+}
