1ebb5a64235410b20d3a1b592e4150f14e5cea6e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-02 13:14:47 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsMonitoringService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.FileVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface INewsMonitoringService {
 	public List<FileVO> getNewsForRegisterUsers(FileVO inputs);
@@ -16,5 +17,5 @@ public interface INewsMonitoringService {
 	public List<FileVO> getSourceCountDetailsForGraph(Date fromDate,Date toDate,String fileType,Long regId,FileVO fileInputVO);
 	public List<FileVO> getLanguageCountDetailsForGraph(Date fromDate,Date toDate,String fileType,Long regId,FileVO fileInputVO);
 	public List<FileVO> getNewsImpCountDetailsForGraph(Date fromDate,Date toDate,String fileType,Long regId,FileVO fileInputVO);
-	 	 
+	public ResultStatus updateDeleteNews(FileVO fileVO,String task); 	 
 }
