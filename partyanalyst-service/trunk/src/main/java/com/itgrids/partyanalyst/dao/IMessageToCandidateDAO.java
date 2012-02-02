2067f7fac63f6e9eba5c1aef8ff9b2f0e91d8714
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-02 13:12:23 +0000

    Updated By SriHari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMessageToCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMessageToCandidateDAO.java
@@ -1,9 +1,17 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.CandidateCommentsVO;
 import com.itgrids.partyanalyst.model.MessageToCandidate;
 
 public interface IMessageToCandidateDAO extends GenericDao<MessageToCandidate, Long>{
+	
 
+	 public List getAllOpenedMessages(Date firstDate, Date secondDate);
+	 public Integer controlMessages(Long id,String message, String isApproved);
+	 public List<Object[]> getUserMessages(Long candidateId);
 }
