8a901301bdf8c121d97667fa451861ddad88f9db
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-02 13:19:35 +0000

    Updated By SriHari

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/MessageToCandidateDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/MessageToCandidateDAOHibernateTest.java
@@ -1,8 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IMessageToCandidateDAO;
+import com.itgrids.partyanalyst.dto.CandidateCommentsVO;
+
 
 public class MessageToCandidateDAOHibernateTest extends BaseDaoTestCase {
 	private IMessageToCandidateDAO messageToCandidateDAO;
@@ -12,9 +17,36 @@ public class MessageToCandidateDAOHibernateTest extends BaseDaoTestCase {
 		this.messageToCandidateDAO = messageToCandidateDAO;
 	}
 	
-	public void test()
+    public void test()
 	{
 		messageToCandidateDAO.getAll();
 		
 	}
+   /*public void testGetAllOpenedComments()
+	{
+		System.out.println();
+		List list = messageToCandidateDAO.getAllOpenedMessages(DateService.convertStringToDate("2010-12-13", "yyyy-MM-dd"),
+				DateService.convertStringToDate("2010-12-13", "yyyy-MM-dd"));
+		System.out.println(list.size());
+	}*/
+  
+		
+	
+	/*public void testControlMessages()
+	{
+		Integer result = messageToCandidateDAO.controlMessages(13L, "gk", "true");
+		System.out.println(result);
+		
+	}*/
+	/*public void testgetUserMessages()
+	{
+       List<Object[]> list = messageToCandidateDAO.getUserMessages(13889l);
+		
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println(params.toString());
+		}
+	}*/
 }
