5349d7f9d87ddf8863a58764337a9cdf797118c2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-06 18:30:01 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NominationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NominationDAO.java
@@ -3137,7 +3137,7 @@ public class NominationDAO extends GenericDaoHibernate<Nomination, Long> impleme
 		
 	public List<Object[]> getTopVotesGainedCandidates(Long electionId,int maxResult){
 		StringBuilder query = new StringBuilder();
-		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName,model.candidateResult.votesEarned,model.constituencyElection.constituency.constituencyId," +
+		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName,model.constituencyElection.constituencyElectionResult.validVotes,model.candidateResult.votesEarned,model.constituencyElection.constituency.constituencyId," +
 		" model.constituencyElection.constituency.name ,model.candidateResult.votesPercengate from Nomination model where model.constituencyElection.election.electionId = :electionId ");
 		
 		query.append("  order by model.candidateResult.votesEarned desc ");	
@@ -3152,8 +3152,8 @@ public class NominationDAO extends GenericDaoHibernate<Nomination, Long> impleme
 	public List<Object[]> getHighestLowestMarginVotesInanElection(Long electionId,int maxResult,String type)
 	{
 		StringBuilder query = new StringBuilder();
-		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName, (model.candidateResult.votesEarned - model2.candidateResult.votesEarned)," +
-				" model.constituencyElection.constituency.constituencyId,model.constituencyElection.constituency.name from Nomination model,Nomination model2 " +
+		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName,model.constituencyElection.constituencyElectionResult.validVotes,model.candidateResult.votesEarned," +
+				" model.constituencyElection.constituency.constituencyId,model.constituencyElection.constituency.name ,(model.candidateResult.votesEarned - model2.candidateResult.votesEarned) from Nomination model,Nomination model2 " +
 				" where model.constituencyElection.constiElecId = model2.constituencyElection.constiElecId and model.constituencyElection.election.electionId = ? and model.candidateResult.rank = 1 and model2.candidateResult.rank = 2 ");
 		
 		if(type.equalsIgnoreCase("HighestMarginGained"))
@@ -3169,8 +3169,8 @@ public class NominationDAO extends GenericDaoHibernate<Nomination, Long> impleme
 	}
 	public List<Object[]> getTopVotesGainedPercentage(Long electionId,int maxResult){
 		StringBuilder query = new StringBuilder();
-		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName, (model.candidateResult.votesEarned / model.constituencyElection.constituencyElectionResult.validVotes *100)," +
-				" model.constituencyElection.constituency.constituencyId,model.constituencyElection.constituency.name from Nomination model where model.constituencyElection.election.electionId = ? ");
+		query.append("select model.candidate.candidateId,model.candidate.lastname,model.party.partyId,model.party.shortName,model.constituencyElection.constituencyElectionResult.validVotes,model.candidateResult.votesEarned," +
+				" model.constituencyElection.constituency.constituencyId,model.constituencyElection.constituency.name ,(model.candidateResult.votesEarned / model.constituencyElection.constituencyElectionResult.validVotes *100) from Nomination model where model.constituencyElection.election.electionId = ? ");
 				
             query.append("  order by (model.candidateResult.votesEarned / model.constituencyElection.constituencyElectionResult.validVotes *100) desc ");	
 				
