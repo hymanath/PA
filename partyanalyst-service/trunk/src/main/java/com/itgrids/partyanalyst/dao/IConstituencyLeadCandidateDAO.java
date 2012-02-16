27135a644b000892123ff0315133c96c3ce1ebc6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-16 07:33:29 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyLeadCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyLeadCandidateDAO.java
@@ -1,9 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ConstituencyLeadCandidate;
 
 public interface IConstituencyLeadCandidateDAO  extends GenericDao<ConstituencyLeadCandidate, Long>{
 	
+	public List<Object> getCandidateResultStatus(Long candidateId,Long constiElecId);
+	
+	public List<Object> checkCandidateResultExist(Long constiElecId);
 }
