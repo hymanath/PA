10d22d3ff92878f4dfa15437bf1a1bda5474feb1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-16 07:33:46 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyLeadCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyLeadCandidateDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IConstituencyLeadCandidateDAO;
 import com.itgrids.partyanalyst.model.ConstituencyLeadCandidate;
@@ -8,5 +11,21 @@ import com.itgrids.partyanalyst.model.ConstituencyLeadCandidate;
 public class ConstituencyLeadCandidateDAO  extends GenericDaoHibernate<ConstituencyLeadCandidate, Long> implements IConstituencyLeadCandidateDAO{
 	public ConstituencyLeadCandidateDAO() {
 		super(ConstituencyLeadCandidate.class);
+	}
+	public List<Object> getCandidateResultStatus(Long candidateId,Long constiElecId)
+	{
+		Query query = getSession().createQuery("Select model.status from ConstituencyLeadCandidate model where model.constituencyElection.constiElecId =:constiElecId " +
+				" and model.candidate.candidateId =:candidateId ");
+		query.setLong("constiElecId", constiElecId);
+		query.setLong("candidateId", candidateId);
+        
+       return query.list();
+	}
+	public List<Object> checkCandidateResultExist(Long constiElecId)
+	{
+		Query query = getSession().createQuery("Select model.constituencyLeadCandidateId from ConstituencyLeadCandidate model where model.constituencyElection.constiElecId =:constiElecId ");
+		query.setLong("constiElecId", constiElecId);
+        
+       return query.list();
 	}
 }
