f3307c2e626eeb0ddf0159b2a551cfb2ebdc67f9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-18 06:07:10 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyLeadCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyLeadCandidateDAO.java
@@ -8,7 +8,20 @@ import com.itgrids.partyanalyst.model.ConstituencyLeadCandidate;
 
 public interface IConstituencyLeadCandidateDAO  extends GenericDao<ConstituencyLeadCandidate, Long>{
 	
+	@SuppressWarnings("unchecked")
+	public List getCountOfOldConstituenciesInAElection(Long electionId);
+	
+	@SuppressWarnings("unchecked")
+	public List getCountOfDelimitedConstituenciesInAElection(Long electionId);
+	
+	public List getLeadingConstituenciesCount(Long electionId);
+
+	public List<Object[]> getPartyLeadingOrWinningConstituencies(Long electionId);
+	
+	
 	public List<Object> getCandidateResultStatus(Long candidateId,Long constiElecId);
 	
 	public List<Object> checkCandidateResultExist(Long constiElecId);
+	
+	public List getElectionIds(Long electionId);
 }
