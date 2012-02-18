1314b05445a4e1c82e3db952ecf1d8eea70f3ae2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-18 06:08:25 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyLeadCandidateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyLeadCandidateDAO.java
@@ -12,6 +12,54 @@ public class ConstituencyLeadCandidateDAO  extends GenericDaoHibernate<Constitue
 	public ConstituencyLeadCandidateDAO() {
 		super(ConstituencyLeadCandidate.class);
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List getCountOfOldConstituenciesInAElection(Long electionId){
+		
+		return getHibernateTemplate().find("select count(model.constituencyElection.constituency.constituencyId) from ConstituencyLeadCandidate model " +
+				" where model.constituencyElection.constiElecId in (select model1.constiElecId from ConstituencyElection model1" +
+				" where model1.election.electionId =? and model1.constituency.startDate is null and " +
+				" model1.constituency.deformDate is null)",electionId );
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List getCountOfDelimitedConstituenciesInAElection(Long electionId){
+		
+		return getHibernateTemplate().find("select count(model.constituencyElection.constituency.constituencyId) from ConstituencyLeadCandidate model " +
+				" where model.constituencyElection.constiElecId in (select model1.constiElecId from ConstituencyElection model1" +
+				" where model1.election.electionId =? and model1.constituency.startDate is not null and " +
+				" model1.constituency.deformDate is null)",electionId );
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List getLeadingConstituenciesCount(Long electionId){
+		
+		 return getHibernateTemplate().find("select count(model.constituencyElection.constituency.constituencyId) from ConstituencyLeadCandidate model where " +
+		 		" model.constituencyElection.constiElecId in (select model1.constiElecId from ConstituencyElection model1 " +
+		 		" where model1.election.electionId = ?)",electionId);
+	}
+
+/*	@SuppressWarnings("unchecked")
+	public List<Object[]> getPartyLeadingOrWinningConstituencies(Long electionId) {
+		Object[] params = {electionId};
+		return getHibernateTemplate().find("select " +
+				" count(model.constituencyElection.constituency.constituencyId)," +
+				" model.party.shortName ,model1.status from" +
+				" Nomination model ,ConstituencyLeadCandidate model1 where" +
+				" model.constituencyElection.constiElecId = model1.constituencyElection.constiElecId and " +
+				" model.constituencyElection.constiElecId in " +
+				"(select model1.constituencyElection.constiElecId from ConstituencyLeadCandidate model1 where " +
+				" model1.constituencyElection.election.electionId = ? ) group by model.party.shortName,model1.status " ,params);
+	}*/
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getPartyLeadingOrWinningConstituencies(Long electionId)
+	{
+		return getHibernateTemplate().find("select model.party.shortName,model2.constituencyElection.constituency.startDate,model2.status,count(model2.status) from " +
+				" Nomination model, ConstituencyLeadCandidate model2 where model.constituencyElection.constiElecId = model2.constituencyElection.constiElecId and " +
+				" model2.constituencyElection.election.electionId = ? and model.candidate.candidateId = model2.candidate.candidateId group by model.party.partyId,model2.constituencyElection.constituency.startDate,model2.status",electionId);
+	}
 	public List<Object> getCandidateResultStatus(Long candidateId,Long constiElecId)
 	{
 		Query query = getSession().createQuery("Select model.status from ConstituencyLeadCandidate model where model.constituencyElection.constiElecId =:constiElecId " +
@@ -27,5 +75,10 @@ public class ConstituencyLeadCandidateDAO  extends GenericDaoHibernate<Constitue
 		query.setLong("constiElecId", constiElecId);
         
        return query.list();
+	}
+	
+	public List getElectionIds(Long electionId){
+		return getHibernateTemplate().find("Select model.election.electionId from ConstituencyElection model " +
+				"where model.constituency.deformDate ='2009-01-03' and model.election.electionId =?",electionId);
 	}
 }
