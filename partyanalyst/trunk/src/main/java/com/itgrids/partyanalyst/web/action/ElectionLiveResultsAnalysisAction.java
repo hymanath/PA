1a8efcaeead34d9d77d4877a3463cf19cd85ff10
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-18 06:12:28 +0000

    For Election Live Result Page --  Swathi

---
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ElectionLiveResultsAnalysisAction.java
@@ -0,0 +1,138 @@
+package com.itgrids.partyanalyst.web.action;
+
+import java.text.ParseException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.json.JSONObject;
+
+import com.itgrids.partyanalyst.dto.ElectionLiveResultVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IElectionLiveResultsAnalysisService;
+import com.itgrids.partyanalyst.service.IStaticDataService;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ElectionLiveResultsAnalysisAction extends ActionSupport implements ServletRequestAware,ServletResponseAware{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3569298270801964639L;
+	
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private IElectionLiveResultsAnalysisService electionLiveResultsAnalysisService;
+	private IStaticDataService staticDataService;
+	private List<SelectOptionVO> statesList;
+	private ElectionLiveResultVO electionLiveResultVO;
+	private List<ElectionLiveResultVO> electionLiveResultVOList;
+	private JSONObject jObj;
+	private String task;
+	
+	public JSONObject getJObj() {
+		return jObj;
+	}
+
+	public void setJObj(JSONObject obj) {
+		jObj = obj;
+	}
+
+	public IStaticDataService getStaticDataService() {
+		return staticDataService;
+	}
+
+	public List<SelectOptionVO> getStatesList() {
+		return statesList;
+	}
+
+	public void setStaticDataService(IStaticDataService staticDataService) {
+		this.staticDataService = staticDataService;
+	}
+
+	public void setStatesList(List<SelectOptionVO> statesList) {
+		this.statesList = statesList;
+	}
+
+	public IElectionLiveResultsAnalysisService getElectionLiveResultsAnalysisService() {
+		return electionLiveResultsAnalysisService;
+	}
+
+	public void setElectionLiveResultsAnalysisService(
+			IElectionLiveResultsAnalysisService electionLiveResultsAnalysisService) {
+		this.electionLiveResultsAnalysisService = electionLiveResultsAnalysisService;
+	}
+
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public void setServletResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+	 public void setElectionLiveResultVO(ElectionLiveResultVO electionLiveResultVO) {
+		this.electionLiveResultVO = electionLiveResultVO;
+	}
+
+	public ElectionLiveResultVO getElectionLiveResultVO() {
+		return electionLiveResultVO;
+	}
+
+	public void setElectionLiveResultVOList(List<ElectionLiveResultVO> electionLiveResultVOList) {
+		this.electionLiveResultVOList = electionLiveResultVOList;
+	}
+
+	public List<ElectionLiveResultVO> getElectionLiveResultVOList() {
+		return electionLiveResultVOList;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public String execute(){
+		 
+		 return SUCCESS;
+	 }
+	 
+	public String ajaxCallHandlerElectionLiveResults(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			if(jObj.getString("task").equalsIgnoreCase("getConstituenciesCount")){
+				Long electionId = new Long (jObj.getString("electionId"));
+				electionLiveResultVO = electionLiveResultsAnalysisService.getCountOfConstituenciesForAElection(electionId);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return Action.SUCCESS;
+	}
+	
+	public String getPartyWinningOrLeadingConstituenciesCount(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			if(jObj.getString("task").equalsIgnoreCase("getPartyWonOrLeadConstituenciesCount")){
+				
+				Long electionId = new Long(jObj.getString("electionId"));
+				electionLiveResultVOList = electionLiveResultsAnalysisService.getLeadingOrWinningContituenciesForAParty(electionId);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return Action.SUCCESS;
+	}
+
+}
