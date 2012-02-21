469936ea78707fda8af568d8dc50039a3e4c526c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-21 07:12:24 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionResultsUpdationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IElectionResultsUpdationService.java
@@ -3,6 +3,8 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.PartyElectionResultsVO;
+import com.itgrids.partyanalyst.dto.PositionManagementVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface IElectionResultsUpdationService {
@@ -12,5 +14,19 @@ public interface IElectionResultsUpdationService {
 	public List<PartyElectionResultsVO> updateCandidateResults(Long candidateId,Long constiElecId,String status);
 	
 	public List<SelectOptionVO> getElectionYears(Long stateId, String electionType);
+	
+	public ResultStatus addNewPosition(String position);
+	
+	public List<SelectOptionVO> getAllPosition();
+	
+	public ResultStatus assignScopeToPosition(Long electionGoverningBodyPositionId,Long electionTypeId,Long stateId,String type);
+	 
+    public List<SelectOptionVO> getAllElectionTypes();
+	 
+	public List<SelectOptionVO> getAllElectionYears(Long stateId, String electionType);
+	 
+	public ResultStatus assignCandidateToAPosition(PositionManagementVO positionManagementVO);
+	
+	 public List<SelectOptionVO> getCandidates(PositionManagementVO positionManagementVO);
 	
 }
