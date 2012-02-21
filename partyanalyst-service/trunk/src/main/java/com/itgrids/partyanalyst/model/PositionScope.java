54640c2570d56f82ec2d06d156a26224460f16d5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-21 07:10:22 +0000

    Created by Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PositionScope.java
@@ -0,0 +1,90 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+
+@Entity
+@Table(name= "position_scope")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PositionScope extends BaseModel implements Serializable {
+
+	private Long positionScopeId;
+	private ElectionScope electionScope;
+	private ElectionGoverningBodyPosition electionGoverningBodyPosition;
+	private String status;
+	private Set<ElectionGoverningBody> electionGoverningBody = new HashSet<ElectionGoverningBody>(0);
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="position_scope_id", unique = true, nullable = false)
+	public Long getPositionScopeId() {
+		return positionScopeId;
+	}
+	public void setPositionScopeId(Long positionScopeId) {
+		this.positionScopeId = positionScopeId;
+	}
+	
+	@ManyToOne(cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "election_scope_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public ElectionScope getElectionScope() {
+		return electionScope;
+	}
+	public void setElectionScope(ElectionScope electionScope) {
+		this.electionScope = electionScope;
+	}
+	
+	
+	@ManyToOne(cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "election_governing_body_position_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public ElectionGoverningBodyPosition getElectionGoverningBodyPosition() {
+		return electionGoverningBodyPosition;
+	}
+	public void setElectionGoverningBodyPosition(
+			ElectionGoverningBodyPosition electionGoverningBodyPosition) {
+		this.electionGoverningBodyPosition = electionGoverningBodyPosition;
+	}
+	
+	@Column(name = "status", length = 10)
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "electionGoverningBodyPosition")
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Set<ElectionGoverningBody> getElectionGoverningBody() {
+		return electionGoverningBody;
+	}
+	public void setElectionGoverningBody(
+			Set<ElectionGoverningBody> electionGoverningBody) {
+		this.electionGoverningBody = electionGoverningBody;
+	}
+	
+	
+}
