6afd893863b0ecf2d72af5584c5a1415ef066834
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-21 07:20:06 +0000

    Updated by Mahesh

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AssignCandidateToElectionAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/AssignCandidateToElectionAction.java
@@ -1,19 +1,27 @@
 package com.itgrids.partyanalyst.web.action;
 
 import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.PositionManagementVO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.helper.EntitlementsHelper;
 import com.itgrids.partyanalyst.service.ICandidateDetailsService;
+import com.itgrids.partyanalyst.service.IElectionResultsUpdationService;
 import com.itgrids.partyanalyst.service.IStaticDataService;
-import com.itgrids.partyanalyst.service.impl.StaticDataService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -32,6 +40,9 @@ public class AssignCandidateToElectionAction extends ActionSupport implements Se
 	private List<SelectOptionVO> optionVOList;
 	private List<SelectOptionVO> statesList;
 	private ICandidateDetailsService candidateDetailsService;
+	private IElectionResultsUpdationService electionResultsUpdationService;
+	private EntitlementsHelper entitlementsHelper;
+	private ResultStatus resultStatus;
 	
 	public ICandidateDetailsService getCandidateDetailsService() {
 		return candidateDetailsService;
@@ -65,8 +76,17 @@ public class AssignCandidateToElectionAction extends ActionSupport implements Se
 	public void setServletResponse(HttpServletResponse response) {
 		this.response = response;
 		
-	}
+	}	
 	
+	public IElectionResultsUpdationService getElectionResultsUpdationService() {
+		return electionResultsUpdationService;
+	}
+
+	public void setElectionResultsUpdationService(
+			IElectionResultsUpdationService electionResultsUpdationService) {
+		this.electionResultsUpdationService = electionResultsUpdationService;
+	}
+
 	public void setTask(String task) {
 		this.task = task;
 	}
@@ -90,9 +110,33 @@ public class AssignCandidateToElectionAction extends ActionSupport implements Se
 	public List<SelectOptionVO> getOptionVOList() {
 		return optionVOList;
 	}
+	
+
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+
+	public EntitlementsHelper getEntitlementsHelper() {
+		return entitlementsHelper;
+	}
+
+	public void setEntitlementsHelper(EntitlementsHelper entitlementsHelper) {
+		this.entitlementsHelper = entitlementsHelper;
+	}
 
 	public String execute(){
-		
+
+		HttpSession session = request.getSession();
+	
+	if(session.getAttribute(IConstants.USER) == null && 
+			!entitlementsHelper.checkForEntitlementToViewReport(null, IConstants.ASSIGN_A_ELECTION_GOVERNING_BODY))
+		return IConstants.NOT_LOGGED_IN;
+	if(!entitlementsHelper.checkForEntitlementToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.ASSIGN_A_ELECTION_GOVERNING_BODY))
+		return ERROR;
 		statesList = staticDataService.getParticipatedStatesForAnElectionType(2l);
 		
 		return SUCCESS;
@@ -128,10 +172,85 @@ public class AssignCandidateToElectionAction extends ActionSupport implements Se
 				Long partyId = new Long(jObj.getString("partyId"));
 				Long electionId = new Long(jObj.getString("electionId"));
 				optionVOList = candidateDetailsService.getCandidatesBasedOnSelection(candidateName,partyId,electionId);
+			}			
+			else if(jObj.getString("task").trim().equalsIgnoreCase("getAllPosition"))
+			{
+				optionVOList = electionResultsUpdationService.getAllPosition();
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("getAllElectionTypes"))
+			{
+				optionVOList = electionResultsUpdationService.getAllElectionTypes();
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("getAllElectionYears"))
+			{
+				optionVOList = electionResultsUpdationService.getAllElectionYears(jObj.getLong("stateId"),jObj.getString("eleType").trim());
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("getStatesForAssign"))
+			{
+				optionVOList = staticDataService.getParticipatedStatesForAnElectionType(jObj.getLong("eleType"));
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("getCandidatesToShow"))
+			{
+				PositionManagementVO positionManagementVO = new PositionManagementVO();
+				positionManagementVO.setElectionTypeId(jObj.getLong("electionTypeId"));
+				positionManagementVO.setCandidateName(jObj.getString("candidateName").trim());
+				positionManagementVO.setYear(jObj.getString("year").trim());
+				positionManagementVO.setPartyId(jObj.getLong("partyId"));
+				positionManagementVO.setStateId(jObj.getLong("stateId"));
+				positionManagementVO.setDistrictId(jObj.getLong("districtId"));
+				positionManagementVO.setTehilId(jObj.getLong("tehilId"));
+				positionManagementVO.setLocalElecBodyId(jObj.getLong("location"));
+				positionManagementVO.setResult(jObj.getString("result").trim());
+				optionVOList = electionResultsUpdationService.getCandidates(positionManagementVO);
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		return Action.SUCCESS;
 	}
+	
+public String updateElectionInformation(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			
+			if(jObj.getString("task").trim().equalsIgnoreCase("addNewPosition"))
+			{
+				resultStatus = electionResultsUpdationService.addNewPosition(jObj.getString("newPosition").trim());
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("assignScopeToPosition"))
+			{
+				resultStatus = electionResultsUpdationService.assignScopeToPosition(jObj.getLong("positionId"),jObj.getLong("electionTypeId"),jObj.getLong("stateId"),jObj.getString("type"));
+				
+			}
+			else if(jObj.getString("task").trim().equalsIgnoreCase("assignCandidateToPosition"))
+			{
+				PositionManagementVO positionManagementVO = new PositionManagementVO();
+				positionManagementVO.setElectionTypeId(jObj.getLong("electionTypeId"));
+				positionManagementVO.setStateId(jObj.getLong("stateId"));	 
+				positionManagementVO.setYear(jObj.getString("year").trim());	   
+				positionManagementVO.setCandidateId(jObj.getLong("candidateId"));
+				positionManagementVO.setPartyId(jObj.getLong("partyId"));
+				positionManagementVO.setDistrictId(jObj.getLong("districtId"));
+				positionManagementVO.setTehilId(jObj.getLong("tehilId"));
+				positionManagementVO.setLocalElecBodyId(jObj.getLong("location"));
+				positionManagementVO.setElectionGovBodyPosId(jObj.getLong("electionGovBodyPosId"));
+				positionManagementVO.setType(jObj.getString("type").trim());
+				positionManagementVO.setFromDate(getDate(jObj.getString("fromDate").trim()));
+				positionManagementVO.setToDate(getDate(jObj.getString("toDate").trim()));
+				positionManagementVO.setStatus(jObj.getString("status").trim());
+				resultStatus = electionResultsUpdationService.assignCandidateToAPosition(positionManagementVO);
+				
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return Action.SUCCESS;
+	}
+  public Date getDate(String dateStr){
+	  String[] dateArray =  dateStr.split("-");
+	  Calendar cal = Calendar.getInstance(); 
+	  cal.set(Integer.parseInt(dateArray[0]),Integer.parseInt(dateArray[1])-1, Integer.parseInt(dateArray[2]));
+	  return cal.getTime();
+   }
 }
