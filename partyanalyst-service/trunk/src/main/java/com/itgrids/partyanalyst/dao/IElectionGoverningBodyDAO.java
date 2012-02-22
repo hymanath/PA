339fb7978bc4b836562816974641f8757d728aa9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-22 16:48:12 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IElectionGoverningBodyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IElectionGoverningBodyDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ElectionGoverningBody;
 
 public interface IElectionGoverningBodyDAO extends GenericDao<ElectionGoverningBody, Long>{
 
+	public List<Object[]> getAllMinistersIdsAndMinistry(Long electionId);
 }
