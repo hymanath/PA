ddd8cbb1a590d8ce96da484f15e08aadcf85a554
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-22 16:49:44 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ElectionGoverningBodyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ElectionGoverningBodyDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IElectionGoverningBodyDAO;
@@ -12,4 +14,10 @@ public class ElectionGoverningBodyDAO extends GenericDaoHibernate<ElectionGovern
 		super(ElectionGoverningBody.class);
 	}
 	
+	public List<Object[]> getAllMinistersIdsAndMinistry(Long electionId)
+	{
+		
+		return getHibernateTemplate().find("select model.candidate.candidateId,model.candidate.lastname,model.positionScope.electionGoverningBodyPosition.governingBodyPosition ," +
+				" model.party.partyId ,model.party.shortName from ElectionGoverningBody model where model.election.electionId = ?",electionId);
+	}
 }
