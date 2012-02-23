be5d99cf5b1bf4692384687c3b3da887623e7b86
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-23 10:41:48 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPositionScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPositionScopeDAO.java
@@ -8,4 +8,10 @@ import com.itgrids.partyanalyst.model.PositionScope;
 
 public interface IPositionScopeDAO  extends GenericDao<PositionScope, Long>  {
 	public List<PositionScope> getPositionScopes(Long electionScopeId,Long electionGoverningBodyPositionId,String type);
+	
+	public List<Object[]> getElectionTypeDetails(Long electionGoverningBodyPositionId,String posType);
+	
+	public List<Object> getPositionTypeDetails(Long electionGoverningBodyPositionId);
+	
+	public List<Object[]> getStateDetails(Long electionGoverningBodyPositionId,String posType);
 }
