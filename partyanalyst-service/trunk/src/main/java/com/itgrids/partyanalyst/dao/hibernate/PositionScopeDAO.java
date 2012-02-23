634aeb64329903fb02bca09554de131ca46b307f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-23 10:42:32 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PositionScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PositionScopeDAO.java
@@ -18,4 +18,22 @@ public class PositionScopeDAO  extends GenericDaoHibernate<PositionScope, Long> 
 		return getHibernateTemplate().find("select model from PositionScope model where model.electionScope.electionScopeId = ?  and model.electionGoverningBodyPosition.governingBodyPositionId = ? and " +
 				" model.status =? ",data);
 	}
+	public List<Object[]> getElectionTypeDetails(Long electionGoverningBodyPositionId,String posType)
+	{
+		Object[] data = {electionGoverningBodyPositionId,posType};
+		return getHibernateTemplate().find("select distinct model.electionScope.electionType.electionTypeId ,model.electionScope.electionType.electionType from PositionScope model where " +
+				"  model.electionGoverningBodyPosition.governingBodyPositionId = ? and model.status = ? ",data);
+	}
+	public List<Object> getPositionTypeDetails(Long electionGoverningBodyPositionId)
+	{
+		Object[] data = {electionGoverningBodyPositionId};
+		return getHibernateTemplate().find("select distinct model.status from PositionScope model where " +
+				"  model.electionGoverningBodyPosition.governingBodyPositionId = ? ",data);
+	}
+	public List<Object[]> getStateDetails(Long electionGoverningBodyPositionId,String posType)
+	{
+		Object[] data = {electionGoverningBodyPositionId,posType};
+		return getHibernateTemplate().find("select distinct model.electionScope.state.stateId ,model.electionScope.state.stateName from PositionScope model where " +
+				"  model.electionGoverningBodyPosition.governingBodyPositionId = ? and model.status = ?  ",data);
+	}
 }
