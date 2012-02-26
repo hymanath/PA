9fcba9fb85335d172bfac582c3a30f02546863d9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-26 12:05:54 +0000

    Updated by AnuRadha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMinisterTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.MinisterType;
+
+public interface IMinisterTypeDAO extends GenericDao<MinisterType, Long>{
+
+	
+}
