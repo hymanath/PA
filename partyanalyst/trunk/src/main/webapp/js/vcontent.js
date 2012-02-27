1d6e8cc60dd76e7d628f038f04c484e9aec5909b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-02-27 13:41:56 +0000

    Updated by AnuRadha

--- partyanalyst/trunk/src/main/webapp/js/vcontent.js
+++ partyanalyst/trunk/src/main/webapp/js/vcontent.js
@@ -5,6 +5,6 @@ ns4=document.layers
 var speed=5
 
 if (iens6){
-document.write('<div id="container" style="position:relative;width:438px;height:251px;overflow:hidden;margin-top:14px">')
-document.write('<div id="content" style="position:absolute;width:438px;left:0;top:0">')
+document.write('<div id="container" style="position:relative;width:308px;height:251px;overflow:hidden;margin-top:14px">')
+document.write('<div id="content" style="position:absolute;width:308px;left:0;top:0">')
 }
