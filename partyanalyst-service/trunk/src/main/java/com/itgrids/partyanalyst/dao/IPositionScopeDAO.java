e16879b8c76dd199fc3424ed881ee6071718caf3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-03-02 11:39:46 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPositionScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPositionScopeDAO.java
@@ -7,11 +7,13 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PositionScope;
 
 public interface IPositionScopeDAO  extends GenericDao<PositionScope, Long>  {
-	public List<PositionScope> getPositionScopes(Long electionScopeId,Long electionGoverningBodyPositionId,String type);
+	public List<PositionScope> getPositionScopes(Long electionScopeId,Long electionGoverningBodyPositionId,Long electionType,Long ministerTypeId);
 	
-	public List<Object[]> getElectionTypeDetails(Long electionGoverningBodyPositionId,String posType);
+	public List<Object[]> getElectionTypeDetails(Long electionGoverningBodyPositionId);
 	
-	public List<Object> getPositionTypeDetails(Long electionGoverningBodyPositionId);
+	public List<Object[]> getPositionTypeDetails(Long electionGoverningBodyPositionId,Long electionType);
 	
-	public List<Object[]> getStateDetails(Long electionGoverningBodyPositionId,String posType);
+	public List<Object[]> getStateDetails(Long electionGoverningBodyPositionId,Long electionType,Long ministerTypeId);
+	
+	public List<Object[]> getMinisteryTypeDetails(Long electionGoverningBodyPositionId,Long electionType);
 }
