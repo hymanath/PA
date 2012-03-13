059f333814f0b94de9eb0874585bf4c087b13168
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-03-13 14:50:26 +0000

    Updated by Sri Hari

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/MessageToPartyDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/MessageToPartyDAOHibernateTest.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IMessageToPartyDAO;
+import com.itgrids.partyanalyst.service.impl.DateService;
 
 public class MessageToPartyDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -11,10 +14,24 @@ public class MessageToPartyDAOHibernateTest extends BaseDaoTestCase{
 	public void setMessageToPartyDAO(IMessageToPartyDAO messageToPartyDAO) {
 		this.messageToPartyDAO = messageToPartyDAO;
 	}
-	public void test()
+	/*public void test()
 	{
 		messageToPartyDAO.getAll();
+	}*/
+	/* public void testGetAllOpenedComments()
+	{
+		 
+		System.out.println();
+		List list = messageToPartyDAO.getAllPartyMessages(DateService.convertStringToDate("2012-03-05", "yyyy-MM-dd"),
+				DateService.convertStringToDate("2012-03-05", "yyyy-MM-dd"),"Rejected");
+		System.out.println(list.size());
+	}*/
+	public void testadminModifiedMessages()
+	{
+		Integer result =messageToPartyDAO.adminModifiedMessages(10l,"suri messaged is updated","true"); 
+		System.out.println(result);
 	}
+
 
 
 }
