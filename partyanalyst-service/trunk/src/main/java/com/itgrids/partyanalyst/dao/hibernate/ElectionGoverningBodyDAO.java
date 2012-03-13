630961d3a17021fc48c0621d99b1a54b30c2d2bd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-03-13 13:37:01 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ElectionGoverningBodyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ElectionGoverningBodyDAO.java
@@ -21,4 +21,22 @@ public class ElectionGoverningBodyDAO extends GenericDaoHibernate<ElectionGovern
 		return getHibernateTemplate().find("select model.candidate.candidateId,model.candidate.lastname,model.positionScope.electionGoverningBodyPosition.governingBodyPosition ," +
 				" model.party.partyId ,model.party.shortName from ElectionGoverningBody model where model.election.electionId = ?",electionId);
 	}
+	public List<Object[]> getAllStatesForMinisters()
+	{
+		
+		return getHibernateTemplate().find("select distinct model.election.electionScope.state.stateId,model.election.electionScope.state.stateName " +
+				"  from ElectionGoverningBody model where model.election.electionScope.electionType.electionType = ? order by model.election.electionScope.state.stateName","Assembly");
+	}
+	public List<Object[]> getAllYearsAndElecIdsForAssembly(Long stateId)
+	{
+		
+		return getHibernateTemplate().find("select distinct model.election.electionId,model.election.electionYear from ElectionGoverningBody model " +
+				"   where model.election.electionScope.state.stateId = ? order by model.election.electionYear ",stateId);
+	}
+	public List<Object[]> getAllYearsAndElecIdsForParliament()
+	{
+		
+		return getHibernateTemplate().find("select distinct model.election.electionId,model.election.electionYear from ElectionGoverningBody model " +
+				"   where model.election.electionScope.electionType.electionType = ? order by model.election.electionYear ","Parliament");
+	}
 }
