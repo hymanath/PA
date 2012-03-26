7ebe81f5c94f5874604fcbc3f64d2cff933d6c81
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-03-26 15:00:28 +0000

    Updated by Sasikanth - for update emails

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageUpdatesEmailDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageUpdatesEmailDAO.java
@@ -1,7 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-
 import com.itgrids.partyanalyst.dao.ISpecialPageUpdatesEmailDAO;
 import com.itgrids.partyanalyst.model.SpecialPageUpdatesEmail;
 
@@ -10,5 +10,11 @@ public class SpecialPageUpdatesEmailDAO extends GenericDaoHibernate<SpecialPageU
 	public SpecialPageUpdatesEmailDAO()
 	{
 	   super(SpecialPageUpdatesEmail.class);
+	}
+	
+	public List<Object> getSpecialPageUpdatesEmail(String emailId, Long specialPageId) 
+	{
+		Object[] params = {specialPageId,emailId};
+		return getHibernateTemplate().find("select model from SpecialPageUpdatesEmail model where model.specialPage.specialPageId = ? and model.email = ?",params);
 	}
 }
