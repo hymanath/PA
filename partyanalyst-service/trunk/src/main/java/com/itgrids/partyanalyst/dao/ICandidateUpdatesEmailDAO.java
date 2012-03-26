3a9533112784212583ddc8abd82fe1f8bdd5d0aa
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-03-26 14:58:09 +0000

    Updated by Sasikanth - for update emails

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICandidateUpdatesEmailDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICandidateUpdatesEmailDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.CandidateUpdatesEmail;
 
 public interface ICandidateUpdatesEmailDAO extends GenericDao<CandidateUpdatesEmail,Long>{
-
+	
+	public List<Object> getCandidateUpdatesEmail(String emailId ,Long candidateId);
+	
 }
