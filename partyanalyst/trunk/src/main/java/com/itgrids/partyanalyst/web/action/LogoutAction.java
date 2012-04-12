748d3157e6f127856609f1e7225e429827ef6499
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-12 10:54:06 +0000

    updated by kamalakar

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/LogoutAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/LogoutAction.java
@@ -9,6 +9,9 @@ import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.util.ServletContextAware;
 
+import com.itgrids.partyanalyst.dto.UserTrackingVO;
+import com.itgrids.partyanalyst.service.ILoginService;
+import com.itgrids.partyanalyst.util.IWebConstants;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LogoutAction extends ActionSupport implements ServletContextAware, ServletRequestAware,ServletResponseAware{
@@ -18,6 +21,15 @@ public class LogoutAction extends ActionSupport implements ServletContextAware, 
 	private HttpSession session;
 	private ServletContext context;
 	private HttpServletResponse response;
+	private ILoginService loginService;
+
+	public ILoginService getLoginService() {
+		return loginService;
+	}
+
+	public void setLoginService(ILoginService loginService) {
+		this.loginService = loginService;
+	}
 
 	public void setServletContext(ServletContext context) {
 		this.context = context;
@@ -34,7 +46,14 @@ public class LogoutAction extends ActionSupport implements ServletContextAware, 
 	public String execute()
 	{
 		session = request.getSession();
+		
+		UserTrackingVO userTrackingVO = new UserTrackingVO();
+		userTrackingVO.setSessionId(session.getId());
+		userTrackingVO.setStatus(IWebConstants.LOGOUT);
+		loginService.saveUserSessionDetails(userTrackingVO);
+		
 		session.invalidate();
+		
 		try{
 			response.sendRedirect("homePage.action");
 		}catch (Exception e) {
