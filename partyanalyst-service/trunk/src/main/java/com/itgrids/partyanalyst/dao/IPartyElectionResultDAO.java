c1e0d4cc294611148f8fa6d6bb38c1a1217b7860
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-13 06:29:08 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyElectionResultDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyElectionResultDAO.java
@@ -31,7 +31,7 @@ public interface IPartyElectionResultDAO extends GenericDao<PartyElectionResult,
 	
 	public List<Object[]> getPartiesBasicResultForAnElection(Long electionId,List<Long> partiesList);
 	
-	public List<PartyElectionResult> getPartyElectionResultsBasedOnPartyId(Long partyId,String electionType);
+	public List<PartyElectionResult> getPartyElectionResultsBasedOnPartyId(Long partyId,String electionType,boolean includeBielections);
 	
 	public List<PartyElectionResult> getPartyElectionResultsBasedOnPartyIdAndElecId(Long partyId,String electionType,Long electionId);
 
