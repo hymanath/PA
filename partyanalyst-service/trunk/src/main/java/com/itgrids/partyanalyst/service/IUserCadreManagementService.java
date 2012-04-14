c0459754f8b11a6b67ce35cc0aa19e12e9eb2538
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-14 14:33:47 +0000

    Updated by  Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IUserCadreManagementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IUserCadreManagementService.java
@@ -5,6 +5,12 @@ import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.UserCadresInfoVO;
 
 public interface IUserCadreManagementService {
+	
 	public CadreManagementVO getUserData(RegistrationVO user);
+	
 	public CadreManagementVO getUserTodaysData(RegistrationVO user);
+	
+	public CadreManagementVO getUserPlannedEvents(Long userId);
+	
+	public CadreManagementVO getUserTodaysImpDates(RegistrationVO user);
 }
