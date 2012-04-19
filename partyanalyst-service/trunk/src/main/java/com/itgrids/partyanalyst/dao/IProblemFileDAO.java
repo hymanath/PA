53e670ba27f978875123400b83a556adb99999ed
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-19 10:47:17 +0000

    updated by nagamani.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFileDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFileDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -13,4 +15,16 @@ public interface IProblemFileDAO extends GenericDao<ProblemFile, Long> {
 	public List<Object[]> getAllNonApprovedImagesBetweenDatesWithCompleteData();
 	
 	public List<Object[]> getAllNonApprovedFilesAndProblemDetails();
+	
+	public List<Object[]> getAllApprovedImagesBetweenDates(Date startDate,Date endDate,String status);
+	
+	public List<Object[]> getAllNoNApprovalImagesBetweenDates(Date startDate,Date endDate,String status);
+	
+	public List<Object[]> getAllPostedImagesBetweenDates(Date startDate,Date endDate,String status);
+	
+	public List<Object[]> getApprovalImagesForParticularDate(Date particularDate,String status);
+	
+	public List<Object[]> getAllNonApprovalImagesForParticularDate(Date particularDate,String status);
+	
+	public List<Object[]> getAllImagesForParticularDate(Date particularDate,String status);
 }
