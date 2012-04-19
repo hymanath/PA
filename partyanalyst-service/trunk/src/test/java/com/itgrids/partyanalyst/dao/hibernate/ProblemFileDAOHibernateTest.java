0b0e1b1c7639f7bdd603ac78a08bbe0a2aadd3e4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-19 10:38:05 +0000

    updated by nagamani.

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFileDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFileDAOHibernateTest.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
@@ -7,13 +11,19 @@ import org.appfuse.dao.BaseDaoTestCase;
 import com.itgrids.partyanalyst.dao.IProblemFileDAO;
 
 public class ProblemFileDAOHibernateTest extends BaseDaoTestCase{
-	private IProblemFileDAO problemFileDAO;
-
 	
+	private IProblemFileDAO problemFileDAO;
 
 	public void setProblemFileDAO(IProblemFileDAO problemFileDAO) {
 		this.problemFileDAO = problemFileDAO;
 	}
+
+	public IProblemFileDAO getProblemFileDAO() {
+		return problemFileDAO;
+	}
+
+	
+
 	
 	/*public void test()
 	{
@@ -26,7 +36,21 @@ public class ProblemFileDAOHibernateTest extends BaseDaoTestCase{
 		System.out.println(result.size());
 		}*/
 	
-	public void testGetAllNonApprovedFilesAndProblemDetails()
+	/*public IProblemFileDAO getProblemFileDAO() {
+		return problemFileDAO;
+	}
+
+
+
+
+	public void setProblemFileDAO(IProblemFileDAO problemFileDAO) {
+		this.problemFileDAO = problemFileDAO;
+	}*/
+
+
+
+
+/*public void testGetAllNonApprovedFilesAndProblemDetails()
 	{
 		List<Object[]> list = problemFileDAO.getAllNonApprovedFilesAndProblemDetails();
 		
@@ -38,5 +62,124 @@ public class ProblemFileDAOHibernateTest extends BaseDaoTestCase{
 			for(Object obj: params)
 				System.out.print("----"+obj.toString());
 		}
+	}*/
+	/*@SuppressWarnings("deprecation")
+	public void testgetAllApprovedImagesBetweenDates()
+	{
+		
+		Date startDate=null;
+		Date endDate=null;
+		DateFormat format = 
+			 new SimpleDateFormat("yyyy-MM-dd");
+		 
+		try {
+			 startDate = (Date)format.parse("2011-04-09 01:33:43");
+			
+			endDate = (Date)format.parse("2011-04-09 21:35:51");
+			
+			String query = "date(model.dateUpdated) =?";
+		} catch (ParseException e1) {
+				e1.printStackTrace();
+		}
+		
+		List<Object[]>  result = problemFileDAO.getAllApprovedImagesBetweenDates(startDate,endDate,"NEW");
+		
+			for(Object[] params : result)
+			{
+				System.out.println(params[0]);
+				System.out.println(params[1]);
+				System.out.println(params[2]);
+				
+			}
+		
+	
+	}*/
+	
+	/*@SuppressWarnings("deprecation")
+	public void testgetAllNoNApprovalImagesBetweenDates() throws ParseException
+	{
+		Date startDate = null;
+		Date endDate = null;
+		DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		startDate = (Date)format.parse("2011-04-09 01:33:43");
+		endDate = (Date)format.parse("2011-04-03 20:50:29");
+		String query = "date(model.dateUpdated) =?";
+		List<Object[]> result = problemFileDAO.getAllNoNApprovalImagesBetweenDates(startDate, endDate, "NEW");
+		for(Object[] params : result)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1]);
+			
+		}
 	}
+	*/
+	/*
+	@SuppressWarnings("unchecked")
+	public void testgetAllPostedImagesBetweenDates() throws ParseException
+	{
+	Date startDate = null;
+	Date endDate = null;
+	DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+	startDate = (Date)format.parse("2012-04-13 10:30:09");
+	endDate = (Date)format.parse("2012-04-18 14:59:46");
+	String query = "date(model.dateUpdated) = ?";
+	List<Object[]> result = problemFileDAO.getAllPostedImagesBetweenDates(startDate, endDate, "NEW");
+	for(Object[] params : result)
+	{
+		System.out.println(params[0]);
+		System.out.println(params[1].toString());
+	}
+	
+	}*/
+	/*
+	@SuppressWarnings("unchecked")
+	public void testgetApprovalImagesForParticularDate() throws ParseException
+	{
+		Date particularDate = null;
+		DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		particularDate = (Date)format.parse("2011-04-09 01:33:43");
+		
+		String query = "date(model.dateUpdated) = ?";
+		List<Object[]> result = problemFileDAO.getApprovalImagesForParticularDate(particularDate, "NEW");
+		for(Object[] params : result)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1]);
+		}
+		}*/
+	/*@SuppressWarnings("unchecked")
+	public void testgetAllNonApprovalImagesForParticularDate() throws ParseException
+	{
+		Date particularDate = null;
+		DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		particularDate = (Date)format.parse("2011-04-09 01:33:43");
+		
+		String query = "date(model.dateUpdated) = ?";
+		List<Object[]> result = problemFileDAO.getApprovalImagesForParticularDate(particularDate, "NEW");
+		for(Object[] params : result)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1]);
+		}
+		}*/
+	
+	public void testgetAllgetAllImagesForParticularDate() throws ParseException
+	{
+		Date particularDate = null;
+		DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		particularDate = (Date)format.parse("2012-04-18 17:27:12");
+		
+		String query = "date(model.dateUpdated) = ?";
+		List<Object[]> result = problemFileDAO.getApprovalImagesForParticularDate(particularDate, "NEW");
+		for(Object[] params : result)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1]);
+			System.out.println(params[2]);
+			System.out.println(params[3]);
+		}
+		}
+	
+	
+	
 }
