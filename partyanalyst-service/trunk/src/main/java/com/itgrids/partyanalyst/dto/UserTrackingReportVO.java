6393d6004bc9ffa32dff42a13e3bcdbb10805285
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-19 11:44:06 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserTrackingReportVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserTrackingReportVO.java
@@ -1,11 +1,25 @@
 package com.itgrids.partyanalyst.dto;
 
-public class UserTrackingReportVO {
+import java.io.Serializable;
+import java.util.Date;
+
+public class UserTrackingReportVO extends UserTrackingVO implements Serializable{
+	
+	private static final long serialVersionUID = 2217831199065585193L;
 	private Long uniqueVisitors;
 	private String totalTimeSpent;
 	private String avgTimeSpent;
 	private Long totalNoOfPagesAccessed;
 	private float avgNoOfPagesAccessed;
+	private Date time;
+	private String landingPage;
+	private String exitPage;
+	private String spentTime;
+	private Date fromDate;
+	private Date toDate;
+	private String userName;
+	private Long noOfPages;
+	
 	
 	public Long getUniqueVisitors() {
 		return uniqueVisitors;
@@ -13,6 +27,45 @@ public class UserTrackingReportVO {
 	public void setUniqueVisitors(Long uniqueVisitors) {
 		this.uniqueVisitors = uniqueVisitors;
 	}
+	
+	public Date getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+	public Date getToDate() {
+		return toDate;
+	}
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+	
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public String getLandingPage() {
+		return landingPage;
+	}
+	public void setLandingPage(String landingPage) {
+		this.landingPage = landingPage;
+	}
+	public String getExitPage() {
+		return exitPage;
+	}
+	public void setExitPage(String exitPage) {
+		this.exitPage = exitPage;
+	}
+	public String getSpentTime() {
+		return spentTime;
+	}
+	public void setSpentTime(String spentTime) {
+		this.spentTime = spentTime;
+	}
+	
 	public String getTotalTimeSpent() {
 		return totalTimeSpent;
 	}
@@ -25,6 +78,14 @@ public class UserTrackingReportVO {
 	public void setAvgTimeSpent(String avgTimeSpent) {
 		this.avgTimeSpent = avgTimeSpent;
 	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 	public Long getTotalNoOfPagesAccessed() {
 		return totalNoOfPagesAccessed;
 	}
