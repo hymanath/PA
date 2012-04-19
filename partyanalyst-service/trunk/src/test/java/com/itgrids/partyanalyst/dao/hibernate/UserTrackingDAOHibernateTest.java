fa57a1097c77d8f0c3bbdae45202695134bbc812
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-19 11:45:09 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserTrackingDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserTrackingDAOHibernateTest.java
@@ -16,12 +16,36 @@ public class UserTrackingDAOHibernateTest extends BaseDaoTestCase{
 		this.userTrackingDAO = userTrackingDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		userTrackingDAO.getAll();
+	}*/
+	/*public void testGetHostNameAndNoOfPagesForAUser()
+	{
+		List<Object[]> list = userTrackingDAO.getHostNameAndNoOfPagesForAVisitor(new Date() , new Date());
+		System.out.println(list.size());
+		if(list != null && list.size() > 0 )
+			
+			for(Object[] params : list)
+			{
+				System.out.println(params[0]+ " " +params[1]+ " " +params[2]+ " " +params[3]);
+				Date max = (Date)params[2];
+				Date min = (Date)params[3];
+				
+				Long SponOnPage = (max.getTime() - min.getTime())/(1000*60);
+				System.out.println(max);
+				System.out.println(min);
+				System.out.println((max.getTime() - min.getTime())/(1000*60));
+				Date dd = new Date(SponOnPage);
+				System.out.println("spent on time is........" +dd.getMinutes());
+				String s = Long.toString(dd.getMinutes());
+				System.out.println(s.toString());
+				
+			}
 	}
+	*/
 	
-	public void testGetUniqueVisitorsBetweenDates(){
+	/*public void testGetUniqueVisitorsBetweenDates(){
 		Long sessCnt=(Long)userTrackingDAO.getUniqueVisitorsBetweenDates(new Date(2012-04-12),new Date() , IConstants.PARTY_ANALYST_USER);
 		System.out.println("Session Count: \t"+sessCnt.longValue());
 	}
@@ -30,8 +54,15 @@ public class UserTrackingDAOHibernateTest extends BaseDaoTestCase{
 		  Calendar cal = Calendar.getInstance(); 
 		  cal.set(Integer.parseInt(dateArray[0]),Integer.parseInt(dateArray[1])-1, Integer.parseInt(dateArray[2]));
 		  return cal.getTime();
-	  }
-	
+	  }*/
+		
+	public void testGetHostNameAndNoOfPagesForAUser(){
+		List<Object[]> list = userTrackingDAO.getHostNameAndNoOfPagesForAUser(new Date(),new Date(),null);
+		System.out.println(list.size());
+		if(list != null && list.size() > 0) 
+		for(Object[] params : list){
+			System.out.println(params[0] + "" + params[1] + "" +params[2] + "" +params[3]);
+			System.out.println(params[1].toString()+" "+params[2].toString());
+		}
+	}
 }
-
-
