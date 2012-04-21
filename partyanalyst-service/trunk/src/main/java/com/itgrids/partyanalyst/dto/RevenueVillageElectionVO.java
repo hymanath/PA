e8d93e258231bf43fc191194ee186c7d79936590
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-21 08:37:56 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/RevenueVillageElectionVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/RevenueVillageElectionVO.java
@@ -1,13 +1,50 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.util.List;
+import java.util.Set;
 
 public class RevenueVillageElectionVO {
 
 	private Long townshipId;
 	private String townshipName;
+	private Long totalVoters;
+	private Long votesPolled;
 	private List<PartyElectionResultVO> partyElectionResultVOs;
 	private List<SelectOptionVO> booths;
+	private Set<SelectOptionVO> boothsSet;
+	private List<SelectOptionVO> hamletsOfTownship;
+
+	public Long getTotalVoters() {
+		return totalVoters;
+	}
+
+	public void setTotalVoters(Long totalVoters) {
+		this.totalVoters = totalVoters;
+	}
+
+	public Long getVotesPolled() {
+		return votesPolled;
+	}
+
+	public void setVotesPolled(Long votesPolled) {
+		this.votesPolled = votesPolled;
+	}
+
+	public Set<SelectOptionVO> getBoothsSet() {
+		return boothsSet;
+	}
+
+	public void setBoothsSet(Set<SelectOptionVO> boothsSet) {
+		this.boothsSet = boothsSet;
+	}
+
+	public List<SelectOptionVO> getHamletsOfTownship() {
+		return hamletsOfTownship;
+	}
+
+	public void setHamletsOfTownship(List<SelectOptionVO> hamletsOfTownship) {
+		this.hamletsOfTownship = hamletsOfTownship;
+	}
 
 	public Long getTownshipId() {
 		return townshipId;
