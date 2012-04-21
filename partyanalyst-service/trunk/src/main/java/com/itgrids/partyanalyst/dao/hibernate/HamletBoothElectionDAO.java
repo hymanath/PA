0f3fd7de96ad002b0ba9fd4c65d2bb07d1144abb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-21 08:36:31 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HamletBoothElectionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HamletBoothElectionDAO.java
@@ -54,12 +54,12 @@ public class HamletBoothElectionDAO extends GenericDaoHibernate<HamletBoothElect
 	
 	@SuppressWarnings("unchecked")
 	public List<Object[]> findPanchayatWiseVotingTrendsForATehsil(Long tehsilId, String electionIds)
-	{
+	{	Object[] params = {tehsilId, tehsilId};
 		return getHibernateTemplate().find("select model2.panchayat.panchayatId, model2.panchayat.panchayatName, " +
 				" sum(model.boothConstituencyElection.boothResult.validVotes), model.boothConstituencyElection.constituencyElection.election.electionId" +
-				" from HamletBoothElection model,PanchayatHamlet model2 where model.boothConstituencyElection.booth.tehsil.tehsilId = ? " +
+				" from HamletBoothElection model,PanchayatHamlet model2 where model.boothConstituencyElection.booth.tehsil.tehsilId = ? and model2.panchayat.tehsil.tehsilId = ? " +
 				" and model.boothConstituencyElection.constituencyElection.election.electionId in ("+electionIds+
-				" ) and model2.hamlet.hamletId = model.hamlet.hamletId group by model2.panchayat.panchayatId,model.boothConstituencyElection.constituencyElection.election.electionId",tehsilId);
+				" ) and model2.hamlet.hamletId = model.hamlet.hamletId group by model2.panchayat.panchayatId,model.boothConstituencyElection.constituencyElection.election.electionId",params);
 	}
 		
 }
