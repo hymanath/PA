01f50f666d6c40226caa698d551b1971ae5ea57c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-25 09:26:37 +0000

    updated by kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/EmailDetailsVO.java
@@ -0,0 +1,47 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class EmailDetailsVO implements Serializable{
+
+	private static final long serialVersionUID = 2598121949467285613L;
+	
+	private String subject;
+	private String toAddress;
+	private String fromAddress;
+	private String content;
+	private String host;
+	
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getToAddress() {
+		return toAddress;
+	}
+	public void setToAddress(String toAddress) {
+		this.toAddress = toAddress;
+	}
+	public String getFromAddress() {
+		return fromAddress;
+	}
+	public void setFromAddress(String fromAddress) {
+		this.fromAddress = fromAddress;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
