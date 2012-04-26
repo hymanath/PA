ea8af5f793f3fc0b135f5bb9d8216b0a35f58908
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-26 12:51:02 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyPageCustomPagesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyPageCustomPagesDAO.java
@@ -3,6 +3,8 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
 import com.itgrids.partyanalyst.dao.IPartyPageCustomPagesDAO;
 import com.itgrids.partyanalyst.model.PartyPageCustomPages;
 
@@ -20,4 +22,26 @@ public class PartyPageCustomPagesDAO extends GenericDaoHibernate<PartyPageCustom
 				"model.party.partyId = ? order by model.customPage.customPageType.orderNo",partyId);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<PartyPageCustomPages> partyExistsOrNot(Long pageId,String customPageName)
+	{
+		Object[] params = {pageId,customPageName};
+		return getHibernateTemplate().find("select model.customPage.customPageId from PartyPageCustomPages model where model.party.partyId = ? and model.customPage.name = ?",params);
+	}
+	public List<Object[]> getPartyCustomPage(Long pageId)
+	{
+		StringBuilder query = new StringBuilder();
+		query.append("select  model.customPage.name,model.customPage.customPageType.customPageType,model.customPage.customPageType.customPageTypeId from PartyPageCustomPages model "
+				+ " where model.party.partyId =:pageId");
+		
+		Query queryObject = getSession().createQuery(query.toString());
+		queryObject.setLong("pageId", pageId);
+		return queryObject.list();
+		
+	}
+	@SuppressWarnings("unchecked")
+	public List<Object> getPartyPageId(Long pageId)
+	{
+		return getHibernateTemplate().find("select model.customPage.customPageId from PartyPageCustomPages model where model.party.partyId = ? ",pageId);
+	}
 }
