166accb26f3a6e6d7bbf55ad3da7f13cb3f40f2f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-26 12:49:59 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CustomPageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CustomPageDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICustomPageDAO;
 import com.itgrids.partyanalyst.model.CustomPage;
@@ -10,6 +11,22 @@ public class CustomPageDAO extends GenericDaoHibernate<CustomPage, Long> impleme
 	public CustomPageDAO()
 	{
 		super(CustomPage.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public int updateCustompage( Long customPageId,String customPageName,Long CustomPageType)
+	{
+		StringBuilder query = new StringBuilder();
+		query.append("Update CustomPage model set model.name = ? , model.customPageType.customPageTypeId = ? where model.customPageId = ?");
+		
+		Query queryObject = getSession().createQuery(query.toString());
+		
+		queryObject.setString(0, customPageName);
+		queryObject.setLong(1, CustomPageType);
+		queryObject.setLong(2, customPageId);
+		
+		return queryObject.executeUpdate();
+		
 	}
 
 }
