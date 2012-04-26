cc47034242d7c457489861d7aedc34df1fb33816
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-26 12:51:39 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageCustomPagesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageCustomPagesDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageCustomPagesDAO;
 import com.itgrids.partyanalyst.model.SpecialPageCustomPages;
@@ -20,5 +21,29 @@ public class SpecialPageCustomPagesDAO extends GenericDaoHibernate<SpecialPageCu
 		return getHibernateTemplate().find("select model.customPage.name,model.customPage.customPageType.customPageType from SpecialPageCustomPages model where " +
 				"model.specialPage.specialPageId = ? order by model.customPage.customPageType.orderNo",specialPageId);
 		
+	}
+	@SuppressWarnings("unchecked")
+	public List<SpecialPageCustomPages> specialIdExistsOrNot(Long pageId,String customPageName)
+	{
+		Object[] params = {pageId,customPageName};
+		return getHibernateTemplate().find("select model.customPage.customPageId from SpecialPageCustomPages model where model.specialPage.specialPageId = ? and model.customPage.name = ?",params);
+		
+	}
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getSpecialCustomPage(Long pageId)
+	{
+		StringBuilder query = new StringBuilder();
+		query.append("select model.customPage.name,model.customPage.customPageType.customPageType,model.customPage.customPageType.customPageTypeId from SpecialPageCustomPages model"
+				+ " where model.specialPage.specialPageId =:pageId");
+		
+		Query queryobj = getSession().createQuery(query.toString());
+		queryobj.setLong("pageId", pageId);
+		
+		 return queryobj.list();
+	}
+	@SuppressWarnings("unchecked")
+	public List<Object> getSpecialPageId(Long pageId)
+	{
+		return getHibernateTemplate().find("select model.customPage.customPageId from SpecialPageCustomPages model where model.specialPage.specialPageId = ? ",pageId);
 	}
 }
