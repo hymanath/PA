2958a172559f9fedb8f212c12b733a28338eb59f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-26 12:46:53 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyPageCustomPagesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyPageCustomPagesDAO.java
@@ -7,5 +7,11 @@ import com.itgrids.partyanalyst.model.PartyPageCustomPages;
 public interface IPartyPageCustomPagesDAO extends GenericDao<PartyPageCustomPages,Long>{
 	
 	public List<Object[]> getCustomPagesOfAPartyPage(Long partyId);
+	
+	public List<PartyPageCustomPages> partyExistsOrNot(Long pageId,String customPageName);
+	
+	public List<Object[]> getPartyCustomPage(Long pageId);
+	
+	public List<Object> getPartyPageId(Long pageId);
 
 }
