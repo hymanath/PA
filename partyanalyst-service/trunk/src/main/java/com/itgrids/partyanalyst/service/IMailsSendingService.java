3db9021acaac1b16bf52521e7fb02cdd8ddcd537
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-04-28 11:17:15 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailsSendingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailsSendingService.java
@@ -6,4 +6,5 @@ public interface IMailsSendingService {
 	
 	public ResultStatus sendEmailFriendRequest(String userName,String email,String requestFrom,String senderName , String msg);
 
+	public ResultStatus acceptEmailFriendRequest(String userName,String email,String requestFrom,String senderName);
 }
