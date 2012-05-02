c3a8505a66e63189ffe4f1b74e294c4efcfcf028
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-02 08:47:40 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailsSendingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMailsSendingService.java
@@ -1,12 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
+import com.itgrids.partyanalyst.dto.EmailDetailsVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface IMailsSendingService {
 	
-	public ResultStatus sendEmailFriendRequest(String userName,String email,String requestFrom,String senderName , String msg);
+	public ResultStatus sendEmailFriendRequest(EmailDetailsVO emailDetailsVO);
 
-	public ResultStatus acceptEmailFriendRequest(String userName,String email,String requestFrom,String senderName);
+	public ResultStatus acceptEmailFriendRequest(EmailDetailsVO emailDetailsVO);
 	
-	public ResultStatus sendMessageToConnectUser(String userName,String email,String requestFrom,String msg,String senderName);
+	public ResultStatus sendMessageToConnectUser(EmailDetailsVO emailDetailsVO);
 }
