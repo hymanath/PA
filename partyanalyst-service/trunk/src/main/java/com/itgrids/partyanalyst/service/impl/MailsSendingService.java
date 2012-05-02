ffe0b4c9507f7042dcc6ed39bf489ac731d5ec3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-02 09:13:00 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/MailsSendingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/MailsSendingService.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import org.apache.log4j.Logger;
+
 import com.itgrids.partyanalyst.dto.EmailDetailsVO;
+import com.itgrids.partyanalyst.dto.ResultCodeMapper;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.service.IMailService;
 import com.itgrids.partyanalyst.service.IMailsSendingService;
@@ -8,8 +11,10 @@ import com.itgrids.partyanalyst.service.IMailsTemplateService;
 
 public class MailsSendingService implements IMailsSendingService{
 	
+	private static final Logger log = Logger.getLogger(MailsSendingService.class);
 	private IMailService mailService;
 	private IMailsTemplateService mailsTemplateService;
+	
 	
 	public IMailsTemplateService getMailsTemplateService() {
 		return mailsTemplateService;
@@ -24,55 +29,88 @@ public class MailsSendingService implements IMailsSendingService{
 		this.mailService = mailService;
 	}
 	
-	public ResultStatus sendEmailFriendRequest(EmailDetailsVO emailDetVO)
+	public ResultStatus sendEmailFriendRequest(EmailDetailsVO emailDetailsVO)
 	{
 		ResultStatus resultStatus = new ResultStatus();
-		String subject = ""+emailDetVO.getSenderName()+" sent a Friend Request in PartyAnalyst.com";
-		
-		String content = " <div style='border:1px solid #CCCCCC'>"+mailsTemplateService.getHeader()+"<br/><div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emailDetVO.getFromAddress()+",</b><br/></div><div style='margin-left: 45px; margin-bottom: 39px;line-height:1.5em;'><b><font style='color:blue;'>"+emailDetVO.getSenderName()+"</font></b> has sent you a friend request in PartyAnalyst.com. <br/>  To respond " +
-				"to friend request, <a href='http://www.partyanalyst.com/loginInputAction.action'><b>Login here.</b></a></div></div>" ;
-		
-		EmailDetailsVO emailDetailsVO = new EmailDetailsVO();
-		emailDetailsVO.setToAddress(emailDetVO.getToAddress());
-		emailDetailsVO.setHost(emailDetVO.getHost());
-		emailDetailsVO.setSubject(subject);
-		emailDetailsVO.setContent(content);
-		resultStatus = mailService.sendEmail(emailDetailsVO ,emailDetVO.getHost() );
-		return resultStatus;
+		try
+		{
+			if(emailDetailsVO == null)
+			{
+				resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+				log.error("EmailDetailsVO is Null");
+				return resultStatus;
+			}
+				
+			String subject = ""+emailDetailsVO.getSenderName()+" sent a Friend Request in PartyAnalyst.com";
+			String content = " <div style='border:1px solid #CCCCCC'>"+mailsTemplateService.getHeader()+"<br/><div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emailDetailsVO.getFromAddress()+",</b><br/></div><div style='margin-left: 45px; margin-bottom: 39px;line-height:1.5em;'><b><font style='color:blue;'>"+emailDetailsVO.getSenderName()+"</font></b> has sent you a friend request in PartyAnalyst.com. <br/>  To respond " +
+					"to friend request, <a href='http://www.partyanalyst.com/loginInputAction.action'><b>Login here.</b></a></div></div>" ;
+			emailDetailsVO.setSubject(subject);
+			emailDetailsVO.setContent(content);
+			resultStatus = mailService.sendEmail(emailDetailsVO ,emailDetailsVO.getHost());
+			
+			resultStatus.setResultCode(ResultCodeMapper.SUCCESS);
+			return resultStatus;
+		}catch (Exception e) {
+			resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+			log.error("Exception Occured in sendEmailFriendRequest() Method, Exception is  - "+e );
+			return resultStatus;
+		}
 	}
 	
-	public ResultStatus acceptEmailFriendRequest(EmailDetailsVO emaDetailsVO)
+	public ResultStatus acceptEmailFriendRequest(EmailDetailsVO emailDetailsVO)
 	{
 		ResultStatus resultStatus = new ResultStatus();
-		EmailDetailsVO emailDetailsVO = new EmailDetailsVO();
-		String subject = ""+emaDetailsVO.getSenderName()+" accepted Friend Request in PartyAnalyst.com."; 
-		String content = "<div style = 'border:1px solid #CCCCCC'>"+mailsTemplateService.getHeader()+"<br/><div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emaDetailsVO.getSenderName()+",</b></div>" +
-				"<div style='margin-left: 45px; margin-bottom: 40px;line-height: 1.5em;'><font style='color:blue'><b>"+emaDetailsVO.getFromAddress()+"</b></font> has accepted your friend request in PartyAnalyst.com.<br/>Do you want to send a message? " +
-						"<a href='http://www.partyanalyst.com/loginInputAction.action'><b> Login Here</b> </a></div></div>";
-		
-		emailDetailsVO.setHost(emaDetailsVO.getHost());
-		emailDetailsVO.setSubject(subject);
-		emailDetailsVO.setContent(content);
-		emailDetailsVO.setToAddress(emaDetailsVO.getToAddress());
-		resultStatus = mailService.sendEmail(emailDetailsVO, emaDetailsVO.getHost());
-		return resultStatus;
+		try
+		{
+			if(emailDetailsVO == null)
+			{
+				resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+				log.error("EmailDetailsVO is Null");
+				return resultStatus;
+			}
+			
+			String subject = ""+emailDetailsVO.getSenderName()+" accepted Friend Request in PartyAnalyst.com."; 
+			String content = "<div style = 'border:1px solid #CCCCCC'>"+mailsTemplateService.getHeader()+"<br/><div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emailDetailsVO.getSenderName()+",</b></div>" +
+					"<div style='margin-left: 45px; margin-bottom: 40px;line-height: 1.5em;'><font style='color:blue'><b>"+emailDetailsVO.getFromAddress()+"</b></font> has accepted your friend request in PartyAnalyst.com.<br/>Do you want to send a message? " +
+							"<a href='http://www.partyanalyst.com/loginInputAction.action'><b> Login Here</b> </a></div></div>";
+			
+			emailDetailsVO.setContent(content);
+			emailDetailsVO.setSubject(subject);
+			resultStatus = mailService.sendEmail(emailDetailsVO, emailDetailsVO.getHost());
+			return resultStatus;
+		}catch (Exception e) {
+			resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+			log.error("Exception Occured in acceptEmailFriendRequest() Method, Exception is  - "+e );
+			return resultStatus;
+		}
 	}
 	
-	public ResultStatus sendMessageToConnectUser(EmailDetailsVO emailDetVO)
+	public ResultStatus sendMessageToConnectUser(EmailDetailsVO emailDetailsVO)
 	{
 		ResultStatus resultStatus = new ResultStatus();
-		EmailDetailsVO emailDetailsVO = new EmailDetailsVO();
-		String subject = ""+emailDetVO.getSenderName()+" send a message in PartyAnalyst.com.";
-		
-		String content = "<div style='border:1px solid #CCCCCC;'>"+mailsTemplateService.getHeader()+"<br/>" +
-				"<div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emailDetVO.getFromAddress()+",</b><br/>" +
-						"</div><div style='margin-left: 45px; margin-bottom: 40px;line-height: 1.5em;'><font style='color:blue;'><b>"+emailDetVO.getSenderName()+"</b></font> has sent a message in PartyAnalyst.com.<br/> Do you want to send the reply? <a href='http://www.partyanalyst.com/loginInputAction.action'>Login Here</a></div></div>";
-		emailDetailsVO.setContent(content);
-		emailDetailsVO.setSubject(subject);
-		emailDetailsVO.setHost(emailDetVO.getHost());
-		emailDetailsVO.setToAddress(emailDetVO.getToAddress());
-		resultStatus = mailService.sendEmail(emailDetailsVO, emailDetVO.getHost());
-		return resultStatus;
+		try
+		{
+			if(emailDetailsVO == null)
+			{
+				resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+				log.error("EmailDetailsVO is Null");
+				return resultStatus;
+			}
+			
+			String subject = ""+emailDetailsVO.getSenderName()+" send a message in PartyAnalyst.com.";
+			String content = "<div style='border:1px solid #CCCCCC;'>"+mailsTemplateService.getHeader()+"<br/>" +
+					"<div style='margin-left:26px;margin-top:20px;margin-bottom: 7px;'><b>Hai "+emailDetailsVO.getFromAddress()+",</b><br/>" +
+							"</div><div style='margin-left: 45px; margin-bottom: 40px;line-height: 1.5em;'><font style='color:blue;'><b>"+emailDetailsVO.getSenderName()+"</b></font> has sent a message in PartyAnalyst.com.<br/> Do you want to send the reply? <a href='http://www.partyanalyst.com/loginInputAction.action'>Login Here</a></div></div>";
+			emailDetailsVO.setContent(content);
+			emailDetailsVO.setSubject(subject);
+			resultStatus = mailService.sendEmail(emailDetailsVO, emailDetailsVO.getHost());
+			return resultStatus;
+			
+		}catch (Exception e) {
+			resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+			log.error("Exception Occured in sendMessageToConnectUser() Method, Exception is  - "+e );
+			return resultStatus;
+		}
 	}
 
 }
