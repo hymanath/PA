d4ad31a43a0a604b1723a6942da016fd294209af
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-03 10:22:51 +0000

    updated by kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SearchEngineIPAddressDAO.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.dao.ISearchEngineIPAddressDAO;
+import com.itgrids.partyanalyst.model.SearchEngineIPAddress;
+
+public class SearchEngineIPAddressDAO extends GenericDaoHibernate<SearchEngineIPAddress,Long> implements ISearchEngineIPAddressDAO{
+
+	public SearchEngineIPAddressDAO()
+	{
+		super(SearchEngineIPAddress.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getAllSearchEngineIPAddresses()
+	{
+		return getHibernateTemplate().find("select distinct model.ipAddress from SearchEngineIPAddress model");
+	}
+}
