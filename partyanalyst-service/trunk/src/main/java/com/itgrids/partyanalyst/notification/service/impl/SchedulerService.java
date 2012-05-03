8edd3bbae78d395c458def895724f7539121422e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-03 10:23:27 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/SchedulerService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/SchedulerService.java
@@ -1,14 +1,13 @@
 package com.itgrids.partyanalyst.notification.service.impl;
 
-import com.itgrids.partyanalyst.dto.EmailDetailsVO;
 import com.itgrids.partyanalyst.notification.service.ISchedulerService;
 import com.itgrids.partyanalyst.service.IMailService;
-import com.itgrids.partyanalyst.utils.IConstants;
 
 public class SchedulerService implements ISchedulerService{
+	
 	private IMailService mailService;
 	
-public IMailService getMailService() {
+	public IMailService getMailService() {
 		return mailService;
 	}
 
