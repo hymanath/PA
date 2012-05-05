160c4e4b28a7b2fe332f9878b53e1737568dbba9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-05 06:45:42 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SearchEngineIPAddressDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SearchEngineIPAddressDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
@@ -20,7 +22,7 @@ public class SearchEngineIPAddressDAOHibernateTest extends BaseDaoTestCase{
 		searchEngineIPAddressDAO.getAll();
 	}*/
 	
-	public void testGetAllSearchEngineIPAddresses()
+	/*public void testGetAllSearchEngineIPAddresses()
 	{
 		List<String> list = searchEngineIPAddressDAO.getAllSearchEngineIPAddresses();
 		
@@ -28,6 +30,22 @@ public class SearchEngineIPAddressDAOHibernateTest extends BaseDaoTestCase{
 		
 		for(String str : list)
 			System.out.println(str);
+	}*/
+	
+	public void testGetAllIPAddress()
+	{
+		List<String> list = searchEngineIPAddressDAO.getAllIPAddress();
+		System.out.println(list.size());
+		
+		for(String str : list)
+		{
+			try {
+				InetAddress addr = InetAddress.getByName(str);
+				System.out.println(str +" --- "+addr.getHostName());
+			} catch (UnknownHostException e) {
+				
+			}
+		}
 	}
 
 }
