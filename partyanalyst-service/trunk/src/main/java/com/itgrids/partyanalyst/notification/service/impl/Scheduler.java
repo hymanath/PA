39d6412d64f8a9bbf274bf1c99202b62694bfd9e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-05 06:45:20 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/Scheduler.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/Scheduler.java
@@ -1,17 +1,35 @@
 package com.itgrids.partyanalyst.notification.service.impl;
 import com.itgrids.partyanalyst.notification.service.ISchedulerService;
+import com.itgrids.partyanalyst.service.IMailsSendingService;
 
 public class Scheduler {
-private ISchedulerService schedulerService;
+	
+	private ISchedulerService schedulerService;
+	private IMailsSendingService mailsSendingService;
+	
+	public IMailsSendingService getMailsSendingService() {
+		return mailsSendingService;
+	}
 
-public ISchedulerService getSchedulerService() {
-	return schedulerService;
-}
+	public void setMailsSendingService(IMailsSendingService mailsSendingService) {
+		this.mailsSendingService = mailsSendingService;
+	}
 
-public void setSchedulerService(ISchedulerService schedulerService) {
-	this.schedulerService = schedulerService;
-}
-public void runningBatchJobMethods(){
-	schedulerService.sendingMails();
-}
+	public ISchedulerService getSchedulerService() {
+		return schedulerService;
+	}
+	
+	public void setSchedulerService(ISchedulerService schedulerService) {
+		this.schedulerService = schedulerService;
+	}
+	
+	public void runTheBatchJobForEveryDay()
+	{
+		schedulerService.deleteSearchEngineAccessedURLsFromUserTracking();
+	}
+	
+	public void runTheBatchJobForEveryWeek()
+	{
+		mailsSendingService.sendMailsToPasswordnotUpdatedusers();
+	}
 }
