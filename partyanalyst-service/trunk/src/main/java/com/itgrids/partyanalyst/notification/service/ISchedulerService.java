e441989edb5ac55fc3c7d3dc3749cb247d7ebe92
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-05 06:45:08 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/ISchedulerService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/ISchedulerService.java
@@ -1,5 +1,12 @@
 package com.itgrids.partyanalyst.notification.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.ResultStatus;
+
 public interface ISchedulerService {
-	public void sendingMails();
+	
+	public ResultStatus deleteSearchEngineAccessedURLsFromUserTracking();
+	
+	public List<String> getAllSearchEngineIPAddresses();
 }
