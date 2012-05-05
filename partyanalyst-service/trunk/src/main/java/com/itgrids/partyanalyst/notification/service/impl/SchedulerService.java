5f849951b949d135da202d1b5e256042c851418f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-05 06:45:20 +0000

    updated by kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/SchedulerService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/notification/service/impl/SchedulerService.java
@@ -1,11 +1,42 @@
 package com.itgrids.partyanalyst.notification.service.impl;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.dao.ISearchEngineIPAddressDAO;
+import com.itgrids.partyanalyst.dao.IUserTrackingDAO;
+import com.itgrids.partyanalyst.dto.ResultCodeMapper;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.notification.service.ISchedulerService;
 import com.itgrids.partyanalyst.service.IMailService;
+import com.itgrids.partyanalyst.utils.DateUtilService;
 
 public class SchedulerService implements ISchedulerService{
 	
 	private IMailService mailService;
+	private final static Logger log = Logger.getLogger(SchedulerService.class);
+	
+	private ISearchEngineIPAddressDAO searchEngineIPAddressDAO;
+	private IUserTrackingDAO userTrackingDAO;
+	private DateUtilService dateUtilService = new DateUtilService();
+	
+	public IUserTrackingDAO getUserTrackingDAO() {
+		return userTrackingDAO;
+	}
+
+	public void setUserTrackingDAO(IUserTrackingDAO userTrackingDAO) {
+		this.userTrackingDAO = userTrackingDAO;
+	}
+
+	public ISearchEngineIPAddressDAO getSearchEngineIPAddressDAO() {
+		return searchEngineIPAddressDAO;
+	}
+
+	public void setSearchEngineIPAddressDAO(
+			ISearchEngineIPAddressDAO searchEngineIPAddressDAO) {
+		this.searchEngineIPAddressDAO = searchEngineIPAddressDAO;
+	}
 	
 	public IMailService getMailService() {
 		return mailService;
@@ -15,8 +46,33 @@ public class SchedulerService implements ISchedulerService{
 		this.mailService = mailService;
 	}
 
-	public void sendingMails()
+	public ResultStatus deleteSearchEngineAccessedURLsFromUserTracking()
 	{
-		
+		ResultStatus resultStatus = new ResultStatus();
+		try{
+			List<String> ipAddressList = getAllSearchEngineIPAddresses();
+			
+			if(ipAddressList != null && ipAddressList.size() > 0)
+			{
+				int deletedRecords = userTrackingDAO.deleteSearchEngineAccessedURLsFromUserTracking(ipAddressList,dateUtilService.getCurrentDateAndTime(),dateUtilService.getCurrentDateAndTime());
+				log.info(deletedRecords+" No of Records Deleted from UserTracking");
+			}
+			return resultStatus;
+		}catch (Exception e) {
+			log.error("Exception Occured in deleteSearchEngineAccessedURLsFromUserTracking() Method, Exception is - "+e);
+			resultStatus.setResultCode(ResultCodeMapper.FAILURE);
+			return resultStatus;
+		}
+	}
+	
+	public List<String> getAllSearchEngineIPAddresses()
+	{
+		log.debug("Entered into getAllSearchEngineIPAddresses() Method");
+		try{
+			return searchEngineIPAddressDAO.getAllSearchEngineIPAddresses();
+		}catch (Exception e) {
+			log.error("Exception Occured in getAllSearchEngineIPAddresses() Method, Exception is - "+e);
+			return null;
+		}
 	}
 }
