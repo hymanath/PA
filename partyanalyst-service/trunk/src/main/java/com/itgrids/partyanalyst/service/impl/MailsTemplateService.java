1e0b3b8f6a4d28b05928cb7e2629d63d08e0bcd5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-10 13:09:12 +0000

    Updated By AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/MailsTemplateService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/MailsTemplateService.java
@@ -7,7 +7,7 @@ public class MailsTemplateService implements IMailsTemplateService{
 	public String getHeader(){
 		
 		
-		String header = "<img src='http://www.partyanalyst.com/PartyAnalyst/images/icons/homePage_new/homePage_header_beta2.jpg' width='600px' height='100px'/>";
+		String header = "<img src='http://www.partyanalyst.com/PartyAnalyst/images/icons/homePage_new/homePage_header_beta2.jpg' height='100px' width='100%'/>";
 		
 		return header;
 	}
