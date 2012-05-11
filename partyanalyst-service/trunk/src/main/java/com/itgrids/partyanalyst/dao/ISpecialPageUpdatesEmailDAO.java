09c0304bc119ae231eb3aa1288ed826a24226676
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-11 12:30:20 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageUpdatesEmailDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpecialPageUpdatesEmailDAO.java
@@ -7,5 +7,7 @@ import com.itgrids.partyanalyst.model.SpecialPageUpdatesEmail;
 public interface ISpecialPageUpdatesEmailDAO extends GenericDao<SpecialPageUpdatesEmail, Long>{
 	
 	public List<Object> getSpecialPageUpdatesEmail(String emailId ,Long specialPageId);
+	
+	public List<SpecialPageUpdatesEmail> getAllSubscriberDetails();
 
 }
