daa38115688bedb1f40369cf6279a0f88589ab33
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-11 12:29:28 +0000

    Created by Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyCandidateSpecialPageScheduleService.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.Date;
+
+public interface IPartyCandidateSpecialPageScheduleService {
+
+	public void sendMailsToAllSubscriders(Date startDate,Date endDate);
+	
+}
