d86ccca2a089d68447ceb31fa2af7c54df868ba0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-11 12:19:24 +0000

    Updated by Mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyUpdatesEmailDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyUpdatesEmailDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import com.itgrids.partyanalyst.dao.IPartyUpdatesEmailDAO;
 import com.itgrids.partyanalyst.model.PartyUpdatesEmail;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class PartyUpdatesEmailDAO  extends GenericDaoHibernate<PartyUpdatesEmail,Long> implements IPartyUpdatesEmailDAO{
 
@@ -12,10 +13,18 @@ public PartyUpdatesEmailDAO()
 	super(PartyUpdatesEmail.class);
 }
 
+@SuppressWarnings("unchecked")
 public List<Object> getPartyUpdatesEmail(String emailId, Long partyId) 
 {
 	Object[] params = {partyId,emailId};
 	return getHibernateTemplate().find("select model from PartyUpdatesEmail model where model.party.partyId = ? and model.email = ?",params);
+}
+
+@SuppressWarnings("unchecked")
+public List<PartyUpdatesEmail> getAllSubscriberDetails() 
+{
+
+	return getHibernateTemplate().find("select model from PartyUpdatesEmail model where model.unsubscribed = ?",IConstants.FALSE);
 }
 
 }
