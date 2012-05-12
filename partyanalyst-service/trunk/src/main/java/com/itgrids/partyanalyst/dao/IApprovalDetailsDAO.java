77c4031c41b494abd520dde32600ddb49cf9e8ae
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-12 06:51:27 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IApprovalDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IApprovalDetailsDAO.java
@@ -9,6 +9,7 @@ import com.itgrids.partyanalyst.model.ApprovalDetails;
 public interface IApprovalDetailsDAO extends GenericDao<ApprovalDetails, Long> {
 
 	public  Integer updateSetIsApprovedStatusToPostedProblems(List<Long> approvalDetailsIds, String isApproved);
+	public Long getCountOfNewlyPostedCommentsByUser();
 
 	
 	
