a27d1853f300c997963ad7d0307b13ae9808bfb4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-19 05:42:50 +0000

    Updated by Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.FilePath;
+
+public interface IFilePathDAO extends GenericDao<FilePath, Long> {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFileSourceLanguageDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.FileSourceLanguage;
+
+public interface IFileSourceLanguageDAO extends GenericDao<FileSourceLanguage, Long>  {
+
+}
