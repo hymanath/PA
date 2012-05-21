3f086856186c72ef758183433de03ee855ad0c3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-21 05:32:53 +0000

    Updated by Mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.FilePaths;
+
+public interface IFilePathsDAO extends GenericDao<FilePaths, Long> {
+
+}
