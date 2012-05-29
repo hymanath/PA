f79350032f227b7b9ed70fd9b533d9c3c76b1bde
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-29 15:02:54 +0000

    For File Uploading -- Swathi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SourceLanguageDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SourceLanguageDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ISourceLanguageDAO;
@@ -11,9 +13,14 @@ public class SourceLanguageDAOHibernateTest extends BaseDaoTestCase {
 	public void setSourceLanguageDAO(ISourceLanguageDAO sourceLanguageDAO) {
 		this.sourceLanguageDAO = sourceLanguageDAO;
 	}
-	public void test()
+	/*public void test()
 	{
 		sourceLanguageDAO.getAll();
 		
+	}*/
+	public void testGetLanguageIdByLanguage()
+	{
+		List<Object> list = sourceLanguageDAO.getLanguageIdByLanguage("No Language");
+		System.out.println(list.get(0));
 	}
 }
