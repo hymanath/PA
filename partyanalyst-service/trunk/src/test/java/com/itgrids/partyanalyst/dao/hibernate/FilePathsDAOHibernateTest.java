a86d85b40818fe51a6792338e2c9b2a976bcca0a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-29 15:02:01 +0000

    For File Uploading -- Swathi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IFilePathsDAO;
@@ -12,7 +14,13 @@ public class FilePathsDAOHibernateTest extends BaseDaoTestCase {
 		this.filePathsDAO = filePathsDAO;
 	}
 	
-	public void test(){
+	/*public void test(){
+		
 		filePathsDAO.getAll();
-	 }
+	 }*/
+	public void testGetMaxOrderNo()
+	{
+		List<Object> list = filePathsDAO.getMaxOrderNo();
+		System.out.println(list.size());
+	}
 }
