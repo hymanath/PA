f7743eaf612fbd27da66b9a18fa6ff6b2b9074b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-29 14:55:21 +0000

    For File Uploading -- Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceLanguageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceLanguageDAO.java
@@ -7,6 +7,8 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.SourceLanguage;
 
 public interface ISourceLanguageDAO extends GenericDao<SourceLanguage, Long> {
+	
 	public List<Object[]> getSourceLanguageDetails();
 	
+	public List<Object> getLanguageIdByLanguage(String language);
 }
