ea44c7699984a9db53529e8a3383ee4f39dab951
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-29 14:55:00 +0000

    For File Uploading -- Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceDAO.java
@@ -7,5 +7,8 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.Source;
 
 public interface ISourceDAO extends GenericDao<Source, Long>{
+	
 	public List<Object[]> getSourceDetails();
+	
+	public List<Object> getSourceIdBySource(String source);
 }
