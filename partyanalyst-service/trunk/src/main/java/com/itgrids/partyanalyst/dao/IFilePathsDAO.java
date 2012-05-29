725c0fe1e7cb2d508fa612f59d84fd2f3e8e3c6c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-05-29 14:54:16 +0000

    For File Uploading -- Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathsDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.FilePaths;
 
 public interface IFilePathsDAO extends GenericDao<FilePaths, Long> {
 
+	public List<Object> getMaxOrderNo();
 }
