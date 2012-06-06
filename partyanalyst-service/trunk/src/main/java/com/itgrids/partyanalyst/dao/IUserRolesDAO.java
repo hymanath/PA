fa2f0d4ee7e580a19421d12e3626b701bf5933b0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-06 07:40:26 +0000

    Updated by Anuradha-kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserRolesDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.UserRoles;
+
+public interface IUserRolesDAO extends GenericDao<UserRoles,Long>{
+
+}
