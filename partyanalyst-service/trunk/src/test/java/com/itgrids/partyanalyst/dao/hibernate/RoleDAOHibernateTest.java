aea676ecba380886a284d090d4ebbd44286bf159
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-07 09:15:33 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/RoleDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/RoleDAOHibernateTest.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IRoleDAO;
+import com.itgrids.partyanalyst.model.Role;
 
 public class RoleDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -11,10 +14,25 @@ public class RoleDAOHibernateTest extends BaseDaoTestCase{
 	public void setRoleDAO(IRoleDAO roleDAO) {
 		this.roleDAO = roleDAO;
 	}
-
+	public IRoleDAO getRoleDAO() {
+		return roleDAO;
+	}
 	public void test()
 	{
 		roleDAO.getAll();
 	}
+	
+	public void testGetRoleType()
+	{
+		List<Role> role = roleDAO.getRoleType();
+		for(Role roles : role)
+		{
+			System.out.println(roles.getRoleType().toString());
+			
+		}
+		
+	}
+
+	
 
 }
