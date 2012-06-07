d55daa902f40b475681b00c3e22132d15c0687bf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-07 14:00:11 +0000

    Uploaded by Swathi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserRolesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserRolesDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IUserRolesDAO;
@@ -11,4 +13,9 @@ public class UserRolesDAO extends GenericDaoHibernate<UserRoles,Long> implements
 	{
 		super(UserRoles.class);
 	}
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getUserRoles(Long userId) {
+		return getHibernateTemplate().find("select model.role.roleId , model.role.roleType from UserRoles model where model.user.registrationId =?",userId);
+	}
+	
 }
