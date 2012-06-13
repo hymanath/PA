f39ace97dc0fffc16ee060fdc747bce2fb6a5626
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-13 13:03:35 +0000

    Updated by Anuradha

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserDAOHibernateTest.java
@@ -1,8 +1,14 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IUserDAO;
+import com.itgrids.partyanalyst.model.User;
+import com.itgrids.partyanalyst.utils.IConstants;
+
 
 public class UserDAOHibernateTest extends BaseDaoTestCase{
 
@@ -11,9 +17,33 @@ public class UserDAOHibernateTest extends BaseDaoTestCase{
 	public void setUserDAO(IUserDAO userDAO) {
 		this.userDAO = userDAO;
 	}
-	
+
 	public void test()
 	{
 		userDAO.getAll();
 	}
+	
+	/*public void testCheckUserPassword()
+	{
+		List<User> list = userDAO.checkUserPassword("7258695", 12l);
+		System.out.println(list.size());
+	}*/
+	
+	/*public void testChangeUserPassword()
+	{
+	Integer result = userDAO.changeUserPassword("7258695", 12l, IConstants.TRUE, new Date());
+	System.out.println(result);
+	}*/
+	
+	/*public void testGetUserIdByUserName()
+	{
+		List<Object> list = userDAO.getUserIdByUserName("Anuradha@aa.com");
+		System.out.println(list.get(0));
+	}*/
+	
+	/*public void testFindByUserNameAndPassword()
+	{
+		List<User> list = userDAO.findByUserNameAndPassword("aaaa@aa.com", "3685171");
+		System.out.println(list.size());
+	}*/
 }
