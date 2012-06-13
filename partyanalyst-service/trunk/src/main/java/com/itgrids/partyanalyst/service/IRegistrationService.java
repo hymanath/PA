0819e2aef25cf92ae93af13044b90879eef2961b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-13 12:41:49 +0000

    Updated by Anuradha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IRegistrationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IRegistrationService.java
@@ -1,10 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
 import java.util.List;
+
 import com.itgrids.partyanalyst.dto.EntitlementVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
-import com.itgrids.partyanalyst.model.Registration;
+import com.itgrids.partyanalyst.model.User;
 
 public interface IRegistrationService {
 
@@ -22,5 +23,5 @@ public interface IRegistrationService {
 	 
 	 public List<String> getRoleTypeForUser();
 	 
-	 public ResultStatus saveDataInToUserRolesTable(Registration registration,RegistrationVO values); 
+	 public ResultStatus saveDataInToUserRolesTable(User user,RegistrationVO values);
 }
