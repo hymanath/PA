99ee172992352ada82d5d4f5f6d546b70412a7d1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-13 12:38:36 +0000

    Updated by Anuradha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/UserCategory.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/UserCategory.java
@@ -1,16 +1,20 @@
 package com.itgrids.partyanalyst.model;
 
 import java.io.Serializable;
-
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "user_category")
@@ -20,6 +24,7 @@ public class UserCategory extends BaseModel implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Long userCatagoryId;
 	private String userCatagory;
+	private Set<User> users = new HashSet<User>(0);
 	
 	public UserCategory(){
 		
@@ -51,5 +56,15 @@ public class UserCategory extends BaseModel implements Serializable{
 	
 	public void setUserCatagory(String userCatagory) {
 		this.userCatagory = userCatagory;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "userCategory")
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
 	}	 
 }
