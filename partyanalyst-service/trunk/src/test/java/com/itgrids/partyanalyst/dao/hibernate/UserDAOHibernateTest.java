278bdc5b4fbf2a7557adf0f1cb4253414327d4a7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-18 14:27:08 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserDAOHibernateTest.java
@@ -1,14 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
 import java.util.Date;
+
 import java.util.List;
-
 import org.appfuse.dao.BaseDaoTestCase;
-
 import com.itgrids.partyanalyst.dao.IUserDAO;
-import com.itgrids.partyanalyst.model.User;
-import com.itgrids.partyanalyst.utils.IConstants;
-
 
 public class UserDAOHibernateTest extends BaseDaoTestCase{
 
@@ -41,9 +38,30 @@ public class UserDAOHibernateTest extends BaseDaoTestCase{
 		System.out.println(list.get(0));
 	}*/
 	
-	public void testFindByUserNameAndPassword()
+	/*public void testFindByUserNameAndPassword()
 	{
 		User user = userDAO.findByUserNameAndPassword("aaaa@aa.com", "3685171");
 		System.out.println(user.getUserName());
+	}*/
+	
+	/*public void test()
+	{
+		System.out.println(userDAO.getUserProfileImageNameByUserId(1l));
+	}*/
+	
+	public void testGetUserLocationDetailsByUserIds()
+	{
+		List<Long> userIds = new ArrayList<Long>(0);
+		userIds.add(1l);
+		
+		List<Object[]> list = userDAO.getUserLocationDetailsByUserIds(userIds);
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println();
+			for(Object obj : params)
+			System.out.print("----\t"+obj.toString());
+		}
 	}
 }
