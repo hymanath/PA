778076b13057d5272cd8d52330e2338f3278726a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-21 03:48:48 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserGroupPrivilegesDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserGroupPrivilegesDAOHibernateTest.java
@@ -3,7 +3,6 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
-import org.junit.Test;
 
 import com.itgrids.partyanalyst.dao.IUserGroupPrivilegesDAO;
 import com.itgrids.partyanalyst.model.UserGroupPrivileges;
@@ -14,7 +13,6 @@ public class UserGroupPrivilegesDAOHibernateTest extends BaseDaoTestCase {
 
 	
 	public IUserGroupPrivilegesDAO getUserGroupPrivileges() {
-		 System.out.println("66666666666666666666666666666666666");
 		return userGroupPrivilegesDAO;
 	}
    
@@ -25,14 +23,7 @@ public class UserGroupPrivilegesDAOHibernateTest extends BaseDaoTestCase {
    
 	public void testFindByUserGroupPrivilegeId()
 	{
-		System.out.println("wwwwwwwwwwwwwwwwww");
-    List<UserGroupPrivileges>result=userGroupPrivilegesDAO.findByUserGroupPrivilegeId(401L);
-    for(UserGroupPrivileges userPrivilage:result)
-    {
-    System.out.println("read write user " +userPrivilage.getReadRightUserId().toString());
-    System.out.println("write write user" +userPrivilage.getWriteRightUserId().toString());
-    System.out.println("personal user group" +userPrivilage.getPersonalUserGroup().toString());
-    }
-    assertEquals(2,result.size());
+		List<UserGroupPrivileges> result = userGroupPrivilegesDAO.findByUserGroupPrivilegeId(401L);
+		assertEquals(2,result.size());
 	}
 }
