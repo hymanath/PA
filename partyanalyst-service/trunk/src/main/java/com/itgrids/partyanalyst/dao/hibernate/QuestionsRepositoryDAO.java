d58ecb8fea167087226db917dde92e381da3be03
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-28 17:27:07 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/QuestionsRepositoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/QuestionsRepositoryDAO.java
@@ -1,25 +1,8 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.criterion.Expression;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.orm.hibernate3.HibernateCallback;
-
-import com.itgrids.partyanalyst.model.Registration;
-import com.itgrids.partyanalyst.model.OpinionPollQuestions;
-import com.itgrids.partyanalyst.model.OpinionPollQuestionOptions;
-import com.itgrids.partyanalyst.model.OpinionPollResult;
-import com.itgrids.partyanalyst.model.OpinionPoll;
-import com.itgrids.partyanalyst.model.QuestionsRepository;
-
 import com.itgrids.partyanalyst.dao.IQuestionsRepositoryDAO;
+import com.itgrids.partyanalyst.model.QuestionsRepository;
 
 public class QuestionsRepositoryDAO extends GenericDaoHibernate<QuestionsRepository, Long> implements IQuestionsRepositoryDAO {
 
