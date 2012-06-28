7da7ca0503f4e6d404671edf99ce6ef7d7b5aef4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-28 17:27:36 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserEventsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserEventsDAO.java
@@ -38,21 +38,6 @@ public class UserEventsDAO extends GenericDaoHibernate<UserEvents, Long> impleme
 		
 		Object[] params = {"NO",userId, new SimpleDateFormat("yyyy-MM-dd").format(startDate)};
 		return getHibernateTemplate().find("from UserEvents model where model.isDeleted=? and model.user.userId = ? and DATEDIFF(model.startDate, ?) = 0", params) ;
-		/*Query query = getSession().createQuery("SELECT  ue.user_event_id, ue.registration.registarion_id, ue.description, ue.location_type, ue.location_id, ue.start_date, ue.end_date, ue.title, ue.is_deleted, " +
-												"CASE " +
-												"WHEN ue.location_type='MANDAL' " +
-												"THEN (select tehsil_name from tehsil where tehsil_id = ue.location_id) " +
-												"WHEN ue.location_type ='DISTRICT' " +
-												"THEN select district_name from district where district_id = ue.location_id) " +
-												"END location " +
-												"FROM user_event ue " +
-												"WHERE ue.user_id = r.registration_id " +
-												"AND r.registration_id = :userId " +
-												"AND is_deleted = 'NO' " +
-												"AND DATEDIFF(ue.startDate, :date) = 0");
-		query.setLong("userId", userId);
-		query.setString("date", new SimpleDateFormat("yyyy-MM-dd").format(startDate));
-		return (List<UserEvents>) query.list();*/
-										
+											
 	}
 }
