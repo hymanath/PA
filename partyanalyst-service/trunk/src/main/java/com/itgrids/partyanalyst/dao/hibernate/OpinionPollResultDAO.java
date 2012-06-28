023bce79f44f43864e3a04896c0414c424b7b96f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-28 17:26:58 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/OpinionPollResultDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/OpinionPollResultDAO.java
@@ -1,25 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.criterion.Expression;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.orm.hibernate3.HibernateCallback;
-
-import com.itgrids.partyanalyst.model.Registration;
-import com.itgrids.partyanalyst.model.OpinionPollQuestions;
-import com.itgrids.partyanalyst.model.OpinionPollQuestionOptions;
-import com.itgrids.partyanalyst.model.OpinionPollResult;
-import com.itgrids.partyanalyst.model.OpinionPoll;
-import com.itgrids.partyanalyst.model.QuestionsRepository;
-
 import com.itgrids.partyanalyst.dao.IOpinionPollResultDAO;
+import com.itgrids.partyanalyst.model.OpinionPollResult;
 
 public class OpinionPollResultDAO extends GenericDaoHibernate<OpinionPollResult, Long> implements IOpinionPollResultDAO {
 
