ad6302be89dc71118f3a2864fd5fdfdc84aaff83
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-28 17:27:22 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserApprovalDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserApprovalDetailsDAO.java
@@ -2,18 +2,10 @@ package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Calendar;
-//import java.text.SimpleDateFormat;
-
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
-
 import com.itgrids.partyanalyst.dao.IUserApprovalDetailsDAO;
-import com.itgrids.partyanalyst.model.Cadre;
 import com.itgrids.partyanalyst.model.UserApprovalDetails;
-import com.itgrids.partyanalyst.model.ApprovalDetails;
-import com.itgrids.partyanalyst.model.AnanymousUser;
-import com.itgrids.partyanalyst.model.UserProblemApproval;
 
 
 @SuppressWarnings("unused")
