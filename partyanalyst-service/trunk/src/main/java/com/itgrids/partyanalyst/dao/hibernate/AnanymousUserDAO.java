c6c2b31fb80df869062e79e133aa269db6b386e5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-06-28 17:26:16 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AnanymousUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AnanymousUserDAO.java
@@ -1,19 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
-
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
 import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.criterion.Expression;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.orm.hibernate3.HibernateCallback;
-
 import com.itgrids.partyanalyst.dao.IAnanymousUserDAO;
 import com.itgrids.partyanalyst.model.AnanymousUser;
 import com.itgrids.partyanalyst.utils.IConstants;
