47ab9759a2874cb9d296091a9ee5c46cc3e0d80f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:50:49 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLikesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemLikesDAO.java
@@ -1,8 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.ProblemLikes;
 
 public interface IProblemLikesDAO extends GenericDao<ProblemLikes,Long>{
+	
+	public List<Long> getAllLikes(Long problemId);
+	public List<Long> getAllDisLikes(Long problemId);
 
 }
