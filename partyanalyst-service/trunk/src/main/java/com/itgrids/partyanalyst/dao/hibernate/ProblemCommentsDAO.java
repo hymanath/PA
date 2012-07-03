cf64d4885b68e7be72f23f3cbfa121e13b9a71c9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:51:44 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemCommentsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemCommentsDAO.java
@@ -1,13 +1,21 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import com.itgrids.partyanalyst.dao.IProblemCommentsDAO;
 import com.itgrids.partyanalyst.model.ProblemComments;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class ProblemCommentsDAO extends GenericDaoHibernate<ProblemComments,Long> implements IProblemCommentsDAO{
 
 	public ProblemCommentsDAO()
 	{
 		super(ProblemComments.class);
+	}
+	
+	public List<Long> getAllApprovedComments(Long problemId)
+	{
+		return getHibernateTemplate().find("select count(model.problemCommentsId) from ProblemComments model where model.isApproved ='"+IConstants.TRUE+"' and model.isDelete = null and model.problem.problemId = ?",problemId);
 	}
 }
