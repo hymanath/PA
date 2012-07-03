35b2e997921406d1dbff17304031a01c13beab3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:53:45 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserProblemDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserProblemDAOHibernateTest.java
@@ -1,7 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 import com.itgrids.partyanalyst.dao.IUserProblemDAO;
+import com.itgrids.partyanalyst.model.UserProblem;
 
 public class UserProblemDAOHibernateTest extends BaseDaoTestCase{
 
@@ -15,4 +19,37 @@ public class UserProblemDAOHibernateTest extends BaseDaoTestCase{
 	{
 		userProblemDAO.getAll();
 	}
-}
+	public void testgetAllValidProblemIds()
+	{
+		
+		List<Long> list = userProblemDAO.getAllValidProblemIds(1, 3);
+		System.out.println(list.size());
+		
+		
+	}
+	
+	/*public void testgetAllValidProblemsCount()
+	{
+		List<Long> list = userProblemDAO.getAllValidProblemIdsCount();
+		System.out.println(list.size());
+	}*/
+	
+/*public void testgetProblemCompleteInfo()
+	{
+		List<Object[]> list = userProblemDAO.getProblemCompleteInfo(1l);
+		for(Object[] params : list)
+		{
+			System.out.println(params[0]);
+			System.out.println(params[1].toString());
+		}
+	}*/
+	
+	/*public void testgetAllPRoblemDetails()
+	{
+		List<UserProblem> userproblem = userProblemDAO.getAllProblemDetails(1l);
+		for(UserProblem list : userproblem)
+		{
+			System.out.println(list.getProblem().getTitle());
+		}
+	}
+*/}
