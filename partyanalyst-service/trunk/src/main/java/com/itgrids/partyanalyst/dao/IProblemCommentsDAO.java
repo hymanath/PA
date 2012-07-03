2f9cf84c4ec645c3fada7db4ac7c43e0f5313d77
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:50:18 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemCommentsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemCommentsDAO.java
@@ -1,8 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
+
+
 import com.itgrids.partyanalyst.model.ProblemComments;
 
 public interface IProblemCommentsDAO extends GenericDao<ProblemComments,Long>{
+	
+	public List<Long> getAllApprovedComments(Long problemId);
 
 }
