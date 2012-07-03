e8d1b4c9a1cbfcacb9ea144739f23a286292f6da
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:51:59 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemLikesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemLikesDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IProblemLikesDAO;
 import com.itgrids.partyanalyst.model.ProblemLikes;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class ProblemLikesDAO extends GenericDaoHibernate<ProblemLikes,Long> implements IProblemLikesDAO{
 
@@ -11,4 +14,14 @@ public class ProblemLikesDAO extends GenericDaoHibernate<ProblemLikes,Long> impl
 	{
 		super(ProblemLikes.class);
 	}
+	
+	public List<Long> getAllLikes(Long problemId)
+	{
+		return getHibernateTemplate().find("select count(model.problemLikesId) from ProblemLikes model where model.problem.problemId = ? and model.isLiked = '"+IConstants.TRUE+"' ",problemId);	
+	}
+	public List<Long> getAllDisLikes(Long problemId)
+	{
+		return getHibernateTemplate().find("select count(model.problemLikesId) from ProblemLikes model where model.problem.problemId = ? and model.isLiked = '"+IConstants.FALSE+"' ",problemId);	
+	}
+	
 }
