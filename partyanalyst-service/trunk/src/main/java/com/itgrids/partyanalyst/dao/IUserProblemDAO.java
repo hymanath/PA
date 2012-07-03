75eef98b5ed0a403b3dff3ca82e997ca0799d73f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-03 05:51:07 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserProblemDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserProblemDAO.java
@@ -1,8 +1,20 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
+
+
 import com.itgrids.partyanalyst.model.UserProblem;
 
 public interface IUserProblemDAO extends GenericDao<UserProblem,Long>{
+	
+	public List<Long> getAllValidProblemIds(Integer startIndex, Integer maxIndex);
+	
+	public List<Long> getAllValidProblemIdsCount();
+	
+	public List<Object[]> getProblemCompleteInfo(Long problemId);
+	
+	public List<UserProblem> getAllProblemDetails(Long problemId);
 
 }
