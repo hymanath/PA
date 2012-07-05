f2f5fd1b6cb614633615619378cebaad385e2e22
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-05 13:38:17 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ProblemBeanVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ProblemBeanVO.java
@@ -103,7 +103,16 @@ public class ProblemBeanVO extends ResultStatus implements Serializable{
     private String problemVisibility;
     private Boolean hasFreeUserRole;
 	private Boolean hasPartyAnalystUserRole;
+	private String referenceNo;
 	
+	public String getReferenceNo() {
+		return referenceNo;
+	}
+
+	public void setReferenceNo(String referenceNo) {
+		this.referenceNo = referenceNo;
+	}
+
 	public String getIdentifiedOn() {
 		return identifiedOn;
 	}
