eb6b1ccc6a358dd77e128418dac9565a65b7f9f9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-07 08:08:14 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemCommentsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemCommentsDAOHibernateTest.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IProblemCommentsDAO;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class ProblemCommentsDAOHibernateTest extends BaseDaoTestCase{
 
@@ -12,8 +16,29 @@ public class ProblemCommentsDAOHibernateTest extends BaseDaoTestCase{
 		this.problemCommentsDAO = problemCommentsDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		problemCommentsDAO.getAll();
+	}*/
+	
+	
+	public void testfindUserApprovalStatusbetweendates()throws Exception
+	{  
+		SimpleDateFormat sdf = new SimpleDateFormat(IConstants.DATE_PATTERN_YYYY_MM_DD);
+					
+		System.out.println("details");
+		List<Object[]> list = problemCommentsDAO. findUserApprovalStatusbetweendates(sdf.parse("2012/6/30"),
+				sdf.parse("2012/6/30"));				
+			System.out.println("size"+list.size());
+		for(Object[] obj : list)
+		{
+			System.out.println("1"+obj[0]);
+			System.out.println("2"+obj[1]);
+			System.out.println("3"+obj[2]);
+			System.out.println("4"+obj[3]);
+			//System.out.println("5"+obj[4]);		
+			
+		}
 	}
+
 }
