f5a044971be287f81051134b5ae73caf1d0ca05c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-07 08:01:23 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFilesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFilesDAO.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.ProblemFiles;
 
 public interface IProblemFilesDAO extends GenericDao<ProblemFiles,Long>{
+	public List<Object[]> getCurrentDateFiles(Date currentDate,Date endDate,String isApproved);
 
 }
