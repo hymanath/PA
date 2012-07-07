f85f923d5a1fd482ce6f8cf915604b3147996791
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-07 08:00:21 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemAssignedDepartmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemAssignedDepartmentDAO.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.ProblemAssignedDepartment;
 
 public interface IProblemAssignedDepartmentDAO extends GenericDao<ProblemAssignedDepartment,Long>{
+	
+	public List<ProblemAssignedDepartment> getAllActivitesByProblem(Long problemId);
 
 }
