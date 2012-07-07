ac818979422a82e8e7206ddfcfed7e658ae1dc96
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-07 08:03:57 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFilesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemFilesDAO.java
@@ -1,6 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
 import com.itgrids.partyanalyst.dao.IProblemFilesDAO;
 import com.itgrids.partyanalyst.model.ProblemFiles;
 
@@ -9,5 +14,28 @@ public class ProblemFilesDAO extends GenericDaoHibernate<ProblemFiles,Long> impl
 	public ProblemFilesDAO()
 	{
 		super(ProblemFiles.class);
+	}
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getCurrentDateFiles(Date currentDate,Date endDate,String isApproved)
+	{
+		StringBuilder query = new StringBuilder();
+		query.append(" select model.problemFilesId,model.file.fileTitle,model.file.fileDescription,model.problem.title,model.isApproved,date(model.problem.existingFrom),date(model.problem.identifiedOn),model.user.firstName,model.user.lastName");
+		query.append(" from ProblemFiles model where model.isDelete =:isDelete and model.problem.isDelete =:isDelete ");
+		if(currentDate != null)
+			query.append(" and date(model.insertedTime) >=:currentDate");
+		 if(endDate != null)
+			query.append(" and date(model.insertedTime) <=:endDate");
+		 if(isApproved != null)
+			query.append(" and  model.isApproved=:isApproved");
+		Query queryObject = getSession().createQuery(query.toString());
+		if(currentDate != null)
+			queryObject.setParameter("currentDate", currentDate);
+		 if(endDate != null)
+			queryObject.setParameter("endDate", endDate);
+		 if(isApproved != null)
+			queryObject.setParameter("isApproved", isApproved);
+		
+		queryObject.setParameter("isDelete", "false");
+		return queryObject.list();
 	}
 }
