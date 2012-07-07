ad5c119976d232e3fe4becc5b40fae9ad3a72b52
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-07 08:02:43 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedDepartmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedDepartmentDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
 import com.itgrids.partyanalyst.dao.IProblemAssignedDepartmentDAO;
 import com.itgrids.partyanalyst.model.ProblemAssignedDepartment;
 
@@ -10,5 +13,11 @@ public class ProblemAssignedDepartmentDAO extends GenericDaoHibernate<ProblemAss
 	public ProblemAssignedDepartmentDAO()
 	{
 		super(ProblemAssignedDepartment.class);
+	}
+	@SuppressWarnings("unchecked")
+	public List<ProblemAssignedDepartment> getAllActivitesByProblem(Long userProblemId)
+	{
+		return getHibernateTemplate().find("from ProblemAssignedDepartment model where model.userProblem.userProblemId = ? ",userProblemId);
+				
 	}
 }
