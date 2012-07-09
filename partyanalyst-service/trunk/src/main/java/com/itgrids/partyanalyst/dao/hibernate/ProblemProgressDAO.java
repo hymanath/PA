31ab6c7d0e41b0aaf5f8ed07bad9cbdd4262684f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-09 16:10:33 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemProgressDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ProblemProgressDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
 import com.itgrids.partyanalyst.dao.IProblemProgressDAO;
 import com.itgrids.partyanalyst.model.AssignedProblemProgress;
 import com.itgrids.partyanalyst.model.ProblemProgress;
@@ -14,4 +15,10 @@ public class ProblemProgressDAO extends GenericDaoHibernate<ProblemProgress,Long
 		super(ProblemProgress.class);
 	}
 	
+	
+	@SuppressWarnings("unchecked")
+	public List<ProblemProgress>  getProblemPrograss(Long userProblemId)
+	{
+		return getHibernateTemplate().find("from ProblemProgress model where model.userProblem.userProblemId = ? ",userProblemId);
+	}	
 }
