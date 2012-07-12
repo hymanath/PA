23d75a408876b3212f49584fe25337b57261b7f2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 14:38:54 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IClassifiedProblemsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IClassifiedProblemsDAO.java
@@ -8,4 +8,5 @@ import com.itgrids.partyanalyst.model.ClassifiedProblems;
 
 public interface IClassifiedProblemsDAO extends GenericDao<ClassifiedProblems,Long>{
 	public List<Long> checkIfProblemAlreadyClassified(Long userProblemId);
+	public List<ClassifiedProblems> getClassifiedproblemByUserProblemId(Long userProblemId);
 }
