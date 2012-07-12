1da2327d98ae2860e26e686050cc0bbdd06fe51d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 14:38:00 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFilesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemFilesDAO.java
@@ -8,7 +8,9 @@ import com.itgrids.partyanalyst.model.ProblemFiles;
 
 public interface IProblemFilesDAO extends GenericDao<ProblemFiles,Long>{
 	public List<Object[]> getCurrentDateFiles(Date currentDate,Date endDate,String isApproved);
-	
+
 	public List<Object> getNoOfFilesUploadedForAUser(Long problemId);
+	
+	public List<Object[]> getProblemRelatedFilesForAUser(Long problemId,Long userId);
 
 }
