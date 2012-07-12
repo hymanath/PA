165b9cdb2eba769d206951ca26ff911e864ed241
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 10:22:09 +0000

    Updated by Anuradha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICommentsDataService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICommentsDataService.java
@@ -29,23 +29,36 @@ public interface ICommentsDataService {
 	public List<ElectionCommentsVO> getConstituencyCommentsData(String electionType,String electionYear,Long electionId,Long constituencyId,
 			String categoryType);
 	
-	public CommentCategoryCandidate saveCandidateCommentForAnElection(String electionType,String electionYear,Long electionId, Long constituencyId,
-			Long candidateId,String commentDesc,String commentedBy,Long commentCategoryId, Long userId, String userType, Float severity);
+	/*public CommentCategoryCandidate saveCandidateCommentForAnElection(String electionType,String electionYear,Long electionId, Long constituencyId,
+			Long candidateId,String commentDesc,String commentedBy,Long commentCategoryId, Long userId, String userType, Float severity);*/
 	
-	public CommentCategoryParty savePartyCommentForAnElection(String electionType,String electionYear,Long electionId,Long partyId,
-			String commentDesc,String commentedBy,Long commentCategoryId);
+	public CommentCategoryCandidate saveCandidateCommentForAnElection(String electionType, String electionYear, Long electionId,
+			Long constituencyId, Long candidateId,final String commentDesc,	final String commentedBy,Long commentCategoryId, 
+			final Long userId, final String userType, final Float severity,final Boolean hasFreeUserRole, final Boolean hasPartyAnalystUserRole);
+	
+	/*public CommentCategoryParty savePartyCommentForAnElection(String electionType,String electionYear,Long electionId,Long partyId,
+			String commentDesc,String commentedBy,Long commentCategoryId);*/
+	
+	public CommentCategoryParty savePartyCommentForAnElection(
+			String electionType, String electionYear, Long electionId,
+			final Long partyId,final String commentDesc,final String commentedBy,Long commentCategoryId,Boolean hasFreeUserRole,Boolean hasPartyAnalystUserRole) ;
 	
 	public CommentCategoryConstituency saveConstituencyCommentForAnElection(String electionType,String electionYear,
 			Long electionId,Long constituencyId,String commentDesc,String commentedBy,Long commentCategoryId);
 	
-	public CandidateCommentsVO saveCandidateCommentsToDB(String electionType, String electionYear, Long electionId,	Long constituencyId, 
-			Long candidateId,String commentDesc,String commentedBy,Long commentCategoryId, Long userId, String userType, Long severityPercent);
+	/*public CandidateCommentsVO saveCandidateCommentsToDB(String electionType, String electionYear, Long electionId,	Long constituencyId, 
+			Long candidateId,String commentDesc,String commentedBy,Long commentCategoryId, Long userId, String userType, Long severityPercent);*/
+	public CandidateCommentsVO saveCandidateCommentsToDB(String electionType, String electionYear, Long electionId,
+			Long constituencyId, Long candidateId,String commentDesc,String commentedBy,Long commentCategoryId, 
+			Long userId, String userType, Long severityPercent,Boolean hasFreeUserRole, Boolean hasPartyAnalystUserRole);
 	
 	public ConstituencyCommentsVO saveConstituencyCommentsToDB(String electionType, String electionYear, Long electionId,
 			Long constituencyId,String commentDesc,String commentedBy,Long commentCategoryId);
 	
+	/*public PartyCommentsVO savePartyCommentsToDB(String electionType, String electionYear, Long electionId,
+			Long partyId,String commentDesc,String commentedBy,Long commentCategoryId);*/
 	public PartyCommentsVO savePartyCommentsToDB(String electionType, String electionYear, Long electionId,
-			Long partyId,String commentDesc,String commentedBy,Long commentCategoryId);
+			Long partyId,String commentDesc,String commentedBy,Long commentCategoryId,Boolean hasFreeUserRole,Boolean hasPartyAnalystUserRole);
 	
 	public List<SelectOptionVO> getCandidateCommentsCategoryStatics(Long candidateRank);
 	
