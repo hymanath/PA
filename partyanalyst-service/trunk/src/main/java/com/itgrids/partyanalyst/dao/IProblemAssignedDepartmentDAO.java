5e3675fe0c8ecb46e572ce839e8a44b72c728097
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 12:10:58 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemAssignedDepartmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemAssignedDepartmentDAO.java
@@ -3,10 +3,25 @@ package com.itgrids.partyanalyst.dao;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
+
+
 import com.itgrids.partyanalyst.model.ProblemAssignedDepartment;
+import com.itgrids.partyanalyst.model.UserProblem;
 
 public interface IProblemAssignedDepartmentDAO extends GenericDao<ProblemAssignedDepartment,Long>{
 	
 	public List<ProblemAssignedDepartment> getAllActivitesByProblem(Long problemId);
+	
+	public List<Object[]> getAssignedProblemsProgressByLocation(Long problemId);
+	
+	public List<UserProblem> getDepartmentWiseProblemsBasedOnStatus(Long userId,Long deptOrgId,String statusStr);
+	
+	public List<Object> getDepartmentWiseProblemStatus(Long userId,Long deptOrgId);
+	
+	public List<Long>  getDeptWiseAssignedProblemProgressIds(Long userId,String deptLocationStr);
+	
+	public List<Object[]> getProblemsStatusBasedOnAssignedProblemProgressId(Long userId,List<Long> progressIdList);
+	
+	public List<UserProblem> getProblemsBasedOnAssignedProblemProgressIdAndStatus(Long userId,List<Long> progressIdList,String deptStr,String statusStr);
 
 }
