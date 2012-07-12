3a41ce3b92722d6fc5ffed082246f46870309c2f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 05:44:48 +0000

    Updated by Anuradha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ProblemProgress.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ProblemProgress.java
@@ -81,7 +81,7 @@ public class ProblemProgress extends BaseModel implements Serializable{
 	}
 
 	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-	@JoinColumn(name="activity_id")
+	@JoinColumn(name="problem_activity_id")
 	@LazyToOne(LazyToOneOption.NO_PROXY)
 	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public ProblemActivity getProblemActivity() {
