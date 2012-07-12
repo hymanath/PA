7bbc9f95cfa77465a7bb6377585986969ba02dc7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 12:34:01 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreProblemsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreProblemsDAO.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.ICadreProblemsDAO;
+import com.itgrids.partyanalyst.model.Cadre;
 import com.itgrids.partyanalyst.model.CadreProblems;
+import com.itgrids.partyanalyst.model.Problem;
+import com.itgrids.partyanalyst.model.ProblemHistory;
+
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class CadreProblemsDAO extends GenericDaoHibernate<CadreProblems,Long> implements ICadreProblemsDAO{
 
@@ -14,11 +19,74 @@ public class CadreProblemsDAO extends GenericDaoHibernate<CadreProblems,Long> im
 		super(CadreProblems.class);
 	}
 	
+	
 	@SuppressWarnings("unchecked")
 	public List<Object[]> getCadreDetailsAndMobileNoByProblemId(Long problemId)
 	{
 		return getHibernateTemplate().find("select model.cadre.firstName,model.cadre.lastName,model.cadre.mobile from CadreProblems model where model.problem.problemId = ?",problemId);
 	}
+	public List<Object> getCadreProblemsCountForAnUser(Long userId)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find("select model.problem.problemId from CadreProblems model,UserProblem model2 where model2.user.userId=? and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and  model.problem.isApproved = 'true'",params);
+	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> getCadreProblemsCountInARegion(Long userId,String locationStr)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find(" select model.problem.problemId from CadreProblems model,UserProblem model2 where model2.user.userId = ? "+locationStr+" and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and model.problem.isApproved = 'true'",userId);
+	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Problem> getCadreProblemsInARegion(Long userId,String locationStr)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find("select model.problem from CadreProblems model,UserProblem model2 where model2.user.userId = ? "+locationStr+" and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and model.problem.isApproved = 'true'",params);
+	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Problem> getCadreProblemsForAnUser(Long userId)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find(" select model.problem from CadreProblems model,UserProblem model2 where model2.user.userId=? and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and  model.problem.isApproved = 'true'",params);
+	}
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Cadre> getCadreForCadreProblemsInARegion(Long userId,String locationStr)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find("select distinct model.cadre from CadreProblems model,UserProblem model2 where model2.user.userId = ? "+locationStr+" and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and model.problem.isApproved = 'true'",params);
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	public List<Cadre> getCadreForCadreProblemsForAnUser(Long userId)
+	{
+		Object[] params = {userId};
+		return getHibernateTemplate().find(" select distinct model.cadre from CadreProblems model,UserProblem model2 where model2.user.userId=? and model.problem.problemId = model2.problem.problemId and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and  model.problem.isApproved = 'true'",params);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> getProblemStatusOfACadre(Long cadreId)
+	{
+		Object[] params = {cadreId};
+		return getHibernateTemplate().find(" select model.problem.problemStatus.status from CadreProblems model where " +
+				" model.cadre.cadreId = ? and (model.problem.isDelete is null or model.problem.isDelete='"+IConstants.FALSE+"') and  model.problem.isApproved = 'true'",params);
+	}
+	
+	
+	
+	
 	
 	
 }
