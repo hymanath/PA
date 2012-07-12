0fcc183674b812be0143cf2fe887eecf93244dd8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 12:09:20 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreProblemsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreProblemsDAO.java
@@ -1,11 +1,28 @@
 package com.itgrids.partyanalyst.dao;
 
 import java.util.List;
+
 import org.appfuse.dao.GenericDao;
+
+
+import com.itgrids.partyanalyst.model.Cadre;
 import com.itgrids.partyanalyst.model.CadreProblems;
+import com.itgrids.partyanalyst.model.Problem;
 
 public interface ICadreProblemsDAO extends GenericDao<CadreProblems,Long>{
-	
 	public List<Object[]> getCadreDetailsAndMobileNoByProblemId(Long problemId);
+	public List<Object> getCadreProblemsCountForAnUser(Long userId);
+	
+	public List<Object> getCadreProblemsCountInARegion(Long userId,String locationStr);
+	
+	public List<Problem> getCadreProblemsInARegion(Long userId,String locationStr);
+	
+	public List<Problem> getCadreProblemsForAnUser(Long userId);
+	
+	public List<Cadre> getCadreForCadreProblemsInARegion(Long userId,String locationStr);
+	
+	public List<Cadre> getCadreForCadreProblemsForAnUser(Long userId);
+	
+	public List<Object> getProblemStatusOfACadre(Long cadreId);
 
 }
