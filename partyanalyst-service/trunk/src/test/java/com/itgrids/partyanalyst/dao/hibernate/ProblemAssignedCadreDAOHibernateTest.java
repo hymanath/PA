d6ba74e3fed9a53a359563a868ded34805fa126b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-12 13:06:51 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedCadreDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedCadreDAOHibernateTest.java
@@ -1,7 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 import com.itgrids.partyanalyst.dao.IProblemAssignedCadreDAO;
+import com.itgrids.partyanalyst.model.Cadre;
+import com.itgrids.partyanalyst.model.UserProblem;
 
 public class ProblemAssignedCadreDAOHibernateTest extends BaseDaoTestCase{
 
@@ -16,4 +20,52 @@ public class ProblemAssignedCadreDAOHibernateTest extends BaseDaoTestCase{
 	{
 		problemAssignedCadreDAO.getAll();
 	}
+	
+	/*public void testgetAssignedCadreProblemsInARegion()
+	{
+		Long userId = 1l;
+		String locationStr = " and model.userProblem.problem.problemCompleteLocation.state.stateId = 10l";
+		List<UserProblem> userproblem = problemAssignedCadreDAO.getAssignedCadreProblemsInARegion(userId, locationStr);
+	}*/
+	/*public void testgetAssignedCadreProblemsCountForAnUser()
+	{
+		List<Object> value = problemAssignedCadreDAO.getAssignedCadreProblemsCountForAnUser(1l);
+		System.out.println(value.size());
+	}*/
+	
+	/*public void testgetAssignedCadreProblemsCountInARegion()
+	{
+		String locationStr = "and model.userProblem.problem.problemCompleteLocation.state.stateId = 10l";
+		List<Object> value = problemAssignedCadreDAO.getAssignedCadreProblemsCountInARegion(1l,locationStr);
+		System.out.println(value.size());
+	}
+*/
+	/*public void testgetAssignedCadreProblemsForAnUser()
+		{
+	List<UserProblem> user = problemAssignedCadreDAO.getAssignedCadreProblemsForAnUser(1l);
+	for(UserProblem problemDetails  : user)
+	{
+		System.out.println(problemDetails.getProblem().getTitle());
+	}
+		}*/
+	
+	/*public void testgetCadreForCadreProblemsInARegion()
+	{
+		String locationStr = "and model.userProblem.problem.problemCompleteLocation.state.stateId = 1";
+		List<Cadre> list = problemAssignedCadreDAO.getCadreForCadreProblemsInARegion(1l,locationStr);
+		for(Cadre params : list)
+		{
+			System.out.println(params.getFirstName());
+		}
+	}*/
+	
+	public void testgetCadreForCadreProblemsForAnUser()
+	{
+		List<Cadre> list = problemAssignedCadreDAO.getCadreForCadreProblemsForAnUser(1l);
+		for(Cadre params : list)
+		{
+			System.out.println(params.getFirstName());
+		}
+	}
+
 }
--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedDepartmentDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ProblemAssignedDepartmentDAOHibernateTest.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IProblemAssignedDepartmentDAO;
+import com.itgrids.partyanalyst.model.UserProblem;
 
 public class ProblemAssignedDepartmentDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -18,4 +22,62 @@ public class ProblemAssignedDepartmentDAOHibernateTest extends BaseDaoTestCase{
 		problemAssignedDepartmentDAO.getAll();
 	}
 
+	
+	/*public void testgetAssignedProblemsProgressByLocation()
+	{
+		List<Object[]> list = problemAssignedDepartmentDAO.getAssignedProblemsProgressByLocation(23l);
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println(params[0].toString());
+		}
+	}*/
+	
+	/*public void testgetDepartmentWiseProblemsBasedOnStatus()
+	{
+		String status = "Total";
+		List<UserProblem> list = problemAssignedDepartmentDAO.getDepartmentWiseProblemsBasedOnStatus(1l, 1l,"and model.userProblem.problem.problemStatus.status = 'Total' ");
+		for(UserProblem user : list)
+		{
+			System.out.println(user.getProblem().getDescription());
+		}
+	}*/
+	
+	/*public void testgetDepartmentWiseProblemStatus()
+	{
+		List<Object> list = problemAssignedDepartmentDAO.getDepartmentWiseProblemStatus(1l, 1l);
+		System.out.println(list.size());
+		System.out.println(list);
+	}*/
+	
+	/*public void testgetDeptWiseAssignedProblemProgressIds()
+	{
+		List<Long> value = problemAssignedDepartmentDAO.getDeptWiseAssignedProblemProgressIds(1l, "and model.departmentLocation.state.stateId = 10");
+		System.out.println(value);
+	}*/
+	
+	/*public void testgetProblemsStatusBasedOnAssignedProblemProgressId()
+	{
+		List<Long> problemIds = new ArrayList<Long>();
+		problemIds.add(23l);
+		List<Object[]> list = problemAssignedDepartmentDAO.getProblemsStatusBasedOnAssignedProblemProgressId(1l, problemIds);
+		System.out.println(list.size());
+	}*/
+	
+	public void testgetProblemsBasedOnAssignedProblemProgressIdAndStatus()
+	{
+		
+		
+		Long userId=1l;
+		List<Long> progressIdList = new ArrayList<Long>();
+		progressIdList.add(23l);
+		String deptStr="and model.departmentOrganisation.departmentOrganisationId = 1";
+		String statusStr="and model.userProblem.problem.problemStatus.status = 'New'";
+		List<UserProblem> list = problemAssignedDepartmentDAO.getProblemsBasedOnAssignedProblemProgressIdAndStatus(userId,progressIdList,deptStr,statusStr);
+		for(UserProblem user : list)
+		{
+			System.out.println(user.getProblem().getTitle());
+		}
+	}
 }
