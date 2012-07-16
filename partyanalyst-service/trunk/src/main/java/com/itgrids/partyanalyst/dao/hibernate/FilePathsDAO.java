70b51b2289f74303511fa56e3dedddc16ac42be5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-16 16:09:57 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IFilePathsDAO;
 import com.itgrids.partyanalyst.model.FilePaths;
@@ -18,12 +19,33 @@ public class FilePathsDAO extends GenericDaoHibernate<FilePaths,Long> implements
 	{
 		return getHibernateTemplate().find("select max(model.orderNo) from FilePaths model");
 	}
-	public List<Object[]> getProblemRelatedFiles(Long problemId,Long userId){
+	public List<Object[]> getProblemRelatedFiles(final Long problemId,final Long userId){
 		Object[] params = {problemId,userId};
 		return getHibernateTemplate().find("select model.fileSourceLanguage.file.fileTitle,model.fileSourceLanguage.file.fileDescription," +
 				"model.filePath from FilePaths model where model.fileSourceLanguage.file.fileId in (select model1.file.fileId from ProblemFiles " +
 				" model1 where model1.isApproved='true' and model1.isDelete ='false' and model1.problem.problemId =? and model1.user.userId = ?)" +
 				" order by model.fileSourceLanguage.file.fileId desc",params);
+	}
+	public List<Object[]> getProblemFiles(final Long problemId,final Long userId,final String visibility){
+
+		StringBuilder query = new StringBuilder();
+		query.append("select model.fileSourceLanguage.file.fileTitle,model.fileSourceLanguage.file.fileDescription,model.filePath,model1.user.userId,model1.user.firstName,model1.user.lastName " +
+				" from FilePaths model,ProblemFiles model1 where model.fileSourceLanguage.file.fileId = model1.file.fileId  and model1.isApproved='true' and model1.isDelete ='false' and model1.problem.problemId = :problemId ");
+		if(visibility != null)
+			query.append(" and model1.visibility.type = :visibility ");
+		if(userId != null)
+			query.append(" and model1.user.userId :userId ");
+		query.append(" order by model1.updatedTime desc ");
+		
+		Query queryObj = getSession().createQuery(query.toString());
+		
+		queryObj.setParameter("problemId", problemId);
+		if(visibility != null)
+			queryObj.setParameter("visibility", visibility);
+		if(userId != null)
+		    queryObj.setParameter("userId", userId);
+		return queryObj.list();
+		
 	}
 
 }
