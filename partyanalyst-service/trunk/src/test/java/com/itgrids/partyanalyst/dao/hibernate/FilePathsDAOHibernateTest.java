f98d30c8dd480b186517e70f008fc85894215aad
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-16 16:13:00 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IFilePathsDAO;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class FilePathsDAOHibernateTest extends BaseDaoTestCase {
    
@@ -18,9 +19,15 @@ public class FilePathsDAOHibernateTest extends BaseDaoTestCase {
 		
 		filePathsDAO.getAll();
 	 }*/
-	public void testGetMaxOrderNo()
+	/*public void testGetMaxOrderNo()
 	{
 		List<Object> list = filePathsDAO.getMaxOrderNo();
+		System.out.println(list.size());
+	}*/
+	
+	public void testGetProblemFiles()
+	{
+		List<Object[]> list = filePathsDAO.getProblemFiles(1l,null,IConstants.PUBLIC);
 		System.out.println(list.size());
 	}
 }
