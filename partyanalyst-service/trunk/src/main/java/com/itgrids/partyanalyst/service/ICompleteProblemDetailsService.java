7c4585eea13bc23414d5553924f96a1806a1d1f9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-19 06:39:17 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICompleteProblemDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICompleteProblemDetailsService.java
@@ -1,7 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.CompleteProblemDetailsVO;
+import com.itgrids.partyanalyst.dto.UserCommentsInfoVO;
 
 public interface ICompleteProblemDetailsService {
 	public CompleteProblemDetailsVO getProblemCompleteDetails(Long problemId,Long userId,String userStatus);
+	public List<UserCommentsInfoVO> getPostedComments(final Long problemId,final Long userId);
 }
