658c93fc0d65eae1657bdaf8ecac86f6105945d8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-25 14:24:12 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AbusedCommentsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AbusedCommentsDAO.java
@@ -1,14 +1,78 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
-import com.itgrids.partyanalyst.dao.IAbusedComments;
+import com.itgrids.partyanalyst.dao.IAbusedCommentsDAO;
 import com.itgrids.partyanalyst.model.AbusedComments;
+import com.itgrids.partyanalyst.utils.IConstants;
 
-public class AbusedCommentsDAO extends GenericDaoHibernate<AbusedComments,Long> implements IAbusedComments {
+public class AbusedCommentsDAO extends GenericDaoHibernate<AbusedComments,Long> implements IAbusedCommentsDAO {
 	
 	public AbusedCommentsDAO() {
 		super(AbusedComments.class);
 	}
-
+	@SuppressWarnings("unchecked")
+	 public List getAllAbuseComment(Date firstDate, Date secondDate,String selectstatus){
+	    	Object[] params = {firstDate, secondDate };
+	    	
+	    	StringBuilder query = new StringBuilder();
+	    	query.append("select model.abusedCommentsId,model.user.firstName,model.user.lastName,model.comment.comment,model.status,model.isDelete,model.time from AbusedComments model where  date(model.time) >= ? and date(model.time) <= ?");
+	    	
+	    	  
+	    		    if(selectstatus.equals("New"))
+	    		       query.append(" and model.status ='false' and model.isDelete = 'false'");
+	    		    else if(selectstatus.equals("Approved"))
+	        		   query.append(" and model.status ='true' and model.isDelete = 'false'");
+	    		    else if(selectstatus.equals("Rejected"))
+	        		    query.append(" and model.isDelete ='true'");
+	    	  
+	    	   
+	    	query.append(" order by model.time desc ");
+	    	Query queObject = getSession().createQuery(query.toString());
+	    	queObject.setDate(0,firstDate);
+	    	queObject.setDate(1,secondDate);
+	    	
+	    	
+	    	return queObject.list();
+	    	
+	    	/*return getHibernateTemplate().find("select model.candidate.lastname,model.name,model.message,"+
+	    			"model.constituency.name,model.isApproved where date(model.time) >= ? "+
+	    			"and date(model.time) <= ? "+params);*/
+	    }
+	@SuppressWarnings("unchecked")
+	 public Integer controlAbuseComments(Long id,String status,String isDelete)
+		{
+			StringBuilder query = new StringBuilder();
+	    	query.append("update AbusedComments model set ");   
+	    	   
+	           if(status != null)
+	           {
+	        	   if(isDelete != null)
+	        		   query.append(" model.status =:status ,model.isDelete =:isDelete");
+	           }
+	           if(status == null)
+	        	   query.append(" model.isDelete =:isDelete");
+		    	   
+	           if(isDelete == null)
+	        	  query.append(" model.status =:status");
+	           
+	    	query.append(" where model.abusedCommentsId =:id");
+	    	Query queObject = getSession().createQuery(query.toString());
+	    	
+	    	if(status != null)
+	    		queObject.setString("status", status);
+	    	if(isDelete != null)
+	    		queObject.setString("isDelete", isDelete);
+	    	
+	    	queObject.setLong("id", id);
+	    	
+			return queObject.executeUpdate();
+				
+		}
+	
+	
 }
