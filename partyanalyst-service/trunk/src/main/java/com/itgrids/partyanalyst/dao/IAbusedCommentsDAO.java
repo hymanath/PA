1b2b0d44268573b703ca72b990e2f198e0148b72
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-25 14:23:31 +0000

    updated by srihari

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAbusedCommentsDAO.java
@@ -0,0 +1,15 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AbusedComments;
+
+public interface IAbusedCommentsDAO extends GenericDao<AbusedComments,Long>{
+	 public List getAllAbuseComment(Date firstDate, Date secondDate,String selectstatus);
+	 
+	 public Integer controlAbuseComments(Long id, String status,String isDelete);
+
+}
