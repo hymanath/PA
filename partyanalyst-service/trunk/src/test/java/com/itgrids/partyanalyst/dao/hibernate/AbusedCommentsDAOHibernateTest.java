98f76a27c90225df45d4e2ed3aae6abe02639f35
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-25 14:28:44 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AbusedCommentsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AbusedCommentsDAOHibernateTest.java
@@ -1,15 +1,41 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
-import com.itgrids.partyanalyst.dao.IAbusedComments;
+import com.itgrids.partyanalyst.dao.IAbusedCommentsDAO;
+import com.itgrids.partyanalyst.service.impl.DateService;
 
 public class AbusedCommentsDAOHibernateTest extends BaseDaoTestCase{
 	
-	private IAbusedComments abusedComments;
+	private IAbusedCommentsDAO abusedCommentsDAO;
 
-	public void setAbusedComments(IAbusedComments abusedComments) {
-		this.abusedComments = abusedComments;
+	
+
+	    public void setAbusedCommentsDAO(IAbusedCommentsDAO abusedCommentsDAO) {
+		this.abusedCommentsDAO = abusedCommentsDAO;
 	}
+
+
+
+		/*public void test()
+		{
+	    	abusedCommentsDAO.getAll();
+			
+		}*/
+	   /* public void testGetAllAbuseComment()
+		{
+			System.out.println();
+			List list = abusedCommentsDAO.getAllAbuseComment(
+					DateService.convertStringToDate("2012-7-23", "yyyy-MM-dd"),DateService.convertStringToDate("2012-7-23", "yyyy-MM-dd"),"NEW");
+			System.out.println(list.size());
+		}*/
+	    public void testControlAbuseComments()
+		{
+			System.out.println();
+			Integer list = abusedCommentsDAO.controlAbuseComments(3l,"true","false");
+			System.out.println(list);
+		}
 
 }
