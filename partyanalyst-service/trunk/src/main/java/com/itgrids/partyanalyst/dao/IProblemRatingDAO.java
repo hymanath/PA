1270763be2c3aef92c20acb9a98cd69d05609b5f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-07-26 14:28:58 +0000

    Updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemRatingDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IProblemRatingDAO.java
@@ -8,8 +8,12 @@ import com.itgrids.partyanalyst.model.ProblemRating;
 
 public interface IProblemRatingDAO extends GenericDao<ProblemRating, Long>{
 	
-	public Double getAverageRatingOfAProblem(Long problemId);
+	public List<Object[]> getAverageRatingOfAProblem(Long problemId);
 	
 	public List<Object[]> getRatingWiseCountOfAProblem(Long problemId);
+	
+	public List<Long> getIsRatingGivenByUser(Long problemId,Long userId);
+	
+	public List<Integer> getRatingGivenByUser(Long problemId,Long userId);
 
 }
