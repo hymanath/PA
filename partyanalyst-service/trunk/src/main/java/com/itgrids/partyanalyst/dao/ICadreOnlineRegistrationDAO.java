e80d641cdb730c56a13a911b3692431cf609071b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-10 11:20:05 +0000

    updated by AnuRadha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreOnlineRegistrationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CadreOnlineRegistration;
+
+public interface ICadreOnlineRegistrationDAO extends GenericDao<CadreOnlineRegistration, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICandidateCadreOnlineRegistrationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CandidateCadreOnlineRegistration;
+
+public interface ICandidateCadreOnlineRegistrationDAO extends GenericDao<CandidateCadreOnlineRegistration, Long>{
+
+}
