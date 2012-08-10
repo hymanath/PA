8b0587a21982ca9242d62aada4a3c401c46c0ff5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-10 11:20:05 +0000

    updated by AnuRadha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyCadreOnlineRegistrationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.PartyCadreOnlineRegistration;
+
+public interface IPartyCadreOnlineRegistrationDAO extends GenericDao<PartyCadreOnlineRegistration, Long>{
+
+}
