e00fa7802119344e0b2bd4fb53110b226768163e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-10 07:26:06 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IProblemManagementService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IProblemManagementService.java
@@ -196,4 +196,8 @@ public String getProblemLocationString(ProblemCompleteLocation problemCompleteLo
 	
 	public String getIsUserRatedAProblem(Long userId, Long problemId);
 	
+	public List<CompleteProblemDetailsVO> getProblemDetailsForUpdate(Long problemId);
+	
+	public ResultStatus updateProblemDetails(Long problemId,List<CompleteProblemDetailsVO> completeProblemDetailsVO);
+	
 }
