83d392ace1c6dbc530d4f22a0efd185abe6d73c8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-11 13:04:30 +0000

    updated by srihari

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CandidateDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CandidateDetailsService.java
@@ -3930,9 +3930,9 @@ public List<SelectOptionVO> getCandidatesOfAUser(Long userId)
 				comment.setMessage(abusedComments.getComment().getComment());
 				if(abusedComments.getStatus().equalsIgnoreCase(IConstants.TRUE))
 				     comment.setStatus(IConstants.APPROVED);
-				else if(abusedComments.getStatus().equalsIgnoreCase(IConstants.FALSE))
+				else if(abusedComments.getStatus().equalsIgnoreCase(IConstants.FALSE) && abusedComments.getIsDelete().equalsIgnoreCase(IConstants.FALSE))
 				     comment.setStatus(IConstants.NEW);
-				else if(abusedComments.getIsDelete().equalsIgnoreCase(IConstants.TRUE))
+				else if(abusedComments.getIsDelete().equalsIgnoreCase(IConstants.TRUE) && abusedComments.getStatus().equalsIgnoreCase(IConstants.FALSE))
 				     comment.setStatus(IConstants.REJECTED);
 				comment.setTime(abusedComments.getTime().toString());
 				
