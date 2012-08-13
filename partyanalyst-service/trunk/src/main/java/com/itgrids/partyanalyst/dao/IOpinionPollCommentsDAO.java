156e7531239ac8928179abcfa555b24040cc899e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-13 10:07:09 +0000

    updated by Samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IOpinionPollCommentsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IOpinionPollCommentsDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.OpinionPollComments;
 
 public interface IOpinionPollCommentsDAO  extends GenericDao<OpinionPollComments, Long>{
+	
+	public List<Object[]> getCommentDetailsByQuestionId(Long pollId);
 
 }
