1d2578e17c833672338246f10f05a6f0b48378c2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-08-13 10:06:47 +0000

    updated by Samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAbusedCommentsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAbusedCommentsDAO.java
@@ -11,5 +11,7 @@ public interface IAbusedCommentsDAO extends GenericDao<AbusedComments,Long>{
 	 public List<AbusedComments> getAllAbuseComment(Date firstDate, Date secondDate,String selectstatus);
 	 
 	 public Integer controlAbuseComments(Long id, String status,String isDelete);
+	 
+	 public Long checkForAlreadyAbused(Long cmntId);
 
 }
