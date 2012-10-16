f041f0f31ca11cf11f5765d90824223fb79a55a5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-10-16 07:27:35 +0000

    Updated by Nagamani

--- partyanalyst/trunk/src/main/webapp/js/specialPage/specialPage.js
+++ partyanalyst/trunk/src/main/webapp/js/specialPage/specialPage.js
@@ -745,6 +745,11 @@ var rparam ="task="+YAHOO.lang.JSON.stringify(jsObj);
 	
 	errorDivEle.innerHTML = str;
 }
+function removeAllUnwantedCharactersInArray(str)
+{
+   var strng = str.replace(/[\\\\%\&\#\"+"]/g," ");
+   return replaceEnterKey(strng,"  ");
+}
 function updateProfileDiscription(){
 	var specialPageId = document.getElementById('specialPageId').value;
 	var orderNoArr = [];
@@ -755,7 +760,7 @@ function updateProfileDiscription(){
 		
 			orderNoArr.push(document.getElementById('orderNoId_'+i).value);
 			profDescIdArr.push(document.getElementById('specialPageDescriptionId_'+i).value);
-			descriptionArr.push(removeAllUnwantedCharacters(document.getElementById('descId_'+i).value));		
+			descriptionArr.push(removeAllUnwantedCharactersInArray(document.getElementById('descId_'+i).value));		
    		}
   
  var jsObj =
