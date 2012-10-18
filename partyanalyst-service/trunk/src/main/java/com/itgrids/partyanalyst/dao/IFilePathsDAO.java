2341ea6d6624dbe1d262d4319103d3b9eb33d894
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-10-18 11:48:04 +0000

    Updated by AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFilePathsDAO.java
@@ -11,4 +11,5 @@ public interface IFilePathsDAO extends GenericDao<FilePaths, Long> {
 	public List<Object> getMaxOrderNo();
 	public List<Object[]> getProblemRelatedFiles(Long problemId,Long userId);
 	public List<Object[]> getProblemFiles(final Long problemId,final Long userId,final String visibility);
+	public List<Object> getFilePaths(long fileId);
 }
