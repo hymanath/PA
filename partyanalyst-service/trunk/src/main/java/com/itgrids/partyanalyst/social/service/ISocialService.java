b4bdceac6d5e3fb6155e2abd10818e257aa2b21e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-10-31 13:56:17 +0000

    Updated by Junior Team

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/social/service/ISocialService.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.social.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.social.model.CandidateSocial;
+
+public interface ISocialService {
+	//public List<CandidateSocial> getNames();
+	
+	public List getNames();
+}
