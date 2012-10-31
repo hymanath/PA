774c475fbf257ba3d886ef0a0dad24bdb1737f88
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-10-31 13:55:48 +0000

    Updated by Junior Team

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/social/dao/IPartySocialDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.social.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.social.model.PartySocial;
+
+public interface IPartySocialDAO  extends GenericDao<PartySocial, Long> {
+	public List<PartySocial> getNames();
+}
