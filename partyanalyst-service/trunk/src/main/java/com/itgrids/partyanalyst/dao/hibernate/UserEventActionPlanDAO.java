b3c50a74562a6ee3af11e8fc855e93fdb7d46779
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-11-02 09:32:41 +0000

    cadre event creation by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserEventActionPlanDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserEventActionPlanDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IUserEventActionPlanDAO;
 import com.itgrids.partyanalyst.model.UserEventActionPlan;
@@ -19,7 +20,29 @@ public class UserEventActionPlanDAO extends GenericDaoHibernate<UserEventActionP
 		
 		return getHibernateTemplate().find("from UserEventActionPlan model where model.userEvents.userEventsId = ?", userEventsId) ;
 	}
-
-	
+    public List<Long> getEventActionPlanIds(Long eventId){
+    	Query queryObject = getSession().createQuery("select model.eventActionPlanId from UserEventActionPlan model where model.userEvents.userEventsId = ?");
+		queryObject.setParameter(0, eventId);	
+		 return queryObject.list(); 	
+    }
+    public List<Long> getEventActionPlanIds(Long eventId,List<Long> eventActionPlanIds){
+    	Query queryObject = getSession().createQuery("select model.eventActionPlanId from UserEventActionPlan model where model.userEvents.userEventsId = (:eventId) and model.eventActionPlanId not in (:eventActionPlanIds)");
+		queryObject.setParameter("eventId", eventId);	
+		queryObject.setParameterList("eventActionPlanIds", eventActionPlanIds);		
+		 return queryObject.list(); 	
+    }
+	public void removeEventActionPlans(Long eventId){
+		
+		Query queryObject = getSession().createQuery("delete from UserEventActionPlan model where model.userEvents.userEventsId = ?");
+		queryObject.setParameter(0, eventId);	
+		 queryObject.executeUpdate();
+	}
+   public void removeDeletedEventActionPlans(Long eventId,List<Long> eventActionPlanIds){
+		
+		Query queryObject = getSession().createQuery("delete from UserEventActionPlan model where model.userEvents.userEventsId = (:eventId) and model.eventActionPlanId not in (:eventActionPlanIds)");
+		queryObject.setParameter("eventId", eventId);	
+		queryObject.setParameterList("eventActionPlanIds", eventActionPlanIds);	
+		 queryObject.executeUpdate();
+	}
 	
 }
