3f9ecd32711cd3ed42622d299f5ad893380f28cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-11-29 13:47:11 +0000

    Updated by Prasad

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/BoothDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/BoothDAOHibernateTest.java
@@ -78,7 +78,7 @@ public class BoothDAOHibernateTest extends BaseDaoTestCase{
 			System.out.println(values[0]+"\t"+values[1]+"\t"+values[2]+"\t"+values[3]);
 	}*/
 	
-	public void testmandalInfo()
+	/*public void testmandalInfo()
 	{
 		List votersList  = boothDAO.findVoterInformationByMandalIdsAndDelimitationYear("849,851,852,853,854,859,860,862","2004", 235l);
 		List<VotersInfoForMandalVO> votersInfoForMandalList = new ArrayList<VotersInfoForMandalVO>();
@@ -100,7 +100,7 @@ public class BoothDAOHibernateTest extends BaseDaoTestCase{
 		{
 			System.out.println(obj.getMandalId()+" - "+obj.getMandalName()+" - "+obj.getTotalVoters()+" - "+obj.getTotalMaleVoters()+" - "+obj.getTotalMaleVoters());
 		}
-	}
+	}*/
 	
 	/*public void testFindbyConstituencyNameDistrictIdAndElectionYear(){
 		List list = boothDAO.findbyConstituencyNameDistrictIdAndElectionYear("Kavali", 19l, 2009l);
@@ -161,12 +161,24 @@ public class BoothDAOHibernateTest extends BaseDaoTestCase{
 	{
 	 List<Booth> list = boothDAO.getPollingStationByConstituencyId(232l);
 	 System.out.println(list.size());
-	}*/
-	
+	 
+	 for (Booth booth : list) {
+		 
+		 System.out.println(booth.getLocation());
+		
+	}
+	}
+	*/
 	/*public void testgetPollingStationByMandalId()
 	{
-	 List<Booth> list = boothDAO.getPollingStationByMandalId(295l);
+	 List<Booth> list = boothDAO.getPollingStationByMandalId(283l);
 	 System.out.println(list.size());
+	 
+	 for (Booth booth : list) {
+		 
+		 System.out.println(booth.getPartNo());
+		
+	}
 	}*/
 	
 	
