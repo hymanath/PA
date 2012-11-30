fb68225b2d88551207f5ebb38a0f59c0bf480b2e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-11-30 09:06:32 +0000

    Updated by Nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/VotersAnalysisService.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.IVotersAnalysisService;
+import com.sun.xml.internal.org.jvnet.fastinfoset.VocabularyApplicationData;
+
+public class VotersAnalysisService implements IVotersAnalysisService{
+
+}
