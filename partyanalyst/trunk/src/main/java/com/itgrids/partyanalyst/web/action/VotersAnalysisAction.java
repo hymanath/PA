9e93564271931c7201113af4b73400987e46fa6d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-01 06:51:11 +0000

    Updated by Nagamani

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VotersAnalysisAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VotersAnalysisAction.java
@@ -1,18 +1,22 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.service.ICrossVotingEstimationService;
+import com.itgrids.partyanalyst.service.impl.RegionServiceDataImp;
+import com.itgrids.partyanalyst.utils.IConstants;
+import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
-import com.sun.org.apache.xpath.internal.Arg;
 
 public class VotersAnalysisAction extends ActionSupport implements ServletRequestAware{
 	
@@ -21,13 +25,43 @@ public class VotersAnalysisAction extends ActionSupport implements ServletReques
 	private HttpSession session;
 	
 	private String task;
+	JSONObject jObj;
 	
-	JSONObject jObj = null;
+	private RegionServiceDataImp regionServiceDataImp;
+	
+	private List<SelectOptionVO> namesList;
+	
+	private static final Logger log = Logger.getLogger(VotersAnalysisAction.class);
+
+	public List<SelectOptionVO> getNamesList() {
+		return namesList;
+	}
+
+	public JSONObject getjObj() {
+		return jObj;
+	}
+
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+
+	public void setNamesList(List<SelectOptionVO> namesList) {
+		this.namesList = namesList;
+	}
+
+
 	
 	private ICrossVotingEstimationService crossVotingEstimationService;
 	
 	private List<SelectOptionVO> constituencyList;
 	
+	public RegionServiceDataImp getRegionServiceDataImp() {
+		return regionServiceDataImp;
+	}
+
+	public void setRegionServiceDataImp(RegionServiceDataImp regionServiceDataImp) {
+		this.regionServiceDataImp = regionServiceDataImp;
+	}
 	public List<SelectOptionVO> getConstituencyList() {
 		return constituencyList;
 	}
@@ -89,6 +123,33 @@ public class VotersAnalysisAction extends ActionSupport implements ServletReques
 		constituencyList = crossVotingEstimationService.getConstituenciesForElectionYearAndTypeWithUserAccess(userID,electionYear,electionTypeId);
 		return SUCCESS;
 		
+	}
+	
+	public String AjaxHandler()
+	{
+		String param;
+		param = getTask();
+		
+		try{
+			jObj = new JSONObject(param);	
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			log.error("Exception Occured in getRequestMessagesForUser() Method,Exception is- "+e);
+		}
+		if(jObj.getString("task").equalsIgnoreCase("getMandalList"))
+		{
+			String selectedVal=jObj.getString("selected");
+			
+			//List<SelectOptionVO> mandals=regionServiceDataImp.getMandalsByConstituencyID(new Long(selectedVal));
+			
+			namesList = regionServiceDataImp.getSubRegionsInConstituency(new Long(selectedVal), IConstants.PRESENT_YEAR, null);
+			SelectOptionVO obj = new SelectOptionVO(0L,"Select Mandal");
+			namesList.add(0, obj);
+			
+			
+		}
+		return Action.SUCCESS;
 	}
 	
 }
