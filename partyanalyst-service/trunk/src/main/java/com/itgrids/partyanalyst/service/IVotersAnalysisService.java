2a022abba1dec72d267ea16419dfa4359b2a9801
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-03 11:38:35 +0000

    updated by Prasad

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
@@ -1,5 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+
 public interface IVotersAnalysisService {
+	
+	public List<SelectOptionVO> publicationDetailsBasedOnConstituency(Long constituencyId);
 
 }
