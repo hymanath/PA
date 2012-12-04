0cddcf51efdbc1faa7846a741ba56f6f0bc91946
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-04 14:59:10 +0000

    updated by Prasad

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothPublicationVoterDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothPublicationVoterDAO.java
@@ -18,87 +18,146 @@ public class BoothPublicationVoterDAO extends
 		super(BoothPublicationVoter.class);
 		
 	}
- public List<BoothPublicationVoter> checkForVoterExistenceInBoothPublicationVoter(Long boothId , Long voterId){
-		
-		
-		String queryString = "select model from BoothPublicationVoter model where model.booth.boothId = ? and model.voter.voterId = ?";
-		
-		Query query  = getSession().createQuery(queryString);
-		
-		
-		query.setParameter(0, boothId);
-		query.setParameter(1, voterId);
-		
-		return query.list();
-		
-		
-	}
- 
- 
-	public List<Voter> getVotersDetailsByBoothId(Long boothId, Integer startIndex,
-			Integer maxRecords, String order, String columnName) {  
+	 public List<BoothPublicationVoter> checkForVoterExistenceInBoothPublicationVoter(Long boothId , Long voterId){
+			
+			
+			String queryString = "select model from BoothPublicationVoter model where model.booth.boothId = ? and model.voter.voterId = ?";
+			
+			Query query  = getSession().createQuery(queryString);
+			
+			
+			query.setParameter(0, boothId);
+			query.setParameter(1, voterId);
+			
+			return query.list();
+			
+			
+		}
 	 
-		String queryString = "select model.voter from BoothPublicationVoter model " +
-				"where model.booth.boothId = ? order by model.voter."+columnName+" "+order;
-
-		Query query = getSession().createQuery(queryString);
-
-		query.setParameter(0, boothId);
-		query.setFirstResult(startIndex);
-		query.setMaxResults(maxRecords);
-
-		return query.list();
 	 
- }  
+	 public List<Voter> getVotersDetailsByBoothId(Long boothId, Integer startIndex,
+				Integer maxRecords, String order, String columnName) {  
+		 
+			String queryString = "select model.voter from BoothPublicationVoter model " +
+					"where model.booth.boothId = ? order by model.voter."+columnName+" "+order;
 
-	public List<Voter> getVotersDetailsForPanchayatByPublicationId(
-			Long panchayatId, Long publicationDateId, Integer startIndex,
-			Integer maxRecords, String order, String columnName) {
+			Query query = getSession().createQuery(queryString);
+
+			query.setParameter(0, boothId);
+			query.setFirstResult(startIndex);
+			query.setMaxResults(maxRecords);
+
+			return query.list();
+		 
+	 }  
+
+		public List<Voter> getVotersDetailsForPanchayatByPublicationId(
+				Long panchayatId, Long publicationDateId, Integer startIndex,
+				Integer maxRecords, String order, String columnName) {
+			
+			Query query = getSession()
+					.createQuery(
+							"select model.voter from BoothPublicationVoter model where " +
+							"model.booth.publicationDate.publicationDateId = :publicationDateId and "+
+							" model.booth.boothId in(select distinct model1.booth.boothId from " +
+							" HamletBoothPublication model1 where model1.booth.publicationDate.publicationDateId = :publicationDateId " +
+							"and  model1.hamlet.hamletId in(select distinct model2.hamlet.hamletId from " +
+							"PanchayatHamlet model2 where model2.panchayat.panchayatId =:panchayatId )) order by model.voter."+columnName+" "+order);
+			
+			query.setParameter("publicationDateId", publicationDateId);
+			query.setParameter("panchayatId", panchayatId);
+			query.setFirstResult(startIndex);
+			query.setMaxResults(maxRecords);
+			return query.list();
+		}
 		
-		Query query = getSession()
-				.createQuery(
-						"select model.voter from BoothPublicationVoter model where " +
-						"model.booth.publicationDate.publicationDateId = :publicationDateId and "+
-						" model.booth.boothId in(select distinct model1.booth.boothId from " +
-						" HamletBoothPublication model1 where model1.booth.publicationDate.publicationDateId = :publicationDateId " +
-						"and  model1.hamlet.hamletId in(select distinct model2.hamlet.hamletId from " +
-						"PanchayatHamlet model2 where model2.panchayat.panchayatId =:panchayatId )) order by model.voter."+columnName+" "+order);
 		
-		query.setParameter("publicationDateId", publicationDateId);
-		query.setParameter("panchayatId", panchayatId);
-		query.setFirstResult(startIndex);
-		query.setMaxResults(maxRecords);
-		return query.list();
+		public List getVotersCountForPanchayatByPublicationId(Long panchayatId,Long publicationDateId){
+			
+			Query query = getSession()
+					.createQuery(
+							"select count(model.voter.voterId) from BoothPublicationVoter model where model.booth.publicationDate.publicationDateId = :publicationDateId and "
+									+ " model.booth.boothId in(select distinct model1.booth.boothId from HamletBoothPublication model1 where model1.booth.publicationDate.publicationDateId = :publicationDateId and "
+									+ " model1.hamlet.hamletId in(select distinct model2.hamlet.hamletId from PanchayatHamlet model2 where model2.panchayat.panchayatId =:panchayatId ))");
+			query.setParameter("publicationDateId", publicationDateId);
+			query.setParameter("panchayatId", panchayatId);
+
+			return query.list();
+			
+		}
+		
+		public List getVotersCountByBoothId(Long boothId){
+			
+			String queryString = "select count(model.voter.voterId) from BoothPublicationVoter model where model.booth.boothId = ?";
+
+			Query query = getSession().createQuery(queryString);
+
+			query.setParameter(0, boothId);
+
+			return query.list();
+			
+			
+		}
+		
+
+	
+		/**
+		 * @return object[]
+		 * @author prasad
+		 * @param constituencyId
+		 */
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getPublicationDetailsBasedOnConstituency(Long constituencyId)
+	{
+		return getHibernateTemplate().find("select distinct model.publicationDate.publicationDateId, " +
+				"model.publicationDate.date from Booth model where " +
+				"model.constituency.constituencyId = ? order by model.publicationDate.date)",constituencyId);
 	}
+	/**
+	 * @return object[]
+	 * @author prasad
+	 * @param PanchayatId
+	 * @param publicationDateId
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Object[]> findImpFamilesBasedOnPanchayat(Long PanchayatId,Long publicationDateId){
+		Object[] params = {PanchayatId,publicationDateId};
+		return getHibernateTemplate().find("select model.voter.houseNo,count(model.voter.houseNo) from " +
+				"BoothPublicationVoter model where model.voter.hamlet.hamletId in" +
+				"(select model1.hamlet.hamletId from PanchayatHamlet model1 " +
+				"where model1.panchayat.panchayatId = ? ) and model.booth.publicationDate.publicationDateId = ? " +
+				" group by model.voter.hamlet.hamletId,model.voter.houseNo", params);
+		}
 	
+	/**
+	 * @return object[]
+	 * @author prasad
+	 * @param constituencyId
+	 * @param publicationDateId
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Object[]> findImpFamilesBasedOnConstituencyId(Long constituencyId,Long publicationDateId){
+		Object[] params = {constituencyId,publicationDateId};
+		return getHibernateTemplate().find("select model.voter.houseNo,count(model.voter.houseNo) from " +
+				"BoothPublicationVoter model where model.booth.constituency.constituencyId = ? and" +
+				" model.booth.publicationDate.publicationDateId = ? " +
+				" group by model.voter.hamlet.hamletId,model.voter.houseNo", params);
+		}
 	
-	public List getVotersCountForPanchayatByPublicationId(Long panchayatId,Long publicationDateId){
-		
-		Query query = getSession()
-				.createQuery(
-						"select count(model.voter.voterId) from BoothPublicationVoter model where model.booth.publicationDate.publicationDateId = :publicationDateId and "
-								+ " model.booth.boothId in(select distinct model1.booth.boothId from HamletBoothPublication model1 where model1.booth.publicationDate.publicationDateId = :publicationDateId and "
-								+ " model1.hamlet.hamletId in(select distinct model2.hamlet.hamletId from PanchayatHamlet model2 where model2.panchayat.panchayatId =:panchayatId ))");
-		query.setParameter("publicationDateId", publicationDateId);
-		query.setParameter("panchayatId", panchayatId);
-
-		return query.list();
+	/**
+	 * @return object[]
+	 * @author prasad
+	 * @param boothId
+	 * @param publicationDateId
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Object[]> findImpFamilesBasedOnBoothId(Long boothId,Long publicationDateId){
+		Object[] parms = {boothId,publicationDateId};
+		return getHibernateTemplate().find("select model.voter.houseNo,count(model.voter.houseNo) from " +
+				"BoothPublicationVoter model where model.booth.boothId= ? and " +
+				"model.booth.publicationDate.publicationDateId = ? " +
+				"group by model.voter.hamlet.hamletId,model.voter.houseNo",parms);
 		
 	}
-	
-	public List getVotersCountByBoothId(Long boothId){
-		
-		String queryString = "select count(model.voter.voterId) from BoothPublicationVoter model where model.booth.boothId = ?";
-
-		Query query = getSession().createQuery(queryString);
-
-		query.setParameter(0, boothId);
-
-		return query.list();
-		
-		
-	}
-	
-
 	
 }
