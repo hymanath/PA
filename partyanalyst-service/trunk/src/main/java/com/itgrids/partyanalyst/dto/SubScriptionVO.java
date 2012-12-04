8dc0b6809e931746d4767c85d34ffc80a1a7a2d3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-04 05:42:18 +0000

    Updated by AnuRadha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/SubScriptionVO.java
@@ -0,0 +1,29 @@
+package com.itgrids.partyanalyst.dto;
+
+public class SubScriptionVO {
+
+	private Long id;
+	private String type;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
