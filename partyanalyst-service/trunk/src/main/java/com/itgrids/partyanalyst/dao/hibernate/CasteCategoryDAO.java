314613dab8d7318a04b688a1da36441ed5cd0a84
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-10 12:48:44 +0000

    Updated by Junior Team

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CasteCategoryDAO.java
@@ -0,0 +1,34 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.ICasteCategoryDAO;
+import com.itgrids.partyanalyst.model.CasteCategory;
+
+public class CasteCategoryDAO  extends GenericDaoHibernate<CasteCategory, Long> implements 
+ICasteCategoryDAO{
+	
+	public CasteCategoryDAO() {
+		super(CasteCategory.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public List<Object[]> getCasteCategoryDetails(){
+		
+		Query query = getSession().createQuery("select model.casteCategoryId,model.categoryName from CasteCategory model");
+		
+		return query.list();
+	
+	}
+	public List<Object[]> getAllCasteCategoryDetails(){
+		
+		Query query = getSession().createQuery("select model.casteCategoryId , model.categoryName from CasteCategory model");
+		return query.list();
+		
+		
+	}
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CasteCategoryGroupDAO.java
@@ -0,0 +1,53 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.ICasteCategoryGroupDAO;
+import com.itgrids.partyanalyst.model.CasteCategoryGroup;
+
+public class CasteCategoryGroupDAO extends GenericDaoHibernate<CasteCategoryGroup, Long> implements 
+ICasteCategoryGroupDAO{
+
+	public CasteCategoryGroupDAO(){
+		super(CasteCategoryGroup.class);
+	}
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getCasteCategoryGroupNames(Long casteCategoryId){
+		
+		Query query = getSession().createQuery("select model.casteCategoryGroupId,model.casteCategoryGroupName from CasteCategoryGroup model where model.casteCategory.casteCategoryId = ?");
+		query.setParameter(0,casteCategoryId);
+		System.out.println("value in CasteCategoryGroupNames:"+query.list());
+		return query.list();
+	
+	}
+@SuppressWarnings("unchecked")
+public List<Object[]> getAllCasteCategoryGroupDetails(Long casteCategoryId){
+		
+		Query query = getSession().createQuery("select model.casteCategoryGroupId , model.casteCategoryGroupName from CasteCategoryGroup model where model.casteCategory.casteCategoryId=?");
+		query.setParameter(0,casteCategoryId);
+		return query.list();
+		
+		
+	}
+@SuppressWarnings("unchecked")
+public List<Object[]> getAllCasteCategoryGroupInfoDetails(){
+	
+	Query query = getSession().createQuery("select model.casteCategoryGroupId , model.casteCategoryGroupName from CasteCategoryGroup model");
+	
+	return query.list();
+	
+	
+}
+
+@SuppressWarnings("unchecked")
+public Long getCasteNamesOfCategories(Long casteCategoryId){
+	
+Query query=getSession().createQuery("select model.casteCategoryGroupId from CasteCategoryGroup model where model.casteCategory.casteCategoryId=?");
+query.setParameter(0,casteCategoryId);
+return (Long)query.uniqueResult();
+}
+	
+}
