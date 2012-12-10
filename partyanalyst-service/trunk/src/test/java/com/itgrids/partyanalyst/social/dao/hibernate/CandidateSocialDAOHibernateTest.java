68d4a1a552ac2d31ef0df37d5e45d1a136fc0c9f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-10 12:53:33 +0000

    Updated by Junior Team

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/social/dao/hibernate/CandidateSocialDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/social/dao/hibernate/CandidateSocialDAOHibernateTest.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.social.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.social.dao.ICandidateSocialDAO;
+import com.itgrids.partyanalyst.social.model.CandidateSocial;
 
 public class CandidateSocialDAOHibernateTest extends BaseDaoTestCase{
 
@@ -15,5 +18,10 @@ public class CandidateSocialDAOHibernateTest extends BaseDaoTestCase{
 	public void test()
 	{
 		candidateSocialDAO.getAll();
+	}
+	
+	public void testgetCandidateUrlDetails(){
+		List<CandidateSocial> list=candidateSocialDAO.getCandidateUrlDetails(4975l);
+		System.out.println(list.size());
 	}
 }
