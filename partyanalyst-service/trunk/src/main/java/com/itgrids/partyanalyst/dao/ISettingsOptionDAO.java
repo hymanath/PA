5d5958d73235fe66157ae3f6395e8f0328596bb4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-15 05:47:43 +0000

    Updated by Nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISettingsOptionDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.SettingsOption;
+
+public interface ISettingsOptionDAO extends GenericDao<SettingsOption, Long>{
+
+}
