71e53c12186bfa6ea3dab44df7465025cf243261
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-15 10:39:47 +0000

    Updated by Nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IJobDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.Job;
+
+public interface IJobDAO extends GenericDao<Job, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IJobRunDetailsDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.Job;
+import com.itgrids.partyanalyst.model.JobRunDetails;
+
+public interface IJobRunDetailsDAO extends GenericDao<JobRunDetails, Long>{
+
+}
