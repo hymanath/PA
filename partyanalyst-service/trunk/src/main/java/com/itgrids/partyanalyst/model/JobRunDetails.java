943784e98ef2edeee97a01c3644f94f02a94e17d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-15 10:40:15 +0000

    Updated by Nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/JobRunDetails.java
@@ -0,0 +1,99 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+@Entity
+@Table(name="job_run_details")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+
+public class JobRunDetails implements Serializable{
+	
+	private Long jobRunDetailsId;
+	
+	private Job job;
+	
+	private Date startTime;
+	
+	private Date endTime;
+	
+	private String status;
+	
+	//Default Constructor
+	
+	public JobRunDetails()
+	{
+		
+	}
+	
+	//Full Constructor
+	public JobRunDetails(Long jobRunDetailsId,Job job,Date startTime,Date endTime,String status)
+	{
+		
+		this.jobRunDetailsId = jobRunDetailsId;
+		this.job = job;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.status = status;
+		
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "job_run_details_id", unique = true, nullable = false)
+	public Long getJobRunDetailsId() {
+		return jobRunDetailsId;
+	}
+
+	public void setJobRunDetailsId(Long jobRunDetailsId) {
+		this.jobRunDetailsId = jobRunDetailsId;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "job_id")
+	public Job getJob() {
+		return job;
+	}
+
+	public void setJob(Job job) {
+		this.job = job;
+	}
+	
+	@Column(name = "start_time", length = 10)
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	@Column(name = "end_time", length = 10)
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	@Column(name="status", length = 45)
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
