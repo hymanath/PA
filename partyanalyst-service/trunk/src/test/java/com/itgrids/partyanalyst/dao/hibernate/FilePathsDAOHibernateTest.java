930cae0ef082f7a6ca1ed38f4e5cd5edd522c588
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-17 13:53:26 +0000

    updated by anil

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FilePathsDAOHibernateTest.java
@@ -1,11 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IFilePathsDAO;
-import com.itgrids.partyanalyst.utils.IConstants;
 
 public class FilePathsDAOHibernateTest extends BaseDaoTestCase {
    
@@ -31,8 +32,17 @@ public class FilePathsDAOHibernateTest extends BaseDaoTestCase {
 		System.out.println(list.size());
 	}*/
 	
-	public void testGetFilePaths()
+	/*public void testGetFilePaths()
 	{
 		System.out.println(filePathsDAO.getFilePaths(15343l));
+	}*/
+	public void testGetFilePathsBasedOnFileTypeIds()
+	{long[] ids= {1,2,5,10,11,24,25,35};
+	List<Long> filetypeIds =new ArrayList<Long>();
+	for (int index = 0; index < ids.length; index++)
+    {
+    	filetypeIds.add(ids[index]);
+    }
+		System.out.println((filePathsDAO.getFilePathsBasedOnFileTypeIds(filetypeIds)).size());
 	}
 }
