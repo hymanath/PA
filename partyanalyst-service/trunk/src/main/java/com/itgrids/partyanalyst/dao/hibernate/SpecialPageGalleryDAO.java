c88fb2d2db9c52584e89f042f912450fdfe4f438
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-17 13:46:42 +0000

    updated by anil

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SpecialPageGalleryDAO.java
@@ -1,14 +1,22 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.itgrids.partyanalyst.dao.ISpecialPageGalleryDAO;
 import com.itgrids.partyanalyst.model.File;
+import com.itgrids.partyanalyst.model.FilePaths;
 import com.itgrids.partyanalyst.model.SpecialPage;
 import com.itgrids.partyanalyst.model.SpecialPageGallery;
+import com.itgrids.partyanalyst.utils.DateUtilService;
 
 public class SpecialPageGalleryDAO extends GenericDaoHibernate<SpecialPageGallery,Long> implements ISpecialPageGalleryDAO{
 
@@ -139,4 +147,141 @@ public class SpecialPageGalleryDAO extends GenericDaoHibernate<SpecialPageGaller
 		
 		return query.list();
 	}
+	
+	      // MyMethods
+public List<File> getExpiredVideosList1( String contentType){
+		
+		
+		
+		Query query = getSession().createQuery("  select  model from File model where model.fileId in (select model1.file.fileId from FileGallary model1" +
+				" where model1.gallary.gallaryId in( select model2.gallaryId from Gallary model2 where model2.contentType.contentType =:contentType and  model2.isDelete =:isDelete " +
+				" and model2.isPrivate =:isPrivate) and model1.isDelete =:isDelete and model1.isPrivate =:isPrivate ) order by model.fileDate ASC");
+		
+		//query.setParameter("specialPageId", specialPageId);
+		query.setParameter("contentType", contentType);
+		query.setParameter("isDelete", "false");
+		query.setParameter("isPrivate", "false");
+		//query.setFirstResult(firstRecord);
+		//query.setMaxResults(maxRecord);
+		return query.list();
+		
+	}
+public List<Object[]> getExpiredVideosList( Date from, Date to ,String contentType  ){
+	
+	String from1="2011-10-30";
+	String to1="2011-12-30";
+	
+	Query query = getSession().createQuery("select  model.filePath,model.fileSourceLanguage.fileSourceLanguageId from FilePaths model where model.fileType.fileTypeId " +
+			                                "in (select model2.fileTypeId from FileType model2 where model2.type=:contentType) " +
+			                                "and model.fileSourceLanguage.fileSourceLanguageId " +
+			                                "in (select model3.fileSourceLanguageId from FileSourceLanguage  model3 where model3.file.fileId " +
+			                                "in( select model5.file.fileId from  FileGallary model5 where DATE(model5.updateddate)  BETWEEN :startDate AND :toDate " +
+			                                "and model5.isDelete =:isDelete and model5.isPrivate =:isPrivate  )) " +
+			                               " and  model.lastUpdatedDate < :today " );
+	
+	//query.setParameter("specialPageId", specialPageId); and model4.updateddate <=:toDate and lastVerifiedDate < :today  
+	 query.setParameter("startDate",from );
+	 query.setParameter("toDate",to );
+     query.setParameter("today", new DateUtilService().getDayBeforeYesterDayDate());
+	 query.setParameter("contentType", contentType);
+     query.setParameter("isDelete", "false");
+	 query.setParameter("isPrivate", "false");
+     query.setFirstResult(0);
+	 query.setMaxResults(50);
+	
+	  return query.list();
+	
+}
+
+  
+ /* public int  markCheckedVideos( List<String> paths){
+	  Session session=getSession();
+	  Transaction tx=session.beginTransaction();
+	
+	  Query query = session.createQuery(" update FilePaths model set model.lastVerifiedDate=:newDate where model.file_paths in(:paths)").setParameterList("paths", paths);
+
+	  return  query.executeUpdate();
+
+		
+	}*/
+  public List<Object> getFileSourceLanguageIds(List<String> filePaths)
+	{
+		Query query = getSession().createQuery("select model.fileSourceLanguage.fileSourceLanguageId from  FilePaths model " +
+				                                "where model.filePath in(:filePaths)").setParameterList("filePaths", filePaths);
+		
+		
+		return query.list();
+	}
+  public List<Object> getFileIds(List<Object> languageIds)
+ 	{
+ 		Query query = getSession().createQuery("select model.file.fileId  from  FileSourceLanguage  model  " +
+ 				                                 "where model.fileSourceLanguageId in(:languageIds)").setParameterList("languageIds", languageIds);
+ 		
+ 		
+ 		return query.list();
+ 	}
+  
+  
+  
+  public int[] deleteYoutubeVideoRecords(List<String> filePaths ,List<Object> fileIds, List<Object> fileSourceLanguageIds, List<Long> languageIds){
+		int[] count=null;
+	    int[] results=null;
+	 // Session session=getSession();
+		// Transaction tx=session.beginTransaction();
+	             
+	   return null;
+	}
+  public int  updateLastUpdateDateInFilePaths(List<Long> languageIds)
+  {
+	  Query query = getSession().createQuery("update  FilePaths model set  model.lastUpdatedDate=:today where  model.fileSourceLanguage.fileSourceLanguageId in(:languageIds)"); 
+      query.setParameter("today", new DateUtilService().getCurrentDateAndTime()); 
+      query.setParameterList("languageIds", languageIds); 
+      return  query.executeUpdate();
+  }
+  public int  deleteRecordsFromFilePath(List<String> filePaths)
+  {
+	   Query query = getSession().createQuery("delete  from FilePaths model where model.filePath in(:filePaths)").setParameterList("filePaths", filePaths); 
+	   return query.executeUpdate();
+  }
+  public int  deleteRecordsFromFileSourceLanguage(List<Object> fileSourceLanguageIds)
+  {
+		 Query query = getSession().createQuery("delete  from  FileSourceLanguage model where model.fileSourceLanguageId in (:fileSourceLanguageIds)").setParameterList("fileSourceLanguageIds", fileSourceLanguageIds);
+		 return query.executeUpdate();
+  }
+  public int  deleteRecordsFromFileGallary(List<Object> fileIds)
+  {
+	   Query query = getSession().createQuery("delete from  FileGallary model  where  model.file.fileId in (:fileID)");  //.setParameterList("fileID", fileIds); 
+       //query.setParameter("isDelete", "true"); 
+       query.setParameterList("fileID", fileIds);
+       return query.executeUpdate();
+  }
+  public int   deleteRecordsFromFile(List<Object> fileIds)
+  {      
+	    List<Object> fileIds1=  verifyBatchVideos(fileIds);
+		 Query query = getSession().createQuery("delete  from   File model where model.fileId in(:fileIds)").setParameterList("fileIds", fileIds1);
+		return query.executeUpdate();
+  }
+  
+  public List<Object> verifyBatchVideos(List<Object> fileIds)
+  {
+	  List<Object> list=new ArrayList<Object>();
+	  Query query=null;
+	   for(int i=0;i<fileIds.size();i++)
+	   {
+		  query = getSession().createQuery("select count(*)  from  FileSourceLanguage model where model.file.fileId = :fileID ").setParameter("fileID", fileIds.get(i));
+          List<Long> cnt=query.list();
+          
+          if(cnt.get(0).intValue()<1)
+        	  list.add(fileIds.get(i));
+		 
+		 
+	 }
+	  return list;
+  }
+  
+  
+  
+  
+  
+
 }
