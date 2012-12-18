5eef290cad6250e8b999332c3974ae0b0c684a9c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-18 09:37:51 +0000

    favorite  links implementation - samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFavoriteLinkPageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFavoriteLinkPageDAO.java
@@ -1,9 +1,17 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.model.FavoriteLinkPage;
 
 public interface IFavoriteLinkPageDAO extends GenericDao<FavoriteLinkPage, Long>{
+	
+
+	public List<FavoriteLinkPage> getFavoriteLinkByActionName(String link);
+	
+	
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FavoriteLinkPageDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FavoriteLinkPageDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IFavoriteLinkPageDAO;
 import com.itgrids.partyanalyst.model.FavoriteLinkPage;
@@ -10,6 +13,18 @@ public class FavoriteLinkPageDAO  extends GenericDaoHibernate<FavoriteLinkPage, 
 	
 	public FavoriteLinkPageDAO(){
 		super(FavoriteLinkPage.class);
+		
+	}
+	
+	
+	public List<FavoriteLinkPage> getFavoriteLinkByActionName(String link){
+		
+		Query query =getSession().createQuery("select model from FavoriteLinkPage model where model.url = ?");
+		
+		
+		query.setParameter(0, link);
+		
+		return query.list();
 		
 	}
 
