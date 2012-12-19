42e66bc11f05617d6a591f0d16380261f9d7f773
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-19 15:05:18 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ThumbnailService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ThumbnailService.java
@@ -45,7 +45,7 @@ public void setFilePathsDAO(IFilePathsDAO filePathsDAO) {
 	this.filePathsDAO = filePathsDAO;
 }
 
-  @Override
+  
 	public String  crateThumnailForAdmin(int[] ids1,String realContextPath)
 	
 	{ 
@@ -114,7 +114,7 @@ public void setFilePathsDAO(IFilePathsDAO filePathsDAO) {
 	      return flag;
 	}
 
-@Override	
+
 public void  crateThumnailDynamically( List<String> filePaths, String realContextPath, int width ,int height   ) 
 	
 	{ 
@@ -215,7 +215,7 @@ log.debug("profiles thumnail creation");
    		}
 	
 	
-	@Override
+	
 	public  String  resizeImages(String path, String destinationSize, int width ,int height ,File f2,String realContextPath) 
 	
 	{
