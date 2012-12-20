6f8603622aa794a66f046f98d01a0d158170f2a1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-20 05:05:21 +0000

    Updated by sravanthi

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/UserVoterDetails.java
@@ -0,0 +1,83 @@
+package com.itgrids.partyanalyst.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "user_voter_details")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class UserVoterDetails implements java.io.Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long userVoterDetailsId;
+	private User user;
+	private Voter voter;
+	private Party party;
+	
+	public UserVoterDetails(){
+	}
+	
+	public UserVoterDetails(Long userVoterDetailsId,User user,Voter voter,Party party){
+		this.userVoterDetailsId=userVoterDetailsId;
+		this.user=user;
+		this.voter=voter;
+		this.party=party;
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="user_voter_details_id", unique = true, nullable = false)
+	public Long getUserVoterDetailsId() {
+		return userVoterDetailsId;
+	}
+	public void setUserVoterDetailsId(Long userVoterDetailsId) {
+		this.userVoterDetailsId = userVoterDetailsId;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="voter_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Voter getVoter() {
+		return voter;
+	}
+	public void setVoter(Voter voter) {
+		this.voter = voter;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="party_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Party getParty() {
+		return party;
+	}
+	public void setParty(Party party) {
+		this.party = party;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
