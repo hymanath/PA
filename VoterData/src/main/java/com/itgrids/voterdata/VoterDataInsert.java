155c318499399ad6f08a5890dc5c8e3b4f53f9b1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-23 15:59:20 +0000

    Updated by Kamalakar

---
+++ VoterData/src/main/java/com/itgrids/voterdata/VoterDataInsert.java
@@ -0,0 +1,151 @@
+package com.itgrids.voterdata;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class VoterDataInsert {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL_TEMP = "jdbc:mysql://localhost/itgrids";
+	//static final String DB_URL_LIVE = "jdbc:mysql://199.85.212.11/dakavara_pa";
+	static final String DB_URL_LIVE = "jdbc:mysql://localhost/dakavara_pa";
+	static final String USER_TEMP = "root";
+	static final String PASSWORD_TEMP = "root";
+	
+	static final String USER_LIVE = "root";
+	static final String PASSWORD_LIVE = "root";
+	//static final String USER_LIVE = "dakavara_pa";
+	//static final String PASSWORD_LIVE = "1tGrids@123";
+	
+	static Connection connectionTemp = null;
+	static Connection connectionLive = null;
+	static Statement stmtTemp = null;
+	static Statement stmtLive = null;
+	
+	public static void main(String[] args)
+	{
+		VoterDataInsert voterDataInsert = new VoterDataInsert();
+		System.out.println("----- Total Voters Inserted ----->"+voterDataInsert.insertVoterData(299, 6, 86117, 100000));
+	}
+	
+	public int insertVoterData(int constituencyId, int publicationId,int startIndex,int maxResults)
+	{
+		try{
+			Class.forName(JDBC_DRIVER);
+    		System.out.println("Connecting to database...");
+    		connectionTemp = DriverManager.getConnection(DB_URL_TEMP,USER_TEMP,PASSWORD_TEMP);
+    		connectionLive = DriverManager.getConnection(DB_URL_LIVE,USER_LIVE,PASSWORD_LIVE);
+    		
+    		stmtTemp = connectionTemp.createStatement();
+    		stmtLive = connectionLive.createStatement();
+    		
+    		ResultSet rs = stmtTemp.executeQuery(" select voter_id,name,house_no,guardian_name,relation,sex,age, " +
+    				" booth_id from voter where constituency_id = "+constituencyId+" limit "+startIndex+","+maxResults);
+    		
+    		String voterId = null;
+    		String name = null;
+    		String houseNo = null;
+    		String relativeName = null;
+    		String relationType = null;
+    		String gender = null;
+    		int age = 0;
+    		int index = 0;
+    		int index2 = 1;
+    		
+    		List<VoterInfo> voterDeials = new ArrayList<VoterInfo>(0);
+    		VoterInfo voter = null;
+    		System.out.println(new Date());
+    		while(rs.next())
+    		{
+    			try{
+    			voter = new VoterInfo();
+    			voterId = rs.getString("voter_id");
+    			name = rs.getString("name");
+    			houseNo = rs.getString("house_no");
+    			relativeName = rs.getString("guardian_name");
+    			relationType = rs.getString("relation");
+    			gender = rs.getString("sex");
+    			age = Integer.parseInt(rs.getString("age"));
+    			
+    			voter.setBoothNo(rs.getString("booth_id"));
+    			voter.setVoterId(voterId);
+    			voterDeials.add(voter);
+    			
+    			if(gender.equalsIgnoreCase("Male"))
+    				gender = "M";
+    			else
+    				gender = "F";
+    			
+    			String insertQuery = "INSERT INTO voter(voter_id_card_no,name,house_no,relative_name,relationship_type,gender,age,inserted_time) " +
+        				" VALUES ('"+voterId+"','"+name+"','"+houseNo+"','"+relativeName+"','"+relationType+"','"+gender+"',"+age+",now())";
+    			
+    			stmtLive.executeUpdate(insertQuery);
+    			index++;
+    			System.out.println(index+")\tVoter Inserted with Voter ID - "+voterId);
+    			    			
+    			}catch (Exception e) {
+    				e.printStackTrace();
+    			}
+    		}
+    		
+    		
+    		for(VoterInfo voterInfo : voterDeials)
+    		{
+    			try{
+    			Statement st = connectionLive.createStatement();
+        		ResultSet rs2 = st.executeQuery(" select booth_id from booth where constituency_id = "+constituencyId+" and part_no = "+voterInfo.getBoothNo()+"" +
+        				" and publication_date_id = "+publicationId);
+        		int boothId = 0;
+        		int vId = 0;
+        		
+        		while(rs2.next())
+        		{
+        			boothId = rs2.getInt("booth_id");
+        		}
+        		
+        		Statement st2 = connectionLive.createStatement();
+        		ResultSet rs3 = st2.executeQuery("select voter_id from voter where voter_id_card_no = '"+voterInfo.getVoterId()+"'");
+        		
+        		while(rs3.next())
+        		{
+        			vId = rs3.getInt("voter_id");
+        		}
+        		
+        		String insertQuery = "INSERT INTO booth_publication_voter(booth_id,voter_id) VALUES("+boothId+","+vId+")"; 
+        		Statement st4 = connectionLive.createStatement();
+        		st4.executeUpdate(insertQuery);
+        		index2++;
+        		System.out.println(index2+")\tVoter Inserted in Booth Publication with Voter ID - "+vId);
+        		        		
+    			}catch (Exception e) {
+        			e.printStackTrace();
+        		}
+    		}
+    		
+    		System.out.println("-------------- **************** -------------------------");
+    		System.out.println("Voters inserted are --> "+index+"");
+    		System.out.println("Voters inserted BP are --> "+index2+"");
+    		System.out.println("-------------- **************** -------------------------");
+    		System.out.println(new Date());
+    		
+			return index;
+		}catch (Exception e) {
+			e.printStackTrace();
+    		return 0;
+		}
+		finally{
+    		try {
+    			connectionLive.close();
+    			connectionTemp.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+    	}
+	}
+}
