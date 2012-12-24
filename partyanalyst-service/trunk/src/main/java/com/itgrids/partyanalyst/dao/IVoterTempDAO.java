4cac9824d6943ee0a6c212203f68101b67e76de6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-24 12:12:57 +0000

    Updated by Kamalakar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterTempDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.VoterTemp;
+
+public interface IVoterTempDAO extends GenericDao<VoterTemp,Long>{
+
+}
