8381504dff521856b3de3188a06d5c65ab0d4c4d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-26 15:04:24 +0000

    Updated by AnuRadha

--- partyanalyst/trunk/src/main/webapp/js/publicProfile/publicProfilePage.js
+++ partyanalyst/trunk/src/main/webapp/js/publicProfile/publicProfilePage.js
@@ -1,13 +1,13 @@
 
 var friendsInPP = [];
-
+var startIndex;
 $("document").ready(function(){
 	
 	$('.friendsInPP').click(function(){
 		$(".placeholderCenterDiv").children().remove();
 		$(".placeholderCenterDiv").css('display','inline-block');
 		$("#problemsDiv").children().remove();
-		//$('.friendsDiv').css("display","none");
+
 		for(var i in friendsInPP)
 		{
 		   var name = friendsInPP[i].firstName+" "+friendsInPP[i].lastName;
@@ -24,15 +24,19 @@ $("document").ready(function(){
 	    }
 	});
 
-
-	$("#specialPageLink").click(function(){
-		
-		var jsObj ={
-			task:"getSpecialPages"
+	 $(".problemLink").click(function(){
+		var linkType = "problemLink";
+		startIndex = 0;
+	
+		 var jsObj ={
+				startIndex   : startIndex,
+				maxIndex     : 10,
+				linkType     : linkType,
+				task:"getProblemDetails"
 		};
 		var rparam ="task="+YAHOO.lang.JSON.stringify(jsObj);
-		var url = "getSpecialPageForPublicProfileAction.action?"+rparam;	
-		callAjax1(jsObj,url);
+		var url = "getProblemDetailsForPublicProfileAction.action?"+rparam;	
+		callAjaxForProfilePage(jsObj,url);
 	});
 
 
@@ -40,7 +44,7 @@ $("document").ready(function(){
 
 
 
-function callAjax1(jsObj,url){
+function callAjaxForProfilePage(jsObj,url){
 	var results;	
 	var callback = {			
 	    success : function( o ) {
@@ -48,15 +52,16 @@ function callAjax1(jsObj,url){
 					
 					results = YAHOO.lang.JSON.parse(o.responseText);
 					
-					 if(jsObj.task == "getSpecialPages")
-					{
-						showSpecialPages(results);
-					}
-					
-					
-					
-					
-			}catch (e) {   		
+					if(jsObj.task == "getStreamingData")
+						showStreamingData(results,jsObj);
+
+					else if(jsObj.task == "getProblemDetails")
+						showProblemDetails(results,jsObj);
+					 
+
+
+			}catch (e) {  
+                  $("#subscriptionsStreamingAjaxImg").hide();			
 			   	//alert("Invalid JSON result" + e);   
 			}  
 	    },
@@ -69,26 +74,101 @@ function callAjax1(jsObj,url){
 	YAHOO.util.Connect.asyncRequest('GET', url, callback);
 }
 
-function showSpecialPages(results)
+function showProblemDetails(results,jsObj)
 {
-	$("#headerDiv").html('');
-	$(".placeholderCenterDiv").children().remove();
-	clearAllSubscriptionDivs();
-	if(results.length == 0)
+	$("#problemsDiv").children().remove();
+	if(results == null || results.length == 0)
 	{
-		$(".templateDiv").html("Special Pages Are Not Available.");
-		return;
+
 	}
 	for(var i in results)
 	{
-		var template = $(".templateDiv");
-		var templateClone = template.clone();
-		templateClone.removeClass('templateDiv');
-		templateClone.find(".connectedPersonName").html(''+results[i].title+'');
-		templateClone.find(".imgClass").html('<img src="'+results[i].eventImagePath+'"/>');
-		templateClone.find(".constituencyName").html(''+results[i].title+'');
-		templateClone.appendTo(".placeholderCenterDiv");
+	  var problemList = results[i].problemBeanVOList;
+	  var postedDate = results[i].postDate;
+		
+		for(var j in problemList)
+		{
+		  var template = $(".problemTemplate");
+		  var templateClone = template.clone();
+		  templateClone.removeClass("problemTemplate");
+		  templateClone.find('.problemTitle').html(''+problemList[j].problem+'');
+		  templateClone.find('.problemDescription').html(''+problemList[j].description+'');
+		  templateClone.appendTo('.placeholderCenterDiv');
+		}
 	}
 }
 
+function executeOnload()
+{
+	startIndex = 0;
+	
+	var jsObj ={
+		startIndex   : startIndex,
+		maxIndex     : 10,
+		profileId    :profileId,
+		task:"getStreamingData"
+		};
+	var rparam ="task="+YAHOO.lang.JSON.stringify(jsObj);
+	var url = "getStreamingDataForProfileUserAction.action?"+rparam;	
+	callAjaxForProfilePage(jsObj,url);
+}
 
+function showStreamingData(results,jsObj)
+{
+	$("#problemsDiv").children().remove();
+	var status;
+	if(results != null)
+	{
+		for(var i in results)
+		{
+			var postedDate = $('<div></div>');
+			postedDate.append('<span>'+results[i].postedDate+'</span>');
+			postedDate.appendTo('.placeholderCenterDiv');
+			var list = results[i].problemBeanVOList;
+			for(var j in list)
+			{
+				
+				 if(list[j].responseType == "Problems")
+				{
+					var template = $('.problemTemplate');
+					var templateClone = template.clone();
+					templateClone.removeClass('problemTemplate');
+					templateClone.find('.problemImg').html('<img src="pictures/profiles/'+list[j].userImageURL+'" class="span1"/>');
+					templateClone.find('.postedPersonName').html(''+list[j].postedPersonName+' Posted');
+					templateClone.find('.problemTitle').html(''+list[j].problem+'');
+					templateClone.find('.problemDescription').html(''+list[j].description+'');
+					templateClone.find('.problemFromDate').html('Existing From: '+list[j].existingFrom+'');
+					
+					templateClone.appendTo('.placeholderCenterDiv');
+				}
+
+				
+				if(list[j].responseType == "Comments")
+				{
+					var imageStr = "/PartyAnalyst/images/candidates/"+list[j].name;
+					var template = $('.politicalReasonsTemplate');
+					var templateClone = template.clone();
+					templateClone.removeClass('politicalReasonsTemplate');
+					templateClone.find('.postedBy').html('<img src="pictures/profiles/'+list[j].userImageURL+'" class="span1"/>');
+					
+					if(list[j].problemImpactLevelId == 1)
+						status = "winning";
+					else
+						status = "losing";
+					templateClone.find('.headingCls').html('<h5>'+list[j].postedPersonName+'</h5> Posted a Political Reason for '+list[j].name+' '+status+' '+list[j].constituency+' '+list[j].problemType+' constituency');
+					templateClone.find('.candidateImg').html('<img src="'+imageStr+'.jpg" onerror="setDefaultImage(this)" style="width:100px;height:100px;vertical-align:middle;"></img>');
+					templateClone.find('.politicalReaCls').html('<b>Political Reason:</b> '+list[j].problem+'');
+					templateClone.find('.politicalDescCls').html('<b>Description:</b> '+list[j].description+'');
+					templateClone.find('.polReaPostedDate').html('Posted On: '+list[j].postDate+'');
+					templateClone.appendTo('.placeholderCenterDiv');
+				}
+			}
+		}
+	}
+
+}
+
+function setDefaultImage(img)
+{
+		img.src = "images/candidates/human.jpg";
+}
