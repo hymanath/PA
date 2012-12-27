78061efcbbb29d7d4096ac55a2bef4ad06c52da6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-27 17:41:21 +0000

    updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICategoryValuesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICategoryValuesDAO.java
@@ -9,4 +9,6 @@ import com.itgrids.partyanalyst.model.CategoryValues;
 public interface ICategoryValuesDAO  extends GenericDao<CategoryValues, Long>{
 
 	public List<Object[]> getVoterCategoryValues(Long voterCategoryId);
+	
+	public List<CategoryValues> getCategoryValues();
 }
