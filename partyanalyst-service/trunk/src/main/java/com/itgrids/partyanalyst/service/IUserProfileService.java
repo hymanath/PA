70ab4a435083318a7c48cac578a1b6af7420eedd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-28 05:43:47 +0000

    Updated by AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IUserProfileService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IUserProfileService.java
@@ -10,4 +10,6 @@ public interface IUserProfileService {
 	public List<UserProfileVO> getPartyAnalystLatestUpdates(Date fromDate,Date toDate,Long userId);
 	
 	public List<ProblemBeanVO> getStreamingDataForPublicProfileByProfileId(Long userId,int startIndex, int maxIndex);
+	
+	public String getUserConnectStatus(Long profileId, Long userId);
 }
