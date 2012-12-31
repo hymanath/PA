12da587b3855b29b0a99aeab05e9df4b070b2960
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-31 12:12:57 +0000

    Updated by Sravanthi

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterCategoryValuesDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterCategoryValuesDAOHibernateTest.java
@@ -2,7 +2,10 @@ package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.BaseDaoTestCase;
 
+import antlr.collections.List;
+
 import com.itgrids.partyanalyst.dao.IVoterCategoryValuesDAO;
+import com.itgrids.partyanalyst.model.VoterCategoryValues;
 
 public class VoterCategoryValuesDAOHibernateTest  extends BaseDaoTestCase{
 
@@ -13,5 +16,9 @@ public class VoterCategoryValuesDAOHibernateTest  extends BaseDaoTestCase{
 		this.voterCategoryValuesDAO = voterCategoryValuesDAO;
 	}
 	
+	public void testgetVoterCategoryValues1(){
+		java.util.List<VoterCategoryValues> list=voterCategoryValuesDAO.getVoterCategoryValues1();
+		System.out.println(list.size());
+	}
 	
 }
