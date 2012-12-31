a2b11618a5d6346624ed0a9fb0c4a9eeb2f21b7a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-31 12:15:38 +0000

    Updated by Sravanthi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterCategoryValuesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterCategoryValuesDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.VoterCategoryValues;
 
 public interface IVoterCategoryValuesDAO extends GenericDao<VoterCategoryValues, Long>{
-
+	
+	//public void flushAndclearSession();
+	public List<VoterCategoryValues> getVoterCategoryValues1();
 }
