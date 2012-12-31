deade86b5f372dcbe9d8d0d70771e1e707710ea5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-31 12:15:19 +0000

    Updated by Sravanthi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterCategoryValuesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterCategoryValuesDAO.java
@@ -1,7 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
 import com.itgrids.partyanalyst.dao.IVoterCategoryValuesDAO;
+import com.itgrids.partyanalyst.model.CategoryValues;
 import com.itgrids.partyanalyst.model.VoterCategoryValues;
 
 public class VoterCategoryValuesDAO extends GenericDaoHibernate<VoterCategoryValues, Long> implements IVoterCategoryValuesDAO{
@@ -9,6 +14,18 @@ public class VoterCategoryValuesDAO extends GenericDaoHibernate<VoterCategoryVal
 	public VoterCategoryValuesDAO() {
 		super(VoterCategoryValues.class);
 		// TODO Auto-generated constructor stub
+	}
+	
+	/*public void flushAndclearSession(){
+		getSession().flush();
+		getSession().clear();
+	}
+	*/
+	@SuppressWarnings("unchecked")
+	public List<VoterCategoryValues> getVoterCategoryValues1(){
+		/*Query queryObject = getSession().createQuery("select model.voter.voterId from VoterCategoryValues model");
+		return queryObject.list();*/
+		return getHibernateTemplate().find("from VoterCategoryValues model");
 	}
 
 }
