a8172423084256e945dfba689886ff27bb808b23
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2012-12-31 12:13:46 +0000

    Updated by Sravanthi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/SelectOptionVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/SelectOptionVO.java
@@ -8,7 +8,26 @@ public class SelectOptionVO implements Serializable, Comparable<SelectOptionVO> 
 	private String name;
 	private Long id;
 	private String url;
+	private String type;
+	private String value;
 	
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 	public String getUrl() {
 		return url;
 	}
