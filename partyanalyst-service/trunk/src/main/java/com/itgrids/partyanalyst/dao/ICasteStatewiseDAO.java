63734d45d1967fad8c8941af99f61574a1c430f1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-02 11:08:01 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICasteStatewiseDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICasteStatewiseDAO.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
-import com.itgrids.partyanalyst.model.CasteStatewise;
+import com.itgrids.partyanalyst.model.CasteState;
 
-public interface ICasteStatewiseDAO extends GenericDao<CasteStatewise, Long>{
+public interface ICasteStatewiseDAO extends GenericDao<CasteState, Long>{
 	
 	public List<Object[]> getStatewiseCastNames(Long casteCategoryGroupId);
 	 public List<Object[]> getCasteNamesByAutoPopulate(Long stateId,String letters) ;
