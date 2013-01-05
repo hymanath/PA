f2ad86ee15823bfeab1a68f52c6f5ede82489a3a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-05 14:26:49 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
@@ -44,7 +44,8 @@ public interface IVotersAnalysisService {
 	public List<VotersDetailsVO> getAgewiseVotersDetailsForBoothsByLocalElectionBodyId(Long localElectionBodyId,Long publicationDateId);
 	public List<VotersDetailsVO> getAgewiseVotersDetailsForTehsilsByConstituencyId(Long constituencyId,Long publicationDateId);
 	
-	public List<VoterHouseInfoVO> getVoterDetailsByCaste(Long id,Long publicationDateId,String caste);
+    //public List<VoterHouseInfoVO> getVoterDetailsByCaste(Long id,Long publicationDateId,String caste);
+	public List<VoterHouseInfoVO> getVoterDetailsByCaste(Long id,Long publicationDateId,Long casteStateId,String type);
 
 	public VoterHouseInfoVO getVoterPersonalDetailsByVoterId(Long voterId,Long user);
 	
