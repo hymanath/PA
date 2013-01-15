527718d4f912fee02b81de16dc330de9afed6851
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-15 14:17:27 +0000

    Updated by Nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAOHibernateTest.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IUserVoterDetailsDAO;
@@ -12,10 +15,21 @@ public class UserVoterDetailsDAOHibernateTest extends BaseDaoTestCase  {
 		this.userVoterDetailsDAO = userVoterDetailsDAO;
 	}
 
-	public void testgetUserVoterDetails()
+	/*public void testgetUserVoterDetails()
 	{
 		Object object=userVoterDetailsDAO.getUserVoterDetails(5105l,5l);
 		System.out.println(object);
-	}
+	}*/
+		
+		public void testgetCasteByVoterId()
+		{
+			List<Long> voterIDs = new ArrayList<Long>();
+			voterIDs.add(459028l);
+			//voterIDs.add(459029l);
+			//voterIDs.add(459030l);
+			
+			List<Object[]> list = userVoterDetailsDAO.getCasteByVoterId(1l, voterIDs);
+			System.out.println(list.size());
+		}
 
 }
