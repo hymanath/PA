a6c084d24e251364aa6502c163d3b148205eece0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-15 08:59:22 +0000

    Updated by AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/VoterReportLevel.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/VoterReportLevel.java
@@ -1,16 +1,23 @@
 package com.itgrids.partyanalyst.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "voter_report_level")
@@ -22,7 +29,10 @@ public class VoterReportLevel extends BaseModel implements Serializable{
 	private Long voterReportLevelId;
 	private String reportLevel;
 	private Long orderNo ;
-	
+	private Set<VoterInfo> voterInfos = new HashSet<VoterInfo>(0);
+	private Set<VoterAgeInfo> voterAgeInfos = new HashSet<VoterAgeInfo>(0);
+	private Set<VoterFamilyInfo> voterFamilyInfos = new HashSet<VoterFamilyInfo>(0);
+
 	public VoterReportLevel(){}
 	public VoterReportLevel(String reportLevel, Long orderNo)
 	{
@@ -56,5 +66,32 @@ public class VoterReportLevel extends BaseModel implements Serializable{
 		this.orderNo = orderNo;
 	}
 	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY , mappedBy = "voterReportLevel")
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public Set<VoterInfo> getVoterInfos() {
+		return voterInfos;
+	}
+	public void setVoterInfos(Set<VoterInfo> voterInfos) {
+		this.voterInfos = voterInfos;
+	}
 	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "voterReportLevel")
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public Set<VoterAgeInfo> getVoterAgeInfos() {
+		return voterAgeInfos;
+	}
+	public void setVoterAgeInfos(Set<VoterAgeInfo> voterAgeInfos) {
+		this.voterAgeInfos = voterAgeInfos;
+	}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "voterReportLevel")
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public Set<VoterFamilyInfo> getVoterFamilyInfos() {
+		return voterFamilyInfos;
+	}
+	public void setVoterFamilyInfos(Set<VoterFamilyInfo> voterFamilyInfos) {
+		this.voterFamilyInfos = voterFamilyInfos;
+	}
+	
+		
 }
