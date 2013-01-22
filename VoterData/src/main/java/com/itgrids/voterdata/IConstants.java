0ca4b8c4fc06a837a2c5d331e52b1f7f40fcced3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-22 06:54:54 +0000

    Updated by Kamalakar

--- VoterData/src/main/java/com/itgrids/voterdata/IConstants.java
+++ VoterData/src/main/java/com/itgrids/voterdata/IConstants.java
@@ -4,5 +4,6 @@ public interface IConstants {
 
 	public static final String VOTER_DATA_SITE_URL = "http://123.176.47.55/DraftRolls/Rolls.aspx";
 	public static final String FIREFOX_PATH = "C:/Program Files/Mozilla Firefox/firefox.exe";
-	public static final String DOWNLOAD_DIRECTORY = "D:\\voters\\AP\\Nellore\\Kavali";
+	public static final String DOWNLOAD_DIRECTORY = "D:\\Kamal\\Karim_nagar\\28-Vemulawada";
+	public static final Boolean IS_MISSED_FILES_DOWNLOAD = true;
 }
