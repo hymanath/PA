03dd41b077803a702a2287ecb179510ce1787742
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-01-28 14:01:03 +0000

    updated by Prasad

--- partyanalyst/trunk/src/main/webapp/js/constituencyManagement/constituencyManagement.js
+++ partyanalyst/trunk/src/main/webapp/js/constituencyManagement/constituencyManagement.js
@@ -148,18 +148,18 @@ function showProblemsStatusCount(results) {
 	problems_OptionsContent += '	<TR>';
 	problems_OptionsContent += '		<TH valign="top">View Problems from:</TH>';
 	problems_OptionsContent += '		<TD><input type="text" id="existingFromText" readonly="readonly" style="margin-top:0px;" name="existingFromText" size="20"/>';
-	problems_OptionsContent += '			<DIV class="yui-skin-sam"><DIV id="existingFromText_Div" class="tinyDateCal" style="z-index: 9999;"></DIV></DIV></TD>';
+	problems_OptionsContent += '			<DIV class="yui-skin-sam"><DIV id="existingFromText_Div" class="textBoxStyle" style="z-index: 9999;"></DIV></DIV></TD>';
 	problems_OptionsContent += '		</TD>';
-	problems_OptionsContent += '		<TD valign="top">';
+	/*problems_OptionsContent += '		<TD valign="top">';
 	problems_OptionsContent += '			<A href="javascript:{}" title="Click To Select A Date" onclick="showDateCal()"><IMG width="23" height="23" src="images/icons/constituencyManagement/calendar.jpeg" border="0"/></A>';
-	problems_OptionsContent += '		</TD>';
+	problems_OptionsContent += '		</TD>';*/
 	problems_OptionsContent += '		<TH valign="top">To:</TH>';
 	problems_OptionsContent += '		<TD><input type="text" id="tillDateText" value="' + todayDate + '" name="tillDateText" size="20" readonly="readonly" class="textBoxStyle"/>';
 	problems_OptionsContent += '			<DIV class="yui-skin-sam"><DIV id="till_Div" class="tinyDateCal" style="z-index: 9999;"></DIV></DIV>';
 	problems_OptionsContent += '		</TD>';
-	problems_OptionsContent += '		<TD valign="top">';
+	/*problems_OptionsContent += '		<TD valign="top">';
 	problems_OptionsContent += '			<A href="javascript:{}" title="Click To Select A Date" onclick="showDateCal1()"><IMG width="23" height="23" src="images/icons/constituencyManagement/calendar.jpeg" border="0"/></A>';
-	problems_OptionsContent += '		</TD>';
+	problems_OptionsContent += '		</TD>';*/
 	problems_OptionsContent += '	</TR>';
 
 	problems_OptionsContent += '	<TR>';
@@ -198,7 +198,20 @@ function showProblemsStatusCount(results) {
 	problems_OptionsContent += '</tr>';
 	problems_OptionsContent += '</table>';
 	problems_OptionsEl.innerHTML = problems_OptionsContent;
-
+	$("#existingFromText" ).datepicker({
+			dateFormat: "dd/mm/yy",
+			changeMonth: true,
+            changeYear: true,
+			maxDate: new Date()
+			
+		});
+	$("#tillDateText" ).datepicker({
+			dateFormat: "dd/mm/yy",
+			changeMonth: true,
+            changeYear: true,
+			maxDate: new Date()
+			
+		});
 	buildProblemsDetailsDT(problemMgmtObj.initialProblems);
 }
 
