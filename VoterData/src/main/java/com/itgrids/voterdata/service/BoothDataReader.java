3c0f0fdd801adfb6053b9b8e3b80198e31224690
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-07 17:57:23 +0000

    Booth Data Reading From Pdf - Kamalakar Dandu

--- VoterData/src/main/java/com/itgrids/voterdata/service/BoothDataReader.java
+++ VoterData/src/main/java/com/itgrids/voterdata/service/BoothDataReader.java
@@ -7,12 +7,9 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
@@ -31,16 +28,14 @@ public class BoothDataReader {
 	
     public static void main(String args[]) throws IOException {
         PDDocument pd = null;
-        Pattern p = Pattern.compile("Male Female Total\\r\\n([\\s0-9a-zA-Z]*)\\r\\n:");
-       
         try {
 
                 File inputDir = new File(args[0]);
-                File resultFile  = new File(args[0]+"/VotesMissed.txt");
-                BufferedWriter outwriter = new BufferedWriter(new FileWriter(resultFile));
+                File BoothInfo  = new File(args[0]+"/BoothDataReader.txt");
+                BufferedWriter boothInfoReader = new BufferedWriter(new FileWriter(BoothInfo));
                 StringBuilder sb = null;
-                StringBuilder sb2 = new StringBuilder();
-                int totalMissedVotes = 0;
+                StringBuilder sb2 = null;
+                StringBuilder boothSB = new StringBuilder();
                 List<BoothVO> boothsInfoList = new ArrayList<BoothVO>(0);
                 Class.forName("com.mysql.jdbc.Driver");
                 conn = DriverManager.getConnection(DB_URL,USER,PASS);
@@ -51,106 +46,59 @@ public class BoothDataReader {
                         return filename.endsWith('.' + "pdf");
                     }
                 })) {
-                	
+                	try{
                 	stmt = conn.createStatement();
                 	sb = new StringBuilder();
+                	sb2 = new StringBuilder();
                     pd = PDDocument.load(input);
                     String constituencyId = null;
                     PDFTextStripper stripper = new PDFTextStripper();
 
                     sb.append(stripper.getText(pd));
-                    sb.indexOf("Draft Photo Electoral Roll",100);
-                    sb.delete(sb.indexOf("Draft Photo Electoral Roll",100), sb.length()-1);
-                    sb.delete(0,sb.indexOf("Male Female Total"));
+                    sb2.append(stripper.getText(pd));
+                    String str1 = "deletions and corrections under Special \r\nSummary Revision 2013";
+                    String str2 = " \r\n\r\nPhoto Electoral Roll - 2013";
+                    String str3 = "District\r\n:";
+                    String str4 = "Roll Identification: Basic roll of  Special";
+                    sb2.delete(sb2.indexOf(str3)+str3.length()+1,sb2.indexOf(str4));
+                    sb.delete(0,sb.indexOf(str1)+str1.length()+1);
+                    sb.delete(sb.indexOf(str2), sb.length()-1);
+                    sb.delete(0,sb.indexOf("\r\n")+2);
+                    System.out.println(sb2.toString());
+                    if(sb.toString().contains("\r\n"))
+                    	sb.delete(0,sb.indexOf("\r\n"));
                     
-                    //System.out.println(sb.toString());
-                                       
                     String [] fileName = input.getName().split("-");
-                    Matcher m = p.matcher(sb);
                     
-                    m.find();
-                    String str = m.group(1).replaceAll("\\r\\n","").trim();
-                    System.out.println(str);
+                    String str = sb.toString();
+                    str = str.replaceAll("\\r\\n","").trim();
                     String[] voters = str.split(" ");
-                    
-                    List<Integer>snoList = new ArrayList<Integer>(0);
-                    List<Integer>missedVoters = new ArrayList<Integer>(0);
                     
                     constituencyId = fileName[0];
                     BoothVO boothVO = new BoothVO();
+                    boothVO.setFileName(args[0]+"\\"+input.getName());
                     boothVO.setTotalVoters(new Integer(voters[8]));
                     boothVO.setMaleVoters(new Integer(voters[4]));
                     boothVO.setFemaleVoters(new Integer(voters[6]));
-                    boothVO.setName(fileName[3].substring(0,fileName[3].length()-4));
-                    boothVO.setPartNo(fileName[2]);
+                    boothVO.setName(fileName[3].substring(0,fileName[3].length()-4).trim());
+                    boothVO.setPartNo(fileName[2].trim());
+                    boothVO.setConstituencyId(constituencyId);
+                    boothVO.setConstituencyName(fileName[1].trim());
                     
-                    ResultSet rs = stmt.executeQuery("select sno from voter_temp where constituency_id = '"+constituencyId+"'" +
-                    		" and booth_id = '"+boothVO.getPartNo()+"'");
-                    while(rs.next())
-                    	snoList.add(rs.getInt("sno"));
-                    
-                    for(int i=1;i<=boothVO.getTotalVoters();i++)
-                    	if(!snoList.contains(i))
-                    	{
-                    		missedVoters.add(i);
-                    		totalMissedVotes++;
-                    	}
-                    
-                    boothVO.setInsertedVotes(snoList.size());
-                    boothVO.setMissedVotes(boothVO.getTotalVoters() - boothVO.getInsertedVotes());
-                    boothVO.setMissedVotesList(missedVoters);
-                    
-                    if(missedVoters.size() > 0)
-                    	boothVO.setVotesMissed(true);
-                    
+                    boothSB.append("Booth - "+boothVO.getPartNo()+"\tTotal - "+boothVO.getTotalVoters()+"\tMale - "+boothVO.getMaleVoters()+"\tFemale - "+boothVO.getFemaleVoters()+"\n");
+                    System.out.println("Booth - "+boothVO.getPartNo()+"\tTotal - "+boothVO.getTotalVoters()+"\tMale - "+boothVO.getMaleVoters()+"\tFemale - "+boothVO.getFemaleVoters());
                     boothsInfoList.add(boothVO);
                     if (pd != null) {
                         pd.close();
                     }
-            }
-            sb2.append("Booth Details Of Missed Voters : ("+totalMissedVotes+")\n");
-            sb2.append("--------------------------------------------------\n");
-            
-            int index = 1;
-            for(BoothVO boothVO : boothsInfoList)
-            {
-            	if(boothVO.isVotesMissed())
-            	{
-            		sb2.append(index+") Booth Part No - "+boothVO.getPartNo()+"\tName - "+boothVO.getName()+"\n");
-            		sb2.append("\tTotal Voters   : "+boothVO.getTotalVoters()+"\n");
-            		sb2.append("\tInserted Votes : "+boothVO.getInsertedVotes()+"\n");
-            		sb2.append("\tMissed Votes   : "+boothVO.getMissedVotes()+"\n");
-            		sb2.append("\tMissed Votes SNo  : ");
-            		for(int sno : boothVO.getMissedVotesList())
-            			sb2.append(sno+", ");
-            		sb2.append("\n");
-            		index++;
-            		totalMissedVotes = totalMissedVotes + boothVO.getMissedVotesList().size();
-            	}
-            }
-            sb2.append("--------------------------------------------------\n\n");
-            sb2.append("All Booth Details:\n");
-            sb2.append("--------------------------------------------------\n");
-           
-            for(int j=0;j<boothsInfoList.size();j++)
-            {
-            	BoothVO boothVO = boothsInfoList.get(j);
-        		sb2.append(j+") Booth Part No - "+boothVO.getPartNo()+"\tName - "+boothVO.getName()+"\n");
-        		sb2.append("\tTotal Voters   : "+boothVO.getTotalVoters()+"\n");
-        		sb2.append("\tInserted Votes : "+boothVO.getInsertedVotes()+"\n");
-        		sb2.append("\tMissed Votes   : "+boothVO.getMissedVotes()+"\n");
-        		sb2.append("\tMissed Votes SNo  : ");
-        		for(int sno : boothVO.getMissedVotesList())
-        			sb2.append(sno+", ");
-        		sb2.append("\n");
-            }
-            sb2.append("--------------------------------------------------\n");
-            outwriter.write(sb2.toString());
-            outwriter.close();
-            System.out.println(sb2.toString());
+                	}catch (Exception e) {e.printStackTrace();}
+        }
+            boothInfoReader.write(boothSB.toString());
+            boothInfoReader.close();
+            	
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+       	
 }
