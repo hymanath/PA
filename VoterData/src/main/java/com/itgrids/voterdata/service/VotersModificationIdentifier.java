1453adc5520d8dd5805d96ba2a621678225b2bee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-16 13:14:05 +0000

    New Voters and Deleted Voters Indentification - Kamalakar Dandu

--- VoterData/src/main/java/com/itgrids/voterdata/service/VotersModificationIdentifier.java
+++ VoterData/src/main/java/com/itgrids/voterdata/service/VotersModificationIdentifier.java
@@ -17,6 +17,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 import com.itgrids.voterdata.VO.BoothVO;
+import com.itgrids.voterdata.VO.VoterInfo;
 
 public class VotersModificationIdentifier {
 	
@@ -28,17 +29,21 @@ public class VotersModificationIdentifier {
 		static Connection conn = null;
 		static Statement stmt = null;
 	
-    public static void main(String args[]) throws IOException {
+		public static void main(String args[]) throws IOException {
         PDDocument pd = null;
         try {
 
                 File inputDir = new File(args[0]);
-                File BoothInfo  = new File(args[0]+"/BoothDataReader.txt");
-                BufferedWriter boothInfoReader = new BufferedWriter(new FileWriter(BoothInfo));
+                File resultFile  = new File(args[0]+"/NewVoterData.txt");
+                File resultFile2  = new File(args[0]+"/deletedVoterData.txt");
+                BufferedWriter outwriter = new BufferedWriter(new FileWriter(resultFile));
+                BufferedWriter outwriter2 = new BufferedWriter(new FileWriter(resultFile2));
                 StringBuilder sb = null;
-                StringBuilder sb2 = null;
-                StringBuilder boothSB = new StringBuilder();
-                List<BoothVO> boothsInfoList = new ArrayList<BoothVO>(0);
+                StringBuilder addVotersStr = new StringBuilder();
+                StringBuilder deletedVotersStr = new StringBuilder();
+                List<VoterInfo> newVoters = new ArrayList<VoterInfo>(0);
+                List<VoterInfo> deletedVoters = new ArrayList<VoterInfo>(0);
+                
                 Class.forName("com.mysql.jdbc.Driver");
                 conn = DriverManager.getConnection(DB_URL,USER,PASS);
                 
@@ -51,60 +56,58 @@ public class VotersModificationIdentifier {
                 	try{
                 	stmt = conn.createStatement();
                 	sb = new StringBuilder();
-                	sb2 = new StringBuilder();
                     pd = PDDocument.load(input);
                     String constituencyId = null;
                     PDFTextStripper stripper = new PDFTextStripper();
 
                     sb.append(stripper.getText(pd));
-                    sb2.append(stripper.getText(pd));
-                    File resultFile  = new File(args[0]+"/VoterData.txt");
-                    BufferedWriter outwriter = new BufferedWriter(new FileWriter(resultFile));
-                    outwriter.write(sb2.toString());
-                    outwriter.close();
+                    
+                    String [] fileName = input.getName().split("-");
+                    constituencyId = fileName[0];
                     
                     String str1 = "Component - I";
                     String str2 = "Component - II";
-                    String str3 = "District\r\n:";
-                    String str4 = "Roll Identification: Basic roll of  Special";
+                    String str3 = "Component - III";
                     
                     String additionsStr = sb.substring(sb.indexOf(str1), sb.indexOf(str2));
+                    String deletionsStr = sb.substring(sb.indexOf(str2), sb.indexOf(str3));
                     //System.out.println(additionsStr);
+                    System.out.println(deletionsStr);
                     
                     Pattern p = Pattern.compile("Age:\\r\\nHouse No:\\r\\n(Husband's Name:|Father's Name:|Mother's Name:|Other's Name:)\\r\\nElector's Name:\\r\\n([A-Z\\d]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\nSex:\\s([a-zA-Z]*)\\r\\n([0-9\\-_/A-Za-z\\.\\s\\?\\+\\=\\`\\(\\)\\/\\*\\,\\\\]*)\\r\\n([\\s0-9]*)\\r\\n([\\s0-9]*)\\r\\n");
                 	Matcher m = p.matcher(additionsStr);
                 	
                 	while (m.find()) 
                     {
-                		System.out.println((m.group(1).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(2).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(3).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(4).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(5).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(6).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(7).replaceAll("\\r\\n","").trim()));
-                		System.out.println((m.group(8).replaceAll("\\r\\n","").trim()));
+                		VoterInfo voterInfo = new VoterInfo();
+                		voterInfo.setBoothName(fileName[3].substring(0,fileName[3].length()-4).trim());
+                		voterInfo.setBoothNo(fileName[2].trim());
+                		voterInfo.setConstituency(fileName[1].trim());
+                		voterInfo.setConstituencyId(constituencyId);
+                		voterInfo.setVoterId((m.group(2).replaceAll("\\r\\n","").trim()));
+                		voterInfo.setsNo(new Long((m.group(8).replaceAll("\\r\\n","").trim())));
+                		newVoters.add(voterInfo);
                     }
                 	
-                    String [] fileName = input.getName().split("-");
-                    
-                    constituencyId = fileName[0];
-                    BoothVO boothVO = new BoothVO();
-                    boothVO.setFileName(args[0]+"\\"+input.getName());
-                    boothVO.setName(fileName[3].substring(0,fileName[3].length()-4).trim());
-                    boothVO.setPartNo(fileName[2].trim());
-                    boothVO.setConstituencyId(constituencyId);
-                    boothVO.setConstituencyName(fileName[1].trim());
-                    
-                    boothSB.append("Booth - "+boothVO.getPartNo()+"\tTotal - "+boothVO.getTotalVoters()+"\tMale - "+boothVO.getMaleVoters()+"\tFemale - "+boothVO.getFemaleVoters()+"\n");
-                    boothsInfoList.add(boothVO);
                     if (pd != null) {
                         pd.close();
                     }
                 	}catch (Exception e) {e.printStackTrace();}
         }
-            boothInfoReader.write(boothSB.toString());
-            boothInfoReader.close();
+                
+        if(newVoters.size() > 0)
+        {
+        	int index = 0;
+        	for(VoterInfo voterInfo : newVoters)
+        	{
+        		String tempStr = ++index+")\tPart No - "+voterInfo.getBoothNo()+"\tS.NO - "+voterInfo.getsNo()+"\t Voter ID - "+voterInfo.getVoterId()
+        				+"\tConstituency Id - "+voterInfo.getConstituencyId()+"\tConstituency - "+voterInfo.getConstituency()+"\n";
+        		System.out.print(tempStr);
+        		addVotersStr.append(tempStr);
+        	}
+        	outwriter.write(addVotersStr.toString());
+        	outwriter.close();
+        }
             	
         } catch (Exception e) {
             e.printStackTrace();
