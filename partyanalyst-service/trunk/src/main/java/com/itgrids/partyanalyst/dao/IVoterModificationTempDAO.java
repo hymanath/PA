6a378c730fea9f221060e234c903d535e3e7a30d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-18 13:07:11 +0000

    Missed Voters Finding Changes - kamalakar dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterModificationTempDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterModificationTempDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.VoterModificationTemp;
 
 public interface IVoterModificationTempDAO extends GenericDao<VoterModificationTemp,Long>{
 
+	public List<Object[]> getVoterIDAndStatusFromVoterModificationTempByConstituencyId(Long constituencyId);
 }
