e11d995ab34fa53a789dafd055e93106f23214c9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-18 13:07:18 +0000

    Missed Voters Finding Changes - kamalakar dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationTempDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationTempDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IVoterModificationTempDAO;
@@ -10,6 +12,12 @@ public class VoterModificationTempDAO extends GenericDaoHibernate<VoterModificat
 	public VoterModificationTempDAO()
 	{
 		super(VoterModificationTemp.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getVoterIDAndStatusFromVoterModificationTempByConstituencyId(Long constituencyId)
+	{
+		return getHibernateTemplate().find("select model.voterId,model.status from VoterModificationTemp model where model.constituencyId = ?",constituencyId);
 	}
 
 }
