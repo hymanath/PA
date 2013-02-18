008cd54f438be4c49be56b1e322dad23af4d052b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-18 10:03:58 +0000

    New Voters and Deleted Voters Indentification - Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterModificationTempDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.VoterModificationTemp;
+
+public interface IVoterModificationTempDAO extends GenericDao<VoterModificationTemp,Long>{
+
+}
