2aaacf0f00dc18fc2283b05560a681a2679095b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-19 17:06:15 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAOHibernateTest.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 import com.itgrids.partyanalyst.dao.IVoterModificationDAO;
 
@@ -11,8 +14,18 @@ public class VoterModificationDAOHibernateTest extends BaseDaoTestCase{
 		this.voterModificationDAO = voterModificationDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		voterModificationDAO.getAll();
+	}*/
+	
+	public void testGetAddedAndDeletedVotersCountInBetweenPublicationsInALocation()
+	{	
+		List<Long>  publicationIdsList = new ArrayList<Long>(0);
+		 publicationIdsList.add(8l);
+		List<Object[]> list = voterModificationDAO.getAddedAndDeletedVotersCountInBetweenPublicationsInALocation("panchayat",1l,232l,publicationIdsList);
+		System.out.println(list.size());
+		System.out.println(list.get(0)[0]+"\t"+list.get(0)[1]);
+		System.out.println(list.get(1)[0]+"\t"+list.get(1)[1]);
 	}
 }
