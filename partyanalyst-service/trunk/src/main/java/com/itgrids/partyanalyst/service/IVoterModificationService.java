d7b2f6edcba7fc21c8be55f184b8e1ab4b809e0e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-20 14:10:24 +0000

    Updated by AnuRadha VoterModification

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVoterModificationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVoterModificationService.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.VoterAgeRangeVO;
+import com.itgrids.partyanalyst.excel.booth.VoterModificationAgeRangeVO;
 import com.itgrids.partyanalyst.excel.booth.VoterModificationVO;
 
 public interface IVoterModificationService {
@@ -14,5 +15,7 @@ public interface IVoterModificationService {
 	public List<Long> getPreviousPublicationIds(Long publicationDateId);
 	
 	public List<VoterAgeRangeVO> getVoterInfoByPublicationDateList(String locationType,Long locationValue,Long constituencyId,Long fromPublicationDateId,Long toPublicationDateId);	
+	
+	public List<VoterModificationAgeRangeVO> getVotersAddedAndDeletedCountAgeWiseInBeetweenPublications(String locationType,Long locationValue,Long constituencyId,Long fromPublicationDateId,Long toPublicationDateId);
 	
 }
