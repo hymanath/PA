4f227d1cce71e5064b8876339019c4562a11c49c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-20 13:02:54 +0000

    Updated for Voters Modification Age Range - Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAOHibernateTest.java
@@ -19,11 +19,21 @@ public class VoterModificationDAOHibernateTest extends BaseDaoTestCase{
 		voterModificationDAO.getAll();
 	}*/
 	
-	public void testGetAddedAndDeletedVotersCountInBetweenPublicationsInALocation()
+	/*public void testGetAddedAndDeletedVotersCountInBetweenPublicationsInALocation()
 	{	
 		List<Long>  publicationIdsList = new ArrayList<Long>(0);
 		 publicationIdsList.add(8l);
 		List<Object[]> list = voterModificationDAO.getAddedAndDeletedVotersCountInBetweenPublicationsInALocation("panchayat",1l,232l,publicationIdsList);
+		System.out.println(list.size());
+		System.out.println(list.get(0)[0]+"\t"+list.get(0)[1]);
+		System.out.println(list.get(1)[0]+"\t"+list.get(1)[1]);
+	}*/
+	
+	public void testGetAgeWiseAddedAndDeletedVotersCountInBetweenPublicationsInALocation()
+	{
+		List<Long>  publicationIdsList = new ArrayList<Long>(0);
+		publicationIdsList.add(8l);
+		List<Object[]> list = voterModificationDAO.getAgeWiseAddedAndDeletedVotersCountInBetweenPublicationsInALocation("constituency",232l,232l, publicationIdsList, 18l, 25l);
 		System.out.println(list.size());
 		System.out.println(list.get(0)[0]+"\t"+list.get(0)[1]);
 		System.out.println(list.get(1)[0]+"\t"+list.get(1)[1]);
