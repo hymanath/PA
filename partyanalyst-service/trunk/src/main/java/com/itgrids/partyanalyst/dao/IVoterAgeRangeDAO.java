1bb7880f5f9440369b658bce900a64cce23db386
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-20 13:02:03 +0000

    Updated for Voters Modification Age Range - Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterAgeRangeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterAgeRangeDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.VoterAgeRange;
@@ -7,5 +9,7 @@ import com.itgrids.partyanalyst.model.VoterAgeRange;
 public interface IVoterAgeRangeDAO extends GenericDao<VoterAgeRange, Long>{
 
 	public Long getVoterAgeRangeIdByType(String ageRange);
+	
+	public List<String> getAllVoterAgeRanges();
 	
 }
