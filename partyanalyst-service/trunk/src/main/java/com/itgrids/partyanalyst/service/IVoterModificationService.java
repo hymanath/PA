847428a8bd9fb2e9fb9333ecf18c3f20c0697280
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-20 09:33:54 +0000

    Voter Modification Service Created - Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVoterModificationService.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.excel.booth.VoterModificationVO;
+
+public interface IVoterModificationService {
+	
+	public VoterModificationVO getAddedAndDeletedVotersCountInBetweenPublicationsInALocation(String locationType,Long locationValue,Long constituencyId,Long fromPublicationDateId,Long toPublicationDateId);
+	
+	public List<Long> getVoterPublicationIdsBetweenTwoPublications(Long fromPublicationDateId,Long toPublicationDateId);
+	
+	public List<Long> getPreviousPublicationIds(Long publicationDateId);
+}
