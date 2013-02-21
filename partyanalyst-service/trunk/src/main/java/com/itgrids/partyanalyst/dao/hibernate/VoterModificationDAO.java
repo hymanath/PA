39a2c655808334075eeb1561b732587f8f8c5f4e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-02-21 06:08:42 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationDAO.java
@@ -48,6 +48,38 @@ public class VoterModificationDAO extends GenericDaoHibernate<VoterModification,
 	}
 	
 	@SuppressWarnings("unchecked")
+	public List<Object[]> getGenderWiseVoterModificationsBetweenPublications(String locationType,Long locationValue,Long constituencyId,List<Long> publicationIdsList)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append(" select count(model.voter.voterId),model.status,model.voter.gender from VoterModification model, BoothPublicationVoter model2 where model.voter.voterId = model2.voter.voterId and ");
+		str.append(" model.publicationDate.publicationDateId in(:publicationIdsList) ");
+		
+		if(locationType.equalsIgnoreCase("constituency"))
+			str.append(" and model2.booth.constituency.constituencyId = :locationValue ");
+		else if(locationType.equalsIgnoreCase("mandal"))
+			str.append(" and model2.booth.tehsil.tehsilId = :locationValue and model2.booth.localBody is null");
+		else if(locationType.equalsIgnoreCase("panchayat"))
+			str.append(" and model2.booth.pancahayat.pancahayatId = :locationValue and model2.booth.localBody is null");
+		else if(locationType.equalsIgnoreCase("booth"))
+			str.append(" and model2.booth.boothId = :locationValue ");
+		else if(locationType.equalsIgnoreCase("localElectionBody") || locationType.equalsIgnoreCase("Local Election Body"))
+			str.append(" model.booth.localBody.localElectionBodyId = :locationValue and model2.booth.constituency.constituencyId = :constituencyId");
+		else if(locationType.equalsIgnoreCase("ward"))
+			str.append(" and model2.localBodyWard.constituencyId = :locationValue and model2.booth.constituency.constituencyId = :constituencyId");
+		
+		str.append(" group by model.status,model.voter.gender ");
+		
+		Query query = getSession().createQuery(str.toString());
+		query.setParameterList("publicationIdsList",publicationIdsList);
+		query.setParameter("locationValue",locationValue);
+		
+		if(locationType.equalsIgnoreCase("localElectionBody") || locationType.equalsIgnoreCase("Local Election Body") ||
+				locationType.equalsIgnoreCase("ward"))
+			query.setParameter("constituencyId",constituencyId);
+		return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
 	public List<Object[]> getAgeWiseAddedAndDeletedVotersCountInBetweenPublicationsInALocation(String locationType,Long locationValue,Long constituencyId,List<Long> publicationIdsList,Long ageFrom, Long ageTo)
 	{
 		StringBuilder str = new StringBuilder();
