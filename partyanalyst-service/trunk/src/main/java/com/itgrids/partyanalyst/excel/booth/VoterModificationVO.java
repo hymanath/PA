a479685c12887a9ddeb0c629aaf0fdc381cf2725
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-03-02 10:32:07 +0000

    showing sub level voter modification report implementation - samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/excel/booth/VoterModificationVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/excel/booth/VoterModificationVO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.excel.booth;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class VoterModificationVO implements Serializable{
 
@@ -8,9 +9,78 @@ public class VoterModificationVO implements Serializable{
 	
 	private Long previousPublicationId;
 	private Long presentPublicationId;
-	private Long addedCount;
-	private Long deletedCount;
+	private Long addedCount = 0L;
+	private Long deletedCount = 0L;
+	private String name;
+	private Long id;
 	
+	private Long maleVotersAdded = 0L;
+	private Long femaleVotersAdded = 0L;
+	private Long maleVotersDeleted = 0L;
+	private Long femaleVotersDeleted = 0L;
+	
+	
+
+	public Long getMaleVotersDeleted() {
+		return maleVotersDeleted;
+	}
+
+	public void setMaleVotersDeleted(Long maleVotersDeleted) {
+		this.maleVotersDeleted = maleVotersDeleted;
+	}
+
+	public Long getFemaleVotersDeleted() {
+		return femaleVotersDeleted;
+	}
+
+	public void setFemaleVotersDeleted(Long femaleVotersDeleted) {
+		this.femaleVotersDeleted = femaleVotersDeleted;
+	}
+
+	public Long getMaleVotersAdded() {
+		return maleVotersAdded;
+	}
+
+	public void setMaleVotersAdded(Long maleVotersAdded) {
+		this.maleVotersAdded = maleVotersAdded;
+	}
+
+	public Long getFemaleVotersAdded() {
+		return femaleVotersAdded;
+	}
+
+	public void setFemaleVotersAdded(Long femaleVotersAdded) {
+		this.femaleVotersAdded = femaleVotersAdded;
+	}
+
+	private List<VoterModificationVO> modifiedVotersList;
+	private List<VoterModificationVO> modifiedLocalBodyVotersList;
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public List<VoterModificationVO> getModifiedVotersList() {
+		return modifiedVotersList;
+	}
+
+	public void setModifiedVotersList(List<VoterModificationVO> modifiedVotersList) {
+		this.modifiedVotersList = modifiedVotersList;
+	}
+
 	public Long getPreviousPublicationId() {
 		return previousPublicationId;
 	}
@@ -36,5 +106,15 @@ public class VoterModificationVO implements Serializable{
 	}
 	public void setDeletedCount(Long deletedCount) {
 		this.deletedCount = deletedCount;
+	}
+	
+
+	public List<VoterModificationVO> getModifiedLocalBodyVotersList() {
+		return modifiedLocalBodyVotersList;
+	}
+
+	public void setModifiedLocalBodyVotersList(
+			List<VoterModificationVO> modifiedLocalBodyVotersList) {
+		this.modifiedLocalBodyVotersList = modifiedLocalBodyVotersList;
 	}
 }
