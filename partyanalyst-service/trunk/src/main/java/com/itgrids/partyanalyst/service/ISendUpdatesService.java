5f7989358e19895180626e8baf6c0840a0e2a480
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-03-06 14:19:35 +0000

    updated by srishailam and sravanthi

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISendUpdatesService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISendUpdatesService.java
@@ -3,6 +3,9 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.excel.booth.VoterVO;
 
 public interface ISendUpdatesService {
 	
@@ -11,6 +14,7 @@ public interface ISendUpdatesService {
 	public List<RegistrationVO> sendSmsForAllUsersFromAdmin(String message);
 	public String sendEmailsFromAdminToUsers(List<String> userId,String message,String subject);
 	public List<RegistrationVO>  getUsersForSendingEmails(Long selectedState,Long selectedDistrict,Long selectedConstituency,Long userType,Long locationScope);
-
-
+	public ResultStatus sendSMSToSelectedPeople(Long userId,String scope,Long scopeId,String content,String type);
+	public Long getLatestPublicationDateId();
+	public List<SelectOptionVO> getBoothsForWardId(Long wardId,Long publicationDateId);
 }
