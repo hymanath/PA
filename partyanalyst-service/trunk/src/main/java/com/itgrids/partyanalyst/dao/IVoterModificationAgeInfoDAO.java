5d45b15e0b4008b3cf6632ae8ca3ad0c83254019
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-03-08 08:05:53 +0000

    updated by Nagamani --delete voter modification data from intermediate tables

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterModificationAgeInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterModificationAgeInfoDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.VoterModificationAgeInfo;
@@ -7,5 +9,5 @@ import com.itgrids.partyanalyst.model.VoterModificationAgeInfo;
 public interface IVoterModificationAgeInfoDAO extends GenericDao<VoterModificationAgeInfo,Long>
 {
 	
-
+	public Integer deleteVoterModicationAgeInfoById(List<Long> voterModificationInfoId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VoterModificationInfoDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IVoterModificationInfoDAO;
 import com.itgrids.partyanalyst.model.VoterModificationInfo;
@@ -10,5 +13,21 @@ public class VoterModificationInfoDAO extends GenericDaoHibernate<VoterModificat
 	public VoterModificationInfoDAO()
 	{
 		super(VoterModificationInfo.class);
+	}
+	
+	public Integer  deletevotermodificationInfoByConstituencyId(Long constituencyId,Long publicationId)
+	{
+		Query query =getSession().createQuery("delete from VoterModificationInfo model where model.constituencyId=:constituencyId and model.publicationDate.publicationDateId=:publicationId");
+		query.setParameter("constituencyId", constituencyId);
+		query.setParameter("publicationId", publicationId);
+		return query.executeUpdate();
+	}
+	
+	public List<Long> getVoterModificationInfoIds(Long constituencyId,Long publicationId)
+	{
+		Query query =getSession().createQuery("select model.voterModificationInfoId from  VoterModificationInfo model where model.constituencyId=:constituencyId and model.publicationDate.publicationDateId=:publicationId");
+		query.setParameter("constituencyId", constituencyId);
+		query.setParameter("publicationId", publicationId);
+		return query.list();
 	}
 }
