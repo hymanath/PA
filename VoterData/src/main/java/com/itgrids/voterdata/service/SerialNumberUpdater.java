84c94eef5d4747f667a04385978d4ad9b0c78e74
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-03-09 15:49:22 +0000

    Updated by Kamalakar Dandu

--- VoterData/src/main/java/com/itgrids/voterdata/service/SerialNumberUpdater.java
+++ VoterData/src/main/java/com/itgrids/voterdata/service/SerialNumberUpdater.java
@@ -13,6 +13,7 @@ import java.io.Reader;
 import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -59,28 +60,57 @@ public class SerialNumberUpdater {
         }
     }
     
-    public static Integer updateSerialNUmber(Map<String,Long> votersSerailNoMap)
+    public static List<Long> updateSerialNumber(String constituencyId, String boothId, Map<String,Long> votersSerailNoMap)
     {
     	try{
     		Class.forName("com.mysql.jdbc.Driver");
     		conn = DriverManager.getConnection(DB_URL,USER,PASS);
     		stmt = conn.createStatement();
+    		List<Long> missedIds = new ArrayList<Long>(0);
+    		Map<String,Long> serialNosMap = new HashMap<String, Long>(0);
+    		Map<String,Long> diffSnosMap = new HashMap<String, Long>(0);
+    		
+    		String selectQuery = " Select voter_id, sno from voter_temp where constituency_id = '"+constituencyId+"' and booth_id = '"+boothId+"'";
+    		ResultSet rs = stmt.executeQuery(selectQuery);
+    		
+    		while(rs.next())
+    		{
+    			serialNosMap.put(rs.getString("voter_id"),new Integer(rs.getInt("sno")).longValue());
+    		}
     		
     		for(Map.Entry<String,Long> entry : votersSerailNoMap.entrySet())
     		{
-    			try{
-    			String updateQuery = "update voter_temp set sno = "+entry.getValue()+" where voter_id = '"+entry.getKey()+"'";
-    			int result = stmt.executeUpdate(updateQuery);
-    			if(result > 0)
-    				System.out.println(result+" record updated");
-    			}catch(Exception e)
-    			{
-    				System.out.println("Exception Occured While Saving the Voter ID -"+entry.getKey());
-    				System.out.println("Exception is -"+e);
-    			}
+    			String voterId = entry.getKey();
+    			Long sno = entry.getValue();
+    			
+    			Long sno2 = serialNosMap.get(voterId);
+    			if(sno2 == null)
+    				missedIds.add(sno);
+    			else if(!sno.equals(sno2))
+    				diffSnosMap.put(voterId, sno);
+    			
+    		}
+    		if(diffSnosMap.size() > 0)
+    		{
+    			System.out.println("In Booth : "+boothId+" "+diffSnosMap.size()+" Voter SNOs will be Updated");
+	    		for(Map.Entry<String,Long> entry : diffSnosMap.entrySet())
+	    		{
+	    			try{
+	    			String updateQuery = "update voter_temp set sno = "+entry.getValue()+" where voter_id = '"+entry.getKey()+"'";
+	    			int result = stmt.executeUpdate(updateQuery);
+	    			
+	    			if(result == 0)
+	    				missedIds.add(entry.getValue());
+	    				
+	    			}catch(Exception e)
+	    			{
+	    				System.out.println("Exception Occured While Saving the Voter ID -- "+entry.getKey()+" -- "+entry.getValue());
+	    				System.out.println("Exception is -"+e);
+	    			}
+	    		}
     		}
     		
-    		return null;
+    		return missedIds;
     	}catch (Exception e) {
     		e.printStackTrace();
     		return null;
@@ -98,8 +128,8 @@ public class SerialNumberUpdater {
         PDDocument pd = null;
         int i = 0;
         
-        Pattern p1 = Pattern.compile("\\r\\n([\\s0-9]*)\\r\\nElector's Name:\\r\\nSex:Age:\\r\\nHouse No:\\r\\n([A-Z\\d]*)\\r\\n");
-        Pattern p2 = Pattern.compile("Elector's Name:\\r\\n([A-Z\\d]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\nSex:\\s([a-zA-Z]*)\\r\\n([0-9\\-_/A-Za-z\\.\\s\\?\\+\\=\\`\\/\\*\\,\\\\]*)\\r\\n([\\s0-9]*)\\r\\n([\\s0-9]*)\\r\\n");
+        Pattern p1 = Pattern.compile("([\\s0-9]*)\\r\\nElector's Name:\\r\\nSex:Age:\\r\\nHouse No:\\r\\n([A-Z\\d]*)\\r\\n");
+        Pattern p2 = Pattern.compile("Elector's Name:\\r\\n([A-Z\\d]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\n([A-Za-z\\.\\s\\r\\n]*)\\r\\nSex:\\s([a-zA-Z]*)\\r\\n([0-9\\-_/A-Za-z\\.\\s\\?\\+\\=\\`\\/\\*\\,\\:\\;\\\\]*)\\r\\n([\\s0-9]*)\\r\\n([\\s0-9]*)\\r\\n");
         Pattern p3 = Pattern.compile("([\\s0-9]*)\\r\\nAge:\\r\\nHouse No:\\r\\n(Husband's Name:|Father's Name:|Mother's Name:|Other's Name:)\\r\\nElector's Name:\\r\\n([A-Z\\d]*)\\r\\n");
         Pattern p4 = Pattern.compile("\\r\\n([\\s0-9]*)\\r\\nAge:\\r\\nHouse No:\\r\\n(Husband's Name:|Father's Name:|Mother's Name:|Other's Name:)\\r\\nElector's Name:\\r\\n([A-Z\\d]*)\\r\\n");
         
@@ -107,7 +137,6 @@ public class SerialNumberUpdater {
 
                 BufferedWriter writer = new BufferedWriter(new FileWriter(new File(args[0] + "/" + "VotersList.txt")));
                 File inputDir = new File(args[0]);
-                String voterInfo = "";
                 StringBuilder sb2 = new StringBuilder();
                 File resultFile  = new File(args[0]+"/VoterData.txt");
                 BufferedWriter outwriter = new BufferedWriter(new FileWriter(resultFile));
@@ -138,37 +167,71 @@ public class SerialNumberUpdater {
                     
                     VoterInfo voter = null;
                     List<VoterInfo> voterInfoList = new ArrayList<VoterInfo>(10);
-                    outwriter.write(pattern2Str);
+                    
+                    System.out.println("Reading "+input.getName()+"..........");
                     
                     while(m1.find())
                     {
-                    	votersSerailNoMap.put(m1.group(2).replaceAll("\\r\\n","").trim(), Long.valueOf(m1.group(1).replaceAll("\\r\\n","").trim()));
+                    	try{
+                    		String sno = m1.group(1).trim();
+                    		while(sno.contains("\n"))
+                    			sno = sno.substring(sno.indexOf("\n")).trim();
+                    		votersSerailNoMap.put(m1.group(2).replaceAll("\\r\\n","").trim(), Long.valueOf(sno.replaceAll("\\r\\n","").trim()));
+                    	}catch (Exception e) {
+                    		System.out.println(m1.group(1));
+                    		System.out.println("Exception in M1");
+                    	}
                     }
                     while(m2.find())
                     {
+                    	try{
                     	votersSerailNoMap.put(m2.group(1).replaceAll("\\r\\n","").trim(), Long.valueOf(m2.group(7).replaceAll("\\r\\n","").trim()));
+                    	}catch (Exception e) {
+                    		System.out.println("Exception in M2");
+                    	}
                     }
                     
                     while(m3.find())
                     {
-                    	votersSerailNoMap.put(m3.group(3).replaceAll("\\r\\n","").trim(), Long.valueOf(m3.group(1).replaceAll("\\r\\n","").trim()));
+                    	try{
+                    		String sno = m3.group(1).replaceAll("\\r\\n","").trim();
+                    		if(sno.contains(" "))
+                    			sno = sno.substring(sno.indexOf(" ")+1);
+                    		votersSerailNoMap.put(m3.group(3).replaceAll("\\r\\n","").trim(), Long.valueOf(sno));
+                    	
+                    }catch (Exception e) {
+                    	System.out.println("Exception in M3");
+                    }
                     }
                     while(m4.find())
                     {
-                    	System.out.println(m4.group(3).replaceAll("\\r\\n","").trim());
+                    	try{
                     	String sNoStr = m4.group(1).trim();
-                    	if(sNoStr.contains("\r\n"))
-                    		sNoStr = sNoStr.substring(sNoStr.indexOf("\r\n"));
-                    	sNoStr = sNoStr.replaceAll("\\r\\n","").trim();
-                    	System.out.println(sNoStr);
+                    	while(sNoStr.contains("\n"))
+                    		sNoStr = sNoStr.substring(sNoStr.indexOf("\n")).trim();
                     	votersSerailNoMap.put(m4.group(3).replaceAll("\\r\\n","").trim(), Long.valueOf(sNoStr));
+                    	}catch (Exception e) {
+                    		System.out.println("Exception in M4");
+                    	}
                     }
                     
                     if (pd != null) {
                         pd.close();
                     }
-                    updateSerialNUmber(votersSerailNoMap);
-                    //outwriter.write(sb.toString());
+                    
+                    List<Long>missedIds =  updateSerialNumber(fileName[0],fileName[2],votersSerailNoMap);
+                    for(long k = 1L;k<=votersSerailNoMap.size();k++)
+                    {
+                    	if(!votersSerailNoMap.containsValue(k))
+                    		System.out.println(k+" Not Read");
+                    }
+                    sb2.append("\nBooth - "+fileName[2]+" : Total - ("+votersSerailNoMap.size()+")\tMissed Voters -("+missedIds.size()+")\n");
+                    if(missedIds.size() > 0)
+                    {
+                    	sb2.append("\tMissed Voters SNO : ");
+                    	for(Long sno : missedIds)
+                    		sb2.append(sno+", ");
+                    }
             }
             outwriter.write(sb2.toString());
             writer.close();
