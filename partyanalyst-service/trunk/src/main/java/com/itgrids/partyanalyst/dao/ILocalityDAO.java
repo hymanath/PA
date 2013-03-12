96e6ccfe722f57a7a3f89d104895300e902d5bbf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-03-12 07:06:22 +0000

    Bugs fix - samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocalityDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocalityDAO.java
@@ -1,9 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Locality;
 
 public interface ILocalityDAO extends GenericDao<Locality, Long>{
+	
+	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId);
+
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocalityDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocalityDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ILocalityDAO;
 import com.itgrids.partyanalyst.model.Locality;
@@ -10,6 +13,19 @@ public class LocalityDAO extends GenericDaoHibernate<Locality, Long> implements 
 	public LocalityDAO() {
 		super(Locality.class);
 		
+	}
+	
+	
+	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId)
+	{
+		
+		Query query = getSession().createQuery("select model.localityId , model.name from Locality model " +
+				"where model.hamlet.hamletId = :hamletId and model.user.userId = :userId");
+		
+		query.setParameter("hamletId", hamletId);
+		query.setParameter("userId", userId);
+		
+		return query.list();
 	}
 
 }
