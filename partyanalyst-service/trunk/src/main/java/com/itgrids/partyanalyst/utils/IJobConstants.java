4c4a4f01acefbdb690c5e12ae1228cbaea06e595
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-03 10:53:38 +0000

    updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IJobConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IJobConstants.java
@@ -3,4 +3,5 @@ package com.itgrids.partyanalyst.utils;
 public interface IJobConstants {
 	public static final Long PARTY_CANDIDATE_SPECIALPAGE_UPDATES = 1L;
 	public static final String ADMIN_GROUP_MAILS = "info@partyanalyst.com";
+	public static final String[] ADMIN_GROUP_IDS = {"info@partyanalyst.com","k.mahesh@itgrids.com","kamalakardandu@gmail.com","a.dakavaram@gmail.com"};
 }
