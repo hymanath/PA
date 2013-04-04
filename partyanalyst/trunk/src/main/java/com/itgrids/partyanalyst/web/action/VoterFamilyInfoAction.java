8727416f1605758b068cbc08a740802c39f03b1e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-04 06:53:34 +0000

    updated by Nagamani

---
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VoterFamilyInfoAction.java
@@ -0,0 +1,97 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class VoterFamilyInfoAction  extends ActionSupport implements ServletRequestAware{
+	private HttpServletRequest request;
+	private HttpSession session;
+	
+	private String buildType;
+	private Long publicationDateId;
+	
+	private Long id;
+	private String type;
+	private String typename;
+	
+	private Long constituencyId;
+	
+	private String publicationYear;
+	
+	
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public String getPublicationYear() {
+		return publicationYear;
+	}
+	public void setPublicationYear(String publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+	public String getBuildType() {
+		return buildType;
+	}
+	public void setBuildType(String buildType) {
+		this.buildType = buildType;
+	}
+	public Long getPublicationDateId() {
+		return publicationDateId;
+	}
+	public void setPublicationDateId(Long publicationDateId) {
+		this.publicationDateId = publicationDateId;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTypename() {
+		return typename;
+	}
+	public void setTypename(String typename) {
+		this.typename = typename;
+	}
+	public HttpSession getSession() {
+		return session;
+	}
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+		
+	}
+	
+	public String execute() throws Exception
+	{
+		
+		
+		return SUCCESS;
+		
+	}
+
+	
+	
+}
