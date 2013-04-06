5bb15b93f6ad7f0d57c7a0f15ca99dc531eb3d1b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-06 10:51:50 +0000

    Bugs Fix - samba

--- partyanalyst/trunk/src/main/webapp/js/voterAnalysis/voterAnalysis1.js
+++ partyanalyst/trunk/src/main/webapp/js/voterAnalysis/voterAnalysis1.js
@@ -8236,12 +8236,12 @@ function  buildFamilyMembers1(result,jsObj,type){
 
 function getImpFamiliesVotersForHamletBooth1()
 {
-	
+		
 	var mainreqid = $("#constituencyList").val();
 	var constituencyId = $('#constituencyList').val();
 //var urlstr = "voterFamilyInfoAction.action?type=booth&id=123020&publicationDateId=8&typename=''&constituencyId=232&requestFor=hamletBooth&task=importantFamiliesinfoForHamletsByBooth ";
 
-var urlstr = "voterFamilyInfoAction.action?type=booth&id="+selectedId+"&publicationDateId="+mainpublicationId+"&typename=''&constituencyId="+constituencyId+"&requestFor=hamletBooth&task=importantFamiliesinfoForHamletsByBooth ";
+var urlstr = "voterFamilyInfoAction.action?type=booth&id="+selectedId+"&publicationDateId="+mainpublicationId+"&typename=''&constituencyId="+constituencyId+"&requestFor=hamletBooth&task=importantFamiliesinfoForHamletsByBooth&publicationYear="+publicationYear+" ";
 	var browser1 = window.open(urlstr,"familyWiseDetails","scrollbars=yes,height=600,width=700,left=200,top=200");	
 		browser1.focus();
 }
