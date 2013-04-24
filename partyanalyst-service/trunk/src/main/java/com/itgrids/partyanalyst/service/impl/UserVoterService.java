e216563d57bf3382a30c6e1390c6d4673d76e9a1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-24 13:05:17 +0000

    updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/UserVoterService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/UserVoterService.java
@@ -6,44 +6,44 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import com.itgrids.partyanalyst.dao.IUserVoterCategoryDAO;
+import com.itgrids.partyanalyst.dao.IVoterCategoryValueDAO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.service.IUserVoterService;
 
 public class UserVoterService implements IUserVoterService{
 
-	private static final Logger log = Logger.getLogger(VotersAnalysisService.class);
-	private IUserVoterCategoryDAO userVoterCategoryDAO;
+	private static final Logger LOG = Logger.getLogger(UserVoterService.class);
+	
+	private IVoterCategoryValueDAO voterCategoryValueDAO;
 
-	public IUserVoterCategoryDAO getUserVoterCategoryDAO() {
-		return userVoterCategoryDAO;
+	public IVoterCategoryValueDAO getVoterCategoryValueDAO() {
+		return voterCategoryValueDAO;
 	}
 
-	public void setUserVoterCategoryDAO(IUserVoterCategoryDAO userVoterCategoryDAO) {
-		this.userVoterCategoryDAO = userVoterCategoryDAO;
+	public void setVoterCategoryValueDAO(
+			IVoterCategoryValueDAO voterCategoryValueDAO) {
+		this.voterCategoryValueDAO = voterCategoryValueDAO;
 	}
-	
-	
-	
-	public List<SelectOptionVO> getUserVoterCategoryList(List<Long> userId)
+
+
+	public List<SelectOptionVO> getUserVoterCategoryList(List<Long> userIdsList)
 	{
 		List<SelectOptionVO> resultList = new ArrayList<SelectOptionVO>(0);
 		try{
-			if(userId != null && userId.size() > 0)
+			if(userIdsList != null && userIdsList.size() > 0)
 			{
-			List<Object[]> list = userVoterCategoryDAO.getUserCategoriesByUserList(userId);
-			if(list != null && list.size() > 0)
+				List<Object[]> list = voterCategoryValueDAO.getUserVoterCategories(userIdsList);
+				
+				if(list != null && list.size() > 0)
 				for(Object[] params : list)
-				{
 					resultList.add(new SelectOptionVO((Long)params[0],params[1].toString()));	
-				}
-			
-		}
+			}
+			return resultList;
 		}
 		catch(Exception e)
 		{
-			log.error("error occured in the getUserVoterCategoryList() method in VotersAnalysis" , e) ;
-			
+			LOG.error("error occured in the getUserVoterCategoryList() method in VotersAnalysis" , e) ;
+			return resultList;
 		}
-		return resultList;
 	}
 }
