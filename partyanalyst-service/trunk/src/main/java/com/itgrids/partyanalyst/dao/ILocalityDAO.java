648e06f790fdb6b6fb64e9e79b11dc8699085133
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-28 07:57:42 +0000

    updated by anil ,wards implementation

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocalityDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocalityDAO.java
@@ -8,8 +8,8 @@ import com.itgrids.partyanalyst.model.Locality;
 
 public interface ILocalityDAO extends GenericDao<Locality, Long>{
 	
-	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId);
+	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId ,String type ,String queryCondition);
 	public List<Object[]> getLocalitiesForWard(Long wardId , Long userId);
 
-
+	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId );
 }
