3808ba43fdd02ec4f03d530e26711819362d9d2e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-04-28 07:07:47 +0000

    updated by mahesh

---
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SubRegionsWiseAnalysisAction.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.web.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class SubRegionsWiseAnalysisAction {
+     
+	 public String execute(){
+		 
+		 return Action.SUCCESS;
+	 }
+}
