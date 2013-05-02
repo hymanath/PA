4ae04352ef8c86f2d6057d247c770ec9e6ab5272
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-02 09:06:46 +0000

    Updated by AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocalityDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocalityDAO.java
@@ -35,13 +35,17 @@ public class LocalityDAO extends GenericDaoHibernate<Locality, Long> implements 
 		
 		return query.list();
 	}
-	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long hamletId)
+	public List<Object[]> getAllLocalitiesForHamlet(Long userId , Long id,String type)
 	{
+		StringBuilder queryObj = new StringBuilder();
+		queryObj.append("select model.localityId , model.name from Locality model where model.user.userId = :userId ");
+		if(type.equalsIgnoreCase(IConstants.HAMLET))
+			queryObj.append(" and model.hamlet.hamletId = :id ");
+		else if(type.equalsIgnoreCase(IConstants.CUSTOMWARD))
+			queryObj.append(" and model.ward.constituencyId = :id ");
 		
-		Query query = getSession().createQuery("select model.localityId , model.name from Locality model " +
-				"where model.hamlet.hamletId = :hamletId and model.user.userId = :userId");
-		
-		query.setParameter("hamletId", hamletId);
+		Query query = getSession().createQuery(queryObj.toString());
+		query.setParameter("id", id);
 		query.setParameter("userId", userId);
 		
 		return query.list();
