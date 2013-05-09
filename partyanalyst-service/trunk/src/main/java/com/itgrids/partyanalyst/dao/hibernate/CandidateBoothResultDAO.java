6b0c45c5b87ab2a87b7bb6b1a7f4fb406011bd2d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-09 07:18:18 +0000

    updated by Nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateBoothResultDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateBoothResultDAO.java
@@ -1034,38 +1034,7 @@ public class CandidateBoothResultDAO extends GenericDaoHibernate<CandidateBoothR
 	}
 	
 	
-	public List<Object[]> getMandalResult(Long constituencyId,Long electionId)
-	{
-		
-		Query query = getSession().createQuery("select distinct model.boothConstituencyElection.booth.tehsil.tehsilId,model.boothConstituencyElection.booth.tehsil.tehsilName,model.boothConstituencyElection.booth.boothId,model.boothConstituencyElection.boothResult.validVotes from" +
-				" CandidateBoothResult model where model.boothConstituencyElection.booth.constituency.constituencyId =:constituencyId and model.boothConstituencyElection.constituencyElection.election.electionId = :electionId and model.boothConstituencyElection.booth.localBody is null group by model.boothConstituencyElection.booth.tehsil.tehsilId");
-		query.setParameter("constituencyId", constituencyId);
-		query.setParameter("electionId", electionId);
-		
-		return query.list();
-	}
 	
-	public List<Object[]> getmandalEleAndPartiesResults(List<Long> boothIds,Long electionId,List<Long> partyIds)
-	{
-		Query query = getSession().createQuery("select model.nomination.party.partyId, model.nomination.party.shortName,"+
-		" sum(model.votesEarned) from CandidateBoothResult model where model.boothConstituencyElection.constituencyElection.election.electionId = ? and model.nomination.party.partyId in(:partyIds) " +
-		" and model.boothConstituencyElection.booth.boothId in(:boothIds) and model.boothConstituencyElection.booth.localBody is null group by model.nomination.party.partyId order by sum(model.votesEarned) desc");
-		query.setParameter(0,electionId);
-		query.setParameterList("partyIds",partyIds);
-		query.setParameterList("boothIds",boothIds);
-		return query.list();
-	}
-	
-	public List<Object[]> getMuncipalityResult(Long constituencyId,Long electionId)
-	{
-		
-		Query query = getSession().createQuery("select distinct model.boothConstituencyElection.booth.localBody.localElectionBodyId,model.boothConstituencyElection.booth.localBody.name,model.boothConstituencyElection.booth.boothId,model.boothConstituencyElection.boothResult.validVotes from" +
-				" CandidateBoothResult model where model.boothConstituencyElection.booth.constituency.constituencyId =:constituencyId and model.boothConstituencyElection.constituencyElection.election.electionId = :electionId group by model.boothConstituencyElection.booth.localBody.localElectionBodyId");
-		query.setParameter("constituencyId", constituencyId);
-		query.setParameter("electionId", electionId);
-		
-		return query.list();
-	}
 	public List<Object[]> getMandalResultsForElectionAndConstituency(Long constituencyId, List<Long> electionIds,List<Long> partyIds){
 		Query query = getSession().createQuery("select model.boothConstituencyElection.booth.tehsil.tehsilId," +
 				" model.boothConstituencyElection.booth.tehsil.tehsilName,  " +
