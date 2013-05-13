211274d33676e998313aeb7b9a09f33e99baa424
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-13 07:45:19 +0000

    Updated by Kamalakar Dandu

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ValidationToolsAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/ValidationToolsAction.java
@@ -11,14 +11,16 @@ import org.json.JSONObject;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.excel.booth.DataValidationVO;
+import com.itgrids.partyanalyst.helper.EntitlementsHelper;
 import com.itgrids.partyanalyst.service.ICrossVotingEstimationService;
 import com.itgrids.partyanalyst.service.IDataValidationService;
 import com.itgrids.partyanalyst.service.IVotersAnalysisService;
 import com.itgrids.partyanalyst.utils.IConstants;
-import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ValidationToolsAction extends ActionSupport implements ServletRequestAware{
+	
+	private static final long serialVersionUID = 79823356220535818L;
 	private HttpServletRequest request;
 	private HttpSession session;
 	private String task;
@@ -29,7 +31,14 @@ public class ValidationToolsAction extends ActionSupport implements ServletReque
 	private IVotersAnalysisService votersAnalysisService;
 	private IDataValidationService dataValidationService;
 	private List<DataValidationVO> resultList;
+	private EntitlementsHelper entitlementsHelper;
 	
+	public EntitlementsHelper getEntitlementsHelper() {
+		return entitlementsHelper;
+	}
+	public void setEntitlementsHelper(EntitlementsHelper entitlementsHelper) {
+		this.entitlementsHelper = entitlementsHelper;
+	}
 	public HttpServletRequest getRequest() {
 		return request;
 	}
@@ -112,6 +121,13 @@ public class ValidationToolsAction extends ActionSupport implements ServletReque
 		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
 		if(user == null)
 			return INPUT;
+		
+		if(session.getAttribute(IConstants.USER) == null && 
+				!entitlementsHelper.checkForEntitlementToViewReport(null, IConstants.VOTER_SEARCH_AND_EDIT))
+			return INPUT;
+		if(!entitlementsHelper.checkForEntitlementToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.VOTER_SEARCH_AND_EDIT))
+			return ERROR;
+		
 		constituencyList = user.getUserAccessVoterConstituencies();
 		if(constituencyList == null || constituencyList.isEmpty()){
 			Long userID = user.getRegistrationID();
@@ -130,20 +146,25 @@ public class ValidationToolsAction extends ActionSupport implements ServletReque
 		String param;
 		param = getTask();
 		HttpSession session = request.getSession();
-		RegistrationVO user=(RegistrationVO) session.getAttribute("USER");
-		if(user == null)
-		return ERROR;
-		Long userID = user.getRegistrationID();
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		
+		Long userId = null;
+		if(user != null && user.getRegistrationID() != null)
+			if(user.getParentUserId()!=null)
+				userId = user.getMainAccountId();
+			else
+				userId = user.getRegistrationID();
+		else 
+		  return "error";
+		
 		try{
 			jObj = new JSONObject(param);	
-			
-		}catch (Exception e) {
+		}catch (Exception e){
 			e.printStackTrace();
-			
-		}
+		}	
 		if(jObj.getString("task").equalsIgnoreCase("getSubLevelInfo"))
 		{
-			resultList = dataValidationService.getHamletsAssignedValidation(jObj.getLong("constituencyId"),jObj.getLong("publicationDateId"));
+			resultList = dataValidationService.getHamletsAssignedValidation(jObj.getLong("constituencyId"),jObj.getLong("publicationDateId"),userId);
 		}
 		return SUCCESS;
 		
