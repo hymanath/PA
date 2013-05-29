89c0ee480919d7a0077f3307b6f2d9a36d20f25c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-29 12:04:26 +0000

    updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/CommonStringUtils.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/CommonStringUtils.java
@@ -1,16 +1,16 @@
 package com.itgrids.partyanalyst.utils;
 
-import org.hibernate.mapping.Array;
 
 public class CommonStringUtils {
 	
 	public static String removeSpecialCharsFromAString(String textValue){
 		if(textValue != null){
-			textValue += "";
-			String[] j=IConstants.SPECIALCHARS;
+			/*String[] j=IConstants.SPECIALCHARS;
 			for(int i=0;i<j.length;i++){
 				textValue=textValue.replace(j[i], "");
-			}
+			}*/
+			textValue = textValue.replaceAll("\uFFFD", "");
+			textValue = textValue.replaceAll("&#65533;", "");
 		}
 		return textValue;
 	}
