92ac3e90128e8b528d9d2a96711b0fdc10d98c2f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-29 07:52:36 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICustomVoterDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICustomVoterDAO.java
@@ -1,19 +1,24 @@
 package com.itgrids.partyanalyst.dao;
 
 import java.util.List;
-
 import org.appfuse.dao.GenericDao;
-
 import com.itgrids.partyanalyst.model.CustomVoter;
 
 public interface ICustomVoterDAO extends GenericDao<CustomVoter,Long>{
 	
 	public List<Object[]> getVoterGroupNamesByVoterIdsList(List<Long> voterIdsList);
+	
 	public void removeCustomVoterDetails(Long customVoterId);
+	
 	public List<CustomVoter> getCustomVoterByVoterIdAndUserId(Long voterId , Long customGroupId);
+	
 	public List<Long> getCustomGroupIdByVoterIdAndUserId(Long voterId , Long userId);
+	
 	public List<Object[]> getAllVotersGroups(List<Long> voterIds , Long userId);
+	
 	public List<Long> getCustomVoterIdByVoterIdAndUserId(Long voterId , Long userId);
+	
+	public List<Object[]> getVotersInfoBycustomVoterGroupId(Long customVoterGroupId,Long userId);
 
 
 		
