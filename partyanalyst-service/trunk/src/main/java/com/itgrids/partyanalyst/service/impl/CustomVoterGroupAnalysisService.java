48f21fd970a0ab055f65981662edc9a353fc56e7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-29 06:35:58 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CustomVoterGroupAnalysisService.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICustomVoterGroupAnalysisService;
+
+public class CustomVoterGroupAnalysisService implements ICustomVoterGroupAnalysisService {
+	
+public static Logger Log = Logger.getLogger(CustomVoterGroupAnalysisService.class);
+
+}
