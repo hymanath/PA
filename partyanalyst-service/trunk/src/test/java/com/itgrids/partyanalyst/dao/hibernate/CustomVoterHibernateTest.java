a39b16c5f22eeb1e9fa05e5af8d61a7b2e9908b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-05-29 07:50:59 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CustomVoterHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CustomVoterHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ICustomVoterDAO;
@@ -8,8 +10,15 @@ public class CustomVoterHibernateTest  extends BaseDaoTestCase{
 	
 	private ICustomVoterDAO customVoterDAO;
 
-    public void setCustomVoterGroupDAO(ICustomVoterDAO customVoterDAO) {
-	 this.customVoterDAO = customVoterDAO;
-   }
+    
+    public void setCustomVoterDAO(ICustomVoterDAO customVoterDAO) {
+		this.customVoterDAO = customVoterDAO;
+	}
+
+	public void testGetVotersInfoBycustomVoterGroupId()
+    {
+    	List<Object[]> list = customVoterDAO.getVotersInfoBycustomVoterGroupId(1l,1l);
+    	System.out.println(list.size());
+    }
 
 }
