3927ebe132bb6dec475b0731ba330809b448625f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-01 10:39:13 +0000

    Updated by Sasi,Age Wise Voters Info in CustomVoterGroups

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICustomVoterGroupAnalysisService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICustomVoterGroupAnalysisService.java
@@ -2,10 +2,11 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.VotersDetailsVO;
 import com.itgrids.partyanalyst.dto.VoterCastInfoVO;
 
 public interface ICustomVoterGroupAnalysisService {
 	
 	public List<VoterCastInfoVO> getCasteWiseCustomVotersCount(Long customVoterGroupId,Long userId);
-
+	public VotersDetailsVO getAgeWiseCustomVotersInGroup(Long userId,Long customGroupId,Long publicationDateId);
 }
