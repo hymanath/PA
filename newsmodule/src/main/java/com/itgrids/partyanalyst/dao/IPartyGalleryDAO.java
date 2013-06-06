a70392662d55c1c2b144442fa6279ae475fb51cf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-06 09:38:19 +0000

    updated by anil

---
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IPartyGalleryDAO.java
@@ -0,0 +1,42 @@
+package com.itgrids.partyanalyst.dao;
+import java.util.List;
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.File;
+import com.itgrids.partyanalyst.model.Party;
+import com.itgrids.partyanalyst.model.PartyGallery;
+
+public interface IPartyGalleryDAO extends GenericDao<PartyGallery,Long>{
+
+	public List<Object[]> getPartyGallaryDetail(Long partyId,int firstResult,int maxResult,String type) ;
+	
+	public List<File> getFirstFourNewsForParty(Long partyId,int firstResult,int maxResult,String queryType);
+	
+	public List<Object[]> getNewsByScope(Long partyId,Long scopeType,int startIndex,int maxResults,String queryType , String sourceStr , String languageStr);
+	
+	public List<Object[]> getAllRecordInGallary(Long gallaryId);
+	
+	public List<Object[]> getGallariesByPartyId(Long partyId,String contentType);
+	
+	public List<Object[]> getPartyGalleriesDescForUpdate(Long gallaryId,Long partyId);
+	
+	public List<Party> getPartyByGalleryId(Long gallaryId);
+	
+	public List<Object> getPartyVisibility(Long gallaryId);
+	
+	public List<Object> getOtherGalleries(Long partyId,List<Long>gallaryIds,String contentType);
+	
+	public List<Object[]> getNewsForParty(Long partyId,int firstResult,int maxResult,String queryType);
+	
+	public Integer deletePartyGallary(Long gallaryId);
+	
+	public List<Object[]> getAllNewsDetails(Long partyId,int firstResult,int maxResult,String queryType);
+	
+	public List<Long> getNewsCountByScope(Long partyId,Long scopeType,String queryType);
+	
+	public List<Object[]> getPartyGallaryByPartyId(Long partyId, String contentType, String gallaryName);
+	//tdp news int
+	public List<Object[]> getAllNewsDetailsForState(Long partyId,int firstResult,int maxResult,String queryType,long stateId,long scopeValue);
+	public List<Object[]> getAllNewsDetailsForDistrict(Long partyId,int firstResult,int maxResult,String queryType,long distScope ,List<Long> districtIds);
+	public List<Object[]> getGallariesByPartyIdForState(Long partyId,String contentType,int firstResult,int maxResult);
+
+}
