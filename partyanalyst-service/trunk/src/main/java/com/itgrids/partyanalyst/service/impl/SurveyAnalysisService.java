2c504c7ca1fba82aaca1dae85c82cda754f8827b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-12 07:36:26 +0000

    updated by mahesh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SurveyAnalysisService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ISurveyAnalysisService;
+
+public class SurveyAnalysisService implements ISurveyAnalysisService {
+  
+}
