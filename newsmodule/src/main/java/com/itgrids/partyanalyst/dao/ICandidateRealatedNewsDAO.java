f4f3e8f9a9286c4d6d820cd15f717611e63be217
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-17 10:11:38 +0000

    New upload for multiple candidates - samba

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidateRealatedNewsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.dao;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.partyanalyst.model.CandidateRealatedNews;
-
-public interface ICandidateRealatedNewsDAO extends GenericDao<CandidateRealatedNews, Long>{
-
-}
---
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateRelatedNewsDAO.java
@@ -0,0 +1,32 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.ICandidateRelatedNewsDAO;
+import com.itgrids.partyanalyst.model.CandidateRealatedNews;
+
+public class CandidateRelatedNewsDAO extends GenericDaoHibernate<CandidateRealatedNews, Long> implements 
+ICandidateRelatedNewsDAO {
+	
+	
+	public CandidateRelatedNewsDAO() {
+		super(CandidateRealatedNews.class);
+	}
+	
+	public List<Object[]> getAllfileGallariesOfCandidate(Long candidateId)
+	{
+		Query query = getSession().createQuery("select model.fileGallary.fileGallaryId , model.fileGallary.file.fileTitle " +
+				"from CandidateRealatedNews model where model.candidate.candidateId = :candidateId and model.fileGallary.isDelete = :isDelete " +
+				"order by model.fileGallary.file.fileTitle");
+		
+		query.setParameter("candidateId", candidateId);
+		query.setParameter("isDelete", "false");
+		return query.list();
+		
+		
+	}
+
+}
