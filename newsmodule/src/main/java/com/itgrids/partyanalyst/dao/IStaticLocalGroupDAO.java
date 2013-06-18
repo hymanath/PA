ecdd0ffdf3f8c7056b760a89cb15b40047e0ea4b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-18 12:06:43 +0000

    Updated by Srishailam (copy rights to TDP Party)

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IStaticLocalGroupDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IStaticLocalGroupDAO.java
@@ -1,9 +1,6 @@
 /* 
- * Copyright (c) 2009 IT Grids.
+ * Copyright (c) 2013 TDP PARTY .
  * All Rights Reserved.
- *
- * IT Grids Confidential Information.
- * Created on October 25, 2010
  */
 package com.itgrids.partyanalyst.dao;
 
