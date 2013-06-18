37cb298b4dea7229530826a3965de8931a93ba64
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-18 12:07:05 +0000

    Updated by Srishailam (copy rights to TDP Party)

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HamletDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HamletDAO.java
@@ -1,9 +1,6 @@
 /* 
- * Copyright (c) 2009 IT Grids.
+ * Copyright (c) 2013 TDP PARTY .
  * All Rights Reserved.
- *
- * IT Grids Confidential Information.
- * Created on December 1, 2009
  */
 package com.itgrids.partyanalyst.dao.hibernate;
 
