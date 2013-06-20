01ede51e8c7ea84e8c1ef84dcf6f7eaa03544008
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-20 10:24:30 +0000

    Project Created by Kamalakar Dandu

---
+++ ElectoralConnect/src/main/java/com/itgrids/electoralconnect/util/IWebConstants.java
@@ -0,0 +1,34 @@
+package com.itgrids.electoralconnect.util;
+
+public interface IWebConstants {
+
+	public static final String USER = "USER";
+	public static final long FILE_MAX_LENGTH = 2097152;
+	public static final int FILE_RANDOM_NO = 1000000000;
+	
+	public static final String UPLOADED_FILES = "uploaded_files";
+	public static final String PARTY_PROFILE = "party_profile";
+	public static final String CANDIDATE_PROFILE = "candidate_profile";
+	public static final String SPECIAL_PAGE = "special_page";
+	public static final String NEWS_GALLERY = "news_gallery";
+	public static final String PHOTO_GALLERY = "photo_gallery";
+	
+	public static final String PANCHAYATS = "panchayats";
+	public static final String REVENUE_VILLAGES = "revenueVillages";
+	
+	public static final String LEFT_NAVIGATION = "left_navigation";
+	public static final String BANNER = "banner";
+	public static final String RIGHT_NAVIGATION = "right_navigation";
+	public static final String CENTER_DIV = "center_div";
+	
+	public static final String CUSTOM_JSP_PAGES_PATH = "custom_jsp_pages";
+	
+	public static final String LOGIN = "Login";
+	public static final String LOGOUT = "Logout";
+	public static final String FAILURE = "failure";
+	public static final String SUCCESS = "success";
+	public static final String PROBLEM_FILES = "Problem_Files";
+	
+	public static final String CHART_URL_IN_SERVER = "";
+	public static final String STATIC_CONTENT_FOLDER_URL = "";
+}
---
+++ ElectoralConnect/src/main/webapp/index.jsp
@@ -0,0 +1,1 @@
+<% response.sendRedirect("loginPageAction.action"); %>
