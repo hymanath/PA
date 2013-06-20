e5ffc8b1dcb3a03a31b20bb67d23166ef917f393
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-20 13:39:11 +0000

    Updated By Srishailam

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FileGallaryDAO.java
@@ -2793,7 +2793,7 @@ public List<Object[]> getNewsByForConstituencyWithMuncipalityWithWards(NewsCount
 			 if(queryType.equalsIgnoreCase("Private"))
 			 query.append(" and model.isPrivate = :isPrivate and ( (model.gallary.isPrivate='true') or(model.gallary.isPrivate='false' and model.isPrivate ='true') ) ");
 			 //query.append(" and model.file.locationValue = :locationValue and model.file.regionScopes.regionScopesId = :locId ");
-			 query.append(" group by model2.gallery.gallaryId order by model2.gallery.updateddate desc ");
+			 query.append(" order by model.updateddate desc ");
 			 Query queryObject = getSession().createQuery(query.toString());
 
 			 queryObject.setLong("partyId", IConstants.TDPID);
