7ecfb602e3ec27c022e007c559d2ed088cea42c4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-20 13:36:55 +0000

    Updated By Srishailam

--- newsmodule/src/main/java/com/itgrids/partyanalyst/web/action/AdminPageAction.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/web/action/AdminPageAction.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.web.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 
+import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -11,6 +13,7 @@ import com.opensymphony.xwork2.ActionSupport;
 public class AdminPageAction extends ActionSupport implements ServletRequestAware{
 
 	private HttpServletRequest request;
+	private HttpSession session;
 	//@Override
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
@@ -18,7 +21,11 @@ public class AdminPageAction extends ActionSupport implements ServletRequestAwar
 
 	public String execute()
 	{
-		
+		session = request.getSession();	
+		RegistrationVO user = (RegistrationVO)session.getAttribute("USER");
+		if(user == null)
+			return ERROR;
+		else
 		return Action.SUCCESS;
 	}
 	
