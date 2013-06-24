a3d247c0ea24a6c6d41461397089660e0b707402
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-24 12:24:42 +0000

    Updated by AnuRadha

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/INewsDetailsDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/INewsDetailsDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.NewsDetails;
 
 public interface INewsDetailsDAO extends GenericDao<NewsDetails,Long> {
+	
+	public List<Object[]> getEditionAndPageNoByFileSourceId(Long fileSourceLanguageId);
 
 }
--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NewsDetailsDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NewsDetailsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.INewsDetailsDAO;
 import com.itgrids.partyanalyst.model.NewsDetails;
@@ -8,5 +11,15 @@ import com.itgrids.partyanalyst.model.NewsDetails;
 public class NewsDetailsDAO  extends GenericDaoHibernate<NewsDetails,Long> implements INewsDetailsDAO {
 	public NewsDetailsDAO() {
 		super(NewsDetails.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getEditionAndPageNoByFileSourceId(Long fileSourceLanguageId)
+	{
+		Query query = getSession().createQuery(" select model.pageNo, model.edition from NewsDetails model where " +
+				" model.fileSourceLanguage.fileSourceLanguageId =:fileSourceLanguageId ");
+		query.setParameter("fileSourceLanguageId", fileSourceLanguageId);
+		return query.list();
+		
 	}
 }
