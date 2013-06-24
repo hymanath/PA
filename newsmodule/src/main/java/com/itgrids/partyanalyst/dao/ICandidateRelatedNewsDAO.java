dcc2029b5f036135f6614d8d28581256c3a65a80
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-24 13:58:14 +0000

    Issues in news update and selecting news in a date range  - samba

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidateRelatedNewsDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidateRelatedNewsDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -7,6 +8,8 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.CandidateRealatedNews;
 
 public interface ICandidateRelatedNewsDAO extends GenericDao<CandidateRealatedNews, Long>{
-	public List<Object[]> getAllfileGallariesOfCandidate(Long candidateId);
+	public List<Object[]> getAllfileGallariesOfCandidate(Long candidateId  ,Date fromDate  ,Date toDate);
 	public List<Object[]> getCandidates();
+	public List<Object[]> getCandidatesContainsNews();
+
 }
