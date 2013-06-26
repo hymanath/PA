2f10744a698f74636bf228787442bde4d0c60fad
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-26 21:58:12 +0000

    Updated by Sasi,For Booth Wise Election Results

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAreaTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAreaTypeDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.AreaType;
 
 public interface IAreaTypeDAO extends GenericDao<AreaType,Long>{
-
+	public List<Object[]> getElectionYears(Long stateId,Long electionType);
+	public List<Object[]> getPartiesForElection(Long electionId,Long constituencyId);
 }
