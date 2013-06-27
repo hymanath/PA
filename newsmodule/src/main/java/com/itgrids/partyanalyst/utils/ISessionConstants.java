7b1751c9e47346e5c96bb5b25b5d895e1323b530
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-27 14:04:43 +0000

    Updated By Srishailam (changed pom.xml)

---
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/utils/ISessionConstants.java
@@ -0,0 +1,94 @@
+package com.itgrids.partyanalyst.utils;
+
+public interface ISessionConstants {
+	//REGIONAL VARIABLES FOR CURRENT ADDRESS
+	public static final String STATES = "statesList";
+	public static final String DISTRICTS = "districtsList";
+	public static final String CONSTITUENCIES = "constituenciesList";
+	//parliament Constituencies
+	public static final String P_CONSTITUENCIES = "p_constituencies";
+	public static final String MANDALS = "mandalsList";
+	public static final String VILLAGES = "villagesList";
+	public static final String WARDS_OR_HAMLETS = "wardsOrHamletsList";
+	public static final String BOOTHS = "boothsList";
+	// ADDRESS VARIBALES FOR OFFICIAL ADDRESS
+	public static final String STATES_O = "statesList_o";
+	public static final String DISTRICTS_O = "districtsList_o";
+	public static final String CONSTITUENCIES_O = "constituenciesList_o";
+	public static final String P_CONSTITUENCIES_O = "p_constituencies_o";
+	public static final String MANDALS_O = "mandalsList_o";
+	public static final String VILLAGES_O = "villagesList_o";
+	public static final String BOOTHS_O = "boothsList_o";
+	// cadre levle variables
+	public static final String CADRE_LEVELS_LIST = "cadreLevelsList";
+	public static final String DISTRICTS_C = "districtsList_c";
+	public static final String STATES_C = "statesList_c";
+	public static final String P_CONSTITUENCIES_C = "p_constituencies_c";
+	public static final String CONSTITUENCIES_C = "constituenciesList_c";
+	public static final String MANDALS_C = "mandalsList_c";
+	public static final String VILLAGES_C = "villagesList_c";
+	public static final String BOOTHS_C = "boothsList_c";
+	
+	//location variables used in add new problem ap is shorct cut for add new problem
+	public static final String STATES_AP = "statesList_ap";
+	public static final String DISTRICTS_AP = "districtsList_ap";
+	public static final String CONSTITUENCIES_AP = "constituenciesList_ap";
+	//parliament Constituencies
+	public static final String P_CONSTITUENCIES_AP = "p_constituencies_ap";
+	public static final String MANDALS_AP = "mandalsList_ap";
+	public static final String VILLAGES_AP = "villagesList_ap";
+	public static final String WARDS_OR_HAMLETS_AP = "wardsOrHamletsList_ap";
+	public static final String BOOTHS_AP = "boothsList_ap";
+	
+	
+	//contains social categories like sc, st etc.
+	public static final String SOCIALCATEGORIES = "socialStatus";
+	// contains educational qualifications
+	public static final String EDU_QUALIFICATIONS = "eduQualsList";
+	
+	public static final String OCCUPATIONS = "occupationsList";
+	public static final String LANGUAGES = "languagesList";
+	
+	//party committees specific to party
+	public static final String PARTY_COMMITTEES = "partyCommittees";
+	public static final String COMMITTEE_DESIGNATIONS = "designations";
+	
+	//cadre skills list based on party
+	public static final String CADRE_SKILLS = "cadreSkills";
+	// training camps based on party
+	public static final String PARTY_TRAINING_CAMPS="partyTrainingCamps";
+	public static final String GENDERS = "genders";
+	public static final String DOB_OPTIONS = "dob_Options";
+	public static final String CADRETYPES = "cadreType";
+	public static final String LANGUAGE_OPTIONS = "language_options";
+	//booleans used whether to show or hide the corresponding content 
+	public static final String PARTY_COMMITTEES_FLAG = "partyCommittees_flag"; 
+	public static final String CADRE_SKILLS_FLAG = "cadreSkills_flag"; 
+	public static final String PARTY_TRAINING_CAMPS_FLAG = "partyTrainingCamps_flag";
+	//relations list
+	public static final String FAMILY_RELATIONS = "familyRelationsList";
+	
+	//list of information sources for problem complains, local policitcal changes, influencing people 
+	public static final String INFO_SOURCES = "informationSourcesList";
+	
+	//list of information static(main) parties 
+	public static final String MAIN_PARTIES = "mainPartiesList";
+	
+	//list of impacted regions for local policitcal changes, influencing people, problem management 
+	public static final String IMPACTED_REGIONS = "impactedRegionsList";
+	public static final String PROBLEM_TYPES = "problemTypesList";
+	
+	
+	public static final String USER_GROUP_CATEGORIES = "USER_GROUP_CATEGORIES";
+	public static final String USER_GROUP_SCOPES = "USER_GROUP_SCOPES";
+	public static final String CADRE_ROLES_LIST = "CadreRolesList";
+	
+	public static final String STATES_PROB 			= "statesListForProb";
+	public static final String DISTRICTS_PROB 		= "districtsListForProb";
+	public static final String CONSTITUENCIES_PROB	= "costituenciesListForProb";
+	public static final String MANDALS_PROB 		= "mandalsListForProb";
+	public static final String VILLAGES_PROB 		= "villagesListForProb";
+	
+	public static final String WINDOW_TASK = "windowTask";
+	public static final String BLOOD_GROUPS = "bloodGroups";
+}
