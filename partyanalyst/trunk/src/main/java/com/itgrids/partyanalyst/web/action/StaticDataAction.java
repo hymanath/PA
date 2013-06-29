ee9142a111da953b673c63b7ef243b8d51b87c49
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-06-29 11:39:12 +0000

    updated by prasad.

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/StaticDataAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/StaticDataAction.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,9 +11,11 @@ import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
+import com.google.gdata.util.parser.Action;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.helper.EntitlementsHelper;
+import com.itgrids.partyanalyst.service.ICrossVotingEstimationService;
 import com.itgrids.partyanalyst.service.IRegionServiceData;
 import com.itgrids.partyanalyst.service.IStaticDataService;
 import com.itgrids.partyanalyst.utils.IConstants;
@@ -32,7 +35,12 @@ public class StaticDataAction  extends ActionSupport implements ServletRequestAw
 	private List<SelectOptionVO> partyList;
 	private List<SelectOptionVO> commonList;
 	private EntitlementsHelper entitlementsHelper;
-	
+	private List<SelectOptionVO> userAccessConstituencyList;
+	private ICrossVotingEstimationService crossVotingEstimationService;
+	private List<Long> constituencyIds;
+	private List<SelectOptionVO> returnList;
+	JSONObject jObj;
+	private String task;
 	public void setServletRequest(HttpServletRequest arg0) {
 		request = arg0;
 	}
@@ -100,14 +108,79 @@ public class StaticDataAction  extends ActionSupport implements ServletRequestAw
 		this.entitlementsHelper = entitlementsHelper;
 	}
 
+	
+	public List<SelectOptionVO> getUserAccessConstituencyList() {
+		return userAccessConstituencyList;
+	}
+
+	public void setUserAccessConstituencyList(
+			List<SelectOptionVO> userAccessConstituencyList) {
+		this.userAccessConstituencyList = userAccessConstituencyList;
+	}
+
+	public ICrossVotingEstimationService getCrossVotingEstimationService() {
+		return crossVotingEstimationService;
+	}
+
+	public void setCrossVotingEstimationService(
+			ICrossVotingEstimationService crossVotingEstimationService) {
+		this.crossVotingEstimationService = crossVotingEstimationService;
+	}
+
+	public IStaticDataService getStaticDataService() {
+		return staticDataService;
+	}
+	
+	public List<Long> getConstituencyIds() {
+		return constituencyIds;
+	}
+
+	public void setConstituencyIds(List<Long> constituencyIds) {
+		this.constituencyIds = constituencyIds;
+	}
+
+	
+	public List<SelectOptionVO> getreturnList() {
+		return returnList;
+	}
+
+	public void setreturnList(List<SelectOptionVO> returnList) {
+		this.returnList = returnList;
+	}
+
+	
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
 	public String getStatesList() throws Exception{
 		HttpSession session = request.getSession();
-		if(session.getAttribute(IConstants.USER) == null && 
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		if(user == null && 
 				!entitlementsHelper.checkForEntitlementToViewReport(null, IConstants.PARTY_BOOTHWISE_RESULTS_REPORT))
 			return INPUT;
 		if(!entitlementsHelper.checkForEntitlementToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.PARTY_BOOTHWISE_RESULTS_REPORT))
 			return ERROR;
-		states = regionServiceDataImp.getStatesByCountryFromBooth(1L);
+		Long userID = user.getRegistrationID();
+		Long electionYear = new Long(IConstants.PRESENT_ELECTION_YEAR);
+		Long electionTypeId = new Long(IConstants.ASSEMBLY_ELECTION_TYPE_ID);
+		userAccessConstituencyList = crossVotingEstimationService.getConstituenciesForElectionYearAndTypeWithUserAccess(userID,electionYear,electionTypeId);
+		if(userAccessConstituencyList != null && userAccessConstituencyList.size() > 0)
+			constituencyIds = new ArrayList<Long>();
+		{
+			for (SelectOptionVO constituencyList : userAccessConstituencyList) {
+				constituencyIds.add(constituencyList.getId());
+			}
+		}
+		
+		//states = regionServiceDataImp.getStatesByCountryFromBooth(1L);
+		states = staticDataService.getStatesByConstituency(constituencyIds);
+		
+		
 		Collections.sort(states);
 		states.add(0, new SelectOptionVO(0L,"Select State"));
 		
@@ -142,5 +215,47 @@ public class StaticDataAction  extends ActionSupport implements ServletRequestAw
 			log.debug("commonList.size():::"+commonList.size());
 		return SUCCESS;
 	}
+	
+	public String getDistrictsAndConstituencysList1()
+	{
+		try {
+			jObj = new JSONObject(getTask());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		HttpSession session = request.getSession();
+		RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+		if(user == null && 
+				!entitlementsHelper.checkForEntitlementToViewReport(null, IConstants.PARTY_BOOTHWISE_RESULTS_REPORT))
+			return INPUT;
+		if(!entitlementsHelper.checkForEntitlementToViewReport((RegistrationVO)session.getAttribute(IConstants.USER), IConstants.PARTY_BOOTHWISE_RESULTS_REPORT))
+			return ERROR;
+		Long userID = user.getRegistrationID();
+		Long electionYear = new Long(IConstants.PRESENT_ELECTION_YEAR);
+		Long electionTypeId = new Long(IConstants.ASSEMBLY_ELECTION_TYPE_ID);
+		userAccessConstituencyList = crossVotingEstimationService.getConstituenciesForElectionYearAndTypeWithUserAccess(userID,electionYear,electionTypeId);
+		if(userAccessConstituencyList != null && userAccessConstituencyList.size() > 0)
+			constituencyIds = new ArrayList<Long>();
+		{
+			for (SelectOptionVO constituencyList : userAccessConstituencyList) {
+				constituencyIds.add(constituencyList.getId());
+			}
+		}
+		if(jObj.getString("task").equalsIgnoreCase("getDistricts"))
+		{
+			Long stateId = jObj.getLong("stateId");
+			returnList = staticDataService.getDistrictsByConstituency(constituencyIds,stateId);
+		}
+		
+		else if(jObj.getString("task").equalsIgnoreCase("getConstituencys"))
+		{
+			Long districtId = jObj.getLong("districtId");
+			returnList = staticDataService.getConstituencyForSelDistrict(constituencyIds,districtId);
+		}
+		
+		return SUCCESS;
+	}
+	
+	
 
 }
