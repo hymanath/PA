83d31d5706befd1c51467e3113ec6f4f35eb449d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-01 06:54:07 +0000

    Files to be changed while deploying code for tdp project by mahesh

---
+++ partyanalyst/trunk/others/IConstants.java
@@ -0,0 +1,7 @@
+
+public class IConstants {
+	
+    public final static String LOCAL_HOST = "tdpserver";
+	
+    public final static String DEPLOYED_HOST = "tdpserver";
+}
---
+++ partyanalyst/trunk/others/IWebConstants.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.util;
+
+public interface IWebConstants {
+
+	public static final String CHART_URL_IN_SERVER = "/static_content/charts/";
+	public static final String STATIC_CONTENT_FOLDER_URL = "/static_content/";
+}
