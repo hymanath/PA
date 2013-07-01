7813522428e703c4a0cd14a90e8701cc97b9dc01
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-01 11:18:50 +0000

    updated by prasad.For User Creation By Admin

---
+++ ElectoralConnect/src/main/java/com/itgrids/electoralconnect/web/action/AdminPageAction.java
@@ -0,0 +1,24 @@
+package com.itgrids.electoralconnect.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class AdminPageAction extends ActionSupport implements ServletRequestAware{
+
+	private HttpServletRequest request;
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request  = request;
+	}
+	
+	public String execute()
+	{
+		return Action.SUCCESS;
+	}
+
+}
