684366a8cdb5a0bffe2ef7daeab6fff1f9413578
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-03 09:48:10 +0000

    updated by prasad.For Comments Displaying

--- ElectoralConnect/src/main/java/com/itgrids/electoralconnect/dao/ICommentDAO.java
+++ ElectoralConnect/src/main/java/com/itgrids/electoralconnect/dao/ICommentDAO.java
@@ -1,10 +1,16 @@
 package com.itgrids.electoralconnect.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.electoralconnect.model.Comment;
-import com.itgrids.electoralconnect.model.UserRoles;
 
 public interface ICommentDAO  extends GenericDao<Comment, Long> {
 	
+	//public List<Object[]> getTop5Comments(Long announcementid);
+	
+	public List<Object[]> getAllComments(Long announcementid,int minIndex,int maxIndex);
+	
+	public Long getTotalCommentsCountByAnnouncementId(Long id);
 }
--- ElectoralConnect/src/test/java/com/itgrids/electoralconnect/dao/hibernatetest/CommentDAOHibernateTest.java
+++ ElectoralConnect/src/test/java/com/itgrids/electoralconnect/dao/hibernatetest/CommentDAOHibernateTest.java
@@ -1,9 +1,11 @@
 package com.itgrids.electoralconnect.dao.hibernatetest;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.electoralconnect.dao.ICommentDAO;
-import com.itgrids.electoralconnect.dao.IUserRolesDAO;
+
 
 public class CommentDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -14,8 +16,28 @@ public class CommentDAOHibernateTest extends BaseDaoTestCase{
 	}
 
 
-	public void test(){
+	/*public void test(){
 		commentDAO.getAll();
-	}
+	}*/
 
+	/*public void testgetTop5Comments()
+	{
+		List<Object[]> values = commentDAO.getTop5Comments(1l);
+		for (Object[] parms : values) {
+			System.out.println(parms[0]  +":"+ parms[1]);
+		}
+	}*/
+	
+	/*public void testgetTop5Comments()
+	{
+		List<Object[]> values = commentDAO.getAllComments(1l,0,3);
+		for (Object[] parms : values) {
+			System.out.println(parms[0]  +":"+ parms[1]);
+		}
+	}*/
+	
+	public void testCount()
+	{
+		System.out.println(commentDAO.getTotalCommentsCountByAnnouncementId(1l));
+	}
 }
