a5a92a0e4e0f412eaec5e3a8e553b9162289fe1f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-04 07:58:04 +0000

    updated by prasad.For Admin Page

--- ElectoralConnect/src/main/java/com/itgrids/electoralconnect/web/action/AdminRegisterSaveAction.java
+++ ElectoralConnect/src/main/java/com/itgrids/electoralconnect/web/action/AdminRegisterSaveAction.java
@@ -1,6 +1,8 @@
 package com.itgrids.electoralconnect.web.action;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
 import com.itgrids.electoralconnect.dto.RegistrationVO;
@@ -16,8 +18,9 @@ import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
 import com.opensymphony.xwork2.validator.annotations.ValidatorType;
 
 @SuppressWarnings("serial")
-public class RegisterSaveAction extends ActionSupport implements ServletRequestAware{
+public class AdminRegisterSaveAction extends ActionSupport implements ServletRequestAware{
 
+	private static final Logger LOG = Logger.getLogger(AdminRegisterSaveAction.class);
 	private String firstName;
 	private String lastName;
 	private String emailId;
@@ -27,6 +30,7 @@ public class RegisterSaveAction extends ActionSupport implements ServletRequestA
 	private HttpServletRequest request;
 	private IMailService mailService;
 	private String userType;
+	private Long type;
 	public String getFirstName() {
 		return firstName;
 	}
@@ -88,32 +92,40 @@ public class RegisterSaveAction extends ActionSupport implements ServletRequestA
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
+	
+	public Long getType() {
+		return type;
+	}
+
+	public void setType(Long type) {
+		this.type = type;
+	}
 
 	public String execute()
 	{
-		//HttpSession session = request.getSession();
-		LOG.debug("In RegisterUserActions execute()");
-		System.out.println(firstName+" "+lastName);
-		UserProfileVO userProfileVO=new UserProfileVO();
-		userProfileVO.setFirstName(firstName);
-		userProfileVO.setLastName(lastName);
-		userProfileVO.setEmailId(emailId);
-		userProfileVO.setMobileNo(mobileNo);
-		userProfileVO.setEpicId(epicId);
-		userProfileVO.setUserType(userType);
-		RegistrationVO  regVO = userService.registerUser(userProfileVO);
-		String requestURL= request.getRequestURL().toString();
-		//System.out.println(requestURL);
-		//session.setAttribute("USER", regVO);
-		String requestFrom = "";
-		if(requestURL.contains("www.partyanalyst.com"))
-			requestFrom = IConstants.SERVER;
-		else
-			requestFrom = IConstants.LOCALHOST;
-		
-		ResultStatus rs = mailService.sendRegistrationNotification(regVO,requestFrom);
-		
+		try {
+			LOG.debug("In AdminRegisterSaveAction execute()");
+			UserProfileVO userProfileVO=new UserProfileVO();
+			userProfileVO.setFirstName(firstName);
+			userProfileVO.setLastName(lastName);
+			userProfileVO.setEmailId(emailId);
+			userProfileVO.setMobileNo(mobileNo);
+			userProfileVO.setEpicId(epicId);
+			userProfileVO.setUserType(userType);
+			RegistrationVO  regVO = userService.registerUser(userProfileVO);
+			String requestURL= request.getRequestURL().toString();
+			String requestFrom = "";
+			if(requestURL.contains("www.partyanalyst.com"))
+				requestFrom = IConstants.SERVER;
+			else
+				requestFrom = IConstants.LOCALHOST;
+			
+			ResultStatus rs = mailService.sendRegistrationNotification(regVO,requestFrom);
+		} catch (Exception e) {
+			LOG.error("Exception raised in execute() method in AdminRegisterSaveAction Action", e);
+		}
 		return SUCCESS;
+	
 	}
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
