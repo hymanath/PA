26cdaf2c00520c14345b376d2ebe90ade29f5a0b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-05 06:58:45 +0000

    Updated by AnuRadha

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidateNewsResponseDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidateNewsResponseDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -17,5 +18,9 @@ public interface ICandidateNewsResponseDAO extends GenericDao<CandidateNewsRespo
    public List<Long> getResponseFileGallaryidForANews(List<Long> fileGallaryIds);
    
    public List<Long> getFileGalleryIdByResponseGalleryId(Long fileGalleryId);
+   
+   public List<Long> getResponseNewsCount(List<Long> fileGalleryIdsList);
+   
+   public List<Object[]> getResponseNewsCountForAParty(Date fromDate,Date toDate,Long partyId,List<Long> categoryIdsList,List<Long> galleryIdsList,List<Long> locationIdsList,Long locationScopeId,String tempVar);
 	
 }
