37ac513bd5ccec4b2f2655acca7686708b5e124c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-12 10:44:24 +0000

    updated by anil

--- newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidateRelatedNewsDAOHibernateTest.java
+++ newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidateRelatedNewsDAOHibernateTest.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.BaseDaoTestCase;
@@ -16,12 +17,19 @@ public class CandidateRelatedNewsDAOHibernateTest extends BaseDaoTestCase{
 	}
 	
 	public void testGetAllfileGallariesOfCandidate(){
-		List<Object[]> items = candidateRelatedNewsDAO.getCandidatesContainsNews();
-		
+		//List<Object[]> items = candidateRelatedNewsDAO.getCandidatesContainsNews();
+	// List<?> items =  candidateRelatedNewsDAO.getNotResponseCountBasedTotalNewsCount(null, null,872l, null, null, null, null, null, null, null, null);//.getNotResponseCountBasedTotalNewsCount(null,null,872,null,null,null,null,null,null,null,null);
+		List<Object[]> items = candidateRelatedNewsDAO.getNotResponseCountPerfect(null,null,872l,null,null,null,null,"all",null,null,null);
 		System.out.println(items.size());
-		for (Object[] objects : items) {
-			System.out.println(objects[2]);
-		}
+		long count = 0;
+	/*	for (Object[] objects : items) {
+			System.out.println(objects[0]+"--"+objects[1]+"--"+objects[2]+"--"+objects[3]);
+			count+=((Long)objects[0]);
+			
+		}*/
+		System.out.println(count);
+		
+		
 		
 	}
 }
