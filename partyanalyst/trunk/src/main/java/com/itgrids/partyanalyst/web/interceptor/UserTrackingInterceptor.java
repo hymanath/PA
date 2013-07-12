da668d1332d51c9d5a25a28cbf7e88cdb0887d09
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-12 05:42:27 +0000

    updated by Kamalakar Dandu

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/interceptor/UserTrackingInterceptor.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/interceptor/UserTrackingInterceptor.java
@@ -8,43 +8,45 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
-import org.apache.struts2.StrutsStatics;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 
+import com.itgrids.partyanalyst.dao.IAccessRestrictedSessionDAO;
+import com.itgrids.partyanalyst.dao.IUserLoginDetailsDAO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.UserTrackingVO;
+import com.itgrids.partyanalyst.service.IUserTrackingService;
+import com.itgrids.partyanalyst.util.IWebConstants;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 import com.opensymphony.xwork2.interceptor.Interceptor;
-import com.itgrids.partyanalyst.dao.IUserLoginDetailsDAO;
-import com.itgrids.partyanalyst.dao.IUserLoginSessionDAO;
-import com.itgrids.partyanalyst.dto.RegistrationVO;
-import com.itgrids.partyanalyst.dto.UserTrackingVO;
-
-import com.itgrids.partyanalyst.util.IWebConstants;
-import com.itgrids.partyanalyst.utils.IConstants;
-
-import com.itgrids.partyanalyst.service.IUserTrackingService;
 
 
 public class UserTrackingInterceptor extends AbstractInterceptor implements ServletRequestAware,ServletResponseAware,Interceptor  {
 	private IUserTrackingService userTrackingService;
 	private HttpServletResponse response;
-	private IUserLoginSessionDAO userLoginSessionDAO;
 	private IUserLoginDetailsDAO userLoginDetailsDAO;
-	
-    private List<String> userRoles;
-	 
+	private IAccessRestrictedSessionDAO accessRestrictedSessionDAO;
+	private List<String> userRoles;
 	private HttpServletRequest request;
 	private static final long serialVersionUID = 1L;
 	
- 
+	public IAccessRestrictedSessionDAO getAccessRestrictedSessionDAO() {
+		return accessRestrictedSessionDAO;
+	}
+
+	public void setAccessRestrictedSessionDAO(
+			IAccessRestrictedSessionDAO accessRestrictedSessionDAO) {
+		this.accessRestrictedSessionDAO = accessRestrictedSessionDAO;
+	}
 	
 	public HttpServletResponse getResponse() {
 		return response;
 	}
 
 	public void setServletRequest(HttpServletRequest request) {
-		this.request=request;
+		this.request = request;
 	}
 
 	public List<String> getUserRoles() {
@@ -71,64 +73,38 @@ public class UserTrackingInterceptor extends AbstractInterceptor implements Serv
 		this.userLoginDetailsDAO = userLoginDetailsDAO;
 	}
 
-	public IUserLoginSessionDAO getUserLoginSessionDAO() {
-		return userLoginSessionDAO;
-	}
-
-	public void setUserLoginSessionDAO(IUserLoginSessionDAO userLoginSessionDAO) {
-		this.userLoginSessionDAO = userLoginSessionDAO;
-	}
-
-	
 	public String intercept(ActionInvocation invocation) throws Exception 
 	{
 		try{
-		HttpServletRequest servletRequest = ServletActionContext.getRequest();
-		HttpSession session = servletRequest.getSession();
+		request = ServletActionContext.getRequest();
+		HttpSession session = request.getSession();
 		RegistrationVO registrationVO = (RegistrationVO)session.getAttribute(IWebConstants.USER);
-		if(registrationVO != null){
-		if(registrationVO.getRegistrationID() != null)
+		
+		if(registrationVO != null && registrationVO.getRegistrationID() != null)
         {
 			userRoles = registrationVO.getUserRoles();
 			
-			if(userRoles.contains(IConstants.PARTY_ANALYST_USER) && registrationVO.isMultipleAccessRestriction() == true)
+			if(userRoles.contains(IConstants.PARTY_ANALYST_USER) && registrationVO.isMultipleAccessRestriction())
 			{
-				
-				List<String> sessionIds = userLoginSessionDAO.getSessionIds(new Long(registrationVO.getRegistrationID()),session.getId());
-	    		if(sessionIds != null && sessionIds.size() > 0)
+				String sessionId = accessRestrictedSessionDAO.verifySessionIdAvailability(session.getId());
+	    		if(sessionId != null)
 	    		{
-	    			
-	    			userLoginSessionDAO.deleteSessionsFromUserLoginSession(session.getId());
+	    			accessRestrictedSessionDAO.deleteSessionsFromAccessRestrictedSession(session.getId());
 	    			session.invalidate();
-	    			HttpServletResponse response = (HttpServletResponse)invocation.getInvocationContext().get(StrutsStatics.HTTP_RESPONSE);
 	    			return "multipleAccess";
-	    			
 	    		}
 			}
-
-        }
 		}
 		if(invocation.getProxy().getMethod().equalsIgnoreCase("execute"))
 		{	
-			UserTrackingVO userTrackingVO;
-		
-			userTrackingVO = new UserTrackingVO();
-			
-			userTrackingVO.setRemoteAddress(servletRequest.getRemoteAddr());
-            userTrackingVO.setRequestURI(servletRequest.getRequestURI().substring(1).concat(servletRequest.getQueryString() != null ? "?"+servletRequest.getQueryString():""));
+			UserTrackingVO userTrackingVO = new UserTrackingVO();
+			userTrackingVO.setRemoteAddress(request.getRemoteAddr());
+            userTrackingVO.setRequestURI(request.getRequestURI().substring(1).concat(request.getQueryString() != null ? "?"+request.getQueryString():""));
             userTrackingVO.setSessionId(session.getId());
             
-            if(session != null)
-            {
-            	//RegistrationVO registrationVO = (RegistrationVO)session.getAttribute(IWebConstants.USER);
-            	
-            	if(registrationVO != null)
-            	{
-            		
-            		userTrackingVO.setUserId(registrationVO.getRegistrationID());
-            		
-            	}
-            }
+        	if(registrationVO != null)
+        		userTrackingVO.setUserId(registrationVO.getRegistrationID());
+        	
             userTrackingService.saveUserTrackingDetails(userTrackingVO);
 		}				
 		return invocation.invoke();
@@ -138,14 +114,9 @@ public class UserTrackingInterceptor extends AbstractInterceptor implements Serv
 	}
 
 
-	public void destroy() {
-		
-		
-	}
+	public void destroy() {}
 
-	public void init() {
-	
-	}
+	public void init() {}
 
 	public void setServletResponse(HttpServletResponse response) {
 		this.response = response;
