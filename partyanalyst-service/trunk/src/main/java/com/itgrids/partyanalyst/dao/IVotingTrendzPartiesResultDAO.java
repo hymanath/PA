63ce0f9783f06189c60baaa2972a5c369a16794e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-17 10:50:32 +0000

    updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVotingTrendzPartiesResultDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.VotingTrendzPartiesResult;
+
+public interface IVotingTrendzPartiesResultDAO extends GenericDao<VotingTrendzPartiesResult,Long>{
+
+}
