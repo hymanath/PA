f28cf1c0679acc604d42fa6f5ad05cafa711fa79
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-17 10:50:32 +0000

    updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterBasicInfoDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.VoterBasicInfo;
+
+public interface IVoterBasicInfoDAO extends GenericDao<VoterBasicInfo,Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVotingTrendzDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.VotingTrendz;
+
+public interface IVotingTrendzDAO extends GenericDao<VotingTrendz,Long>{
+
+}
