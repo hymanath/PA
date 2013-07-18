c06b2179e885a15657e26b4080360a56c71c5e9a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-18 14:30:11 +0000

    updated by nagamani

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VotersAnalysisAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/VotersAnalysisAction.java
@@ -626,7 +626,7 @@ public class VotersAnalysisAction extends ActionSupport implements ServletReques
 		}
 		else if(jObj.getString("task").equalsIgnoreCase("getPublicationDate") || jObj.getString("task").equalsIgnoreCase("getPublicationDateForCast")
 				||jObj.getString("task").equalsIgnoreCase("getPublicationDateForParty")
-				||jObj.getString("task").equalsIgnoreCase("getPublicationDateForModification"))
+				||jObj.getString("task").equalsIgnoreCase("getPublicationDateForModification") || jObj.getString("task").equalsIgnoreCase("getPublicationDateForVotingTrendz"))
 		{
 			Long selectedId = jObj.getLong("selected");
 			namesList = votersAnalysisService.publicationDetailsBasedOnConstituency(selectedId);
