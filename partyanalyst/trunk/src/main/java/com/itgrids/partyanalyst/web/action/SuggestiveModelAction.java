ff570d8dbefc888df1a8806242207f00772ad456
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-25 09:01:45 +0000

    Updated by AnuRadha -

--- partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SuggestiveModelAction.java
+++ partyanalyst/trunk/src/main/java/com/itgrids/partyanalyst/web/action/SuggestiveModelAction.java
@@ -1,21 +1,99 @@
 package com.itgrids.partyanalyst.web.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.jfree.util.Log;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.OptionVO;
+import com.itgrids.partyanalyst.service.ISuggestiveModelService;
 import com.opensymphony.xwork2.Action;
 
 public class SuggestiveModelAction  implements ServletRequestAware {
+	
 	private HttpServletRequest request;
+	private ISuggestiveModelService suggestiveModelService;
+	private HttpSession session;
+	private String task;
+	JSONObject jObj;
+	private OptionVO optionVO;
+	
+	private static final Logger log = Logger.getLogger(SuggestiveModelAction.class);
 	
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 		
 	}
 	
+	public ISuggestiveModelService getSuggestiveModelService() {
+		return suggestiveModelService;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public void setSuggestiveModelService(
+			ISuggestiveModelService suggestiveModelService) {
+		this.suggestiveModelService = suggestiveModelService;
+	}
+
+	public HttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public JSONObject getjObj() {
+		return jObj;
+	}
+
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+	public OptionVO getOptionVO() {
+		return optionVO;
+	}
+
+	public void setOptionVO(OptionVO optionVO) {
+		this.optionVO = optionVO;
+	}
+
 	public String execute(){
 		
 		return Action.SUCCESS;
 	}
+	
+	public String getPartyPerformanceForSelectedLocation()
+	{
+		try{
+		session = request.getSession();
+		jObj = new JSONObject(getTask());
+		
+		optionVO = suggestiveModelService.getPartyPerformanceForSelectedLocation(jObj.getLong("constituencyId"),jObj.getLong("electionId"),jObj.getLong("partyId"),jObj.getLong("locationId"),jObj.getString("locationType"));	
+			
+		}catch (Exception e) {
+		 e.printStackTrace();
+		 Log.error(" Exception Occured in getPartyPerformanceForSelectedLocation() method, Exception - "+e);
+		}
+		return Action.SUCCESS;
+	}
+	
 }
