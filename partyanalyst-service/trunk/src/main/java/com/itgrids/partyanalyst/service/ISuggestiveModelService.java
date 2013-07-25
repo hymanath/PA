3325b5ecaf57ebf869fbfd72b26537d25c28571d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-25 08:56:10 +0000

    Updated by AnuRadha -

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
@@ -1,5 +1,10 @@
 package com.itgrids.partyanalyst.service;
 
+
+import com.itgrids.partyanalyst.dto.OptionVO;
+
 public interface ISuggestiveModelService {
+	
+	public OptionVO getPartyPerformanceForSelectedLocation(Long constituencyId,Long electionId,Long partyId,Long locationId,String type);
 
 }
