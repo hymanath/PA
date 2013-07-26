c7ffe74de6e58c460c0481f4d891d396ce9aeddc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-26 14:47:24 +0000

    Updated by AnuRadha -

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/SuggestiveRange.java
@@ -0,0 +1,87 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "suggestive_range")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class SuggestiveRange extends BaseModel implements Serializable{
+
+	private static final long serialVersionUID = -1153751405063419593L;
+    private Long suggestiveRangeId;
+    private String name;
+    private String range;
+    private Double minValue;
+    private Double maxValue;
+    
+    public SuggestiveRange(){}
+    
+    public SuggestiveRange(String name,String range,Double minValue,Double maxValue){
+    
+    	this.name = name;
+    	this.range = range;
+    	this.minValue = minValue;
+    	this.maxValue = maxValue;
+   
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "suggestive_range_id",unique = true, nullable = false)
+	public Long getSuggestiveRangeId() {
+		return suggestiveRangeId;
+	}
+
+	public void setSuggestiveRangeId(Long suggestiveRangeId) {
+		this.suggestiveRangeId = suggestiveRangeId;
+	}
+    
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+	@Column(name = "range")
+	public String getRange() {
+		return range;
+	}
+
+	public void setRange(String range) {
+		this.range = range;
+	}
+  
+	@Column(name = "min_value")
+	public Double getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(Double minValue) {
+		this.minValue = minValue;
+	}
+    
+	@Column(name = "max_value")
+	public Double getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(Double maxValue) {
+		this.maxValue = maxValue;
+	}
+    
+    
+	
+}
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
@@ -3,16 +3,17 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
-import com.itgrids.partyanalyst.dto.OptionVO;
+import com.itgrids.partyanalyst.dto.PartyPositionVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface ISuggestiveModelService {
 	
-	public OptionVO getPartyPerformanceForSelectedLocation(Long constituencyId,Long electionId,Long partyId,Long locationId,String type);
-
 	public List<SelectOptionVO> getConstituenciesForUserAccessByStateId(Long electionScope,Long stateId,Long userId);
 	
 	public List<SelectOptionVO> getPartyDetailsByMandal(Long mandalId);
 	
 	public List<SelectOptionVO> getElectionIdsAndYearsBytehsilId(List<Long> electionScope,Long partyId,Long tehsilId);
+	
+	public List<PartyPositionVO> getPartyPerformenceReport(Long constituencyId,Long partyId,Long locationId,String locationType,Long electionId,String tempVar);
+
 }
