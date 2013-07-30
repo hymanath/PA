42e85c65bdc50b59cba6ceb01026bcec6d08b1de
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-07-30 05:38:38 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISuggestiveModelService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.itgrids.partyanalyst.dto.PartyPositionVO;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.excel.booth.VoterVO;
 
 public interface ISuggestiveModelService {
 	
@@ -15,5 +16,7 @@ public interface ISuggestiveModelService {
 	public List<SelectOptionVO> getElectionIdsAndYearsBytehsilId(List<Long> electionScope,Long partyId,Long tehsilId);
 	
 	public List<PartyPositionVO> getPartyPerformenceReport(Long constituencyId,Long partyId,Long locationId,String locationType,Long electionId,String tempVar);
+	
+	 public List<VoterVO> getDeletedVoterInfo(List<Long> panchayatIds);
 
 }
