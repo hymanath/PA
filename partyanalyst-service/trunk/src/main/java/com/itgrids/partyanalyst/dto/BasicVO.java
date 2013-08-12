27311258196887ee0d414cba8922b70ece76148f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-12 10:58:29 +0000

    Updated by Sravanthi,for Suggestive model

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BasicVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BasicVO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class BasicVO implements Serializable{
@@ -8,7 +11,92 @@ public class BasicVO implements Serializable{
 	private Long id;
 	private Long count;
 	private String name;
+	private String mandalName;
+	private String casteName;
 	private Double perc;
+	private List<BasicVO> selectedCasteDetails = new ArrayList<BasicVO>();
+	private List<BasicVO> hamletVoterInfo = new ArrayList<BasicVO>();
+	private List<BasicVO> hamletCasteInfo = new ArrayList<BasicVO>();
+	private List<BasicVO> panchayatVoterInfo = new ArrayList<BasicVO>();
+	private List<VoterCastInfoVO> casteList = new ArrayList<VoterCastInfoVO>();
+	private Long hamletId;
+	private String hamletName;
+	private Long voterCount;
+	private List casteNames;
+	private String persent;
+	
+	public List<BasicVO> getSelectedCasteDetails() {
+		return selectedCasteDetails;
+	}
+	public void setSelectedCasteDetails(List<BasicVO> selectedCasteDetails) {
+		this.selectedCasteDetails = selectedCasteDetails;
+	}
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	public String getPersent() {
+		return persent;
+	}
+	public void setPersent(String persent) {
+		this.persent = persent;
+	}
+	public String getCasteName() {
+		return casteName;
+	}
+	public void setCasteName(String casteName) {
+		this.casteName = casteName;
+	}
+	public List<BasicVO> getPanchayatVoterInfo() {
+		return panchayatVoterInfo;
+	}
+	public void setPanchayatVoterInfo(List<BasicVO> panchayatVoterInfo) {
+		this.panchayatVoterInfo = panchayatVoterInfo;
+	}
+	public List getCasteNames() {
+		return casteNames;
+	}
+	public void setCasteNames(List casteNames) {
+		this.casteNames = casteNames;
+	}
+	public List<VoterCastInfoVO> getCasteList() {
+		return casteList;
+	}
+	public void setCasteList(List<VoterCastInfoVO> casteList) {
+		this.casteList = casteList;
+	}
+	public List<BasicVO> getHamletVoterInfo() {
+		return hamletVoterInfo;
+	}
+	public void setHamletVoterInfo(List<BasicVO> hamletVoterInfo) {
+		this.hamletVoterInfo = hamletVoterInfo;
+	}
+	public List<BasicVO> getHamletCasteInfo() {
+		return hamletCasteInfo;
+	}
+	public void setHamletCasteInfo(List<BasicVO> hamletCasteInfo) {
+		this.hamletCasteInfo = hamletCasteInfo;
+	}
+	public Long getVoterCount() {
+		return voterCount;
+	}
+	public void setVoterCount(Long voterCount) {
+		this.voterCount = voterCount;
+	}
+	public Long getHamletId() {
+		return hamletId;
+	}
+	public void setHamletId(Long hamletId) {
+		this.hamletId = hamletId;
+	}
+	public String getHamletName() {
+		return hamletName;
+	}
+	public void setHamletName(String hamletName) {
+		this.hamletName = hamletName;
+	}
 	public Long getId() {
 		return id;
 	}
