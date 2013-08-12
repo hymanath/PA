10c2fabc0f36d76f5122ba0591193aa983a699a3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-12 05:59:42 +0000

    Updated by Sasi ..Update and Delete Category

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IUserNewsCategoryDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IUserNewsCategoryDAO.java
@@ -1,10 +1,20 @@
 package com.itgrids.partyanalyst.dao;
 
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.UserNewsCategory;
 
 public interface IUserNewsCategoryDAO extends GenericDao<UserNewsCategory, Long>{
+	
+	public List<Object[]> allCategoriesOfUser(Long userId);
+	
+	public Integer updateCategory(Long userId,Long categoryId,String categoryName,String visibility);
+	
+	public Integer updateCategoryName(Long userId,Long categoryId,String categoryName);
+	
+	public Integer updateCategoryStatus(Long userId,Long categoryId,String status);
 	
 }
