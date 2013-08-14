f19ab9ac9ed315a96d5e577a6ac1361a454f379d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-14 09:20:35 +0000

    Updated by AnuRadha -

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyHierarchyInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyHierarchyInfoDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ConstituencyHierarchyInfo;
@@ -7,5 +9,8 @@ import com.itgrids.partyanalyst.model.ConstituencyHierarchyInfo;
 public interface IConstituencyHierarchyInfoDAO extends GenericDao<ConstituencyHierarchyInfo, Long>{
 	
 	public Integer deleteConstituencyBasicInfo(Long constituencyId,Long publicationId,Long userId);
+	
+	public List<ConstituencyHierarchyInfo> getConstituencyHierarchyInfoList(Long constituencyId,Long userId);
+	
 
 }
