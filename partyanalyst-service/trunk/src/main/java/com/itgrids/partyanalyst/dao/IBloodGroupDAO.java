83b86a766b2b9b1897d4812c5e41cf6333aea3ce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-14 09:18:57 +0000

    Updated by AnuRadha -

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBloodGroupDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBloodGroupDAO.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.BloodGroup;
 
 public interface IBloodGroupDAO extends GenericDao<BloodGroup,Long>{
+	
+	public List<BloodGroup> getBloodGroupList();
 
 }
