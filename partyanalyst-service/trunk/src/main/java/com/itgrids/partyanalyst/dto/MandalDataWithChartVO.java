f905a01161c2f47d48774dc5a7db3e5adc996b69
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-19 11:08:10 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MandalDataWithChartVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MandalDataWithChartVO.java
@@ -1,12 +1,13 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MandalDataWithChartVO {
 
 	String chart;
 	List<MandalAllElectionDetailsVO> mandalAllElectionDetailsVO;
-	
+	private List<MandalAllElectionDetailsVO> mandalAllElectionDetailsVO1 = new ArrayList<MandalAllElectionDetailsVO>(0);
 	public MandalDataWithChartVO(){
 		
 	}
@@ -15,6 +16,17 @@ public class MandalDataWithChartVO {
 			List<MandalAllElectionDetailsVO> mandalAllElectionDetailsVO) {
 		this.chart = chart;
 		this.mandalAllElectionDetailsVO = mandalAllElectionDetailsVO;
+	}
+
+	
+
+	public List<MandalAllElectionDetailsVO> getMandalAllElectionDetailsVO1() {
+		return mandalAllElectionDetailsVO1;
+	}
+
+	public void setMandalAllElectionDetailsVO1(
+			List<MandalAllElectionDetailsVO> mandalAllElectionDetailsVO1) {
+		this.mandalAllElectionDetailsVO1 = mandalAllElectionDetailsVO1;
 	}
 
 	public String getChart() {
