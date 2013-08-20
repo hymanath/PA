4ef7327968548c1419bcf522b3e9c4add0bae018
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-20 15:50:49 +0000

    updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothPublicationVoterDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothPublicationVoterDAO.java
@@ -427,4 +427,8 @@ public interface IBoothPublicationVoterDAO extends
 	  public List<Object[]> getVoterDetailsForMessageCenter(Long constituencyId,Long publicationDateId,Long locationValue,String queryStr,Integer startIndex, Integer maxIndex,Long userId);
 	  
 	  
+	  
+	  public List<Object[]> getRecordsFromBoothPublicationVoter(Long constituencyId, Long publicationDateId);
+	  
+	  public List<Object[]> getVoterDetailsOfAConstituency(Long constituencyId, Long publicationDateId, Long userId);
 }
