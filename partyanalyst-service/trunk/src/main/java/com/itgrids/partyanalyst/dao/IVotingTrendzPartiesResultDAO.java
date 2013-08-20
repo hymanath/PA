d766459e60641c4c9b78b7bec70222572496c1e9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-20 15:53:29 +0000

    updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVotingTrendzPartiesResultDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVotingTrendzPartiesResultDAO.java
@@ -6,8 +6,12 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.VotingTrendzPartiesResult;
 
 public interface IVotingTrendzPartiesResultDAO extends GenericDao<VotingTrendzPartiesResult,Long>{
+	
 	public List<Long> getVotingTrendzIds(Long constituenyId);
+	
 	public Integer deletePartyResultByConstituencyId(List<Long> votingTrendzIds);
 	
-	public List<VotingTrendzPartiesResult> getVotingTrendzPartiesResultList();
+	public List<VotingTrendzPartiesResult> getVotingTrendzPartiesResultList(Long constituencyId);
+	
+	public List<Object[]> getpartiesListForVotingTrendz(Long constituencyId);
 }
