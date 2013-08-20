b39ca0eb65903292c4e8700a2334983cb57af653
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-08-20 16:00:57 +0000

    updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VotingTrendzPartiesResultDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VotingTrendzPartiesResultDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IVotingTrendzPartiesResultDAO;
@@ -18,8 +20,16 @@ public class VotingTrendzPartiesResultDAOHibernateTest extends BaseDaoTestCase{
 		votingTrendzPartiesResultDAO.getAll();
 	}*/
 	
-	public void testgetVotingTrendzPartiesResultList()
+	/*public void testgetVotingTrendzPartiesResultList()
 	{
-		votingTrendzPartiesResultDAO.getVotingTrendzPartiesResultList();
+		votingTrendzPartiesResultDAO.getVotingTrendzPartiesResultList(232l);
+	}*/
+	
+	public void testGetpartiesListForVotingTrendz()
+	{
+		List<Object[]> list = votingTrendzPartiesResultDAO.getpartiesListForVotingTrendz(221l);
+		System.out.println(list.size());
+		for(Object[] params : list)
+			System.out.println(params[0]+" -- "+params[1]);
 	}
 }
