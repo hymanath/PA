ad5d64ef6678a0a4571dbfdff268eec6a526501a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-09-06 09:28:24 +0000

    Updated by Kamalakar Dandu

--- partyanalyst/trunk/others/IWebConstants.java
+++ partyanalyst/trunk/others/IWebConstants.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.util;
 
 public interface IWebConstants {
 
-	public static final String CHART_URL_IN_SERVER = "/static_content/charts/";
-	public static final String STATIC_CONTENT_FOLDER_URL = "/static_content/";
+	public static final String CHART_URL_IN_SERVER = "/app/static_content/charts/";
+	public static final String STATIC_CONTENT_FOLDER_URL = "/app/static_content/";
+	public static final String VOICE_RECORDINGS_FOLDER_URL = "/app/static_content/voice_recordings/";
 }
