3f37dbd3f32f12d3579c2bf5c0bdd6a509c812c1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-09-18 05:27:55 +0000

    News Display Server Side Pagination - samba

--- partyanalyst/trunk/src/main/webapp/js/newsDisplay/newsDisplay.js
+++ partyanalyst/trunk/src/main/webapp/js/newsDisplay/newsDisplay.js
@@ -1,4 +1,10 @@
 
+var previousSelectedColumn = "categoryType";
+var selectedColumn = "categoryType";
+var previousDirection = "asc";
+var selectedDirection = "asc";
+var modifiedInd = false;
+var totalRecords = 1000;
 function getFlagStatus(contentId){
 
 	
@@ -516,4 +522,222 @@ function getLocations1(id){
    document.getElementById("showScopeSubs").innerHTML = str;
    getStatesForLocationScope();
   }
+}
+function buildVotersByLocCustomWardDataTable1(task,queryType,fileType,sourceId,languegeId,categoryId,newsImportanceId,locationScope,location,title,fromDate,toDate)
+{
+	
+	$("#searchAjaxImgSpan").css('display','none');
+	YAHOO.widget.DataTable.news = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var user = oData;
+	var title = oRecord.getData("fileTitle1");
+	var fileId = oRecord.getData("fileId");
+
+
+	//BY SAMBA START
+	var gallaryId = oRecord.getData("contentId");
+	//elLiner.innerHTML ="<a href='javascript:{}' onclick='showNews(\""+fileId+"\")'>"+title+"</a>";
+	elLiner.innerHTML ="<a href='javascript:{}' onclick='getVideoDetails(\""+gallaryId+"\")'>"+title+"</a>";
+
+	//elLiner.innerHTML ="<a href='javascript:{}' onclick='showNews(\""+fileId+"\")'>"+title+"</a>";
+
+		//BY SAMBA END
+
+  };
+ YAHOO.widget.DataTable.edit = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var user = oData;
+	var fileId= oRecord.getData("fileId");
+
+
+	var str='';
+	//str+="<a style='float:left;' title='edit this news' href='javascript:{}' onclick='editNewsDetails("+fileId+")'><img style='text-decoration: none; border: 0px none;' src='images/icons/edit.png'></a>";
+
+	str+="<a style='float:left;' title='edit this news' href='javascript:{}' onclick='editNewsDetails("+fileId+")'><i class='icon-pencil'></i></a>";
+
+	//str+="<a style='float:right;' title='delete this news'  href='javascript:{}' onclick='updateDeleteNews(\"Delete\","+fileId+");'><img style='text-decoration: none; border: 0px none;' src='images/icons/delete.png'></a>";
+
+   str+="<a style='float:right;' title='delete this news'  href='javascript:{}' onclick='updateDeleteNews(\"Delete\","+fileId+");'><i class='icon-remove-sign'></i></a>";
+
+
+	elLiner.innerHTML =str;
+		
+  };
+  /*YAHOO.widget.DataTable.delet = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var fileId= oRecord.getData("fileId");
+	elLiner.innerHTML ="<a href='javascript:{}' onclick='updateDeleteNews(\"Delete\","+fileId+");'><img style='text-decoration: none; border: 0px none;' src='images/icons/delete.png'></a>";
+		
+  };*/
+
+   YAHOO.widget.DataTable.visibilty = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var visibility= oRecord.getData("visibility");
+	var contentId= oRecord.getData("contentId");
+    var str='';
+	if(visibility == "false"){
+	  str+="<div id='visibilityDiv"+contentId+"'><a href='javaScript:{updateVisibilityToPrivate("+contentId+")}' title='make this news as private'><i class='icon-bullhorn'></i></a><input type='hidden' name='visibility' id=visibility"+contentId+"/ value='public'></input></div>";
+
+	  str+="<div id='nonVisibilityDiv"+contentId+"' style='display:none;'><a href='javaScript:{updateVisibilityToPublic("+contentId+")}' title='make this news as public'><i class='icon-lock'></i></a><input type='hidden' name='visibility' id=visibility"+contentId+" value='private'></input></div>";
+	}
+	   
+	//elLiner.innerHTML ="";
+	else if(visibility == "true"){
+
+		 str+="<div id='visibilityDiv"+contentId+"'  style='display:none;'><a href='javaScript:{updateVisibilityToPrivate("+contentId+")}' title='make this news as private'><i class='icon-bullhorn'></i></a><input type='hidden' name='visibility' id=visibility"+contentId+"/ value='public'></input></div>";
+
+	     str+="<div id='nonVisibilityDiv"+contentId+"'><a href='javaScript:{updateVisibilityToPublic("+contentId+")}' title='make this news as public'><i class='icon-lock'></i></a><input type='hidden' name='visibility' id=visibility"+contentId+" value='private'></input></div>";
+	
+	}
+
+	elLiner.innerHTML = str;
+	  
+
+		
+  };
+
+  YAHOO.widget.DataTable.flagSet = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var flagSet= oRecord.getData("flagSet");
+	var contentId= oRecord.getData("contentId");
+    var str='';
+
+	if(flagSet == "true"){
+
+		str+="<div id='flagSetDiv"+contentId+"'><a href='javaScript:{removeFlagInTable("+contentId+")}' title='unflag this news'><i class='icon-flag'></i></a><input type='hidden' id='flag"+contentId+"' value='flagged'></input></div>";
+
+		str+="<div id='unFlagSetDiv"+contentId+"' style='display:none;'><a href='javaScript:{saveFlagInTable("+contentId+")}' title='flag this news' class='unflagClass'><i class='icon-flag'></i></a><input type='hidden' id='flag"+contentId+"' value='flagged'></input></div>";
+	
+	
+	}
+	  
+	// elLiner.innerHTML ="";
+	else if(flagSet == "false"){
+
+		str+="<div id='flagSetDiv"+contentId+"'  style='display:none;'><a href='javaScript:{removeFlagInTable("+contentId+")}' title='unflag this news'><i class='icon-flag'></i></a><input type='hidden' id='flag"+contentId+"' value='flagged'></input></div>";
+
+		str+="<div id='unFlagSetDiv"+contentId+"'><a href='javaScript:{saveFlagInTable("+contentId+")}' title='flag this news' class='unflagClass'><i class='icon-flag'></i></a><input type='hidden' id='flag"+contentId+"' value='flagged'></input></div>";
+	
+	
+	}
+
+	 elLiner.innerHTML = str;
+		
+  };
+
+  YAHOO.widget.DataTable.notes = function(elLiner, oRecord, oColumn, oData) 
+  {
+	var user = oData;
+	var notesExist= oRecord.getData("notesExist");
+
+	if(notesExist == "true")
+	  elLiner.innerHTML ="<div style='text-align:center;'><a href='javaScript:{}' ><i class='icon-edit'></i></a></div>";
+	else
+	  elLiner.innerHTML ="<div style='text-align:center;'><a href='javaScript:{}' class='unflagClass'><i class='icon-edit'></i></a></div>";
+
+		
+  };
+	
+var str='';
+var votersByLocBoothColumnDefs = [
+{key:"categoryType", label: '<span onClick="updateColumn(\'categoryType\')">NEWS CATEGORY<span>',sortable: true, resizeable:true},
+{key:"gallaryName", label: '<span onClick="updateColumn(\'gallaryName\')">GALLERY<span>',sortable: true, resizeable:true},
+{key:"source", label: '<span onClick="updateColumn(\'source\')">SOURCE<span>',sortable: false, resizeable:true},
+{key:"fileTitle1", label: '<span onClick="updateColumn(\'fileTitle1\')">TITLE<span>',formatter:YAHOO.widget.DataTable.news, sortable: true, resizeable:true},
+{key:"description", label:'<span onClick="updateColumn(\'description\')">DESCRIPTIONS<span>',sortable: true, resizeable:true},
+{key:"locationScopeValue", label:'<span onClick="updateColumn(\'locationScopeValue\')">IMPACT AREA<span>',sortable: true, resizeable:true},
+{key:"locationValue", label:'<span onClick="updateColumn(\'locationValue\')">AREA NAME<span>',sortable: false, resizeable:true},
+{key:"fileDate", label: '<span onClick="updateColumn(\'fileDate\')">NEWS DATE<span>',sortable: true, resizeable:true},
+
+{key:"visibility", label: "<i class=' icon-eye-open'></i>",formatter:YAHOO.widget.DataTable.visibilty, sortable: false},
+{key:"flagSet", label: "<i class='icon-flag'></i>", sortable: false,formatter:YAHOO.widget.DataTable.flagSet},
+{key:"notesExist", label: "<i class='icon-edit'></i>",formatter:YAHOO.widget.DataTable.notes, sortable: false},
+{key:"update", label: "",formatter:YAHOO.widget.DataTable.edit}
+/*{key:"", label: "a", width:60,formatter:YAHOO.widget.DataTable.Type},
+
+{key:"",label:"b",sortable:true,width:50},
+{key:"", label: "c", formatter:YAHOO.widget.DataTable.ActionLink}
+{key:"",label:"",sortable:true,width:50},*/
+];
+
+var votersByLocBoothDataSource = new YAHOO.util.DataSource("getNewsToDisplayAction1.action?queryType="+queryType+"&fileType="+fileType+"&sourceId="+sourceId+"&languegeId="+languegeId+"&categoryId="+categoryId+"&newsImportanceId="+newsImportanceId+"&locationScope="+locationScope+"&location="+location+"&fromDate="+fromDate+"&toDate="+toDate+"&title"+title+"&timeST="+timeST+"&task="+task+"&");
+votersByLocBoothDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
+votersByLocBoothDataSource.responseSchema = {
+resultsList: "fileVOList",
+fields: ["categoryType", "gallaryName", "source", "fileTitle1","description","locationScopeValue","locationValue","fileDate","contentId","flagSet","visibility","notesExist","fileId","fileDateAsString","categoryId","locationScope","newsImportanceId","keywords","fileVOList","location","locationVal","fileGallaryId"],
+
+metaFields: {
+totalRecords: "fileVOList[0].count" // Access to value in the server response
+},
+};
+
+
+
+	var myConfigs = {
+	initialRequest: "sort=categoryType&dir=asc&startIndex=0&results=100", // Initial request for first page of data
+
+	initialRequest: "sort="+selectedColumn+"&dir="+selectedDirection+"&startIndex=0&results=100", // Initial request for first page of data
+	dynamicData: true, // Enables dynamic server-driven data
+	   paginator : new YAHOO.widget.Paginator({ 
+					rowsPerPage    : 100 
+					})  // Enables pagination
+	};
+    if(modifiedInd){
+
+		 var stindx = 0;
+		 try{
+		  stindx =  (parseInt($.trim($('.yui-pg-current-page').html()))-1)*100;
+		  myConfigs["paginator"] = new YAHOO.widget.Paginator({ 
+						rowsPerPage    : 100 ,
+						initialPage:$('.yui-pg-current-page').html(),
+						totalRecords:totalRecords
+						}) 
+		  }catch(e){}
+		 myConfigs["initialRequest"] ="sort="+selectedColumn+"&dir="+selectedDirection+"&startIndex="+stindx+"&results=100&initialPage ="+$('.yui-pg-current-page').html();
+		}
+
+
+var votersByLocBoothDataTable = new YAHOO.widget.DataTable("showNews",
+votersByLocBoothColumnDefs, votersByLocBoothDataSource, myConfigs);
+
+
+votersByLocBoothDataTable.handleDataReturnPayload = function(oRequest, oResponse, oPayload) {
+pResults = oResponse.resultsCount;
+oPayload.totalRecords = oResponse.meta.totalRecords;
+totalRecords = oResponse.meta.totalRecords;
+
+newsDetails = oResponse.results;
+
+   if ($('#searchBtn').length > 0)
+			$('#searchBtn').attr('disabled',false);
+
+return oPayload;
+}
+
+return {
+oDS: votersByLocBoothDataSource,
+oDT: votersByLocBoothDataTable
+};
+}
+
+function updateColumn(columnName)
+{	
+	selectedColumn = columnName;
+
+	if(previousSelectedColumn == selectedColumn)
+	{
+		if(previousDirection == "asc")
+		{
+			selectedDirection = "desc";
+		}else if(previousDirection == "desc")
+		{
+            selectedDirection = "asc";
+		}
+
+	}else
+	{
+        selectedDirection = "asc";
+	}
+
+	previousSelectedColumn = columnName;
 }
