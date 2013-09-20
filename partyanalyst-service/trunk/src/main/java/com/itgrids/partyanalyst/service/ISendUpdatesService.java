f208a0f695da20e79a3bd98012ded1a16d21be2c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-09-20 13:52:48 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISendUpdatesService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISendUpdatesService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
-import com.itgrids.partyanalyst.excel.booth.VoterVO;
 
 public interface ISendUpdatesService {
 	
@@ -13,7 +12,8 @@ public interface ISendUpdatesService {
 	
 	public List<RegistrationVO> sendSmsForAllUsersFromAdmin(String message);
 	public String sendEmailsFromAdminToUsers(List<String> userId,String message,String subject);
-	public List<RegistrationVO>  getUsersForSendingEmails(Long selectedState,Long selectedDistrict,Long selectedConstituency,Long userType,Long locationScope);
+	//public List<RegistrationVO>  getUsersForSendingEmails(Long selectedState,Long selectedDistrict,Long selectedConstituency,Long userType,Long locationScope,String freeUserType);
+	public List<RegistrationVO>  getUsersForSendingEmail(RegistrationVO regiVO,String order,String sortBy,int startIndex,int maxIndex);
 	public ResultStatus sendSMSToSelectedPeople(Long userId,String scope,Long scopeId,String content,String type);
 	public Long getLatestPublicationDateId();
 	public List<SelectOptionVO> getBoothsForWardId(Long wardId,Long publicationDateId);
