0da2342c99b4ca744dc62d489e4001e611780ce7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-10-04 11:33:42 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyInfoService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyInfoService.java
@@ -6,7 +6,7 @@ import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.dto.VotersDetailsVO;
 
 public interface IConstituencyInfoService {
-	public List<SelectOptionVO> getConstituencies();
+	
 	
 	public List<VotersDetailsVO> getConstituencyBasicInfoById(Long constituencyId,Long publicationId,Long userId);
 }
