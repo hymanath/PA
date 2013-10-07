ef60c44217e652a13ef3e79af2cbf9c001d3ce93
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-10-07 08:09:20 +0000

    updated by nagamani

--- partyanalyst/trunk/src/main/webapp/js/constituencyInfo.js
+++ partyanalyst/trunk/src/main/webapp/js/constituencyInfo.js
@@ -1,6 +1,6 @@
 function buildConstituencyBasicInfo(results)
 	{
- 	
+
         $("#basicInfoAjaxImage").css("display","none");
 	    var mandalList = results[0].mandalList;
 		var localbodyList = results[0].localbodiesList;
@@ -12,36 +12,40 @@ function buildConstituencyBasicInfo(results)
 		str+='<tr>';
 		str+='<th>Mandal</th>';
 		str+='<th>Panchayat</th>';
-		str+='<th>Booth</th>';
 		str+='<th>Hamlet</th>';
+		str+='<th>Booth</th>';
 		str+='</tr>';
 		for(var i in mandalList)
 		{
+		
 		for(var j=0;j<mandalList[i].selectOptionsList.length;j++)
 			{
-		 str+='<tr>';
+		  for(var k=0;k<mandalList[i].selectOptionsList[j].selectOptionsList1.length;k++)
+				{
+		str+='<tr>';		
 		 str+='<td>'+mandalList[i].name+'</td>';
 		 str+='<td>'+mandalList[i].selectOptionsList[j].name+'</td>';
-		  str+='<td>';
-		 for(var k=0;k<mandalList[i].selectOptionsList[j].selectOptionsList.length;k++)
-				{
-			 var temp =mandalList[i].selectOptionsList[j].selectOptionsList.length - 1; 
-		str+='<span>'+mandalList[i].selectOptionsList[j].selectOptionsList[k].name+'</span> ';
-		if(k!=temp)
-		str +=',  ';
-				}
-		str+='</td>';
 		 str+='<td>';
-		 for(var k1=0;k1<mandalList[i].selectOptionsList[j].selectOptionsList1.length;k1++)
+		// var temp =mandalList[i].selectOptionsList[j].selectOptionsList1.length - 1; 
+		str+='<span>'+mandalList[i].selectOptionsList[j].selectOptionsList1[k].name+'</span> ';
+	
+		str+='</td>';
+		var hamletsize = mandalList[i].selectOptionsList[j].selectOptionsList1.length;
+		if(k == 0)
+		{
+	    str+='<td rowspan="hamletsize">';
+		 for(var k1=0;k1<mandalList[i].selectOptionsList[j].selectOptionsList.length;k1++)
 				{
-		 var temp1 =mandalList[i].selectOptionsList[j].selectOptionsList1.length - 1; 
-		str+='<span>'+mandalList[i].selectOptionsList[j].selectOptionsList1[k1].name+'</span> ';
+		 var temp1 =mandalList[i].selectOptionsList[j].selectOptionsList.length - 1; 
+		str+='<span>'+mandalList[i].selectOptionsList[j].selectOptionsList[k1].name+'</span> ';
 		if(k1!=temp1)
 		str +=',  ';
 				}
 		str+='</td>';
-	   	str+='</tr>';
+		}
+			str+='</tr>';
 			}
+		}
 	
 	}
 		str+='</table>';
