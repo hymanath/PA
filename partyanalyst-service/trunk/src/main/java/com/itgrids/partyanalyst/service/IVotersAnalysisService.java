2edf8141a08871423f1734fc5c1c0d578fe4dd67
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-10-09 14:04:34 +0000

    Updated by AnuRadha (inserting locality data in intermediate tables)

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IVotersAnalysisService.java
@@ -125,7 +125,7 @@ public interface IVotersAnalysisService {
 	public List<VotersInfoForMandalVO> getPreviousVotersCountDetailsForAllLevels(
 			 Long constituencyId,Long mandalId, Long panchayatId, Long boothId , String type,Long userId);
 	
-	public ResultStatus insertVotersDataInIntermediateTables(Long reportLevelValue, Long publicationDateId,Long userId,boolean hamletChecked,boolean hamletBoothChecked);
+	public ResultStatus insertVotersDataInIntermediateTables(Long reportLevelValue, Long publicationDateId,Long userId,boolean hamletChecked,boolean hamletBoothChecked,boolean localityChecked);
 	
 	public List<VotersDetailsVO> getVoterAgeWiseDetails(Long constituencyId, Long mandalId,
 			 Long panchayatId , Long boothId, Long publicationDateId, String type,Long userId);
