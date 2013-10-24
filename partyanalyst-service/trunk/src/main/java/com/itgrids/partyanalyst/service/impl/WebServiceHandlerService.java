6e9346fb0127a00650ed2119996039cdfca43abf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-10-24 06:28:41 +0000

    Web Services implementation - samba

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/WebServiceHandlerService.java
@@ -0,0 +1,44 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ILoginService;
+import com.itgrids.partyanalyst.service.IWebServiceHandlerService;
+
+public class WebServiceHandlerService implements IWebServiceHandlerService {
+	
+	private static final Logger log = Logger.getLogger(WebServiceHandlerService.class);
+	
+	private ILoginService loginService;
+
+	
+	
+	public ILoginService getLoginService() {
+		return loginService;
+	}
+
+
+
+	public void setLoginService(ILoginService loginService) {
+		this.loginService = loginService;
+	}
+
+
+
+	public String checkForUserAuthentication(String userName , String passWord)
+	{
+		log.debug("Entered into the checkForUserAuthentication  method in WebServiceHandlerService");
+		try
+		{
+			return loginService.checkUserCredentials(userName,passWord);
+			
+		}catch(Exception e)
+		{
+			log.error("Exception raised in checkForUserAuthentication  method in WebServiceHandlerService");
+			e.printStackTrace();
+		}
+		return "error";
+	}
+
+
+}
