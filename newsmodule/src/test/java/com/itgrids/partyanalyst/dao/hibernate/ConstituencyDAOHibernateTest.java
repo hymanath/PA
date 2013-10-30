b977ceb754ce2c781159542e80efe47d5a14bd5a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-10-30 07:00:30 +0000

    Updated By AnuRadha

--- newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyDAOHibernateTest.java
+++ newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ConstituencyDAOHibernateTest.java
@@ -14,7 +14,7 @@ public class ConstituencyDAOHibernateTest extends BaseDaoTestCase{
 		this.constituencyDAO = constituencyDAO;
 	}
 	
-	public void testfindByStateIdForUser(){
+	/*public void testfindByStateIdForUser(){
 		
 	List<Object[]> constituencies = constituencyDAO.findConstituencyByDistrictElectionType(7l,"Assembly");
 	
@@ -22,6 +22,19 @@ public class ConstituencyDAOHibernateTest extends BaseDaoTestCase{
 			System.out.println(object[0]);
 			System.out.println(object[1]);
 			}
+	}*/
+	
+	/*public void testgetStateDistrictConstituency()
+	{
+		List list = constituencyDAO.getStateDistrictConstituency(232L);
+		for(Object[] params:(List<Object[]>)list)
+		 System.out.println(params[0]+" "+params[1]+" "+params[2]+" "+params[3]+" "+params[4]);
+ 	}*/
+	
+	
+	public void testgetStateByConstituencyId()
+	{
+	  System.out.println(constituencyDAO.getStateByConstituencyId(232l).size());	
 	}
 	
 }
--- newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/DelimitationConstituencyMandalDAOHibernateTest.java
+++ newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/DelimitationConstituencyMandalDAOHibernateTest.java
@@ -21,10 +21,18 @@ public class DelimitationConstituencyMandalDAOHibernateTest extends BaseDaoTestC
 	}
 	*/
 	
-	public void testgetMandalsOfConstituency()
+	/*public void testgetMandalsOfConstituency()
 	{
 		List list = delimitationConstituencyMandalDAO.getMandalsOfConstituency(27772L);
 		System.out.println(list.size());
 	}
-
+*/
+  public void testgetStateDistrictConstituencyIdByMandalId()
+  {
+	  List<Object[]> list = delimitationConstituencyMandalDAO.getStateDistrictConstituencyIdByMandalId(832L);
+	  System.out.println(list.size());
+	  for(Object[] params:list)
+	   System.out.println(params[0]+" "+params[1]+" "+params[2]+" "+params[3]);
+  }
+	
 }
