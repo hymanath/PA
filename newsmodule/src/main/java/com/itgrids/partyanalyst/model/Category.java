8be40967f211e38f478e358cc9595336b54336b2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-04 11:09:45 +0000

    Updated by Praneetha

--- newsmodule/src/main/java/com/itgrids/partyanalyst/model/Category.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/model/Category.java
@@ -11,11 +11,15 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
 import org.hibernate.annotations.NotFoundAction;
 import com.itgrids.partyanalyst.model.BaseModel;
 
@@ -24,6 +28,10 @@ import com.itgrids.partyanalyst.model.BaseModel;
 @Table(name="category")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Category extends BaseModel implements java.io.Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1902840178686322806L;
 	private Long categoryId;
 	private String categoryType;
 	private Long orderNo;
@@ -34,17 +42,20 @@ public class Category extends BaseModel implements java.io.Serializable{
 	private String isPrivate;
 	private Date createdDate;
 	private Date updateddate;
+	private MainCategory maincategory;
 	
+
 	public Category(){
 		
 	}
-	public Category(String categoryType,Long orderNo,String isDelete,String isPrivate,Date createdDate,Date updateddate){
+	public Category(String categoryType,Long orderNo,String isDelete,String isPrivate,Date createdDate,Date updateddate,MainCategory maincategory){
 		this.categoryType=categoryType;
 		this.orderNo=orderNo;
 		this.createdDate = createdDate;
 		this.updateddate = updateddate;
 		this.isPrivate = isPrivate;
 		this.isDelete = isDelete;
+		this.maincategory=maincategory;
 		
 	}
 	
@@ -128,5 +139,14 @@ public class Category extends BaseModel implements java.io.Serializable{
 		this.updateddate = updateddate;
 	}
 	
-	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "main_category_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public MainCategory getMaincategory() {
+		return maincategory;
+	}
+	public void setMaincategory(MainCategory maincategory) {
+		this.maincategory = maincategory;
+	}
 }
