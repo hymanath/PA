590cd99646d1aa48ec2aa8fce8beb9daae3a1aef
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-06 11:19:18 +0000

    Updated By AnuRadha

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GallaryKeywordDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GallaryKeywordDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IGallaryKeywordDAO;
 import com.itgrids.partyanalyst.model.GallaryKeyword;
@@ -11,6 +14,24 @@ public class GallaryKeywordDAO extends GenericDaoHibernate<GallaryKeyword, Long>
 	public GallaryKeywordDAO() {
 		super(GallaryKeyword.class);
 		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getGallaryKeywords(List<String> keywordsList)
+	{
+	  Query query = getSession().createQuery(" select model.gallary.gallaryId,model.keyword.type from GallaryKeyword model " +
+	  		" where model.keyword.type in (:keywordsList) ");
+	  
+	  query.setParameterList("keywordsList", keywordsList);
+	  return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getGallaryMappedKeywordsList(List<String> keywordsList)
+	{
+	  Query query = getSession().createQuery(" select model.keyword.type from GallaryKeyword model where model.keyword.type in (:keywordsList) ");
+	  query.setParameterList("keywordsList", keywordsList);
+	  return query.list();
 	}
 
 }
--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/KeywordDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/KeywordDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IKeywordDAO;
 import com.itgrids.partyanalyst.model.Keyword;
@@ -12,5 +15,40 @@ public class KeywordDAO extends GenericDaoHibernate<Keyword, Long> implements IK
 		
 	}
 	
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getTotalKeyWords()
+	{
+		Query query = getSession().createQuery(" select model.keywordId,model.type from Keyword model order by model.type ");
+		return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getKeyWordsList(List<String> keywordList)
+	{
+	  StringBuilder str = new StringBuilder();
+	  str.append(" select model.keywordId,model.type from Keyword model where model.type in (:keywordList) ");
+	  Query query = getSession().createQuery(str.toString());
+	  query.setParameterList("keywordList", keywordList);
+	  return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getKeyWordIdByName(String keywordName)
+	{
+	  StringBuilder str = new StringBuilder();
+	  str.append(" select model.keywordId,model.type from Keyword model where model.type =:keywordName ");
+	  Query query = getSession().createQuery(str.toString());
+	  query.setParameter("keywordName", keywordName);
+	  return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getExistingKeywordsByKeywordsList(List<String> keywordList)
+	{
+		Query query = getSession().createQuery(" select model.type from Keyword model where model.type in(:keywordList) ");
+		query.setParameterList("keywordList", keywordList);
+		return query.list();
+	}
 
 }
