48fb1efc29ae4c8cafddbc2548aa6a5064fd2e5c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-15 12:38:57 +0000

    Updated By AnuRadha

---
+++ newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/BenefitDAOHibernateTest.java
@@ -0,0 +1,25 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IBenefitDAO;
+
+public class BenefitDAOHibernateTest extends BaseDaoTestCase{
+
+	private IBenefitDAO benefitDAO;
+
+	public void setBenefitDAO(IBenefitDAO benefitDAO) {
+		this.benefitDAO = benefitDAO;
+	}
+	
+	/*public void test()
+	{
+		benefitDAO.getAll();	
+	}*/
+	
+	public void testGetBenifitsList()
+	{
+		System.out.println(benefitDAO.getBenifitsList().size());
+	}
+	
+}
