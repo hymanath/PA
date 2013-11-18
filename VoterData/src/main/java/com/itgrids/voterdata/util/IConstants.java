97a83e5f1d3063e2212cd6034579adf2b377cbab
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-18 14:39:25 +0000

    updated by Kamalakar Dandu

--- VoterData/src/main/java/com/itgrids/voterdata/util/IConstants.java
+++ VoterData/src/main/java/com/itgrids/voterdata/util/IConstants.java
@@ -2,11 +2,12 @@ package com.itgrids.voterdata.util;
 
 public interface IConstants {
 
-	public static final String VOTER_DATA_SITE_URL = "http://117.239.55.71/DraftRolls/Rolls.aspx";
+	public static final String VOTER_DATA_SITE_URL = "http://123.176.47.55/DraftRolls/Rolls.aspx";
 	public static final String FIREFOX_PATH = "C:/Program Files/Mozilla Firefox/firefox.exe";
 	public static final String DOWNLOAD_DIRECTORY = "D:\\Kamal\\274-Raptadu";
-	public static final Boolean IS_MISSED_FILES_DOWNLOAD = true;
-	public static final int PATTERN = 1;
+	public static final Boolean IS_MISSED_FILES_DOWNLOAD = false;
+	public static int PATTERN = 9;
 	public static final String ADDED = "Added";
 	public static final String DELETED = "Deleted";
+	public static Boolean CONSIDER_DUPLICATES = true;
 }
