34775e22e46753d34c66bfbcf5c8871194c98e48
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-21 11:33:12 +0000

    Updated By Srishailam

--- newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidatePartyKeywordDAOHibernateTest.java
+++ newsmodule/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidatePartyKeywordDAOHibernateTest.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ICandidatePartyKeywordDAO;
+import com.itgrids.partyanalyst.model.CandidatePartyKeyword;
 
 
 public class CandidatePartyKeywordDAOHibernateTest extends BaseDaoTestCase{
@@ -14,9 +17,22 @@ public class CandidatePartyKeywordDAOHibernateTest extends BaseDaoTestCase{
 	this.candidatePartyKeywordDAO = candidatePartyKeywordDAO;
   }
   
-  public void test()
+ /* public void test()
   {
 	  candidatePartyKeywordDAO.getAll(); 
   }
-  
+  */
+  public void testremoveDublicateData()
+  {
+	 // @SuppressWarnings("unchecked")
+	//List<CandidatePartyKeyword> list= (List<CandidatePartyKeyword>) candidatePartyKeywordDAO.getCandidateFileDetails(1L,2L);
+	  //Long no = candidatePartyKeywordDAO.removeKeywordsList(9L);
+	  //List<Object[]> list = candidatePartyKeywordDAO.getCandidatePartyKeywordListByUserwise();
+	  List<CandidatePartyKeyword> list = candidatePartyKeywordDAO.getCandidatePartyKeywordList(1L);
+	  System.out.println(list.size());
+	  System.out.println(list);
+
+
+  }
+
 }
