e458ca22442aec76e2b8bb930e63bd00544e42a6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-21 13:40:41 +0000

    Updated By AnuRadha

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/INewsReportDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/INewsReportDAO.java
@@ -7,9 +7,15 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.NewsReport;
 
 public interface INewsReportDAO extends GenericDao<NewsReport, Long>{
-	public List<Object[]> getNewsReports(Long userId);
+	
+	public List<Object[]> getNewsReports(Long userId,Integer startIndex,Integer maxIndex);
+	
 	public Long checkValidUserForReport(Long userId,Long reportId);
+	
 	public Long checkValidReportKey(String key);
+	
 	public void updateKey(String key);
+	
 	public void updateNewKey(String key,Long reportId);
+	
 }
