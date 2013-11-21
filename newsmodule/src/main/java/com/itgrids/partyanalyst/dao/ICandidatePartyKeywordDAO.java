191a99c70c03c837ad2e3b04abc85bd0613b24f9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-11-21 11:28:06 +0000

    Updated By Srishailam

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidatePartyKeywordDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICandidatePartyKeywordDAO.java
@@ -1,9 +1,22 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CandidatePartyKeyword;
 
 public interface ICandidatePartyKeywordDAO extends GenericDao<CandidatePartyKeyword, Long>{
-
+	
+	public List<CandidatePartyKeyword> getCandidatePartyKeywordList(Long keywordId);
+	
+	public CandidatePartyKeyword getCandidateFileDetails(Long candidateFileId,Long newKeywordID);
+	
+	public Long removeDublicateData(Long candidatePartyKeywordId,Long keywordId);
+	
+	public Long removeKeywordsList(Long keywordId);
+	
+	public List<CandidatePartyKeyword> getCandidatePartyKeywordListByUserwise(Long candidatePartyFileId,Long keywordId);
+	
+	public List<Long> getCandidateFileIds(Long keywordId);
 }
