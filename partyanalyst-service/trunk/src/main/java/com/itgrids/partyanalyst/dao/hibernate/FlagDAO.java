bd196b70d508d8480e04adb7aa7db4f410ef75f7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-12-12 09:08:04 +0000

    updated by nagamani -- flag implementation

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FlagDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FlagDAO.java
@@ -1,6 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
+
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IFlagDAO;
 import com.itgrids.partyanalyst.model.Flag;
@@ -11,6 +16,23 @@ public class FlagDAO extends GenericDaoHibernate<Flag,Long> implements IFlagDAO
 	public FlagDAO() {
 		super(Flag.class);
 		
+	}
+	
+	
+	public List<Object> checkFlagName(String flagName)
+	{
+		return getHibernateTemplate().find("select model.flagId from Flag model where model.name = ?",flagName);
+	}
+	public List<Flag> getAllFlags()
+	{
+		return getHibernateTemplate().find("select model from Flag model order by model.flagId desc");
+	}
+	
+	public Integer deleteFlag(Long flagId)
+	{
+		Query query = getSession().createQuery("delete from Flag model where model.flagId = :flagId");
+		query.setParameter("flagId", flagId);
+		return query.executeUpdate();
 	}
 
 }
