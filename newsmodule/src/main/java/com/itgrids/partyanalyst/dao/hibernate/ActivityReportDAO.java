9132d1b9f4a0c5f483123a065fe9e42d98ced861
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-12-19 08:14:10 +0000

    updated by prasad

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IActivityReportDAO;
 import com.itgrids.partyanalyst.model.ActivityReport;
@@ -8,5 +9,12 @@ import com.itgrids.partyanalyst.model.ActivityReport;
 public class ActivityReportDAO   extends GenericDaoHibernate<ActivityReport, Long>  implements IActivityReportDAO{
     public ActivityReportDAO(){
     	super(ActivityReport.class);
+    }
+    
+    public String getCategoeryIds(String key)
+    {
+    	Query query = getSession().createQuery("select model.categories from ActivityReport model where model.reportKey = :key");
+    	query.setParameter("key", key);
+    	return (String) query.uniqueResult();
     }
 }
--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportFilesDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportFilesDAO.java
@@ -12,13 +12,15 @@ public class ActivityReportFilesDAO  extends GenericDaoHibernate<ActivityReportF
    public ActivityReportFilesDAO(){
 	   super(ActivityReportFiles.class);
    }
-   public List<Object[]> getActivitiesList(String key){
+   @SuppressWarnings("unchecked")
+public List<Object[]> getActivitiesList(String key,List<Long> catgList){
 	   Query query = getSession().createQuery("select model.file.synopsysDescription,model.file.synopsysFont.fontId,model.file.fileDate," +
 	   		"model.file.userAddress.district.districtId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.constituencyId," +
 	   		"model.file.userAddress.constituency.name,cpc.gallary.gallaryId,cpc.gallary.name from ActivityReportFiles model,CandidatePartyCategory cpc where model.activityReport.reportKey = :key " +
-	   		"  and model.file.fileId = cpc.candidatePartyFile.file.fileId and cpc.gallary.gallaryId in(select AR.categories from ActivityReport AR where AR.reportKey = :key) " +
+	   		"  and model.file.fileId = cpc.candidatePartyFile.file.fileId and cpc.gallary.gallaryId in(:catgList) " +
 	   		" order by cpc.gallary.gallaryId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.name,model.file.fileDate desc");
 	   query.setParameter("key", key);
+	   query.setParameterList("catgList", catgList);
 	   return query.list();
    }
 }
