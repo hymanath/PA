ff753135bd402c5ca956c2047fbe2e1b67191220
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2013-12-20 08:31:44 +0000

    updated by prasad

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportFilesDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityReportFilesDAO.java
@@ -14,11 +14,12 @@ public class ActivityReportFilesDAO  extends GenericDaoHibernate<ActivityReportF
    }
    @SuppressWarnings("unchecked")
 public List<Object[]> getActivitiesList(String key,List<Long> catgList){
-	   Query query = getSession().createQuery("select distinct model.file.synopsysDescription,model.file.synopsysFont.fontId,model.file.fileDate," +
-	   		"model.file.userAddress.district.districtId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.constituencyId," +
-	   		"model.file.userAddress.constituency.name,cpc.gallary.gallaryId,cpc.gallary.name from ActivityReportFiles model,CandidatePartyCategory cpc where model.activityReport.reportKey = :key " +
+	   Query query = getSession().createQuery(" select distinct model.file.synopsysDescription,model.file.synopsysFont.fontId,model.file.fileDate, " +
+	   		"  model.file.userAddress.district.districtId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.constituencyId, " +
+	   		"  model.file.userAddress.constituency.name,cpc.gallary.gallaryId,cpc.gallary.name from ActivityReportFiles model,CandidatePartyCategory cpc where model.activityReport.reportKey = :key " +
 	   		"  and model.file.fileId = cpc.candidatePartyFile.file.fileId and cpc.gallary.gallaryId in(:catgList) " +
-	   		" order by cpc.gallary.gallaryId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.name,model.file.fileDate desc");
+	   		"  and model.file.isDeleted = 'N' " +
+	   		"  order by cpc.gallary.gallaryId,model.file.userAddress.district.districtName,model.file.userAddress.constituency.name,model.file.fileDate desc");
 	   query.setParameter("key", key);
 	   query.setParameterList("catgList", catgList);
 	   return query.list();
