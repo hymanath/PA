f96e9cc1aa88c7c4b567328653b4e4b2f22a041f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-10 12:20:55 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PredictedVoterCaste.java
@@ -0,0 +1,76 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+@Entity
+@Table(name = "predicted_voter_caste")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PredictedVoterCaste  extends BaseModel implements java.io.Serializable {
+	
+	private Long predictedVoterCasteId;
+	private Voter voter;
+	
+	private CasteState casteState;
+	public PredictedVoterCaste()
+	{
+		
+	}
+	public PredictedVoterCaste( Long predictedVoterCasteId,Voter voter,CasteState casteState)
+	{
+		this.predictedVoterCasteId =predictedVoterCasteId;
+		this.voter =voter;
+		this.casteState = casteState;
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="predicted_voter_caste_id", unique = true, nullable = false)
+	public Long getPredictedVoterCasteId() {
+		return predictedVoterCasteId;
+	}
+
+	public void setPredictedVoterCasteId(Long predictedVoterCasteId) {
+		this.predictedVoterCasteId = predictedVoterCasteId;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="voter_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Voter getVoter() {
+		return voter;
+	}
+
+	public void setVoter(Voter voter) {
+		this.voter = voter;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="caste_state_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public CasteState getCasteState() {
+		return casteState;
+	}
+
+	public void setCasteState(CasteState casteState) {
+		this.casteState = casteState;
+	}
+	
+	
+	
+
+}
