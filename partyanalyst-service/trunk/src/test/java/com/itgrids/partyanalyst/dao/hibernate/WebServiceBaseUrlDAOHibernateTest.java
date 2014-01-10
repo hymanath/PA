9d765a65a97c67eb46ed10bdc53182ee4831a4ba
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-10 07:01:07 +0000

    Updated By Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/WebServiceBaseUrlDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/WebServiceBaseUrlDAOHibernateTest.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IWebServiceBaseUrlDAO;
+import com.itgrids.partyanalyst.model.WebServiceBaseUrl;
 
 public class WebServiceBaseUrlDAOHibernateTest extends BaseDaoTestCase{
 
@@ -17,10 +18,16 @@ public class WebServiceBaseUrlDAOHibernateTest extends BaseDaoTestCase{
 		webServiceBaseUrlDAO.getAll();
 	}*/
 	
-	public void testGetBaseURLForAnApp()
+	/*public void testGetBaseURLForAnApp()
 	{
 		webServiceBaseUrlDAO.getAll();
 		System.out.println(webServiceBaseUrlDAO.getBaseURLForAnApp("IPAD"));
+	}*/
+	
+	public void testGetBaseUrlDataForAnApp()
+	{
+		WebServiceBaseUrl url = webServiceBaseUrlDAO.getBaseUrlDataForAnApp("IPAD");
+		System.out.println(url.getUrl());
 	}
 	
 }
