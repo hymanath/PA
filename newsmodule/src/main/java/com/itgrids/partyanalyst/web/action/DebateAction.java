bca59a1bc82f58c587cb47da15375d8110df037d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-24 13:30:13 +0000

    updated by prasad , debate integration

--- newsmodule/src/main/java/com/itgrids/partyanalyst/web/action/DebateAction.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/web/action/DebateAction.java
@@ -1,10 +1,15 @@
 package com.itgrids.partyanalyst.web.action;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 
+import com.itgrids.partyanalyst.dto.DebateDetailsVO;
+import com.itgrids.partyanalyst.dto.DebateVO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.service.IDebateService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -16,8 +21,12 @@ public class DebateAction extends ActionSupport implements ServletRequestAware{
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = Logger.getLogger(DebateAction.class); 
 	private HttpServletRequest request;
+	private HttpSession session;
 	private IDebateService debateService;
-	@Override
+	private ResultStatus resultStatus;
+	private DebateVO debateVO;
+	
+	
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 	}
@@ -27,9 +36,87 @@ public class DebateAction extends ActionSupport implements ServletRequestAware{
 		this.debateService = debateService;
 	}
 
+	
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+
+
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+
+	
+
+	public DebateVO getDebateVO() {
+		return debateVO;
+	}
+
+
+	public void setDebateVO(DebateVO debateVO) {
+		this.debateVO = debateVO;
+	}
+
 
 	public String execute()
 	{
+		try {
+			LOG.info("Entered into execute methon in DebateAction Class");
+			session = request.getSession();
+			RegistrationVO regVO = (RegistrationVO) session.getAttribute("USER");
+			if(regVO == null)
+			{
+				return Action.ERROR;
+			}
+		} catch (Exception e) {
+			LOG.error("Exception occured in execute methon in DebateAction Class",e);
+		}
+		
+		return Action.SUCCESS;
+	}
+	
+	
+	
+	
+	
+	public String saveDebateDetial()
+	{
+		try 
+		{
+			LOG.info("Entered into saveDebateDetial methon in DebateAction Class");
+			session = request.getSession();
+			RegistrationVO regVO = (RegistrationVO) session.getAttribute("USER");
+			if(regVO == null)
+			{
+				return Action.ERROR;
+			}
+			 DebateDetailsVO debateDetailsVO = new DebateDetailsVO();
+			 resultStatus = debateService.saveDebateDetails(debateDetailsVO);
+		} 
+		catch (Exception e)
+		{
+			LOG.error("Exception occured in saveDebateDetial methon in DebateAction Class",e);
+		}
+		return Action.SUCCESS;
+	}
+	
+	public String retriveDebateDetails()
+	{
+		try 
+		{
+			LOG.info("Entered into saveDebateDetial methon in DebateAction Class");
+			session = request.getSession();
+			RegistrationVO regVO = (RegistrationVO) session.getAttribute("USER");
+			if(regVO == null)
+			{
+				return Action.ERROR;
+			}
+			debateVO = debateService.getDebateDetailsForSelected(Long.parseLong(request.getParameter("debateId")));
+		} 
+		catch (Exception e)
+		{
+			LOG.error("Exception occured in saveDebateDetial methon in DebateAction Class",e);
+		}
 		return Action.SUCCESS;
 	}
 
