d6477e936b825abb37c73aca3b87f9f2e60c7547
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-25 13:36:28 +0000

    Updated by Sravanthi

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CharacteristicsDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CharacteristicsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICharacteristicsDAO;
 import com.itgrids.partyanalyst.model.Characteristics;
@@ -11,5 +14,12 @@ public class CharacteristicsDAO extends GenericDaoHibernate<Characteristics, Lon
 		super(Characteristics.class);
 
 	}
+			
+	@SuppressWarnings("unchecked")
+	public List<Characteristics> getCharacteristicsDetails(){
+		Query query = getSession().createQuery("select model from Characteristics model where model.isDeleted != 'Y'");
+		 
+		return query.list();
+	 }
 
 }
