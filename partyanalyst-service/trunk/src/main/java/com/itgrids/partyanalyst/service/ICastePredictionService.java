3f0084c683c74884343ad3606e937fc6037b3cdf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-25 15:36:30 +0000

    Updated By Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICastePredictionService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICastePredictionService.java
@@ -2,10 +2,16 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.excel.booth.VoterVO;
 
 public interface ICastePredictionService {
 	
-	public List<VoterVO> getCountForCaste(String usertype);
+	public List<VoterVO> getCountForCaste(Long userId);
+	
+	public List<SelectOptionVO> getPredictedCasteList();
+	
+	public ResultStatus insertPredictedCasteIntoMainCasteTables(Long userId,Long casteStateId,Integer firstRecord,Integer maxRecords);
 
 }
