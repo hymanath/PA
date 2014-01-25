c789345048b2dd3377c9de9f99ecf7616313e22a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-25 13:36:28 +0000

    Updated by Sravanthi

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICharacteristicsDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/ICharacteristicsDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Characteristics;
 
 public interface ICharacteristicsDAO extends GenericDao<Characteristics, Long>{
 
+	public List<Characteristics> getCharacteristicsDetails();
 }
