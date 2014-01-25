97c1bf2513a222c14a375d4f9a01772b28df2b61
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-01-25 15:38:56 +0000

    Updated By Kamalakar Dandu

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/PredictedCasteAssignAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/PredictedCasteAssignAction.java
@@ -12,6 +12,7 @@ import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.excel.booth.VoterVO;
 import com.itgrids.partyanalyst.service.ICastePredictionService;
 import com.opensymphony.xwork2.Action;
@@ -19,16 +20,24 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class PredictedCasteAssignAction extends ActionSupport implements ServletRequestAware{
 	
+	private static final long serialVersionUID = 7126714305674261144L;
 	private static final Logger log = Logger.getLogger(PredictedCasteAssignAction.class);
+	
 	private HttpServletRequest request;
 	private ICastePredictionService castePredictionService;
 	private ResultStatus resultStatus;
 	private List<VoterVO> casteCountList;
 	private HttpSession session;
-	
+	private List<SelectOptionVO> predictionCasteList;
 	private String task;
 	JSONObject jObj;
 	
+	public List<SelectOptionVO> getPredictionCasteList() {
+		return predictionCasteList;
+	}
+	public void setPredictionCasteList(List<SelectOptionVO> predictionCasteList) {
+		this.predictionCasteList = predictionCasteList;
+	}
 	public ResultStatus getResultStatus() {
 		return resultStatus;
 	}
@@ -72,7 +81,6 @@ public class PredictedCasteAssignAction extends ActionSupport implements Servlet
 		this.castePredictionService = castePredictionService;
 	}
 	
-
 	public List<VoterVO> getCasteCountList() {
 		return casteCountList;
 	}
@@ -81,22 +89,24 @@ public class PredictedCasteAssignAction extends ActionSupport implements Servlet
 	}
 	public String execute()
 	{
-		
-		
-		return Action.SUCCESS;
-		
+		try{
+			predictionCasteList = castePredictionService.getPredictedCasteList();
+			return Action.SUCCESS;
+			
+		}catch(Exception e)
+		{
+			log.error("Exception is - ",e);
+			return Action.SUCCESS;
+		}
 	}
 	
 	public String getCasteCount()
 	{
 		try{
-			
 			jObj = new JSONObject(getTask());
 			session = request.getSession();
 			RegistrationVO regVo = (RegistrationVO) session.getAttribute("USER");
-			String userType = regVo.getUserType();
-			casteCountList = castePredictionService.getCountForCaste(userType);
-			
+			casteCountList = castePredictionService.getCountForCaste(regVo.getRegistrationID());
 		}
 		catch(Exception e)
 		{
@@ -107,23 +117,30 @@ public class PredictedCasteAssignAction extends ActionSupport implements Servlet
 	}
 	
 	
-	public String insertPredictedCaste()
+	public ResultStatus insertPredictedCaste()
 	{
+		resultStatus = new ResultStatus();
 		try{
 			
 			jObj = new JSONObject(getTask());
 			session = request.getSession();
 			RegistrationVO regVo = (RegistrationVO) session.getAttribute("USER");
-		
+			if(regVo == null)
+				return null;
 			
+			Long casteStateId = jObj.getLong("casteStateId");
+			Integer firstRecord = jObj.getInt("firstRecord");
+			Integer maxRecords = jObj.getInt("maxRecords");
 			
+			resultStatus = castePredictionService.insertPredictedCasteIntoMainCasteTables(regVo.getRegistrationID(),casteStateId,firstRecord,maxRecords);
+			return resultStatus;
 		}
 		catch(Exception e)
 		{
 			log.error("Exception Occured in getCasteCount() method in PredictedCasteAssignAction ", e);
+			return resultStatus;
 		}
 
-		return Action.SUCCESS;
 	}
 	
 	
