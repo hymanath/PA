606a17a12397a3f19d68903795770392c4661b82
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-03 09:33:10 +0000

    Updated By Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAO.java
@@ -2722,16 +2722,19 @@ IUserVoterDetailsDAO{
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<Object[]> getHamletBoothInfo(Long constituencyId,Long userId)
+	public List<Object[]> getHamletBoothInfo(Long constituencyId,Long publicationDateId,Long userId)
 	{
 		Query query = getSession().createQuery("select model.hamlet.hamletId,model2.booth.boothId,model2.booth.publicationDate.publicationDateId from UserVoterDetails model,BoothPublicationVoter model2 " +
-				" where model.voter.voterId = model2.voter.voterId and model.hamlet is not null and model.user.userId = :userId and model2.booth.constituency.constituencyId = :constituencyId " +
+				" where model.voter.voterId = model2.voter.voterId and model.hamlet is not null and model.user.userId = :userId and model2.booth.constituency.constituencyId = :constituencyId and model2.booth.publicationDate.publicationDateId = :publicationDateId " +
 				" group by model2.booth.publicationDate.publicationDateId,model.hamlet.hamletId,model2.booth.boothId order by model2.booth.publicationDate.publicationDateId ");
 		
 		query.setParameter("constituencyId",constituencyId);
+		query.setParameter("publicationDateId",publicationDateId);
 		query.setParameter("userId",userId);
 		return query.list();
 	}
+	
+	@SuppressWarnings("unchecked")
 	public List<Object[]> getWardIdsByLocalEleBodyIdPublicationId(Long constituencyId,Long userId,Long publicationDateId,Long localEleBodyId)
 	{
 		Query query = getSession().createQuery(" select distinct model.ward.constituencyId,model.ward.name,model.ward.localElectionBody.localElectionBodyId from UserVoterDetails model, BoothPublicationVoter model2 where model.voter.voterId = model2.voter.voterId and " +
