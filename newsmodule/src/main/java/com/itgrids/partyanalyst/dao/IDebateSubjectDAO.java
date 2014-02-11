3fcbc3a5cd63441d25320cb326317cd6e715acd2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-11 07:03:06 +0000

    Updated By Srishailam

--- newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IDebateSubjectDAO.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/dao/IDebateSubjectDAO.java
@@ -11,9 +11,9 @@ public interface IDebateSubjectDAO extends GenericDao<DebateSubject, Long>{
 
 	public List<Object[]> getDebateSubjectDetails(Long debateId);
 	
-	public List<Object[]> getDebateDetalsForSelectedDates(Date fromDate,Date toDate);
+	//public List<Object[]> getDebateDetalsForSelectedDates(Date fromDate,Date toDate);
 	
-	public List<Object[]> getDebateDetalsForSelectedCriteria(Date fromDate,Date toDate,Long channelId,Long partyId,Long candidateId);
+	public List<Object[]> getDebateDetalsForSelectedCriteria(Date fromDate,Date toDate,Long channelId,Long partyId,Long candidateId,String sortBy,String sort,int startIndex,int maxIndex,String isCount);
 	
 	public List<Object[]> searchCriteriaForDebateSearch(String searchString);
 }
