de5ad6998028ca4d092dc05f7f0b47bbecb9d6f6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-15 09:01:39 +0000

    updated by nagamani -- catse wise reports in suggestive model

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/UserVoterDetailsDAO.java
@@ -2883,4 +2883,33 @@ IUserVoterDetailsDAO{
 		query.setParameterList("voterIds",voterIds);
 		return query.list();	
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getCasteReport(Long constituencyId,Long publicationId,String type,Long userId)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("select distinct count(model.voter.voterId),model.casteState.caste.casteName,model.casteState.caste.casteId, ");
+		if(type.equalsIgnoreCase(IConstants.MANDAL))
+			str.append("bpv.booth.tehsil.tehsilId,bpv.booth.tehsil.tehsilName ");
+		else if(type.equalsIgnoreCase(IConstants.PANCHAYAT))
+			str.append("bpv.booth.panchayat.panchayatId,bpv.booth.panchayat.panchayatName ");
+		else if(type.equalsIgnoreCase(IConstants.BOOTH))
+			str.append("bpv.booth.boothId,bpv.booth.partNo,bpv.booth.panchayat.panchayatId,bpv.booth.panchayat.panchayatName ");
+			str.append("from UserVoterDetails model,BoothPublicationVoter bpv where model.user.userId = :userId and bpv.voter.voterId = model.voter.voterId" +
+				" and bpv.booth.publicationDate.publicationDateId = :publicationId and bpv.booth.constituency.constituencyId = :constituencyId");
+			if(type.equalsIgnoreCase(IConstants.MANDAL))
+				str.append("  group by model.casteState.caste.casteId,bpv.booth.tehsil.tehsilId order by model.casteState.caste.casteName");
+		
+			else if(type.equalsIgnoreCase(IConstants.PANCHAYAT))
+				str.append("  group by model.casteState.caste.casteId,bpv.booth.panchayat.panchayatId order by model.casteState.caste.casteName");
+			else if(type.equalsIgnoreCase(IConstants.BOOTH))
+				str.append("  group by model.casteState.caste.casteId,bpv.booth.boothId order by bpv.booth.panchayat.panchayatName, bpv.booth.boothId," +
+						" model.casteState.caste.casteName");
+		Query query = getSession().createQuery(str.toString());
+		query.setParameter("constituencyId", constituencyId);
+		query.setParameter("publicationId", publicationId);
+		query.setParameter("userId", userId);
+		
+		return query.list();
+	}
 }
