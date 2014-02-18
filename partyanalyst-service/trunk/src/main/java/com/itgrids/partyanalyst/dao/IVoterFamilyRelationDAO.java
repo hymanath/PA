c72229c24a3585c2e038b372aa104e046f757990
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-18 08:54:27 +0000

    HOUSE HOLD REPORT TASK -- SASI

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVoterFamilyRelationDAO.java
@@ -0,0 +1,10 @@
+
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.VoterFamilyRelation;
+
+public interface IVoterFamilyRelationDAO extends GenericDao<VoterFamilyRelation, Long>{
+	
+}
