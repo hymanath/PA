1929c250252f8127c947d7b7c89b578545dac8e2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-18 08:54:39 +0000

    HOUSE HOLD REPORT TASK -- SASI

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/VoterFamilyRelationDAOHibernateTest.java
@@ -0,0 +1,25 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IHHOptionTypeDAO;
+import com.itgrids.partyanalyst.dao.IHHOptionsDAO;
+import com.itgrids.partyanalyst.dao.ISurveyQuestionDAO;
+import com.itgrids.partyanalyst.dao.IVoterFamilyRelationDAO;
+
+public class VoterFamilyRelationDAOHibernateTest extends BaseDaoTestCase{
+	
+	private IVoterFamilyRelationDAO voterFamilyRelationDAO;
+	
+	
+	
+	public IVoterFamilyRelationDAO getVoterFamilyRelationDAO() {
+		return voterFamilyRelationDAO;
+	}
+
+
+	public void test(){
+		System.out.println("hhOptionsDAO");
+	}
+
+}
