24496b586adba1d63b705f336b27a02c49b7cfc1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-18 04:49:03 +0000

    HOUSE HOLD REPORT TASK -- SASI

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IHouseHoldSurveyReportService.java
@@ -0,0 +1,19 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.GenericVO;
+import com.itgrids.partyanalyst.dto.HHQuestionDetailsVO;
+import com.itgrids.partyanalyst.dto.HHSurveyVO;
+
+public interface IHouseHoldSurveyReportService {
+	public List<HHSurveyVO> getHHSurveyQuestionOptions(Long surveyId,Long boothId,String houseNo);
+	
+	public List<GenericVO> getAllOptionTypes();
+	public String createSurveySubCategory(String name,boolean isChild,Long parentId);
+	public String saveHouseHoldQuestionDetails(final HHQuestionDetailsVO qstnDtls);
+	public List<GenericVO> getSubSurveyTypesByMainTypeId(Long mainTypeId);
+	public List<GenericVO> getSurveyTypes();
+	public String saveQuestOptnsOfHH(Long boothId,String houseNo,List<HHSurveyVO> questOptsList);
+	
+}
