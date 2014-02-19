006738c53de2ca396703ee94afb0a7a81bf467de
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-19 06:16:41 +0000

    CADRE/IP/MOBILE NO - WEBSERVICE TASK -- SASI

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/VoterDetailsVO.java
@@ -0,0 +1,134 @@
+package com.itgrids.partyanalyst.dto;
+
+public class VoterDetailsVO {
+	private String voterName;
+	private Long voterId;
+	private String age;
+	private String gender;
+	private Long constituencyId;
+	private Long boothId;
+	private Long publicationDateId;
+	private Long casteGroupId;
+	private String caste;
+	private Long hamletId;
+	private Long wardId;
+	private Long tehsilId;
+	private Long localElectionBodyId;
+	private Long districtId;
+	private String mobileNo;
+	private String relativeName;
+	private String houseNo;
+	private String wardName;
+	
+	
+	public String getRelativeName() {
+		return relativeName;
+	}
+	public void setRelativeName(String relativeName) {
+		this.relativeName = relativeName;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public Long getHamletId() {
+		return hamletId;
+	}
+	public void setHamletId(Long hamletId) {
+		this.hamletId = hamletId;
+	}
+	public Long getWardId() {
+		return wardId;
+	}
+	public void setWardId(Long wardId) {
+		this.wardId = wardId;
+	}
+	public Long getTehsilId() {
+		return tehsilId;
+	}
+	public void setTehsilId(Long tehsilId) {
+		this.tehsilId = tehsilId;
+	}
+	public Long getLocalElectionBodyId() {
+		return localElectionBodyId;
+	}
+	public void setLocalElectionBodyId(Long localElectionBodyId) {
+		this.localElectionBodyId = localElectionBodyId;
+	}
+	public Long getCasteGroupId() {
+		return casteGroupId;
+	}
+	public void setCasteGroupId(Long casteGroupId) {
+		this.casteGroupId = casteGroupId;
+	}
+	public String getCaste() {
+		return caste;
+	}
+	public void setCaste(String caste) {
+		this.caste = caste;
+	}
+	public String getVoterName() {
+		return voterName;
+	}
+	public void setVoterName(String voterName) {
+		this.voterName = voterName;
+	}
+	public Long getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(Long voterId) {
+		this.voterId = voterId;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	public Long getPublicationDateId() {
+		return publicationDateId;
+	}
+	public void setPublicationDateId(Long publicationDateId) {
+		this.publicationDateId = publicationDateId;
+	}
+	public String getWardName() {
+		return wardName;
+	}
+	public void setWardName(String wardName) {
+		this.wardName = wardName;
+	}
+	
+	
+}
