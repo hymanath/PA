609051ccc8c860a40b1e259269b85a6268e52d3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-19 06:16:14 +0000

    CADRE/IP/MOBILE NO - WEBSERVICE TASK -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICastePredictionService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICastePredictionService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.itgrids.partyanalyst.excel.booth.VoterVO;
+import com.itgrids.partyanalyst.model.UserVoterDetails;
 
 public interface ICastePredictionService {
 	
@@ -13,5 +14,7 @@ public interface ICastePredictionService {
 	public List<SelectOptionVO> getPredictedCasteList();
 	
 	public ResultStatus insertPredictedCasteIntoMainCasteTables(Long userId,Long casteStateId,Integer firstRecord,Integer maxRecords);
+	
+	public UserVoterDetails insertCasteAndPhoneNumberFromAndroid(Long voterId,Long casteStateId,String phoneNumber);
 
 }
