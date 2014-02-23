e00a6cf7ddc385e5c53e8887f9a3c15002c5841f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-23 11:28:47 +0000

    Updated By Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothActivitiesDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.BoothActivities;
+
+public interface IBoothActivitiesDAO extends GenericDao<BoothActivities,Long>{
+
+}
