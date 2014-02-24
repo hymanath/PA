70fdd64d29d8bec1c167fd4ed95cf968c3d93869
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-02-24 21:51:01 +0000

    updated by anil

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyTrendsDAO.java
@@ -0,0 +1,29 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on July 3, 2009
+ */
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.entity.Country;
+import com.itgrids.partyanalyst.model.PartyTrainingCamps;
+import com.itgrids.partyanalyst.model.PartyTrends;
+
+
+/**
+ * Interface for CountryDAO.
+ * 
+ * @author <a href="mailto:shan.amrita@gmail.com">Shan Nagarajan</a>
+ */
+
+public interface IPartyTrendsDAO extends GenericDao<PartyTrends, Long> {
+	
+	public List<?> loadConst();
+	public List<?> loadEntitiesForXl(List<Long> constIds );
+}
