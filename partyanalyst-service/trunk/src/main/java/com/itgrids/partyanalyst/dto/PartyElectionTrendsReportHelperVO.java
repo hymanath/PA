27d32936e82f3993016c52974d77caaefff67102
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-07 01:01:15 +0000

    updated by anil

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PartyElectionTrendsReportHelperVO.java
@@ -0,0 +1,89 @@
+package com.itgrids.partyanalyst.dto;
+
+public class PartyElectionTrendsReportHelperVO  {
+	//private String party;
+	//private Integer electionYear;
+//	private Long  totalVoters;
+//	private Long totalVotesPolled;
+	private String name;
+	private Long votesEarned=0L;	
+	private Long marginVotes=0L;
+	private Double marginVotesPercentage=0.0;
+	private Double percentage=0.0;
+	
+	public Double getMarginVotesPercentage() {
+		return marginVotesPercentage;
+	}
+	public void setMarginVotesPercentage(Double marginVotesPercentage) {
+		this.marginVotesPercentage = marginVotesPercentage;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(Double percentage) {
+		this.percentage = percentage;
+	}
+	public Long getMarginVotes() {
+		return marginVotes;
+	}
+	public void setMarginVotes(Long marginVotes) {
+		this.marginVotes = marginVotes;
+	}
+	public Long getVotesEarned() {
+		return votesEarned;
+	}
+	public void setVotesEarned(Long votesEarned) {
+		this.votesEarned = votesEarned;
+	}
+
+	public PartyElectionTrendsReportHelperVO(String name,Long votesEarned, Double percentage
+			) {
+		super();
+		this.name = name;
+		this.percentage = percentage;
+		
+		this.votesEarned = votesEarned;
+	}
+	/*public PartyElectionTrendsReportHelperVO(String name, Double percentage,
+			Long marginVotes, Double marginVotesPercentage, Long votesEarned) {
+		super();
+		this.name = name;
+		this.percentage = percentage;
+		this.marginVotes = marginVotes;
+		this.marginVotesPercentage = marginVotesPercentage;
+		this.votesEarned = votesEarned;
+	}*/
+	public PartyElectionTrendsReportHelperVO(String name, Long votesEarned,
+			Long marginVotes, Double marginVotesPercentage, Double percentage) {
+		super();
+		this.name = name;
+		this.votesEarned = votesEarned;
+		this.marginVotes = marginVotes;
+		this.marginVotesPercentage = marginVotesPercentage;
+		this.percentage = percentage;
+	}
+	public PartyElectionTrendsReportHelperVO() {
+		super();
+	}
+	
+	
+	
+	
+	
+	
+	
+	//private String electionType;
+	//private Long voterSize;
+//	private Long presVotesEarned;
+//	private Long prevVotesEarned;
+//	private String presPercentage;
+//	private String prevPercentage;
+	
+	
+}
