b34c599ea979a2c6e096e98be4d201a1614425c7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-07 01:01:26 +0000

    updated by anil

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStratagicReportsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStratagicReportsService.java
@@ -1,10 +1,15 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.Collection;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.AgeRangeVO;
+import com.itgrids.partyanalyst.dto.PartyElectionTrendsReportVO;
 
 
 public interface IStratagicReportsService {
 	public List<AgeRangeVO> getBoothWiseAddedAndDeletedVoters(Long constituencyId,Long publicationDateId);
+	public List<PartyElectionTrendsReportVO> getPreviousTrendsReport(Long constId);
+	public List<PartyElectionTrendsReportVO> getPreviousTrendsReportParliament(Long constId);
+
 }
