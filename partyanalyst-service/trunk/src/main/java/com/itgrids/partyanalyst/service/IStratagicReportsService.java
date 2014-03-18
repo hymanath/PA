d7a615b766647546e209cba38df7b8f4aa61267c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-18 06:18:07 +0000

    PARTY VARIATIONS IN DIFF ELECTIONS -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStratagicReportsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStratagicReportsService.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.itgrids.partyanalyst.dto.AgeRangeVO;
+import com.itgrids.partyanalyst.dto.PartyEffectVO;
 import com.itgrids.partyanalyst.dto.PartyElectionTrendsReportVO;
+import com.itgrids.partyanalyst.dto.PartyPositionResultsVO;
+import com.itgrids.partyanalyst.dto.PartyPositionVO;
 import com.itgrids.partyanalyst.dto.PartyResultsVO;
 import com.itgrids.partyanalyst.dto.PartyResultsVerVO;
 
@@ -15,5 +19,7 @@ public interface IStratagicReportsService {
 	public PartyResultsVerVO getZptcMptcResultsOfConstituency(Long constiutencyId);
 	public PartyResultsVerVO getMuncipalCorpPrevResultsInGHMC(Long constiutencyId);
 	public PartyResultsVerVO getMuncipalCorpPrevResults(Long constiutencyId);
+	
+	public PartyPositionResultsVO getPartyChanges(Long constituencyId,List<Long> assemblyEleIdsList,List<Long> partiesSelected);
 
 }
