6d78548be03ce8dc798f1091574c14328258b1b4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-19 12:38:00 +0000

    Updated By Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStratagicReportServiceForMLASuccess.java
@@ -0,0 +1,19 @@
+package com.itgrids.partyanalyst.service;
+
+import com.itgrids.partyanalyst.dto.CasteStratagicReportVO;
+import com.itgrids.partyanalyst.dto.HouseHoldsVO;
+import com.itgrids.partyanalyst.dto.VoterStratagicReportVo;
+
+public interface IStratagicReportServiceForMLASuccess {
+
+	public HouseHoldsVO getHouseHoldInfoByConstituency(Long userId,Long constituencyId,Long publicationDateId);
+	
+	public VoterStratagicReportVo getVotersInfoByConstituency(Long userId,Long constituencyId,Long publicationDateId);
+	
+	public VoterStratagicReportVo getFirstTimeVotersInfoByConstituency(Long userId,Long constituencyId,Long publicationDateId);
+	
+	public VoterStratagicReportVo getAgeWiseVotersInfoByConstituency(Long userId,Long constituencyId,Long publicationDateId);
+	
+	public CasteStratagicReportVO getCasteWiseVotersInfoByConstituency(Long userId,Long constituencyId,Long publicationDateId);
+
+}
