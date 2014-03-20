f4b1ddf50d900416e0cd894574282ae9c5d7b4ac
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-20 13:06:21 +0000

    STRATAGIC REPORTS PDF TASK -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/DelimitationEffectVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/DelimitationEffectVO.java
@@ -20,6 +20,30 @@ public class DelimitationEffectVO implements Serializable{
 	private List<DelimitationEffectVO> delimitationEffectVO;
 	private List<DelimitationEffectVO> presentElections;
 	private List<DelimitationEffectVO> pervElections;
+	
+	private String heading1;
+	private String heading2;
+	private String heading3;
+	
+	
+	public String getHeading1() {
+		return heading1;
+	}
+	public void setHeading1(String heading1) {
+		this.heading1 = heading1;
+	}
+	public String getHeading2() {
+		return heading2;
+	}
+	public void setHeading2(String heading2) {
+		this.heading2 = heading2;
+	}
+	public String getHeading3() {
+		return heading3;
+	}
+	public void setHeading3(String heading3) {
+		this.heading3 = heading3;
+	}
 	public Long getPresentCount() {
 		return presentCount;
 	}
