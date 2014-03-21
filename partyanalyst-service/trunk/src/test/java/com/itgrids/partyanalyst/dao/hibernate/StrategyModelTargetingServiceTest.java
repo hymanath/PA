5604ae8b0297a5d3a9c6ba4c6ce6faa61286a520
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-21 08:13:51 +0000

    updated by mahesh

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/StrategyModelTargetingServiceTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/StrategyModelTargetingServiceTest.java
@@ -45,6 +45,6 @@ public class StrategyModelTargetingServiceTest  extends BaseDaoTestCase{
 		castePercents.put(189l,0.8f);
 		castePercents.put(0l,0.4f);
 		strategyVO.setCastePercents(castePercents);
-		strategyModelTargetingService.getPrioritiesToTarget(strategyVO);
+		strategyModelTargetingService.getPrioritiesToTarget(strategyVO,"");
 	}
 }
