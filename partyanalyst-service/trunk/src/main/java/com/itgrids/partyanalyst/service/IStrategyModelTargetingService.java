6891e71683c2c9473f435220884ab37f6b064b53
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-03-23 11:40:59 +0000

    Strategy report changes by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStrategyModelTargetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStrategyModelTargetingService.java
@@ -1,7 +1,43 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itgrids.partyanalyst.dto.ImpFamilesVO;
+import com.itgrids.partyanalyst.dto.OrderOfPriorityVO;
+import com.itgrids.partyanalyst.dto.PanchayatVO;
+import com.itgrids.partyanalyst.dto.PartyEffectVO;
+import com.itgrids.partyanalyst.dto.PartyPositionVO;
 import com.itgrids.partyanalyst.dto.StrategyVO;
 
 public interface IStrategyModelTargetingService {
-	public void getPrioritiesToTarget(StrategyVO strategyVO,String path);
+	public List<Object> getPrioritiesToTarget(StrategyVO strategyVO,String path);
+	
+	public void generateImpFamilesTable(Document document , List<ImpFamilesVO> list);
+	
+	public void generateCasteWiseTable(Document document,Map<String,Float> casteNamePercMap);
+	
+	public void panchayatwisePartyPerformanceTable(Document document,List<PartyPositionVO> panchayatList,Long rank);
+	
+	public void panchayatWiseTargetVotesTable(Document document,List<PanchayatVO> totalCastesList);
+	 
+	public void panchayatWiseTargetYoungVotesTable(Document document,List<PanchayatVO> totalCastesList,String type);
+	 
+	public void prpEffectTableTable(Document document,List<PartyEffectVO> list);
+	
+	public void generatePdfForMatrixReport(Document document,List<PartyPositionVO>  previousTrends);
+	
+	public void orderOFPriorityTable(Document document,List<OrderOfPriorityVO> list,int count);
+	
+	public void buildPanchayatsClassificationBlock(Document document, List<OrderOfPriorityVO>  panchayatsClassific);
+	
+	public void buildChart(Document doc);
+	
+	public void buildChartForPartyPerformanceReort(Document doc,List<PartyPositionVO> list);
+	
+	public void buildPiChart(Document document,List<OrderOfPriorityVO> panchayatsClassification,PdfWriter writer);
+	
+	
 }
