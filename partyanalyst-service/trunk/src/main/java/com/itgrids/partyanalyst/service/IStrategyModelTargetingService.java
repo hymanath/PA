b6616af912a6b739b93e072af0f09d69aade89a6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-01 15:08:55 +0000

    Question Rating Section -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStrategyModelTargetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IStrategyModelTargetingService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itgrids.partyanalyst.dto.EffectedBoothsResponse;
 import com.itgrids.partyanalyst.dto.ImpFamilesVO;
 import com.itgrids.partyanalyst.dto.OrderOfPriorityVO;
 import com.itgrids.partyanalyst.dto.PanchayatVO;
@@ -42,6 +43,8 @@ public interface IStrategyModelTargetingService {
 	public void getTopPanchayats(StrategyVO strategyVO,Document document,PdfWriter writer);
 	
 	 public void buildChartForPartyPerformanceReort(Document document,List<PartyPositionVO> list,PdfWriter writer,String heading);
+	 
+	 public EffectedBoothsResponse getPanchayatCategoriesForInfectedBooths(Long constituencyId);
 	 
 	 public List<Object> getCriticalPanchayats(Long constituencyId);
 }
