4cadfadcddf7554de473ea43ed1db0187f570058
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-01 15:08:44 +0000

    Question Rating Section -- SASI

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IInfectedBoothsService.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on October 2, 2009
+ */
+package com.itgrids.partyanalyst.service;
+
+/*
+ * 	This Service is for Getting Infected Booths for Selected Constituency
+ * 	@Author	SASI
+ * 	@Since	28th March 2014
+ * 	 	
+ * */
+public interface IInfectedBoothsService {
+	
+	
+}
