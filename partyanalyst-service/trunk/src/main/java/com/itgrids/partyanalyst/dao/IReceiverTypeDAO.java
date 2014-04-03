86ea867ddb264346bbc1153e2d2abcf06c6d1e73
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-03 15:49:39 +0000

    Updated by Sravanthi and Nagamani for Information Monitoring System

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IReceiverTypeDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.io.Serializable;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ReceiverType;
+
+public interface IReceiverTypeDAO extends GenericDao<ReceiverType, Serializable>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserSmsSentDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.UserSmsSent;
+
+public interface IUserSmsSentDAO extends GenericDao<UserSmsSent, Long>{
+
+}
