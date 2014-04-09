5dfd8a72c371d3f92bb5aefbbdf634bce33714dd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-09 14:01:24 +0000

    updated by Divya

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICasteContainConstituency.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import java.util.Set;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.CasteContainConstituency;
+import com.itgrids.partyanalyst.model.VoterCastInfo;
+
+
+public interface ICasteContainConstituency extends GenericDao<CasteContainConstituency,Long>{
+	
+}
