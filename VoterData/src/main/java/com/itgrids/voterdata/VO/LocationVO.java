b79eca1d38986c1054eb01dd674ad03473de2328
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-16 06:58:51 +0000

    Updated by Kamalakar

---
+++ VoterData/src/main/java/com/itgrids/voterdata/VO/LocationVO.java
@@ -0,0 +1,21 @@
+package com.itgrids.voterdata.VO;
+
+public class LocationVO {
+
+	private String location;
+	private String houseNo = "";
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	
+}
