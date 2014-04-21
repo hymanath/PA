6abdd1f83b4545a7149166a2ddc30a82d652f82b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-21 22:09:30 +0000

    Updated by Kamalakar

--- VoterData/src/main/java/com/itgrids/voterdata/util/IConstants.java
+++ VoterData/src/main/java/com/itgrids/voterdata/util/IConstants.java
@@ -2,17 +2,17 @@ package com.itgrids.voterdata.util;
 
 public interface IConstants {
 
-	public static final String VOTER_DATA_SITE_URL = "http://123.176.47.55/DraftRolls/Rolls.aspx";
+	public static final String VOTER_DATA_SITE_URL = "http://ceoaperms.ap.gov.in/Electoral_Rolls/Rolls.aspx";
 	public static final String FIREFOX_PATH = "C:/Program Files/Mozilla Firefox/firefox.exe";
-	public static final String DOWNLOAD_DIRECTORY = "F:\\VoterData\\PDF\\karimnagar";
-	public static final Boolean IS_MISSED_FILES_DOWNLOAD = false;
+	public static final String DOWNLOAD_DIRECTORY = "D:\\Kamal\\Hyderabad\\Bahadoorpura";
+	public static final Boolean IS_MISSED_FILES_DOWNLOAD = true;
 	public static int PATTERN = 4;
 	public static final String ADDED = "Added";
 	public static final String DELETED = "Deleted";
 	public static Boolean CONSIDER_DUPLICATES = true;
 	public static final String DB_NAME = "ac234";
 	public static final String CONSTITUENCY_NAME = "Atmakur";
-	public static final String DISTRICT_IDS ="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23";
+	public static final String DISTRICT_IDS ="1,";
 	
 	
 }
