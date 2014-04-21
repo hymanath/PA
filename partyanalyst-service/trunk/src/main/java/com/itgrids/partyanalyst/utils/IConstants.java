3b1ccc4ab6e7ab16522b65f0155f5cd24c90ea26
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-04-21 22:26:56 +0000

    Updated by Kamalakar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -741,7 +741,6 @@ public interface IConstants {
     public static final String NORMAL="NORMAL";
     public static final String FORWARD="FORWARD";
     public static final String RESEND="RESEND";
-   public static final String DISTRICT_IDS ="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23";
    // public static final String DISTRICT_IDS ="1";
     
 }
