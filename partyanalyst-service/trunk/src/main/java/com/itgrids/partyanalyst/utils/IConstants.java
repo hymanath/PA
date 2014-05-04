1be34fd92441966665aec977759524be3ca6d5ec
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-04 11:59:29 +0000

    SMS SERVER USERNAME PWD UPDATE -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -520,8 +520,11 @@ public interface IConstants {
 	public static final String LOGOUT = "Logout";
 	public static final String PROBLEM_FILES = "Problem_Files";
 	
-	public static final String ADMIN_USERNAME_FOR_SMS = "partyanalyst";
-	public static final String ADMIN_PASSWORD_FOR_SMS = "dakaV@ram";
+	//public static final String ADMIN_USERNAME_FOR_SMS = "partyanalyst";
+	public static final String ADMIN_USERNAME_FOR_SMS = "koffeemedia";
+	//public static final String ADMIN_PASSWORD_FOR_SMS = "dakaV@ram";
+	public static final String ADMIN_PASSWORD_FOR_SMS = "9885410393";
+	
 	public static final String ADMIN_SENDERID_FOR_SMS = "16242";
 	
 	public static final String DEFAULT_MAIL_SERVER = "server";
