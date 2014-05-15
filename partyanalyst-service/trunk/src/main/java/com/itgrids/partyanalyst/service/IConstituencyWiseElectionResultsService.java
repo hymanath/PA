8422726771dbbbb79d73176e7bc8facc0d6f16b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-15 08:59:52 +0000

    updated by sreedhar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyWiseElectionResultsService.java
@@ -0,0 +1,5 @@
+package com.itgrids.partyanalyst.service;
+
+public interface IConstituencyWiseElectionResultsService {
+	public Object constituencyResults(Long stateNo,Long level,Long constituencyNo,String description);
+}
