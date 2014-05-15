9872c38ab20b92c1b579fdf5f2c06a82a6a0a9c2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-15 15:02:57 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/StateSubRegionDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IStateSubRegionDAO;
+
+public class StateSubRegionDAOHibernateTest extends BaseDaoTestCase{
+	
+	private IStateSubRegionDAO stateSubRegionDAO;
+
+	public void setStateSubRegionDAO(IStateSubRegionDAO stateSubRegionDAO) {
+		this.stateSubRegionDAO = stateSubRegionDAO;
+	}
+	
+	
+}
---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/StateSubRegionDistrictDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IStateSubRegionDistrictDAO;
+
+public class StateSubRegionDistrictDAOHibernateTest extends BaseDaoTestCase{
+	private IStateSubRegionDistrictDAO stateSubRegionDistrictDAO;
+
+	public void setStateSubRegionDistrictDAO(
+			IStateSubRegionDistrictDAO stateSubRegionDistrictDAO) {
+		this.stateSubRegionDistrictDAO = stateSubRegionDistrictDAO;
+	}
+	
+	
+}
