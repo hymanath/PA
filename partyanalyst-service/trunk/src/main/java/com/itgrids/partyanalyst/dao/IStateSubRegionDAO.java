b42b922f0e2eb7836a907019a36e5dfca251d702
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-15 15:02:14 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStateSubRegionDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.StateSubRegion;
+
+public interface IStateSubRegionDAO extends GenericDao<StateSubRegion, Long>{
+
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStateSubRegionDistrictDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.StateSubRegionDistrict;
+
+public interface IStateSubRegionDistrictDAO extends GenericDao<StateSubRegionDistrict, Long>{
+
+}
