41715de06a388ca2cd63917832996f5ee7374ee5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-16 00:56:42 +0000

    UPDATED BY  - PRASAD

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAcPcWiseElectionResultService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAcPcWiseElectionResultService.java
@@ -3,9 +3,12 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.BasicVO;
+import com.itgrids.partyanalyst.dto.GenericVO;
 
 public interface IAcPcWiseElectionResultService 
 {
 
 	public List<BasicVO> getPartyWiseComperassionResult(Long stateId,Long electionId,List<Long> partyIds,Long electionScopeId,String scope);
+	
+	public List<GenericVO> cbnOrModiEffect(Long electionId,Long stateid,Long partyId,Long electionScopeId);
 }
