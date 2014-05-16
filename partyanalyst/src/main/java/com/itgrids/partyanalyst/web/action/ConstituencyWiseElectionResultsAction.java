ba54645fce5730a4b8ae1ac14005c30177549af3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-16 01:35:12 +0000

    updated by anil

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/ConstituencyWiseElectionResultsAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/ConstituencyWiseElectionResultsAction.java
@@ -1,14 +1,20 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.Properties;
+import java.util.Timer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.itgrids.partyanalyst.service.IConstituencyWiseElectionResultsService;
+import com.itgrids.partyanalyst.service.impl.ConstituencyWiseElectionResultsService;
 import com.itgrids.partyanalyst.utils.IConstants;
+import com.itgrids.partyanalyst.utils.MultiThreadRunner;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -27,8 +33,9 @@ public class ConstituencyWiseElectionResultsAction extends ActionSupport impleme
 	}
 	
     IConstituencyWiseElectionResultsService constituencyWiseElectionResultsService; 
-		
-
+    
+  @Autowired	
+  private  MultiThreadRunner multiThreadRunner; 
 	
 	
 	public void setConstituencyWiseElectionResultsService(
@@ -75,18 +82,18 @@ public class ConstituencyWiseElectionResultsAction extends ActionSupport impleme
 			else
 				return ERROR;
 		   String statNo=request.getParameter("stateNo");
-		   Long stateNo=0l;
+		  /* Long stateNo=0l;
 		   if(statNo.trim().length()!=0)
 		   {
 			   stateNo=Long.parseLong(statNo);
 			   
-		   }
+		   }*/
 		    
 		    Long Level=Long.parseLong(request.getParameter("Level"));
 		    Long constituencyNo=Long.parseLong(request.getParameter("constituencyNo"));
 		    String description=request.getParameter("description");
 		   
-		    constituencyWiseElectionResultsService.constituencyResults(stateNo,Level,constituencyNo,description);
+		    constituencyWiseElectionResultsService.constituencyResults(statNo,Level,constituencyNo,description);
 		    
 		    
 		
@@ -117,12 +124,47 @@ public class ConstituencyWiseElectionResultsAction extends ActionSupport impleme
 			else
 				return ERROR;
 			String givenPassword=request.getParameter("password");
-			String takenPassword="ssssss";
+			String takenPassword="rak@start";
 			Long status=Long.parseLong(request.getParameter("status"));
 			Long level=Long.parseLong(request.getParameter("level"));
 			
 			if(givenPassword.equalsIgnoreCase(takenPassword))
 			{
+				/* runner.startTask(2, 5,"parliament");
+			        Thread.sleep(10000);
+			        //timer.cancel();
+			        System.out.println("now");
+			        //timer=new Timer();
+			        
+			        runner.startTaskForParliament(2, 5,"hello");
+			        Thread.sleep(10000);
+			        timer.cancel();*/
+				
+			Properties prop=	ConstituencyWiseElectionResultsService.loadProperties();
+			int period=Integer.valueOf(prop.get("periodInterval").toString()) ;
+			int period1=Integer.valueOf(prop.get("periodIntervalForParliament").toString()) ;
+				if(level.equals(1L)) //assembly
+				{
+					if(status.equals(1L))
+					{ 
+						multiThreadRunner.timer=new Timer();
+						multiThreadRunner.startTask(2, period,"Constituency");
+					}else if(status.equals(2L))
+					{
+						multiThreadRunner.timer.cancel();
+					}
+				
+				}else if(level.equals(2l))//parliament
+				{
+					
+					if(status.equals(1L))
+					{   multiThreadRunner.timer1=new Timer();
+						multiThreadRunner.startTaskForParliament(2, period1,"Parliament");
+					}else if(status.equals(2L))
+					{
+						multiThreadRunner.timer1.cancel();
+					}
+				}
 				
 				
 				
