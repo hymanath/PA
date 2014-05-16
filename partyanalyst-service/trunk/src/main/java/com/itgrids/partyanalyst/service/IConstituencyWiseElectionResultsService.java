449531c0052545b5ba1326374dff43d086b0d40b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-16 01:32:59 +0000

    updated by anil

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyWiseElectionResultsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IConstituencyWiseElectionResultsService.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.service;
 
 public interface IConstituencyWiseElectionResultsService {
-	public Object constituencyResults(Long stateNo,Long level,Long constituencyNo,String description);
+	public Object constituencyResults(String stateNo,Long level,Long constituencyNo,String description);
+	public void bulkDataInsertUsingRemotingWebserviceCall(String insertFor);
+    public void bulkDataInsertUsingRemotingWebserviceCallParliament(String insertFor,Long stateId);
+
 }
