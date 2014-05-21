e136a6975eadaaad4a3ea66d21b1293b8dcdf1d8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-21 07:24:35 +0000

    updated by srisailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAcPcWiseElectionResultService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAcPcWiseElectionResultService.java
@@ -11,4 +11,9 @@ public interface IAcPcWiseElectionResultService
 	public List<BasicVO> getPartyWiseComperassionResult(Long stateId,Long electionId,List<Long> partyIds,Long electionScopeId,String scope);
 	
 	public List<GenericVO> cbnOrModiEffect(Long electionId,Long stateid,List<Long> partyId,Long electionScopeId);
+	
+	public List<BasicVO> filterToGetPartyWiseComperassionResult(Long stateId,Long electionId,List<Long> partyIds,Long electionScopeId,String scope,List<Long> subRegionId);
+	
+	public List<BasicVO> searchPartyWiseComparissionResult(Long stateId,Long electionId,List<Long> partyIds,Long electionScopeId,String scope,List<Long> subRegionId,String searchName);
+	
 }
