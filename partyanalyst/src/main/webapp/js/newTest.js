20bfe54fb415035a88e2075d3e7b2441593ea004
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-21 13:48:02 +0000

    election results page changes by mahesh

--- partyanalyst/src/main/webapp/js/newTest.js
+++ partyanalyst/src/main/webapp/js/newTest.js
@@ -158,7 +158,7 @@ var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats");
  }
  else
  {
-	var no_res_msg = '<div style="height:150px;padding:10px 0;font-size:14px;font-weight:bold">Analysis for this constituency is not available</div>';
+	var no_res_msg = '<div style="height:150px;padding:10px 0;font-size:14px;font-weight:bold">Data not available</div>';
 	document.getElementById('unemp_chart').innerHTML = no_res_msg;
  }
 
