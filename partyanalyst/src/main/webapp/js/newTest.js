ae88d82c00226bc991ce454b1b4c84bf1f21f993
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-21 14:48:10 +0000

    election results page changes by mahesh

--- partyanalyst/src/main/webapp/js/newTest.js
+++ partyanalyst/src/main/webapp/js/newTest.js
@@ -11,8 +11,8 @@ var width = width2;
  {
 	  var Needle, arc, arcEndRad, arcStartRad, barWidth, chart, chartInset, degToRad, el, endPadRad, margin, needle, numSections, padRad, percToDeg, percToRad, percent, radius, sectionIndx, sectionPerc, startPadRad, svg, totalPercent, _i;
 	 // var lvl_arr = new Array("115","135","155","175","195");
-var lvl_arr = new Array("No Modi wave","Modi wave","Modi High tide","Modi Tsunami");
-var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats"); 
+var lvl_arr = new Array("No MODI Wave","MODI Wave","MODI High Tide","MODI Tsunami");
+var seat_arr = new Array("115 Seats","175 Seats","275 Seats","350 Seats"); 
 	  percent = un_emp;
 
 	  barWidth = 40;
@@ -36,7 +36,7 @@ var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats");
 		left: 0
 	  };
 
-	  height = 170;
+	  height = 220;
 
 	  radius = width/2 ;
 
@@ -51,11 +51,11 @@ var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats");
 	  degToRad = function(deg) {
 		return deg * Math.PI / 180;
 	  };
-		var fontsz = 12+"px";
+		var fontsz = 14+"px";
 		if(width < 300){
 			fontsz = 9;
 		}
-	  svg = el.append('svg').attr('width', (width+10) + margin.left + margin.right).attr('height', (height+35) + margin.top + margin.bottom);
+	  svg = el.append('svg').attr('width', (width+60) + margin.left + margin.right).attr('height', (height+70) + margin.top + margin.bottom);
 
 	  chart = svg.append('g').attr('transform', "translate(" + ((width + margin.left) /2) + ", " + ((height + margin.top)) + ")");
 	  for (sectionIndx = _i = 1; 1 <= numSections ? _i <= numSections : _i >= numSections; sectionIndx = 1 <= numSections ? ++_i : --_i) {
@@ -70,7 +70,7 @@ var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats");
 		chart.append("text")
 		
 	  .style("font-size",fontsz)
-	  .style("font-family","sans-serif")
+	  .style("font-family","cursive")
 	  .style("font-weight","bold")
 	  .attr("fill","black")
 	  .attr("dx","15")
@@ -82,10 +82,11 @@ var seat_arr = new Array("115 seats","175 seats","275 seats","350 seats");
 	   .style("color","red");
 	  
 	   chart.append("text").style("font-size",fontsz)
+	   .style("font-family","cursive")
        .style("font-weight","bold")
 		.attr("fill","black")
 		.attr("dx","38")
-		.attr("dy","1")
+		.attr("dy","0")
 		.append("textPath")
 		.attr("xlink:href","#chart-color" + sectionIndx)
 		.text(seat_arr[sectionIndx-1])
