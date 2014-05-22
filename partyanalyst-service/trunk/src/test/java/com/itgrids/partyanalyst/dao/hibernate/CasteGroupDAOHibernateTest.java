8fb442f8a44d96dc61f394237856acf1c13be0eb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-22 13:21:27 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CasteGroupDAOHibernateTest.java
@@ -0,0 +1,21 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICasteGroupDAO;
+
+public class CasteGroupDAOHibernateTest extends BaseDaoTestCase{
+	
+	private ICasteGroupDAO casteGroupDAO;
+
+	
+	public void setCasteGroupDAO(ICasteGroupDAO casteGroupDAO) {
+		this.casteGroupDAO = casteGroupDAO;
+	}
+	
+	public void test()
+	{
+		
+	}
+
+}
---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ConstiCasteGroupPercDAOHibernateTest.java
@@ -0,0 +1,24 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.dao.IConstiCasteGroupPercDAO;
+import com.itgrids.partyanalyst.model.ConstiCasteGroupPerc;
+
+public class ConstiCasteGroupPercDAOHibernateTest extends BaseDaoTestCase{
+
+	
+	private IConstiCasteGroupPercDAO constiCasteGroupPercDAO;
+
+	public void setConstiCasteGroupPercDAO(
+			IConstiCasteGroupPercDAO constiCasteGroupPercDAO) {
+		this.constiCasteGroupPercDAO = constiCasteGroupPercDAO;
+	}
+	
+	
+	public void test()
+	{
+		
+	}
+}
