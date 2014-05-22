3862c37ddbf3e06c6e687424c366cfc4015de970
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-22 15:01:10 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStateSubRegionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IStateSubRegionDAO.java
@@ -1,10 +1,15 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.StateSubRegion;
 
 public interface IStateSubRegionDAO extends GenericDao<StateSubRegion, Long>{
 
+	public List<Object[]> getStateRegionsBySubRegionIds(List<Long> regionIds);
+	
+	public List<Object[]> getStateSubRegionsByRegionId(Long stateRegionId);
 	
 }
