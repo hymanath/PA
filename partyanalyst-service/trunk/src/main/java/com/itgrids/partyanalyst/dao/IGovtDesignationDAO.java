e7f20e9b7f711a0545943617cbf0ca4e54f98065
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-24 06:23:37 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtDesignationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.GovtDesignation;
+
+public interface IGovtDesignationDAO extends GenericDao<GovtDesignation, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyDesignationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.PartyDesignation;
+
+public interface IPartyDesignationDAO extends GenericDao<PartyDesignation, Long>{
+
+}
