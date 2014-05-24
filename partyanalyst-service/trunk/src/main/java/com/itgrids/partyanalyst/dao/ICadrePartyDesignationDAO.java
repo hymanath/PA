42de181f640d5ca9619e420a462d480854d56d3d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-24 14:05:51 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadrePartyDesignationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadrePartyDesignationDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadrePartyDesignation;
 
 public interface ICadrePartyDesignationDAO extends GenericDao<CadrePartyDesignation, Long>{
-
+	public List<Object[]> getPartyDesignationsByCadreId(Long cadreId);;
 }
