96cb2fa2b364c2bccfb669cc3422ac12f5216042
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-24 06:23:37 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISourceIncomeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.SourceIncome;
+
+public interface ISourceIncomeDAO extends GenericDao<SourceIncome, Long>{
+
+}
