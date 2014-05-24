166f9ce86d13451d31322f3d5825207c76069f92
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-24 09:06:15 +0000

    updated by mahesh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahaNaduService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahaNaduService.java
@@ -5,5 +5,13 @@ import java.util.List;
 import com.itgrids.partyanalyst.dto.SelectOptionVO;
 
 public interface IMahaNaduService {
-	public List<SelectOptionVO> getBoothsInAConstituency(Long constituencyId,Long publicationID);
+	 public List<SelectOptionVO> getBoothsInAConstituency(Long constituencyId,Long publicationID);
+	
+	 public List<SelectOptionVO> getIncomeSources();
+	
+	 public List<SelectOptionVO> getCasteCategories();
+	 
+	 public List<SelectOptionVO> getPartyDesignations();
+	 
+	 public List<SelectOptionVO> getgovernmentDesignations();
 }
