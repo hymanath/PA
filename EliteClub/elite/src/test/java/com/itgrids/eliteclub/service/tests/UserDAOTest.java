16598f43a93fed7520f6a0c083b897d0dfbfafb1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-27 07:32:44 +0000

    updated by anil

--- EliteClub/elite/src/test/java/com/itgrids/eliteclub/service/tests/UserDAOTest.java
+++ EliteClub/elite/src/test/java/com/itgrids/eliteclub/service/tests/UserDAOTest.java
@@ -1,5 +1,20 @@
 package com.itgrids.eliteclub.service.tests;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -13,6 +28,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.itgrids.eliteclub.dao.UserDAO;
 import com.itgrids.eliteclub.model.User;
+import com.mysql.jdbc.Buffer;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -51,7 +67,148 @@ public class UserDAOTest {
     System.out.println(u.getUserId());
     	
     }
+    public static void main(String[] args) throws IOException {
+		List<Integer> al =new ArrayList<Integer>();
+    	for(int i=1;i<=150;i++)
+    	{
+    		try {
+    		main(i);
+    		}catch (Exception e) {
+    			e.printStackTrace();
+    			al.add(i);
+			}
+    	}
+    	for (Integer integer : al) {
+			System.out.println(integer);
+		}
+	}
     
+    public static void main(int j) throws IOException {
+		
+    	
+    	//String url="http://affidavitarchive.nic.in/DynamicAffidavitDisplay/CANDIDATEAFFIDAVIT.aspx?YEARID=May-2014 ( GEN )&AC_No=84&st_code=S01&constType=AC";
+   	 /* String k="0";
+    	if(j>=100)
+   		   k=0+""+(j-100);
+    	else*/
+    		String k=j+"";
+    		if(k.length()>2)
+    			k=k.substring(1, k.length());
+    		
+    	String url="http://eciresults.ap.nic.in/ConstituencywiseS121"+k+".htm?ac="+j;
+   	    System.out.println(url);
+   	    InputStream in = UserDAOTest.class.getClassLoader().getResourceAsStream("electionresults2014.properties");
+    	Properties prop = new  Properties();
+    	prop.load(in);
+    	
+    	String url1=(String) prop.get("url");    	
+    	System.out.println(url1);
+    	System.out.println( prop.get("stateNo"));
+    	System.out.println( prop.get("electionId"));
+    	
+   	  
+   	 
+   	  
+   	  HttpClient client = null;
+    
+         GetMethod get=null;
+    		client = new HttpClient(new MultiThreadedHttpConnectionManager());
+
+    		client.getHttpConnectionManager().getParams().setConnectionTimeout(Integer.parseInt("30000"));
+    		get=new GetMethod(url);
+    		
+    		try {
+    		int status=	client.executeMethod(get);
+    		System.out.println(status);
+    		}catch (Exception e) {
+    			e.printStackTrace();
+			}
+    		
+    		//System.out.println(get.getResponseBodyAsString());
+    		
+    		//String sb=get.getResponseBodyAsString();
+    		
+    	InputStream fis=	get.getResponseBodyAsStream();
+    	BufferedReader br= new BufferedReader(new InputStreamReader(fis));
+    	StringBuffer sb =new StringBuffer();
+    	
+    	String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+         
+            line = br.readLine();
+        }
+    	
+    		
+    		get=null;
+    		
+    		 String patern="<div id=\"div1\"[\\s\\S]*?<table.*?>(<tr.*?>(<td.*?>.*</td>)*</tr>)</table>"; 
+    		 String pattern2="(.*)<td.*?>([\\s\\S]+?)</td>(.*)";
+    		 
+    		 Pattern pat =    Pattern.compile(patern, Pattern.CASE_INSENSITIVE);
+ 			Matcher matcher=pat.matcher(sb);
+ 			String str=null;
+ 			 while (matcher.find()) {
+ 			   System.out.println("inside matcher==================");
+ 			
+ 			 //  System.out.println(matcher.group(0).toString());
+ 		//	System.out.println("===="+  matcher.group(1).toString());
+ 			//System.out.println("======="+matcher.group(2).toString());
+ 		//	System.out.println("======="+matcher.group(3).toString());
+ 				// System.out.println(matcher.group(3));
+ 			str=matcher.group(2).toString();
+ 			    }
+ 			 if(str==null)
+ 				 return;
+ 			  Pattern pat1 =    Pattern.compile(pattern2, Pattern.CASE_INSENSITIVE);
+ 				Matcher matcher1=pat1.matcher(str);
+ 				
+ 				String[] strArray=str.split("<td.*?>");
+ 				
+ 				
+ 				System.out.println(strArray);
+ 				
+ 				 int flagCount=0;
+ 				for(int i=3;i<strArray.length;i++)
+ 				{
+ 					if(flagCount<=2) {
+ 						System.out.print("======="+strArray[i].toString());
+ 						
+ 						}
+ 						else {
+ 							System.out.println();
+ 							flagCount=-1;
+ 						}
+ 						flagCount++;
+ 				}
+ 				//System.exit(0);
+ 				
+ 				
+ 				/*int count=1;
+ 				
+ 				 int flagCount=0;
+ 				System.out.println(str);
+ 				 while (matcher1.find()) {
+ 			
+ 					  System.out.println("inside matcher1==================");
+ 					if(count>2)
+ 					{
+ 						if(flagCount<=2) {
+ 						System.out.print("======="+matcher1.group(1).toString());
+ 						
+ 						}
+ 						else {
+ 							System.out.println();
+ 							flagCount=0;
+ 						}
+ 						flagCount++;
+ 					}
+ 					count++;
+ 					    }*/
+ 				 System.out.println();
+    
+    }
     
     
 }
