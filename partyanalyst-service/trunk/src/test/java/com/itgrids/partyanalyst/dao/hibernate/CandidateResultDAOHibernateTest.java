7cec3a44b457068b1dd28e6b7dc36bf55f4ed722
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-05-30 08:22:14 +0000

    UPDATED BY  - PRASAD

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidateResultDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CandidateResultDAOHibernateTest.java
@@ -1,11 +1,16 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ICandidateResultDAO;
+import com.itgrids.partyanalyst.dto.GenericVO;
 
 
 public class CandidateResultDAOHibernateTest extends BaseDaoTestCase {
@@ -149,5 +154,199 @@ public class CandidateResultDAOHibernateTest extends BaseDaoTestCase {
 	}*/
 	
 	
+	
+	public void testCbnEffect()
+	{
+		cbnEffectCalucation();
+	}
+	public List<GenericVO> cbnEffectCalucation()
+	{
+		List<GenericVO> returnList = null;
+		
+		try
+		{
+			
+			Map<Long,GenericVO> mapFor2009 = null;
+			Map<Long,GenericVO> mapFor2009Win = null;
+			Map<Long,GenericVO> mapFor2009Loss = null;
+			
+			Map<Long,GenericVO> mapFor2012 = null;
+			Map<Long,GenericVO> mapFor2012Win = null;
+			Map<Long,GenericVO> mapFor2012Loss = null;
+			List<Object[]> resultFor2009Election = candidateResultDAO.getElectionResultForCbnEffect(38l, 872l);
+			System.out.println(resultFor2009Election.size());
+			if(resultFor2009Election != null && resultFor2009Election.size() > 0 )
+			{
+				 mapFor2009 = new HashMap<Long, GenericVO>();
+				 mapFor2009Win = new HashMap<Long, GenericVO>();
+				 mapFor2009Loss = new HashMap<Long, GenericVO>();
+				for (Object[] objects : resultFor2009Election)
+				{
+					GenericVO genericVO = new GenericVO();
+					genericVO.setId((Long)objects[0]);
+					genericVO.setPerc(Double.valueOf(objects[1].toString()));
+					genericVO.setRank((Long)objects[2]);
+					if((Long)objects[2] == 1l)
+					{
+						mapFor2009Win.put((Long)objects[0], genericVO);
+					}
+					else
+					{
+						mapFor2009Loss.put((Long)objects[0], genericVO);
+					}
+					mapFor2009.put((Long)objects[0], genericVO);
+				}
+			}
+			List<Object[]> resultFor2012Election = candidateResultDAO.getElectionResultForCbnEffect(258l, 872l);
+			System.out.println(resultFor2012Election.size());
+			if(resultFor2012Election != null && resultFor2012Election.size() > 0 )
+			{
+				 mapFor2012 = new HashMap<Long, GenericVO>();
+				 mapFor2012Win = new HashMap<Long, GenericVO>();
+				 mapFor2012Loss = new HashMap<Long, GenericVO>();
+				for (Object[] objects : resultFor2012Election)
+				{
+					GenericVO genericVO = new GenericVO();
+					genericVO.setId((Long)objects[0]);
+					genericVO.setPerc(Double.valueOf(objects[1].toString()));
+					genericVO.setRank((Long)objects[2]);
+					if((Long)objects[2] == 1l)
+					{
+						mapFor2012Win.put((Long)objects[0], genericVO);
+					}
+					else
+					{
+						mapFor2012Loss.put((Long)objects[0], genericVO);
+					}
+					mapFor2012.put((Long)objects[0], genericVO);
+				}
+			}
+			Map<Long,String> constiMap = new HashMap<Long, String>();
+		
+			
+			//1) 2009 LOST 			2014 LOST
+			
+			List<Long> firstRuleConstituencyes = new ArrayList<Long>(mapFor2009Loss.keySet());
+			firstRuleConstituencyes.addAll(new ArrayList<Long>(mapFor2012Loss.keySet()));
+			
+			Set<Long> firstIds = findDuplicates(firstRuleConstituencyes);
+			if(firstIds != null && firstIds.size() > 0)
+			{
+				for (Long constituencyId : firstIds)
+				{
+					Double avilPerc = mapFor2012Loss.get(constituencyId).getPerc().doubleValue() - mapFor2009Loss.get(constituencyId).getPerc().doubleValue();
+					if(avilPerc > 0.0)
+					{
+						constiMap.put(constituencyId, "CBN EFFECT");
+					}
+					else if (avilPerc == 0)
+					{
+						constiMap.put(constituencyId, "NEUTRAL");
+					}
+					else
+					{
+						constiMap.put(constituencyId, "NO CBN EFFECT");
+					}
+				}
+			}
+			
+			// 2) 2009 LOST 				2014 WON
+			List<Long> secondRuleConstituencyes = new ArrayList<Long>(mapFor2009Loss.keySet());
+			secondRuleConstituencyes.addAll(new ArrayList<Long>(mapFor2012Win.keySet()));
+			
+			Set<Long> secondIds = findDuplicates(secondRuleConstituencyes);
+			if(secondIds != null && secondIds.size() > 0)
+			{
+				for (Long constituencyId : secondIds)
+				{
+					Double avilPerc = mapFor2012Win.get(constituencyId).getPerc().doubleValue() - mapFor2009Loss.get(constituencyId).getPerc().doubleValue();
+					if(avilPerc > 0.0)
+					{
+						constiMap.put(constituencyId, "CBN EFFECT");
+					}
+					else
+					{
+						constiMap.put(constituencyId, "NEUTRAL");
+					}
+				}
+			}
+			
+			//3)2009 WON 				2014 LOST
+			List<Long> thirdRuleConstituencyes = new ArrayList<Long>(mapFor2009Win.keySet());
+			thirdRuleConstituencyes.addAll(new ArrayList<Long>(mapFor2012Loss.keySet()));
+			Set<Long> thirdIds = findDuplicates(thirdRuleConstituencyes);
+			if(thirdIds != null && thirdIds.size() > 0)
+			{
+				for (Long constituencyId : thirdIds)
+				{
+					Double avilPerc = mapFor2012Loss.get(constituencyId).getPerc().doubleValue() - mapFor2009Win.get(constituencyId).getPerc().doubleValue();
+					if(avilPerc > 0.0)
+					{
+						constiMap.put(constituencyId, "NEUTRAL");
+					}
+					else if (avilPerc == 0)
+					{
+						constiMap.put(constituencyId, "NEUTRAL");
+					}
+					else
+					{
+						constiMap.put(constituencyId, "NO CBN EFFECT");
+					}
+				}
+			}
+			
+			//2009 WON 				2014 WON
+			List<Long> fourthRuleConstituencyes = new ArrayList<Long>(mapFor2009Win.keySet());
+			fourthRuleConstituencyes.addAll(new ArrayList<Long>(mapFor2012Win.keySet()));
+			Set<Long> fourthIds = findDuplicates(fourthRuleConstituencyes);	
+			if(fourthIds != null && fourthIds.size() > 0)
+			{
+				for (Long constituencyId : fourthIds)
+				{
+					Double avilPerc = mapFor2012Win.get(constituencyId).getPerc().doubleValue() - mapFor2009Win.get(constituencyId).getPerc().doubleValue();
+					if(avilPerc > 0.0)
+					{
+						constiMap.put(constituencyId, "CBN EFFECT");
+					}
+					else 
+					{
+						constiMap.put(constituencyId, "NEUTRAL");
+					}
+					
+				}
+			}
+			
+			List<Long> constituencyes = new ArrayList<Long>(constiMap.keySet());
+			for (Long long1 : constituencyes) {
+				System.out.println(long1 +":"+ constiMap.get(long1));
+				//System.out.println(constiMap.get(long1));
+				//System.out.println("*********************");
+			}
+		}
+		
+		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			//LOG.error("Exception Raised In cbnEffectCalucation", e);
+		}
+		return returnList;
+	}
+	
+	
+	public Set<Long> findDuplicates(List<Long> listContainingDuplicates)
+	{ 
+	  final Set<Long> setToReturn = new HashSet<Long>(); 
+	  final Set<Long> set1 = new HashSet<Long>(); 
+
+	  for (Long yourInt : listContainingDuplicates)
+	  {
+	   if (!set1.add(yourInt))
+	   {
+	    setToReturn.add(yourInt);
+	   }
+	  }
+	  return setToReturn;
+	}
 	
 }
