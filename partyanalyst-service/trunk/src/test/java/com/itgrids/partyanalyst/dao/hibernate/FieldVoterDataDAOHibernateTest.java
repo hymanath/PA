0c87ec939f2b8d80032fb9375e15629ba000a49f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-10 11:07:17 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/FieldVoterDataDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IFieldVoterDataDAO;
+
+public class FieldVoterDataDAOHibernateTest extends BaseDaoTestCase{
+private IFieldVoterDataDAO fieldVoterDataDAO;
+
+public void setFieldVoterDataDAO(IFieldVoterDataDAO fieldVoterDataDAO) {
+	this.fieldVoterDataDAO = fieldVoterDataDAO;
+}
+
+
+
+}
