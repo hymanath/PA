e621d83e483688689ddcc3ed0edbd203fd302395
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-10 11:07:58 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVoterDataDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.FieldVoterData;
+
+public interface IFieldVoterDataDAO extends GenericDao<FieldVoterData, Long>{
+
+}
