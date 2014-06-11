6d9b85b8777de0eb3f1ee9d0c3f5662c3c2b24be
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-11 07:13:58 +0000

    updated by nagamani

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/FieldVoterDataAction.java
@@ -0,0 +1,96 @@
+package com.itgrids.partyanalyst.web.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.ICrossVotingEstimationService;
+import com.itgrids.partyanalyst.service.IVotersAnalysisService;
+import com.itgrids.partyanalyst.utils.IConstants;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FieldVoterDataAction extends ActionSupport implements ServletRequestAware{
+	private HttpServletRequest request;
+	private HttpSession session;
+	
+	private String task;
+	JSONObject jObj;
+	private List<SelectOptionVO> constituencyList, userAccessConstituencyList;
+	@Autowired
+	private ICrossVotingEstimationService crossVotingEstimationService;
+	@Autowired
+	private IVotersAnalysisService votersAnalysisService;
+	public List<SelectOptionVO> getConstituencyList() {
+		return constituencyList;
+	}
+	public void setConstituencyList(List<SelectOptionVO> constituencyList) {
+		this.constituencyList = constituencyList;
+	}
+	public List<SelectOptionVO> getUserAccessConstituencyList() {
+		return userAccessConstituencyList;
+	}
+	public void setUserAccessConstituencyList(
+			List<SelectOptionVO> userAccessConstituencyList) {
+		this.userAccessConstituencyList = userAccessConstituencyList;
+	}
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	public HttpSession getSession() {
+		return session;
+	}
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public JSONObject getjObj() {
+		return jObj;
+	}
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+	@Override
+	public void setServletRequest(HttpServletRequest arg0) {
+		this.request = arg0;
+	}
+	
+	
+	
+	public String execute()
+	{
+		HttpSession session = request.getSession();
+		RegistrationVO user=(RegistrationVO) session.getAttribute("USER");
+		if(user == null)
+		 return INPUT;
+		
+		constituencyList = user.getUserAccessVoterConstituencies();
+		if(constituencyList == null || constituencyList.isEmpty()){
+			Long userID = user.getRegistrationID();
+			Long electionYear = new Long(IConstants.PRESENT_ELECTION_YEAR);
+			Long electionTypeId = new Long(IConstants.ASSEMBLY_ELECTION_TYPE_ID);
+			userAccessConstituencyList = crossVotingEstimationService.getConstituenciesForElectionYearAndTypeWithUserAccess(userID,electionYear,electionTypeId);
+			constituencyList = votersAnalysisService.getConstituencyList(userAccessConstituencyList);
+			constituencyList.add(0, new SelectOptionVO(0L,"Select Constituency"));
+			user.setUserAccessVoterConstituencies(constituencyList);
+		}
+		return SUCCESS;
+		
+	}
+	
+}
