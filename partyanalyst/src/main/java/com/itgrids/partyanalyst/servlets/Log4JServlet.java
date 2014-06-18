100d64f86ac3b0a9470f6187f66e7ae2949ad514
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-18 08:51:09 +0000

    PMD Checker changes by mahesh

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/servlets/Log4JServlet.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/servlets/Log4JServlet.java
@@ -20,12 +20,12 @@ import org.apache.log4j.PropertyConfigurator;
 public class Log4JServlet extends HttpServlet {
 
 
-	  private static Logger logger = Logger.getLogger(Log4JServlet.class);
+	  private final static Logger LOG = Logger.getLogger(Log4JServlet.class);
 	
    public void init() throws ServletException{
-	   logger.debug("Debug: Log4jServlet initialized");
-	   logger.fatal("fatal: Log4jServlet initialized");
-	   logger.error("error: Log4jServlet initialized");
+	   LOG.debug("Debug: Log4jServlet initialized");
+	   LOG.fatal("fatal: Log4jServlet initialized");
+	   LOG.error("error: Log4jServlet initialized");
       String log4jfile = getInitParameter("log4j-init-file");
       if (log4jfile != null) {
          String propfile = getServletContext().getRealPath(log4jfile);
