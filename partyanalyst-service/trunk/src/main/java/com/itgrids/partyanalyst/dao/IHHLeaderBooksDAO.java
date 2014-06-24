8f19c46cd4ca4989b09ce3ed2b9bb1d395739b2c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-24 17:36:59 +0000

    HouseHolds Add Books & Assign Booths & Books -- SASI

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IHHLeaderBooksDAO.java
@@ -0,0 +1,21 @@
+
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.HHLeaderBooks;
+
+public interface IHHLeaderBooksDAO extends GenericDao<HHLeaderBooks, Long>{
+	public List<Long> getAllLeaderBooksInConstituency(Long leaderId,Long constituencyId);
+	public List<Object[]> getAllBooksInConstituency(Long constituencyId);
+	public List<Object[]> getAllBooksNotAssigned();
+	public Integer deleteLeaderWithBooks(List<Long> books,Long constituencyId);
+	public Integer updateLeaderForBooks(Long bookId,Long leaderId);
+	public List<Object[]> getAllLeaderBooksInConstituencyWithNo(Long leaderId,Long constituencyId);
+	public List<Object[]> getAllLeaderBooksInConstituencyWithNoOfBookIds(Long leaderId,Long constituencyId,List<Long> bookIds);
+	public List<Object[]> getAllBooksInConstituencyWithBookNo(Long constituencyId,Long bookNo);
+	public List<Object[]> getAllBooksNotAssignedWithBookNo(Long bookNo);
+	public List<Object[]> getBooksOfLeader(Long leaderId);
+}
