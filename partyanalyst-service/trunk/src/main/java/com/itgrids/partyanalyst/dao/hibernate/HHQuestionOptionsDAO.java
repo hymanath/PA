ec0368608a4ccc8e27acac1e44ea5ec12b998967
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-30 13:29:50 +0000

    HouseHolds Summary Reports -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HHQuestionOptionsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HHQuestionOptionsDAO.java
@@ -1,15 +1,33 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IHHQuestionOptionsDAO;
 import com.itgrids.partyanalyst.model.HHQuestionOptions;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 
 public class HHQuestionOptionsDAO extends GenericDaoHibernate<HHQuestionOptions,Long> implements IHHQuestionOptionsDAO {
 	
 	public HHQuestionOptionsDAO() {
 		super(HHQuestionOptions.class);
+	}
+	
+	public List<Object[]> getOptionsForQuestions(Long questionId){
+		Query query = getSession().createQuery(" select model.hhOptions.optionsId," +
+				" model.hhOptions.options," +
+				" model.hhSurveyQuestion.hhoptionType.optionTypeId " +
+				" from HHQuestionOptions model" +
+				" where model.hhOptions.isDelete =:delStatus " +
+				" and model.hhSurveyQuestion.surveyQuestionId =:questionId ");
+		
+		query.setParameter("delStatus", IConstants.FALSE);
+		query.setParameter("questionId", questionId);
+		
+		return query.list();
 	}
 	
 	
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyAnswersDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyAnswersDAO.java
@@ -35,5 +35,28 @@ public class HHSurveyAnswersDAO extends GenericDaoHibernate<HHSurveyAnswers,Long
 		
 	}
 	
+	public List<Object[]> getQuestionWiseSummary(Long questionId,Long constituencyId){
+		Query query = getSession().createQuery(" select model2.houseHolds.houseHoldId," + // HOUSEHOLD ID -- 0
+				" model.hhOptions.optionsId," + // 1 -- OPTION ID
+				"  model.hhOptions.options," + // 2 -- OPTION
+				"  model.hhSurveyQuestion.hhoptionType.optionTypeId," +// 3 -- OPTION TYPE
+				"  model.remarks," + // 4 -- COMMENT
+				"  model.hhSurveyQuestion.surveyQuestionId," + // 5 -- QUESTION ID
+				"  model.hhSurveyQuestion.question" + // 6 -- QUESTION
+				"  from HHSurveyAnswers model,HHBoothLeader model1,HouseHoldVoter model2" +
+				"  where" +
+				"  model.houseHold.houseHoldId = model2.houseHolds.houseHoldId " +
+				"  and model2.hhLeader.id = model1.hhLeader.id" +
+				"  and model1.constituency.constituencyId = :constituencyId" +
+				"  and model1.hhLeader.is_active = 'YES'" +
+				"  and model2.isDelete = 'FALSE'" +
+				"  and model.hhSurveyQuestion.surveyQuestionId =:questionId" +
+				"  group by model2.houseHolds.houseHoldId");
+		
+		query.setParameter("questionId", questionId);
+		query.setParameter("constituencyId", constituencyId);
+		return query.list();
+	}
+	
 	
 }
