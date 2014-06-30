ed183d1cae347096e39544c0e26cefeab1f14a43
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-30 13:30:14 +0000

    HouseHolds Summary Reports -- SASI

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyAnswerDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyAnswerDAOHibernateTest.java
@@ -11,15 +11,24 @@ public class HHSurveyAnswerDAOHibernateTest extends BaseDaoTestCase{
 	
 	private IHHSurveyAnswersDAO hhSurveyAnswersDAO;
 	
-	
-
 	public IHHSurveyAnswersDAO getHhSurveyAnswersDAO() {
 		return hhSurveyAnswersDAO;
 	}
 
+	public void setHhSurveyAnswersDAO(IHHSurveyAnswersDAO hhSurveyAnswersDAO) {
+		this.hhSurveyAnswersDAO = hhSurveyAnswersDAO;
+	}
+
+
+	/*public void test(){
+		List<HHSurveyAnswers> list=hhSurveyAnswersDAO.getSurveyAnswersByHouseHoldId(21l);
+		System.out.println(list.size());
+	}*/
+
+
 
 	public void test(){
-		List<HHSurveyAnswers> list=hhSurveyAnswersDAO.getSurveyAnswersByHouseHoldId(21l);
+		List<Object[]> list = hhSurveyAnswersDAO.getQuestionWiseSummary(2l, 282l);
 		System.out.println(list.size());
 	}
 
--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyQuestionDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/HHSurveyQuestionDAOHibernateTest.java
@@ -5,20 +5,30 @@ import java.util.List;
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IHHSurveyQuestionDAO;
-import com.itgrids.partyanalyst.model.HHSurveyQuestion;
 
 public class HHSurveyQuestionDAOHibernateTest extends BaseDaoTestCase{
 	
 	private IHHSurveyQuestionDAO hhSurveyQuestionDAO;
 
+	public IHHSurveyQuestionDAO getHhSurveyQuestionDAO() {
+		return hhSurveyQuestionDAO;
+	}
+
 	public void setHhSurveyQuestionDAO(IHHSurveyQuestionDAO hhSurveyQuestionDAO) {
 		this.hhSurveyQuestionDAO = hhSurveyQuestionDAO;
 	}
 
+	
 
 
-	public void test(){
+
+	/*public void test(){
 		List<HHSurveyQuestion> list=hhSurveyQuestionDAO.getModelBySurveyId(1l);
+		System.out.println(list.size());
+	}*/
+	
+	public void test(){
+		List<Object[]> list = hhSurveyQuestionDAO.getAllQuestionInSurvey(1l);
 		System.out.println(list.size());
 	}
 
