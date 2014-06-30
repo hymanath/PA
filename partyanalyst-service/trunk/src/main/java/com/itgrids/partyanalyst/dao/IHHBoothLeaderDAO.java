31cff233ca8861499c4493bcd2c89008d4024691
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-06-30 06:03:34 +0000

    HouseHolds Summary Reports -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IHHBoothLeaderDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IHHBoothLeaderDAO.java
@@ -14,4 +14,7 @@ public interface IHHBoothLeaderDAO extends GenericDao<HHBoothLeader, Long>{
 	public List<Object[]> getBoothsOfLeader(Long leaderId);
 	public List<Object[]> getLeadersOfConstituency(Long constituencyId);
 	public Integer deleteLeaderWithBooths(List<Long> boothIds,Long leaderId);
+	
+	public List<Object[]> getConstituenciesOfHouseHolds();
+	//public List<Object[]> getActiveLeadersOfConstituency(Long constituencyId);	
 }
