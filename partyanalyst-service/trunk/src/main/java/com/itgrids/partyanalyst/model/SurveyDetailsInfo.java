63f4e989f40bb946f67c7f4baa8438c6b400fb2b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-01 09:22:01 +0000

    CADRE SURVEY  - PRASAD

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/SurveyDetailsInfo.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/SurveyDetailsInfo.java
@@ -2,6 +2,22 @@ package com.itgrids.partyanalyst.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
 /**
  * 
  * @author Prasad Thiragabathina
@@ -9,6 +25,9 @@ import java.util.Date;
  * This Model Describes About Stores all survey details .
  *
  */
+@Entity
+@Table(name = "survey_details_info;")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class SurveyDetailsInfo
 {
 
@@ -29,42 +48,65 @@ public class SurveyDetailsInfo
 	private String latitude;
 	private Date insertedTime;
 	private Date updatedTime;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "survey_details_info;", unique = true, nullable = false)
 	public Long getSurveyDetailsInfoId() {
 		return surveyDetailsInfoId;
 	}
 	public void setSurveyDetailsInfoId(Long surveyDetailsInfoId) {
 		this.surveyDetailsInfoId = surveyDetailsInfoId;
 	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "survey_user_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public SurveyUser getSurveyUser() {
 		return surveyUser;
 	}
 	public void setSurveyUser(SurveyUser surveyUser) {
 		this.surveyUser = surveyUser;
 	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "survey_surveior_type_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public SurveySurveyorType getSurveySurveyorType() {
 		return surveySurveyorType;
 	}
 	public void setSurveySurveyorType(SurveySurveyorType surveySurveyorType) {
 		this.surveySurveyorType = surveySurveyorType;
 	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "voter_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public Voter getVoter() {
 		return voter;
 	}
 	public void setVoter(Voter voter) {
 		this.voter = voter;
 	}
+	
+	@Column(name="mobile_number")
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+	@Column(name="cadre")
 	public String getIsCadre() {
 		return isCadre;
 	}
 	public void setIsCadre(String isCadre) {
 		this.isCadre = isCadre;
 	}
+	@Column(name="influencing_people")
 	public String getIsInfluencingPeople() {
 		return isInfluencingPeople;
 	}
@@ -72,60 +114,85 @@ public class SurveyDetailsInfo
 		this.isInfluencingPeople = isInfluencingPeople;
 	}
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "caste_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public CasteState getCaste() {
 		return caste;
 	}
 	public void setCaste(CasteState caste) {
 		this.caste = caste;
 	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "hamlet_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public Hamlet getHamlet() {
 		return hamlet;
 	}
 	public void setHamlet(Hamlet hamlet) {
 		this.hamlet = hamlet;
 	}
+	
+	@Column(name="local_area")
 	public String getLocalArea() {
 		return localArea;
 	}
 	public void setLocalArea(String localArea) {
 		this.localArea = localArea;
 	}
+	
+	@Column(name="caste_name")
 	public String getCasteName() {
 		return casteName;
 	}
 	public void setCasteName(String casteName) {
 		this.casteName = casteName;
 	}
+	
+	@Column(name="hamelt_name")
 	public String getHamletName() {
 		return hamletName;
 	}
 	public void setHamletName(String hamletName) {
 		this.hamletName = hamletName;
 	}
+	
+	@Column(name="date")
 	public Date getDate() {
 		return date;
 	}
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	
+	@Column(name="longitude")
 	public String getLongitude() {
 		return longitude;
 	}
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
+	
+	@Column(name="latitude")
 	public String getLatitude() {
 		return latitude;
 	}
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
+	
+	@Column(name="inserted_time")
 	public Date getInsertedTime() {
 		return insertedTime;
 	}
 	public void setInsertedTime(Date insertedTime) {
 		this.insertedTime = insertedTime;
 	}
+	
+	@Column(name="updated_time")
 	public Date getUpdatedTime() {
 		return updatedTime;
 	}
