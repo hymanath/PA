3686029c9198ffb59c1a42a03c98a6a84b8a8d5e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-02 06:39:58 +0000

    included by anil for  caste  android sync

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/webservice/android/components/WebServiceHandler.java
@@ -0,0 +1,56 @@
+package com.itgrids.partyanalyst.webservice.android.components;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.itgrids.partyanalyst.dto.EffectedBoothsResponse;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.VoterDetailsVO;
+import com.itgrids.partyanalyst.dto.WSResultVO;
+import com.itgrids.partyanalyst.service.IWebServiceHandlerService;
+import com.itgrids.partyanalyst.webservice.utils.VoterTagVO;
+import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLoginVO;
+
+@Component
+@Path("/android/fieldData")
+public class WebServiceHandler {
+	
+	@Autowired
+	private IWebServiceHandlerService  webServiceHandlerService;
+	
+	
+	@POST
+	@Path("/loginFieldDataUser")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void loginFieldDataUser (UserLoginVO inputs)
+	{
+		
+		Map<String,String> userDetails= new HashMap<String, String>();
+		userDetails.put("","");
+		try{
+			
+		
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+	}
+	
+	
+}
