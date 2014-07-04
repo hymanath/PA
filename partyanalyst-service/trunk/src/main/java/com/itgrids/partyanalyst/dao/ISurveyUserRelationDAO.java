46c44586bd5fddfb9f39836dfb8eb351c775c69e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-04 11:58:57 +0000

    CADRE SURVEY  - PRASAD

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserRelationDAO.java
@@ -6,9 +6,12 @@ import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.SurveyUserRelation;
 
-public interface ISurveyUserRelationDAO  extends GenericDao<SurveyUserRelation, Long>{
+public interface ISurveyUserRelationDAO  extends GenericDao<SurveyUserRelation, Long>
+{
 
 	public List<Object[]> getLeadersByConstituency();
 	public List<Object[]> getUsersByConstituencyAndLeader(Long leaderId,Long constituencyId);
 	public List<Object[]> getUsersList(Long usertypeId);
+	public List<Object[]> getUserForAssignedUser(Long leaderId);
+	public int updateUserLeaderRelations(Long userTypeId,List<Long> surveyUserIds,Long leaderId,Long constituencyId);
 }
