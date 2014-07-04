c9a38f88a935f238964470183d5b5b0445cc3034
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-04 05:45:07 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SurveyUserBoothAssignDAOHibernate.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ISurveyUserBoothAssignDAO;
+
+public class SurveyUserBoothAssignDAOHibernate extends BaseDaoTestCase{
+	private ISurveyUserBoothAssignDAO surveyUserBoothAssignDAO;
+
+	public void setSurveyUserBoothAssignDAO(
+			ISurveyUserBoothAssignDAO surveyUserBoothAssignDAO) {
+		this.surveyUserBoothAssignDAO = surveyUserBoothAssignDAO;
+	}
+	public void test()
+	{
+		//surveyUserBoothAssignDAO.getAll();
+		
+		
+		
+	}
+
+}
