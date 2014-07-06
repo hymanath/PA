a40bc9c7d39fba93623ea00949633d7d604888a2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-06 10:34:07 +0000

    updated by anil

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webservice/android/abstractservice/IWebServiceHandlerService1.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webservice/android/abstractservice/IWebServiceHandlerService1.java
@@ -1,12 +1,16 @@
 package com.itgrids.partyanalyst.webservice.android.abstractservice;
 
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SurveyResponceVO;
+import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLocationTrackingVo;
 import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLoginVO;
 
 public interface IWebServiceHandlerService1 {
 	
 	public Object checkForUserAuthentication(UserLoginVO inputvo);
 	public Object saveSurveyFieldUsers(SurveyResponceVO inputResponse);
+	public ResultStatus saveUserTrackingLocation(UserLocationTrackingVo userLocationTrackingVo);
+
 
 /*	public ResultStatus checkUserAuthenticationAndUpdateAuthorisedTime(String userId,String macAdressId);
 */	
