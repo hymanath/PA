d308b46a2c6d097137059f38a1b87154def00991
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-07 08:17:47 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICandidateUpdationDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICandidateUpdationDetailsService.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.service;
 
-public interface ICandidateUpdationDetailsService {
+import java.util.List;
 
+import com.itgrids.partyanalyst.dto.CandidateVO;
+
+public interface ICandidateUpdationDetailsService {
+	public CandidateVO getCandidateDetailsForElection(Long electionId,Long districtId);
 }
