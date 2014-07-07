15951ffe2b9ecc9bf20eb6fdc53e8f71d9a29257
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-07 07:02:31 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CandidateUpdationDetailsService.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICandidateUpdationDetailsService;
+
+public class CandidateUpdationDetailsService implements ICandidateUpdationDetailsService{
+
+	
+	
+	private static final Logger LOG = Logger.getLogger(CandidateUpdationDetailsService.class);
+}
