73a9fc2f2f9f794ee3c6ce6fb4a4923740f728ce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-07 08:17:47 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CandidateUpdationDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CandidateUpdationDetailsService.java
@@ -1,12 +1,30 @@
 package com.itgrids.partyanalyst.service.impl;
 
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itgrids.partyanalyst.dao.INominationDAO;
+import com.itgrids.partyanalyst.dto.CandidateVO;
 import com.itgrids.partyanalyst.service.ICandidateUpdationDetailsService;
 
 public class CandidateUpdationDetailsService implements ICandidateUpdationDetailsService{
 
+private static final Logger LOG = Logger.getLogger(CandidateUpdationDetailsService.class);
 	
+	@Autowired
+	private INominationDAO nominationDAO;
 	
-	private static final Logger LOG = Logger.getLogger(CandidateUpdationDetailsService.class);
+	public CandidateVO getCandidateDetailsForElection(Long electionId,Long districtId)
+	{
+		CandidateVO resultVo = new CandidateVO();
+		
+		
+		
+		
+		return resultVo;
+	}
+	
 }
