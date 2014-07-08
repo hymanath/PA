645f0672d830f78163c38c89b60e26197da26017
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-08 12:01:23 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserTabAssignDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserTabAssignDAO.java
@@ -1,10 +1,15 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.SurveyUserTabAssign;
 
 public interface ISurveyUserTabAssignDAO  extends GenericDao<SurveyUserTabAssign, Long>
 {
-
+	public List<Object[]> getSurveyTabsBySurveyUserId(Long leaderId);
+	
+	public List<Object[]> getSurveyTabsBySurveyUserIdsList(List<Long> surveyUserIdsList);
+	
 }
