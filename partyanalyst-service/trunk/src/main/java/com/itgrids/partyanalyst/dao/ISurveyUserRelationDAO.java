572e3f55a392280b57dad95584b8a16b593d7cde
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-08 12:01:07 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserRelationDAO.java
@@ -10,8 +10,14 @@ public interface ISurveyUserRelationDAO  extends GenericDao<SurveyUserRelation, 
 {
 
 	public List<Object[]> getLeadersByConstituency();
+	
 	public List<Object[]> getUsersByConstituencyAndLeader(Long leaderId,Long constituencyId);
+	
 	public List<Object[]> getUsersList(Long usertypeId);
+	
 	public List<Object[]> getUserForAssignedUser(Long leaderId,Long userType);
+	
 	public int updateUserLeaderRelations(Long userTypeId,List<Long> surveyUserIds,Long leaderId);
+	
+	public List<Object[]> getUserForAssignedUsers(Long leaderId);
 }
