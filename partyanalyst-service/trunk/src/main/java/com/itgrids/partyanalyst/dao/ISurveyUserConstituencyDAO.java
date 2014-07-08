e8169ef706ac437cc27c72180e9409ce0d5871df
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-08 12:00:23 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyUserConstituencyDAO.java
@@ -7,7 +7,12 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.SurveyUserConstituency;
 
 public interface ISurveyUserConstituencyDAO extends GenericDao<SurveyUserConstituency, Long>{
-
+	
+	public List<Object[]> getSurveyConstituencyList();
+	
+	public List<Object[]> getSurveyConstituencyLeadersList(Long constituencyId);
+	
 	public List<Long> getAlreadyAssignedUsers();
+	
 	public List<Long> getAlreadyAssignedConstituencies();
 }
