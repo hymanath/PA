a3ea11e080104e9d2f8e0aaf31fc7056e3d5496c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-08 12:02:30 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SurveyUserTabAssignDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SurveyUserTabAssignDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ISurveyUserTabAssignDAO;
 import com.itgrids.partyanalyst.model.SurveyUserTabAssign;
@@ -10,6 +13,22 @@ public class SurveyUserTabAssignDAO extends GenericDaoHibernate<SurveyUserTabAss
 	public SurveyUserTabAssignDAO()
 	{
 		super(SurveyUserTabAssign.class);
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getSurveyTabsBySurveyUserId(Long leaderId){
+		
+		return getHibernateTemplate().find(" select model.surveyUserTabAssignId, model.tabNo from SurveyUserTabAssign model where model.surveyUser.surveyUserId = "+leaderId+" order by model.tabNo asc ");
+	}
+	
+	public List<Object[]> getSurveyTabsBySurveyUserIdsList(List<Long> surveyUserIds){
+		
+		Query query = getSession().createQuery("select distinct model.surveyUser.surveyUserId,model.tabNo from SurveyUserTabAssign model where " +
+				"  model.surveyUser.surveyUserId in (:surveyUserIds)  and model.activeStatus = 'Y' ");
+		query.setParameterList("surveyUserIds", surveyUserIds);
+		
+		return query.list();
 	}
 
 }
