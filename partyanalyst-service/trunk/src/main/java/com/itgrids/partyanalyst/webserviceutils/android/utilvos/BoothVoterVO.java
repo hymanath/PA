29683effd643a4aa3e46bde262e404bc8e5f59f6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-09 07:39:59 +0000

    Caste collection app reports implementation - samba

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webserviceutils/android/utilvos/BoothVoterVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webserviceutils/android/utilvos/BoothVoterVO.java
@@ -7,9 +7,16 @@ public class BoothVoterVO {
 	
 	private List<Long> voterIds = new ArrayList<Long>();
 	private String partNo;
+	private Long boothId;
 
 	
 	
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
 	public List<Long> getVoterIds() {
 		return voterIds;
 	}
