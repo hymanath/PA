1942e1eb64d372229caddbd65051e82592f82a93
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-10 12:38:22 +0000

    updated by Srishailam

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SurveyCallStatusDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ISurveyCallStatusDAO;
+
+public class SurveyCallStatusDAOHibernateTest extends BaseDaoTestCase{
+
+		private ISurveyCallStatusDAO surveyCallStatusDAO;
+
+		public void setSurveyCallStatusDAO(ISurveyCallStatusDAO surveyCallStatusDAO) {
+			this.surveyCallStatusDAO = surveyCallStatusDAO;
+		}
+		
+		
+}
