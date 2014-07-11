fe56d94457cf3381879ea6a999780423d248e5c6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-11 13:28:01 +0000

    updated by anil

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webservice/android/components/WebServiceHandler2.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webservice/android/components/WebServiceHandler2.java
@@ -1,32 +1,22 @@
 package com.itgrids.partyanalyst.webservice.android.components;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import com.itgrids.partyanalyst.dto.EffectedBoothsResponse;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SurveyResponceVO;
-import com.itgrids.partyanalyst.dto.VoterDetailsVO;
-import com.itgrids.partyanalyst.dto.WSResultVO;
 import com.itgrids.partyanalyst.webservice.android.abstractservice.IWebServiceHandlerService1;
-import com.itgrids.partyanalyst.webservice.android.concreteservice.WebServiceHandlerService1;
-import com.itgrids.partyanalyst.webservice.utils.VoterTagVO;
 import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLocationTrackingVo;
-import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLoginVO;
+import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserLoginUtils;
 import com.itgrids.partyanalyst.webserviceutils.android.utilvos.UserResponseVO;
 
 @Component
@@ -35,6 +25,8 @@ public class WebServiceHandler2 {
 	
 	//@Autowired
 	//@Qualifier("web1")
+	private final static Logger LOG = Logger.getLogger(WebServiceHandler2.class);
+
 	private IWebServiceHandlerService1  webServiceHandlerService1;
 	
 	
@@ -53,7 +45,7 @@ public class WebServiceHandler2 {
 	@Path("/loginFieldDataUser")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Object loginFieldDataUser (UserLoginVO inputs)
+	public Object loginFieldDataUser (UserLoginUtils inputs)
 	{
 		UserResponseVO out=null;
 		Map<String,String> userDetails= new HashMap<String, String>();
@@ -113,10 +105,12 @@ public class WebServiceHandler2 {
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			
+			LOG.error("Exception raised in saveSurveyUserTrackingDetails service in SurveyDataDetailsService", e);
+
 		}
 		
-		if(!out.getMessage().equalsIgnoreCase("Success"))
+		if(out.getMessage()!=null&&!out.getMessage().equalsIgnoreCase("Success"))
 			return "{\"status\":\"Failure\"}";
 		
 		 else return "{\"status\":\"Success\"}";
