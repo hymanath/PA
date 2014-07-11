473ff12d71187fc1f10d10f4d24eaf23b5d66302
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-11 13:26:48 +0000

    updated by anil

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/webserviceutils/android/utilvos/UserLoginUtils.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.itgrids.partyanalyst.webserviceutils.android.utilvos;
+
+/**
+ * @author Administrator Jul 10, 2014
+ *
+ */
+/**
+ * @author Administrator Jul 10, 2014
+ *
+ */
+public class UserLoginUtils  extends UserLoginVO{
+
+	
+	private String boothId;
+	private String partNo;
+	private Long userId;
+	private String type;
+	
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(String boothId) {
+		this.boothId = boothId;
+	}
+	public String getPartNo() {
+		return partNo;
+	}
+	public void setPartNo(String partNo) {
+		this.partNo = partNo;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+}
