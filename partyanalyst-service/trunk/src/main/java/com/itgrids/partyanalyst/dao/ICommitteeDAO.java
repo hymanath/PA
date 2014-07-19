e95ded2f8ade262557b04c2b16e7fe1106cc9955
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-19 09:32:58 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICommitteeDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Committee;
 
 public interface ICommitteeDAO extends GenericDao<Committee, Long>{
-
+	public List<Long> getAllCommitteeLevelValuesByCommitteeLevel(Long committeeLevelId);
+	public List<Object[]> getAllCommitteesForCommitteeLevelValues(Long committeeLevelValueId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CommitteeDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICommitteeDAO;
 import com.itgrids.partyanalyst.model.Committee;
@@ -12,4 +15,20 @@ public class CommitteeDAO extends GenericDaoHibernate<Committee, Long> implement
 		
 	}
 
+	
+	public List<Long> getAllCommitteeLevelValuesByCommitteeLevel(Long committeeLevelId)
+	{		
+		Query query = getSession().createQuery("select model.commiiteeLevelValue from Committee model where model.committeeLevel.commiiteeLevelId = :committeeLevelId");		
+		query.setParameter("committeeLevelId", committeeLevelId);
+		return query.list();
+	}
+	
+
+	public List<Object[]> getAllCommitteesForCommitteeLevelValues(Long committeeLevelValueId)
+	{		
+		Query query = getSession().createQuery("select distinct model.committeeId,model.name from Committee model where model.commiiteeLevelValue = :committeeLevelValueId");		
+		query.setParameter("committeeLevelValueId", committeeLevelValueId);
+		return query.list();
+	}
+	
 }
