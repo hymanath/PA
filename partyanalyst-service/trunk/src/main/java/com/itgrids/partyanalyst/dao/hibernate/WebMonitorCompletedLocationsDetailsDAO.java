9a133e1a6bd0c8ea7f257b258883831caffc2e85
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-21 10:20:47 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/WebMonitorCompletedLocationsDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/WebMonitorCompletedLocationsDetailsDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IWebMonitorCompletedLocationsDetailsDAO;
 import com.itgrids.partyanalyst.model.WebMonitorCompletedLocationsDetails;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class WebMonitorCompletedLocationsDetailsDAO extends GenericDaoHibernate<WebMonitorCompletedLocationsDetails, Long> implements IWebMonitorCompletedLocationsDetailsDAO{
 	
@@ -11,4 +15,15 @@ public class WebMonitorCompletedLocationsDetailsDAO extends GenericDaoHibernate<
 		super(WebMonitorCompletedLocationsDetails.class);
 	}
 
+	public Long getSurveyWMCompletedCountByConstId(Long scopeId,List<Long> boothIds){
+		
+		Query query = getSession().createQuery(" select count(distinct WMCLD.locationValue) from WebMonitorCompletedLocationsDetails WMCLD where WMCLD.locationScopeId = :scopeId and " +
+				" WMCLD.locationValue  in (:boothIds) ");
+		
+		query.setParameter("locationScopeId", IConstants.BOOTH_SCOPE_ID);
+		query.setParameterList("boothIds", boothIds);
+				
+		return (Long) query.uniqueResult();
+		
+	}
 }
