36c100e6d80177d8b8886f4506b25d96e1a8850a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-21 08:58:53 +0000

    Updating location completion details implementation - samba

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IWebMonitorCompletedLocationsDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.WebMonitorCompletedLocationsDetails;
+
+public interface IWebMonitorCompletedLocationsDetailsDAO extends GenericDao<WebMonitorCompletedLocationsDetails,Long>{
+
+}
