9fd627ff62ed964de9583557ced61ad35ae1c4ac
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-21 10:20:22 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IWebMonitorCompletedLocationsDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IWebMonitorCompletedLocationsDetailsDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.WebMonitorCompletedLocationsDetails;
 
 public interface IWebMonitorCompletedLocationsDetailsDAO extends GenericDao<WebMonitorCompletedLocationsDetails,Long>{
-
+	
+	public Long getSurveyWMCompletedCountByConstId(Long scopeId,List<Long> boothIds);
+	
 }
