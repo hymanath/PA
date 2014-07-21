70b0524c283f00eecee97e079fdf9121aacf2247
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-21 10:15:53 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/WebMonitorCompletedLocationsDetailsDAOHibernateTest.java
@@ -0,0 +1,25 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IWebMonitorCompletedLocationsDetailsDAO;
+
+public class WebMonitorCompletedLocationsDetailsDAOHibernateTest extends BaseDaoTestCase{
+	
+	
+ private IWebMonitorCompletedLocationsDetailsDAO webMonitorCompletedLocationsDetailsDAO;
+
+public void setWebMonitorCompletedLocationsDetailsDAO(
+		IWebMonitorCompletedLocationsDetailsDAO webMonitorCompletedLocationsDetailsDAO) {
+	this.webMonitorCompletedLocationsDetailsDAO = webMonitorCompletedLocationsDetailsDAO;
+}
+ 
+
+public void test()
+{
+	List<Long> list = webMonitorCompletedLocationsDetailsDAO.getPanchayatBoothsByConstituencyId(8l);
+	System.out.println(list.size());
+}
+}
