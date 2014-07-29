804a66f7b2a247e6e4862b3635a9ace2d0558c92
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-29 06:35:05 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothPublicationVoterDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothPublicationVoterDAO.java
@@ -7180,8 +7180,9 @@ public List<Object[]> getLatestBoothDetailsOfConstituency(Long constituencyId)
 	}
 	public List<Object[]> getTotalVotersForAllConstituencies(List<Long> constituencyIds)
 	{
-		Query query = getSession().createQuery("select model.booth.constituency.constituencyId,model.booth.constituency.name,count(model.voter.voterId) from BoothPublicationVoter model where model.booth.publicationDate.publicationDateId = 11 and model.booth.constituency.constituencyId  in( :constituencyIds)");
+		Query query = getSession().createQuery("select model.booth.constituency.constituencyId,model.booth.constituency.name,count(model.voter.voterId) from BoothPublicationVoter model where model.booth.publicationDate.publicationDateId = :publicationId and model.booth.constituency.constituencyId in( :constituencyIds)");
 		query.setParameterList("constituencyIds", constituencyIds);
+		query.setParameter("publicationId", IConstants.VOTER_DATA_PUBLICATION_ID);
 		return query.list();
 	}
 	
