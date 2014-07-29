04e75bfcb575c1b14fdf7e45236693134fa2c1dd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-29 15:23:04 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISurveyCompletedDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISurveyCompletedDetailsService.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.SurveyDashBoardVO;
 import com.itgrids.partyanalyst.dto.SurveyReportVO;
 
 public interface ISurveyCompletedDetailsService {
@@ -9,4 +10,5 @@ public interface ISurveyCompletedDetailsService {
 	public String saveBoothStatusDetails(Long locationValue,Long statusId,String locationType);
 	public List<SurveyReportVO> getSurveyCompletedLocationsDetailsForSurveyStartedConstituencies();
 
+	public SurveyDashBoardVO getCompletdConstituenciesDetails();
 }
