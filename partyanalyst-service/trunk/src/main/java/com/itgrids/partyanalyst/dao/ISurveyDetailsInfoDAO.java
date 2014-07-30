6cd2f69ec74e43381e5dc68f655861cfcb3e4bf2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-07-30 11:49:01 +0000

    CADRE SURVEY  - PRASAD

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyDetailsInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISurveyDetailsInfoDAO.java
@@ -130,7 +130,7 @@ public interface ISurveyDetailsInfoDAO  extends GenericDao<SurveyDetailsInfo, Lo
 	public List<Long> getUsersByDate(Long constituencyId,Date date);
 	public List<Object[]> getStartedBoothsDetailsByConstituencyIds(List<Long> constituencyIds);
 	public List<Long> getBoothsInProcessByConstituencyId(Long constituencyId);
-
+	public List<Object[]> getProcessingConstituencyes();
 
 }
 
