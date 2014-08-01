96ff43afcd5d442c541dc2862fab0e6fb75f9ab5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-08-01 06:07:29 +0000

    update by sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICandidateDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICandidateDetailsDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CandidateDetails;
 
 public interface ICandidateDetailsDAO extends GenericDao<CandidateDetails, Long>
 {
- 
+	
 }
