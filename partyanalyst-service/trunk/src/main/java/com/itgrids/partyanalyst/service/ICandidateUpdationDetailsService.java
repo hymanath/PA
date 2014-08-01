fecf9696a2dd2ac7c1ff67c7fac5d64be7d196b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-08-01 06:12:14 +0000

    update by sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICandidateUpdationDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICandidateUpdationDetailsService.java
@@ -3,11 +3,14 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.CandidateVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface ICandidateUpdationDetailsService {
 	
 public CandidateVO getCandidateDetailsForElection(Long electionId,Long districtId);
 public Object gettingElectionYears(Long electionTypeId);
 public Object getAllDistrictsForAState(Long stateId);
+public List<CandidateVO> gettingCandidateDetails(Long electionScopeId,Long electionId,List<Long> districtIds);
+public ResultStatus updateDetailsofACandidate(List<CandidateVO> candidateVOList);
 
 }
