954fa8943b78f421bfc7a0185ef2b7bdddbc65e6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-08-25 11:05:22 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyTehsilDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ConstituencyTehsil;
+
+public interface IConstituencyTehsilDAO extends GenericDao<ConstituencyTehsil, Long>{
+
+}
