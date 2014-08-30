6385312a5c2550930970f8c6b1e5764a8315db37
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-08-30 13:30:32 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BoothWiseSurveyStatusDetailsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BoothWiseSurveyStatusDetailsVO.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoothWiseSurveyStatusDetailsVO {
 	
 	private Long boothId;
@@ -11,6 +14,87 @@ public class BoothWiseSurveyStatusDetailsVO {
 	private String wmDvCompleted = "N";
 	private Long totalVoters;
 	
+	
+	private String dcName;
+	private String dcMobileNo;
+	private String leaderName;
+	private String leaderMobile;
+	private String constituency;
+	private Long count;
+	private List<String> boothList = new ArrayList<String>(0);
+	private String startTime;
+	private String endTime;
+	private String workedTime;
+	private List<BoothWiseSurveyStatusDetailsVO> boothWiseSurveyDetailsVOList = new ArrayList<BoothWiseSurveyStatusDetailsVO>(0);
+	
+	
+	public List<String> getBoothList() {
+		return boothList;
+	}
+	public void setBoothList(List<String> boothList) {
+		this.boothList = boothList;
+	}
+	public String getDcName() {
+		return dcName;
+	}
+	public void setDcName(String dcName) {
+		this.dcName = dcName;
+	}
+	public String getDcMobileNo() {
+		return dcMobileNo;
+	}
+	public void setDcMobileNo(String dcMobileNo) {
+		this.dcMobileNo = dcMobileNo;
+	}
+	public String getLeaderName() {
+		return leaderName;
+	}
+	public void setLeaderName(String leaderName) {
+		this.leaderName = leaderName;
+	}
+	public String getLeaderMobile() {
+		return leaderMobile;
+	}
+	public void setLeaderMobile(String leaderMobile) {
+		this.leaderMobile = leaderMobile;
+	}
+	public String getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(String constituency) {
+		this.constituency = constituency;
+	}
+	public Long getCount() {
+		return count;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getWorkedTime() {
+		return workedTime;
+	}
+	public void setWorkedTime(String workedTime) {
+		this.workedTime = workedTime;
+	}
+	public List<BoothWiseSurveyStatusDetailsVO> getBoothWiseSurveyDetailsVOList() {
+		return boothWiseSurveyDetailsVOList;
+	}
+	public void setBoothWiseSurveyDetailsVOList(
+			List<BoothWiseSurveyStatusDetailsVO> boothWiseSurveyDetailsVOList) {
+		this.boothWiseSurveyDetailsVOList = boothWiseSurveyDetailsVOList;
+	}
 	public Long getTotalVoters() {
 		return totalVoters;
 	}
