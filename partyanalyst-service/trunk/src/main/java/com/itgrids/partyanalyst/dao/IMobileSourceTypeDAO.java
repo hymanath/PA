e4b1f94ab0714c65013c5b23322e49ac431de859
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-09-11 08:51:19 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMobileSourceTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.MobileSourceType;
+
+public interface IMobileSourceTypeDAO extends GenericDao<MobileSourceType, Long>{
+
+}
