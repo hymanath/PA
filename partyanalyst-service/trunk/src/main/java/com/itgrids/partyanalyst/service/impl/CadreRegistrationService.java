a013d88c717115f4e0a634d5a2009f165ab04d75
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-09-26 07:25:06 +0000

    Cadre Registration - PRASAD

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreRegistrationService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ICadreRegistrationService;
+
+public class CadreRegistrationService implements ICadreRegistrationService {
+
+}
