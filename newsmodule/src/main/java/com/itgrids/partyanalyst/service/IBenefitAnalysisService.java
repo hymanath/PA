166f73aeec792a0bfef8703dd487f62919499abe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-09-26 05:34:47 +0000

    benifit implementation by mahesh

--- newsmodule/src/main/java/com/itgrids/partyanalyst/service/IBenefitAnalysisService.java
+++ newsmodule/src/main/java/com/itgrids/partyanalyst/service/IBenefitAnalysisService.java
@@ -1,5 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
-public interface IBenefitAnalysisService {
+import java.util.Date;
+import java.util.List;
 
+import com.itgrids.partyanalyst.dto.BenfitVO;
+
+public interface IBenefitAnalysisService {
+	
+	public List<BenfitVO> getCategoryWiseBenifit(Date fromDate,Date toDate,Long stateId,List<Long> partyIds);
+	
+	public List<BenfitVO> getCandidateGroupWiseBenifit(Long groupId,Date fromDate,Date toDate,Long stateId,Long partyId);
 }
