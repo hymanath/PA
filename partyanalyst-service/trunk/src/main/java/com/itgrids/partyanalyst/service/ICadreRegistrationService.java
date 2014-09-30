f1831acb0dc62bf9691128a222fa7a512c41f0db
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-09-30 14:40:19 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationService.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.CadreRegistrationVO;
+import com.itgrids.partyanalyst.dto.GenericVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.VoterInfoVO;
 
@@ -10,8 +11,14 @@ public interface ICadreRegistrationService {
 
 	public ResultStatus saveCadreRegistration(final CadreRegistrationVO cadreRegistrationVO,String registrationType);
 	
-	public List<VoterInfoVO> getSearchDetailsCadreRegistration(Long constituencyId, String seachType, String candidateName, String voterCardId, String houseNo);
+	public List<VoterInfoVO> getSearchDetailsCadreRegistration(Long constituencyId, String seachType, String candidateName, String voterCardId, String houseNo,Long panchayatId,Long boothId,Long locationId);
 	
 	public List<VoterInfoVO> getCandidateInfoBySearchCriteria(String searchType, Long candidateId);
+	
+	public List<GenericVO> getBoothCoverdVillagesDetails(List<Long> boothIds);
+	
+	public List<GenericVO> getBoothForPanchayats(Long constituencyId, Long locationId);
+	
+	public List<GenericVO> getConstiteuncyDetailsByConstiteuncy(Long constituencyId);
 	
 }
