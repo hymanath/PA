b5f3dda3588a1feb9f096a23971e910e936aeb0c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-01 12:31:23 +0000

    WorkStartedConstituencyCount task by Sreenivas

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreDashBoardService.java
@@ -1,5 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.CadreRegisterInfo;
+
 public interface ICadreDashBoardService {
+	
+	public List<CadreRegisterInfo> getWorkStartedConstituencyCount();
 
 }
