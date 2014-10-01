e80aeb5645df1d863363a610b09495eb3c817802
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-01 05:43:55 +0000

    updated by mahesh

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreDashBoardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreDashBoardAction.java
@@ -4,17 +4,53 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 
+import com.itgrids.partyanalyst.service.ICadreDashBoardService;
 import com.opensymphony.xwork2.Action;
 
 public class CadreDashBoardAction implements ServletRequestAware {
 
 	private HttpServletRequest request;
 	
+	private ICadreDashBoardService cadreDashBoardService;
+	
+	public ICadreDashBoardService getCadreDashBoardService() {
+		return cadreDashBoardService;
+	}
+
+	public void setCadreDashBoardService(
+			ICadreDashBoardService cadreDashBoardService) {
+		this.cadreDashBoardService = cadreDashBoardService;
+	}
+
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 	}
 
 	public String execute(){
+		
+		return Action.SUCCESS;
+	}
+	
+	public String getDashBoardBasicInfo(){
+		
+		
+		return Action.SUCCESS;
+	}
+	
+    public String getRecentlyRegisteredCadresInfo(){
+		
+		
+		return Action.SUCCESS;
+	}
+    
+   public String getAssemblyWiseCompletedPercentage(){
+		
+		
+		return Action.SUCCESS;
+	}
+   
+   public String getDistrictWiseCompletedPercentage(){
+		
 		
 		return Action.SUCCESS;
 	}
