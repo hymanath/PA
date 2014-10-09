e61649635533d3b8ad959be078d97e8077c5cb79
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-09 11:43:04 +0000

    commited by balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SurveyDataDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SurveyDataDetailsService.java
@@ -2845,19 +2845,7 @@ public class SurveyDataDetailsService implements ISurveyDataDetailsService
 	{
 		List<SelectOptionVO> resultList = new ArrayList<SelectOptionVO>();
 		try {
-			List<Object[]> totalConstituenciesList = constituencyDAO.getAllAssemblyConstituenciesByStateId(1L);
-			//List<Long> constituenciesList = surveyUserConstituencyDAO.getAlreadyAssignedConstituencies(userTypeId);
-			
-			for(Object[] parms:totalConstituenciesList)
-			{
-				SelectOptionVO vo = new SelectOptionVO();
-				//if(!constituenciesList.contains((Long)parms[0])){
-				vo.setId((Long)parms[0]);
-				vo.setName(parms[1].toString());
-				resultList.add(vo);
-				//}
-			}
-			
+			resultList = getAssemblyConstituenciesByStateId(0L,1L);//constituencyDAO.getAllAssemblyConstituenciesByStateId(1L);		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
