a4f6c058c5eb9a4358b3d0cabe8643333b9f3077
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-10 16:25:38 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadrePreviousRolesDAOHibernateTest.java
@@ -0,0 +1,47 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICadrePreviousRolesDAO;
+import com.itgrids.partyanalyst.dto.GenericVO;
+
+public class CadrePreviousRolesDAOHibernateTest extends BaseDaoTestCase{
+
+	private ICadrePreviousRolesDAO cadrePreviousRolesDAO;
+
+	public void setCadrePreviousRolesDAO(
+			ICadrePreviousRolesDAO cadrePreviousRolesDAO) {
+		this.cadrePreviousRolesDAO = cadrePreviousRolesDAO;
+	}
+	
+	public void testDetails()
+	{
+		List<Object[]> participationInfo = cadrePreviousRolesDAO.getexistingRolesForTdpCadreByTdpCadreId(1400565L);
+		List<GenericVO> returnList = new ArrayList<GenericVO>();
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		
+		try {
+			
+			if(participationInfo != null && participationInfo.size()>0)
+			{
+				for (Object[] participation : participationInfo)
+				{
+					//participation
+					String date = format.format(format1.parse(participation[2].toString()));
+					System.out.println(date);
+				}
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+	}
+}
