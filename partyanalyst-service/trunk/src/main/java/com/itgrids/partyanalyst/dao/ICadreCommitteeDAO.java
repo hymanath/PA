11eb37046890f3d640d9b15eb266722e61a1513e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-13 18:07:04 +0000

    Cadre Levels to Select Boxes -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreCommittee;
 
 public interface ICadreCommitteeDAO extends GenericDao<CadreCommittee, Long>{
-
+	public List<Object[]> getAllCadreCommittees();
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeLevelDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeLevelDAO.java
@@ -1,10 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreCommitteeLevel;
 
-public interface ICadreCommitteeLevelDAO extends GenericDao<CadreCommitteeLevel, Long>
-{
-
+public interface ICadreCommitteeLevelDAO extends GenericDao<CadreCommitteeLevel, Long>{
+	public List<Object[]> getAllCadreCommitteeLevels();
 }
