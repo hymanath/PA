1b51d56390c80449480f2ae7317591f94bf5458f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-13 18:07:04 +0000

    Cadre Levels to Select Boxes -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRolesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRolesDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreRoles;
 
 public interface ICadreRolesDAO extends GenericDao<CadreRoles, Long>{
-
+	public List<Object[]> getAllCadreRoles();
 }
