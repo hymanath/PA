93be7e9099ed607c61c749862b37b3d6162c8fd4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-13 18:07:14 +0000

    Cadre Levels to Select Boxes -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreCommitteeDAO;
 import com.itgrids.partyanalyst.model.CadreCommittee;
@@ -10,6 +13,13 @@ public class CadreCommitteeDAO extends GenericDaoHibernate<CadreCommittee, Long>
 	public CadreCommitteeDAO() 
 	{
 		super(CadreCommittee.class);
+	}
+	
+	public List<Object[]> getAllCadreCommittees(){
+		Query query = getSession().createQuery(" select model.cadreCommitteeId," +
+				" model.name from CadreCommittee model ");
+		
+		return query.list();
 	}
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRolesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRolesDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreRolesDAO;
 import com.itgrids.partyanalyst.model.CadreRoles;
@@ -10,6 +13,13 @@ public class CadreRolesDAO extends GenericDaoHibernate<CadreRoles, Long> impleme
 	public CadreRolesDAO()
 	{
 		super(CadreRoles.class);
+	}
+	
+	public List<Object[]> getAllCadreRoles(){
+		Query query = getSession().createQuery(" select model.cadreRolesId," +
+				" model.role from CadreRoles model ");
+		
+		return query.list();
 	}
 
 }
