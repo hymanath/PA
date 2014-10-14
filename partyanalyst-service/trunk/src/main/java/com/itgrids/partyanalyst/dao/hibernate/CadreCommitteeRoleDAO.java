b5511f369cc9a37f08c3d374de3c9076d331f147
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-14 13:38:27 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeRoleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeRoleDAO.java
@@ -25,4 +25,23 @@ public class CadreCommitteeRoleDAO extends GenericDaoHibernate<CadreCommitteeRol
 		query.setParameter("cadreCommiteeId", cadreCommiteeId);
 		return query.list();
 	}
+	
+	public List<Object[]> getCommitteeRolesByLevelId(Long cadreCommitteeLevelId)
+	{
+		Query query = getSession().createQuery("select DISTINCT model.cadreCommittee.cadreCommitteeId, model.cadreCommittee.name from CadreCommitteeRole model " +
+				" where model.cadreCommitteeLevelId = :cadreCommitteeLevelId order by model.cadreCommittee.name ");
+		query.setParameter("cadreCommitteeLevelId", cadreCommitteeLevelId);
+		return query.list();
+		
+	}
+	
+	public List<Object[]> getCommitteeRolesByLevelIdAndCommitteeId(Long cadreCommitteeLevelId,Long cadreCommiteeId)
+	{
+		Query query = getSession().createQuery("select DISTINCT  model.cadreRoles.cadreRolesId,model.cadreRoles.role  from CadreCommitteeRole model " +
+				" where model.cadreCommitteeLevelId = :cadreCommitteeLevelId and model.cadreCommittee.cadreCommitteeId = :cadreCommiteeId order by model.cadreRoles.role ");
+		
+		query.setParameter("cadreCommitteeLevelId", cadreCommitteeLevelId);
+		query.setParameter("cadreCommiteeId", cadreCommiteeId);
+		return query.list();
+	}
 }
