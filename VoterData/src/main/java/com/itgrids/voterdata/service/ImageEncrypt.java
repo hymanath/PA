3c7e93210042a7da7412a6a8c73b3384fff264fc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-28 12:34:33 +0000

    Updated by Kamalakar Dandu.

--- VoterData/src/main/java/com/itgrids/voterdata/service/ImageEncrypt.java
+++ VoterData/src/main/java/com/itgrids/voterdata/service/ImageEncrypt.java
@@ -1,7 +1,7 @@
 package com.itgrids.voterdata.service;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.io.RandomAccessFile;
 import java.util.Date;
 
@@ -11,41 +11,59 @@ public class ImageEncrypt {
 	public static void main(String[] args)
 	{
 		ImageEncrypt imageEncrypt = new ImageEncrypt();
-		imageEncrypt.imageEncrypt("D:\\Photos\\344\\");
+		imageEncrypt.imageEncrypt("D:\\Photos\\Voter_Images_Encript\\282\\",
+				"D:\\Photos\\Voter_Images_Encript_Logs\\282.txt");
 	}
 	
-	public void imageEncrypt(String filePath)
+	@SuppressWarnings("deprecation")
+	public void imageEncrypt(String filePath,String logPath)
 	{
 		try{
+		BufferedWriter outwriter = new BufferedWriter(new FileWriter(new File(logPath)));
+		StringBuilder sb = new StringBuilder();
 		File folder = new File(filePath);
 		File[] listOfFiles = folder.listFiles();
 		Date date = new Date();
 		System.out.println("Start Time " + date.toGMTString());
-		    for (int i = 0; i < listOfFiles.length; i++) {
-		      if (listOfFiles[i].isFile()) {
-		    	  RandomAccessFile raf = new RandomAccessFile(listOfFiles[i], "rw");
-		    	  raf.seek(5L);
-		    	  raf.write(17);
-		    	  raf.close();
-		        //System.out.println("File " + listOfFiles[i].getName());
-		      }
-		      else if (listOfFiles[i].isDirectory()) {
-		    	  System.out.println("Folder ::" + listOfFiles[i].getName());
-		    	  File[] listOfFiles1 = listOfFiles[i].listFiles();
-		    	  for (int j = 0; j < listOfFiles1.length; j++) {
-				      if (listOfFiles1[j].isFile()) {
-				    	  RandomAccessFile raf = new RandomAccessFile(listOfFiles1[j], "rw");
-				    	  raf.seek(5L);
-				    	  raf.write(17);
-				    	  raf.close();
-				       
-				      }
-		    	  }
-		       
-		      }
-		    }
-		    Date date1 = new Date();
-		    System.out.println("End Time " + date1.toGMTString());
+		int totalPhotos = 0;
+		    
+		for (int i = 0; i < listOfFiles.length; i++) 
+	    {
+		  totalPhotos++;
+	      if (listOfFiles[i].isFile()) 
+	      {
+	    	  RandomAccessFile raf = new RandomAccessFile(listOfFiles[i], "rw");
+	    	  raf.seek(5L);
+	    	  raf.write(17);
+	    	  raf.close();
+	      }
+	      else if (listOfFiles[i].isDirectory()) 
+	      {
+	    	  int boothCount = 0;
+	    	  File[] listOfFiles1 = listOfFiles[i].listFiles();
+	    	  for (int j = 0; j < listOfFiles1.length; j++)
+	    	  {
+	    		  totalPhotos++;
+	    		  boothCount++;
+			      if (listOfFiles1[j].isFile())
+			      {
+			    	  RandomAccessFile raf = new RandomAccessFile(listOfFiles1[j], "rw");
+			    	  raf.seek(5L);
+			    	  raf.write(17);
+			    	  raf.close();
+			      }
+	    	  }
+	    	  System.out.println(listOfFiles[i].getName()+" Contains -->\t"+boothCount+"\tImages");
+	    	  sb.append(listOfFiles[i].getName()+"\t"+boothCount+"\n");
+	      }
+	    }
+		
+		System.out.println("Total Images are --> "+totalPhotos);
+		sb.append("Total Images are --> "+totalPhotos);
+		outwriter.write(sb.toString());
+        outwriter.close();
+	    Date date1 = new Date();
+	    System.out.println("End Time " + date1.toGMTString());
 	}catch(Exception e)
 	{
 		e.printStackTrace();
