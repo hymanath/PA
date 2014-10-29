e25d34767ec2eeeddfba6d541b1458df749640cf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-29 12:08:30 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTxnUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTxnUserDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreTxnUser;
@@ -7,5 +9,5 @@ import com.itgrids.partyanalyst.model.CadreTxnUser;
 
 public interface ICadreTxnUserDAO  extends GenericDao<CadreTxnUser, Long>{
 	
-
+	public List checkUserExists(Long userId , String mobileNo);
 }
