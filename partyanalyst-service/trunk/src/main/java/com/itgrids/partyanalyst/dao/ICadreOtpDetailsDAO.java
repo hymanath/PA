0b19cf7060e15e6e2c19e0f70ee2f0fc69ad2628
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-29 12:08:30 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreOtpDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreOtpDetailsDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreOtpDetails;
 
 
 public interface ICadreOtpDetailsDAO extends GenericDao<CadreOtpDetails, Long>{
-
+	public Integer updateIsDeleted(String mobileNo);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTxnDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTxnDetailsDAO.java
@@ -6,4 +6,5 @@ import com.itgrids.partyanalyst.model.CadreTxnDetails;
 
 
 public interface ICadreTxnDetailsDAO extends GenericDao<CadreTxnDetails, Long>{
+	public Integer updateCompleteStatus(String uniqueKey,Long constituencyId);
 }
