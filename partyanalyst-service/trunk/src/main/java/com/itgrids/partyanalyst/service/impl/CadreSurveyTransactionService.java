bd4248bf01f61495c1b4df8a4d14be9c80db74e8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-29 06:06:21 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreSurveyTransactionService.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICadreSurveyTransactionService;
+
+
+public class CadreSurveyTransactionService implements ICadreSurveyTransactionService{
+
+	private final static Logger LOG = Logger.getLogger(CadreSurveyTransactionService.class);
+	
+}
