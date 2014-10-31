cc7b738603f1c74719c030c0520455933a2b5a52
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-31 08:09:06 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/service/CadreSurveyTransactionServiceTest.java
@@ -0,0 +1,194 @@
+package com.itgrids.partyanalyst.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.appfuse.dao.BaseDaoTestCase;
+import org.junit.Test;
+
+import com.itgrids.partyanalyst.dao.ICadreOtpDetailsDAO;
+import com.itgrids.partyanalyst.dao.ICadreSurveyUserDAO;
+import com.itgrids.partyanalyst.dao.ICadreTxnDetailsDAO;
+import com.itgrids.partyanalyst.dao.ICadreTxnUserDAO;
+import com.itgrids.partyanalyst.dao.IConstituencyDAO;
+import com.itgrids.partyanalyst.dto.SurveyTransactionVO;
+import com.itgrids.partyanalyst.service.impl.SmsCountrySmsService;
+
+public class CadreSurveyTransactionServiceTest  extends BaseDaoTestCase {
+
+	private ICadreTxnDetailsDAO cadreTxnDetailsDAO;
+	private ICadreSurveyUserDAO cadreSurveyUserDAO;
+	private ICadreOtpDetailsDAO cadreOtpDetailsDAO;
+
+	public void setCadreTxnDetailsDAO(ICadreTxnDetailsDAO cadreTxnDetailsDAO) {
+		this.cadreTxnDetailsDAO = cadreTxnDetailsDAO;
+	}
+	public void setCadreSurveyUserDAO(ICadreSurveyUserDAO cadreSurveyUserDAO) {
+		this.cadreSurveyUserDAO = cadreSurveyUserDAO;
+	}
+	public void setCadreOtpDetailsDAO(ICadreOtpDetailsDAO cadreOtpDetailsDAO) {
+		this.cadreOtpDetailsDAO = cadreOtpDetailsDAO;
+	}
+	
+	@Test
+	public void testDetails()
+	{
+		SurveyTransactionVO returnVO = new SurveyTransactionVO();
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		try {
+			
+			Date today = new Date();
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(today);
+			
+			cal.add(Calendar.DATE, -1);
+			Date yesterDay = cal.getTime();
+			
+			Long totalTeamCount = cadreSurveyUserDAO.getTotalCadreSurveyDetails();
+			List<Long> teaminfo = cadreTxnDetailsDAO.getTotalCadreSurveyTxnTeamSize(yesterDay);
+			Long workingTeamSize = 0L;
+			returnVO.setTeamSize(totalTeamCount);
+			returnVO.setIdleTeamSize( totalTeamCount - workingTeamSize );
+			
+			List<Object[]> yesterTransactions = cadreTxnDetailsDAO.getTransactionDetailsByDates(null,yesterDay);
+			
+			if(yesterTransactions != null && yesterTransactions.size()>0)
+			{
+				for (Object[] transaction : yesterTransactions)
+				{
+					Long paidAmount = transaction[0] != null ?  Long.valueOf(transaction[0].toString()):0L;
+					Long pendingAmount = transaction[1] != null ? Long.valueOf(transaction[1].toString()) :0L;
+					Long totalAmount = transaction[2] != null ? Long.valueOf(transaction[2].toString()) :0L;
+				
+					returnVO.setSubmittedCount(paidAmount / 100 );
+					returnVO.setNotSubmittedCount(pendingAmount / 100 );					
+					returnVO.setYesterDayCount(totalAmount / 100);
+					
+					returnVO.setDepositedAmount(paidAmount);
+					returnVO.setRemainingAmount(pendingAmount);
+					returnVO.setActualAmount(totalAmount);
+				}
+			}
+			
+			cal.add(Calendar.DATE, -7); 
+			Date oneWeekBackDate = cal.getTime();
+			
+			List<Object[]> thisWeekTransactions = cadreTxnDetailsDAO.getTransactionDetailsByDates(today,oneWeekBackDate);
+			
+			if(thisWeekTransactions != null && thisWeekTransactions.size()>0)
+			{
+				for (Object[] transactions : thisWeekTransactions )
+				{
+					Long totalWeekAmount = transactions[2] != null ? Long.valueOf(transactions[2].toString()) :0L;
+					returnVO.setWeekCount(totalWeekAmount / 100);
+				}
+			}
+			
+			List<Object[]> totalTransactions = cadreTxnDetailsDAO.getTransactionDetailsByDates(today,null);
+			
+			if(totalTransactions != null && totalTransactions.size()>0)
+			{
+				for (Object[] totalTransactins : totalTransactions )
+				{
+					Long totalTransactionsCount = totalTransactins[2] != null ? Long.valueOf(totalTransactins[2].toString()) :0L;
+					returnVO.setRecordsCount( totalTransactionsCount / 100 );
+				}
+			}
+			
+			
+			List<Object[]> yesterDayOtpInfo = cadreOtpDetailsDAO.getOtpDetailsForDates(null,yesterDay);
+			Map<String,Long> transactionOtpMap = new TreeMap<String,Long>();
+			transactionOtpMap.put("Y", 0L);
+			transactionOtpMap.put("N", 0L);
+			
+			if(yesterDayOtpInfo != null && yesterDayOtpInfo.size()>0)
+			{
+				for (Object[] otp : yesterDayOtpInfo )
+				{
+					if(transactionOtpMap.get(otp[0] != null ? otp[0].toString().trim():"") != null)
+					{
+						Long count = transactionOtpMap.get(otp[0] != null ? otp[0].toString().trim():"");
+						count = count + ((otp[1] != null ? Long.valueOf(otp[1].toString().trim()):0L) + (otp[2] != null ? Long.valueOf(otp[2].toString().trim()):0L) ) ;
+						
+						transactionOtpMap.put(otp[0] != null ? otp[0].toString().trim():"", count != 0L ? count / 100:0L );
+					}
+				}
+			}
+			
+			if(transactionOtpMap != null && transactionOtpMap.size()>0)
+			{
+				for( String status : transactionOtpMap.keySet()) 
+				{
+					if(status != null && status.trim().length()>0)
+					{
+						if(status.trim().equalsIgnoreCase("Y"))
+						{
+							returnVO.setOtpConfirmCount(transactionOtpMap.get(status.trim()));
+						}
+						if(status.trim().equalsIgnoreCase("N"))
+						{
+							returnVO.setRemainingOTPCount(transactionOtpMap.get(status.trim()));
+						}
+					}
+				}
+			}
+			
+			Long totalOTPGeneratedCount = returnVO.getOtpConfirmCount() + returnVO.getRemainingOTPCount();
+			returnVO.setOtpRequestCount(totalOTPGeneratedCount);
+			
+			List<Object[]> otpNotRequestedTxnsDetailsList = cadreOtpDetailsDAO.getOfflineTxnDetailsIdsForDates(null,yesterDay);
+			
+			Map<String,Long> transactionNONOtpMap = new TreeMap<String,Long>();
+			transactionNONOtpMap.put("Y", 0L);
+			transactionNONOtpMap.put("N", 0L);
+			
+			if(otpNotRequestedTxnsDetailsList != null && otpNotRequestedTxnsDetailsList.size()>0)
+			{
+				Long totalRecords = 0L;
+				for (Object[] txnInfo : otpNotRequestedTxnsDetailsList)
+				{
+					if(transactionNONOtpMap.get(txnInfo[0] != null ? txnInfo[0].toString().trim():"") != null)
+					{
+						Long count = transactionNONOtpMap.get(txnInfo[0] != null ? txnInfo[0].toString().trim():"");
+						count = count+((txnInfo[1] != null ? Long.valueOf(txnInfo[1].toString().trim()):0L));
+						totalRecords = totalRecords + count;
+						
+						transactionNONOtpMap.put(txnInfo[0] != null ? txnInfo[0].toString().trim():"",count != 0L ? count / 100:0L );
+					}
+				}
+				
+				returnVO.setOtpNonRequestCount(totalRecords != 0L ? totalRecords:0L );
+			}
+			
+			if(transactionNONOtpMap != null && transactionNONOtpMap.size()>0)
+			{
+				for( String txnStatus : transactionNONOtpMap.keySet()) 
+				{
+					if(txnStatus != null && txnStatus.trim().length()>0)
+					{
+						if(txnStatus.trim().equalsIgnoreCase("Y"))
+						{
+							returnVO.setNonOTPConfirmCount(transactionOtpMap.get(txnStatus.trim()));
+						}
+						if(txnStatus.trim().equalsIgnoreCase("N"))
+						{
+							returnVO.setRemainingNonOTPCount(transactionOtpMap.get(txnStatus.trim()));
+						}
+					}
+				}
+			}
+			
+			Long notPaidRecords = returnVO.getYesterDayCount() - (returnVO.getOtpConfirmCount() + returnVO.getNonOTPConfirmCount());
+			
+			returnVO.setPendingCount(notPaidRecords);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
