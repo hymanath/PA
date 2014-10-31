323b8ee4fa822443f69de3aa1d9b781b1bacc01a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-10-31 08:08:11 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreSurveyTransactionService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreSurveyTransactionService.java
@@ -1,9 +1,18 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.CadreTransactionVO;
 import com.itgrids.partyanalyst.dto.ReconciliationVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.dto.SurveyTransactionVO;
 
 public interface ICadreSurveyTransactionService {
+
+	public SurveyTransactionVO getCadreSurveyTransactionDetails();	
+	public SurveyTransactionVO getDayWiseReportByDates(String fromDateStr , String toDateStr);	
+	public SurveyTransactionVO getLocationWiseTransactionDetails(String fromDateStr , String toDateStr, String searchType, List<Long> locationIdList);	
+	public List<SelectOptionVO> getParliamentsForState(String electionType,Long stateTypeId);
 	
 	public String genarateOTPAndSaveTxnDetails(CadreTransactionVO inputVO);
 	public String updateTxnStatus(CadreTransactionVO inputVo);
