1517c58fa4aec6b8e07fb366d2c8fb22c8c75812
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-08 19:47:49 +0000

    developed by Balu

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegAmountDetailsDAOHibernateTest.java
@@ -0,0 +1,17 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICadreRegAmountDetailsDAO;
+
+public class CadreRegAmountDetailsDAOHibernateTest extends BaseDaoTestCase {
+
+	private ICadreRegAmountDetailsDAO cadreRegAmountDetailsDAO;
+
+	public void setCadreRegAmountDetailsDAO(
+			ICadreRegAmountDetailsDAO cadreRegAmountDetailsDAO) {
+		this.cadreRegAmountDetailsDAO = cadreRegAmountDetailsDAO;
+	}
+	
+	
+}
---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegAmountFileDAOHibernateTest.java
@@ -0,0 +1,18 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICadreRegAmountFileDAO;
+import com.itgrids.partyanalyst.dao.IConstituencyDAO;
+
+public class CadreRegAmountFileDAOHibernateTest extends BaseDaoTestCase{
+
+	private ICadreRegAmountFileDAO cadreRegAmountFileDAO;
+
+	public void setCadreRegAmountFileDAO(
+			ICadreRegAmountFileDAO cadreRegAmountFileDAO) {
+		this.cadreRegAmountFileDAO = cadreRegAmountFileDAO;
+	}
+	
+	
+}
