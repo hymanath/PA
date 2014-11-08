2f2f2677e65ccde3ea004b334eda5abb43e04b85
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-08 19:46:34 +0000

    developed by Balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegAmountDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CadreRegAmountDetails;
+
+public interface ICadreRegAmountDetailsDAO extends GenericDao<CadreRegAmountDetails, Long>{
+
+}
