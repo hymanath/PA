a2ebd5c8a50245a09a2d252ec5181cae0c46ca8c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-09 02:29:18 +0000

    updated by sandeep

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegAmountUploadService.java
@@ -0,0 +1,5 @@
+package com.itgrids.partyanalyst.service;
+
+public interface ICadreRegAmountUploadService {
+	public void getDashBoardBasicInfo(String date,String fileUrl);
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreRegAmountUploadService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ICadreRegAmountUploadService;
+
+public class CadreRegAmountUploadService implements ICadreRegAmountUploadService{
+	public void getDashBoardBasicInfo(String date,String fileUrl){
+		System.out.println(date + " " + fileUrl);
+		
+	}
+}
