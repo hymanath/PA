3d16a1014bfe79c5922d61897bdf33de24b7d060
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-09 04:29:00 +0000

    CADRE AMOUNT DETAILS -- SASI

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreRegistrationAmountReportAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreRegistrationAmountReportAction.java
@@ -1,12 +1,17 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.CadreAmountDetailsVO;
 import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.service.ICadreRegAmountDetailsService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -15,7 +20,20 @@ public class CadreRegistrationAmountReportAction extends ActionSupport{
 	private static final Logger         		LOG = Logger.getLogger(CadreRegistrationAmountReportAction.class);
 	private HttpSession 						session;
 	private HttpServletRequest                  request;
+	private JSONObject jObj;
+	private String task; 
+	private List<CadreAmountDetailsVO> cadreAmountList;
+	private ICadreRegAmountDetailsService cadreRegAmountDetailsService;
+	
+	
 
+	public ICadreRegAmountDetailsService getCadreRegAmountDetailsService() {
+		return cadreRegAmountDetailsService;
+	}
+	public void setCadreRegAmountDetailsService(
+			ICadreRegAmountDetailsService cadreRegAmountDetailsService) {
+		this.cadreRegAmountDetailsService = cadreRegAmountDetailsService;
+	}
 	public HttpSession getSession() {
 		return session;
 	}
@@ -30,11 +48,27 @@ public class CadreRegistrationAmountReportAction extends ActionSupport{
 	public void setRequest(HttpServletRequest request) {
 		this.request = request;
 	}
-
-	public static Logger getLog() {
-		return LOG;
+	
+	public JSONObject getjObj() {
+		return jObj;
 	}
-
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	
+	
+	public List<CadreAmountDetailsVO> getCadreAmountList() {
+		return cadreAmountList;
+	}
+	public void setCadreAmountList(List<CadreAmountDetailsVO> cadreAmountList) {
+		this.cadreAmountList = cadreAmountList;
+	}
 	public String execute(){
 		try {
 			LOG.info("Entered into execute method in CadreRegistrationAmountReportAction Action");
@@ -56,11 +90,14 @@ public class CadreRegistrationAmountReportAction extends ActionSupport{
 			String endDate = request.getParameter("endDate");
 			String reportValue = request.getParameter("reportValue");
 			
+			cadreAmountList = cadreRegAmountDetailsService.getCadreRegAmountDetails(startDate,endDate,reportValue);
+			
 		}
 		catch(Exception e){
 			LOG.info("Entered into execute method in  CadreRegistrationAmountReportAction Action");
 		}
 		return Action.SUCCESS;
 	}
+	
 	
 }
