532949492251294fc34bae493db47db69873b54f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-09 00:05:21 +0000

    updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegAmountDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegAmountDetailsService.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.service;
 
+import com.itgrids.partyanalyst.dto.CadreRegAmountUploadVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+
 public interface ICadreRegAmountDetailsService {
 
+	public ResultStatus uploadCadreRegAmountDetails(CadreRegAmountUploadVO cadreRegAmountUploadVO);
 }
