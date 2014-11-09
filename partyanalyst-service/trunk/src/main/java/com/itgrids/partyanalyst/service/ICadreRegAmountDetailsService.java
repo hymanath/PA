6460e3ffa96692b62e4823644db18c5b9e3bf9cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-09 04:23:27 +0000

    CADRE AMOUNT DETAILS -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegAmountDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegAmountDetailsService.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.CadreAmountDetailsVO;
 import com.itgrids.partyanalyst.dto.CadreRegAmountUploadVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface ICadreRegAmountDetailsService {
 
 	public ResultStatus uploadCadreRegAmountDetails(CadreRegAmountUploadVO cadreRegAmountUploadVO);
+	public List<CadreAmountDetailsVO> getCadreRegAmountDetails(String fromDt,String toDt,String reportValue);
 }
