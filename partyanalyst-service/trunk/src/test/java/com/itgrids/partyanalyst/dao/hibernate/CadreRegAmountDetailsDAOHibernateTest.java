6c9db7870e2bbceb434c2cd6b86f13b06f8b5942
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-10 03:42:15 +0000

    CADRE AMOUNT DETAILS -- SASI

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegAmountDetailsDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegAmountDetailsDAOHibernateTest.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ICadreRegAmountDetailsDAO;
@@ -13,5 +17,37 @@ public class CadreRegAmountDetailsDAOHibernateTest extends BaseDaoTestCase {
 		this.cadreRegAmountDetailsDAO = cadreRegAmountDetailsDAO;
 	}
 	
+	/*public void test(){
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			String dt = "2014-10-02";
+			String tdt = "2014-10-30";
+			Date fromDate = format.parse(dt);
+			Date toDate = format.parse(tdt);
+			List<Object[]> list = cadreRegAmountDetailsDAO.getAmountDetailsOfUser(fromDate,toDate);
+			list.size();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+	
+	/*public void test1(){
+		try {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			String dt = "2014-09-30";
+			Date amountDate = format.parse(dt);
+			List<Object[]> list = cadreRegAmountDetailsDAO.getAmountDetailsOfUser(amountDate);
+			list.size();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+	
+	public void test(){
+		List<Object[]> list = cadreRegAmountDetailsDAO.getAmountDetailsDateWise();
+		System.out.println(list.size());
+	}
 	
 }
