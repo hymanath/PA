aa917af11bda3d1eeb3cf0e162149e4ade747ac7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-10 06:37:54 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserAssigneeDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICadreSurveyUserAssigneeDAO;
+
+public class CadreSurveyUserAssigneeDAOHibernateTest extends BaseDaoTestCase{
+	private ICadreSurveyUserAssigneeDAO cadreSurveyUserAssigneeDAO;
+
+	public void setCadreSurveyUserAssigneeDAO(
+			ICadreSurveyUserAssigneeDAO cadreSurveyUserAssigneeDAO) {
+		this.cadreSurveyUserAssigneeDAO = cadreSurveyUserAssigneeDAO;
+	}
+	
+
+}
