5bdd12cf0a46400cadf7cbaac3427d99dfac776a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-11 00:28:28 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserAssignDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserAssignDetailsDAO.java
@@ -76,6 +76,12 @@ public class CadreSurveyUserAssignDetailsDAO extends GenericDaoHibernate<CadreSu
 		return query.list();
 	}
 	
+	public List<Object[]> getCandidatesInfo(String queryStr)
+	{
+		Query query = getSession().createQuery(queryStr);
+		return query.list();
+	}
+	
 	public List<Object[]> getTabNos(List<Long> cadreSurveyUserIds)
 	{
 		Query query = getSession().createQuery("select model.cadreSurveyUserId,model.tabNo from CadreSurveyUserAssignDetails model where model.cadreSurveyUserId in (:cadreSurveyUserIds) and model.isDeleted = 'N'");
