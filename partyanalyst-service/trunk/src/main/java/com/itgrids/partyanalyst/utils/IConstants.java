b996476fc266074ac7a9950b1ff6c61837e139cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-13 01:25:03 +0000

    Target Percentage -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -815,4 +815,11 @@ public interface IConstants {
     public static final String DATE_AND_TIME_FORMAT_24HRS = "yyyy-MM-dd HH:mm:ss";
     
     public static final String CADRE_MEMBERSHIPCARD_DISPATCHER = "CADRE_MEMBERSHIPCARD_DISPATCHER";
+    
+    public static final int AP_VOTERS_2014 = 36467129;
+    public static final int TG_VOTERS_2014 = 27922932;
+    
+    public static final int TARGET_CADRE_AP = 2000000;
+    public static final int TARGET_CADRE_TG = 1000000;
+    		
 }
