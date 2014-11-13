d95bfa3815a90027dfa55568ecad63d0c2e287bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-13 03:12:26 +0000

    ILeaderCadreDashBoardService created by sandeep.

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ILeaderCadreDashBoardService.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.CallCenterVO;
+import com.itgrids.partyanalyst.dto.ProblemBeanVO;
+
+public interface ILeaderCadreDashBoardService {
+
+	public void testMethod();
+	
+}
