33b8571aca7a3153f0d217b7532448716543affd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-13 03:17:27 +0000

    LeaderCadreDashBoardAction created by sandeep

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LeaderCadreDashBoardAction.java
@@ -0,0 +1,58 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
+
+import com.itgrids.partyanalyst.helper.EntitlementsHelper;
+import com.itgrids.partyanalyst.service.ILeaderCadreDashBoardService;
+import com.opensymphony.xwork2.Action;
+
+public class LeaderCadreDashBoardAction implements ServletRequestAware {
+
+	private static Logger LOG = Logger.getLogger(LeaderCadreDashBoardAction.class);
+	
+	private HttpServletRequest request;
+	
+	private EntitlementsHelper entitlementsHelper;
+	private JSONObject jObj;
+	private String task;
+	private ILeaderCadreDashBoardService leaderCadreDashBoardService;
+	
+		public String getTask() {
+		return task;
+	}
+
+	public ILeaderCadreDashBoardService getLeaderCadreDashBoardService() {
+			return leaderCadreDashBoardService;
+		}
+
+		public void setLeaderCadreDashBoardService(
+				ILeaderCadreDashBoardService leaderCadreDashBoardService) {
+			this.leaderCadreDashBoardService = leaderCadreDashBoardService;
+		}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public EntitlementsHelper getEntitlementsHelper() {
+		return entitlementsHelper;
+	}
+
+	public void setEntitlementsHelper(EntitlementsHelper entitlementsHelper) {
+		this.entitlementsHelper = entitlementsHelper;
+	}
+ 
+	public String execute(){
+		LOG.info("Entered into execute() in LeaderCadreDashBoardActioon class");
+		leaderCadreDashBoardService.testMethod();
+		return Action.SUCCESS;
+	}
+}
