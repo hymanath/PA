0aaf09866d57b7b0fa1e2abe9348fdb30fef8777
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-13 13:01:10 +0000

    updated by nagamani

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LeaderCadreDashBoardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LeaderCadreDashBoardAction.java
@@ -1,11 +1,14 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.CadreAmountDetailsVO;
 import com.itgrids.partyanalyst.helper.EntitlementsHelper;
 import com.itgrids.partyanalyst.service.ILeaderCadreDashBoardService;
 import com.opensymphony.xwork2.Action;
@@ -20,10 +23,22 @@ public class LeaderCadreDashBoardAction implements ServletRequestAware {
 	private JSONObject jObj;
 	private String task;
 	private ILeaderCadreDashBoardService leaderCadreDashBoardService;
+	private List<CadreAmountDetailsVO> amountDetails;
 	
 		public String getTask() {
 		return task;
 	}
+
+		
+	public List<CadreAmountDetailsVO> getAmountDetails() {
+			return amountDetails;
+		}
+
+
+		public void setAmountDetails(List<CadreAmountDetailsVO> amountDetails) {
+			this.amountDetails = amountDetails;
+		}
+
 
 	public ILeaderCadreDashBoardService getLeaderCadreDashBoardService() {
 			return leaderCadreDashBoardService;
@@ -53,6 +68,18 @@ public class LeaderCadreDashBoardAction implements ServletRequestAware {
 	public String execute(){
 		LOG.info("Entered into execute() in LeaderCadreDashBoardActioon class");
 		leaderCadreDashBoardService.testMethod();
+		return Action.SUCCESS;
+	}
+	
+	public String getLoationWiseLeaderCadreInfo()
+	{
+		try{
+			jObj = new JSONObject(getTask());
+			amountDetails = leaderCadreDashBoardService.getLoationWiseLeaderCadreDetails(jObj.getString("type"),jObj.getLong("stateId"));
+		}
+		catch (Exception e) {
+			LOG.info("Entered into getLoationLeaderCadreInfo() in LeaderCadreDashBoardActioon class");
+		}
 		return Action.SUCCESS;
 	}
 }
