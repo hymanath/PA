6ee852e52558a383f9066344e09b1906cb4075b9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-14 14:55:53 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITabRecordsStatusDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TabRecordsStatus;
+
+public interface ITabRecordsStatusDAO extends GenericDao<TabRecordsStatus, Long>{
+
+}
