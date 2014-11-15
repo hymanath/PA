0634d5886edc7675519103cfef024661bbf7551b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-15 04:23:02 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreSurveyUserAssignDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreSurveyUserAssignDetailsDAO.java
@@ -18,7 +18,7 @@ public interface ICadreSurveyUserAssignDetailsDAO extends GenericDao<CadreSurvey
 	public List<Object[]> getCadreSurveyAssignConstituencyList();
 	
 	public List<Long> checkIsAlreadyAssigned(Long cadreSurveyUserId, Long levelId,Long levelValue, Long constituencyId);
-	public List<Object[]> getCandidatesInfo(String queryStr);
+	public List<Object[]> getCandidatesInfo(String queryStr,List<Long> locationIdsList);
 	public List<Long> isTabAssignedAlready(String tabNo);
 	public List<Object[]> getTabNos(List<Long> cadreSurveyUserIds);
 	public List<Object[]> getCadreSurveyUsers(Long constituencyId);
