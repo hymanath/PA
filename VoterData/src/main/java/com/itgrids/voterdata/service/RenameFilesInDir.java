346a0d68c8c8e323beb35a6910bbcc46d5dc9f21
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-22 01:48:13 +0000

    updated by Kamalakar Dandu

---
+++ VoterData/src/main/java/com/itgrids/voterdata/service/RenameFilesInDir.java
@@ -0,0 +1,38 @@
+package com.itgrids.voterdata.service;
+
+import java.io.File;
+
+public class RenameFilesInDir {
+
+	public static void main(String[] args)
+	{
+		RenameFilesInDir renameFilesInDir = new RenameFilesInDir();
+		renameFilesInDir.rename(args[0]);
+	}
+	public void rename(String dirPath)
+	{
+		try{
+			File dir = new File(dirPath);
+			if(dir.isDirectory())
+			{
+				for(File file : dir.listFiles())
+				{
+					if(!file.isDirectory()){
+						try{
+							String fileName = file.getName();
+							fileName = fileName.substring(0,fileName.length()-1);
+							if(file.renameTo(new File(fileName)))
+								System.out.println("File "+fileName+" is Renamed");
+							else
+								System.out.println("Error in renaming the file "+fileName);
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
