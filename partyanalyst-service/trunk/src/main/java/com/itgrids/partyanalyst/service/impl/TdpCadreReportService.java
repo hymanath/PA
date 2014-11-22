96eb90fc4f2745dfd8fec32a8f761d166e89d115
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-11-22 14:29:39 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/TdpCadreReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/TdpCadreReportService.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -40,6 +41,7 @@ import com.itgrids.partyanalyst.dao.IZebraPrintDetailsDAO;
 import com.itgrids.partyanalyst.dto.CadreRegisterInfo;
 import com.itgrids.partyanalyst.dto.SurveyTransactionVO;
 import com.itgrids.partyanalyst.dto.TdpCadreLocationWiseReportVO;
+import com.itgrids.partyanalyst.dto.ZebraPrintDetailsVO;
 import com.itgrids.partyanalyst.service.ICadreDashBoardService;
 import com.itgrids.partyanalyst.service.ITdpCadreReportService;
 import com.itgrids.partyanalyst.utils.IConstants;
@@ -48,7 +50,6 @@ public class TdpCadreReportService implements ITdpCadreReportService{
 
 	private static final Logger LOG = Logger.getLogger(CadreRegistrationService.class);
 	
-	private ITdpCadreReportService tdpCadreReportService;
 	private ITdpCadreDAO tdpCadreDAO;
 	private IBoothDAO boothDAO;
 	private IVoterInfoDAO voterInfoDAO;
@@ -73,12 +74,7 @@ public class TdpCadreReportService implements ITdpCadreReportService{
 	public void setVoterAgeInfoDAO(IVoterAgeInfoDAO voterAgeInfoDAO) {
 		this.voterAgeInfoDAO = voterAgeInfoDAO;
 	}
-	public void setTdpCadreReportService(
-			ITdpCadreReportService tdpCadreReportService) {
-		this.tdpCadreReportService = tdpCadreReportService;
-	}
-	
-	
+
 	public ITdpCadreDAO getTdpCadreDAO() {
 		return tdpCadreDAO;
 	}
@@ -1338,6 +1334,7 @@ public class TdpCadreReportService implements ITdpCadreReportService{
 		
 		return constituencyReportVO1;
 	}
+	/*
 	public SurveyTransactionVO getMemberShipCardPrintDetails(Long districtId,List<Long> constituencyIds,String fromDateStr,String toDateStr)
 	{
 		SurveyTransactionVO returnVO = new SurveyTransactionVO();
@@ -1455,7 +1452,7 @@ public class TdpCadreReportService implements ITdpCadreReportService{
 		
 		return returnVo;
 	}
-	
+	*/
 	public SurveyTransactionVO getConstituencyDetailsInDistricts(List<Long> districtIdList)
 	{
 		SurveyTransactionVO returnVO = new SurveyTransactionVO();
@@ -1490,4 +1487,302 @@ public class TdpCadreReportService implements ITdpCadreReportService{
 		
 		return returnVO;
 	}
+	
+	public ZebraPrintDetailsVO getMemberShipCardPrintDetails(String searchType,Long stateTypeId,List<Long> selectedLocationIds,String fromDateStr,String toDateStr)
+	{
+
+		ZebraPrintDetailsVO returnVO = new ZebraPrintDetailsVO();
+		
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+
+			if(searchType != null && (selectedLocationIds != null && selectedLocationIds.size()>0))
+			{
+				
+				Date fromDate = format.parse(fromDateStr);
+				Date toDate = format.parse(toDateStr);
+				StringBuilder queryStr = new StringBuilder();
+				List<Object[]> printStatusDetailsList = null;
+				List<ZebraPrintDetailsVO> printStatusList = null;
+				
+				Map<Long,String> parliamentForAssemblyMap = new TreeMap<Long, String>();
+				List<Object[]> locationsList =  delimitationConstituencyAssemblyDetailsDAO.getPcListByRegion(stateTypeId);							
+					
+				List<Long> locationIdList = new ArrayList<Long>(0);
+				List<Long> assemblyIds = new ArrayList<Long>(0);
+				
+				if(locationsList != null && locationsList.size()>0)
+				{
+					for (Object[] param : locationsList)
+					{
+						locationIdList.add(param[0] != null ? Long.valueOf(param[0].toString()) :0L);
+					}
+				}
+						
+				if(locationIdList != null && locationIdList.size()>0)
+				{
+						locationsList = delimitationConstituencyAssemblyDetailsDAO.findAssembliesConstituenciesListForAListOfParliamentConstituency(locationIdList);
+												
+						if(locationsList != null && locationsList.size()>0)
+						{
+							for (Object[] param : locationsList)
+							{
+								assemblyIds.add(param[0] != null ? Long.valueOf(param[0].toString().trim()):0L );
+								parliamentForAssemblyMap.put(param[0] != null ? Long.valueOf(param[0].toString().trim()):0L , param[2] != null ? param[2].toString().trim() :"");
+							}
+						}	
+				}
+				
+				
+				if(searchType.equalsIgnoreCase(IConstants.CONSTITUENCY) || searchType.equalsIgnoreCase(IConstants.STATE))
+				{
+					if( searchType.equalsIgnoreCase(IConstants.STATE))
+					{
+						List<Object[]> constituencyList = constituencyDAO.getAllAssemblyConstituenciesByStateTypeId(stateTypeId, 1L, null);
+						if(constituencyList != null && constituencyList.size()>0)
+						{
+							selectedLocationIds.clear();
+							for (Object[] cosntituency : constituencyList)
+							{
+								selectedLocationIds.add(cosntituency[0] != null? Long.valueOf(cosntituency[0].toString().trim()):0L);
+							}
+						}
+					}
+					queryStr.append(" select UA.constituency.constituencyId, UA.constituency.name, UA.district.districtName, date(ZPD.insertedTime), count(ZPD.zebraPrintDetailsId) ,  ");
+					queryStr.append(" date(ZPD.updatedTime), ZPD.printStatus, count(ZPD.printStatus), ZPD.errorStatus, count(ZPD.errorStatus)  ");
+					queryStr.append(" from ZebraPrintDetails ZPD, TdpCadre TD, UserAddress UA where ");
+					
+					queryStr.append(" TD.tdpCadreId = ZPD.tdpCadreId and TD.userAddress.userAddressId = UA.userAddressId and ((ZPD.printStatus !='' or ZPD.printStatus is null) or ( ZPD.errorStatus !='' or ZPD.errorStatus is null )) and ");
+					queryStr.append(" TD.isDeleted = 'N' and TD.enrollmentYear = 2014 and UA.constituency.constituencyId in (:locationIdList) ");
+					
+					if(fromDate != null && toDate != null)
+					{
+						queryStr.append(" and ( date(ZPD.insertedTime) >= :fromDate and date(ZPD.insertedTime) <= :toDate ) ");
+					}
+					
+					queryStr.append(" group by UA.constituency.constituencyId, date(ZPD.insertedTime) , date(ZPD.updatedTime), ZPD.printStatus, ZPD.errorStatus  order by date(ZPD.updatedTime) asc ");
+					
+				}
+				else if(searchType.equalsIgnoreCase(IConstants.DISTRICT))
+				{
+					queryStr.append(" select UA.district.districtId,UA.district.districtName, UA.district.districtName, date(ZPD.insertedTime), count(ZPD.zebraPrintDetailsId) ,  ");
+					queryStr.append(" date(ZPD.updatedTime), ZPD.printStatus, count(ZPD.printStatus), ZPD.errorStatus, count(ZPD.errorStatus)  ");
+					queryStr.append(" from ZebraPrintDetails ZPD, TdpCadre TD, UserAddress UA where ");
+					
+					queryStr.append(" TD.tdpCadreId = ZPD.tdpCadreId and TD.userAddress.userAddressId = UA.userAddressId and ((ZPD.printStatus !='' or ZPD.printStatus is null) or ( ZPD.errorStatus !='' or ZPD.errorStatus is null )) and ");
+					queryStr.append(" TD.isDeleted = 'N' and TD.enrollmentYear = 2014 and UA.district.districtId in (:locationIdList) ");
+					
+					if(fromDate != null && toDate != null)
+					{
+						queryStr.append(" and ( date(ZPD.insertedTime) >= :fromDate and date(ZPD.insertedTime) <= :toDate ) ");
+					}
+					
+					queryStr.append(" group by UA.district.districtId, date(ZPD.insertedTime) , date(ZPD.updatedTime), ZPD.printStatus, ZPD.errorStatus  order by date(ZPD.updatedTime) asc ");
+				
+				}
+				else if(searchType.equalsIgnoreCase("parliament"))
+				{
+					queryStr.append(" select distinct model2.delimitationConstituency.constituency.constituencyId,model2.delimitationConstituency.constituency.name, model2.delimitationConstituency.constituency.district.districtName, date(ZPD.insertedTime), count(ZPD.zebraPrintDetailsId) ,  ");
+					queryStr.append(" date(ZPD.updatedTime), ZPD.printStatus, count(ZPD.printStatus), ZPD.errorStatus, count(ZPD.errorStatus)  ");
+					queryStr.append(" from ZebraPrintDetails ZPD, TdpCadre TD, UserAddress UA , DelimitationConstituencyAssemblyDetails model2 where ");
+					queryStr.append(" UA.constituency.constituencyId = model2.constituency.constituencyId and model2.delimitationConstituency.year = 2009 and ");
+					queryStr.append(" TD.tdpCadreId = ZPD.tdpCadreId and TD.userAddress.userAddressId = UA.userAddressId and ((ZPD.printStatus !='' or ZPD.printStatus is null) or ( ZPD.errorStatus !='' or ZPD.errorStatus is null )) and ");
+					queryStr.append(" TD.isDeleted = 'N' and TD.enrollmentYear = 2014 and model2.delimitationConstituency.constituency.constituencyId in (:locationIdList) ");
+					
+					if(fromDate != null && toDate != null)
+					{
+						queryStr.append(" and ( date(ZPD.insertedTime) >= :fromDate and date(ZPD.insertedTime) <= :toDate ) ");
+					}
+					
+					queryStr.append(" group by model2.delimitationConstituency.constituency.constituencyId, date(ZPD.insertedTime) , date(ZPD.updatedTime), ZPD.printStatus, ZPD.errorStatus  order by date(ZPD.updatedTime) asc ");
+				
+				}
+				printStatusDetailsList = zebraPrintDetailsDAO.getMemberShipCardPushStatusByDate(selectedLocationIds, fromDate, toDate,queryStr.toString());
+				Map<Long,ZebraPrintDetailsVO> zebraPrintMap = new HashMap<Long, ZebraPrintDetailsVO>(0);
+				
+				Map<String,ZebraPrintDetailsVO>  daywiseVOMap = new TreeMap<String, ZebraPrintDetailsVO>();
+				if(printStatusDetailsList != null && printStatusDetailsList.size()>0)
+				{
+					for (Object[] printStatus : printStatusDetailsList) 
+					{
+						ZebraPrintDetailsVO locationVO = null;
+						
+						if(zebraPrintMap.get(printStatus[0] != null ? Long.valueOf(printStatus[0].toString().trim()):0L) != null)
+						{
+							locationVO = zebraPrintMap.get(printStatus[0] != null ? Long.valueOf(printStatus[0].toString().trim()):0L);
+							printStatusList = locationVO.getZebraPrintDetailsVOList();
+						}
+						else
+						{
+							locationVO = new ZebraPrintDetailsVO();
+							locationVO.setId(printStatus[0] != null ? Long.valueOf(printStatus[0].toString().trim()):0L);
+							locationVO.setName(printStatus[1] != null ? printStatus[1].toString().trim():"");
+							locationVO.setDistrict(printStatus[2] != null ? printStatus[2].toString().trim():"");
+							locationVO.setParliament(parliamentForAssemblyMap.get(locationVO.getId()) != null ? parliamentForAssemblyMap.get(locationVO.getId()):"");
+							locationVO.setDataPushDate(printStatus[3] != null ? format.format(dateFormat.parse(printStatus[3] != null ? printStatus[3].toString().trim():"")):null);
+							printStatusList = new ArrayList<ZebraPrintDetailsVO>();
+						}
+						
+						ZebraPrintDetailsVO reportVO = new ZebraPrintDetailsVO();
+						
+						reportVO.setDataPushDate(printStatus[3] != null ? format.format(dateFormat.parse(printStatus[3] != null ? printStatus[3].toString().trim():"")):null);
+						reportVO.setTotalPushCount(printStatus[4] != null ? Long.valueOf(printStatus[4].toString().trim()):0L);
+						
+						reportVO.setUpdatedDate(printStatus[5] != null ? format.format(dateFormat.parse(printStatus[5] != null ? printStatus[5].toString().trim():"")):null);
+						reportVO.setPrintStatus(printStatus[6] != null ? printStatus[6].toString().trim():null);
+						reportVO.setPrintStatusCount(printStatus[7] != null ? Long.valueOf(printStatus[7].toString().trim()):0L);
+						
+						reportVO.setErrorStatus(printStatus[8] != null ? printStatus[8].toString().trim():null);
+						reportVO.setErrorStatusCount(printStatus[9] != null ? Long.valueOf(printStatus[9].toString().trim()):0L);
+						
+						if(reportVO.getPrintStatus() == null || reportVO.getPrintStatus().trim().length()>0)
+						{
+							printStatusList.add(reportVO);
+							
+							locationVO.setZebraPrintDetailsVOList(printStatusList);
+							
+							zebraPrintMap.put(locationVO.getId(), locationVO);
+							
+							if(reportVO.getUpdatedDate() != null)
+							{
+								daywiseVOMap.put(reportVO.getUpdatedDate(), null);
+							}
+						}
+					}
+				}
+				
+				List<ZebraPrintDetailsVO> finalReturnList = new ArrayList<ZebraPrintDetailsVO>();
+				if(zebraPrintMap != null && zebraPrintMap.size()>0)
+				{
+					for (Long constituencyId : zebraPrintMap.keySet()) 
+					{
+						ZebraPrintDetailsVO constituencyVO = zebraPrintMap.get(constituencyId);
+						printStatusList = new ArrayList<ZebraPrintDetailsVO>();
+						Map<String,ZebraPrintDetailsVO>  daywiseCountMap = new TreeMap<String, ZebraPrintDetailsVO>();
+								
+						if(constituencyVO.getZebraPrintDetailsVOList() != null && constituencyVO.getZebraPrintDetailsVOList().size()>0)
+						{
+							Long totalCount = 0L;
+							Long printCount = 0L;
+							Long errorCount = 0L;
+							
+							if(daywiseVOMap != null && daywiseVOMap.size()>0)
+							{
+								for (ZebraPrintDetailsVO totalCountVO : constituencyVO.getZebraPrintDetailsVOList()) 
+								{
+									totalCount = totalCount + totalCountVO.getTotalPushCount();
+								}
+								for (String date : daywiseVOMap.keySet()) 
+								{
+									ZebraPrintDetailsVO updatedVO = null;
+									ZebraPrintDetailsVO filterVO  = getMatchedZebraPrintDetailsVOByDate(constituencyVO.getZebraPrintDetailsVOList(),date);
+									
+									if(filterVO != null)
+									{										
+										
+										if(filterVO != null && filterVO.getUpdatedDate() != null )
+										{				
+											if(daywiseCountMap.get(filterVO.getUpdatedDate().trim()) != null)
+											{
+												updatedVO = daywiseCountMap.get(filterVO.getUpdatedDate().trim());
+												printCount = updatedVO.getPrintStatusCount();
+												errorCount = updatedVO.getErrorStatusCount();
+											}
+											else
+											{
+												updatedVO = new ZebraPrintDetailsVO();
+											}
+											
+											if(filterVO.getPrintStatus() != null)
+											{
+												printCount = printCount + filterVO.getPrintStatusCount();
+											}
+																					
+											if(filterVO.getErrorStatus() != null)
+											{
+												errorCount = errorCount + filterVO.getErrorStatusCount();
+											}
+											
+											updatedVO.setUpdatedDate(filterVO.getUpdatedDate());
+											updatedVO.setPrintStatusCount(printCount);
+											updatedVO.setErrorStatusCount(errorCount);
+											
+											daywiseCountMap.put(date.trim(), updatedVO);
+											
+										}
+									}									
+									else
+									{
+										updatedVO = new ZebraPrintDetailsVO();
+										updatedVO.setUpdatedDate(" -- ");
+										updatedVO.setPrintStatusCount(0L);
+										updatedVO.setErrorStatusCount(0L);
+										
+										daywiseCountMap.put(date.trim(), updatedVO);
+									}
+								
+								}
+							}
+							
+							if(daywiseCountMap != null && daywiseCountMap.size()>0)
+							{
+								for (String updatedDate : daywiseCountMap.keySet()) 
+								{
+									printStatusList.add(daywiseCountMap.get(updatedDate));
+								}
+							}
+							
+							constituencyVO.setTotalPushCount(totalCount);
+							constituencyVO.setZebraPrintDetailsVOList(printStatusList);
+						}
+						
+						
+
+						finalReturnList.add(constituencyVO);
+					}
+				}
+				
+				System.out.println(finalReturnList.size());
+				if(daywiseVOMap != null && daywiseVOMap.size()>0)
+				{
+					returnVO.getDatesList().addAll(daywiseVOMap.keySet());
+				}
+				returnVO.setZebraPrintDetailsVOList(finalReturnList);
+			}
+		}
+		catch (Exception e)
+		{
+			LOG.error(" exception occured in getMemberShipCardPrintStatusDetails()  @ TdpCadreReportService class.",e);
+		}
+		
+		return returnVO;
+	}
+
+	public ZebraPrintDetailsVO getMatchedZebraPrintDetailsVOByDate(List<ZebraPrintDetailsVO> reportVOList,String dateStr)
+	{
+		ZebraPrintDetailsVO returnVo = null;
+		try {
+			
+			if(reportVOList != null && reportVOList.size()>0)
+			{
+				for (ZebraPrintDetailsVO reportVO : reportVOList) 
+				{
+					if(reportVO.getUpdatedDate() != null)
+					{
+						if(reportVO.getUpdatedDate().trim().equalsIgnoreCase(dateStr.trim()))
+						{
+							return reportVO;
+						}
+					}
+					
+				}
+			}
+		} catch (Exception e) {
+			LOG.error(" exception occured in getMatchedZebraPrintDetailsVOByDate()  @ TdpCadreReportService class.",e);
+		}
+		
+		return returnVo;
+	}
+	
 }
