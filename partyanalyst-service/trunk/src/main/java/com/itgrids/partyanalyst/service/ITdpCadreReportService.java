aa5e43bbb54895f40052056ad602eaa99dd04663
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-01 22:16:07 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITdpCadreReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITdpCadreReportService.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.CadreRegistrationVO;
 import com.itgrids.partyanalyst.dto.SurveyTransactionVO;
 import com.itgrids.partyanalyst.dto.TdpCadreLocationWiseReportVO;
 import com.itgrids.partyanalyst.dto.ZebraPrintDetailsVO;
@@ -12,8 +13,11 @@ public interface ITdpCadreReportService {
 	public ZebraPrintDetailsVO getMemberShipCardPrintDetails(String searchType,Long stateTypeId,List<Long> selectedLocationIds,String fromDateStr,String toDateStr);
 	public SurveyTransactionVO getConstituencyDetailsInDistricts(List<Long> districtIdList);
 	public String updatePrintingStatusInTdpCadreTable();
-	public ZebraPrintDetailsVO createDashBoardForPrintingCardsDetails(String accessType,String accessValue,Long stateTypeId);
+	public ZebraPrintDetailsVO createDashBoardForPrintingCardsDetails(String accessType,String accessValue,Long stateTypeId,String searchType, Long selectedLocationId);
 	public ZebraPrintDetailsVO dashBoardForPrintingCardsDetails(String accessType,String accessValue,Long stateTypeId);
 	
 	public ZebraPrintDetailsVO getCadreDetailsByStatus(Long constituencyId,String status);
+	
+	public List<CadreRegistrationVO> getCadreDetailsInTeluguByMembershipId(String membershipId);
+	
 }
