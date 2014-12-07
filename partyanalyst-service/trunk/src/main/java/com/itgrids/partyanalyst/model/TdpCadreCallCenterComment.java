1efeec57874e3e39ed2930dba7fb3c6d230063f0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-07 12:25:44 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreCallCenterComment.java
@@ -0,0 +1,84 @@
+package com.itgrids.partyanalyst.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+
+
+
+@Entity
+@Table(name = "tdp_cadre_call_center_comment")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class TdpCadreCallCenterComment {
+	
+	
+	private Long tdpCadreCallCenterCommentId;
+	private TdpCadreCallCenterFeedback tdpCadreCallCenterFeedback;
+	private Long tdpCadreCallCenterFeedbackId;
+	private CallCenterFeedback callCenterFeedback;
+	private Long callCenterFeedbackId;
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "tdp_cadre_call_center_comment_id", unique = true, nullable = false)	
+	public Long getTdpCadreCallCenterCommentId() {
+		return tdpCadreCallCenterCommentId;
+	}
+	public void setTdpCadreCallCenterCommentId(Long tdpCadreCallCenterCommentId) {
+		this.tdpCadreCallCenterCommentId = tdpCadreCallCenterCommentId;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "tdp_cadre_call_center_feedback_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	public TdpCadreCallCenterFeedback getTdpCadreCallCenterFeedback() {
+		return tdpCadreCallCenterFeedback;
+	}
+	public void setTdpCadreCallCenterFeedback(
+			TdpCadreCallCenterFeedback tdpCadreCallCenterFeedback) {
+		this.tdpCadreCallCenterFeedback = tdpCadreCallCenterFeedback;
+	}
+	
+	@Column(name="tdp_cadre_call_center_feedback_id")
+	public Long getTdpCadreCallCenterFeedbackId() {
+		return tdpCadreCallCenterFeedbackId;
+	}
+	public void setTdpCadreCallCenterFeedbackId(Long tdpCadreCallCenterFeedbackId) {
+		this.tdpCadreCallCenterFeedbackId = tdpCadreCallCenterFeedbackId;
+	}
+	
+	@Column(name="call_center_feedback_id")
+	public Long getCallCenterFeedbackId() {
+		return callCenterFeedbackId;
+	}
+	public void setCallCenterFeedbackId(Long callCenterFeedbackId) {
+		this.callCenterFeedbackId = callCenterFeedbackId;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "call_center_feedback_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	public CallCenterFeedback getCallCenterFeedback() {
+		return callCenterFeedback;
+	}
+	public void setCallCenterFeedback(CallCenterFeedback callCenterFeedback) {
+		this.callCenterFeedback = callCenterFeedback;
+	}
+	
+	
+	
+	
+}
