b414b4fe6fe68d7dc750c89576352a4e1d1197f1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-09 11:57:04 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/StringUtilService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/StringUtilService.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import com.itgrids.partyanalyst.service.IStringUtilService;
 
 /**
@@ -39,4 +43,83 @@ public class StringUtilService implements IStringUtilService {
 		System.out.println(new StringUtilService().fragmentARegularString(str,index,"-"));
 	}*/
 	
+	
+
+	/*
+	 * Author : Srishailam Pittala
+	 * Date : 9th Dec, 2014
+	 * input : sorted Long Ids
+	 * return  string variable 
+	 * ex: 1,3,4 to 9 (4,5,6,7,8,9) ,11,12,13 to 15 (13,14,15) ,17,19,20 to 25  (20,21,22,23,24,25) , 26
+	 */
+	   public String sortedItemsForSMS(Set<Long> sortedItemsSet) // without duplicates sorted Set
+	   {
+		   String finalStr="";
+		   
+		   try {
+			   if(sortedItemsSet != null && sortedItemsSet.size()>0)
+			   {
+				   List<Long> buildsItems = new ArrayList<Long>();
+				   for (Long item : sortedItemsSet) 
+				   {
+					   buildsItems.add(item);
+				   }
+				   
+				   if(buildsItems != null && buildsItems.size()>0)
+				   {
+					   Long tempItem = 0L;		   
+					   Boolean isNext = false;
+					   Long count = 0L;
+					   for (int i=0;i<buildsItems.size();i++)
+					   {
+						   Long presentItem = buildsItems.get(i);
+
+						   if(i == 0) // first element
+						   {				  
+							   finalStr = presentItem.toString();
+						   }
+						   else
+						   {
+							   if(presentItem != (tempItem+1))
+							   {
+								   if(isNext)
+								   {
+									   if(count >1)
+									   {
+										   finalStr = finalStr+" to "+tempItem.toString()+","+presentItem.toString();
+									   }
+									   else
+									   {
+										   finalStr = finalStr+","+tempItem.toString()+","+presentItem.toString();
+									   }
+								   }
+								   else
+								   {
+									   finalStr = finalStr+","+presentItem.toString();
+								   }
+								   isNext = false;
+								   count = 0L;
+							   }
+							   else
+							   {
+								   count = count+1;
+								   isNext = true;
+								   tempItem = presentItem;
+								   continue;
+							   }
+						   }
+						   tempItem = presentItem;
+					   }
+					   if(isNext)
+					   {
+						   finalStr = finalStr+" to "+tempItem.toString();
+					   }
+					   //System.out.println(finalStr);
+				   }
+			   }
+		} catch (Exception e) {
+			return null;
+		}
+		   return finalStr;
+	   }
 }
