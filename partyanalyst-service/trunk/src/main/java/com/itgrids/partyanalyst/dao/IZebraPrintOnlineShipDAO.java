f863c53c2bd6c0a23ba91f0cd43977a4f8391d23
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-10 15:33:52 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IZebraPrintOnlineShipDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IZebraPrintOnlineShipDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ZebraPrintOnlineShip;
 
 
 public interface IZebraPrintOnlineShipDAO extends GenericDao<ZebraPrintOnlineShip, Long>{
-
+	public List<Object[]> getCadreShippingAddressDetials(Long constituencyId);
 }
