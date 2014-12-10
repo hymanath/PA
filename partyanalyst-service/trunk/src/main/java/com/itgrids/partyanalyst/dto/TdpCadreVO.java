3e08118a0c5aae19574fce8e3441844b33916ce5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-10 12:34:43 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TdpCadreVO.java
@@ -0,0 +1,110 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TdpCadreVO implements java.io.Serializable
+{
+	private Long id;
+	private String cadreName;
+	private String relativeName;
+	private String memberShipNo;
+	private String trNo;
+	private String gender;
+	private Long age;
+	private String voterCardNo;
+	private Long voterId;
+	private String mobileNo;
+	private Long consituencyId;
+	private String imageURL;
+	private String errorStr;
+	
+	private List<TdpCadreVO> tdpCadreDetailsList = new ArrayList<TdpCadreVO>();
+	
+	
+	public String getErrorStr() {
+		return errorStr;
+	}
+	public void setErrorStr(String errorStr) {
+		this.errorStr = errorStr;
+	}
+	public List<TdpCadreVO> getTdpCadreDetailsList() {
+		return tdpCadreDetailsList;
+	}
+	public void setTdpCadreDetailsList(List<TdpCadreVO> tdpCadreDetailsList) {
+		this.tdpCadreDetailsList = tdpCadreDetailsList;
+	}
+	public String getCadreName() {
+		return cadreName;
+	}
+	public void setCadreName(String cadreName) {
+		this.cadreName = cadreName;
+	}
+	public String getRelativeName() {
+		return relativeName;
+	}
+	public void setRelativeName(String relativeName) {
+		this.relativeName = relativeName;
+	}
+	public String getMemberShipNo() {
+		return memberShipNo;
+	}
+	public void setMemberShipNo(String memberShipNo) {
+		this.memberShipNo = memberShipNo;
+	}
+	public String getTrNo() {
+		return trNo;
+	}
+	public void setTrNo(String trNo) {
+		this.trNo = trNo;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getVoterCardNo() {
+		return voterCardNo;
+	}
+	public void setVoterCardNo(String voterCardNo) {
+		this.voterCardNo = voterCardNo;
+	}
+	public Long getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(Long voterId) {
+		this.voterId = voterId;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	public Long getConsituencyId() {
+		return consituencyId;
+	}
+	public void setConsituencyId(Long consituencyId) {
+		this.consituencyId = consituencyId;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
