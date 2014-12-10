bf90a8c6f7f5a6fd9f7804387b4955505020c755
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-10 15:33:52 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ZebraPrintOnlineShipDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ZebraPrintOnlineShipDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IZebraPrintOnlineShipDAO;
 import com.itgrids.partyanalyst.model.ZebraPrintOnlineShip;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 
 
@@ -11,5 +15,24 @@ public class ZebraPrintOnlineShipDAO extends GenericDaoHibernate<ZebraPrintOnlin
 
 	public ZebraPrintOnlineShipDAO() {
 		super(ZebraPrintOnlineShip.class);
+	}
+	
+	
+	public List<Object[]> getCadreShippingAddressDetials(Long constituencyId)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append(" select model.zebraPrintOnlineShipId,model.tdpCadreId, model.voterName, model.image, model.shipAddress,model.memberShipMember  " +
+				" from ZebraPrintOnlineShip model  where model.tdpCadre.enrollmentYear = 2014 and model.tdpCadre.isDeleted = 'N' ");		
+		if(constituencyId.longValue() != 0L){
+			str.append(" and model.tdpCadre.userAddress.constituency.constituencyId = :constituencyId ");
+		}
+		Query query = getSession().createQuery(str.toString()); 
+		
+		if(constituencyId.longValue() != 0L){
+			query.setParameter("constituencyId", constituencyId);
+		}
+		
+	
+		return query.list();
 	}
 }
