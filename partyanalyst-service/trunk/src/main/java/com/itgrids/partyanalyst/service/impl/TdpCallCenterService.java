6e9ecd4ab2a5419f2d0b01077cba35ca60300ef2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-12 10:10:58 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/TdpCallCenterService.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.dao.ITdpCadreSmsStatusDAO;
+import com.itgrids.partyanalyst.service.ITdpCallCenterService;
+
+public class TdpCallCenterService implements ITdpCallCenterService{
+
+}
