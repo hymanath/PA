242ea1b23dedd03fbfdf52ee810f74e9a44295f3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-14 13:31:46 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDateDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreVolunteerDate;
+
+
+
+public interface ITdpCadreVolunteerDateDAO extends GenericDao<TdpCadreVolunteerDate, Long>{
+
+}
