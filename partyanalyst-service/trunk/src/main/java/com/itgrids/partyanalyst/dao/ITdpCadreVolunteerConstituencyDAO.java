caa8a9a90cadb06bac707543161a6e6b39fe44ae
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-14 13:31:46 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerConstituencyDAO.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreVolunteerConstituency;
+
+
+
+
+public interface ITdpCadreVolunteerConstituencyDAO extends GenericDao<TdpCadreVolunteerConstituency, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreVolunteer;
+
+
+
+public interface ITdpCadreVolunteerDAO extends GenericDao<TdpCadreVolunteer, Long>{
+
+}
