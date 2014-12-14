9752dbcaf143d6fcf1116731bfe31693e6949df0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-14 12:51:49 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TdpCadreVolunteerVO.java
@@ -0,0 +1,69 @@
+package com.itgrids.partyanalyst.dto;
+
+public class TdpCadreVolunteerVO {
+private String name;
+private String mobileNo;
+private String email;
+private String address;
+private String lapTop;
+private String internet;
+private String tablet;
+private String smartPhone3G;
+private String smartPhone2G;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getMobileNo() {
+	return mobileNo;
+}
+public void setMobileNo(String mobileNo) {
+	this.mobileNo = mobileNo;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getLapTop() {
+	return lapTop;
+}
+public void setLapTop(String lapTop) {
+	this.lapTop = lapTop;
+}
+public String getInternet() {
+	return internet;
+}
+public void setInternet(String internet) {
+	this.internet = internet;
+}
+public String getTablet() {
+	return tablet;
+}
+public void setTablet(String tablet) {
+	this.tablet = tablet;
+}
+public String getSmartPhone3G() {
+	return smartPhone3G;
+}
+public void setSmartPhone3G(String smartPhone3G) {
+	this.smartPhone3G = smartPhone3G;
+}
+public String getSmartPhone2G() {
+	return smartPhone2G;
+}
+public void setSmartPhone2G(String smartPhone2G) {
+	this.smartPhone2G = smartPhone2G;
+}
+
+
+}
