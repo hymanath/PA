1a0edd05ba3656a01b3426a2c94f61baa386e165
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-15 14:09:55 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -834,7 +834,7 @@ public interface IConstants {
     public static final boolean ENABLE_CADRE_LOGS = false;
     public static final boolean ENABLE_LOGS_SAVE = true;
     public static final Long CADREPERCENTAGE = 30l;
-    public static final String[] excludeUrl ={"callCenterMembershipCardStatusAction","getCadreDetailsForCallCenterAction","saveCallCenterFeedbackAction","getFeedbackDataAction"};
+    public static final String[] excludeUrl ={"callCenterMembershipCardStatusAction","getCadreDetailsForCallCenterAction","saveCallCenterFeedbackAction","getFeedbackDataAction","tdpCadreVolunteerAction"};
     public static final String CADRE_SMS_STATUS_FILES = "cadre_sms_status_files";
     public static final String EMPTY_STRING = "";
     		
