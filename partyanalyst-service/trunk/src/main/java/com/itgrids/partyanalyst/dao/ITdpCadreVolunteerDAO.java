038cc883129bf772a6d2e4ce8203473047346c1c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-16 23:00:54 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreVolunteer;
@@ -7,5 +9,8 @@ import com.itgrids.partyanalyst.model.TdpCadreVolunteer;
 
 
 public interface ITdpCadreVolunteerDAO extends GenericDao<TdpCadreVolunteer, Long>{
-
+	
+	public List<Long> checkVolunteerByMobileOrEmail(String mobileNo, String emailId);
+	
+	public Long getDeviceTotalCount(String deviceType,Long constituencyId,String searchType);
 }
