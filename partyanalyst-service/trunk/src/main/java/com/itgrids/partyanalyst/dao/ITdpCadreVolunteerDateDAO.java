058e268baa86b87d53ad33a51031b4d9f02e58b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-16 05:53:22 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreVolunteerDateDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreVolunteerDate;
@@ -8,4 +10,5 @@ import com.itgrids.partyanalyst.model.TdpCadreVolunteerDate;
 
 public interface ITdpCadreVolunteerDateDAO extends GenericDao<TdpCadreVolunteerDate, Long>{
 
+	public List<Object[]> getAvailableDatesForVolunteers(List<Long> valenteersIdsList);
 }
