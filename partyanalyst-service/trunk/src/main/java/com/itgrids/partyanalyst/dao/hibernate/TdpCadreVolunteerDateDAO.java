32425be3cb3835d918f489ebd472bcb2ca5b678d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-16 05:53:41 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreVolunteerDateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreVolunteerDateDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreVolunteerDateDAO;
 import com.itgrids.partyanalyst.model.TdpCadreVolunteerDate;
@@ -13,4 +16,25 @@ public class TdpCadreVolunteerDateDAO extends GenericDaoHibernate<TdpCadreVolunt
 		super(TdpCadreVolunteerDate.class);
 	}
 
+	public List<Object[]> getAvailableDatesForVolunteers(List<Long> valenteersIdsList)
+	{
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select model.tdpCadreVolunteer.tdpCadreVolunteerId, date(model.date) from TdpCadreVolunteerDate model ");
+		
+		if(valenteersIdsList != null && valenteersIdsList.size()>0)
+		{
+			queryStr.append(" where model.tdpCadreVolunteer.tdpCadreVolunteerId in (:valenteersIdsList)");
+				
+		}
+		
+		queryStr.append(" order by model.tdpCadreVolunteer.tdpCadreVolunteerId asc");
+		
+		Query query = getSession().createQuery(queryStr.toString());
+		if(valenteersIdsList != null && valenteersIdsList.size()>0)
+		{
+			query.setParameterList("valenteersIdsList", valenteersIdsList);
+		}
+		
+		return query.list();
+	}
 }
