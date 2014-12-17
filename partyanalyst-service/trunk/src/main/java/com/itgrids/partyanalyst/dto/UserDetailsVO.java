b02097215e3b0868dafcd1af2c8f061b547b3d1c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-17 15:20:36 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserDetailsVO.java
@@ -0,0 +1,27 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDetailsVO {
+	private String name ;
+	private Long id ;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+}
