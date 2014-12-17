1439d1dfdc37f2944cff4a43b754e4fbe61270a0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-17 02:04:24 +0000

    updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVerifiedDataResponseDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.VerifiedDataResponse;
+
+public interface IVerifiedDataResponseDAO extends GenericDao<VerifiedDataResponse,Long>{
+
+}
