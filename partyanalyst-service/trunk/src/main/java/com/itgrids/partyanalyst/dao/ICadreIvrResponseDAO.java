97aeddc22bfe34e39d7c10c42bfd1b431818ff49
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-18 09:11:01 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreIvrResponseDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CadreIvrResponse;
+
+public interface ICadreIvrResponseDAO extends GenericDao<CadreIvrResponse, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/CadreIvrResponse.java
@@ -0,0 +1,140 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+@Entity
+@Table(name = "cadre_ivr_response")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class CadreIvrResponse {
+
+	
+	private Long  cadreIvrResponseId ;
+	private String mobileNo ;
+	private String responseKey;
+	private String callStatus;
+	private Long duration;
+	private Date startTime;
+	private Date endTime;
+	private TdpCadre tdpCadre;
+	private Long  tdpCadreId;
+	private Date date;
+	private String currentStatus;
+	private String isDeleted;
+	public CadreIvrResponse()
+	{
+		
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="cadre_ivr_response_id", unique=true, nullable=false)
+	public Long getCadreIvrResponseId() {
+		return cadreIvrResponseId;
+	}
+
+	public void setCadreIvrResponseId(Long cadreIvrResponseId) {
+		this.cadreIvrResponseId = cadreIvrResponseId;
+	}
+	@Column(name="mobile_no")
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	@Column(name="response_key")
+	public String getResponseKey() {
+		return responseKey;
+	}
+
+	public void setResponseKey(String responseKey) {
+		this.responseKey = responseKey;
+	}
+	@Column(name="call_status")
+	public String getCallStatus() {
+		return callStatus;
+	}
+
+	public void setCallStatus(String callStatus) {
+		this.callStatus = callStatus;
+	}
+	@Column(name="duration")
+	public Long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Long duration) {
+		this.duration = duration;
+	}
+	@Column(name="start_time")
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	@Column(name="end_time")
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "tdp_cadre_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadre getTdpCadre() {
+		return tdpCadre;
+	}
+	public void setTdpCadre(TdpCadre tdpCadre) {
+		this.tdpCadre = tdpCadre;
+	}
+	@Column(name = "tdp_cadre_id")
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	@Column(name = "date")
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Column(name = "current_status")
+	public String getCurrentStatus() {
+		return currentStatus;
+	}
+	public void setCurrentStatus(String currentStatus) {
+		this.currentStatus = currentStatus;
+	}
+	@Column(name = "is_deleted")
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	
+}
