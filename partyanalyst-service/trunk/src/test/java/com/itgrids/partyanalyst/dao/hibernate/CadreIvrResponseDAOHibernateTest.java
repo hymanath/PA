b39728afe9e28b07139337626791c2cfbad7bb0e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-20 07:08:19 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreIvrResponseDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreIvrResponseDAOHibernateTest.java
@@ -1,8 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.ICadreIvrResponseDAO;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class CadreIvrResponseDAOHibernateTest extends BaseDaoTestCase{
 	
@@ -12,5 +17,21 @@ public class CadreIvrResponseDAOHibernateTest extends BaseDaoTestCase{
 		this.cadreIvrResponseDAO = cadreIvrResponseDAO;
 	}
 	
+	
+	public void test()
+	{
+		Date convertedDate = null;
+		try{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		 convertedDate = sdf.parse("2014-12-19");
+		}
+		catch(Exception e)
+		{
+			
+		}
+	List<Object[]> list =cadreIvrResponseDAO.getIvrCadreDetails(convertedDate,null,"total",0,201);
+	System.out.println(list.size());
+	
+	}
 
 }
