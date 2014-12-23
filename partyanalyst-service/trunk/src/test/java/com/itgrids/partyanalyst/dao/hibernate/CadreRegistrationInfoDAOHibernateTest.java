433952dc4c4a446839efb0b9151fc8e466e00b3d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-23 14:33:41 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegistrationInfoDAOHibernateTest.java
@@ -0,0 +1,18 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ICadreRegistrationInfoDAO;
+
+public class CadreRegistrationInfoDAOHibernateTest extends BaseDaoTestCase{
+
+	
+	private ICadreRegistrationInfoDAO cadreRegistrationInfoDAO;
+
+	public void setCadreRegistrationInfoDAO(
+			ICadreRegistrationInfoDAO cadreRegistrationInfoDAO) {
+		this.cadreRegistrationInfoDAO = cadreRegistrationInfoDAO;
+	}
+	
+	
+}
