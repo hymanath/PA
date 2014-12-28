5e2253c306288e2fc4baea09b806298f84a88ddb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2014-12-28 11:09:12 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IZebraPrintDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IZebraPrintDetailsDAO.java
@@ -42,6 +42,6 @@ public interface IZebraPrintDetailsDAO extends GenericDao<ZebraPrintDetails, Lon
 	public List<Object[]> getAllCadreDetailsByParliament(Date batchCode,String searchType,List<Long> Ids);
 	public List<Object[]> getPrintedCountDetailsByStatusTypeSeacrh(List<Long> locationIdsList, String searchType,String dataType);
 	public List<Object[]> getParliamentWiseResultsByStatusType(List<Long> parliamentIdsList, String dataType);
-	public Long getPrintingCompletedCount();
-	public Long getIvrReadyCount(Date date) ;
+	public Long getPrintingCompletedCount(String state) ;
+	public Long getIvrReadyCount(Date date,String state) ;
 }
