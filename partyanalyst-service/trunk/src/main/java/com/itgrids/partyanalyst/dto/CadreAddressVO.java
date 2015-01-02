5b718417f60fb43f2d94e2fa7b96e0c0d90eac36
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-02 11:17:41 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreAddressVO.java
@@ -0,0 +1,57 @@
+package com.itgrids.partyanalyst.dto;
+
+public class CadreAddressVO {
+	
+	private Long id;
+	private String name;
+	private String address;
+	private String gender;
+	private String mobileNo;
+	private Long age;
+	private String membershipNo;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getMembershipNo() {
+		return membershipNo;
+	}
+	public void setMembershipNo(String membershipNo) {
+		this.membershipNo = membershipNo;
+	}
+	
+	
+
+}
