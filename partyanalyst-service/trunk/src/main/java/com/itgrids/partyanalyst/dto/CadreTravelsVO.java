646934504c4405b30fdf6544642b600ebd12e063
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-03 13:47:25 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreTravelsVO.java
@@ -0,0 +1,55 @@
+package com.itgrids.partyanalyst.dto;
+
+
+
+public class CadreTravelsVO {
+	
+	private String customerId;		
+	private String membershipNo;
+	private String	ticketsCount;	
+	private String ticketCost;				
+	private String	discountPerc;
+	private String dateOfJourney;
+	
+	
+	
+	public String getMembershipNo() {
+		return membershipNo;
+	}
+	public void setMembershipNo(String membershipNo) {
+		this.membershipNo = membershipNo;
+	}
+	
+	public String getTicketCost() {
+		return ticketCost;
+	}
+	public void setTicketCost(String ticketCost) {
+		this.ticketCost = ticketCost;
+	}
+	
+	public String getDateOfJourney() {
+		return dateOfJourney;
+	}
+	public void setDateOfJourney(String dateOfJourney) {
+		this.dateOfJourney = dateOfJourney;
+	}
+	public String getTicketsCount() {
+		return ticketsCount;
+	}
+	public void setTicketsCount(String ticketsCount) {
+		this.ticketsCount = ticketsCount;
+	}
+	public String getDiscountPerc() {
+		return discountPerc;
+	}
+	public void setDiscountPerc(String discountPerc) {
+		this.discountPerc = discountPerc;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+}
