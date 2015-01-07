bf3d2b7455c71334e38677f0f3ccb9390b3509c3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-07 09:18:04 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreImageSinkDataDAOHibernateTest.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ITdpCadreImageSinkDataDAO;
+
+public class TdpCadreImageSinkDataDAOHibernateTest extends BaseDaoTestCase{
+	private ITdpCadreImageSinkDataDAO tdpCadreImageSinkDataDAO;
+
+	public void setTdpCadreImageSinkDataDAO(
+			ITdpCadreImageSinkDataDAO tdpCadreImageSinkDataDAO) {
+		this.tdpCadreImageSinkDataDAO = tdpCadreImageSinkDataDAO;
+	}
+	
+
+}
