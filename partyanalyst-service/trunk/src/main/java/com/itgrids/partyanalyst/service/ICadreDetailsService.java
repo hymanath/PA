da31263ceec152de2fe5b6f86c086d41c46b6fdf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-08 07:44:10 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreDetailsService.java
@@ -3,5 +3,5 @@ package com.itgrids.partyanalyst.service;
 import com.itgrids.partyanalyst.dto.TdpCadreVO;
 
 public interface ICadreDetailsService {
-	public TdpCadreVO searchTdpCadreDetailsBySearchCriteriaForCommitte(Long locationLevel,Long locationValue, String searchName,String memberShipCardNo, String voterCardNo, String trNumber, String mobileNo,String casteStateId,String casteCategoryId);
+	public TdpCadreVO searchTdpCadreDetailsBySearchCriteriaForCommitte(Long locationLevel,Long locationValue, String searchName,String memberShipCardNo, String voterCardNo, String trNumber, String mobileNo,Long casteStateId,String casteCategoryId);
 }
