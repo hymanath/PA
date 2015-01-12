846be3a4c325f6a28604e87b2634f0488b548d9b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-12 09:06:15 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreCommitteeVO.java
@@ -0,0 +1,244 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadreCommitteeVO implements java.io.Serializable{
+	
+	private Long tdpCadreId;
+	private String cadreName;
+	private String memberShipCardId;
+	private String DOB;
+	private String age;
+	private String gender;
+	private String mobileNo;
+	private String adhaarNo;
+	private String voterCardNo;
+	private String education;
+	private String occupation;
+	private String emailId;
+	private String mobileType;
+	private String casteCategory;
+	private String casteName;
+	private String constituency;
+	private String tehsil;
+	private String localElectionBody;
+	private String panchayat;
+	private String address;
+	
+	private String fromDate;
+	private String toDate;
+	
+	private Long committeeLevelId;
+	private Long committeeTypeId;
+	private Long committeeId;
+	private Long roleId;
+	
+	private Long electionTypeId;
+	private Long electioinYearId;
+	private String electionType;
+	private String electionYear;
+	
+	private String imageURL;
+	List<CadreCommitteeVO> previousRoles = new ArrayList<CadreCommitteeVO>();
+	List<CadreCommitteeVO> previousElections = new ArrayList<CadreCommitteeVO>();
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	public String getCadreName() {
+		return cadreName;
+	}
+	public void setCadreName(String cadreName) {
+		this.cadreName = cadreName;
+	}
+	public String getMemberShipCardId() {
+		return memberShipCardId;
+	}
+	public void setMemberShipCardId(String memberShipCardId) {
+		this.memberShipCardId = memberShipCardId;
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String dOB) {
+		DOB = dOB;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getAdhaarNo() {
+		return adhaarNo;
+	}
+	public void setAdhaarNo(String adhaarNo) {
+		this.adhaarNo = adhaarNo;
+	}
+	public String getVoterCardNo() {
+		return voterCardNo;
+	}
+	public void setVoterCardNo(String voterCardNo) {
+		this.voterCardNo = voterCardNo;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getMobileType() {
+		return mobileType;
+	}
+	public void setMobileType(String mobileType) {
+		this.mobileType = mobileType;
+	}
+	public String getCasteCategory() {
+		return casteCategory;
+	}
+	public void setCasteCategory(String casteCategory) {
+		this.casteCategory = casteCategory;
+	}
+	public String getCasteName() {
+		return casteName;
+	}
+	public void setCasteName(String casteName) {
+		this.casteName = casteName;
+	}
+	public String getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(String constituency) {
+		this.constituency = constituency;
+	}
+	public String getTehsil() {
+		return tehsil;
+	}
+	public void setTehsil(String tehsil) {
+		this.tehsil = tehsil;
+	}
+	public String getLocalElectionBody() {
+		return localElectionBody;
+	}
+	public void setLocalElectionBody(String localElectionBody) {
+		this.localElectionBody = localElectionBody;
+	}
+	public String getPanchayat() {
+		return panchayat;
+	}
+	public void setPanchayat(String panchayat) {
+		this.panchayat = panchayat;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+	public String getToDate() {
+		return toDate;
+	}
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+	public Long getCommitteeLevelId() {
+		return committeeLevelId;
+	}
+	public void setCommitteeLevelId(Long committeeLevelId) {
+		this.committeeLevelId = committeeLevelId;
+	}
+	public Long getCommitteeTypeId() {
+		return committeeTypeId;
+	}
+	public void setCommitteeTypeId(Long committeeTypeId) {
+		this.committeeTypeId = committeeTypeId;
+	}
+	public Long getCommitteeId() {
+		return committeeId;
+	}
+	public void setCommitteeId(Long committeeId) {
+		this.committeeId = committeeId;
+	}
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public Long getElectionTypeId() {
+		return electionTypeId;
+	}
+	public void setElectionTypeId(Long electionTypeId) {
+		this.electionTypeId = electionTypeId;
+	}
+	public Long getElectioinYearId() {
+		return electioinYearId;
+	}
+	public void setElectioinYearId(Long electioinYearId) {
+		this.electioinYearId = electioinYearId;
+	}
+	public String getElectionType() {
+		return electionType;
+	}
+	public void setElectionType(String electionType) {
+		this.electionType = electionType;
+	}
+	public String getElectionYear() {
+		return electionYear;
+	}
+	public void setElectionYear(String electionYear) {
+		this.electionYear = electionYear;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	public List<CadreCommitteeVO> getPreviousRoles() {
+		return previousRoles;
+	}
+	public void setPreviousRoles(List<CadreCommitteeVO> previousRoles) {
+		this.previousRoles = previousRoles;
+	}
+	public List<CadreCommitteeVO> getPreviousElections() {
+		return previousElections;
+	}
+	public void setPreviousElections(List<CadreCommitteeVO> previousElections) {
+		this.previousElections = previousElections;
+	}
+	
+}
