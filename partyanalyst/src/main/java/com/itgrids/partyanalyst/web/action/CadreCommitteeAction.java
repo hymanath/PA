8352174458654c71a831dafd1ddbe94b0553bb41
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-12 09:08:23 +0000

    Updated by Srishailam

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreCommitteeAction.java
@@ -0,0 +1,50 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.itgrids.partyanalyst.dto.CadreCommitteeVO;
+import com.itgrids.partyanalyst.service.ICadreCommitteeService;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CadreCommitteeAction   extends ActionSupport implements ServletRequestAware{
+	
+	private static final Logger         		LOG = Logger.getLogger(CadreCommitteeAction.class);
+	private HttpServletRequest         			request;
+	private HttpSession 						session;
+	private ICadreCommitteeService   		 	cadreCommitteeService;
+	private CadreCommitteeVO 					cadreCommitteeVO;
+	
+	public CadreCommitteeVO getCadreCommitteeVO() {
+		return cadreCommitteeVO;
+	}
+
+	public void setCadreCommitteeVO(CadreCommitteeVO cadreCommitteeVO) {
+		this.cadreCommitteeVO = cadreCommitteeVO;
+	}
+
+	public HttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public void setCadreCommitteeService(ICadreCommitteeService cadreCommitteeService) {
+			this.cadreCommitteeService = cadreCommitteeService;
+	}
+	
+	public String execute()
+	{
+		return Action.SUCCESS;
+	}
+}
