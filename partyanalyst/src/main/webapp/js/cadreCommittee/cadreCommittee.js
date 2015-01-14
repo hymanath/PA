7ffb19fd92eff688a81f239cbc47a7b8d3f40b52
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-14 09:05:25 +0000

    Updated by Srishailam

--- partyanalyst/src/main/webapp/js/cadreCommittee/cadreCommittee.js
+++ partyanalyst/src/main/webapp/js/cadreCommittee/cadreCommittee.js
@@ -5,6 +5,7 @@
 		if(searchType == 'basicCommitteeDiv')
 		{
 			$('#'+searchType+'').show();
+			$('#committeeMngtType').val(1);
 			$('#publicrepresantative').hide();
 			$('#mandalaffiliated').hide();			
 			$('#basicCommitteeTab').addClass('arrow_selected');
@@ -13,12 +14,14 @@
 		else if(searchType == 'publicrepresantative')
 		{				
 			$('#basicCommitteeDiv').hide();
+			$('#committeeMngtType').val(2);
 			$('#'+searchType+'').show();
 			$('#mandalaffiliated').hide();
 			$('#publicrepresantativeTab').addClass('arrow_selected');
 		}
 		else if(searchType == 'mandalaffiliated')
 		{				
+			$('#committeeMngtType').val(3);
 			$('#publicrepresantative').hide();
 			$('#basicCommitteeDiv').hide();
 			$('#'+searchType+'').show();
@@ -27,23 +30,23 @@
 		
 	}
 	
-	function validateSearchType(searchTypeId)
+	function validateSearchType(areaTypeId)
 	{
-		if(searchTypeId == 1)
+		if(areaTypeId == 1) //  Village / Ward / Division
 		{
-			$('#searchTypeId').val(searchTypeId);
+			$('#areaTypeId').val(areaTypeId);
 		}
 		
-		else if(searchTypeId == 2)
+		else if(areaTypeId == 2) // Mandal / Town / GHMC 
 		{				
-			$('#searchTypeId').val(searchTypeId);
+			$('#areaTypeId').val(areaTypeId);
 		}
 	}
 	
 	function getCommiteeDetailsForCommittee()
 	{
-		var committeeLevelId = 	$('#searchTypeId').val();
-		var committeeTypeId = 	$('#committeeTypeId').val();
+		var committeeLevelId = 	$('#areaTypeId').val(); //  Village / Ward / Division (or) Mandal / Town / GHMC 
+		var committeeTypeId = 	$('#committeeTypeId').val(); // main committee (or) affiliated committee
 		var committeeId = 	$('#committeeId').val();
 		
 		var jsObj = {
@@ -59,4 +62,124 @@
 			}).done(function(result){
 			});
 			
+	}
+	
+	function getCadreDetailsBySearchCriteria()
+	{
+
+		var locationLevel = 6;
+		var locationValue = 12192;
+		var searchName = 'VASKULA';
+		var mobileNo = '';
+		var casteCategory = '';
+		var casteStateId = 0;
+		var fromAge = 0;
+		var toAge = 0;
+		var memberShipCardNo = '';
+		var trNumber = '';
+		var voterCardNo = '';
+		var gender = 'M';
+		var houseNo = '';
+		$('#cadreDetailsDiv').html('');
+		var jsObj =
+		{
+			locationLevel :locationLevel,
+			locationValue:locationValue,
+			searchName : searchName,
+			mobileNo: mobileNo,
+			casteCategory : casteCategory,
+			fromAge : fromAge,
+			toAge : toAge,
+			memberShipCardNo: memberShipCardNo,
+			casteStateId : casteStateId,
+			trNumber : trNumber,
+			voterCardNo:voterCardNo,
+			gender:gender,
+			houseNo:houseNo
+		}
+		console.log(jsObj);
+		
+		 $.ajax({
+				type : "POST",
+				url : "getCadreSearchDetailsAction.action",
+				data : {task:JSON.stringify(jsObj)} ,
+			}).done(function(result){
+				if(result != null && result.tdpCadreDetailsList != null && result.tdpCadreDetailsList.length>0)
+				{
+					buildCadreDetails(result.tdpCadreDetailsList);
+				}
+				else
+				{
+					console.log("no data available...");
+				}
+			});
+			
+	}
+	
+	function buildCadreDetails(result)
+	{
+		var str ='';
+		
+		if(result != null)
+		{
+			for(var i in result)
+			{			
+				str+='<div class="media">';
+				str+='<a href="#" class="media-left">';
+				str+='<img style="width: 64px; height: 64px;" src="http://www.mytdp.com/images/cadre_images/'+result[i].imageURL+'" />';
+				str+='</a>';
+				str+='<div class="media-body">';
+				str+='<h4 class="media-heading">'+result[i].cadreName+'</h4>';
+				str+='<ul class="list-inline">';
+				str+='<li>Age:33;</i>';
+				str+='<li>Gender: '+result[i].gender+'</i>';
+				str+='<li>Mobile No: '+result[i].mobileNo+'</i>';
+				str+='<li>Caste: '+result[i].casteName+'</i>';
+				str+='<li>Voter ID: '+result[i].voterCardNo+'</i>';
+				//str+='<li>Aadhar: '+result[i].imageURL+'</i>';
+				str+='</ul>';
+				str+='</div>';
+				str+='</div>';
+				str+='<div class="form-inline ">';
+				str+='<label><input type="radio" name="searchedDetails"> &nbsp;&nbsp;</label>';
+				str+='<a target="_blank" href="cadreProfileDetailsAction.action?tdpCadreId=9335312" class="btn btn-success btn-medium">UPDATE PROFILE</a>';
+				str+='</div>	';
+			}
+		}
+		
+		$('#cadreDetailsDiv').html(str);
+	}
+	
+	function addMoreRolesForCadre()
+	{
+	
+		str+='<div class="well well-sm" style="background: none repeat scroll 0% 0% rgba(0, 0, 0, 0.1); border: medium none transparent;margin-bottom:2px;" >';
+		str+='<div class="row">';
+		str+='<div class="form-group col-md-6 col-sm-6 col-xs-6 ">';
+		str+='<label for="exampleInputEmail2" >Committee Level</label>';
+		str+='<select class="form-control "></select>';
+		str+='</div>';
+		str+='<div class="form-group col-md-6 col-sm-6 col-xs-6">';
+		str+='<label >Committee Name</label>';
+		str+='<select class="form-control "></select>';
+		str+='</div>	';				  
+		str+='</div>';
+
+		str+='<div class="row">';
+		str+='<div class="form-group col-md-4 col-sm-4 col-xs-4 ">';
+		str+='<label for="exampleInputEmail2" >Committee Role</label>';
+		str+='<input type="text" class="form-control ">';
+		str+='</div>';
+		str+='<div class="form-group col-md-4 col-sm-4 col-xs-4">';
+		str+='<label >From Date</label>';
+		str+='<input type="text" class="form-control ">';
+		str+='</div>';
+		str+='<div class="form-group col-md-4 col-sm-4 col-xs-4">';
+		str+='<label >To Date</label>';
+		str+='<input type="text" class="form-control ">';
+		str+='</div>	';				  
+		str+='</div>';
+		str+='<a href="javascript:{addMoreRolesForCadre();}" class="btn btn-danger btn-xs ">Tap to Add+ Details</a>	';				
+		str+='</div>';
+		$('#addMoreRolesDiv').append(str);
 	}
