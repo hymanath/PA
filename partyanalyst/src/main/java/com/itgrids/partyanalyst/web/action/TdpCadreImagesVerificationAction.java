21d1039a65ee36e161b6890c9f40432e6095b8f1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-23 13:36:24 +0000

    UPDATED BY PRASAD FOR CADRE IMAGE CHECKING

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/TdpCadreImagesVerificationAction.java
@@ -0,0 +1,27 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TdpCadreImagesVerificationAction extends ActionSupport implements ServletRequestAware{
+
+	
+	private static final long serialVersionUID = 6687324420450621125L;
+	private HttpServletRequest request;
+	
+	
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	
+	public String execute()
+	{
+		return Action.SUCCESS;
+	}
+}
