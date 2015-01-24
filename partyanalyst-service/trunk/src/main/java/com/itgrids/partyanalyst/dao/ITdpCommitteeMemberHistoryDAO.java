3ff546a0c282aca4ce0f142debf08aa1d6b4fab4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-24 12:41:56 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCommitteeMemberHistoryDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCommitteeMemberHistory;
+
+public interface ITdpCommitteeMemberHistoryDAO extends GenericDao<TdpCommitteeMemberHistory, Long>{
+
+}
