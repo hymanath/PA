8b0c40bc74bb9a035e2e54f5bdef7081aa0e4a5f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-26 06:13:37 +0000

    updated by-sreedhar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeIncreasedPositionsDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CadreCommitteeIncreasedPositions;
+import com.itgrids.partyanalyst.model.TdpCommitteeRole;
+
+public interface ICadreCommitteeIncreasedPositionsDAO extends GenericDao<CadreCommitteeIncreasedPositions, Long>
+{
+
+}
