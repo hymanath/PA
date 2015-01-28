77c9ec222c55e627207eeb847574921a983beddc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-28 09:14:17 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreCommitteeReportVO.java
@@ -0,0 +1,67 @@
+package com.itgrids.partyanalyst.dto;
+
+public class CadreCommitteeReportVO {
+
+	
+	private Long committeesCount;
+	private Long startedCommittees;
+	private Long afflCommittees;
+	private Long completedCommittees;
+	private Long membersCount;
+	private Long totalMembers;
+	private Long totalCommittees;
+	private Long mainCommittees;
+	
+	
+	public Long getCommitteesCount() {
+		return committeesCount;
+	}
+	public void setCommitteesCount(Long committeesCount) {
+		this.committeesCount = committeesCount;
+	}
+	public Long getStartedCommittees() {
+		return startedCommittees;
+	}
+	public void setStartedCommittees(Long startedCommittees) {
+		this.startedCommittees = startedCommittees;
+	}
+	public Long getAfflCommittees() {
+		return afflCommittees;
+	}
+	public void setAfflCommittees(Long afflCommittees) {
+		this.afflCommittees = afflCommittees;
+	}
+	public Long getCompletedCommittees() {
+		return completedCommittees;
+	}
+	public void setCompletedCommittees(Long completedCommittees) {
+		this.completedCommittees = completedCommittees;
+	}
+	public Long getMembersCount() {
+		return membersCount;
+	}
+	public void setMembersCount(Long membersCount) {
+		this.membersCount = membersCount;
+	}
+	public Long getTotalMembers() {
+		return totalMembers;
+	}
+	public void setTotalMembers(Long totalMembers) {
+		this.totalMembers = totalMembers;
+	}
+	public Long getTotalCommittees() {
+		return totalCommittees;
+	}
+	public void setTotalCommittees(Long totalCommittees) {
+		this.totalCommittees = totalCommittees;
+	}
+	public Long getMainCommittees() {
+		return mainCommittees;
+	}
+	public void setMainCommittees(Long mainCommittees) {
+		this.mainCommittees = mainCommittees;
+	}
+	
+	
+	
+}
