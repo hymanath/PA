4a453dc478c65e0390d4bbf80aa4c950cd4557be
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-28 15:45:40 +0000

    COMMITTEE UPDATE APPROVE SCREEN -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeIncreasedPositionsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreCommitteeIncreasedPositionsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreCommitteeIncreasedPositionsDAO;
 import com.itgrids.partyanalyst.model.CadreCommitteeIncreasedPositions;
@@ -9,5 +12,28 @@ public class CadreCommitteeIncreasedPositionsDAO extends GenericDaoHibernate<Cad
 {
 	public CadreCommitteeIncreasedPositionsDAO() {
 		super(CadreCommitteeIncreasedPositions.class);
+	}
+	
+	public Long getAllRecordsCount(){
+		Query query = getSession().createQuery(" select count(model.cadreCommitteeIncreasedPositionsId) from CadreCommitteeIncreasedPositions model" +
+				" where model.type ='positionsIncreased'");
+		return (Long) query.uniqueResult();
+	}
+	
+	public List<Object[]> getAllRecordsCount(int stIndex, int endIndex){
+		Query query = getSession().createQuery(" select model.cadreCommitteeIncreasedPositionsId," + // 0 -- REQUEST NO
+				" model.tdpCommitteeRole.tdpCommittee.tdpCommitteeLevel.tdpCommitteeLevelId," + // 1 -- LEVEL ID
+				" model.tdpCommitteeRole.tdpCommittee.tdpCommitteeLevel.tdpCommitteeLevel," + // 2 -- LEVEL NAME
+				" model.tdpCommitteeRole.tdpCommittee.tdpCommitteeLevelValue," + // 3 -- LEVEL VALUE
+				" model.tdpCommitteeRole.tdpCommittee.tdpBasicCommittee.tdpBasicCommitteeId," +// 4 -- COMMITTEE TYPE ID
+				" model.tdpCommitteeRole.tdpCommittee.tdpBasicCommittee.name," + // 5 -- COMMITTEE TYPE NAME
+				" model.tdpCommitteeRole.tdpRoles.tdpRolesId," + // 6 -- ROLE ID
+				" model.tdpCommitteeRole.tdpRoles.role," + // 7 -- ROLE NAME
+				" model.currentCount," + // 8 -- CURRENT COUNT
+				" model.requestCount," + // 9 -- REQUESTED COUNT
+				" model.status " + // 10 -- STATUS 
+				" from CadreCommitteeIncreasedPositions model" +
+				" where model.type ='positionsIncreased'");
+		return query.list();
 	}
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeLevelDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeLevelDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITdpCommitteeLevelDAO;
 import com.itgrids.partyanalyst.model.TdpCommitteeLevel;
@@ -8,6 +11,14 @@ import com.itgrids.partyanalyst.model.TdpCommitteeLevel;
 public class TdpCommitteeLevelDAO extends GenericDaoHibernate<TdpCommitteeLevel, Long>  implements ITdpCommitteeLevelDAO {
 	public TdpCommitteeLevelDAO() {
 		super(TdpCommitteeLevel.class);
+	}
+	
+	public List<Object[]> getAllLevels(){
+		Query query = getSession().createQuery(" select model.tdpCommitteeLevelId," +
+				" model.tdpCommitteeLevel " +
+				" from TdpCommitteeLevel model ");
+		
+		return query.list();
 	}
 	
 }
