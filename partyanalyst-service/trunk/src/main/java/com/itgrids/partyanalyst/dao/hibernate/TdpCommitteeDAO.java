b1eb197106f33149a02afde156a351abe92d256a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-28 19:52:11 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeDAO.java
@@ -81,4 +81,48 @@ public class TdpCommitteeDAO extends GenericDaoHibernate<TdpCommittee, Long>  im
 		return query.list();
 	}
 	
+	public List<Object[]> getTotalCommitteesPanchayatLevel(Long constituencyId){
+		//0count ,1 basic committeeId,2basic committee name,3committeeType
+		Query query = getSession().createQuery("select count(model.tdpCommitteeId),model.tdpBasicCommittee.tdpBasicCommitteeId,model.tdpBasicCommittee.name," +
+				"model.tdpBasicCommittee.tdpCommitteeType.tdpCommitteeTypeId from TdpCommittee model " +
+				" where  model.constituency.constituencyId=:constituencyId and model.tdpCommitteeLevel.tdpCommitteeLevelId in(6,8) group by model.tdpBasicCommittee.tdpBasicCommitteeId ");
+		query.setParameter("constituencyId", constituencyId);
+		return query.list();
+	}
+	
+	public List<Object[]> getLocationWiseDetails(List<Long> locationValues,Long locationTypeId){
+		Query query = getSession().createQuery("select TBC.tdpCommitteeType.tdpCommitteeTypeId ,TBC.name,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed,count(*) " +
+				"from TdpCommittee TC , TdpBasicCommittee TBC where TC.tdpBasicCommitteeId = TBC.tdpBasicCommitteeId and TC.tdpCommitteeLevelId = :locationTypeId " +
+				"and TC.tdpCommitteeLevelValue in (:locationValues) group by TBC.tdpCommitteeTypeId ,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed");
+		//query.setParameter("constituencyId", constituencyId);
+		query.setParameter("locationTypeId", locationTypeId);
+		query.setParameterList("locationValues", locationValues);
+		return query.list();
+	}
+	
+	public List<Object[]> getLocationWiseVillageDetails(Long constituencyId){
+		Query query = getSession().createQuery("select TBC.tdpCommitteeType.tdpCommitteeTypeId ,TBC.name,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed,count(*) " +
+				" from TdpCommittee TC , TdpBasicCommittee TBC where TC.tdpBasicCommitteeId = TBC.tdpBasicCommitteeId and TC.tdpCommitteeLevelId in (6,8) " +
+				" and  TC.constituency.constituencyId = :constituencyId group by TBC.tdpCommitteeTypeId ,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed");
+		query.setParameter("constituencyId", constituencyId);
+		return query.list();
+	}
+	
+	/*public List<Object[]> muncipalList(Long constituencyId,List muncipalIds){
+		Query query = getSession().createQuery("select TBC.tdpCommitteeType.tdpCommitteeTypeId ,TBC.name,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed,count(*) " +
+				"from TdpCommittee TC , TdpBasicCommittee TBC where TC.tdpBasicCommitteeId = TBC.tdpBasicCommitteeId and TC.tdpCommitteeLevelId = 7 " +
+				"and TC.tdpCommitteeLevelValue in (:ids) group by TBC.tdpCommitteeTypeId ,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed");
+		//query.setParameter("constituencyId", constituencyId);
+		query.setParameterList("ids", muncipalIds);
+		return query.list();
+	}
+	
+	public List<Object[]> divisionsList(Long constituencyId,List divisionIds){
+		Query query = getSession().createQuery("select TBC.tdpCommitteeType.tdpCommitteeTypeId ,TBC.name,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed,count(*) " +
+				"from TdpCommittee TC , TdpBasicCommittee TBC where TC.tdpBasicCommitteeId = TBC.tdpBasicCommitteeId and TC.tdpCommitteeLevelId = 9 " +
+				"and TC.tdpCommitteeLevelValue in (:ids) group by TBC.tdpCommitteeTypeId ,TBC.tdpBasicCommitteeId,TC.isCommitteeConfirmed");
+		//query.setParameter("constituencyId", constituencyId);
+		query.setParameterList("ids", divisionIds);
+		return query.list();
+	}*/
 }
