12466d439a80247552cf7f2fa6f6b52a1b25b966
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-28 05:39:56 +0000

    updated by Balu for committee dashboard.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCommitteeDAO.java
@@ -41,4 +41,26 @@ public class TdpCommitteeDAO extends GenericDaoHibernate<TdpCommittee, Long>  im
 		query.setParameter("tdpCommitteeLevelValue", tdpCommitteeLevelValue);
 		return query.list();
 	}
+
+	public List<Object[]> getTotalCommitteesLocationLevelByState(String state,List<Long> levelIds){
+		//0count ,1 isCommitteeConfirmed,2.tdpCommitteeLevelId,3.tdpBasicCommitteeId
+
+		StringBuilder str = new StringBuilder();
+
+		str.append("select count(model.tdpCommitteeId),model.isCommitteeConfirmed,model.tdpCommitteeLevel.tdpCommitteeLevelId," +
+				" model.tdpBasicCommittee.tdpBasicCommitteeId " +
+				" from TdpCommittee model where ");
+		if(state != null)
+		{
+		str.append(" model.state= :state ");
+		}
+		str.append(" and model.tdpCommitteeLevel.tdpCommitteeLevelId in (:levelIds) and model.tdpBasicCommittee.tdpBasicCommitteeId = 1  " +
+				" group by model.tdpCommitteeLevel.tdpCommitteeLevelId,model.isCommitteeConfirmed ");
+
+		Query query = getSession().createQuery(str.toString());
+		query.setParameter("state", state);
+		query.setParameterList("levelIds", levelIds);
+		return query.list();
+	}
+	
 }
