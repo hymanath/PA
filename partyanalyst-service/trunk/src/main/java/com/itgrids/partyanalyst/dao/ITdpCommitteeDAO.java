1bdc9ddae47697d3aca4f7dc76450f37f06e794b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-30 15:22:57 +0000

    updated by-sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCommitteeDAO.java
@@ -26,4 +26,5 @@ public interface ITdpCommitteeDAO  extends GenericDao<TdpCommittee, Long>{
 	public List<Object[]> getLocationsByTypeIdAndLevel(List<Long> levelIds,Long committeTypeId,List<Long> locationValues,String status);
 	public List<Object[]> getLocationByTypeIdAndLevel(List<Long> levelIds,Long committeTypeId,Long constituencyId,String status);
 	public List<Object[]> committeesCountByDistrict(List<Long> levelIds,Date startDate,Date endDate,String type,List<Long> districtIds);
+	public String gettingConfirmedCommittee(Long tdpCommitteeId);
 }
