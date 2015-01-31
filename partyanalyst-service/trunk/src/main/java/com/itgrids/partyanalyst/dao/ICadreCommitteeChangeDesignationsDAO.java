764b4e6dd8a1497c583e3cbef18a6daa1766587a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-01-31 15:16:12 +0000

    updated by-sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeChangeDesignationsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreCommitteeChangeDesignationsDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreCommitteeChangeDesignations;
 
 public interface ICadreCommitteeChangeDesignationsDAO extends GenericDao<CadreCommitteeChangeDesignations, Long>
 {
-
+	public List<Object[]> changeDesignationRecordsForAUser(Long requestUserId);
 }
