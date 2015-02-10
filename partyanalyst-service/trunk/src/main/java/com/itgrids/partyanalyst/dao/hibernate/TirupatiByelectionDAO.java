02cf448801994f18d42e43ab4ad63f0af0a43dcc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-10 05:23:04 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TirupatiByelectionDAO.java
@@ -0,0 +1,17 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.model.TirupatiByelection;
+
+public class TirupatiByelectionDAO extends GenericDaoHibernate<TirupatiByelection, Long>{
+
+	public TirupatiByelectionDAO() {
+		super(TirupatiByelection.class);
+		
+	}
+	
+	
+	
+
+}
