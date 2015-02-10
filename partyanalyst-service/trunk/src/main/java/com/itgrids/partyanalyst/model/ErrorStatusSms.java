8bb8bb83544760b6e4fbe2e2bfc035e3a650461a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-10 09:23:55 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ErrorStatusSms.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ErrorStatusSms.java
@@ -2,13 +2,19 @@ package com.itgrids.partyanalyst.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
 
 
 
@@ -21,6 +27,7 @@ public class ErrorStatusSms {
 	private Long errorStatusSmsId;
 	private String message;
 	private String mobileNo;
+	private TwoWaySmsMobile twoWaySmsMobile;
 	
 	
 	@Id
@@ -49,5 +56,19 @@ public class ErrorStatusSms {
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "twoway_sms_mobile_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TwoWaySmsMobile getTwoWaySmsMobile() {
+		return twoWaySmsMobile;
+	}
+	public void setTwoWaySmsMobile(TwoWaySmsMobile twoWaySmsMobile) {
+		this.twoWaySmsMobile = twoWaySmsMobile;
+	}
+	
+	
 
 }
