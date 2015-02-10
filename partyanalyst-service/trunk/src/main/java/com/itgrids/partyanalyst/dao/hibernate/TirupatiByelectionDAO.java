2565680c317b4c9af1c7eaaf2c7269cde29b406e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-10 07:10:21 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TirupatiByelectionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TirupatiByelectionDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.model.TirupatiByelection;
 
@@ -11,7 +14,21 @@ public class TirupatiByelectionDAO extends GenericDaoHibernate<TirupatiByelectio
 		
 	}
 	
-	
+
+	public List<TirupatiByelection> getModelByType(Long typeId,String type)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("select * from model from TirupatiByelection model");
+		if(typeId == 1)
+		 str.append(" where model.clusterName = :type");
+		else if(typeId == 2)
+			 str.append(" where model.divisionName = :type");	
+		Query query = getSession().createQuery(str.toString());	
+		query.setParameter("type", type);
+		return query.list();
+		
+		
+	}
 	
 
 }
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ByeElectionVO.java
@@ -0,0 +1,134 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class ByeElectionVO {
+
+	
+	private Long id;
+	private String name;
+	private String type;
+	private Long boothId;
+	private Long prevBoothId;
+	private String partNo;
+	private String prevPartNo;
+	private String boothLocation;
+	private Long totalVoters = 0l;
+	private Long preTotalVoters =0l;
+	private Long preVotersInPresent=0l;
+	private String prevPercentage;
+	private Long polledVotes;
+	private Long prevPolledVotes;
+	private String percentage;
+	List<ByeElectionVO> recognizeList;
+	List<ByeElectionVO> unRecognizeList;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	
+	public Long getPrevBoothId() {
+		return prevBoothId;
+	}
+	public void setPrevBoothId(Long prevBoothId) {
+		this.prevBoothId = prevBoothId;
+	}
+	public String getPrevPartNo() {
+		return prevPartNo;
+	}
+	public void setPrevPartNo(String prevPartNo) {
+		this.prevPartNo = prevPartNo;
+	}
+	public String getPartNo() {
+		return partNo;
+	}
+	public void setPartNo(String partNo) {
+		this.partNo = partNo;
+	}
+	
+	public String getBoothLocation() {
+		return boothLocation;
+	}
+	public void setBoothLocation(String boothLocation) {
+		this.boothLocation = boothLocation;
+	}
+	public Long getTotalVoters() {
+		return totalVoters;
+	}
+	public void setTotalVoters(Long totalVoters) {
+		this.totalVoters = totalVoters;
+	}
+	public Long getPreTotalVoters() {
+		return preTotalVoters;
+	}
+	public void setPreTotalVoters(Long preTotalVoters) {
+		this.preTotalVoters = preTotalVoters;
+	}
+	public Long getPreVotersInPresent() {
+		return preVotersInPresent;
+	}
+	public void setPreVotersInPresent(Long preVotersInPresent) {
+		this.preVotersInPresent = preVotersInPresent;
+	}
+	public String getPrevPercentage() {
+		return prevPercentage;
+	}
+	public void setPrevPercentage(String prevPercentage) {
+		this.prevPercentage = prevPercentage;
+	}
+	public Long getPolledVotes() {
+		return polledVotes;
+	}
+	public void setPolledVotes(Long polledVotes) {
+		this.polledVotes = polledVotes;
+	}
+	public Long getPrevPolledVotes() {
+		return prevPolledVotes;
+	}
+	public void setPrevPolledVotes(Long prevPolledVotes) {
+		this.prevPolledVotes = prevPolledVotes;
+	}
+	public String getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(String percentage) {
+		this.percentage = percentage;
+	}
+	public List<ByeElectionVO> getRecognizeList() {
+		return recognizeList;
+	}
+	public void setRecognizeList(List<ByeElectionVO> recognizeList) {
+		this.recognizeList = recognizeList;
+	}
+	public List<ByeElectionVO> getUnRecognizeList() {
+		return unRecognizeList;
+	}
+	public void setUnRecognizeList(List<ByeElectionVO> unRecognizeList) {
+		this.unRecognizeList = unRecognizeList;
+	}
+	
+	
+	
+	
+	
+}
