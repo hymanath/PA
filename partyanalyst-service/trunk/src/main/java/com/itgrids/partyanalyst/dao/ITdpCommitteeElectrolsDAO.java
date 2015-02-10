c683bce096bd517102a290b2ffa251ff782819c9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-10 10:54:05 +0000

    CONSTITUENCY COMMITTEE SUMMARY ISSUES FIXING -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCommitteeElectrolsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCommitteeElectrolsDAO.java
@@ -14,5 +14,6 @@ public interface ITdpCommitteeElectrolsDAO  extends GenericDao<TdpCommitteeElect
 	public List<Object[]> getElectrolsForPanchayatsWards(List<Long> locationIds, String locationType);
 	public List<Object[]> getElctoralInfoForALocation(Long locationValue);
 	public List<Object[]> getElectrolsOfPanchayatAndWards(Long locationId, Long locationType, Long basicCommitteeTypeId);
+	public List<Object[]> getElectrolsForPanchayatsWardsWithOutDuplicates(List<Long> locationIds, String locationType, List<Long> tdpCadreIds);
 
 }
