eec0da97b3dc98d25cea9bf3baf4bf93d411c7f0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-10 14:03:57 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/TirupatiByelectionDAOHibernateTest.java
@@ -0,0 +1,18 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ITirupatiByelectionDAO;
+
+public class TirupatiByelectionDAOHibernateTest extends BaseDaoTestCase{
+	
+	private ITirupatiByelectionDAO tirupatiByelectionDAO;
+
+	public void setTirupatiByelectionDAO(
+			ITirupatiByelectionDAO tirupatiByelectionDAO) {
+		this.tirupatiByelectionDAO = tirupatiByelectionDAO;
+	}
+	
+	
+
+}
