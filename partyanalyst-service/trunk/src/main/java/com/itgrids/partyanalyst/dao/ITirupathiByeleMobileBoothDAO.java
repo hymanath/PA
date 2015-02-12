916c0c92042a04536a4f93443d513588e249724d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-12 11:22:26 +0000

    REQ Fixing -- SASI

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITirupathiByeleMobileBoothDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITirupathiByeleMobileBoothDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TirupathiByeleMobileBooth;
 
 public interface ITirupathiByeleMobileBoothDAO extends GenericDao<TirupathiByeleMobileBooth, Long>{
-	
+	public List<String> getMobileNosOfBooth(Long boothId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TirupathiByeleMobileBoothDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TirupathiByeleMobileBoothDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITirupathiByeleMobileBoothDAO;
 import com.itgrids.partyanalyst.model.TirupathiByeleMobileBooth;
@@ -9,5 +12,13 @@ public class TirupathiByeleMobileBoothDAO extends GenericDaoHibernate<TirupathiB
 
 	public TirupathiByeleMobileBoothDAO() {
 		super(TirupathiByeleMobileBooth.class);
+	}
+	
+	public List<String> getMobileNosOfBooth(Long boothId){
+		Query query = getSession().createQuery(" select distinct model.mobileNo from TirupathiByeleMobileBooth model " +
+				" where model.booth.boothId= :boothId ");
+		
+		query.setParameter("boothId", boothId);
+		return query.list();
 	}
 }
