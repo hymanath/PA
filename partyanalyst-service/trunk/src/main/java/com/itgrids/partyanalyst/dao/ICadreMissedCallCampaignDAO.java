09699b75fcdda2bf8cd2f63ce85e49df133a9f86
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-02-20 06:31:59 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreMissedCallCampaignDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreMissedCallCampaignDAO.java
@@ -1,10 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreMissedCallCampaign;
 
 
 public interface ICadreMissedCallCampaignDAO extends GenericDao<CadreMissedCallCampaign, Long>{
-
+	public Long getAllMissedCallsCount(Date startDate, Date endDate);
+	public List<String> getAllMobileNos(Date startDate, Date endDate);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreMissedCallCampaignDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreMissedCallCampaignDAO.java
@@ -1,6 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreMissedCallCampaignDAO;
 import com.itgrids.partyanalyst.model.CadreMissedCallCampaign;
@@ -13,5 +17,50 @@ public class CadreMissedCallCampaignDAO extends GenericDaoHibernate<CadreMissedC
 		super(CadreMissedCallCampaign.class);
 	}
 
+	
+
+	
+	public Long getAllMissedCallsCount(Date startDate, Date endDate)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("SELECT count(distinct model.mobileNumber) from CadreMissedCallCampaign model "); 
+		
+		if(startDate != null && endDate != null && !startDate.equals(endDate))
+		str.append(" and date(model.insertedTime) >=:startDate and date(model.insertedTime) <=:endDate");
+		else if(startDate != null && endDate != null && startDate.equals(endDate))
+		str.append(" and date(model.insertedTime) >=:startDate");
+		
+	
+		Query query = getSession().createSQLQuery(str.toString());
+		if(startDate != null && endDate != null && !startDate.equals(endDate)){
+			query.setDate("startDate", startDate);
+			query.setDate("endDate", endDate);
+		}
+		else if(startDate != null && endDate != null && startDate.equals(endDate))
+			query.setDate("startDate", startDate);
+		
+		return  (Long) query.uniqueResult();
+	}
+	
+	public List<String> getAllMobileNos(Date startDate, Date endDate)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("select distinct model.mobileNumber from CadreMissedCallCampaign model " );
+				
+		if(startDate != null && endDate != null && !startDate.equals(endDate))
+		str.append(" where date(model.insertedTime) >=:startDate and date(model.insertedTime) <=:endDate");
+		else if(startDate != null && endDate != null && startDate.equals(endDate))
+		str.append(" where date(model.insertedTime) >=:startDate");
+		
+		Query query = getSession().createSQLQuery(str.toString());
+		if(startDate != null && endDate != null && !startDate.equals(endDate)){
+			query.setDate("startDate", startDate);
+			query.setDate("endDate", endDate);
+		}
+		else if(startDate != null && endDate != null && startDate.equals(endDate))
+			query.setDate("startDate", startDate);
+	
+		return  query.list();
+	}
 
 }
