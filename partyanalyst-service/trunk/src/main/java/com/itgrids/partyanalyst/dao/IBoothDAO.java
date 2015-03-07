0928ac2b8d61cb0238ec0eee2f7733fb00d61392
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-03-07 07:04:24 +0000

    updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothDAO.java
@@ -443,4 +443,6 @@ public interface IBoothDAO extends GenericDao<Booth, Long>{
 	public List<Object[]> getConstituenciesNameByType(List<Long> ids,String type);
 	
 	public List<Object[]> getAllBoothsOfAConstituency(Long constituencyId,Long publicationDateId);
+	
+	public List<Object[]> getAllTheBoothsDetailsByConstituencyIdForCTP(Long constituencyId);
 }
