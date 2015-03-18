1875af197eb0d2796b0c7d9f29e5b88ab85e9770
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-03-18 11:13:47 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreVoterSearchService.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICadreVoterSearchService;
+
+
+
+public class CadreVoterSearchService implements ICadreVoterSearchService{
+
+	private static final Logger LOG = Logger.getLogger(CadreRegistrationService.class);
+	
+
+}
