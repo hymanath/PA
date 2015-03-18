55273a875d3de41256aff28940f4847204d374a1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-03-18 11:19:18 +0000

    updated by praneetha

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadreVoterSearchAction.java
@@ -0,0 +1,111 @@
+package com.itgrids.partyanalyst.web.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
+
+import com.itgrids.partyanalyst.dto.CadreAddressVO;
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
+import com.itgrids.partyanalyst.service.IMahaNaduService;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CadreVoterSearchAction extends ActionSupport implements ServletRequestAware{
+
+	private static final Logger LOG = Logger.getLogger(TdpCadreReportAction.class);
+	
+	private HttpServletRequest request;	
+	private String task;
+	private JSONObject jobj;
+	private List<SelectOptionVO> casteDetails;
+	private List<CadreAddressVO> resultList;
+	private IMahaNaduService mahaNaduService;
+	//private ICadreVoterSearchService cadreVoterSearchService;
+
+	public IMahaNaduService getMahaNaduService() {
+		return mahaNaduService;
+	}
+	public void setMahaNaduService(IMahaNaduService mahaNaduService) {
+		this.mahaNaduService = mahaNaduService;
+	}
+	public List<CadreAddressVO> getResultList() {
+		return resultList;
+	}
+	public void setResultList(List<CadreAddressVO> resultList) {
+		this.resultList = resultList;
+	}
+	public List<SelectOptionVO> getCasteDetails() {
+		return casteDetails;
+	}
+	public void setCasteDetails(List<SelectOptionVO> casteDetails) {
+		this.casteDetails = casteDetails;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public JSONObject getJobj() {
+		return jobj;
+	}
+	public void setJobj(JSONObject jobj) {
+		this.jobj = jobj;
+	}
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+public String execute(){
+		
+		try {
+			LOG.info("Entered into  cadreVoterSearch method");
+			HttpSession session = request.getSession();
+			RegistrationVO user = (RegistrationVO)session.getAttribute("USER");
+			
+			if(user == null)
+				return Action.INPUT;
+			
+			casteDetails = mahaNaduService.getCasteCategories();
+			
+			
+		} catch (Exception e) {
+			LOG.error("Exception raised in cadreVoterSearch method",e);
+		}
+		return Action.SUCCESS;
+	}
+	
+	
+	public String getAllDistricts(){
+		LOG.info("Entered into getAllDistricts method");
+		HttpSession session = request.getSession();
+		RegistrationVO user = (RegistrationVO)session.getAttribute("USER");
+		
+		try {		
+			jobj = new JSONObject(request.getParameter("task"));
+			String type=jobj.getString("type");
+			
+			if(jobj.getString("task").equalsIgnoreCase("getDistricts")){
+			 //resultList= cadreVoterSearchService.getAllDistrictsAndConstis(type,jobj.getLong("stateId"));	
+			}
+			else if(jobj.getString("task").equalsIgnoreCase("getConstituencies")){
+				//resultList = cadreVoterSearchService.getAllDistrictsAndConstis(type,jobj.getLong("districtId"));	
+			}
+			
+		
+		}
+		catch(Exception e){
+			LOG.error("Exception raised in getExistingCadreInfoForCommittee method in CadreRegistrationAction action", e);
+		}
+		return Action.SUCCESS;
+		
+		
+	}
+	
+}
---
+++ partyanalyst/src/main/webapp/jsp/cadreVoterSearch.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@taglib prefix="s" uri="/struts-tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="java.util.ResourceBundle;" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Cadre Or Voter Search</title>
+
+
+
+<script type = "text/javascript">
+
+</script>
+</body>
+</html>
