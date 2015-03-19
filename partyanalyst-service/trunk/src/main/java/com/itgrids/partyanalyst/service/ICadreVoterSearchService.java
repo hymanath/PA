d93eb88bdb75468111475f25a0fdb8222e09ac44
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-03-19 12:46:01 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreVoterSearchService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreVoterSearchService.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.service;
 
-public interface ICadreVoterSearchService {
+import java.util.List;
 
+import com.itgrids.partyanalyst.dto.CadreAddressVO;
+
+public interface ICadreVoterSearchService {
+	public List<CadreAddressVO> getAllDistrictsAndConstis(String type,Long id);
 }
