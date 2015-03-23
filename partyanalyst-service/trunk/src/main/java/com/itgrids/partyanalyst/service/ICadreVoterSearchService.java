92926f7e8e3e1c9cf8fd1e88b6bd4050efbdd8f7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-03-23 14:26:32 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreVoterSearchService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreVoterSearchService.java
@@ -3,9 +3,10 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.CadreAddressVO;
+import com.itgrids.partyanalyst.dto.LocationWiseBoothDetailsVO;
 import com.itgrids.partyanalyst.dto.TdpCadreVO;
 
 public interface ICadreVoterSearchService {
-	public List<CadreAddressVO> getAllDistrictsAndConstis(String type,Long id);
+	public List<LocationWiseBoothDetailsVO> getAllDistrictsAndConstis(String type,Long id);	
 	public List<TdpCadreVO> getCadreVoterDetailsBySearchCriteria(String searchType,Long stateId, String locationType,Long locationId,Long casteStateId, String nameStr,String isFinal);
 }
