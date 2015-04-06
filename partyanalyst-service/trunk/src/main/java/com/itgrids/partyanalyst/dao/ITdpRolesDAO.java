98e3d39927ae302367e904165ac5b1305ba6f1c7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-04-06 13:36:22 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpRolesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpRolesDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpRoles;
 
 public interface ITdpRolesDAO  extends GenericDao<TdpRoles, Long>{
+	public List<Object[]> getRoles();
 
 }
