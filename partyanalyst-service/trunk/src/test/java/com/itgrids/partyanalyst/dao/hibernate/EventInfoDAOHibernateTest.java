24d9bed12e989c3304750aad14390f547d3fe254
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-04-24 05:56:41 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/EventInfoDAOHibernateTest.java
@@ -0,0 +1,17 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IEventInfoDAO;
+
+public class EventInfoDAOHibernateTest extends BaseDaoTestCase{
+	private IEventInfoDAO eventInfoDAO;
+
+	public void setEventInfoDAO(IEventInfoDAO eventInfoDAO) {
+		this.eventInfoDAO = eventInfoDAO;
+	}
+	
+	
+	
+
+}
