42a91dd9d2c6022f08b059a159ae2800f6b4fe49
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-04-24 05:56:28 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventInfoDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.EventInfo;
+
+public interface IEventInfoDAO extends GenericDao<EventInfo, Long>{
+
+}
