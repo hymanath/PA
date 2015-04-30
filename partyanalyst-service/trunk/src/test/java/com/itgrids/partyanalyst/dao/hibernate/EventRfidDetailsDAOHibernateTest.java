4d0e1e6f83130a6a60bceede2170ca429d28de90
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-04-30 00:58:11 +0000

    updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/EventRfidDetailsDAOHibernateTest.java
@@ -0,0 +1,19 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IEventRfidDetailsDAO;
+
+public class EventRfidDetailsDAOHibernateTest extends BaseDaoTestCase{
+
+	private IEventRfidDetailsDAO eventRfidDetailsDAO;
+
+	public void setEventRfidDetailsDAO(IEventRfidDetailsDAO eventRfidDetailsDAO) {
+		this.eventRfidDetailsDAO = eventRfidDetailsDAO;
+	}
+	
+	public void test()
+	{
+		eventRfidDetailsDAO.getAll();
+	}
+}
