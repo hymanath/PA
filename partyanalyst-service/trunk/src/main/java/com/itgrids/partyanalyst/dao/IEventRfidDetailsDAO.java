a81ba71286a582095b8734b0fe7ce269cc9ac7b4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-04-30 00:57:41 +0000

    updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventRfidDetailsDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.EventRfidDetails;
+
+public interface IEventRfidDetailsDAO extends GenericDao<EventRfidDetails,Long>{
+
+}
