2284451b2baa7efcf21be3f429f12d6a2b35db0f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-11 16:15:44 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventRfidDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventRfidDetailsDAO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.EventRfidDetails;
 
 public interface IEventRfidDetailsDAO extends GenericDao<EventRfidDetails,Long>{
-
+	public List<Object[]> getEventRFIDDetailsByEventIds(List<Long> eventIds);
 }
