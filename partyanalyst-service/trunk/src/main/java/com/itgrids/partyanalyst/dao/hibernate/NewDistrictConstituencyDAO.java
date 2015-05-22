13750d98e912557eb802b3698c8fae01a48d2f5f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-22 12:09:56 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NewDistrictConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NewDistrictConstituencyDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.INewDistrictConstituencyDAO;
 import com.itgrids.partyanalyst.model.NewDistrictConstituency;
@@ -10,4 +13,12 @@ public class NewDistrictConstituencyDAO extends GenericDaoHibernate<NewDistrictC
 		super(NewDistrictConstituency.class);
 	}
 
+	public List<Object[]> getConstituencyListForDistrict(Long districtId)	
+	{
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select distinct model.constituency.constituencyId ,model.constituency.name from NewDistrictConstituency model where model.district.districtId = :districtId " +
+				" order by model.constituency.name asc ");
+		Query query = getSession().createQuery(queryStr.toString());
+		return query.list();
+	}
 }
