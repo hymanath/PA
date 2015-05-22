6675edae769c00d126e21828f0b19221f05627a0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-22 12:09:33 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INewDistrictConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INewDistrictConstituencyDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.NewDistrictConstituency;
 
 public interface INewDistrictConstituencyDAO  extends GenericDao<NewDistrictConstituency, Long>{
-
+	public List<Object[]> getConstituencyListForDistrict(Long districtId);
 }
