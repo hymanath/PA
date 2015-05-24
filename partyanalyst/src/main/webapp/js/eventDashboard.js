8cbc81fc151d70261e0f229ffe3b1769c5f9ccc8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-24 10:22:25 +0000

    update by nagamani

--- partyanalyst/src/main/webapp/js/eventDashboard.js
+++ partyanalyst/src/main/webapp/js/eventDashboard.js
@@ -2,7 +2,7 @@ var eventColorArr = [];
 function setcolorsForEvents()
 	{
 		eventColorArr = new Array();
-		var colorStatic = new Array('#FFEE1A','#7FE6E5','#00B17D','#D64D54','#5C2D25','#7AA300','#7A7ACC','#5CB8E6','#2E5C00','#194775','#73008C','#1F8F99','#7A637A','#A6A88A','#996633','#2E0F2E','#5C5C8A','#1F7A7A','#944DDB','#738ACF','#120B0B','#800B0B','#1FCB9D','#90446A','#A9F5A9','#F8E0F7','#CECEF6','#A9F5D0','#8000FF','#E3F6CE','#CEE3F6','#E0F8E0','#5E610B');
+		var colorStatic = new Array('#FFEE1A','#48D1CC','#00B17D','#D64D54','#5C2D25','#7AA300','#7A7ACC','#5CB8E6','#2E5C00','#194775','#73008C','#1F8F99','#7A637A','#A6A88A','#996633','#2E0F2E','#5C5C8A','#1F7A7A','#944DDB','#738ACF','#120B0B','#800B0B','#1FCB9D','#90446A','#A9F5A9','#F8E0F7','#CECEF6','#A9F5D0','#8000FF','#E3F6CE','#CEE3F6','#E0F8E0','#5E610B','#4682B4','#008080','#9ACD32','#800080');
 		var colorArray = new Array();
 		var colorCount = 0;
 		if(subEvents.length > 0)
