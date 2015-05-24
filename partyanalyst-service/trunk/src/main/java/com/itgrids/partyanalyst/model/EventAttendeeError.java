48f0f9bd817790032ba6b08df95c9768575b845b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-24 07:37:09 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/EventAttendeeError.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/EventAttendeeError.java
@@ -3,17 +3,23 @@ package com.itgrids.partyanalyst.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.LazyToOne;
 import org.hibernate.annotations.LazyToOneOption;
 import org.hibernate.annotations.NotFoundAction;
-
+@Entity
+@Table(name = "event_attendee_error")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class EventAttendeeError {
 
 	
