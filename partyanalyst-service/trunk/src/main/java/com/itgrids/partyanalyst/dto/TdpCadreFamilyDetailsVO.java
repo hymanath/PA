c6b83afbfa2afcff92742b792e79713d7ba5ff9c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-25 09:17:24 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TdpCadreFamilyDetailsVO.java
@@ -0,0 +1,128 @@
+package com.itgrids.partyanalyst.dto;
+
+public class TdpCadreFamilyDetailsVO {
+	
+	private Long tdpCadreId;
+	private String votercardNo;
+	private String name;
+	private Long age;
+	private String mobileNo;
+	private String gender;
+	private Long educationId;
+	private Long casteStateId;
+	private Long occupationId;
+	private Long bloodGroupId;
+	private String dob;
+	private String email;
+	private String marriageDay;
+	private String partyMemberSince;
+	private String whatsappStatus;
+	private Long relationId;
+	
+	
+	
+	public Long getRelationId() {
+		return relationId;
+	}
+	public void setRelationId(Long relationId) {
+		this.relationId = relationId;
+	}
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	public String getVotercardNo() {
+		return votercardNo;
+	}
+	public void setVotercardNo(String votercardNo) {
+		this.votercardNo = votercardNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Long getEducationId() {
+		return educationId;
+	}
+	public void setEducationId(Long educationId) {
+		this.educationId = educationId;
+	}
+	public Long getCasteStateId() {
+		return casteStateId;
+	}
+	public void setCasteStateId(Long casteStateId) {
+		this.casteStateId = casteStateId;
+	}
+	public Long getOccupationId() {
+		return occupationId;
+	}
+	public void setOccupationId(Long occupationId) {
+		this.occupationId = occupationId;
+	}
+	public Long getBloodGroupId() {
+		return bloodGroupId;
+	}
+	public void setBloodGroupId(Long bloodGroupId) {
+		this.bloodGroupId = bloodGroupId;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMarriageDay() {
+		return marriageDay;
+	}
+	public void setMarriageDay(String marriageDay) {
+		this.marriageDay = marriageDay;
+	}
+	public String getPartyMemberSince() {
+		return partyMemberSince;
+	}
+	public void setPartyMemberSince(String partyMemberSince) {
+		this.partyMemberSince = partyMemberSince;
+	}
+	public String getWhatsappStatus() {
+		return whatsappStatus;
+	}
+	public void setWhatsappStatus(String whatsappStatus) {
+		this.whatsappStatus = whatsappStatus;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
