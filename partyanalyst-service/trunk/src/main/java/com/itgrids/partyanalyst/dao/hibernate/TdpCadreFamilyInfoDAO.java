4c093109b5f9c118ab481fed6c5ccebdd5a28d3d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-25 08:46:38 +0000

    updated by praneetha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreFamilyInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreFamilyInfoDAO.java
@@ -2,11 +2,11 @@ package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
-import com.itgrids.partyanalyst.dao.ITdpCadreFamilyInfo;
+import com.itgrids.partyanalyst.dao.ITdpCadreFamilyInfoDAO;
 import com.itgrids.partyanalyst.model.TdpCadreFamilyInfo;
 
 
-public class TdpCadreFamilyInfoDAO extends GenericDaoHibernate<TdpCadreFamilyInfo, Long> implements ITdpCadreFamilyInfo{
+public class TdpCadreFamilyInfoDAO extends GenericDaoHibernate<TdpCadreFamilyInfo, Long> implements ITdpCadreFamilyInfoDAO{
 
 	public TdpCadreFamilyInfoDAO() {
 		super(TdpCadreFamilyInfo.class);
