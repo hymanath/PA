23e62d4a1acd669208364e36ec4ce24219c0a8a0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-25 06:31:10 +0000

    updated by praneetha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreFamilyInfo.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreFamilyInfo;
+
+
+public interface ITdpCadreFamilyInfo extends GenericDao<TdpCadreFamilyInfo, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreFamilyInfo.java
@@ -0,0 +1,330 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+
+
+
+@Entity
+@Table(name = "tdp_cadre_family_info")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class TdpCadreFamilyInfo  extends BaseModel implements java.io.Serializable {
+
+	
+	
+	private Long tdpCadreFamilyInfoId;
+	private Long tdpCadreId;
+	private TdpCadre tdpCadre;
+	private Long voterId;
+	private Voter voter;
+	private String name;
+	private Long age;
+	private String mobileNo;
+	private String gender;
+	private Long educationId;
+	
+	private EducationalQualifications education;
+	private Long occupationId;
+	private Occupation occupation;	
+	private String isDeleted;
+	private Date insertedTime;
+	private Date updatedTime;
+	private Long relationId;
+	private Long insertedBy;
+	private Long updatedBy;	
+	private User insertedUser;
+	private User updatedUser;
+	
+	
+	private String email;
+	private Long casteStateId;
+	private CasteState casteState;
+	private Date marriageDay;
+	private Date dob;
+	private Date partyMemberSince;
+	private String whatsappStatus;
+	
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "tdp_cadre_family_info_id", unique = true, nullable = false)
+	public Long getTdpCadreFamilyInfoId() {
+		return tdpCadreFamilyInfoId;
+	}
+	public void setTdpCadreFamilyInfoId(Long tdpCadreFamilyInfoId) {
+		this.tdpCadreFamilyInfoId = tdpCadreFamilyInfoId;
+	}
+	
+	@Column(name="tdp_cadre_id")
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "tdp_cadre_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadre getTdpCadre() {
+		return tdpCadre;
+	}
+	public void setTdpCadre(TdpCadre tdpCadre) {
+		this.tdpCadre = tdpCadre;
+	}
+	
+	@Column(name="voter_id")
+	public Long getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(Long voterId) {
+		this.voterId = voterId;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "voter_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Voter getVoter() {
+		return voter;
+	}
+	public void setVoter(Voter voter) {
+		this.voter = voter;
+	}
+	
+	
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name="age")
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	
+	
+	@Column(name="mobile_no")
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	@Column(name="gender")
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	@Column(name="education_id")
+	public Long getEducationId() {
+		return educationId;
+	}
+	public void setEducationId(Long educationId) {
+		this.educationId = educationId;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "education_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public EducationalQualifications getEducation() {
+		return education;
+	}
+	public void setEducation(EducationalQualifications education) {
+		this.education = education;
+	}
+	
+	
+	@Column(name="occupation_id")
+	public Long getOccupationId() {
+		return occupationId;
+	}
+	public void setOccupationId(Long occupationId) {
+		this.occupationId = occupationId;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "occupation_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Occupation getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(Occupation occupation) {
+		this.occupation = occupation;
+	}
+	
+		
+	@Column(name="is_deleted")
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	
+	@Column(name="inserted_time")
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	
+	@Column(name="updated_time")
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	
+	
+	@Column(name="relation_id")
+	public Long getRelationId() {
+		return relationId;
+	}
+	public void setRelationId(Long relationId) {
+		this.relationId = relationId;
+	}
+	
+	@Column(name="inserted_by")
+	public Long getInsertedBy() {
+		return insertedBy;
+	}
+	public void setInsertedBy(Long insertedBy) {
+		this.insertedBy = insertedBy;
+	}
+	
+	
+	
+	@Column(name="updated_by")
+	public Long getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Long updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "inserted_by" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getInsertedUser() {
+		return insertedUser;
+	}
+	public void setInsertedUser(User insertedUser) {
+		this.insertedUser = insertedUser;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "updated_by" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getUpdatedUser() {
+		return updatedUser;
+	}
+	public void setUpdatedUser(User updatedUser) {
+		this.updatedUser = updatedUser;
+	}	
+	
+	
+	@Column(name="email")
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Column(name="caste_state_id")
+	public CasteState getCasteState() {
+		return casteState;
+	}
+	public void setCasteState(CasteState casteState) {
+		this.casteState = casteState;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "caste_state_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Long getCasteStateId() {
+		return casteStateId;
+	}
+	public void setCasteStateId(Long casteStateId) {
+		this.casteStateId = casteStateId;
+	}
+	
+
+	@Column(name="dob")
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	@Column(name="party_member_since")
+	public Date getPartyMemberSince() {
+		return partyMemberSince;
+	}
+	public void setPartyMemberSince(Date partyMemberSince) {
+		this.partyMemberSince = partyMemberSince;
+	}
+	
+	
+	@Column(name="whatsapp_status")
+	public String getWhatsappStatus() {
+		return whatsappStatus;
+	}
+	public void setWhatsappStatus(String whatsappStatus) {
+		this.whatsappStatus = whatsappStatus;
+	}
+	
+	
+	@Column(name="marriage_day")
+	public Date getMarriageDay() {
+		return marriageDay;
+	}
+	public void setMarriageDay(Date marriageDay) {
+		this.marriageDay = marriageDay;
+	}
+
+	
+}
