d7dedabee3e42dc46dc1c0fd50e6915f690b17df
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-26 09:34:17 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreFamilyInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreFamilyInfoDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
 import java.util.List;
+import java.util.Set;
 
 import org.appfuse.dao.GenericDao;
 
@@ -10,4 +11,5 @@ import com.itgrids.partyanalyst.model.TdpCadreFamilyInfo;
 public interface ITdpCadreFamilyInfoDAO extends GenericDao<TdpCadreFamilyInfo, Long>{
 	public Integer deleteFamilyInfoByCadre(Long tdpCadreId);
 	public List<TdpCadreFamilyInfo> getCadreFamilyDetailsBytdpCadreId(Long tdpCadreId);
+	public List<TdpCadreFamilyInfo> getCadresFamilyDetailsBytdpCadreIdList(Set<Long> tdpCadreIdsSet);
 }
