1a4973ab70cf6ae7065cd561bb793ae650828461
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-05-29 05:34:03 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreIvrResponseDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/CadreIvrResponseDAOHibernateTest.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class CadreIvrResponseDAOHibernateTest extends BaseDaoTestCase{
 	}
 	
 	
-	public void test()
+	/*public void test()
 	{
 		Date convertedDate = null;
 		try{
@@ -32,6 +33,34 @@ public class CadreIvrResponseDAOHibernateTest extends BaseDaoTestCase{
 	List<Object[]> list =cadreIvrResponseDAO.getIvrCadreDetails(convertedDate,null,"total",0,201);
 	System.out.println(list.size());
 	
+	}*/
+	
+	public void testGetStateWiseCommitterIvrDetails()
+	{
+		List<Long> distIds = new ArrayList<Long>();
+		distIds.add(11l);
+		distIds.add(12l);
+		distIds.add(13l);
+		distIds.add(14l);
+		distIds.add(15l);
+		distIds.add(16l);
+		distIds.add(17l);
+		distIds.add(18l);
+		distIds.add(19l);
+		distIds.add(20l);
+		distIds.add(21l);
+		distIds.add(22l);
+		distIds.add(23l);
+		List<Object[]> list = cadreIvrResponseDAO.getStateWiseCommitterIvrDetails(distIds,3l);
+		
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println();
+			for(Object obj : params)
+				System.out.print("\t"+obj);
+		}
 	}
 
 }
