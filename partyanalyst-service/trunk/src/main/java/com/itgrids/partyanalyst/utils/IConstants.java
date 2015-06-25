0c1950d57afd1c1d7a3b803d6104ce28cd104901
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-06-25 12:07:45 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -887,4 +887,7 @@ public interface IConstants {
     public static final String RMQ_QUEUE = "attendee_queue";
     public static final String ROUTING_KEY = "event_Ws_184375";
     
+    public static final String 	SURVEY_WEBSERVICE_USERNAME = "itgrids";
+    public static final String  SURVEY_WEBSERVICE_PASSWORD = "servey@android";
+    
 }
