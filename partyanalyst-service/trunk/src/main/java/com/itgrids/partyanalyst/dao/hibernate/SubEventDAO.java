866c89e0a3efb9c047e9eff4ecba7f786471a8f6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-07-20 17:42:31 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SubEventDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SubEventDAO.java
@@ -1,11 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import org.appfuse.dao.jpa.GenericDaoJpa;
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.ISubEventDAO;
 import com.itgrids.partyanalyst.model.SubEvent;
 
-public class SubEventDAO extends GenericDaoJpa<SubEvent, Long> implements ISubEventDAO{
+public class SubEventDAO extends GenericDaoHibernate<SubEvent, Long> implements ISubEventDAO{
 
 	public SubEventDAO() {
 		super(SubEvent.class);
