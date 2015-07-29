5267df82a89d22c35e0074b7dd3800f2f0a9700b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-07-29 16:54:37 +0000

    updated by   - balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateContestedLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CandidateContestedLocationDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICandidateContestedLocationDAO;
 import com.itgrids.partyanalyst.model.CandidateContestedLocation;
@@ -12,4 +13,12 @@ public class CandidateContestedLocationDAO extends GenericDaoHibernate<Candidate
 		
 	}
 
+	public CandidateContestedLocation getCandidateContestedLocationDetailsByCadre(Long cadreId){
+		
+		Query query=getSession().createQuery("select model from CandidateContestedLocation model " +
+				" where model.tdpCadre.tdpCadreId =:cadreId ");
+		
+		query.setParameter("cadreId", cadreId);
+		return 	(CandidateContestedLocation) query.uniqueResult();
+	}
 }
