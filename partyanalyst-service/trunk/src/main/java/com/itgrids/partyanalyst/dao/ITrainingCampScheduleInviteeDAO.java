1f3523d023c4af886b88012bf4172b05a338f3ad
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-03 13:21:37 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampScheduleInviteeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TrainingCampScheduleInvitee;
+
+public interface ITrainingCampScheduleInviteeDAO extends GenericDao<TrainingCampScheduleInvitee, Long>{
+
+}
