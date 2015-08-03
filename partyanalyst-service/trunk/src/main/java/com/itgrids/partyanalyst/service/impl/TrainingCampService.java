52240ef62161d7497419f73ff2ff307179404b6a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-03 07:12:01 +0000

    updated by   - balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/TrainingCampService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ITrainingCampService;
+
+public class TrainingCampService implements ITrainingCampService{
+
+	public static Logger log = Logger.getLogger(TrainingCampService.class);
+}
