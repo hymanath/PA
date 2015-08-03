bf53f38bdc9326baf9e4f4d55998f724b63225b1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-03 13:25:46 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampDistrictDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampDistrictDAO.java
@@ -1,6 +1,8 @@
-package com.itgrids.grievance.dao;
 
+package com.itgrids.partyanalyst.dao;
 import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TrainingCampDistrict;
 
 public interface ITrainingCampDistrictDAO extends GenericDao<TrainingCampDistrict, Long>{
 
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampDistrictDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampDistrictDAO.java
@@ -1,9 +1,15 @@
-package com.itgrids.grievance.dao.impl;
-
+package com.itgrids.partyanalyst.dao.hibernate;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
-import com.itgrids.grievance.dao.ITrainingCampDistrictDAO;
+
+import com.itgrids.partyanalyst.dao.ITrainingCampDistrictDAO;
+import com.itgrids.partyanalyst.model.TrainingCampDistrict;
 
 public class TrainingCampDistrictDAO extends GenericDaoHibernate<TrainingCampDistrict, Long> implements ITrainingCampDistrictDAO{
+
+	public TrainingCampDistrictDAO() {
+		super(TrainingCampDistrict.class);
+		// TODO Auto-generated constructor stub
+	}
 
 }
