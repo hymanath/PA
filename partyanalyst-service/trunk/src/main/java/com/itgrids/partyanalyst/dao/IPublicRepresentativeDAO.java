583db0c6ed05c81581405031f7dba5c348971f08
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-04 16:55:54 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPublicRepresentativeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPublicRepresentativeDAO.java
@@ -7,6 +7,6 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PublicRepresentative;
 
 public interface IPublicRepresentativeDAO extends GenericDao<PublicRepresentative, Long> {
-	public List<Long> getRepresentativesByPositions(Long representativeLevelId,List<Long> locationValuesList,Long positionId);
+	public List<Integer> getRepresentativesByPositions(Long representativeLevelId,List<Long> locationValuesList,Long positionId);
 	public List<Object[]> getCandidateInfoByCandidateIds(List<Long> candidateIdsList);
 }
