df407b449b49260523b97ac34981d5f0f40c070f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-05 07:10:08 +0000

    updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TrainingCampVO.java
@@ -0,0 +1,149 @@
+package com.itgrids.partyanalyst.dto;
+
+public class TrainingCampVO implements java.io.Serializable{
+
+	private Long id;
+	private String name;
+	private String trainingCampName;
+	private String trainerName;
+	private String scheduledDate;
+	private Long interestedCount;
+	private Long notInterestedCount;
+	private Long nextBatchInterestedCount;
+	private Long assignedCount;
+	private Long availableMembersCount;
+	private Long totalProgrammesCount;
+	private Long dialedCallsCount;
+	private Long todayAllocatedCount;
+	private Long allocatedCallsCount;
+	private Long filledCount;
+	private Long scheduleConfirmationCount;
+	private Long batchConfirmationCount;
+	private Long assignedCalls;
+	private Long completedCalls;
+	private Long pendingCalls;
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getTrainingCampName() {
+		return trainingCampName;
+	}
+	public String getTrainerName() {
+		return trainerName;
+	}
+	public String getScheduledDate() {
+		return scheduledDate;
+	}
+	public Long getInterestedCount() {
+		return interestedCount;
+	}
+	public Long getNotInterestedCount() {
+		return notInterestedCount;
+	}
+	public Long getNextBatchInterestedCount() {
+		return nextBatchInterestedCount;
+	}
+	public Long getAssignedCount() {
+		return assignedCount;
+	}
+	public Long getAvailableMembersCount() {
+		return availableMembersCount;
+	}
+	public Long getTotalProgrammesCount() {
+		return totalProgrammesCount;
+	}
+	public Long getDialedCallsCount() {
+		return dialedCallsCount;
+	}
+	public Long getTodayAllocatedCount() {
+		return todayAllocatedCount;
+	}
+	public Long getAllocatedCallsCount() {
+		return allocatedCallsCount;
+	}
+	public Long getFilledCount() {
+		return filledCount;
+	}
+	public Long getScheduleConfirmationCount() {
+		return scheduleConfirmationCount;
+	}
+	public Long getBatchConfirmationCount() {
+		return batchConfirmationCount;
+	}
+	public Long getAssignedCalls() {
+		return assignedCalls;
+	}
+	public Long getCompletedCalls() {
+		return completedCalls;
+	}
+	public Long getPendingCalls() {
+		return pendingCalls;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setTrainingCampName(String trainingCampName) {
+		this.trainingCampName = trainingCampName;
+	}
+	public void setTrainerName(String trainerName) {
+		this.trainerName = trainerName;
+	}
+	public void setScheduledDate(String scheduledDate) {
+		this.scheduledDate = scheduledDate;
+	}
+	public void setInterestedCount(Long interestedCount) {
+		this.interestedCount = interestedCount;
+	}
+	public void setNotInterestedCount(Long notInterestedCount) {
+		this.notInterestedCount = notInterestedCount;
+	}
+	public void setNextBatchInterestedCount(Long nextBatchInterestedCount) {
+		this.nextBatchInterestedCount = nextBatchInterestedCount;
+	}
+	public void setAssignedCount(Long assignedCount) {
+		this.assignedCount = assignedCount;
+	}
+	public void setAvailableMembersCount(Long availableMembersCount) {
+		this.availableMembersCount = availableMembersCount;
+	}
+	public void setTotalProgrammesCount(Long totalProgrammesCount) {
+		this.totalProgrammesCount = totalProgrammesCount;
+	}
+	public void setDialedCallsCount(Long dialedCallsCount) {
+		this.dialedCallsCount = dialedCallsCount;
+	}
+	public void setTodayAllocatedCount(Long todayAllocatedCount) {
+		this.todayAllocatedCount = todayAllocatedCount;
+	}
+	public void setAllocatedCallsCount(Long allocatedCallsCount) {
+		this.allocatedCallsCount = allocatedCallsCount;
+	}
+	public void setFilledCount(Long filledCount) {
+		this.filledCount = filledCount;
+	}
+	public void setScheduleConfirmationCount(Long scheduleConfirmationCount) {
+		this.scheduleConfirmationCount = scheduleConfirmationCount;
+	}
+	public void setBatchConfirmationCount(Long batchConfirmationCount) {
+		this.batchConfirmationCount = batchConfirmationCount;
+	}
+	public void setAssignedCalls(Long assignedCalls) {
+		this.assignedCalls = assignedCalls;
+	}
+	public void setCompletedCalls(Long completedCalls) {
+		this.completedCalls = completedCalls;
+	}
+	public void setPendingCalls(Long pendingCalls) {
+		this.pendingCalls = pendingCalls;
+	}
+	
+	
+	
+	
+}
