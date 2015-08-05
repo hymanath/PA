9e46eaf1cf226ff48d5c250632de72879276b333
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-05 11:46:44 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeDAO.java
@@ -1,6 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITrainingCampScheduleInviteeDAO;
 import com.itgrids.partyanalyst.model.TrainingCampScheduleInvitee;
@@ -9,7 +13,40 @@ public class TrainingCampScheduleInviteeDAO extends GenericDaoHibernate<Training
 
 	public TrainingCampScheduleInviteeDAO() {
 		super(TrainingCampScheduleInvitee.class);
-		// TODO Auto-generated constructor stub
 	}
 
+	public List<Object[]> getCampusWiseBatchWiseMembersDetails(String membersType, String searchType, Date startDate, Date endDate)
+	{
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append("select TCSI.trainingCampSchedule.trainingCamp.campName,TCSI.trainingCampSchedule.trainingCampScheduleCode ");
+		queryStr.append(" ,TCSI.scheduleInviteeStatus.scheduleInviteeStatusId,TCSI.scheduleInviteeStatus.status ,count(distinct TCSI.tdpCadreId) ");
+		
+		queryStr.append(" from TrainingCampScheduleInvitee TCSI,TrainingCampBatch TCB where TCSI.trainingCampSchedule.trainingCampId = TCB.trainingCampBatchId ");
+		
+		if(startDate != null && endDate != null)
+		{
+			queryStr.append(" and (date(TCSI.trainingCampSchedule.fromDate) >=:startDate and date(TCSI.trainingCampSchedule.fromDate) <=:endDate) ");
+		}
+		if(searchType != null && searchType.equalsIgnoreCase("notStarted"))
+		{
+			queryStr.append(" and TCSI.trainingCampSchedule.status ='Not Started' ");
+		}
+		else if(searchType != null && searchType.equalsIgnoreCase("running"))
+		{
+			queryStr.append(" and TCSI.trainingCampSchedule.status ='Progress' ");
+		}
+		else if(searchType != null && searchType.equalsIgnoreCase("completed"))
+		{
+			queryStr.append(" and TCSI.trainingCampSchedule.status ='Completed' ");
+		}
+		queryStr.append(" group by TCSI.trainingCampSchedule.trainingCamp.trainingCampId,TCSI.trainingCampSchedule.trainingCampScheduleCode,TCSI.scheduleInviteeStatus.scheduleInviteeStatusId order by " +
+				" TCSI.trainingCampSchedule.trainingCamp.trainingCampId ");
+		Query query = getSession().createQuery(queryStr.toString());
+		if(startDate != null && endDate != null)
+		{
+			query.setDate("startDate", startDate);
+			query.setDate("endDate", endDate);
+		}
+		return query.list();
+	}
 }
