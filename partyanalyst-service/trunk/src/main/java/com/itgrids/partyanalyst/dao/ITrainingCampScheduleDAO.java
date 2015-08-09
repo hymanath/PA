b63cb158b643ecbeb749bcda0e1101134f48155d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-09 03:49:08 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampScheduleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampScheduleDAO.java
@@ -7,5 +7,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.TrainingCampSchedule;
 
 public interface ITrainingCampScheduleDAO extends GenericDao<TrainingCampSchedule, Long>{
+	public List<Object[]> getProgrammesListByCampsList(List<Long> campsList);
+	public List<Object[]> getScheduledDetailsByProgrammes(List<Long> programmesList);
 	public List<Object[]> getSchedules();
 }
