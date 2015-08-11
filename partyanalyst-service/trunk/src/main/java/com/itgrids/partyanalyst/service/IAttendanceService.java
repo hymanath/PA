7cd8df46e9917c4433898d1335a1657bf816a687
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-11 22:33:47 +0000

    Created by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAttendanceService.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.service;
+
+import com.itgrids.partyanalyst.dto.AttendanceVO;
+
+public interface IAttendanceService {
+
+	public AttendanceVO saveAttendance(AttendanceVO attendanceVO);
+}
