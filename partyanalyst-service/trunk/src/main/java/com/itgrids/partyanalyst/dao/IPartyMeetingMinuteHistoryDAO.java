b1f9029675f08113750880f910fbbcb7eb19027a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-11 14:21:28 +0000

    Created by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingMinuteHistoryDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.PartyMeetingMinuteHistory;
+
+public interface IPartyMeetingMinuteHistoryDAO extends GenericDao<PartyMeetingMinuteHistory,Long>{
+
+}
