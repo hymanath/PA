f95e4a82bd493c8f481ba521a0c5e14d37a67f6d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-11 07:27:45 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITrainingCampService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITrainingCampService.java
@@ -2,10 +2,12 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.BasicVO;
 import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.TraingCampCallerVO;
 import com.itgrids.partyanalyst.dto.TraingCampDataVO;
+import com.itgrids.partyanalyst.dto.TrainingCadreVO;
 import com.itgrids.partyanalyst.dto.TrainingCampScheduleVO;
 import com.itgrids.partyanalyst.dto.TrainingCampVO;
 import com.itgrids.partyanalyst.dto.TrainingMemberVO;
@@ -20,9 +22,16 @@ public interface ITrainingCampService {
 	public List<TraingCampCallerVO> getBatchCallStatusCount(Long userId,Long callPurposeId);
 	public List<Long> getTrainingCampUserTypeIds();
 	public TrainingMemberVO getScheduleCallMemberDetails(TraingCampDataVO inputVo);
+	public List<BasicVO> getAllPrograms();
+	public List<BasicVO> getCampsByProgramId(Long programId);
+	public List<BasicVO> getSchedulesByCampId(Long campId);
+	public List<BasicVO> getBatchesByScheduleId(Long scheduleId);
 	//public List<BasicVO> getAllPrograms();
 	//public List<BasicVO> getAllschedules();
 	public TrainingCampScheduleVO getTrainingProgramMembersBatchCount(String startDateString,String endDateString);
+	public List<TraingCampCallerVO> getScheduleStatusList();
+	public List<TraingCampCallerVO> getStatusList();
+	public ResultStatus updateCadreStatusForTraining(TrainingCadreVO inputVO);
 	public TrainingCampScheduleVO getScheduleAndConfirmationCallsOfCallerToAgent(List<Long> userIds,String startDateString,String endDateString);
 	public List<IdNameVO> getAllTrainingCampsInfoByDistrictIds(List<Long> districtIds);
 	public List<IdNameVO> getProgrammesDetailsByCamps(List<Long> campIdsList);
