ef3b3de5c3467ba3dd1762bcd370dc78caaabd79
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-11 22:33:04 +0000

    Created by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/AttendanceVO.java
@@ -0,0 +1,137 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class AttendanceVO implements Serializable{
+
+	private static final long serialVersionUID = -6548396594726688141L;
+	
+	private Long partyMeetingId;
+	private Long trainingCampScheduleId;
+	private Long trainingCampBatchId;
+	private Long trainingCampTopicId;
+	private String membershipId;
+	private String attendedTime;
+	private String rfid;
+	private String imei;
+	private String uniqueKey;
+	private String latitude;
+	private String longitude;
+	private Long tabUserId;
+	private Long currentTabUserId;
+	private String errorDescription;
+	private String syncSource;
+	private String type;
+	private String status;
+	private Long tdpCadreId;
+	
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSyncSource() {
+		return syncSource;
+	}
+	public void setSyncSource(String syncSource) {
+		this.syncSource = syncSource;
+	}
+	public String getErrorDescription() {
+		return errorDescription;
+	}
+	public void setErrorDescription(String errorDescription) {
+		this.errorDescription = errorDescription;
+	}
+	public Long getPartyMeetingId() {
+		return partyMeetingId;
+	}
+	public void setPartyMeetingId(Long partyMeetingId) {
+		this.partyMeetingId = partyMeetingId;
+	}
+	public Long getTrainingCampScheduleId() {
+		return trainingCampScheduleId;
+	}
+	public void setTrainingCampScheduleId(Long trainingCampScheduleId) {
+		this.trainingCampScheduleId = trainingCampScheduleId;
+	}
+	public Long getTrainingCampBatchId() {
+		return trainingCampBatchId;
+	}
+	public void setTrainingCampBatchId(Long trainingCampBatchId) {
+		this.trainingCampBatchId = trainingCampBatchId;
+	}
+	public Long getTrainingCampTopicId() {
+		return trainingCampTopicId;
+	}
+	public void setTrainingCampTopicId(Long trainingCampTopicId) {
+		this.trainingCampTopicId = trainingCampTopicId;
+	}
+	public String getMembershipId() {
+		return membershipId;
+	}
+	public void setMembershipId(String membershipId) {
+		this.membershipId = membershipId;
+	}
+	public String getAttendedTime() {
+		return attendedTime;
+	}
+	public void setAttendedTime(String attendedTime) {
+		this.attendedTime = attendedTime;
+	}
+	public String getRfid() {
+		return rfid;
+	}
+	public void setRfid(String rfid) {
+		this.rfid = rfid;
+	}
+	public String getImei() {
+		return imei;
+	}
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+	public String getUniqueKey() {
+		return uniqueKey;
+	}
+	public void setUniqueKey(String uniqueKey) {
+		this.uniqueKey = uniqueKey;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public Long getTabUserId() {
+		return tabUserId;
+	}
+	public void setTabUserId(Long tabUserId) {
+		this.tabUserId = tabUserId;
+	}
+	public Long getCurrentTabUserId() {
+		return currentTabUserId;
+	}
+	public void setCurrentTabUserId(Long currentTabUserId) {
+		this.currentTabUserId = currentTabUserId;
+	}
+	
+}
