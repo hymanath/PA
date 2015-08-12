2e217d02286d7f8371849f3cd6d154dda920a494
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-12 15:41:38 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAtrPointDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAtrPointDAO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyMeetingAtrPoint;
 
 public interface IPartyMeetingAtrPointDAO extends GenericDao<PartyMeetingAtrPoint,Long>{
-
+	public List<Object[]> getPartyMeetingsATRPointsDetls(Long partyMeetingTypeId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDocumentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDocumentDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyMeetingDocument;
 
 public interface IPartyMeetingDocumentDAO extends GenericDao<PartyMeetingDocument,Long>{
 
+	public List<Object[]> getPartyMeetingsDocumentsDetls(Long partyMeetingTypeId);
 }
