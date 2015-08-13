c494af3612a04fba4d311020d8ad0c9526f54c2e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-13 17:13:33 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
@@ -4,5 +4,6 @@ import com.itgrids.partyanalyst.dto.PartyMeetingVO;
 
 public interface IPartyMeetingService {
 	public PartyMeetingVO getMeetingTypeWiseDescription(Long partyMeetingTypeId);
-	public PartyMeetingVO getPartyMeetingDetailsBySearchType(String memberShipNo,Long constituencyId);
+	public PartyMeetingVO getPartyMeetingDetailsBySearchType(Long tdpCadreId );
+	public PartyMeetingVO getPartyMeetingsForCadrePeople(Long tdpCadreId);
 }
