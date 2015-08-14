bb0bc17349ff38d81678d444689a4725f6430a27
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-14 12:41:18 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDAO.java
@@ -1,11 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyMeeting;
 
 public interface IPartyMeetingDAO extends GenericDao<PartyMeeting,Long>{
-	public List<Object[]> getAllMeetings(Long meetingType,Long locationLevel,Long meetingLocation);
+	public List<Object[]> getAllMeetings(Long meetingType,Long locationLevel,Long stateId,Long districtId,Long constituencyId,Long mandalId,Long townId,Long divisonId,Long villageId,Long wardId,Date startDate,Date endDate);
 	public List<Object[]> getPartyMeetingDetailsByMeetingIdList(List<Long> patyMeetingIdsList);
 }
