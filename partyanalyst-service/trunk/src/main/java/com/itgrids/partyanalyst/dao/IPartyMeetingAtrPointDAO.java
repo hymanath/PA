acdea080f56a8c874637a2f8073151356e3aedce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-17 14:58:11 +0000

    updating & deleting atr -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAtrPointDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAtrPointDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -8,4 +9,6 @@ import com.itgrids.partyanalyst.model.PartyMeetingAtrPoint;
 public interface IPartyMeetingAtrPointDAO extends GenericDao<PartyMeetingAtrPoint,Long>{
 	public List<Object[]> getPartyMeetingsATRPointsDetls(Long partyMeetingTypeId);
 	public List<Object[]> getAtrDetailsForAMeeting(Long partyMeetingId);
+	public Integer updateMeetingAtrPoint(Long atrId,String request,String actionTaken,String raisedBy,Long updatedBy,Date updatedTime,Long locatoionId);
+	public Integer deleteMeetingAtrPoint(Long atrId,Long updatedBy,Date updatedTime);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
@@ -8,4 +8,6 @@ public interface IPartyMeetingService {
 	public PartyMeetingVO getPartyMeetingsForCadrePeople(Long tdpCadreId);
 	public String updateMeetingPoint(final Long minuteId,final String minuteText,final Long updatedBy);
 	public String deleteMeetingMinutePoint(Long minuteId,Long updateBy);
+	public String updateMeetingAtrPoint(final Long atrId, final String request,final String actionTaken,final String raisedBy,final Long updatedBy,final Long locationId);
+	public String deleteMeetingAtrPoint(final Long atrId,final Long updatedBy);
 }
