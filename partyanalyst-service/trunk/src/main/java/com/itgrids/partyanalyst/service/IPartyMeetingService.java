949b398501bce6c5db5a2d15ac93cfb28c157953
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-17 17:54:36 +0000

    saving minute points -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingService.java
@@ -6,7 +6,7 @@ public interface IPartyMeetingService {
 	public PartyMeetingVO getMeetingTypeWiseDescription(Long partyMeetingTypeId);
 	public PartyMeetingVO getPartyMeetingDetailsBySearchType(Long tdpCadreId );
 	public PartyMeetingVO getPartyMeetingsForCadrePeople(Long tdpCadreId);
-	public String updateMeetingPoint(final Long minuteId,final String minuteText,final Long updatedBy);
+	public String updateMeetingPoint(final Long minuteId,final String minuteText,final Long updatedBy,Long partyMeetingId);
 	public String deleteMeetingMinutePoint(Long minuteId,Long updateBy);
 	public String updateMeetingAtrPoint(final Long atrId, final String request,final String actionTaken,final String raisedBy,final Long updatedBy,final Long locationId);
 	public String deleteMeetingAtrPoint(final Long atrId,final Long updatedBy);
