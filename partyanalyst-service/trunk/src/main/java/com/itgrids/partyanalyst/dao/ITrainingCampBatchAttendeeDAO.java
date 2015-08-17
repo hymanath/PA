541b7836b39c10c168bdf1381387b800d7e89527
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-17 13:54:14 +0000

    training camp main dashboard-sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampBatchAttendeeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampBatchAttendeeDAO.java
@@ -10,5 +10,9 @@ import com.itgrids.partyanalyst.model.TrainingCampBatchAttendee;
 public interface ITrainingCampBatchAttendeeDAO extends GenericDao<TrainingCampBatchAttendee, Long>{
 	
 	public List<Object[]> getBatchWiseProgramWiseAcceptedMemeberDetails(String searchType, Date startDate, Date endDate);
-
+    
+	public List<Object[]> getTdpCadreDetailsforASchedule(Long scheduleId);
+	public List<Object[]> getAchievementsForCadreBySchedule(Long scheduleId);
+	public List<Object[]> getGoalsForCadreBySchedule(Long scheduleId);
+	
 }
