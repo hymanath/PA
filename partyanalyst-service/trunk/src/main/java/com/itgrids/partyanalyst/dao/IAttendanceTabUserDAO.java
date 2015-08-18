95c861b1282cfba9e478f4f3da07205872f65dc1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-18 02:51:30 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAttendanceTabUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAttendanceTabUserDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.AttendanceTabUser;
 
 public interface IAttendanceTabUserDAO extends GenericDao<AttendanceTabUser,Long>{
-
+	
+	public List<Object[]> getAttendanceTabUserByUsernameAndPassword(String username,String password);
 }
