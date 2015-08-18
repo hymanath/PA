bd16132e334714183b45ed1a8ad89152a680378d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-18 02:54:12 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AttendanceTabUserDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AttendanceTabUserDAOHibernateTest.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IAttendanceTabUserDAO;
@@ -12,8 +14,14 @@ public class AttendanceTabUserDAOHibernateTest extends BaseDaoTestCase{
 		this.attendanceTabUserDAO = attendanceTabUserDAO;
 	}
 	
-	public void test()
+	/*public void test()
 	{
 		attendanceTabUserDAO.getAll();
+	}*/
+	
+	public void testGetAttendanceTabUserByUsernameAndPassword()
+	{
+		List<Object[]> list = attendanceTabUserDAO.getAttendanceTabUserByUsernameAndPassword("kamal","Dandu");
+		System.out.println(list.get(0)[0]);
 	}
 }
