870af9595ab10e04f64437bf43a8b3f0fa4a768d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-19 14:29:45 +0000

    updated by -sreedhar

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampCadreAchievementHistoryDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TrainingCampCadreAchievementHistory;
+
+public interface ITrainingCampCadreAchievementHistoryDAO extends GenericDao<TrainingCampCadreAchievementHistory,Long>{
+
+}
