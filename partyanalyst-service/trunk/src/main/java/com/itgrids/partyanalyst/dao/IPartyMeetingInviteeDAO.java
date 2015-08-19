deb82562e84a057dc78331361cee3a0c4f39414d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-19 01:08:03 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingInviteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingInviteeDAO.java
@@ -7,6 +7,10 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyMeetingInvitee;
 
 public interface IPartyMeetingInviteeDAO extends GenericDao<PartyMeetingInvitee,Long>{
+	
 	public List<Object[]> getPartyMeetingsInvitationsDetailsByCadreIds(List<Long> tdpCadreIdsList);
+	
 	public List<Object[]> getPartyMeetingsInvitationDetlsByCadreIds(List<Long> tdpCadreIdsList,Long partyMeetingTypeId);
+	
+	public List<String> getPartyMeetingInvittees(Long partyMeetingId);
 }
