b9f0ba9e04e5f2fbeaf6b4c78cd4b19716d364ff
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-20 16:41:48 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAttendanceDAO.java
@@ -1,8 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.Attendance;
 
 public interface IAttendanceDAO extends GenericDao<Attendance,Long>{
+	
+	public List<Long> getPrimarykey(String uniqueKey,String imei,Long tabPrimaryKey);
 
 }
