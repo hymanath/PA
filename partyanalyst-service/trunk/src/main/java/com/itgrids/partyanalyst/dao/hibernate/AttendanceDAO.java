33813b586407fe854ef33802a11e3d0286e39b74
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-20 16:42:00 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AttendanceDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAttendanceDAO;
 import com.itgrids.partyanalyst.model.Attendance;
@@ -10,5 +13,16 @@ public class AttendanceDAO extends GenericDaoHibernate<Attendance,Long> implemen
 	public AttendanceDAO()
 	{
 		super(Attendance.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Long> getPrimarykey(String uniqueKey,String imei,Long tabPrimaryKey)
+	{
+		Query query = getSession().createQuery("SELECT model.attendanceId FROM Attendance model where model.uniqueKey = :uniqueKey and model.imei = :imei and model.tabPrimaryKey = :tabPrimaryKey" +
+				" order by model.insertedTime DESC");
+		query.setParameter("uniqueKey",uniqueKey);
+		query.setParameter("imei",imei);
+		query.setParameter("tabPrimaryKey",tabPrimaryKey);
+		return query.list();
 	}
 }
