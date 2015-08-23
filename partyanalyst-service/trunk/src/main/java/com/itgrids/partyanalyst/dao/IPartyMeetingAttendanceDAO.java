61bcda779b989dac3106171e5fa8636fce2de91d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-23 03:32:59 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAttendanceDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -7,10 +8,10 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PartyMeetingAttendance;
 
 public interface IPartyMeetingAttendanceDAO extends GenericDao<PartyMeetingAttendance,Long>{
-	public List<Object[]> getPartyMeetingsAttendenceDetailsByCadreId(List<Long> tdpCadreIdsList);
+	public List<Object[]> getPartyMeetingsAttendenceDetailsByCadreId(List<Long> tdpCadreIdsList,Date toDayDate);
 	public List<Object[]> getTotalAttendedDetailsForCadreIds(List<Long> tdpCadreIdsList,Long partyMeetingTypeId);
 	public List<Object[]> getAbsentMemberDeails(Long partyMeetingTypeId);
-	public List<Object[]> getAttendenceForCadre(Long tdpCadreId);
+	public List<Object[]> getAttendenceForCadre(Long tdpCadreId,Date todayDate);
 	
 	public List<Object[]> getTotalAttendentsOfMeetings(List<Long> partyMeetingIds);
 	public List<Object[]> getInviteesAttendedCountOfMeetings(List<Long> partyMeetingIds);
