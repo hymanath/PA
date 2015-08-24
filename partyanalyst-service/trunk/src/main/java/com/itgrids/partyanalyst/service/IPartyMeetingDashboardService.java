5271ffcd855da0aa1c038416df9fb9ad559fd2e7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-24 08:01:02 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingDashboardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPartyMeetingDashboardService.java
@@ -1,5 +1,10 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.Date;
+
+import com.itgrids.partyanalyst.dto.MeetingSummeryVO;
+
 public interface IPartyMeetingDashboardService {
 
+	public MeetingSummeryVO getMeetingsSummeryForDashboard(Long partyMeetingLevelId,Date fromDate,Date toDate,Long partyMeetingTypeId,Long locationLevelId,Long locationValue);
 }
