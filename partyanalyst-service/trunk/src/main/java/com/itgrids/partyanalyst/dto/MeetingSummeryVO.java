7a32fe5712df3ff99df99a8dd91ccec955b3b311
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-24 08:00:11 +0000

    Updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MeetingSummeryVO.java
@@ -0,0 +1,133 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class MeetingSummeryVO implements Serializable{
+
+	private static final long serialVersionUID = 1525502626789879502L;
+	
+	private Long totalInvitees;
+	private Long totalAttended;
+	private Long totalAbsent;
+	private Long totalNonInvitees;
+	private Long totalInviteeAttended;
+	private Long totalCandidateInvitees;
+	private Long totalCommitteeMemberInvitees;
+	private Long totalCandidateAttended;
+	private Long totalCommitteeMemberAttended;
+	private Long totalCandidateInviteeAttended;
+	private Long totalCandidateAbsent;
+	private Long totalCommitteeMemberInviteeAttended;
+	private Long totalNonInviteeCandidate;
+	private Long totalNonInviteeCommitteeMember;
+	private Long totalCommitteeMemberAbsent;
+	private Long totalCandidateNonInvitees;
+	private Long totalCommitteeMemberNonInvitees;
+	
+	public Long getTotalInvitees() {
+		return totalInvitees;
+	}
+	public void setTotalInvitees(Long totalInvitees) {
+		this.totalInvitees = totalInvitees;
+	}
+	public Long getTotalAttended() {
+		return totalAttended;
+	}
+	public void setTotalAttended(Long totalAttended) {
+		this.totalAttended = totalAttended;
+	}
+	public Long getTotalAbsent() {
+		return totalAbsent;
+	}
+	public void setTotalAbsent(Long totalAbsent) {
+		this.totalAbsent = totalAbsent;
+	}
+	public Long getTotalNonInvitees() {
+		return totalNonInvitees;
+	}
+	public void setTotalNonInvitees(Long totalNonInvitees) {
+		this.totalNonInvitees = totalNonInvitees;
+	}
+	public Long getTotalInviteeAttended() {
+		return totalInviteeAttended;
+	}
+	public void setTotalInviteeAttended(Long totalInviteeAttended) {
+		this.totalInviteeAttended = totalInviteeAttended;
+	}
+	public Long getTotalCandidateInvitees() {
+		return totalCandidateInvitees;
+	}
+	public void setTotalCandidateInvitees(Long totalCandidateInvitees) {
+		this.totalCandidateInvitees = totalCandidateInvitees;
+	}
+	public Long getTotalCommitteeMemberInvitees() {
+		return totalCommitteeMemberInvitees;
+	}
+	public void setTotalCommitteeMemberInvitees(Long totalCommitteeMemberInvitees) {
+		this.totalCommitteeMemberInvitees = totalCommitteeMemberInvitees;
+	}
+	public Long getTotalCandidateAttended() {
+		return totalCandidateAttended;
+	}
+	public void setTotalCandidateAttended(Long totalCandidateAttended) {
+		this.totalCandidateAttended = totalCandidateAttended;
+	}
+	public Long getTotalCommitteeMemberAttended() {
+		return totalCommitteeMemberAttended;
+	}
+	public void setTotalCommitteeMemberAttended(Long totalCommitteeMemberAttended) {
+		this.totalCommitteeMemberAttended = totalCommitteeMemberAttended;
+	}
+	public Long getTotalCandidateInviteeAttended() {
+		return totalCandidateInviteeAttended;
+	}
+	public void setTotalCandidateInviteeAttended(Long totalCandidateInviteeAttended) {
+		this.totalCandidateInviteeAttended = totalCandidateInviteeAttended;
+	}
+	public Long getTotalCandidateAbsent() {
+		return totalCandidateAbsent;
+	}
+	public void setTotalCandidateAbsent(Long totalCandidateAbsent) {
+		this.totalCandidateAbsent = totalCandidateAbsent;
+	}
+	public Long getTotalCommitteeMemberInviteeAttended() {
+		return totalCommitteeMemberInviteeAttended;
+	}
+	public void setTotalCommitteeMemberInviteeAttended(
+			Long totalCommitteeMemberInviteeAttended) {
+		this.totalCommitteeMemberInviteeAttended = totalCommitteeMemberInviteeAttended;
+	}
+	public Long getTotalNonInviteeCandidate() {
+		return totalNonInviteeCandidate;
+	}
+	public void setTotalNonInviteeCandidate(Long totalNonInviteeCandidate) {
+		this.totalNonInviteeCandidate = totalNonInviteeCandidate;
+	}
+	public Long getTotalNonInviteeCommitteeMember() {
+		return totalNonInviteeCommitteeMember;
+	}
+	public void setTotalNonInviteeCommitteeMember(
+			Long totalNonInviteeCommitteeMember) {
+		this.totalNonInviteeCommitteeMember = totalNonInviteeCommitteeMember;
+	}
+	public Long getTotalCommitteeMemberAbsent() {
+		return totalCommitteeMemberAbsent;
+	}
+	public void setTotalCommitteeMemberAbsent(Long totalCommitteeMemberAbsent) {
+		this.totalCommitteeMemberAbsent = totalCommitteeMemberAbsent;
+	}
+	public Long getTotalCandidateNonInvitees() {
+		return totalCandidateNonInvitees;
+	}
+	public void setTotalCandidateNonInvitees(Long totalCandidateNonInvitees) {
+		this.totalCandidateNonInvitees = totalCandidateNonInvitees;
+	}
+	public Long getTotalCommitteeMemberNonInvitees() {
+		return totalCommitteeMemberNonInvitees;
+	}
+	public void setTotalCommitteeMemberNonInvitees(
+			Long totalCommitteeMemberNonInvitees) {
+		this.totalCommitteeMemberNonInvitees = totalCommitteeMemberNonInvitees;
+	}
+	
+}
