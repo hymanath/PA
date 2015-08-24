31973f857bb4969593ee8e0c41ee5184b522f724
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-24 08:01:36 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/PartyMeetingsDashBoardService.java
+++
@@ -1,7 +0,0 @@
-package com.itgrids.partyanalyst.service.impl;
-
-import com.itgrids.partyanalyst.service.IPartyMeetingsDashBoardService;
-
-public class PartyMeetingsDashBoardService implements IPartyMeetingsDashBoardService{
-
-}
