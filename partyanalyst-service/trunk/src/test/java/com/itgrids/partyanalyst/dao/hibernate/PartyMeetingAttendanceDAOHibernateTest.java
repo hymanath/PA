396fe82f60698529c75fc0ba47098cc16667a349
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-24 08:01:18 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingAttendanceDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingAttendanceDAOHibernateTest.java
@@ -15,11 +15,38 @@ public class PartyMeetingAttendanceDAOHibernateTest extends BaseDaoTestCase{
 		this.partyMeetingAttendanceDAO = partyMeetingAttendanceDAO;
 	}
 	
-	public void test(){
+	/*public void test(){
 		List<Long> partyMeetingIds = new ArrayList<Long>();
 		partyMeetingIds.add(32l);
 		List<Object[]> list = partyMeetingAttendanceDAO.getTotalAttendentsOfMeetings(partyMeetingIds);
 		List<Object[]> list1 = partyMeetingAttendanceDAO.getInviteesAttendedCountOfMeetings(partyMeetingIds);
 		System.out.println(list.size());
+	}*/
+	
+	/*public void testGetAttendanceForMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingAttendanceDAO.getAttendanceForMeetings(list);
+		System.out.println(list2.size());
+	}*/
+	
+	public void testGetCandidateAttendanceForMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingAttendanceDAO.getCandidateAttendanceForMeetings(list);
+		System.out.println(list2.size());
+	}
+	
+	public void testGetCommitteeMemberAttendanceForMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingAttendanceDAO.getCommitteeMemberAttendanceForMeetings(list);
+		System.out.println(list2.size());
 	}
 }
--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingInviteeDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingInviteeDAOHibernateTest.java
@@ -16,12 +16,7 @@ public class PartyMeetingInviteeDAOHibernateTest extends BaseDaoTestCase{
 		this.partyMeetingInviteeDAO = partyMeetingInviteeDAO;
 	}
 	
-	/*public void test()
-	{
-		partyMeetingInviteeDAO.getAll();
-	}*/
-	
-	public void test(){
+	/*public void test(){
 		List<Long> partyMeetingIds = new ArrayList<Long>();
 		partyMeetingIds.add(1l);
 		partyMeetingIds.add(3l);
@@ -29,5 +24,45 @@ public class PartyMeetingInviteeDAOHibernateTest extends BaseDaoTestCase{
 		List<Object[]> list = partyMeetingInviteeDAO.getPartyMeetingInviteesForMeetings(partyMeetingIds);
 		
 		System.out.println(list.size());
+	}*/
+	
+	/*public void testGetPartyMeetingInvittees()
+	{
+		List<String> list = partyMeetingInviteeDAO.getPartyMeetingInvittees(1l);
+		System.out.println(list.size());
+		for(String str : list)
+			System.out.println(str);
+	}*/
+	
+	/*public void testGetInviteesForPartyMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingInviteeDAO.getInviteesForPartyMeetings(list);
+		System.out.println(list2.size());
+		for(Object[] params : list2)
+			System.out.println(params[0].toString()+"\t"+params[1]);
+	}*/
+	
+	/*public void testGetPublicRepresentativeInviteesForPartyMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingInviteeDAO.getPublicRepresentativeInviteesForPartyMeetings(list);
+		System.out.println(list2.size());
+		for(Object[] params : list2)
+			System.out.println(params[0].toString()+"\t"+params[1]);
+	}*/
+	public void testGetPublicRepresentativeInviteesForPartyMeetings()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(1l);
+		list.add(2l);
+		List<Object[]> list2 = partyMeetingInviteeDAO.getCommitteeMemberInviteesForPartyMeetings(list);
+		System.out.println(list2.size());
+		for(Object[] params : list2)
+			System.out.println(params[0].toString()+"\t"+params[1]);
 	}
 }
