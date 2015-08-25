1e7425690baea8ab5e4b0fc76a3b667ecd13d5bb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-25 13:52:41 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeDAO.java
@@ -30,7 +30,7 @@ public class TrainingCampScheduleInviteeDAO extends GenericDaoHibernate<Training
 				"TCSIC.trainingCampScheduleInvitee.trainingCampSchedule.trainingCampScheduleId ");
 		
 		queryStr.append(" from TrainingCampScheduleInviteeCaller TCSIC,TrainingCampBatch TCB where TCSIC.trainingCampScheduleInvitee.trainingCampSchedule.trainingCampId = TCB.trainingCampBatchId  and " +
-				" TCSIC.trainingCampScheduleInvitee.scheduleInviteeStatus.scheduleInviteeStatusId not in (1,10) " ); // except invitee status
+				" TCSIC.trainingCampScheduleInvitee.scheduleInviteeStatus.scheduleInviteeStatusId not in (1) " ); // except invitee status
 		
 		if(startDate != null && endDate != null)
 		{
