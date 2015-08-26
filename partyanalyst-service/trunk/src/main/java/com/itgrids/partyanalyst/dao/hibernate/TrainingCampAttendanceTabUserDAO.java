11e907236a29b66a76622ed3709e2de6d9f89550
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-26 17:38:55 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampAttendanceTabUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampAttendanceTabUserDAO.java
@@ -19,7 +19,19 @@ public class TrainingCampAttendanceTabUserDAO extends GenericDaoHibernate<Traini
 	@SuppressWarnings("unchecked")
 	public List<TrainingCampSchedule> getTrainingCampSchedulesOfAttendanceTabUser(Long attendanceTabUserId)
 	{
-		Query query = getSession().createQuery("SELECT model.trainingCampSchedule FROM TrainingCampAttendanceTabUser model where model.attendanceTabUser.attendanceTabUserId = :attendanceTabUserId and model.attendanceTabUser.isEnabled = 'Y'");
+		Query query = getSession().createQuery("SELECT model.trainingCampSchedule FROM TrainingCampAttendanceTabUser model where model.attendanceTabUser.attendanceTabUserId = :attendanceTabUserId and model.attendanceTabUser.isEnabled = 'Y'" +
+				" group by model.trainingCampSchedule.trainingCampScheduleId");
+		query.setParameter("attendanceTabUserId",attendanceTabUserId);
+		return query.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getTrainingCampBatchesOfScheduleForTabUser(Long trainingCampScheduleId,Long attendanceTabUserId)
+	{
+		Query query = getSession().createQuery("SELECT model.trainingCampBatch.trainingCampBatchId,model.trainingCampBatch.trainingCampBatchName,model.trainingCampBatch.trainingCampBatchCode," +
+				" model.trainingCampBatch.fromDate,model.trainingCampBatch.toDate from TrainingCampAttendanceTabUser model " +
+				" where model.trainingCampSchedule.trainingCampScheduleId = :trainingCampScheduleId and model.attendanceTabUser.attendanceTabUserId = :attendanceTabUserId");
+		query.setParameter("trainingCampScheduleId",trainingCampScheduleId);
 		query.setParameter("attendanceTabUserId",attendanceTabUserId);
 		return query.list();
 	}
