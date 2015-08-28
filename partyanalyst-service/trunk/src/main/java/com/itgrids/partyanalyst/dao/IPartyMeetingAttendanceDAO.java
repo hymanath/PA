d4f6707d626081a7fc6e5906293a397d3c5226f3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-08-28 15:07:41 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingAttendanceDAO.java
@@ -9,8 +9,8 @@ import com.itgrids.partyanalyst.model.PartyMeetingAttendance;
 
 public interface IPartyMeetingAttendanceDAO extends GenericDao<PartyMeetingAttendance,Long>{
 	public List<Object[]> getPartyMeetingsAttendenceDetailsByCadreId(List<Long> tdpCadreIdsList,Date toDayDate);
-	public List<Object[]> getTotalAttendedDetailsForCadreIds(List<Long> tdpCadreIdsList,Long partyMeetingTypeId);
-	public List<Object[]> getAbsentMemberDeails(Long partyMeetingTypeId);
+	public List<Object[]> getTotalAttendedDetailsForCadreIds(List<Long> tdpCadreIdsList,Long partyMeetingTypeId,Date todayDate);
+	public List<Object[]> getAbsentMemberDeails(List<Long> tdpCadreIdsList ,Long partyMeetingTypeId);
 	public List<Object[]> getAttendenceForCadre(Long tdpCadreId,Date todayDate);
 	
 	public List<Object[]> getTotalAttendentsOfMeetings(List<Long> partyMeetingIds);
