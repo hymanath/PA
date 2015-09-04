42c63cbccda2109a0a7ee706f8cd66864f4f0b6b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-09-04 10:00:28 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampScheduleInviteeTrackDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampScheduleInviteeTrackDAO.java
@@ -1,11 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.TraingCampDataVO;
 import com.itgrids.partyanalyst.model.TrainingCampScheduleInviteeTrack;
 
 public interface ITrainingCampScheduleInviteeTrackDAO extends GenericDao<TrainingCampScheduleInviteeTrack, Long>{
 	public List<Object[]> getScheduleConfirmationDetails(Long purposeId,Long userId);
+	public List<Object[]> getMemberRemarks(TraingCampDataVO inputVo,List<Long> statusIds,String statusType,String status,Date toDayDate);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeTrackDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampScheduleInviteeTrackDAO.java
@@ -1,11 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITrainingCampScheduleInviteeTrackDAO;
+import com.itgrids.partyanalyst.dto.TraingCampDataVO;
 import com.itgrids.partyanalyst.model.TrainingCampScheduleInviteeTrack;
 
 public class TrainingCampScheduleInviteeTrackDAO extends GenericDaoHibernate<TrainingCampScheduleInviteeTrack, Long> implements ITrainingCampScheduleInviteeTrackDAO{
@@ -36,6 +38,50 @@ public class TrainingCampScheduleInviteeTrackDAO extends GenericDaoHibernate<Tra
 
 		query.setParameter("purposeId", purposeId);
 		query.setParameter("userId", userId);
+		
+		return query.list();
+	}
+	
+	
+	public List<Object[]> getMemberRemarks(TraingCampDataVO inputVo,List<Long> statusIds,String statusType,String status,Date toDayDate)
+	{
+		StringBuilder str = new StringBuilder();
+		
+		str.append("select model.trainingCampScheduleInvitee.tdpCadre.tdpCadreId," +
+				" model.remarks,model.insertedTime" +
+				" from TrainingCampScheduleInviteeTrack model left join model.campCallStatus campCallStatus " +
+				" where model.trainingCampCallerId = :callerId " +
+				" and model.trainingCampScheduleInvitee.trainingCampSchedule.trainingCampProgram.trainingCampProgramId = :programId" +
+				" and model.trainingCampScheduleInvitee.trainingCampSchedule.trainingCamp.trainingCampId =:campId" +
+				" and model.trainingCampScheduleInvitee.trainingCampSchedule.trainingCampScheduleId =:scheduleId");
+				//" and model.trainingCampScheduleInvitee.trainingCampBatch.batchStatus.batchStatusId in(1,2) ");
+		if(status.equalsIgnoreCase("undialed"))
+			str.append(" and campCallStatus.campCallStatusId is null");
+		if((statusIds != null && statusIds.size() > 0) && statusType.equalsIgnoreCase("callStatus"))
+					str.append(" and campCallStatus.campCallStatusId in(:statusIds)");
+		if((statusIds != null && statusIds.size() > 0) && statusType.equalsIgnoreCase("scheduleCallStatus"))
+			str.append(" and model.trainingCampScheduleInvitee.scheduleInviteeStatus.scheduleInviteeStatusId in(:statusIds)");
+		if(inputVo.getBatchId() > 0)
+			str.append(" and model.trainingCampScheduleInvitee.trainingCampBatch.trainingCampBatchId = :batchId " );
+		str.append(" and model.campCallPurposeId = :callPurposeId");
+		
+		if(toDayDate != null)
+		 str.append(" and date(model.trainingCampScheduleInvitee.callBackTime) =:toDayDate ");
+		
+		str.append(" order by model.trainingCampScheduleInviteeTrackId desc");
+		Query query = getSession().createQuery(str.toString());
+		query.setParameter("callerId", inputVo.getUserId());
+		query.setParameter("callPurposeId", inputVo.getPurposeId());
+		query.setParameter("programId", inputVo.getProgramId());
+		query.setParameter("campId", inputVo.getCampId());
+		query.setParameter("scheduleId", inputVo.getScheduleId());
+		if(statusIds != null && statusIds.size() > 0)
+		query.setParameterList("statusIds", statusIds);
+		if(inputVo.getBatchId() > 0)
+		query.setParameter("batchId", inputVo.getBatchId());	
+		
+		if(toDayDate != null)
+		 query.setDate("toDayDate", toDayDate);
 		
 		return query.list();
 	}
