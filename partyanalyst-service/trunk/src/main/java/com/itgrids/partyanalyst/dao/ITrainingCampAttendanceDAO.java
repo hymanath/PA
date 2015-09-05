831f935f997136f8412ff1be5d5c0e4471d5400c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-09-05 11:41:35 +0000

    updated by -sreedhar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampAttendanceDAO.java
@@ -6,5 +6,10 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.TrainingCampAttendance;
 
 public interface ITrainingCampAttendanceDAO extends GenericDao<TrainingCampAttendance,Long>{
+	
+	public List<Object[]>	 getAttendedCountForBatchesByLocation(List<Long> batchIds);
+	
+	public List<Object[]> getAttendedCadreCountByBatchIds(List<Long> batchIds,String type);
+	public List<Object[]> getInvitedCadreCountByBatchIds(List<Long> batchIds,String type);
 	public List<Object[]> getCompletedCounts(List<Long> batchIds);
 }
