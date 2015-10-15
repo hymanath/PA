144c789effb8fd42b36bf795200d97f86dfe173a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-10-15 15:10:55 +0000

    updated by sandeep

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampFeedbackAnswerDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TrainingCampFeedbackAnswer;
+
+public interface ITrainingCampFeedbackAnswerDAO extends GenericDao<TrainingCampFeedbackAnswer, Long>{
+	public List<Object[]> getFeedbackDetailsForCadre(Long cadreId);
+}
