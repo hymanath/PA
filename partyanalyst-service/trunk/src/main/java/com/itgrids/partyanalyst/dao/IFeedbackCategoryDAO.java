b7f31266c0776f3c54713be499f4734e8fabd9b2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-10-15 15:10:44 +0000

    feedback answers for cadre page -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFeedbackCategoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFeedbackCategoryDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.FeedbackCategory;
 
 public interface IFeedbackCategoryDAO extends GenericDao<FeedbackCategory, Long>{
-
+	public List<Object[]> getAllNamesOfIds(List<Long> ids);
 }
