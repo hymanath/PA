d0391b18397ce39a09d3ff9dff2764b4a66e41c5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-10-15 15:11:21 +0000

    feedback answers for cadre page -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FeedbackCategoryDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/FeedbackCategoryDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IFeedbackCategoryDAO;
 import com.itgrids.partyanalyst.model.FeedbackCategory;
@@ -11,5 +14,11 @@ public class FeedbackCategoryDAO extends GenericDaoHibernate<FeedbackCategory, L
 		super(FeedbackCategory.class);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public List<Object[]> getAllNamesOfIds(List<Long> ids){
+		Query query = getSession().createQuery(" select model.feedbackCategoryId,model.categoryName " +
+				" from FeedbackCategory model where model.feedbackCategoryId in (:ids)");
+		query.setParameterList("ids", ids);
+		return query.list();
+	}
 }
