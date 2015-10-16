27f0dd4c089acac9890068b8dfda6fe0a0ca7deb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-10-16 14:24:48 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampAttendanceDAO.java
@@ -14,7 +14,7 @@ public interface ITrainingCampAttendanceDAO extends GenericDao<TrainingCampAtten
 	public List<Object[]> getInvitedCadreCountByBatchIds(List<Long> batchIds,String type);
 	public List<Object[]> getCompletedCounts(List<Long> batchIds);
 	
-	public List<Object[]> getAttendedlocWiseCountsByProgramOrCampOrBatch(String queryString,Long programId,Long campId,Long batchId,Date fromDate,Date toDate,Date currDate);
+	public List<Object[]> getAttendedlocWiseCountsByProgramOrCampOrBatch(String queryString,Long programId,Long campId,Long batchId,Date fromDate,Date toDate,Date currDate,String callFrom);
 	public Long getAttendedCountByBatch(Long batchId,Date fromDate,Date todate);
 	public List<Object[]> getDateWiseCountsByBatch(Long batchId,Date fromDate,Date toDate);
 	public List<Object[]> getCampWiseAttendedCountByProgram(Long programId,Date fromDate,Date toDate);
@@ -26,8 +26,8 @@ public interface ITrainingCampAttendanceDAO extends GenericDao<TrainingCampAtten
 	public List<Long> getCompletedCountsForADay(Long batchId,Date date);
 	public List<Object[]> getCompletedCountsForABatch(Long batchId,List<Date> dates);
 	public List<Object[]> getCompletedCountDetails(List<Long> batchIds);
-	public List<Object[]> getInviteeAttendedCounts(List<Long> batchIds,Date fromDate,Date toDate);
-	public List<Object[]> getInviteeAttendedDetails(List<Long> batchIds,Date fromDate,Date toDate);
+	//public List<Object[]> getInviteeAttendedCounts(List<Long> batchIds,Date fromDate,Date toDate);
+	//public List<Object[]> getInviteeAttendedDetails(List<Long> batchIds,Date fromDate,Date toDate);
 	public List<Object[]> getInviteeAttendedCountsForCenter(Long centerId,Long programId,Date fromDate,Date toDate);
 	public List<Long> getAttendedDetailsForCenter(Long centerId,Long programId,Date fromDate,Date toDate);
 }
