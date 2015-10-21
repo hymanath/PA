a71821bd5fe414f62ca551d664cb09aa5bf5e959
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-10-21 06:53:40 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CategoryFeedbackVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CategoryFeedbackVO.java
@@ -10,14 +10,24 @@ public class CategoryFeedbackVO {
 	private String mainCategoryName;
 	private Long subCategoryId;
 	private String subCategoryName;
+	private Long id;
+	private String name;
 	private List<CategoryFeedbackVO> categoryFeedBackList = new ArrayList<CategoryFeedbackVO>(0);
 	private List<String> description = new ArrayList<String>(0);
 	
 	
-	
-	
-	
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public List<String> getDescription() {
 		return description;
 	}
