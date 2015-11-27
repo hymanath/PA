661ed67b66c406cf828b1c9092bcdfb15c172d40
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-11-27 13:35:57 +0000

    Updated by Kamalakar Dandu

--- RMQ/src/com/itgrids/utils/IConstants.java
+++ RMQ/src/com/itgrids/utils/IConstants.java
@@ -2,9 +2,15 @@ package com.itgrids.utils;
 
 public interface IConstants {
 
-	public static final String RMQ_USERNAME = "itgrids";
+	/*public static final String RMQ_USERNAME = "itgrids";
 	public static final String RMQ_PASSWORD = "1tGrids456";
 	public static final String VIRTUALHOST = "/";
 	public static final String HOST = "queue-1595090053.us-east-1.elb.amazonaws.com";
+	public static final Integer PORT = 5672;*/
+	
+	public static final String RMQ_USERNAME = "guest";
+	public static final String RMQ_PASSWORD = "guest";
+	public static final String VIRTUALHOST = "/";
+	public static final String HOST = "localhost";
 	public static final Integer PORT = 5672;
 }
