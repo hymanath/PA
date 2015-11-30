bedc42fa1369a804b4c0250b3e214c28762bf020
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-11-30 13:29:34 +0000

    Activity Models and DAOs -- Sravanth

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/ActivityLocationInfoDAOHibernateTest.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IActivityLocationInfoDAO;
+
+public class ActivityLocationInfoDAOHibernateTest extends BaseDaoTestCase{
+	
+	private IActivityLocationInfoDAO activityLocationInfoDAO;
+
+	public IActivityLocationInfoDAO getActivityLocationInfoDAO() {
+		return activityLocationInfoDAO;
+	}
+	public void setActivityLocationInfoDAO(
+			IActivityLocationInfoDAO activityLocationInfoDAO) {
+		this.activityLocationInfoDAO = activityLocationInfoDAO;
+	}
+
+	public void test(){
+		
+	}
+}
