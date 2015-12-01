5fe61957e3ff7487b3980f2b34026e13b336d55d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-01 13:48:11 +0000

    model for LocationInfo - balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocationInfoDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.LocationInfo;
+
+public interface ILocationInfoDAO extends GenericDao<LocationInfo, Long>{
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/LocationInfo.java
@@ -0,0 +1,103 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "location_info")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class LocationInfo extends BaseModel implements Serializable{
+
+	private Long locationInfoId;
+	private Long levelId;
+	private Long scopeId;
+	private Long scopeValue;
+	private Long count;
+	
+	private RegionScopes regionScopes;
+	private RegionScopes regionScopes2;
+	
+	//location_info_id,level_id,scope_id,scope_value,count
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "location_info_id", unique = true, nullable = false)
+	public Long getLocationInfoId() {
+		return locationInfoId;
+	}
+	public void setLocationInfoId(Long locationInfoId) {
+		this.locationInfoId = locationInfoId;
+	}
+	
+	@Column(name = "level_id")
+	public Long getLevelId() {
+		return levelId;
+	}
+	public void setLevelId(Long levelId) {
+		this.levelId = levelId;
+	}
+	
+	@Column(name = "scope_id")
+	public Long getScopeId() {
+		return scopeId;
+	}
+	public void setScopeId(Long scopeId) {
+		this.scopeId = scopeId;
+	}
+	
+	@Column(name = "scope_value")
+	public Long getScopeValue() {
+		return scopeValue;
+	}
+	public void setScopeValue(Long scopeValue) {
+		this.scopeValue = scopeValue;
+	}
+	@Column(name = "count")
+	public Long getCount() {
+		return count;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="level_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public RegionScopes getRegionScopes() {
+		return regionScopes;
+	}
+	public void setRegionScopes(RegionScopes regionScopes) {
+		this.regionScopes = regionScopes;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="scope_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public RegionScopes getRegionScopes2() {
+		return regionScopes2;
+	}
+	public void setRegionScopes2(RegionScopes regionScopes2) {
+		this.regionScopes2 = regionScopes2;
+	}
+	
+	
+	
+}
