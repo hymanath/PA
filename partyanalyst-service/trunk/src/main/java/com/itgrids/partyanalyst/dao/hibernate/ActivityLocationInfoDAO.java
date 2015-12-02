e89e68f7862511f93e03515f1b8fa3aa1dc82118
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-02 04:12:25 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityLocationInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityLocationInfoDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -16,17 +17,20 @@ public class ActivityLocationInfoDAO extends GenericDaoHibernate<ActivityLocatio
 	}
 
 	
-	public List<Object[]> getUpdatedLocationsListForScope(Long activityScopeId)
+	public List<Object[]> getUpdatedLocationsListForScope(Long activityScopeId,Date startDate,Date endDate)
 	{
 		StringBuilder queryStr = new StringBuilder();
-		queryStr.append(" select distinct model.locationValue,date(model.plannedDate),date(model.conductedDate) from ActivityLocationInfo model where " +
+		queryStr.append(" select distinct model.locationValue,date(model.plannedDate),date(model.conductedDate),model.locationLevel from ActivityLocationInfo model where " +
 				"model.activityScopeId =:activityScopeId ");
-		
+		if(startDate != null && endDate != null)
+			queryStr.append(" and (date(model.plannedDate) >= :startDate and date(model.plannedDate) <= :endDate ) ");
+
 		Query query = getSession().createQuery(queryStr.toString());
 		query.setParameter("activityScopeId", activityScopeId);
-		
+		if(startDate != null && endDate != null){
+			query.setDate("startDate", startDate);
+			query.setDate("endDate", endDate);
+		}
 		return query.list();
 	}
-	
-	
 }
