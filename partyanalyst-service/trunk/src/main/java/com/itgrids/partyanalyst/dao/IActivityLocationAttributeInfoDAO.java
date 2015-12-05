b888bf4186ab9a88ba1d49fb7588029d145ddb64
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-05 09:57:03 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityLocationAttributeInfoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.dao;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.partyanalyst.model.ActivityLocationAttributeInfo;
-
-public interface IActivityLocationAttributeInfoDAO extends GenericDao<ActivityLocationAttributeInfo, Long>{
-
-}
