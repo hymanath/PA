8e7cc28d366f25f10ad8eb43a8edd6d9e2dbadf5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-05 09:57:03 +0000

    updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityAttributeQuestionnaireInfoDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityAttributeQuestionnaireInfo;
+
+public interface IActivityAttributeQuestionnaireInfoDAO extends GenericDao<ActivityAttributeQuestionnaireInfo, Long>{
+
+}
