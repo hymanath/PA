e4c939a8c1f664b3305a7567094d489a0886f57f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-08 13:44:18 +0000

    ActivityTeamModels -- Teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityTeamLocationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityTeamLocation;
+
+public interface IActivityTeamLocationDAO extends GenericDao<ActivityTeamLocation, Long> {
+
+}
