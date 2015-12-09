423171d641666c8b41342f99f9139cede95cbfcf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-09 05:55:03 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocationInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILocationInfoDAO.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.SearchAttributeVO;
 import com.itgrids.partyanalyst.model.LocationInfo;
 
 public interface ILocationInfoDAO extends GenericDao<LocationInfo, Long>{
 	
 	public List<Object[]> getAssemblyWiseTotalCounts(List<Long> levelIds,List<Long> constIds);
+	public List<Object[]> areaCountListByAreaIdsOnScope(SearchAttributeVO searchAttributeVO);
 }
