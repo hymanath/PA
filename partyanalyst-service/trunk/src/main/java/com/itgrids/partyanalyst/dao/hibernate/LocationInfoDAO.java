42adc5743ebb97365601d7ef7e8c136aa091a6e0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-09 05:55:12 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocationInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LocationInfoDAO.java
@@ -6,6 +6,7 @@ import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ILocationInfoDAO;
+import com.itgrids.partyanalyst.dto.SearchAttributeVO;
 import com.itgrids.partyanalyst.model.LocationInfo;
 
 public class LocationInfoDAO extends GenericDaoHibernate<LocationInfo, Long> implements ILocationInfoDAO{
@@ -23,6 +24,23 @@ public class LocationInfoDAO extends GenericDaoHibernate<LocationInfo, Long> imp
 				  " group by model.scopeValue ");
 		query.setParameterList("constIds",constIds);
 		query.setParameterList("levelIds",levelIds);
+		return query.list();
+	}
+	
+	public List<Object[]> areaCountListByAreaIdsOnScope(SearchAttributeVO searchAttributeVO)
+	{
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select distinct model.levelId, model.count from LocationInfo model where model.scopeId =:scopeId and model.scopeValue =:scopeValue ");
+		if(searchAttributeVO.getLocationTypeIdsList() != null && searchAttributeVO.getLocationTypeIdsList().size()>0)
+			queryStr.append(" and model.levelId in (:requiredAreasIds) ");
+		queryStr.append(" order by model.levelId ");
+		Query query = getSession().createQuery(queryStr.toString());
+		
+		if(searchAttributeVO.getLocationTypeIdsList() != null && searchAttributeVO.getLocationTypeIdsList().size()>0)
+			query.setParameterList("requiredAreasIds", searchAttributeVO.getLocationTypeIdsList());
+		query.setParameter("scopeId", searchAttributeVO.getScopeId());
+		query.setParameter("scopeValue", searchAttributeVO.getScopeValue());
+		
 		return query.list();
 	}
 }
