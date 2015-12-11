f042d672bf574ea7ddc7f73ceb4a8c19bf0fac0e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-11 07:10:18 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityInfoDocumentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityInfoDocumentDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -16,13 +17,27 @@ public class ActivityInfoDocumentDAO extends GenericDaoHibernate<ActivityInfoDoc
 		
 	}
 	
-	public List<Object[]> getEventDocuments(EventDocumentVO inputVO)
+	public List<Object[]> getEventDocuments(EventDocumentVO inputVO,Date startDate,Date endDate)
 	{
 		StringBuilder str = new StringBuilder();
 		str.append("select model.activityDocument.documentName,model.activityDocument.path,model.day" +
 				"  from ActivityInfoDocument model where model.activityDocument.activityScopeId = :activityDocumentId");
 		if(inputVO.getDay() > 0)
 			str.append(" and model.day = :day");
+		if(inputVO.getLocationScope().equalsIgnoreCase("state"))
+		{
+			if(inputVO.getLocationValue() == 36)
+			{
+				str.append(" and model.userAddress.district.districtId <= 10 ");
+			}
+			
+			if(inputVO.getLocationValue() == 1)
+			{
+				str.append(" and model.userAddress.district.districtId >= 11 ");
+			}
+		}
+	
+			
 		if(inputVO.getLocationScope().equalsIgnoreCase("district"))
 			str.append(" and model.userAddress.district.districtId = :locationValue");
 		if(inputVO.getLocationScope().equalsIgnoreCase("constituency"))
@@ -33,7 +48,10 @@ public class ActivityInfoDocumentDAO extends GenericDaoHibernate<ActivityInfoDoc
 			str.append(" and model.userAddress.panchayat.panchayatId = :locationValue");
 		if(inputVO.getLocationScope().equalsIgnoreCase("village") && inputVO.getLocationValue().toString().substring(0, 1).equalsIgnoreCase("1"))
 			str.append(" and model.userAddress.localElectionBody.localElectionBodyId = :locationValue");
-		
+		if(startDate != null)
+		{
+			str.append(" and date(model.activityDocument.activityDate) >=:startDate and date(model.activityDocument.activityDate) <=:endDate");
+		}
 		Query query = getSession().createQuery(str.toString());
 		if(inputVO.getDay() > 0)
 			query.setParameter("day", inputVO.getDay());
@@ -42,7 +60,11 @@ public class ActivityInfoDocumentDAO extends GenericDaoHibernate<ActivityInfoDoc
 		query.setParameter("locationValue", new Long(inputVO.getLocationValue().toString().substring(1)));
 		else
 			query.setParameter("locationValue",inputVO.getLocationValue());
-		
+		if(startDate != null)
+		{
+			query.setDate("startDate", startDate);
+			query.setDate("endDate", endDate);
+		}
 		return query.list();
 	}
 
