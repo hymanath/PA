67d7d1a7c3e0aa0ce1c008006ad64d4c6c9fd8cf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-11 14:41:37 +0000

    Updated By - AnuRadha

---
+++ partyanalyst/src/main/webapp/dragAndDropPhoto/js/custom.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+    
+
+	$('#filer_input2').filer({
+    changeInput: '<div class="jFiler-input-dragDrop"><div class="jFiler-input-inner"><div class="jFiler-input-icon"><i class="icon-jfi-folder"></i></div><div class="jFiler-input-text"><h3>Click on this box</h3> <span style="display:inline-block; margin: 15px 0">or</span></div><a class="jFiler-input-choose-btn blue">Browse Files</a></div></div>',
+    showThumbs: true,
+    theme: "dragdropbox",
+    templates: {
+        box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+        item: '<li class="jFiler-item">\
+                    <div class="jFiler-item-container">\
+                        <div class="jFiler-item-inner">\
+                            <div class="jFiler-item-thumb">\
+                                <div class="jFiler-item-status"></div>\
+                                <div class="jFiler-item-info">\
+                                    <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                    <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                </div>\
+                                {{fi-image}}\
+                            </div>\
+                            <div class="jFiler-item-assets jFiler-row">\
+                                <ul class="list-inline pull-left"></ul>\
+                                <ul class="list-inline pull-right">\
+                                </ul>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </li>',
+        itemAppend: '<li class="jFiler-item">\
+                        <div class="jFiler-item-container">\
+                            <div class="jFiler-item-inner">\
+                                <div class="jFiler-item-thumb">\
+                                    <div class="jFiler-item-status"></div>\
+                                    <div class="jFiler-item-info">\
+                                        <span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name | limitTo: 25}}</b></span>\
+                                        <span class="jFiler-item-others">{{fi-size2}}</span>\
+                                    </div>\
+                                    {{fi-image}}\
+                                </div>\
+                                <div class="jFiler-item-assets jFiler-row">\
+                                    <ul class="list-inline pull-left">\
+                                        <li><span class="jFiler-item-others">{{fi-icon}}</span></li>\
+                                    </ul>\
+                                    <ul class="list-inline pull-right">\
+                                    </ul>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </li>',
+					 progressBar: '<div class="bar"></div>',
+            itemAppendToEnd: false,
+            removeConfirmation: true,
+            _selectors: {
+                list: '.jFiler-items-list',
+                item: '.jFiler-item',
+                progressBar: '.bar',
+                remove: '.jFiler-item-trash-action'
+            }
+        },
+        dragDrop: {
+            dragEnter: null,
+            dragLeave: null,
+            drop: null,
+        },
+       uploadFile: {
+		  
+            url: "eventsUploadFormAction.action",
+            data: {"activityScopeId":$("#activityLevelList").val(),"day":$("#day").val(),
+			     "levelId":$("#levelIds").val(),"stateId":$("#statesDivId").val(),
+				 "districtId":$("#districtId").val(),"constituencyId":$("#constituencyId").val(),
+				 "manOrMunId":$("#mandalList").val(),"panchayatId":$("#panchaytList").val(),
+				 "activityDate":$("#activityDate").val()},
+            type: 'POST',
+			
+           enctype: 'multipart/form-data',
+            beforeSend: function(){},
+            success: function(data, el){
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                    $("<div class=\"jFiler-item-others text-success\"><i class=\"icon-jfi-check-circle\"></i> Success</div>").hide().appendTo(parent).fadeIn("slow");    
+                });
+            },
+            error: function(el){
+                var parent = el.find(".jFiler-jProgressBar").parent();
+                el.find(".jFiler-jProgressBar").fadeOut("slow", function(){
+                    $("<div class=\"jFiler-item-others text-error\"><i class=\"icon-jfi-minus-circle\"></i> Error</div>").hide().appendTo(parent).fadeIn("slow");    
+                });
+            },
+            statusCode: null,
+            onProgress: null,
+            onComplete: null
+        },
+		
+		
+		files: null,
+        addMore: false,
+        clipBoardPaste: true,
+        excludeName: null,
+        beforeRender: null,
+        afterRender: null,
+        beforeShow: null,
+        beforeSelect: null,
+        onSelect: null,
+        afterShow: null,
+        onRemove: function(itemEl, file, id, listEl, boxEl, newInputEl, inputEl){
+            var file = file.name;
+            $.post('./php/remove_file.php', {file: file});
+        },
+        onEmpty: null,
+        options: null,
+        captions: {
+            button: "Choose Files",
+            feedback: "Choose files To Upload",
+            feedback2: "files were chosen",
+            drop: "Drop file here to Upload",
+            removeConfirmation: "Are you sure you want to remove this file?",
+            errors: {
+                filesLimit: "Only {{fi-limit}} files are allowed to be uploaded.",
+                filesType: "Only Images are allowed to be uploaded.",
+                filesSize: "{{fi-name}} is too large! Please upload file up to {{fi-maxSize}} MB.",
+                filesSizeAll: "Files you've choosed are too large! Please upload files up to {{fi-maxSize}} MB."
+            }
+        }
+		//progressBar: '<div class="bar"></div>',
+        //itemAppendToEnd: true,
+        //removeConfirmation: true,
+		//statusCode: null,
+        //onProgress: null,
+        /*_selectors: {
+            list: '.jFiler-items-list',
+            item: '.jFiler-item',
+            remove: '.jFiler-item-trash-action'
+        }*/
+    
+	
+});
+    
+});
