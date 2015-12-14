8873ac98c3db4781e175e9b1809828a0f2b3d375
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-14 15:07:39 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/CasteContainConstituency.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/CasteContainConstituency.java
@@ -1,7 +1,6 @@
 package com.itgrids.partyanalyst.model;
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
