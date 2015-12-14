7c7d26d47f2409bc087a4f96cea35043c9105b55
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-14 15:38:10 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityService.java
@@ -3,10 +3,11 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 import java.util.Map;
 
+import com.itgrids.partyanalyst.dto.ActivityDocumentVO;
 import com.itgrids.partyanalyst.dto.ActivityVO;
 import com.itgrids.partyanalyst.dto.BasicVO;
-import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.EventFileUploadVO;
+import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SearchAttributeVO;
 
@@ -17,11 +18,12 @@ public interface IActivityService {
 	public ActivityVO getActivityDetailsBySearchCriteria(SearchAttributeVO searchAttributeVO);
 	public List<IdNameVO> getActivityLevelsList();
 	public BasicVO getActivityTypeList();
-	
+
 	public ResultStatus eventsUploadForm(EventFileUploadVO eventFileUploadVO);
 	
 	public ResultStatus deleteEventUploadFilebyActivityInfoDocId(Long acitivityInfoDocId);
 	public ResultStatus saveActivityQuestionnaireDetails(final ActivityVO finalvo);
 	public ActivityVO getQuestionnaireForScope(Long scopeId);
 
+	public ResultStatus uploadActivityDocuments(Long activityId,String sourcePath,ActivityDocumentVO activityLocation);
 }
