05a6612165bc38f0a6934c470fa6effadc738d7a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-14 15:08:10 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/CommentData.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/CommentData.java
@@ -23,8 +23,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
