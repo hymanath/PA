a135e723a828dbc2f3ed76ac2dfca801c6a49cbb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-16 09:18:25 +0000

    Updated By - AnuRadha

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TrainingCampFeedBackVO.java
@@ -0,0 +1,32 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class TrainingCampFeedBackVO {
+	
+	private String name;
+	private Long id;
+	private List<TrainingCampFeedBackVO> subList;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<TrainingCampFeedBackVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<TrainingCampFeedBackVO> subList) {
+		this.subList = subList;
+	}
+	
+	
+}
