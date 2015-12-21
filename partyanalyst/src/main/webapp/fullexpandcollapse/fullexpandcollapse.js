d092aa43f2b6fc5ffbd9216966db7d7a7b20bd9d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2015-12-21 13:30:51 +0000

    [itg12212015]:Menu Integration For CadreTraining Module By Hyma

--- partyanalyst/src/main/webapp/fullexpandcollapse/fullexpandcollapse.js
+++ partyanalyst/src/main/webapp/fullexpandcollapse/fullexpandcollapse.js
@@ -6,26 +6,12 @@ $(document).ready(function(){
 		fullCollapse: true,
 		 collapsed: true,
 		mode: 'overlap',
-		menuWidth: '24%',
+		menuWidth: '30%',
 	    menuHeight: '100%',
 		// Just for fun also changing the look of the menu
 		wrapperClass: 'mlpm_w',
 		menuInactiveClass: 'mlpm_inactive',
 		onItemClick: function() {
-			// First argument is original event object
-			var event = arguments[0],
-				// Second argument is menu level object containing clicked item (<div> element)
-				$menuLevelHolder = arguments[1],
-				// Third argument is clicked item (<li> element)
-				$item = arguments[2],
-				// Fourth argument is instance settings/options object
-				options = arguments[3];
-
-			// You can do some cool stuff here before
-			// redirecting to href location
-			// like logging the event or even
-			// adding some parameters to href, etc...
-
 			// Anchor href
 			var itemHref = $item.find( 'a:first' ).attr( 'href' );
 			// Redirecting the page
@@ -52,12 +38,12 @@ $(document).ready(function(){
 	
 	$(document).on('click', '.levelHolderClass', function(){  
 
-    if($(this).find("i").hasClass("floatLeft"))
-    {
-      $(this).find("i").trigger('click');
-    }
-    
-  });
+		if($(this).find("i").hasClass("floatLeft"))
+		{
+		  $(this).find("i").trigger('click');
+		}
+		
+	  });
 
 	
 
