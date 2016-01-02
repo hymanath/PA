0e36bc75aa39e90c56ba5b867768c53ce2d3d5ef
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-02 12:40:52 +0000

    Updated By - AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityService.java
@@ -10,6 +10,7 @@ import com.itgrids.partyanalyst.dto.EventFileUploadVO;
 import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.SearchAttributeVO;
+import com.itgrids.partyanalyst.model.UserAddress;
 
 public interface IActivityService {
 	
@@ -21,11 +22,17 @@ public interface IActivityService {
 
 	public ResultStatus eventsUploadForm(EventFileUploadVO eventFileUploadVO);
 	
-	public ResultStatus deleteEventUploadFilebyActivityInfoDocId(Long acitivityInfoDocId);
+	public ResultStatus deleteEventUploadFilebyActivityInfoDocId(String acitivityInfoDocId);
 	public ResultStatus saveActivityQuestionnaireDetails(final ActivityVO finalvo);
 	public ActivityVO getQuestionnaireForScope(Long scopeId);
 
 	public ResultStatus uploadActivityDocuments(Long activityId,String sourcePath,ActivityDocumentVO activityLocation);
 	public List<IdNameVO> getActivityLeadersDetailsByActivityScope(List<Long> activityScopeIds,boolean showCounts);
 	public List<IdNameVO> getTeamMembersByLeaderAndActivityScope(List<Long> teamLeaderIds,List<Long> activityScopeIds);
+	
+	public UserAddress saveUserAddressByLevelIdAndLevelValue(Long levelId,Long levelValue);
+	
+	public List<BasicVO> getActivityDocumentsImages(Long levelId,Long levelValue,Long day,Integer startIndex,Integer maxIndex,Long activityScopeId,String activityDate);
+	
+	
 }
