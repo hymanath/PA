d459a9b41137f67c8c61b4efb904584e70525af8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-02 12:39:39 +0000

    Updated By - AnuRadha

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TrainingCampFeedBackVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TrainingCampFeedBackVO.java
@@ -8,6 +8,13 @@ public class TrainingCampFeedBackVO {
 	private Long id;
 	private String constituency;
 	private String district;
+	private String question;
+	private String answer;
+	private Long constituencyId;
+	private String membershipId;
+	private String imagePath;
+	private String title;
+	private String location_field;
 	
 	private List<TrainingCampFeedBackVO> subList;
 	
@@ -30,6 +37,61 @@ public class TrainingCampFeedBackVO {
 	public void setSubList(List<TrainingCampFeedBackVO> subList) {
 		this.subList = subList;
 	}
+	public String getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(String constituency) {
+		this.constituency = constituency;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public String getMembershipId() {
+		return membershipId;
+	}
+	public void setMembershipId(String membershipId) {
+		this.membershipId = membershipId;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLocation_field() {
+		return location_field;
+	}
+	public void setLocation_field(String location_field) {
+		this.location_field = location_field;
+	}
+	
 	
 	
 }
