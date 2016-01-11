2b150a4a8560beff3bcac64c76c10d23d1fc3c9a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-11 09:06:48 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityAttendanceService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IActivityAttendanceService.java
@@ -1,5 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.ActivityAttendanceInfoVO;
+import com.itgrids.partyanalyst.dto.SearchAttributeVO;
+
 public interface IActivityAttendanceService {
+	public List<ActivityAttendanceInfoVO> getLocationWiseActivityDetails(SearchAttributeVO searchVO);
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ActivityAttendanceService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ActivityAttendanceService.java
@@ -1,7 +1,18 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.ActivityAttendanceInfoVO;
+import com.itgrids.partyanalyst.dto.SearchAttributeVO;
 import com.itgrids.partyanalyst.service.IActivityAttendanceService;
 
 public class ActivityAttendanceService implements IActivityAttendanceService {
 
+	
+	public List<ActivityAttendanceInfoVO> getLocationWiseActivityDetails(SearchAttributeVO searchVO)
+	{
+		
+		
+		return null;
+	}
 }
