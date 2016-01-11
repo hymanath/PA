8f5bfd8b0991ff4f8c331c6ca807675393a18d72
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-11 06:27:45 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/ActivityAttendanceInfoVO.java
@@ -0,0 +1,101 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class ActivityAttendanceInfoVO {
+	
+	private Long id;
+	private String name;
+	private List<ActivityAttendanceInfoVO> subList;
+	private List<ActivityAttendanceInfoVO> localBodyList;
+	
+	private Long totalLocations=0l;
+	private Long totalMembers=0l;
+	
+	private Long totalWebCadreAttendance=0l;
+	private Long totalInfoCellCadreAttendance=0l;
+	
+	private Long totalWebPublicAttendance=0l;
+	private Long totalInfoCellPublicAttendance=0l;
+	
+	private Long totalWebPhotosAttendance=0l;
+	private Long totalInfoCellPhotosAttendance=0l;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<ActivityAttendanceInfoVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<ActivityAttendanceInfoVO> subList) {
+		this.subList = subList;
+	}
+	public List<ActivityAttendanceInfoVO> getLocalBodyList() {
+		return localBodyList;
+	}
+	public void setLocalBodyList(List<ActivityAttendanceInfoVO> localBodyList) {
+		this.localBodyList = localBodyList;
+	}
+	public Long getTotalLocations() {
+		return totalLocations;
+	}
+	public void setTotalLocations(Long totalLocations) {
+		this.totalLocations = totalLocations;
+	}
+	public Long getTotalMembers() {
+		return totalMembers;
+	}
+	public void setTotalMembers(Long totalMembers) {
+		this.totalMembers = totalMembers;
+	}
+	public Long getTotalWebCadreAttendance() {
+		return totalWebCadreAttendance;
+	}
+	public void setTotalWebCadreAttendance(Long totalWebCadreAttendance) {
+		this.totalWebCadreAttendance = totalWebCadreAttendance;
+	}
+	public Long getTotalInfoCellCadreAttendance() {
+		return totalInfoCellCadreAttendance;
+	}
+	public void setTotalInfoCellCadreAttendance(Long totalInfoCellCadreAttendance) {
+		this.totalInfoCellCadreAttendance = totalInfoCellCadreAttendance;
+	}
+	public Long getTotalWebPublicAttendance() {
+		return totalWebPublicAttendance;
+	}
+	public void setTotalWebPublicAttendance(Long totalWebPublicAttendance) {
+		this.totalWebPublicAttendance = totalWebPublicAttendance;
+	}
+	public Long getTotalInfoCellPublicAttendance() {
+		return totalInfoCellPublicAttendance;
+	}
+	public void setTotalInfoCellPublicAttendance(Long totalInfoCellPublicAttendance) {
+		this.totalInfoCellPublicAttendance = totalInfoCellPublicAttendance;
+	}
+	public Long getTotalWebPhotosAttendance() {
+		return totalWebPhotosAttendance;
+	}
+	public void setTotalWebPhotosAttendance(Long totalWebPhotosAttendance) {
+		this.totalWebPhotosAttendance = totalWebPhotosAttendance;
+	}
+	public Long getTotalInfoCellPhotosAttendance() {
+		return totalInfoCellPhotosAttendance;
+	}
+	public void setTotalInfoCellPhotosAttendance(Long totalInfoCellPhotosAttendance) {
+		this.totalInfoCellPhotosAttendance = totalInfoCellPhotosAttendance;
+	}
+	
+	
+	
+	
+	
+ 
+}
