51a53db97d22b5e0404a5d6ec8f0b5cd20b804c2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-11 06:15:41 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ActivityAttendanceService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.IActivityAttendanceService;
+
+public class ActivityAttendanceService implements IActivityAttendanceService {
+
+}
