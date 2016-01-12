a64c7a387f5b6f283e0278b535163d0d745b9033
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-12 07:31:28 +0000

    update by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ActivityAttendanceService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/ActivityAttendanceService.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import com.itgrids.partyanalyst.dao.IActivityInfoDocumentDAO;
 import com.itgrids.partyanalyst.dao.IActivityLocationAttendanceDAO;
+import com.itgrids.partyanalyst.dao.IActivityScopeDAO;
 import com.itgrids.partyanalyst.dto.ActivityAttendanceInfoVO;
 import com.itgrids.partyanalyst.dto.SearchAttributeVO;
+import com.itgrids.partyanalyst.model.ActivityScope;
 import com.itgrids.partyanalyst.service.IActivityAttendanceService;
 import com.itgrids.partyanalyst.utils.IConstants;
 
@@ -14,6 +16,17 @@ public class ActivityAttendanceService implements IActivityAttendanceService {
 
 	private IActivityInfoDocumentDAO activityInfoDocumentDAO;
 	private IActivityLocationAttendanceDAO activityLocationAttendanceDAO;
+	private IActivityScopeDAO activityScopeDAO;
+	
+	
+	public IActivityScopeDAO getActivityScopeDAO() {
+		return activityScopeDAO;
+	}
+
+	public void setActivityScopeDAO(IActivityScopeDAO activityScopeDAO) {
+		this.activityScopeDAO = activityScopeDAO;
+	}
+
 	public IActivityInfoDocumentDAO getActivityInfoDocumentDAO() {
 		return activityInfoDocumentDAO;
 	}
@@ -37,13 +50,44 @@ public class ActivityAttendanceService implements IActivityAttendanceService {
 		ActivityAttendanceInfoVO returnVO = new ActivityAttendanceInfoVO();
 		List<ActivityAttendanceInfoVO> returnList = new ArrayList<ActivityAttendanceInfoVO>();
 		returnVO.setSubList(returnList);
+		List<Long> levelIds = new ArrayList<Long>();
+		Long activityLevelId =null;
+		ActivityScope activityScope = activityScopeDAO.get(searchVO.getAttributesIdsList().get(0));
+		searchVO.setScopeId(activityScope.getScopeId());
+		searchVO.setScopeValue(activityScope.getScopeValue());
+		activityLevelId = activityScope.getActivityLevel().getActivityLevelId();
+		
+		if(activityLevelId != null && activityLevelId.longValue()>0L)
+		{
+			if(activityLevelId.longValue() == 1L)
+			{
+				searchVO.getLocationTypeIdsList().add(6L);
+				searchVO.getLocationTypeIdsList().add(8L);
+			}
+			else if(activityLevelId.longValue() == 2L)
+			{
+				searchVO.getLocationTypeIdsList().add(5L);
+				searchVO.getLocationTypeIdsList().add(7L);
+				searchVO.getLocationTypeIdsList().add(9L);
+			}
+			else if(activityLevelId.longValue() == 3L)
+			{
+				searchVO.getLocationTypeIdsList().add(10L);
+			}
+			else if(activityLevelId.longValue() == 4L)
+			{
+				searchVO.getLocationTypeIdsList().add(11L);
+			}
+		}
+		
+		
 		getCadreAttendanceCount(searchVO,returnVO);
 		getPublicAttendanceCount(searchVO,returnVO);
 		getPhotosCount(searchVO,returnVO);
 		return returnVO;
 	}
 	
-	public ActivityAttendanceInfoVO getCadreAttendanceCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
+	public void getCadreAttendanceCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
 	{
 		try{
 			 List<Object[]> list = null;
@@ -121,12 +165,12 @@ public class ActivityAttendanceService implements IActivityAttendanceService {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		return returnVO;
+		
 		
 	}
 	
 	
-	public ActivityAttendanceInfoVO getPublicAttendanceCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
+	public void getPublicAttendanceCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
 	{
 		try{
 			 List<Object[]> list = null;
@@ -204,11 +248,11 @@ public class ActivityAttendanceService implements IActivityAttendanceService {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		return returnVO;
+	
 		
 	}
 	
-	public ActivityAttendanceInfoVO getPhotosCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
+	public void getPhotosCount(SearchAttributeVO searchVO,ActivityAttendanceInfoVO returnVO)
 	{
 		try{
 			 List<Object[]> list = null;
@@ -273,7 +317,7 @@ public class ActivityAttendanceService implements IActivityAttendanceService {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		return returnVO;
+	
 		
 	}
 	
