6ee5c05ad28daf9db1dc13e9f29f0dd9a85de8a1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 08:01:53 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGreaterMuncipalWardDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.GreaterMuncipalWard;
+
+public interface IGreaterMuncipalWardDAO extends GenericDao<GreaterMuncipalWard,Long>{
+	public List<Object[]> getWardsByWardsIdsLsit(Long publicationDateId,List<Long> getWardsByWardsIdsLsit);
+}
