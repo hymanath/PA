7f15fed2b7701761742438df4198fa1f434f56a2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 22:14:17 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IUserLocationTrackingDAO.java
@@ -0,0 +1,20 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.UserLocationTracking;
+
+public interface IUserLocationTrackingDAO extends GenericDao<UserLocationTracking,Long>{
+	
+	public List<UserLocationTracking> getUserLocationByUserIdAndUniqueId(
+			Long userId, String uniqueId);
+	
+	 public List<UserLocationTracking> getUserTrackingDetails(Long userId,Date surveyDate);
+	 
+	 public List<Object[]> getLatestUserTrackingDetals(Date date);
+
+
+}
