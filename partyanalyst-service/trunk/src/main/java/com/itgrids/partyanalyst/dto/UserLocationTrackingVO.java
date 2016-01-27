e23073852245b8bf61a3b488c47d56d23f646944
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 22:15:00 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UserLocationTrackingVO.java
@@ -0,0 +1,59 @@
+package com.itgrids.partyanalyst.dto;
+
+public class UserLocationTrackingVO {
+	
+	
+	private Long userId;
+	private String longitude;
+	private String latitude;
+	private String uniqueId;
+	private String date;
+	private String time;
+	private String userName;
+	
+
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getUniqueId() {
+		return uniqueId;
+	}
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
