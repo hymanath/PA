4e49f9709d76f154f953d659f81048d533f0c317
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 15:51:09 +0000

    Updated by Kamalakar Dandu

--- VoterData/src/main/java/com/itgrids/voterdata/service/sqlite/GhmcVoterSlipSqliteGenerator.java
+++ VoterData/src/main/java/com/itgrids/voterdata/service/sqlite/GhmcVoterSlipSqliteGenerator.java
@@ -23,6 +23,7 @@ public class GhmcVoterSlipSqliteGenerator {
 		
 		GhmcVoterSlipSqliteGenerator generator = new GhmcVoterSlipSqliteGenerator();
 		generator.genearateSqlite("E:/KP/ghmcVoterMain.sqlite","E:/KP/Sqlites",31917,114);
+		generator.genearateSqlite("E:/KP/ghmcVoterMain.sqlite","E:/KP/Sqlites",31926,123);
 	}
 	
 	public boolean genearateSqlite(String basePath,String targetFolder,int wardId,int wardNo)
@@ -41,20 +42,37 @@ public class GhmcVoterSlipSqliteGenerator {
 			File targetFile = new File(targetFolder+"/"+wardNo+".sqlite");
 			FileUtils.copyFile(new File(basePath), targetFile);
 			
-			rs = stmt.executeQuery("SELECT V.voter_id,V.house_no,V.name,V.relationship_type,V.relative_name,V.gender,V.age,V.voter_id_card_no " +
-					" FROM voter V,booth_publication_voter BPV,booth B WHERE V.voter_id = BPV.voter_id AND BPV.booth_id = B.booth_id AND " +
+			rs = stmt.executeQuery("SELECT V.voter_id,V.house_no,V.name,V.relationship_type,V.relative_name,V.gender,V.age,V.voter_id_card_no,TC.tdp_cadre_id,TC.membership_id " +
+					" FROM booth_publication_voter BPV,booth B,voter V LEFT OUTER JOIN tdp_cadre TC ON V.voter_id = TC.voter_id AND TC.is_deleted = 'N' AND TC.enrollment_year = 2014 " +
+					" WHERE V.voter_id = BPV.voter_id AND BPV.booth_id = B.booth_id AND " +
 					" B.ward_id = "+wardId+"  AND B.publication_date_id = 17 GROUP BY V.voter_id");
 			
 			connection = DriverManager.getConnection("jdbc:sqlite:"+targetFile.getAbsolutePath());
 			connection.setAutoCommit(false);
 			statement = connection.createStatement();
-			
+			int count = 0;
 			while(rs.next())
 			{
 				try{
-					statement.executeUpdate("INSERT INTO voter(voter_id,house_no,name,relationship_type,relative_name,gender,age,voter_id_card_no,is_sms_sent) VALUES (" +
+					Integer tdpCadreId = rs.getInt(9);
+					String isCadre = "N";
+					String tdpCadreStr = "NULL";
+					String membershipstr = "NULL";
+					
+					if(tdpCadreId != null && tdpCadreId > 0)
+					{
+						isCadre = "Y";
+						tdpCadreStr = tdpCadreId.toString();
+						membershipstr = "'"+rs.getString(10)+"'";
+					}
+						
+					statement.executeUpdate("INSERT INTO voter(voter_id,house_no,name,relationship_type,relative_name,gender,age,voter_id_card_no,is_sms_sent,tdp_cadre_id,membership_id,is_cadre) VALUES (" +
 							"'"+rs.getInt(1)+"','"+rs.getString(2)+"','"+rs.getString(3)+"','"+rs.getString(4)+"','"+rs.getString(5)+"','"+rs.getString(6)+"'," +
-									"'"+rs.getInt(7)+"','"+rs.getString(8)+"','N')");
+									"'"+rs.getInt(7)+"','"+rs.getString(8)+"','N',"+tdpCadreStr+","+membershipstr+",'"+isCadre+"')");
+					count++;
+					
+					if(count%1000 == 0)
+						System.out.println(count+" --> Voters Insetred in Ward -- "+wardNo);
 				}catch(Exception e)
 				{
 					e.printStackTrace();
