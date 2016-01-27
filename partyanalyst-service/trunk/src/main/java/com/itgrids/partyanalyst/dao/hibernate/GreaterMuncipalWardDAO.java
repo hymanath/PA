be9d002e2037b2fe4b18928aa3ea00520f22644d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 08:02:06 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GreaterMuncipalWardDAO.java
@@ -0,0 +1,28 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IGreaterMuncipalWardDAO;
+import com.itgrids.partyanalyst.model.GreaterMuncipalWard;
+
+public class GreaterMuncipalWardDAO extends GenericDaoHibernate<GreaterMuncipalWard,Long > implements IGreaterMuncipalWardDAO{
+
+	public GreaterMuncipalWardDAO() {
+		super(GreaterMuncipalWard.class);
+	}
+
+public List<Object[]> getWardsByWardsIdsLsit(Long publicationDateId,List<Long> getWardsByWardsIdsLsit){
+		
+		
+		Query query = getSession().createQuery(" select distinct model.ward.constituencyId,model.divisionName from GreaterMuncipalWard model, Booth model2 " +
+				" where model.ward.constituencyId = model2.localBodyWard.constituencyId and model2.publicationDate.publicationDateId = :publicationDateId and " +
+				" model.ward.constituencyId in (:getWardsByWardsIdsLsit)  " +
+				" order by model.ward.constituencyId");			
+			query.setParameterList("getWardsByWardsIdsLsit", getWardsByWardsIdsLsit);
+			query.setParameter("publicationDateId", publicationDateId);			
+			return query.list();
+		}
+}
