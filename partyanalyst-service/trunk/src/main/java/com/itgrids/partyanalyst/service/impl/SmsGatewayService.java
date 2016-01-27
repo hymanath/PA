fe44320a30d05f7e158172c85d2ac70601b71f1b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-01-27 23:35:33 +0000

    Updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/SmsGatewayService.java
@@ -0,0 +1,54 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ISmsGatewayService;
+
+public class SmsGatewayService implements ISmsGatewayService{
+
+	private static final Logger LOG = Logger.getLogger(SmsGatewayService.class);
+	
+	public String sendSMS(String mobileNo,String message,String username,String password)
+	{
+
+		HttpClient client = new HttpClient(new MultiThreadedHttpConnectionManager());
+		client.getHttpConnectionManager().getParams().setConnectionTimeout(
+			Integer.parseInt("30000"));
+	
+		boolean isEnglish = true;
+		
+		PostMethod post = new PostMethod("http://smscountry.com/SMSCwebservice_Bulk.aspx");
+		
+		post.addParameter("User",username);
+		post.addParameter("passwd",password);
+		//post.addParameter("sid",IConstants.ADMIN_SENDERID_FOR_SMS);
+	    post.addParameter("mobilenumber", mobileNo);
+		post.addParameter("message", message);
+		post.addParameter("mtype", isEnglish ? "N" : "OL");
+		post.addParameter("DR", "Y");
+		
+		/* PUSH the URL */
+		try 
+		{
+			int statusCode = client.executeMethod(post);
+			
+			if (statusCode != HttpStatus.SC_OK) {
+				LOG.error("SmsCountrySmsService.sendSMS failed: "+ post.getStatusLine());
+				return "error";
+			}
+			else{
+				return "success";
+			}
+
+		}catch (Exception e) {
+				LOG.error("Exception rised in sending sms while cadre registration",e);
+				return "exception";
+		} finally {
+				post.releaseConnection();
+		}
+	}
+}
