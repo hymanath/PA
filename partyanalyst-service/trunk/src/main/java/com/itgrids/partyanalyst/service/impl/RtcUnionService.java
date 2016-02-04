cda15b88067d6190da10928d8bec1114a8683bfa
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-04 07:20:30 +0000

    Rtc Union Registration - Balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/RtcUnionService.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IRtcUnionService;
+
+public class RtcUnionService implements IRtcUnionService{
+
+	 private static final Logger LOG = Logger.getLogger(RtcUnionService.class);
+	
+}
