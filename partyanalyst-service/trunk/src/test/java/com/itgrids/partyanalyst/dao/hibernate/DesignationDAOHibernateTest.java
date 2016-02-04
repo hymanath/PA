d4c931a9139a33e165e662967495b21247f75dea
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-04 07:14:57 +0000

    RTC Models And DAOs -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/DesignationDAOHibernateTest.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IDesignationDAO;
+
+public class DesignationDAOHibernateTest extends BaseDaoTestCase{
+
+	private IDesignationDAO designationDAO;
+
+	public IDesignationDAO getDesignationDAO() {
+		return designationDAO;
+	}
+
+	public void setDesignationDAO(IDesignationDAO designationDAO) {
+		this.designationDAO = designationDAO;
+	}
+	
+	public void test(){
+		
+	}
+}
---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/TdpMemberTypeDAOHibernateTest.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ITdpMemberTypeDAO;
+
+public class TdpMemberTypeDAOHibernateTest extends BaseDaoTestCase{
+
+	private ITdpMemberTypeDAO tdpMemberTypeDAO;
+
+	public ITdpMemberTypeDAO getTdpMemberTypeDAO() {
+		return tdpMemberTypeDAO;
+	}
+
+	public void setTdpMemberTypeDAO(ITdpMemberTypeDAO tdpMemberTypeDAO) {
+		this.tdpMemberTypeDAO = tdpMemberTypeDAO;
+	}
+	
+	public void test(){
+		
+	}
+}
