628a49edc5c0517c88926c922556e7bdde696f51
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-06 10:45:25 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AssemblyLocalElectionBodyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AssemblyLocalElectionBodyDAO.java
@@ -310,7 +310,11 @@ public class AssemblyLocalElectionBodyDAO extends GenericDaoHibernate<AssemblyLo
 	}
 	public List<Object[]> getAllLocalBodiesInAConstituencyList(List<Long> constituencyIds) {
 		//0localBodyId,1constituencyId
-		Query query = getSession().createQuery("select distinct model.localElectionBody.localElectionBodyId,model.constituency.name, model.localElectionBody.electionType.electionType,model.constituency.constituencyId" +
+		Query query = getSession().createQuery("select distinct model.localElectionBody.localElectionBodyId," +
+				" model.constituency.name," +
+				" model.localElectionBody.electionType.electionType," +
+				" model.constituency.constituencyId," +
+				" model.localElectionBody.name " +
 				"  from AssemblyLocalElectionBody model " +
 				" where model.constituency.constituencyId in(:constituencyIds) and model.year = (select max(model2.year) from AssemblyLocalElectionBody model2)");
 		query.setParameterList("constituencyIds",constituencyIds);
