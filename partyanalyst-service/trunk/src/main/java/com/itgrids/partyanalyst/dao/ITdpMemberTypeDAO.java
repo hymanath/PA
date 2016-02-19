0d12bf81624a34a772dff12cc8c02fc17f8623b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-19 08:53:15 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpMemberTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpMemberTypeDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpMemberType;
 
 public interface ITdpMemberTypeDAO extends GenericDao<TdpMemberType, Long>{
-
+	public List<Object[]> getCadreMemberTypeListByYear(Long year);
 }
