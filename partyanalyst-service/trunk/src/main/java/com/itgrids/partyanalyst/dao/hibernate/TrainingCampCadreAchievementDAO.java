07071d33c62172f87c5abf85273d3236c3924006
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-19 08:53:47 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampCadreAchievementDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TrainingCampCadreAchievementDAO.java
@@ -35,15 +35,19 @@ public class TrainingCampCadreAchievementDAO extends GenericDaoHibernate<Trainin
 		query.setParameter("trainingCampBatchId",batchId);
 		return (Long)query.uniqueResult();
 	}
-	public int deleteAchievementsforACadre(Long tdpCadreId,Long batchId){
-		
-		Query query=getSession().createQuery(" delete from TrainingCampCadreAchievement model " +
-		" where model.tdpCadreId=:tdpCadreId and model.trainingCampBatchId =:trainingCampBatchId and model.achievement is not null " +
-		" and model.trainingCampBatch.attendeeTypeId=1 and model.trainingCampBatch.isCancelled ='false' ");
-		query.setParameter("tdpCadreId",tdpCadreId);
-		query.setParameter("trainingCampBatchId",batchId);
-		int count = query.executeUpdate();	
-		return count;
+	public int deleteAchievementsforACadre(Long tdpCadreId,Long batchId)
+	{
+		try{
+			Query query=getSession().createQuery(" delete from TrainingCampCadreAchievement model " +
+					" where model.tdpCadreId=:tdpCadreId and model.trainingCampBatchId =:trainingCampBatchId and model.achievement is not null " +
+					" and model.trainingCampBatch.attendeeTypeId=1 and model.trainingCampBatch.isCancelled ='false' ");
+					query.setParameter("tdpCadreId",tdpCadreId);
+					query.setParameter("trainingCampBatchId",batchId);
+			int count = query.executeUpdate();	
+			return count;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
-
 }
