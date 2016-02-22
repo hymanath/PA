9ccab8ca8f9aa3084c6230f444d77b2e4fdba42b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-22 21:43:41 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISmsSenderService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ISmsSenderService.java
@@ -4,6 +4,6 @@ import com.itgrids.partyanalyst.dto.MobileAppUserVoterVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface ISmsSenderService {
-	public ResultStatus sendSmsToCadre(Integer startIndex,Integer maxIndex);
+	public ResultStatus sendSmsToCadre(MobileAppUserVoterVO inputVO);
 	public ResultStatus sendSmsToVoter(MobileAppUserVoterVO inputVO);
 }
