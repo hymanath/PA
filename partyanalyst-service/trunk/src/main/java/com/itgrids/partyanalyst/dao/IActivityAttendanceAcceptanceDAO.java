f7740ce71c14f5b4c65314c249bef63b565d6d74
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-23 08:31:05 +0000

    updated by-teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityAttendanceAcceptanceDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityAttendanceAcceptance;
+
+public interface IActivityAttendanceAcceptanceDAO extends GenericDao<ActivityAttendanceAcceptance,Long>{
+}
