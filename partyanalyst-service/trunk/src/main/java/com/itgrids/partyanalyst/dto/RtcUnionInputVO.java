42d491863804f0ece095591c0f2dcf0b6d708b87
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-25 13:51:37 +0000

    update by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/RtcUnionInputVO.java
@@ -0,0 +1,60 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.List;
+
+public class RtcUnionInputVO {
+
+	private List<Long> memeberTypeIds;
+	private String locationType;
+	private Long locationId;
+	private String startDate;
+	private String toDate;
+	private String dateType;
+	private String appType;
+	public List<Long> getMemeberTypeIds() {
+		return memeberTypeIds;
+	}
+	public void setMemeberTypeIds(List<Long> memeberTypeIds) {
+		this.memeberTypeIds = memeberTypeIds;
+	}
+	public String getLocationType() {
+		return locationType;
+	}
+	public void setLocationType(String locationType) {
+		this.locationType = locationType;
+	}
+	public Long getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(Long locationId) {
+		this.locationId = locationId;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getToDate() {
+		return toDate;
+	}
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+	public String getDateType() {
+		return dateType;
+	}
+	public void setDateType(String dateType) {
+		this.dateType = dateType;
+	}
+	public String getAppType() {
+		return appType;
+	}
+	public void setAppType(String appType) {
+		this.appType = appType;
+	}
+	
+	
+	
+	
+}
