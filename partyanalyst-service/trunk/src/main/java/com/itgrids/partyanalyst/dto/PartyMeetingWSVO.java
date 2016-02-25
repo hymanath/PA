7b9837c3bd2ed5b6b84d68b74749fce3ee045031
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-25 11:50:59 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PartyMeetingWSVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PartyMeetingWSVO.java
@@ -7,6 +7,7 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,10 +42,128 @@ public class PartyMeetingWSVO {
 	private String memberType;
 	private List<PartyMeetingWSVO> partyMeetingWSVoList = new ArrayList<PartyMeetingWSVO>();
 	private List<PartyMeetingWSVO> designationWiseCountsList = new ArrayList<PartyMeetingWSVO>();
+	private String status;
+	
+	/* tab details */
+	
+	private Long 			tabDetailsId;
+	private Date 			attendedTime;
+	private String 			imei;
+	private String 			uniqueKey;
+	private Date 			insertedTime;
+	private String 			latitude;
+	private String 			longitude;
+	private Long 			tabUserId;
+	private Long 			currentTabUserId;
+	private String 			syncSource;
+	private Long 			insertedBy;
+	private Long 			tabPrimaryKey;
+	private List<String> 	availableDates=new ArrayList<String>(0);
+	private String 			surveyDate;
+	private String 			userName;
+	
+	/* */
 	
 	
 	public String getRoles() {
 		return roles;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Long getTabDetailsId() {
+		return tabDetailsId;
+	}
+	public void setTabDetailsId(Long tabDetailsId) {
+		this.tabDetailsId = tabDetailsId;
+	}
+	public Date getAttendedTime() {
+		return attendedTime;
+	}
+	public void setAttendedTime(Date attendedTime) {
+		this.attendedTime = attendedTime;
+	}
+	public String getImei() {
+		return imei;
+	}
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+	public String getUniqueKey() {
+		return uniqueKey;
+	}
+	public void setUniqueKey(String uniqueKey) {
+		this.uniqueKey = uniqueKey;
+	}
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public Long getTabUserId() {
+		return tabUserId;
+	}
+	public void setTabUserId(Long tabUserId) {
+		this.tabUserId = tabUserId;
+	}
+	public Long getCurrentTabUserId() {
+		return currentTabUserId;
+	}
+	public void setCurrentTabUserId(Long currentTabUserId) {
+		this.currentTabUserId = currentTabUserId;
+	}
+	public String getSyncSource() {
+		return syncSource;
+	}
+	public void setSyncSource(String syncSource) {
+		this.syncSource = syncSource;
+	}
+	public Long getInsertedBy() {
+		return insertedBy;
+	}
+	public void setInsertedBy(Long insertedBy) {
+		this.insertedBy = insertedBy;
+	}
+	public Long getTabPrimaryKey() {
+		return tabPrimaryKey;
+	}
+	public void setTabPrimaryKey(Long tabPrimaryKey) {
+		this.tabPrimaryKey = tabPrimaryKey;
+	}
+	public List<String> getAvailableDates() {
+		return availableDates;
+	}
+	public void setAvailableDates(List<String> availableDates) {
+		this.availableDates = availableDates;
+	}
+	public String getSurveyDate() {
+		return surveyDate;
+	}
+	public void setSurveyDate(String surveyDate) {
+		this.surveyDate = surveyDate;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	public void setRoles(String roles) {
 		this.roles = roles;
