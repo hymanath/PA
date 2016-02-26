5d15ee34e6704ff66fa428469d244dde74cce27d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-02-26 05:29:41 +0000

    updated by sravanth

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UnionTabUserVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/UnionTabUserVO.java
@@ -1,14 +1,46 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnionTabUserVO {
+	
 	private Long unionTabUserId;
+	private Long id;
 	private String name;
 	private String mobileNum;
 	private String uname;
 	private String pwd;
 	private String msg;
 	
+	private Long memberTypeId;
+	private String memberTypeName;
+	private List<UnionTabUserVO> memberTypesList = new ArrayList<UnionTabUserVO>(0);
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getMemberTypeId() {
+		return memberTypeId;
+	}
+	public void setMemberTypeId(Long memberTypeId) {
+		this.memberTypeId = memberTypeId;
+	}
+	public String getMemberTypeName() {
+		return memberTypeName;
+	}
+	public void setMemberTypeName(String memberTypeName) {
+		this.memberTypeName = memberTypeName;
+	}
+	public List<UnionTabUserVO> getMemberTypesList() {
+		return memberTypesList;
+	}
+	public void setMemberTypesList(List<UnionTabUserVO> memberTypesList) {
+		this.memberTypesList = memberTypesList;
+	}
 	public String getMsg() {
 		return msg;
 	}
