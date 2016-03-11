7bdf1df547fa523e3a3cf8d20d5861683c7e5e4b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-11 09:02:06 +0000

    AppointmentCandidate Model Created By ---Santosh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AppointmentCandidate.java
@@ -0,0 +1,239 @@
+package com.itgrids.partyanalyst.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+
+@Entity
+@Table(name="appointment_candidate")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class AppointmentCandidate extends BaseModel {
+
+	 private Long appointmentCandidateId;
+	 private Long appointmentId;
+	 private String name;
+	 private Long designationId;
+	 private String mobileNo;
+	 private Long locationScopeId;
+	 private Long locationValue;
+	 private Long addressId;
+	 private Long voterId;
+	 private String voterIdCardNo;
+	 private Long tdpCadreId;
+	 private String membershipId;
+	 private Long createdBy;
+	 private Long updatedBy;
+	 private Date insertedTime;
+	 private Date updatedTime;
+	 
+	 private Appointment appointment;
+	 private AppointmentCandidateDesignation candidateDesignation;
+	 private UserAddress userAddress;
+	 private Voter voter;
+	 private TdpCadre tdpCadre;
+	 private User createdUser;
+	 private User updateUser; 
+	 
+		
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 @Column(name = "appointment_candidate_id", unique = true, nullable = false)
+	 public Long getAppointmentCandidateId() {
+		return appointmentCandidateId;
+	}
+	public void setAppointmentCandidateId(Long appointmentCandidateId) {
+		this.appointmentCandidateId = appointmentCandidateId;
+	}
+	@Column(name = "appointment_id")
+	public Long getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(Long appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Column(name = "designation_id")
+	public Long getDesignationId() {
+		return designationId;
+	}
+	public void setDesignationId(Long designationId) {
+		this.designationId = designationId;
+	}
+	@Column(name = "mobile_no")
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	@Column(name = "location_scope_id")
+	public Long getLocationScopeId() {
+		return locationScopeId;
+	}
+	public void setLocationScopeId(Long locationScopeId) {
+		this.locationScopeId = locationScopeId;
+	}
+	@Column(name  = "location_value")
+	public Long getLocationValue() {
+		return locationValue;
+	}
+	public void setLocationValue(Long locationValue) {
+		this.locationValue = locationValue;
+	}
+	@Column(name = "address_id")
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	@Column(name = "voter_id")
+	public Long getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(Long voterId) {
+		this.voterId = voterId;
+	}
+	@Column(name = "voter_id_card_no")
+	public String getVoterIdCardNo() {
+		return voterIdCardNo;
+	}
+	public void setVoterIdCardNo(String voterIdCardNo) {
+		this.voterIdCardNo = voterIdCardNo;
+	}
+	@Column(name = "tdp_cadre_id")
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	@Column(name = "membership_id")
+	public String getMembershipId() {
+		return membershipId;
+	}
+	public void setMembershipId(String membershipId) {
+		this.membershipId = membershipId;
+	}
+	@Column(name = "created_by")
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+	@Column(name = "updated_by")
+	public Long getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Long updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	@Column(name = "inserted_time")
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	@Column(name = "updated_time")
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="appointment_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Appointment getAppointment() {
+		return appointment;
+	}
+	public void setAppointment(Appointment appointment) {
+		this.appointment = appointment;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="designation_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public AppointmentCandidateDesignation getCandidateDesignation() {
+		return candidateDesignation;
+	}
+	public void setCandidateDesignation(
+			AppointmentCandidateDesignation candidateDesignation) {
+		this.candidateDesignation = candidateDesignation;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="address_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public UserAddress getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(UserAddress userAddress) {
+		this.userAddress = userAddress;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="voter_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Voter getVoter() {
+		return voter;
+	}
+	public void setVoter(Voter voter) {
+		this.voter = voter;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="tdp_cadre_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadre getTdpCadre() {
+		return tdpCadre;
+	}
+	public void setTdpCadre(TdpCadre tdpCadre) {
+		this.tdpCadre = tdpCadre;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="created_by", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getCreatedUser() {
+		return createdUser;
+	}
+	public void setCreatedUser(User createdUser) {
+		this.createdUser = createdUser;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="updated_by", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(User updateUser) {
+		this.updateUser = updateUser;
+	}
+}
