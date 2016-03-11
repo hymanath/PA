5a0f7a12c7563df088c4b7623fa24a3fdd74843d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-11 07:24:55 +0000

    AppointmentStatus model added by-swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AppointmentStatus.java
@@ -0,0 +1,38 @@
+package com.itgrids.partyanalyst.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "appointment_status")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class AppointmentStatus extends BaseModel {
+	private Long appointmentStatusId;
+	private String status;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "appointment_status_id", unique = true, nullable = false)
+	public Long getAppointmentStatusId() {
+		return appointmentStatusId;
+	}
+	public void setAppointmentStatusId(Long appointmentStatusId) {
+		this.appointmentStatusId = appointmentStatusId;
+	}
+	@Column(name = "status")
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
