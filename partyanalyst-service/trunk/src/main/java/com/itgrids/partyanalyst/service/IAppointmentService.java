ce56c173c9d9c2444bb07555ed836dba46327d43
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-11 15:01:09 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAppointmentService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAppointmentService.java
@@ -1,8 +1,15 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.AppointmentVO;
+import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
 
 public interface IAppointmentService {
 	public ResultStatus saveAppointment(final AppointmentVO appointmentVO,final Long loggedUserId);
+	public List<IdNameVO> getAppointmentStatusList();
+	public List<IdNameVO> getAppCandidateDesigList();
+	public List<IdNameVO> getAppointmentPriorityList();
+	public List<IdNameVO> getAppmntLblStatusList();
 }
