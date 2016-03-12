7d1ff00cd62459666109a08b120670fc547a7089
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-12 02:39:48 +0000

    Updated by Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/EventInfoDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/EventInfoDAOHibernateTest.java
@@ -1,5 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IEventInfoDAO;
@@ -11,7 +16,35 @@ public class EventInfoDAOHibernateTest extends BaseDaoTestCase{
 		this.eventInfoDAO = eventInfoDAO;
 	}
 	
-	
+	public void testGetEventDataByReportLevelId()
+	{
+		try{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date stratDate = sdf.parse("2016-03-11");
+		Date endDate = sdf.parse("2016-03-11");
+		List<Long> eventIds = new ArrayList<Long>(0);
+		
+		eventIds.add(2l);
+		eventIds.add(3l);
+		eventIds.add(4l);
+		eventIds.add(5l);
+		eventIds.add(6l);
+		eventIds.add(14l);
+		eventIds.add(15l);
+		
+		List<Object[]> list = eventInfoDAO.getEventDataByReportLevelId(4L,1L,1L,eventIds,stratDate,endDate);
+		System.out.println(list.size());
+		
+		for(Object[] params : list)
+		{
+			System.out.println();
+			for(Object obj : params)
+				System.out.print("\t"+obj);
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
 
 }
