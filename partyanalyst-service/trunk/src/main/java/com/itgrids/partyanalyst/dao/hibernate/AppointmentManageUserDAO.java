102ed45d1f6909fbd3645ad0d27693dc91bb4895
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-14 13:55:00 +0000

    Appointment User Bringed Based On User Id--Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentManageUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentManageUserDAO.java
@@ -1,10 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAppointmentManageUserDAO;
 import com.itgrids.partyanalyst.model.AppointmentManageUser;
+import com.itgrids.partyanalyst.model.AppointmentUser;
 
 public class AppointmentManageUserDAO extends GenericDaoHibernate<AppointmentManageUser, Long> implements IAppointmentManageUserDAO{
 	
@@ -12,5 +15,10 @@ public class AppointmentManageUserDAO extends GenericDaoHibernate<AppointmentMan
 		super(AppointmentManageUser.class);
 	}
 	
-
+	@Override
+	public List<Object[]> getAppointmentUsersDtlsByUserId(Long userId) {
+	   Query query=getSession().createQuery(" select model.appointmentUser.appointmenUserId,model.appointmentUser.name,model.appointmentUser.mobile  from AppointmentManageUser model where model.user.userId=:userId");
+	    query.setParameter("userId", userId);
+		return query.list();
+	}
 }
