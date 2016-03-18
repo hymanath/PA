3aab3d5253d9bb66e70ebf023ff16e373c762edc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-18 10:27:54 +0000

    commited by Swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -18,6 +19,13 @@ public class AppointmentDAO extends GenericDaoHibernate<Appointment, Long> imple
 	}
 	public List<Object[]> getTotalAppointmentStatus(){
 		Query query = getSession().createQuery("select model.appointmentStatusId, count(model.appointmentStatusId) from Appointment model where model.isDeleted='N' group by model.appointmentStatusId order by model.appointmentStatusId");
+		return query.list();
+	}
+	public List<Object[]> getTotalAppointmentStatusForToday(Date today){
+		Query query = getSession().createQuery("select model.appointmentStatusId, count(model.appointmentStatusId) from Appointment model " +
+				"where model.isDeleted='N' and (model.insertedTime =:today or model.updatedTime =:today) " +
+				"group by model.appointmentStatusId order by model.appointmentStatusId");
+		query.setDate("today", today);
 		return query.list();
 	}
 
