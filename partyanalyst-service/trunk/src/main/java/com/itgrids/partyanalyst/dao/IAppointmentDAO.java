829f2b2360749a65cdd4e56d38f1b99593bdbdd6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-18 10:27:31 +0000

    commited by Swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -8,5 +9,5 @@ import com.itgrids.partyanalyst.model.Appointment;
 
 public interface IAppointmentDAO extends GenericDao<Appointment, Long>{
 	public List<Object[]> getTotalAppointmentStatus();
-
+	public List<Object[]> getTotalAppointmentStatusForToday(Date today);
 }
