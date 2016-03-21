45598e3a2bfab57fcb45c6e9ff358d56b69d9d8c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-21 13:45:09 +0000

    commited by Swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentCandidateRelationDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AppointmentCandidateRelation;
+
+public interface IAppointmentCandidateRelationDAO extends
+		GenericDao<AppointmentCandidateRelation, Long> {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILabelAppointmentDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.LabelAppointment;
+
+public interface ILabelAppointmentDAO extends
+		GenericDao<LabelAppointment, Long> {
+
+}
