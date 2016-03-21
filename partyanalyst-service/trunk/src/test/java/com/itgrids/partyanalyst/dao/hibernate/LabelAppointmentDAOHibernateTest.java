eec420904531be8db586848a7bb7e77eedcc2935
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-21 13:56:28 +0000

    commited by Swadhin

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/LabelAppointmentDAOHibernateTest.java
@@ -0,0 +1,13 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ILabelAppointmentDAO;
+
+public class LabelAppointmentDAOHibernateTest extends BaseDaoTestCase {
+	private ILabelAppointmentDAO labelAppointmentDAO;
+	public void test(){
+		
+	}
+
+}
