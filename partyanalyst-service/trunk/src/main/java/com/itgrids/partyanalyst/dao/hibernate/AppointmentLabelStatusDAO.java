ec397779a9bb44570ee3191a42577360652426aa
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-22 09:33:08 +0000

    name changed -- sandeep

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentLabelStatusDAO.java
@@ -0,0 +1,21 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IAppointmentLabelStatusDAO;
+import com.itgrids.partyanalyst.model.AppointmentLabelStatus;
+
+public class AppointmentLabelStatusDAO extends GenericDaoHibernate<AppointmentLabelStatus, Long> implements IAppointmentLabelStatusDAO {
+	
+	public AppointmentLabelStatusDAO() {
+		super(AppointmentLabelStatus.class);
+	}
+	public List<Object[]> getAppmntLblStatusList(){
+		Query query = getSession().createQuery("select model.appointmentLableStatusId, model.status from AppointmentLabelStatus model");
+		return query.list();
+	}
+
+}
