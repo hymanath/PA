36ba7909de19ad9c5d905930e3b4de023462b635
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-22 09:31:19 +0000

    name changed -- sandeep

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentLabelDAO.java
@@ -0,0 +1,15 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AppointmentLabel;
+
+public interface IAppointmentLabelDAO extends GenericDao<AppointmentLabel, Long> {
+	
+	public List<Object[]> getLabelDtslByDate(Date date,Long appntmntUsrId);
+	public Integer deleteAppointmentLabel(Long appointmentLabelId);
+
+}
