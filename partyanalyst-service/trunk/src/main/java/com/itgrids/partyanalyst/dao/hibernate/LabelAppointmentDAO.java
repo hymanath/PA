e41654205d966075fbdbd1e381e5a275e5da0b3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-22 16:13:50 +0000

    status wise counts for lables -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LabelAppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LabelAppointmentDAO.java
@@ -1,18 +1,41 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ILabelAppointmentDAO;
 import com.itgrids.partyanalyst.model.LabelAppointment;
 
-public class LabelAppointmentDAO extends
-		GenericDaoHibernate<LabelAppointment, Long> implements
-		ILabelAppointmentDAO {
+public class LabelAppointmentDAO extends GenericDaoHibernate<LabelAppointment, Long> implements	ILabelAppointmentDAO {
 
 	public LabelAppointmentDAO(){
 		super(LabelAppointment.class);
 	}
 
+	public List<Object[]> getLableDetailsWithStatusWiseCounts(Date labelDate,Long userId){
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(" select model.appointmentLabel.appointmentLabelId,model.appointmentLabel.labelName," +
+				" model.appointmentLabel.appointmentLabelStatus.appointmentLabelStatusId, " +
+				" model.appointmentLabel.appointmentLabelStatus.status, " +
+				" model.appointment.appointmentStatusId,model.appointment.appointmentStatus.status,count(model.appointment.appointmentStatusId) " +
+				" from LabelAppointment model " +
+				" where model.appointmentLabel.isDeleted='N' and model.appointment.isDeleted='N' " );
+		if(labelDate != null)
+				sb.append("and date(model.appointmentLabel.updatedTime)=:labelDate ");
+		sb.append(" and model.appointmentLabel.insertedBy=:userId ");
+		sb.append(" group by model.appointment.appointmentStatusId,model.appointmentLabel.appointmentLabelId ");
+		
+		Query query = getSession().createQuery(sb.toString());
+		
+		query.setParameter("userId", userId);
+		
+		if(labelDate != null)
+			query.setParameter("labelDate", labelDate);
+		
+		return query.list();
+	}
 }
