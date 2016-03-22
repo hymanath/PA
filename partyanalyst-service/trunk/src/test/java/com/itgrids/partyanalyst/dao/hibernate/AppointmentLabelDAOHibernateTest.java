e34d260c8a1bd19068bdcbe3264039d799c7f243
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-22 09:35:46 +0000

    name changed -- sandeep

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentLabelDAOHibernateTest.java
@@ -0,0 +1,23 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IAppointmentLabelDAO;
+
+public class AppointmentLabelDAOHibernateTest extends BaseDaoTestCase {
+	
+	private IAppointmentLabelDAO appointmentLabelDAO;
+
+	public IAppointmentLabelDAO getAppointmentLabelDAO() {
+		return appointmentLabelDAO;
+	}
+
+	public void setAppointmentLabelDAO(IAppointmentLabelDAO appointmentLabelDAO) {
+		this.appointmentLabelDAO = appointmentLabelDAO;
+	}
+	
+	public void test(){
+		
+	}
+
+}
---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentLabelStatusDAOHibernateTest.java
@@ -0,0 +1,23 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IAppointmentLabelStatusDAO;
+
+public class AppointmentLabelStatusDAOHibernateTest extends BaseDaoTestCase {
+
+	private IAppointmentLabelStatusDAO appointmentLabelStatusDAO;
+
+	public IAppointmentLabelStatusDAO getAppointmentLabelStatusDAO() {
+		return appointmentLabelStatusDAO;
+	}
+
+	public void setAppointmentLabelStatusDAO(
+			IAppointmentLabelStatusDAO appointmentLabelStatusDAO) {
+		this.appointmentLabelStatusDAO = appointmentLabelStatusDAO;
+	}
+	 
+	public void test(){
+		
+	}
+}
