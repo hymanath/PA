13f87d8159d3235e392d5d51f26050d008dece54
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-22 16:12:09 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILabelAppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILabelAppointmentDAO.java
@@ -1,10 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.LabelAppointment;
 
-public interface ILabelAppointmentDAO extends
-		GenericDao<LabelAppointment, Long> {
-
+public interface ILabelAppointmentDAO extends GenericDao<LabelAppointment, Long> {
+	
+	public List<Object[]> getLableDetailsWithStatusWiseCounts(Date labelDate,Long userId);
 }
