0b6fb982f49ae1cc1024f34b9a0e3e2a86cda9d3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-23 08:43:23 +0000

    commited by Swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentCandidateRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentCandidateRelationDAO.java
@@ -2,13 +2,13 @@ package com.itgrids.partyanalyst.dao;
 
 import java.util.List;
 import java.util.Set;
-
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.AppointmentCandidateRelation;
 
 public interface IAppointmentCandidateRelationDAO extends GenericDao<AppointmentCandidateRelation, Long> {
-	
+	public List<Object[]> getAllAppointmentDetails(int startIndex,int maxIndex);
+	public List<Object[]> countAppointmentDetails();
 	public List<Object[]> getAppointmentsBySearchCriteria(Long designationId,Long priorityId,Long statusId,Long districtId,Long constituencyId);
 	public List<Object[]> getAppointmentRelatedCandidates(List<Long> appointmentIds);
 	public List<Object[]> getCandidatePreviousApptDetails(List<Long> candidateIds);
