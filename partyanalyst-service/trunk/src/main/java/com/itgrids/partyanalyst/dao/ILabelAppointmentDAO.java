10dd3f9fecc0e1f98b52295072bdb5046d798764
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-23 12:49:11 +0000

    set appointments to the label - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILabelAppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILabelAppointmentDAO.java
@@ -12,8 +12,8 @@ public interface ILabelAppointmentDAO extends GenericDao<LabelAppointment, Long>
 	public  List<Object[]> getLableDetailsWithStatusWiseCounts(Date labelDate,Long userId);
 	public  List<Object[]>  checkLabelWithAppointment(Long appointmentLabelId,List<Long> appointmentIds);
 	public List<Long> getAppointmentsForALabel(Long appointmentLabelId);
-	public Integer deleteLabeledAppointments(List<Long> appointmentIds);
-	public Integer updateLabeledAppointments(List<Long> appointmentIds,Long userId,Date date);
+	public Integer deleteLabeledAppointments(Long appointmentLabelId,List<Long> appointmentIds);
+	public Integer updateLabeledAppointments(Long appointmentLabelId,List<Long> appointmentIds,Long userId,Date date);
 	public List<LabelAppointment> getDetailsOfLabelledAppointments(Long appointmentLabelId , List<Long> appointmentIds);
 	public List<Object[]> getAppointmentsOfALableForUpdate(Long lableId);
 }
