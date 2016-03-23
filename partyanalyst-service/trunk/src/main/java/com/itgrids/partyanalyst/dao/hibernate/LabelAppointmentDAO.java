f4aa420457750bf62ca10436c4e38e9cae91a187
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-03-23 13:30:22 +0000

    Appointments DashBoard Status Block- Balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LabelAppointmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LabelAppointmentDAO.java
@@ -8,6 +8,7 @@ import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ILabelAppointmentDAO;
 import com.itgrids.partyanalyst.model.LabelAppointment;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class LabelAppointmentDAO extends GenericDaoHibernate<LabelAppointment, Long> implements	ILabelAppointmentDAO {
 
@@ -39,6 +40,7 @@ public class LabelAppointmentDAO extends GenericDaoHibernate<LabelAppointment, L
 		return query.list();
 	}
 	
+
 	public  List<Object[]>  checkLabelWithAppointment(Long appointmentLabelId,List<Long> appointmentIds){
 		
 		Query query = getSession().createQuery(" select model.labelAppointmentId ,model.appointmentLabelId,model.appointmentId " +
@@ -97,4 +99,188 @@ public class LabelAppointmentDAO extends GenericDaoHibernate<LabelAppointment, L
 		query.setParameter("lableId", lableId);
 		return query.list();
 	}
+	
+	
+	/*select al.appointment_label_id,a.appointment_id,a.appointment_status_id
+	from label_appointment la,appointment_label al,appointment a,appointment_time_slot  ats
+	where 
+	la.appointment_label_id = al.appointment_label_id
+	and a.appointment_id = la.appointment_id
+	and ats.appointment_id = a.appointment_id
+	and ats.from_date >= :fromDate 
+	and ats.to_date <= :toDate
+	group by al.appointment_label_id,a.appointment_id,a.appointment_status_id;*/
+	
+	public List<Object[]> getLabelAppointmentsForFixed(Date toDayDate,String searchType,String type){
+		
+		StringBuilder str = new StringBuilder();
+		
+		str.append(" select model.appointmentLabel.appointmentLabelId,model.appointmentLabel.labelName," +
+				" model.appointment.appointmentStatusId,model.appointment.appointmentStatus.status, " +
+				" count(distinct model.appointment.appointmentId)" +
+				" from LabelAppointment model,AppointmentTimeSlot ATS" +
+				" where model.appointment.isDeleted = 'N'" +
+				" and model.appointmentLabel.isDeleted = 'N'" +
+				" and model.isDeleted = 'N' " +
+				" and ATS.appointmentId = model.appointment.appointmentId ");
+		
+		if(type !=null && type.trim().equalsIgnoreCase("overall")){
+			if(toDayDate !=null){
+				if(searchType !=null && searchType.trim().equalsIgnoreCase("Inprogress")){
+					str.append(" and ATS.fromDate  >= :toDayDate and ATS.toDate <= :toDayDate ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Upcoming")){
+					str.append(" and ATS.fromDate > :toDayDate  ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Completed")){
+					str.append(" and ATS.toDate <  :toDayDate  ");
+				}			
+			}
+		}else{
+			if(toDayDate !=null){
+				if(searchType !=null && searchType.trim().equalsIgnoreCase("Inprogress")){
+					str.append(" and date(ATS.fromDate)  >= :toDayDate and date(ATS.toDate) <= :toDayDate ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Upcoming")){
+					str.append(" and date(ATS.fromDate) > :toDayDate  ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Completed")){
+					str.append(" and date(ATS.toDate) <  :toDayDate  ");
+				}
+			}
+		}
+		
+		str.append(" group by model.appointmentLabel.appointmentLabelId,model.appointment.appointmentStatusId ");
+		
+		
+		Query query = getSession().createQuery(str.toString());
+		
+		if(toDayDate !=null){
+			query.setDate("toDayDate", toDayDate);
+		}
+		
+		return query.list();
+	}
+	
+	public List<Object[]> getStatusLabelAppointments(Date toDayDate,String type){
+		
+		/*select al.appointment_label_id,a.appointment_status_id,count(distinct a.appointment_id) 
+		from label_appointment la,appointment_label al,appointment a
+		where 
+		la.appointment_label_id = al.appointment_label_id
+		and la.appointment_id = a.appointment_id
+		and a.appointment_status_id in (4,5,6)
+		group by al.appointment_label_id,a.appointment_status_id;*/
+		
+		StringBuilder str = new StringBuilder();
+		str.append(" select model.appointmentLabel.appointmentLabelId,model.appointmentLabel.labelName," +
+				" model.appointment.appointmentStatusId,model.appointment.appointmentStatus.status," +
+				"  count(distinct model.appointment.appointmentId) " +
+				" from LabelAppointment model" +
+				" where model.appointment.isDeleted = 'N'" +
+				" and model.appointmentLabel.isDeleted = 'N'" +
+				" and model.isDeleted = 'N' ");
+		
+		
+		if(type !=null && type.trim().equalsIgnoreCase("toDay")){
+				if(toDayDate !=null){
+					str.append(" and date(model.appointment.updatedTime) = :toDayDate ");
+				}
+		}
+		
+		str.append(" and model.appointment.appointmentStatusId in (:statusIds) ");		
+		str.append(" group by model.appointmentLabel.appointmentLabelId,model.appointment.appointmentStatusId ");
+		
+		Query query = getSession().createQuery(str.toString());
+		
+		if(type !=null && type.trim().equalsIgnoreCase("toDay")){
+			if(toDayDate !=null){
+				query.setDate("toDayDate", toDayDate);
+			}
+		}
+		query.setParameterList("statusIds",IConstants.APPOINTMENT_STATUS_IDS);
+		return query.list();
+	}
+	
+public List<Object[]> getLabelAppointmentsForFixedSatus(Date toDayDate,String searchType,String type){
+		
+		StringBuilder str = new StringBuilder();
+		
+		str.append(" select " +
+				" model.appointment.appointmentStatusId,model.appointment.appointmentStatus.status, " +
+				" count(distinct model.appointment.appointmentId)" +
+				" from LabelAppointment model,AppointmentTimeSlot ATS" +
+				" where model.appointment.isDeleted = 'N'" +
+				" and model.isDeleted = 'N' " +
+				" and ATS.appointmentId = model.appointment.appointmentId ");
+		
+		if(type !=null && type.trim().equalsIgnoreCase("overall")){
+			if(toDayDate !=null){
+				if(searchType !=null && searchType.trim().equalsIgnoreCase("Inprogress")){
+					str.append(" and ATS.fromDate  >= :toDayDate and ATS.toDate <= :toDayDate ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Upcoming")){
+					str.append(" and ATS.fromDate > :toDayDate  ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Completed")){
+					str.append(" and ATS.toDate <  :toDayDate  ");
+				}			
+			}
+		}else{
+			if(toDayDate !=null){
+				if(searchType !=null && searchType.trim().equalsIgnoreCase("Inprogress")){
+					str.append(" and date(ATS.fromDate)  >= :toDayDate and date(ATS.toDate) <= :toDayDate ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Upcoming")){
+					str.append(" and date(ATS.fromDate) > :toDayDate  ");
+				}else if(searchType !=null && searchType.trim().equalsIgnoreCase("Completed")){
+					str.append(" and date(ATS.toDate) <  :toDayDate  ");
+				}
+			}
+		}
+		
+		str.append(" group by model.appointment.appointmentStatusId ");
+		
+		
+		Query query = getSession().createQuery(str.toString());
+		
+		if(toDayDate !=null){
+			query.setDate("toDayDate", toDayDate);
+		}
+		
+		return query.list();
+	}
+
+public List<Object[]> getLabelAppointmentsStatus(Date toDayDate,String type){
+	
+	/*select al.appointment_label_id,a.appointment_status_id,count(distinct a.appointment_id) 
+	from label_appointment la,appointment_label al,appointment a
+	where 
+	la.appointment_label_id = al.appointment_label_id
+	and la.appointment_id = a.appointment_id
+	and a.appointment_status_id in (4,5,6)
+	group by al.appointment_label_id,a.appointment_status_id;*/
+	
+	StringBuilder str = new StringBuilder();
+	str.append(" select " +
+			" model.appointment.appointmentStatusId,model.appointment.appointmentStatus.status," +
+			"  count(distinct model.appointment.appointmentId) " +
+			" from LabelAppointment model" +
+			" where model.appointment.isDeleted = 'N'" +
+			" and model.appointmentLabel.isDeleted = 'N'" +
+			" and model.isDeleted = 'N' ");
+	
+	
+	if(type !=null && type.trim().equalsIgnoreCase("toDay")){
+			if(toDayDate !=null){
+				str.append(" and date(model.appointment.updatedTime) = :toDayDate ");
+			}
+	}
+	
+	str.append(" and model.appointment.appointmentStatusId in (:statusIds) ");		
+	str.append(" group by model.appointment.appointmentStatusId ");
+	
+	Query query = getSession().createQuery(str.toString());
+	
+	if(type !=null && type.trim().equalsIgnoreCase("toDay")){
+		if(toDayDate !=null){
+			query.setDate("toDayDate", toDayDate);
+		}
+	}
+	query.setParameterList("statusIds",IConstants.APPOINTMENT_STATUS_IDS);
+	return query.list();
+}
 }
