02189d024273fcfb9c3eabc2ccb41e2b832362d6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-07 15:03:54 +0000

    New Creation of AppointmentReportDashBoardAction class and appointmentReportDashBoardAction.jsp --------Santosh

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AppointmentReportDashBoardAction.java
@@ -0,0 +1,23 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AppointmentReportDashBoardAction extends ActionSupport implements ServletRequestAware{ 
+	
+	
+	private HttpServletRequest request;
+	
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public String execute(){
+		return Action.SUCCESS;
+	}
+
+}
