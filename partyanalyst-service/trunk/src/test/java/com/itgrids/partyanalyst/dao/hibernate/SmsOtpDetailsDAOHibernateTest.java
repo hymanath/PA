d1f163dab81aeac81075caad6858974ef95bef16
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-16 15:07:00 +0000

    commited by Swadhin

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/SmsOtpDetailsDAOHibernateTest.java
@@ -0,0 +1,17 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.ISmsOtpDetailsDAO;
+
+public class SmsOtpDetailsDAOHibernateTest extends BaseDaoTestCase {
+	private ISmsOtpDetailsDAO smsOtpDetailsDAO;
+
+	public void setSmsOtpDetailsDAO(ISmsOtpDetailsDAO smsOtpDetailsDAO) {
+		this.smsOtpDetailsDAO = smsOtpDetailsDAO;
+	}
+	public void test(){
+		
+	}
+
+}
