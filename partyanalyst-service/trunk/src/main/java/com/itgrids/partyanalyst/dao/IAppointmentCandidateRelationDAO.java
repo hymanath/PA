1e68cec9d5ef527cd75decff015ac973ed33260f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-18 11:14:10 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentCandidateRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentCandidateRelationDAO.java
@@ -16,19 +16,19 @@ public interface IAppointmentCandidateRelationDAO extends GenericDao<Appointment
 	public List<Object[]> getCandidatePreviousApptDetails(List<Long> candidateIds);
 	public List<Object[]> getAppointmentCandidateDetails(List<Long> appointmentIds);
 	public List<Object[]> getAppointmentSearchDetails(Date fromDate,Date toDate,AppointmentInputVO inputVo,String searchType);
-	public List<Object[]> getLastVisitsByCandidates(List<Long> candidateIds);
-	public List<Object[]> getCandidatePreviousApptDetails1(List<Long> candidateIds);
+	public List<Object[]> getLastVisitsByCandidates(List<Long> candidateIds,Long apptUserId);
+	public List<Object[]> getCandidatePreviousApptDetails1(List<Long> candidateIds,Long apptUserID);
 	public List<Object[]> getAppointmentCandidateMobileNos(Long appointmentId);
 	public List<Object[]> getAppointmentStatusOverview();
-	public List<Object[]> getAppointStatusOverviewforCandidate(Long apointmntcandidteId);
+	public List<Object[]> getAppointStatusOverviewforCandidate(Long apointmntcandidteId,Long apptUserId);
    public List<String> getAppointmentIdsforSendSms(Long appointmentId);
 	
 	public List<Object[]> getApptAndMembersCountsByStatus(Long apptUserId);
 	public List<Object[]> getLabelledAndNonLabelledApptIdsForWaitingStatus(Long apptUserId,String labelStatus,Long waitingAppointmentStatusId);
 	public List<Object[]> getOnlyFixedStatusCounts(Long apptUserId,Date currentDateAndTime,Long fixedAppointmentStatusId);
 	public List<Object[]> getAttendedStatusCounts(Long apptUserId,Date currentDateAndTime,Long attendedAppointmentStatusId,Long fixedAppointmentStatusId);
-	public List<Object[]> getFixedAttendedCount(Long apointmntcandidteId);
-	public List<Object[]> getAppointmentHistoryDetailsByCandidateId(Long apointmntcandidteId);
+	public List<Object[]> getFixedAttendedCount(Long apointmntcandidteId,Date currentDateAndTime,Long apptUserId);
+	public List<Object[]> getAppointmentHistoryDetailsByCandidateId(Long apointmntcandidteId,Long apptUserId);
 	
 	public List<Long> getApptCandidIds(Long appointmentId);
 	public List<Object[]> checkApptsAsVoid(Date insertedTime,Long apptStatusId,List<Long> apptCandiIds);
