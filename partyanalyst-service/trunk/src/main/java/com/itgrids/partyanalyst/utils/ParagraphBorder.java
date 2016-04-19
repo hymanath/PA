d7c7abb5bcedb7bba8eb3416baff7c0ad09098c4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-19 13:34:22 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ParagraphBorder.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/ParagraphBorder.java
@@ -6,25 +6,41 @@ import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class ParagraphBorder extends PdfPageEventHelper {
-    public boolean active = false;
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+	 public boolean active = false;
+	    public void setActive(boolean active) {
+	        this.active = active;
+	    }
 
-    public float offset = 5;
-    public float startPosition;
+	    public float offset = 5;
+	    public float startPosition;
 
-    public void onParagraph(PdfWriter writer, Document document, float paragraphPosition) {
-        this.startPosition = paragraphPosition;
-    }
+	    @Override
+	    public void onStartPage(PdfWriter writer, Document document) {
+	        startPosition = document.top();
+	    }
 
-    public void onParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) {
-        if (active) {
-            PdfContentByte cb = writer.getDirectContentUnder();
-            cb.rectangle(document.left(), paragraphPosition - offset,
-                document.right() - document.left(), startPosition - paragraphPosition);
-            cb.stroke();
-        }
-    
-    }
+	    @Override
+	    public void onParagraph(PdfWriter writer, Document document, float paragraphPosition) {
+	        this.startPosition = paragraphPosition;
+	    }
+
+	    @Override
+	    public void onEndPage(PdfWriter writer, Document document) {
+	        if (active) {
+	            PdfContentByte cb = writer.getDirectContentUnder();
+	            cb.rectangle(document.left(), document.bottom() - offset,
+	                document.right() - document.left(), startPosition - document.bottom());
+	            cb.stroke();
+	        }
+	    }
+
+	    @Override
+	    public void onParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) {
+	        if (active) {
+	            PdfContentByte cb = writer.getDirectContentUnder();
+	            cb.rectangle(document.left(), paragraphPosition - offset,
+	                document.right() - document.left(), startPosition - paragraphPosition);
+	            cb.stroke();
+	        }
+	    }
     }
