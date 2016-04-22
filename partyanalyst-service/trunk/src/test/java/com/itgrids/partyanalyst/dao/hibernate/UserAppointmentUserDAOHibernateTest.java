6a6cd2fb940865d4c711ca63584b632ebeac394d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-22 08:30:41 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/UserAppointmentUserDAOHibernateTest.java
@@ -0,0 +1,23 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IUserAppointmentUserDAO;
+
+public class UserAppointmentUserDAOHibernateTest extends BaseDaoTestCase{
+
+	private IUserAppointmentUserDAO userAppointmentUserDAO;
+
+	public IUserAppointmentUserDAO getUserAppointmentUserDAO() {
+		return userAppointmentUserDAO;
+	}
+
+	public void setUserAppointmentUserDAO(
+			IUserAppointmentUserDAO userAppointmentUserDAO) {
+		this.userAppointmentUserDAO = userAppointmentUserDAO;
+	}
+	
+	public void test(){
+		
+	}
+}
