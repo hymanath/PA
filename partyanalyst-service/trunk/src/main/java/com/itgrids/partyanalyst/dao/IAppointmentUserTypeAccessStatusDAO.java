7d75fef4083ae3d0057435c316a9787fe0092a50
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-22 06:02:29 +0000

    models and daos for appts - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAppointmentUserTypeAccessStatusDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AppointmentUserTypeAccessStatus;
+
+public interface IAppointmentUserTypeAccessStatusDAO extends GenericDao<AppointmentUserTypeAccessStatus,Long>{
+
+}
