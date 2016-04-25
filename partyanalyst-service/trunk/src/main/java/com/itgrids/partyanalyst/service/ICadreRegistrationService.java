64a91f2db3dd3ffdad46fae69e2a8ffa0b68e6ef
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-25 17:23:29 +0000

    Updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationService.java
@@ -189,4 +189,5 @@ public interface ICadreRegistrationService {
 	//public List<GenericVO> getCadreMemberTypeListByYear(Long year);
 	public AffiliatedCadreVO getAllTotalCountsForAll(String searchType);
 	public List<TdpCadreVO> getLocationwiseCadreRegistraionDetailsForAffliatedCadre(List<Long> membereTypeIdsList,String searchTypeStr,String startDate,String toDate);
+	public String  updatePaymentStatus(Long tdpCadreId);
 }
