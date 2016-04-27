8ab47c58a82d045ea4161a552ed970de6ab34bd2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-27 15:19:40 +0000

    updated by-teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityQuestionnaireDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityQuestionnaireDAO.java
@@ -35,9 +35,15 @@ public class ActivityQuestionnaireDAO extends GenericDaoHibernate<ActivityQuesti
 	}
 	
 	public List<Object[]> getQuestionnareForScopeId(Long scopeId){
+		    
+		    Query query = getSession().createQuery(" select distinct model.activityQuestion.activityQuestionId , model.activityQuestion.question from ActivityQuestionnaire model " +
+		              " where model.activityScope.activityScopeId = :scopeId and model.isDeleted = 'N' ");
+		    query.setParameter("scopeId", scopeId);
+		    return query.list();
+		  }
 	    
 	    Query query = getSession().createQuery(" select distinct model.activityQuestion.activityQuestionId , model.activityQuestion.question from ActivityQuestionnaire model " +
-	              " where model.activityScope.activityScopeId = :scopeId and model.parentActivityQuestionnaireId = null and model.isDeleted = 'N' ");
+	              " where model.activityScope.activityScopeId = :scopeId and model.isDeleted = 'N' ");
 	    query.setParameter("scopeId", scopeId);
 	    return query.list();
 	  }
