aa72ed45edc8026fa701aec3891c001214692d21
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-04-29 17:14:33 +0000

    Updated by srishailam

--- payment_gateway/src/main/java/com/itgrids/paymentgateway/service/impl/PaymentGatewayService.java
+++ payment_gateway/src/main/java/com/itgrids/paymentgateway/service/impl/PaymentGatewayService.java
@@ -1,28 +1,60 @@
 package com.itgrids.paymentgateway.service.impl;
 
 import java.util.List;
+import java.util.zip.Adler32;
 
 import org.apache.log4j.Logger;
 
 import com.itgrids.paymentgateway.dao.IPaymentMethodDAO;
-import com.itgrids.paymentgateway.model.PaymentMethod;
+import com.itgrids.paymentgateway.dto.PamentGatewayVO;
 import com.itgrids.paymentgateway.service.IPaymentGatewayService;
+import com.itgrids.paymentgateway.utils.IConstants;
+import com.itgrids.paymentgateway.utils.MD5Algoritm;
 
 public class PaymentGatewayService implements IPaymentGatewayService{
 	
 	private static final Logger LOG = Logger.getLogger(PaymentGatewayService.class);
 	
 	private IPaymentMethodDAO paymentMethodDAO;
-
+	private MD5Algoritm md5Algoritm = new MD5Algoritm();
+	
 	public void setPaymentMethodDAO(IPaymentMethodDAO paymentMethodDAO) {
 		this.paymentMethodDAO = paymentMethodDAO;
+	}
+	
+	public PamentGatewayVO getPaymentBasicInfoByPaymentGateWayType(Long gateWayId,String randomNo,String enrollId){
+		PamentGatewayVO pamentGatewayVO = new PamentGatewayVO();
+		try {
+				String encryptedCodeMN = md5Algoritm.generateMD5Encrypt(randomNo);
+				String encryptedCodeEN = md5Algoritm.generateMD5Encrypt(enrollId);
+				
+				String WorkingKey =IConstants.TGNF_ENROLLMENT_WORKING_KEY;
+		        String MerchantId =IConstants.TGNF_ENROLLMENT_MERCHANT_ID;
+		        String Amount = IConstants.TGNF_ENROLLMENT_AMOUNT;
+		        String redirectUrl = IConstants.TGNF_REGISTRATION_REDIRECTURL+"?mn="+encryptedCodeMN.trim()+"&en="+encryptedCodeEN.trim()+"";
+		        String OrderId =IConstants.TGNF_ENROLLMENT_RANDOMNUMBERCODE+randomNo;
+		        String str = MerchantId + "|" + OrderId + "|" + Amount + "|" + redirectUrl + "|" + WorkingKey;
+		        Adler32  adl = new Adler32();
+		        adl.update(str.getBytes());
+		        //return (Long.valueOf(adl.getValue()).toString());
+
+		        pamentGatewayVO.setCheckSum(Long.valueOf(adl.getValue()).toString());
+		        pamentGatewayVO.setWorkingKey(WorkingKey);
+		        pamentGatewayVO.setAmount(Amount);
+		        pamentGatewayVO.setMerchantId(MerchantId);
+		        pamentGatewayVO.setRedirectURL(redirectUrl);
+		        pamentGatewayVO.setOrderNo(OrderId);
+		        
+		} catch (Exception e) {
+			LOG.error("error occured while generating payment gateway basic details.");
+		}
+		return pamentGatewayVO;
 	}
 	
 	public List<Object[]> getAllPaymentMethodIds()
 	{
 		try{
 			List<Object[]> list = paymentMethodDAO.getAllPaymentMethodIds();
-			
 			if(list != null && list.size() > 0)
 			{
 				System.out.println(list.size());
