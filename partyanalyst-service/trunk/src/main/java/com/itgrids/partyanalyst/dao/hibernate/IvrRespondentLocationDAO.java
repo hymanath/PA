9f20605e83738e07ad9fe6595090c77a0cee89fe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-05 11:58:51 +0000

    updated by srujana

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/IvrRespondentLocationDAO.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.dao.IIvrRespondentLocationDAO;
+import com.itgrids.partyanalyst.model.IvrRespondentLocation;
+import com.itgrids.partyanalyst.model.TdpCadre;
+
+public class IvrRespondentLocationDAO extends
+		GenericDaoHibernate<IvrRespondentLocation, Long> implements
+		IIvrRespondentLocationDAO {
+	public IvrRespondentLocationDAO() {
+		super(IvrRespondentLocation.class);
+	}
+
+}
