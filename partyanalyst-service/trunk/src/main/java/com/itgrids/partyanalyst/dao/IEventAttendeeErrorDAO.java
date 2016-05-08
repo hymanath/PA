1eebdf3857e2ab7cb092576b862386764973b602
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-08 15:18:08 +0000

    get preentry valid and invalid counts   - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventAttendeeErrorDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEventAttendeeErrorDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -8,5 +9,5 @@ import com.itgrids.partyanalyst.model.EventAttendeeError;
 
 public interface IEventAttendeeErrorDAO extends GenericDao<EventAttendeeError, Long>{
 	public List checkEventsyncDataInError(String rfid,String imei,Long eventId,String uniqueKey);
-
+	public Long getPreEntryInvalidCount(Long preEnytryEventId,Date startDate,Date endDate);
 }
