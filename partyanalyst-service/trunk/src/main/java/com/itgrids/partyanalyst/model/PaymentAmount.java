ce77fa14222f072cf275255188f46563704f2411
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-10 13:48:31 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PaymentAmount.java
@@ -0,0 +1,102 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "payment_amount")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PaymentAmount extends BaseModel implements Serializable{
+	
+	private Long paymentAmountId;
+	private Long paymentModuleId;
+	private Long paymentModuleSubTypeId;
+	private String amount;
+	private String isDeleted;
+	private String isActive;
+	
+	private PaymentModule paymentModule;
+	private PaymentModuleSubType paymentModuleSubType;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "payment_amount_id", unique = true, nullable = false)
+	public Long getPaymentAmountId() {
+		return paymentAmountId;
+	}
+	public void setPaymentAmountId(Long paymentAmountId) {
+		this.paymentAmountId = paymentAmountId;
+	}
+	@Column(name = "payment_module_id")
+	public Long getPaymentModuleId() {
+		return paymentModuleId;
+	}
+	public void setPaymentModuleId(Long paymentModuleId) {
+		this.paymentModuleId = paymentModuleId;
+	}
+	@Column(name = "payment_module_sub_type_id")
+	public Long getPaymentModuleSubTypeId() {
+		return paymentModuleSubTypeId;
+	}
+	public void setPaymentModuleSubTypeId(Long paymentModuleSubTypeId) {
+		this.paymentModuleSubTypeId = paymentModuleSubTypeId;
+	}
+	@Column(name = "amount")
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	@Column(name = "is_deleted")
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	@Column(name = "is_active")
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="payment_module_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PaymentModule getPaymentModule() {
+		return paymentModule;
+	}
+	public void setPaymentModule(PaymentModule paymentModule) {
+		this.paymentModule = paymentModule;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="payment_module_sub_type_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PaymentModuleSubType getPaymentModuleSubType() {
+		return paymentModuleSubType;
+	}
+	public void setPaymentModuleSubType(PaymentModuleSubType paymentModuleSubType) {
+		this.paymentModuleSubType = paymentModuleSubType;
+	}
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PaymentMethod.java
@@ -0,0 +1,66 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name="payment_method")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PaymentMethod extends BaseModel  implements Serializable{
+
+	private static final long serialVersionUID = 49424353562240371L;
+	
+	private Long paymentMethodId;
+	private String method;
+	private String isActive;
+	private String isDeleted;
+	
+	//public PaymentMethod(){}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="payment_method_id", unique=true, nullable=false)
+	public Long getPaymentMethodId() {
+		return paymentMethodId;
+	}
+
+	public void setPaymentMethodId(Long paymentMethodId) {
+		this.paymentMethodId = paymentMethodId;
+	}
+
+	@Column(name = "method", length = 10)
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	@Column(name="is_active")
+	public String getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	@Column(name="is_deleted")
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	
+}
