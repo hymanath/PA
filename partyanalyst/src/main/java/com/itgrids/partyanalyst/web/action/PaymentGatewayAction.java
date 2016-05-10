98023f1c2cb1be5d65e4afe1237601f117f851a8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-10 13:57:09 +0000

    Updated By Srishailam

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/PaymentGatewayAction.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.web.action;
+
+import org.apache.log4j.Logger;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PaymentGatewayAction extends ActionSupport{
+	private final static Logger LOG =  Logger.getLogger(PaymentGatewayAction.class);
+}
