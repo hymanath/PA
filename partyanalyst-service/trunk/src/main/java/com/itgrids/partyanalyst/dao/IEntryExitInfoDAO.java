114274f17cf3bf965f1c25da5657825482c35710
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-10 14:08:48 +0000

    Mahanadu DashBoard _ Balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEntryExitInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IEntryExitInfoDAO.java
@@ -5,5 +5,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.EntryExitInfo;
 
 public interface IEntryExitInfoDAO  extends GenericDao<EntryExitInfo, Long>{
+	
+	public EntryExitInfo getEntryDetails(Long parentEventId);
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EntryExitInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EntryExitInfoDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IEntryExitInfoDAO;
 import com.itgrids.partyanalyst.model.EntryExitInfo;
@@ -10,4 +11,14 @@ public class EntryExitInfoDAO  extends GenericDaoHibernate<EntryExitInfo, Long> 
 	{
 		super(EntryExitInfo.class);
 	}
+	
+	public EntryExitInfo getEntryDetails(Long parentEventId){
+		
+		Query query = getSession().createQuery("select model from EntryExitInfo model " +
+				" where model.parentEventId = :parentEventId ");
+		
+		query.setParameter("parentEventId", parentEventId);
+		return (EntryExitInfo) query.uniqueResult();
+	}
+	
 }
