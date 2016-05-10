bd5abe82e4121f43bb7f90f46b404ede57829129
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-10 13:56:22 +0000

    Updated By Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/PaymentGatewayService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IPaymenyGatewayService;
+
+public class PaymentGatewayService implements IPaymenyGatewayService{
+	private final static Logger LOG =  Logger.getLogger(PaymentGatewayService.class);
+	
+}
