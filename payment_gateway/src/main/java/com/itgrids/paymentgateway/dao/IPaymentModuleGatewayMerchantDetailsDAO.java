e91b6326f8d4955c1aa3a87ba581700bf777b6a9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-11 05:32:06 +0000

    commited by Swadhin

--- payment_gateway/src/main/java/com/itgrids/paymentgateway/dao/IPaymentModuleGatewayMerchantDetailsDAO.java
+++ payment_gateway/src/main/java/com/itgrids/paymentgateway/dao/IPaymentModuleGatewayMerchantDetailsDAO.java
@@ -1,9 +1,10 @@
 package com.itgrids.paymentgateway.dao;
 
+import java.util.List;
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.paymentgateway.model.PaymentModuleGatewayMerchantDetails;
 
 public interface IPaymentModuleGatewayMerchantDetailsDAO extends GenericDao<PaymentModuleGatewayMerchantDetails,Long>{
-
+	public List<Object[]> getTransactionMerchantDetailsByRegistrationType(String moduleStr,String subTypeStr);
 }
