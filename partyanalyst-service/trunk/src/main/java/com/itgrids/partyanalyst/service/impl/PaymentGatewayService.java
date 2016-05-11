af259f5e46191e66692c0d4f7f7fe4c785a57290
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-11 13:24:49 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/PaymentGatewayService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/PaymentGatewayService.java
@@ -2,17 +2,33 @@ package com.itgrids.partyanalyst.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Adler32;
 
 import org.apache.log4j.Logger;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.itgrids.partyanalyst.dao.IPaymentAmountDAO;
+import com.itgrids.partyanalyst.dao.IPaymentMethodDAO;
 import com.itgrids.partyanalyst.dao.IPaymentModuleGatewayMerchantDetailsDAO;
+import com.itgrids.partyanalyst.dao.IPaymentTransactionDAO;
 import com.itgrids.partyanalyst.dto.PaymentGatewayVO;
+import com.itgrids.partyanalyst.dto.PaymentTransactionVO;
+import com.itgrids.partyanalyst.model.PaymentMethod;
+import com.itgrids.partyanalyst.model.PaymentTransaction;
 import com.itgrids.partyanalyst.service.IPaymenyGatewayService;
+import com.itgrids.partyanalyst.utils.IConstants;
+import com.itgrids.partyanalyst.utils.MD5Algoritm;
 
 public class PaymentGatewayService implements IPaymenyGatewayService{
-	private final static Logger LOG =  Logger.getLogger(PaymentGatewayService.class);
 	
+	private static final Logger LOG = Logger.getLogger(PaymentGatewayService.class);
+	
+	private IPaymentMethodDAO paymentMethodDAO;
+	private IPaymentTransactionDAO paymentTransactionDAO;
+	private MD5Algoritm md5Algoritm = new MD5Algoritm();
+	private TransactionTemplate transactionTemplate;
 	private IPaymentAmountDAO paymentAmountDAO;
 	private IPaymentModuleGatewayMerchantDetailsDAO paymentModuleGatewayMerchantDetailsDAO;
 	
@@ -30,7 +46,32 @@ public class PaymentGatewayService implements IPaymenyGatewayService{
 	public void setPaymentAmountDAO(IPaymentAmountDAO paymentAmountDAO) {
 		this.paymentAmountDAO = paymentAmountDAO;
 	}
+	public IPaymentMethodDAO getPaymentMethodDAO() {
+		return paymentMethodDAO;
+	}
 
+	public IPaymentTransactionDAO getPaymentTransactionDAO() {
+		return paymentTransactionDAO;
+	}
+
+	public void setPaymentTransactionDAO(
+			IPaymentTransactionDAO paymentTransactionDAO) {
+		this.paymentTransactionDAO = paymentTransactionDAO;
+	}
+
+	public TransactionTemplate getTransactionTemplate() {
+		return transactionTemplate;
+	}
+
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+
+	public void setPaymentMethodDAO(IPaymentMethodDAO paymentMethodDAO) {
+		this.paymentMethodDAO = paymentMethodDAO;
+	}
+	
+	
 	public Long getPaymentAmountByRegistrationType(String moduleStr,String subTypeStr){
 		Long amount = 0l;
 		try {
@@ -67,5 +108,95 @@ public class PaymentGatewayService implements IPaymenyGatewayService{
 			LOG.error("Exception raised in getTransactionMerchantDetailsByRegistrationType  method in PaymentGatewayService.",e);
 		}
 		return returnVo;
+	}
+
+	public PaymentGatewayVO getPaymentBasicInfoByPaymentGateWayType(Long gateWayId,String randomNo,String enrollId){
+		PaymentGatewayVO pamentGatewayVO = new PaymentGatewayVO();
+		try {
+				String encryptedCodeMN = md5Algoritm.generateMD5Encrypt(randomNo);
+				String encryptedCodeEN = md5Algoritm.generateMD5Encrypt(enrollId);
+				
+				String WorkingKey =IConstants.TGNF_ENROLLMENT_WORKING_KEY;
+		        String MerchantId =IConstants.TGNF_ENROLLMENT_MERCHANT_ID;
+		        String Amount = IConstants.TGNF_ENROLLMENT_AMOUNT;
+		        String redirectUrl = IConstants.TGNF_REGISTRATION_REDIRECTURL+"?mn="+encryptedCodeMN.trim()+"&en="+encryptedCodeEN.trim()+"";
+		        String OrderId =IConstants.TGNF_ENROLLMENT_RANDOMNUMBERCODE+randomNo;
+		        String str = MerchantId + "|" + OrderId + "|" + Amount + "|" + redirectUrl + "|" + WorkingKey;
+		        Adler32  adl = new Adler32();
+		        adl.update(str.getBytes());
+		        //return (Long.valueOf(adl.getValue()).toString());
+
+		        pamentGatewayVO.setCheckSum(Long.valueOf(adl.getValue()).toString());
+		        pamentGatewayVO.setWorkingKey(WorkingKey);
+		        pamentGatewayVO.setAmount(Amount);
+		        pamentGatewayVO.setMerchantId(MerchantId);
+		        pamentGatewayVO.setRedirectURL(redirectUrl);
+		        pamentGatewayVO.setOrderNo(OrderId);
+		        
+		} catch (Exception e) {
+			LOG.error("error occured while generating payment gateway basic details.");
+		}
+		return pamentGatewayVO;
+	}
+	
+	public List<Object[]> getAllPaymentMethodIds()
+	{
+		try{
+			List<Object[]> list = paymentMethodDAO.getAllPaymentMethodIds();
+			if(list != null && list.size() > 0)
+			{
+				System.out.println(list.size());
+				return list;
+			}
+		}catch(Exception e)
+		{
+			LOG.error(e);
+		}
+		return null;
+	}
+	
+	public Long addPaymentMethod(String method)
+	{
+		try{
+			PaymentMethod paymentMethod = new PaymentMethod();
+			paymentMethod.setMethod(method);
+			paymentMethod = paymentMethodDAO.save(paymentMethod);
+			return paymentMethod.getPaymentMethodId();
+		}catch(Exception e)
+		{
+			LOG.error(e);
+			return null;
+		}
+	}
+	public String savePaymenyTransactionDetails(final PaymentTransactionVO paymentTransactionVO){
+		try{
+			if(paymentTransactionVO != null)
+			{
+				transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+					public void doInTransactionWithoutResult(TransactionStatus status) {
+						PaymentTransaction paymentTransaction = new PaymentTransaction();
+						paymentTransaction.setPaymentModuleGatewayMerchantDetailsId(paymentTransactionVO.getPaymentModuleGatewayMerchantDetailsId());
+						paymentTransaction.setPaymentGatewayId(paymentTransactionVO.getPaymentGatewayId());
+						paymentTransaction.setPaymentMethodId(paymentTransactionVO.getPaymentMethodId());
+						paymentTransaction.setTransactionId(paymentTransactionVO.getTransactionId());
+						paymentTransaction.setTransactionStatusId(paymentTransactionVO.getTransactionStatusId());
+						paymentTransaction.setTransactionTime(paymentTransactionVO.getTransactionTime());
+						paymentTransaction.setUuid(paymentTransactionVO.getUuid());
+						paymentTransaction.setAmount(paymentTransactionVO.getAmount());
+						paymentTransaction.setIpAddress(paymentTransactionVO.getIpAddress());
+						paymentTransaction.setStatusCode(paymentTransactionVO.getStatusCode());
+						paymentTransaction.setPreUrl(paymentTransactionVO.getPreUrl());
+						paymentTransaction.setPostUrl(paymentTransactionVO.getPostUrl());
+						paymentTransaction.setRedirectUrl(paymentTransactionVO.getRedirectUrl());
+						paymentTransaction.setReferenceUserId(paymentTransactionVO.getReferenceUserId());
+						paymentTransaction.setPaymentModuleId(paymentTransactionVO.getPaymentModuleId());
+						paymentTransactionDAO.save(paymentTransaction);
+					}});
+				return "success";
+			}
+		}catch(Exception e){
+			LOG.error("error occured while saving payment transaction details.");
+		}
+		return "failure";
 	}
 }
