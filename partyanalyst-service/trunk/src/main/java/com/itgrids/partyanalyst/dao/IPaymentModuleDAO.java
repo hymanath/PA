183c86e8d96c502c7900c25adc6f521e90e11693
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-11 06:03:24 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPaymentModuleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPaymentModuleDAO.java
@@ -2,7 +2,7 @@ package com.itgrids.partyanalyst.dao;
 
 import org.appfuse.dao.GenericDao;
 
-import com.itgrids.paymentgateway.model.PaymentModule;
+import com.itgrids.partyanalyst.model.PaymentModule;
 
 public interface IPaymentModuleDAO extends GenericDao<PaymentModule,Long>{
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPaymentModuleSubTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPaymentModuleSubTypeDAO.java
@@ -2,7 +2,7 @@ package com.itgrids.partyanalyst.dao;
 
 import org.appfuse.dao.GenericDao;
 
-import com.itgrids.paymentgateway.model.PaymentModuleSubType;
+import com.itgrids.partyanalyst.model.PaymentModuleSubType;
 
 public interface IPaymentModuleSubTypeDAO extends GenericDao<PaymentModuleSubType,Long>{
 }
