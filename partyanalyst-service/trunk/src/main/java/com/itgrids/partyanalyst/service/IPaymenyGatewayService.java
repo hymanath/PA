6db38ed28e7d3c1323a2dffe4f9f67cbc0b18cb8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-12 12:19:11 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPaymenyGatewayService.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.service;
-
-import com.itgrids.partyanalyst.dto.PaymentGatewayVO;
-
-public interface IPaymenyGatewayService {
-	
-	public Long getPaymentAmountByRegistrationType(String moduleStr,String subTypeStr);
-	public PaymentGatewayVO getTransactionMerchantDetailsByRegistrationType(String moduleStr,String subTypeStr);
-}
