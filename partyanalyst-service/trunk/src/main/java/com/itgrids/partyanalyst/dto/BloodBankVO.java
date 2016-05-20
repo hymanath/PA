a7ddd420323a18296ffcf51b599381014119a5b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-20 09:32:23 +0000

    vo class added by-swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BloodBankVO.java
@@ -0,0 +1,94 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class BloodBankVO implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private String father;
+	private String sex;
+	private Long age;
+	private String dob;
+	private String married;
+	private String mobile;
+	private String email;
+	private String education;
+	private String occupation;
+	private String address;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFather() {
+		return father;
+	}
+	public void setFather(String fatherName) {
+		this.father = fatherName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getMarried() {
+		return married;
+	}
+	public void setMarried(String married) {
+		this.married = married;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
