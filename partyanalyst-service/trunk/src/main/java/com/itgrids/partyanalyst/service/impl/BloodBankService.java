0e9a33451f5ecd474ce672a67f1c8b0d950ba70b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-20 09:18:07 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/BloodBankService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IBloodBankService;
+
+public class BloodBankService implements IBloodBankService{
+
+	private static final Logger   LOG = Logger.getLogger(BloodBankService.class);
+}
