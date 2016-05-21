f5839a1f9ad7c627f9f6acf7ff61cd750ed4b1c3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-21 14:22:38 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BloodDonorInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BloodDonorInfoDAO.java
@@ -1,6 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IBloodDonorInfoDAO;
 import com.itgrids.partyanalyst.model.BloodDonorInfo;
@@ -10,4 +14,31 @@ public class BloodDonorInfoDAO extends GenericDaoHibernate<BloodDonorInfo, Long>
 	public BloodDonorInfoDAO(){
 		super(BloodDonorInfo.class);
 	}
+	
+public List<Object[]> getBloodDonorCounts(Date campFromDate,Date campToDate ){
+		
+		Query query = getSession().createQuery(" select count(model.tdpCadreId)," +
+				" model.bloodDonation.acceptanceStatusId " +
+				" from BloodDonorInfo model " +
+				" where " +
+				" date(model.insertedTime) between :campFromDate and :campToDate " +
+				" group by model.bloodDonation.acceptanceStatusId ");
+		
+		query.setDate("campFromDate",campFromDate);
+		query.setDate("campToDate",campToDate);
+		return query.list();
+	}
+public List<Object[]> getBloodDonorDayWiseCounts(Date campFromDate,Date campToDate ){
+	
+	Query query = getSession().createQuery(" select count(model.tdpCadreId)," +
+			" date(model.insertedTime)," +
+			" model.bloodDonation.acceptanceStatusId " +
+			" from BloodDonorInfo model " +
+			" date(model.insertedTime) between :campFromDate and :campToDate " +
+			" group by date(model.insertedTime),model.bloodDonation.acceptanceStatusId ");
+	
+	query.setDate("campFromDate",campFromDate);
+	query.setDate("campToDate",campToDate);
+	return query.list();
+   }
 }
