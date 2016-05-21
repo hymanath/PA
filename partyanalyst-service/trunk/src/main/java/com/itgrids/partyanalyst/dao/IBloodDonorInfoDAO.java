c3deec6b7d287435ea4be776f6365c9295db3e5f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-21 14:22:38 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBloodDonorInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBloodDonorInfoDAO.java
@@ -1,9 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.BloodDonorInfo;
 
 public interface IBloodDonorInfoDAO extends GenericDao<BloodDonorInfo, Long> {
+	public List<Object[]> getBloodDonorCounts(Date campFromDate,Date campToDate );
+	public List<Object[]> getBloodDonorDayWiseCounts(Date campFromDate,Date campToDate );
 
 }
