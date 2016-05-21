d289f4594edfc806b2a4c02b5166b8d5f72d44f8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-21 09:40:37 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BloodDonationCampDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BloodDonationCampDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IBloodDonationCampDAO;
 import com.itgrids.partyanalyst.model.BloodDonationCamp;
@@ -10,5 +11,12 @@ public class BloodDonationCampDAO extends GenericDaoHibernate<BloodDonationCamp,
 	public BloodDonationCampDAO(){
 		super(BloodDonationCamp.class);
 	}
-
+	
+	public Object[] getCampDates(Long campId){
+		Query query = getSession().createQuery(" select model.fromDate,model.toDate " +
+				" from BloodDonationCamp model " +
+				" where model.bloodDonationCampId=:campId ");
+		query.setParameter("campId", campId);
+		return (Object[]) query.uniqueResult();
+	}
 }
