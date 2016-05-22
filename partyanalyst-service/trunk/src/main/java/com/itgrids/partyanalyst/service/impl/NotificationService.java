ee775c50434ee745aeb079b3cb8b224e984bbd1d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-22 15:15:43 +0000

    updated by--teja, srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/NotificationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/NotificationService.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.transaction.TransactionStatus;
@@ -8,11 +11,11 @@ import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.itgrids.partyanalyst.dao.hibernate.NotificationDeviceDAO;
+import com.itgrids.partyanalyst.dao.hibernate.NotificationsDAO;
 import com.itgrids.partyanalyst.dto.NotificationDeviceVO;
-import com.itgrids.partyanalyst.dto.ResultCodeMapper;
-import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.model.NotificationDevice;
 import com.itgrids.partyanalyst.service.INotificationService;
+import com.itgrids.partyanalyst.utils.CommonMethodsUtilService;
 import com.itgrids.partyanalyst.utils.DateUtilService;
 
 public class NotificationService implements INotificationService{
@@ -20,7 +23,28 @@ public class NotificationService implements INotificationService{
 	private TransactionTemplate transactionTemplate = null;
 	private NotificationDeviceDAO notificationDeviceDAO;
 	private DateUtilService dateUtilService;
+	private NotificationsDAO notificationsDAO;
+	private CommonMethodsUtilService commonMethodsUtilService = new CommonMethodsUtilService();
 	
+	
+	
+	public CommonMethodsUtilService getCommonMethodsUtilService() {
+		return commonMethodsUtilService;
+	}
+
+	public void setCommonMethodsUtilService(
+			CommonMethodsUtilService commonMethodsUtilService) {
+		this.commonMethodsUtilService = commonMethodsUtilService;
+	}
+
+	public NotificationsDAO getNotificationsDAO() {
+		return notificationsDAO;
+	}
+
+	public void setNotificationsDAO(NotificationsDAO notificationsDAO) {
+		this.notificationsDAO = notificationsDAO;
+	}
+
 	public DateUtilService getDateUtilService() {
 		return dateUtilService;
 	}
@@ -47,10 +71,10 @@ public class NotificationService implements INotificationService{
 	
 	
 	
-	 public ResultStatus saveUsersDataInNotificationDeviceTable(final NotificationDeviceVO notifyVO)
+	 public List<NotificationDeviceVO> saveUsersDataInNotificationDeviceTable(final NotificationDeviceVO notifyVO)
 	  {
 		 log.info("Entered into saveVotersDataInVoterInfoTable() Method...");
-		  ResultStatus resultStatus = new ResultStatus();
+		 List<NotificationDeviceVO> returnList = null;
 		  try{
 			  transactionTemplate.execute(new TransactionCallbackWithoutResult() {
 					protected void doInTransactionWithoutResult(TransactionStatus status) 
@@ -70,14 +94,49 @@ public class NotificationService implements INotificationService{
 						}
 					}
 				});
-			  resultStatus.setResultCode(ResultCodeMapper.SUCCESS);
-			  return resultStatus;
+			  returnList =  getActiveNotifications(notifyVO.getNotificationTypeId(),notifyVO.getLastNotificationId());
+			  return returnList;
 		  }catch (Exception e) {
 			  e.printStackTrace();
 			  log.error("Exception Occured in saveUsersDataInNotificationDeviceTable() Method, Exception - "+e);
-			  resultStatus.setResultCode(ResultCodeMapper.FAILURE);
-			  return resultStatus;
+			  returnList= new ArrayList<NotificationDeviceVO>(0);
+			  returnList.add(new NotificationDeviceVO("FAILURE"));
+			  return null;
 		}
 	  }
+	 public List<NotificationDeviceVO> getActiveNotifications(Long notificationTypeId,Long lastNotificationId){
+		 List<NotificationDeviceVO>  returnList = null;
+		 try {
+			 log.error("Entered in to getActiveNotifications() Method ");
+			 Map<Long, NotificationDeviceVO> statusMap = new LinkedHashMap<Long, NotificationDeviceVO>(0);
+			 List<Object[]> notifyDetails = notificationsDAO.getNotificationsDetailsByNotification(notificationTypeId,lastNotificationId);
+			 if(notifyDetails != null && notifyDetails.size()>0){
+				 for (Object[] status : notifyDetails) {
+					 NotificationDeviceVO notificationTypeVO= statusMap.get(commonMethodsUtilService.getLongValueForObject(status[0]));
+					 if(notificationTypeVO == null)
+					 {
+						 notificationTypeVO = new NotificationDeviceVO();
+						 notificationTypeVO.setNotificationType(commonMethodsUtilService.getStringValueForObject(status[1])); // New status[1]
+						 notificationTypeVO.setNotificationTypeId(commonMethodsUtilService.getLongValueForObject(status[0]));
+					 }
+					 NotificationDeviceVO notificationVo = new NotificationDeviceVO();
+					 notificationVo.setId(commonMethodsUtilService.getLongValueForObject(status[2]));
+					 notificationVo.setNotification(commonMethodsUtilService.getStringValueForObject(status[3]));
+					 notificationTypeVO.getActiveNotofocations().add(notificationVo) ;
+					 statusMap.put((Long)status[0], notificationTypeVO);
+				}
+				 
+				 if(commonMethodsUtilService.isMapValid(statusMap)){
+					 returnList= new ArrayList<NotificationDeviceVO>(0);
+					 returnList.addAll(statusMap.values());
+				 }
+			 }
+		 }catch(Exception e){
+			 log.error("Exception occured in getActiveNotifications() Method ");
+		 }
+		return returnList;
+		 
+	 }
+
 }
 
