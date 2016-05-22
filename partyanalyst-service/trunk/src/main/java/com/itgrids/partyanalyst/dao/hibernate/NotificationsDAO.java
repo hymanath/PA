14ad2d5b22072ef9ccfee3d8fa284894724cc111
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-22 15:14:43 +0000

    updated by--teja, srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NotificationsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NotificationsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.INotificationsDAO;
 import com.itgrids.partyanalyst.model.Notifications;
@@ -9,5 +12,18 @@ public class NotificationsDAO extends GenericDaoHibernate<Notifications,Long> im
 	
 	public NotificationsDAO() {
 		super(Notifications.class);
+	}
+	
+	public List<Object[]> getNotificationsDetailsByNotification (Long notificationTypeId,Long lastNotificationId){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append("select distinct  model.notificationTypeId,model2.notificationType, model.notificationsId, model.notification ");
+		queryStr.append(" from Notifications model,NotificationType model2 where model.isActive='true' ");
+		if(notificationTypeId != null && notificationTypeId.longValue()>0L)
+			queryStr.append(" and model.notificationTypeId=:notificationTypeId ");
+		if(lastNotificationId != null && lastNotificationId.longValue()>0L)
+			queryStr.append(" and model.notificationsId > :notificationTypeId ");
+		queryStr.append(" order by model.notificationTypeId,model.orderNo ");
+		Query query = getSession().createQuery(queryStr.toString());
+			return query.list();
 	}
 }
