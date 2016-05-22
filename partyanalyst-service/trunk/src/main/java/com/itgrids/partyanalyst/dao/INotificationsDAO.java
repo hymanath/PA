25fef6095f2cfcf4daddd0b7f2a7b28c72cce8ad
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-22 15:14:32 +0000

    updated by--teja, srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationsDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Notifications;
 
 public interface INotificationsDAO extends GenericDao<Notifications,Long>{
+	public List<Object[]> getNotificationsDetailsByNotification (Long notificationTypeId,Long lastNotificationId);
 }
