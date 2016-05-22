dc4fe6985173200aebef1f2767374ff2c52eb8a6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-22 15:15:29 +0000

    updated by--teja, srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INotificationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INotificationService.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.NotificationDeviceVO;
-import com.itgrids.partyanalyst.dto.ResultStatus;
 
 
 public interface INotificationService {
-	 public ResultStatus saveUsersDataInNotificationDeviceTable(final NotificationDeviceVO notifyVO);
+	 public List<NotificationDeviceVO> saveUsersDataInNotificationDeviceTable(final NotificationDeviceVO notifyVO);
+	 public List<NotificationDeviceVO> getActiveNotifications(Long notificationTypeId,Long lastNotificationId);
 }
