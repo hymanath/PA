dabbd54a7596fcd12939e567c22e25cc105caa7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-24 11:38:26 +0000

    const wise cadre count in campus -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyDAO.java
@@ -312,4 +312,5 @@ public interface IConstituencyDAO extends GenericDao<Constituency, Long>{
 	 public List<Object[]> getConstituenciesByStateId(Long stateId);
 	 public List<Object[]> getConstituenciesByDistrictIds(List<Long> districtIds);
 	 public List<Object[]> getDefaultConstituencyAndDistrictForAState(Long stateId);
+	 public List<Object[]> getConstituencyIdAndNameByStateForRegion(Long stateId,String type);
 }
