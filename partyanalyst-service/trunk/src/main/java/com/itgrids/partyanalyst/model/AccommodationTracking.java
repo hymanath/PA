cc2f6ed6fbed3103be5f87dedff5fb7d594fca33
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-24 08:01:07 +0000

    updated by--teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AccommodationTracking.java
@@ -0,0 +1,162 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "activity_info_file")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class AccommodationTracking extends BaseModel implements Serializable{
+	
+	private Long accommodationTrackingId;
+	private Long notificationTypeId;
+	private Long locationTypeId;
+	private String locationValue;
+	private String locationName;
+	private String address;
+	private String contactPerson;
+	private String mobileNo;
+	private String longitude;
+	private String latitude;
+	private String isActive;
+	private Date insertedTime;
+	private Date updatedTime;
+	
+	private RegionScopes RegionScopes;
+	private NotificationType notificationType;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "accommodation_tracking_id", nullable = false, unique = true)
+	public Long getAccommodationTrackingId() {
+		return accommodationTrackingId;
+	}
+	public void setAccommodationTrackingId(Long accommodationTrackingId) {
+		this.accommodationTrackingId = accommodationTrackingId;
+	}
+	@Column(name = "notification_type_id")
+	public Long getNotificationTypeId() {
+		return notificationTypeId;
+	}
+	public void setNotificationTypeId(Long notificationTypeId) {
+		this.notificationTypeId = notificationTypeId;
+	}
+	@Column(name = "location_type_id")
+	public Long getLocationTypeId() {
+		return locationTypeId;
+	}
+	public void setLocationTypeId(Long locationTypeId) {
+		this.locationTypeId = locationTypeId;
+	}
+	@Column(name = "location_value")
+	public String getLocationValue() {
+		return locationValue;
+	}
+	public void setLocationValue(String locationValue) {
+		this.locationValue = locationValue;
+	}
+	@Column(name = "location_name")
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	@Column(name = "address")
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Column(name = "contact_person")
+	public String getContactPerson() {
+		return contactPerson;
+	}
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+	@Column(name = "mobile_no")
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	@Column(name = "longitude")
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	@Column(name = "latitude")
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	@Column(name = "is_active")
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	@Column(name = "inserted_time")
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	@Column(name = "updated_time")
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="region_scopes_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public RegionScopes getRegionScopes() {
+		return RegionScopes;
+	}
+	public void setRegionScopes(RegionScopes regionScopes) {
+		RegionScopes = regionScopes;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="notification_type_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public NotificationType getNotificationType() {
+		return notificationType;
+	}
+	public void setNotificationType(NotificationType notificationType) {
+		this.notificationType = notificationType;
+	}
+	
+	
+	
+}
