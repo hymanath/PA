a885940a8a03330550e232acfab5dff2cc4ccf98
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-05-25 12:03:04 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationDeviceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationDeviceDAO.java
@@ -7,6 +7,6 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.NotificationDevice;
 
 public interface INotificationDeviceDAO extends GenericDao<NotificationDevice, Long>{
-	public List<Long> getIsExist(Long registeredId,Long projectId,String imeiNo);
+	public List<Long> getIsExist(Long projectId,String imeiNo);
 	public List<String> getNotificationActiveKeys();
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INotificationTypeDAO.java
@@ -9,4 +9,5 @@ import com.itgrids.partyanalyst.model.NotificationType;
 public interface INotificationTypeDAO extends GenericDao<NotificationType,Long>{
 	public List<Object[]> getNotificationTypes();
 	public List<Object[]> getNotificationType();
+	public List<Object[]> getAllNotificationType();
 }
