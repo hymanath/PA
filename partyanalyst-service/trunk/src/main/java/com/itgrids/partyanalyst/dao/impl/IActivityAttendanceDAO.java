54351d63ec383c5f0776c7d1b712f7305c3ebab1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-06-24 20:45:58 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/impl/IActivityAttendanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/impl/IActivityAttendanceDAO.java
@@ -10,5 +10,7 @@ import com.itgrids.partyanalyst.model.ActivityLocationInfoDates;
 public interface IActivityAttendanceDAO extends
 		GenericDao<ActivityAttendance, Long> {
 	
-	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId);
+	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId,Long activityLevelId);
+	public List<Object[]> getCadreAttendanceDetls(Long tdpCadreId) ;
+	public List<Object[]> getCanditeActivtyAttendanceLocationsDtls(Long tdpCadreId,Long activityLevelId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/impl/IActivityInviteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/impl/IActivityInviteeDAO.java
@@ -8,5 +8,5 @@ import com.itgrids.partyanalyst.model.ActivityInvitee;
 import com.itgrids.partyanalyst.model.ActivityLocationInfoDates;
 
 public interface IActivityInviteeDAO extends GenericDao<ActivityInvitee, Long> {
-	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId);
+	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId,Long activityLevelId);
 }
