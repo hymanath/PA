ecebc151d781b982f5b6d657a76c624d2e482231
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-06-24 20:45:44 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityInviteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityInviteeDAO.java
@@ -15,13 +15,14 @@ public class ActivityInviteeDAO extends
 		super(ActivityInvitee.class);
 
 	}
-	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId){
+	public List<Object[]> getActivityScopeAndLevels(Long tdpCadreId,Long activityLevelId){
 		Query query = getSession().createQuery(" select distinct model.activityScope.activityScopeId,count(distinct model.activityDate)," +
 				" model.activityDate, model.day " +
 				" from ActivityInvitee model where model.tdpCadre.tdpCadreId = :tdpCadreId " +
-				" and model.activityScope.isDeleted = 'N' group by model.tdpCadre.tdpCadreId,model.activityScope.activityScopeId ");
-query.setParameter("tdpCadreId", tdpCadreId);
-return query.list();
+				" and model.activityScope.isDeleted = 'N' and  model.activityScope.activityLevelId=:activityLevelId  group by model.tdpCadre.tdpCadreId,model.activityScope.activityScopeId ");
+				query.setParameter("tdpCadreId", tdpCadreId);
+				query.setParameter("activityLevelId", activityLevelId);
+				return query.list();
 	}
 	
 }
