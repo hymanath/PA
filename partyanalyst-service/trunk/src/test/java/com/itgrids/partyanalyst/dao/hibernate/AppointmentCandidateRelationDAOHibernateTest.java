9061c4c4aa1478d5ce65e64d7dc8d52ddbf193c5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-06-30 19:48:31 +0000

    Updated By Kamalakar Dandu

--- partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentCandidateRelationDAOHibernateTest.java
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AppointmentCandidateRelationDAOHibernateTest.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.appfuse.dao.BaseDaoTestCase;
 
 import com.itgrids.partyanalyst.dao.IAppointmentCandidateRelationDAO;
@@ -9,5 +12,13 @@ public class AppointmentCandidateRelationDAOHibernateTest extends BaseDaoTestCas
 	public void test(){
 		//List<Object[]> list = appointmentCandidateRelationDAO.getAllAppointmentDetails();
 		//System.out.println(list);
+	}
+	
+	public void testGetCandidateAppointmentDetails()
+	{
+		List<Long> list = new ArrayList<Long>(0);
+		list.add(2l);
+		List<Object[]> a = appointmentCandidateRelationDAO.getCandidateAppointmentDetails(list, 8598052l);
+		System.out.println(a.size());
 	}
 }
