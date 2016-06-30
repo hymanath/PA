1b7079d96d5064693e2dc63228378ab184fc1145
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-06-30 13:18:23 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AlertService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AlertService.java
@@ -5,51 +5,243 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+import com.itgrids.partyanalyst.dao.IAlertCandidateDAO;
+import com.itgrids.partyanalyst.dao.IAlertDAO;
 import com.itgrids.partyanalyst.dao.ICandidateDAO;
+import com.itgrids.partyanalyst.dao.IConstituencyDAO;
+import com.itgrids.partyanalyst.dao.IDistrictDAO;
+import com.itgrids.partyanalyst.dao.ILocalElectionBodyDAO;
+import com.itgrids.partyanalyst.dao.IStateDAO;
+import com.itgrids.partyanalyst.dao.ITehsilDAO;
+import com.itgrids.partyanalyst.dao.IUserAddressDAO;
+import com.itgrids.partyanalyst.dto.AlertVO;
 import com.itgrids.partyanalyst.dto.BasicVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.model.Alert;
+import com.itgrids.partyanalyst.model.AlertCandidate;
+import com.itgrids.partyanalyst.model.UserAddress;
 import com.itgrids.partyanalyst.service.IAlertService;
+import com.itgrids.partyanalyst.utils.DateUtilService;
 
 public class AlertService implements IAlertService{
-	private static final Logger LOG = Logger.getLogger(AlertService.class);
-	@Autowired
-	private ICandidateDAO candidateDAO;
-      
-	public ICandidateDAO getCandidateDAO() {
-		return candidateDAO;
-	}
+private TransactionTemplate transactionTemplate = null;
+private IAlertDAO alertDAO;
+private IAlertCandidateDAO alertCandidateDAO; 
+private IUserAddressDAO userAddressDAO;
+private IStateDAO stateDAO;
+private IDistrictDAO districtDAO;
+private ITehsilDAO tehsilDAO;
+private ILocalElectionBodyDAO localElectionBodyDAO;
+private IConstituencyDAO constituencyDAO;
+private static final Logger LOG = Logger.getLogger(AlertService.class);
 
-	public void setCandidateDAO(ICandidateDAO candidateDAO) {
-		this.candidateDAO = candidateDAO;
-	}
+private ICandidateDAO candidateDAO;
+  
 
-	public List<BasicVO> getCandidatesByName(String candidateName){
-    	List<BasicVO> list = new ArrayList<BasicVO>();
-    	 List<Object[]> candidate=null;
-    	 if(candidateName != null && candidateName.length() > 0)
-    		 candidate=candidateDAO.getCandidatesByName(candidateName);
-    	 else
-    		 candidate=candidateDAO.getCandidatesByName();
-    	
-    	 try{
-    		 LOG.info("entered into getCandidatesByName()");
-    	 if(candidate !=null && candidate.size()>0){
-    		 
-    		 for (Object[] objects : candidate) {
-    			BasicVO vo= new BasicVO();
-    			 vo.setId(Long.valueOf(objects[0].toString()));//candidate id
-    			 vo.setName(objects[1].toString());//last name
-    			 list.add(vo);
-			}
-    	 }
-    	 }
-    	 catch (Exception e) {
-    		 e.printStackTrace();
-			LOG.error("Exception ocured in getCandidatesByName()"+e);
+
+
+public IStateDAO getStateDAO() {
+	return stateDAO;
+}
+
+public void setStateDAO(IStateDAO stateDAO) {
+	this.stateDAO = stateDAO;
+}
+
+public IDistrictDAO getDistrictDAO() {
+	return districtDAO;
+}
+
+public void setDistrictDAO(IDistrictDAO districtDAO) {
+	this.districtDAO = districtDAO;
+}
+
+public ITehsilDAO getTehsilDAO() {
+	return tehsilDAO;
+}
+
+public void setTehsilDAO(ITehsilDAO tehsilDAO) {
+	this.tehsilDAO = tehsilDAO;
+}
+
+public ILocalElectionBodyDAO getLocalElectionBodyDAO() {
+	return localElectionBodyDAO;
+}
+
+public void setLocalElectionBodyDAO(ILocalElectionBodyDAO localElectionBodyDAO) {
+	this.localElectionBodyDAO = localElectionBodyDAO;
+}
+
+public ICandidateDAO getCandidateDAO() {
+	return candidateDAO;
+}
+
+public void setCandidateDAO(ICandidateDAO candidateDAO) {
+	this.candidateDAO = candidateDAO;
+}
+
+public IConstituencyDAO getConstituencyDAO() {
+	return constituencyDAO;
+}
+public void setConstituencyDAO(IConstituencyDAO constituencyDAO) {
+	this.constituencyDAO = constituencyDAO;
+}
+public IAlertDAO getAlertDAO() {
+	return alertDAO;
+}
+public void setAlertDAO(IAlertDAO alertDAO) {
+	this.alertDAO = alertDAO;
+}
+
+public IUserAddressDAO getUserAddressDAO() {
+	return userAddressDAO;
+}
+public void setUserAddressDAO(IUserAddressDAO userAddressDAO) {
+	this.userAddressDAO = userAddressDAO;
+}
+public IAlertCandidateDAO getAlertCandidateDAO() {
+	return alertCandidateDAO;
+}
+public void setAlertCandidateDAO(IAlertCandidateDAO alertCandidateDAO) {
+	this.alertCandidateDAO = alertCandidateDAO;
+}
+public TransactionTemplate getTransactionTemplate() {
+	return transactionTemplate;
+}
+public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+	this.transactionTemplate = transactionTemplate;
+}
+public List<BasicVO> getCandidatesByName(String candidateName){
+	List<BasicVO> list = new ArrayList<BasicVO>();
+	 List<Object[]> candidate=null;
+	 if(candidateName != null && candidateName.length() > 0)
+		 candidate=candidateDAO.getCandidatesByName(candidateName);
+	 else
+		 candidate=candidateDAO.getCandidatesByName();
+	
+	 try{
+		 LOG.info("entered into getCandidatesByName()");
+	 if(candidate !=null && candidate.size()>0){
+		 
+		 for (Object[] objects : candidate) {
+			BasicVO vo= new BasicVO();
+			 vo.setId(Long.valueOf(objects[0].toString()));//candidate id
+			 vo.setName(objects[1].toString());//last name
+			 list.add(vo);
 		}
-		return list;
-    }
-    
-    
+	 }
+	 }
+	 catch (Exception e) {
+		 e.printStackTrace();
+		LOG.error("Exception ocured in getCandidatesByName()"+e);
+	}
+	return list;
+}
+
+public String createAlert(final AlertVO inputVO,final Long userId)
+	{
+	String resultStatus = (String) transactionTemplate
+			.execute(new TransactionCallback() {
+				public Object doInTransaction(TransactionStatus status) {
+					String rs = new String();
+					try {
+				 DateUtilService date = new DateUtilService();
+				 Alert alert = new Alert();
+				 
+				 alert.setAlertSeverityId(inputVO.getSeverity());
+				 alert.setAlertTypeId(inputVO.getAlertTypeId());
+				 alert.setImpactLevelId(inputVO.getLocationLevelId());
+				 alert.setImpactLevelValue(inputVO.getLocationValue());
+				 alert.setDescription(inputVO.getDesc());
+				 alert.setCreatedBy(userId);
+				 alert.setUpdatedBy(userId);
+				 alert.setAlertStatusId(1l);
+				 alert.setUserTypeId(inputVO.getUserTypeId());
+				 alert.setCreatedTime(date.getCurrentDateAndTime());
+				 alert.setUpdatedTime(date.getCurrentDateAndTime());
+				 UserAddress userAddress = saveUserAddress(inputVO);
+				 alert.setAddressId(userAddress.getUserAddressId());
+				 alert = alertDAO.save(alert);
+				 
+				 AlertCandidate alertCandidate = new AlertCandidate();
+				 alertCandidate.setAlertId(alert.getAlertId());
+				 alertCandidate.setCandidateId(inputVO.getCandidateId());
+				 alertCandidate.setAlertImpactId(inputVO.getAlertImpactId());
+				 alertCandidateDAO.save(alertCandidate);
+				 rs = "success";
+						
+					}
+					catch (Exception ex) {
+						 rs = "fail";
+						
+						return rs;
+					}
+						return rs;
+				}
+
+			});
+	return resultStatus;
+	}
+
+	public UserAddress saveUserAddress(final AlertVO inputVO)
+	{
+		UserAddress userAddress = new UserAddress();
+		try{
+			
+			if(inputVO.getLocationLevelId().longValue() == 2l)
+			{
+				userAddress.setState(stateDAO.get(inputVO.getStateId()));
+			}
+			else if(inputVO.getLocationLevelId().longValue() == 3l)
+			{
+				userAddress.setState(stateDAO.get(inputVO.getStateId()));
+				userAddress.setDistrict(districtDAO.get(inputVO.getDistrictId()));
+				
+			}
+			
+			else if(inputVO.getLocationLevelId().longValue() == 4l)
+			{
+				userAddress.setState(stateDAO.get(inputVO.getStateId()));
+				userAddress.setDistrict(districtDAO.get(inputVO.getDistrictId()));
+				userAddress.setConstituency(constituencyDAO.get(inputVO.getConstituencyId()));
+			}
+			else if(inputVO.getLocationLevelId().longValue() == 5l || inputVO.getLocationLevelId().longValue() == 7l)
+			{
+				userAddress.setState(stateDAO.get(inputVO.getStateId()));
+				userAddress.setDistrict(districtDAO.get(inputVO.getDistrictId()));
+				userAddress.setConstituency(constituencyDAO.get(inputVO.getConstituencyId()));
+				if(inputVO.getLocationLevelId() ==  5l)
+					userAddress.setTehsil(tehsilDAO.get(inputVO.getTehsilId()));
+				else
+					userAddress.setLocalElectionBody(localElectionBodyDAO.get(inputVO.getTehsilId()));	
+			}
+			
+			else if(inputVO.getLocationLevelId().longValue() == 6l || inputVO.getLocationLevelId().longValue() == 8l)
+			{
+				userAddress.setState(stateDAO.get(inputVO.getStateId()));
+				userAddress.setDistrict(districtDAO.get(inputVO.getDistrictId()));
+				userAddress.setConstituency(constituencyDAO.get(inputVO.getConstituencyId()));
+				if(inputVO.getLocationLevelId() ==  6l)
+				{
+					userAddress.setTehsil(tehsilDAO.get(inputVO.getTehsilId()));
+					userAddress.setPanchayatId(inputVO.getPanchayatId());
+				}
+				else
+				{
+					userAddress.setLocalElectionBody(localElectionBodyDAO.get(inputVO.getTehsilId()));	
+					userAddress.setWard(constituencyDAO.get(inputVO.getPanchayatId()));
+				}
+			}
+			
+			userAddress = userAddressDAO.save(userAddress);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return userAddress;
+	}
 
 }
