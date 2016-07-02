7bb8aee920728222726fcad121107a2f59e7bacf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-02 15:11:35 +0000

    committed by krishna

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
@@ -1,11 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.Alert;
 
 public interface IAlertDAO extends GenericDao<Alert, Long> {
-	public List<Object[]> getLocationLevelWiseAlerts(List<Long> userTypeIds);
+	public List<Object[]> getLocationLevelWiseAlerts(List<Long> userTypeIds,Date startDate,Date endDate);
 
 }
