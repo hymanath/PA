7c473aed5c23933f9fd30fb1e489fc4eb8fb03c1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-03 02:05:49 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActivityScopeDAO.java
@@ -1,13 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IActivityScopeDAO;
+import com.itgrids.partyanalyst.dto.AddressVO;
 import com.itgrids.partyanalyst.model.ActivityScope;
 
 public class ActivityScopeDAO extends GenericDaoHibernate<ActivityScope, Long> implements IActivityScopeDAO{
@@ -68,21 +68,31 @@ public class ActivityScopeDAO extends GenericDaoHibernate<ActivityScope, Long> i
 		return (Object[])query.uniqueResult();
 	}
 	
-	public List<Object[]> getActivityLevelsDetails(Long requiredAtrrIds){
+	public List<Object[]> getActivityLevelsDetails(Long requiredAtrrId){
+		
+		Query query = getSession().createQuery(" select model.activityLevelId, model.activityScopeId, model.activity.activityName "+
+				" from ActivityScope model,ActivityScopeRequiredAttributes model1 " +
+				" where model.activityScopeId = model1.activityScope.activityScopeId " +
+				" and model1.requiredAttribute.requiredAttributeId = :requiredAtrrId " +
+				" and model1.isActive = 'Y' and model.activity.isActive='Y' and model.isDeleted = 'N'");
+		
+		query.setParameter("requiredAtrrId", requiredAtrrId);
+		
 		
 		/*Query query = getSession().createQuery(" select model.activityLevelId," +
 							" count(model.activityLevelId) " +
 							" from ActivityScope model " +
 							" where model.isDeleted = 'N' " +
 							" group by model.activityLevel.activityLevelId ");*/
-		Query query = getSession().createQuery(" select model.activityLevelId," +
+		/*Query query = getSession().createQuery(" select model.activityLevelId," +
 				" count(model.activityLevelId) " +
 				" from ActivityScope model,ActivityScopeRequiredAttributes model1 " +
 				" where model.activityScopeId = model1.activityScope.activityScopeId " +
 				" and model1.requiredAttribute.requiredAttributeId = :requiredAtrrIds " +
 				" and model1.isActive = 'Y' and model1.isActive = 'Y' and model.isDeleted = 'N' " +
 				" group by model.activityLevel.activityLevelId ");
-		query.setParameter("requiredAtrrIds", requiredAtrrIds);
+		query.setParameter("requiredAtrrIds", requiredAtrrIds);*/
+		
 		return query.list();
 	}
 	
@@ -148,5 +158,82 @@ public class ActivityScopeDAO extends GenericDaoHibernate<ActivityScope, Long> i
 		
 		query.setParameter("scopeId", scopeId);
 		return (Long) query.uniqueResult();
+	}
+	
+	public List<Object[]> getActivitiesScopeDetailsByAddress(AddressVO addressVO){
+		StringBuilder queryStr = new StringBuilder();
+		boolean isLocationAvailable =false;
+		queryStr.append(" select model.activityLevelId,count(model.activityLevelId)  from ActivityScope model " +
+							" where ");
+		if(!isLocationAvailable && (addressVO.getPanchaytId() != null && addressVO.getPanchaytId()>0L) || (addressVO.getWardId() != null && addressVO.getWardId()>0L))
+			isLocationAvailable =true;
+		else if(!isLocationAvailable && (addressVO.getTehsilId() != null && addressVO.getTehsilId()>0L) || (addressVO.getLocalElectionBodyId() != null && addressVO.getLocalElectionBodyId()>0L))
+			isLocationAvailable =true;
+		else if(!isLocationAvailable && addressVO.getDistrictId() != null && addressVO.getDistrictId()>0L)
+			isLocationAvailable =true;
+		else if(!isLocationAvailable && addressVO.getStateId() != null && addressVO.getStateId()>0L)
+			isLocationAvailable =true;
+		else if(!isLocationAvailable && addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			isLocationAvailable =true;
+		
+		
+		if(isLocationAvailable)
+			queryStr.append(" ( ");
+		
+		if((addressVO.getPanchaytId() != null && addressVO.getPanchaytId()>0L) || (addressVO.getWardId() != null && addressVO.getWardId()>0L)){
+			queryStr.append(" 	( model.scopeId in (6,8) and model.scopeValue =:villageORWardId)  ");
+		if((addressVO.getTehsilId() != null && addressVO.getTehsilId()>0L) || (addressVO.getLocalElectionBodyId() != null && addressVO.getLocalElectionBodyId()>0L))
+			queryStr.append(" OR ( model.scopeId in (5,7) and model.scopeValue =:mandalORMuncId)  " );
+		if(addressVO.getDistrictId() != null && addressVO.getDistrictId()>0L)
+			queryStr.append(" OR ( model.scopeId = 3 and model.scopeValue =:districtId )  " );
+		if(addressVO.getStateId() != null && addressVO.getStateId()>0L)
+			queryStr.append(" OR ( model.scopeId = 2 and model.scopeValue =:stateId)  ");
+		if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			queryStr.append(" OR ( model.scopeId = 4 and model.scopeValue =:constituencyId)  " );
+		}		
+		else if((addressVO.getTehsilId() != null && addressVO.getTehsilId()>0L) || (addressVO.getLocalElectionBodyId() != null && addressVO.getLocalElectionBodyId()>0L)){
+			queryStr.append("  ( model.scopeId = in (5,7) and model.scopeValue =:mandalORMuncId)  " );
+		if(addressVO.getDistrictId() != null && addressVO.getDistrictId()>0L)
+			queryStr.append(" OR ( model.scopeId = 3 and model.scopeValue =:districtId )  " );
+		if(addressVO.getStateId() != null && addressVO.getStateId()>0L)
+			queryStr.append(" OR ( model.scopeId = 2 and model.scopeValue =:stateId)  ");
+		if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			queryStr.append(" OR ( model.scopeId = 4 and model.scopeValue =:constituencyId)  " );
+		}
+		else if(addressVO.getDistrictId() != null && addressVO.getDistrictId()>0L){
+			queryStr.append("  ( model.scopeId = 3 and model.scopeValue =:districtId )  " );
+		if(addressVO.getStateId() != null && addressVO.getStateId()>0L)
+			queryStr.append(" OR ( model.scopeId = 2 and model.scopeValue =:stateId)  ");
+		if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			queryStr.append(" OR ( model.scopeId = 4 and model.scopeValue =:constituencyId)  " );
+		}
+		else if(addressVO.getStateId() != null && addressVO.getStateId()>0L){
+			queryStr.append(" ( model.scopeId = 2 and model.scopeValue =:stateId)  ");
+		if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			queryStr.append(" OR ( model.scopeId = 4 and model.scopeValue =:constituencyId)  " );
+		}
+		else if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			queryStr.append("  ( model.scopeId = 4 and model.scopeValue =:constituencyId)  " );
+		
+		if(isLocationAvailable)
+			queryStr.append(" ) and  ");
+		
+		queryStr.append("  model.isDeleted = 'N' and model.activity.isActive='Y'  group by model.activityLevel.activityLevelId");
+		Query query = getSession().createQuery(queryStr.toString());
+		
+		if((addressVO.getPanchaytId() != null && addressVO.getPanchaytId()>0L) || (addressVO.getWardId() != null && addressVO.getWardId()>0L))
+			query.setParameter("villageORWardId", (addressVO.getPanchaytId() != null && addressVO.getPanchaytId()>0L) ? addressVO.getPanchaytId() :(addressVO.getWardId() != null && addressVO.getWardId()>0L)?addressVO.getWardId():null );
+		if((addressVO.getTehsilId() != null && addressVO.getTehsilId()>0L) || (addressVO.getLocalElectionBodyId() != null && addressVO.getLocalElectionBodyId()>0L))
+			query.setParameter("mandalORMuncId", addressVO.getTehsilId() != null && addressVO.getTehsilId()>0L ? addressVO.getTehsilId()  : (addressVO.getLocalElectionBodyId() != null && addressVO.getLocalElectionBodyId()>0L) ?addressVO.getLocalElectionBodyId():null);
+		if(addressVO.getDistrictId() != null && addressVO.getDistrictId()>0L){
+			addressVO.setDistrictId(addressVO.getDistrictId() <=10?36L:1L);
+			query.setParameter("districtId", addressVO.getDistrictId());
+		}
+		if(addressVO.getStateId() != null && addressVO.getStateId()>0L)
+			query.setParameter("stateId", addressVO.getStateId());
+		if(addressVO.getConstituencyId() != null && addressVO.getConstituencyId()>0L)
+			query.setParameter("constituencyId",addressVO.getConstituencyId() );
+		
+		return query.list();
 	}
 }
