0265c68a4d3009544d8a682d22f6296a8018e36a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-06 09:43:02 +0000

    Debate bugs - Balu

--- partyanalyst/src/main/webapp/js/debate.js
+++ partyanalyst/src/main/webapp/js/debate.js
@@ -3018,3 +3018,21 @@ function clearFields(type){
 		$(".clearErrCls").html("");
 	}
 }
+function onlyAlphabets(e, t) {
+            try {
+                if (window.event) {
+                    var charCode = window.event.keyCode;
+                }
+                else if (e) {
+                    var charCode = e.which;
+                }
+                else { return true; }
+                if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || (charCode ==32)  || (charCode ==8) || (charCode >36 && charCode <41) || (charCode==46))
+                    return true;
+                else
+                    return false;
+            }
+            catch (err) {
+                alert(err.Description);
+            }
+        }
