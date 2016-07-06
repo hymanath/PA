d3eec1e0441a070be6a99db2da8fe65dbaa7fed8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-06 14:34:31 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDepartmentDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.Department;
+
+public interface IDepartmentDAO extends GenericDao<Department, Long> {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyOfficeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.PartyOffice;
+
+public interface IPartyOfficeDAO extends GenericDao<PartyOffice, Long> {
+
+}
