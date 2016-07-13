6fe10d5f8904d5379f75c83bd2169eb87cdb811d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-13 14:17:17 +0000

    Updated By Hyma

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/DepartmentBoardPositionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/DepartmentBoardPositionDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IDepartmentBoardPositionDAO;
 import com.itgrids.partyanalyst.model.DepartmentBoardPosition;
@@ -11,5 +14,12 @@ public class DepartmentBoardPositionDAO extends GenericDaoHibernate<DepartmentBo
 		super(DepartmentBoardPosition.class);
 		
 	}
-
+	public List<Object[]> getDepartmentBoardPositionsByDeptIdABrdId(Long deapartmentId,Long boardId){
+		Query query = getSession().createQuery(" select  model.position.positionId,model.position.positionName,model.departmentBoardPositionId from DepartmentBoardPosition where " +
+				" model.departments.departmentId =:deapartmentId and model.board.boardId =:boardId and model.isDeleted = 'N' ");
+		
+		query.setParameter("deapartmentId", deapartmentId);
+		query.setParameter("boardId", boardId);
+		return query.list();
+	}
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/NominatedPostProfileService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/NominatedPostProfileService.java
@@ -1,13 +1,174 @@
 package com.itgrids.partyanalyst.service.impl;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
+import com.itgrids.partyanalyst.dao.IBoardLevelDAO;
+import com.itgrids.partyanalyst.dao.IDepartmentBoardDAO;
+import com.itgrids.partyanalyst.dao.IDepartmentBoardPositionDAO;
+import com.itgrids.partyanalyst.dao.IDepartmentDAO;
+import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.service.INominatedPostProfileService;
+import com.itgrids.partyanalyst.utils.CommonMethodsUtilService;
+import com.itgrids.partyanalyst.utils.SetterAndGetterUtilService;
 
 
 public class NominatedPostProfileService implements INominatedPostProfileService{
 
 	private final static Logger LOG =  Logger.getLogger(NominatedPostProfileService.class);
+	private IBoardLevelDAO boardLevelDAO;
+	private SetterAndGetterUtilService setterAndGetterUtilService ;
+	private CommonMethodsUtilService commonMethodsUtilService = new CommonMethodsUtilService();
+	private IDepartmentDAO departmentDAO;
+	private IDepartmentBoardDAO departmentBoardDAO;
+	private IDepartmentBoardPositionDAO departmentBoardPositionDAO;
+	
+	
+	public IDepartmentBoardPositionDAO getDepartmentBoardPositionDAO() {
+		return departmentBoardPositionDAO;
+	}
 
+
+	public void setDepartmentBoardPositionDAO(
+			IDepartmentBoardPositionDAO departmentBoardPositionDAO) {
+		this.departmentBoardPositionDAO = departmentBoardPositionDAO;
+	}
+
+
+	public IDepartmentBoardDAO getDepartmentBoardDAO() {
+		return departmentBoardDAO;
+	}
+
+
+	public void setDepartmentBoardDAO(IDepartmentBoardDAO departmentBoardDAO) {
+		this.departmentBoardDAO = departmentBoardDAO;
+	}
+
+
+	public CommonMethodsUtilService getCommonMethodsUtilService() {
+		return commonMethodsUtilService;
+	}
+
+
+	public void setCommonMethodsUtilService(
+			CommonMethodsUtilService commonMethodsUtilService) {
+		this.commonMethodsUtilService = commonMethodsUtilService;
+	}
+
+
+	public IDepartmentDAO getDepartmentDAO() {
+		return departmentDAO;
+	}
+
+
+	public void setDepartmentDAO(IDepartmentDAO departmentDAO) {
+		this.departmentDAO = departmentDAO;
+	}
+
+
+	public SetterAndGetterUtilService getSetterAndGetterUtilService() {
+		return setterAndGetterUtilService;
+	}
+
+
+	public void setSetterAndGetterUtilService(
+			SetterAndGetterUtilService setterAndGetterUtilService) {
+		this.setterAndGetterUtilService = setterAndGetterUtilService;
+	}
+
+
+	public IBoardLevelDAO getBoardLevelDAO() {
+		return boardLevelDAO;
+	}
+
+
+	public void setBoardLevelDAO(IBoardLevelDAO boardLevelDAO) {
+		this.boardLevelDAO = boardLevelDAO;
+	}
+
+
+	/**
+	 * @Author  Hyma
+	 * @Version NominatedPostProfileService.java  July 13, 2016 05:30:00 PM 
+	 * @return List<IdNameVO>
+	 * description  { Getting All BoardLevels From Database }
+	 */
+	public List<IdNameVO> getBoardLevels(){
+		List<IdNameVO> returnList = new ArrayList<IdNameVO>();
+		try{
+		List<Object[]> list = boardLevelDAO.getBoardLevels();
+		if(commonMethodsUtilService.isListOrSetValid(list)){
+			String[] setterPropertiesList = {"id","name"};
+			returnList = (List<IdNameVO>) setterAndGetterUtilService.setValuesToVO(list, setterPropertiesList, "com.itgrids.partyanalyst.dto.IdNameVO");
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+			LOG.error("Exception Occured in getBoardLevels()", e);
+		}
+		return returnList;
+	}
+	/**
+	 * @Author  Hyma
+	 * @Version NominatedPostProfileService.java  July 13, 2016 05:30:00 PM 
+	 * @return List<IdNameVO>
+	 * description  { Getting All Departments From Database }
+	 */
+	public List<IdNameVO> getDepartments(){
+		List<IdNameVO> returnList = new ArrayList<IdNameVO>();
+		try{
+		List<Object[]> list = departmentDAO.getDepartments();
+		if(commonMethodsUtilService.isListOrSetValid(list)){
+			String[] setterPropertiesList = {"id","name"};
+			returnList = (List<IdNameVO>) setterAndGetterUtilService.setValuesToVO(list, setterPropertiesList, "com.itgrids.partyanalyst.dto.IdNameVO");
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+			LOG.error("Exception Occured in getDepartments()", e);
+		}
+		return returnList;
+	}
+
+	/**
+	 * @Author  Hyma
+	 * @Version NominatedPostProfileService.java  July 13, 2016 05:30:00 PM 
+	 * @return List<IdNameVO>
+	 * description  { Getting All DepartmentBoards From Database }
+	 */
+	public List<IdNameVO> getDepartmentBoard(Long depmtId){
+		List<IdNameVO> returnList = new ArrayList<IdNameVO>();
+		try{
+		List<Object[]> list = departmentBoardDAO.getDepartmentBoardByDeptId(depmtId);
+		if(commonMethodsUtilService.isListOrSetValid(list)){
+			String[] setterPropertiesList = {"id","name","districtid"};
+			returnList = (List<IdNameVO>) setterAndGetterUtilService.setValuesToVO(list, setterPropertiesList, "com.itgrids.partyanalyst.dto.IdNameVO");
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+			LOG.error("Exception Occured in getDepartmentBoard()", e);
+		}
+		return returnList;
+	}
+	/**
+	 * @Author  Hyma
+	 * @Version NominatedPostProfileService.java  July 13, 2016 05:30:00 PM 
+	 * @return List<IdNameVO>
+	 * description  { Getting All Departments From Database }
+	 */
+	public List<IdNameVO> getDepartmentBoardPositions(Long deptId,Long boardId){
+		List<IdNameVO> returnList = new ArrayList<IdNameVO>();
+		try{
+		List<Object[]> list = departmentBoardPositionDAO.getDepartmentBoardPositionsByDeptIdABrdId(deptId,boardId);
+		if(commonMethodsUtilService.isListOrSetValid(list)){
+			String[] setterPropertiesList = {"id","name","districtid"};
+			returnList = (List<IdNameVO>) setterAndGetterUtilService.setValuesToVO(list, setterPropertiesList, "com.itgrids.partyanalyst.dto.IdNameVO");
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+			LOG.error("Exception Occured in getDepartmentBoardPositions()", e);
+		}
+		return returnList;
+	}
 }
