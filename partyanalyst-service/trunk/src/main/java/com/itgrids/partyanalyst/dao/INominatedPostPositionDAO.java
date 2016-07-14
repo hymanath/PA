64be2a9e5e8e1a0ab0b479405e0caf391284f5ec
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-14 12:54:52 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostPositionDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.NominatedPostPosition;
+
+public interface INominatedPostPositionDAO extends GenericDao<NominatedPostPosition, Long>{
+
+}
