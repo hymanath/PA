1b00bd115c72834e44304f488567f64ec7f7e247
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-14 12:31:31 +0000

    Nominated Post updated by--teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/NominatedPostVO.java
@@ -0,0 +1,86 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class NominatedPostVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private Long stateId;
+	private Long districtId;
+	private Long ConstituencyId;
+	private Long panchayatId;
+	private String hno;
+	private String address1;
+	private String address2;
+	private String pincode;
+	private String 	mobileNo;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getStateId() {
+		return stateId;
+	}
+	public void setStateId(Long stateId) {
+		this.stateId = stateId;
+	}
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public Long getConstituencyId() {
+		return ConstituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		ConstituencyId = constituencyId;
+	}
+	public Long getPanchayatId() {
+		return panchayatId;
+	}
+	public void setPanchayatId(Long panchayatId) {
+		this.panchayatId = panchayatId;
+	}
+	public String getHno() {
+		return hno;
+	}
+	public void setHno(String hno) {
+		this.hno = hno;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+}
