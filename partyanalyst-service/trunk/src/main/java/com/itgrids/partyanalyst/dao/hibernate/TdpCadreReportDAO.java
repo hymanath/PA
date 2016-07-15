9e4caba790211fdfe9d045807d6344ad191fc3a9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-15 16:50:30 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreReportDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreReportDAO.java
@@ -16,10 +16,15 @@ public class TdpCadreReportDAO extends
 	
 	public List<Object[]> getCadreReportDetails(Long cadreId){
 		StringBuilder queryStr = new StringBuilder();
-		queryStr.append(" select distinct TCR.reportType, TCR.reportName, TCR.reportPath, TCR.insertedTime from TdpCadreReport TCR where TCR.tdpCadreId = :cadreId order by TCR.reportType");
+		queryStr.append(" select distinct TCR.reportType, TCR.reportName, TCR.reportPath, TCR.insertedTime, tdpCadreReportStatus.status from TdpCadreReport TCR " +
+				" left join TCR.tdpCadreReportStatus tdpCadreReportStatus where TCR.tdpCadreId = :cadreId order by TCR.reportType");   
 		Query query = getSession().createQuery(queryStr.toString());
 		query.setParameter("cadreId", cadreId);
 		return query.list();
 	}
-	
+	/*public List<Object[]> getCadreReportList(Long cadreId){  
+		Query query = getSession().createQuery("select TCR.tdpCadreId, TCR.reportName, TCR.reportPath from TdpCadreReport TCR where TCR.tdpCadreId = :cadreId");
+		query.setParameter("cadreId", cadreId);
+		return query.list();
+	}*/
 }
