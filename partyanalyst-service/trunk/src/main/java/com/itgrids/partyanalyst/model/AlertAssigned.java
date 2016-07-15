0271dde68071fb1176ec42c5b1064a323f65e104
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-15 06:52:16 +0000

    updated by srujana

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AlertAssigned.java
@@ -0,0 +1,113 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "alert_assigned")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class AlertAssigned extends BaseModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Long alertAssignedId;
+	private Alert alert;
+	private Long alertId;
+	private TdpCadre tdpCadre;
+	private Long tdpCadreId;
+	private Date insertedTime;
+	private Date updatedTime;
+	private User user;
+	private Long createdBy;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "alert_assigned_id", unique = true, nullable = false)
+	public Long getAlertAssignedId() {
+		return alertAssignedId;
+	}
+	public void setAlertAssignedId(Long alertAssignedId) {
+		this.alertAssignedId = alertAssignedId;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "alert_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Alert getAlert() {
+		return alert;
+	}
+	public void setAlert(Alert alert) {
+		this.alert = alert;
+	}
+	@Column(name="alert_id")
+	public Long getAlertId() {
+		return alertId;
+	}
+	public void setAlertId(Long alertId) {
+		this.alertId = alertId;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tdp_cadre_id")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadre getTdpCadre() {
+		return tdpCadre;
+	}
+	public void setTdpCadre(TdpCadre tdpCadre) {
+		this.tdpCadre = tdpCadre;
+	}
+	@Column(name="tdp_cadre_id")
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	@Column(name = "inserted_time", length = 10)
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	@Column(name = "updated_time", length = 10)
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "created_by")
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@Column(name="created_by")
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+	
+	
+}
