806fa737c868a014d88b1a297c4caafaba8c1453
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-16 06:14:46 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/NominatedPostMemberDAOHibernateTest.java
@@ -0,0 +1,23 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.INominatedPostMemberDAO;
+
+public class NominatedPostMemberDAOHibernateTest extends BaseDaoTestCase{
+
+	private INominatedPostMemberDAO nominatedPostMemberDAO;
+
+	public INominatedPostMemberDAO getNominatedPostMemberDAO() {
+		return nominatedPostMemberDAO;
+	}
+
+	public void setNominatedPostMemberDAO(
+			INominatedPostMemberDAO nominatedPostMemberDAO) {
+		this.nominatedPostMemberDAO = nominatedPostMemberDAO;
+	}
+	
+	public void test(){
+		
+	}
+}
