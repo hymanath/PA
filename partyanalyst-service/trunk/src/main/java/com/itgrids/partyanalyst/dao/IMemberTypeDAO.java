2301309ed0a9f9a08db766ad99fc4459b3d6e27b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-19 05:05:42 +0000

    commenting code - Balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMemberTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMemberTypeDAO.java
@@ -2,8 +2,6 @@ package com.itgrids.partyanalyst.dao;
 
 import org.appfuse.dao.GenericDao;
 
-import com.itgrids.partyanalyst.model.MemberType;
-
-public interface IMemberTypeDAO extends GenericDao<MemberType, Long>{
+public interface IMemberTypeDAO /*extends GenericDao<MemberType, Long>*/{
 
 }
