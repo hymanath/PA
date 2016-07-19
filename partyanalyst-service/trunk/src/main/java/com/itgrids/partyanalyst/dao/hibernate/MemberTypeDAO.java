adeb621c7d0ab195b0bf962aee23890bb9753923
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-19 06:56:05 +0000

    updated by nagamani

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MemberTypeDAO.java
@@ -0,0 +1,16 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+
+import com.itgrids.partyanalyst.dao.IMemberTypeDAO;
+import com.itgrids.partyanalyst.model.MemberType;
+
+public class MemberTypeDAO extends GenericDaoHibernate<MemberType, Long> implements IMemberTypeDAO{
+
+	public MemberTypeDAO() {
+		super(MemberType.class);
+		// TODO Auto-generated constructor stub
+	}
+
+}
