0478490ba5d272c3087bf63031f6e6ea2c2d44f9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-19 14:42:12 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
@@ -8,5 +8,6 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.NominatedPost;
 
 public interface INominatedPostDAO extends GenericDao<NominatedPost, Long>{
+	public List<Object[]> getTotalAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList);
 	public List<Object[]> getAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList);
 }
