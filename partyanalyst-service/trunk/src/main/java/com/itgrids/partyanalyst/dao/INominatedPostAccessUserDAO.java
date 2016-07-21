ddcb4122668bb2ecbfd2b3b8571ee625608227ee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-21 13:27:58 +0000

    updated by Srishailam(kamal)

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostAccessUserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.dao;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.partyanalyst.model.NominatedPostAccessUser;
-
-public interface INominatedPostAccessUserDAO extends GenericDao<NominatedPostAccessUser, Long> {
-
-}
