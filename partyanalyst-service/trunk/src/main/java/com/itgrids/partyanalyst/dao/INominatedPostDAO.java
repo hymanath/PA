238684aafd579bf0069bcace8d3be0d2f05dc481
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-22 07:23:30 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
@@ -8,8 +8,8 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.NominatedPost;
 
 public interface INominatedPostDAO extends GenericDao<NominatedPost, Long>{
-	public List<Object[]> getTotalAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList);
-	public List<Object[]> getAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList);
+	public List<Object[]> getTotalAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList,Long stateId);
+	public List<Object[]> getAvaiablePostDetails(Long boardLevelId,Date startDate,Date endDate,List<Long> statusList,Long stateId);
 	public List<Object[]> getNominatedPostsByBoardsAndDepts(Long boardLevelId,List<Long> levelValue,List<Long> deptId,List<Long> boardId);
 	public List<Object[]> getDepartmentWiseBoardAndPositionDetails(Long boardLevelId,List<Long> levelValue,List<Long> deptId,List<Long> boardId);
 }
