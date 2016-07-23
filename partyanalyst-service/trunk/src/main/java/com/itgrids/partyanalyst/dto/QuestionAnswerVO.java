606fa6a0959e88b59c40b461263b25acacc6f44b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-23 10:20:47 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/QuestionAnswerVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/QuestionAnswerVO.java
@@ -34,7 +34,25 @@ public class QuestionAnswerVO implements Serializable {
 	private String name;
 	private String candidateName;
 	private Long candidateId;
+	private String startDate;
+	private String endDate;
+
 	
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
 	
 	
 	
