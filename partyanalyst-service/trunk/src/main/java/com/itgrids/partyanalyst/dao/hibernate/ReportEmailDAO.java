ece35d76c82ae93320bec5833cdec4ee17f89cbe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-27 16:45:01 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ReportEmailDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ReportEmailDAO.java
@@ -18,4 +18,19 @@ public class ReportEmailDAO extends GenericDaoHibernate<ReportEmail, Long> imple
 		query.setParameter("emailReportId", emailReportId);
 		return query.list();
 	}
+	public List<Object[]> getDeptList(Long emailReportId){
+		Query query = getSession().createQuery(" select reportEmail.emailReport.emailReportId, reportEmail.userEmail.userEmailId, reportEmail.userEmail.email, userEmailDepartment.departmentId" +
+											   " from ReportEmail reportEmail, UserEmailDepartment userEmailDepartment " +
+											   " where reportEmail.userEmail.userEmailId = userEmailDepartment.userEmail.userEmailId and reportEmail.emailReport.emailReportId = :emailReportId and reportEmail.isEnabled = 'Y' ");
+		query.setParameter("emailReportId", emailReportId);
+		return query.list();
+	}
 }
+/*select RE.email_report_id, UED.user_email_id, UE.email, UED.department_id
+from 
+report_email RE, user_email_department UED, user_email UE
+where 
+RE.user_email_id = UED.user_email_id and UE.user_email_id = UED.user_email_id and RE.is_enabled = 'Y'
+order by 
+RE.user_email_id;
+*/
