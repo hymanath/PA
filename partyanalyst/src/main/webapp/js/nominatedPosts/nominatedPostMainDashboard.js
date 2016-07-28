c46ed318f15668bba7b163a1801869d69e9dab7d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-28 14:01:48 +0000

    In NominatedPost Dashboard Page Showing Status wise count By Sending position id By Hyma

--- partyanalyst/src/main/webapp/js/nominatedPosts/nominatedPostMainDashboard.js
+++ partyanalyst/src/main/webapp/js/nominatedPosts/nominatedPostMainDashboard.js
@@ -27,4 +27,26 @@ $(document).ready(function(){
       }).done(function(result){
     });	
  }
+ function getAllPositions(){
+	var jsObj={}
+      $.ajax({
+         type:'GET',
+         url:'getAllPositionsAction.action',
+         dataType: 'json',
+         data: {task:JSON.stringify(jsObj)}
+      }).done(function(result){
+		  buildPositions(result);
+    }); 
+ }
+ function buildPositions(result){
+	 var str ='';
+	 if(result != null && result.length > 0){
+		  str+='<li role="presentation" class="active"><a href="#departments1" aria-controls="departments1" role="tab" data-toggle="tab">Labour </a></li>';
+		 for(var i in result){
+			str+='<li role="presentation" class="active"><a href="#departments1" aria-controls="departments1" role="tab" data-toggle="tab" attr_positionId="'+result[i].name+'">'+result[i].name+'</a></li>'; 
+		 }
+	 }
+	 $("#positionsDiv").html(str);
+ }
+ getAllPositions();
 	
