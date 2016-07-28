4e18f8c09c3c751a62f0c5df90d9b5fc2e5390d2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-28 06:45:32 +0000

    get loggedin user usertype and access level in core dashboard page - SREEDHAR

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CoreDashboardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CoreDashboardAction.java
@@ -1,12 +1,16 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.itgrids.partyanalyst.dto.UserDataVO;
 import com.itgrids.partyanalyst.service.ICoreDashboardService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -17,21 +21,52 @@ public class CoreDashboardAction extends ActionSupport implements ServletRequest
 	
 	//Instance variables
 	private HttpServletRequest request;
-	
+	private String task;
+	private JSONObject jObj;
+	private List<UserDataVO> userDataVOList;
+	private UserDataVO userDataVO;
 	//Attributes
 	private ICoreDashboardService coreDashboardService;
 	
-	//setters
+	//setters And Getters
 	public void setCoreDashboardService(ICoreDashboardService coreDashboardService) {
 		this.coreDashboardService = coreDashboardService;
 	}
 	
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public JSONObject getjObj() {
+		return jObj;
+	}
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
 	
+	public List<UserDataVO> getUserDataVOList() {
+		return userDataVOList;
+	}
+
+	public void setUserDataVOList(List<UserDataVO> userDataVOList) {
+		this.userDataVOList = userDataVOList;
+	}
+
+	public UserDataVO getUserDataVO() {
+		return userDataVO;
+	}
+
+	public void setUserDataVO(UserDataVO userDataVO) {
+		this.userDataVO = userDataVO;
+	}
+
 	//Implementation method
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 	}
-    
+
 	//business methods
 	public String execute(){
 		try {
@@ -47,4 +82,28 @@ public class CoreDashboardAction extends ActionSupport implements ServletRequest
 		}
 		return Action.SUCCESS;
 	}
+	public String getUserAccessLevelAndValues(){
+		try{
+			LOG.info("Entered into getUserAccessLevelAndValues()  of CoreDashboardAction");
+			jObj = new JSONObject(getTask());
+			userDataVOList = coreDashboardService.getUserAccessLevelAndValues(jObj.getLong("userId"));
+			
+		}catch(Exception e){
+			LOG.error("Exception raised at getUserAccessLevelAndValues() method of AppointmentAction", e);
+		}
+		return Action.SUCCESS;
+	}
+	public String getUserTypeByUserId(){
+		try{
+			LOG.info("Entered into getUserTypeByUserId()  of CoreDashboardAction");
+			jObj = new JSONObject(getTask());
+			userDataVO = coreDashboardService.getUserTypeByUserId(jObj.getLong("userId"));
+			
+		}catch(Exception e){
+			LOG.error("Exception raised at getUserTypeByUserId() method of AppointmentAction", e);
+		}
+		return Action.SUCCESS;
+	}
+	
+	
 }
