82f1854be7724f977cae76c62f58cd511695147d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-07-30 15:13:53 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INominatedPostMainDashboardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INominatedPostMainDashboardService.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.CastePositionVO;
+import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.dto.NominatedPostDashboardVO;
 
@@ -11,5 +12,11 @@ public interface INominatedPostMainDashboardService {
 	 public List<CastePositionVO> getLocationWiseCastePositionCount(Long LocationLevelId);
 	 public List<CastePositionVO> getLocationWiseCasteGroupPositionCount(Long LocationLevelId);
 	 public List<IdNameVO> getPositions();
-	public NominatedPostDashboardVO getAllPositionWiseStatus(Long positionId);
+	 public NominatedPostDashboardVO getAllPositionWiseStatus(Long positionId);
+	 public List<IdAndNameVO> getCastGroupList();
+	 public List<IdAndNameVO> getApplicationStatusList();
+	 public List<IdAndNameVO> getPositionList();
+	 public List<IdAndNameVO> getLocationLevelList();
+	 public List<IdAndNameVO> getDepartmentList();
+	 public List<IdAndNameVO> getBoardList();
 }
