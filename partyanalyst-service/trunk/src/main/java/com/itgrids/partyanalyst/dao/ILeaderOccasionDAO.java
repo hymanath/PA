641522ed834b0c1fed366fdb61185e7eb2bd6248
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-04 15:03:00 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILeaderOccasionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILeaderOccasionDAO.java
@@ -13,9 +13,6 @@ public interface ILeaderOccasionDAO extends GenericDao<LeaderOccasion, Long>{
 	
 
 	public List<Object[]> getLeaderOccasionDetailsForTaday(String searchTypeStr,Long betweenDatesCount,Long occastionTypeId);
-	public Long getTotalBirthDays(Date fromDate,Date toDate);
-	public Long getTotalWishedBirthDays(Date fromDate,Date toDate);
-	//public List<Long> getLeaderBirthDayWishingDetails(Date fromDate, Date toDate);
-	//public List<LeaderOccasionWishDetails> getWishingDetails(Long searchId,String year);
-	//public List<Object[]> getLeaderOccasionDetailsForTaday1(List<Integer> months,List<Integer> days,Long occastionTypeId);
+	public Long getLeaderOccasionCountDetailsForTaday(String searchTypeStr,Long betweenDatesCount,Long occastionTypeId);
+	public List<Object[]> getleaderOccasionWishDetails();
 }
