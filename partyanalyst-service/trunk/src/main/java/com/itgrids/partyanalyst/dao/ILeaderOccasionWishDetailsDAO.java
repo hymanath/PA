8d9ec0bf684b800de15d7fb3e08a47a3cf0432d4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-04 15:03:00 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILeaderOccasionWishDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ILeaderOccasionWishDetailsDAO.java
@@ -1,10 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.LeaderOccasionWishDetails;
 import com.itgrids.partyanalyst.model.OccasionType;
 
 public interface ILeaderOccasionWishDetailsDAO extends GenericDao< LeaderOccasionWishDetails, Long>{
+	public List<Object[]> getTotalDaysCountsForWishedCount(List<Long> totalIds,String year) ;
+	public LeaderOccasionWishDetails getLeaderOccassiobnWishngDetails(Long searchId,String year);
 
 }
