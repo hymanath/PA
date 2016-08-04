93eddd047a73187d67748e0b5d082601c20257cc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-04 15:01:49 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBirthDayDetailsService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IBirthDayDetailsService.java
@@ -1,10 +1,11 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.BirthDayDetailsVO;
 
 public interface IBirthDayDetailsService {
 
-	public BirthDayDetailsVO getLeaderOccasionDetails(Long occastionTypeId);
+	public List<BirthDayDetailsVO> getLeaderOccasionDetails(Long occastionTypeId,String dataBuildTypeStr,String memberTypeStr);
 	public String getWishingDetails(Long searchId);
-	public BirthDayDetailsVO getAllLstOfDaysForBdayDtails(String searchType,Long occastionTypeId);
 }
