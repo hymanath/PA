6c7ee32ede40711dd0e42cc7a7712cd190991748
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-04 15:02:40 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LeaderOccasionWishDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/LeaderOccasionWishDetailsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ILeaderOccasionWishDetailsDAO;
 import com.itgrids.partyanalyst.model.LeaderOccasionWishDetails;
@@ -10,7 +13,25 @@ public class LeaderOccasionWishDetailsDAO extends GenericDaoHibernate<LeaderOcca
 
 	public LeaderOccasionWishDetailsDAO() {
 		super(LeaderOccasionWishDetails.class);
-		// TODO Auto-generated constructor stub
 	}
-
+	
+public List<Object[]> getTotalDaysCountsForWishedCount(List<Long> totalIds,String year) {
+		
+	Query query = getSession().createQuery( "select distinct model.leaderOccasionId,model.leaderOccasion.tdpCadreId from  LeaderOccasionWishDetails model " +
+			" where model.isdeleted='false' " +
+			" and model.leaderOccasion.tdpCadreId in (:totalIds) and model.year=:year " );
+		
+		query.setParameterList("totalIds", totalIds);
+		query.setParameter("year", year);
+		return  query.list();
+	}
+public LeaderOccasionWishDetails getLeaderOccassiobnWishngDetails(Long searchId,String year){
+	
+	Query query = getSession().createQuery( "select model from  LeaderOccasionWishDetails model " +
+			" where model.isdeleted='false' " +
+			" and model.leaderOccasionId=:searchId and model.year=:year " );
+		query.setParameter("searchId", searchId);
+		query.setParameter("year", year);
+		return (LeaderOccasionWishDetails)query.uniqueResult();
+	}
 }
