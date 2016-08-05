bc5185992f6fb2ff2caca508b38e3d9e4ad33601
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-05 09:39:39 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VolunteersCadreDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VolunteersCadreDetailsDAO.java
@@ -1,7 +1,13 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
 import com.itgrids.partyanalyst.dao.IVolunteersCadreDetailsDAO;
+import com.itgrids.partyanalyst.model.Party;
+import com.itgrids.partyanalyst.model.VolunteersCadreDetails;
 
-public class VolunteersCadreDetailsDAO implements IVolunteersCadreDetailsDAO{
-
+public class VolunteersCadreDetailsDAO extends GenericDaoHibernate<VolunteersCadreDetails, Long> implements IVolunteersCadreDetailsDAO{
+	public VolunteersCadreDetailsDAO(){
+		super(VolunteersCadreDetails.class);
+	}
 }
