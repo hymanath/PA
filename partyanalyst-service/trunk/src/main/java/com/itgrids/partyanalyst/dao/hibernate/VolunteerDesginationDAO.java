bf7d222e33cda644eaef9a81ca54c3a9f4d6998b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-05 09:38:54 +0000

    updated by sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VolunteerDesginationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/VolunteerDesginationDAO.java
@@ -1,7 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
 import com.itgrids.partyanalyst.dao.IVolunteerDesginationDAO;
+import com.itgrids.partyanalyst.model.VolunteerDesgination;
 
-public class VolunteerDesginationDAO implements IVolunteerDesginationDAO{
-
+public class VolunteerDesginationDAO extends GenericDaoHibernate<VolunteerDesgination, Long> implements IVolunteerDesginationDAO{
+	public VolunteerDesginationDAO(){
+		super(VolunteerDesgination.class);
+	}
 }
