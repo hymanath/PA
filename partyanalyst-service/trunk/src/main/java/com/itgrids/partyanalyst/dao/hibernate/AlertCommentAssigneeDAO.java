9d2404ce83f3769a2d0d9e5f5ebc79b6ef40a6c7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-09 15:12:41 +0000

    updated by nagamani

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCommentAssigneeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCommentAssigneeDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAlertCommentAssigneeDAO;
 
@@ -12,5 +15,19 @@ public class AlertCommentAssigneeDAO extends
 	public AlertCommentAssigneeDAO() {
 		super(AlertCommentAssignee.class);
 	}
+	
+	public List<Object[]> getAlertCommentAssignedCandidates(Long alertId)
+	{
+		StringBuilder str = new StringBuilder();
+		str.append("select distinct model.alertCommentId,model.assignTdpCadre.tdpCadreId, model.assignTdpCadre.firstname"+
+				" from AlertCommentAssignee model where model.alertComment.alert.isDeleted ='N'  ");
+		if(alertId != null && alertId > 0)
+			str.append(" and  model.alertComment.alert.alertId = :alertId");
+		Query query = getSession().createQuery(str.toString());
+		if(alertId != null && alertId > 0)
+		query.setParameter("alertId", alertId);
+		return query.list();
+	}
+
 
 }
