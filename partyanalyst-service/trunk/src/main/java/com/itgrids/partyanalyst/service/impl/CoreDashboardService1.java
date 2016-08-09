e6d49b3489175f34f72a36e2e19eb0f5214d24e1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-09 09:56:11 +0000

    coredashboard1 implemented --- Aravind

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardService1.java
@@ -0,0 +1,21 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICoreDashboardService1;
+
+public class CoreDashboardService1 implements ICoreDashboardService1{
+	
+	private final static Logger LOG = Logger.getLogger(CoreDashboardService1.class);
+	
+	//Attributes
+	
+	
+	//setters
+	
+	
+	
+	//business methods.
+	
+	
+}
