e3e9f6f4ee4295547cc4e1ce7638008d5421930e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-16 12:05:43 +0000

    GO saving -- sandeep

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtOrderDocumentsDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.GovtOrder;
+import com.itgrids.partyanalyst.model.GovtOrderDocuments;
+
+public interface IGovtOrderDocumentsDAO extends GenericDao<GovtOrderDocuments, Long>{
+
+}
