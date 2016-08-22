5bc8b249466a338ff7a2dc334d49ad661ff43c67
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-22 16:20:04 +0000

    user type wise counts top 5 strong or top 5 poor in new core dashboard page approch 2- SREEDHAR

--- partyanalyst/src/main/webapp/newCoreDashBoard/js/newCoreDashboard.js
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/newCoreDashboard.js
@@ -631,17 +631,55 @@ function getDistrictWiseCommitteesCountReport(){
        var startDateString = '01/01/2014';
 	   var endDateString = '11/08/2016';
 		
-		var jsObj ={userAccessLevelId:globalUserAccessLevelId,
-					userAccessLevelValuesArray:globalUserAccessLevelValues,
-					state:state,
-					basicCommitteeIdsArray : basicCommitteeIdsArray,
-					startDateString : startDateString,
- 			        endDateString :   endDateString
+		var jsObj ={
+			          userAccessLevelId:globalUserAccessLevelId,
+					  userAccessLevelValuesArray:globalUserAccessLevelValues,
+					  state:state,
+					  basicCommitteeIdsArray : basicCommitteeIdsArray,
+					  startDateString : startDateString,
+ 			          endDateString :   endDateString,
+					  userId :1,
+					  activityMemberId : 0,
+					  userTypeId : globalUserTypeId
 					}
 		
 		$.ajax({
 			type : 'POST',
 			url : 'getUserTypeWiseCommitteesCompletedCountsAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			
+			
+		});
+	}
+	function getUserTypeWiseCommitteesCompletedCounts1(){
+		
+		var state ='AP';
+		var basicCommitteeIdsArray= [];
+       basicCommitteeIdsArray.push(1);
+       basicCommitteeIdsArray.push(2);
+       basicCommitteeIdsArray.push(3);
+      
+	   
+       var startDateString = '01/01/2014';
+	   var endDateString = '11/08/2016';
+		
+		var jsObj ={
+			          userAccessLevelId:globalUserAccessLevelId,
+					  userAccessLevelValuesArray:globalUserAccessLevelValues,
+					  state:state,
+					  basicCommitteeIdsArray : basicCommitteeIdsArray,
+					  startDateString : startDateString,
+ 			          endDateString :   endDateString,
+					  userId :1,
+					  activityMemberId : 0,
+					  userTypeId : globalUserTypeId
+					}
+		
+		$.ajax({
+			type : 'POST',
+			url : 'getUserTypeWiseCommitteesCompletedCountsAction1.action',
 			dataType : 'json',
 			data : {task:JSON.stringify(jsObj)}
 		}).done(function(result){
