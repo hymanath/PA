026f5fde5b02360d71e74714c4179ea40d8ae0bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-24 09:46:22 +0000

    Tdpcadre health report DAO_updated by nandini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreHealthReportDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.DashboardComment;
+import com.itgrids.partyanalyst.model.TdpCadreHealthReport;
+
+public interface ITdpCadreHealthReportDAO extends GenericDao<TdpCadreHealthReport, Long>{
+
+}
