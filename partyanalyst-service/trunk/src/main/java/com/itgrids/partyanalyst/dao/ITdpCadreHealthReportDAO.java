9e0a2f7f9edf6b4593e590fa3f8f39b5e678c4d5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-25 14:13:02 +0000

    updated by srujana

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreHealthReportDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreHealthReportDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
-import com.itgrids.partyanalyst.model.DashboardComment;
 import com.itgrids.partyanalyst.model.TdpCadreHealthReport;
 
 public interface ITdpCadreHealthReportDAO extends GenericDao<TdpCadreHealthReport, Long>{
+	public List<Object[]> getCadreHealthReport(Long tdpCadreId);
 
 }
