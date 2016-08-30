8ae54104784b5f43866bbb72e8dc0f08aaa587e9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-08-30 11:43:00 +0000

    updated by -- SRAVANTH

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NominatedPostDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/NominatedPostDAO.java
@@ -493,7 +493,8 @@ public class NominatedPostDAO extends GenericDaoHibernate<NominatedPost, Long> i
 		    queryStr.append(" select distinct model.nominatedPostMember.nominatedPostPosition.departments.departmentId," +
 		    		       " model.nominatedPostMember.nominatedPostPosition.departments.deptName from NominatedPost " +
 		    		       " model where model.nominatedPostMember.nominatedPostPosition.isDeleted='N' " +
-		    		       " and model.nominatedPostStatus.nominatedPostStatusId=1 and model.nominationPostCandidateId is null ");
+		    		       //" and model.nominatedPostStatus.nominatedPostStatusId=1" +
+		    		       " and model.nominationPostCandidateId is null ");
 		   
 		    if(postType != null && postType.longValue() > 0)
 		          queryStr.append(" and model.nominatedPostMember.nominatedPostPosition.departments.postType.postTypeId=:postTypeId ");
@@ -601,7 +602,8 @@ public class NominatedPostDAO extends GenericDaoHibernate<NominatedPost, Long> i
 		    queryStr.append(" select distinct model.nominatedPostMember.nominatedPostPosition.position.positionId," +
 		    		        " model.nominatedPostMember.nominatedPostPosition.position.positionName from NominatedPost " +
 		    		        " model where model.nominatedPostMember.nominatedPostPosition.isDeleted='N'  " +
-		    		        "  and model.nominatedPostStatus.nominatedPostStatusId=1 and model.nominationPostCandidateId is null  "); 
+		    		        //"  and model.nominatedPostStatus.nominatedPostStatusId=1" +
+		    		        " and model.nominationPostCandidateId is null  "); 
 		    
 		       if(departmentId != null && departmentId.longValue()> 0L){
 			      queryStr.append(" and model.nominatedPostMember.nominatedPostPosition.departments.departmentId =:deapartmentId ");	
