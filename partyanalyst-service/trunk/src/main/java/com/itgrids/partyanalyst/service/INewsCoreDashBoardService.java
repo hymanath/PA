3a99ef1c3850913aab93eaaccd662aed1a58b129
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-09-10 16:31:24 +0000

    news module in core dashboard -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsCoreDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsCoreDashBoardService.java
@@ -8,4 +8,5 @@ import com.itgrids.partyanalyst.dto.UserTypeVO;
 public interface INewsCoreDashBoardService {
 	public List<List<UserTypeVO>> getUserTypeWiseNewsCounts(Long userId,Long activityMemberId,Long userTypeId,String state,String fromDate,String toDate,Long benefitId);
 	public List<ChildUserTypeVO> getPartyComparisonChildUserTypeMembers(Long parentActivityMemberId,Long childUserTypeId,String state,String startDate,String endDate);
+	public List<ChildUserTypeVO> getPartyCompareSubLevelMemberDetails(Long activityMemberId,Long userTypeId,String state,String startDate,String endDate);
 }
