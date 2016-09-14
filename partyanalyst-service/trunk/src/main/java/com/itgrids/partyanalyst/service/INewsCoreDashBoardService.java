ed96bd8e2b6d6b828e13a0d1a32944f9edd97069
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-09-14 12:07:06 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsCoreDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/INewsCoreDashBoardService.java
@@ -1,12 +1,15 @@
 package com.itgrids.partyanalyst.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.itgrids.partyanalyst.dto.ChildUserTypeVO;
+import com.itgrids.partyanalyst.dto.CoreDashBoardVO;
 import com.itgrids.partyanalyst.dto.UserTypeVO;
 
 public interface INewsCoreDashBoardService {
 	public List<List<UserTypeVO>> getUserTypeWiseNewsCounts(Long userId,Long activityMemberId,Long userTypeId,String state,String fromDate,String toDate,Long benefitId,List<Long> npIds);
 	public List<ChildUserTypeVO> getPartyComparisonChildUserTypeMembers(Long parentActivityMemberId,Long childUserTypeId,String state,String startDate,String endDate,List<Long> npIdsList);
 	public List<ChildUserTypeVO> getPartyCompareSubLevelMemberDetails(Long activityMemberId,Long userTypeId,String state,String startDate,String endDate,List<Long> npIds);
+	public List<ChildUserTypeVO> getCommanPartyComparisonChildUserTypeMembers(Map<Long,UserTypeVO> childActivityMembersMap,List<CoreDashBoardVO> wsResultList,String output);
 }
