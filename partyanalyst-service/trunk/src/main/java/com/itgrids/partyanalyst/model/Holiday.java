b9608993bb3b3cb274fe41c961bb1476ac2ed581
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-09-26 11:42:23 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/Holiday.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/Holiday.java
@@ -18,6 +18,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 public class Holiday extends BaseModel implements Serializable {
 	private Long holidayId;
 	private Date date;
+	private String remarks;
+	private String isHoliday;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "holiday_id", unique = true, nullable = false)
@@ -34,6 +36,21 @@ public class Holiday extends BaseModel implements Serializable {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	@Column(name = "remarks")
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	@Column(name = "is_holiday")
+	public String getIsHoliday() {
+		return isHoliday;  
+	}
+	public void setIsHoliday(String isHoliday) {
+		this.isHoliday = isHoliday;
+	}
+	
 	
 	
 }
