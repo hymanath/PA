4a6d0ec9be3ce79340e4d2759a288b7fb8c5e111
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-09-27 15:08:49 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostFinalDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostFinalDAO.java
@@ -23,9 +23,9 @@ public interface INominatedPostFinalDAO extends GenericDao<NominatedPostFinal, L
 	public List<Object[]> getCasteCategoryGroupWiseCountsForPosition(Long positionId,Long levelId,Long deptId,Long boardId,Long casteGroupId,Long applStatusId,String type,Long stateId);
 	public List<Object[]> getApplicationStatusList();
 	public List<Long> getNominatedPostApplicationIdsByMemberOfFinalReview(Long memberId);
-	public List<Object[]> getCandidateCasteList(Long locationLevelId,Long stateId);
+	public List<Object[]> getCandidateCasteList(Long locationLevelId,Long stateId,Long positionId);
 	public List<Object[]> getLocationWiseCastePositionCount(Long LocationLevelId,Long positionId,Long stateId);
-	public List<Object[]> getCasteGroup(Long locationLevelId,Long stateId);
+	public List<Object[]> getCasteGroup(Long locationLevelId,Long stateId,Long positionId);
 	public List<Object[]> getLocationWiseCasteGroupPositionCount(Long LocationLevelId,Long positionId,Long stateId);
 	public List<Object[]> getCasteWisePositionsCountsByPosition(Long positionId,Long levelId,Long deptId,Long boardId,Long casteGroupId,Long applStatusId,Long casteId,Long stateId);
 	public List<Object[]> getPositionCountForGender(Long positionId,Long boardLevelId,Long deptId,Long boardId,Long castegroupId,Long positionStatusId,Long stateId,Long districtId,String type);
