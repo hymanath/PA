99b852ec0189a573f23babf3f9cfb8a2ab56116d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-01 15:06:55 +0000

    updated by- SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardCadreRegistrationService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.CadreRegistratedCountVO;
+
+public interface ICoreDashboardCadreRegistrationService {
+	
+	public CadreRegistratedCountVO showCadreRegistreredCount();
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardCadreRegistrationService.java
@@ -0,0 +1,43 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.dto.CadreRegistratedCountVO;
+import com.itgrids.partyanalyst.service.ICoreDashboardCadreRegistrationService;
+import com.itgrids.partyanalyst.utils.IConstants;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+
+public class CoreDashboardCadreRegistrationService implements ICoreDashboardCadreRegistrationService {
+		
+	private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistrationService.class);
+	
+	
+	public CadreRegistratedCountVO showCadreRegistreredCount(){
+	    CadreRegistratedCountVO regCountVO = null;
+	    try {
+	      
+	      
+	         ClientConfig clientConfig = new DefaultClientConfig();
+	         
+	         clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+	         Client client = Client.create(clientConfig);
+	       
+	         String webServiceUrl  = IConstants.CADRE_REGISTRATION_URL + "WebService/showCadreRegisteredCount";
+	           
+	         WebResource webResource = client.resource( webServiceUrl );
+	           
+	         regCountVO = webResource.accept("application/json").post(CadreRegistratedCountVO.class);
+	        
+	        
+	    } catch (Exception e) {
+	      LOG.error("Exception raised at showCadreRegistreredCount", e);
+	    }
+	    return regCountVO;
+	  }
+	
+	
+}
