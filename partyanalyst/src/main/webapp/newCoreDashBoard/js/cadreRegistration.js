330bd414c34459650c0d1401627a5fb80c64c294
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-03 13:02:37 +0000

    updated by- SREEDHAR

--- partyanalyst/src/main/webapp/newCoreDashBoard/js/cadreRegistration.js
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/cadreRegistration.js
@@ -276,6 +276,19 @@ $('#genSec').highcharts({
 				str+='<div class="bg_ED pad_15">';
 					str+='<div class="row">';
 						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
+							str+='<p style="font-size: 12px">Total Target</p> <h4 class="cadreCount">75000</span></h4>';
+						str+='</div>';
+						var totalAchievedPercantage = '0';
+						if(result.totalCount != null && result.totalCount > 0){
+			               totalAchievedPercantage =  ((result.totalCount*100)/(75000)).toFixed(1) ;
+		                }
+						str+='<div class="col-md-7 col-xs-12 col-sm-12 pad_left0 m_top10">';
+							str+='<h5 class="text-success">Achieved %<span class="cadreCount1 pull-right">'+totalAchievedPercantage+'</span></h5>';
+						str+='</div>';
+						
+					str+='</div>';
+					str+='<div class="row m_top10">';
+						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
 							str+='<h5 class="text-capital">total</h5>';
 							str+='<h3 class="cadreCount">'+emptyCheck(result.totalCount)+'</h3>';
 						str+='</div>';
@@ -283,14 +296,26 @@ $('#genSec').highcharts({
 							str+='<h4 class="f_16 text-success">Renewal  <span class="pull-right cadreCount f_14">'+emptyCheck(result.renewalCount)+'</span></h4>';
 							str+='<h4 class="f_16" style="color:#F7A423">New  <span class="pull-right cadreCount f_14">'+emptyCheck(result.newCount)+'</span></h4>';
 						str+='</div>';
-						str+='<div id="totalOverAllRegistrationGraph" class="chartLiD" style="height:120px" ></div>';
 					str+='</div>';
 				str+='</div>';
+				str+='<div id="totalOverAllRegistrationGraph" class="chartLiD" style="height:120px" ></div>';
 			str+='</div>';
 			
 			str+='<div class="col-md-6 col-xs-12 col-sm-12">';
 				str+='<div class="bg_ED pad_15">';
 					str+='<div class="row">';
+						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
+							str+='<p style="font-size:12px;">Today Target</p> <h4 class="cadreCount">9000</span></h4>';
+						str+='</div>';
+						var todayAchievedPercantage = '0';
+						if(result.todayTotalCount !=null && result.todayTotalCount > 0){
+			               todayAchievedPercantage =  ((result.todayTotalCount*100)/(9000)).toFixed(1) ;
+		                }
+						str+='<div class="col-md-7 col-xs-12 col-sm-12 pad_left0 m_top10">';
+							str+='<h5 class="text-success">Achieved %<span class="cadreCount1 pull-right">'+todayAchievedPercantage+'</span></h5>';
+						str+='</div>';
+					str+='</div>';
+					str+='<div class="row m_top10">';
 						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
 							str+='<h5 class="text-capital">today</h5>';
 							str+='<h3 class="cadreCount">'+emptyCheck(result.todayTotalCount)+'</h3>';
@@ -299,9 +324,9 @@ $('#genSec').highcharts({
 							str+='<h4 class="f_16 text-success">Renewal  <span class="pull-right cadreCount f_14">'+emptyCheck(result.todayRenewalCount)+'</span></h4>';
 							str+='<h4 class="f_16" style="color:#F7A423">New  <span class="pull-right cadreCount f_14">'+emptyCheck(result.todayNewCount)+'</span></h4>';
 						str+='</div>';
-						str+='<div id="todayOverAllRegistrationGraph" class="chartLiD" style="height:120px" ></div>';
 					str+='</div>';
 				str+='</div>';
+				str+='<div id="todayOverAllRegistrationGraph" class="chartLiD" style="height:120px" ></div>';
 			str+='</div>';
 		str+='</div>';
 		
