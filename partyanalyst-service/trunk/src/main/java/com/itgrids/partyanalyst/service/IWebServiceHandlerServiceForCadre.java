e82c5e3b01a10b9f6f052702934db8ff1937805a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-12 14:09:20 +0000

    web services for cadre tab online - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IWebServiceHandlerServiceForCadre.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IWebServiceHandlerServiceForCadre.java
@@ -2,7 +2,11 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
+import com.itgrids.partyanalyst.dto.CadreRegistrationVO;
 import com.itgrids.partyanalyst.dto.IdAndNameVO;
+import com.itgrids.partyanalyst.dto.TdpCadreVO;
+import com.itgrids.partyanalyst.dto.VoterSearchVO;
+import com.itgrids.partyanalyst.dto.WebServiceCadreVO;
 
 public interface IWebServiceHandlerServiceForCadre {
 	
@@ -11,4 +15,7 @@ public interface IWebServiceHandlerServiceForCadre {
 	public List<IdAndNameVO> getConstitencyWiseTehsil(Long consistuencyId);
 	public List<IdAndNameVO> getAllPanchayatsForMandal(Long mandalOrLocalElectionBodyId);
 	public List<IdAndNameVO> getAllBoothsForPanchayat(Long panchayatId);
+	public List<VoterSearchVO> getVotersBySearch(WebServiceCadreVO inputVO);
+	public CadreRegistrationVO getRegistrationPersonDetails(WebServiceCadreVO inputVO);
+	public List<TdpCadreVO> getTdpCadresBySearch(WebServiceCadreVO inputVO);
 }
