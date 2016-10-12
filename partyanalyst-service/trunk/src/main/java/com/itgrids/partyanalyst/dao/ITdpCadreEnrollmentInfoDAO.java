fd4acbf2b1585c2fcf8c42ef5b06894806cb8af4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-12 11:46:05 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreEnrollmentInfoDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreEnrollmentInfo;
+
+public interface ITdpCadreEnrollmentInfoDAO extends GenericDao<TdpCadreEnrollmentInfo, Long>{
+}
