8662e8a7f82671af41711096b9646bdac798ef75
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-13 10:56:45 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TabUserEnrollmentInfoDAO.java
@@ -0,0 +1,37 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.Date;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.ITabUserEnrollmentInfoDAO;
+import com.itgrids.partyanalyst.model.TabUserEnrollmentInfo;
+
+public class TabUserEnrollmentInfoDAO extends GenericDaoHibernate<TabUserEnrollmentInfo, Long> implements ITabUserEnrollmentInfoDAO {
+	public TabUserEnrollmentInfoDAO() {
+		super(TabUserEnrollmentInfo.class);
+	}
+	public Long getTotalTabUserWorkingInField(Date surveyTime, Long stateId){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select count(distinct TUEI.tabUserInfoId) from TabUserEnrollmentInfo TUEI where " +
+						" date(TUEI.surveyTime) = (:surveyTime) and " +
+						" TUEI.stateId = :stateId ");
+		Query query = getSession().createQuery(queryStr.toString());
+		query.setDate("surveyTime", surveyTime);
+		query.setParameter("stateId", stateId);
+		return (Long) query.uniqueResult(); 
+	}//SELECT count(distinct TUEI.tab_user_info_id) from tab_user_enrollment_info TUEI where date(TUEI.survey_time) = '2016-10-13' and TUEI.state_id = 1;
+	public Long getTotalRecordSubmitedByTabUser(Date surveyTime, Long stateId){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select sum(TUEI.totalRecords) from TabUserEnrollmentInfo TUEI where " +
+						" date(TUEI.surveyTime) = (:surveyTime) and " +
+						" TUEI.stateId = :stateId ");
+		Query query = getSession().createQuery(queryStr.toString());
+		query.setDate("surveyTime", surveyTime);
+		query.setParameter("stateId", stateId);
+		return (Long) query.uniqueResult(); 
+	}//SELECT sum(TUEI.total_records) from tab_user_enrollment_info TUEI where date(TUEI.survey_time) = '2016-10-13' and TUEI.state_id = 1;
+
+}
+
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentInfoDAO.java
@@ -1,6 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreEnrollmentInfoDAO;
 import com.itgrids.partyanalyst.model.TdpCadreEnrollmentInfo;
@@ -11,4 +16,34 @@ public class TdpCadreEnrollmentInfoDAO extends GenericDaoHibernate<TdpCadreEnrol
 	public TdpCadreEnrollmentInfoDAO() {
 		super(TdpCadreEnrollmentInfo.class);
 	}
+	public List<Object[]>  getTotalNewRenewalCadreStateWise(String startDate, String endDate){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select " +
+						" S.state_id as sId, S.state_name as sName,sum(TCEI.total_cadre) as totalCadre, sum(TCEI.new_cadre) as newCadre, sum(TCEI.renewal_cadre) as renewalCadre " +
+						" from tdp_cadre_enrollment_info TCEI, state S "+ 
+						" where TCEI.state_id = S.state_id ");
+		if(startDate != null && startDate.length() >= 10 && endDate != null && endDate.length() >= 10){
+			queryStr.append("and date(TCEI.survey_time) between :startDate and :endDate");
+		}
+		queryStr.append(" group by S.state_id order by TCEI.state_id");
+		SQLQuery query = getSession().createSQLQuery(queryStr.toString())
+				.addScalar("sId", Hibernate.LONG)
+				.addScalar("sName", Hibernate.STRING)
+				.addScalar("totalCadre", Hibernate.LONG)
+				.addScalar("newCadre", Hibernate.LONG)
+				.addScalar("renewalCadre", Hibernate.LONG);
+		if(startDate != null && startDate.length() >= 10 && endDate != null && endDate.length() >= 10){
+			query.setParameter("startDate", startDate);
+			query.setParameter("endDate", endDate);
+		}
+		return query.list();
+	}  
+	public Long getTotalConstituencyForCdrRegStarted(Long stateId){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select count(distinct TCEI.constituencyId) from TdpCadreEnrollmentInfo TCEI where TCEI.stateId = :stateId");
+		Query query = getSession().createQuery(queryStr.toString());
+		query.setParameter("stateId", stateId);
+		return (Long) query.uniqueResult();
+	}//select  count(distinct constituency_id) from tdp_cadre_enrollment_info TCEI where TCEI.state_id = 1;
+	
 }
