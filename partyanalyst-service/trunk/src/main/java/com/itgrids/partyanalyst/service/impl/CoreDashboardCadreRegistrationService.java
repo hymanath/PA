10198af98ae74b7450e938119cdb1f2430901f62
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-13 13:56:18 +0000

    committed by ----------Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardCadreRegistrationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardCadreRegistrationService.java
@@ -724,10 +724,21 @@ private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistratio
 	    return null;    
 	  }
 	
-	
-	 public List<List<UserTypeVO>> getUserTypeWiseTotalCadreRegistrationCount(Long activityMemberId,Long stateId,Long userTypeId,Long userId,String fromDateStr,String toDateStr){
+	/**
+	* @param Long activityMemberId
+	* @param Long stateId
+	* @param Long userTypeId
+	* @param userId
+	* @param fromDateStr
+	* @param toDateStr
+	* @return  List<List<UserTypeVO>>
+	* @author Santosh 
+	* @Description :This Service Method is used to get top5 positive or top5 negative members tdp cadre count. 
+	* @since 13-OCT-2016
+	*/
+   public List<List<UserTypeVO>> getUserTypeWiseTotalCadreRegistrationCount(Long activityMemberId,Long stateId,Long userTypeId,Long userId,String fromDateStr,String toDateStr){
 		 
-		 List<List<UserTypeVO>> resultList = new ArrayList<List<UserTypeVO>>();
+		  List<List<UserTypeVO>> resultList = new ArrayList<List<UserTypeVO>>();
 		  Map<Long,Set<Long>> locationLevelMap = null;
 		  Map<Long,Map<Long,UserTypeVO>> userTypeMapDtls = null;
 		  Map<String,Long> totalCadreTargetMap = new HashMap<String, Long>(0);
@@ -736,9 +747,7 @@ private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistratio
 		  Date toDate=null;
 		  Date fromDate=null;
 		  
-		   try{
-			    
-			   
+		try{
 			   if(fromDateStr != null && fromDateStr.trim().length()>0 && toDateStr!= null && toDateStr.trim().length()>0){
 					 toDate = sdf.parse(toDateStr);
 					 fromDate = sdf.parse(fromDateStr);
@@ -753,65 +762,96 @@ private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistratio
 			     locationLevelMap = activityMemberVO.getLocationLevelIdsMap();
 			     
 			     if(locationLevelMap != null && locationLevelMap.size() > 0){
+			    	 
 					  for(Entry<Long,Set<Long>> entry:locationLevelMap.entrySet()){
+						  
 						List<Object[]> returnObjList = tdpCadreTargetCountDAO.getTotalCadreTargetCountLocationWise(entry.getKey(),new ArrayList<Long>(entry.getValue()),stateId);
-						   if(returnObjList != null && returnObjList.size() > 0){
+						  
+						if(returnObjList != null && returnObjList.size() > 0){
+							
 							   for (Object[] param : returnObjList) {
+								   
 								String locationLevelAndId = entry.getKey()+"-"+param[0].toString();
 								totalCadreTargetMap.put(locationLevelAndId, param[1] != null ? (Long)param[1]:0l);
+								
 							}
-						   }
-					   }  
+						}
+					}  
 				}
 				if(locationLevelMap != null && locationLevelMap.size() > 0){
+					
 					  for(Entry<Long,Set<Long>> entry:locationLevelMap.entrySet()){
+						  
 						List<Object[]> returnObjList = tdpCadreDAO.getTotalCadreCountLocationWise(entry.getKey(),new ArrayList<Long>(entry.getValue()),stateId,fromDate,toDate);
-						   if(returnObjList != null && returnObjList.size() > 0){
+						  
+						if(returnObjList != null && returnObjList.size() > 0){
+							
 							   for (Object[] param : returnObjList) {
+								   
 								 String locationLevelAndId = entry.getKey()+"-"+param[0].toString();
 								 totCadreMap.put(locationLevelAndId, param[1] != null ? (Long)param[1]:0l);
-							}
-						   }
-					   }  
+								 
+							 }
+						}
+					 }  
 				}
 				//setting target Count
 				if(userTypeMapDtls != null && userTypeMapDtls.size() > 0){
+					
 					  for (Entry<Long, Map<Long, UserTypeVO>> entry : userTypeMapDtls.entrySet()) {
+						  
 					      Map<Long,UserTypeVO> userTypeMap = entry.getValue();
+					      
 					      for(UserTypeVO vo:userTypeMap.values()){
+					    	  
 					    	  for(Long locationValueId:vo.getLocationValuesSet()){
+					    		  
 					    		  String key = vo.getLocationLevelId()+"-"+locationValueId;
+					    		  
 					    		  if(totalCadreTargetMap.get(key) != null){
+					    			  
 							    		 vo.setTotalTargetCount(vo.getTotalTargetCount()+totalCadreTargetMap.get(key)); 
 							    	 }
 					    	  }
 					      }
-				}  
+				     }  
 				}
 				//setting cadre  Count
 				if(userTypeMapDtls != null && userTypeMapDtls.size() > 0){
+					
 					  for(Entry<Long, Map<Long, UserTypeVO>> entry : userTypeMapDtls.entrySet()){
+						  
 					      Map<Long,UserTypeVO> userTypeMap = entry.getValue();
 					      for(UserTypeVO vo:userTypeMap.values()){
+					    	  
 					    	   for(Long locationValueId:vo.getLocationValuesSet()){
-					    			 String key = vo.getLocationLevelId()+"-"+locationValueId;   
+					    		   
+					    			 String key = vo.getLocationLevelId()+"-"+locationValueId; 
+					    			 
 					    			 if(totCadreMap.get(key) != null){
+					    				 
 					    	    		 vo.setTotalCadreCount(vo.getTotalCadreCount()+totCadreMap.get(key)); 
 					    	    	 }
 					    	   }
 					      }
-				}	  
+				    }	  
 				}
 				//Calculating percentage
 				if(userTypeMapDtls != null && userTypeMapDtls.size() > 0){
+					
 					  for(Entry<Long, Map<Long, UserTypeVO>> entry : userTypeMapDtls.entrySet()){
+						  
 					      Map<Long,UserTypeVO> userTypeMap = entry.getValue();
+					      
 					      for(UserTypeVO vo:userTypeMap.values()){
-					    	 	vo.setTotalCadreCountPer(calculatePercantage(vo.getTotalCadreCount(),vo.getTotalTargetCount()));  
-							  }
-				}
+					    	  
+					    	 	vo.setTotalCadreCountPer(calculatePercantage(vo.getTotalCadreCount(),vo.getTotalTargetCount()));
+					    	 	
+						}
+				 }
+			  }
 				
-				if(userTypeMapDtls!=null && userTypeMapDtls.size()>0){
+			 if(userTypeMapDtls!=null && userTypeMapDtls.size()>0){
 			        Map<Long,UserTypeVO> orgSecAndSecMap = new LinkedHashMap<Long,UserTypeVO>();
 			        Map<Long,UserTypeVO>  secreteriesMap = null;
 			        if(userTypeMapDtls.containsKey(11l)){
@@ -830,21 +870,21 @@ private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistratio
 			        if(organizingSecreteriesMap!=null && organizingSecreteriesMap.size()>0){
 			        	userTypeMapDtls.put(4l, orgSecAndSecMap); 
 			        }
-			      }
+			   }
 				
-				if(userTypeMapDtls != null && userTypeMapDtls.size() > 0){
+			   if(userTypeMapDtls != null && userTypeMapDtls.size() > 0){
 					  for(Entry<Long, Map<Long, UserTypeVO>> entry:userTypeMapDtls.entrySet()){
 					   Map<Long,UserTypeVO> userTypeMap = entry.getValue();
 					   resultList.add(new ArrayList<UserTypeVO>(userTypeMap.values()));
-				}
+				      }
 				}
 			 	if(resultList != null && resultList.size() > 0){
 					for(List<UserTypeVO> memberList:resultList){
 						Collections.sort(memberList, cadreRegistrationCountPercDesc);
 					}
 				}
-				}
-		   }
+				
+		 }
 		   catch(Exception e){
 			   LOG.error("Exception raised in getUserTypeWiseTotalCadreRegistrationCount() in CadreRegistrationService service", e); 
 		   }
@@ -852,21 +892,19 @@ private final static Logger LOG = Logger.getLogger(CoreDashboardCadreRegistratio
 	   }
 	   public static Comparator<UserTypeVO> cadreRegistrationCountPercDesc = new Comparator<UserTypeVO>() {
 			public int compare(UserTypeVO member2, UserTypeVO member1) {
-
 			Double perc2 = member2.getTotalCadreCountPer();
 			Double perc1 = member1.getTotalCadreCountPer();
 			//descending order of percantages.
 			 return perc1.compareTo(perc2);
 			}
-			}; 
+	  }; 
 	   public Double calculatePercantage(Long subCount,Long totalCount){
 			Double d=0.0d;
 			if(subCount.longValue()>0l && totalCount.longValue()==0l)
 			LOG.error("Sub Count Value is "+subCount+" And Total Count Value  "+totalCount);
-
 			if(totalCount.longValue() > 0l){
 				 d = new BigDecimal(subCount * 100.0/totalCount).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();	 
 			}
 			return d;
-			} 
+	  } 
 }
