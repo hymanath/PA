0d16c7f34c2f43ca8c7378daf10467e2f4ba5fdb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-13 13:33:25 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/GISVisualizationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/GISVisualizationService.java
@@ -2,8 +2,10 @@ package com.itgrids.partyanalyst.service.impl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
@@ -11,6 +13,7 @@ import com.itgrids.partyanalyst.dao.IAssemblyLocalElectionBodyDAO;
 import com.itgrids.partyanalyst.dao.IAssemblyLocalElectionBodyWardDAO;
 import com.itgrids.partyanalyst.dao.IBoothConstituencyElectionDAO;
 import com.itgrids.partyanalyst.dao.IBoothDAO;
+import com.itgrids.partyanalyst.dao.ICadreSurveyUserAssignDetailsDAO;
 import com.itgrids.partyanalyst.dao.IConstituencyDAO;
 import com.itgrids.partyanalyst.dao.IDelimitationConstituencyAssemblyDetailsDAO;
 import com.itgrids.partyanalyst.dao.IDelimitationConstituencyDAO;
@@ -21,6 +24,7 @@ import com.itgrids.partyanalyst.dao.IPanchayatDAO;
 import com.itgrids.partyanalyst.dao.IStateDAO;
 import com.itgrids.partyanalyst.dao.ITdpCadreDAO;
 import com.itgrids.partyanalyst.dao.ITehsilDAO;
+import com.itgrids.partyanalyst.dto.GISUserTrackingVO;
 import com.itgrids.partyanalyst.dto.GISVisualizationBasicVO;
 import com.itgrids.partyanalyst.dto.GISVisualizationDetailsVO;
 import com.itgrids.partyanalyst.dto.GISVisualizationParameterVO;
@@ -54,7 +58,15 @@ public class GISVisualizationService implements IGISVisualizationService{
 	private IPanchayatDAO panchayatDAO;
 	private IRegionServiceData regionServiceDataImp;
 	private ITdpCadreDAO tdpCadreDAO;
+	private ICadreSurveyUserAssignDetailsDAO cadreSurveyUserAssignDetailsDAO;
 	
+	public ICadreSurveyUserAssignDetailsDAO getCadreSurveyUserAssignDetailsDAO() {
+		return cadreSurveyUserAssignDetailsDAO;
+	}
+	public void setCadreSurveyUserAssignDetailsDAO(
+			ICadreSurveyUserAssignDetailsDAO cadreSurveyUserAssignDetailsDAO) {
+		this.cadreSurveyUserAssignDetailsDAO = cadreSurveyUserAssignDetailsDAO;
+	}
 	public IBoothDAO getBoothDAO() {
 		return boothDAO;
 	}
@@ -219,9 +231,8 @@ public class GISVisualizationService implements IGISVisualizationService{
 		GISVisualizationDetailsVO parentLocationVO = new GISVisualizationDetailsVO();
 	  try {
 		  Map<Long,GISVisualizationDetailsVO> locationsMap = null;
-		   //if(inputVO.getChildLocationTypeId().longValue()>0L){
-			   locationsMap =  getLocationDetailsBasedOnParameters(inputVO);
-		  // }
+		   locationsMap =  getLocationDetailsBasedOnParameters(inputVO);
+		   updateUserTrackingDetasil(locationsMap,inputVO);
 		   
 		   List<Object[]> locationDetails = tdpCadreDAO.getLocationsRegistrationsDetails(inputVO);
 		   Long dailyTargetCount = IConstants.DAY_WISE_TARGET_REGISTRATIONS_COUNT;
@@ -251,6 +262,7 @@ public class GISVisualizationService implements IGISVisualizationService{
 		   parentLocationVO.setNotStartedCount(parentLocationVO.getTotalCount()-parentLocationVO.getStartedCount());
 		   parentLocationVO.getLocationsList().addAll(locationsMap.values());
 		   List<GISVisualizationBasicVO> statusList = updateLocationPerformanceStatusDetails(locationsMap);
+
 		   if(commonMethodsUtilService.isListOrSetValid(statusList))
 			   parentLocationVO.getStatusList().addAll(statusList);
 		   
@@ -259,7 +271,103 @@ public class GISVisualizationService implements IGISVisualizationService{
 	}
 	  return parentLocationVO;
 	}
-	
+	public List<GISUserTrackingVO> updateUserTrackingDetasil(Map<Long,GISVisualizationDetailsVO> locationsMap,GISVisualizationParameterVO inputVO){
+		List<GISUserTrackingVO> userTrackingList = new ArrayList<GISUserTrackingVO>(0);
+		try {
+				List<Object[]> assignedUsersList = cadreSurveyUserAssignDetailsDAO.getUserTrackingDetails(inputVO);
+				if(commonMethodsUtilService.isListOrSetValid(assignedUsersList)){
+					for (Object[] param : assignedUsersList) {
+						Long locationId = commonMethodsUtilService.getLongValueForObject(param[0]);
+						GISVisualizationDetailsVO locationVO = locationsMap.get(locationId);
+						if(locationVO != null){
+							GISUserTrackingVO trackingVO = new GISUserTrackingVO();
+							trackingVO.setAllocatedCount(commonMethodsUtilService.getLongValueForObject(param[2]));
+							locationVO.setUserTrackingVO(trackingVO);
+						}
+					}
+				}
+				
+				List<Object[]> trackingList = tdpCadreDAO.getLocationsUserTrackingDetails(inputVO,"all");
+				Set<Long> userIdsList = new HashSet<Long>(0);
+				if(commonMethodsUtilService.isListOrSetValid(trackingList)){
+					for (Object[] param : trackingList) {
+						Long locationId = commonMethodsUtilService.getLongValueForObject(param[0]);
+						GISVisualizationDetailsVO locationVO = locationsMap.get(locationId);
+						if(locationVO != null){
+							GISUserTrackingVO trackingVO = locationVO.getUserTrackingVO();
+							if(trackingVO != null){
+								trackingVO.setRegisteredCount(trackingVO.getRegisteredCount()+commonMethodsUtilService.getLongValueForObject(param[7]));
+								
+								GISUserTrackingVO userVO = new GISUserTrackingVO();
+								userVO.setId(commonMethodsUtilService.getLongValueForObject(param[6]));
+								userVO.setRegisteredCount(commonMethodsUtilService.getLongValueForObject(param[7]));
+								if(!userIdsList.contains(userVO.getId())){
+									trackingVO.getUsersList().add(userVO);
+									userIdsList.add(userVO.getId());
+								
+								}
+							}
+						}
+					}
+				}
+			
+				List<Object[]> LastOneHrTrackingList = tdpCadreDAO.getLocationsUserTrackingDetails(inputVO,"LastOneHr");
+				Set<Long> OneHruserIdsList = new HashSet<Long>(0);
+				if(commonMethodsUtilService.isListOrSetValid(LastOneHrTrackingList)){
+					for (Object[] param : LastOneHrTrackingList) {
+						Long locationId = commonMethodsUtilService.getLongValueForObject(param[0]);
+						GISVisualizationDetailsVO locationVO = locationsMap.get(locationId);
+						if(locationVO != null){
+							GISUserTrackingVO trackingVO = locationVO.getUserTrackingVO();
+							if(trackingVO != null){
+								trackingVO.setLastOneHrCount(trackingVO.getLastOneHrCount()+commonMethodsUtilService.getLongValueForObject(param[7]));
+								
+								GISUserTrackingVO userVO = new GISUserTrackingVO();
+								userVO.setId(commonMethodsUtilService.getLongValueForObject(param[6]));
+								userVO.setLastOneHrCount(commonMethodsUtilService.getLongValueForObject(param[7]));
+								
+								if(!OneHruserIdsList.contains(userVO.getId())){
+									trackingVO.getLastOneHrusersList().add(userVO);
+									OneHruserIdsList.add(userVO.getId());
+									
+								}
+							}
+						}
+					}
+				}
+				
+				for (Long locationId : locationsMap.keySet()) {
+					GISVisualizationDetailsVO locationVO =locationsMap.get(locationId);
+					if(locationVO != null){
+						GISUserTrackingVO trackingVO = locationVO.getUserTrackingVO();
+						if(trackingVO != null){
+							if(commonMethodsUtilService.isListOrSetValid(trackingVO.getUsersList())){
+								try{
+									Float avg1 = (float)(trackingVO.getRegisteredCount()/trackingVO.getUsersList().size());
+									Double avgPerc1 = Math.floor(Double.valueOf(commonMethodsUtilService.roundTo2DigitsFloatValueAsString(avg1)));
+									trackingVO.setAvgOutput(avgPerc1.longValue());
+								} catch (Exception e) {
+									LOG.error("101 : Exception Occured While updating percentage ranges in updateUserTrackingDetasil method of GISVisualizationService ",e);
+								}
+							}
+							if(commonMethodsUtilService.isListOrSetValid(trackingVO.getLastOneHrusersList())){
+								try{
+									Float avg = (float)(trackingVO.getLastOneHrCount()/trackingVO.getLastOneHrusersList().size());
+									Double avgPerc = Math.floor(Double.valueOf(commonMethodsUtilService.roundTo2DigitsFloatValueAsString(avg)));
+									trackingVO.setLastOneHrAvgCount(avgPerc.longValue());
+								} catch (Exception e) {
+									LOG.error("102 :  Exception Occured While updating percentage ranges in updateUserTrackingDetasil method of GISVisualizationService ",e);
+								}
+							}
+						}
+					}
+				}
+				
+		} catch (Exception e) {
+			LOG.error("Exception Occured in updateUserTrackingDetasil Method in GISVisualizationService Class",e);
+		}
+		return userTrackingList;
+	}
 	public List<GISVisualizationBasicVO> updateLocationPerformanceStatusDetails(Map<Long,GISVisualizationDetailsVO> locationsMap){
 		List<GISVisualizationBasicVO> statusList = new ArrayList<GISVisualizationBasicVO>(0);
 		Map<String ,GISVisualizationBasicVO> statusMap = new HashMap<String, GISVisualizationBasicVO>(0);
@@ -281,32 +389,36 @@ public class GISVisualizationService implements IGISVisualizationService{
 			
 			if(commonMethodsUtilService.isMapValid(locationsMap)){
 				for (Long locationId : locationsMap.keySet()) {
-					GISVisualizationDetailsVO vo = locationsMap.get(locationId);
-					if(vo != null){
-						Double perc = Double.valueOf(vo.getPerc());
-						if(perc>=90.0){
-							GISVisualizationBasicVO statusVO = statusMap.get("VERY GOOD".trim());
-							statusVO.setCount(statusVO.getCount()+1);
+					try {
+						GISVisualizationDetailsVO vo = locationsMap.get(locationId);
+						if(vo != null){
+							Double perc = Double.valueOf(vo.getPerc());
+							if(perc>=90.0){
+								GISVisualizationBasicVO statusVO = statusMap.get("VERY GOOD".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}
+							else if(perc>=80.0 && perc<90.0){
+								GISVisualizationBasicVO statusVO = statusMap.get("GOOD".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}
+							else if(perc>=60.0 && perc<80.0){
+								GISVisualizationBasicVO statusVO = statusMap.get("OK".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}
+							else if(perc>=30.0 && perc<60.0){
+								GISVisualizationBasicVO statusVO = statusMap.get("POOR".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}
+							else if(perc>=10.0 && perc<30.0){
+								GISVisualizationBasicVO statusVO = statusMap.get("VERY POOR".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}else{
+								GISVisualizationBasicVO statusVO = statusMap.get("OTHERS".trim());
+								statusVO.setCount(statusVO.getCount()+1);
+							}
 						}
-						else if(perc>=80.0 && perc<90.0){
-							GISVisualizationBasicVO statusVO = statusMap.get("GOOD".trim());
-							statusVO.setCount(statusVO.getCount()+1);
-						}
-						else if(perc>=60.0 && perc<80.0){
-							GISVisualizationBasicVO statusVO = statusMap.get("OK".trim());
-							statusVO.setCount(statusVO.getCount()+1);
-						}
-						else if(perc>=30.0 && perc<60.0){
-							GISVisualizationBasicVO statusVO = statusMap.get("POOR".trim());
-							statusVO.setCount(statusVO.getCount()+1);
-						}
-						else if(perc>=10.0 && perc<30.0){
-							GISVisualizationBasicVO statusVO = statusMap.get("VERY POOR".trim());
-							statusVO.setCount(statusVO.getCount()+1);
-						}else{
-							GISVisualizationBasicVO statusVO = statusMap.get("OTHERS".trim());
-							statusVO.setCount(statusVO.getCount()+1);
-						}
+					} catch (Exception e) {
+						LOG.error("103 :  Exception Occured While updating percentage ranges in updateLocationPerformanceStatusDetails method of GISVisualizationService ",e);
 					}
 				}
 			}
