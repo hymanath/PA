d50ed1813bd15cab6b80b46044d2629be1b97e45
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-14 14:26:59 +0000

    updated by srishailam

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/filter/CORSFilter.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/filter/CORSFilter.java
@@ -1,5 +1,9 @@
 package com.itgrids.partyanalyst.web.filter;
 
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.ext.Provider;
+
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
@@ -10,18 +14,48 @@ import com.sun.jersey.spi.container.ContainerResponseFilter;
  * @date 13th Oct, 2016
  * @description: This filter used to give web service access to other domains (other than www.mytdp.com) 
  */
-public class CORSFilter implements ContainerResponseFilter {
-    
-	public ContainerResponse filter(ContainerRequest request,ContainerResponse response) {
 
-        response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHttpHeaders().add("Access-Control-Allow-Headers","origin, content-type, accept, authorization");
-        response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHttpHeaders().add("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        return response;
-    }
-	
+@Provider
+public class CORSFilter implements ContainerResponseFilter  {//extends OncePerRequestFilter  {
+    	 
+	    public ContainerResponse filter(ContainerRequest req, ContainerResponse crunchifyContainerResponse) {
+	 
+	        ResponseBuilder crunchifyResponseBuilder = Response.fromResponse(crunchifyContainerResponse.getResponse());
+	        
+	        // *(allow from all servers) OR http://crunchify.com/ OR http://example.com/
+	        crunchifyResponseBuilder.header("Access-Control-Allow-Origin", "*")
+	        
+	        // As a part of the response to a request, which HTTP methods can be used during the actual request.
+	        .header("Access-Control-Allow-Methods", "API, CRUNCHIFYGET, GET, POST, PUT, UPDATE, OPTIONS")
+	        
+	        // How long the results of a request can be cached in a result cache.
+	        .header("Access-Control-Max-Age", "151200")
+	        
+	        // As part of the response to a request, which HTTP headers can be used during the actual request.
+	        .header("Access-Control-Allow-Headers", "x-requested-with,Content-Type");
+	 
+	        String crunchifyRequestHeader = req.getHeaderValue("Access-Control-Request-Headers");
+	 
+	        if (null != crunchifyRequestHeader && !crunchifyRequestHeader.equals(null)) {
+	            crunchifyResponseBuilder.header("Access-Control-Allow-Headers", crunchifyRequestHeader);
+	        }
+	 
+	        crunchifyContainerResponse.setResponse(crunchifyResponseBuilder.build());
+	        return crunchifyContainerResponse;
+	    }
+
+	/*protected void doFilterInternal(HttpServletRequest request,	HttpServletResponse response, FilterChain filterChain)throws ServletException, IOException {
+			response.addHeader("Access-Control-Allow-Origin", "*");
+	       // if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+	            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+	            //response.addHeader("Access-Control-Max-Age", "1");
+	        //}
+	        filterChain.doFilter(request, response);
+	}
+	*/
 	/*
+	
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN_VALUE = "*";
 
@@ -46,7 +80,7 @@ public class CORSFilter implements ContainerResponseFilter {
             ACCESS_CONTROL_ALLOW_HEADERS_VALUE,
             ACCESS_CONTROL_ALLOW_METHODS_VALUE
     };
-    @Override
+   
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
         for (int i = 0; i < ALL_HEADERs.length; i++) {
             ArrayList<Object> value = new ArrayList<>();
@@ -55,5 +89,5 @@ public class CORSFilter implements ContainerResponseFilter {
         }
         return response;
     }
-	 * */
+	*/
 }
