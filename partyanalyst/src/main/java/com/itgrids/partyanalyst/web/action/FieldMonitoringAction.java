986afd519197f4015650443a2b6281f75ca61843
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-15 10:35:36 +0000

    UPDATED BY - SREEDHAR

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/FieldMonitoringAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/FieldMonitoringAction.java
@@ -1,10 +1,14 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.service.IFieldMonitoringService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -15,26 +19,87 @@ public class FieldMonitoringAction extends ActionSupport implements ServletReque
 	
 	//instance variables
 		private HttpServletRequest request;
-		
+		private JSONObject jObj;
+		private String task;
+		private List<IdAndNameVO> idAndNameVOList;
 		
 	//Attributes
 	   private IFieldMonitoringService fieldMonitoringService;
 	
-	
-	
+	 //implementation methods
+	   public void setServletRequest(HttpServletRequest request) {
+			this.request = request;
+		}
+	   
 	//setters and getters.
 	   public void setFieldMonitoringService(IFieldMonitoringService fieldMonitoringService) {
 			this.fieldMonitoringService = fieldMonitoringService;
 	   }
-
-	//implementation methods
-	public void setServletRequest(HttpServletRequest request) {
-		this.request = request;
+	   public JSONObject getjObj() {
+		   return jObj;
+	   }
+	   public void setjObj(JSONObject jObj) {
+			this.jObj = jObj;
+	   }
+	   public String getTask() {
+			return task;
+	   }
+	   public void setTask(String task) {
+		  this.task = task;
+	   }
+	public List<IdAndNameVO> getIdAndNameVOList() {
+		return idAndNameVOList;
 	}
 
+	public void setIdAndNameVOList(List<IdAndNameVO> idAndNameVOList) {
+		this.idAndNameVOList = idAndNameVOList;
+	}
 
+	//Business methods
 	public String execute(){
 		return Action.SUCCESS;
+	}
+	
+	
+	public String getVendors(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			Long stateId = jObj.getLong("stateId");
+			idAndNameVOList =fieldMonitoringService.getVendors(stateId);
+		} catch (Exception e) {
+			LOG.error("Exception raised at getVendors()  of FieldMonitoringAction", e);
+		}
+	
+	    return Action.SUCCESS;
+	}
+	
+	public String getVendorDistricts(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			Long stateId = jObj.getLong("stateId");
+			Long vendorId = jObj.getLong("vendorId");
+			idAndNameVOList =fieldMonitoringService.getVendorDistricts(stateId,vendorId);
+		} catch (Exception e) {
+			LOG.error("Exception raised at getVendorDistricts()  of FieldMonitoringAction", e);
+		}
+	
+	    return Action.SUCCESS;
+	}
+	
+	public String getVendorConstituencies(){
+		
+		try {
+			jObj = new JSONObject(getTask());
+			Long vendorId = jObj.getLong("vendorId");
+			Long districtId = jObj.getLong("districtId");
+			idAndNameVOList =fieldMonitoringService.getVendorConstituencies(vendorId,districtId);
+		} catch (Exception e) {
+			LOG.error("Exception raised at getVendorConstituencies()  of FieldMonitoringAction", e);
+		}
+	
+	    return Action.SUCCESS;
 	}
 	
 }
