f5ac104e3da5be3c93644d1e2d65efed24a14041
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-15 10:32:07 +0000

    UPDATED BY - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorLocationDAO.java
@@ -1,9 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.FieldVendorLocation;
 
 public interface IFieldVendorLocationDAO extends GenericDao<FieldVendorLocation, Long>{
-
+	
+	public List<Object[]> getVendors(Long stateId);
+	public List<Object[]> getVendorDistricts(Long stateId,Long vendorId);
+	public List<Object[]> getVendorConstituencies(Long vendorId,Long districtId);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/FieldMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/FieldMonitoringService.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import com.itgrids.partyanalyst.dao.IFieldVendorLocationDAO;
 import com.itgrids.partyanalyst.dao.IFieldVendorTabUserDAO;
+import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.service.IFieldMonitoringService;
 
 public class FieldMonitoringService implements IFieldMonitoringService {
@@ -24,6 +28,70 @@ public class FieldMonitoringService implements IFieldMonitoringService {
 		this.fieldVendorTabUserDAO = fieldVendorTabUserDAO;
 	}
 	
-  
+    public List<IdAndNameVO> getVendors(Long stateId){
+    	List<IdAndNameVO> returnList = null;
+    	try{
+    		
+			List<Object[]> list =   fieldVendorLocationDAO.getVendors(stateId);
+    		
+			returnList = setDataToIdNameVOList(list);
+			
+		}catch(Exception e) {
+			LOG.error("Exception occurred at getVendors() of FieldMonitoringService", e);
+		}
+    	return returnList;
+    }
 	
+    public List<IdAndNameVO> setDataToIdNameVOList(List<Object[]> list){
+    	
+    	List<IdAndNameVO> returnList = null;
+    	if( list != null && list.size() > 0){
+    		
+    		returnList = new ArrayList<IdAndNameVO>();
+    		
+    		for(Object[] obj : list){
+    			IdAndNameVO VO = new IdAndNameVO();
+    			VO.setId(obj[0]!= null ?(Long)obj[0]:0l);
+    			VO.setName(obj[1]!= null ?obj[1].toString():"");
+    			returnList.add(VO);
+    		}
+    	}
+    	return returnList;
+    }
+    
+    public List<IdAndNameVO> getVendorDistricts(Long stateId,Long vendorId){
+    	List<IdAndNameVO> returnList = null;
+    	try{
+    		
+			List<Object[]> list =   fieldVendorLocationDAO.getVendorDistricts(stateId, vendorId);
+    		
+			returnList = setDataToIdNameVOList(list);
+			
+		}catch(Exception e) {
+			LOG.error("Exception occurred at getVendorDistricts() of FieldMonitoringService", e);
+		}
+    	return returnList;
+    }
+    
+    public List<IdAndNameVO> getVendorConstituencies(Long vendorId,Long districtId){
+    	List<IdAndNameVO> returnList = null;
+    	try{
+    		
+			List<Object[]> list =   fieldVendorLocationDAO.getVendorConstituencies(vendorId,districtId);
+    		
+			returnList = setDataToIdNameVOList(list);
+			
+		}catch(Exception e) {
+			LOG.error("Exception occurred at getVendorConstituencies() of FieldMonitoringService", e);
+		}
+    	return returnList;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
