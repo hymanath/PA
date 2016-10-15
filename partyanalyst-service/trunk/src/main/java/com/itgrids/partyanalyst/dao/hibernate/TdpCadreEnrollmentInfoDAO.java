97589fdfb36ac9eef7ba4dfe51b1f99f16db4369
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-15 08:24:13 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentInfoDAO.java
@@ -9,6 +9,7 @@ import org.hibernate.SQLQuery;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreEnrollmentInfoDAO;
 import com.itgrids.partyanalyst.model.TdpCadreEnrollmentInfo;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class TdpCadreEnrollmentInfoDAO extends GenericDaoHibernate<TdpCadreEnrollmentInfo, Long> implements ITdpCadreEnrollmentInfoDAO {
 
@@ -16,12 +17,22 @@ public class TdpCadreEnrollmentInfoDAO extends GenericDaoHibernate<TdpCadreEnrol
 	public TdpCadreEnrollmentInfoDAO() {
 		super(TdpCadreEnrollmentInfo.class);
 	}
-	public List<Object[]>  getTotalNewRenewalCadreStateWise(String startDate, String endDate){
+	public List<Object[]>  getTotalNewRenewalCadreStateWise(Long accessLvlId,List<Long> accessLvlValue,Long stateId,String startDate, String endDate){
 		StringBuilder queryStr = new StringBuilder();
 		queryStr.append(" select " +
 						" S.state_id as sId, S.state_name as sName,sum(TCEI.total_cadre) as totalCadre, sum(TCEI.new_cadre) as newCadre, sum(TCEI.renewal_cadre) as renewalCadre " +
-						" from tdp_cadre_enrollment_info TCEI, state S "+ 
-						" where TCEI.state_id = S.state_id ");
+						" from " +
+						" tdp_cadre_enrollment_info TCEI, " +
+						" user_address UA " +
+						" state S ");
+		if(accessLvlId != null && accessLvlId.longValue()==IConstants.DISTRICT_LEVEl_ACCESS_ID){
+			queryStr.append(" , district D ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.PARLIAMENT_LEVEl_ACCESS_ID){
+			queryStr.append(" , constituency C ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.ASSEMBLY_LEVEl_ACCESS_ID){
+			queryStr.append(" , constituency C ");  
+		}
+		queryStr.append(" where TCEI.state_id = S.state_id ");
 		if(startDate != null && startDate.length() >= 10 && endDate != null && endDate.length() >= 10){
 			queryStr.append("and date(TCEI.survey_time) between :startDate and :endDate");
 		}
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentYearDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreEnrollmentYearDAO.java
@@ -35,6 +35,116 @@ public class TdpCadreEnrollmentYearDAO extends GenericDaoHibernate<TdpCadreEnrol
 		
 		return (Long) query.uniqueResult();
 	}
+	public List<Object[]> getTotalRenewlCadreLocationWise(Long accessLvlId,List<Long> accessLvlValue,Long stateId,Date frmDt, Date toDt){
+		StringBuilder queryStr = new StringBuilder();  
+		queryStr.append(" select ");
+		
+		if(accessLvlId != null && accessLvlId.longValue()==IConstants.STATE_LEVEl_ACCESS_ID){
+	         queryStr.append(" TC.userAddress.state.stateId, count(distinct TC.tdpCadreId)");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.DISTRICT_LEVEl_ACCESS_ID){
+	             queryStr.append(" TC.userAddress.district.districtId, count(distinct TC.tdpCadreId)");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.PARLIAMENT_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.parliamentConstituency.constituencyId, count(distinct TC.tdpCadreId)");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.ASSEMBLY_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.constituency.constituencyId, count(distinct TC.tdpCadreId)");  
+		}
+		queryStr.append(" from  TdpCadreEnrollmentYear TCEY1, TdpCadreEnrollmentYear TCEY2, TdpCadre TC, UserAddress UA where" +
+						" TC.tdpCadreId = TCEY1.tdpCadre.tdpCadreId AND " +
+						" TC.tdpCadreId = TCEY2.tdpCadre.tdpCadreId AND " +
+						" TC.isDeleted = 'N' AND " +
+						" TC.enrollmentYear = 2014 AND " +    
+						" TCEY1.isDeleted = 'N' AND " +
+						" TCEY1.enrollmentYear.enrollmentYearId = 4 AND " +
+						" TCEY2.isDeleted = 'N' AND " +
+						" TCEY2.enrollmentYear.enrollmentYearId = 3 AND ");
+		if(frmDt!= null && toDt!=null){
+			  queryStr.append(" date(TC.surveyTime) between :fromDate and :toDate and ");	 
+	   	}
+		if(accessLvlId != null && accessLvlId.longValue()==IConstants.STATE_LEVEl_ACCESS_ID){
+	         queryStr.append(" TC.userAddress.state.stateId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.DISTRICT_LEVEl_ACCESS_ID){
+	             queryStr.append(" TC.userAddress.district.districtId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.PARLIAMENT_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.parliamentConstituency.constituencyId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.ASSEMBLY_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.constituency.constituencyId in (:accessLvlValue) and  ");  
+		}
+		if(stateId != null && stateId.longValue() > 0){
+			if(stateId.longValue()==1l){
+				queryStr.append(" TC.userAddress.district.districtId > 10 and  model.tdpCadre.userAddress.state.stateId = 1  " );
+			}else if(stateId.longValue()==36l){
+				queryStr.append(" TC.userAddress.district.districtId < 11 ");
+			}
+		}   
+	  
+	   
+		if(accessLvlId != null && accessLvlId.longValue()==IConstants.STATE_LEVEl_ACCESS_ID){
+			queryStr.append(" group by TC.userAddress.state.stateId ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.DISTRICT_LEVEl_ACCESS_ID){
+            queryStr.append(" group by TC.userAddress.district.districtId ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.PARLIAMENT_LEVEl_ACCESS_ID){
+			queryStr.append(" group by TC.userAddress.parliamentConstituency.constituencyId ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.ASSEMBLY_LEVEl_ACCESS_ID){
+			queryStr.append(" group by TC.userAddress.constituency.constituencyId ");  
+		}
+	
+		Query query = getSession().createQuery(queryStr.toString());    
+		if(frmDt!= null && toDt!=null){
+			query.setDate("fromDate", frmDt);
+		  	query.setDate("toDate", toDt);
+   	  	}
+		if(accessLvlId != null){
+			query.setParameterList("accessLvlValue", accessLvlValue);
+		}
+		return query.list();   
+	}
+	public List<Object[]> getTotalRenewlCadreSourceWise(Long accessLvlId,List<Long> accessLvlValue,Long stateId,Date frmDt, Date toDt){
+		StringBuilder queryStr = new StringBuilder();  
+		queryStr.append(" select ");
+		
+		
+	    queryStr.append(" TC.dataSourceType, count(distinct TC.tdpCadreId)");  
+		
+		queryStr.append(" from  TdpCadreEnrollmentYear TCEY1, TdpCadreEnrollmentYear TCEY2, TdpCadre TC, UserAddress UA where" +
+						" TC.tdpCadreId = TCEY1.tdpCadre.tdpCadreId AND " +
+						" TC.tdpCadreId = TCEY2.tdpCadre.tdpCadreId AND " +
+						" TC.isDeleted = 'N' AND " +
+						" TC.enrollmentYear = 2014 AND " +    
+						" TCEY1.isDeleted = 'N' AND " +
+						" TCEY1.enrollmentYear.enrollmentYearId = 4 AND " +
+						" TCEY2.isDeleted = 'N' AND " +
+						" TCEY2.enrollmentYear.enrollmentYearId = 3 AND ");
+		if(frmDt!= null && toDt!=null){
+			  queryStr.append(" date(TC.surveyTime) between :fromDate and :toDate and ");	 
+	   	}
+		if(accessLvlId != null && accessLvlId.longValue()==IConstants.STATE_LEVEl_ACCESS_ID){
+	         queryStr.append(" TC.userAddress.state.stateId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.DISTRICT_LEVEl_ACCESS_ID){
+	             queryStr.append(" TC.userAddress.district.districtId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.PARLIAMENT_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.parliamentConstituency.constituencyId in (:accessLvlValue) and ");  
+		}else if(accessLvlId != null && accessLvlId.longValue()==IConstants.ASSEMBLY_LEVEl_ACCESS_ID){
+	          queryStr.append(" TC.userAddress.constituency.constituencyId in (:accessLvlValue) and  ");  
+		}
+		if(stateId != null && stateId.longValue() > 0){
+			if(stateId.longValue()==1l){
+				queryStr.append(" TC.userAddress.district.districtId > 10 and  model.tdpCadre.userAddress.state.stateId = 1  " );
+			}else if(stateId.longValue()==36l){
+				queryStr.append(" TC.userAddress.district.districtId < 11 ");
+			}
+		}  
+		queryStr.append(" group by TC.dataSourceType ");  
+		
+		Query query = getSession().createQuery(queryStr.toString());    
+		if(frmDt!= null && toDt!=null){
+			query.setDate("fromDate", frmDt);
+		  	query.setDate("toDate", toDt);
+   	  	}
+		if(accessLvlId != null){
+			query.setParameterList("accessLvlValue", accessLvlValue);
+		}
+		return query.list();   
+	}
 	
 	public List<Object[]> getTotalRenewlCadreBasedOnUserType(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Long userType,String levelType,Date fromDate,Date toDate){
 	
