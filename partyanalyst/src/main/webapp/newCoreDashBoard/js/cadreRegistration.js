c5eda4854bd3651bbc3f17626ae269e8c3fe36a5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-17 15:41:44 +0000

    committed by swadhin

--- partyanalyst/src/main/webapp/newCoreDashBoard/js/cadreRegistration.js
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/cadreRegistration.js
@@ -251,7 +251,7 @@ $('#genSec').highcharts({
 
 	function cadreRegistrationBasicCall(globalActivityMemberId){
 		showCadreRegistreredCount(globalActivityMemberId);
-		getEnumeratorsInfo();
+		getEnumeratorsInfo(globalActivityMemberId);
 		getCadreRecentTime();  
 	}
 
@@ -513,237 +513,6 @@ $('#genSec').highcharts({
 			});
 	         
 	}
-	function buildTotalTodayRegistrationsTS(result){  
-		
-	 var str= '';
-	 // Total today Registrations block
-	  str+='<div class="row m_top10">';
-			str+='<div class="col-md-6 col-xs-12 col-sm-12">';
-				str+='<div class="bg_ED pad_15">';
-					str+='<div class="row m_top10">';
-						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
-							str+='<h5 class="text-capital">total</h5>';
-							str+='<h3 class="cadreCount">'+emptyCheck(result.totalCount)+'</h3>';
-						str+='</div>';
-						str+='<div class="col-md-7 col-xs-12 col-sm-12 pad_left0">';
-							str+='<h4 class="f_16 text-success">Renewal  <span class="pull-right cadreCount f_14">'+emptyCheck(result.renewalCount)+'</span></h4>';
-							str+='<h4 class="f_16" style="color:#F7A423">New  <span class="pull-right cadreCount f_14">'+emptyCheck(result.newCount)+'</span></h4>';
-						str+='</div>';
-					str+='</div>';
-					str+='<div id="totalOverAllRegistrationGraph1" class="chartLiD" style="height:120px" ></div>'; 
-				str+='</div>';
-				
-			str+='</div>';
-			
-			str+='<div class="col-md-6 col-xs-12 col-sm-12">';
-				str+='<div class="bg_ED pad_15">';
-					
-					str+='<div class="row m_top10">';
-						str+='<div class="col-md-5 col-xs-12 col-sm-12 pad_right0">';
-							str+='<h5 class="text-capital">today</h5>';
-							str+='<h3 class="cadreCount">'+emptyCheck(result.todayTotalCount)+'</h3>';
-						str+='</div>';
-						str+='<div class="col-md-7 col-xs-12 col-sm-12 pad_left0">';
-							str+='<h4 class="f_16 text-success">Renewal  <span class="pull-right cadreCount f_14">'+emptyCheck(result.todayRenewalCount)+'</span></h4>';
-							str+='<h4 class="f_16" style="color:#F7A423">New  <span class="pull-right cadreCount f_14">'+emptyCheck(result.todayNewCount)+'</span></h4>';
-						str+='</div>';
-
-					str+='</div>';
-					str+='<div id="todayOverAllRegistrationGraph" class="chartLiD" style="height:120px" ></div>';
-				str+='</div>';
-				
-			str+='</div>';
-		str+='</div>';
-		
-		$("#totalTodayCadreRegistrationBlockDivTSId").html(str);  
-		$('.cadreCount').each(function () {
-				$(this).prop('Counter',0).animate({
-					Counter: $(this).text()
-				}, {
-					duration: 1500,
-					easing: 'swing',
-					step: function (now) {
-						if(now != null && now > 0)
-						$(this).text(Math.ceil(now));
-					}
-				});
-			});
-		//total block graph.
-			var totalRenewalCountArray = [];
-			var totalNewCountArray =[];
-			totalRenewalCountArray.push(emptyCheck(result.renewalCount));
-			totalNewCountArray.push(emptyCheck(result.newCount));
-			
-			$('#totalOverAllRegistrationGraph1').highcharts({  
-				colors: ['#53BF8B','#f7a423'],
-				chart: {
-					backgroundColor: '#EDEEF0',
-					type: 'column'
-				},
-				title: {
-					text: ''
-				},
-				xAxis: {
-					min: 0,
-					 gridLineWidth: 0,
-					 minorGridLineWidth: 0,
-					categories: ['Andhra Pradesh']
-				},
-				yAxis: {
-					min: 0,
-				   gridLineWidth: 0,
-					minorGridLineWidth: 0,
-					title: {
-						text: ''
-					},
-					stackLabels: {
-						enabled: true,
-						style: {
-							fontWeight: 'bold',
-							color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-						}
-					}
-				},
-				legend: {
-					enabled: false,
-					align: 'right',
-					x: -30,
-					verticalAlign: 'top',
-					y: 25,
-					floating: true,
-					backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-					borderColor: '#CCC',
-					borderWidth: 1,
-					shadow: false
-				},
-				tooltip: {
-					formatter: function () {
-						var s = '<b>' + this.x + '</b>';
-
-						$.each(this.points, function () {
-							s += '<br/><b style="color:'+this.series.color+'">' + this.series.name + '</b> : ' +
-								Highcharts.numberFormat(this.percentage,1)+'%' +' - ' +
-								(this.y);
-						});
-
-						return s;
-					},
-					shared: true
-				},
-				plotOptions: {
-					column: {
-						stacking: 'percent',
-						dataLabels: {
-							enabled: true,
-							 formatter: function() {
-								if (this.y === 0) {
-									return null;
-								} else {
-									return Highcharts.numberFormat(this.percentage,1) + '%';
-								}
-							} 
-						  
-						}
-					}
-				},
-				series: [{
-					name: 'Renewal',
-					data: totalRenewalCountArray
-				}, {
-					name: 'New',
-					data: totalNewCountArray
-				}]
-			});
-		
-		   //today block graph.
-			var todayRenewalCountArray = [];
-			var todayNewCountArray =[];
-			todayRenewalCountArray.push(emptyCheck(result.todayRenewalCount));
-			todayNewCountArray.push(emptyCheck(result.todayNewCount));
-			$('#todayOverAllRegistrationGraph').highcharts({
-				colors: ['#53BF8B','#f7a423'],
-				chart: {
-					backgroundColor: '#EDEEF0', 
-					type: 'column'
-				},
-				title: {
-					text: ''
-				},
-				xAxis: {
-					min: 0,
-					 gridLineWidth: 0,
-					 minorGridLineWidth: 0,
-					categories: ['Andhra Pradesh']
-				},
-				yAxis: {
-					min: 0,
-				   gridLineWidth: 0,
-					minorGridLineWidth: 0,
-					title: {
-						text: ''
-					},
-					stackLabels: {
-						enabled: true,
-						style: {
-							fontWeight: 'bold',
-							color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-						}
-					}
-				},
-				legend: {
-					enabled: false,
-					align: 'right',
-					x: -30,
-					verticalAlign: 'top',
-					y: 25,
-					floating: true,
-					backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-					borderColor: '#CCC',
-					borderWidth: 1,
-					shadow: false
-				},
-				tooltip: {
-					formatter: function () {
-						var s = '<b>' + this.x + '</b>';
-
-						$.each(this.points, function () {
-							s += '<br/><b style="color:'+this.series.color+'">' + this.series.name + '</b> : ' +
-								Highcharts.numberFormat(this.percentage,1)+'%' +' - ' +
-								(this.y);
-						});
-
-						return s;
-					},
-					shared: true
-				},
-				plotOptions: {
-					column: {
-						stacking: 'percent',
-						dataLabels: {
-							enabled: true,
-							  formatter: function() {
-								if (this.y === 0) {
-									return null;
-								} else {
-									return Highcharts.numberFormat(this.percentage,1) + '%';
-								}
-							} 
-						  
-						}
-					}
-				},
-				series: [{
-					name: 'Renewal',
-					data: todayRenewalCountArray
-				}, {
-					name: 'New',
-					data: todayNewCountArray
-				}]  
-			});
-	         
-	}
-	
-	
 	function emptyCheck(filedValue){
 		var returnVal = ' - ';
 		if( filedValue !=null && filedValue > 0){
@@ -751,25 +520,31 @@ $('#genSec').highcharts({
 		}
 		return returnVal;
 	}
-	
-	function getEnumeratorsInfo(){
-		
+	//swadhin
+	function getEnumeratorsInfo(globalActivityMemberId){
 		$("#enumeratorsInfoDivId").html('<div class="spinner"><div class="dot1"></div><div class="dot2"></div></div>');
-		var cadreDataRetrieveType  = "intermediate";
-		$.ajax({
-			type : 'GET',
-			url : 'getEnumeratorsInfoAction.action',
+		var startDate = '';    
+		var endDate = '';  
+		var jsObj={  
+			activityMemberId : globalActivityMemberId,
+			stateId : globalStateId,         
+			startDate : '02/10/2016',      
+			endDate : '17/10/2016'
+		};
+		$.ajax({          
+			type : 'GET',       
+			url : 'getStateDtls.action',  
 			dataType : 'json',
-			data : { retrieveType : cadreDataRetrieveType }
+			data : {task :JSON.stringify(jsObj)} 
 		}).done(function(result){
-            if(result != null){
-				buildEnumeratorsInfo(result);
-			}else{
-				$("#enumeratorsInfoDivId").html('NO DATA AVAILABLE');
-			}	
-		});	
-           
+				if(result != null){
+					buildEnumeratorsInfo(result);
+				}else{
+					$("#enumeratorsInfoDivId").html('NO DATA AVAILABLE');
+				}	
+		});
 	}
+	
 	function buildEnumeratorsInfo(result){
 		//Enumerators block
 			var str1='';
@@ -781,24 +556,24 @@ $('#genSec').highcharts({
 								str1+='<img src="newCoreDashBoard/img/AP.png" class="img-responsive" alt="Andhra Prasdesh" style="width:65px"/>';
 							str1+='</td>';
 							str1+='<td>';
-								str1+='<h5>Total ';
-								/* if(result.totalCount != null && result.totalCount > 0){
-									str1+='- <span class="text-muted">'+result.+'%</span></h5>';
-								} */
+								str1+='<h5>Total-';
+								if(result.totalCount != null && result.totalCount > 0){
+									str1+='- <span class="text-muted">'+result.totalPercent+'%</span></h5>';
+								}
 								str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.totalCount)+'</h3>';
 							str1+='</td>';
 							str1+='<td>';
 								str1+='<h5>Today'; 
-								if(result.todayPercenCount != null && result.todayPercenCount > 0){
-									str1+='- <span class="text-muted">'+result.todayPercenCount+'%</span></h5>';
-								}
+								//if(result.todayPercenCount != null && result.todayPercenCount > 0){
+									str1+='- <span class="text-muted">'+result.totalPercentToday+'%</span></h5>';
+								//}
 								str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.todayTotalCount)+'</h3>';
 							str1+='</td>';
 							str1+='<td>';
-								str1+='<h5 class="EnumCadreCount cadreCount">'+emptyCheck(result.totalStartConstituCount)+' ';
-								if(result.totalStartConstituPer != null && result.totalStartConstituPer > 0){
-									str1+='- <small class="text-muted">'+result.totalStartConstituPer+'%</small></h5>';
-								}
+								str1+='<h5 class="EnumCadreCount cadreCount">'+emptyCheck(result.constStartedCount)+' ';
+								//if(result.totalStartConstituPer != null && result.totalStartConstituPer > 0){
+									str1+='- <small class="text-muted">'+result.constStartedCountPer+'%</small></h5>';
+								//}
 								str1+='<h5>Started<br> Constituencies</h5>';
 							str1+='</td>';
 						str1+='</tr>';
@@ -808,17 +583,17 @@ $('#genSec').highcharts({
 					str1+='<span style="position: relative; text-align: center; top: -20px; padding: 3px 8px; background-color: #edeef0; left: 35%;">Today Eumerators Info</span>';
 					str1+='<div class="row" style="margin-top:-10px">';
 						str1+='<div class="col-md-4 col-xs-12 col-sm-4 text-center">';
-							str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.todayFieldMembersCount)+'</h3>';
-							str1+='<h5 class="text-capital">today field members</h5>';
+							str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.inField)+'</h3>';
+							str1+='<h5 class="text-capital">IN FIELD NOW</h5>';
 						str1+='</div>';
 						str1+='<div class="col-md-4 col-xs-12 col-sm-4 text-center">';
-							str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.inFieldCount)+'</h3>';
-							str1+='<h5 class="text-capital">in field now</h5>';
+							//str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.inFieldCount)+'</h3>';
+							//str1+='<h5 class="text-capital">in field now</h5>';
 						str1+='</div>';
 						str1+='<div class="col-md-4 col-xs-12 col-sm-4 text-center">';
 							//str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.todaySubmittedCount)+'</h3>';
-							str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.todayTotalCount)+'</h3>';
-							str1+='<h5 class="text-capital">today submitted data</h5>';
+							str1+='<h3 class="EnumCadreCount cadreCount">'+emptyCheck(result.totalSubmittedToday)+'</h3>';
+							str1+='<h5 class="text-capital">today submitted data</h5>';    
 						str1+='</div>';
 					str1+='</div>';
 				str1+='</div>';
