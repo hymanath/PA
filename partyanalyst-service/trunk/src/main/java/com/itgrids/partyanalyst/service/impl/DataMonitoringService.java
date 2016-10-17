2965464489f6bda731d6e667baabe7e21986be41
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-17 07:03:10 +0000

    committed by krishna

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/DataMonitoringService.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IDataMonitoringService;
+
+public class DataMonitoringService implements IDataMonitoringService {
+	
+	private static final Logger LOG = Logger.getLogger(DataMonitoringService.class);
+
+}
