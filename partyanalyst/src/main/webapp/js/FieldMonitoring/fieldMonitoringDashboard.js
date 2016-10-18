80237f3f6c26b838e5f8eb9e52360e317d5572e0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-18 09:12:05 +0000

    updated by - SREEDHAR

--- partyanalyst/src/main/webapp/js/FieldMonitoring/fieldMonitoringDashboard.js
+++ partyanalyst/src/main/webapp/js/FieldMonitoring/fieldMonitoringDashboard.js
@@ -1,55 +1,61 @@
-alert(54)
-getStatusWiseIssueTypeCount();
-function getStatusWiseIssueTypeCount(){
+	
+	function onLoadCalls(){
+		getIssueStatusWiseCounts();
+		getIssueTypeWiseCounts();
+	}
+	
+	function getIssueStatusWiseCounts(){
 			
 			var jsObj = { 
-			              fromDate : 12/07/2016,
-			              toDate : 12/07/2016 
+			              fromDate : "12/07/2016",
+			              toDate : "12/07/2016"  
 						}
-			
 			$.ajax({
 				type : 'GET',
-				url : 'getStatusWiseIssueTypeCountAction.action',
+				url : 'getIssueStatusWiseCountsAction.action',
 				dataType : 'json',
 				data : {task:JSON.stringify(jsObj)}  
 			}).done(function(result){
-				if(result != null && result.length >0)
-				//buildStatusWiseIssueTypesCount(result);
+			if(result != null && result.length >0){
+				buildIssueStatusWiseCounts(result);
+			}	
 			});
+	}
+	function buildIssueStatusWiseCounts(result){
+			var str='';
 			
-		}
-		 getAllIssueStatusCount();
-		function getAllIssueStatusCount(){
+			str +='<h4 class="text-capital panel-title"><b>field monitoring system</b></h4>';
+			str +='<div class="row">';
+			str +='<div class="col-md-8 col-xs-12 col-sm-10">';
+			str +='<ul class="dashedB">';
+			for(var i in result){
+			   str +='<li>'+result[i].name+' issues<span>'+result[i].inviteeCount+'</span></li>';
+			}
+			str +='</ul>';
+			str +='</div>';
+			str +='</div>';
+			$("#statusCountDivId").html(str);
+		
+		}	
+	function getIssueTypeWiseCounts(){
 			
 			var jsObj = { 
-			              fromDate : 12/07/2016,
-			              toDate : 12/07/2016  
+			              fromDate : "12/07/2016",
+			              toDate : "12/07/2016" 
 						}
-			
 			$.ajax({
 				type : 'GET',
-				url : 'getAllIssueStatusCountAction.action',
+				url : 'getIssueTypeWiseCountsAction.action',
 				dataType : 'json',
 				data : {task:JSON.stringify(jsObj)}  
 			}).done(function(result){
-			if(result != null && result.length >0)
-				buildStatusCount(result);
+				if(result != null && result.length >0){getIssueTypeWiseCounts
+					
+				}
 			});
+			
 		}
-		function buildStatusCount(result){
-		var str='';
 		
-		str +='h4 class="text-capital panel-title"><b>field monitoring system</b></h4>';
-        str +='<div class="row">';
-        str +='<div class="col-md-8 col-xs-12 col-sm-10">';
-        str +='<ul class="dashedB">';
-		for(var i in result){
-        str +='<li>open issues<span>1000</span></li>';
-        
-		}
-        str +='</ul>';
-        str +='</div>';
-        str +='</div>';
-		$("#statusCountDivId").html(str);
 		
-		}
+		
+		
