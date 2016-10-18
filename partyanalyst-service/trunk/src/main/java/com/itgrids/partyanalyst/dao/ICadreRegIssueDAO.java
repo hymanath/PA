b85f57caa8aba67fbf71148d4b417d70aaa96576
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-18 08:33:43 +0000

    updated by -- SRAVANTH

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueDAO.java
@@ -9,10 +9,10 @@ import com.itgrids.partyanalyst.model.CadreRegIssue;
 
 public interface ICadreRegIssueDAO extends GenericDao<CadreRegIssue, Long> {
 	public List<Object[]> getTabUsersDetailsByVendorAndLocation(Long vendorId,Date fromDate,Date toDate,String locationType,Long locationVal);
-	public List<Object[]> getLastHourCounts(Long vendorId,Date currentDate,String locationType,Long locationVal);
+	public List<Object[]> getLastHourCounts(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal);
 	public List<Object[]> getcadreRegIssuesCounts(Long vendorId,String locationType,Long locationVal);
 	public Long getTotalDataCollectorsCountsVendorAndLocation(Long vendorId,Date fromDate,Date toDate,String locationType,Long locationVal);
-	public Long getActiveDataCollectorsCountsVendorAndLocation(Long vendorId,Date currentDate,String locationType,Long locationVal);
+	public Long getActiveDataCollectorsCountsVendorAndLocation(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal);
 	public List<Object[]> getCadreIssueStatusCount(Date fromDate,Date toDate);
 	public List<Object[]> getStatusWiseIssueTypeCount(Date fromDate,Date toDate);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegIssueDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegIssueDAO.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreRegIssueDAO;
@@ -61,8 +62,46 @@ public class CadreRegIssueDAO extends GenericDaoHibernate<CadreRegIssue, Long> i
 		return query.list();
 	}
 	
-	public List<Object[]> getLastHourCounts(Long vendorId,Date currentDate,String locationType,Long locationVal){
+	public List<Object[]> getLastHourCounts(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal){
 		StringBuilder sb = new StringBuilder();
+		sb.append("select model1.cadreSurveyUser.cadreSurveyUserId," +
+					" model.tdpCadre.tabUserInfo.tabUserInfoId," +
+					" count(model.tdpCadre.tdpCadreId)" +
+					" from TdpCadreEnrollmentYear model,FieldVendorTabUser model1,FieldVendorLocation model2,Constituency C" +
+					" where model.tdpCadre.insertedBy.cadreSurveyUserId = model1.cadreSurveyUser.cadreSurveyUserId" +
+					" and model1.fieldVendor.fieldVendorId = model2.fieldVendor.fieldVendorId" +
+					" and model1.fieldVendor.fieldVendorId = :vendorId" +
+					" and model2.constituency.constituencyId = C.constituencyId");
+		
+		if(locationType != null && locationType.trim().equalsIgnoreCase("state"))
+			sb.append(" and C.state.stateId = :locationVal");
+		else if(locationType != null && locationType.trim().equalsIgnoreCase("district"))
+			sb.append(" and C.district.districtId = :locationVal");
+		else
+			sb.append(" and C.constituencyId = :locationVal");
+				
+		if(lastOneHourTime!= null && today!=null){
+			sb.append(" and model.tdpCadre.surveyTime between :lastOneHourTime and :today");	 
+		}
+		
+		sb.append(" and model.tdpCadre.enrollmentYear = 2014" +
+					" and model.enrollmentYear.enrollmentYearId = 4" +
+					" and model.isDeleted = 'N'" +
+					" and model.tdpCadre.isDeleted = 'N'" +
+					" and model1.isDeleted = 'N'" +
+					" and model2.isDeleted = 'N'" +
+					" group by model.tdpCadre.insertedBy,model.tdpCadre.tabUserInfoId");
+		
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("vendorId", vendorId);
+		query.setParameter("locationVal", locationVal);
+		if(lastOneHourTime!= null && today!=null){
+			query.setDate("lastOneHourTime", lastOneHourTime);
+			query.setDate("today", today);
+		}
+		
+		return query.list();
+		/*StringBuilder sb = new StringBuilder();
 		sb.append("select FVTU.cadre_survey_user_id," +
 					" TC.tab_user_info_id," +
 					" count(TC.tdp_cadre_id)" +
@@ -98,7 +137,7 @@ public class CadreRegIssueDAO extends GenericDaoHibernate<CadreRegIssue, Long> i
 			query.setParameter("currentTime", currentDate);
 		}
 		
-		return query.list();
+		return query.list();*/
 	}
 	
 	public List<Object[]> getcadreRegIssuesCounts(Long vendorId,String locationType,Long locationVal){
@@ -166,12 +205,48 @@ public class CadreRegIssueDAO extends GenericDaoHibernate<CadreRegIssue, Long> i
 			query.setDate("toDate", toDate);
 		}
 		
-		return Long.valueOf(query.uniqueResult().toString());
+		return (Long) query.uniqueResult();
 	}
 	
-	public Long getActiveDataCollectorsCountsVendorAndLocation(Long vendorId,Date currentDate,String locationType,Long locationVal){
+	public Long getActiveDataCollectorsCountsVendorAndLocation(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal){
 		StringBuilder sb = new StringBuilder();
-		sb.append("select count(distinct TC.created_by)" +
+		sb.append("select count(distinct model.tdpCadre.insertedBy)" +
+					" from TdpCadreEnrollmentYear model,FieldVendorTabUser model1,FieldVendorLocation model2,Constituency C" +
+					" where model.tdpCadre.insertedBy.cadreSurveyUserId = model1.cadreSurveyUser.cadreSurveyUserId" +
+					" and model1.fieldVendor.fieldVendorId = model2.fieldVendor.fieldVendorId" +
+					" and model1.fieldVendor.fieldVendorId = :vendorId" +
+					" and model2.constituency.constituencyId = C.constituencyId");
+		
+		if(locationType != null && locationType.trim().equalsIgnoreCase("state"))
+			sb.append(" and C.state.stateId = :locationVal");
+		else if(locationType != null && locationType.trim().equalsIgnoreCase("district"))
+			sb.append(" and C.district.districtId = :locationVal");
+		else
+			sb.append(" and C.constituencyId = :locationVal");
+				
+		if(lastOneHourTime!= null && today!=null){
+			sb.append(" and model.tdpCadre.surveyTime between :lastOneHourTime and :today");	 
+		}
+		
+		sb.append(" and model.tdpCadre.enrollmentYear = 2014" +
+					" and model.enrollmentYear.enrollmentYearId = 4" +
+					" and model.isDeleted = 'N'" +
+					" and model.tdpCadre.isDeleted = 'N'" +
+					" and model1.isDeleted = 'N'" +
+					" and model2.isDeleted = 'N'" +
+					" group by model1.fieldVendor.fieldVendorId");
+		
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("vendorId", vendorId);
+		query.setParameter("locationVal", locationVal);
+		if(lastOneHourTime!= null && today!=null){
+			query.setDate("lastOneHourTime", lastOneHourTime);
+			query.setDate("today", today);
+		}
+		
+		return (Long) query.uniqueResult();
+		/*StringBuilder sb = new StringBuilder();
+		sb.append("select count(distinct TC.created_by) as count" +
 					" from tdp_cadre_enrollment_year TCEY,tdp_cadre TC,field_vendor_tab_user FVTU,field_vendor_location FVL,constituency C" +
 					" where TCEY.tdp_cadre_id = TC.tdp_cadre_id" +
 					" and TC.created_by = FVTU.cadre_survey_user_id" +
@@ -197,14 +272,14 @@ public class CadreRegIssueDAO extends GenericDaoHibernate<CadreRegIssue, Long> i
 					" and FVL.is_deleted = 'N'" +
 					" group by FVL.field_vendor_id");
 		
-		Query query = getSession().createSQLQuery(sb.toString());
+		Query query = getSession().createSQLQuery(sb.toString()).addScalar("count",Hibernate.LONG);
 		query.setParameter("vendorId", vendorId);
 		query.setParameter("locationVal", locationVal);
 		if(currentDate != null){
 			query.setParameter("currentTime", currentDate);
 		}
 		
-		return Long.valueOf(query.uniqueResult().toString());
+		return (Long) query.uniqueResult();*/
 	}
 
 	public List<Object[]> getCadreIssueStatusCount(Date fromDate,Date toDate){
