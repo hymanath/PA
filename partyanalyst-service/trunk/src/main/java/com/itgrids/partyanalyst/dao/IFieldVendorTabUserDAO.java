7b6a2b0027046a24968831e92add8d405ebf24d9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-18 12:29:11 +0000

    updated by -- SRAVANTH

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorTabUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorTabUserDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.FieldVendorTabUser;
 
 public interface IFieldVendorTabUserDAO extends GenericDao<FieldVendorTabUser, Long>{
 
+	public Long getTotalDataCollectorsCount(Date startDate,Date endDate);
+	public Long getActiveDataCollectorsCount(Date lastHourTime,Date today);
 }
