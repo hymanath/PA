1f5b58aee6ede0af11f36024c4ad12d1250008ed
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-19 05:13:27 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueTrackDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueTrackDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreRegIssueTrack;
 
 public interface ICadreRegIssueTrackDAO extends GenericDao<CadreRegIssueTrack, Long> {
-
+	
+	public List<Object[]> trackingRegIssueByRegIssueId(Long cadreRegIssueId);
 }
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataVerificationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreDataVerification;
+
+public interface ITdpCadreDataVerificationDAO extends GenericDao<TdpCadreDataVerification, Long>{
+
+}
