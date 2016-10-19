632fc82e8b9dc5765b16a777087f411e05029776
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-19 14:56:51 +0000

    commit by nandhini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITabUserOtpDetailsDAO.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TabUserOtpDetails;
+
+public interface ITabUserOtpDetailsDAO extends GenericDao<TabUserOtpDetails, Long>{
+
+	public Long checkOTPDetails(String mobileNo,String otpNo,Date currentTime);
+	public TabUserOtpDetails checkValidOTPIsExists(Long tabUserInfoId,Long cadreSurveyUserId,String mobileNo,Date currentTime);
+}
