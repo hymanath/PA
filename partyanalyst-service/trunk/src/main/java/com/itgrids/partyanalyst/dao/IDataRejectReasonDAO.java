141d6f0e645cc59e22de96544d0a41914779eb5e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-19 05:13:27 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDataRejectReasonDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.DataRejectReason;
+
+public interface IDataRejectReasonDAO extends GenericDao<DataRejectReason, Long>{
+
+}
