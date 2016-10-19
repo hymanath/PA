8bac5674b8309933e504ebccb8ee24afc73c6e5b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-19 06:46:48 +0000

    updated by -- SRAVANTH

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssueDAO.java
@@ -10,7 +10,7 @@ import com.itgrids.partyanalyst.model.CadreRegIssue;
 public interface ICadreRegIssueDAO extends GenericDao<CadreRegIssue, Long> {
 	public List<Object[]> getTabUsersDetailsByVendorAndLocation(Long vendorId,Date fromDate,Date toDate,String locationType,Long locationVal);
 	public List<Object[]> getLastHourCounts(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal);
-	public List<Object[]> getcadreRegIssuesCounts(Long vendorId,String locationType,Long locationVal);
+	public List<Object[]> getcadreRegIssuesCounts(Long vendorId,String locationType,Long locationVal,Date startDate,Date endDate);
 	public Long getTotalDataCollectorsCountsVendorAndLocation(Long vendorId,Date fromDate,Date toDate,String locationType,Long locationVal);
 	public Long getActiveDataCollectorsCountsVendorAndLocation(Long vendorId,Date lastOneHourTime,Date today,String locationType,Long locationVal);
 	public List<Object[]> getIssueStatusWiseCounts(Date fromDate,Date toDate);
