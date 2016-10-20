ee97bef3c151433121af855eda27804990bd2704
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-20 06:31:27 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
@@ -1,11 +1,13 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
 import com.itgrids.partyanalyst.dto.DataMonitoringOverviewVO;
 
 import com.itgrids.partyanalyst.dto.IdNameVO;
 
 public interface IDataMonitoringService {
 	public IdNameVO getTotalRegCdrVendorWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
-
+	public List<IdNameVO> getTotalRegCdrVendorAndTabUserWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
 	public DataMonitoringOverviewVO getDataMonitoringOverViewDetails(String fromDateStr,String toDateStr);
 }
