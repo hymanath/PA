38082eca4876263fb071c0d51f196922913d1d8e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-21 07:08:11 +0000

    Updated by Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
@@ -10,6 +10,6 @@ public interface IDataMonitoringService {
 	public IdNameVO getTotalRegCdrVendorWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
 	public List<IdNameVO> getTotalRegCdrVendorAndTabUserWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
 	public DataMonitoringOverviewVO getDataMonitoringOverViewDetails(String fromDateStr,String toDateStr);
-	public List<List<IdNameVO>> getVerifiedDtls(Long surveyUserId, Long tabUserId, Long webUserId, String startDate, String endDate);
+	public List<List<IdNameVO>> getVerifiedDtls(Long surveyUserId, Long tabUserId, Long webUserId, String startDate, String endDate,Integer minValue,Integer maxValue);
 	public List<DataMonitoringOverviewVO> getRegistrationDetailsUserWise(String fromDateStr,String toDateStr,String dataSourceType,String verificationStatus);
 }
