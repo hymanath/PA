67a4f5a7550181c9ae4979d1080458370f0dc25f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-21 11:50:58 +0000

    Updated by Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDataMonitoringService.java
@@ -11,7 +11,7 @@ public interface IDataMonitoringService {
 	public IdNameVO getTotalRegCdrVendorWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
 	public List<IdNameVO> getTotalRegCdrVendorAndTabUserWise(Long stateId, Long vendorId, Long distId, Long constId, String startDate, String endDate);
 	public DataMonitoringOverviewVO getDataMonitoringOverViewDetails(String fromDateStr,String toDateStr);
-	public List<List<IdNameVO>> getVerifiedDtls(Long surveyUserId, Long tabUserId, Long webUserId, String startDate, String endDate,Integer minValue,Integer maxValue);
+	public List<List<IdNameVO>> getVerifiedDtls(Long surveyUserId, Long tabUserId, Long webUserId, String startDate, String endDate,Integer minValue,Integer maxValue,String resultType,String verificationStatus);
 	public List<DataMonitoringOverviewVO> getRegistrationDetailsUserWise(String fromDateStr,String toDateStr,String dataSourceType,String verificationStatus);
 	public List<IdNameVO> getDataRejectReason();
 	public ResultStatus updateRejectDtls(List<IdNameVO> idNameVOs);
