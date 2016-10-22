c29a921333fec3fd800330fce3570cee2a7d1102
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-22 13:42:08 +0000

    pushing data to intermediate tables - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreDateWiseInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreDateWiseInfoDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreDateWiseInfoDAO;
 import com.itgrids.partyanalyst.model.TdpCadreDateWiseInfo;
@@ -10,5 +11,15 @@ public class TdpCadreDateWiseInfoDAO extends GenericDaoHibernate<TdpCadreDateWis
 	public TdpCadreDateWiseInfoDAO() {
 		super(TdpCadreDateWiseInfo.class);
 	}
-
+	
+	 public int deleteAllRecords(){
+	    	
+    	Query query = getSession().createSQLQuery(" delete from tdp_cadre_date_wise_info ");
+    	return query.executeUpdate();
+    }
+	 
+    public int setPrimaryKeyAutoIncrementToOne(){
+    	Query query = getSession().createSQLQuery(" ALTER TABLE tdp_cadre_date_wise_info AUTO_INCREMENT = 1 ");
+    	return query.executeUpdate();
+    }
 }
