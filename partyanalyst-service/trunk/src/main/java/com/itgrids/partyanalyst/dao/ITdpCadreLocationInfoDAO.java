c70a3effa4c001dd1f87b25b814701aebb704664
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-24 14:43:23 +0000

    Updated By Hyma : Overall Visualization webservice call

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreLocationInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreLocationInfoDAO.java
@@ -1,11 +1,15 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.GISVisualizationParameterVO;
 import com.itgrids.partyanalyst.model.TdpCadreLocationInfo;
 
 public interface ITdpCadreLocationInfoDAO extends GenericDao<TdpCadreLocationInfo,Long>{
 	
 	public int deleteAllRecords();
 	public int setPrimaryKeyAutoIncrementToOne();
+	public List<Object[]> getLocationsRegistrationsDetails(GISVisualizationParameterVO inputVO);
 }
