2c0f8a01e2882aa08e1c8731291cc6b61b1ecfc2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-25 12:54:50 +0000

    committed by krishna

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/DataReconsolidationService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.IDataReconsolidationService;
+
+public class DataReconsolidationService implements IDataReconsolidationService {
+
+}
