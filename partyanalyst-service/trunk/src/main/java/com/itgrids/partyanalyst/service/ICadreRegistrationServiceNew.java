c51279118ca66618c81ebf6282da578b5de6b684
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-26 11:12:24 +0000

    pushing data to intermediate tables - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationServiceNew.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICadreRegistrationServiceNew.java
@@ -5,5 +5,6 @@ import com.itgrids.partyanalyst.dto.ResultStatus;
 public interface ICadreRegistrationServiceNew {
 	
 	public ResultStatus pushTotalTodayTdpCadreDataToIntermediate();
+	public ResultStatus pushTotalTodayTdpCadreDataToIntermediateByLowLevel();
 	public ResultStatus pushTdpCadreDataToIntermediateDateWise();
 }
