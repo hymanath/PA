ad1f137f5175c8f905bc5f15bd3446d24d2dfa33
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-26 10:36:14 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTabRecordsStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreTabRecordsStatusDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreTabRecordsStatus;
 
 public interface ICadreTabRecordsStatusDAO extends GenericDao<CadreTabRecordsStatus, Long>{
 
+	public Integer deleteExstngCadreTdpRecords(Long cadreSurveyUserId,Long tabUserInfoId,Date surveyDate);
 }
