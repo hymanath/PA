1a2d7711f8557ed37b605d7799c4db5c6658c80e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-26 12:48:19 +0000

    Updated by Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreTargetCountDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreTargetCountDAO.java
@@ -9,7 +9,7 @@ import com.itgrids.partyanalyst.dto.GISVisualizationParameterVO;
 import com.itgrids.partyanalyst.model.TdpCadreTargetCount;
 
 public interface ITdpCadreTargetCountDAO extends GenericDao<TdpCadreTargetCount, Long> {
-	public List<Object[]> getTotalCadreTargetCountLocationWise(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Long enrollmentYearId);
+	public List<Object[]> getTotalCadreTargetCountLocationWise(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Long enrollmentYearId,Long activityMemberId);
 	public List<Object[]> getTotalCadreTargetCountLocationType(String locationType,Long stateId,Long entollmentTearId);
 	public Long getTargetCount(Long stateId);
 	public List<Object[]> getTdpCadreTargetCountLocationWise(Long enrollmentYearId);
