29350316b1d7dcdb75ad612a33f45b3c9fa59561
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-26 10:36:54 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreTabRecordsStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreTabRecordsStatusDAO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreTabRecordsStatusDAO;
 import com.itgrids.partyanalyst.model.CadreTabRecordsStatus;
@@ -13,4 +15,21 @@ public class CadreTabRecordsStatusDAO extends GenericDaoHibernate<CadreTabRecord
 		super(CadreTabRecordsStatus.class);
 	}
 	
+	public Integer deleteExstngCadreTdpRecords(Long cadreSurveyUserId,Long tabUserInfoId,Date surveyDate)
+	{
+		StringBuilder queryStr=new StringBuilder();
+		  	queryStr.append(" delete from CadreTabRecordsStatus model" +
+				" where model.cadreSurveyUserId := cadreSurveyUserId" +
+				" and model.tabUserInfoId := tabUserInfoId");
+		if(surveyDate != null)
+			queryStr.append("and date(model.surveyDate) :=surveyDate");
+		
+		Query query = getSession().createQuery(queryStr.toString());
+			query.setParameter("cadreSurveyUserId", cadreSurveyUserId);
+			query.setParameter("tabUserInfoId", tabUserInfoId);
+			if(surveyDate != null)
+			query.setParameter("surveyDate", surveyDate);
+		
+			return query.executeUpdate();
+	}
 }
