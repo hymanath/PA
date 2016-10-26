8d5feb6ed0d89961d7b769e346cd2a5327a10073
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-26 07:01:02 +0000

    Updated by Santosh

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ParliamentAssemblyDAO.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+
+import com.itgrids.partyanalyst.dao.IParliamentAssemblyDAO;
+import com.itgrids.partyanalyst.model.District;
+import com.itgrids.partyanalyst.model.ParliamentAssembly;
+
+public class ParliamentAssemblyDAO extends GenericDaoHibernate<ParliamentAssembly, Long> implements
+		IParliamentAssemblyDAO {
+	public ParliamentAssemblyDAO() {
+		super(ParliamentAssembly.class);
+	}
+}
