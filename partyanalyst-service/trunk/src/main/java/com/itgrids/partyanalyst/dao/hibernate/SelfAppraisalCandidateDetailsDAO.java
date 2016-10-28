f9214fcc05ac9c4f9308681c568f93ff0a85d60d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-28 14:43:06 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SelfAppraisalCandidateDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/SelfAppraisalCandidateDetailsDAO.java
@@ -16,29 +16,57 @@ public class SelfAppraisalCandidateDetailsDAO extends GenericDaoHibernate<SelfAp
 	  }
 	  
 	  public List<Object[]> getSubmittedToursLeadersDetails(Date fromDate,Date toDate,Long desigId){
-	      StringBuilder queryStr = new StringBuilder();
-	      queryStr.append( " select " +
-	                   " model.selfAppraisalCandidate.selfAppraisalDesignationId," +
-	                   " count(distinct model.selfAppraisalCandidate.selfAppraisalCandidateId)," +
-	                   " sum(model.ownTours)," +
-	                   " sum(model.inchargeTours) " +
-	                   " from SelfAppraisalCandidateDetails model " +
-	                   " where model.selfAppraisalCandidate.isActive='Y' "); 
-	                    if(fromDate != null && toDate != null ){
-	                      queryStr.append(" and date(model.updatedTime) between :fromDate and :toDate ");
-	                    }
-	                    if(desigId != null){
-	                      queryStr.append(" and model.selfAppraisalCandidate.selfAppraisalDesignationId = :desigId");
-	                    }
-	                    queryStr.append("group by model.selfAppraisalCandidate.selfAppraisalDesignationId ");
-	                    Query query = getSession().createQuery(queryStr.toString());
-	                    if(fromDate != null && toDate != null ){
-	                      query.setDate("fromDate", fromDate);
-	                      query.setDate("toDate", toDate);
-	                    }
-	                    if(desigId != null){
-	                    	query.setParameter("desigId",desigId);
-	                 }    
-	                    return query.list();
-	    }
+		  StringBuilder queryStr = new StringBuilder();
+		  queryStr.append( " select " +
+		    		       " model.selfAppraisalCandidate.selfAppraisalDesignationId," +
+		    		       " count(distinct model.selfAppraisalCandidate.selfAppraisalCandidateId)," +
+		    		       " sum(model.ownTours)," +
+		    		       " sum(model.inchargeTours) " +
+		    		       " from SelfAppraisalCandidateDetails model " +
+		    		       " where model.selfAppraisalCandidate.isActive='Y' "); 
+		                if(fromDate != null && toDate != null ){
+		                	queryStr.append(" and date(model.updatedTime) between :fromDate and :toDate ");
+		                }
+		                if(desigId != null){
+		                	queryStr.append(" and model.selfAppraisalCandidate.selfAppraisalDesignationId = :desigId ");
+		                }
+		                queryStr.append(" group by model.selfAppraisalCandidate.selfAppraisalDesignationId ");
+		                Query query = getSession().createQuery(queryStr.toString());
+		                if(fromDate != null && toDate != null ){
+		                	query.setDate("fromDate", fromDate);
+		                	query.setDate("toDate", toDate);
+		                }
+		                if(desigId != null){
+		     			   query.setParameter("desigId",desigId); 
+		     		    }    
+		                return query.list();
+	  }
+	  public List<Object[]> getCandidateDtlsList(Date startDate, Date endDate,List<Long> candidateList){
+		  StringBuilder queryStr = new StringBuilder();
+		  queryStr.append(" select " +
+		  				  " SACD.selfAppraisalCandidateDetailsId, " +
+		  				  " SACD.selfAppraisalCandidate.selfAppraisalCandidateId, " +
+		  				  " SACD.selfAppraisalCandidate.tdpCadre.firstname, " +
+		  				  " SACD.month, " +
+		  				  " SACD.year, " +
+		  				  " SACD.ownTours, " +
+		  				  " SACD.inchargeTours," +    
+		  				  " SACD.remarks, " +
+		  				  " SACD.reportPath " +          
+		  				  " from " +
+		  				  " SelfAppraisalCandidateDetails SACD " +  
+		  				  " where " +
+		  				  " SACD.selfAppraisalCandidate.selfAppraisalCandidateId in (:candidateList) and " +
+		  				  " date(SACD.updatedTime) between :fromDate and :toDate ");
+		  Query query = getSession().createQuery(queryStr.toString());
+		  if(startDate != null && endDate != null ){
+			  query.setDate("fromDate", startDate);
+			  query.setDate("toDate", endDate);
+          }
+		  if(candidateList != null && candidateList.size() > 0){
+			  query.setParameterList("candidateList",candidateList);
+		  }
+		  
+		  return query.list();
+	  }
 }
