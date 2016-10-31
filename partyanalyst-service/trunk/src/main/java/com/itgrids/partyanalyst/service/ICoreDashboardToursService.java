9fcb04c89dee45c4875e2af394ff48515a225c22
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-31 05:57:58 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
@@ -6,5 +6,6 @@ import com.itgrids.partyanalyst.dto.ToursBasicVO;
 
 public interface ICoreDashboardToursService {
 	public ToursBasicVO getToursBasicOverviewCountDetails(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId);
+	public String getDesigWiseMemberDtls(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId);
 	public List<ToursBasicVO> getDistrictWiseToursSubmitedDetails(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId,Long userTypeId);
 }
