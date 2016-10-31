e3ea913feacb0ce625ae7e7bed50d40cde99a2b6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-31 20:44:43 +0000

    updated by -- SRAVANTH

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegUserDAO.java
@@ -6,6 +6,6 @@ import com.itgrids.partyanalyst.model.CadreRegUser;
 
 public interface ICadreRegUserDAO extends GenericDao<CadreRegUser, Long> {
 
-	public Long getCadreRegUserByUser(Long userId,Long stateId);
+	public Long getCadreRegUserByUser(Long userId);
 	public Long getCadreRegUserByUserForDataMonitoring(Long userId);
 }
