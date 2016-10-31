39bdc5a74202b2bedaa1592be10ae9409e303a95
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-10-31 05:55:22 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateLocationDAO.java
@@ -10,4 +10,6 @@ import com.itgrids.partyanalyst.model.SelfAppraisalCandidateLocation;
 public interface ISelfAppraisalCandidateLocationDAO extends GenericDao<SelfAppraisalCandidateLocation, Long> {
 	 public List<Object[]> getCandiateLocationScopeIdAndValues(Long candidateId);
 	 public List<Object[]> getNoOfLeadersCntDesignationByBasedOnUserAccessLevel(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId);
+	 public List<Object[]> getDesigWiseAllCandidate(Long stateId,Long userAccessLevelId,Set<Long> locationValueSet);
+	 public List<Object[]> getLocationListByCndIdAndScopeId(Set<Long> cndId, Set<Long> selfAppLocationScpId);
 }
