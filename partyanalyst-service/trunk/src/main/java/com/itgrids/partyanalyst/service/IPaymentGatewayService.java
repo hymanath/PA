5b04951272e12a3e9fa5c1a041296f9d20a098c5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-01 23:40:58 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPaymentGatewayService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IPaymentGatewayService.java
@@ -8,5 +8,6 @@ public interface IPaymentGatewayService {
 	public PaymentGatewayVO getPaymentAmountByRegistrationType(String moduleStr,String subTypeStr);
 	public PaymentGatewayVO getTransactionMerchantDetailsByRegistrationType(String moduleStr,String subTypeStr);
 	public String savePaymenyTransactionDetails(final PaymentTransactionVO paymentTransactionVO);
-	public PaymentGatewayVO getPaymentBasicInfoByPaymentGateWayType(Long gateWayId,String randomNo,String enrollId,String moduleStr,String subTypeStr);
+	public PaymentGatewayVO getPaymentBasicInfoByPaymentGateWayType(Long gateWayId,String randomNo,String enrollId,String moduleStr,String subTypeStr,String otherAmount);
+	public String updateTransactionTrackingDetals(final String OrderId,final String status);
 }
