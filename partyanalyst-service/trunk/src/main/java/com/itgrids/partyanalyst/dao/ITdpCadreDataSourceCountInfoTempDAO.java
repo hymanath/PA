4554bd32adab9b7277d5055f248f77cb310e99b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-03 16:14:17 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataSourceCountInfoTempDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreDataSourceCountInfoTemp;
+
+public interface ITdpCadreDataSourceCountInfoTempDAO extends
+		GenericDao<TdpCadreDataSourceCountInfoTemp, Long> {
+
+}
