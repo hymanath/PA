6542da81229073f2e0def95b02fbf203ca2dc9bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-03 15:25:13 +0000

    intermediate tables hour wise today - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreUserHourRegInfo.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreUserHourRegInfo.java
@@ -3,6 +3,7 @@
  */
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -16,4 +17,7 @@ import com.itgrids.partyanalyst.model.TdpCadreUserHourRegInfo;
 public interface ITdpCadreUserHourRegInfo extends GenericDao<TdpCadreUserHourRegInfo, Long>{
 
 	public List<Object[]> getTabUserLastOneHourData(Long lstHour,List<Long> tabUserInfoIds);
+	public List<Object[]> getTdpCadreDataHourWiseForTabUsers(Date date,Integer hour);
+	public int deleteAllRecords(Date fromDate);
+	public List<Object[]> getTdpCadreDataHourWiseForTabUsersOverall();
 }
