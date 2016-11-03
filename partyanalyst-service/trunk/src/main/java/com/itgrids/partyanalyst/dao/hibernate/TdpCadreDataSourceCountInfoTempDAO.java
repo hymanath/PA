28355237b9c84be6bec9f41b2df4896001474eda
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-03 18:20:07 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreDataSourceCountInfoTempDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreDataSourceCountInfoTempDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreDataSourceCountInfoTempDAO;
 import com.itgrids.partyanalyst.model.TdpCadreDataSourceCountInfoTemp;
@@ -9,4 +10,12 @@ public class TdpCadreDataSourceCountInfoTempDAO extends GenericDaoHibernate<TdpC
 	public TdpCadreDataSourceCountInfoTempDAO(){
 		super(TdpCadreDataSourceCountInfoTemp.class);  
 	}
+	public int deleteAllRecords(){
+    	Query query = getSession().createSQLQuery(" delete from tdp_cadre_data_source_count_info_temp ");
+    	return query.executeUpdate();
+	 }
+	 public int setPrimaryKeyAutoIncrementToOne(){
+    	Query query = getSession().createSQLQuery(" ALTER TABLE tdp_cadre_data_source_count_info_temp AUTO_INCREMENT = 1 ");
+    	return query.executeUpdate();
+	 }  
 }
