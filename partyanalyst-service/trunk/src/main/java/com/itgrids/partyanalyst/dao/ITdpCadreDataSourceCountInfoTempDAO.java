3b51cadcc812d85f8df858f22cb85e04756c41ba
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-03 18:19:52 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataSourceCountInfoTempDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataSourceCountInfoTempDAO.java
@@ -6,5 +6,8 @@ import com.itgrids.partyanalyst.model.TdpCadreDataSourceCountInfoTemp;
 
 public interface ITdpCadreDataSourceCountInfoTempDAO extends
 		GenericDao<TdpCadreDataSourceCountInfoTemp, Long> {
+	public int deleteAllRecords();
+	public int setPrimaryKeyAutoIncrementToOne();
+	
 
 }
