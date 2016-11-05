52a955413b707ca7949148837c7df1872a23afb8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-05 08:08:16 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorTabUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IFieldVendorTabUserDAO.java
@@ -23,4 +23,5 @@ public interface IFieldVendorTabUserDAO extends GenericDao<FieldVendorTabUser, L
 	public Long getTodayActiveMbrsCount(Long stateId,Long districtId,Long constituencyId,Long cadreRegUserId,Long userId,Date today);
 	public Long getOneHourActiveUsersCount(Long stateId,Long districtId,Long constituencyId,Long cadreRegUserId,Long userId,Date lastHourTime,Date today);
 	public Long getAssignedUsersCountForRegUser(Long cadreRegUserId);
+	public List<Object[]> getLocationWiseIssuesCount(String issueType,Long issueStatusId,Long stateId,Date fromDate,Date toDate);
 }
