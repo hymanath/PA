696a5f546af5f6dcd29658d956d1e6bf88b5cd25
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-07 07:52:06 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardCadreRegistrationService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardCadreRegistrationService.java
@@ -37,7 +37,7 @@ public interface ICoreDashboardCadreRegistrationService {
 	public List<UserTypeVO> getSelectedChildTypeMembersForCadreReg(Long parentActivityMemberId,List<Long> childUserTypeIds,Long stateId,String startDate, String endDate);
 	public CadreRegistratedCountVO getStateDtls(Long activityMemberId,Long stateId,String startDate, String endDate);
 	public List<CadreRegistratedCountVO> getSourceOfRegistrationDtls(Long activityMemberId,Long stateId,String startDate, String endDate);
-	public String generatingAndSavingOTPDetails(String mobileNo);
+	public String generatingAndSavingOTPDetails(Long tdpCadreId,String mobileNoStr);
 	public List<CadreRegistratedCountVO> getDtlsOfBellowLvlMember(Long activityMemberId,Long stateId,String startDate, String endDate);
 	public CadreRegistratedCountVO getEnumerationDtlsForMem(Long activityMemberId,Long stateId,String startDate, String endDate);
 	public String getOtpStatus(Long cadreId,String otp);
