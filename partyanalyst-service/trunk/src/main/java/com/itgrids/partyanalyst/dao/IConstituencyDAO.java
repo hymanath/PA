091b5a5b1d57acbbdf3ede0244e93c2937a4296e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-07 20:28:58 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyDAO.java
@@ -330,4 +330,5 @@ public interface IConstituencyDAO extends GenericDao<Constituency, Long>{
 	 public List<Object[]> getDistrictBasedOnConstituenciesId(Set<Long> constitueciesIds);
 	 public List<Object[]> getDistAndConDtslByConstituenciesIds(List<Long> constitueciesIds);
 	 public List<Object[]> getConstituenciesList(List<Long> distIds);
+	 public Long getDistId(Long locationId);
 }
