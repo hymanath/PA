ca0aaa7cb8288853e5bb53b13bbb33cd6dde1562
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-07 12:56:58 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssuePersonDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegIssuePersonDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CadreRegIssuePerson;
 
 public interface ICadreRegIssuePersonDAO extends GenericDao<CadreRegIssuePerson, Long>{
+	public List<Object[]> getCadreRegIssuePersonDetails(Date fromDate,Date toDate);
 
 }
