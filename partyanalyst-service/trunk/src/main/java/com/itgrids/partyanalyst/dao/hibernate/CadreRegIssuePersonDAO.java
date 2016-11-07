8b943f34f535dd823f90883fff89c152342e679f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-07 12:57:16 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegIssuePersonDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreRegIssuePersonDAO.java
@@ -1,13 +1,33 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import java.util.Date;
+import java.util.List;
 
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.ICadreRegIssuePersonDAO;
 import com.itgrids.partyanalyst.model.CadreRegIssuePerson;
 
-public class CadreRegIssuePersonDAO extends GenericDaoHibernate<CadreRegIssuePerson, Long>{
+public class CadreRegIssuePersonDAO extends GenericDaoHibernate<CadreRegIssuePerson, Long> implements ICadreRegIssuePersonDAO{
 
 	public CadreRegIssuePersonDAO() {
 		super(CadreRegIssuePerson.class);
+		
+	}
+	
+	public List<Object[]> getCadreRegIssuePersonDetails(Date fromDate,Date toDate){
+		Query query = getSession().createQuery("select model.cadreRegIssuePersonId," +
+				" model.cadreRegIssue.cadreRegIssueId," +
+				" model.mandal," +
+				" model.name," +
+				" model.mobileNo" +
+				" from CadreRegIssuePerson model" +
+				" where model.isDeleted = 'N' and date(model.insetedTime) between :fromDate and :toDate");
+		query.setParameter("fromDate", fromDate);
+		query.setParameter("toDate", toDate);
+		return query.list();
+		
 		
 	}
 
