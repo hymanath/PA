03a09c44b1632499ad611afd7ddbff92ea3255dc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-09 16:26:05 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegUserTabUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreRegUserTabUserDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.GISVisualizationParameterVO;
 import com.itgrids.partyanalyst.model.CadreRegUserTabUser;
 
 public interface ICadreRegUserTabUserDAO extends GenericDao<CadreRegUserTabUser, Long>{
@@ -17,15 +18,15 @@ public interface ICadreRegUserTabUserDAO extends GenericDao<CadreRegUserTabUser,
 	public List<Object[]> getAllAssignedDistricts(String searchType);
 	public Long getAssignedUsersCountForRegUser(Long cadreRegUserId);
 	
-	public List<Object[]> getFieldMonitoringUserWiseDetails();
-	public List<Object[]> getIssueTypeWiseCountsForFieldMonrUsers(Date today);
-	public List<Object[]> getIssueStatusWiseCountsForFieldMonrUsers(Date today);
+	public List<Object[]> getFieldMonitoringUserWiseDetails(GISVisualizationParameterVO inputVO);
+	public List<Object[]> getIssueTypeWiseCountsForFieldMonrUsers(Date today,GISVisualizationParameterVO inputVO);
+	public List<Object[]> getIssueStatusWiseCountsForFieldMonrUsers(Date today,GISVisualizationParameterVO inputVO);
 	public List<Object[]> getConstituencyWiseFMUsersDetails();
 	public List<Object[]> getTotalUsersCountForFMUsers();
-	public List<Object[]> getTotalRegisteredUsers();
-	public List<Object[]> getTodayStartedUsersOfFMUser(Date today);
-	public List<Object[]> getTodayTotalIssues(Date today);
-	public List<Object[]> getTodayTotalStartedIssues(Date today);
-	public List<Object[]> getStartedUsersIssueTypeWiseCountsForFieldMonrUsers(Date today);
-	public List<Object[]> getLastOneHourUsersOfFMUser(Date lastHourTime,Date today);
+	public List<Object[]> getTotalRegisteredUsers(GISVisualizationParameterVO inputVO);
+	public List<Object[]> getTodayStartedUsersOfFMUser(Date today,GISVisualizationParameterVO inputVO);
+	public List<Object[]> getTodayTotalIssues(Date today,GISVisualizationParameterVO inputVO);
+	public List<Object[]> getTodayTotalStartedIssues(Date today,GISVisualizationParameterVO inputVO);
+	public List<Object[]> getStartedUsersIssueTypeWiseCountsForFieldMonrUsers(Date today,GISVisualizationParameterVO inputVO);
+	public List<Object[]> getLastOneHourUsersOfFMUser(Date lastHourTime,Date today,GISVisualizationParameterVO inputVO);
 }
