8d96bd7390ebef46fcfb1318a854d4d3b7ebb3dd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-11 07:37:23 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateDetailsDAO.java
@@ -9,12 +9,14 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.SelfAppraisalCandidateDetails;
 
 public interface ISelfAppraisalCandidateDetailsDAO extends GenericDao<SelfAppraisalCandidateDetails, Long> {
-	public List<Object[]> getSubmittedToursLeadersDetails(Date fromDate,Date toDate,Long desigId);
+	public List<Object[]> getSubmittedToursLeadersDetails(Date fromDate,Date toDate,List<Long> desigId);
 	public List<Object[]> getCandidateDtlsList(Date startDate, Date endDate,List<Long> candidateList);
 	public List<Object[]> getToursDetailstDesignationByBasedOnUserAccessLevel(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Date fromDate,Date toDate);
 	public List<Object[]> getMemberDtls(Long candidateDtlsId);
-	public Long getTourCount(Long candidateId,List<Long> locValLst);
-	public List<Object[]> getCndWiseAndLocValWiseCountList();
+	public Long getTourCount(Long candidateId,List<Long> locValLst); 
+	public List<Object[]> getCndWiseAndLocValWiseCountList(Date fromDate,Date toDate);
 	public List<Object[]> getToursVisitedDetailsDistrictWiseBasedOnUserAccessLevel(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Date fromDate,Date toDate,String reportType);
 	public List<Object[]> getToursVisitedDetailsLocationWiseBasedOnUserAccessLevel(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Date fromDate,Date toDate,String locationType,Long candiateId);
+	public List<Object[]> getCommendAndFilePathDtls(List<Long> cndIdListForCmtAndFile, Date fromDate, Date toDate);
+	public List<Object[]> getSubmittedToursDetails(Date startDate, Date endDate, List<Long> desigIdList,Long userAccessLevelId, Set<Long> locationValueSet);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISelfAppraisalCandidateLocationDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +11,7 @@ import com.itgrids.partyanalyst.model.SelfAppraisalCandidateLocation;
 public interface ISelfAppraisalCandidateLocationDAO extends GenericDao<SelfAppraisalCandidateLocation, Long> {
 	 public List<Object[]> getCandiateLocationScopeIdAndValues(Long candidateId);
 	 public List<Object[]> getNoOfLeadersCntDesignationByBasedOnUserAccessLevel(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId);
-	 public List<Object[]> getDesigWiseAllCandidate(Long stateId,Long userAccessLevelId,Set<Long> locationValueSet);
+	 public List<Object[]> getDesigWiseAllCandidate(Long stateId,Long userAccessLevelId,Set<Long> locationValueSet, List<Long> desigList);
 	 public List<Object[]> getLocationListByCndIdAndScopeId(Set<Long> cndId, Set<Long> selfAppLocationScpId);
+	 public List<Object[]> getTotalLeadersDesignationBy(List<Long> desigIdList,Long userAccessLevelId,Set<Long> locationValueSet,Date fromDate,Date toDate);
 }
