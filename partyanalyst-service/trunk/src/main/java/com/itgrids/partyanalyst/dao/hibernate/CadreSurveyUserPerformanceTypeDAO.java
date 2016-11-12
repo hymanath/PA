e86175b35e382b47e4eb953bbe97746d1307f11d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-12 14:18:27 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserPerformanceTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserPerformanceTypeDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICadreSurveyUserPerformanceTypeDAO;
 import com.itgrids.partyanalyst.model.ActivityLocationAttendance;
@@ -10,6 +13,15 @@ public class CadreSurveyUserPerformanceTypeDAO  extends GenericDaoHibernate<Cadr
 
 	public CadreSurveyUserPerformanceTypeDAO() {
 		super(CadreSurveyUserPerformanceType.class);
+	}
+	
+	public List<Object[]> getCadrePerformanceTypeList(){
+		Query query = getSession().createQuery("select model.type," +
+				" model.cadreSurveyUserPerformanceTypeId" +
+				" from CadreSurveyUserPerformanceType model" +
+				" where model.isDeleted = 'false'" +
+				" order by model.orderNo");
+	    return query.list();
 	}
 
 }
