590384cd693ada4f556a2fed92aebb3517f9591e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-12 14:17:40 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreSurveyUserPerformanceTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICadreSurveyUserPerformanceTypeDAO.java
@@ -1,10 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActivityLocationAttendance;
 import com.itgrids.partyanalyst.model.CadreSurveyUserPerformanceType;
 
 public interface ICadreSurveyUserPerformanceTypeDAO extends GenericDao<CadreSurveyUserPerformanceType, Long>{
+	
+	public List<Object[]> getCadrePerformanceTypeList();
 
 }
