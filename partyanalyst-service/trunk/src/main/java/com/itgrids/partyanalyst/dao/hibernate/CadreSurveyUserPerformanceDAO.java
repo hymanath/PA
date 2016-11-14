c25db590bfa84d23699b65499bf67055605301ce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-14 11:52:49 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserPerformanceDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CadreSurveyUserPerformanceDAO.java
@@ -41,4 +41,13 @@ public class CadreSurveyUserPerformanceDAO extends  GenericDaoHibernate<CadreSur
 		query.setDate("today", today);
 		return query.list();
 	}
+	
+	public List<Long> getCadreSurveyUserId(Date today){
+		Query query= getSession().createQuery("select distinct model.cadreSurveyUser.cadreSurveyUserId" +
+				" from CadreSurveyUserPerformance model" +
+				" where date(model.surveyTime) = :today" +
+				" and  model.isDeleted = 'false'");
+		query.setDate("today", today);
+		return query.list();
+	}
 }
