8967acd981cb77613754e908650e729974dbd732
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-15 13:36:59 +0000

    updated by--teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreDataSourceTypeInfo.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreDataSourceTypeInfo.java
@@ -1,39 +1,31 @@
 package com.itgrids.partyanalyst.model;
 
-import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.LazyToOne;
-import org.hibernate.annotations.LazyToOneOption;
-import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name="tdp_cadre_data_source_type_info")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
-public class TdpCadreDataSourceTypeInfo extends BaseModel implements Serializable{
+public class TdpCadreDataSourceTypeInfo {
 	
 	private Long tdpCadreDataSourceTypeInfoId;
-	private Long type;
-	private Long dataSourceType;
+	private String type;
+	private String dataSourceType;
 	private Long cadre2016;
 	private Long newCadre;
-	private Date newCadrePercent;
-	private Date renewalCadre;
+	private String newCadrePercent;
+	private Long renewalCadre;
 	private String renewalCadrePercent;
-	private String insertedTime;
+	private Date insertedTime;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -45,17 +37,17 @@ public class TdpCadreDataSourceTypeInfo extends BaseModel implements Serializabl
 		this.tdpCadreDataSourceTypeInfoId = tdpCadreDataSourceTypeInfoId;
 	}
 	@Column(name = "type")
-	public Long getType() {
+	public String getType() {
 		return type;
 	}
-	public void setType(Long type) {
+	public void setType(String type) {
 		this.type = type;
 	}
 	@Column(name = "data_source_type")
-	public Long getDataSourceType() {
+	public String getDataSourceType() {
 		return dataSourceType;
 	}
-	public void setDataSourceType(Long dataSourceType) {
+	public void setDataSourceType(String dataSourceType) {
 		this.dataSourceType = dataSourceType;
 	}
 	@Column(name = "cadre_2016")
@@ -73,17 +65,17 @@ public class TdpCadreDataSourceTypeInfo extends BaseModel implements Serializabl
 		this.newCadre = newCadre;
 	}
 	@Column(name = "new_cadre_percent")
-	public Date getNewCadrePercent() {
+	public String getNewCadrePercent() {
 		return newCadrePercent;
 	}
-	public void setNewCadrePercent(Date newCadrePercent) {
+	public void setNewCadrePercent(String newCadrePercent) {
 		this.newCadrePercent = newCadrePercent;
 	}
 	@Column(name = "renewal_cadre")
-	public Date getRenewalCadre() {
+	public Long getRenewalCadre() {
 		return renewalCadre;
 	}
-	public void setRenewalCadre(Date renewalCadre) {
+	public void setRenewalCadre(Long renewalCadre) {
 		this.renewalCadre = renewalCadre;
 	}
 	@Column(name = "renewal_cadre_percent")
@@ -94,11 +86,12 @@ public class TdpCadreDataSourceTypeInfo extends BaseModel implements Serializabl
 		this.renewalCadrePercent = renewalCadrePercent;
 	}
 	@Column(name = "inserted_time")
-	public String getInsertedTime() {
+	public Date getInsertedTime() {
 		return insertedTime;
 	}
-	public void setInsertedTime(String insertedTime) {
+	public void setInsertedTime(Date insertedTime) {
 		this.insertedTime = insertedTime;
 	}
-
+	
+	
 }
