bf76c5e51dff87af5a62d8cfae85068c7dc6070e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-15 16:35:22 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataSourceTypeInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataSourceTypeInfoDAO.java
@@ -1,9 +1,18 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreDataSourceTypeInfo;
 
 public interface ITdpCadreDataSourceTypeInfoDAO extends GenericDao<TdpCadreDataSourceTypeInfo, Long>{
-
+	
+	public int deleteAllRecords();
+	
+	public List<Object[]> getTdpCadreCountsByDataDourceType(Date date);
+	public List<Object[]> getRenewalTdpCadreCountsByDataDourceType(Date date);
+	public List<Object[]> getTdpCadreRecordsCountByPartyOffice(Date date);
+	public List<Object[]> getRenewalTdpCadreRecordsCountByPartyOffice(Date date);
 }
