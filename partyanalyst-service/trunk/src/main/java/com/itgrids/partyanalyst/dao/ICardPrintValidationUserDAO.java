e0326de4b50a753f8c8ed39c0b50807fc2db0d85
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-15 16:35:22 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationUserDAO.java
@@ -5,5 +5,6 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.CardPrintValidationUser;
 
 public interface ICardPrintValidationUserDAO extends GenericDao<CardPrintValidationUser,Long>{
-
+	
+	public  Object[] getCardPrinterUserDetails(String username,String password);
 }
