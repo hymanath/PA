d53f155f9ebe53221138be36679de4d00ae042b4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-15 11:01:53 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITabUserLocationDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITabUserLocationDetailsDAO.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.GISVisualizationParameterVO;
 import com.itgrids.partyanalyst.model.TabUserLocationDetails;
 
 public interface ITabUserLocationDetailsDAO extends GenericDao<TabUserLocationDetails, Long>{
 
 	public List<Object[]> getLattitudeLangitudeOfTabUser(Long locationId,Date startDate,Date endDate,String type);
+	public List<Object[]> getLattitudLangitudeOfTabUser(GISVisualizationParameterVO inputVO,Date startDate,Date endDate);
 	//public List<Object[]> getLocationWiseTabUserTrackingDetails(GISVisualizationParameterVO inputVO,String type);
 	public List<Object[]> getSurveyUserTrackingDtls(Long cadreSurveyUserId,Date fromDate,Date toDate);
 	public List<Object[]> getSurveyUserLatestTimeLongitudeAndLatituedeLocationWise(String locationType,List<Long> locationValues,Date fromDate,Date toDate);
