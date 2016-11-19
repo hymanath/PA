1f3c4d43bd529757faed3dad05f0195867139285
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-19 08:54:34 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataVerificationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreDataVerificationDAO.java
@@ -8,4 +8,7 @@ import com.itgrids.partyanalyst.model.TdpCadreDataVerification;
 
 public interface ITdpCadreDataVerificationDAO extends GenericDao<TdpCadreDataVerification, Long>{
 	public Long getActiveTeamMemberCnt(Long stateId);
+	public Long getTotalRegistered(Long stateId,Long districtId,Long constituencyId,Long cadreSurveyUserId,Date fromDate,Date toDate);
+	public Long getVerifiedPassedCount(Long stateId,Long districtId,Long constituencyId,Long cadreSurveyUserId,Date fromDate,Date toDate);
+	public Long getVerifiedRejectedCount(Long stateId,Long districtId,Long constituencyId,Long cadreSurveyUserId,Date fromDate,Date toDate);
 }
