dae8edd1b6455b7e0d3030f37cdc77c7925b6a50
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-19 20:40:09 +0000

    Updated By Kamalakar Dandu

---
+++ VoterData/src/main/java/com/itgrids/voterdata/service/mongodb/IMongoConstants.java
@@ -0,0 +1,8 @@
+package com.itgrids.voterdata.service.mongodb;
+
+public interface IMongoConstants {
+
+	public static final int  MONGO_PORT = 27017;
+	public static final String  MONGO_URL = "mytdp.in";
+	
+}
