e463dbf7bc0ec94a96e5fab6ca078c7f932ac219
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-21 11:10:29 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
@@ -7,7 +7,7 @@ import com.itgrids.partyanalyst.dto.ToursBasicVO;
 
 public interface ICoreDashboardToursService {
 	public ToursBasicVO getToursBasicOverviewCountDetails(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId,Long userTypeId);
-	public List<List<ToursBasicVO>> getDesigWiseMemberDtls(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId,Long userTypeId);
+	public List<List<ToursBasicVO>> getDesigWiseMemberDtls(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId,Long userTypeId, String level);
 	public List<ToursBasicVO> getDistrictWiseToursSubmitedDetails(Long stateId,String fromDateStr,String toDateStr,Long activityMemberId,Long userTypeId);
 	public ToursBasicVO getTopPoorToursLocationDetails(Long activityMemberId,Long userTypeId,Long stateId,String fromDateStr,String toDateStr);
 	public List<ToursBasicVO> getMemberDtlsForADesignation(List<Long> disigList,Long stateId,String fromDateStr,String toDateStr,Long activityMemberId);
