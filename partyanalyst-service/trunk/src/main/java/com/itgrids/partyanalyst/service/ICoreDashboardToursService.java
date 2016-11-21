68646ea1b37402aa372f7f633f7f93a6ab26a87b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-21 12:10:50 +0000

    UPDATED BY SRINU

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardToursService.java
@@ -15,7 +15,7 @@ public interface ICoreDashboardToursService {
 	public ToursBasicVO getDesignationDtls(Long activityMemberId, List<Long> desigIdList, String startDateStr, String endDateStr);
 	public ToursBasicVO getLeaderAverageToursBasedOnAccessLevel(Long candidateId,Long stateId,String fromDateStr,String toDateStr,Long userTypeId);
 	public List<ToursBasicVO> getTourSubmittedLeadersDetails(List<Long> designationIds,String isSubmitted,String fromDateStr,String toDateStr,Long acitvityMemberId,Long stateId,Long userTypeId);
-	public List<TabLoginAuthVO> getTabLoginDetails(String cadreSurveyUserName);
-	public List<TabLoginAuthVO> getTabUserDetails(String imeiNo);
+	public List<TabLoginAuthVO> getDetailsByUserName(String userName);
+	public List<TabLoginAuthVO> getUpdatedIMEINumberDetails(String imeiNo);
 	public String savingTabUserDetails(final Long loginUserId,final String userName,final String imeiNo);
 }
