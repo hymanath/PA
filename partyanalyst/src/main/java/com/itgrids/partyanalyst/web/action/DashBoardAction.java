cae1f33156fea4500d4748ca2f03ad496b26a335
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-22 08:11:54 +0000

    updated by srishailam

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/DashBoardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/DashBoardAction.java
@@ -566,6 +566,10 @@ public class DashBoardAction extends ActionSupport implements ServletRequestAwar
 		if(user != null && user.getEntitlements() != null && user.getEntitlements().size()>0){
 			entitlements = user.getEntitlements();
 			Long userId = user.getRegistrationID();
+			if(entitlements.contains("CADRE_WEB_REGISTRATION_2016"))
+			{
+				return "cadrewebregistration2016";
+			}
 			if(entitlements.contains("TOUR_USER_ENTITLEMENT")){
 				return "tourUserEntitlement";      
 			}
