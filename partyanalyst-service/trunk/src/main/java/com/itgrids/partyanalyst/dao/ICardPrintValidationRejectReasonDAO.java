575de1f6a839851f881dca3f798c31d9643c2ee3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-23 04:46:38 +0000

    Card Printing Web Service calls - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationRejectReasonDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CardPrintValidationRejectReason;
+
+public interface ICardPrintValidationRejectReasonDAO extends GenericDao<CardPrintValidationRejectReason,Long>{
+
+}
