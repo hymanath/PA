9949f3008468cf9617492abb24957a2422ef23bd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-23 04:46:53 +0000

    Card Printing Web Service calls - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreCardPrintDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreCardPrintDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreCardPrint;
 
 public interface ITdpCadreCardPrintDAO extends GenericDao<TdpCadreCardPrint,Long>{
-
+	
+	public List<Object[]> getCardPrintDetailsByMemberShipId(String memberShipId);
 }
