f8d01884f778885e79e402fb9293e7e3f5087e7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-23 04:49:47 +0000

    Card Printing Web Service calls - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreCardPrintDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreCardPrintDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITdpCadreCardPrintDAO;
 import com.itgrids.partyanalyst.model.TdpCadreCardPrint;
@@ -10,4 +13,19 @@ public class TdpCadreCardPrintDAO extends GenericDaoHibernate<TdpCadreCardPrint,
 	public TdpCadreCardPrintDAO() {
 		super(TdpCadreCardPrint.class);
 	}
+	
+	public List<Object[]> getCardPrintDetailsByMemberShipId(String memberShipId){
+		
+		Query query = getSession().createQuery("" +
+		" select model.tdpCadreId , model.memberShipId , model.cadreName , model.imagePath," +//3
+		"        model.districtName , model.constituencyName , model.mandalName , model.panchayatName," +//7
+		"        model.muncipalityName,model.wardName,model.boothName,model.areaCovered,model.houseNo," +//12
+		"        model.cardPrintVendorId,model.boxNo" +//14
+		" from   TdpCadreCardPrint model " +
+		" where  model.tdpCadre.isDeleted = 'N' and model.tdpCadre.enrollmentYear = 2014 and " +
+		"        model.memberShipId = :memberShipId " );
+		query.setParameter("memberShipId",memberShipId );
+		return query.list();
+	}
+	
  }
