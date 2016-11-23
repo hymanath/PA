4e884fec0aeec0d164fd46c7570c3decb1680b50
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-23 04:51:22 +0000

    Card Printing Web Service calls - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IWebServiceHandlerServiceForCadre.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IWebServiceHandlerServiceForCadre.java
@@ -2,11 +2,13 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 
-import com.itgrids.partyanalyst.dto.CadreRegistrationVO;
 import com.itgrids.partyanalyst.dto.CardPrintValidationUserVO;
+import com.itgrids.partyanalyst.dto.CardPrintValidationVO;
 import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.dto.ImageCadreVO;
 import com.itgrids.partyanalyst.dto.NewCadreRegistrationVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.dto.TdpCadrePrintDetailsVO;
 import com.itgrids.partyanalyst.dto.TdpCadreVO;
 import com.itgrids.partyanalyst.dto.VoterSearchVO;
 import com.itgrids.partyanalyst.dto.WebServiceCadreVO;
@@ -23,5 +25,8 @@ public interface IWebServiceHandlerServiceForCadre {
 	public List<TdpCadreVO> getTdpCadresBySearch(WebServiceCadreVO inputVO);
 	
 	public void saveCadreImage(ImageCadreVO inputVO);
+	
 	public CardPrintValidationUserVO validateCardPrintUserLogin(String username,String password);
+	public TdpCadrePrintDetailsVO getTdpCadrePrintDetailsByMemberShipId(String memberShipId);
+	public ResultStatus updateCardPrintValidStatus(CardPrintValidationVO inputVO);
 }
