c3bf5850fa82dfc140a63ce1508d435cd2a62ba5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-23 04:50:17 +0000

    Card Printing Web Service calls - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TdpCadrePrintDetailsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/TdpCadrePrintDetailsVO.java
@@ -8,6 +8,22 @@ public class TdpCadrePrintDetailsVO implements Serializable{
 
 	private Long   tdpCadreId;
 	private String memberShipId;
+	private String cadreName;
+	private String imagePath;
+	
+	private String districtName;
+	private String constituencyName;
+	private String mandalName;
+	private String panchayatName;
+	private String muncipalityName;
+	private String wardName;
+	private String boothName;
+	private String areaCovered;
+	private String houseNo;
+	
+	private Long cardPrintVendorId;
+	private String boxNo;
+	
 	public Long getTdpCadreId() {
 		return tdpCadreId;
 	}
@@ -19,6 +35,85 @@ public class TdpCadrePrintDetailsVO implements Serializable{
 	}
 	public void setMemberShipId(String memberShipId) {
 		this.memberShipId = memberShipId;
+	}
+	public String getCadreName() {
+		return cadreName;
+	}
+	public void setCadreName(String cadreName) {
+		this.cadreName = cadreName;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public String getConstituencyName() {
+		return constituencyName;
+	}
+	public void setConstituencyName(String constituencyName) {
+		this.constituencyName = constituencyName;
+	}
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	public String getPanchayatName() {
+		return panchayatName;
+	}
+	public void setPanchayatName(String panchayatName) {
+		this.panchayatName = panchayatName;
+	}
+	public String getMuncipalityName() {
+		return muncipalityName;
+	}
+	public void setMuncipalityName(String muncipalityName) {
+		this.muncipalityName = muncipalityName;
+	}
+	public String getWardName() {
+		return wardName;
+	}
+	public void setWardName(String wardName) {
+		this.wardName = wardName;
+	}
+	public String getBoothName() {
+		return boothName;
+	}
+	public void setBoothName(String boothName) {
+		this.boothName = boothName;
+	}
+	public String getAreaCovered() {
+		return areaCovered;
+	}
+	public void setAreaCovered(String areaCovered) {
+		this.areaCovered = areaCovered;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	public Long getCardPrintVendorId() {
+		return cardPrintVendorId;
+	}
+	public void setCardPrintVendorId(Long cardPrintVendorId) {
+		this.cardPrintVendorId = cardPrintVendorId;
+	}
+	
+	public String getBoxNo() {
+		return boxNo;
+	}
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
 	}
 	
 }
