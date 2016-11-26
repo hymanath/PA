07a11ed9bd9a4e90c80f0bd4fe596c1ca21d4ab5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-26 05:29:11 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreAgeInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreAgeInfoDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreAgeInfo;
@@ -7,4 +9,11 @@ import com.itgrids.partyanalyst.model.TdpCadreAgeInfo;
 public interface ITdpCadreAgeInfoDAO extends GenericDao<TdpCadreAgeInfo,Long>{
 	
 	public int pushCadreCountsLocationWiseByAge();
+	
+	public List<Object[]> getStateWiseAgeWiseCadreCounts(Long stateId);
+	public List<Object[]> getDistrictwiseAgeWiseCadreCounts(Long stateId );
+	public List<Object[]> getConstituencyWiseAgeWiseCadreCounts(Long stateId , Long districtId );
+	
+	public List<Object[]> getStateWiseAgeWiseCadreCountsByConstituencies(Long stateId);
+	
 }
