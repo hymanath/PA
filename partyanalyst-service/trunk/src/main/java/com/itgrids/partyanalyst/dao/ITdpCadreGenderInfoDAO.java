2fe50cffc9b4d66652a4cbe3b89be2e5418c6a38
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-28 12:40:10 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.TdpCadreGenderInfo;
@@ -7,4 +9,8 @@ import com.itgrids.partyanalyst.model.TdpCadreGenderInfo;
 public interface ITdpCadreGenderInfoDAO extends GenericDao<TdpCadreGenderInfo,Long>{
 	
 	public int pushCadreCountsLocationWiseByGender();
+	
+	public List<Object[]> stateWiseCadreGenderCounts(Long stateId);
+	public List<Object[]> districtWiseCadreGenderCounts(Long stateId , Long districtId );
+	public List<Object[]> constituencyWiseCadreGenderCounts(Long stateId , Long districtId );
 }
