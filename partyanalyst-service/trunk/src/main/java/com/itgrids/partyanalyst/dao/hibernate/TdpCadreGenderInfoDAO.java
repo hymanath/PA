616aabc3706f54fd7452e2df3fabc18df42dcf98
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-28 12:40:24 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreGenderInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TdpCadreGenderInfoDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
 
@@ -17,4 +19,78 @@ public class TdpCadreGenderInfoDAO extends GenericDaoHibernate<TdpCadreGenderInf
 		Query query = getSession().createSQLQuery("CALL tdp_cadre_gender_info(); ");
 		return query.executeUpdate();  
 	}
+	
+	public List<Object[]> stateWiseCadreGenderCounts(Long stateId){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select info.gender , sum(info.cadre2014) , sum(info.cadre2016) , sum(info.newCadre) , sum(info.renewalCadre) " +
+				"   from   TdpCadreGenderInfo info ");
+		if(stateId != null && stateId > 0l){
+			sb.append(" where info.stateId = :stateId ");
+		}
+		sb.append(" group by info.gender");
+		Query query = getSession().createQuery(sb.toString());
+		
+		if(stateId != null && stateId > 0l){
+		  query.setParameter("stateId",stateId);
+		}
+		return query.list();
+	}
+	
+	public List<Object[]> districtWiseCadreGenderCounts(Long stateId , Long districtId ){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		
+		sb.append(" select  info.gender , sum(info.cadre2014) , sum(info.cadre2016) , sum(info.newCadre) , sum(info.renewalCadre), " +//4
+				"           info.district.districtId,info.district.districtName,info.state.stateId,info.state.stateName " +//8
+				"   from   TdpCadreGenderInfo info " +
+				"   where  info.locationScopeId = 4 ");
+		
+		if(stateId != null && stateId > 0l){
+			sb.append(" and info.stateId = :stateId ");
+		}
+		if(districtId != null && districtId > 0l){
+			sb.append(" and info.districtId = :districtId ");
+		}
+		sb.append(" group by info.districtId , info.gender " +
+				"   order by info.state.stateName ,info.district.districtName ");
+		
+		Query query = getSession().createQuery(sb.toString());
+		
+		if(stateId != null && stateId > 0l){
+			  query.setParameter("stateId",stateId);
+		}
+		if(districtId != null && districtId > 0l){
+			  query.setParameter("districtId",districtId);
+		}
+		return query.list();
+	}
+	
+	public List<Object[]> constituencyWiseCadreGenderCounts(Long stateId , Long districtId ){
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	sb.append("select info.gender , sum(info.cadre2014) , sum(info.cadre2016) , sum(info.newCadre) , sum(info.renewalCadre) ," +
+    			"         C.constituencyId,C.name,info.districtId ,info.district.districtName" +
+    			"  from   TdpCadreGenderInfo info , Constituency C " +
+    			"  where  info.locationValue = C.constituencyId and " +
+    			"         info.locationScopeId = 4 ");
+    	if(stateId != null && stateId > 0l){
+			sb.append(" and info.stateId = :stateId");
+		}
+    	if(districtId != null && districtId > 0l){
+    		sb.append(" and info.districtId = :districtId");
+    	}
+    	sb.append(" group by C.constituencyId, info.gender ");
+    	
+    	Query query = getSession().createQuery(sb.toString());
+    	if(stateId != null && stateId > 0l){
+    		query.setParameter("stateId", stateId);
+		}
+    	if(districtId != null && districtId > 0l){
+    		query.setParameter("districtId", districtId);
+    	}
+    	return query.list();
+    }
 }
