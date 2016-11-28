640e663faa1e5ea6f3db1e3028efc20b2386cb4d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-11-28 16:40:44 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/utils/IConstants.java
@@ -1224,4 +1224,7 @@ public interface IConstants {
 	public static final Long STATE_SUB_LEVEL_DESIG_IDS [] = {1L,2L,3L,4L,5L,6L,7L,8L,9L};
 	
 	public static final String CADRE_NEW_MINORITY_CASTE_IDS = "81,97,107,292,299,300,301";
+	
+	public static final Long CNP_VISHAKAPATTANAM_RURAL_DISTRICT_ID=334L;
+	public static final Long CNP_MANCHERIAL_DISTRICT_ID=0L;
 }
