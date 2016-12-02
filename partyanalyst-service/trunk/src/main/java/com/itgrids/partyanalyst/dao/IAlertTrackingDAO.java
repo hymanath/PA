9369926bfdfa517398979d32a675c01518d7ea7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-02 13:04:08 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertTrackingDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertTrackingDAO.java
@@ -9,6 +9,7 @@ import com.itgrids.partyanalyst.model.AlertUserType;
 
 public interface IAlertTrackingDAO extends GenericDao<AlertTracking, Long> {
 	public List<Object[]> getAlertTrackingDetails(Long alertId);
+	public List<Object[]> getAlertTrackingDetailsList(Long alertId);
 	
 
 }
