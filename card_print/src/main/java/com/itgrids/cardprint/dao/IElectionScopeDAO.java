5bcfc435169aac1919ffc3bd08c08384cc46144a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 13:08:30 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IElectionScopeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.cardprint.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.cardprint.model.ElectionScope;
+
+public interface IElectionScopeDAO extends GenericDao<ElectionScope, Long>{
+
+}
