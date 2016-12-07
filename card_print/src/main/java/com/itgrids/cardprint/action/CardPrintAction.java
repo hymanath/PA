92cc7c6d25bfcf4955bc31ec243b50598ff082db
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 16:37:27 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/action/CardPrintAction.java
+++ card_print/src/main/java/com/itgrids/cardprint/action/CardPrintAction.java
@@ -3,12 +3,16 @@ package com.itgrids.cardprint.action;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
 import com.itgrids.cardprint.dto.BasicVO;
+import com.itgrids.cardprint.dto.PrintStatusUpdateVO;
+import com.itgrids.cardprint.dto.ResultStatus;
+import com.itgrids.cardprint.dto.UserVO;
 import com.itgrids.cardprint.service.ICardPrintService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -22,6 +26,7 @@ public class CardPrintAction extends ActionSupport implements ServletRequestAwar
 	private String task;
 	private JSONObject jobj;
 	private List<BasicVO> basicVOList;
+	private ResultStatus resultStatus;
 	
 	//Attributes
 	private ICardPrintService cardPrintService;
@@ -56,6 +61,13 @@ public class CardPrintAction extends ActionSupport implements ServletRequestAwar
 	}
 	public void setBasicVOList(List<BasicVO> basicVOList) {
 		this.basicVOList = basicVOList;
+	}
+	
+	public ResultStatus getResultStatus() {
+		return resultStatus;
+	}
+	public void setResultStatus(ResultStatus resultStatus) {
+		this.resultStatus = resultStatus;
 	}
 	//Implementation Methods
 	public void setServletRequest(HttpServletRequest request) {
@@ -95,6 +107,35 @@ public class CardPrintAction extends ActionSupport implements ServletRequestAwar
 	}
 	
 	public String getCardPrintUpdationDetails(){
+		return Action.SUCCESS;
+	}
+	
+
+	public String  saveConstituencyPrintStatus(){
+		try{
+			
+			HttpSession session = request.getSession();
+			UserVO user = (UserVO) session.getAttribute("USER");
+			if(user == null || user.getUserId() == null){
+			    resultStatus = new ResultStatus();
+				resultStatus.setResultCode(0);
+				resultStatus.setExceptionMsg("USER HAS LOGGED OUT.PLEASE SIGN IN..");
+				return Action.SUCCESS;
+			}
+			
+			jobj = new JSONObject(getTask());
+			
+			PrintStatusUpdateVO inputVO = new PrintStatusUpdateVO();
+			inputVO.setUserId(user.getUserId());
+			inputVO.setConstituencyId(jobj.getLong("constituencyId"));
+			inputVO.setPrintStatusId(jobj.getLong("printStatusId"));
+			inputVO.setRemarks(jobj.getString("remarks"));
+			
+			resultStatus = cardPrintService.saveConstituencyPrintStatus(inputVO);
+			
+		}catch(Exception e){
+			LOG.error("Exception Occurred At saveConstituencyPrintStatus() in CardPrintAction class",e) ;
+		}
 		return Action.SUCCESS;
 	}
 }
