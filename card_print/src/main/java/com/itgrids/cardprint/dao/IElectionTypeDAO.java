e65d000276553e1dbec1a21ddc2d51590ab71c0a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 13:08:30 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IElectionTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.cardprint.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.cardprint.model.ElectionType;
+
+public interface IElectionTypeDAO extends GenericDao<ElectionType, Long>{
+
+}
---
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IStateDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.cardprint.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.cardprint.model.State;
+
+public interface IStateDAO extends GenericDao<State, Long> {
+
+}
