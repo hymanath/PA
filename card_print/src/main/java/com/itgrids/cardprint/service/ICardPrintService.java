ed18a5369f150493e15e7b9267b355e7a0b4d86d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 11:47:17 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/service/ICardPrintService.java
+++ card_print/src/main/java/com/itgrids/cardprint/service/ICardPrintService.java
@@ -3,8 +3,12 @@ package com.itgrids.cardprint.service;
 import java.util.List;
 
 import com.itgrids.cardprint.dto.BasicVO;
+import com.itgrids.cardprint.dto.ResultStatus;
 
 public interface ICardPrintService {
 	
 	public List<BasicVO>  getAllVendors();
+	public List<BasicVO>  getAllPrintStatus();
+	
+	public ResultStatus saveConstituencyPrintStatus(final Long constituencyId , final Long printVendorId , final Long printStatusId,final String remarks,final Long userId);
 }
