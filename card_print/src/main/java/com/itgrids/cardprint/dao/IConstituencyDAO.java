1402a25ba5c7374cf52b824c2038c7846c94f105
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 13:08:05 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.cardprint.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.cardprint.model.Constituency;
 
 public interface IConstituencyDAO extends GenericDao<Constituency, Long> {
-
+	
+	public List<Object[]> getAllAssemblyConstituencies();
 }
