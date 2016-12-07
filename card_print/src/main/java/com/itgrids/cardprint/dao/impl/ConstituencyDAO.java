d3eb2e389e70b4d7d928b31456e2b7b56fbe8fa2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 13:08:40 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/impl/ConstituencyDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/impl/ConstituencyDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.cardprint.dao.impl;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.cardprint.dao.IConstituencyDAO;
 import com.itgrids.cardprint.model.Constituency;
@@ -12,5 +13,14 @@ public class ConstituencyDAO extends GenericDaoHibernate<Constituency, Long> imp
 	public ConstituencyDAO(){
 		super(Constituency.class);
 	}
-
+	
+	public List<Object[]> getAllAssemblyConstituencies() {
+		
+		Query query = getSession().createQuery("" +
+		" select model.constituencyId , model.name from Constituency model " +
+		" where  model.electionScope.electionType.electionTypeId = 2 and model.deformDate is null " +
+		" order by model.name");
+		
+		return query.list();
+	}
 }
