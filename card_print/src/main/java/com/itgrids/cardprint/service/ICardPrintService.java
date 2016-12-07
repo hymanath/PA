a4a885fb7029847ad8a3500d6203f325e4623d4c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 11:00:57 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/service/ICardPrintService.java
@@ -0,0 +1,10 @@
+package com.itgrids.cardprint.service;
+
+import java.util.List;
+
+import com.itgrids.cardprint.dto.BasicVO;
+
+public interface ICardPrintService {
+	
+	public List<BasicVO>  getAllVendors();
+}
