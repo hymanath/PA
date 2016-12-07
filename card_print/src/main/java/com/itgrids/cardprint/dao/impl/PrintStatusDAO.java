ed19174562d0a5d3283a6645727458defb4bef77
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 11:46:45 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/impl/PrintStatusDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/impl/PrintStatusDAO.java
@@ -3,15 +3,20 @@ package com.itgrids.cardprint.dao.impl;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.cardprint.dao.IPrintStatusDAO;
 import com.itgrids.cardprint.model.PrintStatus;
-import com.itgrids.cardprint.model.User;
 
 public class PrintStatusDAO extends GenericDaoHibernate<PrintStatus, Long> implements IPrintStatusDAO {
 
 	public PrintStatusDAO(){
 		super(PrintStatus.class);
+	}
+	
+	public List<Object[]> getAllPrintStatus(){
+		Query query = getSession().createQuery(" select model.printStatusId , model.printStatus , model.description from PrintStatus model");
+		return query.list();
 	}
 
 }
