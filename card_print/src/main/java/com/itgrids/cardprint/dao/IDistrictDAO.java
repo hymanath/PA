1e2a493f0e50efe53d4e29fb0bd4e6f0ffd328ce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 13:08:20 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IDistrictDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.cardprint.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.cardprint.model.District;
+
+public interface IDistrictDAO extends GenericDao<District, Long>{
+
+}
