318c25a3e29f9426f996e202a9c673e012c4e159
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 11:00:27 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/ICardPrintVendorDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/ICardPrintVendorDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.cardprint.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.cardprint.model.CardPrintVendor;
 
 public interface ICardPrintVendorDAO extends GenericDao<CardPrintVendor, Long> {
-
+	
+	public List<Object[]> getAllVendors();
 }
