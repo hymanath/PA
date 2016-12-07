e08b0ce5a85fccddd887709731703904eacf4727
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-07 11:46:36 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/IPrintStatusDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IPrintStatusDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.cardprint.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.cardprint.model.PrintStatus;
 
 public interface IPrintStatusDAO extends GenericDao<PrintStatus, Long> {
-
+	
+	public List<Object[]> getAllPrintStatus();
 }
