42fdfaafd2d9817af178d2dbf08768c31416d4c7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-08 07:05:24 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IConstituencyPrintStatusDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ConstituencyPrintStatus;
+
+public interface IConstituencyPrintStatusDAO extends GenericDao<ConstituencyPrintStatus, Long> {
+
+}
