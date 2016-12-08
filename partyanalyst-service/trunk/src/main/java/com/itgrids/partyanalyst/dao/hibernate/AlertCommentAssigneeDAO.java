3137a3041de9750938cd7c4f9e11f16b91dface0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-08 16:06:14 +0000

    Alert - News - Actionable  --- Balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCommentAssigneeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCommentAssigneeDAO.java
@@ -3,6 +3,7 @@ package com.itgrids.partyanalyst.dao.hibernate;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAlertCommentAssigneeDAO;
@@ -27,6 +28,26 @@ public class AlertCommentAssigneeDAO extends
 		if(alertId != null && alertId > 0)
 		query.setParameter("alertId", alertId);
 		return query.list();
+	}
+	
+	public List<Object[]> getAssignedCandidateAlertComment(Long alertId,List<Long> tdpCadreIds){
+		
+		StringBuilder str = new StringBuilder();
+		
+		str.append(" select distinct ACA.assign_tdp_cadre_id as cadreId,AC.comments as comments from alert_comment_assignee ACA,alert_comment AC " +
+				   " where ACA.alert_comment_id = AC.alert_comment_id " +
+				    " and ACA.assign_tdp_cadre_id in (:tdpCadreIds) " +
+				    " and AC.alert_id = :alertId " +
+				    " and AC.comments is not null " );
+		Query query = getSession().createSQLQuery(str.toString())
+				.addScalar("cadreId", Hibernate.LONG)
+				.addScalar("comments", Hibernate.STRING);
+		
+		query.setParameter("alertId", alertId);
+		query.setParameterList("tdpCadreIds", tdpCadreIds);
+		
+		return query.list();
+		
 	}
 
 
