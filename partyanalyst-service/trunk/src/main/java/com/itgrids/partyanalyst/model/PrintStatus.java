9de9bbd902ea849b55bf239bcd18753f3af385ea
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-08 07:06:11 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PrintStatus.java
@@ -0,0 +1,46 @@
+package com.itgrids.partyanalyst.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "print_status")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class PrintStatus extends BaseModel implements java.io.Serializable {
+	
+	private Long printStatusId;
+	private String printStatus;
+	private String description;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "print_status_id", unique = true, nullable = false)
+	public Long getPrintStatusId() {
+		return printStatusId;
+	}
+	public void setPrintStatusId(Long printStatusId) {
+		this.printStatusId = printStatusId;
+	}
+	@Column(name = "print_status")
+	public String getPrintStatus() {
+		return printStatus;
+	}
+	public void setPrintStatus(String printStatus) {
+		this.printStatus = printStatus;
+	}
+	@Column(name = "description")
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
