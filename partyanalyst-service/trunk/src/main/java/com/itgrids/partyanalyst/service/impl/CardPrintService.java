7a2b85e9f2c009acf80fd12b62004d1ea16e6136
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-08 08:06:03 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CardPrintService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.ICardPrintService;
+
+public class CardPrintService implements ICardPrintService{
+
+	private static final Logger LOG = Logger.getLogger(FieldMonitoringService.class);
+}
