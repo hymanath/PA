0d410bea3ddddb1eaf4c38037e4747c4b32aa344
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-13 10:50:32 +0000

    updated by -- SRAVANTH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CardPrintingDispatchVO.java
@@ -0,0 +1,99 @@
+package com.itgrids.partyanalyst.dto;
+
+public class CardPrintingDispatchVO {
+
+	private Long id;
+	private String name;
+	
+	private String boxNo;
+	private Long mandalId;
+	private String mandalName;
+	private Long panchayatId;
+	private String panchayatName;
+	private Long noOfCards;
+	private String isQAPassed;
+	private String errorPerc;
+	private Long validatedCardsCount;
+	private String status;
+	private Long errorCount;
+	
+	
+	public Long getErrorCount() {
+		return errorCount;
+	}
+	public void setErrorCount(Long errorCount) {
+		this.errorCount = errorCount;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBoxNo() {
+		return boxNo;
+	}
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
+	}
+	public Long getMandalId() {
+		return mandalId;
+	}
+	public void setMandalId(Long mandalId) {
+		this.mandalId = mandalId;
+	}
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	public Long getPanchayatId() {
+		return panchayatId;
+	}
+	public void setPanchayatId(Long panchayatId) {
+		this.panchayatId = panchayatId;
+	}
+	public String getPanchayatName() {
+		return panchayatName;
+	}
+	public void setPanchayatName(String panchayatName) {
+		this.panchayatName = panchayatName;
+	}
+	public Long getNoOfCards() {
+		return noOfCards;
+	}
+	public void setNoOfCards(Long noOfCards) {
+		this.noOfCards = noOfCards;
+	}
+	public String getIsQAPassed() {
+		return isQAPassed;
+	}
+	public void setIsQAPassed(String isQAPassed) {
+		this.isQAPassed = isQAPassed;
+	}
+	public String getErrorPerc() {
+		return errorPerc;
+	}
+	public void setErrorPerc(String errorPerc) {
+		this.errorPerc = errorPerc;
+	}
+	public Long getValidatedCardsCount() {
+		return validatedCardsCount;
+	}
+	public void setValidatedCardsCount(Long validatedCardsCount) {
+		this.validatedCardsCount = validatedCardsCount;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICardPrintService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICardPrintService.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.service;
 
 import java.util.List;
 import com.itgrids.partyanalyst.dto.CardPrintVO;
+import com.itgrids.partyanalyst.dto.CardPrintingDispatchVO;
 
 public interface ICardPrintService {
 	public List<CardPrintVO> getVendorNames();
@@ -9,4 +10,5 @@ public interface ICardPrintService {
 	public List<CardPrintVO> getConstListByVendor(Long vendorId,Long districtId);
 	public CardPrintVO getStatusWisePrintingConstituencyDetails(Long stateId,Long vendorId,String startDateStr,String endDateStr);
 	public CardPrintVO getDistrictWiseStatusWiseConstituenciesCounts(Long vendorId,String fromDateStr,String toDateStr);
+	public List<CardPrintingDispatchVO> getPrintingDispatchDetails(Long vendorId,Long districtId,Long constituencyId);
 }
