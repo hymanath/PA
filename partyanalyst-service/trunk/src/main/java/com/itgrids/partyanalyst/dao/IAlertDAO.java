8c8594795b6bab9c05abf554e139514ddfcbff18
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-14 05:46:13 +0000

    Alert Last Updated time showing -----Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
@@ -53,4 +53,5 @@ public interface IAlertDAO extends GenericDao<Alert, Long> {
 	List<Object[]> getAlertDtlsAssignedByPartyCommite(Long userAccessLevelId, List<Long> userAccessLevelValues, Long stateId, List<Long> impactLevelIds, Date fromDate, Date toDate, List<Long> tdpCommitteeLevelIds, Long cadreId, Long tdpBasicCommitteeId, Long designationId,Long statusId);
 	public List<Object[]> getAlertDetailsByCadreWise(Long userAccessLevelId, List<Long> userAccessLevelValues,Date fromDate, Date toDate, Long stateId,List<Long> impactLevelIds,Long tdpCadreId,Long statusId,String resultType);
 	public List<Object[]> getDistrictAndStateImpactLevelWiseAlertDtls(Long userAccessLevelId, List<Long> userAccessLevelValues,Date fromDate, Date toDate, Long stateId,List<Long> impactLevelIds,Long districtId);
+	 public Date getAlertLastUpdatedTime();
 }
