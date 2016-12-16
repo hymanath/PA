ba907bf5711f7ee43fd4a597f1dfc3408cad41b1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-16 20:58:48 +0000

    updated by - SREEDHAR

--- partyanalyst/src/main/webapp/js/userWiseCadreDemographicReport.js
+++ partyanalyst/src/main/webapp/js/userWiseCadreDemographicReport.js
@@ -64,3 +64,94 @@
 	
 	
   //AGE WISE FUNCTIONS
+  function getAgeWiseTdpCadreSummaryReport(){
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>');
+		var jobj =	{  
+			          accessType : accessType,
+                      locationArray : locationArray 
+					}
+		
+		$.ajax({
+			type : 'POST',
+			url : 'ageWiseCadreSummaryReportAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("age wise summary report..");
+		});
+	}
+	function getDistrictWisegeWiseTdpCadreCounts(){
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner "><div class="dot1"></div><div class="dot2"></div></div></div>');
+		 
+		var jobj ={ locationArray : districtArray ,searchType : "district" }
+		
+		$.ajax({
+			type : 'POST',
+			url : 'getLocationWisegeWiseCadreCountsAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("age - dist");
+		});
+	}
+	function getConstituencyWisegeWiseTdpCadreCounts(){
+		
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner "><div class="dot1"></div><div class="dot2"></div></div></div>');
+		var jobj =	{  locationArray : constituencyArray ,searchType : "constituency"  }
+		$.ajax({
+			type : 'POST',
+			url : 'getLocationWisegeWiseCadreCountsAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("age - const");
+		});
+	}
+	
+	
+	//GENDER WISE
+	function getGenderSummaryCounts(){
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>');
+		var jobj =	{  
+			          accessType : accessType,
+                      locationArray : locationArray 
+					}
+		
+		$.ajax({
+			type : 'POST',
+			url : 'getGenderSummaryCountsByUserAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("gender - summary");
+		});
+	}
+	function getDistrictWiseCadreGenderCounts(){
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner "><div class="dot1"></div><div class="dot2"></div></div></div>');
+		 
+		 var jobj ={ locationArray : districtArray ,searchType : "district"  }
+		
+		$.ajax({
+			type : 'POST',
+			url : 'locationWiseCadreGenderCountsByUserAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("gender - district");
+		});
+	}
+	function getConstituencyWiseCadreGenderCounts(stateId,districtId,searchType){
+		 //$("#").html('<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner "><div class="dot1"></div><div class="dot2"></div></div></div>');
+		 
+		var jobj =	{  locationArray : constituencyArray ,searchType : "constituency" }
+		
+		$.ajax({
+			type : 'POST',
+			url : 'locationWiseCadreGenderCountsByUserAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jobj)}
+		}).done(function(result){
+			alert("gender - constituency..");
+		});
+	}
+	
