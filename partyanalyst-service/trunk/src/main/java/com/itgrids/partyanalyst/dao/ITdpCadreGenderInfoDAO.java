1deb0e02f55a0d93b57fd5016bb7850111855a82
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-16 21:22:02 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoDAO.java
@@ -13,4 +13,8 @@ public interface ITdpCadreGenderInfoDAO extends GenericDao<TdpCadreGenderInfo,Lo
 	public List<Object[]> stateWiseCadreGenderCounts(Long stateId);
 	public List<Object[]> districtWiseCadreGenderCounts(Long stateId , Long districtId );
 	public List<Object[]> constituencyWiseCadreGenderCounts(Long stateId , Long districtId );
+	
+	public List<Object[]> privilegedStateWiseCadreGenderCounts(List<Long> locationIdList, String accessType);
+	public List<Object[]> privilegedDistrictWiseCadreGenderCounts(List<Long> districtIdList );
+	public List<Object[]> privilegedConstituencyWiseCadreGenderCounts(List<Long> locationIdList );
 }
