6c86b3a120806096720b8f5b796b72eae0640adc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-17 15:25:00 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingSessionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingSessionDAO.java
@@ -1,10 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+import java.util.Set;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.LeaderOccasionWishDetails;
 import com.itgrids.partyanalyst.model.PartyMeetingSession;
 
 public interface IPartyMeetingSessionDAO extends GenericDao< PartyMeetingSession, Long>{
-
+	public List<Object[]> getSessionDetailsForPartyMeetings(Set<Long> partyMeetingsIds);
 }
