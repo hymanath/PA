c6592009c5f26d77d230f5cf761248604e79421c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-17 08:31:25 +0000

    commit by nandhini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingSessionDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.LeaderOccasionWishDetails;
+import com.itgrids.partyanalyst.model.PartyMeetingSession;
+
+public interface IPartyMeetingSessionDAO extends GenericDao< PartyMeetingSession, Long>{
+
+}
