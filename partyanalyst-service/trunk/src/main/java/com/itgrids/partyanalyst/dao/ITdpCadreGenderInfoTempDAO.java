56dfc6c90b4a46d6ca775b303ac6f6d31a04d83a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-19 15:48:09 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoTempDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreGenderInfoTempDAO.java
@@ -5,5 +5,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.TdpCadreGenderInfoTemp;
 
 public interface ITdpCadreGenderInfoTempDAO extends GenericDao<TdpCadreGenderInfoTemp,Long> {
-
+	
+	public int deleteAllRecords();
+	
 }
