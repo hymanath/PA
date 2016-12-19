caf0df9a6bbef51bde9c3640c9e274800fe3e034
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-19 09:17:56 +0000

    committed by krishna

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreCasteStateInfoTempDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.TdpCadreCasteStateInfoTemp;
+
+public interface ITdpCadreCasteStateInfoTempDAO extends GenericDao<TdpCadreCasteStateInfoTemp,Long> {
+
+}
