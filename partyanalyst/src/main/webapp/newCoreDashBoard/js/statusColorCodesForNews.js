9ace2119c47d4d4d70cf3d9ffdc70e9ac21104b4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-19 13:43:01 +0000

    core dashboard ui build --- Aravind

--- partyanalyst/src/main/webapp/newCoreDashBoard/js/statusColorCodesForNews.js
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/statusColorCodesForNews.js
@@ -1,6 +1,8 @@
 
 var statusColorArr = [];
 var statusarr = ['TDP','YSRC','INC','BJP'];
+var specialMeetingsColorArr=[];
+var sessionArr = ['All Sessions','Session-1','Session-2','Session-3','Session-4','Session-5','Session-6','Session-7','Session-8','Session-9','Session-10'];
 function setcolorsForStatus()
 	{
 		statusColorArr = new Array();
@@ -35,6 +37,43 @@ function setcolorsForStatus()
 			
 				if(statusColorArr[i].status.toLowerCase() == status.toLowerCase())
 					return statusColorArr[i].color;
+			}
+		}
+	}
+	
+	function setSessioncolorsForStatus()
+	{
+		sessionstatusColorArr = new Array();
+		var colorStatic = new Array('#0066DC','#320199','#660032','#B114E5','#EF0EAC','#2A6EB7','#517296','#CE7393','#5410A8','#E27C88','#7744DD');
+		
+		var colorCount = 0;
+		
+		for(var i in sessionArr)
+		{
+		 
+				var obj = {
+				 status : sessionArr[i],
+				 color : colorStatic[colorCount]
+				}
+				sessionstatusColorArr.push(obj)
+			  
+				if(colorCount == (colorStatic.length)-1)
+				colorCount = 0;
+				 colorCount++;
+		}
+		
+		return sessionstatusColorArr;
+	}
+	function getColorCodeBySessionStatus(status)
+	{
+
+		if(sessionstatusColorArr != null && sessionstatusColorArr.length > 0)
+		{
+			for(var i in sessionstatusColorArr)
+			{
+			
+				if(sessionstatusColorArr[i].status.toLowerCase() == status.toLowerCase())
+					return sessionstatusColorArr[i].color;
 			}
 		}
 	}
