2eda42a1c6ac6dc616659c9a376cd1688e4c9369
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-19 10:50:55 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingSessionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingSessionDAO.java
@@ -19,32 +19,42 @@ public class PartyMeetingSessionDAO extends GenericDaoHibernate<PartyMeetingSess
 	public List<Object[]> getSessionDetailsForPartyMeetings(Set<Long> partyMeetingsIds){
 		StringBuilder queryStr = new StringBuilder();
 		queryStr.append(" select  distinct model.partyMeetingId, " +//0
-				        " model.partyMeetingSessionId, " +//1
-				        " model.sessionType.type, '' , " +//2
-				        " model.startTime, " +//4
-				        " model.endTime," +//5
-				        " model.lateTime," +//6
-				        " model.sessionType.startTime," +//7
-				        " model.sessionType.endTime," +//8
-				        " model.sessionType.lateTime, "+//9
-				        " model.sessionType.sessionTypeId   " +//10
-				        " from PartyMeetingSession model where model.isDeleted='N' and " +
+		        " model.partyMeetingSessionId, " +//1
+		        " model.sessionType.type, '' , " +//2
+		        " model.startTime, " +//4
+		        " model.endTime," +//5
+		        " model.lateTime," +//6
+		        " model.sessionType.startTime," +//7
+		        " model.sessionType.endTime," +//8
+		        " model.sessionType.lateTime, "+//9
+		        " model.sessionType.sessionTypeId   " +//10
+		        " from PartyMeetingSession model where model.isDeleted='N' and " +
 				" model.partyMeetingId in (:partyMeetingsIds)  order by model.orderNo asc ");
 		Query query = getSession().createQuery(queryStr.toString());
 		query.setParameterList("partyMeetingsIds", partyMeetingsIds);		
 		return query.list();
 	}
 	
-	public List<Object[]> getSessionDetailsForPartiMeetings(Set<Long> partyMeetingsTypeIds){
+	public List<Object[]> getSessionDetailsForPartiMeetings(Set<Long> partyMeetingsTypeIds,List<Long> partyMeetingsIds){
 		StringBuilder queryStr = new StringBuilder();
-		queryStr.append(" select  distinct model.partyMeetingId, model.partyMeetingSessionId, model.sessionType.type, '' , " +
-				" model.startTime, model.endTime,model.lateTime,model.sessionType.startTime,model.sessionType.endTime,model.sessionType.lateTime," +
-				" model.partyMeeting.partyMeetingTypeId, model.partyMeeting.meetingName " +
-				"  from PartyMeetingSession model where model.isDeleted='N' and " +
-				" model.partyMeeting.partyMeetingTypeId in (:partyMeetingsTypeIds)  order by  model.orderNo asc ");
-		Query query = getSession().createQuery(queryStr.toString());
-		query.setParameterList("partyMeetingsTypeIds", partyMeetingsTypeIds);		
-		return query.list();
+		if(partyMeetingsTypeIds != null && partyMeetingsTypeIds.size()>0){
+			queryStr.append(" select  distinct model.partyMeetingId, model.partyMeetingSessionId, model.sessionType.type, '' , " +
+					" model.startTime, model.endTime,model.lateTime,model.sessionType.startTime,model.sessionType.endTime,model.sessionType.lateTime," +
+					" model.partyMeeting.partyMeetingTypeId, model.partyMeeting.meetingName " +
+					"  from PartyMeetingSession model where model.isDeleted='N' and " +
+					" model.partyMeeting.partyMeetingTypeId in (:partyMeetingsTypeIds)  ");
+			if(partyMeetingsIds != null && partyMeetingsIds.size()>0){
+				queryStr.append(" and model.partyMeeting.partyMeetingId in (:partyMeetingIds) ");
+			}
+			queryStr.append("  order by  model.orderNo asc ");
+			Query query = getSession().createQuery(queryStr.toString());
+			query.setParameterList("partyMeetingsTypeIds", partyMeetingsTypeIds);
+			if(partyMeetingsIds != null && partyMeetingsIds.size()>0){
+				query.setParameterList("partyMeetingIds",partyMeetingsIds);
+			}
+			return query.list();
+		}
+		return null;
 	}
 	
 }
