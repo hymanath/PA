67066fa0c3eb9e44a23e61a5ed7966d70a64a75a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-22 12:02:57 +0000

    updated by srujana

---
+++ partyanalyst/src/main/webapp/js/Tours/toursDetails.js
@@ -0,0 +1,319 @@
+function getCandidateList(designationId){ 
+		var jsObj = {     
+			designationId : designationId
+		}
+		$.ajax({
+			type : 'POST',
+			url : 'getCandidateListAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			$("#memberSlctBxId").empty();
+			$("#memberSlctBxId").append("  <option value='0'>Select Name</option>");
+			if(result != null && result.length > 0){
+				for(var i in result){
+					$("#memberSlctBxId").append("<option value="+result[i].id+">"+result[i].name+"</option>");  
+				}
+				// $("#memberSlctBxId").append("<option value='-1'>Other Name</option>");  
+			}
+			$(".selectChosen").trigger("chosen:updated");
+		});
+	}
+	$(document).on("change","#designationSlctBxId",function(){
+		var designationId = $(this).val();
+		$("#selectedMemberDtslDivId").html(' ');
+		$(".showDivCls").hide();
+		 if(designationId != null && designationId > 0){
+			 getCandidateList(designationId);
+		 }
+	});
+	$(document).on("change","#memberSlctBxId",function(){
+		var candidateId = $(this).val();
+		 if(candidateId != null && candidateId > 0){
+			//$(".otherMemberBlockCls").hide();
+			//$(".showDivCls").show();
+			//$(".hideProfileDivCls").show();
+			$(".ownDivCls").hide();
+			$(".inchageDivCls").hide();
+			 getCandiateSearchDetails(candidateId);
+		 }else if(candidateId == -1){
+			//$(".otherMemberBlockCls").show();
+			//$(".showDivCls").hide();
+			//$(".hideProfileDivCls").hide();
+			 //$("#searchValueInputBoxId").val(' ');
+			 //$("#searchValueInputBoxId").attr("placeholder","Enter MemberShip Number");	
+			getConstituenciesList();
+		 }
+	});
+	function getCandiateSearchDetails(candidateId){
+    $("#selectProfileId").show();		
+	$("#selectedMemberDtslDivId").html(' ');
+	$(".showDivCls").hide();
+	var jsObj = { 
+			 candidateId : candidateId
+			}
+		$.ajax({
+			type : 'POST',
+			url : 'getCandiateDetailsAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			$("#profileProcessingImgId").hide();
+			
+			if(result != null){ 
+				buildSelectedProfileRslt(result);
+			}
+		});
+	}
+	function buildSelectedProfileRslt(result){
+		var str='';
+		  str+='<h4 class="panel-title text-capital">selected profile</h4>';
+			str+='<ul class="list-inline">';
+			str+='<li>';
+				str+='<div class="panel panel-default panelProfile">';
+					str+='<div class="panel-body">';
+						str+='<img src="http://mytdp.com/images/cadre_images/'+result.image+'" onerror="setDefaultImage(this);" style="width: 50px; height: 50px;"></img>';
+						str+='<p>'+result.name+'</p>';
+						str+='<p>'+result.memberShipNo+'</p>';
+						str+='<p>'+result.mobileNo+'</p>';
+					str+='</div>';
+					str+='<div class="panel-footer">';
+						str+='<label class="checkbox-inline">';
+							str+='<input id="profileCheckboxId" type="checkbox"/>Select Profile';
+						str+='</label>';
+					str+='</div>';
+					
+				str+='</div>';
+			str+='</li>';
+            str+='</ul>';
+		$("#selectedMemberDtslDivId").html(str);
+		
+		var locationList = result.subList;
+	     if(locationList != null && locationList.length > 0){
+			 $("#inchargeSelectBoxId").removeAttr("name");
+			 $("#inchageLocationScopeId").removeAttr("name");
+			 $("#inchageownLocationScopeValue").removeAttr("name");
+			 
+			 $("#ownSelectBoxId").removeAttr("name");
+			 $("#ownLocationScopeId").removeAttr("name");
+			 $("#ownLocationScopeValue").removeAttr("name");
+			 
+			 $("#ownLocationScopeId").removeAttr("value");
+			 $("#ownLocationScopeValue").removeAttr("value");
+			 
+			 $("#inchageLocationScopeId").removeAttr("value");
+			 $("#inchageownLocationScopeValue").removeAttr("value");
+			 
+			 $("#ownSelectBoxId").removeClass("isActive");
+			 $("#inchargeSelectBoxId").removeClass("isActive");             
+ 
+			 for(var i in locationList){
+				 if(locationList[i].locationScopeId != null && locationList[i].locationScopeId==1){
+					 if(locationList[i].type != null && locationList[i].type=="Own"){
+						 $("#ownLabelId").html("Own DIstrict");
+						 $("#ownLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#ownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#ownSelectBoxId").attr("name","toursInputVO.ownTours");
+						 $("#ownLocationScopeId").attr("name","toursInputVO.ownLocationScopeId");
+						 $("#ownLocationScopeValue").attr("name","toursInputVO.ownLocationId");
+						 $("#ownSelectBoxId").addClass("isActive");
+						 
+						 $(".ownDivCls").show();
+					 }else if(locationList[i].type != null && locationList[i].type=="Incharge"){
+						$("#inchargeLableId").html("Incharge DIstrict");
+						 $("#inchageLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#inchageownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#inchargeSelectBoxId").attr("name","toursInputVO.inchargeTours");
+						 $("#inchageLocationScopeId").attr("name","toursInputVO.inchargeLocationScopeId");
+						 $("#inchageownLocationScopeValue").attr("name","toursInputVO.inchargeLocationId");
+						 $("#inchargeSelectBoxId").addClass("isActive");
+					    $(".inchageDivCls").show();   
+					 }
+				 }else if(locationList[i].locationScopeId != null && locationList[i].locationScopeId==2){
+					 if(locationList[i].type != null && locationList[i].type=="Own"){  
+						 $("#ownLabelId").html("Own Parliament");
+						 $("#ownLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#ownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#ownSelectBoxId").attr("name","toursInputVO.ownTours");
+						 $("#ownLocationScopeId").attr("name","toursInputVO.ownLocationScopeId");
+						 $("#ownLocationScopeValue").attr("name","toursInputVO.ownLocationId");
+						 $("#ownSelectBoxId").addClass("isActive");
+						 $(".ownDivCls").show();    
+					 }else if(locationList[i].type != null && locationList[i].type=="Incharge"){
+						$("#inchargeLableId").html("Incharge Parliament");
+						 $("#inchageLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#inchageownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#inchargeSelectBoxId").attr("name","toursInputVO.inchargeTours");
+						 $("#inchageLocationScopeId").attr("name","toursInputVO.inchargeLocationScopeId");
+						 $("#inchageownLocationScopeValue").attr("name","toursInputVO.inchargeLocationId");
+						 $("#inchargeSelectBoxId").addClass("isActive");
+						 
+						$(".inchageDivCls").show(); 
+					 }
+				 }else if(locationList[i].locationScopeId != null && locationList[i].locationScopeId==3){
+					  if(locationList[i].type != null && locationList[i].type=="Own"){
+						 $("#ownLabelId").html("Own Assembly");
+						 $("#ownLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#ownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#ownSelectBoxId").attr("name","toursInputVO.ownTours");
+						 $("#ownLocationScopeId").attr("name","toursInputVO.ownLocationScopeId");
+						 $("#ownLocationScopeValue").attr("name","toursInputVO.ownLocationId");
+						 $("#ownSelectBoxId").addClass("isActive");
+						 
+						 $(".ownDivCls").show();
+					 }else if(locationList[i].type != null && locationList[i].type=="Incharge"){
+						$("#inchargeLableId").html("Incharge Assembly");
+						 $("#inchageLocationScopeId").attr("value",locationList[i].locationScopeId);
+						 $("#inchageownLocationScopeValue").attr("value",locationList[i].locationValue);
+						 
+						 $("#inchargeSelectBoxId").attr("name","toursInputVO.inchargeTours");
+						 $("#inchageLocationScopeId").attr("name","toursInputVO.inchargeLocationScopeId");
+						 $("#inchageownLocationScopeValue").attr("name","toursInputVO.inchargeLocationId");
+						 $("#inchargeSelectBoxId").addClass("isActive");
+						 
+						$(".inchageDivCls").show();     
+					 }
+				 }
+			 }
+		 }
+	}
+	getDesigationList(); //default call 
+	function getDesigationList(){ 	
+		var jsObj = { }
+		$.ajax({
+			type : 'POST',
+			url : 'getDesigationListAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			if(result != null && result.length > 0){
+				$("#designationSlctBxId").empty();
+				$("#designationSlctBxId").append("<option value='0'>Select Designation Level</option>");
+				for(var i in result){
+				 $("#designationSlctBxId").append("<option value="+result[i].id+">"+result[i].name+"</option>");	
+				}
+			}
+			$(".selectChosen").trigger("chosen:updated");
+		});
+	}
+	getAllTourCategorys();
+	function getAllTourCategorys(){ 
+		var jsObj = {     
+			
+		}
+		$.ajax({
+			type : 'POST',
+			url : 'getAllTourCategorysAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			$("#tourCategoryId").empty();
+			$("#tourCategoryId").append("  <option value='0'>Select Category</option>");
+			if(result != null && result.length > 0){
+				for(var i in result){
+					$("#tourCategoryId").append("<option value="+result[i].id+">"+result[i].name+"</option>");  
+				}
+				// $("#memberSlctBxId").append("<option value='-1'>Other Name</option>");  
+			}
+			$(".selectChosen").trigger("chosen:updated");
+		});
+	}
+	getAllTourTypes()
+	function getAllTourTypes(){ 
+		var jsObj = {     
+			
+		}
+		$.ajax({
+			type : 'POST',
+			url : 'getAllTourTypesAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			$("#tourTypeId").empty();
+			$("#tourTypeId").append("  <option value='0'>Select TourTypes</option>");
+			if(result != null && result.length > 0){
+				for(var i in result){
+					$("#tourTypeId").append("<option value="+result[i].id+">"+result[i].name+"</option>");  
+				}
+				// $("#memberSlctBxId").append("<option value='-1'>Other Name</option>");  
+			}
+			$(".selectChosen").trigger("chosen:updated");
+		});
+	}
+	function savingApplication(){
+		$("#errMnthId").html("");
+		$("#errYearId").html("");
+		$(".textErrCls").html("");
+		$("#errFileId").html("");
+		var flag = true;
+		var filerKit = $("#update_TourFileId2").prop("jFiler");
+		var monthValue = $("#monthSelectBoxId").val();
+		if(monthValue == 0){
+			$("#errMnthId").html("Please Select Month.");
+			return;
+		}
+		var yearValue = $("#yearId").val();
+		if(yearValue == 0){  
+			$("#errYearId").html("Please Select Year.");
+			return;  
+		}
+		$(".isActive").each(function(){
+			var value = $(this).val();
+			var bool = $.isNumeric(value);
+			if(bool == false){
+				var errId = $(this).attr("attr_err_id");
+				$("#"+errId).html("Please Enter Numbers.");
+				flag = false;
+				return false;      
+			}  
+		});
+		
+		var childEleCount = $(".jFiler-items-list").children().length;
+		if(childEleCount > 1){
+			$("#errFileId").html("Please Select Only One Document."); 
+			filerKit.reset();   
+			flag = false;         			
+		}
+		
+		if(flag == false){
+			return;  
+		}
+		var uploadHandler = { 
+			upload: function(o) {
+				$("#savingAjaxImg").css("display","none");
+				uploadResult = o.responseText;
+				showSbmitStatus(uploadResult);
+			}
+		};
+		YAHOO.util.Connect.setForm('submitApplication',true);  
+		YAHOO.util.Connect.asyncRequest('POST','savingTourDtlsApplicationAction.action',uploadHandler);
+	}
+	function showSbmitStatus(uploadResult){
+			$(".clearFieldCls").val(' ');
+			$("#monthSelectBoxId").val(0);
+			$("#yearId").val(0);    
+			$("#update_TourFileId2").val('');        
+			$(".selectChosen").trigger("chosen:updated");
+			var filerKit = $("#update_TourFileId2").prop("jFiler");
+			//if(uploadResult != null && uploadResult == '<pre>"Saved"</pre>'){
+			setTimeout(function () {
+			$("#successSpanId").html("<center style='color: green; font-size: 16px;'>Saved Successfully</center>").fadeOut(3000);
+			}, 500);  
+			$("#successSpanId").show();
+			$("#successSpanId").html("");       
+			filerKit.reset();
+			/* }else{
+				setTimeout(function () {      
+					$("#successSpanId").html("<center style='color: red; font-size: 16px;'>Failed</center>").fadeOut(3000);
+					}, 500);  
+					$("#successSpanId").show();
+					$("#successSpanId").html("");          
+			} */
+				
+	}
+
--- partyanalyst/src/main/webapp/jsp/toursDetails.jsp
+++ partyanalyst/src/main/webapp/jsp/toursDetails.jsp
@@ -7,9 +7,21 @@
 <link href="Assets/css/bootstrap.css" rel="stylesheet" type="text/css">
 <link href="dist/tourDetails/tours_custom.css" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
-
-<script src="js/Tours/toursDetails.js" type="text/javascript"></script>
-
+<link href="dist/Plugins/Chosen/chosen.css" rel="stylesheet" type="text/css">
+<!-- for file uploader -->
+<link href="dragAndDropPhoto/css/jquery.filer.css" type="text/css" rel="stylesheet" />
+<link href="dragAndDropPhoto/css/themes/jquery.filer-dragdropbox-theme.css" type="text/css" rel="stylesheet" />  
+<!-- for file uploader -->
+<script type="text/javascript" src="js/yahoo/yahoo-min.js"></script>
+<script type="text/javascript" src="js/yahoo/yahoo-dom-event.js"></script> 
+<script type="text/javascript" src="js/yahoo/animation-min.js"></script> 	
+<script type="text/javascript" src="js/yahoo/container-min.js"></script> 
+<script type="text/javascript" src="js/yahoo/dom-min.js"></script> 
+<script type="text/javascript" src="js/yahoo/yui-min.js"></script>
+<script type="text/javascript" src="js/yahoo/connection-min.js"></script> 
+<script type="text/javascript" src="js/yahoo/datasource-min.js"></script>  
+<script type="text/javascript" src="js/yahoo/get-min.js"></script> 	  
+<link href="dist/DateRange/daterangepicker.css" type="text/css" rel="stylesheet"/>  
 </head>
 <body>
 	<div class="container">
@@ -51,6 +63,7 @@
 					</div>
 				</div>
 			</div>
+			<form name="submitApplication" method="post">
 			<div class="col-md-12 col-xs-12 col-sm-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
@@ -60,32 +73,40 @@
 						<div class="row">
 							<div class="col-md-3 col-xs-12 col-sm-3">
 								<label>Designation Level</label>
-								<select>
-									<option>a</option>
-								</select>
+								<select  class="selectChosen" id="designationSlctBxId">
+                            	<option value="0">Select Designation Level</option>
+                            </select>
 							</div>
 							<div class="col-md-3 col-xs-12 col-sm-3">
 								<label>Select Name</label>
-								<select>
-									<option>a</option>
-								</select>
+								<select class="selectChosen" id="memberSlctBxId" name="toursInputVO.candidateId">    
+                            	<option value="0">Select Name</option>
+                            </select>
 							</div>
 						
 							<div class="col-md-12 col-xs-12 col-sm-12 m_top10">
-								<h4 class="panel-title text-capital">selected profile</h4>
-								<div class="pad_15 b_1">
+								<!--<h4 class="panel-title text-capital" style="display:none" id="selectrofileId" >selected profile</h4>-->
+								<div class="pad_15 ">
 									<ul class="list-inline profileUl">
 										<li>
-											<div class="pad_10">
+											<!--<div class="pad_10">
 												<img src="Assests/img/profile.png" style="height: 30px;width: 30px;"/>
 												<p>Kollu Ravindra</p>
 												<p>IXR9816267</p>
 												<p>+91 9988998899</p>
-											</div>
-											<div class="pad_10 borderGreen">
+											</div>-->
+											<div class="row m_top20 hideProfileDivCls">
+                    	                <div class="col-md-12 col-xs-12 col-sm-12">
+                         	             <div class="block">
+							            <div ><center ><img style="display: none;" src="images/icons/loading.gif" id="profileProcessingImgId"></center></div>
+                                        <div id="selectedMemberDtslDivId"></div>
+                                           </div>
+                                       </div>
+                                   </div>
+											<!--<div class="pad_10 borderGreen">
 												<label class="checkbox-inline">
 													<input type="checkbox"/>Select Profile</label>
-											</div>
+											</div>-->
 										</li>
 									</ul>
 								</div>
@@ -108,9 +129,12 @@
 											</div>
 											<div class="col-md-4 col-xs-12 col-sm-4">
 												<label>Tour Category</label>
-												<select>
+												<!--<select>
 													<option>a</option>
-												</select>
+												</select> -->
+												<select class="selectChosen" id="tourCategoryId" name="toursInputVO.tourCategoryId">    
+                            	                   <option value="0">Tour Category</option>
+                                                 </select>
 											</div>
 											<div class="col-md-2 col-xs-12 col-sm-2">
 												<label>Tour Location</label>
@@ -120,14 +144,17 @@
 											</div>
 											<div class="col-md-3 col-xs-12 col-sm-3">
 												<label>Tour Type</label>
-												<select>
+												<!-- <select>
 													<option>a</option>
-												</select>
+												</select> -->
+												<select class="selectChosen" id="tourTypeId" name="toursInputVO.tourTypeId">    
+                            	                   <option value="0">Tour Type</option>
+                                                 </select>
 											</div>
 										</div>
 										<div class="row m_top10">
 											<div class="col-md-12 col-xs-12 col-sm-12">
-												<label>Add Commment/Tour Description</label>
+												<label>Add Comment/Tour Description</label>
 												<textarea class="form-control"></textarea>
 											</div>
 										</div>
@@ -135,6 +162,22 @@
 									<div class="panel-footer borderGreen text-right">
 										<button class="btn btn-success">+ ADD TOUR</button>
 									</div>
+									<!--<div class="row showDivCls" id="uploadFlDivId" style="display:none;">--> 
+							      <div class="col-md-12 col-xs-12 col-sm-12 m_top20">
+								<h3 class="m_0 text-success font_weight" style="margin-left:425px;">UPLOAD SCAN COPY</h3>  
+								<input type="file" id="update_TourFileId2" multiple="multiple"  name="files[]" class="m_top20"/>
+								<span id="errFileId" style="color:red;margin-left:470px;"></span>   
+							  </div>
+						      <!--</div>-->  
+							  <!--<div class="row showDivCls" style="display:none;">-->  
+							<div class="col-md-4 col-md-offset-4">
+								<span class="updateTourStatusCls"></span>
+								<button class="btn btn-success btn-block" onclick="savingApplication();" type="button">SUBMIT APPLICATION</button>
+								<span id="successSpanId"></span>  
+							</div>   
+							  <div class="col-md-12 col-sm-12 col-xs-12" id="statusId"></div>
+						<!--</div>-->  
+					</form>
 								</div>
 							</div>
 						</div>
@@ -146,5 +189,24 @@
 	</div>
 <script src="Assets/js/jquery-1.11.3.js" type="text/javascript"></script>
 <script src="Assets/js/bootstrap.js" type="text/javascript"></script>
+<script src="dist/Plugins/Chosen/chosen.jquery.js" type="text/javascript"></script>
+<script src="js/Tours/toursDetails.js" type="text/javascript"></script>
+ <!-- for file uploader  -->              
+<script type="text/javascript" src="dragAndDropPhoto/js/customNominated.jquery.filter.min.js?v=1.0.5"></script>
+<script type="text/javascript" src="dragAndDropPhoto/js/customNominatedPost.js?v=1.0.5"></script>       
+<script type="text/javascript" src="dragAndDropPhoto/js/updateTourFile.js?v=1.0.5"></script> 
+<script type="text/javascript" src="dragAndDropPhoto/js/updateTourFile2.js?v=1.0.5"></script>                
+<!-- for file uploader -->
+<script src="js/Tours/updateToursDetails.js" type="text/javascript"></script>     
+<script src="dist/2016DashBoard/Plugins/Datatable/jquery.dataTables.js" type="text/javascript"></script>
+<script type="text/javascript">
+initializeFile2();     
+var windowUrl = window.location.href;
+var wurl = windowUrl.substr(0,(windowUrl.indexOf("/updateToursDetailsAction")));
+wurl = wurl.replace("/PartyAnalyst","");
+$(document).on("click",".submitedDataModal",function(){
+	$("#myModal").modal('show');
+});     
+</script>
 </body>
 </html>
