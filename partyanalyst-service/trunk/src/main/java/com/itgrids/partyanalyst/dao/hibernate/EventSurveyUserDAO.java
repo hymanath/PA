082293368bf7f82e1b2cd0ed3e60799e0894e92a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-24 14:02:49 +0000

    commit by nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EventSurveyUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/EventSurveyUserDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.FlushMode;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
@@ -44,5 +46,42 @@ public class EventSurveyUserDAO extends GenericDaoHibernate<EventSurveyUser, Lon
 				" and model.isEnabled = 'N' ");
 		query.setParameter("userId", userId);
 		return (Long)query.uniqueResult();
+	}
+	public List<Object[]> getPeshiAppForGrievance(Date fromDate,Date toDate,String memberShipId){
+		StringBuilder sb =  new StringBuilder();
+		sb.append("select Complaint_id as complaintId,Subject as subject," +
+				" description as description,Raised_Date as raisedDate,updated_at as updated," +
+				" Completed_Status as completedStatus,type_of_issue as typeOfIssue," +
+				" scan_copy as scanCopy" +
+				" from complaint_master " +
+				" where type_of_issue not in ('Insurance','Trust Education Support') " +
+				" and Subject != '' and Subject is not null and delete_status is null and state_id_cmp in(1,2)" +
+				" and Completed_Status is not null" +
+				" and membership_id =:memberShipId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(Raised_Date) between :fromDate and :toDate");
+		}
+		Query query = getSession().createSQLQuery(sb.toString()).addScalar("complaintId", Hibernate.LONG)
+				.addScalar("subject", Hibernate.STRING)
+				.addScalar("description", Hibernate.STRING)
+				.addScalar("raisedDate", Hibernate.DATE)
+				.addScalar("updated", Hibernate.DATE)
+				.addScalar("completedStatus", Hibernate.STRING)
+				.addScalar("typeOfIssue", Hibernate.STRING)
+				.addScalar("scanCopy", Hibernate.STRING);
+		query.setParameter("memberShipId", memberShipId);
+		if(fromDate != null && toDate != null){
+			query.setDate("fromDate", fromDate);
+			query.setDate("toDate", toDate);
+		}
+		return query.list();
+	}
+	public List<Object[]> getFilePathUrlForComplaintIds(List<Long> complaintIds){
+		Query query = getSession().createSQLQuery("select csc.complaint_id as complaintId,sc.scanned_copy_path as filePath" +
+				" from complaint_scanned_copy csc,scanned_copy sc " +
+				" where csc.scanned_copy_id = sc.scanned_copy_id and" +
+				" csc.complaint_id in (:complaintIds) ").addScalar("complaintId", Hibernate.LONG).addScalar("filePath", Hibernate.STRING);
+		query.setParameterList("complaintIds", complaintIds);
+		return query.list();
 	}
 }
