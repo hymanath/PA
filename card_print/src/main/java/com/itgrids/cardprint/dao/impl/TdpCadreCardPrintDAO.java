5dacf56e94355d23aa38f226c3fd031f6ebdaf26
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-26 10:54:40 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/dao/impl/TdpCadreCardPrintDAO.java
@@ -0,0 +1,24 @@
+package com.itgrids.cardprint.dao.impl;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.cardprint.dao.ITdpCadreCardPrintDAO;
+import com.itgrids.cardprint.model.TdpCadreCardPrint;
+
+public class TdpCadreCardPrintDAO extends GenericDaoHibernate<TdpCadreCardPrint, Long> implements ITdpCadreCardPrintDAO {
+
+	public TdpCadreCardPrintDAO(){
+		super(TdpCadreCardPrint.class);
+	}
+    
+	public Long getCardPrintVendorIdByConstituencyId(Long constituencyId){
+		
+		Query query = getSession().createQuery("" +
+		"select distinct model.cardPrintVendorId  " +
+		"from   TdpCadreCardPrint model " +
+		"where  model.constituencyId = :constituencyId ");
+		query.setParameter("constituencyId",constituencyId );
+		return (Long)query.uniqueResult();
+	}
+}
