9beaccfc6e5e68d2a6901f6b58958ceb2a9b9341
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-26 11:03:19 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PrintVO.java
@@ -0,0 +1,74 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PrintVO implements Serializable{
+	
+	private Long tdpCadreCardPrintId;
+	private Date printTime;
+	private String serialNumber;
+	private String printStatus;
+	private String printCode;
+	private String printDesc;
+	private String printerSerialNumber;
+	private String boxNo;
+	private String pcNo;
+	
+	
+	public Long getTdpCadreCardPrintId() {
+		return tdpCadreCardPrintId;
+	}
+	public void setTdpCadreCardPrintId(Long tdpCadreCardPrintId) {
+		this.tdpCadreCardPrintId = tdpCadreCardPrintId;
+	}
+	public Date getPrintTime() {
+		return printTime;
+	}
+	public void setPrintTime(Date printTime) {
+		this.printTime = printTime;
+	}
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public String getPrintStatus() {
+		return printStatus;
+	}
+	public void setPrintStatus(String printStatus) {
+		this.printStatus = printStatus;
+	}
+	public String getPrintCode() {
+		return printCode;
+	}
+	public void setPrintCode(String printCode) {
+		this.printCode = printCode;
+	}
+	public String getPrintDesc() {
+		return printDesc;
+	}
+	public void setPrintDesc(String printDesc) {
+		this.printDesc = printDesc;
+	}
+	public String getPrinterSerialNumber() {
+		return printerSerialNumber;
+	}
+	public void setPrinterSerialNumber(String printerSerialNumber) {
+		this.printerSerialNumber = printerSerialNumber;
+	}
+	public String getBoxNo() {
+		return boxNo;
+	}
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
+	}
+	public String getPcNo() {
+		return pcNo;
+	}
+	public void setPcNo(String pcNo) {
+		this.pcNo = pcNo;
+	}
+	
+}
