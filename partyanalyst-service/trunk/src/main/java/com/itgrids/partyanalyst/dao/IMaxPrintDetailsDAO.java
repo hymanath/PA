c047d60f305a6708149fe896a2b0dd55e5cf2d3d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-26 11:01:42 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMaxPrintDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.MaxPrintDetails;
+
+public interface IMaxPrintDetailsDAO extends GenericDao<MaxPrintDetails, Long> {
+	
+}
