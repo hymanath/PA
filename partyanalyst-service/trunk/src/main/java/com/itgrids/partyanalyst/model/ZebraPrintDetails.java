9a906a17588b62a80d07766cc939cfa7be832e73
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-26 11:03:54 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ZebraPrintDetails.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/ZebraPrintDetails.java
@@ -18,50 +18,55 @@ import org.hibernate.annotations.LazyToOne;
 import org.hibernate.annotations.LazyToOneOption;
 import org.hibernate.annotations.NotFoundAction;
 
-/**
- * 
- * @author Srishailam Pittala
- *
- */
+
 
 @Entity
-@Table(name = "zebra_print_details")
+@Table(name = "zebra_print_details")//zebra_print_details20
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class ZebraPrintDetails extends BaseModel implements java.io.Serializable {
 
 	private Long zebraPrintDetailsId;
-	private String voterName;
-	private String panchayatName;
-	private String mandalName;
-	private String muncipalityName;
-	private String consiteuncyName;
-	private String districtName;
-	private TdpCadre tdpCadre;
+	private Long tdpCadreCardPrintId;
 	private Long tdpCadreId;
-	private String memberShipNumber;
-	private String voterCardNo;
-	private String image;
-	private String constituencyType;
-	private String refNo;
-	private String userName;
-	private String serialNo;
-	private Date insertedTime;
-	private Date updatedTime;
-	private String printStatus;
-	private String printSerialNo;
-	private String reserveStatus;
-	private String pcNo;
-	private String printerIndex;
-	private Date downloadTime;
-	private String updateStatus;
-	private String errorStatus;
-	private String photoType;
-	private String voterImagePath;
+	private String memberShipId;
+	private String cadreName;
+	private String relativeName;
+	private String imagePath;
 	private String mobileNo;
-	private String jobCode;
-	private String partNo;
+	
+	private String locationType;
+	private Long  districtId;
+	private String districtName;
+	private Long constituencyId;
+	private String constituencyName;
+	private Long mandalId;
+	private String mandalName;
+	private Long panchayatId;
+	private String panchayatName;
+	private Long muncipalityId;
+	private String muncipalityName;
+	private Long wardId;
+	private String wardName;
+	private Long boothId;
+	private String boothName;
+	private Long serialNoInBooth;
 	private String areaCovered;
 	private String houseNo;
+	
+	private Date pushTime;
+	private String constituencyType;
+	private Date printTime;
+	private String serialNumber;
+	private String printStatus;
+	private String printCode;
+	private String printDesc;
+	private String printerSerialNumber;
+	private String boxNo;
+	private String pcNo;
+	
+	private District district;
+	private Constituency constituency;
+	private TdpCadreCardPrint tdpCadreCardPrint;
 	
 	
 	@Id
@@ -74,63 +79,13 @@ public class ZebraPrintDetails extends BaseModel implements java.io.Serializable
 		this.zebraPrintDetailsId = zebraPrintDetailsId;
 	}
 	
-	@Column(name="voter_name")
-	public String getVoterName() {
-		return voterName;
-	}
-	public void setVoterName(String voterName) {
-		this.voterName = voterName;
-	}
 	
-	@Column(name="panchayat_name")
-	public String getPanchayatName() {
-		return panchayatName;
+	@Column(name = "tdp_cadre_card_print_id")
+	public Long getTdpCadreCardPrintId() {
+		return tdpCadreCardPrintId;
 	}
-	public void setPanchayatName(String panchayatName) {
-		this.panchayatName = panchayatName;
-	}
-	
-	@Column(name="mandal_name")
-	public String getMandalName() {
-		return mandalName;
-	}
-	public void setMandalName(String mandalName) {
-		this.mandalName = mandalName;
-	}
-	
-	@Column(name="muncipality_name")
-	public String getMuncipalityName() {
-		return muncipalityName;
-	}
-	public void setMuncipalityName(String muncipalityName) {
-		this.muncipalityName = muncipalityName;
-	}
-	
-	@Column(name="constituency_name")
-	public String getConsiteuncyName() {
-		return consiteuncyName;
-	}
-	public void setConsiteuncyName(String consiteuncyName) {
-		this.consiteuncyName = consiteuncyName;
-	}
-	
-	@Column(name="district_name")
-	public String getDistrictName() {
-		return districtName;
-	}
-	public void setDistrictName(String districtName) {
-		this.districtName = districtName;
-	}
-	
-	@ManyToOne(fetch = FetchType.LAZY )
-	@JoinColumn(name = "tdp_cadre_id" , insertable = false, updatable = false)
-	@LazyToOne(LazyToOneOption.NO_PROXY)
-	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
-	public TdpCadre getTdpCadre() {
-		return tdpCadre;
-	}
-	public void setTdpCadre(TdpCadre tdpCadre) {
-		this.tdpCadre = tdpCadre;
+	public void setTdpCadreCardPrintId(Long tdpCadreCardPrintId) {
+		this.tdpCadreCardPrintId = tdpCadreCardPrintId;
 	}
 	
 	@Column(name="tdp_cadre_id")
@@ -140,157 +95,35 @@ public class ZebraPrintDetails extends BaseModel implements java.io.Serializable
 	public void setTdpCadreId(Long tdpCadreId) {
 		this.tdpCadreId = tdpCadreId;
 	}
-	
-	@Column(name="member_ship_member")
-	public String getMemberShipNumber() {
-		return memberShipNumber;
+	@Column(name="membership_id")
+	public String getMemberShipId() {
+		return memberShipId;
 	}
-	public void setMemberShipNumber(String memberShipNumber) {
-		this.memberShipNumber = memberShipNumber;
+	public void setMemberShipId(String memberShipId) {
+		this.memberShipId = memberShipId;
 	}
-	
-	@Column(name="voter_id_card_number")
-	public String getVoterCardNo() {
-		return voterCardNo;
+	@Column(name="cadre_name")
+	public String getCadreName() {
+		return cadreName;
 	}
-	public void setVoterCardNo(String voterCardNo) {
-		this.voterCardNo = voterCardNo;
+	public void setCadreName(String cadreName) {
+		this.cadreName = cadreName;
 	}
 	
-	@Column(name="image")
-	public String getImage() {
-		return image;
+	@Column(name="relative_name")
+	public String getRelativeName() {
+		return relativeName;
 	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-	
-	@Column(name="constituency_type")
-	public String getConstituencyType() {
-		return constituencyType;
-	}
-	public void setConstituencyType(String constituencyType) {
-		this.constituencyType = constituencyType;
+	public void setRelativeName(String relativeName) {
+		this.relativeName = relativeName;
 	}
 	
-	@Column(name="ref_number")
-	public String getRefNo() {
-		return refNo;
+	@Column(name="image_path")
+	public String getImagePath() {
+		return imagePath;
 	}
-	public void setRefNo(String refNo) {
-		this.refNo = refNo;
-	}
-	
-	@Column(name="user_name")
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	@Column(name="serial_number")
-	public String getSerialNo() {
-		return serialNo;
-	}
-	public void setSerialNo(String serialNo) {
-		this.serialNo = serialNo;
-	}
-	
-	@Column(name="inserted_time")
-	public Date getInsertedTime() {
-		return insertedTime;
-	}
-	public void setInsertedTime(Date insertedTime) {
-		this.insertedTime = insertedTime;
-	}
-	
-	@Column(name="update_time")
-	public Date getUpdatedTime() {
-		return updatedTime;
-	}
-	public void setUpdatedTime(Date updatedTime) {
-		this.updatedTime = updatedTime;
-	}
-	
-	@Column(name="print_status")
-	public String getPrintStatus() {
-		return printStatus;
-	}
-	public void setPrintStatus(String printStatus) {
-		this.printStatus = printStatus;
-	}
-	
-	@Column(name="printer_serial_number")
-	public String getPrintSerialNo() {
-		return printSerialNo;
-	}
-	public void setPrintSerialNo(String printSerialNo) {
-		this.printSerialNo = printSerialNo;
-	}
-	
-	@Column(name="reserve_status")
-	public String getReserveStatus() {
-		return reserveStatus;
-	}
-	public void setReserveStatus(String reserveStatus) {
-		this.reserveStatus = reserveStatus;
-	}
-	
-	@Column(name="pc_no")
-	public String getPcNo() {
-		return pcNo;
-	}
-	public void setPcNo(String pcNo) {
-		this.pcNo = pcNo;
-	}
-	
-	@Column(name="printer_index")
-	public String getPrinterIndex() {
-		return printerIndex;
-	}
-	public void setPrinterIndex(String printerIndex) {
-		this.printerIndex = printerIndex;
-	}
-	
-	@Column(name="download_time")
-	public Date getDownloadTime() {
-		return downloadTime;
-	}
-	public void setDownloadTime(Date downloadTime) {
-		this.downloadTime = downloadTime;
-	}
-	
-	@Column(name="update_status")
-	public String getUpdateStatus() {
-		return updateStatus;
-	}
-	public void setUpdateStatus(String updateStatus) {
-		this.updateStatus = updateStatus;
-	}
-	
-	@Column(name="error_Status")
-	public String getErrorStatus() {
-		return errorStatus;
-	}
-	public void setErrorStatus(String errorStatus) {
-		this.errorStatus = errorStatus;
-	}
-	
-	@Column(name="photo_type")
-	public String getPhotoType() {
-		return photoType;
-	}
-	public void setPhotoType(String photoType) {
-		this.photoType = photoType;
-	}
-	
-	@Column(name="voter_image_path")
-	public String getVoterImagePath() {
-		return voterImagePath;
-	}
-	public void setVoterImagePath(String voterImagePath) {
-		this.voterImagePath = voterImagePath;
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
 	}
 	
 	@Column(name="mobile_no")
@@ -301,20 +134,132 @@ public class ZebraPrintDetails extends BaseModel implements java.io.Serializable
 		this.mobileNo = mobileNo;
 	}
 	
-	@Column(name="job_code")
-	public String getJobCode() {
-		return jobCode;
+	@Column(name="location_type")
+	public String getLocationType() {
+		return locationType;
 	}
-	public void setJobCode(String jobCode) {
-		this.jobCode = jobCode;
+	public void setLocationType(String locationType) {
+		this.locationType = locationType;
 	}
 	
-	@Column(name="part_no")
-	public String getPartNo() {
-		return partNo;
+	@Column(name="district_id")
+	public Long getDistrictId() {
+		return districtId;
 	}
-	public void setPartNo(String partNo) {
-		this.partNo = partNo;
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	
+	@Column(name="district_name")
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	
+	@Column(name="constituency_id")
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	
+	@Column(name="constituency_name")
+	public String getConstituencyName() {
+		return constituencyName;
+	}
+	public void setConstituencyName(String constituencyName) {
+		this.constituencyName = constituencyName;
+	}
+	
+	@Column(name="mandal_id")
+	public Long getMandalId() {
+		return mandalId;
+	}
+	public void setMandalId(Long mandalId) {
+		this.mandalId = mandalId;
+	}
+	
+	@Column(name="mandal_name")
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	
+	@Column(name="panchayat_id")
+	public Long getPanchayatId() {
+		return panchayatId;
+	}
+	public void setPanchayatId(Long panchayatId) {
+		this.panchayatId = panchayatId;
+	}
+	
+	@Column(name="panchayat_name")
+	public String getPanchayatName() {
+		return panchayatName;
+	}
+	public void setPanchayatName(String panchayatName) {
+		this.panchayatName = panchayatName;
+	}
+	
+	@Column(name="muncipality_id")
+	public Long getMuncipalityId() {
+		return muncipalityId;
+	}
+	public void setMuncipalityId(Long muncipalityId) {
+		this.muncipalityId = muncipalityId;
+	}
+	
+	@Column(name="muncipality_name")
+	public String getMuncipalityName() {
+		return muncipalityName;
+	}
+	public void setMuncipalityName(String muncipalityName) {
+		this.muncipalityName = muncipalityName;
+	}
+	
+	@Column(name="ward_id")
+	public Long getWardId() {
+		return wardId;
+	}
+	public void setWardId(Long wardId) {
+		this.wardId = wardId;
+	}
+	
+	@Column(name="ward_name")
+	public String getWardName() {
+		return wardName;
+	}
+	public void setWardName(String wardName) {
+		this.wardName = wardName;
+	}
+	
+	@Column(name="booth_id")
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	
+	@Column(name="booth_name")
+	public String getBoothName() {
+		return boothName;
+	}
+	public void setBoothName(String boothName) {
+		this.boothName = boothName;
+	}
+	
+	@Column(name="serial_no_in_booth")
+	public Long getSerialNoInBooth() {
+		return serialNoInBooth;
+	}
+	public void setSerialNoInBooth(Long serialNoInBooth) {
+		this.serialNoInBooth = serialNoInBooth;
 	}
 	
 	@Column(name="area_covered")
@@ -331,6 +276,120 @@ public class ZebraPrintDetails extends BaseModel implements java.io.Serializable
 	}
 	public void setHouseNo(String houseNo) {
 		this.houseNo = houseNo;
+	}
+	
+	@Column(name="push_time")
+	public Date getPushTime() {
+		return pushTime;
+	}
+	public void setPushTime(Date pushTime) {
+		this.pushTime = pushTime;
+	}
+	
+	
+	@Column(name="constituency_type")
+	public String getConstituencyType() {
+		return constituencyType;
+	}
+	public void setConstituencyType(String constituencyType) {
+		this.constituencyType = constituencyType;
+	}
+	
+	@Column(name="print_time")
+	public Date getPrintTime() {
+		return printTime;
+	}
+	public void setPrintTime(Date printTime) {
+		this.printTime = printTime;
+	}
+	
+	@Column(name="serial_number")
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	
+	@Column(name="print_status")
+	public String getPrintStatus() {
+		return printStatus;
+	}
+	public void setPrintStatus(String printStatus) {
+		this.printStatus = printStatus;
+	}
+	
+	@Column(name="print_code")
+	public String getPrintCode() {
+		return printCode;
+	}
+	public void setPrintCode(String printCode) {
+		this.printCode = printCode;
+	}
+	
+	@Column(name="print_desc")
+	public String getPrintDesc() {
+		return printDesc;
+	}
+	public void setPrintDesc(String printDesc) {
+		this.printDesc = printDesc;
+	}
+	
+	@Column(name="printer_serial_number")
+	public String getPrinterSerialNumber() {
+		return printerSerialNumber;
+	}
+	public void setPrinterSerialNumber(String printerSerialNumber) {
+		this.printerSerialNumber = printerSerialNumber;
+	}
+	
+	@Column(name="box_no")
+	public String getBoxNo() {
+		return boxNo;
+	}
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
+	}
+	@Column(name="pc_no")
+	public String getPcNo() {
+		return pcNo;
+	}
+	public void setPcNo(String pcNo) {
+		this.pcNo = pcNo;
+	}
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "constituency_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Constituency getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(Constituency constituency) {
+		this.constituency = constituency;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "district_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public District getDistrict() {
+		return district;
+	}
+	public void setDistrict(District district) {
+		this.district = district;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "tdp_cadre_card_print_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadreCardPrint getTdpCadreCardPrint() {
+		return tdpCadreCardPrint;
+	}
+	public void setTdpCadreCardPrint(TdpCadreCardPrint tdpCadreCardPrint) {
+		this.tdpCadreCardPrint = tdpCadreCardPrint;
 	}
 	
 	
