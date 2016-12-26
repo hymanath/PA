4bc4da9c428c747ada44807c95b2ef023b0e1520
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-26 11:02:25 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreCardPrintDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITdpCadreCardPrintDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
+import com.itgrids.partyanalyst.dto.PrintVO;
 import com.itgrids.partyanalyst.model.TdpCadreCardPrint;
 
 public interface ITdpCadreCardPrintDAO extends GenericDao<TdpCadreCardPrint,Long>{
@@ -12,4 +13,6 @@ public interface ITdpCadreCardPrintDAO extends GenericDao<TdpCadreCardPrint,Long
 	public List<Object[]> getCardPrintDetailsByMemberShipId(String memberShipId);
 	public List<Object[]> getStatusWisePrintingCardsCounts(Long stateId,Long vendorId,Date fromDate,Date toDate);
 	public List<Object[]> getBoxWisePrintingDispatchDetails(Long vendorId,Long districtId,Long constituencyId);
+	
+	public Integer updateAppntmntStatusById(PrintVO printVO);
 }
