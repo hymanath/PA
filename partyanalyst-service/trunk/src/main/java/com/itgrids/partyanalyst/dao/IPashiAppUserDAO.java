cb645aba394809d361b90463d305e489577a695a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-28 13:47:35 +0000

    UPDATED BY SRINU changed loginservice code

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPashiAppUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPashiAppUserDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.PashiAppUser;
 
 public interface IPashiAppUserDAO extends GenericDao<PashiAppUser, Long>{
 	
-
+	public List<PashiAppUser> checkUserPassword(String userName, String password);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PashiAppUserDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PashiAppUserDAO.java
@@ -1,6 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IPashiAppUserDAO;
 import com.itgrids.partyanalyst.model.PashiAppUser;
@@ -10,5 +14,55 @@ public class PashiAppUserDAO extends GenericDaoHibernate<PashiAppUser, Long> imp
 	public PashiAppUserDAO() {
 		super(PashiAppUser.class);
 	}
+	
+	
+	  @SuppressWarnings("unchecked")
+	  public List<PashiAppUser> checkUserPassword(String userName, String password) 
+	  {
+	    Object[] parameters = {userName, password};
+	    return getHibernateTemplate().find("select model from PashiAppUser model where model.userName = ? " +
+	    		" and model.password = ? and model.isDeleted = 'N' ",parameters);
+	  }
+
+
+	//@SuppressWarnings("unchecked")
+//	public String checkUserPassword(String userName, String password) 
+	//{
+		
+		/*	
+		 * Object[] parameters = {userName, password};
+			return getHibernateTemplate().find("select model.userName from PashiAppUser model where " +
+				" model.userName = ? and model.password = ? and model.isDeleted = 'N' ",parameters);
+		*/	
+		
+		/*
+			Query query = getSession().createQuery("select model.userName from PashiAppUser model where " +
+				" model.userName = ? and model.password = ? and model.isDeleted = 'N' ");
+		
+		  	query.setParameter(0, userName);
+	      	query.setParameter(1, password);
+	       	// query.setFirstResult(0);
+	     	// query.setMaxResults(100);
+	      return (String) query.uniqueResult();
+	      */
+		
+		/*
+			Object[] parameters = {userName, password};
+			Query query = getSession().createQuery("select model.userName, model.password from PashiAppUser model where " +
+				" model.userName = :srinu1 and model.password = :srinu2 and model.isDeleted = 'N' ");
+		
+			query.setParameter("srinu1", userName);
+			query.setParameter("srinu2", password);
+	    
+	     // query.setFirstResult(0);
+	     // query.setMaxResults(100);
+	      
+			return query.list();
+	        */
+		
+		//Criteria aa = getSession().createCriteria(PashiAppUser.class);
+	//}
+	
+	
 	
 }
