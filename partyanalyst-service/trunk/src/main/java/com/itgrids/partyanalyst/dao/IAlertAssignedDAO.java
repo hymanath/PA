db83eb15593099b1da2d1417da217ac41f8aabf3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-29 13:11:10 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -11,4 +12,5 @@ public interface IAlertAssignedDAO extends GenericDao<AlertAssigned, Long> {
 	public List<Long> checkCadreExistsForAlert(List<Long> tdpCadreIds,Long alertId);
 	public List<Long> getDeleteAlertAssignedCandidates(Long alertId,Long tdpCadreId);
 	public List<Object[]> getAlertAssignedCandidate(Long alertId);
+	public List<Object[]> getTdpCadreWiseAssignedAlertDetails(Long tdpCadreId,Date fromDate, Date toDate,Long alertTypeId);
 }
