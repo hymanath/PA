1e8fa5a88c1a0ade888b4efcd2f308f372b0f00d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-30 07:10:23 +0000

    updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/AlertVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/AlertVO.java
@@ -37,8 +37,22 @@ public class AlertVO {
 	private List<AlertVO> subList1;
 	private List<AlertVO> subList2;
 	private String title;
+	private String date1;
+	private String date2;
 	
 	
+	public String getDate1() {
+		return date1;
+	}
+	public void setDate1(String date1) {
+		this.date1 = date1;
+	}
+	public String getDate2() {
+		return date2;
+	}
+	public void setDate2(String date2) {
+		this.date2 = date2;
+	}
 	public String getAlertTypeName() {
 		return alertTypeName;
 	}
