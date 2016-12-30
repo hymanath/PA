3d627c2239281e8c459e519f847ac4d71103f6f3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2016-12-30 08:12:04 +0000

    updated by - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintStatusDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CardPrintStatus;
 
 public interface ICardPrintStatusDAO extends GenericDao<CardPrintStatus, Long> {
-
+	
+	public List<Object[]> getAllCardPrintStatus();
 }
