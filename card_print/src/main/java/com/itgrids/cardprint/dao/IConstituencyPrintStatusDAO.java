61da7e52913ac86ca847874a2b92a47c52f7f647
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-06 12:01:50 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyPrintStatusDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyPrintStatusDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.cardprint.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.cardprint.model.ConstituencyPrintStatus;
 
 public interface IConstituencyPrintStatusDAO extends GenericDao<ConstituencyPrintStatus, Long> {
-
+	
+	public List<Long> getConstituencyPrintStatus(Long constituencyId);
 }
