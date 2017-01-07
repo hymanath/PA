5a0f5733f227d812924e9667ed1f7bc21122edbb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-07 13:55:26 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dto/BasicVO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dto/BasicVO.java
@@ -1,9 +1,13 @@
 package com.itgrids.cardprint.dto;
 
+import java.util.List;
+
 public class BasicVO {
 	
 	private Long id;
 	private String name;
+	
+	private List<BasicVO> subList ;
 	
 	public Long getId() {
 		return id;
@@ -17,5 +21,12 @@ public class BasicVO {
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	public List<BasicVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<BasicVO> subList) {
+		this.subList = subList;
+	}
+	
 }
