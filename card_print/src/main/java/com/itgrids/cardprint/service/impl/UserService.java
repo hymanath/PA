e1263f46a4164ea2862a5fcc0a05a8ce11133e06
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-07 08:09:13 +0000

    adding entitlements based on userType - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/service/impl/UserService.java
+++ card_print/src/main/java/com/itgrids/cardprint/service/impl/UserService.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import com.itgrids.cardprint.dao.IUserDAO;
 import com.itgrids.cardprint.dto.UserVO;
 import com.itgrids.cardprint.model.User;
+import com.itgrids.cardprint.model.UserType;
 import com.itgrids.cardprint.service.IUserService;
 import com.itgrids.cardprint.util.SHA512;
 
@@ -37,9 +38,16 @@ public class UserService implements IUserService{
 				 String shapwd = sha512.hashText(hashKey + uniqueKey);
 				
 				 if(password.equals(shapwd)){
+					 
 					 	userVO.setName(user.getName());
 						userVO.setUserId(user.getUserId());
 						
+						//Getting UserType for the user.
+						UserType userType = user.getUserType();
+						if(userType != null && userType.getUserTypeId() != null && userType.getUserTypeId().longValue() > 0l){
+							userVO.setUserTypeId(userType.getUserTypeId());
+							userVO.setUserType(userType.getUserType() != null ?userType.getUserType() : "");
+						}
 						/*//Entitlements
 						List<String> entitlements=new ArrayList<String>();
 						entitlements = userEntitlementGroupDAO.getUserEntitleMents(user.getUserId());
