c4d7e9a18822407b9ce7d40794a9fa9cb59597f6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-07 15:53:19 +0000

    updated by - SREEDHAR

---
+++ card_print/src/main/java/com/itgrids/cardprint/dto/PrintStatusVO.java
@@ -0,0 +1,38 @@
+package com.itgrids.cardprint.dto;
+
+import java.io.Serializable;
+
+public class PrintStatusVO implements Serializable{
+	
+	private Long id;
+	private String name;
+	private Long count = 0l;
+	
+	public PrintStatusVO(){}
+	public PrintStatusVO(Long id , String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getCount() {
+		return count;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	
+	
+	
+}
