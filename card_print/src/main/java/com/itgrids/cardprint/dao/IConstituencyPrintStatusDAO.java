27405f4ade7e21ec0c24b6975e80d47a53433c52
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-07 10:38:28 +0000

    getting const based on vendor - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyPrintStatusDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/IConstituencyPrintStatusDAO.java
@@ -9,4 +9,5 @@ import com.itgrids.cardprint.model.ConstituencyPrintStatus;
 public interface IConstituencyPrintStatusDAO extends GenericDao<ConstituencyPrintStatus, Long> {
 	
 	public List<Long> getConstituencyPrintStatus(Long constituencyId);
+	public List<Object[]> getConstituenciesByPrintVendor(Long printVendorId);
 }
