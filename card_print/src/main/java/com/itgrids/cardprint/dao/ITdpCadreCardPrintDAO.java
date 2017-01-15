56c591e0ca556145e43dc9b80dc21b175b0604a4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-15 15:45:04 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/ITdpCadreCardPrintDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/ITdpCadreCardPrintDAO.java
@@ -7,4 +7,5 @@ import com.itgrids.cardprint.model.TdpCadreCardPrint;
 public interface ITdpCadreCardPrintDAO extends GenericDao<TdpCadreCardPrint, Long> {
 	
 	public Long getCardPrintVendorIdByConstituencyId(Long constituencyId);
+	public void flushAndclearSession();
 }
