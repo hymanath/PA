90178ffc33d098761bb796798d68a5ffa96bfdd6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-15 11:42:33 +0000

    QA verification page - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/service/ICardPrintService.java
+++ card_print/src/main/java/com/itgrids/cardprint/service/ICardPrintService.java
@@ -3,6 +3,8 @@ package com.itgrids.cardprint.service;
 import java.util.List;
 
 import com.itgrids.cardprint.dto.BasicVO;
+import com.itgrids.cardprint.dto.CardPrintVO;
+import com.itgrids.cardprint.dto.CardPrintingDispatchVO;
 import com.itgrids.cardprint.dto.PrintStatusUpdateVO;
 import com.itgrids.cardprint.dto.ResultStatus;
 
@@ -13,8 +15,9 @@ public interface ICardPrintService {
 	public List<BasicVO>  getAllAssemblyConstituencies();
 	public List<BasicVO>  getConstituenciesByPrintVendor(Long printVendorId);
 	public BasicVO getVendorIdAndConstituenciesByLoggedInUser(Long userId);
-	
 	public ResultStatus saveConstituencyPrintStatus(final PrintStatusUpdateVO inputVO);
-	
 	public ResultStatus updatePrintDetailsToTdpCadreCardPrint(final Long cardPrintVendorId , final Long constituencyId);
+	public List<CardPrintVO> getDstrListByVendor(Long vendorId);
+	public List<CardPrintVO> getConstListByVendor(Long vendorId,Long districtId);
+	public List<CardPrintingDispatchVO> getPrintingDispatchDetails(Long vendorId,Long districtId,Long constituencyId);
 }
