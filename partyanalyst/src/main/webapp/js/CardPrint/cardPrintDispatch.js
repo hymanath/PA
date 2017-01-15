0ae97ead0a993d14bb046d0c9af9a75fc1df564a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-15 11:48:10 +0000

    QA verification page changes- SREEDHAR

--- partyanalyst/src/main/webapp/js/CardPrint/cardPrintDispatch.js
+++ partyanalyst/src/main/webapp/js/CardPrint/cardPrintDispatch.js
@@ -3,7 +3,7 @@ function onloads(){
 	getDistrictList(0);
 }
 function getDistrictList(vendorId){
-	getConstituencyList();
+	//getConstituencyList();
 	$("#districtListId").empty();
 	var vendorId = vendorId;
 	var jsObj = { 
@@ -119,44 +119,74 @@ function getDispatchDetails(){
 	});
 }
 
-function buildDispatchingDetails(result){
-	var str=''
-	
-	str+='<table class="table tablePrinting text-capital">';
-		str+='<thead>';
-			str+='<th>Select</th>';
-			str+='<th>BOX ID</th>';
-			str+='<th>mandal/town/division</th>';
-			str+='<th>village/ward</th>';
-			str+='<th>no of cards</th>';
-			str+='<th>qa passed</th>';
-			str+='<th>error %</th>';
-			str+='<th>validated cards</th>';
-			str+='<th>status</th>';
-		str+='</thead>';
-		str+='<tbody>';
-		for(var i in result){
-			str+='<tr>';
-				str+='<td>';
-					if(result[i].isQAPassed == 'YES')
-						str+='<input type="checkbox"/>';
-					else
-						str+='<input type="checkbox" disabled/>';
-				str+='</td>';
-				str+='<td>'+result[i].boxNo+'</td>';
-				str+='<td>'+result[i].mandalName+'</td>';
-				str+='<td>'+result[i].panchayatName+'</td>';
-				str+='<td>'+result[i].noOfCards+'</td>';
-				str+='<td>'+result[i].isQAPassed+'</td>';
-				str+='<td>'+result[i].errorPerc+'%</td>';
-				str+='<td>'+result[i].validatedCardsCount+'</td>';
-				str+='<td>'+result[i].status+'</td>';
-			str+='</tr>';
-		}
-		str+='</tbody>';
-	str+='</table>';
-	
-	$("#updateBtnId").show();
-	$("#dispatchDivIdImg").hide();
-	$("#dispatchingDiv").html(str);
-}
+	function buildDispatchingDetails(result){
+		//summary
+		var str1 = '';
+		str1+='<div class="panel panel-default panelWhite">';
+				str1+='<div class="panel-heading">';
+					str1+='<h3 class="panel-title text-capital"> PRINTED CARDS SUMMARY FOR THE CONSTITUENCY</h3>';
+				str1+='</div>';
+				str1+='<div class="panel-body">';
+				  str1+='<div class="col-md-3 col-xs-12 col-sm-4" style="font-size:16px;" ><b>Total Cadre :</b> '+result[0].totalCadre+'</div>';
+				  str1+='<div class="col-md-3 col-xs-12 col-sm-4" style="font-size:16px;"><b> Printed Cadre :</b> '+result[0].printedCadre+'</div>';
+				  str1+='<div class="col-md-3 col-xs-12 col-sm-4" style="font-size:16px;"><b>UnPrinted Cadre :</b> '+result[0].unPrintedCadre+'</div>';
+			  str1+='</div>';
+		str1+='</div>';
+		$('#summaryDivId').html(str1);
+		
+		//table
+		var str=''
+		str+='<table class="table tablePrinting text-capital">';
+			str+='<thead>';
+				str+='<th>Select</th>';
+				str+='<th>BOX ID</th>';
+				str+='<th>mandal/town/division</th>';
+				str+='<th>village/ward</th>';
+				str+='<th>no of cards</th>';
+				str+='<th>validated cards</th>';
+				str+='<th>qa passed</th>';
+				str+='<th>error %</th>';
+				str+='<th>status</th>';
+			str+='</thead>';
+			str+='<tbody>';
+			for(var i in result){
+				str+='<tr>';
+					str+='<td>';
+						if(result[i].isQAPassed == 'YES')
+							str+='<input type="checkbox"/>';
+						else
+							str+='<input type="checkbox" disabled/>';
+					str+='</td>';
+					
+					if(result[i].boxNo != null && result[i].boxNo > 0){
+						str+='<td>'+result[i].boxNo+'</td>';
+					}else{
+						str+='<td> - </td>';
+					}
+					str+='<td>'+result[i].mandalName+'</td>';
+					str+='<td>'+result[i].panchayatName+'</td>';
+					
+					if(result[i].noOfCards != null && result[i].noOfCards > 0){
+						str+='<td>'+result[i].noOfCards+'</td>';
+					}
+					if(result[i].validatedCardsCount != null && result[i].validatedCardsCount > 0){
+						str+='<td>'+result[i].validatedCardsCount+'</td>';
+						str+='<td>'+result[i].isQAPassed+'</td>';
+						str+='<td>'+result[i].errorPerc+'%</td>';
+						str+='<td>'+result[i].status+'</td>';
+					}else{
+						str+='<td> - </td>';
+						str+='<td> - </td>';
+						str+='<td> - </td>';
+						str+='<td> - </td>';
+					}
+					
+				str+='</tr>';
+			}
+			str+='</tbody>';
+		str+='</table>';
+		
+		$("#updateBtnId").show();
+		$("#dispatchDivIdImg").hide();
+		$("#dispatchingDiv").html(str);
+	}
