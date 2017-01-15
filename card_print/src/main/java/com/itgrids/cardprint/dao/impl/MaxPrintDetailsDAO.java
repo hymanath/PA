f950cc6d33a03aa1582a17d3a3a3cc62417e9c58
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-15 15:45:24 +0000

    updated by - SREEDHAR

--- card_print/src/main/java/com/itgrids/cardprint/dao/impl/MaxPrintDetailsDAO.java
+++ card_print/src/main/java/com/itgrids/cardprint/dao/impl/MaxPrintDetailsDAO.java
@@ -16,8 +16,8 @@ public class MaxPrintDetailsDAO extends GenericDaoHibernate<MaxPrintDetails, Lon
 	
 	public List<Object[]>  getPrintDetailsByConstituencyId(Long constituencyId){
 		Query query = getSession().createQuery("" +
-		" select  model.tdpCadreCardPrintId , model.printTime , model.serialNumber , model.printStatus , " +
-		"         model.printCode , model.printDesc , model.printerSerialNumber , model.boxNo , model.pcNo " +
+		" select  model.tdpCadreCardPrintId , model.printTime , model.serialNumber , model.printStatus , " +//3
+		"         model.printCode , model.printDesc , model.printerSerialNumber , model.boxNo , model.pcNo , model.outerBoxNo " +//9
 		" from   MaxPrintDetails model " +
 		" where  model.constituencyId = :constituencyId");
 		query.setParameter("constituencyId",constituencyId );
