b2c8b5546aeb337b9e5a66020ea92c7fa2c0e2bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-17 13:25:56 +0000

    Updated by santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActionTypeStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActionTypeStatusDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActionTypeStatus;
 
 public interface IActionTypeStatusDAO extends GenericDao<ActionTypeStatus, Long> {
-
+	public List<Object[]> getAlertActionTypeWiseStatus();
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActionTypeStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/ActionTypeStatusDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IActionTypeStatusDAO;
 import com.itgrids.partyanalyst.model.ActionTypeStatus;
@@ -9,5 +12,20 @@ public class ActionTypeStatusDAO extends GenericDaoHibernate<ActionTypeStatus, L
 
 	public ActionTypeStatusDAO(){
 		super(ActionTypeStatus.class);
+	}
+	public List<Object[]> getAlertActionTypeWiseStatus(){
+	  StringBuilder queryStr = new StringBuilder();
+	    queryStr.append(" select " +
+	   		" model.actionType.actionTypeId," +
+	   		" model.actionType.typeName," +
+	   		" model.actionTypeStatusId," +
+	   		" model.status " +
+	   		" from ActionTypeStatus model " +
+	   		" group by model.actionType.actionTypeId," +
+	   		" model.actionTypeStatusId " +
+	   		" order by " +
+	   		" model.actionType.actionTypeId ");
+	    Query query = getSession().createQuery(queryStr.toString());
+	     return query.list();
 	}
 }
