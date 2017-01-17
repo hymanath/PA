f9d4ddb7c73d6918435ad40a562dc6d25271766b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-17 11:55:34 +0000

    QA app dashboard changes - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICardPrintService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICardPrintService.java
@@ -18,7 +18,7 @@ public interface ICardPrintService {
 	public List<CardPrintVO> getConstListByVendor(Long vendorId,Long districtId);
 	public CardPrintVO getStatusWisePrintingConstituencyDetails(Long stateId,Long vendorId,String startDateStr,String endDateStr);
 	public CardPrintVO getDistrictWiseStatusWiseConstituenciesCounts(Long vendorId,String fromDateStr,String toDateStr);
-	public List<CardPrintingDispatchVO> getPrintingDispatchDetails(Long vendorId,Long districtId,Long constituencyId);
+	public CardPrintingDispatchVO getPrintingDispatchDetails(Long vendorId,Long districtId,Long constituencyId);
 	public CardPrintVO getVendorWiseStatusWiseConstituenciesDetails(Long stateId,String fromDateStr,String toDateStr);
 	
 	public PrintUpdateDetailsStatusVO updatePrintDetailsToTdpCadreCardPrint(final List<PrintVO> printList);
