dece87eddb577b0037df83141ba6ebe647205856
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-18 12:23:54 +0000

    Qa android dashboard web services - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CardPrintValidationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CardPrintValidationDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -33,6 +34,121 @@ public class CardPrintValidationDAO extends GenericDaoHibernate<CardPrintValidat
 								" and model.printStatus = 'R'" +
 								" group by model.boxNo");
 		query.setParameterList("boxNos", boxNos);
+		
+		return query.list();
+	}
+	
+	//
+	public List<Object[]> constWiseBoxesCountAndValidatedCardsCount(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select TC.userAddress.constituency.constituencyId ,TC.userAddress.constituency.name,count(distinct CPV.boxNo),count(distinct CPV.tdpCadreId) " +
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by TC.userAddress.constituency.constituencyId " +
+				"   order by TC.userAddress.constituency.name ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
+		
+		return query.list();
+	}
+	
+	public List<Object[]> constWiseAcceptedAndRejectedCards(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select TC.userAddress.constituency.constituencyId ,CPV.printStatus,count(distinct CPV.tdpCadreId) " +
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by TC.userAddress.constituency.constituencyId,CPV.printStatus ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
+		
+		return query.list();
+	}
+	public List<Object[]> getConstWiseBoxWiseValidatedCardsCount(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select TC.userAddress.constituency.constituencyId ,CPV.boxNo,count(distinct CPV.tdpCadreId) " +//2
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by TC.userAddress.constituency.constituencyId,CPV.boxNo ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
+		
+		return query.list();
+	}
+	
+	public List<Object[]> getConstWiseBoxWiseStatusWiseCounts(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select TC.userAddress.constituency.constituencyId ,CPV.boxNo,CPV.printStatus , count(distinct CPV.tdpCadreId) " +//3
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by TC.userAddress.constituency.constituencyId,CPV.boxNo,CPV.printStatus ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
+		
+		return query.list();
+	}
+	
+	public List<Object[]> getBoxWiseValidatedCardsCountByUser(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select CPV.boxNo,count(distinct CPV.tdpCadreId),TC.userAddress.constituency.name  " +//2
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by CPV.boxNo " +
+				  " order by TC.userAddress.constituency.name ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
+		
+		return query.list();
+	}
+	public List<Object[]> getBoxWiseStatusWiseCountsByUser(Long userId , Date fromDate , Date toDate){
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select CPV.boxNo , CPV.printStatus , count(distinct CPV.tdpCadreId) " +//2
+				  " from  CardPrintValidation CPV , TdpCadre TC " +
+				  " where CPV.tdpCadreId = TC.tdpCadreId and " +
+				  "       CPV.cardPrintValidationUserId = :userId ");
+		if(fromDate != null && toDate != null){
+			sb.append(" and date(CPV.insertedTime) between :fromDate and :toDate ");
+		}
+		sb.append(" group by CPV.boxNo , CPV.printStatus ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("userId", userId);
+		query.setDate("fromDate", fromDate);
+		query.setDate("toDate", toDate);
 		
 		return query.list();
 	}
