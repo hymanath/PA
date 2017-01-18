f0f703a67b658111098c1c4c5fb10670847eced1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-18 12:23:32 +0000

    Qa android dashboard web services - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -10,4 +11,12 @@ public interface ICardPrintValidationDAO extends GenericDao<CardPrintValidation,
 
 	public List<Object[]> getValidatedCardsCountsForBoxNos(List<String> boxNos);
 	public List<Object[]> getErrorCardsCountsForBoxNos(List<String> boxNos);
+	
+	public List<Object[]> constWiseBoxesCountAndValidatedCardsCount(Long userId , Date fromDate , Date toDate);
+	public List<Object[]> constWiseAcceptedAndRejectedCards(Long userId , Date fromDate , Date toDate);
+	public List<Object[]> getConstWiseBoxWiseValidatedCardsCount(Long userId , Date fromDate , Date toDate);
+	public List<Object[]> getConstWiseBoxWiseStatusWiseCounts(Long userId , Date fromDate , Date toDate);
+	
+	public List<Object[]> getBoxWiseValidatedCardsCountByUser(Long userId , Date fromDate , Date toDate);
+	public List<Object[]> getBoxWiseStatusWiseCountsByUser(Long userId , Date fromDate , Date toDate);
 }
