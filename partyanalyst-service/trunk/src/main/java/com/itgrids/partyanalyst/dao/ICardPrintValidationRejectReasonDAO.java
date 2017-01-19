bf2561247d656f9a94350492005ba6715c1dc40f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 15:07:02 +0000

    same tdpcadre gets validated in qa scenario - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationRejectReasonDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationRejectReasonDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.CardPrintValidationRejectReason;
 
 public interface ICardPrintValidationRejectReasonDAO extends GenericDao<CardPrintValidationRejectReason,Long>{
-
+	
+	public List<Long> checkRejectReasonsByCardPrintValidationId(Long cardPrintValidationId);
+	public Integer deleteCardPrintValidationRejectReasonIds(List<Long> ids);
 }
