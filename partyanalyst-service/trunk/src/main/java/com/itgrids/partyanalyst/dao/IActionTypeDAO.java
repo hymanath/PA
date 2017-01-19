25c3c0406dc0840d4de9aa6ad263c546cf43566b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 12:59:33 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActionTypeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActionTypeDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActionType;
 
 public interface IActionTypeDAO extends GenericDao<ActionType, Long> {
-
+	public List<Object[]> getActionTypeList();
 }
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDocumentDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AlertDocument;
+
+public interface IAlertDocumentDAO extends GenericDao<AlertDocument, Long> {
+
+}
