f304ed443a12ef4c5c6ed17965fcad68ae951816
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 14:44:29 +0000

    finalized party meetings---by Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITrainingCampService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ITrainingCampService.java
@@ -134,4 +134,6 @@ public interface ITrainingCampService {
 	public List<SimpleVO> getMandalsForDistrictIdDetails(List<Long> list);
 	public List<CallStatusVO> getMeetingTypesNew(List<Long> locationLevels);
 	public List<CallStatusVO> getFinalAllMeetings(Long meetingType,Long locationLevel,List<Long> stateIds,List<Long> districtIds,List<Long> constituencyIds,List<Long> mandalTownDivisonIds,List<Long> villageWardIds,String startDateString,String endDateString);
+	public ResultStatus saveFinalizedMeetingDetails(final Long partyMeetingId,final String memberType,final String membershipId,final String name,
+			final String mobileNo,final String remark,final String statusId,final String updateBy,final Long userId);
 }
