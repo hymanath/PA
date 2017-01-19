32b947a809c52f3d761e7c4e4eaf99e03ba408b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 15:07:27 +0000

    same tdpcadre gets validated in qa scenario - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ICardPrintValidationTrackDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.CardPrintValidationTrack;
+
+public interface ICardPrintValidationTrackDAO extends GenericDao<CardPrintValidationTrack,Long>{
+
+}
