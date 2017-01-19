65fe22fa5d4e338b6285246db815478bc5483e44
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 15:08:13 +0000

    same tdpcadre gets validated in qa scenario - SREEDHAR

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CardPrintValidationRejectReasonDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/CardPrintValidationRejectReasonDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ICardPrintValidationRejectReasonDAO;
 import com.itgrids.partyanalyst.model.CardPrintValidationRejectReason;
@@ -10,5 +13,22 @@ public class CardPrintValidationRejectReasonDAO extends GenericDaoHibernate<Card
 	public CardPrintValidationRejectReasonDAO() {
 		super(CardPrintValidationRejectReason.class);
 	}
-
+	
+	public List<Long> checkRejectReasonsByCardPrintValidationId(Long cardPrintValidationId){
+		
+		Query query = getSession().createQuery("" +
+		"select model.cardPrintValidationRejectReasonId " +
+		"from   CardPrintValidationRejectReason model where model.cardPrintValidationId =:cardPrintValidationId and model.isDeleted = 'N' ");
+		query.setParameter("cardPrintValidationId", cardPrintValidationId);
+		return query.list();
+	}
+	
+	public Integer deleteCardPrintValidationRejectReasonIds(List<Long> ids)
+	{
+		Query query = getSession().createQuery("update CardPrintValidationRejectReason model set model.isDeleted = 'Y'   " +
+				" where model.cardPrintValidationRejectReasonId in (:ids) ");
+		query.setParameterList("ids", ids);
+		return query.executeUpdate();
+	}
+	
 }
