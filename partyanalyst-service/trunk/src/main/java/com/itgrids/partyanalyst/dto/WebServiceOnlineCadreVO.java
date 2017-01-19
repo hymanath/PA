c7c5b3b9f999a236151a74eca297f1eef52de9ee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-19 14:53:25 +0000

    updated by srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/WebServiceOnlineCadreVO.java
@@ -0,0 +1,112 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class WebServiceOnlineCadreVO implements Serializable{
+	
+	private static final long serialVersionUID = 6825969668387035067L;
+
+	private Long constituencyId;
+	private Long mandalId;
+	private Long villageId;
+	private Long boothId;
+	private String name;
+	private String mobileNo;
+	private String houseNo;
+	
+	private Long voterId;
+	private Long familyVoterId;
+	private Long tdpCadreId;
+	private String status;
+	
+	private String memberShipNo;
+	private String voterNo;
+	private Long cadreSurveyUserId;
+	
+	
+	public Long getCadreSurveyUserId() {
+		return cadreSurveyUserId;
+	}
+	public void setCadreSurveyUserId(Long cadreSurveyUserId) {
+		this.cadreSurveyUserId = cadreSurveyUserId;
+	}
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public Long getMandalId() {
+		return mandalId;
+	}
+	public void setMandalId(Long mandalId) {
+		this.mandalId = mandalId;
+	}
+	public Long getVillageId() {
+		return villageId;
+	}
+	public void setVillageId(Long villageId) {
+		this.villageId = villageId;
+	}
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	public Long getVoterId() {
+		return voterId;
+	}
+	public void setVoterId(Long voterId) {
+		this.voterId = voterId;
+	}
+	public Long getFamilyVoterId() {
+		return familyVoterId;
+	}
+	public void setFamilyVoterId(Long familyVoterId) {
+		this.familyVoterId = familyVoterId;
+	}
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMemberShipNo() {
+		return memberShipNo;
+	}
+	public void setMemberShipNo(String memberShipNo) {
+		this.memberShipNo = memberShipNo;
+	}
+	public String getVoterNo() {
+		return voterNo;
+	}
+	public void setVoterNo(String voterNo) {
+		this.voterNo = voterNo;
+	}
+
+}
