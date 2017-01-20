504da8a9915a9ae52eac38101c195f893bc965ee
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-20 11:14:09 +0000

    finalized party meetings---by Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingUpdationDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingUpdationDetailsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IPartyMeetingUpdationDetailsDAO;
 import com.itgrids.partyanalyst.model.PartyMeetingUpdationDetails;
@@ -10,5 +13,25 @@ public class PartyMeetingUpdationDetailsDAO extends GenericDaoHibernate<PartyMee
 	public PartyMeetingUpdationDetailsDAO() {
 		super(PartyMeetingUpdationDetails.class);
 	}
-
+	  @SuppressWarnings("unchecked")
+	public List<Object[]> getCommentsDetailsByPartyMeetingId(Long partyMeetingId){
+		  
+	      	Query query = getSession().createQuery(" select model.partyMeetingUpdationDetailsId,model.comment,model.name,model.insertedTime," +
+	      			" model.mobileNo ,tdpCadre.tdpCadreId,tdpCadre.memberShipNo,tdpCadre.image " +
+	      			" from PartyMeetingUpdationDetails model " +
+	      			" left join model.tdpCadre tdpCadre where " +
+	      			" model.partyMeetingId = :partyMeetingId and model.isDeleted= 'false' order by model.insertedTime desc");
+	      	
+	      	query.setParameter("partyMeetingId",partyMeetingId);
+	      	return query.list();
+	      }
+	public List<Object[]> getCommentsAvailableByPartyMeetingId(List<Long> partyMeetingIds){
+		  
+      	Query query = getSession().createQuery(" select model.partyMeetingId,model.comment " +
+      			" from PartyMeetingUpdationDetails model where " +
+      			" model.partyMeetingId in(:partyMeetingIds) and model.isDeleted= 'false' ");
+      	
+      	query.setParameterList("partyMeetingIds",partyMeetingIds);
+      	return query.list();
+      }
 }
