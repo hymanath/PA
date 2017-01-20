72d5ee5a90f0170f78e29589697212c2556998b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-20 11:13:50 +0000

    finalized party meetings---by Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingUpdationDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingUpdationDetailsDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.PartyMeetingUpdationDetails;
 
 public interface IPartyMeetingUpdationDetailsDAO extends GenericDao<PartyMeetingUpdationDetails, Long>{
+	public List<Object[]> getCommentsAvailableByPartyMeetingId(List<Long> partyMeetingIds);
+	public List<Object[]> getCommentsDetailsByPartyMeetingId(Long partyMeetingId);
 
 }
