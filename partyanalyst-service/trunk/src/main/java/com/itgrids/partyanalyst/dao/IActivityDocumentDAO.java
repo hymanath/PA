a2ddba9d2b9517ad7a77f6e56109d333d99a8d9a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-23 09:15:32 +0000

    Updated By Hyma : dao ucommitting.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityDocumentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityDocumentDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActivityDocument;
 
 public interface IActivityDocumentDAO extends GenericDao<ActivityDocument, Long>{
 
+	
+	public List<Object[]> getImagesCoveredAndTotalImagesForConstituencies(Long districtId,Long activityScopeId,String  searchType,String  type);
 }
