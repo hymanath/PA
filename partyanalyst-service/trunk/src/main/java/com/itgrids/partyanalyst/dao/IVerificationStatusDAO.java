3a8300b8b5f57d9b5d1aa33f23deaddd0d899485
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-25 16:13:50 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVerificationStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IVerificationStatusDAO.java
@@ -14,7 +14,8 @@ public interface IVerificationStatusDAO extends GenericDao<VerificationStatus, L
 	public Object[] getAertStausIdAndName(Long alertId);
 	public Long getAlertStatusId(Long alertId);
 	public List<Object[]> getStatusWiseAlertCount(Long stateId,Date fromDate,Date toDate,Long alertTypeId);
-	public List<Object[]> getAllAlerts(List<Long> sourceIds,AlertInputVO inputVO,Date fromDate,Date toDate);
+	public List<Object[]> getAllAlerts(List<Long> sourceIds,AlertInputVO inputVO,Date fromDate,Date toDate,Date fromDate2,Date toDate2);
 	public List<Object[]> getAlertCountStatusWiseBasedOnActionType(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,Date fromDate,Date toDate,List<Long> alertType,List<Long> editionTypes);
 	public List<Object[]> getActionTypeAlertDetails(Date fromDate, Date toDate, Long stateId, Long alertTypeId, Long alertActionStatusId, Long userAccessLevelId, List<Long> userAccessLevelValues,List<Long> editionList,Long actionTypeId);
+	public List<Object[]> getTotalStatus();
 }
