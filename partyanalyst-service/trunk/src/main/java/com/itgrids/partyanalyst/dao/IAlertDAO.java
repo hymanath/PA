3fceb495b02086ca55eec209943afa4b51ab906c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-30 11:54:34 +0000

    Getting Alert Details By AlertType in CoreDashboard Alert Module by -- Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
@@ -78,4 +78,5 @@ public interface IAlertDAO extends GenericDao<Alert, Long> {
 	public List<Object[]> getProgramCommitteeTypeAlertDtlsForOrganization(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,List<Long> impactLevelIds,Date fromDate,Date toDate,List<Long> alertTypeList,List<Long> editionTypeList);
 	public List<Object[]> getAllAlertDtlsForOrganization(Long userAccessLevelId,Set<Long> userAccessLevelValues,Long stateId,List<Long> impactLevelIds,Date fromDate,Date toDate,List<Long> alertTypeList,List<Long> editionTypeList);
 	public List<Object[]> getTotalAlertGroupByDistForOrganization(Date fromDate, Date toDate, Long stateId, List<Long> scopeIdList, Long userAccessLevelId, List<Long> userAccessLevelValues,List<Long> alertTypeList, List<Long> editionList);
+	 public List<Object[]> getAlertDtlsByAlertTypeId(Date fromDate, Date toDate, Long stateId, Long alertTypeId,Long userAccessLevelId, List<Long> userAccessLevelValues);
 }
