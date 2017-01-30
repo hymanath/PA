2d08c0dbce8ab26ada0eda2e9794bb7a3eeba786
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-01-30 13:32:35 +0000

    commit by Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityConductedInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityConductedInfoDAO.java
@@ -13,6 +13,6 @@ public interface IActivityConductedInfoDAO extends GenericDao<ActivityConductedI
 	public List<Object[]> getIVRCountsForScopeIds(List<Long> activityScopeIds);
 	public List<Object[]> getInfocellCountsForScopeIds(List<Long> activityScopeIds);
 	public List<Object[]> activitiesDistrictWiseCohort(List<Long> activityIdsLst,Date startDate,Date endDate,Long scopeId);
-	public List<Object[]> getDistrictWiseActivityCounts(Long districtId,Long activityScopeId, String searchType,Long stateId,String countType);
+	public List<Object[]> getDistrictWiseActivityCounts(Long districtId,Long activityScopeId, String searchType,Long stateId,String countType,String type);
 	public List<Object[]> getActivityConductedCntBasedOnUserAccesslevel(Long userAccessLevelId,List<Long> userAccessLevelValues,Long stateId,Long activityId,List<Long> activityLevelIds,Date fromDate,Date toDate);
 }
