bf069c0754b54bad08fc6569f3fc7ea38731bd80
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-02 11:57:46 +0000

    Updated By Hyma : Activities Queries  Changed  As per  UserAccessLevelId,UserLevelValues.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityInfoDocumentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityInfoDocumentDAO.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.dao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import org.appfuse.dao.GenericDao;
 
@@ -10,13 +11,13 @@ import com.itgrids.partyanalyst.dto.SearchAttributeVO;
 import com.itgrids.partyanalyst.model.ActivityInfoDocument;
 
 public interface IActivityInfoDocumentDAO extends GenericDao<ActivityInfoDocument, Long>{
-	public List<Object[]> getEventDocuments(EventDocumentVO inputVO,Date startDate,Date endDate);
+	public List<Object[]> getEventDocuments(EventDocumentVO inputVO,Date startDate,Date endDate,Long userAccessLevelId,Set<Long> userAccessLevelValues);
 	public List<Object[]> getLocations(EventDocumentVO inputVO,Date startDate,Date endDate,String type);
 	
 	
 	public Integer deleteEventUploadFilebyActivityInfoDocId(List<Long> activityInfoDocIdList);
 	public List<Object[]> getAvailableDates(EventDocumentVO inputVO,Date startDate,Date endDate,String type);
-	public Long getEventDocumentsCount(EventDocumentVO inputVO,Date startDate,Date endDate);
+	public Long getEventDocumentsCount(EventDocumentVO inputVO,Date startDate,Date endDate,Long userAccessLevelId,Set<Long> userAccessLevelValues);
 	public List<Object[]> getLocationWiseImageCount(EventDocumentVO inputVO,Date startDate,Date endDate,String type);
 	
 	public List<Object[]> getActivityDocumentsImagesByLevelIdAndLevelValue(Long levelId,Long levelValue,Long day,Long activityScopeId,Date activityDate,Integer startIndex,Integer maxIndex);
@@ -24,8 +25,10 @@ public interface IActivityInfoDocumentDAO extends GenericDao<ActivityInfoDocumen
 	public Long getActivityDocumentsImagesCountByLevelIdAndLevelValue(Long levelId,Long levelValue,Long day,Long activityScopeId,Date activityDate);
 	public List<Object[]> getActivityInfoImagesCount(SearchAttributeVO inputVO,Long stateId);
 	
-	public List<Object[]> getEventDocumentsByLocationInfo(EventDocumentVO inputVO,Date startDate,Date endDate);
-	public Long getEventDocumentsCountByLocationInbfo(EventDocumentVO inputVO,Date startDate,Date endDate);
+	public List<Object[]> getEventDocumentsByLocationInfo(EventDocumentVO inputVO,Date startDate,Date endDate,
+			Long userAccessLevelId,Set<Long> userAccessLevelValues);
+	public Long getEventDocumentsCountByLocationInbfo(EventDocumentVO inputVO,Date startDate,Date endDate,
+			Long userAccessLevelId,Set<Long> userAccessLevelValues);
 	public List<Object[]> getDayWiseActivityInfoImagesCount(SearchAttributeVO inputVO,Long stateId);
 	//public List<Object[]> getActivityQuestionnairesCountsByLocation(SearchAttributeVO searchAttributeVO,Long stateId);
 	public List<Object[]> getLocationValue(Long activityScopeId);
