f8db73cd15a93af632cd54f4b64ef975616029d6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-02 15:01:05 +0000

    commit by Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingUpdationDetailsDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/PartyMeetingUpdationDetailsDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
@@ -34,4 +35,53 @@ public class PartyMeetingUpdationDetailsDAO extends GenericDaoHibernate<PartyMee
       	query.setParameterList("partyMeetingIds",partyMeetingIds);
       	return query.list();
       }
+	public List<Object[]> getUpdatedDetails(Long locationLevelId,Date startDate,Date endDate){
+		StringBuilder sb = new StringBuilder();
+		    sb.append("select distinct model.partyMeeting.partyMeetingId," +
+		    		" model.partyMeeting.meetingName," +
+		    		" model.user.userName" +
+		    		" from PartyMeetingUpdationDetails model " +
+		    		" where model.isDeleted= 'false'");
+		    if(locationLevelId != null && locationLevelId.longValue() == 7l)
+		    	sb.append(" and model.partyMeeting.partyMeetingLevel.partyMeetingLevelId in (7,8)");
+		    else if(locationLevelId != null && locationLevelId.longValue() == 4l)
+		    	sb.append(" and model.partyMeeting.partyMeetingLevel.partyMeetingLevelId in (4,5,6)");
+		    else if(locationLevelId != null && (locationLevelId.longValue() == 3l || locationLevelId.longValue() == 2l))
+		    	sb.append(" and model.partyMeeting.partyMeetingLevel.partyMeetingLevelId = :locationLevelId");
+		    if(startDate != null && endDate != null){
+	            sb.append(" and  ( (date(model.partyMeeting.startDate)>=:startDate and date(model.partyMeeting.startDate)<=:endDate) or (date(model.partyMeeting.endDate)>=:startDate and date(model.partyMeeting.endDate)<=:endDate) ) ");
+	        }
+		
+		 Query query = getSession().createQuery(sb.toString());
+		 	if(locationLevelId != null && (locationLevelId.longValue() == 3l || locationLevelId.longValue() == 2l))
+		 		query.setParameter("locationLevelId", locationLevelId);
+		 	if(startDate != null && endDate != null){
+		 		query.setDate("startDate", startDate);
+		 		query.setDate("endDate", endDate);
+		 	}
+		 	return query.list();
+	}
+	public List<Object[]> getDocumentList(Long partyMeetingId){
+		Query query = getSession().createQuery("select model.partyMeetingUpdationDetailsId," +
+		    	" model.name," +
+		    	" model.mobileNo," +
+		    	" model.comment," +
+		    	" model.insertedTime," +
+		    	" tdpCadre.memberShipNo " +
+				" from PartyMeetingUpdationDetails model" +
+				" left join model.tdpCadre tdpCadre" +
+				" where model.partyMeeting.partyMeetingId = :partyMeetingId" +
+				" and model.isDeleted= 'false'");
+		query.setParameter("partyMeetingId", partyMeetingId);
+		
+		return query.list();
+	}
+	public String getMemberShipNo(Long partyMeetingId){
+		Query query = getSession().createQuery("select model.tdpCadre.memberShipNo " +
+				" from  PartyMeetingUpdationDetails model" +
+				" where model.partyMeeting.partyMeetingId = :partyMeetingId" +
+				" and model.isDeleted= 'false'");
+		query.setParameter("partyMeetingId", partyMeetingId);
+		return  (String) query.uniqueResult();
+	}
 }
