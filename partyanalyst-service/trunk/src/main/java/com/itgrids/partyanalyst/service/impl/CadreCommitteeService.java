63c71ad883cabb3fb940f5b898173ecaa764b4c7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-07 15:42:18 +0000

    Roles based propsed and finalized candidates in committees

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreCommitteeService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CadreCommitteeService.java
@@ -2172,13 +2172,13 @@ public class CadreCommitteeService implements ICadreCommitteeService
 	}
 
 	public CadreCommitteeVO searchTdpCadreDetailsBySearchCriteriaForCadreCommitte(Long locationLevel,Long locationId, String searchName,String memberShipCardNo,
-			String voterCardNo, String trNumber, String mobileNo,Long casteStateId,String casteCategory,Long fromAge,Long toAge,String houseNo,String gender,int startIndex,int maxIndex,boolean isRemoved,Long enrollmentId)
+			String voterCardNo, String trNumber, String mobileNo,Long casteStateId,String casteCategory,Long fromAge,Long toAge,String houseNo,String gender,int startIndex,int maxIndex,boolean isRemoved,Long enrollmentId,String searchType)
 	{
 		CadreCommitteeVO cadreCommitteeVO = new CadreCommitteeVO();
 		try {
 			List<Long> tdpCadreIdsNominatedIdsList = new ArrayList<Long>(0);
 			TdpCadreVO tdpCadreVO = cadreDetailsService.searchTdpCadreDetailsBySearchCriteriaForCommitte(locationLevel,locationId, searchName,memberShipCardNo, 
-					voterCardNo, trNumber, mobileNo,casteStateId,casteCategory,fromAge,toAge,houseNo,gender,startIndex,maxIndex,isRemoved,enrollmentId);
+					voterCardNo, trNumber, mobileNo,casteStateId,casteCategory,fromAge,toAge,houseNo,gender,startIndex,maxIndex,isRemoved,enrollmentId,searchType);
 			List<CadreCommitteeVO> cadreCommitteeList = null;
 			if(tdpCadreVO != null)
 			{
@@ -2375,7 +2375,7 @@ public class CadreCommitteeService implements ICadreCommitteeService
 		}
 	}
 	public void setCurrentElectrolInfo(List<CadreCommitteeVO> cadreCommitteeList,List<Long> tdpCadreIdsList){
-		List<Object[]> tdpCommitteeMemberList = tdpCommitteeElectrolsDAO.getTdpCommitteeElectrolsForTdpCadreIdList(tdpCadreIdsList);
+		List<Object[]> tdpCommitteeMemberList = tdpCommitteeElectrolsDAO.getTdpCommitteeElectrolsForTdpCadreIdList(tdpCadreIdsList,IConstants.CURRENT_ENROLLMENT_ID);
 		
 		if(tdpCommitteeMemberList != null && tdpCommitteeMemberList.size()>0)
 		{
