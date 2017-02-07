6dfd151583b5c47eee36503209e7debb3e783ab2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-07 07:18:13 +0000

    adding new fileds into model for alert - Balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AlertStatus.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AlertStatus.java
@@ -2,15 +2,22 @@ package com.itgrids.partyanalyst.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "alert_status")
@@ -19,6 +26,14 @@ public class AlertStatus extends BaseModel implements Serializable {
 	private Long alertStatusId;
 	private String alertStatus;
 	private Long statusOrder;
+	
+	private Long alertTypeId;
+	private Long govtDepartmentId;
+	
+	private AlertType alertType;
+	private GovtDepartment govtDepartment;
+	
+	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -47,6 +62,49 @@ public class AlertStatus extends BaseModel implements Serializable {
 	public void setStatusOrder(Long statusOrder) {
 		this.statusOrder = statusOrder;
 	}
+
+	@Column(name = "alert_type_id")
+	public Long getAlertTypeId() {
+		return alertTypeId;
+	}
+
+	public void setAlertTypeId(Long alertTypeId) {
+		this.alertTypeId = alertTypeId;
+	}
+
+	@Column(name = "govt_department_id")
+	public Long getGovtDepartmentId() {
+		return govtDepartmentId;
+	}
+
+	public void setGovtDepartmentId(Long govtDepartmentId) {
+		this.govtDepartmentId = govtDepartmentId;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "alert_type_id", insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public AlertType getAlertType() {
+		return alertType;
+	}
+
+	public void setAlertType(AlertType alertType) {
+		this.alertType = alertType;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "govt_department_id", insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public GovtDepartment getGovtDepartment() {
+		return govtDepartment;
+	}
+
+	public void setGovtDepartment(GovtDepartment govtDepartment) {
+		this.govtDepartment = govtDepartment;
+	}
+	
 	
 
 }
