b47430cedac4f951d4f631157c3298648851f09b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 07:27:02 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtDepartmentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtDepartmentDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 
 import com.itgrids.partyanalyst.dao.IGovtDepartmentDAO;
@@ -10,6 +12,9 @@ public class GovtDepartmentDAO extends GenericDaoHibernate<GovtDepartment, Long>
 	public GovtDepartmentDAO() {
 		super(GovtDepartment.class);
 		
+	}
+	public List<Object[]> getAllDepartment(){
+		return getHibernateTemplate().find("select model.govtDepartmentId,model.departmentName from GovtDepartment  model order by model.departmentName ");
 	}
 
 }
