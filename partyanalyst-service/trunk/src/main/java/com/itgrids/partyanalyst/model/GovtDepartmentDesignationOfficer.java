6db05a092c3814aec815ac50170aee749e791cb8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 15:09:13 +0000

    CCC Implementation -- SRAVANTH.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtDepartmentDesignationOfficer.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtDepartmentDesignationOfficer.java
@@ -25,13 +25,15 @@ import org.hibernate.annotations.NotFoundAction;
 public class GovtDepartmentDesignationOfficer extends BaseModel implements Serializable{
 
 	private Long govtDepartmentDesignationOfficerId;
-	private Long govtDepartmentDesignationLocationId;
+	private Long govtDepartmentDesignationId;
+	private Long govtDepartmentLevelId;
+	private Long levelValue;
+	private Long addressId;
+	private Long userId;
 	private Long offId;
-	private String officerName;
-	private String mobileNo;
-	private String emailId;
 	
-	private GovtDepartmentDesignationLocation govtDepartmentDesignationLocation;
+	private GovtDepartmentDesignation govtDepartmentDesignation;
+	private GovtDepartmentLevel govtDepartmentLevel;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -44,13 +46,44 @@ public class GovtDepartmentDesignationOfficer extends BaseModel implements Seria
 		this.govtDepartmentDesignationOfficerId = govtDepartmentDesignationOfficerId;
 	}
 	
-	@Column(name = "govt_department_designation_location_id")
-	public Long getGovtDepartmentDesignationLocationId() {
-		return govtDepartmentDesignationLocationId;
+	@Column(name = "govt_department_designation_id")
+	public Long getGovtDepartmentDesignationId() {
+		return govtDepartmentDesignationId;
 	}
-	public void setGovtDepartmentDesignationLocationId(
-			Long govtDepartmentDesignationLocationId) {
-		this.govtDepartmentDesignationLocationId = govtDepartmentDesignationLocationId;
+	public void setGovtDepartmentDesignationId(Long govtDepartmentDesignationId) {
+		this.govtDepartmentDesignationId = govtDepartmentDesignationId;
+	}
+	
+	@Column(name = "govt_department_level_id")
+	public Long getGovtDepartmentLevelId() {
+		return govtDepartmentLevelId;
+	}
+	public void setGovtDepartmentLevelId(Long govtDepartmentLevelId) {
+		this.govtDepartmentLevelId = govtDepartmentLevelId;
+	}
+	
+	@Column(name = "level_value")
+	public Long getLevelValue() {
+		return levelValue;
+	}
+	public void setLevelValue(Long levelValue) {
+		this.levelValue = levelValue;
+	}
+	
+	@Column(name = "address_id")
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	
+	@Column(name = "user_id")
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
 	
 	@Column(name = "off_id")
@@ -61,39 +94,26 @@ public class GovtDepartmentDesignationOfficer extends BaseModel implements Seria
 		this.offId = offId;
 	}
 	
-	@Column(name = "officer_name")
-	public String getOfficerName() {
-		return officerName;
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="govt_department_designation_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public GovtDepartmentDesignation getGovtDepartmentDesignation() {
+		return govtDepartmentDesignation;
 	}
-	public void setOfficerName(String officerName) {
-		this.officerName = officerName;
-	}
-	
-	@Column(name = "mobile_no")
-	public String getMobileNo() {
-		return mobileNo;
-	}
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-	
-	@Column(name = "email_id")
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
+	public void setGovtDepartmentDesignation(
+			GovtDepartmentDesignation govtDepartmentDesignation) {
+		this.govtDepartmentDesignation = govtDepartmentDesignation;
 	}
 	
 	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-	@JoinColumn(name="govt_department_designation_location_id", insertable=false, updatable = false)
+	@JoinColumn(name="govt_department_level_id", insertable=false, updatable = false)
 	@LazyToOne(LazyToOneOption.NO_PROXY)
 	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
-	public GovtDepartmentDesignationLocation getGovtDepartmentDesignationLocation() {
-		return govtDepartmentDesignationLocation;
+	public GovtDepartmentLevel getGovtDepartmentLevel() {
+		return govtDepartmentLevel;
 	}
-	public void setGovtDepartmentDesignationLocation(
-			GovtDepartmentDesignationLocation govtDepartmentDesignationLocation) {
-		this.govtDepartmentDesignationLocation = govtDepartmentDesignationLocation;
+	public void setGovtDepartmentLevel(GovtDepartmentLevel govtDepartmentLevel) {
+		this.govtDepartmentLevel = govtDepartmentLevel;
 	}
 }
