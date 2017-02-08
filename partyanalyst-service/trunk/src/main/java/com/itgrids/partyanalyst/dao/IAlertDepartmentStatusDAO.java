b5b77966264a729a5a718f6006acb3f68aa16e9f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 14:13:26 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDepartmentStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDepartmentStatusDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.AlertDepartmentStatus;
 
 public interface IAlertDepartmentStatusDAO extends GenericDao<AlertDepartmentStatus, Long>{
-
+	public List<Object[]> getAllStatus();
 }
