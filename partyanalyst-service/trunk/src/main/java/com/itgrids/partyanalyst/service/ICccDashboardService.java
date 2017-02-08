06587d5611fd36c8b1b9c541a22fa334e644e15e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 07:27:30 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICccDashboardService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.AlertVO;
+
+public interface ICccDashboardService {   
+	public List<AlertVO> getTotalAlertGroupByStatus(String fromDateStr, String toDateStr, Long stateId, List<Long> printIdList, List<Long> electronicIdList, List<Long> deptIdList);
+	public List<AlertVO> getTotalAlertGroupByStatusThenDepartment(String fromDateStr, String toDateStr, Long stateId, List<Long> printIdList, List<Long> electronicIdList, List<Long> deptIdList);
+} 
