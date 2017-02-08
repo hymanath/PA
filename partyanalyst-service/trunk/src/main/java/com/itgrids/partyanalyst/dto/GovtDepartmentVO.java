5b48cd001664dc60f1575bb2794c5e49e9f94824
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 07:38:29 +0000

    CCC Department VO -- SRAVANTH.

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/GovtDepartmentVO.java
@@ -0,0 +1,32 @@
+/**
+ * @author Sravanth
+ * Feb 08, 2017
+ * GovtDepartmentVO.java
+ * PA - SERV - com.itgrids.partyanalyst.dto
+ */
+package com.itgrids.partyanalyst.dto;
+
+/**
+ * @author Sravanth
+ * @date Feb 08, 2017
+ *
+ */
+public class GovtDepartmentVO {
+
+	private Long id;
+	private String name;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
