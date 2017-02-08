6802f65dfd307fa7e4a604d3fd2bc53b8dec0760
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 12:47:22 +0000

    updated by srujana

--- partyanalyst/src/main/webapp/js/cadreCommittee/cadreCommitteeDashboard1.js
+++ partyanalyst/src/main/webapp/js/cadreCommittee/cadreCommitteeDashboard1.js
@@ -49,3 +49,49 @@ function getCommitteeDetailsByEnrollement(){
 				}
 			});
 	}
+	function getCadreEnrollmentYears1(){
+		 var jsObj={
+		
+		       };
+			   
+		 $.ajax({
+			type : "GET",
+			url : "getCadreEnrollmentYearsAction.action",
+			dataType: 'json',
+			data: {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			if(result != null && result.length > 0){
+				for(var i in result){
+					$("#tdpCommitteeYearId1").append('<option value='+result[i].id+'>'+result[i].electionYear+'</option>');
+				}
+			}
+		});
+	}
+	function getCommitteeDetailsByEnrollement1(){
+	  var enrollmentIdsArr = new Array();
+	  enrollmentIdsArr.push($("#tdpCommitteeYearId1").val());
+	var jsObj={
+			enrollmentIdsArr:enrollmentIdsArr
+		};
+			   
+		 $.ajax({
+			type : "GET",
+			url : "getCommitteeDetailsByEnrollementIdAction.action",
+			dataType: 'json',
+			data: {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+			if(result != null && result.length > 0){
+				for(var i in result){
+					 var fYear = result[i].fromDate.split('-')[0];
+					 var fMonth = result[i].fromDate.split('-')[1];
+					 var fDate = result[i].fromDate.split('-')[2];
+					 var tYear = result[i].toDate.split('-')[0];
+					 var tMonth = result[i].toDate.split('-')[1];
+					 var tDate = result[i].toDate.split('-')[2].substring(0,2);
+					 
+					$('#reportrange1').data('daterangepicker').setStartDate(fDate+'/'+fMonth+'/'+fYear);
+					$('#reportrange1').data('daterangepicker').setEndDate(tDate+'/'+tMonth+'/'+tYear);
+				}
+				}
+			});
+	}
