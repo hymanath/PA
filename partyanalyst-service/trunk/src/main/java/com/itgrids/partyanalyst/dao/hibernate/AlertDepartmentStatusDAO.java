d26cbb78eeba0618095c6a7bd72d877e8891bebe
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-08 14:15:01 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertDepartmentStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertDepartmentStatusDAO.java
@@ -1,15 +1,34 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAlertDepartmentStatusDAO;
 import com.itgrids.partyanalyst.model.AlertDepartmentStatus;
+import com.itgrids.partyanalyst.utils.IConstants;
 
 public class AlertDepartmentStatusDAO extends GenericDaoHibernate<AlertDepartmentStatus, Long> implements IAlertDepartmentStatusDAO{
 
 	public AlertDepartmentStatusDAO() {
 		super(AlertDepartmentStatus.class);
 		
+	}
+	public List<Object[]> getAllStatus(){
+		StringBuilder queryStr = new StringBuilder();
+		queryStr.append(" select " +
+						" alertDepartmentStatus.alertStatus.alertStatusId, " +
+						" alertDepartmentStatus.alertStatus.alertStatus " +
+						" from " +
+						" AlertDepartmentStatus alertDepartmentStatus " +
+						" where " +
+						" alertDepartmentStatus.alertType.alertTypeId in ("+IConstants.GOVT_ALERT_TYPE_ID+") ");
+		//queryStr.append(" and alertDepartmentStatus.govtDepartment.govtDepartmentId in (:govtDepartmentId)");
+		
+		queryStr.append(" order by alertDepartmentStatus.alertStatus.statusOrder ");
+		Query query = getSession().createQuery(queryStr.toString());
+		return query.list();
 	}
 
 }
