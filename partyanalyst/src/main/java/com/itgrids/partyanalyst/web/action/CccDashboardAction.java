b6ec24abdbeced28456793314d3b2634cafaf763
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-09 13:17:44 +0000

    Alert Assigning to Officers Saving And Retreiving -- SRAVANTH.

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CccDashboardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CccDashboardAction.java
@@ -1,42 +1,93 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.io.File;
 import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.AlertAssigningVO;
+import com.itgrids.partyanalyst.dto.GovtDepartmentVO;
 import com.itgrids.partyanalyst.dto.ResultStatus;
+import com.itgrids.partyanalyst.service.ICccDashboardService;
+import com.itgrids.partyanalyst.utils.IConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class CccDashboardAction extends ActionSupport implements ServletRequestAware {
-	   private final static Logger LOG = Logger.getLogger(CccDashboardAction.class);
-	   private HttpServletRequest request;
-	   private HttpSession session;
-	   private JSONObject jObj;
 	   
-	   private String task;
-	   private ResultStatus	resultStatus;
-	   private InputStream	inputStream;
-	   private String successMsg;
-	   public JSONObject getjObj() {
-		   return jObj;
-	   }
-	   public void setjObj(JSONObject jObj) {
-		   this.jObj = jObj;
-	   }
-	   public String getTask() {
-		   return task;
-	   }
-	   public void setTask(String task) {
-		   this.task = task;
-	   }
-	   public void setServletRequest(HttpServletRequest request) {
-		   this.request = request;    
+		private final static Logger LOG = Logger.getLogger(CccDashboardAction.class);
+		private HttpServletRequest request;
+		private HttpSession session;
+		private JSONObject jObj;
+		private String task;
+		private ResultStatus	resultStatus;
+		private InputStream	inputStream;
+		private String successMsg;
+		
+		private List<GovtDepartmentVO> govtDeptVoList = new ArrayList<GovtDepartmentVO>();
+		private ICccDashboardService cccDashboardService;
+		private AlertAssigningVO alertAssigningVO;
+		private List<File> imageForDisplay = new ArrayList<File>();
+		private List<String> imageForDisplayContentType = new ArrayList<String>();
+		private List<String> imageForDisplayFileName = new ArrayList<String>();
+		
+		
+		public List<File> getImageForDisplay() {
+			return imageForDisplay;
+		}
+		public void setImageForDisplay(List<File> imageForDisplay) {
+			this.imageForDisplay = imageForDisplay;
+		}
+		public List<String> getImageForDisplayContentType() {
+			return imageForDisplayContentType;
+		}
+		public void setImageForDisplayContentType(
+				List<String> imageForDisplayContentType) {
+			this.imageForDisplayContentType = imageForDisplayContentType;
+		}
+		public List<String> getImageForDisplayFileName() {
+			return imageForDisplayFileName;
+		}
+		public void setImageForDisplayFileName(List<String> imageForDisplayFileName) {
+			this.imageForDisplayFileName = imageForDisplayFileName;
+		}
+		public AlertAssigningVO getAlertAssigningVO() {
+			return alertAssigningVO;
+		}
+		public void setAlertAssigningVO(AlertAssigningVO alertAssigningVO) {
+			this.alertAssigningVO = alertAssigningVO;
+		}
+		public ICccDashboardService getCccDashboardService() {
+			return cccDashboardService;
+		}
+		public void setCccDashboardService(ICccDashboardService cccDashboardService) {
+			this.cccDashboardService = cccDashboardService;
+		}
+		public JSONObject getjObj() {
+			return jObj;
+		}
+		public void setjObj(JSONObject jObj) {
+			this.jObj = jObj;
+		}
+		public String getTask() {
+			return task;
+		}
+		public void setTask(String task) {
+			this.task = task;
+		}
+		public void setServletRequest(HttpServletRequest request) {
+			this.request = request;    
 	   }
 	   public HttpServletRequest getRequest() {
 		   return request;
@@ -59,7 +110,7 @@ public class CccDashboardAction extends ActionSupport implements ServletRequestA
 	   }
 	   
 	   public InputStream getInputStream() {
-		return inputStream;
+			return inputStream;
 	   }
 	   public void setInputStream(InputStream inputStream) {
 		   this.inputStream = inputStream;
@@ -75,12 +126,130 @@ public class CccDashboardAction extends ActionSupport implements ServletRequestA
 	   public static Logger getLog() {
 		   return LOG;
 	   }
-	   //Business method
-	   public String execute(){
-		    return Action.SUCCESS;
-	   }
-	   
-	   public String alertDepartmentLogin(){
-		    return Action.SUCCESS;
-	   }
+	   public List<GovtDepartmentVO> getGovtDeptVoList() {
+			return govtDeptVoList;
+		}
+		public void setGovtDeptVoList(List<GovtDepartmentVO> govtDeptVoList) {
+			this.govtDeptVoList = govtDeptVoList;
+		}
+		
+		
+		//Business method
+		public String execute(){
+			return Action.SUCCESS;
+		}
+   
+		public String alertDepartmentLogin(){
+			return Action.SUCCESS;
+		}
+   
+		public String getDepartmentLevels(){
+			try {
+				jObj = new JSONObject(getTask());
+		
+				govtDeptVoList = cccDashboardService.getDepartmentLevels();
+			} catch (Exception e) {
+				LOG.error("Exception Raised in getDepartmentLevels() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String getLocationsBasedOnLevel(){
+		   try {
+				jObj = new JSONObject(getTask());
+				Long levelId = jObj.getLong("levelId");
+			
+				govtDeptVoList = cccDashboardService.getLocationsBasedOnLevel(levelId);
+		   } catch (Exception e) {
+			   LOG.error("Exception Raised in getLocationsBasedOnLevel() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String getDepartmentsByAlert(){
+		   try {
+				jObj = new JSONObject(getTask());
+				Long alertId = jObj.getLong("alertId");
+			
+				govtDeptVoList = cccDashboardService.getDepartmentsByAlert(alertId);
+		   } catch (Exception e) {
+			   LOG.error("Exception Raised in getDepartmentsByAlert() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String getDesignationsByDepartment(){
+		   try {
+				jObj = new JSONObject(getTask());
+				Long departmentId = jObj.getLong("departmentId");
+				Long levelId = jObj.getLong("levelId");
+			
+				govtDeptVoList = cccDashboardService.getDesignationsByDepartment(departmentId,levelId);
+		   } catch (Exception e) {
+			   LOG.error("Exception Raised in getDesignationsByDepartment() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String getOfficersByDesignationAndLevel(){
+		   try {
+				jObj = new JSONObject(getTask());
+				Long levelId = jObj.getLong("levelId");
+				Long levelValue = jObj.getLong("levelValue");
+				Long designationId = jObj.getLong("designationId");
+			
+				govtDeptVoList = cccDashboardService.getOfficersByDesignationAndLevel(levelId,levelValue,designationId);
+		   } catch (Exception e) {
+			   LOG.error("Exception Raised in getOfficersByDesignationAndLevel() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String assigningAlertToOfficer(){
+		   try {
+			   String imageName=null;
+			   List<String> fileNamesList = new ArrayList<String>(0);
+			   
+			   for(int i=0;i<imageForDisplay.size();i++){
+				   
+				   		String fileType = imageForDisplayContentType.get(i).substring(imageForDisplayContentType.get(i).indexOf("/")+1, imageForDisplayContentType.get(i).length());
+				   		String fileName = imageForDisplayFileName.get(i);
+				   		String[] extName = imageForDisplayFileName.get(i).split("\\.");
+				   		if(Pattern.matches(".*[a-zA-Z]+.*", extName[0])){
+				   			imageName= UUID.randomUUID().toString()+"_"+imageForDisplayFileName.get(i);
+				   			fileNamesList.add(IConstants.ALERT_DEPT_DOCUMENTS+"/"+imageName);
+				   		}else{
+				   			imageName= UUID.randomUUID().toString()+"."+extName[1];
+				   			fileNamesList.add(IConstants.ALERT_DEPT_DOCUMENTS+"/"+imageName);
+				   		}
+				   		String filePath=IConstants.STATIC_CONTENT_FOLDER_PATH+"/"+IConstants.ALERT_DEPT_DOCUMENTS;
+			        	 
+				   		File fileToCreate = new File(filePath,imageName);
+				   		FileUtils.copyFile(imageForDisplay.get(i), fileToCreate);
+					  
+				   		inputStream = new StringBufferInputStream("success");
+		          	}
+			   if(fileNamesList != null && !fileNamesList.isEmpty()){
+				   alertAssigningVO.setDocument(fileNamesList.get(0));
+			   }
+			   
+			   successMsg = cccDashboardService.assigningAlertToOfficer(alertAssigningVO);
+			   
+			} catch (Exception e) {
+				LOG.error("Exception Raised in assigningAlertToOfficer() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
+		   
+		public String getAssignedOfficersDetailsForAlert(){
+		   try {
+				jObj = new JSONObject(getTask());
+				Long alertId = jObj.getLong("alertId");
+			
+				govtDeptVoList = cccDashboardService.getAssignedOfficersDetailsForAlert(alertId);
+		   } catch (Exception e) {
+			   LOG.error("Exception Raised in getAssignedOfficersDetailsForAlert() in CccDashboardAction",e);
+			}
+			   return Action.SUCCESS;
+		}
 }
