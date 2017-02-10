fee457105b5317c6a408a379886f8eb256327c2d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-10 05:51:29 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITvNewsChannelDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITvNewsChannelDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActivityDocument;
 import com.itgrids.partyanalyst.model.TvNewsChannel;
 
 public interface ITvNewsChannelDAO extends GenericDao<TvNewsChannel, Long>{
-
+	public List<Object[]> getChannelList();
 }
