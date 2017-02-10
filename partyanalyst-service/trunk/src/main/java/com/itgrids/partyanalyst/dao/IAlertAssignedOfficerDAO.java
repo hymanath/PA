4ad8fa9749ed7360a1cf9e4604748c7f381f546d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-10 15:53:43 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -9,4 +10,7 @@ import com.itgrids.partyanalyst.model.AlertAssignedOfficer;
 public interface IAlertAssignedOfficerDAO extends GenericDao<AlertAssignedOfficer, Long>{
 
 	public List<Object[]> getAssignedOfficersForAlert(Long alertId);
+	public List<Object[]> getTotalAlertGroupByStatusForGovtOneDept(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
+	public List<Object[]> getDepartmentWiseAlertCount(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
+	public List<Object[]> getTotalAlertGroupByDepartmentThenStatusForGovt(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtAlertDepartmentLocationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtAlertDepartmentLocationDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IGovtAlertDepartmentLocationDAO;
 import com.itgrids.partyanalyst.model.GovtAlertDepartmentLocation;
@@ -11,5 +14,10 @@ public class GovtAlertDepartmentLocationDAO extends GenericDaoHibernate<GovtAler
 		super(GovtAlertDepartmentLocation.class);
 		
 	}
+	public List<Long> getDeptListForUser(Long userId){
+		Query query = getSession().createQuery("select distinct model.govtDepartment.govtDepartmentId from GovtAlertDepartmentLocation model where model.userId = :userId");
+		query.setParameter("userId", userId);
+		return query.list();
+	}  
 
 }
