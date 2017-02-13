f7aacf8198d59a006e3442f135fa14a19987e9cd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-13 17:11:43 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerDAO.java
@@ -8,9 +8,10 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.AlertAssignedOfficer;
 
 public interface IAlertAssignedOfficerDAO extends GenericDao<AlertAssignedOfficer, Long>{
-
+	
 	public List<Object[]> getAssignedOfficersForAlert(Long alertId);
 	public List<Object[]> getTotalAlertGroupByStatusForGovtOneDept(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
 	public List<Object[]> getDepartmentWiseAlertCount(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
 	public List<Object[]> getTotalAlertGroupByDepartmentThenStatusForGovt(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,List<Long> deptIdList);
+	public List<Object[]> getLocationWiseThenStatusWiseAlertCount(Date fromDate,Date toDate,Long stateId,List<Long> printIdList,List<Long> electronicIdList,Long deptId,Long levelValue);
 }
