2247640c37f76d6b8a61d6fae86ef7e41ada7686
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-17 14:17:37 +0000

    updated by - SREEDHAR

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CommitteeResultVO.java
@@ -0,0 +1,93 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class CommitteeResultVO implements Serializable{
+	
+	private boolean errorStatus;
+	private Long errorCode;
+	private String message;
+    private Long tdpRolesId;
+    private String role;
+	private Long  minCount;
+    private Long  occupiedCount;
+    private Long tdpCommitteeId;
+    private List<CommitteeResultVO>  subList;
+	
+
+	public Long getMinCount() {
+		return minCount;
+	}
+
+	public void setMinCount(Long minCount) {
+		this.minCount = minCount;
+	}
+
+	public Long getOccupiedCount() {
+		return occupiedCount;
+	}
+
+	public void setOccupiedCount(Long occupiedCount) {
+		this.occupiedCount = occupiedCount;
+	}
+
+	public Long getTdpRolesId() {
+		return tdpRolesId;
+	}
+
+	public void setTdpRolesId(Long tdpRolesId) {
+		this.tdpRolesId = tdpRolesId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<CommitteeResultVO> getSubList() {
+		return subList;
+	}
+
+	public void setSubList(List<CommitteeResultVO> subList) {
+		this.subList = subList;
+	}
+
+	public boolean isErrorStatus() {
+		return errorStatus;
+	}
+
+	public void setErrorStatus(boolean errorStatus) {
+		this.errorStatus = errorStatus;
+	}
+
+	
+
+	public Long getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(Long errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public Long getTdpCommitteeId() {
+		return tdpCommitteeId;
+	}
+
+	public void setTdpCommitteeId(Long tdpCommitteeId) {
+		this.tdpCommitteeId = tdpCommitteeId;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    	
+}
