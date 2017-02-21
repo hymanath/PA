8048df5f1e2ec1b0598dd3280546aaa24f70cf3c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-21 10:54:28 +0000

    committed by swadhin

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CccDashboardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CccDashboardAction.java
@@ -1004,7 +1004,35 @@ public class CccDashboardAction extends ActionSupport implements ServletRequestA
 				   LOG.error("Exception Raised in getDesigAndStatusWiseAlertsCounts() in CccDashboardAction",e);
 				}
 				   return Action.SUCCESS;
+		}
+		public String getTotalAlertDetailsGroupByDeptThenStatus(){
+			try{
+				session = request.getSession();
+				RegistrationVO regVo = (RegistrationVO)session.getAttribute("USER");
+				Long userId = regVo.getRegistrationID();
+				jObj = new JSONObject(getTask());
+				String fromDate = jObj.getString("fromDate");
+				String toDate = jObj.getString("toDate");
+				Long stateId = jObj.getLong("stateId");
+				Long deptId = jObj.getLong("deptId");
+				Long statusId = jObj.getLong("statusId");
+				String type = jObj.getString("type");
+				JSONArray paperIdArr = jObj.getJSONArray("paperIdArr");  
+				List<Long> paperIdList = new ArrayList<Long>();
+				for (int i = 0; i < paperIdArr.length(); i++){
+					paperIdList.add(Long.parseLong(paperIdArr.getString(i)));        
+				} 
+				
+				JSONArray chanelIdArr = jObj.getJSONArray("chanelIdArr");  
+				List<Long> chanelIdList = new ArrayList<Long>();
+				for (int i = 0; i < chanelIdArr.length(); i++){
+					chanelIdList.add(Long.parseLong(chanelIdArr.getString(i)));        
+				}
+				alertCoreDashBoardVOList = cccDashboardService.getTotalAlertDetailsGroupByDeptThenStatus(fromDate,toDate,stateId,paperIdList,chanelIdList,userId,deptId,statusId);
+			}catch(Exception e){
+				LOG.error("Exception occured in getTotalAlertGroutByDeptThenStatus() of CccDashboardAction",e);
 			}
+			return Action.SUCCESS;
+		}
 		
 }
-//	public List<AlertCoreDashBoardVO> getTotalAlertByOtherStatus(String fromDateStr, String toDateStr, Long stateId, List<Long> printIdList, List<Long> electronicIdList, List<Long> deptIdList,Long statusId,Long userId){
