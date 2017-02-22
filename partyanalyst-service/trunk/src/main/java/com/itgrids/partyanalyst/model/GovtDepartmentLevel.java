a19b2b406e3a25b4e36739cb84b84ebe404eac6b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-02-22 09:40:07 +0000

    Alert Module -- SRAVANTH.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtDepartmentLevel.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtDepartmentLevel.java
@@ -2,15 +2,22 @@ package com.itgrids.partyanalyst.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "govt_department_level")
@@ -19,6 +26,8 @@ public class GovtDepartmentLevel extends BaseModel implements Serializable{
 
 	private Long govtDepartmentLevelId;
 	private String levelName;
+	private Long alertRegionScopesId;
+	private AlertRegionScopes alertRegionScopes;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -36,5 +45,24 @@ public class GovtDepartmentLevel extends BaseModel implements Serializable{
 	}
 	public void setLevelName(String levelName) {
 		this.levelName = levelName;
+	}
+	
+	@Column(name = "alert_region_scopes_id")
+	public Long getAlertRegionScopesId() {
+		return alertRegionScopesId;
+	}
+	public void setAlertRegionScopesId(Long alertRegionScopesId) {
+		this.alertRegionScopesId = alertRegionScopesId;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "alert_region_scopes_id", insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public AlertRegionScopes getAlertRegionScopes() {
+		return alertRegionScopes;
+	}
+	public void setAlertRegionScopes(AlertRegionScopes alertRegionScopes) {
+		this.alertRegionScopes = alertRegionScopes;
 	}
 }
