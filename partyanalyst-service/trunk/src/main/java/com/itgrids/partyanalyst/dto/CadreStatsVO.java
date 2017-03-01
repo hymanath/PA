cb200e983601254435f4837567fb82457cb0bfc9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-01 06:35:25 +0000

    Updated by Srishailam

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreStatsVO.java
@@ -0,0 +1,87 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadreStatsVO implements java.io.Serializable{
+
+	private Long id;
+	private String name;
+	private String locationType;//own or participated
+	private String districtPerc;
+	private String parliamenPerc;
+	private String assemblyPerc;
+	private String mandalORMuncORUrbanPerc;
+	private String panchayatORWardPerc;
+	private String boothPerc;
+	private String boothInfuPerc;
+	private List<CadreStatsVO> subList = new ArrayList<CadreStatsVO>(0);
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocationType() {
+		return locationType;
+	}
+	public void setLocationType(String locationType) {
+		this.locationType = locationType;
+	}
+	public String getDistrictPerc() {
+		return districtPerc;
+	}
+	public void setDistrictPerc(String districtPerc) {
+		this.districtPerc = districtPerc;
+	}
+	public String getParliamenPerc() {
+		return parliamenPerc;
+	}
+	public void setParliamenPerc(String parliamenPerc) {
+		this.parliamenPerc = parliamenPerc;
+	}
+	public String getAssemblyPerc() {
+		return assemblyPerc;
+	}
+	public void setAssemblyPerc(String assemblyPerc) {
+		this.assemblyPerc = assemblyPerc;
+	}
+	public String getMandalORMuncORUrbanPerc() {
+		return mandalORMuncORUrbanPerc;
+	}
+	public void setMandalORMuncORUrbanPerc(String mandalORMuncORUrbanPerc) {
+		this.mandalORMuncORUrbanPerc = mandalORMuncORUrbanPerc;
+	}
+	public String getPanchayatORWardPerc() {
+		return panchayatORWardPerc;
+	}
+	public void setPanchayatORWardPerc(String panchayatORWardPerc) {
+		this.panchayatORWardPerc = panchayatORWardPerc;
+	}
+	public String getBoothPerc() {
+		return boothPerc;
+	}
+	public void setBoothPerc(String boothPerc) {
+		this.boothPerc = boothPerc;
+	}
+	public List<CadreStatsVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<CadreStatsVO> subList) {
+		this.subList = subList;
+	}
+	public String getBoothInfuPerc() {
+		return boothInfuPerc;
+	}
+	public void setBoothInfuPerc(String boothInfuPerc) {
+		this.boothInfuPerc = boothInfuPerc;
+	}
+	
+}
