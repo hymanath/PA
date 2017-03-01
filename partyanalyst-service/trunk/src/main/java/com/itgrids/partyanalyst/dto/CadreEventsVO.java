076344d7eed35be1eb4c70e181cab0200e6dd100
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-01 17:36:58 +0000

    Updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreEventsVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CadreEventsVO.java
@@ -1,14 +1,24 @@
 package com.itgrids.partyanalyst.dto;
 
-public class CadreEventsVO {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadreEventsVO implements java.io.Serializable{
 	public Long id;
 	public String name="";
 	public Long invitedCount;
 	public Long attendedCount;
 	public Long lateCount;
 	public String attendedTime;
+	private List<CadreEventsVO> subList = new ArrayList<CadreEventsVO>(0);
 	
 	
+	public List<CadreEventsVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<CadreEventsVO> subList) {
+		this.subList = subList;
+	}
 	public Long getId() {
 		return id;
 	}
