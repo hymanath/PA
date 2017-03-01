222a6b195e18e3ea942d1cb6fa8567dd9d07a0d9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-01 11:14:04 +0000

    Alert Module -- SRAVANTH.

--- partyanalyst/src/main/webapp/alertDepartment/js/alertUserManagement.js
+++ partyanalyst/src/main/webapp/alertDepartment/js/alertUserManagement.js
@@ -1280,13 +1280,128 @@ function getAlertStatusCommentsTrackingDetails(alertId)
 	  data : {task:JSON.stringify(jsObj)}
 	}).done(function(result){ 
 		if(result != null && result.length > 0)
-			alertComments(result);	
+			buildAlertCommentsNew(result);
+		//alertComments(result);	
 		else
 			$("#alertCommentsDivIdNew").html("");
 	});
 	
 }
-function alertComments(result)
+
+function buildAlertCommentsNew(result){
+	var statusId = 0;
+	var length = result.length;
+	length = length - 1;
+	var str = '';
+	/*if(status == 'false'){  
+		$(".cadreAlertCommentsDivId").show();
+		str+='<div class="panel-group alertCommentsCollapse m_top10" id="accordion" role="tablist" aria-multiselectable="true" style="margin-bottom:0px;">';
+	}*/
+	str+='<div class="panel-group alertCommentsCollapse m_top10" id="accordion" role="tablist" aria-multiselectable="true">';
+	for(var i in result)
+	{
+		statusId = result[i].statusId;
+		str+='<div class="panel panel-default">';
+			str+='<div class="panel-heading" role="tab" id="heading'+i+'">';
+			if(length == i)    
+			{
+				str+='<a role="button" class="alertCommentColapse" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'">';
+			}else{
+				str+='<a class="collapsed alertCommentColapse" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse'+i+'" aria-expanded="false" aria-controls="collapse'+i+'">';
+			}
+			//if(result[i].govtDeptList != null && result[i].govtDeptList.length > 0){
+				if(length != i){
+				str+='<h4 class="panel-title">'+result[i].status+'';
+						str+='<i class="glyphicon glyphicon-ok"></i>';
+						if(result[i].dateStr != null)
+							str+='<span class="pull-right" style="padding-right:20px;">'+result[i].dateStr+'</span>';    
+				str+='</h4>';
+				}else{
+					str+='<h4 class="panel-title">'+result[i].status+'';
+						str+='<i class="glyphicon glyphicon-hourglass"></i>';
+						if(result[i].dateStr != null)
+							str+='<span class="pull-right" style="padding-right:20px;">'+result[i].dateStr+'</span>';
+					str+='</h4>';
+				} 
+			//}
+			/*else{
+				if(length != i){
+				str+='<h4 class="panel-title">'+result[i].status+'';
+						str+='<i class="glyphicon glyphicon-ok"></i>';    
+				str+='</h4>';
+				}else{
+					str+='<h4 class="panel-title">'+result[i].status+'';
+						str+='<i class="glyphicon glyphicon-hourglass"></i>';
+					str+='</h4>';
+				} 
+			}*/
+			
+				str+='</a>';  
+			str+='</div>';
+			if(length == i)  
+			{
+				str+='<div id="collapse'+i+'" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading'+i+'">';
+			}else{
+				str+='<div id="collapse'+i+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading'+i+'">';
+			}
+				str+='<div class="panel-body" style="padding:5px;">';
+					str+='<div class="row">';
+						
+					if(result[i].statusId != null && result[i].statusId > 1){
+						//for(var j in result[i].govtDeptList){
+							str+='<div class="col-md-2 col-xs-12 col-sm-2">';
+								var date = result[i].dateStr
+								var dateArr = date.split("-");
+								var year = dateArr[0];
+								var month = dateArr[1];
+								var day = dateArr[2];
+								str+='<table class="table tableCalendar">';
+									str+='<tr>';
+										str+='<td colspan="2">';
+											str+='<h3>'+day+'</h3>';
+										str+='</td>';
+									str+='</tr>';
+									str+='<tr>';
+										str+='<td>'+getMonth(month)+'</td>';        
+										str+='<td>'+year+'</td>';
+									str+='</tr>';
+								str+='</table>';
+							str+='</div>';
+							str+='<div class="col-md-10 col-xs-12 col-sm-10" style="padding-left:0px;">';
+								str+='<ul class="alertStatusTracking">';
+									str+='<li>';
+										str+='<div class="arrow_box_left">';
+											str+='<div>';
+												str+='<p><span style="color:#A286C0;font-size:13px;">COMMENT SOURCE: '+result[i].timeStr+'</span><br>';
+												//str+='<p><span style="color:#A286C0;font-size:13px;">COMMENT SOURCE:'+result[i].sublist2[j].sublist[k][0].timeString+'</span><br>';
+												//for(var l in result[i].govtDeptList[j].sublist[k])
+												//{
+													str+='<img src="dist/Appointment/img/thumb.jpg" style="width:10px;display:inline-block"/> '+result[i].source+'<br>';
+												//}
+												str+='</p>';
+												str+='<p><span style="color:#A286C0;font-size:13px;">COMMENT:</span><br>';
+												str+='<p>'+result[i].comment+'</p>';
+												str+='<p><span class="pull-right" style="color:#A286C0;font-size:13px;">UPDATED BY: '+result[i].name+'</span></p>';
+												str+='<hr style="margin-top:20px;"/>';
+											str+='</div>';   
+										str+='</div>';    
+									str+='</li>';
+								str+='</ul>';
+							str+='</div>';
+						//}
+					}else{
+						str+='<div class="col-md-12 col-xs-12 col-sm-12"><div  style="background-color:#EEE;padding:10px"><h4 class="panel-title text-capital text-center">please assign alert to officer</h4></div></div>';
+					}
+					str+='</div>';
+				str+='</div>';
+			str+='</div>';
+		str+='</div>';
+	}
+	str+='</div>';
+	$("#alertCommentsDivIdNew").html(str);
+}
+
+/*function alertComments(result)
 {
 	var docName = '';
 	var extName = [];
@@ -1379,16 +1494,6 @@ function alertComments(result)
 												str+='</p>';
 												str+='<p><span style="color:#A286C0;font-size:13px;">COMMENT:</span><br>';
 												str+='<p>'+result[i].govtDeptList[j].comment+'</p>';
-												/*if(result[i].sublist2[j].sublist[k][0].docList != null && result[i].sublist2[j].sublist[k][0].docList.length > 0){
-													str+='<p><span style="color:#A286C0;font-size:13px;">DOCUMENTS:</span><br>';
-													str+='<ul>';
-													for(var t in result[i].sublist2[j].sublist[k][0].docList){
-														docName = result[i].sublist2[j].sublist[k][0].docList[t].name;
-														extName = docName.split("/");
-														str+='<li id="document'+result[i].id+'"><a href="/Reports/'+result[i].sublist2[j].sublist[k][0].docList[t].name+'" target="_blank">'+extName[1]+'</a></li>';          
-													}
-													str+='</ul>';              
-												}*/
 												str+='<p><span class="pull-right" style="color:#A286C0;font-size:13px;">UPDATED BY: '+result[i].govtDeptList[j].name+'</span></p>';
 												str+='<hr style="margin-top:20px;"/>';
 											str+='</div>';   
@@ -1408,7 +1513,7 @@ function alertComments(result)
 	}
 	str+='</div>';
 	$("#alertCommentsDivIdNew").html(str)
-}
+}*/
 function getMonth(month){
 	if(month=="01"){
 		return "Jan"
