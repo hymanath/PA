1d13746afa59a117858f6aec97708c18b8eabfc7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-03 14:17:54 +0000

    Custom meeting released by - Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardPartyMeetingService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardPartyMeetingService.java
@@ -40,6 +40,7 @@ public interface ICoreDashboardPartyMeetingService {
 	public List<IdNameVO> getMeetingMemberDtls(Long partyMeetingMainTypeId,List<Long> partyMeetingTypeIds,String state,String startDateString, String endDateString, Long partyMeetingId, Long sessionId, String status,Long distId,boolean isNonInvitee);
 	public List<IdNameVO> getPartyMeetingSession(Long partyMeetingId);
 	public List<IdNameVO> getPublicRepAndcommitteeInviteeDtls(Long partyMeetingMainTypeId,List<Long> partyMeetingTypeIds,String state,String startDateString, String endDateString,List<Long> partyMeetingIds,String category, List<Long> categoryIds,String location, Long sessionId);
-	public MeetingDetailsInfoVO getMeetingDtls(Long activityMemberId, Long partyMeetingMainTypeId,List<Long> partyMeetingTypeIds,String state,String startDateString,String endDateString);
+	public MeetingDetailsInfoVO getMeetingDtls(Long activityMemberId, Long partyMeetingMainTypeId,List<Long> partyMeetingTypeIds,Long state,String startDateString,String endDateString);
 	public Map<Long,MeetingDetailsInfoVO> getMeetingListDtls(Long activityMemberId,String state,String startDateString,String endDateString);
+	public List<PartyMeetingsDataVO> getCustomPartyMeetingsMainTypeOverViewData(Long activitymemberId,Long partyMeetingMainTypeId,Long state,String startDateString, String endDateString);
 }
