0358d97700de8ebd7bd57495defe33185969c866
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-03 13:36:13 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDocumentDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDocumentDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
 import java.util.List;
+import java.util.Set;
 
 import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.dto.PartyMeetingsInputVO;
 import com.itgrids.partyanalyst.model.PartyMeetingDocument;
 
 public interface IPartyMeetingDocumentDAO extends GenericDao<PartyMeetingDocument,Long>{
@@ -14,4 +17,5 @@ public interface IPartyMeetingDocumentDAO extends GenericDao<PartyMeetingDocumen
 	public List<Object[]> getPartyMeetingDocsOfMeetingIds(List<Long> partyMeetingIds);
 	public List<Object[]> getMinuteAtrDocumentSummaryForAMeeting(Long meetingId,String type,String accessType,List<Long> accessValue);
 	public List<Long> getDocDetails(List<Long> meetingIds,String type);
+	public List<Object[]> getPartyMeetingdocList(PartyMeetingsInputVO inputVO,Long locationId,Set<Long> locationValuesSet);  
 }
