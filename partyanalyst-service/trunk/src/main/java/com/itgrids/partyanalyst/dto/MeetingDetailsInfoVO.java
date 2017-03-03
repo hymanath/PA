67dc2bae4a9459789dacfbe47eac5b034940098c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-03 04:41:25 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MeetingDetailsInfoVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MeetingDetailsInfoVO.java
@@ -16,12 +16,12 @@ public class MeetingDetailsInfoVO implements Serializable {
 	Map<Long, Set<Long>> totalInviteeAttendenceMap = new HashMap<Long, Set<Long>>();
 	Map<Long, Set<Long>> totalNonInviteesMap = new HashMap<Long, Set<Long>>();
 	Map<Long, Set<Long>> totalLateAttendedMap = new HashMap<Long, Set<Long>>();
-	Map<Long,Map<Long,Set<Long>>> totalSessionWiseAttendenceMap = new HashMap<Long,Map<Long,Set<Long>>>();
-	Map<Long, Set<Long>> totalSessionWiseInviteeAttendenceMap = new HashMap<Long, Set<Long>>();
-	Map<Long, Set<Long>> totalSessionWiseNonInviteeAttendenceMap = new HashMap<Long, Set<Long>>();
-	Map<Long, Set<Long>> totalSessionWiseLateInviteeAttendenceMap = new HashMap<Long, Set<Long>>();
+	Map<Long,Map<Long,Set<Long>>> totalMeetingWiseThenSessionWiseAttendenceMap = new HashMap<Long,Map<Long,Set<Long>>>();
+	Map<Long,Map<Long,Set<Long>>> totalMeetingWiseThenSessionWiseInviteeAttendenceMap = new HashMap<Long,Map<Long,Set<Long>>>();
+	Map<Long,Map<Long,Set<Long>>> totalMeetingWiseThenSessionWiseNonInviteeAttendenceMap = new HashMap<Long,Map<Long,Set<Long>>>();
+	Map<Long,Map<Long,Set<Long>>> totalSessionWiseLateInviteeAttendenceMap = new HashMap<Long,Map<Long,Set<Long>>>();
 	
-	public Map<Long, Set<Long>> getTotalInviteesMap() {
+	public Map<Long, Set<Long>> getTotalInviteesMap() { 
 		return totalInviteesMap;    
 	}
 	public void setTotalInviteesMap(Map<Long, Set<Long>> totalInviteesMap) {
@@ -52,34 +52,35 @@ public class MeetingDetailsInfoVO implements Serializable {
 	public void setTotalLateAttendedMap(Map<Long, Set<Long>> totalLateAttendedMap) {
 		this.totalLateAttendedMap = totalLateAttendedMap;
 	}
-	public Map<Long, Map<Long, Set<Long>>> getTotalSessionWiseAttendenceMap() {
-		return totalSessionWiseAttendenceMap;
+	
+	public Map<Long, Map<Long, Set<Long>>> getTotalMeetingWiseThenSessionWiseAttendenceMap() {
+		return totalMeetingWiseThenSessionWiseAttendenceMap;
 	}
-	public void setTotalSessionWiseAttendenceMap(
-			Map<Long, Map<Long, Set<Long>>> totalSessionWiseAttendenceMap) {
-		this.totalSessionWiseAttendenceMap = totalSessionWiseAttendenceMap;
+	public void setTotalMeetingWiseThenSessionWiseAttendenceMap(
+			Map<Long, Map<Long, Set<Long>>> totalMeetingWiseThenSessionWiseAttendenceMap) {
+		this.totalMeetingWiseThenSessionWiseAttendenceMap = totalMeetingWiseThenSessionWiseAttendenceMap;
 	}
-	public Map<Long, Set<Long>> getTotalSessionWiseInviteeAttendenceMap() {
-		return totalSessionWiseInviteeAttendenceMap;
+	
+	public Map<Long, Map<Long, Set<Long>>> getTotalMeetingWiseThenSessionWiseInviteeAttendenceMap() {
+		return totalMeetingWiseThenSessionWiseInviteeAttendenceMap;
 	}
-	public void setTotalSessionWiseInviteeAttendenceMap(
-			Map<Long, Set<Long>> totalSessionWiseInviteeAttendenceMap) {
-		this.totalSessionWiseInviteeAttendenceMap = totalSessionWiseInviteeAttendenceMap;
+	public void setTotalMeetingWiseThenSessionWiseInviteeAttendenceMap(
+			Map<Long, Map<Long, Set<Long>>> totalMeetingWiseThenSessionWiseInviteeAttendenceMap) {
+		this.totalMeetingWiseThenSessionWiseInviteeAttendenceMap = totalMeetingWiseThenSessionWiseInviteeAttendenceMap;
 	}
-	public Map<Long, Set<Long>> getTotalSessionWiseNonInviteeAttendenceMap() {
-		return totalSessionWiseNonInviteeAttendenceMap;
+	public Map<Long, Map<Long, Set<Long>>> getTotalMeetingWiseThenSessionWiseNonInviteeAttendenceMap() {
+		return totalMeetingWiseThenSessionWiseNonInviteeAttendenceMap;
 	}
-	public void setTotalSessionWiseNonInviteeAttendenceMap(
-			Map<Long, Set<Long>> totalSessionWiseNonInviteeAttendenceMap) {
-		this.totalSessionWiseNonInviteeAttendenceMap = totalSessionWiseNonInviteeAttendenceMap;
+	public void setTotalMeetingWiseThenSessionWiseNonInviteeAttendenceMap(
+			Map<Long, Map<Long, Set<Long>>> totalMeetingWiseThenSessionWiseNonInviteeAttendenceMap) {
+		this.totalMeetingWiseThenSessionWiseNonInviteeAttendenceMap = totalMeetingWiseThenSessionWiseNonInviteeAttendenceMap;
 	}
-	public Map<Long, Set<Long>> getTotalSessionWiseLateInviteeAttendenceMap() {
+	public Map<Long, Map<Long, Set<Long>>> getTotalSessionWiseLateInviteeAttendenceMap() {
 		return totalSessionWiseLateInviteeAttendenceMap;
 	}
 	public void setTotalSessionWiseLateInviteeAttendenceMap(
-			Map<Long, Set<Long>> totalSessionWiseLateInviteeAttendenceMap) {
+			Map<Long, Map<Long, Set<Long>>> totalSessionWiseLateInviteeAttendenceMap) {
 		this.totalSessionWiseLateInviteeAttendenceMap = totalSessionWiseLateInviteeAttendenceMap;
 	}
-	
 	
 }
