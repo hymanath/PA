f5489cc2d98e35ba261ebc5a9144a3ddd50e3ccf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-06 09:16:42 +0000

    Custom meeting released by - Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PartyMeetingGroupsMappingInfo.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/PartyMeetingGroupsMappingInfo.java
@@ -2,15 +2,22 @@ package com.itgrids.partyanalyst.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "party_meeting_groups_mapping_info")
@@ -18,12 +25,15 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 public class PartyMeetingGroupsMappingInfo extends BaseModel implements Serializable{
 	
 	private Long partyMeetingGroupsMappingInfoId;
-	private String partyMeetingGroupId;
-	private String partyMeetingId;
+	private Long partyMeetingGroupId;
+	private Long partyMeetingId;
 	private String isDeleted;
+	
+	private PartyMeetingGroup partyMeetingGroup;
+	private PartyMeeting partyMeeting;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "activity_attribute_id", unique = true, nullable = false)
+	@Column(name = "party_meeting_groups_mapping_info_id", unique = true, nullable = false)
 	public Long getPartyMeetingGroupsMappingInfoId() {
 		return partyMeetingGroupsMappingInfoId;
 	}
@@ -31,22 +41,46 @@ public class PartyMeetingGroupsMappingInfo extends BaseModel implements Serializ
 			Long partyMeetingGroupsMappingInfoId) {
 		this.partyMeetingGroupsMappingInfoId = partyMeetingGroupsMappingInfoId;
 	}
-	public String getPartyMeetingGroupId() {
+	@Column(name = "party_meeting_group_id")
+	public Long getPartyMeetingGroupId() {
 		return partyMeetingGroupId;
 	}
-	public void setPartyMeetingGroupId(String partyMeetingGroupId) {
+	public void setPartyMeetingGroupId(Long partyMeetingGroupId) {
 		this.partyMeetingGroupId = partyMeetingGroupId;
 	}
-	public String getPartyMeetingId() {
+	@Column(name = "party_meeting_id")
+	public Long getPartyMeetingId() {
 		return partyMeetingId;
 	}
-	public void setPartyMeetingId(String partyMeetingId) {
+	public void setPartyMeetingId(Long partyMeetingId) {
 		this.partyMeetingId = partyMeetingId;
 	}
+	@Column(name = "is_deleted")
 	public String getIsDeleted() {
 		return isDeleted;
 	}
 	public void setIsDeleted(String isDeleted) {
 		this.isDeleted = isDeleted;
 	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="party_meeting_group_id",updatable = false, insertable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PartyMeetingGroup getPartyMeetingGroup() {
+		return partyMeetingGroup;
+	}
+	public void setPartyMeetingGroup(PartyMeetingGroup partyMeetingGroup) {
+		this.partyMeetingGroup = partyMeetingGroup;
+	}
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="party_meeting_id",updatable = false, insertable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public PartyMeeting getPartyMeeting() {
+		return partyMeeting;
+	}
+	public void setPartyMeeting(PartyMeeting partyMeeting) {
+		this.partyMeeting = partyMeeting;
+	}
+	
 }
