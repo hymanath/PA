230bbe11bf75d8bbddee135e8fceba973a36314f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-11 08:52:52 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MeetingVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/MeetingVO.java
@@ -1,19 +1,31 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MeetingVO implements java.io.Serializable{
+	private Long id;
+	private String meetingName;
 	private String name;
 	private Long levelId;
-	private List<MeetingVO> userAccessLevelList;
-	private List<MeetingVO> userAccessLevelValuesList;
+	private List<MeetingVO> userAccessLevelList = new ArrayList<MeetingVO>();
+	private List<MeetingVO> userAccessLevelValuesList = new ArrayList<MeetingVO>() ;
 	private List<Long> levelValues;
 	private Long count;
 	
-	
-	
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getMeetingName() {
+		return meetingName;
+	}
+	public void setMeetingName(String meetingName) {
+		this.meetingName = meetingName;
+	}
 	public Long getCount() {
 		return count;
 	}
