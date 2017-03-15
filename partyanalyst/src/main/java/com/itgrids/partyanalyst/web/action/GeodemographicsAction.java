f7a6502b340720504457eaaab89524ce9bba095a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-15 14:02:41 +0000

    constituency page functionality released by - Teja

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/GeodemographicsAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/GeodemographicsAction.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.web.action;
 
+import java.text.ParseException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,6 +11,7 @@ import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
 import com.itgrids.api.service.IGeodemographicService;
+import com.itgrids.partyanalyst.dto.SelectOptionVO;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -19,8 +23,16 @@ public class GeodemographicsAction extends ActionSupport implements ServletReque
 	private JSONObject jObj;
 	private String task;
 	private IGeodemographicService geodemographicService;
+	private List<SelectOptionVO> result;
+	
 	
 
+	public List<SelectOptionVO> getResult() {
+		return result;
+	}
+	public void setResult(List<SelectOptionVO> result) {
+		this.result = result;
+	}
 	public void setGeodemographicService(
 			IGeodemographicService geodemographicService) {
 		this.geodemographicService = geodemographicService;
@@ -58,5 +70,65 @@ public class GeodemographicsAction extends ActionSupport implements ServletReque
 	public String execute(){
 		return Action.SUCCESS;
 	}
-
+	public String getDistrictsDetails(){
+			try{
+				jObj = new JSONObject(getTask());
+		
+				result = geodemographicService.getDistricts(Long.valueOf(jObj.getString("locationId")));
+				result.add(0,new SelectOptionVO(0l,"Select District"));
+		}catch(Exception e){
+			LOG.error("Exception Occured in getDistrictsDetails() in GeodemographicsAction ",e);
+		}
+		return SUCCESS;
+	}
+	public String getConstituenciesForADistrict()
+	{
+		try {
+			jObj = new JSONObject(getTask());
+			result = geodemographicService.getConstituenciesByDistrictID(Long.valueOf(jObj.getString("districtId")));
+			 
+			if(result == null || result.size() == 0)
+				result.add(0, new SelectOptionVO(0L,"Select Constituency"));
+			
+			if(result != null && result.size() > 1)
+				result.add(0, new SelectOptionVO(0L,"Select Constituency"));
+		} catch (ParseException e) {
+			LOG.error("Exception Occured in getConstituenciesForADistrict() in GeodemographicsAction ",e);
+		}
+		return Action.SUCCESS;
+	}
+	public String getMandalsForAConstituency(){
+		try {
+			jObj = new JSONObject(getTask());
+			Long constituencyId = jObj.getLong("constituencyId");
+			result = geodemographicService.getMandalsByConstituencyIDFromBooth(constituencyId);
+		} catch (ParseException e) {
+			LOG.error("Exception Occured in getMandalsForAConstituency() in GeodemographicsAction ",e);
+		}
+		return Action.SUCCESS;
+	}
+	public String getVillagesForMandalId()
+	{
+		try
+		{
+		jObj = new JSONObject(getTask());
+		Long mandalId = jObj.getLong("mandalId");
+		result = geodemographicService.getVillagesForMandalId(mandalId);
+		}catch(Exception e)
+		{
+			LOG.error("Exception Occured in getVillagesForMandalId() in GeodemographicsAction ",e);
+		}
+	 return Action.SUCCESS;
+	}
+	  public String getAllBoothsForPanchayat(){
+		  try{
+			  jObj=new JSONObject(getTask());
+			  Long panchayatId = jObj.getLong("panchayatId");
+			  result = geodemographicService.getBoothsList(panchayatId);
+			  
+		  }catch(Exception e){
+			  LOG.error("Entered into getAllBoothsForPanchayat method in GeodemographicsAction....");
+		  }
+		  return Action.SUCCESS;
+	  }
 }
