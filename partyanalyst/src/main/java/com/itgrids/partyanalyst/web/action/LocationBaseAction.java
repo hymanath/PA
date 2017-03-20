47eb65e59ecd2f7e9ccd1838bf7e3b3f6fc6db00
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-20 13:49:17 +0000

    constituency page functionality released by - Teja

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LocationBaseAction.java
@@ -0,0 +1,57 @@
+/* 
+ * Copyright (c) 2009 IT Grids.
+
+ * All Rights Reserved.
+ *
+ * IT Grids Confidential Information.
+ * Created on September 20, 2017
+ * Name:Teja
+ */
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.util.ServletContextAware;
+import org.json.JSONObject;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LocationBaseAction extends ActionSupport implements ServletContextAware {
+	private final static Logger LOG = Logger.getLogger(GeodemographicsAction.class);
+	private HttpServletRequest request;
+	private HttpSession session;
+	private JSONObject jObj;
+	private String task;
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	public HttpSession getSession() {
+		return session;
+	}
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	public JSONObject getjObj() {
+		return jObj;
+	}
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public void setServletContext(ServletContext arg0) {
+		
+	}
+	
+	
+}
---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/NewParliamentPageAction.java
@@ -0,0 +1,62 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.json.JSONObject;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.opensymphony.xwork2.Preparable;
+
+
+public class NewParliamentPageAction extends LocationBaseAction{
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOG = Logger.getLogger(NewParliamentPageAction.class);
+	private HttpServletRequest request;
+	private HttpSession session;
+	private JSONObject jObj;
+	private String task;
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public HttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+
+	public JSONObject getjObj() {
+		return jObj;
+	}
+
+	public void setjObj(JSONObject jObj) {
+		this.jObj = jObj;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public String newParliamentPage(){
+		return Action.SUCCESS;
+	}
+
+	public void setServletRequest(HttpServletRequest arg0) {
+	}
+}
