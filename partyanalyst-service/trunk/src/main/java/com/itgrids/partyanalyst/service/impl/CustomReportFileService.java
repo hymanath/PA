7f21218de86654949e3d31c7bc4fb6c1dfc9ece2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-21 09:44:11 +0000

    new custom report actions and services updated by Swathi & Mounika

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CustomReportFileService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ICustomReportFileService;
+
+public class CustomReportFileService extends AlertService implements ICustomReportFileService{
+
+}
