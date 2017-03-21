346df1bd0ebbb7920c52462f91c04fafb66cd1c6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-21 09:44:11 +0000

    new custom report actions and services updated by Swathi & Mounika

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CustomReportImageService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ICustomReportImageService;
+
+public class CustomReportImageService extends AlertService implements ICustomReportImageService {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CustomReportLocationService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.ICustomReportLocationService;
+
+public class CustomReportLocationService  extends AlertService implements ICustomReportLocationService{
+
+}
