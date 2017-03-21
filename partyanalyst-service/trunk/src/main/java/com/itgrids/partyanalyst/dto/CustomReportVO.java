63e3eabf0c4f4e47b508c4c054c7dfbef2260fe6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-21 10:27:09 +0000

    new custom report actions and services updated by Swathi & Mounika

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CustomReportVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CustomReportVO.java
@@ -6,4 +6,24 @@ public class CustomReportVO implements Serializable {
 	private Long id;
 	private String name;
 	private Long count = 0l;
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Long getCount() {
+		return count;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCount(Long count) {
+		this.count = count;
+	}
+	
+	
 }
