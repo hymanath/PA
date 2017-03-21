c802fb70055d0ffd38079114fbf549f5b94aa8c8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-21 10:23:16 +0000

    new custom report actions and services updated by Swathi & Mounika

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CustomReportVO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class CustomReportVO implements Serializable {
+	private Long id;
+	private String name;
+	private Long count = 0l;
+}
