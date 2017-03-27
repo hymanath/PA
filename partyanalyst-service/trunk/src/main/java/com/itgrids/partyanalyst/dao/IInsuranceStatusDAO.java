900c5ba793ba99f9ede75ac1b7880c1f5bfc38eb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-27 13:11:21 +0000

    Insurance Module in Core Dashboard -- SRAVANTH.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInsuranceStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInsuranceStatusDAO.java
@@ -12,4 +12,7 @@ public interface IInsuranceStatusDAO extends GenericDao<InsuranceStatus, Long>{
 
 	public List<Object[]> getStatusAndInsuranceCompanyWiseComplaints(Long locationId,Set<Long> locationValuesSet,Long stateId,Long cadreYearId,Date fromDate,Date toDate);
 	public List<Object[]> getAllInsuranceCompanies();
+	public List<Long> getStatusAndInsuranceCompanyWiseComplaintIds(Long locationId,Set<Long> locationValuesSet,Long stateId,Long cadreYearId,Date fromDate,Date toDate,
+			List<Long> statusIds,Long companyId,String issueType);
+	public List<Object[]> getStatusAndInsuranceCompanyWiseComplaintDetails(List<Long> complaintIds);
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardInsuranceService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardInsuranceService.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 
 import com.itgrids.partyanalyst.dao.IInsuranceStatusDAO;
 import com.itgrids.partyanalyst.dao.hibernate.ActivityMemberAccessLevelDAO;
+import com.itgrids.partyanalyst.dto.ComplaintMasterVO;
 import com.itgrids.partyanalyst.dto.CoreDashboardInsuranceVO;
 import com.itgrids.partyanalyst.service.ICoreDashboardInsuranceService;
 import com.itgrids.partyanalyst.utils.IConstants;
@@ -154,8 +155,92 @@ public class CoreDashboardInsuranceService implements ICoreDashboardInsuranceSer
 		return null;
 	}
 	
-	/*public List<CoreDashboardInsuranceVO> getInsuraceCompanyAndTypeOfIssueWiseComplaintsDetails(Long activityMemberId,Long cadreYearId,Long stateId,
-			String fromDateStr,String toDateStr){
-		
-	}*/
+	public List<ComplaintMasterVO> getInsuraceStatusWiseComplaintsDetails(Long activityMemberId,Long cadreYearId,Long stateId,
+			String fromDateStr,String toDateStr,String status,Long companyId,String issueType){
+		List<ComplaintMasterVO> returnList = new ArrayList<ComplaintMasterVO>();
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+			Date fromDate = null;
+			Date toDate = null;
+			if(fromDateStr != null && toDateStr != null){
+				fromDate = sdf.parse(fromDateStr);
+				toDate = sdf.parse(toDateStr);
+			}
+			
+			Long locationId = 0L;
+			Set<Long> locationValuesSet = new java.util.HashSet<Long>();  
+			
+			if(stateId != null && (stateId == 0l || stateId == 2l)){
+				locationId = 2l;
+				locationValuesSet.add(2l);
+				if(stateId == 0l)
+					locationValuesSet.add(1l);
+			}
+			else if(stateId != null && stateId.longValue() == 1l){
+				List<Object[]> rtrnUsrAccssLvlIdAndVlusObjLst=activityMemberAccessLevelDAO.getLocationLevelAndValuesByActivityMembersId(activityMemberId);
+			    if(rtrnUsrAccssLvlIdAndVlusObjLst != null && !rtrnUsrAccssLvlIdAndVlusObjLst.isEmpty()){
+				   for (Object[] obj : rtrnUsrAccssLvlIdAndVlusObjLst) {
+					   locationId = Long.valueOf(obj[0] != null ? obj[0].toString():"0");
+					   locationValuesSet.add(Long.valueOf(obj[1] != null ? obj[1].toString():"0"));
+				   }
+			    }
+			}
+			
+			List<Long> statusIds = new ArrayList<Long>();
+			if(status != null && status.trim().length() > 0 && status.trim().equalsIgnoreCase("INTIMATIONS")){
+				statusIds.add(1l);
+				statusIds.add(2l);
+			}else if(status != null && status.trim().length() > 0 && status.trim().equalsIgnoreCase("FORWARDED")){
+				statusIds.add(3l);
+				statusIds.add(7l);
+			}else if(status != null && status.trim().length() > 0 && status.trim().equalsIgnoreCase("SETTLED")){
+				statusIds.add(6l);
+				statusIds.add(8l);
+			}else if(status != null && status.trim().length() > 0 && status.trim().equalsIgnoreCase("REJECTED")){
+				statusIds.add(4l);
+				statusIds.add(5l);
+			}
+			
+			List<Long> complaintIds = insuranceStatusDAO.getStatusAndInsuranceCompanyWiseComplaintIds(locationId, locationValuesSet, stateId, cadreYearId, fromDate, toDate, statusIds, companyId, issueType);
+			if(complaintIds != null && !complaintIds.isEmpty())
+				returnList = getComplaintDetailsByComplaintIds(complaintIds);
+			
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getInsuraceStatusWiseComplaintsDetails on CoreDashboardInsuranceService", e);
+		}
+		return returnList;
+	}
+	
+	public List<ComplaintMasterVO> getComplaintDetailsByComplaintIds(List<Long> complaintIds){
+		List<ComplaintMasterVO> returnList = new ArrayList<ComplaintMasterVO>();
+		try {
+			List<Object[]> list = insuranceStatusDAO.getStatusAndInsuranceCompanyWiseComplaintDetails(complaintIds);
+			if(list != null && !list.isEmpty()){
+				for (Object[] obj : list) {
+					ComplaintMasterVO vo = new ComplaintMasterVO();
+					
+					vo.setComplaintId(Long.valueOf(obj[0] != null ? obj[0].toString():"0"));
+					vo.setName(obj[1] != null ? obj[1].toString():"");
+					vo.setMobileNo(obj[2] != null ? obj[2].toString():"");
+					vo.setMemberShipNo(obj[3] != null ? obj[3].toString():"");
+					vo.setDistrictId(Long.valueOf(obj[4] != null ? obj[4].toString():""));
+					vo.setDistrictName(obj[5] != null ? obj[5].toString():"");
+					vo.setConstituencyId(Long.valueOf(obj[6] != null ? obj[6].toString():""));
+					vo.setConstituencyName(obj[7] != null ? obj[7].toString():"");
+					vo.setSubject(obj[8] != null ? obj[8].toString():"");
+					vo.setDescription(obj[9] != null ? obj[9].toString():"");
+					vo.setTypeOfIssue(obj[10] != null ? obj[10].toString():"");
+					vo.setStatusId(Long.valueOf(obj[11] != null ? obj[11].toString():""));
+					vo.setStatus(obj[12] != null ? obj[12].toString():"");
+					vo.setPostedDate(obj[13] != null ? obj[13].toString():"");
+					
+					returnList.add(vo);
+				}
+			}
+			
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getComplaintDetailsByComplaintIds on CoreDashboardInsuranceService", e);
+		}
+		return returnList;
+	}
 }
