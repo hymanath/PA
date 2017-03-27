40e7997ec2438ff8ee576452a08474caa7230634
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-27 08:19:30 +0000

    file uploading in custom reports -- raghu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CustomReportVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/CustomReportVO.java
@@ -1,13 +1,36 @@
 package com.itgrids.partyanalyst.dto;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class CustomReportVO implements Serializable {
 	private Long id;
 	private String name;
 	private Long count = 0l;
+	private Long submited=0l;
+	private Long notSubmited=0l;
+	private File file;
+	
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
 	public Long getId() {
 		return id;
+	}
+	public Long getSubmited() {
+		return submited;
+	}
+	public void setSubmited(Long submited) {
+		this.submited = submited;
+	}
+	public Long getNotSubmited() {
+		return notSubmited;
+	}
+	public void setNotSubmited(Long notSubmited) {
+		this.notSubmited = notSubmited;
 	}
 	public String getName() {
 		return name;
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICustomReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICustomReportService.java
@@ -1,5 +1,14 @@
 package com.itgrids.partyanalyst.service;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import com.itgrids.partyanalyst.dto.CustomReportVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
+
 public interface ICustomReportService {
+	public List<CustomReportVO> getTotalExpertedReports(Long customReportProgramId);
+	 public ResultStatus saveCustomReportUploadFile(final Map<File,String> mapfiles,final Long userId);
 
 }
