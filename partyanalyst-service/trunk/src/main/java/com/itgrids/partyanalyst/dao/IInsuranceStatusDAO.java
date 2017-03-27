8fea64f27099a3fa0fb2b71f69903a80592e2813
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-27 10:14:14 +0000

    Insurance Module in Core Dashboard -- SRAVANTH.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInsuranceStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IInsuranceStatusDAO.java
@@ -1,9 +1,15 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.InsuranceStatus;
 
 public interface IInsuranceStatusDAO extends GenericDao<InsuranceStatus, Long>{
 
+	public List<Object[]> getStatusAndInsuranceCompanyWiseComplaints(Long locationId,Set<Long> locationValuesSet,Long stateId,Long cadreYearId,Date fromDate,Date toDate);
+	public List<Object[]> getAllInsuranceCompanies();
 }
