f7a0ab17edde295e88518916400745f0b8fcf918
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-28 07:28:17 +0000

    Updated By Hyma : new service class created for assemblySession module

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AssemblySessionService.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IAssemblySessionService;
+
+public class AssemblySessionService implements IAssemblySessionService{
+
+	
+	private final static Logger LOG = Logger.getLogger(AssemblySessionService.class);
+}
+
