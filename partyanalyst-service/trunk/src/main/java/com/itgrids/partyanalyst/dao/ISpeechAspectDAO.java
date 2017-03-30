e55a050c57b8aa607bb63bbf5ac63c182d33dc03
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-30 10:20:39 +0000

    Assembly Session Module commit by - Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpeechAspectDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ISpeechAspectDAO.java
@@ -1,10 +1,14 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.LeaderOccasion;
 import com.itgrids.partyanalyst.model.SpeechAspect;
 
 public interface ISpeechAspectDAO extends GenericDao<SpeechAspect, Long>{
+	
+	public List<Object[]> getAllSpeechAspectNames();
 
 }
