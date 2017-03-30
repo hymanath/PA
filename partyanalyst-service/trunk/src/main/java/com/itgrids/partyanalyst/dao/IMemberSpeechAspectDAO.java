1714b5281f0a60f990a2cf4aa14f6b82c58e80eb
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-30 04:57:51 +0000

    Assembly Session Module commit by - Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMemberSpeechAspectDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMemberSpeechAspectDAO.java
@@ -1,10 +1,16 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.LeaderOccasion;
 import com.itgrids.partyanalyst.model.MemberSpeechAspect;
 
 public interface IMemberSpeechAspectDAO extends GenericDao<MemberSpeechAspect, Long>{
+	
+	public List<Object[]> getNoOfDaysForSession(Long termId,String sesYear,List<Long> sessionIds,Date startDate,Date endDate);
+	public List<Object[]> getDayWisePartyWiseCount(Long termId,String sesYear,List<Long> sessionIds,Date startDate,Date endDate);
 
 }
