d5ebbb7c88bb85a23328083f714bfba0c07ffde6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-30 04:58:04 +0000

    Assembly Session Module commit by - Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AdminHouseTermDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AdminHouseTermDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAdminHouseTermDAO;
 import com.itgrids.partyanalyst.model.AdminHouseTerm;
@@ -9,7 +12,15 @@ public class AdminHouseTermDAO extends GenericDaoHibernate<AdminHouseTerm, Long>
 
 	public AdminHouseTermDAO() {
 		super(AdminHouseTerm.class);
-		
+	}
+	
+	public List<Object[]> getAllElectionYears(){
+		Query query = getSession().createQuery("select model.adminHouseTermId, " +
+				" year(model.fromDate),year(model.toDate) " +
+				" from AdminHouseTerm model " +
+				" where model.adminHouse.adminHouseId = 1" +
+				" and model.isActive = 'Y' and model.isDeleted = 'N' ");
+		return query.list();
 	}
 
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MemberSpeechAspectDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/MemberSpeechAspectDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IMemberSpeechAspectDAO;
-import com.itgrids.partyanalyst.model.LeaderOccasion;
 import com.itgrids.partyanalyst.model.MemberSpeechAspect;
 
 public class MemberSpeechAspectDAO extends GenericDaoHibernate<MemberSpeechAspect, Long> implements IMemberSpeechAspectDAO{
@@ -12,5 +15,88 @@ public class MemberSpeechAspectDAO extends GenericDaoHibernate<MemberSpeechAspec
 		super(MemberSpeechAspect.class);
 		
 	}
+	
+	public List<Object[]> getNoOfDaysForSession(Long termId,String sesYear,List<Long> sessionIds,Date startDate,Date endDate){
+		StringBuilder sb = new StringBuilder();
+			sb.append("select date(model.adminHouseSessionDay.adminHouseSession.fromDate)," +
+				" model.adminHouseSessionDay.adminHouseSession.houseSession.houseSessionId " +
+				" model.adminHouseSessionDay.adminHouseSession.houseSession.sessionName, " +
+				" count(model.adminHouseSessionDay.adminHouseSessionDayId)," +
+				" model.adminHouseSessionDay.adminHouseSession.adminHouseSessionId " +
+				" from MemberSpeechAspect model " +
+				" where model.isDeleted = 'N' ");
+		if(termId != null && termId.longValue() > 0l)
+		{
+			sb.append(" and model.adminHouseSessionDay.adminHouseSession.adminHouseTerm.adminHouseTermId = :termId ");
+		}
+		if(sesYear != null)
+		{
+			sb.append(" and model.adminHouseSessionDay.adminHouseSession.year = :sesYear");
+		}
+		if(sessionIds != null && sessionIds.size() > 0l)
+		{
+			sb.append(" and model.adminHouseSessionDay.adminHouseSession.houseSession.houseSessionId in (:sessionId)");
+		}
+		if(startDate != null && endDate != null)
+	    {
+			sb.append(" and date(model.adminHouseSessionDay.adminHouseSession.fromDate)  between :startDate and :endDate " );
+	    }
+		sb.append(" group by model.adminHouseSessionDay.adminHouseSession.adminHouseSessionId,model.adminHouseSessionDay.adminHouseSession.fromDate");
+		
+		Query query = getSession().createQuery(sb.toString());
+			if(termId != null && termId.longValue() > 0l)
+				query.setParameter("termId", termId);
+			if(sesYear != null)
+				query.setParameter("sesYear", sesYear);
+			if(sessionIds != null && sessionIds.size() > 0l)
+				query.setParameterList("sessionIds", sessionIds);
+			if(startDate != null && endDate != null){
+				query.setDate("startDate", startDate);
+				query.setDate("endDate", endDate);
+			}
+		return query.list();
+	}
+	
+	public List<Object[]> getDayWisePartyWiseCount(Long termId,String sesYear,List<Long> sessionIds,Date startDate,Date endDate){
+	    
+	    StringBuilder str = new StringBuilder();
+	    
+	    str.append(" select count(distinct model.adminHouseMember.adminHouseMemberId),model.adminHouseMember.partyId," +
+	        " model.adminHouseSessionDay.sessionDate,model.adminHouseSessionDay.adminHouseSession.adminHouseSessionId " +
+	        " from MemberSpeechAspect model " +
+	        " where model.isDeleted = 'N' " );
+	    
+	    if(termId != null && termId.longValue() >0l)
+	    {
+	      str.append(" and model.adminHouseSessionDay.adminHouseSession.adminHouseTerm.adminHouseTermId = :termId ");
+	    }
+	    if(sesYear != null)
+		{
+			str.append(" and model.adminHouseSessionDay.adminHouseSession.year = :sesYear");
+		}
+	    if(sessionIds != null && sessionIds.size() >0l)
+	    {
+	      str.append(" and model.adminHouseSessionDay.adminHouseSession.houseSession.houseSessionId in (:sessionId) ");
+	    }
+	    if(startDate != null && endDate != null)
+	    {
+	      str.append(" and date(model.adminHouseSessionDay.adminHouseSession.fromDate)  between :startDate and :endDate " );
+	    }
+	    
+	    str.append(" group by model.adminHouseMember.partyId,model.adminHouseSessionDay.sessionDate,model.adminHouseSessionDay.adminHouseSession.adminHouseSessionId  " );
+	    
+	    Query query = getSession().createQuery(str.toString());
+	    	if(termId != null && termId.longValue() >0l)
+	    		query.setParameter("termId", termId);
+	    	if(sesYear != null)
+				query.setParameter("sesYear", sesYear);
+	    	if(sessionIds != null && sessionIds.size() > 0l)
+				query.setParameterList("sessionIds", sessionIds);
+	    	 if(startDate != null && endDate != null){
+	    		 query.setDate("startDate", startDate);
+				 query.setDate("endDate", endDate);
+	    	 }
+	    return query.list();
+	  }
 
 }
