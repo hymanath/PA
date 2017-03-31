3b1c3a1e635af9c9f68d5d508473cf7c67f5521c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-03-31 14:24:18 +0000

    Assembly Session Module commit by - Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AdminHouseSessionDayDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AdminHouseSessionDayDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAdminHouseSessionDayDAO;
 import com.itgrids.partyanalyst.model.AdminHouseSessionDay;
@@ -12,4 +15,35 @@ public class AdminHouseSessionDayDAO extends GenericDaoHibernate<AdminHouseSessi
 		
 	}
 
+	public List<Object[]> getSingleDate(Long termId,String sessionYear,Long sessionId){
+		StringBuilder sb = new StringBuilder();
+			sb.append("select model.adminHouseSessionDayId," +
+					" date(model.sessionDate)" +
+					" from AdminHouseSessionDay model " +
+					" where model.isDeleted = 'N' and model.adminHouseSession.isActive ='Y' and model.adminHouseSession.isDeleted = 'N' ");
+		if(termId != null && termId.longValue() > 0l)
+		{
+			sb.append(" and model.adminHouseSession.adminHouseTerm.adminHouseTermId = :termId");
+		}
+		if(sessionYear != null && !sessionYear.toString().isEmpty())
+		{
+			sb.append(" and model.adminHouseSession.year = :sessionYear");
+		}
+		if(sessionId != null && sessionId.longValue() > 0l)
+		{
+			sb.append(" and model.adminHouseSession.houseSession.houseSessionId = :sessionId");
+		}
+		Query query = getSession().createQuery(sb.toString());
+		if(termId != null && termId.longValue() > 0l)
+			query.setParameter("termId", termId);
+		if(sessionYear != null && !sessionYear.toString().isEmpty())
+			query.setParameter("sessionYear", sessionYear);
+		if(sessionId != null && sessionId.longValue() > 0l)
+			query.setParameter("sessionId", sessionId);
+		
+		return query.list();
+		
+		
+		
+	}
 }
