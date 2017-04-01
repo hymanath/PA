7dc6eb413691267c19fc39caeb34f6befe6c8881
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-01 13:29:26 +0000

    Assembly Session Module commit by - Nandhini

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAdminHouseSessionDayDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAdminHouseSessionDayDAO.java
@@ -8,5 +8,5 @@ import com.itgrids.partyanalyst.model.AdminHouseSessionDay;
 
 public interface IAdminHouseSessionDayDAO extends GenericDao<AdminHouseSessionDay, Long>{
 	
-	public List<Object[]> getSingleDate(Long termId,String sessionYear,Long sessionId);
+	public List<Object[]> getSingleDate(Long adminHuSessionId);
 }
