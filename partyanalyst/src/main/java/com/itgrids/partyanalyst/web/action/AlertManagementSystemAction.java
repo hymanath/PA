1515c5e8877f8c8b6d0636693b0144fd803a640d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-05 12:10:02 +0000

    AMS New Jsps Creation - Balu

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.RegistrationVO;
 import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
 
-public class AlertManagementSystemAction {
+public class AlertManagementSystemAction extends ActionSupport implements ServletRequestAware{
 
 	private final static Logger LOG = Logger.getLogger(AlertManagementSystemAction.class);
 	private HttpServletRequest request;
@@ -18,49 +20,30 @@ public class AlertManagementSystemAction {
 	private String task;	
 	
 	
-	
-	  public HttpServletRequest getRequest() {
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;    
+	}
+	public HttpServletRequest getRequest() {
 		return request;
-	  }
-
-
-
+	}
 	public void setRequest(HttpServletRequest request) {
 		this.request = request;
 	}
-
-
-
 	public HttpSession getSession() {
 		return session;
 	}
-
-
-
 	public void setSession(HttpSession session) {
 		this.session = session;
 	}
-
-
-
 	public JSONObject getjObj() {
 		return jObj;
 	}
-
-
-
 	public void setjObj(JSONObject jObj) {
 		this.jObj = jObj;
 	}
-
-
-
 	public String getTask() {
 		return task;
 	}
-
-
-
 	public void setTask(String task) {
 		this.task = task;
 	}
