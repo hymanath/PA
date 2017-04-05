f90b086bdc1d41ecde29ba9d6391ac2e15c8fe2f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-05 09:04:00 +0000

    Updated By Hyma : models for govt alert module

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/AlertGovtOfficerSmsDetails.java
@@ -0,0 +1,119 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name="alert_govt_officer_sms_details")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class AlertGovtOfficerSmsDetails extends BaseModel implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long alertGovtOfficerSmsDetailsId;
+	private Long userId;
+	private Long govtOfficerId;
+	private String mobileNo;
+	//private User user;
+	
+	private Long alertId;
+	private Long alertStatusId;
+	private String smsText;
+	private String insertTime;
+	private Alert alert;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "alert_govt_officer_sms_details_id", unique = true, nullable = false)
+	public Long getAlertGovtOfficerSmsDetailsId() {
+		return alertGovtOfficerSmsDetailsId;
+	}
+	public void setAlertGovtOfficerSmsDetailsId(Long alertGovtOfficerSmsDetailsId) {
+		this.alertGovtOfficerSmsDetailsId = alertGovtOfficerSmsDetailsId;
+	}
+	
+	@Column(name="user_id")
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
+	@Column(name="govt_officer_id")
+	public Long getGovtOfficerId() {
+		return govtOfficerId;
+	}
+	public void setGovtOfficerId(Long govtOfficerId) {
+		this.govtOfficerId = govtOfficerId;
+	}
+	
+	@Column(name="mobile_no")
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	@Column(name="alert_id")
+	public Long getAlertId() {
+		return alertId;
+	}
+	public void setAlertId(Long alertId) {
+		this.alertId = alertId;
+	}
+	
+	@Column(name="alert_status_id")
+	public Long getAlertStatusId() {
+		return alertStatusId;
+	}
+	public void setAlertStatusId(Long alertStatusId) {
+		this.alertStatusId = alertStatusId;
+	}
+	
+	@Column(name="sms_text")
+	public String getSmsText() {
+		return smsText;
+	}
+	public void setSmsText(String smsText) {
+		this.smsText = smsText;
+	}
+	
+	@Column(name="insert_time")
+	public String getInsertTime() {
+		return insertTime;
+	}
+	public void setInsertTime(String insertTime) {
+		this.insertTime = insertTime;
+	}
+	
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name = "alert_id" ,insertable = false ,updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Alert getAlert() {
+		return alert;
+	}
+	public void setAlert(Alert alert) {
+		this.alert = alert;
+	}
+	
+}
