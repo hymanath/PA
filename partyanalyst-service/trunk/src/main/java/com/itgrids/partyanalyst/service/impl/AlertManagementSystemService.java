8b6c7c71428c33bc27131ac782b7c8a2f49fff60
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-05 11:39:12 +0000

    AMS New Jsps Creation - Balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AlertManagementSystemService.java
@@ -0,0 +1,7 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import com.itgrids.partyanalyst.service.IAlertManagementSystemService;
+
+public class AlertManagementSystemService implements IAlertManagementSystemService{
+
+}
