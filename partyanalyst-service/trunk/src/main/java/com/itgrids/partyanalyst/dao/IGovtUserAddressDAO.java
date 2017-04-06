61fcafe6e01ecc63ebf4abd05e63898f50b6f905
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 14:16:16 +0000

    DAO AND MODEL CREATED BY SANTOSH

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtUserAddressDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.GovtUserAddress;
+
+public interface IGovtUserAddressDAO extends GenericDao<GovtUserAddress, Long> {
+
+}
