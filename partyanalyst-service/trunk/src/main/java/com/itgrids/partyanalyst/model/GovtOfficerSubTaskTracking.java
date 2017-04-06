5de76fe137752d528dc08e02c20da6ddefd8cf6c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 14:01:08 +0000

    Updated By Hyma : models for govt alert module

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtOfficerSubTaskTracking.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/GovtOfficerSubTaskTracking.java
@@ -22,19 +22,26 @@ import org.hibernate.annotations.NotFoundAction;
 public class GovtOfficerSubTaskTracking {
 
 	private Long govtOfficerSubTaskTrackingId;
-	private Long assignedOfficerId;
-	private Long govtOfficerSubTaskId;
-	private Long govtSubTaskActionTypeId;
+	private Long alertAssignedOfficerId;
+	private Long govtAlertSubTaskId;
+	private Long govtAlertActionTypeId;
 	private Long alertSubTaskStatusId;
-	private AlertSeverity alertSeverity;
+	private Long alertDepartmentCommentId;
+	private Long alertDepartmentDocumentId;
 	private Date dueDate;
 	private Date insertedTime;
+	private Long insertedById;
 	private String isDeleted;
 	private Long alertSeverityId;
 	
-	private GovtSubTaskActionType govtSubTaskActionType;
 	private AlertSubTaskStatus alertSubTaskStatus;
-	
+	private AlertAssignedOfficerNew alertAssignedOfficerNew;
+	private GovtAlertSubTask govtAlertSubTask;
+	private GovtAlertActionType govtAlertActionType;
+	private AlertSeverity alertSeverity;
+	private AlertDepartmentCommentNew alertDepartmentComment;
+	private AlertDepartmentDocumentNew alertDepartmentDocument;
+	private User insertedBy;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -46,31 +53,31 @@ public class GovtOfficerSubTaskTracking {
 		this.govtOfficerSubTaskTrackingId = govtOfficerSubTaskTrackingId;
 	}
 	
-	@Column(name="assigned_officer_id")
-	public Long getAssignedOfficerId() {
-		return assignedOfficerId;
+	@Column(name = "alert_assigned_officer_id")
+	public Long getAlertAssignedOfficerId() {
+		return alertAssignedOfficerId;
 	}
-	public void setAssignedOfficerId(Long assignedOfficerId) {
-		this.assignedOfficerId = assignedOfficerId;
-	}
-	
-	@Column(name="govt_officer_sub_task_id")
-	public Long getGovtOfficerSubTaskId() {
-		return govtOfficerSubTaskId;
-	}
-	public void setGovtOfficerSubTaskId(Long govtOfficerSubTaskId) {
-		this.govtOfficerSubTaskId = govtOfficerSubTaskId;
+	public void setAlertAssignedOfficerId(Long alertAssignedOfficerId) {
+		this.alertAssignedOfficerId = alertAssignedOfficerId;
 	}
 	
-	@Column(name="govt_sub_task_action_type_id")
-	public Long getGovtSubTaskActionTypeId() {
-		return govtSubTaskActionTypeId;
+	@Column(name = "govt_alert_sub_task_id")
+	public Long getGovtAlertSubTaskId() {
+		return govtAlertSubTaskId;
 	}
-	public void setGovtSubTaskActionTypeId(Long govtSubTaskActionTypeId) {
-		this.govtSubTaskActionTypeId = govtSubTaskActionTypeId;
+	public void setGovtAlertSubTaskId(Long govtAlertSubTaskId) {
+		this.govtAlertSubTaskId = govtAlertSubTaskId;
 	}
 	
-	@Column(name="alert_sub_task_status_id")
+	@Column(name = "govt_alert_action_type_id")
+	public Long getGovtAlertActionTypeId() {
+		return govtAlertActionTypeId;
+	}
+	public void setGovtAlertActionTypeId(Long govtAlertActionTypeId) {
+		this.govtAlertActionTypeId = govtAlertActionTypeId;
+	}
+	
+	@Column(name = "alert_sub_task_status_id")
 	public Long getAlertSubTaskStatusId() {
 		return alertSubTaskStatusId;
 	}
@@ -78,14 +85,23 @@ public class GovtOfficerSubTaskTracking {
 		this.alertSubTaskStatusId = alertSubTaskStatusId;
 	}
 	
-	@Column(name="alert_severity_id")
-	public Long getAlertSeverityId() {
-		return alertSeverityId;
+	@Column(name = "alert_severity_id")
+	public Long getAlertDepartmentCommentId() {
+		return alertDepartmentCommentId;
 	}
-	public void setAlertSeverityId(Long alertSeverityId) {
-		this.alertSeverityId = alertSeverityId;
+	public void setAlertDepartmentCommentId(Long alertDepartmentCommentId) {
+		this.alertDepartmentCommentId = alertDepartmentCommentId;
 	}
-	@Column(name="due_date")
+	
+	@Column(name = "alert_department_comment_id")
+	public Long getAlertDepartmentDocumentId() {
+		return alertDepartmentDocumentId;
+	}
+	public void setAlertDepartmentDocumentId(Long alertDepartmentDocumentId) {
+		this.alertDepartmentDocumentId = alertDepartmentDocumentId;
+	}
+	
+	@Column(name = "due_date")
 	public Date getDueDate() {
 		return dueDate;
 	}
@@ -93,7 +109,7 @@ public class GovtOfficerSubTaskTracking {
 		this.dueDate = dueDate;
 	}
 	
-	@Column(name="inserted_by")
+	@Column(name = "inserted_time")
 	public Date getInsertedTime() {
 		return insertedTime;
 	}
@@ -101,7 +117,15 @@ public class GovtOfficerSubTaskTracking {
 		this.insertedTime = insertedTime;
 	}
 	
-	@Column(name="is_deleted")
+	@Column(name = "inserted_by")
+	public Long getInsertedById() {
+		return insertedById;
+	}
+	public void setInsertedById(Long insertedById) {
+		this.insertedById = insertedById;
+	}
+	
+	@Column(name = "is_deleted")
 	public String getIsDeleted() {
 		return isDeleted;
 	}
@@ -109,8 +133,61 @@ public class GovtOfficerSubTaskTracking {
 		this.isDeleted = isDeleted;
 	}
 	
-	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
-	@JoinColumn(name = "alert_severity_id" ,insertable = false ,updatable = false)
+	@Column(name = "govt_officer_sub_task_tracking_id")
+	public Long getAlertSeverityId() {
+		return alertSeverityId;
+	}
+	public void setAlertSeverityId(Long alertSeverityId) {
+		this.alertSeverityId = alertSeverityId;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="alert_sub_task_status_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public AlertSubTaskStatus getAlertSubTaskStatus() {
+		return alertSubTaskStatus;
+	}
+	public void setAlertSubTaskStatus(AlertSubTaskStatus alertSubTaskStatus) {
+		this.alertSubTaskStatus = alertSubTaskStatus;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="alert_assigned_officer_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public AlertAssignedOfficerNew getAlertAssignedOfficerNew() {
+		return alertAssignedOfficerNew;
+	}
+	public void setAlertAssignedOfficerNew(
+			AlertAssignedOfficerNew alertAssignedOfficerNew) {
+		this.alertAssignedOfficerNew = alertAssignedOfficerNew;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="govt_alert_sub_task_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public GovtAlertSubTask getGovtAlertSubTask() {
+		return govtAlertSubTask;
+	}
+	public void setGovtAlertSubTask(GovtAlertSubTask govtAlertSubTask) {
+		this.govtAlertSubTask = govtAlertSubTask;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="govt_alert_action_type_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public GovtAlertActionType getGovtAlertActionType() {
+		return govtAlertActionType;
+	}
+	public void setGovtAlertActionType(GovtAlertActionType govtAlertActionType) {
+		this.govtAlertActionType = govtAlertActionType;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="alert_severity_id", insertable=false, updatable = false)
 	@LazyToOne(LazyToOneOption.NO_PROXY)
 	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
 	public AlertSeverity getAlertSeverity() {
@@ -120,27 +197,44 @@ public class GovtOfficerSubTaskTracking {
 		this.alertSeverity = alertSeverity;
 	}
 	
-	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
-	@JoinColumn(name = "govt_sub_task_action_type_id" ,insertable = false ,updatable = false)
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="alert_department_comment_id", insertable=false, updatable = false)
 	@LazyToOne(LazyToOneOption.NO_PROXY)
 	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
-	public GovtSubTaskActionType getGovtSubTaskActionType() {
-		return govtSubTaskActionType;
+	public AlertDepartmentCommentNew getAlertDepartmentComment() {
+		return alertDepartmentComment;
 	}
-	public void setGovtSubTaskActionType(GovtSubTaskActionType govtSubTaskActionType) {
-		this.govtSubTaskActionType = govtSubTaskActionType;
+	public void setAlertDepartmentComment(
+			AlertDepartmentCommentNew alertDepartmentComment) {
+		this.alertDepartmentComment = alertDepartmentComment;
 	}
 	
-	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
-	@JoinColumn(name = "alert_sub_task_status_id" ,insertable = false ,updatable = false)
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="alert_department_document_id", insertable=false, updatable = false)
 	@LazyToOne(LazyToOneOption.NO_PROXY)
 	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
-	public AlertSubTaskStatus getAlertSubTaskStatus() {
-		return alertSubTaskStatus;
+	public AlertDepartmentDocumentNew getAlertDepartmentDocument() {
+		return alertDepartmentDocument;
 	}
-	public void setAlertSubTaskStatus(AlertSubTaskStatus alertSubTaskStatus) {
-		this.alertSubTaskStatus = alertSubTaskStatus;
+	public void setAlertDepartmentDocument(
+			AlertDepartmentDocumentNew alertDepartmentDocument) {
+		this.alertDepartmentDocument = alertDepartmentDocument;
 	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="inserted_by", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getInsertedBy() {
+		return insertedBy;
+	}
+	public void setInsertedBy(User insertedBy) {
+		this.insertedBy = insertedBy;
+	}
+	
+	
+	
+	
 	
 
 }
