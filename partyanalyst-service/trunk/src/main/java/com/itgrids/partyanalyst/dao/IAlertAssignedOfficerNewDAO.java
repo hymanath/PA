1330e52c2fe6e43560d617d6ffefa651ace367f6
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 10:59:00 +0000

    alert issue -- balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerNewDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertAssignedOfficerNewDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.AlertAssignedOfficerNew;
 
 public interface IAlertAssignedOfficerNewDAO extends GenericDao<AlertAssignedOfficerNew, Long> {
 
+	public List<Object[]> getAssignedStatuses();
 }
