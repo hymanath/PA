f895fd6edb91442be52f9f5201b719de4107be91
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 11:48:57 +0000

    Updated by santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertDepartmentLocationNewDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertDepartmentLocationNewDAO.java
@@ -7,5 +7,5 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.GovtAlertDepartmentLocationNew;
 
 public interface IGovtAlertDepartmentLocationNewDAO extends GenericDao<GovtAlertDepartmentLocationNew, Long> {
-	
+	public List<Object[]> getUserAccessLevels(Long userId);
 }
