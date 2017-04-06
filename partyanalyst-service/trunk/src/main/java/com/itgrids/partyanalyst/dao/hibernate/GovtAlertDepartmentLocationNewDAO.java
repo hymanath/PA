a358820b057ca422b87c5a9dc5ad9984a75dad28
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 03:45:27 +0000

    Updated by santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtAlertDepartmentLocationNewDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtAlertDepartmentLocationNewDAO.java
@@ -1,12 +1,17 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
-import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import java.util.List;
 
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IGovtAlertDepartmentLocationNewDAO;
 import com.itgrids.partyanalyst.model.GovtAlertDepartmentLocationNew;
 
-public class GovtAlertDepartmentLocationNewDAO extends GenericDaoHibernate<GovtAlertDepartmentLocationNew, Long> {
+public class GovtAlertDepartmentLocationNewDAO extends GenericDaoHibernate<GovtAlertDepartmentLocationNew, Long> implements IGovtAlertDepartmentLocationNewDAO {
 
 	public GovtAlertDepartmentLocationNewDAO(){
 		super(GovtAlertDepartmentLocationNew.class);
 	}
+	
 }
