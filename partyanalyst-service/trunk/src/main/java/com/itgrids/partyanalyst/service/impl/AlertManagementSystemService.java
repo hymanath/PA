7152f98f21ecef7de1625d16d71d361df6c18e6b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 08:46:42 +0000

    AMS New Impl - balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AlertManagementSystemService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/AlertManagementSystemService.java
@@ -1,12 +1,52 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
+import com.itgrids.partyanalyst.dao.IAlertAssignedOfficerDAO;
+import com.itgrids.partyanalyst.dao.IAlertAssignedOfficerNewDAO;
+import com.itgrids.partyanalyst.dto.IdNameVO;
 import com.itgrids.partyanalyst.service.IAlertManagementSystemService;
 
 public class AlertManagementSystemService implements IAlertManagementSystemService{
 	
 	private static final Logger LOG = Logger.getLogger(AlertManagementSystemService.class);
 
+	private IAlertAssignedOfficerNewDAO alertAssignedOfficerNewDAO; 
+	
+	public List<IdNameVO> getDepartmentStatus(){	
+		
+		List<IdNameVO> finalList = new ArrayList<IdNameVO>(); 
+		
+		try {
+			
+			IdNameVO VO = new IdNameVO();
+			VO.setId(1l);
+			VO.setName("Pending");
+			finalList.add(VO);
+			
+			List<Object[]> statusList = alertAssignedOfficerNewDAO.getAssignedStatuses();
+			
+			if(statusList !=null && statusList.size()>0){
+				for (Object[] param : statusList) {
+					
+					IdNameVO vo = new IdNameVO();
+					if(param[0] !=null){
+						vo.setId((Long)param[0]);
+						vo.setName(param[1] !=null  ? param[1].toString():"");
+						finalList.add(vo);
+					}	
+				}
+			}
+			
+			
+		} catch (Exception e) {
+			LOG.error("Error occured getDepartmentStatus() method of AlertManagementSystemService{}");
+		}
+		return finalList;
+		
+	}
 	
 }
