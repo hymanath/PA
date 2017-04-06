2581703495ca639a003bd732c39831c5dcfc7689
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-06 08:45:42 +0000

    AMS New Impl - balu

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertAssignedOfficerNewDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertAssignedOfficerNewDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAlertAssignedOfficerNewDAO;
 import com.itgrids.partyanalyst.model.AlertAssignedOfficerNew;
@@ -11,4 +14,17 @@ public class AlertAssignedOfficerNewDAO extends GenericDaoHibernate<AlertAssigne
         public AlertAssignedOfficerNewDAO(){
         	super(AlertAssignedOfficerNew.class);
         }
+        
+        public List<Object[]> getAssignedStatuses(){
+    		
+    		Query query = getSession().createQuery(" SELECT model.alertStatus.alertStatusId,model.alertStatus.alertStatus " +
+    				" FROM AlertAssignedOfficerNew model " +
+    				" WHERE  " +
+    					" model.isDeleted = 'N' " +
+    				" GROUP BY model.alertStatus.alertStatusId " +
+    				" ORDER BY model.alertStatus.statusOrder ");
+    		
+    		return query.list();
+    		
+    	}
 }
