7ae87d23622b6077e32986767c91ceb919dc25b3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-08 05:16:30 +0000

    Alert management system released by --Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
@@ -1,5 +1,8 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -7,6 +10,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.GovtAlertSubTask;
 
 public interface IGovtAlertSubTaskDAO extends GenericDao<GovtAlertSubTask, Long> {
-
+	
 	public List<Object[]> getDistrictOfficerAlertsSubTasksCount(Long govtDepDesigOffcrId,Long govtOffcrId,String countType,String type);
+ 	public List<Object[]> getSubTaskAlertAssignCountsForDeptWiseDetails(Date fromDate, Date toDate,int startIndex,int maxIndex);
 }
