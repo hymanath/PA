700f2ea0e635dde5f894f0e480dbfd203680f916
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-08 05:16:30 +0000

    Alert management system released by --Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtDepartmentScopeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtDepartmentScopeDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.GovtDepartmentScope;
 
 public interface IGovtDepartmentScopeDAO extends GenericDao<GovtDepartmentScope, Long>{
+	public List<Object[]> getgovtDepatScopeDetails(Long scopeId);
 	
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtDepartmentScopeLevelDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtDepartmentScopeLevelDAO.java
@@ -7,7 +7,8 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.GovtDepartmentScopeLevel;
 
 public interface IGovtDepartmentScopeLevelDAO extends GenericDao<GovtDepartmentScopeLevel, Long> {
-
 	public List<Object[]> getDepartmentLevels(Long deptId);
 	public List<Object[]> getParentLevelsOfLevel(Long deptId,Long levelId);
+	public List<Object[]> govtDepartmentScopeLevelDetails(Long scopeId);
+
 }
