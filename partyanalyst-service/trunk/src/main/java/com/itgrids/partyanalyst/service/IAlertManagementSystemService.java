f42559c61e4f0bda1a58e8d4967105969ad6939f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-08 09:59:47 +0000

    Alert Management SYstem Popup- Koushik

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertManagementSystemService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertManagementSystemService.java
@@ -6,6 +6,7 @@ import com.itgrids.partyanalyst.dto.AlertCoreDashBoardVO;
 import com.itgrids.partyanalyst.dto.AlertVO;
 import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.dto.DistrictOfficeViewAlertVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.dto.IdNameVO;
 
 public interface IAlertManagementSystemService {
@@ -16,6 +17,8 @@ public interface IAlertManagementSystemService {
 	public List<AlertVO> getDepartmentScope();
 	public List<IdAndNameVO> getDeptListForUser(Long userId);
 	public DistrictOfficeViewAlertVO getDistrictOfficerAlertsCountView(Long userId);
+	public ResultStatus updateComment(Long alertId,String comment,Long userId);
+	public ResultStatus updateAlertPriority(Long alertId,Long priorityId,Long userId);
 	public List<IdNameVO> getDepartmentLevels(Long departmentId);
 	public List<IdNameVO> getParentLevelsOfLevel(Long departmentId,Long levelId);
 	public List<AlertCoreDashBoardVO> getTotalAlertByStatus(String fromDateStr, String toDateStr, Long stateId, List<Long> printIdList, List<Long> electronicIdList, List<Long> deptIdList,Long statusId);
