c873b5563048002fec8b878ee6f8e288a1322a3a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-08 08:18:19 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
@@ -12,5 +12,6 @@ import com.itgrids.partyanalyst.model.GovtAlertSubTask;
 public interface IGovtAlertSubTaskDAO extends GenericDao<GovtAlertSubTask, Long> {
 	
 	public List<Object[]> getDistrictOfficerAlertsSubTasksCount(Long govtDepDesigOffcrId,Long govtOffcrId,String countType,String type);
+	public List<Object[]> getSubTaskCount(List<Long> alertIds);
  	public List<Object[]> getSubTaskAlertAssignCountsForDeptWiseDetails(Date fromDate, Date toDate,int startIndex,int maxIndex);
 }
