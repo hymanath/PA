9d8c3f3b1cc5ed440182971b0c3d0c9923006b3f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-09 15:39:47 +0000

    committed by swadhin

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
@@ -870,6 +870,39 @@ public class AlertManagementSystemAction extends ActionSupport implements Servle
 		}
 		return Action.SUCCESS;	
 	}
+	public String getStateThenGovtDeptScopeWiseAlertCount(){
+		try {
+			session = request.getSession();
+			RegistrationVO regVo = (RegistrationVO)session.getAttribute("USER");
+			Long userId = regVo.getRegistrationID();
+			jObj = new JSONObject(getTask());
+			String fromDate = jObj.getString("fromDate");
+			String toDate = jObj.getString("toDate");
+			Long stateId = jObj.getLong("stateId");
+			
+			JSONArray paperIdArr = jObj.getJSONArray("paperIdArr");  
+			List<Long> paperIdList = new ArrayList<Long>();
+			if(paperIdArr != null && paperIdArr.length() > 0){
+				for (int i = 0; i < paperIdArr.length(); i++){
+					paperIdList.add(Long.parseLong(paperIdArr.getString(i)));        
+				} 
+			}
+			
+			JSONArray chanelIdArr = jObj.getJSONArray("chanelIdArr");  
+			List<Long> chanelIdList = new ArrayList<Long>();
+			if(chanelIdArr != null && chanelIdArr.length() > 0){
+				for (int i = 0; i < chanelIdArr.length(); i++){
+					chanelIdList.add(Long.parseLong(chanelIdArr.getString(i)));          
+				}  
+			}
+			Long parentGovtDepartmentScopeId = jObj.getLong("parentGovtDepartmentScopeId");      
+			Long govtDepartmentId = jObj.getLong("govtDepartmentId");
+			alertCoreDashBoardVOs = alertManagementSystemService.getStateThenGovtDeptScopeWiseAlertCount(fromDate,toDate,stateId,paperIdList,chanelIdList,userId,govtDepartmentId,parentGovtDepartmentScopeId);
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getStateThenGovtDeptScopeWiseAlertCount() method, Exception - ",e); 
+		}
+		return Action.SUCCESS;	
+	}
 	
 	public String getDistrictLevelDeptWiseFlterClick(){
 		try{
