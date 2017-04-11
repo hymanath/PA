6e1a0215534e8b90adffd0048181b6212ecbf55e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-11 13:01:34 +0000

    Alert management system released by --Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtAlertSubTaskDAO.java
@@ -20,11 +20,15 @@ public interface IGovtAlertSubTaskDAO extends GenericDao<GovtAlertSubTask, Long>
  	public List<Object[]> getSubOrdinateTasksDetails(Long userId,Date fromDate,Date endDate , List<Long> govtScopeIds,List<Long> locationValues,Long levelId,List<Long> levelValues,
 			List<Long> desigIds,Long priorityId,String type);
  	 public List<Long> getDistrictOfficerSubTasksAlertIds(Long govtDepDesigOffcrId,Long govtOffcrId,String countType,String type);
- 	public List<Object[]> getDistrictOfficerSubTaskAlerts(Date fromDate,Date toDate,Long stateId,List<Long> electronicIdList,List<Long> printIdList,
+  	public List<Object[]> getDistrictOfficerSubTaskAlerts(Date fromDate,Date toDate,Long stateId,List<Long> electronicIdList,List<Long> printIdList,
  			Long levelId,List<Long> levelValues,Long govtDepartmentId,Long parentGovtDepartmentScopeId,List<Long> deptScopeIdList,String type);
- 	 public Integer updateSubTaskPriority(Long subTaskId,Long id,Long userId,Date date);
+	 public Integer updateSubTaskPriority(Long subTaskId,Long id,Long userId,Date date);
  	public Integer updateSubTaskDueDate(Long subTaskId,Date dueDate,Long userId,Date date);
  	public List<Object[]> getSubTaskInfoForAlert(Long alertId);
  	 public List<Long> getDistrictOfficerAlertsDetails(Date fromDate,Date toDate,Long stateId,Long levelId,List<Long> levelValues,
 		        Long govtDepartmentId,Long parentGovtDepartmentScopeId,Long govtDeptWorkLocId,Long statusId,Long childGovtScopeId);
+ 	public List<Object[]> getDistrictOfficerMyAssignedSubTasksStatusWiseDetails(Long govtDepDesigOffcrId,Long govtOffcrId);
+ 	public List<Object[]> getDistrictOfficerMySubTasksStatusWiseDetails(Long govtDepDesigOffcrId,Long govtOffcrId);
+ 	public List<Object[]> getDistrictOfficerMyAssignedSubTasksCountsView(Long govtDepDesigOffcrId,Long govtOffcrId,String type);
+ 	public List<Object[]> getDistrictOfficerAlertsSubTasksCountsView(Long govtDepDesigOffcrId,Long govtOffcrId,String type);
 }
