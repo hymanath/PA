b41d74e944d49cb4f3b8e7662652131c135d7979
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-12 13:08:55 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertTrackingDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertTrackingDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -11,6 +12,6 @@ public interface IAlertTrackingDAO extends GenericDao<AlertTracking, Long> {
 	public List<Object[]> getAlertTrackingDetails(Long alertId);
 	public List<Object[]> getAlertTrackingDetailsList(Long alertId,boolean hasTrue);
 	public List<Long> lastUpdatedstatus(Long alertId);
-	
-
+	public List<Object[]> getStatuswiseAlertsDetails(String mobileNo,Long userId, Date startDate, Date endDate,Long departmentId);
+	public List<Object[]> getAlertFeedbackStatuswiseAlertsDetails(String mobileNo,Long userId, Date startDate, Date endDate,Long departmentId);
 }
