ded21cc055ee4b72bc364a335bf670c3a21311de
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-12 20:08:18 +0000

    Updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertIssueSubTypeDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.partyanalyst.model.AlertIssueSubType;
+
+public interface IAlertIssueSubTypeDAO extends GenericDao<AlertIssueSubType,Long>{
+
+	public List<Object> getSubTypesByAlertIssueType(Long alertIssueTypeId);
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertIssueSubTypeDAO.java
@@ -0,0 +1,26 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IAlertIssueSubTypeDAO;
+import com.itgrids.partyanalyst.model.AlertIssueSubType;
+
+public class AlertIssueSubTypeDAO extends GenericDaoHibernate<AlertIssueSubType,Long> implements IAlertIssueSubTypeDAO{
+
+	public AlertIssueSubTypeDAO()
+	{
+		super(AlertIssueSubType.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> getSubTypesByAlertIssueType(Long alertIssueTypeId)
+	{
+		Query query = getSession().createQuery("SELECT model.alertIssueSubTypeId,model.issueType FROM AlertIssueSubType model WHERE " +
+				" model.alertIssueType.alertIssueTypeId = :alertIssueTypeId order by model.order_no");
+		query.setParameter("alertIssueTypeId",alertIssueTypeId);
+		return query.list();
+	}
+}
