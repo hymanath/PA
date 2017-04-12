5701d5ce131d54913d500c194c33d3061522e2ca
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-12 20:08:37 +0000

    Updated by Kamalakar Dandu

---
+++ partyanalyst-service/trunk/src/test/java/com/itgrids/partyanalyst/dao/hibernate/AlertIssueSubTypeDAOHibernateTest.java
@@ -0,0 +1,27 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.BaseDaoTestCase;
+
+import com.itgrids.partyanalyst.dao.IAlertIssueSubTypeDAO;
+
+public class AlertIssueSubTypeDAOHibernateTest extends BaseDaoTestCase{
+
+	private IAlertIssueSubTypeDAO alertIssueSubTypeDAO;
+
+	public void setAlertIssueSubTypeDAO(IAlertIssueSubTypeDAO alertIssueSubTypeDAO) {
+		this.alertIssueSubTypeDAO = alertIssueSubTypeDAO;
+	}
+	
+	public void test()
+	{
+		alertIssueSubTypeDAO.getAll();
+	}
+	
+	public void testGetSubTypesByAlertIssueType()
+	{
+		List<Object> list = alertIssueSubTypeDAO.getSubTypesByAlertIssueType(1L);
+		System.out.println(list.size());
+	}
+}
