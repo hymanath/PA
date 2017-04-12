c5db71309a030e3e4c57552445e8518ba03046a5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-12 20:13:00 +0000

    Updated By Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertCallerDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertCallerDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.AlertCaller;
 
 public interface IAlertCallerDAO extends GenericDao<AlertCaller, Long>{
-
+	public List<Long> checkIsExist(String mobileNO,String callerName);
 }
