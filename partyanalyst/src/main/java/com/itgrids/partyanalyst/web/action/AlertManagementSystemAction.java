1801ed9a23eb13a73124524292a193ac6837ea19
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-12 07:02:55 +0000

    updated by srujana

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
@@ -1885,4 +1885,48 @@ public class AlertManagementSystemAction extends ActionSupport implements Servle
 			}
 			return Action.SUCCESS;	
 		}
+		public String getStateThenGovtDeptScopeWiseAlertCountStatus1(){
+			try {
+				session = request.getSession();
+				RegistrationVO regVo = (RegistrationVO)session.getAttribute("USER");
+				Long userId = regVo.getRegistrationID();
+				jObj = new JSONObject(getTask());
+				String fromDate = jObj.getString("fromDate");
+				String toDate = jObj.getString("toDate");
+				Long stateId = jObj.getLong("stateId");
+				
+				JSONArray paperIdArr = jObj.getJSONArray("paperIdArr");  
+				List<Long> paperIdList = new ArrayList<Long>();
+				if(paperIdArr != null && paperIdArr.length() > 0){
+					for (int i = 0; i < paperIdArr.length(); i++){
+						paperIdList.add(Long.parseLong(paperIdArr.getString(i)));        
+					} 
+				}
+				
+				JSONArray chanelIdArr = jObj.getJSONArray("chanelIdArr");  
+				List<Long> chanelIdList = new ArrayList<Long>();
+				if(chanelIdArr != null && chanelIdArr.length() > 0){
+					for (int i = 0; i < chanelIdArr.length(); i++){
+						chanelIdList.add(Long.parseLong(chanelIdArr.getString(i)));          
+					}  
+				}
+				Long parentGovtDepartmentScopeId = jObj.getLong("parentGovtDepartmentScopeId");      
+				Long govtDepartmentId = jObj.getLong("govtDepartmentId");
+				String sortType = jObj.getString("sortType");
+				String order = jObj.getString("order");
+				Long districtWorkLocationId = jObj.getLong("districtWorkLocationId");
+				Long divisionWorkLocationId = jObj.getLong("divisionWorkLocationId");
+				Long subDivisionWorkLocationId = jObj.getLong("subDivisionWorkLocationId");
+				String alertType = jObj.getString("alertType");
+				String searchType = jObj.getString("searchType");
+				if(searchType.equalsIgnoreCase("statusWise"))
+				 alertCoreDashBoardVOs = alertManagementSystemService.getWorkLocationWiseThenGovtDeptScopeWiseAlertCountForOverview(fromDate,toDate,stateId,paperIdList,chanelIdList,userId,govtDepartmentId,parentGovtDepartmentScopeId,sortType,order,alertType,districtWorkLocationId,divisionWorkLocationId,subDivisionWorkLocationId,group);
+				else if(searchType.equalsIgnoreCase("scopeWise"))
+				  alertCoreDashBoardVOs=alertManagementSystemService.getWorkLocationWiseThenGovtDeptScopeWiseAlertCount(fromDate,toDate,stateId,paperIdList,chanelIdList,userId,govtDepartmentId,parentGovtDepartmentScopeId,sortType,order,alertType,districtWorkLocationId,divisionWorkLocationId,subDivisionWorkLocationId,group,searchType);
+					
+			} catch (Exception e) {
+				LOG.error("Exception Occured in getStateThenGovtDeptScopeWiseAlertCount() method, Exception - ",e); 
+			}
+			return Action.SUCCESS;	
+		}
 }
