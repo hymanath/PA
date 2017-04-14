3f0a630757dc4343b3a398e86ba45fa6fe196ee7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-14 16:31:48 +0000

    Location Report Alert Grievance Module -- NANDHINI.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/KeyValueVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/KeyValueVO.java
@@ -1,12 +1,17 @@
 package com.itgrids.partyanalyst.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KeyValueVO implements Serializable {
 
 	private Long id;
 	private String name;
-	private Long count;
+	private Long count = 0l;
+	private List<KeyValueVO> list = new ArrayList<KeyValueVO>(0);
+	private Long totalCount = 0l;
+	
 	
 	public Long getId() {
 		return id;
@@ -25,5 +30,17 @@ public class KeyValueVO implements Serializable {
 	}
 	public void setCount(Long count) {
 		this.count = count;
+	}
+	public List<KeyValueVO> getList() {
+		return list;
+	}
+	public void setList(List<KeyValueVO> list) {
+		this.list = list;
+	}
+	public Long getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(Long totalCount) {
+		this.totalCount = totalCount;
 	}
 }
