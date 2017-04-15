0f107a8785a93448e4ef1aad297ea3258d8acb94
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-15 13:43:09 +0000

    updated by srujana

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertSeverityDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertSeverityDAO.java
@@ -1,10 +1,13 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Alert;
 import com.itgrids.partyanalyst.model.AlertSeverity;
 
 public interface IAlertSeverityDAO extends GenericDao<AlertSeverity, Long> {
+	public List<Object[]> getFilterSectionDetailsOnSeverity();
 
 }
