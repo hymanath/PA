b6eddf59799798b19809adb56512a1c9cba78546
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-17 05:30:42 +0000

    UI Glitches --- Aravind

--- partyanalyst/src/main/webapp/alertDepartment/js/newAlertManagementSystem.js
+++ partyanalyst/src/main/webapp/alertDepartment/js/newAlertManagementSystem.js
@@ -6,7 +6,7 @@ var globalNewsPaperIdArr = [];
 var globalChannelIdArr = [];
 var globalDepartmentIdArr = [];
 var globalUserLevelId=0;
-var globalUserLevelValues = 0;	
+var globalUserLevelValues = [0];
 var globalDesignationId=0;
 var spinner = '<div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div></div>';
 
