98bbead330b8e4c63db64daaafecf631b1b3e8d4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-26 14:14:42 +0000

    LocationWise Report commit By -- NANDHINI.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertStatusDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertStatusDAO.java
@@ -1,5 +1,6 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.GenericDao;
@@ -11,5 +12,6 @@ public interface IAlertStatusDAO extends GenericDao<AlertStatus, Long> {
 	public List<Object[]> getAllStatus();
 	public List<Object[]> getAlertStatusInfoForReOpen();
 	public List<Object[]> getAlertStatusDtlsBasidOnAlertIds(List<Long> statusIds);
+	public List<String> getMonthAndYear(Date fromDate,Date toDate);
 
 }
