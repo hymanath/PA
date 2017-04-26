86db249aff5a4cf588c8371a1b67ede0f4818170
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-04-26 15:43:03 +0000

    BABU LOCATION WISE GRIEVANCE REPORTS

--- partyanalyst/src/main/webapp/js/grievanceReport/grievanceReport.js
+++ partyanalyst/src/main/webapp/js/grievanceReport/grievanceReport.js
@@ -1,42 +1,41 @@
- alert(2)
-    var start = moment().subtract(29, 'days');
-    var end = moment();
 
-    function cb(start, end) {
-        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
+var start = moment().subtract(29, 'days');
+var end = moment();
 
-    $('#reportrange').daterangepicker({
-        startDate: start,
-        endDate: end,
-        ranges: {
-           'Today': [moment(), moment()],
-           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-           'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        }
-    }, cb);
+function cb(start, end) {
+	$('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+}
 
-    cb(start, end); 
+$('#reportrange').daterangepicker({
+	startDate: start,
+	endDate: end,
+	ranges: {
+	   'Today': [moment(), moment()],
+	   'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+	   'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+	   'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+	   'This Month': [moment().startOf('month'), moment().endOf('month')],
+	   'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+	}
+}, cb);
+
+cb(start, end); 
     
 	
     $('#table1').DataTable();
 	
     $('#table2').DataTable();
 
-	
-	getGrievanceReport();
+getGrievanceReport();
  
 function getGrievanceReport(){
-	alert(22)
+	
 	 	var jsObj ={
-			fromDate: 01/01/2016,                       
-			toDateStr:01/12/2017,  
+			fromDate: "01/04/2016",                       
+			toDateStr:"01/04/2017",  
 			deptId:49,
-			sourceId:1,
-			rangeType:"Day",
+			sourceId:0,
+			rangeType:"week",
 			stateId:1
 	    }
 	    $.ajax({
@@ -44,9 +43,51 @@ function getGrievanceReport(){
 	    url: 'getGrievanceReportAction.action',
 	    data: {task :JSON.stringify(jsObj)}
 	    }).done(function(result){
-	    	
+	    	if(result !=null && result.length>0){
+				buildGrievanceReport(result);
+			}
 	    });
 	    
+}	 
+function buildGrievanceReport(result) {
+	var str='';
+		 str+='<table id="table1" class="table table-bordered " cellspacing="0">';
+		 str+='<colgroup>'
+			str+='</colgroup>'   
+			     str+=' <thead>';
+                       str+='<tr>';
+                       str+=' <th>District</th>';
+                       str+=' <th>Total</th>';
+                      
+                       for(var i in result[0].subList1){       
+						    str+=' <th>'+result[0].subList1[i].statusType+'</th>';
+					   }
+					   for(var j in result[0].subList2){         
+						    str+=' <th style="background-color:#ecebd6">'+result[0].subList2[j].day+'</th>';
+					   }
+                       str+=' </tr>';
+                      str+='</thead>';
+					   str+='<tbody>';
+					   for(var i in result){
+						   str+='<tr>'; 
+						  str+='<td>  <a class="js-open-modal " href="#" data-toggle="modal" data-target="#myModal2" >'+
+						  ' <i class="glyphicon glyphicon-plus-sign"></i>'+result[i].name+'</a></td>';
+						  str+='<td>'+result[i].totalAlertCnt+'</td>';
+						  for(var j in result[i].subList1){ 
+							str+='<td>'+result[i].subList1[j].totalAlertCnt+'</td>';
+						  }
+						  for(var j in result[i].subList2){ 
+							str+='<td>'+result[i].subList2[j].totalAlertCnt+'</td>';
+						  }     
+						  
+							str+='</tr>';   
+					    }
+	
+					   str+='</tbody>';
+					   str+='</table>';
+					   $('#grivenaceTableId').html(str);
+	
+}	
 }
 
 getAverageIssuePendingDays();
