9cf7c732c26656a481d919c0285abb31f74675ae
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-02 14:09:59 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertService.java
@@ -138,5 +138,6 @@ public interface IAlertService {
     public List<AlertCoreDashBoardVO> getGrievanceReportBasedOnLocationAndStatus(String fromDateStr,String toDateStr,Long stateId,Long deptId,Long sourceId,Long locationId,Long statusId);
     public List<AlertCoreDashBoardVO> getGrievanceReportDtlsForBellowLocation(String fromDateStr,String toDateStr,Long stateId,Long deptId,Long sourceId,Long locationId,Long statusId,String areaType,String groupType);
     public List<IdNameVO> getDeptListForGrievance();
-    public List<Object[]> getTotalAlertGroupByCategoryThenStatus(String fromDateStr, String toDateStr, Long stateId, Long departmentId,Long sourceId,Long locationId,Long statusId);
+    public List<AlertOverviewVO> getGrievanceReportDayWise(String fromDateStr, String toDateStr, Long stateId,Long departmentId, Long sourceId, String rangeType,Long LocationId,Long stsId,String groupType);
+    public List<AlertOverviewVO> getTotalAlertGroupByCategoryThenStatus(String fromDateStr, String toDateStr, Long stateId, Long departmentId,Long sourceId,Long locationId,Long statusId);
 }
