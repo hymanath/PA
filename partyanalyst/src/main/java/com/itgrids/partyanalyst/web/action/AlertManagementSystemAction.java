e61db7457b5ed8e360fcbc4d80a55e0c4529d44e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-13 07:22:59 +0000

    committed by swadhin

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/AlertManagementSystemAction.java
@@ -83,9 +83,18 @@ public class AlertManagementSystemAction extends ActionSupport implements Servle
 	private List<GrievanceAlertVO> grievanceAlertVo;
 	private FilterSectionVO filterDetilsList;
 	private String officerNameAnddesgnationName;
+	private List<IdNameVO> idNameVOList;
 	private Long alertDataId;
 	
 	
+	public List<IdNameVO> getIdNameVOList() {
+		return idNameVOList;
+	}
+
+	public void setIdNameVOList(List<IdNameVO> idNameVOList) {
+		this.idNameVOList = idNameVOList;
+	}
+
 	public List<List<AlertTrackingVO>> getListOfalertTrackingVOList() {
 		return listOfalertTrackingVOList;
 	}
@@ -3547,6 +3556,89 @@ public String getAlertSourceWiseAlert(){
 		}
 		return Action.SUCCESS;
  }
+	public String officerWiseAlertReport(){
+		try{
+			session = request.getSession();
+		   	RegistrationVO regVo = (RegistrationVO)session.getAttribute("USER");
+			Long userId = regVo.getRegistrationID();
+			idNameVOList = alertManagementSystemService.getDeptListForGreivanceReport(userId);  
+			return Action.SUCCESS;
+		}catch(Exception e){
+			LOG.error("Excpetion raised at getGrievanceReport Method",e);
+		}
+		return Action.SUCCESS;
+	}
+	public String getOfficerLocationWiseDepartmentOverviewAlertCount(){
+		try {
+			session = request.getSession();
+			RegistrationVO regVo = (RegistrationVO)session.getAttribute("USER");
+			Long userId = regVo.getRegistrationID();
+			
+			jObj = new JSONObject(getTask());
+			String fromDateStr = jObj.getString("fromDateStr");
+			String toDateStr = jObj.getString("toDateStr");
+			Long stateId = jObj.getLong("stateId");
+			
+			JSONArray printIdArr = null;
+			JSONArray electronicIdArr = null;
+			
+			List<Long> printIdList = new ArrayList<Long>(0);
+			List<Long> electronicIdList = new ArrayList<Long>();
+			
+			printIdArr = jObj.getJSONArray("printIdArr");  
+			
+			if(printIdArr != null && printIdArr.length() > 0){
+				for (int i = 0; i < printIdArr.length(); i++){
+					printIdList.add(Long.parseLong(printIdArr.getString(i)));        
+				} 
+			}
+			
+			
+			electronicIdArr = jObj.getJSONArray("electronicIdArr");
+			
+			if(electronicIdArr != null && electronicIdArr.length() > 0){
+				for (int i = 0; i < electronicIdArr.length(); i++){
+					electronicIdList.add(Long.parseLong(electronicIdArr.getString(i)));        
+				} 
+			}
+			
+			Long govtDepartmentId = jObj.getLong("govtDepartmentId");
+			Long parentGovtDepartmentScopeId = jObj.getLong("parentGovtDepartmentScopeId");
+			String sortingType = jObj.getString("sortingType");
+			String order = jObj.getString("order");
+			String alertType = jObj.getString("alertType");
+			
+			String group = jObj.getString("group");
+			JSONArray subLevelsArr = jObj.getJSONArray("subLevels");  
+			List<Long> sublevels = new ArrayList<Long>();
+			if(subLevelsArr != null && subLevelsArr.length() > 0){
+				for (int i = 0; i < subLevelsArr.length(); i++){
+					sublevels.add(Long.parseLong(subLevelsArr.getString(i)));          
+				}  
+			}
+			
+			JSONArray callCenterIdArr = jObj.getJSONArray("chanelIdArr");  
+			List<Long> callCenterIdList = new ArrayList<Long>();
+			if(callCenterIdArr != null && callCenterIdArr.length() > 0){
+				for (int i = 0; i < callCenterIdArr.length(); i++){
+					callCenterIdList.add(Long.parseLong(callCenterIdArr.getString(i)));        
+				} 
+			}
+			
+			String searchType = jObj.getString("searchType");
+			Long filterParentScopeId = jObj.getLong("filterParentScopeId");
+			Long filterScopeValue = jObj.getLong("filterScopeValue");
+			
+	        alertCoreDashBoardVOs = alertManagementSystemService.getOfficerLocationWiseDepartmentOverviewAlertCount(fromDateStr,
+			toDateStr,stateId,printIdList,electronicIdList,userId,govtDepartmentId,parentGovtDepartmentScopeId,sortingType,
+			order,alertType,group,callCenterIdList,sublevels,filterParentScopeId,filterScopeValue,searchType);
+	        
+			
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getWorkLocationWiseThenGovtDeptScopeWiseAlertCountForOverview() method, Exception - ",e);
+		}
+		return Action.SUCCESS;	
+	}
 	public String getLocationFilterClickDetails(){
 		try {
 			session = request.getSession();
