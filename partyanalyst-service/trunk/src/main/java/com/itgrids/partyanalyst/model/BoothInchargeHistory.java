005a83c1e6dc31b2fe39cce7d72a3b55a4c4ef9a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-15 05:39:39 +0000

    BoothWiseDetails updated by Nandhini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/BoothInchargeHistory.java
@@ -0,0 +1,190 @@
+package com.itgrids.partyanalyst.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "booth_incharge_history")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class BoothInchargeHistory extends BaseModel implements Serializable{
+
+	private Long boothInchargeHistoryId;
+	private Long boothInchargeId;
+	private Long boothId;
+	private Long tdpCadreId;
+	private String isActive;
+	private String isDeleted;
+	private Long boothInchargeEnrollmentId;
+	private Long insertedBy;
+	private Long updatedBy;
+	private Date insertedTime;
+	private Date updatedTime;
+	
+	private Booth booth;
+	private TdpCadre tdpCadre;
+	private BoothInchargeEnrollment boothInchargeEnrollment;
+	private User user;
+	private BoothIncharge boothIncharge;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "booth_incharge_history_id", unique = true, nullable = false)
+	public Long getBoothInchargeHistoryId() {
+		return boothInchargeHistoryId;
+	}
+	public void setBoothInchargeHistoryId(Long boothInchargeHistoryId) {
+		this.boothInchargeHistoryId = boothInchargeHistoryId;
+	}
+	
+	@Column(name="booth_incharge_id")
+	public Long getBoothInchargeId() {
+		return boothInchargeId;
+	}
+	public void setBoothInchargeId(Long boothInchargeId) {
+		this.boothInchargeId = boothInchargeId;
+	}
+	
+	@Column(name="booth_id")
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	
+	@Column(name="tdp_cadre_id")
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	
+	@Column(name="is_active")
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	
+	@Column(name="is_deleted")
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	@Column(name="booth_incharge_enrollment_id")
+	public Long getBoothInchargeEnrollmentId() {
+		return boothInchargeEnrollmentId;
+	}
+	public void setBoothInchargeEnrollmentId(Long boothInchargeEnrollmentId) {
+		this.boothInchargeEnrollmentId = boothInchargeEnrollmentId;
+	}
+	
+	@Column(name="inserted_by")
+	public Long getInsertedBy() {
+		return insertedBy;
+	}
+	public void setInsertedBy(Long insertedBy) {
+		this.insertedBy = insertedBy;
+	}
+	
+	@Column(name="updated_by")
+	public Long getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Long updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	
+	@Column(name="inserted_time")
+	public Date getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(Date insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	
+	@Column(name="updated_time")
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="booth_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public Booth getBooth() {
+		return booth;
+	}
+	public void setBooth(Booth booth) {
+		this.booth = booth;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="tdp_cadre_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public TdpCadre getTdpCadre() {
+		return tdpCadre;
+	}
+	public void setTdpCadre(TdpCadre tdpCadre) {
+		this.tdpCadre = tdpCadre;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="booth_incharge_enrollment_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public BoothInchargeEnrollment getBoothInchargeEnrollment() {
+		return boothInchargeEnrollment;
+	}
+	public void setBoothInchargeEnrollment(BoothInchargeEnrollment boothInchargeEnrollment) {
+		this.boothInchargeEnrollment = boothInchargeEnrollment;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="inserted_by", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="booth_incharge_id", insertable=false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public BoothIncharge getBoothIncharge() {
+		return boothIncharge;
+	}
+	public void setBoothIncharge(BoothIncharge boothIncharge) {
+		this.boothIncharge = boothIncharge;
+	}
+}
