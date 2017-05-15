91cbc2a49fed4c44a26d13c9ff7676f7e7f5456b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-15 05:38:50 +0000

    BoothWiseDetails updated by Nandhini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeEnrollmentDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.BoothInchargeEnrollment;
+
+public interface IBoothInchargeEnrollmentDAO extends GenericDao<BoothInchargeEnrollment, Long>{
+
+}
