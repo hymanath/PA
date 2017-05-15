bf6d15c1e1b6d90f68a82718fb8eaf8e4f392d97
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-15 05:39:23 +0000

    BoothWiseDetails updated by Nandhini

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BoothInchargesVO.java
@@ -0,0 +1,72 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class BoothInchargesVO implements Serializable {
+	
+	private Long boothId;
+	private Long tdpCadreId;
+	private String isActive;
+	private String isDeleted;
+	private Long boothInchargeEnrollmentId;
+	private Long insertedBy;
+	private Long updatedBy;
+	private String insertedTime;
+	private String updatedTime;
+	
+	
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	public Long getTdpCadreId() {
+		return tdpCadreId;
+	}
+	public void setTdpCadreId(Long tdpCadreId) {
+		this.tdpCadreId = tdpCadreId;
+	}
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public Long getBoothInchargeEnrollmentId() {
+		return boothInchargeEnrollmentId;
+	}
+	public void setBoothInchargeEnrollmentId(Long boothInchargeEnrollmentId) {
+		this.boothInchargeEnrollmentId = boothInchargeEnrollmentId;
+	}
+	public Long getInsertedBy() {
+		return insertedBy;
+	}
+	public void setInsertedBy(Long insertedBy) {
+		this.insertedBy = insertedBy;
+	}
+	public Long getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Long updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getInsertedTime() {
+		return insertedTime;
+	}
+	public void setInsertedTime(String insertedTime) {
+		this.insertedTime = insertedTime;
+	}
+	public String getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(String updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+}
