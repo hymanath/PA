d973373f89be57c5829d85569ed9c893c9c21baa
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-17 08:50:05 +0000

    LOCATION WISE GRIEVANCE REPORTS - BABU

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IAlertService.java
@@ -153,4 +153,6 @@ public interface IAlertService {
     public List<AlertTrackingVO> getSocialAlertFeedBackDetails(Long userId,String startdateStr,String endDateStr,String mobileNo,Long departmentId);
     public String saveAlertFeedbackStatusDetails(final AlertVO alertvo,final Long userId);
     public List<AlertOverviewVO> getAlertImpactLocationWiseLocationSubTemplate(List<Long> locationIds);
+    public List<AlertsSummeryVO> getAlertEfficiencyList2(List<Long> departmentIds,List<Long> sourceIds,List<Long> alertstatusIds
+			,String startDate,String endDate,String rangeType);
 }
