534ade9b786a517b5261328eca99b058bcf7bf8e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-22 13:06:32 +0000

    Updated by srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahaNaduService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahaNaduService.java
@@ -52,8 +52,8 @@ public interface IMahaNaduService {
 	 public PartyMeetingVO getParticipatedCandidateEventDetails(Long tdpCadreId);
 	 public List<MahanaduEventVO> getAttendeeSummaryForEventsList(Long eventId,Long stateId,Long reportLevelId,List<Long> subEventIds);
 	 public List<IdNameVO> getSubEventsOfEvent(Long eventId);
-	 public List<MahanaduEventVO> getDaysUniqueAndRevisitSummary(Long eventId,Long stateId,Long reportLevelId,List<Long> subEventIds,String dateValues);
-	 public List<MahanaduEventVO> getDayWiseVisitSummary(Long eventId,Long stateId,Long reportLevelId,List<Long> subEventIds,String date);
+	 public List<MahanaduEventVO> getDaysUniqueAndRevisitSummary(Long eventId,Long stateId,Long reportLevelId,List<Long> subEventIds,String dateValues,List<Long> enrollmentIdsList);
+	 public List<MahanaduEventVO> getDayWiseVisitSummary(Long eventId,Long stateId,Long reportLevelId,List<Long> subEventIds,String date,List<Long> enrollmentIdsList);
 	 
 	 public ResultStatus insertDataintoEventInfo1(Date startDate,Date endDate,Long parentEventId,List<Long> subEventIds);
 	 public List<IdNameVO> getSubEventsOfNewEvent(Long eventId);
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahanaduDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMahanaduDashBoardService.java
@@ -12,11 +12,11 @@ public interface IMahanaduDashBoardService {
 	public List<MahanaduVisitVO> getTodayTotalAndCurrentUsersInfo(Date startDate,Date endDate);
 	public void getTodayTotalVisitorsForWeb();
 	public void getTodayTotalVisitorsForSchedular();
-	public List<MahanaduVisitVO> getTodayTotalAndCurrentUsersInfoList(Long eventId,String dateValues);
-	public MahanaduVisitVO getTodayTotalAndCurrentUsersInfoListNew(Long eventId,String eventDate);
+	public List<MahanaduVisitVO> getTodayTotalAndCurrentUsersInfoList(Long eventId,String dateValues,List<Long> enrollmentIdsList);
+	public MahanaduVisitVO getTodayTotalAndCurrentUsersInfoListNew(Long eventId,String eventDate,List<Long> enrollmentIdsList);
 	public MahanaduEventVO getDistrictWiseTotalAndPresentCadre(Long eventId,List<Long> stateIds,String date);
 	public Long getTodayCount(Long eventId);
-	public List<MahanaduEventVO> getHourWiseNowInCampusCadresCount(String dayCount,Long eventId);
+	public List<MahanaduEventVO> getHourWiseNowInCampusCadresCount(String dayCount,Long eventId,List<Long> enrollmentIdsList);
 	public List<IdNameVO> getEventDates(Long eventId);
 	public MahanaduEventVO getConstituencyWiseMembersCountInCampus(Long eventId,List<Long> stateIds,String date);
 	//public List<CandidateDetailsVO> getCandidateDetails(Long designationId,String inviteeType,Long eventId,String day);
