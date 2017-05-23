fa656cac1312b4a178f331892eb28278bc081ba7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-23 15:01:47 +0000

    Updated by Sanjeev

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/BloodDonationVo.java
@@ -0,0 +1,35 @@
+package com.itgrids.partyanalyst.dto;
+
+public class BloodDonationVo {
+
+	private String name;
+	private String mobileNo;
+	private String memberShipId;
+	private String dateTime;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getMemberShipId() {
+		return memberShipId;
+	}
+	public void setMemberShipId(String memberShipId) {
+		this.memberShipId = memberShipId;
+	}
+	public String getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+
+}
