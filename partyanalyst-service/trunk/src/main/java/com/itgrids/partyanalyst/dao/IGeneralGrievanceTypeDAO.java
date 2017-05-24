8aa826657c330b32ce7473788ef1e35f2e4d233a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-24 14:39:55 +0000

    Updated by -- SRAVANTH.

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGeneralGrievanceTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.GeneralGrievanceType;
+
+public interface IGeneralGrievanceTypeDAO extends GenericDao<GeneralGrievanceType, Long>{
+
+}
