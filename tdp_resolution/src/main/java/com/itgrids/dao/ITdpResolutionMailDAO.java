c9c689eed9c1036a13221949815f681387fc4874
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-25 14:07:18 +0000

    Updated by Sanjeev

---
+++ tdp_resolution/src/main/java/com/itgrids/dao/ITdpResolutionMailDAO.java
@@ -0,0 +1,11 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.TdpResolutionMail;
+
+public interface ITdpResolutionMailDAO extends GenericDao<TdpResolutionMail,Integer> {
+	
+	
+
+}
