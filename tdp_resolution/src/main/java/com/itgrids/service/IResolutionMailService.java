fbaa80678ed5071533f0be653778262445a1df93
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-27 05:15:13 +0000

    Updated by Sanjeev

--- tdp_resolution/src/main/java/com/itgrids/service/IResolutionMailService.java
+++ tdp_resolution/src/main/java/com/itgrids/service/IResolutionMailService.java
@@ -1,10 +1,11 @@
 package com.itgrids.service;
 
+import java.util.List;
 import com.itgrids.dto.TdpResolutionVo;
 
 public interface IResolutionMailService {
 
 		public String sentEmails(TdpResolutionVo tdpResolutionVo);
 
-
+		public List getResolutions(String days);
 }
