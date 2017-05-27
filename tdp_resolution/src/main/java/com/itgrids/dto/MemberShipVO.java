fb94b317ca1734865c9faa17cbf320da64671586
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-27 05:13:57 +0000

    Updated by Sanjeev

---
+++ tdp_resolution/src/main/java/com/itgrids/dto/MemberShipVO.java
@@ -0,0 +1,211 @@
+package com.itgrids.dto;
+
+public class MemberShipVO {
+
+	private String entryType;
+	private Long districtId;
+	private String district;
+	private Long constituencyId;
+	private String constituency;
+	private Long mandalId;
+	private String mandal;
+	private String panchayat;
+	private Long panchayatId;
+	private Long boothId;
+	private Long localBodyId;
+	private Long townId;
+	
+	private String regType;
+	
+	private String date;
+	private String mobileNo;
+	private String trNo;
+	private String cardNo;
+	private Long constituecyNo;
+	private String isAddress;
+	private String memberShipNumber;
+	
+	private String name;
+	private Long userId;
+	private String uname;
+	private String pwd;
+	private String isOtherState;
+	private String relativeName;
+	private String voterIdCardNo;
+	private Long enrollmentId;
+	
+	public Long getEnrollmentId() {
+		return enrollmentId;
+	}
+	public void setEnrollmentId(Long enrollmentId) {
+		this.enrollmentId = enrollmentId;
+	}
+	public String getVoterIdCardNo() {
+		return voterIdCardNo;
+	}
+	public void setVoterIdCardNo(String voterIdCardNo) {
+		this.voterIdCardNo = voterIdCardNo;
+	}
+	public String getRelativeName() {
+		return relativeName;
+	}
+	public void setRelativeName(String relativeName) {
+		this.relativeName = relativeName;
+	}
+	public String getIsOtherState() {
+		return isOtherState;
+	}
+	public void setIsOtherState(String isOtherState) {
+		this.isOtherState = isOtherState;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMemberShipNumber() {
+		return memberShipNumber;
+	}
+	public void setMemberShipNumber(String memberShipNumber) {
+		this.memberShipNumber = memberShipNumber;
+	}
+	public String getIsAddress() {
+		return isAddress;
+	}
+	public void setIsAddress(String isAddress) {
+		this.isAddress = isAddress;
+	}
+	
+	public Long getConstituecyNo() {
+		return constituecyNo;
+	}
+	public void setConstituecyNo(Long constituecyNo) {
+		this.constituecyNo = constituecyNo;
+	}
+	public String getCardNo() {
+		return cardNo;
+	}
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getTrNo() {
+		return trNo;
+	}
+	public void setTrNo(String trNo) {
+		this.trNo = trNo;
+	}
+	public Long getLocalBodyId() {
+		return localBodyId;
+	}
+	public void setLocalBodyId(Long localBodyId) {
+		this.localBodyId = localBodyId;
+	}
+	public Long getBoothId() {
+		return boothId;
+	}
+	public void setBoothId(Long boothId) {
+		this.boothId = boothId;
+	}
+	public String getEntryType() {
+		return entryType;
+	}
+	public void setEntryType(String entryType) {
+		this.entryType = entryType;
+	}
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public Long getConstituencyId() {
+		return constituencyId;
+	}
+	public void setConstituencyId(Long constituencyId) {
+		this.constituencyId = constituencyId;
+	}
+	public String getConstituency() {
+		return constituency;
+	}
+	public void setConstituency(String constituency) {
+		this.constituency = constituency;
+	}
+	public Long getMandalId() {
+		return mandalId;
+	}
+	public void setMandalId(Long mandalId) {
+		this.mandalId = mandalId;
+	}
+	public String getMandal() {
+		return mandal;
+	}
+	public void setMandal(String mandal) {
+		this.mandal = mandal;
+	}
+	public String getPanchayat() {
+		return panchayat;
+	}
+	public void setPanchayat(String panchayat) {
+		this.panchayat = panchayat;
+	}
+	public Long getPanchayatId() {
+		return panchayatId;
+	}
+	public void setPanchayatId(Long panchayatId) {
+		this.panchayatId = panchayatId;
+	}
+	public String getRegType() {
+		return regType;
+	}
+	public void setRegType(String regType) {
+		this.regType = regType;
+	}
+	public Long getTownId() {
+		return townId;
+	}
+	public void setTownId(Long townId) {
+		this.townId = townId;
+	}
+	
+
+
+}
