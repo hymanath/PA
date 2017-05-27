37605717e76ca54131c69c56d952e3a476eba732
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-27 05:49:51 +0000

    Meekosam Grievance Module -- SRAVANTH.

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/MeekosamGrievanceService.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.partyanalyst.service.IMeekosamGrievanceService;
+
+public class MeekosamGrievanceService implements IMeekosamGrievanceService{
+
+	private final static Logger LOG =  Logger.getLogger(MeekosamGrievanceService.class);
+}
