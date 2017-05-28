a80e89de0a5d040d0b99b54287602137ab3b0030
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-28 13:01:15 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMeekosamGrievanceService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IMeekosamGrievanceService.java
@@ -1,5 +1,18 @@
 package com.itgrids.partyanalyst.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.IdAndNameVO;
+import com.itgrids.partyanalyst.dto.IdNameVO;
+import com.itgrids.partyanalyst.dto.PetitionerDetailsVO;
+
 public interface IMeekosamGrievanceService {
+	public String saveMeekosamPetitionerDetails(final PetitionerDetailsVO inputvo);
+	public List<IdNameVO> getAllMandalsByDistrictID(Long districtId);
+	public List<IdNameVO> getAllHamletByPanchayatID(Long panchayatId);
+	public List<IdAndNameVO> getMeekosamOccupationList();
+	public List<IdAndNameVO> getMeekosamCasteCategoryList();
+	public List<IdAndNameVO> getMeekosamArgeeCategoryList();
+	public List<IdAndNameVO> getMeekosamAnnualIncomeList();
 
 }
