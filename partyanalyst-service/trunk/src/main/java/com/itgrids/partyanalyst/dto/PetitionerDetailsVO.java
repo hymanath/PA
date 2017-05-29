be2fbc1a224702d17d108dd304d2dd5cbf07c484
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-29 05:13:29 +0000

    committed by swadhin

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/PetitionerDetailsVO.java
@@ -0,0 +1,174 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+
+public class PetitionerDetailsVO implements Serializable {
+		private String name;
+		private String relativeName;
+		private Long age;
+		private String dateOfBirth;
+		private String gender;
+		private String houseNo;
+		private String aadharNo;
+		private String voterCardNo;
+		private String mobileNo;
+		private String emailId;
+		private Long occupationId;
+		private Long casteCategoryId;
+		private Long argeeCategoryId;
+		private Long annulIncomeId;
+		private Long districtId;
+		private Long constituencyId;
+		private String  tehsil;
+		private Long panchayatId;
+		private Long hamletId;
+		private Long userId;
+		private Long meekosamOccupationId;
+		private Long meekosamCasteCategoryId;
+		private Long meekosamArgeeCategoryId;
+		private Long meekosamAnnualIncomeId;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getRelativeName() {
+			return relativeName;
+		}
+		public void setRelativeName(String relativeName) {
+			this.relativeName = relativeName;
+		}
+		public Long getAge() {
+			return age;
+		}
+		public void setAge(Long age) {
+			this.age = age;
+		}
+		public String getDateOfBirth() {
+			return dateOfBirth;
+		}
+		public void setDateOfBirth(String dateOfBirth) {
+			this.dateOfBirth = dateOfBirth;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getHouseNo() {
+			return houseNo;
+		}
+		public void setHouseNo(String houseNo) {
+			this.houseNo = houseNo;
+		}
+		public String getAadharNo() {
+			return aadharNo;
+		}
+		public void setAadharNo(String aadharNo) {
+			this.aadharNo = aadharNo;
+		}
+		public String getVoterCardNo() {
+			return voterCardNo;
+		}
+		public void setVoterCardNo(String voterCardNo) {
+			this.voterCardNo = voterCardNo;
+		}
+		public String getMobileNo() {
+			return mobileNo;
+		}
+		public void setMobileNo(String mobileNo) {
+			this.mobileNo = mobileNo;
+		}
+		public String getEmailId() {
+			return emailId;
+		}
+		public void setEmailId(String emailId) {
+			this.emailId = emailId;
+		}
+		public Long getOccupationId() {
+			return occupationId;
+		}
+		public void setOccupationId(Long occupationId) {
+			this.occupationId = occupationId;
+		}
+		public Long getCasteCategoryId() {
+			return casteCategoryId;
+		}
+		public void setCasteCategoryId(Long casteCategoryId) {
+			this.casteCategoryId = casteCategoryId;
+		}
+		public Long getArgeeCategoryId() {
+			return argeeCategoryId;
+		}
+		public void setArgeeCategoryId(Long argeeCategoryId) {
+			this.argeeCategoryId = argeeCategoryId;
+		}
+		public Long getAnnulIncomeId() {
+			return annulIncomeId;
+		}
+		public void setAnnulIncomeId(Long annulIncomeId) {
+			this.annulIncomeId = annulIncomeId;
+		}
+		public Long getDistrictId() {
+			return districtId;
+		}
+		public void setDistrictId(Long districtId) {
+			this.districtId = districtId;
+		}
+		public Long getConstituencyId() {
+			return constituencyId;
+		}
+		public void setConstituencyId(Long constituencyId) {
+			this.constituencyId = constituencyId;
+		}
+		public String getTehsil() {
+			return tehsil;
+		}
+		public void setTehsil(String tehsil) {
+			this.tehsil = tehsil;
+		}
+		public Long getPanchayatId() {
+			return panchayatId;
+		}
+		public void setPanchayatId(Long panchayatId) {
+			this.panchayatId = panchayatId;
+		}
+		public Long getHamletId() {
+			return hamletId;
+		}
+		public void setHamletId(Long hamletId) {
+			this.hamletId = hamletId;
+		}
+		public Long getMeekosamOccupationId() {
+			return meekosamOccupationId;
+		}
+		public void setMeekosamOccupationId(Long meekosamOccupationId) {
+			this.meekosamOccupationId = meekosamOccupationId;
+		}
+		public Long getMeekosamCasteCategoryId() {
+			return meekosamCasteCategoryId;
+		}
+		public void setMeekosamCasteCategoryId(Long meekosamCasteCategoryId) {
+			this.meekosamCasteCategoryId = meekosamCasteCategoryId;
+		}
+		public Long getMeekosamArgeeCategoryId() {
+			return meekosamArgeeCategoryId;
+		}
+		public void setMeekosamArgeeCategoryId(Long meekosamArgeeCategoryId) {
+			this.meekosamArgeeCategoryId = meekosamArgeeCategoryId;
+		}
+		public Long getMeekosamAnnualIncomeId() {
+			return meekosamAnnualIncomeId;
+		}
+		public void setMeekosamAnnualIncomeId(Long meekosamAnnualIncomeId) {
+			this.meekosamAnnualIncomeId = meekosamAnnualIncomeId;
+		}
+		public Long getUserId() {
+			return userId;
+		}
+		public void setUserId(Long userId) {
+			this.userId = userId;
+		}
+}
