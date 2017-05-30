b8cf1b4867496f1bde83edd221511817d7d22eca
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-30 13:03:59 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMeekosamPetitionerDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IMeekosamPetitionerDAO.java
@@ -1,10 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.MeekosamPetitioner;
 
-public interface IMeekosamPetitionerDAO extends
-		GenericDao<MeekosamPetitioner, Long> {
-
+public interface IMeekosamPetitionerDAO extends GenericDao<MeekosamPetitioner, Long> {
+	public List<Object[]> getPetitionerDetailsByCardNoMobileNoAadharNo(String cardNo,String type);
 }
