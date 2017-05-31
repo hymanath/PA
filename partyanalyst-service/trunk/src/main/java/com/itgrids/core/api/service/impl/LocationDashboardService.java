80dc9a6e781822deef89a48006a6c53c38fa035f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-05-31 12:32:56 +0000

    constituency page functionality released by - Teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/impl/LocationDashboardService.java
@@ -0,0 +1,10 @@
+package com.itgrids.core.api.service.impl;
+
+import org.apache.log4j.Logger;
+
+import com.itgrids.core.api.service.ILocationDashboardService;
+
+public class LocationDashboardService implements ILocationDashboardService{
+	private final static Logger LOG =  Logger.getLogger(LocationDashboardService.class);
+	
+}
