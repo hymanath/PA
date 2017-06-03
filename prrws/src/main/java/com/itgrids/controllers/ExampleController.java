2ad56942393d262c1c437106796b34f3c93c1dd5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-03 13:21:16 +0000

    updated by -Swadhin

--- prrws/src/main/java/com/itgrids/controllers/ExampleController.java
+++ prrws/src/main/java/com/itgrids/controllers/ExampleController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +25,8 @@ public class ExampleController {
 	@Autowired
 	private IExampleService exampleService;
 
-	@RequestMapping(value = "/getExample/{id}", method = RequestMethod.GET)
+	
+	@GetMapping(value = "/getExample/{id}")
 	public Response getExample(@PathVariable int id) {
 
 		Response response = null;
