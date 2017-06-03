b0cbdc5d43f0ade942b26be374988d908e17a3b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-03 11:27:32 +0000

    updated by-swadhin

---
+++ prrws/src/main/java/com/itgrids/service/impl/FundManagementDashboardService.java
@@ -0,0 +1,22 @@
+package com.itgrids.service.impl;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itgrids.dao.IFundSanctionDAO;
+import com.itgrids.service.IFundManagementDashboardService;
+/*
+ * Author : Swadhin K Lenka
+ * Date : 03/06/2017
+ */
+@Service
+@Transactional
+public class FundManagementDashboardService implements IFundManagementDashboardService {
+	private static final Logger LOG = Logger.getLogger(FundManagementDashboardService.class);
+	
+	@Autowired
+	private IFundSanctionDAO fundSanctionDAO;
+	
+}
