157ab2f8bcf8539ab889f6298fc1fb74edd4dd8a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-05 07:42:48 +0000

    PRRWS --Committed by Swathi

---
+++ prrws/src/main/java/com/itgrids/dto/IdNameVO.java
@@ -0,0 +1,47 @@
+package com.itgrids.dto;
+
+import java.io.Serializable;
+
+public class IdNameVO implements Serializable{
+	
+	private static final long serialVersionUID = -5436365311016546551L;
+	
+	private Long id;
+	private String name;
+	private Double total;
+	private double average;
+	private double percentage;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getTotal() {
+		return total;
+	}
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	public double getAverage() {
+		return average;
+	}
+	public void setAverage(double average) {
+		this.average = average;
+	}
+	public double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+	
+	
+}
