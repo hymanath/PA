c0fc118eb8ea5c16172e0c3919afe0e44b66337f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-07 12:29:00 +0000

    commited by raghu to get fininacal years

--- prrws/src/main/java/com/itgrids/dao/IFinancialYearDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IFinancialYearDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.FinancialYear;
 
 public interface IFinancialYearDAO extends GenericDao<FinancialYear,Long>{
+	public List<Object[]> getAllFiniancialYears();
 	
 }
