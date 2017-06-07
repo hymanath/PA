3486acc6cd4dcf055fbbdad2343cd0a2a5de5d50
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-07 06:24:05 +0000

    Dashboard Front End - Koushik

--- prrws/src/main/webapp/Assests/fundManagament/fundManagementDashboard.js
+++ prrws/src/main/webapp/Assests/fundManagament/fundManagementDashboard.js
@@ -1,25 +1,434 @@
 +function ($) {
 	onLoadCalls();
 	onLoadClicks();
+	onLoadInitialisations();
 	
 	///Please do write the onload calls in the onLoadCalls function and the clicks in the onLoadClicks and initialisation of any kind of plugin in the onLoadInitialisations
 	function onLoadCalls()
 	{
-		getLocationWiseAmountDetails();
-		getAverageFundForAnyLevel();
-		getAverageFundForScheme();
-		getTotalFunds();
-		getLocationWiseFundDetails();
+		getLocationWiseFundDetails(3,'highest','highFundDist');
+		getLocationWiseFundDetails(4,'highest','highFundCons');
+		getLocationWiseFundDetails(3,'lowest','lowFundDist');
+		getLocationWiseFundDetails(4,'lowest','lowFundCons');
+		getLocationWiseAmountDetails(2,'stateLevlOvervw');
+		getLocationWiseAmountDetails(3,'distLevlOvervw');
+		getLocationWiseAmountDetails(4,'consLevlOvervw');
+		getLocationWiseAmountDetails(5,'mandalLevlOvervw');
+		// getTotalFunds();
+		//getTotalSchemes(4,'mandalLevlOvervw');
+		// getSchemeWiseHighestAndLowestFund("highest");
+		// getSchemeWiseHighestAndLowestFund("lowest");
+		// getTotalLocationsByScopeId(4);
+		// getTotalLocationsByScopeId(5);
+		// getTotalLocationsByScopeId(6);
+		//getAverageFundForAnyLevel();
+		//getAverageFundForScheme();
 	}
 	function onLoadClicks()
 	{
 		
 	}
-	function onLoadInitialisations()   
+	function onLoadInitialisations()
 	{
 		
 	}
+	function getLocationWiseFundDetails(locationId,dataType,divId)
+	{
+		var levelValues = [];
+		var financialYrIdArr = [1,2];
+		var sourceIdsArr = [];
+		var schemeIdsArr = [];
+		var deptIdsArr = [];
+  
+		  var json = {
+			levelId : 4, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr,
+			type:dataType
+		  }
+		$.ajax({
+			url: 'getLocationWiseFundDetails',
+			data: JSON.stringify(json),
+			type: "POST",
+			dataType: 'json', 
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success: function(ajaxresp) {
+				buildLocationWiseFundDetails(ajaxresp,divId)
+			},
+			error: function(request,error) { 
+				//alert(request.responseText);
+				//alert(error);
+			}
+		});
+	}
+	function buildLocationWiseFundDetails(ajaxresp,divId)
+	{
+		var str='';
+		str+='<div class="col-sm-12">';
+			//str+='<h4 class="panel-title">High Funded District</h4>';
+			if(ajaxresp.name != null)
+			{
+				str+='<h4 class="panel-title text-capital district-name"> '+ajaxresp.name+'</h4>';
+			}
+			str+='<h3><i class="fa fa-inr"></i>: '+ajaxresp.totalAmt+'<small class="text-success">'+ajaxresp.perc+' %</small></h3>';
+			if(ajaxresp.subList.length > 0)
+			{
+				str+='<ul class="list-inline">';
+					for(var i in ajaxresp.subList)
+					{
+						str+='<li class="text-center">';
+							str+='<p class="text-muted">'+ajaxresp.subList[i].name+'</p>';
+							str+='<p class="text-success"><small>'+ajaxresp.subList[i].percentage+'%</small></p>';
+							str+='<p class="panel-title"><i class="fa fa-inr"></i>: '+ajaxresp.subList[i].total+'</p>';
+						str+='</li>';
+					}
+				str+='</ul>';
+			}
+		str+='</div>';
+		$("#"+divId).html(str);
+	}
+	function getLocationWiseAmountDetails(levelId,divId){
+		var levelValues = [];
+		var financialYrIdList = [1];
+		var json = {
+			levelId : levelId, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdList,
+			sortingType : "name",      //name,count    
+			order : "desc",   //asc,desc
+			fromDateStr : "01/06/2017",       
+			toDateStr : "10/06/2017"
+		}
+		$.ajax({
+			url : "getLocationWiseAmountDetails",  
+			data : JSON.stringify(json),
+			type : "POST",
+			dataTypa : 'json',   
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success : function(ajaxresp){
+				buildLocationWiseAmountDetails(ajaxresp,divId);
+			},
+			error : function(request,error){
+				alert(error);
+			}
+		});
+	}
+	function buildLocationWiseAmountDetails(result,divId)
+	{
+		var str='';
+		str+='<div class="scroller'+divId+'">';
+			str+='<div class="chart'+divId+'"></div>';
+		str+='</div>';
+		$("#"+divId).html(str);
+		if(result !=null && result.length>0)
+		{
+			var locationNamesArr=[];
+			var amountArr =[];
+			for(var i in result){
+				locationNamesArr.push(result[i].locationName);
+				
+				if(result[i].locationList1 !=null &&  result[i].locationList1.length>0){
+					for(var j in result[i].locationList1){
+						
+						amountArr.push(result[i].locationList1[j].amount)
+					}
+				}
+				var mainJosnObjArr=[];
+				if(amountArr != null && amountArr.length > 0){
+					mainJosnObjArr.push({name:'2014-2015',data:amountArr,color:"#FF872C"});  
+				}
+				
+			}
+			var length = result.length
+			var height = '';
+			if(length == 0)
+			{
+				height = length * 100;
+			}else if(length > 3){
+				height = length * 40;
+			}
+			if(length > 8)
+			{
+				$(".scroller"+divId).mCustomScrollbar({setHeight:'400px'})
+			}
+			$(".chart"+divId).height(height);
+			
+			$(".chart"+divId).highcharts({
+				chart: {
+					type: 'bar',
+					backgroundColor:'transparent'
+				
+				},
+				title: {
+					text: null
+				},
+				subtitle: {
+					text: null
+				},
+				xAxis: {
+					min: 0,
+					gridLineWidth: 0,
+					minorGridLineWidth: 0,
+					categories: locationNamesArr
+				},
+				yAxis: {
+						min: 0,
+						gridLineWidth: 0,
+						minorGridLineWidth: 0,
+					title: {
+						text: null
+					},
+					labels: {
+						enabled:false
+					},
+					stackLabels: {
+						//useHTML: true,
+						//align: 'left',
+						enabled: true,
+						style: {
+							fontWeight: 'bold',
+							color: (Highcharts.theme && Highcharts.theme.textColor) || '#333'
+						},
+						formatter: function() {
+						
+							//return '<span style="top:16px; position: absolute;"><br/>'+this.options.alertPerc[this.x]+'%'+' '+'('+this.total+')</span>';
+							//return this.options.alertPerc[this.x]+'%'+' '+'('+this.total+')';
+							return (this.total);
+						} 
+					
+					}
+				
+				},
+				tooltip: {
+					formatter: function () {
+					var s = '<b>' + this.x + '</b>';
+
+						$.each(this.points, function () {
+							if(this.series.name != "Series 1")  
+							s += '<br/><b style="color:'+this.series.color+'">' + this.series.name + '</b> : ' +
+							this.y/* +' - ' +
+							(Highcharts.numberFormat(this.percentage,1)+'%'); */
+						});
+
+						return s;
+					},
+					shared: true
+				},
+				plotOptions: {
+					bar: {
+						stacking: 'normal',
+						pointWidth: 30,
+						gridLineWidth: 15
+					}
+				},
+				legend: {
+					verticalAlign:'top',
+					enabled: true
+				},
+				series: mainJosnObjArr
+			});
+		}
+	}
 	
+	function getTotalFunds()
+	{
+		  var levelValues = [];
+		  var financialYrIdArr = [1,2];
+		  var sourceIdsArr = [];
+		  var schemeIdsArr = [];
+		  var deptIdsArr = [];
+		  
+		  var json = {
+			levelId : 4, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr
+		  }
+		$.ajax({
+			url: 'getTotalFunds',
+			data: JSON.stringify(json),
+			type: "POST",
+			dataType: 'json', 
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success: function(ajaxresp) {
+			
+			},
+			error: function(request,error) { 
+		
+		
+			}
+		});
+   }
+	function getSchemeWiseLocationWiseAmountDetails(){
+		var levelValues = [];
+		var financialYrIdArr = [1,2];
+		var sourceIdsArr = [];
+		var schemeIdsArr = [];
+		var deptIdsArr = [];
+
+		var json = {
+			levelId : 4, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr
+		}
+		$.ajax({
+			url : "getFinancialYearWiseScheameDetails",  
+			data : JSON.stringify(json),
+			type : "POST",
+			dataTypa : 'json',   
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success : function(ajaxresp){
+				if(ajaxresp != null && ajaxresp.length>0)
+					buildSchemewiseReport(ajaxresp);
+				else
+					alert('No data available..');
+				},
+			error : function(request,error){
+				alert(error);
+			}
+		});
+	}
+	
+	function buildSchemewiseReport(result){
+		alert("result");
+	}
+	function getTotalSchemes(levelId,divId)
+	{
+		  var levelValues = [];
+		  var financialYrIdArr = [1,2];
+		  var sourceIdsArr = [];
+		  var schemeIdsArr = [];
+		  var deptIdsArr = [];
+		  
+		  var json = {
+			levelId : levelId, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr
+		  }
+		$.ajax({
+			url: 'getTotalSchemes',
+			data: JSON.stringify(json),
+			type: "POST",
+			dataType: 'json', 
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success: function(ajaxresp) {
+				buildLocationWiseAmountDetails(ajaxresp,divId);
+			},
+			error: function(request,error) { 
+				//alert(request.responseText);
+				//alert(error);
+			}
+		});
+	}
+	function buildTotalSchemes()
+	{
+		
+	}
+	function getSchemeWiseHighestAndLowestFund(type)
+	{
+		  var levelValues = [];
+		  var financialYrIdArr = [1,2];
+		  var sourceIdsArr = [];
+		  var schemeIdsArr = [];
+		  var deptIdsArr = [];
+		  
+		  var json = {
+			levelId : 4, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr,
+			type:type
+		  }
+		$.ajax({
+			url: 'getSchemeWiseHighestAndLowestFund',
+			data: JSON.stringify(json),
+			type: "POST",
+			dataType: 'json', 
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success: function(ajaxresp) {
+			},
+			error: function(request,error) { 
+				//alert(request.responseText);
+				//alert(error);
+			}
+		});
+	}
+	function getTotalLocationsByScopeId(locScopeId)
+	{
+		  var levelValues = [];
+		  var financialYrIdArr = [1,2];
+		  var sourceIdsArr = [];
+		  var schemeIdsArr = [];
+		  var deptIdsArr = [];
+  
+		  var json = {
+			levelId : 4, 
+			levelValues : levelValues ,
+			financialYrIdList : financialYrIdArr,
+			fromDateStr : "01-06-2013",       
+			toDateStr : "10-06-2020",
+			sourceIdsList:sourceIdsArr,
+			schemeIdsList:schemeIdsArr,
+			deptIdsList:deptIdsArr
+		  }
+		$.ajax({
+			url: 'getTotalLocationsByScopeId',
+			data: JSON.stringify(json),
+			type: "POST",
+			dataType: 'json', 
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			},
+			success: function(ajaxresp) {
+			},
+			error: function(request,error) { 
+			//alert(request.responseText);
+			//alert(error);
+			}
+        });
+	}	
 	function getAverageFundForScheme(){
 		var financialYrIdList = [1]; 
 		var deptId = 0;
@@ -79,102 +488,4 @@
 			}
 		});
 	}
-	function getLocationWiseAmountDetails(){   
-		var levelValues = [];
-		var financialYrIdList = [1];
-		var json = {
-			levelId : 3, 
-			levelValues : levelValues ,
-			financialYrIdList : financialYrIdList,
-			sortingType : "name",      //name,count    
-			order : "desc",   //asc,desc
-			fromDateStr : "01/06/2017",       
-			toDateStr : "10/06/2017"
-		}
-		$.ajax({
-			url : "getLocationWiseAmountDetails",  
-			data : JSON.stringify(json),
-			type : "POST",
-			dataTypa : 'json',   
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			},
-			success : function(ajaxresp){
-				buildLocationWiseAmountDetails(ajaxresp);
-			},
-			error : function(request,error){
-				alert(error);
-			}
-		});
-	}
-	function buildLocationWiseAmountDetails(result)
-	{
-		var str='';
-		
-	}
-	function getLocationWiseFundDetails()
-	{
-		var financialYrId = 3;
-		var departmentId =0;
-		var sourceId = 0;
-		var startDate ='02/06/2017';
-		var endDate='02/06/2017';
-		var locationScopeId=4;
-		var type ='highest';
-
-		var json = {financialYrId:financialYrId,departmentId:departmentId,sourceId:sourceId,
-		startDate:startDate,endDate:endDate,locationScopeId:locationScopeId,type:type}
-		$.ajax({
-			url: 'getLocationWiseFundDetails',
-			data: JSON.stringify(json),
-			type: "POST",
-			dataType: 'json', 
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			},
-			success: function(ajaxresp) {
-				
-			},
-			error: function(request,error) { 
-				//alert(request.responseText);
-				//alert(error);
-			}
-		});
-	}
-	function buildLocationWiseFundDetails()
-	{
-		
-	}
-   function getTotalFunds()
-   {
-		var financialYrId;
-		var departmentId;
-		var sourceId;
-		var startDate;
-		var endDate;
-
-		var json = {financialYrId:financialYrId,departmentId:departmentId,sourceId:sourceId,
-		startDate:startDate,endDate:endDate}
-		$.ajax({
-			url: 'getTotalFunds',
-			data: JSON.stringify(json),
-			type: "POST",
-			dataType: 'json', 
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			},
-			success: function(ajaxresp) {
-			
-			},
-			error: function(request,error) { 
-		
-		
-			}
-		});
-   }
-
-
 }(jQuery);
--- prrws/src/main/webapp/jsp/fundManagementDashboard.jsp
+++ prrws/src/main/webapp/jsp/fundManagementDashboard.jsp
@@ -4,9 +4,11 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>FMS DASHBOARD</title>
 <link href="Assests/css/bootstrap.css" type="text/css" rel="stylesheet"/>
 <link href="Assests/css/custom.less" type="text/less" rel="stylesheet"/>
+<link href="Assests/Plugins/Scroller/jquery.mCustomScrollbar.css" type="text/less" rel="stylesheet"/>
+<script src="https://use.fontawesome.com/07d3416f74.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>
 </head>
 <body>
@@ -26,12 +28,152 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12">
+					<table class="table table-bordered table-overview">
+						<tr>
+							<td>
+								<h4 class="panel-title">High Funded District</h4>
+								<div id="highFundDist"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">High Funded Constituency</h4>
+								<div id="highFundCons"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">High Funded Mandal</h4>
+								<div id="highFundMandal"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">High Funded Village</h4>
+								<div id="highFundVillage"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">High Funded Source</h4>
+								<div id="highFundSource"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">High Funded Scheme</h4>
+								<div id="highFundScheme"></div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h4 class="panel-title">Low Funded District</h4>
+								<div id="lowFundDist"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Low Funded Costituency</h4>
+								<div id="lowFundCons"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Low Funded Mandal</h4>
+								<div id="lowFundMandal"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Low Funded Village</h4>
+								<div id="lowFundVillage"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Low Funded Source</h4>
+								<div id="highFundSource"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Low Funded Scheme</h4>
+								<div id="highFundScheme"></div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h4 class="panel-title">Average Funded District</h4>
+								<div id="avgFundDist"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Average Funded Costituency</h4>
+								<div id="avgFundCons"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Average Funded Mandal</h4>
+								<div id="avgFundMandal"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Average Funded Village</h4>
+								<div id="avgFundScheme"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Average Funded Source</h4>
+								<div id="avgFundScheme"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Average Funded Scheme</h4>
+								<div id="avgFundScheme"></div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<h4 class="panel-title">Total Funded District</h4>
+								<div id="totFundDist"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Total Funded Costituency</h4>
+								<div id="totFundCons"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Total Funded Mandal</h4>
+								<div id="totFundMandal"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Total Funded Village</h4>
+								<div id="totFundVillage"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Total Funded Source</h4>
+								<div id="totFundSource"></div>
+							</td>
+							<td>
+								<h4 class="panel-title">Total Funded Scheme</h4>
+								<div id="totFundScheme"></div>
+							</td>
+						</tr>
+					</table>
+				</div>
+				<div class="col-sm-12">
 					<div class="panel panel-default panel-black">
 						<div class="panel-heading">
-							<h4 class="panel-title"></div>
+							<h4 class="panel-title text-capital">state level overview</h4>
 						</div>
 						<div class="panel-body">
-							<button class="btn btn-success" onclick="getSchemeWiseLocationWiseAmountDetails();"> Get Scheme Data </button>
+							<div id="stateLevlOvervw"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="panel panel-default panel-black">
+						<div class="panel-heading">
+							<h4 class="panel-title text-capital">district level overview</h4>
+						</div>
+						<div class="panel-body">
+							<div id="distLevlOvervw"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="panel panel-default panel-black">
+						<div class="panel-heading">
+							<h4 class="panel-title text-capital">mandal level overview</h4>
+						</div>
+						<div class="panel-body">
+							<div id="mandalLevlOvervw"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="panel panel-default panel-black">
+						<div class="panel-heading">
+							<h4 class="panel-title text-capital">constituency level overview</h4>
+						</div>
+						<div class="panel-body">
+							<div class="consLevlOvervwScroll">
+								<div id="consLevlOvervw"></div>
+							</div>
 						</div>
 					</div>
 				</div>				
@@ -42,7 +184,11 @@
 <footer></footer>
 <script type="text/javascript" src="Assests/js/jquery-1.11.3.js"></script>
 <script type="text/javascript" src="Assests/js/bootstrap.js"></script>
+<script src="Assests/Plugins/Highcharts/highcharts.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Scroller/jquery.mCustomScrollbar.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Scroller/jquery.mousewheel.js" type="text/javascript"></script>
 <script type="text/javascript" src="Assests/fundManagament/fundManagementDashboard.js"></script>
+
 <!--Please do write the onload calls in the onLoadCalls function and the clicks in the onLoadClicks and initialisation of any kind of plugin in the onLoadInitialisations-->
 </body>
 </html>
