680d0ac7efeaa355f463120869948076a4880e91
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-09 05:30:03 +0000

    updated by-swadhin

--- prrws/src/main/java/com/itgrids/dao/IFundSanctionDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IFundSanctionDAO.java
@@ -11,7 +11,7 @@ public interface IFundSanctionDAO extends GenericDao<FundSanction,Long>{
 	public List<Long> getLocationValues(Long scopeId);
 	public List<Object[]> getFinancialYearWiseScheameDetails(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,List<Long> schemeIdsList,Date startDate,Date endDate,Long searchScopeId,List<Long> searchScopeValuesList);
 	public List<Object[]> getFinancialYearWiseFundDetails(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,List<Long> schemeIdsList,Long searchScopeId);
-	public List<Object[]> getLocationWiseAmount(Long levelId, List<Long> levelValues,Date fromDate,Date toDate,List<Long> financialYrIdList,Long deptId,Long sourceId);
+	public List<Object[]> getLocationWiseAmount(Long levelId, List<Long> levelValues,Date fromDate,Date toDate,List<Long> financialYrIdList,List<Long> deptId,List<Long> sourceId);
 	public List<Object[]> getFinancialYearWiseDeptsWiseScheameDetails(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,List<Long> schemeIdsList,Date startDate,Date endDate,Long searchScopeId,List<Long> searchScopeValuesList);
 	public List<Object[]> getLocationInfo(Long levelId, List<Long> levelValues);
 	public List<Object[]> getLocationsCountDetails(Long locatioinTypeId,List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,Long searchLevlId,List<Long> searchLvlVals);
@@ -23,10 +23,10 @@ public interface IFundSanctionDAO extends GenericDao<FundSanction,Long>{
 			List<Long> sourceIdsList,Date sDate,Date eDate,Long scopeId,Long searchLevlId,List<Long> searchLvlVals);
 	public List<Object[]> getLocationWiseGrantTypesFund(List<Long> financialYearIdsList,List<Long> deptIdsList,
 			List<Long> sourceIdsList,Date sDate,Date eDate,Long locationScopeId,Long locationVal,Long searchLevlId,List<Long> searchLvlVals);
-	public List<Object[]> getTotalFundAndCountDtls(List<Long> financialYrIdList,Long departmentId,Long sourceId,Date sDate,Date eDate,Long scopeId,String group,Long deptId);
-	public List<Object[]> getTotalFundForScheme(List<Long> financialYrIdList,Long departmentId,Long sourceId,Long schemeId,Date sDate,Date eDate,Long deptId);
+	public List<Object[]> getTotalFundAndCountDtls(List<Long> financialYrIdList,List<Long> departmentId,List<Long> sourceId,Date sDate,Date eDate,Long scopeId,String group);
+	public List<Object[]> getTotalFundForScheme(List<Long> financialYrIdList,List<Long> departmentId,List<Long> sourceId,List<Long> schemeId,Date sDate,Date eDate);
 	public Long getTotalSchemes(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,Date sDate,Date eDate,Long searchLevlId,List<Long> searchLvlVals);
 	public List<Long> getLocationBlockLevelIds(Long locationId,Long locationLevelId,Long blockLevelId);
-	public List<Object[]> getAllDistrictByStateId(Long superLocationId,List<Long> financialYrIdList,Long deptId,Long sourceId,Long locationScopeId,Date sDate,Date eDate);
-	public List<Object[]> getAllConstituencyByDistrictId(Long superLocationId,List<Long> financialYrIdList,Long deptId,Long sourceId,Long locationScopeId,Date sDate,Date eDate);
+	public List<Object[]> getAllDistrictByStateId(Long superLocationId,List<Long> financialYrIdList,List<Long> deptId,List<Long> sourceId,Long locationScopeId,Date sDate,Date eDate);
+	public List<Object[]> getAllConstituencyByDistrictId(Long superLocationId,List<Long> financialYrIdList,List<Long> deptId,List<Long> sourceId,Long locationScopeId,Date sDate,Date eDate);
 }
