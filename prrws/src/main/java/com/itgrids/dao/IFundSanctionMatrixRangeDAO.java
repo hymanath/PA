bb8ad410bf0d408b42af7b377746937c1b8fb6e7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-14 07:33:27 +0000

    updated by-swadhin

--- prrws/src/main/java/com/itgrids/dao/IFundSanctionMatrixRangeDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IFundSanctionMatrixRangeDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.FundSanctionMatrixRange;
 
 public interface IFundSanctionMatrixRangeDAO extends GenericDao<FundSanctionMatrixRange,Long> {
-
+	public List<Object[]> getFundSanctionRangeList();
 }
--- prrws/src/main/java/com/itgrids/dao/impl/FundSanctionMatrixDetailsDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/FundSanctionMatrixDetailsDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.dao.impl;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,6 +19,22 @@ public class FundSanctionMatrixDetailsDAO extends GenericDaoHibernate<FundSancti
 
 	public FundSanctionMatrixDetailsDAO() {
 		super(FundSanctionMatrixDetails.class);
+	}
+	@Override
+	public List<Object[]> getPreviousYearDtls(Long scopeId,Long previousYearId){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select fundSanctionMatrixDetails.fundSanctionMatrixRangeId, fundSanctionMatrixDetails.scopeValue "
+				+ " from FundSanctionMatrixDetails fundSanctionMatrixDetails "
+				+ " where "
+				+ " fundSanctionMatrixDetails.previousYearId = :previousYearId and "
+				+ " fundSanctionMatrixDetails.scopeId = :scopeId and "
+				+ " fundSanctionMatrixDetails.isDeleted = 'N' "
+				+ " order by "
+				+ " fundSanctionMatrixDetails.fundSanctionMatrixRangeId ");
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("scopeId", scopeId);
+		query.setParameter("previousYearId", previousYearId);
+		return query.list();
 	}
 
 }
