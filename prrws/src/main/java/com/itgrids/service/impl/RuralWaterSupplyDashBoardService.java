465602ea62bf93d0c842c5e4565004c42bc5b22b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-15 05:13:11 +0000

    rws assets -- sandeep

---
+++ prrws/src/main/java/com/itgrids/service/impl/RuralWaterSupplyDashBoardService.java
@@ -0,0 +1,12 @@
+package com.itgrids.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itgrids.service.IRuralWaterSupplyDashBoardService;
+
+@Service
+@Transactional
+public class RuralWaterSupplyDashBoardService implements IRuralWaterSupplyDashBoardService{
+
+}
