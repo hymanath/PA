8dd60df61be246f4aaa2387574fb47510abb60f0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-16 14:25:31 +0000

    swapna updated

--- prrws/src/main/java/com/itgrids/service/integration/impl/INREGSTCSService.java
+++ prrws/src/main/java/com/itgrids/service/integration/impl/INREGSTCSService.java
@@ -2,9 +2,11 @@ package com.itgrids.service.integration.impl;
 
 import java.util.List;
 
+import com.itgrids.dto.FarmPondOverviewVO;
 import com.itgrids.dto.IdNameVO;
 import com.itgrids.dto.InputVO;
 import com.itgrids.dto.LabourBudgetOverViewVO;
+import com.itgrids.dto.NregsDataVO;
 import com.itgrids.dto.NregsDataVO;
 import com.itgrids.dto.NregsOverviewVO;
 import com.itgrids.dto.NregsProjectsVO;
@@ -14,6 +16,8 @@ public interface INREGSTCSService {
 	public List<NregsProjectsVO> getNREGSProjectsOverview(InputVO inputVO);
 	public LabourBudgetOverViewVO getLabourBudgetOverview(InputVO inputVO);
 	public List<IdNameVO> getLabourBudgetExpenditure(InputVO inputVO);
+	 public FarmPondOverviewVO getFarmPondOverview(InputVO inputVO);
+	 public List<NregsDataVO> getFarmPondData(InputVO inputVO);
 	public NregsOverviewVO getNregaIHHLOverview(InputVO inputVO);
 	public List<NregsDataVO> getNregaLevelsOverviewForIHHL(InputVO inputVO);
 	public List<NregsDataVO> getNregaLevelwiseOverviewForLabourBudgetData(InputVO inputVO);
