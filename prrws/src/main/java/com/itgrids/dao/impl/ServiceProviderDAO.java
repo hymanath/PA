70a890fba68eb137f8b508d3dd9cbaeb39484975
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-22 12:43:37 +0000

    Updated by Kamalakar Dandu

--- prrws/src/main/java/com/itgrids/dao/impl/ServiceProviderDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/ServiceProviderDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.dao.impl;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.springframework.stereotype.Repository;
 
 import com.itgrids.dao.IServiceProviderDAO;
 import com.itgrids.model.ServiceProvider;
 
+@Repository
 public class ServiceProviderDAO extends GenericDaoHibernate<ServiceProvider,Long> implements IServiceProviderDAO{
 
 	public ServiceProviderDAO()
