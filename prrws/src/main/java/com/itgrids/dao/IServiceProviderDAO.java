b585d63a3836d5abe2dce079be4fed02266874ab
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-22 11:39:59 +0000

    Updated by Kamalakar Dandu

---
+++ prrws/src/main/java/com/itgrids/dao/IServiceProviderDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.model.ServiceProvider;
+
+public interface IServiceProviderDAO extends GenericDao<ServiceProvider,Long>{
+
+}
---
+++ prrws/src/main/java/com/itgrids/dao/IWebserviceCallDetailsDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.itgrids.model.WebserviceCallDetails;
+
+public interface IWebserviceCallDetailsDAO extends GenericDao<WebserviceCallDetails,Long>{
+
+}
