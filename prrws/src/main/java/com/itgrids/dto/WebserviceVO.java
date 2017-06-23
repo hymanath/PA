f30aaa35b90459c6d6a21631cc0ffe01e2cafbf4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-23 10:25:42 +0000

    Updated by Kamalakar Dandu

---
+++ prrws/src/main/java/com/itgrids/dto/WebserviceVO.java
@@ -0,0 +1,57 @@
+package com.itgrids.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class WebserviceVO implements Serializable{
+
+	private static final long serialVersionUID = -4789785769354271294L;
+	
+	private String url;
+	private Long statusCode;
+	private Date callTime;
+	private Long timeTaken;
+	
+	private String status;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Long getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(Long statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public Date getCallTime() {
+		return callTime;
+	}
+
+	public void setCallTime(Date callTime) {
+		this.callTime = callTime;
+	}
+
+	public Long getTimeTaken() {
+		return timeTaken;
+	}
+
+	public void setTimeTaken(Long timeTaken) {
+		this.timeTaken = timeTaken;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
