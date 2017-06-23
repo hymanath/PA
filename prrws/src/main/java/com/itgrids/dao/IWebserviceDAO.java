b40151bb58149c1f3d19cbdf7e097925f29a1397
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-23 10:25:26 +0000

    Updated by Kamalakar Dandu

--- prrws/src/main/java/com/itgrids/dao/IWebserviceDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IWebserviceDAO.java
@@ -1,8 +1,11 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 import com.itgrids.model.Webservice;
 
 public interface IWebserviceDAO extends GenericDao<Webservice,Long>{
 
+	public List<Long> getWebserviceIdByUrl(String url);
 }
