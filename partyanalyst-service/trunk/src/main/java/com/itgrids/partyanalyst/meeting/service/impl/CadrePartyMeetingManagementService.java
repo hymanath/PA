4962723169181853c0fa89f021f7cfc954205eea
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-27 10:16:45 +0000

    new package updated by -sai

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/meeting/service/impl/CadrePartyMeetingManagementService.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.meeting.service.impl;
+
+import com.itgrids.partyanalyst.meeting.service.ICadrePartyMeetingManagementService;
+
+public class CadrePartyMeetingManagementService implements
+		ICadrePartyMeetingManagementService {
+
+}
