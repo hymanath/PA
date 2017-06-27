6befe6a7f40347665a5d5799146db4c9bce687d4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-06-27 09:48:14 +0000

    partymeetingmanagement updated by freshers team

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/CadrePartyMeetingManagementAction.java
@@ -0,0 +1,47 @@
+package com.itgrids.partyanalyst.web.action;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.itgrids.partyanalyst.meeting.service.ICadrePartyMeetingManagementService;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+public class CadrePartyMeetingManagementAction extends ActionSupport implements
+		ServletRequestAware {
+	private static final long serialVersionUID = 1L;
+
+	private static Logger LOG = Logger
+			.getLogger(CadrePartyMeetingManagementAction.class);
+	private HttpServletRequest request;
+    private ICadrePartyMeetingManagementService cadrePartyMeetingManagementService;
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+
+	}
+
+	
+	public ICadrePartyMeetingManagementService getCadrePartyMeetingManagementService() {
+		return cadrePartyMeetingManagementService;
+	}
+
+	public void setCadrePartyMeetingManagementService(
+			ICadrePartyMeetingManagementService cadrePartyMeetingManagementService) {
+		this.cadrePartyMeetingManagementService = cadrePartyMeetingManagementService;
+	}
+
+	public String execute() {
+		cadrePartyMeetingManagementService.test();
+		return Action.SUCCESS;
+	}
+
+}
