58d8afd69337be65209a2a442f0281b646bbf7c5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-03 14:04:55 +0000

    Updated By Hyma :Default Locations Added for Overview & SchemeLevel & DeptSchemeLevel views

--- prrws/src/main/java/com/itgrids/dao/ITehsilConstituencyDAO.java
+++ prrws/src/main/java/com/itgrids/dao/ITehsilConstituencyDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.dao;
 
+import java.util.List;
+import java.util.Set;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.TehsilConstituency;
 
 public interface ITehsilConstituencyDAO extends GenericDao<TehsilConstituency,Long> {
 
+	public List<Object[]> getNonFundedLocations(Set<Long> keysList,Long searchLevelId);
 }
