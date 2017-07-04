06da5c8eb506396c75c73c29dc611b9cb67948b2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-04 06:11:32 +0000

    Alerts block for constituency page ---Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertDAO.java
@@ -187,4 +187,7 @@ public interface IAlertDAO extends GenericDao<Alert, Long> {
 	public List<Object[]> getAlertsDataForAms(Long alertId);
 	public List<Object[]> getLocationWiseAlertStatusCountsNew(Date fromDate , Date toDate,Long deptId,String year,Long locationTypeId,List<Long> locationValues,
  			Long searchLevelId,List<Long> searchLevelValues);
+	public List<Object[]> getAlertImpactLevelWiseDetailsForConstituencyInfo(Date fromDate , Date toDate,Long constituencyId,List<Long> alertTypeIds);
+	public List<Object[]> getAlertStatusWiseDetailsForConstituencyInfo(Date fromDate , Date toDate,Long constituencyId,List<Long> alertTypeIds);
+	public List<Object[]> getTotalAlertDetailsCount(Date fromDate,Date toDate,Long constituencyId,List<Long> alertTypeIds);
 }
