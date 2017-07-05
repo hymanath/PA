7cd0bce6f6d500e4011d63ac9168ab7f0426d65f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-05 10:00:06 +0000

    updated by srujana

---
+++ prrws/src/main/java/com/itgrids/dao/IPrConstituencyDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PrConstituency;
+
+public interface IPrConstituencyDAO extends GenericDao<PrConstituency, Long> {
+
+}
---
+++ prrws/src/main/java/com/itgrids/model/PrDistrict.java
@@ -0,0 +1,41 @@
+package com.itgrids.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "district_code")
+public class PrDistrict implements Serializable {
+	private Long prDistrictId;
+	private String districtName;
+	private String districtCode;
+	@Id
+	@Column(name="pr_district_id")
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	public Long getPrDistrictId() {
+		return prDistrictId;
+	}
+	public void setPrDistrictId(Long prDistrictId) {
+		this.prDistrictId = prDistrictId;
+	}
+	@Column(name="district_name")
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	@Column(name="district_code")
+	public String getDistrictCode() {
+		return districtCode;
+	}
+	public void setDistrictCode(String districtCode) {
+		this.districtCode = districtCode;
+	}
+	
+}
