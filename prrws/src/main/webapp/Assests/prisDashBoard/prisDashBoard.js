5df612dad12213438f547d33fbe031246435380a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-06 14:00:43 +0000

    Teja Pris Dashboard Services--updated by

--- prrws/src/main/webapp/Assests/prisDashBoard/prisDashBoard.js
+++ prrws/src/main/webapp/Assests/prisDashBoard/prisDashBoard.js
@@ -1,6 +1,8 @@
 var globalFromDate = moment().subtract(1,'month').startOf("month").format('DD-MM-YYYY');
 var globalToDate = moment().format('DD-MM-YYYY');
 var blockNameArr=[{name:'District',id:'3'},{name:'Constituency',id:'4'},{name:'Mandal',id:'5'}]
+var spinner = '<div class="row"><div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div></div>';
+buildLevelWiseDetailsBlock();
 var maxHeight = 0;
 $(".blockHeights").each(function(){
    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
@@ -25,15 +27,34 @@ $("#singleDateRangePicker").daterangepicker({
 		  format: 'DD-MM-YYYY'
 		}
 });
+$("header").on("click",".menu-cls",function(e){
+		e.stopPropagation();
+		$(".menu-data-cls").toggle();
+	});
+	$(document).on("click",function(){
+		$(".menu-data-cls").hide();
+	});
+
+$('#singleDateRangePicker').on('apply.daterangepicker', function(ev, picker) {
+	
+	
+	globalFromDate = picker.startDate.format('DD-MM-YYYY')
+	globalToDate = picker.endDate.format('DD-MM-YYYY')
+	
+	buildLevelWiseDetailsBlock();
+});
 $(document).on('click','.calendar_active_cls li', function(){
-	var date = $(".calendar_active_cls li.active").html();
+	var date = $(this).attr("attr_val");
+	
 	if(date == 'Today')
 	{
 		globalFromDate = moment().format('DD-MM-YYYY');
 		globalToDate = moment().format('DD-MM-YYYY');
+		
 	}else if(date == 'Week'){
 		globalFromDate = moment().subtract(1,'week').format('DD-MM-YYYY');
 		globalToDate = moment().format('DD-MM-YYYY');
+		
 	}else if(date == 'Month'){
 		globalFromDate = moment().subtract(1,'month').startOf("month").format('DD-MM-YYYY');
 		globalToDate = moment().format('DD-MM-YYYY');
@@ -50,8 +71,39 @@ $(document).on('click','.calendar_active_cls li', function(){
 	
 	$(this).closest("ul").find("li").removeClass("active");
 	$(this).addClass("active");
+	if(date != "custom"){
+		buildLevelWiseDetailsBlock();
+	}
+	
 });
-buildLevelWiseDetailsBlock();
+
+function getDistrictOverview(blockId,blockName){
+	$("#"+blockName+'_'+blockId).html(spinner);
+		var json = {
+			fromDate:globalFromDate,
+			toDate:globalToDate,
+			locationId:0,
+			locationType:"district"
+			}
+		$.ajax({                
+			type:'POST',    
+			url: 'getPIRSSurveyInfo',
+			dataType: 'json',
+			data : JSON.stringify(json),
+			beforeSend :   function(xhr){
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			}
+		}).done(function(result){
+			$("#"+blockName+'_'+blockId).html('');
+			if(result !=null && result.subList !=null && result.subList.length>0){
+				buildTableData(result,blockId,blockName);
+			}else{
+				$("#"+blockName+'_'+blockId).html("No Data Available");
+			}
+		});
+	}
+
 function buildLevelWiseDetailsBlock(){
 	var collapse='';
 	if(blockNameArr !=null){
@@ -79,7 +131,7 @@ function buildLevelWiseDetailsBlock(){
 									}
 									
 										collapse+='<div class="panel-body">';
-											collapse+='<div id="'+blockNameArr[i].name+'_'+blockNameArr[i].id+'">ddd</div>';
+											collapse+='<div id="'+blockNameArr[i].name+'_'+blockNameArr[i].id+'"></div>';
 										collapse+='</div>';
 									collapse+='</div>';
 								collapse+='</div>';
@@ -89,41 +141,70 @@ function buildLevelWiseDetailsBlock(){
 				collapse+='</div>';
 			$("#levelWiseBlockId").html(collapse);
 			for(var i in blockNameArr){
-				buildTableData();
+				if(blockNameArr[i].id == 3){
+					getDistrictOverview(blockNameArr[i].id,blockNameArr[i].name);
+				}else if(blockNameArr[i].id == 4){
+					getConstituencyOverview(blockNameArr[i].id,blockNameArr[i].name);
+				}else if(blockNameArr[i].id == 5){
+					getMandalOverview(blockNameArr[i].id,blockNameArr[i].name);
+				}	
+				
 			}
 	}
 }
-
-function buildTableData(){
-	for(var i in blockNameArr){
-	var tableView='';
+function buildTableData(result,blockId,blockName){
+	
+		
+		var tableView='';
 		tableView+='<div class="row">';
 			tableView+='<div class="col-sm-12">';
-					tableView+='<table class="table ">';
+				
+					tableView+='<table class="table dataTable'+blockId+' ">';
 							tableView+='<thead>';
+							if(blockId == 3){
 								tableView+='<th>District</th>';
+							}else if(blockId == 4){
+								tableView+='<th>Constituency</th>';
+							}else if(blockId == 5){
+								tableView+='<th>Mandal</th>';
+							}
+								
 								tableView+='<th><img class="img_widthTable" src="Assests/icons/house_icon.png" alt="house_icon"><br/>TOTAL</th>';
 								tableView+='<th><img class="img_widthTable" src="Assests/icons/Target_icon.png" alt="Target_icon"><br/>TARGET</th>';
 								tableView+='<th><img class="img_widthTable" src="Assests/icons/Achived_icon.png" alt="Achived_icon"><br/>ACHIVED</th>';
 								tableView+='<th><img class="img_widthTable" src="Assests/icons/Achived_%_Icon.png" alt="Achived_icon"><br/>ACHIVED %</th>';
 							tableView+='</thead>';
-						tableView+='</table>';
+							
+							tableView+='<tbody>';
+								for(var i in result.subList){
+								tableView+='<tr>';
+									tableView+='<td>'+result.subList[i].name+'</td>';
+									tableView+='<td>'+result.subList[i].total+'</td>';
+									tableView+='<td>'+result.subList[i].target+'</td>';
+									tableView+='<td>'+result.subList[i].achieved+'</td>';
+									tableView+='<td>'+result.subList[i].achievedPercentage+'</td>';
+								tableView+='</tr>';
+								}
+							tableView+='</tbody>';
+							tableView+='</table>';
 					tableView+='</div>';
 			tableView+='</div>';
 		tableView+='</div>';
-		
-		$("#"+blockNameArr[i].name+'_'+blockNameArr[i].id).html(tableView)
-	}	
+		$("#"+blockName+'_'+blockId).html(tableView);
+		if(blockId == '4' || blockId == '5'){
+			$(".dataTable"+blockId).dataTable();
+			}
+	
 }
-
-
-getDistrictOverview();
-function getDistrictOverview(){
+function getConstituencyOverview(blockId,blockName){
+		$("#"+blockName+'_'+blockId).html(spinner);
 		var json = {
-			fromDate:"07-05-2016",
-			toDate:"07-05-2017",
+			fromDate:globalFromDate,
+			toDate:globalToDate,
 			locationId:0,
-			locationType:"district"
+			locationType:"constituency",
+			filterId:0,
+			filterType:"district"
 			}
 		$.ajax({                
 			type:'POST',    
@@ -135,35 +216,21 @@ function getDistrictOverview(){
 				xhr.setRequestHeader("Content-Type", "application/json");
 			}
 		}).done(function(result){
+			$("#"+blockName+'_'+blockId).html('');
+			if(result !=null && result.subList !=null && result.subList.length>0){
+				buildTableData(result,blockId,blockName);
+			}else{
+				$("#"+blockName+'_'+blockId).html("No Data Available");
+			}
 			
 		});
 	}
-	getConstituencyOverview();
-	function getConstituencyOverview(){
+
+	function getMandalOverview(blockId,blockName){
+		$("#"+blockName+'_'+blockId).html(spinner);
 		var json = {
-			fromDate:"07-05-2016",
-			toDate:"07-05-2017",
-			locationId:0,
-			locationType:"constituency"
-			}
-		$.ajax({                
-			type:'POST',    
-			url: 'getPIRSSurveyInfo',
-			dataType: 'json',
-			data : JSON.stringify(json),
-			beforeSend :   function(xhr){
-				xhr.setRequestHeader("Accept", "application/json");
-				xhr.setRequestHeader("Content-Type", "application/json");
-			}
-		}).done(function(result){
-			
-		});
-	}
-	getMandalOverview();
-	function getMandalOverview(){
-		var json = {
-			fromDate:"07-05-2016",
-			toDate:"07-05-2017",
+			fromDate:globalFromDate,
+			toDate:globalToDate,
 			locationId:447,
 			locationType:"mandal"
 			}
@@ -177,7 +244,12 @@ function getDistrictOverview(){
 				xhr.setRequestHeader("Content-Type", "application/json");
 			}
 		}).done(function(result){
-			
+			$("#"+blockName+'_'+blockId).html('');
+			if(result !=null && result.subList !=null && result.subList.length>0){
+				buildTableData(result,blockId,blockName);
+			}else{
+				$("#"+blockName+'_'+blockId).html("No Data Available");
+			}
 		});
 	}
 	getBasicCountBlock();
@@ -198,6 +270,17 @@ function getDistrictOverview(){
 				xhr.setRequestHeader("Content-Type", "application/json");
 			}
 		}).done(function(result){
-			
+			buildBasicBlock(result);
 		});
 	}
+	function buildBasicBlock(result){
+		$("#totalHouseHolds").append(result.totalHouseHolds);
+		$("#targetOverall").append(result.targetOverall);
+		$("#targetOverallPercent").append(result.targetOverallPercent);
+		$("#achievedOverall").append(result.achievedOverall);
+		$("#achievedOverallpercent").append(result.achievedOverallpercent);
+		$("#subTarget").append(result.subTarget);
+		$("#subTargetPercentage").append(result.subTargetPercentage);
+		$("#subAchieved").append(result.achievedOverall);
+		$("#subAchievedPercentage").append(result.achievedOverallpercent);
+}
