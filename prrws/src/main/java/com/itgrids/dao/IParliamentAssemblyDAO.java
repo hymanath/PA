759542f9d6b6d851425bea75e9f9ac9b49ed0c27
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-07 15:54:22 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/dao/IParliamentAssemblyDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IParliamentAssemblyDAO.java
@@ -10,5 +10,34 @@ public interface IParliamentAssemblyDAO extends GenericDao<ParliamentAssembly,Lo
 	public List<Object[]> getParliamentIdAndName(Long districtId);
 	public List<Object[]> getParliamentByConstIdAndName(Long parliamentId);
 	public List<Object[]> getConsParlimentIds();
+	public List<Object[]> getFMSLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getRWSLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getMGNREGSLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getPRISLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getENCLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromPRISLocationId(String searchLevelValue);
 	
+	public List<Object[]> getFMSLocationIdfromFMSLocationId(String searchLocationValue);
+	public List<Object[]> getRWSLocationIdfromRWSLocationId(String searchLocationValue);
+	public List<Object[]> getMGNREGSLocationIdfromMGNREGSLocationId(String searchLocationValue);
+	public List<Object[]> getPRISLocationIdfromPRISLocationId(String searchLocationValue);
+	public List<Object[]> getENSLocationIdfromENSLocationId(String searchLocationValue);
 }
--- prrws/src/main/java/com/itgrids/dao/ITehsilDAO.java
+++ prrws/src/main/java/com/itgrids/dao/ITehsilDAO.java
@@ -10,4 +10,35 @@ public interface ITehsilDAO extends GenericDao<Tehsil,Long>  {
 
 	public List<Object[]> getTehsilIdAndNameByIds(List<Long> tehsilIds);
 	public List<Object[]> getTehsilsForConstituency(Long constId);
+	
+	public List<Object[]> getFMSLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getFMSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getRWSLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getRWSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getMGNREGSLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromPRISLocationId(String searchLevelValue);
+	public List<Object[]> getMGNREGSLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getPRISLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getPRISLocationIdfromENCLocationId(String searchLevelValue);
+
+	public List<Object[]> getENCLocationIdfromFMSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromRWSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromMGNREGSLocationId(String searchLevelValue);
+	public List<Object[]> getENCLocationIdfromPRISLocationId(String searchLevelValue);
+	
+	public List<Object[]> getFMSLocationIdfromFMSLocationId(String searchLocationValue);
+	public List<Object[]> getRWSLocationIdfromRWSLocationId(String searchLocationValue);
+	public List<Object[]> getMGNREGSLocationIdfromMGNREGSLocationId(String searchLocationValue);
+	public List<Object[]> getPRISLocationIdfromPRISLocationId(String searchLocationValue);
+	public List<Object[]> getENSLocationIdfromENSLocationId(String searchLocationValue);
 }
