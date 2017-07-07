cfffba606b2c050e40df7b6fe871fdf888c91f99
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-07 15:54:48 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/service/IUserService.java
+++ prrws/src/main/java/com/itgrids/service/IUserService.java
@@ -1,11 +1,10 @@
 package com.itgrids.service;
 
+import com.itgrids.dto.AddressVO;
 import com.itgrids.dto.UserVO;
 
 public interface IUserService { 
 	
 	public UserVO userAuthentication(String userName,String password);
-	public String getAssignedSearchIdByTypeId( Long searchLevelId,Long searchLevelValue,String fromPage,String toPage);
-	
-
+	public AddressVO getOriginalLocationIdForTempId( Long searchLevelId,String searchLevelValue,String fromPage,String toPage);
 }
