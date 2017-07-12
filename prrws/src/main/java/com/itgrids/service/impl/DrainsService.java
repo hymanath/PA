9a2f773015e42e19c966be5dc0a53c0208eff1d5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-12 10:41:19 +0000

    updated by swapna

--- prrws/src/main/java/com/itgrids/service/impl/DrainsService.java
+++ prrws/src/main/java/com/itgrids/service/impl/DrainsService.java
@@ -1,5 +1,13 @@
 package com.itgrids.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONArray;
@@ -14,6 +22,12 @@ import com.itgrids.service.IDrainsService;
 import com.itgrids.utils.CommonMethodsUtilService;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+
+import jdk.nashorn.internal.scripts.JO;
+
+
+import jdk.nashorn.internal.scripts.JO;
+
 
 @Service
 @Transactional
@@ -31,9 +45,78 @@ public class DrainsService implements IDrainsService {
 	/*
 	 * Date : 11/07/2017
 	 * Author :Swapna
-	 * @description : getPIRSSurveyInfo
+	 * @description : getDrainsInfobyLocation
 	 */
 
+	@Override
+
+	public List<DrainsVO> getDrainsInfobyLocations(InputVO inputVO) {	
+		List<DrainsVO> drainsVOList= new ArrayList<DrainsVO>();
+		List<DrainsVO> totalOverViewList=new ArrayList<DrainsVO>();
+		List<DrainsVO> list=new ArrayList<DrainsVO>();
+	
+		List<DrainsVO> finalList = new ArrayList<DrainsVO>();	
+
+		List<DrainsVO> defaultList = new ArrayList<DrainsVO>();	
+		//List<Object> allVO=new ArrayList<Object>();
+		
+		try {
+			WebResource webResource = commonMethodsUtilService.getWebResourceObject("http://45.114.245.209/api/drains/?getDrainsInfobyLocation=true&locationId="+inputVO.getLocationId()+"&locationType="+inputVO.getLocationType()+"&fromDate="+inputVO.getFromDate()+"&toDate="+inputVO.getToDate());
+	        
+	        ClientResponse response = webResource.accept("application/json").type("application/json").get(ClientResponse.class);
+		    
+	        if(response.getStatus() != 200){
+	        	throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
+	 	    }else{
+	 	    	 String output = response.getEntity(String.class);
+	 	    	 if(output != null && !output.isEmpty()){
+	 	    		JSONArray finalArray = new JSONArray(output);
+	 	    		if(finalArray!=null && finalArray.length()>0){
+	 	    			for(int i=0;i<finalArray.length();i++){
+	 	    				JSONObject jObj = (JSONObject) finalArray.get(i);
+                            
+	 	    				DrainsVO drainsVO=new DrainsVO(); //MainLocation VO
+	 	    				drainsVO.setId(jObj.getLong("id")); 
+	 	    				drainsVO.setName(jObj.getString("name")); 	
+	 	    				   
+	 	    				   DrainsVO kachaVO=new DrainsVO();
+	 	    				   kachaVO.setKacha(jObj.getLong("kacha"));
+		 	    			   kachaVO.setKachaCleaned(jObj.getLong("kachaCleaned"));
+		 	    			   kachaVO.setKachaCleanedKM(jObj.getDouble("kachhaKM"));
+		 	    			   kachaVO.setKachaPercentage(jObj.getDouble("kachaPercentage"));
+		 	    			   kachaVO.setKachaCleanedKM(jObj.getDouble("kachaCleanedKM"));
+		 	    			    
+		 	    			   DrainsVO pakkaVO=new DrainsVO();
+		 	    			   pakkaVO.setPakka(jObj.getLong("pakka"));
+		 	    			   pakkaVO.setPakkaCleaned(jObj.getLong("pakkaCleaned"));
+		 	    			   pakkaVO.setPakkaCleanedKM(jObj.getDouble("pakkaCleanedKM"));
+		 	    			   pakkaVO.setPakkaPercentage(jObj.getDouble("pakkaPercentage"));
+		 	    			   pakkaVO.setPakkaKM(jObj.getDouble("pakkaKM"));
+		 	    			    
+		 	    			   DrainsVO underGroundVO =new DrainsVO();
+		 	    			   underGroundVO.setUnderground(jObj.getLong("underground"));
+		 	    			   underGroundVO.setUndergroundCleaned(jObj.getLong("undergroundCleaned"));
+		 	    			   underGroundVO.setUndergroundCleanedKM(jObj.getDouble("undergroundCleanedKM"));
+		 	    			   underGroundVO.setUndergroundPercentage(jObj.getDouble("undergroundPercentage"));
+		 	    			   underGroundVO.setUndergroundKM(jObj.getDouble("undergroundKM"));
+	                            
+		 	    			   defaultList.add(kachaVO);
+		 	    			   defaultList.add(pakkaVO);
+		 	    			   defaultList.add(underGroundVO);                            
+	                           drainsVO.getTotalList().addAll(defaultList);                          
+	                           finalList.add(drainsVO);
+		 	    			        			  
+		 	    			    
+	 	    			}
+	 	    		}
+	 	    	 }
+	 	    }
+
+	 	    			} catch (Exception e) {
+	 	    			 	   LOG.error("Exception raised at getDrainsInfobyLocation - DrainsService service", e);
+	 	    			}
+		return  finalList;	
+	}
 	@Override
 	public DrainsVO getDrainsInfobyLocation(InputVO inputVO) {
 		DrainsVO drainsVO= new DrainsVO();
@@ -102,6 +185,62 @@ public class DrainsService implements IDrainsService {
 	    return (double) tmp / factor;
 	}
 }
+		 	    			    			        			    
+	 	    				
+	 	    				/*drainsVO.setKacha(jObj.getLong("kacha"));
+	 	    				drainsVO.setKachhaKM(jObj.getDouble("kachhaKM"));
+	 	    				drainsVO.setKachaCleaned(jObj.getLong("kachaCleaned"));
+	 	    				drainsVO.setKachaCleanedKM(jObj.getDouble("kachaCleanedKM"));
+	 	    				drainsVO.setKachaPercentage(jObj.getDouble("kachaPercentage"));
+	 	    				drainsVO.setPakka(jObj.getLong("pakka") );
+	 	    				drainsVO.setPakkaKM(jObj.getDouble("pakkaKM"));
+	 	    				drainsVO.setPakkaCleaned(jObj.getLong("pakkaCleaned"));
+	 	    			    drainsVO.setPakkaCleanedKM(jObj.getDouble("pakkaCleanedKM"));
+	 	    			    drainsVO.setPakkaPercentage(jObj.getDouble("pakkaPercentage"));
+	 	    			    drainsVO.setUnderground(jObj.getLong("underground"));
+	 	    			    drainsVO.setUndergroundCleaned(jObj.getLong("undergroundCleaned"));
+	 	    			    drainsVO.setUndergroundCleanedKM(jObj.getDouble("undergroundKM"));
+	 	    			    drainsVO.setUndergroundPercentage(jObj.getDouble("undergroundPercentage"));	 
+	 	    			    drainsVO.setUndergroundCleanedKM(jObj.getDouble("undergroundCleanedKM"));
+	 	    				DrainsVO drainsVO=new DrainsVO(); 
+	 	    				drainsVO.setId(jObj.getLong("id")); 
+	 	    				drainsVO.setName(jObj.getString("name"));
+	 	    				
+	 	    				List<DrainsVO> volist = new ArrayList<DrainsVO>();
+	 	    				
+	 	    				drainsVO.setKacha(jObj.getLong("kacha"));
+	 	    				drainsVO.setKachhaKM(jObj.getDouble("kachhaKM"));
+	 	    				drainsVO.setKachaCleaned(jObj.getLong("kachaCleaned"));
+	 	    				drainsVO.setKachaCleanedKM(jObj.getDouble("kachaCleanedKM"));
+	 	    				drainsVO.setKachaPercentage(jObj.getDouble("kachaPercentage"));
+	 	    				drainsVO.setPakka(jObj.getLong("pakka") );
+	 	    				drainsVO.setPakkaKM(jObj.getDouble("pakkaKM"));
+	 	    				drainsVO.setPakkaCleaned(jObj.getLong("pakkaCleaned"));
+	 	    			    drainsVO.setPakkaCleanedKM(jObj.getDouble("pakkaCleanedKM"));
+	 	    			    drainsVO.setUnderground(jObj.getLong("underground"));
+	 	    			    drainsVO.setUndergroundCleaned(jObj.getLong("undergroundCleaned"));
+	 	    			    drainsVO.setUndergroundCleanedKM(jObj.getDouble("undergroundKM"));
+	 	    			    drainsVO.setUndergroundPercentage(jObj.getDouble("undergroundPercentage"));
+	 	    			    
+
+	 	    				drainsVO.setKachaAvailable(!jObj.getString("kacha").equalsIgnoreCase("null") ? drainsVO.getKachaAvailable()+jObj.getLong("kacha") : drainsVO.getKachaAvailable());
+	 	    				drainsVO.setKachaCleaned(!jObj.getString("kachaCleaned").equalsIgnoreCase("null") ? drainsVO.getKachaCleaned()+jObj.getLong("kachaCleaned") : drainsVO.getKachaCleaned());
+	 	    				drainsVO.setKachaAvailableKms(!jObj.getString("kachhaKM").equalsIgnoreCase("null") ? round(drainsVO.getKachaAvailableKms()+jObj.getDouble("kachhaKM"),2) : drainsVO.getKachaAvailableKms());
+	 	    				drainsVO.setKachaCleanedKM(!jObj.getString("kachaCleanedKM").equalsIgnoreCase("null") ? round(drainsVO.getKachaCleanedKM()+jObj.getDouble("kachaCleanedKM"),2) : drainsVO.getKachaCleanedKM());
+	 	    			    //drainsVO.setKachaPercentage(!jObj.getString("kachaPercentage").equalsIgnoreCase("null") ? round(drainsVO.getKachaPercentage()+jObj.getDouble("kachaPercentage"),2) : drainsVO.getKachaPercentage());
+	 	    			  
+	 	    			    drainsVO.setCleaned(jObj.getLong("cleaned"));
+	 	    			    drainsVO.setCleanedKM(jObj.getDouble("cleanedKM"));
+	 	    			    drainsVO.setAvailability(jObj.getLong("availability"));
+	 	    			    drainsVO.setAvailabilityKM(jObj.getDouble("availabilityKM"));
+	 	    			    drainsVO.setPercentage(jObj.getDouble("percentage"));	*/
+
+
+
+
+
+
+
 	
 
 
