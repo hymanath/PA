5acd52ce6a32f1607af267107ca5c096b10dd5c3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-13 07:46:04 +0000

    Updated By Hyma : In Election Booth Committees Page Retrieving BoothInchargeRoles .

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/BoothReportAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/BoothReportAction.java
@@ -11,6 +11,7 @@ import org.json.JSONObject;
 
 import com.itgrids.partyanalyst.dto.BoothAddressVO;
 import com.itgrids.partyanalyst.dto.BoothInchargeDetailsVO;
+import com.itgrids.partyanalyst.dto.IdAndNameVO;
 import com.itgrids.partyanalyst.dto.InputVO;
 import com.itgrids.partyanalyst.service.IBoothDataValidationService;
 import com.opensymphony.xwork2.Action;
@@ -28,7 +29,15 @@ public class BoothReportAction extends ActionSupport implements ServletRequestAw
 	private List<BoothInchargeDetailsVO> boothInchargeDetailsList;
 	private IBoothDataValidationService boothDataValidationService;
 	private List<BoothAddressVO> boothDtlsList;
+	private List<IdAndNameVO> idAndNameVOList;
 	
+	
+	public List<IdAndNameVO> getIdAndNameVOList() {
+		return idAndNameVOList;
+	}
+	public void setIdAndNameVOList(List<IdAndNameVO> idAndNameVOList) {
+		this.idAndNameVOList = idAndNameVOList;
+	}
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 		
@@ -140,6 +149,18 @@ public class BoothReportAction extends ActionSupport implements ServletRequestAw
 			boothDtlsList = boothDataValidationService.getLocationLevelWiseBoothDetails(inputVO);
 		}catch(Exception e){
 			LOG.error("Exception raised at getLocationLevelWiseBoothDetails() method of BoothReportAction", e);
+		}
+		return Action.SUCCESS;
+	}
+	
+	public String getBoothInchargeRoles(){
+		LOG.info("Entered into getLocationLevelWiseBoothCount()  of BoothReportAction ");
+		try{
+			jObj = new JSONObject(getTask());
+			Long boothId = jObj.getLong("boothId");
+			idAndNameVOList = boothDataValidationService.getBoothInchargeRoles(boothId);
+		}catch(Exception e){
+			LOG.error("Exception raised at getBoothInchargeRoles() method of BoothReportAction", e);
 		}
 		return Action.SUCCESS;
 	}
