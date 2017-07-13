7528df4085a0604b4d63a9d1a1cd3202af34cdcf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-13 13:06:13 +0000

    UPDATED BY SRINU model created

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeCommitteeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.BoothInchargeCommittee;
+
+public interface IBoothInchargeCommitteeDAO extends GenericDao<BoothInchargeCommittee, Long>{
+
+}
