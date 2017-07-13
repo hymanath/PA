52b8c77cb95871e9b18a748d6aaefcf927e11bf9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-13 11:38:44 +0000

    MGNREGA Service Modifications -- SRAVANTH.

--- prrws/src/main/java/com/itgrids/dao/impl/PrConstituencyDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PrConstituencyDAO.java
@@ -1,10 +1,12 @@
 package com.itgrids.dao.impl;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.springframework.stereotype.Repository;
 
 import com.itgrids.dao.IPrConstituencyDAO;
 import com.itgrids.model.PrConstituency;
 
+@Repository
 public class PrConstituencyDAO extends GenericDaoHibernate<PrConstituency, Long> implements IPrConstituencyDAO {
 	public PrConstituencyDAO(){
 		super(PrConstituency.class);
