c2ce527427e81ccf1dd1cc0dfb3869b1aedc0239
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-14 14:28:05 +0000

    Query And Service Changed Based on db changed by -- Santosh

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeSerialNoRangeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeSerialNoRangeDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 
 import com.itgrids.partyanalyst.model.BoothInchargeSerialNoRange;
 public interface IBoothInchargeSerialNoRangeDAO extends GenericDao<BoothInchargeSerialNoRange, Long> {
-
+	public List<Object[]> getAllInchargeSerialNoRange();
 }
