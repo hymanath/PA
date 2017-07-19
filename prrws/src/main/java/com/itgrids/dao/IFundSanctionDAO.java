384d74792b18f53a6f98eb543f3d5d0164a352e4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-19 11:23:05 +0000

    updated by-swadhin

--- prrws/src/main/java/com/itgrids/dao/IFundSanctionDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IFundSanctionDAO.java
@@ -36,7 +36,7 @@ public interface IFundSanctionDAO extends GenericDao<FundSanction,Long>{
 	public List<Object[]> getFundSactionCount(List<Long> financialYrIdList);
 	public List<Object[]> getGrantTypeHighestAndLowestFund(List<Long> financialYrIdList, List<Long> deptIdsList,List<Long> sourceIdsList, Date startDate, Date endDate,
 	String type, Long searchLevelId, List<Long> searchLvlVals);
-	public List<Object[]> financialYearWiseFundDetails(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,List<Long> schemeIdsList,Long searchScopeId);
+	public List<Object[]> financialYearWiseFundDetails(List<Long> financialYearIdsList,List<Long> deptIdsList,List<Long> sourceIdsList,List<Long> schemeIdsList,Long searchScopeId,Long searchLevelId, Long searchLevelValue);
 	public List<Object[]> getAllConstituencyByDistrictIds(Long superLocationId,List<Long> financialYrIdList,List<Long> deptIdList,List<Long> sourceIdList,Date sDate,Date eDate,Long scopeId);
 	public List<Object[]> getAllConstituencyByParliamentConstId(Long superLocationId,List<Long> financialYrIdList,List<Long> deptIdList,List<Long> sourceIdList,Date sDate,Date eDate,Long scopeId,Long superLocationLevelId);
 	public List<Object[]> getAllParliamentByStateId(Long superLocationId,List<Long> financialYrIdList,List<Long> deptIdList,List<Long> sourceIdList,Date sDate,Date eDate,Long scopeId);
