0b71db2394a433c2fa329a65ff85178c32e9a09f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-07-21 08:10:22 +0000

    MGNregs Consolidated Module Service Commit by nandhini

--- prrws/src/main/java/com/itgrids/service/integration/NREGSConsolidatedService.java
+++ prrws/src/main/java/com/itgrids/service/integration/NREGSConsolidatedService.java
@@ -1,16 +1,32 @@
 package com.itgrids.service.integration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.itgrids.dao.IConvergenceTypeDAO;
+import com.itgrids.dao.INregaComponentDAO;
+import com.itgrids.dto.IdNameVO;
+import com.itgrids.dto.NregaConsolidatedInputVO;
 import com.itgrids.service.integration.impl.INREGSConsolidatedService;
+import com.itgrids.utils.CommonMethodsUtilService;
 
 @Service
 @Transactional
 public class NREGSConsolidatedService implements INREGSConsolidatedService{
 
 	private static final Logger LOG = Logger.getLogger(NREGSConsolidatedService.class);
+	
+	@Autowired
+	private CommonMethodsUtilService commonMethodsUtilService;
+	@Autowired
+	private IConvergenceTypeDAO convergenceTypeDAO;
+	@Autowired
+	private INregaComponentDAO nregaComponentDAO;
 	
 	/*
 	 * Date : 21/07/2017
@@ -19,4 +35,50 @@ public class NREGSConsolidatedService implements INREGSConsolidatedService{
 	 * 
 	 */
 	//public List<NregaConsolidatedDataVO> getNREGSLevelWiseConsolidatedReport
+	
+	/*
+	 * Date : 21/7/17
+	 * Author : Nandhini.k
+	 * @description : getting All Convergence Types
+	 */
+	public List<IdNameVO> getAllConvergenceTypes(){
+		List<IdNameVO> finalList = new ArrayList<IdNameVO>(0);
+		try{
+			List<Object[]> convergenceList = convergenceTypeDAO.getAllConvergenceTypes();
+			if(commonMethodsUtilService.isListOrSetValid(convergenceList)){
+				for (Object[] objects : convergenceList) {
+					IdNameVO vo = new IdNameVO();
+					vo.setId(commonMethodsUtilService.getLongValueForObject(objects[0]));
+					vo.setName(commonMethodsUtilService.getStringValueForObject(objects[1]));
+					finalList.add(vo);
+				}
+			}
+		}catch (Exception e){
+			LOG.error("Exception raised at getAllConvergenceTypes - NREGSConsolidatedService", e);
+		}
+		return finalList;
+	}
+	
+	/*
+	 * Date : 21/7/17
+	 * Author : Nandhini.k
+	 * @description : getting Component(IHHL OR Labor Budget ETC) By Passing ConvergenceTypeId 
+	 */
+	public List<IdNameVO> getComponentByConvergType(NregaConsolidatedInputVO nregaConsolidatedInputVO){
+		List<IdNameVO> finalList = new ArrayList<IdNameVO>(0);
+		try{
+			List<Object[]> componentList = nregaComponentDAO.getComponentByConvergType(nregaConsolidatedInputVO.getConvergenceTypeId());
+			if(commonMethodsUtilService.isListOrSetValid(componentList)){
+				for (Object[] objects : componentList) {
+					IdNameVO vo = new IdNameVO();
+					vo.setId(commonMethodsUtilService.getLongValueForObject(objects[0]));
+					vo.setName(commonMethodsUtilService.getStringValueForObject(objects[1]));
+					finalList.add(vo);
+				}
+			}
+		}catch (Exception e){
+			LOG.error("Exception raised at getComponentByConvergType - NREGSConsolidatedService", e);
+		}
+		return finalList;
+	}
 }
--- prrws/src/main/webapp/jsp/NregaConsolidated.jsp
+++ prrws/src/main/webapp/jsp/NregaConsolidated.jsp
@@ -1,1 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mahatma Gandhi National Rural EGS</title>
+<link href="Assests/less/bootstrap.less" rel="stylesheet" type="text/less">
+<link href="Assests/css/custom.less" rel="stylesheet" type="text/less"/>
+<link href="Assests/Plugins/DateTime/bootstrap-datetimepicker-build.less" type="text/less" rel="stylesheet"/>
+<link href="Assests/Plugins/Date/daterangepicker.css" type="text/css" rel="stylesheet"/>
+<link href="Assests/Plugins/Scroller/jquery.mCustomScrollbar.css" type="text/less" rel="stylesheet"/>
+<script src="Assests/Plugins/Less/less.js"></script>
+<link href="Assests/css/responsive.css" type="text/css" rel="stylesheet"/>
+<link href="Assests/Plugins/DataTable/dataTable.css" type="text/css" rel="stylesheet"/>
+<link href="Assests/Plugins/Chosen/chosen.css" type="text/css" rel="stylesheet"/>
+
+</head>
+
 <div>Test...</div>
+
+<script src="Assests/js/jquery-3.2.1.js" type="text/javascript"></script>
+<script src="Assests/js/bootstrap.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Chosen/chosen.jquery.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Scroller/jquery.mCustomScrollbar.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Scroller/jquery.mousewheel.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Date/moment.js" type="text/javascript"></script>
+<script src="Assests/Plugins/DateTime/bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+<script src="Assests/Plugins/Date/daterangepicker.js" type="text/javascript"></script>
+<script src="Assests/Plugins/DataTable/dataTable.js" type="text/javascript"></script>
+
+<script>
+//getAllConvergenceTypes();
+function getAllConvergenceTypes()
+{
+	alert(2)
+	var json = {
+	}
+	
+	$.ajax({
+		url: 'getAllConvergenceTypes',
+		data: JSON.stringify(json),
+		type: "POST",
+		dataType: 'json', 
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("Accept", "application/json");
+			xhr.setRequestHeader("Content-Type", "application/json");
+		},
+		success: function(ajaxresp) {
+			//buildNREGSAbstractDataByTypeNew(type,ajaxresp,blockName,locId,locType,levelId)
+		}
+	});
+}
+getComponentByConvergType();
+function getComponentByConvergType()
+{
+	var json = {
+		convergenceTypeId : 1
+	}
+	
+	$.ajax({
+		url: 'getComponentByConvergType',
+		data: JSON.stringify(json),
+		type: "POST",
+		dataType: 'json', 
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("Accept", "application/json");
+			xhr.setRequestHeader("Content-Type", "application/json");
+		},
+		success: function(ajaxresp) {
+			//buildNREGSAbstractDataByTypeNew(type,ajaxresp,blockName,locId,locType,levelId)
+		}
+	});
+}
+</script>
