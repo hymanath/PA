28848ce1368ceae5b2c94111ebad7b5b1611788e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-02 09:43:01 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/webapp/Assests/lightMonitoringDashBoard/lightMonitoringDashBoard.js
@@ -0,0 +1,42 @@
+alert("hai");
+getStatusByLightMonitoringOverview();
+getStatusByLightMonitoringVillagesData();
+
+
+function getStatusByLightMonitoringOverview(){
+		//$("#DepartmentsId").html('');
+		var json = {
+
+		}
+		$.ajax({                
+			type:'POST',    
+			url: 'getStatusByVillages',
+			dataType: 'json',
+			data : JSON.stringify(json),
+			beforeSend :   function(xhr){
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			}
+		}).done(function(result){
+		});
+		
+}
+function getStatusByLightMonitoringVillagesData(){
+	//$("#DepartmentsId").html('');
+	var json = {
+
+	}
+	$.ajax({                
+		type:'POST',    
+		url: 'getVillageIdBasedDetails',
+		dataType: 'json',
+		data : JSON.stringify(json),
+		beforeSend :   function(xhr){
+			xhr.setRequestHeader("Accept", "application/json");
+			xhr.setRequestHeader("Content-Type", "application/json");
+		}
+	}).done(function(result){
+		
+	});
+	
+}
