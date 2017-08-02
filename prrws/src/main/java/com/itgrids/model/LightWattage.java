b116ae476f84e3d4bf0645a7a0f55ff10f5c5ca4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-02 09:42:15 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/java/com/itgrids/model/LightWattage.java
@@ -0,0 +1,63 @@
+package com.itgrids.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "light_wattage_info")
+public class LightWattage {
+	
+	private Long lightWattageInfoId;
+	private  Long wattage;
+	private Long lightCount;
+	private LightMonitoring lightMonitoring ;
+	private Long lightMonitoringId;	
+	@Id
+	@Column(name="light_wattage_info_id")
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	public Long getLightWattageInfoId() {
+		return lightWattageInfoId;
+	}
+	public void setLightWattageInfoId(Long lightWattageInfoId) {
+		this.lightWattageInfoId = lightWattageInfoId;
+	}
+	
+	@Column(name="light_count")
+	public Long getLightCount() {
+		return lightCount;
+	}
+	@Column(name="wattege")
+	public Long getWattage() {
+		return wattage;
+	}
+	public void setWattage(Long wattage) {
+		this.wattage = wattage;
+	}
+	public void setLightCount(Long lightCount) {
+		this.lightCount = lightCount;
+	}
+	@Column(name="light_monitoring_id")
+	public Long getLightMonitoringId() {
+		return lightMonitoringId;
+	}
+	public void setLightMonitoringId(Long lightMonitoringId) {
+		this.lightMonitoringId = lightMonitoringId;
+	}
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "light_monitoring_id",insertable = false, updatable = false)
+	public LightMonitoring getLightMonitoring() {
+		return lightMonitoring;
+	}
+	public void setLightMonitoring(LightMonitoring lightMonitoring) {
+		this.lightMonitoring = lightMonitoring;
+	}
+	
+	
+}
