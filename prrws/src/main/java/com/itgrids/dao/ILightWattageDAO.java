22509d57d081316db8fa8e19e58f1a84d392459e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-02 09:39:53 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/java/com/itgrids/dao/ILightWattageDAO.java
@@ -0,0 +1,13 @@
+package com.itgrids.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.LightWattage;
+
+public interface ILightWattageDAO  extends GenericDao<LightWattage, Long > {
+
+	
+
+}
