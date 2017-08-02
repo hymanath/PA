90d3caea5d1bf190768fcc364d0f7495649200c2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-02 09:41:14 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/java/com/itgrids/dto/ResultVO.java
@@ -0,0 +1,23 @@
+package com.itgrids.dto;
+
+public class ResultVO {
+	
+	
+	private Integer statusCode;
+	private String message;
+	public Integer getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(Integer statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
