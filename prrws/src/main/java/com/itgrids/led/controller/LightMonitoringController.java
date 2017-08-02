65e74d8e646f78eed269a3e8ab034111c7eac55a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-02 09:41:31 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/java/com/itgrids/led/controller/LightMonitoringController.java
@@ -0,0 +1,42 @@
+package com.itgrids.led.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itgrids.dto.LightMonitoringVO;
+import com.itgrids.dto.ResultVO;
+import com.itgrids.led.service.ILightMonitoring;
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
+@EnableAutoConfiguration
+@Controller
+@RequestMapping("/")
+public class LightMonitoringController {
+
+	private static final Logger logger  =  LoggerFactory.getLogger(LightMonitoringController.class);	
+	@Autowired
+	private ILightMonitoring  lightMonitoring;	
+	@RequestMapping(value = "/saveRealtimeStatusByVillages", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody  ResultVO   saveRealtimeStatusByVillages()
+	{
+		        ResultVO  voList= lightMonitoring.saveRealtimeStatusByVillages();
+		 
+		 
+		return voList;
+	}
+	
+	
+
+}
