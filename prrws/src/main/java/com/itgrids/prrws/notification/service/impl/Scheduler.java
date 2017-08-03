131fa5b33ae6b6e02f0bbb5d7b0dfe37e89a2fe4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-03 11:13:29 +0000

    Teja LED Dashboard Services--updated by

---
+++ prrws/src/main/java/com/itgrids/prrws/notification/service/impl/Scheduler.java
@@ -0,0 +1,28 @@
+package com.itgrids.prrws.notification.service.impl;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.itgrids.led.service.ILightMonitoring;
+
+@Configuration
+@EnableScheduling
+public class Scheduler {
+	
+	private static final Logger log = Logger.getLogger(Scheduler.class);
+	@Autowired
+	private ILightMonitoring lightMonitoringService;
+
+	
+	@Scheduled(cron = "0 0/3 * * * *")
+	public void runTheSchedulerEveryDay()
+	{
+		/*if(!IConstants.DEFAULT_SCHEDULER_SEVER.equalsIgnoreCase(IConstants.SERVER))
+			return;*/
+		lightMonitoringService.saveRealtimeStatusByVillages();
+	}
+	
+}
