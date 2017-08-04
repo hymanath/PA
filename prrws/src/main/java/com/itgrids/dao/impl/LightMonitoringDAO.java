bd161dcca5602c68b027590368d76a3857ba8873
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 10:34:00 +0000

    Teja LED Dashboard Services--updated by

--- prrws/src/main/java/com/itgrids/dao/impl/LightMonitoringDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/LightMonitoringDAO.java
@@ -40,7 +40,7 @@ public class LightMonitoringDAO extends GenericDaoHibernate<LightMonitoring ,Lon
 	public List<Object[]> getTotalVillagesDetails() {
 		 StringBuilder sb = new StringBuilder();
 		 sb.append("select sum(model.totalLights),sum(model.totalPanels),sum(model.totalPoles),"
-		 		+ "sum(model.workingLights),sum(model.onLights),sum(model.offLights),sum(model.notWorkingLights)"
+		 		+ "sum(model.workingLights),sum(model.onLights),sum(model.offLights),sum(model.notWorkingLights) "
 		 		+ " from  LightMonitoring model ");
 		 				 Query query = getSession().createQuery(sb.toString());
 		 
@@ -54,26 +54,36 @@ public class LightMonitoringDAO extends GenericDaoHibernate<LightMonitoring ,Lon
 	@Override
 	public List<Object[]> getTotalSurveyDetails() {
 		 StringBuilder sb = new StringBuilder();
-		 sb.append("select  d.districtId,d.districtName,count(distinct t.tehsilId),count(distinct p.panchayatId) from Panchayat p,"
-		 		+ "Tehsil t,District d where p.tehsilId = t.tehsilId and t.districtId = d.districtId and (d.districtId "
-		 		+ " between 11 and 23) group by d.districtId order by d.districtId");
-        
+		 sb.append(" select  count( distinct model.locationAddress.district.districtId),"
+		 		+ " count(distinct model.locationAddress.constituency.constituencyId),"
+		 		+ " count(distinct model.locationAddress.tehsil.tehsilId),"
+		 		+ " count(distinct model.locationAddress.panchayat.panchayatId)"
+		 		+ "  from Panchayat model ");
+		 
+		 sb.append(" where  model.locationAddress.district.districtId between 11 and 23 ");
 	 Query query = getSession().createQuery(sb.toString());
 	 return query.list();
 	}
+	
 	/*
  	 * Date : 03/08/2017
  	 * Author :Swapna
  	 */
      
 	@Override
-	public List<Object[]> getStateLevelWiseSurveyDetails() {
+	public List<Object[]> getAllDitrictWiseSurveyDetails(Date startDate,Date endDate) {
 		 StringBuilder sb = new StringBuilder();
-		 sb.append("select distinct LA.districtId,d.districtName,sum( distinct LA.tehsil_id),sum(LM.totalPoles),sum(LM.totalPanels), sum(LM.totalLights),"
-		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights),FROM "
-		 		+ "LightMonitoring LM,Panchayat P,LocationAddress LA,District d WHERE   LM.panchayatId = P.panchayatId AND "
-		 		+ "P.locationAddressId = LA.locationAddressId AND  LA.districtId=D.districtId and LM.insertredTime = LM.insertredTime GROUP BY "
-		 		+ "LA.districtId");
+		 
+		 sb.append("select distinct d.districtId,d.districtName,sum( distinct LA.tehsil_id),sum(LM.totalPoles),sum(LM.totalPanels),sum(LM.totalLights),"
+		 		+ " sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights) FROM "
+		 		+ " LightMonitoring LM,Panchayat P,LocationAddress LA,District d WHERE   LM.panchayatId = P.panchayatId AND "
+		 		+ " P.locationAddressId = LA.locationAddressId AND  "
+		 		+ " LA.districtId=D.districtId ");
+		 
+		 		if(startDate!= null && endDate!=null){
+		 			sb.append(" date(LM.insertredTime) between :startDate and :endDate and ");
+		 		}
+		 		sb.append(" GROUP BY  LA.districtId ");
         
 	 Query query = getSession().createQuery(sb.toString());
 	 return query.list();
@@ -87,7 +97,7 @@ public class LightMonitoringDAO extends GenericDaoHibernate<LightMonitoring ,Lon
 	public List<Object[]> getConstituencyLevelWiseSurveyDetails() {
 		StringBuilder sb = new StringBuilder();
 		 sb.append("select distinct LA.districtId,d.districtName,C.constituencyId,C.name as constituencyName, sum(LM.totalPoles),sum(LM.totalPanels), sum(LM.totalLights),"
-		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights),FROM "
+		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights) FROM "
 		 		+ "LightMonitoring LM,Panchayat P,LocationAddress LA,District d,Constituency C WHERE  and LM.panchayatId = P.panchayatId AND "
 		 		+ "P.locationAddressId = LA.locationAddressId AND  LA.districtId=D.districtId and LA.constituencyId=C.constituencyId  LM.insertredTime = LM.insertredTime GROUP BY "
 		 		+ "LA.districtId,LA.constituencyId");
@@ -104,7 +114,20 @@ public class LightMonitoringDAO extends GenericDaoHibernate<LightMonitoring ,Lon
 	public List<Object[]> getMandalLevelWiseSurveyDetails() {
 		StringBuilder sb = new StringBuilder();
 		 sb.append("select distinct LA.districtId,d.districtName,C.constituencyId,C.name as constituencyName,T.tehsilId,T.tehsilName,sum(LM.totalPoles),sum(LM.totalPanels), sum(LM.totalLights),"
-		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights),FROM "
+		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights) FROM "
+		 		+ "LightMonitoring LM,Panchayat P,,LocationAddress LA,District d,Constituency C,Tehsil T WHERE PR.panchayatId=P.panchayatId and LM.panchayatId = P.panchayatId AND "
+		 		+ "P.locationAddressId = LA.locationAddressId AND  LA.districtId=D.districtId and LA.constituencyId=C.constituencyId and LA.tehsilId=T.tehsilId and LM.insertredTime = LM.insertredTime GROUP BY "
+		 		+ "LA.districtId,LA.constituencyId,LA.tehsilId");
+       
+	 Query query = getSession().createQuery(sb.toString());
+	 return query.list();
+	}
+	
+	@Override
+	public List<Object[]> getParlaimentWiseSurveyDetails() {
+		StringBuilder sb = new StringBuilder();
+		 sb.append("select distinct LA.districtId,d.districtName,C.constituencyId,C.name as constituencyName,T.tehsilId,T.tehsilName,sum(LM.totalPoles),sum(LM.totalPanels), sum(LM.totalLights),"
+		 		+ "sum(LM.workingLights),sum(LM.onLights),sum(LM.offLights),sum(LM.notWorkingLights) FROM "
 		 		+ "LightMonitoring LM,Panchayat P,,LocationAddress LA,District d,Constituency C,Tehsil T WHERE PR.panchayatId=P.panchayatId and LM.panchayatId = P.panchayatId AND "
 		 		+ "P.locationAddressId = LA.locationAddressId AND  LA.districtId=D.districtId and LA.constituencyId=C.constituencyId and LA.tehsilId=T.tehsilId and LM.insertredTime = LM.insertredTime GROUP BY "
 		 		+ "LA.districtId,LA.constituencyId,LA.tehsilId");
