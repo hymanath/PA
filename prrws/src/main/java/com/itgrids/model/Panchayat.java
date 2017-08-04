639b75128f7aa0e40e4c177b4350de2903ec5d1c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 10:34:49 +0000

    Teja LED Dashboard Services--updated by

--- prrws/src/main/java/com/itgrids/model/Panchayat.java
+++ prrws/src/main/java/com/itgrids/model/Panchayat.java
@@ -31,8 +31,10 @@ public class Panchayat {
 	private String panchayatName;
 	private Long tehsilId;
 	private String nameLocal;
+	private Long locationAddressId;
 
 	private Tehsil tehsil;
+	private LocationAddress locationAddress;
 	
 	@Id
 	@Column(name="panchayat_id")
@@ -77,6 +79,23 @@ public class Panchayat {
 	}
 	public void setTehsil(Tehsil tehsil) {
 		this.tehsil = tehsil;
+	}
+	@Column(name="location_address_id")
+	public Long getLocationAddressId() {
+		return locationAddressId;
+	}
+	public void setLocationAddressId(Long locationAddressId) {
+		this.locationAddressId = locationAddressId;
+	}
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "location_address_id", insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action = NotFoundAction.IGNORE)
+	public LocationAddress getLocationAddress() {
+		return locationAddress;
+	}
+	public void setLocationAddress(LocationAddress locationAddress) {
+		this.locationAddress = locationAddress;
 	}
 
 }
