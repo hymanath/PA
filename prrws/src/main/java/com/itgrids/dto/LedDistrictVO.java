105d228591bbac4d743bee434f43ec84e3dfbb86
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 08:28:22 +0000

    LED dashboard   updated by swapna

---
+++ prrws/src/main/java/com/itgrids/dto/LedDistrictVO.java
@@ -0,0 +1,32 @@
+package com.itgrids.dto;
+
+public class LedDistrictVO {
+	private Long districtId;
+	private String districtName;
+	private Long tehsilId;
+	private Long panchayatId;
+	public Long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(Long districtId) {
+		this.districtId = districtId;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public Long getTehsilId() {
+		return tehsilId;
+	}
+	public void setTehsilId(Long tehsilId) {
+		this.tehsilId = tehsilId;
+	}
+	public Long getPanchayatId() {
+		return panchayatId;
+	}
+	public void setPanchayatId(Long panchayatId) {
+		this.panchayatId = panchayatId;
+	}
+	}
