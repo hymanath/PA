afff3169f9bb99351423f2cd4eab6c2c062aa509
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 10:36:24 +0000

    Teja LED Dashboard Services--updated by

---
+++ prrws/src/main/java/com/itgrids/dto/LedOverviewVo.java
@@ -0,0 +1,35 @@
+package com.itgrids.dto;
+
+public class LedOverviewVo {
+	
+	private Long totalDistCnt = 0l;
+	private Long totalConstituencyCnt = 0l;
+	private Long totalMandalCnt =0l;
+	private Long totalpanchayatCnt =0l;
+	public Long getTotalDistCnt() {
+		return totalDistCnt;
+	}
+	public void setTotalDistCnt(Long totalDistCnt) {
+		this.totalDistCnt = totalDistCnt;
+	}
+	public Long getTotalConstituencyCnt() {
+		return totalConstituencyCnt;
+	}
+	public void setTotalConstituencyCnt(Long totalConstituencyCnt) {
+		this.totalConstituencyCnt = totalConstituencyCnt;
+	}
+	public Long getTotalMandalCnt() {
+		return totalMandalCnt;
+	}
+	public void setTotalMandalCnt(Long totalMandalCnt) {
+		this.totalMandalCnt = totalMandalCnt;
+	}
+	public Long getTotalpanchayatCnt() {
+		return totalpanchayatCnt;
+	}
+	public void setTotalpanchayatCnt(Long totalpanchayatCnt) {
+		this.totalpanchayatCnt = totalpanchayatCnt;
+	}
+	
+	
+}
