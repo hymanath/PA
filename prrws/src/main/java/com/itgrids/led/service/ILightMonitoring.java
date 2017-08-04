c7005e86fd04ed88febe1558a52c631eb909e57e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 10:35:29 +0000

    Teja LED Dashboard Services--updated by

--- prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
+++ prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
@@ -2,16 +2,16 @@ package com.itgrids.led.service;
 
 import java.util.List;
 
-import com.itgrids.dto.LedDistrictVO;
+import com.itgrids.dto.LedOverviewVo;
 import com.itgrids.dto.LightMonitoringVO;
 import com.itgrids.dto.ResultVO;
 
 
 public interface ILightMonitoring {
 	
-	public List<LightMonitoringVO> getRealtimeStatusByVillages();	
+	public List<LightMonitoringVO> getBasicLedOverviewDetails();	
 	//public List<LightMonitoringVO> getVillageIdBasedDetails();
       public ResultVO saveRealtimeStatusByVillages();
-      public  List<LedDistrictVO> getDistrictLevelCount();
+      public List<LedOverviewVo> getLedOverviewForStartedLocationsDetailsCounts();
 
 }
