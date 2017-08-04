abb2cb1258e5050b469c1ac82edf528bb4b00148
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-04 08:29:27 +0000

    LED dashboard   updated by swapna

--- prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
+++ prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
@@ -2,15 +2,16 @@ package com.itgrids.led.service;
 
 import java.util.List;
 
-
+import com.itgrids.dto.LedDistrictVO;
 import com.itgrids.dto.LightMonitoringVO;
 import com.itgrids.dto.ResultVO;
 
 
 public interface ILightMonitoring {
 	
-	//public LightMonitoringVO getRealtimeStatusByVillages();	
+	public List<LightMonitoringVO> getRealtimeStatusByVillages();	
 	//public List<LightMonitoringVO> getVillageIdBasedDetails();
       public ResultVO saveRealtimeStatusByVillages();
+      public  List<LedDistrictVO> getDistrictLevelCount();
 
 }
