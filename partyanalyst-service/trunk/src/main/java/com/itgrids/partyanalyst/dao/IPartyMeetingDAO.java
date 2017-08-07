591f6678a048d72e7762240055d943f9fee0503d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-07 06:11:44 +0000

    commited by sai kumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IPartyMeetingDAO.java
@@ -93,8 +93,8 @@ public interface IPartyMeetingDAO extends GenericDao<PartyMeeting,Long>{
 	public List<Object[]> getPartyMeetingsImagesCoveredByMeetings(List<Long> meetingIds);
 	public List<Object[]> meetingsBaseTotalCountForDiffLevels(PartyMeetingsInputVO inputVO,Long locationId,Set<Long> locationValuesSet,List<Long> locLevelIdList);
 
-	public List<Object[]> getCadrePartyMeetngDeatils(Date fromDate,Date toDate,Long meetigLevelId,int startIndex,int maxIndex);
-	 public Long getCadrePartyMeetngDeatilsCount(Date fromDate,Date toDate,Long meetigLevelId);
+	public List<Object[]> getCadrePartyMeetngDeatils(Date fromDate,Date toDate,List<Long> meetigLevelId,int startIndex,int maxIndex);
+	 public Long getCadrePartyMeetngDeatilsCount(Date fromDate,Date toDate,List<Long> meetigLevelId);
 	 public List<Object[]> getPartyMeetingDetailsByPartyMeetingId(Long patyMeetingId);
 	 public Integer updatePartyMeetingDetails(Long meetingId);
 	 public List<Object[]> getPartyMeetingIdsLevelwise(Date startDate,Date endDate,String level,List<Long> levelValues,Set<Long> levelIds);
