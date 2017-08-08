27a0f91392a5f8a16b73a27fb99361741a1fa7b0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-08 05:56:37 +0000

    Updated by Sanjeev on constituency Page  Info

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeCommitteeDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeCommitteeDAO.java
@@ -1,9 +1,15 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.BoothInchargeCommittee;
 
 public interface IBoothInchargeCommitteeDAO extends GenericDao<BoothInchargeCommittee, Long>{
+	
+	public Long getElectionBoothDetails(Date startDate, Date endDate,Long locationTypeId, Long locationValue, String type,
+			List<Long> committeeEnrollmentYearsIdsLst);
 
 }
