ad5279c60ec27b5b18fe289752b87c39f32bdb3b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-10 12:38:19 +0000

    LED dashboard   updated by swapna

--- prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
+++ prrws/src/main/java/com/itgrids/led/service/ILightMonitoring.java
@@ -3,6 +3,7 @@ package com.itgrids.led.service;
 import java.util.Date;
 import java.util.List;
 
+import com.itgrids.dto.InputVO;
 import com.itgrids.dto.LedOverviewVo;
 import com.itgrids.dto.LightMonitoringVO;
 import com.itgrids.dto.ResultVO;
@@ -14,11 +15,13 @@ public interface ILightMonitoring {
 	//public List<LightMonitoringVO> getVillageIdBasedDetails();
       public ResultVO saveRealtimeStatusByVillages();
       public List<LedOverviewVo> getLedOverviewForStartedLocationsDetailsCounts(String startDate,String endDate);
-      public	List<LightMonitoringVO> getLevelWiseOverviewDetails(String fromDateStr, String toDateStr, String year,
+   /*   public	List<LightMonitoringVO> getLevelWiseOverviewDetails(String fromDateStr, String toDateStr, String year,
 				List<Long> locationValues, Long locationTypeId, Long searchlevelId, List<Long> searchLevelValues);
       
       public	List<LightMonitoringVO> getDistrictLevelWiseOverviewDetails(String fromDateStr, String toDateStr, String year,
   				List<Long> locationValues, Long locationTypeId);
-	List<LightMonitoringVO> getDistrictLevelWiseOverviewDetails(String fromDateStr, String toDateStr,
-			List<Long> locationValues, Long locationTypeId);
+	  List<LightMonitoringVO> getDistrictLevelWiseOverviewDetails(String fromDateStr, String toDateStr,
+			List<Long> locationValues, Long locationTypeId);	*/
+	public List<LightMonitoringVO> getAllLevelWiseDataOverView(String locationType, String displayType,
+			String filterType, Long locationId);
 }
