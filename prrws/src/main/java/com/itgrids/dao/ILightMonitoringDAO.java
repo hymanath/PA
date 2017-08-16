25b9b2da53cc5f42f55e11052f9b1c449a9c2742
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-16 14:02:19 +0000

    Updated by Santosh

--- prrws/src/main/java/com/itgrids/dao/ILightMonitoringDAO.java
+++ prrws/src/main/java/com/itgrids/dao/ILightMonitoringDAO.java
@@ -10,18 +10,10 @@ import com.itgrids.model.LightMonitoring;
 
 
 public interface ILightMonitoringDAO extends GenericDao<LightMonitoring, Long > {
-	
-	
 	public List<Object[]> getTotalVillagesDetails(Date startDate,Date endDate);
 	public List<Object[]> getTotalSurveyDetails(Date startDate,Date endDate);
-	public List<Object[]> getAllDitrictWiseSurveyDetails(Date fromDate,Date toDate,String year,List<Long> locationValues,Long locationTypeId,Long searchlevelId,List<Long> searchLevelValues);
-	public List<Object[]> getConstituencyLevelWiseSurveyDetails();
-	public List<Object[]> getMandalLevelWiseSurveyDetails();
-    public List<LightMonitoring> getLiveDateForCurrentDateSelection(Date date);
-    public List<Object[]> getParlaimentWiseSurveyDetails();
- 	public List<Object[]> getDistrictLevelWise(Date fromDate, Date toDate,  List<Long> locationValues,	Long locationTypeId); 
- 	
- 	public List<Object[]> getLocationsForLEDDashboard(String locationType,String filterType,Long locationId);
+	public List<LightMonitoring> getLiveDateForCurrentDateSelection(Date date);
+  	public List<Object[]> getLocationsForLEDDashboard(String locationType,String filterType,Long locationId);
  	public List<Object[]> getLocationWiseDataForLEDDashboard(String locationType,String filterType,Long locationId,Date formDate,Date toDate);
 
 }
