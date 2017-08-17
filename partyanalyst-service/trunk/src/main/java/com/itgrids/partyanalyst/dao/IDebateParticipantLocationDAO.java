66cbc425b1cefd09726b24a585ec09e01666ec48
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-17 07:31:53 +0000

    updated by srujana

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDebateParticipantLocationDAO.java
@@ -0,0 +1,10 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityAttribute;
+import com.itgrids.partyanalyst.model.DebateParticipantLocation;
+
+public interface IDebateParticipantLocationDAO extends GenericDao<DebateParticipantLocation, Long>{
+
+}
