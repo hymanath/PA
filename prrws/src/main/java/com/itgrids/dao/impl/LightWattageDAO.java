a5e4767081c4110c67ad951806a0914a51f0f452
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-17 06:45:47 +0000

    updated by swapna

--- prrws/src/main/java/com/itgrids/dao/impl/LightWattageDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/LightWattageDAO.java
@@ -24,13 +24,23 @@ public class LightWattageDAO extends GenericDaoHibernate<LightWattage ,Long> imp
 
 }
 	@Override
-	public List<Object[]> getTotalWattege(Date fromDate,Date toDate) {
+	public List<Object[]> getTotalWattege(Date fromDate,Date toDate, String locationType,Long locationValue) {
 		StringBuilder sb = new StringBuilder();
 		 sb.append("select model.wattage ,sum(model.lightCount) "
 				 +" from "
 				+ " LightWattage model  "
 		 		+ " where model.isDeleted = 'N' and model.lightMonitoring.isDeleted ='N' ");
-		 
+		 if( locationType != null && locationType.trim().length() > 0 && locationValue != null && locationValue.longValue() > 0){
+				if(locationType.equalsIgnoreCase("district")){
+					sb.append(" AND model.lightMonitoring.panchayat.locationAddress.district.districtId = :locationValue ");
+				}else if(locationType.equalsIgnoreCase("parliament")){
+					sb.append(" AND model.lightMonitoring.panchayat.locationAddress.parliament.constituencyId = :locationValue ");
+				}else if(locationType.equalsIgnoreCase("constituency")){
+					sb.append(" AND model.lightMonitoring.panchayat.locationAddress.constituency.constituencyId = :locationValue ");
+				}else if(locationType.equalsIgnoreCase("mandal")){
+					sb.append(" AND model.lightMonitoring.panchayat.locationAddress.tehsil.tehsilId = :locationValue ");
+				}
+			}
 		 if(fromDate != null && toDate != null){
 			 sb.append(" and date(model.lightMonitoring.surveyDate) between :fromDate and :toDate ");
 		 }
@@ -40,6 +50,9 @@ public class LightWattageDAO extends GenericDaoHibernate<LightWattage ,Long> imp
 		 if(fromDate != null && toDate != null){
 			 query.setDate("fromDate", fromDate);
 			 query.setDate("toDate", toDate);
+		 }
+		 if(locationType != null && locationType.trim().length() > 0 && locationValue != null && locationValue.longValue() > 0){
+				query.setParameter("locationValue",locationValue);
 		 }
 		return query.list();
 	}
