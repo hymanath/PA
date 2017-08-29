39ff9d2383975db4ea783f017879fe4731d11a8c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-29 14:13:21 +0000

    updated by-swadhin

---
+++ prrws/src/main/java/com/itgrids/dao/IPredExpenditureDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PredExpenditure;
+
+public interface IPredExpenditureDAO extends GenericDao<PredExpenditure, Long> {
+
+}
