ee880cafb33e25dea3b6cd5c8f693783e60158b7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-08-29 07:26:40 +0000

    updated by srujana : debate location and debate participant location saving and retriving

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDebateDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IDebateDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.Debate;
-
+public interface IDebateDAO extends GenericDao<Debate, Long>{
 	public List<Object[]> getDebateDetailsForSelectedDebate(Long debateId,Long stateId);
 	
 	public int removeDebate(Long debateId);
