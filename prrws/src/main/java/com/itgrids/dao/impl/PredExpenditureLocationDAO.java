8ad1990d07ba995aed49559d20263162bfdd4e53
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-05 06:35:36 +0000

    updated by-swadhin

--- prrws/src/main/java/com/itgrids/dao/impl/PredExpenditureLocationDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PredExpenditureLocationDAO.java
@@ -37,17 +37,17 @@ public class PredExpenditureLocationDAO extends GenericDaoHibernate<PredExpendit
 					+ "  state.stateId = 1 and model.isDeleted='N' ");
 		 if (filterType != null && filterType.trim().length() > 0 && locationIds != null && locationIds.size() > 0) {
 				if (filterType.equalsIgnoreCase("district")) {
-					sb.append(" AND district.districtId in(:locationIds) ");
+					sb.append(" AND district.districtId in(:locationIds) and district is not null ");
 				} else if (filterType.equalsIgnoreCase("parliament")) {
-					sb.append(" AND parliament.constituencyId in(:locationIds) ");
+					sb.append(" AND parliament.constituencyId in(:locationIds) and parliament is not null ");
 				} else if (filterType.equalsIgnoreCase("constituency")) {
-					sb.append(" AND constituency.constituencyId in(:locationIds) ");
+					sb.append(" AND constituency.constituencyId in(:locationIds) and constituency is not null ");
 				} else if (filterType.equalsIgnoreCase("mandal")) {
-					sb.append(" AND tehsil.tehsilId in(:locationIds) ");
+					sb.append(" AND tehsil.tehsilId in(:locationIds) and tehsil is not null ");
 				} else if (filterType.equalsIgnoreCase("panchayat")) {
-					sb.append(" AND panchayat.panchayatId in(:locationIds) ");
+					sb.append(" AND panchayat.panchayatId in(:locationIds) and panchayat is not null ");
 				} else if (filterType.equalsIgnoreCase("division")) {
-					sb.append(" AND predddo.predDdoId in(:locationIds) ");
+					sb.append(" AND predddo.predDdoId in(:locationIds) and predddo is not null ");
 				}
 			}
 		 Query query = getSession().createQuery(sb.toString());
@@ -68,21 +68,27 @@ public class PredExpenditureLocationDAO extends GenericDaoHibernate<PredExpendit
 		if (locationType.equalsIgnoreCase("district")) {
 			sbs.append(",district.districtId, district.districtName ");
 			sbg.append(" GROUP BY district.districtId ORDER BY district.districtId ");
+			sbf.append(" AND district is not null ");
 		} else if (locationType.equalsIgnoreCase("parliament")) {
 			sbs.append(",parliament.constituencyId, parliament.name ");
 			sbg.append(" GROUP BY parliament.constituencyId ORDER BY parliament.name ");
+			sbf.append(" AND parliament is not null ");
 		} else if (locationType.equalsIgnoreCase("constituency")) {
 			sbs.append(",constituency.constituencyId, constituency.name ");
 			sbg.append(" GROUP BY constituency.constituencyId ORDER BY constituency.name ");
+			sbf.append(" AND constituency is not null ");
 		} else if (locationType.equalsIgnoreCase("mandal")) {
 			sbs.append(",tehsil.tehsilId,tehsil.tehsilName ");
 			sbg.append(" GROUP BY tehsil.tehsilId ORDER BY tehsil.tehsilName ");
+			sbf.append(" AND tehsil is not null ");
 		} else if (locationType.equalsIgnoreCase("panchayat")) {
 			sbs.append(",panchayat.panchayatId,panchayat.panchayatName ");
-			sbg.append(" GROUP BY model.panchayatId ORDER BY model.panchayatName ");
+			sbg.append(" GROUP BY panchayat.panchayatId ORDER BY panchayat.panchayatName ");
+			sbf.append(" AND panchayat is not null ");
 		} else if (locationType.equalsIgnoreCase("division")) {
 			sbs.append(",predddo.predDdoId,predddo.division ");
 			sbg.append(" GROUP BY predddo.predDdoId ORDER BY predddo.division ");
+			sbf.append(" AND predddo is not null ");
 		}
 		sbs.append(",state.stateId, state.stateName,district.districtId,district.districtName,"
 				+ " constituency.constituencyId,constituency.name,tehsil.tehsilId,tehsil.tehsilName, "
