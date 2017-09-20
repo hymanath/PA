458c1b791765c393a6b86511edbeeea6c129f442
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-20 08:11:09 +0000

    IIC DashBoard Controller and service created by -- Santosh

--- prrws/src/main/java/com/itgrids/controllers/web/ItcController.java
+++ prrws/src/main/java/com/itgrids/controllers/web/ItcController.java
@@ -1,11 +1,14 @@
 package com.itgrids.controllers.web;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itgrids.service.IItcDashboardService;
 
 @EnableAutoConfiguration
 @Controller
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ItcController {
 	
 	private static final Logger LOG = Logger.getLogger(ItcController.class);
+	
+	@Autowired
+	private IItcDashboardService itcDashboardService;
 	
 	@RequestMapping(value ="/itcDashboard",method = RequestMethod.GET)
     public String drainsDashboard(ModelMap model) {
