6b446ba037659695ef18ff1133ed3af050315091
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-21 13:31:31 +0000

    updated by --- Aravind

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/DoorToDoorInputVO.java
@@ -0,0 +1,72 @@
+package com.itgrids.partyanalyst.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class DoorToDoorInputVO implements Serializable{
+	private Long id;
+	private List<Long> levelIds;
+	private List<Long> levelValues;
+	private String startDateStr;
+	private String endDateStr;
+	private String locationType;
+    private List<Long> notStartedList;
+	private Long departmentId;
+	private Long issueTypeId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public List<Long> getLevelIds() {
+		return levelIds;
+	}
+	public void setLevelIds(List<Long> levelIds) {
+		this.levelIds = levelIds;
+	}
+	public List<Long> getLevelValues() {
+		return levelValues;
+	}
+	public void setLevelValues(List<Long> levelValues) {
+		this.levelValues = levelValues;
+	}
+	public String getStartDateStr() {
+		return startDateStr;
+	}
+	public void setStartDateStr(String startDateStr) {
+		this.startDateStr = startDateStr;
+	}
+	public String getEndDateStr() {
+		return endDateStr;
+	}
+	public void setEndDateStr(String endDateStr) {
+		this.endDateStr = endDateStr;
+	}
+	public String getLocationType() {
+		return locationType;
+	}
+	public void setLocationType(String locationType) {
+		this.locationType = locationType;
+	}
+	public List<Long> getNotStartedList() {
+		return notStartedList;
+	}
+	public void setNotStartedList(List<Long> notStartedList) {
+		this.notStartedList = notStartedList;
+	}
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public Long getIssueTypeId() {
+		return issueTypeId;
+	}
+	public void setIssueTypeId(Long issueTypeId) {
+		this.issueTypeId = issueTypeId;
+	}
+	
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDoorToDoorCampaignDashboardService.java
@@ -0,0 +1,22 @@
+package com.itgrids.partyanalyst.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.DoorCampaignDashboardVO;
+import com.itgrids.partyanalyst.dto.DoorToDoorInputVO;
+
+public interface IDoorToDoorCampaignDashboardService {
+
+	public DoorCampaignDashboardVO getUsersCountsByLocation(DoorToDoorInputVO inputVO);
+	public DoorCampaignDashboardVO getHouseHoldsCounts(DoorToDoorInputVO inputVO);
+	public DoorCampaignDashboardVO getGrievancesCounts(DoorToDoorInputVO inputVO);
+	public DoorCampaignDashboardVO getRecentImagesList(DoorToDoorInputVO inputVO);
+	public List<DoorCampaignDashboardVO> getLocationWiseCountDetails(DoorToDoorInputVO inputVO);
+	public List<DoorCampaignDashboardVO> getDepartmentWiseGrievanceCounts(DoorToDoorInputVO inputVO);
+	public List<DoorCampaignDashboardVO> getDepartmentIssueWiseGrievanceCounts(DoorToDoorInputVO inputVO);
+	public List<DoorCampaignDashboardVO> getDepartmentSubIssueWiseGrievanceCounts(DoorToDoorInputVO inputVO);
+	public DoorCampaignDashboardVO getLevelWiseCount(DoorToDoorInputVO inputVO);
+	public DoorCampaignDashboardVO getCampaignCountFrMandalPancMuncip(DoorToDoorInputVO inputVO);
+	
+	public DoorCampaignDashboardVO getUserAccessLevelIdsAndValues(DoorToDoorInputVO inputVO);
+}
