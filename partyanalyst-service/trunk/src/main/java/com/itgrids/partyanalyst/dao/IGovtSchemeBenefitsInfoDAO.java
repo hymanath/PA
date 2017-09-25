fca77cd9c117c9b0f521720bad1bcb00d7f0c642
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-25 14:30:49 +0000

    updated by srujana : issue in constituency page (benefits block)

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtSchemeBenefitsInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtSchemeBenefitsInfoDAO.java
@@ -7,6 +7,7 @@ import org.appfuse.dao.GenericDao;
 import com.itgrids.partyanalyst.model.GovtSchemeBenefitsInfo;
 
 public interface IGovtSchemeBenefitsInfoDAO extends GenericDao<GovtSchemeBenefitsInfo, Long>{
-	public List<Object[]> getGovtSchemeWiseBenefitMemberCount(Long locationType, Long locationValue,List<Long> constituencyIds);	
+	public List<Object[]> getGovtSchemeWiseBenefitMemberCount(Long locationType, Long locationValue,List<Long> constituencyIds);
+	public List<Object[]> getMandalWiseBenefitMemberCountByGovtScheme(Long locationType, Long locationValue,Long govtSchemeId,List<Long> constituencyIds);
 
 }
