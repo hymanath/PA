429620742b3c8a5466b1f2aad6cbfb3174ab36cd
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-25 09:46:38 +0000

    updated by srujana

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreCasteInfo.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/model/TdpCadreCasteInfo.java
@@ -2,13 +2,19 @@ package com.itgrids.partyanalyst.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "tdp_cadre_caste_info")
@@ -22,6 +28,10 @@ public class TdpCadreCasteInfo implements java.io.Serializable{
 	private Long count;
 	private Long casteCategoryId;
 	private Long tdpCadreEnrollmentId;
+	private VoterAgeRange voterAgeRange;
+	private Long voterAgeRangeId;
+	private String gender;
+	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -78,6 +88,30 @@ public class TdpCadreCasteInfo implements java.io.Serializable{
 	}
 	public void setTdpCadreEnrollmentId(Long tdpCadreEnrollmentId) {
 		this.tdpCadreEnrollmentId = tdpCadreEnrollmentId;
+	}
+	@ManyToOne(fetch = FetchType.LAZY )
+	@JoinColumn(name = "voter_age_range_id" , insertable = false, updatable = false)
+	@LazyToOne(LazyToOneOption.NO_PROXY)
+	@org.hibernate.annotations.NotFound(action=NotFoundAction.IGNORE)
+	public VoterAgeRange getVoterAgeRange() {
+		return voterAgeRange;
+	}
+	public void setVoterAgeRange(VoterAgeRange voterAgeRange) {
+		this.voterAgeRange = voterAgeRange;
+	}
+	@Column(name="voter_age_range_id")
+	public Long getVoterAgeRangeId() {
+		return voterAgeRangeId;
+	}
+	public void setVoterAgeRangeId(Long voterAgeRangeId) {
+		this.voterAgeRangeId = voterAgeRangeId;
+	}
+	@Column(name="gender")
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 	
 }
