10934f34352bb9c48f8ea420564b30d586d5e18e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-09-26 12:20:07 +0000

    commited by kondababu trainingCampDistrictDao get camp details

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampDistrictDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/ITrainingCampDistrictDAO.java
@@ -8,4 +8,5 @@ import com.itgrids.partyanalyst.model.TrainingCampDistrict;
 
 public interface ITrainingCampDistrictDAO extends GenericDao<TrainingCampDistrict, Long>{
 	public List<Object[]> getCampDetailsByDistrictIds(List<Long> districtIdsList);
+	public List<Object[]> getTrainingCampDetailsByCampIds(List<Long> trainingCampIdsList);
 }
