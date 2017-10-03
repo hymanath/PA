7bada6f75a572a6d4d62810d11c5554ece28534e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-03 12:50:16 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/impl/LoactionDetailsDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/impl/LoactionDetailsDashBoardService.java
@@ -1,9 +1,12 @@
 package com.itgrids.core.api.service.impl;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import com.itgrids.core.api.service.ILoactionDetailsDashBoardService;
 import com.itgrids.partyanalyst.dao.IPartyMeetingDAO;
+import com.itgrids.partyanalyst.dto.PartyMeetingsVO;
 import com.itgrids.partyanalyst.utils.CommonMethodsUtilService;
 
 public class LoactionDetailsDashBoardService implements ILoactionDetailsDashBoardService {
@@ -16,9 +19,20 @@ public class LoactionDetailsDashBoardService implements ILoactionDetailsDashBoar
 	}
 	public void setCommonMethodsUtilService(
 			CommonMethodsUtilService commonMethodsUtilService) {
-		commonMethodsUtilService = commonMethodsUtilService;
+		this.commonMethodsUtilService = commonMethodsUtilService;
 	}
-	
+	/*
+	 * Swadhin K Lenka
+	 * @see com.itgrids.core.api.service.ILoactionDetailsDashBoardService#getMeetingTypeWiseTotalMeetings(java.lang.Long, java.lang.Long, java.lang.String, java.lang.String)
+	 */
+	public List<PartyMeetingsVO> getMeetingTypeWiseTotalMeetings(Long locationLevel, Long locationId, String fromDateStr,String toDateStr){
+		try{
+			
+		}catch(Exception e){
+			LOG.error("Exception raised at getMeetingTypeWiseTotalMeetings() method of LoactionDetailsDashBoardService", e);
+		}
+		return null;
+	}
 	
 
 }
