6ce0a87a1ff7679f633d560c41f7d2e231f0138d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-05 09:44:04 +0000

    updated by -- sandeep

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IKaizalaAttachementTypeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.partyanalyst.dao;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.partyanalyst.model.KaizalaAttachementType;
-
-public interface IKaizalaAttachementTypeDAO extends GenericDao<KaizalaAttachementType, Long>{
-	public Long checkAttachementTypeExistence(String type);
-}
