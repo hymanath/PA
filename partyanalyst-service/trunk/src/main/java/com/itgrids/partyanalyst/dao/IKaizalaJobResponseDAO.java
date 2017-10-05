145e07c0eca66954a3eb1bd01d4b1cc906ffc937
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-05 10:33:38 +0000

    Updated by ---Teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IKaizalaJobResponseDAO.java
@@ -0,0 +1,8 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.KaizalaJobResponse;
+
+public interface IKaizalaJobResponseDAO extends GenericDao<KaizalaJobResponse, Long> {
+}
