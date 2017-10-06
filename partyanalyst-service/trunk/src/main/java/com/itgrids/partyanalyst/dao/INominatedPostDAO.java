713878f2c757ab0d36237d0fe61e642be190cb7f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-06 11:34:42 +0000

    updated by Hyma  : In constituency page Nominated Post Block detailed view.

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/INominatedPostDAO.java
@@ -75,7 +75,7 @@ public interface INominatedPostDAO extends GenericDao<NominatedPost, Long>{
 	 public List<Object[]> getLocationWiseNominatedPostAnalysisDetails(List<Long> locationValues, Long boardLevelId,Long searchLevelId,String type,List<Long> statusIds);
 	 public List<Object[]> getAllNominatedStatusListLevelWise(List<Long> boardLevelId,	List<Long> levelValues,Long levelId);
 	 public List<Object[]> getLocationWiseNominatedPostCandidateAgeRangeAndCasteCategorDetails(List<Long> locationValues,Long searchLevelId,List<Long> statusIdsList,String type);
-	 public List<Object[]> getAreaWiseDashboardCandidatesCountView(List<Long> locationValues,Long searchLevelId,List<Long> statusIds);
+	 public List<Object[]> getAreaWiseDashboardCandidatesCountView(List<Long> locationValues,Long searchLevelId,List<Long> statusIds,String type);
 	 public List<Object[]> getNominatedPositionWiseCandidates(List<Long> locationValues,Date startDate, Date endDate,Long locationTypeId,String year,Long boardLevelId,Long startIndex,Long endIndex);
 	 public List<Object[]> getNominatedPostLocationStatusWiseCount(List<Long> locationValuesList,Date startDate,Date endDate,Long locationType,Long boardLevelId);
 	 public List<Object[]> getDepartmentWisePostDetails(List<Long> locationValues,Date startDate, Date endDate,Long locationTypeId,String year,Long boardLevelId,Long deptId);
