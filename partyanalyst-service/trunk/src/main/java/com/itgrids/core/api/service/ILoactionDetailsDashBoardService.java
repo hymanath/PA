feb7b5066a2a1012cc971811cbd372dd519196d2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-07 10:23:32 +0000

    committed by swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/ILoactionDetailsDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/ILoactionDetailsDashBoardService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.itgrids.partyanalyst.dto.PartyMeetingsVO;
 
 public interface ILoactionDetailsDashBoardService {
-	public List<PartyMeetingsVO> getMeetingTypeWiseTotalMeetings(Long locationLevel, Long locationId, String fromDateStr,String toDateStr);
-	public List<PartyMeetingsVO> getMeetingLevelWiseTotalMeetings(Long locationLevel, Long locationId, String fromDateStr,String toDateStr);
-	public List<PartyMeetingsVO> getCommitteeMeetingStatistics(Long locationLevel, Long locationId, String fromDateStr,String toDateStr);
-	public List<PartyMeetingsVO> getSpecialMeetingStatistics(Long locationLevel, Long locationId, String fromDateStr,String toDateStr);
-	public List<PartyMeetingsVO> getBelowLevelMeetingConductedCount(Long locationLevel, Long locationId, String fromDateStr,String toDateStr);
+	public List<PartyMeetingsVO> getMeetingTypeWiseTotalMeetings(Long locationLevel, List<Long> locationIds, String fromDateStr,String toDateStr);
+	public List<PartyMeetingsVO> getMeetingLevelWiseTotalMeetings(Long locationLevel, List<Long> locationIds, String fromDateStr,String toDateStr);
+	public List<PartyMeetingsVO> getCommitteeMeetingStatistics(Long locationLevel, List<Long> locationIds, String fromDateStr,String toDateStr);
+	public List<PartyMeetingsVO> getSpecialMeetingStatistics(Long locationLevel, List<Long> locationIds, String fromDateStr,String toDateStr);
+	public List<PartyMeetingsVO> getBelowLevelMeetingConductedCount(Long locationLevel, List<Long> locationIds, String fromDateStr,String toDateStr);
 }
