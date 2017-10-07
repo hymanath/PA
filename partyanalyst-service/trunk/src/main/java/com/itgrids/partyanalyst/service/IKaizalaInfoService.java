7e87c6bff8feb8d79dc93c699a27d35c6e598746
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-07 09:22:38 +0000

    attachmentSaving  method remove from intreface - Nagarjuna Gowd

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IKaizalaInfoService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IKaizalaInfoService.java
@@ -2,5 +2,4 @@ package com.itgrids.partyanalyst.service;
 
 public interface IKaizalaInfoService {
 	public void saveEventResponses(final String output);
-	public void saveAttachmentInfo(final String output);
 }
