53ba3a5f1a01293ad6c085892a43043e3540ec42
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-09 13:11:46 +0000

    committed by swadhin

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LoactionDetailsDashBoardAction.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/LoactionDetailsDashBoardAction.java
@@ -12,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.itgrids.core.api.service.ILoactionDetailsDashBoardService;
+import com.itgrids.partyanalyst.dto.PartyMeetingDataVO;
 import com.itgrids.partyanalyst.dto.PartyMeetingsVO;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
@@ -24,7 +25,7 @@ public class LoactionDetailsDashBoardAction extends ActionSupport implements Ser
 	private String task;
 	
 	private List<PartyMeetingsVO> partyMeetingsVOs;
-	
+	private List<PartyMeetingDataVO> partyMeetingDataVOs;
 	private ILoactionDetailsDashBoardService loactionDetailsDashBoardService;
 	
 	public void setServletRequest(HttpServletRequest request) {
@@ -74,6 +75,14 @@ public class LoactionDetailsDashBoardAction extends ActionSupport implements Ser
 
 	public void setPartyMeetingsVOs(List<PartyMeetingsVO> partyMeetingsVOs) {
 		this.partyMeetingsVOs = partyMeetingsVOs;
+	}
+	
+	public List<PartyMeetingDataVO> getPartyMeetingDataVOs() {
+		return partyMeetingDataVOs;
+	}
+
+	public void setPartyMeetingDataVOs(List<PartyMeetingDataVO> partyMeetingDataVOs) {
+		this.partyMeetingDataVOs = partyMeetingDataVOs;
 	}
 
 	public String getMeetingTypeWiseTotalMeetings(){
@@ -179,6 +188,29 @@ public class LoactionDetailsDashBoardAction extends ActionSupport implements Ser
 				}
 			}
 			partyMeetingsVOs = loactionDetailsDashBoardService.getBelowLevelMeetingConductedCount(locationLevel,locationIdList,fromDateStr,toDateStr);
+			
+		}catch(Exception e){
+			LOG.error("Exception raised at getBelowLevelMeetingConductedCount() method of LoactionDetailsDashBoardAction", e);
+		}
+		return Action.SUCCESS;
+	}
+	public String getMeetingsDetails(){
+		LOG.info("Entered into getMeetingsDetails()  of LoactionDetailsDashBoardAction");
+		try{
+			jObj = new JSONObject(getTask());
+			Long belowlocationId = jObj.getLong("belowlocationId");
+			Long locationLevel = jObj.getLong("locationLevel");
+			String fromDateStr = jObj.getString("fromDateStr");
+			String toDateStr = jObj.getString("toDateStr");
+			List<Long> locationIdList = new ArrayList<Long>();
+			JSONArray locationIdArr=jObj.getJSONArray("locationIds");
+			if(locationIdArr!=null &&  locationIdArr.length()>0){
+				for( int i=0;i<locationIdArr.length();i++){
+					locationIdList.add(Long.valueOf(locationIdArr.getString(i)));
+				}
+			}
+			String meetingType = jObj.getString("meetingType");
+			partyMeetingDataVOs = loactionDetailsDashBoardService.getMeetingsDetails(belowlocationId,locationLevel,locationIdList,fromDateStr,toDateStr,meetingType);
 			
 		}catch(Exception e){
 			LOG.error("Exception raised at getBelowLevelMeetingConductedCount() method of LoactionDetailsDashBoardAction", e);
