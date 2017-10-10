d6d58ecd1cec799085129f05d6b73694cf012a50
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-10 11:23:06 +0000

    Updated by SANJEEV

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/ILocationWiseElectionInformationDetalsService.java
@@ -0,0 +1,11 @@
+package com.itgrids.core.api.service;
+
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.ElectionInformationVO;
+
+public interface ILocationWiseElectionInformationDetalsService {
+
+	public List<ElectionInformationVO> getElectionInformationLocationWiseStatus(Long locationTypeId,
+			Long locationValue,  List<Long> partyIdList, List<Long> electionYrs,List<Long> electionScopeIds, List<String> subTypes);
+}
