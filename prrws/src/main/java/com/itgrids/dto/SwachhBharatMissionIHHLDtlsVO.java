8e710168d893d2ade0737885884717568b653eb5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-13 11:45:28 +0000

    Updated by Santosh

---
+++ prrws/src/main/java/com/itgrids/dto/SwachhBharatMissionIHHLDtlsVO.java
@@ -0,0 +1,110 @@
+package com.itgrids.dto;
+
+import java.util.List;
+
+public class SwachhBharatMissionIHHLDtlsVO {
+
+	private Long target;
+	private Long achivement;
+	private Double percentage;
+	private Long locationId;
+	private String name;
+	private Long grounded;
+	private Long noTGrounded;
+	private Long completed;
+	private Long inProgress;
+	private List<SwachhBharatMissionIHHLDtlsVO> subList;
+	
+	private Long districtCount;
+	private Long constituencyCount;
+	private Long mandalCount;
+	
+	private String range;
+	
+	public Long getTarget() {
+		return target;
+	}
+	public void setTarget(Long target) {
+		this.target = target;
+	}
+	public Long getAchivement() {
+		return achivement;
+	}
+	public void setAchivement(Long achivement) {
+		this.achivement = achivement;
+	}
+	public Double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(Double percentage) {
+		this.percentage = percentage;
+	}
+	public Long getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(Long locationId) {
+		this.locationId = locationId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRange() {
+		return range;
+	}
+	public void setRange(String range) {
+		this.range = range;
+	}
+	public Long getGrounded() {
+		return grounded;
+	}
+	public void setGrounded(Long grounded) {
+		this.grounded = grounded;
+	}
+	public Long getNoTGrounded() {
+		return noTGrounded;
+	}
+	public void setNoTGrounded(Long noTGrounded) {
+		this.noTGrounded = noTGrounded;
+	}
+	public Long getCompleted() {
+		return completed;
+	}
+	public void setCompleted(Long completed) {
+		this.completed = completed;
+	}
+	public Long getInProgress() {
+		return inProgress;
+	}
+	public void setInProgress(Long inProgress) {
+		this.inProgress = inProgress;
+	}
+	public List<SwachhBharatMissionIHHLDtlsVO> getSubList() {
+		return subList;
+	}
+	public void setSubList(List<SwachhBharatMissionIHHLDtlsVO> subList) {
+		this.subList = subList;
+	}
+	public Long getDistrictCount() {
+		return districtCount;
+	}
+	public void setDistrictCount(Long districtCount) {
+		this.districtCount = districtCount;
+	}
+	public Long getConstituencyCount() {
+		return constituencyCount;
+	}
+	public void setConstituencyCount(Long constituencyCount) {
+		this.constituencyCount = constituencyCount;
+	}
+	public Long getMandalCount() {
+		return mandalCount;
+	}
+	public void setMandalCount(Long mandalCount) {
+		this.mandalCount = mandalCount;
+	}
+	
+	
+}
---
+++ prrws/src/main/java/com/itgrids/service/impl/SwachhBharatMissionIHHLService.java
@@ -0,0 +1,105 @@
+package com.itgrids.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.SwachhBharatMissionIHHLDtlsVO;
+import com.itgrids.service.ISwachhBharatMissionIHHLService;
+
+@Service
+@Transactional
+public class SwachhBharatMissionIHHLService implements ISwachhBharatMissionIHHLService{
+
+	private static final Logger LOG = Logger.getLogger(SwachhBharatMissionIHHLService.class);
+	
+	
+	public SwachhBharatMissionIHHLDtlsVO getSwachhBharatMissionOverviewDtls(InputVO inputVO) {
+		SwachhBharatMissionIHHLDtlsVO resultVO = new SwachhBharatMissionIHHLDtlsVO();
+		try {
+			resultVO.setTarget(10000l);
+			resultVO.setAchivement(8000l);
+		} catch (Exception e) {
+			LOG.error("Exception occured at getSwachhBharatMissionOverviewDtls() in SwachhBharatMissionIHHLService class",e);
+		}
+		return resultVO;
+	}
+	public SwachhBharatMissionIHHLDtlsVO getSwachhBharatMissionStatusOverviewDtls(InputVO inputVO) {
+		SwachhBharatMissionIHHLDtlsVO resultVO = new SwachhBharatMissionIHHLDtlsVO();
+		try {
+			resultVO.setTarget(10000l);
+			resultVO.setGrounded(2000l);
+			resultVO.setNoTGrounded(8000l);
+			resultVO.setInProgress(1000l);
+			resultVO.setCompleted(1000l);
+		} catch (Exception e) {
+			LOG.error("Exception occured at getSwachhBharatMissionStatusDtls() in SwachhBharatMissionIHHLService class",e);
+		}
+		return resultVO;
+	}
+	public List<SwachhBharatMissionIHHLDtlsVO> getIHHLCategoryWiseAnalysis(InputVO inputVO) {
+		List<SwachhBharatMissionIHHLDtlsVO> resultList = new ArrayList<SwachhBharatMissionIHHLDtlsVO>(0);
+		try {
+			SwachhBharatMissionIHHLDtlsVO vo = new SwachhBharatMissionIHHLDtlsVO();
+			vo.setRange("100% TO 76%");
+			vo.setDistrictCount(10l);
+			vo.setConstituencyCount(5l);
+			vo.setMandalCount(2l);
+			SwachhBharatMissionIHHLDtlsVO vo1 = new SwachhBharatMissionIHHLDtlsVO();
+			vo1.setRange("75% TO 51%");
+			vo1.setDistrictCount(10l);
+			vo1.setConstituencyCount(5l);
+			vo1.setMandalCount(2l);
+			resultList.add(vo);
+			resultList.add(vo1);
+		} catch (Exception e) {
+			LOG.error("Exception occured at getIHHLCategoryWiseAnalysis() in SwachhBharatMissionIHHLService class",e);
+		}
+		return resultList;
+	}
+	public List<SwachhBharatMissionIHHLDtlsVO> getIHHLAchivementProgressDtls(InputVO inputVO) {
+		List<SwachhBharatMissionIHHLDtlsVO> resultList = new ArrayList<SwachhBharatMissionIHHLDtlsVO>(0);
+		try {
+			SwachhBharatMissionIHHLDtlsVO timeRangeVO = new SwachhBharatMissionIHHLDtlsVO();
+			timeRangeVO.setRange("01-08-2017");
+			timeRangeVO.setTarget(10000l);
+			timeRangeVO.setCompleted(1000l);
+			SwachhBharatMissionIHHLDtlsVO timeRangeVO1 = new SwachhBharatMissionIHHLDtlsVO();
+			timeRangeVO1.setRange("02-08-2017");
+			timeRangeVO1.setTarget(10000l);
+			timeRangeVO1.setCompleted(2000l);
+			SwachhBharatMissionIHHLDtlsVO timeRangeVO2 = new SwachhBharatMissionIHHLDtlsVO();
+			timeRangeVO2.setRange("03-08-2017");
+			timeRangeVO2.setTarget(10000l);
+			timeRangeVO2.setCompleted(3000l);
+			resultList.add(timeRangeVO);
+			resultList.add(timeRangeVO1);
+			resultList.add(timeRangeVO2);
+		} catch (Exception e) {
+			LOG.error("Exception occured at getIHHLAchivementProgressDtls() in SwachhBharatMissionIHHLService class",e);
+		}
+		return resultList;
+	}
+	public List<SwachhBharatMissionIHHLDtlsVO> getSwachhBharatMissionLocationWiseDetails(InputVO inputVO) {
+		List<SwachhBharatMissionIHHLDtlsVO> resultList = new ArrayList<SwachhBharatMissionIHHLDtlsVO>(0);
+		try {
+			SwachhBharatMissionIHHLDtlsVO locationVO = new SwachhBharatMissionIHHLDtlsVO();
+			locationVO.setLocationId(1l);
+			locationVO.setName("Andhra Pradesh");
+			locationVO.setTarget(10000l);
+			locationVO.setGrounded(2000l);
+			locationVO.setNoTGrounded(8000l);
+			locationVO.setInProgress(1000l);
+			locationVO.setCompleted(1000l);
+			locationVO.setPercentage(50d);
+			resultList.add(locationVO);
+		} catch (Exception e) {
+			LOG.error("Exception occured at getSwachhBharatMissionLocationWiseDetails() in SwachhBharatMissionIHHLService class",e);
+		}
+		return resultList;
+	}
+}
