688dbb4b32ad98e39ee1afb9b30090c286b6fd30
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-13 15:52:59 +0000

    Updated by Santosh

--- prrws/src/main/java/com/itgrids/service/impl/SwachhBharatMissionIHHLService.java
+++ prrws/src/main/java/com/itgrids/service/impl/SwachhBharatMissionIHHLService.java
@@ -1,15 +1,22 @@
 package com.itgrids.service.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.itgrids.dto.InputVO;
 import com.itgrids.dto.SwachhBharatMissionIHHLDtlsVO;
 import com.itgrids.service.ISwachhBharatMissionIHHLService;
+import com.itgrids.service.integration.external.WebServiceUtilService;
+import com.sun.jersey.api.client.ClientResponse;
 
 @Service
 @Transactional
@@ -17,6 +24,8 @@ public class SwachhBharatMissionIHHLService implements ISwachhBharatMissionIHHLS
 
 	private static final Logger LOG = Logger.getLogger(SwachhBharatMissionIHHLService.class);
 	
+	@Autowired
+	private WebServiceUtilService webServiceUtilService;
 	
 	/**
 	 * @author Santosh Kumar Verma
@@ -28,12 +37,89 @@ public class SwachhBharatMissionIHHLService implements ISwachhBharatMissionIHHLS
 	public SwachhBharatMissionIHHLDtlsVO getSwachhBharatMissionOverviewDtls(InputVO inputVO) {
 		SwachhBharatMissionIHHLDtlsVO resultVO = new SwachhBharatMissionIHHLDtlsVO();
 		try {
-			resultVO.setTarget(10000l);
-			resultVO.setAchivement(8000l);
+			String str = convertingInputVOToString(inputVO);
+			ClientResponse response = webServiceUtilService.callWebService("http://125.17.121.167/rwsapwebapi/api/IHHLDashBoardUI/GetIHHLDashBoardUIDetails", str);
+			
+			if(response.getStatus() != 200){
+	 	    	  throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
+	 	      }else{
+	 	    	 String output = response.getEntity(String.class);
+	 	    	if(output != null && !output.isEmpty()){
+	 	    		JSONObject jsonObject = new JSONObject(output);
+	 	    		JSONArray locationDtlsArr = jsonObject.getJSONArray("MinisterDBStateFundMngmntData");
+	 	    		if(locationDtlsArr != null && locationDtlsArr.length() > 0){
+	 	    			for(int i=0;i<locationDtlsArr.length();i++){
+	 	    				JSONObject jObj = (JSONObject) locationDtlsArr.get(i);
+	 	    				resultVO.setTarget(jObj.has("TARGET") ? jObj.getLong("TARGET"):0l);
+	 	    				resultVO.setGrounded(jObj.has("GROUNDED") ? jObj.getLong("GROUNDED"):0l);
+	 	    				resultVO.setNoTGrounded(jObj.has("NOTGROUNDED") ? jObj.getLong("NOTGROUNDED"):0l);
+	 	    				resultVO.setInProgress(jObj.has("INPROGRESS") ? jObj.getLong("INPROGRESS"):0l);
+	 	    				resultVO.setCompleted(jObj.has("COMPLETEED") ? jObj.getLong("COMPLETEED"):0l);
+	 	    			}	
+	 	    		}
+	 	    		JSONArray categoryWiseDataArr = jsonObject.getJSONArray("categorycount");
+	 	    		if(categoryWiseDataArr != null && categoryWiseDataArr.length() > 0){
+	 	    			List<SwachhBharatMissionIHHLDtlsVO> categoryList = new ArrayList<>();
+	 	    			for(int i=0;i<categoryWiseDataArr.length();i++){
+	 	    				SwachhBharatMissionIHHLDtlsVO categoryVO = new SwachhBharatMissionIHHLDtlsVO();
+	 	    				JSONObject jObj = (JSONObject) categoryWiseDataArr.get(i);
+	 	    				categoryVO.setRange(jObj.has("CAT") ? jObj.getString("CAT"):"");
+	 	    				String[] strArr = getRankIdBasedOnCategory(categoryVO.getRange());
+	 	    				if (strArr.length > 0) {
+	 	    					categoryVO.setId(Long.valueOf(strArr[0]));
+	 	    					categoryVO.setName(strArr[1]);
+	 	    				}
+	 	    				categoryVO.setDistrictCount(jObj.has("D") ? jObj.getLong("D"):0l);
+	 	    				categoryVO.setConstituencyCount(jObj.has("C") ? jObj.getLong("C"):0l);
+	 	    				categoryVO.setMandalCount(jObj.has("M") ? jObj.getLong("M"):0l);
+	 	    				categoryList.add(categoryVO);
+	 	    			}	
+	 	    			if (categoryList.size() > 0) {
+	 	    				Collections.sort(categoryList, sortListBasedOnId);
+	 	    				resultVO.setSubList(categoryList);
+	 	    			}
+	 	    		}
+	 	    	}
+	 	      }  
 		} catch (Exception e) {
 			LOG.error("Exception occured at getSwachhBharatMissionOverviewDtls() in SwachhBharatMissionIHHLService class",e);
 		}
 		return resultVO;
+	}
+	public static Comparator<SwachhBharatMissionIHHLDtlsVO> sortListBasedOnId = new Comparator<SwachhBharatMissionIHHLDtlsVO>() {
+		public int compare(SwachhBharatMissionIHHLDtlsVO o1, SwachhBharatMissionIHHLDtlsVO o2) {
+			if(o1.getId() != null && o2.getId() != null){
+				return o1.getId().compareTo(o2.getId());
+			}
+			return 0;
+		}
+	};
+	private String[] getRankIdBasedOnCategory(String category) {
+		String[] strArr = new String[2];
+		 try {
+			 
+			 if (category != null && category.trim().length() > 0) {
+				 if(category.equalsIgnoreCase("100 TO 76")){
+					 strArr[0] = "1";
+					 strArr[1] = "A";
+				 } else if (category.equalsIgnoreCase("75 TO 51")) {
+					 strArr[0] = "2";
+					 strArr[1] = "B";
+				 }else if (category.equalsIgnoreCase("50 TO 26")) {
+					 strArr[0] = "3";
+					 strArr[1] = "C";
+				 }else if (category.equalsIgnoreCase("25 TO 0")){
+					 strArr[0] = "4";
+					 strArr[1] = "D";
+				 } else {
+					 strArr[0] = "5";
+					 strArr[1] = "E";
+				 }
+			 }
+		 } catch (Exception e) {
+			 LOG.error("Exception occured at getRankIdBasedOnCategory() in SwachhBharatMissionIHHLService class",e); 
+		 }
+		 return strArr;
 	}
 	/**
 	 * @author Santosh Kumar Verma
@@ -136,5 +222,33 @@ public class SwachhBharatMissionIHHLService implements ISwachhBharatMissionIHHLS
 			LOG.error("Exception occured at getSwachhBharatMissionLocationWiseDetails() in SwachhBharatMissionIHHLService class",e);
 		}
 		return resultList;
+	}
+	public String convertingInputVOToString(InputVO inputVO){
+		String str = "";
+		try {
+			str = "{";
+			
+			if(inputVO.getFromDate() != null )
+			/*	str += "\"fromMonth\" : \""+inputVO.getFromDate()+"\",";
+			if(inputVO.getToDate() != null)
+				str += "\"toMonth\" : \""+inputVO.getToDate()+"\",";*/
+			if(inputVO.getLocation() != null)
+				str += "\"Location\" : \""+inputVO.getLocation()+"\",";
+			/*if(inputVO.getLocationIdStr() != null)
+				str += "\"locationId\" : \""+inputVO.getLocationIdStr()+"\",";else*/
+			 if(inputVO.getLocationId() != null)
+				str += "\"LocationID\" : \""+inputVO.getLocationId()+"\",";
+			if(inputVO.getSubLocation() != null)
+				str += "\"SubLocation\" : \""+inputVO.getSubLocation()+"\",";
+			
+			if(str.length() > 1)
+				str = str.substring(0,str.length()-1);
+			
+			str += "}";
+			
+		} catch (Exception e) {
+			LOG.error("Exception Occured in convertingInputVOToString() method, Exception - ",e);
+		}
+		return str;
 	}
 }
