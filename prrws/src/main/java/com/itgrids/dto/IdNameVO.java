0e441c2e15df06c43c063af3ba1ae71b79c84a2f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-17 10:23:18 +0000

    Exceded Target Works Details in RWS - SRAVANTH.

--- prrws/src/main/java/com/itgrids/dto/IdNameVO.java
+++ prrws/src/main/java/com/itgrids/dto/IdNameVO.java
@@ -23,6 +23,26 @@ public class IdNameVO implements Serializable{
 	private List<Long> componentIds = new ArrayList<Long>(0);
 	private Long orderNo;
 	
+	private Long workId;
+	private String workName;
+	private String adminDate;
+	private String adminNo;
+	private Long sanctionAmount;
+	private String typeOfAssestName;
+	private String groundedDate;
+	private String targetDate;
+	private List<IdNameVO> completedList = new ArrayList<IdNameVO>(0);
+	private Long pwsCount = 0L;
+	private Long cpwsCount = 0L;
+	private Long pwsAmount = 0L;
+	private Long cpwsAmount = 0L;
+	
+	private Long onGoingPwsCount = 0L;
+	private Long onGoingCpwsCount = 0L;
+	private Long onGoingPwsAmount = 0L;
+	private Long onGoingCpwsAmount = 0L;
+	
+	
 	public IdNameVO() {
 		super();
 	}
@@ -115,5 +135,106 @@ public class IdNameVO implements Serializable{
 	public void setComponentNameList(List<String> componentNameList) {
 		this.componentNameList = componentNameList;
 	}
-	
+	public Long getWorkId() {
+		return workId;
+	}
+	public void setWorkId(Long workId) {
+		this.workId = workId;
+	}
+	public String getWorkName() {
+		return workName;
+	}
+	public void setWorkName(String workName) {
+		this.workName = workName;
+	}
+	public String getAdminDate() {
+		return adminDate;
+	}
+	public void setAdminDate(String adminDate) {
+		this.adminDate = adminDate;
+	}
+	public String getAdminNo() {
+		return adminNo;
+	}
+	public void setAdminNo(String adminNo) {
+		this.adminNo = adminNo;
+	}
+	public Long getSanctionAmount() {
+		return sanctionAmount;
+	}
+	public void setSanctionAmount(Long sanctionAmount) {
+		this.sanctionAmount = sanctionAmount;
+	}
+	public String getTypeOfAssestName() {
+		return typeOfAssestName;
+	}
+	public void setTypeOfAssestName(String typeOfAssestName) {
+		this.typeOfAssestName = typeOfAssestName;
+	}
+	public String getGroundedDate() {
+		return groundedDate;
+	}
+	public void setGroundedDate(String groundedDate) {
+		this.groundedDate = groundedDate;
+	}
+	public String getTargetDate() {
+		return targetDate;
+	}
+	public void setTargetDate(String targetDate) {
+		this.targetDate = targetDate;
+	}
+	public List<IdNameVO> getCompletedList() {
+		return completedList;
+	}
+	public void setCompletedList(List<IdNameVO> completedList) {
+		this.completedList = completedList;
+	}
+	public Long getPwsCount() {
+		return pwsCount;
+	}
+	public void setPwsCount(Long pwsCount) {
+		this.pwsCount = pwsCount;
+	}
+	public Long getCpwsCount() {
+		return cpwsCount;
+	}
+	public void setCpwsCount(Long cpwsCount) {
+		this.cpwsCount = cpwsCount;
+	}
+	public Long getPwsAmount() {
+		return pwsAmount;
+	}
+	public void setPwsAmount(Long pwsAmount) {
+		this.pwsAmount = pwsAmount;
+	}
+	public Long getCpwsAmount() {
+		return cpwsAmount;
+	}
+	public void setCpwsAmount(Long cpwsAmount) {
+		this.cpwsAmount = cpwsAmount;
+	}
+	public Long getOnGoingPwsCount() {
+		return onGoingPwsCount;
+	}
+	public void setOnGoingPwsCount(Long onGoingPwsCount) {
+		this.onGoingPwsCount = onGoingPwsCount;
+	}
+	public Long getOnGoingCpwsCount() {
+		return onGoingCpwsCount;
+	}
+	public void setOnGoingCpwsCount(Long onGoingCpwsCount) {
+		this.onGoingCpwsCount = onGoingCpwsCount;
+	}
+	public Long getOnGoingPwsAmount() {
+		return onGoingPwsAmount;
+	}
+	public void setOnGoingPwsAmount(Long onGoingPwsAmount) {
+		this.onGoingPwsAmount = onGoingPwsAmount;
+	}
+	public Long getOnGoingCpwsAmount() {
+		return onGoingCpwsAmount;
+	}
+	public void setOnGoingCpwsAmount(Long onGoingCpwsAmount) {
+		this.onGoingCpwsAmount = onGoingCpwsAmount;
+	}
 }
