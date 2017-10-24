265de6bee5d208a98cafea6f46d672fd7c45e758
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-24 06:51:59 +0000

    Updated by Santosh

--- prrws/src/main/java/com/itgrids/service/IItcDashboardService.java
+++ prrws/src/main/java/com/itgrids/service/IItcDashboardService.java
@@ -18,14 +18,8 @@ import com.itgrids.dto.MeesevaKPIDtlsVO;
 
 public interface IItcDashboardService {
 
-	public ItecOverviewVO getPromotionsOverviewByDepartmentType(InputVO inputVO);
-	public List<ItInformationDtlsVO> getPromotionsDetailedDepartmentWise(InputVO inputVO);
-	public EofficeDtlsVO getEOfficePendencyDtlsByDepartmentType(InputVO inputVO);
-	public List<EofficeDtlsVO> getEOfficePendencyByDepartmentAndDayWise(InputVO inputVO);
 	public List<MeesevaDtlsVO> getMeesevaSLAOverviewDtls(InputVO inputVO);
 	public List<MeesevaDtlsVO> getMeesevaSLAMonitoringDtlsDepartmentWise(InputVO inputVO);
-	public List<MeesevaKPIDtlsVO> getMeesevaKPIIndicatorsProgressDtls(InputVO inputVO);
-	public List<MeesevaKPIDtlsVO> getMeesevaKPIIndicatorsPeriodWise(InputVO inputVO);
 	public CmEoDBDtlsVO getCMEDOBOverview(InputVO inputVO);
 	public List<CmEoDBDtlsVO> getCMEDOBReportStatusWise(InputVO inputVO);
 	public ApInnovationSocietyOverviewVO getAPInnovationSocietyOverview(InputVO inputVO);
