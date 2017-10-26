4bb56f53b44c3352789b269d757f8611cfe6338d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-26 06:15:45 +0000

    Updated by ---Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtSchemesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IGovtSchemesDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.ActivityAttendanceAcceptance;
 import com.itgrids.partyanalyst.model.GovtSchemes;
 
 public interface IGovtSchemesDAO extends GenericDao<GovtSchemes,Long>{
+	public List<Object[]> getAllSchemeDetails();
 }
