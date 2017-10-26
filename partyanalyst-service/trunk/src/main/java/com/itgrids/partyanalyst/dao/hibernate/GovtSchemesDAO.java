750e950ab50a9b3783c53c9063ca922146943aaf
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-26 06:15:32 +0000

    Updated by ---Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtSchemesDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/GovtSchemesDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IGovtSchemesDAO;
 import com.itgrids.partyanalyst.model.GovtSchemes;
@@ -11,5 +14,10 @@ public class GovtSchemesDAO extends GenericDaoHibernate<GovtSchemes, Long> imple
 		super(GovtSchemes.class);
 		
 	}
-	
+	@SuppressWarnings("unchecked")
+	public List<Object[]> getAllSchemeDetails(){
+		Query query = getSession().createQuery("select distinct model.govtSchemesId,model.schemeName from GovtSchemes model");
+		
+		return query.list();
+	}
 }
