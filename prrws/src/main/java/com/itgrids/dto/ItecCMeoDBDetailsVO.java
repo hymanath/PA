1a436a211ff71d85244202b4ecc63f7bdafaada0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-10-26 09:48:15 +0000

    ITEC-CM oeDB Commit By - Nandhini

---
+++ prrws/src/main/java/com/itgrids/dto/ItecCMeoDBDetailsVO.java
@@ -0,0 +1,188 @@
+package com.itgrids.dto;
+
+public class ItecCMeoDBDetailsVO {
+
+	private String dashboardName;
+	private String clearenceName;
+	private String totalApplications;
+	private String totalApproved;
+	private String totalRejected;
+	private String totalPending;
+	private String pendingWithInSLA;
+	private String pendingBeyondSLA;
+	private String totalReApproved;
+	private Long dashBoardNO;
+	private Long clearenceId;
+	private String lowApproveClearenceName;
+	private String lowRejectedClearenceName;
+	private String lowPendingClearenceName;
+	private String lowApprDashBoardName;
+	private String lowRejctedDashBoardName;
+	private String lowPendingDashBoardName;
+	private String highRejectedClearenceName;
+	private String highRejectedDashboardName;
+	private String highPendingClearenceName;
+	private String highPendingDashboardName;
+	private String lowApproval;
+	private String highRejected;
+	private String lowRejected;
+	private String highPending;
+	private String lowPending;
+	
+	public String getDashboardName() {
+		return dashboardName;
+	}
+	public void setDashboardName(String dashboardName) {
+		this.dashboardName = dashboardName;
+	}
+	public String getClearenceName() {
+		return clearenceName;
+	}
+	public void setClearenceName(String clearenceName) {
+		this.clearenceName = clearenceName;
+	}
+	public String getTotalApplications() {
+		return totalApplications;
+	}
+	public void setTotalApplications(String totalApplications) {
+		this.totalApplications = totalApplications;
+	}
+	public String getTotalApproved() {
+		return totalApproved;
+	}
+	public void setTotalApproved(String totalApproved) {
+		this.totalApproved = totalApproved;
+	}
+	public String getTotalRejected() {
+		return totalRejected;
+	}
+	public void setTotalRejected(String totalRejected) {
+		this.totalRejected = totalRejected;
+	}
+	public String getTotalPending() {
+		return totalPending;
+	}
+	public void setTotalPending(String totalPending) {
+		this.totalPending = totalPending;
+	}
+	public String getPendingWithInSLA() {
+		return pendingWithInSLA;
+	}
+	public void setPendingWithInSLA(String pendingWithInSLA) {
+		this.pendingWithInSLA = pendingWithInSLA;
+	}
+	public String getPendingBeyondSLA() {
+		return pendingBeyondSLA;
+	}
+	public void setPendingBeyondSLA(String pendingBeyondSLA) {
+		this.pendingBeyondSLA = pendingBeyondSLA;
+	}
+	public String getTotalReApproved() {
+		return totalReApproved;
+	}
+	public void setTotalReApproved(String totalReApproved) {
+		this.totalReApproved = totalReApproved;
+	}
+	public Long getDashBoardNO() {
+		return dashBoardNO;
+	}
+	public void setDashBoardNO(Long dashBoardNO) {
+		this.dashBoardNO = dashBoardNO;
+	}
+	public Long getClearenceId() {
+		return clearenceId;
+	}
+	public void setClearenceId(Long clearenceId) {
+		this.clearenceId = clearenceId;
+	}
+	public String getLowApproveClearenceName() {
+		return lowApproveClearenceName;
+	}
+	public void setLowApproveClearenceName(String lowApproveClearenceName) {
+		this.lowApproveClearenceName = lowApproveClearenceName;
+	}
+	public String getLowRejectedClearenceName() {
+		return lowRejectedClearenceName;
+	}
+	public void setLowRejectedClearenceName(String lowRejectedClearenceName) {
+		this.lowRejectedClearenceName = lowRejectedClearenceName;
+	}
+	public String getLowPendingClearenceName() {
+		return lowPendingClearenceName;
+	}
+	public void setLowPendingClearenceName(String lowPendingClearenceName) {
+		this.lowPendingClearenceName = lowPendingClearenceName;
+	}
+	public String getLowApprDashBoardName() {
+		return lowApprDashBoardName;
+	}
+	public void setLowApprDashBoardName(String lowApprDashBoardName) {
+		this.lowApprDashBoardName = lowApprDashBoardName;
+	}
+	public String getLowRejctedDashBoardName() {
+		return lowRejctedDashBoardName;
+	}
+	public void setLowRejctedDashBoardName(String lowRejctedDashBoardName) {
+		this.lowRejctedDashBoardName = lowRejctedDashBoardName;
+	}
+	public String getLowPendingDashBoardName() {
+		return lowPendingDashBoardName;
+	}
+	public void setLowPendingDashBoardName(String lowPendingDashBoardName) {
+		this.lowPendingDashBoardName = lowPendingDashBoardName;
+	}
+	public String getHighRejectedClearenceName() {
+		return highRejectedClearenceName;
+	}
+	public void setHighRejectedClearenceName(String highRejectedClearenceName) {
+		this.highRejectedClearenceName = highRejectedClearenceName;
+	}
+	public String getHighRejectedDashboardName() {
+		return highRejectedDashboardName;
+	}
+	public void setHighRejectedDashboardName(String highRejectedDashboardName) {
+		this.highRejectedDashboardName = highRejectedDashboardName;
+	}
+	public String getHighPendingClearenceName() {
+		return highPendingClearenceName;
+	}
+	public void setHighPendingClearenceName(String highPendingClearenceName) {
+		this.highPendingClearenceName = highPendingClearenceName;
+	}
+	public String getHighPendingDashboardName() {
+		return highPendingDashboardName;
+	}
+	public void setHighPendingDashboardName(String highPendingDashboardName) {
+		this.highPendingDashboardName = highPendingDashboardName;
+	}
+	public String getLowApproval() {
+		return lowApproval;
+	}
+	public void setLowApproval(String lowApproval) {
+		this.lowApproval = lowApproval;
+	}
+	public String getHighRejected() {
+		return highRejected;
+	}
+	public void setHighRejected(String highRejected) {
+		this.highRejected = highRejected;
+	}
+	public String getLowRejected() {
+		return lowRejected;
+	}
+	public void setLowRejected(String lowRejected) {
+		this.lowRejected = lowRejected;
+	}
+	public String getHighPending() {
+		return highPending;
+	}
+	public void setHighPending(String highPending) {
+		this.highPending = highPending;
+	}
+	public String getLowPending() {
+		return lowPending;
+	}
+	public void setLowPending(String lowPending) {
+		this.lowPending = lowPending;
+	}
+}
