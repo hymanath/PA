be130fe4ae05f6f5e503ad7a534983bb952af6ed
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-03 13:22:34 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IBiometricEmployeeAttendenceDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.BiometricEmployeeAttendence;
+
+public interface IBiometricEmployeeAttendenceDAO extends GenericDao<BiometricEmployeeAttendence, Long> {
+
+}
