3dd0a3df1b003c39e7c258eebeaa420233f531ba
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-03 13:23:13 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IBioMetricOrgDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.BioMetricOrgDetails;
+
+public interface IBioMetricOrgDetailsDAO extends GenericDao<BioMetricOrgDetails, Long> {
+
+}
