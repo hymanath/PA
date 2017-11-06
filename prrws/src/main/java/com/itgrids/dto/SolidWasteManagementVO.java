c8a33f476421f72379631aa2a7c56260e9590bd2
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-06 10:30:36 +0000

    updated by swapna

---
+++ prrws/src/main/java/com/itgrids/dto/SolidWasteManagementVO.java
@@ -0,0 +1,178 @@
+package com.itgrids.dto;
+
+public class SolidWasteManagementVO {
+	
+	private String id;
+	private String name;
+	private Long mgnres=0L;
+	private Long pr=0L;
+	private Long publicType=0L;
+	private Long tractor=0L;
+	private Long auto=0L;
+	private Long tricycle=0L;
+	private Long evehicle=0L;
+	private Long rfidTags=0L;
+	private Long farmers=0L;
+	private Long nadap=0L;
+	private Long vermi=0L;
+	private Long onekg=0L;
+	private Long fivekg=0L;
+	private Long tenkg=0L;
+	private Long twentyFivekg=0L;
+	private Long fiftykg=0L;
+	private Long vermiStock=0L;
+	private Long farmerCollection=0L;
+	private Long houseCollecion=0L;
+	private Long swmCollection=0L;
+	private Long blocks=0L;
+	private Long rfidTracking=0L;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getMgnres() {
+		return mgnres;
+	}
+	public void setMgnres(Long mgnres) {
+		this.mgnres = mgnres;
+	}
+	public Long getPr() {
+		return pr;
+	}
+	public void setPr(Long pr) {
+		this.pr = pr;
+	}
+	public Long getPublicType() {
+		return publicType;
+	}
+	public void setPublicType(Long publicType) {
+		this.publicType = publicType;
+	}
+	public Long getTractor() {
+		return tractor;
+	}
+	public void setTractor(Long tractor) {
+		this.tractor = tractor;
+	}
+	public Long getAuto() {
+		return auto;
+	}
+	public void setAuto(Long auto) {
+		this.auto = auto;
+	}
+	public Long getTricycle() {
+		return tricycle;
+	}
+	public void setTricycle(Long tricycle) {
+		this.tricycle = tricycle;
+	}
+	public Long getEvehicle() {
+		return evehicle;
+	}
+	public void setEvehicle(Long evehicle) {
+		this.evehicle = evehicle;
+	}
+	public Long getRfidTags() {
+		return rfidTags;
+	}
+	public void setRfidTags(Long rfidTags) {
+		this.rfidTags = rfidTags;
+	}
+	public Long getFarmers() {
+		return farmers;
+	}
+	public void setFarmers(Long farmers) {
+		this.farmers = farmers;
+	}
+	public Long getNadap() {
+		return nadap;
+	}
+	public void setNadap(Long nadap) {
+		this.nadap = nadap;
+	}
+	public Long getVermi() {
+		return vermi;
+	}
+	public void setVermi(Long vermi) {
+		this.vermi = vermi;
+	}
+	public Long getOnekg() {
+		return onekg;
+	}
+	public void setOnekg(Long onekg) {
+		this.onekg = onekg;
+	}
+	public Long getFivekg() {
+		return fivekg;
+	}
+	public void setFivekg(Long fivekg) {
+		this.fivekg = fivekg;
+	}
+	public Long getTenkg() {
+		return tenkg;
+	}
+	public void setTenkg(Long tenkg) {
+		this.tenkg = tenkg;
+	}
+	public Long getTwentyFivekg() {
+		return twentyFivekg;
+	}
+	public void setTwentyFivekg(Long twentyFivekg) {
+		this.twentyFivekg = twentyFivekg;
+	}
+	public Long getFiftykg() {
+		return fiftykg;
+	}
+	public void setFiftykg(Long fiftykg) {
+		this.fiftykg = fiftykg;
+	}
+	public Long getVermiStock() {
+		return vermiStock;
+	}
+	public void setVermiStock(Long vermiStock) {
+		this.vermiStock = vermiStock;
+	}
+	public Long getFarmerCollection() {
+		return farmerCollection;
+	}
+	public void setFarmerCollection(Long farmerCollection) {
+		this.farmerCollection = farmerCollection;
+	}
+	public Long getHouseCollecion() {
+		return houseCollecion;
+	}
+	public void setHouseCollecion(Long houseCollecion) {
+		this.houseCollecion = houseCollecion;
+	}
+	public Long getSwmCollection() {
+		return swmCollection;
+	}
+	public void setSwmCollection(Long swmCollection) {
+		this.swmCollection = swmCollection;
+	}
+	public Long getBlocks() {
+		return blocks;
+	}
+	public void setBlocks(Long blocks) {
+		this.blocks = blocks;
+	}
+	public Long getRfidTracking() {
+		return rfidTracking;
+	}
+	public void setRfidTracking(Long rfidTracking) {
+		this.rfidTracking = rfidTracking;
+	}
+	
+	
+	
+	
+
+}
