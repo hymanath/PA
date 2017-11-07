040338d56aa5fbd7f3dee2712efc31aa6e3cc605
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-07 08:40:14 +0000

    Updated By Hyma  : Solid Waste Management dashboard over View Count service

--- prrws/src/main/webapp/Assests/solidWasteManagement/solidWasteManagement.js
+++ prrws/src/main/webapp/Assests/solidWasteManagement/solidWasteManagement.js
@@ -1,4 +1,4 @@
-getSolidInfoLocationWise();
+//getSolidInfoLocationWise();
 function getSolidInfoLocationWise()
 {
 	
@@ -16,4 +16,30 @@ function getSolidInfoLocationWise()
 		}
 	}).done(function(result){
 	});
+}
+//getSolidWasteManagementOverAllCounts(3,19);
+function getSolidWasteManagementOverAllCounts(locId,locationType){
+	
+	
+	var json = {
+			fromDate : "",
+			toDate : "",
+			locationType : "19" ,
+			locationId:locId
+		}
+		$.ajax({                
+			type:'POST',    
+			url: 'getSolidWasteManagementOverAllCounts',
+			dataType: 'json',
+			data : JSON.stringify(json),
+			beforeSend :   function(xhr){
+				xhr.setRequestHeader("Accept", "application/json");
+				xhr.setRequestHeader("Content-Type", "application/json");
+			}
+		}).done(function(result){
+			
+			if(result != null){
+				
+			}
+		});
 }
