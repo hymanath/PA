70ed0b96f1aa107eeb4aaa9be406e20b90b71309
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-07 08:27:14 +0000

    updated by swapna

--- prrws/src/main/java/com/itgrids/service/ISolidWasteManagementService.java
+++ prrws/src/main/java/com/itgrids/service/ISolidWasteManagementService.java
@@ -1,8 +1,14 @@
 package com.itgrids.service;
 
+import java.util.List;
+
+import com.itgrids.dto.DrainsVO;
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.SolidWasteManagementVO;
 
 public interface ISolidWasteManagementService 
 {
+	public List<SolidWasteManagementVO> getSolidInfoLocationWise(InputVO inputVO);
 	
 }
 
