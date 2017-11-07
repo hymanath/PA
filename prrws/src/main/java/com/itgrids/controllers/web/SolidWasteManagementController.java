e1a74926040738734eb89defb47d427d26c7fb44
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-07 08:26:22 +0000

    updated by swapna solidWasteManagementDashboard  service

--- prrws/src/main/java/com/itgrids/controllers/web/SolidWasteManagementController.java
+++ prrws/src/main/java/com/itgrids/controllers/web/SolidWasteManagementController.java
@@ -1,12 +1,20 @@
 package com.itgrids.controllers.web;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itgrids.dto.DrainsVO;
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.SolidWasteManagementVO;
 import com.itgrids.service.ISolidWasteManagementService;
 
 //import com.itgrids.service.IBioMetricService;
@@ -23,8 +31,21 @@ public class SolidWasteManagementController {
 	   
 	   @GetMapping("/solidWasteManagementDashboard")
 	   public String solidWasteManagementDashboard() {
-		   return "solidWasteManagementDashboard";
+		   return "solidWasteManagementDashBoard";
 	   }
+	   @PostMapping("/getSolidInfoLocationWise")
+		public @ResponseBody List<SolidWasteManagementVO> getSolidInfoLocationWise(@RequestBody InputVO inputVO)
+		{
+			
+			List<SolidWasteManagementVO> solidWasteManagementVO=null;
+			try {
+				solidWasteManagementVO = solidWasteManagementService.getSolidInfoLocationWise(inputVO);
+				
+			} catch (Exception e) {
+				LOG.error("Exception raised at  getSolidInfoLocationWise - SolidWasteManagementController controller", e);
+			}
+			return solidWasteManagementVO;
+		}
 	   
 	   
 }
