9e5a7d839c04f4417d03b305807d3d943fa15d1b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-08 14:38:43 +0000

    commited by sai kumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/ILocationWiseCasteInfoService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/core/api/service/ILocationWiseCasteInfoService.java
@@ -11,5 +11,6 @@ public interface ILocationWiseCasteInfoService {
 	public List<LocationVotersVO> getCasteNAgeWiseVoterNCadreCounts(Long locationTypeId,Long locationValue, Long publicationDateId,Long casteGroupId, Long casteId,Long enrollemntyearId);
 	public List<LocationVotersVO> getVotersAndcadreAgeWiseCount(Long locationTypeId,Long locationValue, Long publicationDateId, Long enrollmentId);
 	public List<LocationVotersVO> getCasteWiseCadreCounts(Long locationTypeId,List<Long> LocationValue, List<Long> enrollmentYearIds);
+	public List<LocationVotersVO> getLocationWiseVoterNCadreCounts(Long locationTypeId,List<Long> locationValue,Long casteId,Long enrollmentYearId,Long publicationDateId);
 
 }
