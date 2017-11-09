d26f21926eabacb719518febe1c5b191eda56d6d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-09 04:34:21 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothInchargeConditionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/BoothInchargeConditionDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IBoothInchargeConditionDAO;
 import com.itgrids.partyanalyst.model.BoothInchargeCondition;
@@ -9,5 +12,12 @@ public class BoothInchargeConditionDAO extends GenericDaoHibernate<BoothIncharge
 
 	public BoothInchargeConditionDAO() {
 		super(BoothInchargeCondition.class);
+	}
+	public List<Long> getboothInchargeConditionIdByRange(Long minValue, Long maxValue){
+		StringBuilder str = new StringBuilder();
+		str.append("select model.boothInchargeConditionId from BoothInchargeCondition model where :minValue >= model.minValue  and :minValue <= model.maxValue ");
+		Query query =  getSession().createQuery(str.toString());
+		query.setParameter("minValue", minValue);
+		return query.list();
 	}
 }
