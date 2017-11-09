437a1787157b580c9a35d2e608221fe98a329577
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-09 04:34:04 +0000

    updated by Srishailam

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeConditionDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IBoothInchargeConditionDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.BoothInchargeCondition;
 
 public interface IBoothInchargeConditionDAO extends GenericDao<BoothInchargeCondition, Long> {
-
+	public List<Long> getboothInchargeConditionIdByRange(Long minValue, Long maxValue);
 }
