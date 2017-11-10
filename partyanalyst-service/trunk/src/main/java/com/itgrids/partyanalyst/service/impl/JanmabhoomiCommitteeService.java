2ad201174dae0ecc4e2a8c0a8a4ca0aca9a575c9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-10 13:27:14 +0000

    Updated By Hyma : janmabhoomi committees dashboard new service creation

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/JanmabhoomiCommitteeService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/JanmabhoomiCommitteeService.java
@@ -1,14 +1,37 @@
 package com.itgrids.partyanalyst.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
+import com.itgrids.partyanalyst.dao.IJbCommitteeDAO;
+import com.itgrids.partyanalyst.dto.JanmabhoomiCommitteeVO;
 import com.itgrids.partyanalyst.service.IJanmabhoomiCommitteeService;
 
 public class JanmabhoomiCommitteeService implements IJanmabhoomiCommitteeService{
 
 	
 	private final static Logger LOG = Logger.getLogger(JanmabhoomiCommitteeService.class);
+	private IJbCommitteeDAO jbCommitteeDAO;
 	
+	public IJbCommitteeDAO getJbCommitteeDAO() {
+		return jbCommitteeDAO;
+	}
+	public void setJbCommitteeDAO(IJbCommitteeDAO jbCommitteeDAO) {
+		this.jbCommitteeDAO = jbCommitteeDAO;
+	}
+
+	public List<JanmabhoomiCommitteeVO> getDistrictWiseCommitteeDetails(String fromDate,String endDate,String type){
+		List<JanmabhoomiCommitteeVO> returnList = new ArrayList<JanmabhoomiCommitteeVO>(); 
+		try{
+			
+			//List<Object[]> list = jbCommitteeDAO.
+		}catch (Exception e) {
+			LOG.error("Excepting Occured in getDistrictWiseCommitteeDetails() of JanmabhoomiCommitteeService ", e);
+		}
+		return returnList;
+	}
 	
 
 }
