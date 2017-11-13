7f2104cbff0f30205066c165e72b5ca3a4a89a80
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-13 10:51:44 +0000

    commited by sai kumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/JbCommitteeMemberDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/JbCommitteeMemberDAO.java
@@ -1,7 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IJbCommitteeMemberDAO;
 import com.itgrids.partyanalyst.model.JbCommitteeMember;
@@ -14,5 +18,39 @@ public class JbCommitteeMemberDAO extends GenericDaoHibernate<JbCommitteeMember,
 		
 	}
 
-	
+	 public List<Object[]> getCommitteMemebersByRoleIds(List<Long> roleIdsList,Date fromDate,Date toDate){
+		 StringBuilder sb = new StringBuilder();
+		   //0 jbCommitteeMemberId,1 jbMemberTypeId,2 memberName, 3 mobileNo,4 isActive,5 status,6 voterIDCardNo
+			sb.append("SELECT model.jbCommitteeMemberId,model.jbCommitteeRole.jbMemberType.jbMemberTypeId, ");
+			sb.append("model.memberName,model.mobileNo,mobileNo.isActive,model.status, ");
+			sb.append("model.tdpCadre.voter.voterIDCardNo ");
+			
+			sb.append("from JbCommitteeMember model ");
+			sb.append("where ");
+			sb.append(" model.jbCommitteeRole.jbCommitteeRoleId in(:roleIdsList) ");
+			
+			if(fromDate !=null && toDate !=null){
+			sb.append("and (date(model.startDate) between :startDate and  :endDate ) ");
+			}
+			
+			Query query = getSession().createQuery(sb.toString());
+			
+			query.setParameterList("roleIdsList", roleIdsList);
+			if(fromDate !=null && toDate !=null){
+		   		query.setDate("startDate", fromDate);
+		   		query.setDate("endDate", toDate);
+		   	}
+			return query.list();
+	 }
+	 
+	 public List<Object[]> getCommitteeWiseTotalMemberAddedCount(){
+		 StringBuilder sb = new StringBuilder();
+		 sb.append("select model.jbCommitteeRole.jbCommittee.jbCommitteeLevel.jbCommitteeLevelId," );
+		 sb.append("model.jbCommitteeRole.jbCommittee.jbCommitteeId,count(model.jbCommitteeMemberId),model.jbCommitteeRole.jbCommittee.isCommitteeConfirmed," +
+		 		"model.jbCommitteeRole.jbCommittee.startDate,model.jbCommitteeRole.jbCommittee.completedDate ");
+		 sb.append("from JbCommitteeMember model where model.jbCommitteeRole.jbCommittee.isDeleted = 'N'  ");
+		 sb.append(" group by model.jbCommitteeRole.jbCommittee.jbCommitteeLevel.jbCommitteeLevelId,model.jbCommitteeRole.jbCommittee.jbCommitteeId ");
+		 Query query = getSession().createQuery(sb.toString());
+		 return query.list();
+	 }
 }
--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/JbCommitteeRoleDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/JbCommitteeRoleDAO.java
@@ -1,7 +1,11 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
 
+import java.util.Date;
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IJbCommitteeRoleDAO;
 import com.itgrids.partyanalyst.model.JbCommitteeRole;
@@ -12,7 +16,25 @@ public class JbCommitteeRoleDAO extends GenericDaoHibernate<JbCommitteeRole, Lon
 		super(JbCommitteeRole.class);
 		
 	}
-
 	
-
+ public List<Object[]> getDesignationsIdsByCommitteeId(Long committeeId){
+	 StringBuilder sb = new StringBuilder();
+	   //0 jbCommitteeRoleId,1 jbMemberTypeId,2 memberType, 3 maxMembers
+		sb.append("SELECT model.jbCommitteeRoleId,model.jbMemberType.jbMemberTypeId,model.jbMemberType.memberType,model.maxMembers ");
+		sb.append("from JbCommitteeRole model ");
+		sb.append("where model.jbCommittee.jbCommitteeId =:committeeId");
+		
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("committeeId", committeeId);
+		
+		return query.list();
+ }
+ public  List<Object[]> getCommitteeWiseTotalMemberCount(){
+	 StringBuilder sb = new StringBuilder();
+      sb.append("select model.jbCommittee.jbCommitteeLevel.jbCommitteeLevelId,model.jbCommittee.jbCommitteeLevel.name,model.jbCommittee.jbCommitteeId,sum(model.maxMembers) ");
+      sb.append(" from JbCommitteeRole model ");
+      sb.append(" group by model.jbCommittee.jbCommitteeLevel.jbCommitteeLevelId,model.jbCommittee.jbCommitteeId ");
+      Query query = getSession().createQuery(sb.toString());
+      return query.list();
+ }
 }
