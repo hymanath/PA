ef3276822e9e0ac66796f42653453bf52044a8de
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-14 09:18:55 +0000

    Updated by Santosh

--- prrws-biometric-soap/src/main/java/com/itgrids/test/Test.java
+++ prrws-biometric-soap/src/main/java/com/itgrids/test/Test.java
@@ -1,8 +1,6 @@
 package com.itgrids.test;
 
 import java.rmi.RemoteException;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.tempuri.BioAuthSoapProxy;
 
@@ -13,22 +11,6 @@ public class Test {
 		BioAuthSoapProxy bioAuthSoapObj = new BioAuthSoapProxy();
 		String jsonString = bioAuthSoapObj.registeredEmployees("27001701024");
 		System.out.println(jsonString);
-		Set<String> testSet = new HashSet<>();
-		System.out.println(Long.valueOf(testSet.size()));
 		
-		String text="sanjeedt";
-		switch(text){
-		 case "santosh":
-		 System.out.println("HI " + text);
-		 break;
-		 case "sumit":
-			 System.out.println("HI");
-			 break;
-		 case "sanjeet":
-			 System.out.println("HI " + text);
-			 break;
-		 default:
-			 System.out.println("select correct no");
-		}
 	} 
 }
