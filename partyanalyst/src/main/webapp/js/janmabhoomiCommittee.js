6a56cea07c28d144eadb5e3932b62c12148160a8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-14 13:55:31 +0000

    Updated By Hyma : janmabhoomi committees dashboard new service creation

--- partyanalyst/src/main/webapp/js/janmabhoomiCommittee.js
+++ partyanalyst/src/main/webapp/js/janmabhoomiCommittee.js
@@ -1,9 +1,27 @@
-getDistrictWiseCommitteeDetails();
+
+getJbCommitteeStatusCount();//1st
+function getJbCommitteeStatusCount(){
+
+	var jsObj={
+		"fromDateStr"	:"",
+		"toDateStr"	    :""
+	}
+	   $.ajax({
+		  type : "POST",
+		  url : "getJbCommitteeStatusCountAction.action",
+		  dataType : 'json',
+		  data : {task :JSON.stringify(jsObj)}
+		}).done(function(result){ 
+			
+		});
+}
+
+getDistrictWiseCommitteeDetails();//2nd
 function getDistrictWiseCommitteeDetails(){
 	var jsObj={
  		"fromDate"			:"",
  		"endDate"			:"",
-		type:"district"
+		type:"district"//constituency,parliament
  	}
  	   $.ajax({
  		  type : "POST",
@@ -14,19 +32,19 @@ function getDistrictWiseCommitteeDetails(){
 			
  		});
   }
-//getJbCommitteeStatusCount();
-function getJbCommitteeStatusCount(){
-
+getJanmabhoomiCommitteeOverview();//3rd
+function getJanmabhoomiCommitteeOverview(){
 	var jsObj={
-		"fromDateStr"	:"",
-		"toDateStr"	    :""
-	}
-	   $.ajax({
-		  type : "POST",
-		  url : "janmabhoomiCommitteeAction.action",
-		  dataType : 'json',
-		  data : {task :JSON.stringify(jsObj)}
-		}).done(function(result){ 
+ 		"fromDate"			:"",
+ 		"toDate"			:"",
+		committeId:1
+ 	}
+ 	   $.ajax({
+ 		  type : "POST",
+ 		  url : "getJanmabhoomiCommitteeOverviewAction.action",
+ 		  dataType : 'json',
+ 		  data : {task :JSON.stringify(jsObj)}
+ 		}).done(function(result){ 
 			
-		});
-}
+ 		});
+  }
