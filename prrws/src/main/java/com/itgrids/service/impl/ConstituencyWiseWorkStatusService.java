0ac493024fa55f5c8bc2c95287f5ccc172299a36
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-17 13:53:52 +0000

    commited by raghu for consitutencyWiseReports

--- prrws/src/main/java/com/itgrids/service/impl/ConstituencyWiseWorkStatusService.java
+++ prrws/src/main/java/com/itgrids/service/impl/ConstituencyWiseWorkStatusService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.itgrids.dao.IAssemblyMlaDAO;
 import com.itgrids.dao.IConstituencyDAO;
 import com.itgrids.dto.InputVO;
 import com.itgrids.dto.LocationVO;
@@ -22,6 +23,9 @@ public class ConstituencyWiseWorkStatusService implements IConstituencyWiseWorkS
 	private IConstituencyDAO constituencyDAO;
 	@Autowired
 	private CommonMethodsUtilService commonMethodsUtilService;
+	@Autowired
+	private IAssemblyMlaDAO assemblyMlaDAO;
+	
 	public List<LocationVO> getLocationsNamesBySubLocation( InputVO inputVO){
 		List<LocationVO> finalList = new ArrayList<LocationVO>(0);
 		List<Object[]> objects=constituencyDAO.getLocationsNamesBySubLocation(inputVO.getLocationId());
@@ -30,6 +34,21 @@ public class ConstituencyWiseWorkStatusService implements IConstituencyWiseWorkS
 				LocationVO vo = new LocationVO();
 				vo.setLocationId(commonMethodsUtilService.getLongValueForObject(param[0]));
 				vo.setLocationName(commonMethodsUtilService.getStringValueForObject(param[1]));
+				finalList.add(vo);
+			}
+		}
+		return finalList;
+	}
+	public List<LocationVO> getDistrictNameAndMlaNameByConsitutency( InputVO inputVO){
+		List<LocationVO> finalList = new ArrayList<LocationVO>(0);
+		List<Object[]> objects=assemblyMlaDAO.getAllConstituencyDetails(inputVO.getLocationId());
+		if(objects != null && objects.size() > 0){
+			for(Object[] param : objects){
+				LocationVO vo = new LocationVO();
+				vo.setLocationId(commonMethodsUtilService.getLongValueForObject(param[0]));
+				vo.setLocationName(commonMethodsUtilService.getStringValueForObject(param[1]));
+				vo.setMlaName(commonMethodsUtilService.getStringValueForObject(param[2]));
+				vo.setWorkName(commonMethodsUtilService.getStringValueForObject(param[3]));
 				finalList.add(vo);
 			}
 		}
