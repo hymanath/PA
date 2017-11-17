f1d70cc36a8acffb93d26a0aa834cd83046764e9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-17 09:12:36 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/service/impl/ConstituencyWiseWorkStatusService.java
@@ -0,0 +1,38 @@
+package com.itgrids.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itgrids.dao.IConstituencyDAO;
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.LocationVO;
+import com.itgrids.service.IConstituencyWiseWorkStatusService;
+import com.itgrids.utils.CommonMethodsUtilService;
+
+@Service
+@Transactional
+public class ConstituencyWiseWorkStatusService implements IConstituencyWiseWorkStatusService {
+	private static final Logger LOG = Logger.getLogger(ConstituencyWiseWorkStatusService.class);
+	@Autowired
+	private IConstituencyDAO constituencyDAO;
+	@Autowired
+	private CommonMethodsUtilService commonMethodsUtilService;
+	public List<LocationVO> getLocationsNamesBySubLocation( InputVO inputVO){
+		List<LocationVO> finalList = new ArrayList<LocationVO>(0);
+		List<Object[]> objects=constituencyDAO.getLocationsNamesBySubLocation(inputVO.getLocationId());
+		if(objects != null && objects.size() > 0){
+			for(Object[] param : objects){
+				LocationVO vo = new LocationVO();
+				vo.setLocationId(commonMethodsUtilService.getLongValueForObject(param[0]));
+				vo.setLocationName(commonMethodsUtilService.getStringValueForObject(param[1]));
+				finalList.add(vo);
+			}
+		}
+		return finalList;
+	}
+}
