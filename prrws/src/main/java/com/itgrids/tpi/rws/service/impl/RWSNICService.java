fe01bd5f724baf72f7156941d8165de5f1c742b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-18 06:06:04 +0000

    Updated By Sanjeev

--- prrws/src/main/java/com/itgrids/tpi/rws/service/impl/RWSNICService.java
+++ prrws/src/main/java/com/itgrids/tpi/rws/service/impl/RWSNICService.java
@@ -1566,12 +1566,14 @@ public class RWSNICService implements IRWSNICService{
 		 	    				subVo.setHabitationName(jobj.getString("habitationName"));
 		 	    				subVo.setHabitationCode(jobj.getString("habitationCode"));
 		 	    				if(jobj.has("completionDate")){
+		 	    					subVo.setSacntionedAmount(jobj.getString("assestType"));
 		 	    					subVo.setCompletionDate(jobj.getString("completionDate"));
 		 	    				}
 		 	    				if(jobj.has("targetDate")){
 		 	    					subVo.setCompletionDate(jobj.getString("targetDate"));
 		 	    				}
 		 	    				if(jobj.has("commssionedDate")){
+		 	    					subVo.setSacntionedAmount(jobj.getString("assestType"));
 		 	    					subVo.setCompletionDate(jobj.getString("commssionedDate"));
 		 	    				}
 		 	    				if(jobj.has("groundingDate")){
