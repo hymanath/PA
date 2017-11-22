b8b7f286eda13ebcb24ccc4bf75077d3d0e13a40
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-22 11:11:28 +0000

    updated by swapna

--- prrws/src/main/java/com/itgrids/controllers/web/LightMonitoringController.java
+++ prrws/src/main/java/com/itgrids/controllers/web/LightMonitoringController.java
@@ -83,10 +83,15 @@ public class LightMonitoringController {
    public @ResponseBody  LightMonitoringVO  getNredCapLightMonitoringLocationWise(@RequestBody InputVO inputVO){
 	   LightMonitoringVO  reusltVO=lightMonitoring.getCompanyWiseLightMonitoringDtls(inputVO.getFromDate(),inputVO.getToDate(),inputVO.getLocationType(),inputVO.getLocationValues(),inputVO.getLightVendorIdList());
        return reusltVO;
- }
+ }   
    @RequestMapping(value ="/getLightsVendors", method = RequestMethod.GET)
    public  @ResponseBody List<LightsVendor> getLightsVendors() {
 	return lightMonitoring.getLightsVendorList();
    }
+   @RequestMapping(value = "/getTimePeriodWiseLightsDetaisl", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+   public @ResponseBody  List<LightMonitoringVO>  getTimePeriodWiseLightsDetaisl(@RequestBody InputVO inputVO){
+	   List<LightMonitoringVO>  voLists=lightMonitoring.getTimePeriodWiseLightsDetaisl(inputVO.getFromDate(),inputVO.getToDate(),inputVO.getLocationType(),inputVO.getLocationValue(),inputVO.getLightVendorIdList());
+       return voLists;
+ }
 }
 
