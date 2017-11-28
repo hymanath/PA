dbf87bbbd72667507cc58e95df53940d25830347
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-28 10:23:25 +0000

    Updated by Santosh

--- prrws/src/main/java/com/itgrids/dao/impl/LightMonitoringDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/LightMonitoringDAO.java
@@ -381,7 +381,7 @@ public class LightMonitoringDAO extends GenericDaoHibernate<LightMonitoring, Lon
 		if (fromDate != null && toDate != null) {
 			sb.append(" and  date(model.surveyDate) between :fromDate and :toDate ");
 		}
-		sb.append(" group by date(model.surveyDate),model.lightsVendor.lightsVendorId ");
+		sb.append(" group by date(model.surveyDate),model.lightsVendor.lightsVendorId order by date(model.surveyDate) ");
 
 		Query query = getSession().createQuery(sb.toString());
 		if (fromDate != null && toDate != null) {
