f299c9a41e11460af9d70497777eef669857e330
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 23:15:35 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/service/IRepresentationRequestService.java
+++ prrws/src/main/java/com/itgrids/service/IRepresentationRequestService.java
@@ -1,9 +1,12 @@
 package com.itgrids.service;
 
+import java.util.List;
+
 import com.itgrids.dto.RepresentationRequestVO;
 import com.itgrids.dto.ResponseVO;
 
 
 public interface IRepresentationRequestService {
 	public ResponseVO saveRepresentRequestDetails(RepresentationRequestVO dataVO);
+	public List<RepresentationRequestVO> getPetitionReferredMemberDetails(RepresentationRequestVO dataVo );
 }
