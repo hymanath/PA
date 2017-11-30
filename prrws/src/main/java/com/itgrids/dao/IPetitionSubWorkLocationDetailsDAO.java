5b16020651a0c9b61acc3eda587ec092c56ba838
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 13:54:44 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPetitionSubWorkLocationDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PetitionSubWorkLocationDetails;
+
+public interface IPetitionSubWorkLocationDetailsDAO extends GenericDao<PetitionSubWorkLocationDetails, Long> {
+
+}
---
+++ prrws/src/main/java/com/itgrids/dao/IPetitionWorkDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PetitionWorkDetails;
+
+public interface IPetitionWorkDetailsDAO extends GenericDao<PetitionWorkDetails, Long> {
+
+}
