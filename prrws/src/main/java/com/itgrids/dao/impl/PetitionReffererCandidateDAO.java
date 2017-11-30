7a65f85ad1beb02264fe62ad036a0d6c779b89f7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 22:47:57 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/impl/PetitionReffererCandidateDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PetitionReffererCandidateDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.dao.impl;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,10 +14,49 @@ import com.itgrids.model.PetitionReffererCandidate;
 @Repository
 public class PetitionReffererCandidateDAO extends GenericDaoHibernate<PetitionReffererCandidate, Long> implements IPetitionReffererCandidateDAO {
 
+	@Autowired
+	SessionFactory sessionFactory;
+	
 	public PetitionReffererCandidateDAO(){
 		super(PetitionReffererCandidate.class);
 	}
 	
-	@Autowired
-	SessionFactory sessionFactory;
+	
+	
+	public List<Object[]> getCandidatseDetailsByDesignationAndLocation(Long designationId,Long locationLevelId,Long locationValue){
+		StringBuilder sb = new StringBuilder();
+		sb.append("select model.petitionReffererCandidateId,model.name,model.petitionDesignation.designationName");//
+		sb.append(",state.stateId,state.stateName ");
+		sb.append(",district.districtId,district.districtName ");
+		sb.append(",constituency.constituencyId,constituency.name ");
+		sb.append(",tehsil.tehsilId,tehsil.tehsilName ");
+		sb.append(",panchayat.panchayatId,panchayat.panchayatName ");
+		sb.append(",model.mobileNo,model.emailId,model.petitionDesignation.petitionDesignationId ");
+		sb.append("from PetitionReffererCandidate model ");
+		sb.append("left join model.locationAddress locationAddress ");
+		sb.append("left join locationAddress.state state ");
+		sb.append("left join locationAddress.district district ");
+		sb.append("left join locationAddress.constituency constituency ");
+		sb.append("left join locationAddress.tehsil tehsil ");
+		sb.append("left join locationAddress.panchayat panchayat ");
+		sb.append("where model.isDeleted ='N' ");
+		if(designationId != null && designationId.longValue() > 0L){
+			sb.append("and model.petitionDesignation.petitionDesignationId =:designationId ");
+		}
+		if(locationLevelId != null && locationLevelId.longValue() == 3L && locationValue != null && locationValue.longValue() > 0){
+			sb.append("and model.locationAddress.districtId = :locationValue ");
+		}else if(locationLevelId != null && locationLevelId.longValue() == 4L && locationValue != null && locationValue.longValue() > 0){
+			sb.append("and model.locationAddress.constituencyId = :locationValue ");
+		}
+		Query query = getSession().createQuery(sb.toString());
+		if(designationId != null && designationId.longValue() > 0L){
+			query.setParameter("designationId", designationId);
+		}
+		if(locationLevelId != null && locationLevelId.longValue() == 3L && locationValue != null && locationValue.longValue() > 0 ){
+			query.setParameter("locationValue", locationValue);
+		}else if(locationLevelId != null && locationLevelId.longValue() == 4L && locationValue != null && locationValue.longValue() > 0 ){
+			query.setParameter("locationValue", locationValue);
+		}
+	return query.list();
+	}
 }
