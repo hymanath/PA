02b34ea2fd5c8f03c7522edca5004359fd4ea254
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 19:40:40 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/service/ILocationDetailsService.java
@@ -0,0 +1,18 @@
+package com.itgrids.service;
+
+import java.util.List;
+
+import com.itgrids.dto.IdNameVO;
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.KeyValueVO;
+import com.itgrids.dto.LocationFundDetailsVO;
+import com.itgrids.dto.LocationVO;
+
+public interface ILocationDetailsService {
+	
+	public List<LocationFundDetailsVO> getAllDistrictsInState(Long stateId);
+	public List<LocationVO> getConstituencyNamesByDistrictId( Long  districtId);
+	public List<KeyValueVO> getTehsilsAndLocalElectionBodyForConstituencyId(Long constituencyId);
+	public List<KeyValueVO> getPanchayatsByTehsilId(Long tehsilId);
+	
+}
