243d4d3a3c8c41c1884837ed37b181a36ffc5a06
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 13:54:44 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPetitionWorkDocumentDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PetitionWorkDocument;
+
+public interface IPetitionWorkDocumentDAO extends GenericDao<PetitionWorkDocument, Long> {
+
+}
