d36701778e7556cee9ef98ef61394501056bca62
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 19:41:38 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/controllers/web/RepresentationRequestController.java
+++ prrws/src/main/java/com/itgrids/controllers/web/RepresentationRequestController.java
@@ -1,17 +1,26 @@
 package com.itgrids.controllers.web;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.KeyValueVO;
+import com.itgrids.dto.LocationFundDetailsVO;
+import com.itgrids.dto.LocationVO;
 import com.itgrids.dto.RepresentationRequestVO;
 import com.itgrids.dto.ResponseVO;
+import com.itgrids.service.ILocationDetailsService;
 import com.itgrids.service.IRepresentationRequestService;
 
 @EnableAutoConfiguration
@@ -22,6 +31,8 @@ public class RepresentationRequestController {
 	
 	@Autowired
 	private IRepresentationRequestService representationRequestService;
+	@Autowired
+	private ILocationDetailsService locationDetailsService;
 	
 	
 	@RequestMapping(value ="/representationRequestEntry", method = RequestMethod.GET)
@@ -32,5 +43,21 @@ public class RepresentationRequestController {
 	@RequestMapping(value ="/saveRepresentRequestDetails",method = RequestMethod.POST)
     public @ResponseBody ResponseVO saveRepresentRequestDetails(@ModelAttribute  RepresentationRequestVO representationRequestVO) {
     	 return representationRequestService.saveRepresentRequestDetails(representationRequestVO);
+    }
+	@RequestMapping(value ="/getAllDistrictsInState",method = RequestMethod.POST)
+    public @ResponseBody List<LocationFundDetailsVO> getAllDistrictsInState(@RequestBody Map<String,String> inputMap ) {
+    	 return locationDetailsService.getAllDistrictsInState(Long.valueOf(inputMap.get("stateId")));
+    }
+	@RequestMapping(value ="/getConstituencyNamesByDistrictId",method = RequestMethod.POST)
+    public @ResponseBody List<LocationVO> getConstituencyNamesByDistrictId(@RequestBody Map<String,String> inputMap ) {
+    	 return locationDetailsService.getConstituencyNamesByDistrictId(Long.valueOf(inputMap.get("districtId")));
+    }
+	@RequestMapping(value ="/getTehsilsAndLocalElectionBodyForConstituencyId",method = RequestMethod.POST)
+    public @ResponseBody List<KeyValueVO> getTehsilsAndLocalElectionBodyForConstituencyId(@RequestBody Map<String,String> inputMap ) {
+    	 return locationDetailsService.getTehsilsAndLocalElectionBodyForConstituencyId(Long.valueOf(inputMap.get("constituencyId")));
+    }
+	@RequestMapping(value ="/getPanchayatsByTehsilId",method = RequestMethod.POST)
+    public @ResponseBody List<KeyValueVO> getPanchayatsByTehsilId(@RequestBody Map<String,String> inputMap ) {
+    	 return locationDetailsService.getPanchayatsByTehsilId(Long.valueOf(inputMap.get("tehsilId")));
     }
 }
