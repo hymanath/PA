4d3270807a03a88d05dc205bda1a3bc66fecb99c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 23:14:19 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/utils/CommonMethodsUtilService.java
+++ prrws/src/main/java/com/itgrids/utils/CommonMethodsUtilService.java
@@ -643,7 +643,7 @@ public class CommonMethodsUtilService {
 						 return "FAILED";
 					 }
 					 
-					 return "/"+yr+"/"+mnth;
+					 return "/"+mnthDir;
 					 
 				
 				} catch (Exception e) {
