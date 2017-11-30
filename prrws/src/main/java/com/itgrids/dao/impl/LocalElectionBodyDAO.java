0526688b6146df8f35b21b28034892bbe6c00457
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 19:40:20 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/impl/LocalElectionBodyDAO.java
@@ -0,0 +1,32 @@
+package com.itgrids.dao.impl;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itgrids.dao.ILocalElectionBodyDAO;
+import com.itgrids.model.LocalElectionBody;
+
+@Repository
+public class LocalElectionBodyDAO extends GenericDaoHibernate<LocalElectionBody, Long> implements ILocalElectionBodyDAO {
+	
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public LocalElectionBodyDAO() {
+		super(LocalElectionBody.class);
+	}
+	
+	public List<Object[]> getLocalElectionBodyByTehsilId(List<Long> tehsilIds){
+		StringBuilder sb = new StringBuilder();
+		sb.append("select model.localElectionBodyId,model.name from LocalElectionBody model "
+				+ "where model.tehsilId in (:tehsilIds) order by model.name ");
+		Query query =getSession().createQuery(sb.toString());
+		query.setParameterList("tehsilIds", tehsilIds);
+		return query.list();
+	}
+}
