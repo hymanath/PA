baf7c570034ba75adbf2d6f3d52d401b85d93e07
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-11-30 22:47:38 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/IPetitionReffererCandidateDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPetitionReffererCandidateDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.PetitionReffererCandidate;
 
 public interface IPetitionReffererCandidateDAO extends GenericDao<PetitionReffererCandidate, Long> {
 
+	public List<Object[]> getCandidatseDetailsByDesignationAndLocation(Long designationId,Long locationLevelId,Long locationValue);
 }
