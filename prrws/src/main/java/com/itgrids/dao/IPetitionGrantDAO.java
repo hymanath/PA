367044b12041673e9b79483c79321409f36f7df0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-04 12:17:58 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPetitionGrantDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PetitionGrant;
+
+public interface IPetitionGrantDAO extends GenericDao<PetitionGrant, Long> {
+
+}
