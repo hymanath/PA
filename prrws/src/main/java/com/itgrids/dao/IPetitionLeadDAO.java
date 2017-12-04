c5583c280d5dffb9193ed421b15983b69a52b267
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-04 12:18:21 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPetitionLeadDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PetitionLead;
+
+public interface IPetitionLeadDAO extends GenericDao<PetitionLead, Long> {
+
+}
