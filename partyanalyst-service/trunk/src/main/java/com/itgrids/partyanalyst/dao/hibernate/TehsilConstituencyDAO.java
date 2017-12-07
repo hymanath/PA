81bdb17b0625f22fc9c32298f8487b59fb4f9fe7
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-07 11:04:26 +0000

    commited by sai kumar

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TehsilConstituencyDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/TehsilConstituencyDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.ITehsilConstituencyDAO;
 import com.itgrids.partyanalyst.model.TehsilConstituency;
@@ -11,5 +14,16 @@ public class TehsilConstituencyDAO extends GenericDaoHibernate<TehsilConstituenc
 		super(TehsilConstituency.class);
 	}
 
-	
+	public List<Long> getTehsilIdsByConstituencyId(List<Long> constituencyIds){
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(" select model.tehsil.tehsilId from  TehsilConstituency model" );
+		sb.append(" where model.constituency.constituencyId in(:constituencyIds) ");
+		Query query = getSession().createQuery(sb.toString());
+		
+		if(constituencyIds !=null && constituencyIds.size() >0){
+			query.setParameterList("constituencyIds", constituencyIds);
+		}
+		return query.list();
+	}
 }
