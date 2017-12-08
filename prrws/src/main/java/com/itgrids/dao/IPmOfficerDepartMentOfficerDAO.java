4aa8d6e8bd75ca045a485a3f5d3be2180b0e644d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-08 12:19:50 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPmOfficerDepartMentOfficerDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmOfficerDepartMentOfficer;
+
+public interface IPmOfficerDepartMentOfficerDAO extends GenericDao<PmOfficerDepartMentOfficer, Long> {
+
+}
