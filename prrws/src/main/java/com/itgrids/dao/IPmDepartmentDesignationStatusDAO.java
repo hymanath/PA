6d5152db6e5bc0e0c8ffacf01ad08fd77438b9bc
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-08 12:19:17 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPmDepartmentDesignationStatusDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmDepartmentDesignationStatus;
+
+public interface IPmDepartmentDesignationStatusDAO extends GenericDao<PmDepartmentDesignationStatus, Long> {
+
+}
