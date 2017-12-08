71c628bf0b92a1ba0419008aa5d396ee5449743d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-08 12:19:50 +0000

    commited by - Babu kurakula

---
+++ prrws/src/main/java/com/itgrids/dao/IPmOfficerUserDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmOfficerUser;
+
+public interface IPmOfficerUserDAO extends GenericDao<PmOfficerUser, Long> {
+
+}
---
+++ prrws/src/main/java/com/itgrids/dao/IPmSubWorkDetailsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmSubWorkDetails;
+
+public interface IPmSubWorkDetailsDAO extends GenericDao<PmSubWorkDetails, Long> {
+
+}
