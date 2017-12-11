ede0a557e659761d954f74c4f4816094995f303d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-11 07:54:49 +0000

    swadhin

--- partyanalyst/src/main/webapp/newCoreDashBoard/js/trainingCampInfo.js
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/trainingCampInfo.js
@@ -1,6 +1,46 @@
 //Training Camp Info
 var getDocumentWidthForCampInfo = $(document).width();
-var globalStateId=1; //default Ap
+var globalStateIdForCampInfo=1; //default Ap
 $("#tdpTriningCampYearId").on('change', function() {
 	var enrollmentYearId=$("#tdpTriningCampYearId").val();
 });
+var globalUserWiseMemberRsltForTrainingCamp;
+getUserTypeWiseTotalEligibleAndAttendedCnt();
+function getUserTypeWiseTotalEligibleAndAttendedCnt(){
+	$("#userTypeWiseTrainingCampInfoTopFiveStrongAndPoorMemsDivId").html('<div class="spinner"><div class="dot1"></div><div class="dot2"></div></div>');
+	var dateStr = '';
+	var enrollmentYrIds = [];
+	enrollmentYrIds.push(2);
+	var programIdArr = [];//[8]
+	if(enrollmentYrIds != null && enrollmentYrIds.length>0){
+		for(var i in enrollmentYrIds){
+			if(parseInt(enrollmentYrIds[i]) == 2){
+				programIdArr=[];
+				programIdArr=[8];
+			}else  if(parseInt(enrollmentYrIds[i]) == 1){
+			}
+		}
+	}
+	var jsObj ={
+		userAccessLevelId:globalUserAccessLevelId,
+		userAccessLevelValuesArray:globalUserAccessLevelValues,
+		activityMemberId : globalActivityMemberId,
+		userTypeId : globalUserTypeId,
+		stateId : globalStateIdForCampInfo,
+		dateStr : dateStr,
+		enrollmentYrIds:enrollmentYrIds,
+		programIdArr:programIdArr
+	}
+		
+	$.ajax({
+		type : 'POST',
+		url : 'getUserTypeWiseTotalEligibleAndAttendedCntActionForTrainingCamp.action',
+		dataType : 'json',
+		data : {task:JSON.stringify(jsObj)}
+	}).done(function(result){
+		 $("#userTypeWiseTrainingCampInfoTopFiveStrongAndPoorMemsDivId").html(' ');
+		 //buildUserTypeWiseTrainingProgramAttendedCountTopFiveStrongResults(result);
+		 globalUserWiseMemberRsltForTrainingCamp = result;
+		 alert("result");
+	});
+}
