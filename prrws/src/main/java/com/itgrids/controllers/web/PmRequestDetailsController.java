f76f48291327a8e65213cfbdef8f0398c2a85681
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-11 10:41:31 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/controllers/web/PmRequestDetailsController.java
+++ prrws/src/main/java/com/itgrids/controllers/web/PmRequestDetailsController.java
@@ -1,9 +1,20 @@
 package com.itgrids.controllers.web;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itgrids.dto.KeyValueVO;
+import com.itgrids.service.impl.LocationDetailsService;
 
 @EnableAutoConfiguration
 @Controller
@@ -11,4 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PmRequestDetailsController {
 
 	   private static final Logger LOG = Logger.getLogger(PmRequestDetailsController.class);
+	   
+	   @RequestMapping(value ="/representationRequestEntry", method = RequestMethod.GET)
+	    public String mgnregsDashBoardPage(ModelMap model) {
+			return "representationRequestEntry";
+	    }
 }
