485fa083f2d6299a2a658563cb65c7941611b6af
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-11 05:47:52 +0000

    training camp script modification  :swadhin

---
+++ partyanalyst/src/main/webapp/newCoreDashBoard/js/trainingCampInfo.js
@@ -0,0 +1,6 @@
+//Training Camp Info
+var getDocumentWidthForCampInfo = $(document).width();
+var globalStateId=1; //default Ap
+$("#tdpTriningCampYearId").on('change', function() {
+	var enrollmentYearId=$("#tdpTriningCampYearId").val();
+});
