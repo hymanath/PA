c3c2fd928fa8cdee48a31f043b00147b01e2b333
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-11 05:42:00 +0000

    coredashboard training camp service modified by- Swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardCoreService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/ICoreDashboardCoreService.java
@@ -3,8 +3,10 @@ package com.itgrids.partyanalyst.service;
 import java.util.List;
 
 import com.itgrids.partyanalyst.dto.TrainingCampVO;
+import com.itgrids.partyanalyst.dto.UserTypeVO;
 
 public interface ICoreDashboardCoreService {
 	public TrainingCampVO getTrainingCampFeedBackDetails(Long userAccessLevelId, List<Long> userAccessLevelValueList,Long stateId, String fromDateStr, String toDateStr, List<Long> enrollmentYearIdList, List<Long> programIdList);
+	public List<List<UserTypeVO>> getUserTypeWiseTotalEligibleAndAttendedCnt(Long userId,Long userTypeId,Long activityMembersId,Long userAccessLevelId,List<Long> userAccessLevelValues,Long stateId,String toDateStr,List<Long> enrollmentYearIds,List<Long> programIdList);
 
 }
