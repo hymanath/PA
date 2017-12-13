647eff3219823d8af2a6bf8bbab02a05f636acc9
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-13 10:34:50 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/IPmRefCandidateDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmRefCandidateDAO.java
@@ -1,9 +1,13 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.PmRefCandidate;
 
 public interface IPmRefCandidateDAO extends GenericDao<PmRefCandidate, Long> {
-
+	public List<Object[]> getAllDistrictsByReferral();
+	public List<Object[]> getAllConstituenciesByReferralAndDistrict(Long districtId);
+	public List<Object[]> getAllMandalsByReferralAndDistrict(Long constituencyId);
 }
