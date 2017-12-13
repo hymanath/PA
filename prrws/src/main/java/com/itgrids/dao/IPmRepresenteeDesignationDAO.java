391e71582b2716bec052d8f7c60f55b1c8779b83
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-13 10:35:36 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/IPmRepresenteeDesignationDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmRepresenteeDesignationDAO.java
@@ -8,4 +8,8 @@ import com.itgrids.model.PmRepresenteeDesignation;
 
 public interface IPmRepresenteeDesignationDAO extends GenericDao<PmRepresenteeDesignation, Long> {
 	public List<PmRepresenteeDesignation> getPmRepresenteeDesignationByRepresenteeId(Long representeeId);
+	public List<Object[]> getAllDistrictsByRepresenteeDesignationWise();
+	public List<Object[]> getDesignationsByRepresenteeDesigtion();
+	public List<Object[]> getAllConstituenciesByRepresenteeDesignationWise(Long districtId);
+	public List<Object[]> getAllMandalsByRepresenteeDesignationAndconstincy(Long constituencyId);
 }
