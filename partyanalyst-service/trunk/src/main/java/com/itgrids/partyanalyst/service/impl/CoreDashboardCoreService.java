63d314b1bc7f6768e07ccbf794d87c37b64ad04a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-13 11:30:05 +0000

    committed by raghu for training camp

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardCoreService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/impl/CoreDashboardCoreService.java
@@ -1171,7 +1171,7 @@ public class CoreDashboardCoreService implements ICoreDashboardCoreService {
 	   	 ClientConfig clientConfig = new DefaultClientConfig();
 	     clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
          Client client = Client.create(clientConfig);
-	         WebResource webResource = client.resource("http://mytdp.com/Survey/WebService/getTrainingCampFeedBackDetails/"+listString+"/");
+          WebResource webResource = client.resource("http://mytdp.com/Survey/WebService/getTrainingCampFeedBackDetails/"+listString+"/");
 	        // WebResource webResource = client.resource("https://mytdp.com/Survey/WebService/getTrainingCampFeedBackDetails/"+listString+"/");
 	         ClientResponse response = webResource.accept("application/json").type("application/json").get(ClientResponse.class);
 	          if(response.getStatus() != 200){
@@ -1238,7 +1238,7 @@ public class CoreDashboardCoreService implements ICoreDashboardCoreService {
 		     clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
 	         Client client = Client.create(clientConfig);
 		     WebResource webResource = client.resource("http://localhost:8080/Survey/WebService/getTrainingFeedBackDetailsOnProgram/"+listString+"/");
-		  // WebResource webResource2 = client.resource("http://mytdp.com/Survey/WebService/getTrainingQuizFeedBackDetails/"+listString+"/");
+		  // WebResource webResource2 = client.resource("http://mytdp.com/Survey/WebService/getTrainingFeedBackDetailsOnProgram/"+listString+"/");
 			 ClientResponse response = webResource.accept("application/json").type("application/json").get(ClientResponse.class);
 			  if(response.getStatus() != 200){
 				  throw new RuntimeException("Failed : HTTP error code : "+ response.getStatus());
@@ -1247,13 +1247,21 @@ public class CoreDashboardCoreService implements ICoreDashboardCoreService {
 			  
 			  List<Object[]>  tempList = null;
 			  List<Object[]>  tempList2 = null;
+			  List<Object[]>  tempList3 = null;
+			  List<Object[]>  tempList4 = null;
 				 //Query query = getSession().createSQLQuery("CALL get_training_camp_attendance_details(:programId,'2010-07-23','2050-08-02',:enrollemntYrId,:basicCommitteeId,'5,7,9,6,8','2','1')")
 			  if(userAccessLevelId.longValue() == IConstants.STATE_LEVEl_ACCESS_ID){
-					List<Long> distList1 = new ArrayList<Long>(){{add(11L);add(12L);add(13L);add(14L);add(15L);add(16L);add(17L);}};
-					tempList  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList1,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
-					List<Long> distList2 = new ArrayList<Long>(){{add(18L);add(19L);add(20L);add(21L);add(22L);add(23L);add(517L);}};
-					tempList2  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList2,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
-					tempList.addAll(tempList2);
+				  List<Long> distList1 = new ArrayList<Long>(){{add(11L);add(12L);add(13L);}};
+				   tempList  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList1,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
+				   List<Long> distList2 = new ArrayList<Long>(){{add(14L);add(15L);add(16L);}};
+				   tempList2  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList2,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
+				   List<Long> distList3 = new ArrayList<Long>(){{add(17L);add(18L);add(19L);add(517L);}};
+				   tempList3  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList3,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
+				   List<Long> distList4 = new ArrayList<Long>(){{add(20L);add(21L);add(22L);add(23L);}};
+				   tempList4  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(3L,distList4,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
+				   tempList.addAll(tempList2);
+				   tempList.addAll(tempList3);
+				   tempList.addAll(tempList4);
 				}else{
 					tempList  = trainingCampAttendanceDAO.getInviteAttendedCountForTrainingCamp(userAccessLevelId,userAccessLevelValues,enrollmentYearIdList,programIdList,commiteeList);//Procedure Call
 				}
@@ -1261,56 +1269,50 @@ public class CoreDashboardCoreService implements ICoreDashboardCoreService {
 			 Set<Long> cadreIds = null;
 			 for(Object[] param :tempList){
 				    if(commonMethodsUtilService.getStringValueForObject(param[7]).trim().equalsIgnoreCase("INVITEE")){
-					 if(StringType == 1L && commonMethodsUtilService.getLongValueForObject(param[25])!= null){
-						cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[25]));
-					}else if(StringType == 2L){
-						cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[11]));
-					}else if(StringType == 3L){
-						cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[13]));
-					}else if(StringType == 4L){
-						cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[15]));
-					}
-					if(cadreIds == null){
-						cadreIds = new HashSet<Long>();
-						if(StringType ==1L && commonMethodsUtilService.getLongValueForObject(param[25])!= null){
-							 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[25]),cadreIds);
-						}else if(StringType ==2L){
-							 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[11]),cadreIds);
-						}else if(StringType ==3L){
-							 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[13]),cadreIds);
-						}else if(StringType ==4L){
-							 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[15]),cadreIds);
+						 if(StringType == 1L && commonMethodsUtilService.getLongValueForObject(param[25])!= null){
+							cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[25]));
+						}else if(StringType == 2L){
+							cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[11]));
+						}else if(StringType == 3L){
+							cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[13]));
+						}else if(StringType == 4L){
+							cadreIds = mainCadreIdsmap.get(commonMethodsUtilService.getLongValueForObject(param[15]));
 						}
-					   }
-					cadreIds.add(commonMethodsUtilService.getLongValueForObject(param[0]));
-				   }
+						if(cadreIds == null){
+							cadreIds = new HashSet<Long>();
+							if(StringType ==1L && commonMethodsUtilService.getLongValueForObject(param[25])!= null){
+								 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[25]),cadreIds);
+							}else if(StringType ==2L){
+								 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[11]),cadreIds);
+							}else if(StringType ==3L){
+								 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[13]),cadreIds);
+							}else if(StringType ==4L){
+								 mainCadreIdsmap.put(commonMethodsUtilService.getLongValueForObject(param[15]),cadreIds);
+							}
+					     }
+					   cadreIds.add(commonMethodsUtilService.getLongValueForObject(param[0]));
+				    }
 			    }
 				 Map<Long,Long> mainMapCount = new HashMap<Long, Long>();
 				 for(Entry<Long,Set<Long>> maincadreId :mainCadreIdsmap.entrySet()){
 					 mainMapCount.put(maincadreId.getKey(), Long.parseLong(String.valueOf(maincadreId.getValue().size())));
 				 }
-				 
+				 Long countOfcadreIds=0L;
 				List<Object[]> finalList= trainingCampBatchAttendeeDAO.getFilteredListCount(cadreIdList,programIdList,commiteeList,userAccessLevelId,userAccessLevelValues,StringType);
-				Long countOfcadreIds = 0L;
-				 for(Object[] param:finalList){
+				 for(Object[] param : finalList){
 					 TrainingCampVO feedbackCountVo = new TrainingCampVO();
 					 feedbackCountVo.setId(commonMethodsUtilService.getLongValueForObject(param[0]));
 					 feedbackCountVo.setName(commonMethodsUtilService.getStringValueForObject(param[1]));
 					 countOfcadreIds= mainMapCount.get(commonMethodsUtilService.getLongValueForObject(param[0]));
 					 if(countOfcadreIds == null){
 						 feedbackCountVo.setFeedBackProgramCount(0L);
-					 }else{
-						 feedbackCountVo.setFeedBackProgramCount(countOfcadreIds);
-					 }
-					 if(countOfcadreIds == null){
 						 feedbackCountVo.setProgramId(0L);
 					 }else{
-						 feedbackCountVo.setProgramId((commonMethodsUtilService.getLongValueForObject(param[2])/countOfcadreIds)*100);
+						 feedbackCountVo.setFeedBackProgramCount(countOfcadreIds);
+						 feedbackCountVo.setAttendedPercentage((((commonMethodsUtilService.getDoubleValueForObject(param[2]))*100)/Double.valueOf((countOfcadreIds.toString()))));
 					 }
-					 finalVo.getBatchDetails().add(feedbackCountVo);
-					 
+					 finalVo.getTrainingCampVOList().add(feedbackCountVo);
 					 }
-				
 			}
 		 return finalVo;
 			  
