3253f4ec1b5dd07acf760f3d9b1c28584ba93b4f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-14 14:33:54 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/IPmOfficerUserDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmOfficerUserDAO.java
@@ -1,9 +1,12 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.PmOfficerUser;
 
 public interface IPmOfficerUserDAO extends GenericDao<PmOfficerUser, Long> {
 
+	public Object[] getPmOffceUserDetails(Long userId);
 }
--- prrws/src/main/java/com/itgrids/dao/IUserDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IUserDAO.java
@@ -7,6 +7,6 @@ import com.itgrids.model.User;
 public interface IUserDAO extends GenericDao<User,Long>{
 	
 	public User loginAuthentication(String userName,String password);
-	public String getUrlForMatchedCredentials(String userName, String password);
+	public Object[] getUrlForMatchedCredentials(String userName, String password);
 	
 }
