7071e07ea9b46b89c2841b81c78edf5d50ce6c86
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-14 14:52:14 +0000

    commited by - Babu kurakula

--- prrws/src/main/java/com/itgrids/dao/IPmDesignationHeirarchyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.itgrids.dao;
-
-import org.appfuse.dao.GenericDao;
-
-import com.itgrids.model.PmDesignationHeirarchy;
-
-public interface IPmDesignationHeirarchyDAO extends GenericDao<PmDesignationHeirarchy, Long> {
-
-}
