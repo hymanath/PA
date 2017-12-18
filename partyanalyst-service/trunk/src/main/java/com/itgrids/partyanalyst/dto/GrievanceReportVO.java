017e0c409cc7fbe7f43672891489e3e979690363
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-18 06:00:36 +0000

    committed by raghu for grivance coreDashBoard

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/GrievanceReportVO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dto/GrievanceReportVO.java
@@ -10,9 +10,87 @@ public class GrievanceReportVO {
 	private String status;
 	private String issueType;
 	private List<ComplaintScanCopyVO> scanCopyList = new ArrayList<ComplaintScanCopyVO>();
+	private Double efficencyPercentage;
+	private Long grivanceCount;
+	private String number;
+	private String districtName;
+	private String consitutencyName;
+	private String mandalName;
+	private String villageName;
+	private String description;
+	private String subject;
+	private String updatedDate;
+	private String referenceName;
 	
 	
 	
+	
+	public String getReferenceName() {
+		return referenceName;
+	}
+	public void setReferenceName(String referenceName) {
+		this.referenceName = referenceName;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getDistrictName() {
+		return districtName;
+	}
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
+	public String getConsitutencyName() {
+		return consitutencyName;
+	}
+	public void setConsitutencyName(String consitutencyName) {
+		this.consitutencyName = consitutencyName;
+	}
+	public String getMandalName() {
+		return mandalName;
+	}
+	public void setMandalName(String mandalName) {
+		this.mandalName = mandalName;
+	}
+	public String getVillageName() {
+		return villageName;
+	}
+	public void setVillageName(String villageName) {
+		this.villageName = villageName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(String updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public Long getGrivanceCount() {
+		return grivanceCount;
+	}
+	public void setGrivanceCount(Long grivanceCount) {
+		this.grivanceCount = grivanceCount;
+	}
+	public Double getEfficencyPercentage() {
+		return efficencyPercentage;
+	}
+	public void setEfficencyPercentage(Double efficencyPercentage) {
+		this.efficencyPercentage = efficencyPercentage;
+	}
 	public List<ComplaintScanCopyVO> getScanCopyList() {
 		return scanCopyList;
 	}
