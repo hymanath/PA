e859ee7df2a2c3f3669a3a682a75b4f8b01e5a5b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-20 11:11:27 +0000

    updated by swapna  --ENCServices

---
+++ prrws/src/main/java/com/itgrids/dto/EncTargetsVO.java
@@ -0,0 +1,166 @@
+package com.itgrids.dto;
+
+public class EncTargetsVO {
+	private Long locationId;
+	private String locationName;
+	private Long q2Achv=0L;
+	private Long scheme=0L;
+	private Double agreementAmount=0.00;
+	private Long q1Achv=0L;
+	private Double totPopu=0.00;
+	private Long q3Achv=0L;
+	private Long q1Target=0L;
+	private Long q3Target=0L;
+	private Long totTarget=0L;
+	private Long q4Target=0L;
+	private Long q2Target=0L;
+	private Long totWorks=0L;
+	private Long q4Achv=0L;
+	private Long q3Per=0L;
+	private Long q1Per=0L;
+	private Long totAchv=0L;
+	private Double totLength=0.00;
+	private Long q2Per=0L;
+	private String schemeName;
+	private Long totPer=0L;
+	private Long q4Per=0L;
+	
+	public Long getLocationId() {
+		return locationId;
+	}
+	public void setLocationId(Long locationId) {
+		this.locationId = locationId;
+	}
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	public Long getQ2Achv() {
+		return q2Achv;
+	}
+	public void setQ2Achv(Long q2Achv) {
+		this.q2Achv = q2Achv;
+	}
+	public Long getScheme() {
+		return scheme;
+	}
+	public void setScheme(Long scheme) {
+		this.scheme = scheme;
+	}
+	public Double getAgreementAmount() {
+		return agreementAmount;
+	}
+	public void setAgreementAmount(Double agreementAmount) {
+		this.agreementAmount = agreementAmount;
+	}
+	public Long getQ1Achv() {
+		return q1Achv;
+	}
+	public void setQ1Achv(Long q1Achv) {
+		this.q1Achv = q1Achv;
+	}
+	public Double getTotPopu() {
+		return totPopu;
+	}
+	public void setTotPopu(Double totPopu) {
+		this.totPopu = totPopu;
+	}
+	public Long getQ3Achv() {
+		return q3Achv;
+	}
+	public void setQ3Achv(Long q3Achv) {
+		this.q3Achv = q3Achv;
+	}
+	public Long getQ1Target() {
+		return q1Target;
+	}
+	public void setQ1Target(Long q1Target) {
+		this.q1Target = q1Target;
+	}
+	public Long getQ3Target() {
+		return q3Target;
+	}
+	public void setQ3Target(Long q3Target) {
+		this.q3Target = q3Target;
+	}
+	public Long getTotTarget() {
+		return totTarget;
+	}
+	public void setTotTarget(Long totTarget) {
+		this.totTarget = totTarget;
+	}
+	public Long getQ4Target() {
+		return q4Target;
+	}
+	public void setQ4Target(Long q4Target) {
+		this.q4Target = q4Target;
+	}
+	public Long getQ2Target() {
+		return q2Target;
+	}
+	public void setQ2Target(Long q2Target) {
+		this.q2Target = q2Target;
+	}
+	public Long getTotWorks() {
+		return totWorks;
+	}
+	public void setTotWorks(Long totWorks) {
+		this.totWorks = totWorks;
+	}
+	public Long getQ4Achv() {
+		return q4Achv;
+	}
+	public void setQ4Achv(Long q4Achv) {
+		this.q4Achv = q4Achv;
+	}
+	public Long getQ3Per() {
+		return q3Per;
+	}
+	public void setQ3Per(Long q3Per) {
+		this.q3Per = q3Per;
+	}
+	public Long getQ1Per() {
+		return q1Per;
+	}
+	public void setQ1Per(Long q1Per) {
+		this.q1Per = q1Per;
+	}
+	public Long getTotAchv() {
+		return totAchv;
+	}
+	public void setTotAchv(Long totAchv) {
+		this.totAchv = totAchv;
+	}
+	public Double getTotLength() {
+		return totLength;
+	}
+	public void setTotLength(Double totLength) {
+		this.totLength = totLength;
+	}
+	public Long getQ2Per() {
+		return q2Per;
+	}
+	public void setQ2Per(Long q2Per) {
+		this.q2Per = q2Per;
+	}
+	public String getSchemeName() {
+		return schemeName;
+	}
+	public void setSchemeName(String schemeName) {
+		this.schemeName = schemeName;
+	}
+	public Long getTotPer() {
+		return totPer;
+	}
+	public void setTotPer(Long totPer) {
+		this.totPer = totPer;
+	}
+	public Long getQ4Per() {
+		return q4Per;
+	}
+	public void setQ4Per(Long q4Per) {
+		this.q4Per = q4Per;
+	}
+	}
