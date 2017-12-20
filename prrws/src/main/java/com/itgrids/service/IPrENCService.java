6d9ba44ae472e9cfb707aecbee8427227aadd204
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-20 11:11:48 +0000

    updated by swapna  --ENCServices

--- prrws/src/main/java/com/itgrids/service/IPrENCService.java
+++ prrws/src/main/java/com/itgrids/service/IPrENCService.java
@@ -2,6 +2,7 @@ package com.itgrids.service;
 
 import java.util.List;
 
+import com.itgrids.dto.EncTargetsVO;
 import com.itgrids.dto.EncVO;
 import com.itgrids.dto.EncWorksVO;
 import com.itgrids.dto.InputVO;
@@ -13,5 +14,9 @@ public interface IPrENCService {
 	public EncVO getStateWiseRoadsInformation(InputVO inputVO);
 
 	public List<EncWorksVO> getLocationWiseWorksInformation(InputVO inputVO);
+	
+	public List<EncTargetsVO> getEncTargetsAchievement(InputVO inputVO);
+	
+	
 
 }
