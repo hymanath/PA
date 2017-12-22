03c8d3f1759548841734316d2b3de18683c82704
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-22 19:28:22 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/dao/impl/PmSubWorkCoveringLetterDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PmSubWorkCoveringLetterDAO.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.appfuse.dao.SearchException;
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -21,4 +22,10 @@ public class PmSubWorkCoveringLetterDAO extends GenericDaoHibernate<PmSubWorkCov
 		super(PmSubWorkCoveringLetter.class);
 	}
 
+	public List<String> getCoveringLetterDetailsByEndorsmentNo(String endorsmenNo){
+		StringBuilder str = new StringBuilder();
+		str.append("select distinct model.document.path from PmSubWorkCoveringLetter model where model.endorsmentNo ='"+endorsmenNo+"'");
+		Query query = getSession().createQuery(str.toString());
+		return query.list();				
+	}
 }
