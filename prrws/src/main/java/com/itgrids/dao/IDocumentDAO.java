fa14dffa1a32685151676637d4fe0c653a19e76f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2017-12-22 03:39:43 +0000

    updated by srishailam

--- prrws/src/main/java/com/itgrids/dao/IDocumentDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IDocumentDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.Document;
 
 public interface IDocumentDAO extends GenericDao<Document, Long> {
-
+	public List<Long> getdocumentByFilePath(String path);
 }
