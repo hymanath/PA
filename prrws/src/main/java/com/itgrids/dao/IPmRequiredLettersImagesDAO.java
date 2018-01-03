dd53f79750a145786e5d8163ef1fdd41e181c43e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-03 11:21:33 +0000

    updated by swapna

---
+++ prrws/src/main/java/com/itgrids/dao/IPmRequiredLettersImagesDAO.java
@@ -0,0 +1,12 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmRequiredLettersImages;
+
+
+
+
+
+public interface IPmRequiredLettersImagesDAO extends GenericDao<PmRequiredLettersImages, Long > {
+}
