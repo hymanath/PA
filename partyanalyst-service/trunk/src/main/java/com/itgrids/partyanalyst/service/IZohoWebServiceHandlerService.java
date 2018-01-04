91e9afd78dadbcffd203c0b1db3f9b8cd4af856b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-04 09:14:56 +0000

    get mobile no based on member ship id for zoho alerts - Nagarjuna Gowd

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IZohoWebServiceHandlerService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IZohoWebServiceHandlerService.java
@@ -2,5 +2,7 @@ package com.itgrids.partyanalyst.service;
 
 public interface IZohoWebServiceHandlerService {
 
+	public String getMobileNoByMemberShip(String memberShipId);
+	
 	public String testMethod();
 }
