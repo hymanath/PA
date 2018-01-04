dc951e30a3b48b91d9a09b1a1dc35e0a29add4ce
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-04 09:16:14 +0000

    get mobile no based on member ship id for zoho alerts - Nagarjuna Gowd

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/webservice/ZohoWebServiceHandler.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/webservice/ZohoWebServiceHandler.java
@@ -1,19 +1,23 @@
 package com.itgrids.partyanalyst.webservice;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.itgrids.partyanalyst.service.IZohoWebServiceHandlerService;
 
 @Component
-@Path("/Zoho/")
+@Path("/Zoho")
 public class ZohoWebServiceHandler {
 
+	private static final Logger LOG = Logger.getLogger(ZohoWebServiceHandler.class);
 	@Autowired
 	private IZohoWebServiceHandlerService zohoWebServiceHandlerService;
 	
@@ -25,4 +29,17 @@ public class ZohoWebServiceHandler {
 		System.out.println(zohoWebServiceHandlerService.testMethod());
 		return "http://mytdp.com/WebService/Zoho/";
     }
+	
+	@GET 
+	@Path("/getMobileNoByMemberShipId/{memberShipId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String getMobileNoByMemberShipId(@PathParam("memberShipId") String memberShipId){
+		try {
+			return zohoWebServiceHandlerService.getMobileNoByMemberShip(memberShipId);
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getMobileNoByMemberShipId() Method in ZohoWebServiceHandler ",e);
+		}
+		return null;
+	}
 }
