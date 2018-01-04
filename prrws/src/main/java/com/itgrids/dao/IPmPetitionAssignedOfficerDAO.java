75ef4b328f7e7196101b468d72b1d5a8e2fe81ea
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-04 06:44:15 +0000

    Updated by krishna

---
+++ prrws/src/main/java/com/itgrids/dao/IPmPetitionAssignedOfficerDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmPetitionAssignedOfficer;
+
+public interface IPmPetitionAssignedOfficerDAO extends GenericDao<PmPetitionAssignedOfficer, Long> {
+
+}
