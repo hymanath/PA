b630e67dc025d5e619e0eac5c5a74d934b3f5d48
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-05 08:57:53 +0000

    Updated By Sanjeev

--- prrws/src/main/webapp/Assests/ruralWaterSupply/ruralWaterSupplyDashBoard.js
+++ prrws/src/main/webapp/Assests/ruralWaterSupply/ruralWaterSupplyDashBoard.js
@@ -1,6 +1,19 @@
  //Angular Start  getAlertsOfCategoryByStatusWise()
-		var glStartDate = "01-04-"+moment().format("YYYY");
-		var glEndDate = "01-04-"+moment().add(1, 'years').format("YYYY");
+	var month =moment().format("M");
+	var glStartDate="";
+	var glEndDate="";
+	var year ="";
+	if(month >= 04){
+		glStartDate = "01-04-"+moment().format("YYYY");
+		glEndDate = "01-04-"+moment().add(1, 'years').format("YYYY");
+		year=moment().format("YYYY");
+	}else{
+		glStartDate = "01-04-"+moment().subtract(1, 'years').format("YYYY");
+		glEndDate = "01-04-"+moment().format("YYYY");
+		year=moment().subtract(1, 'years').format("YYYY");
+	}
+	
+		
 		var globalStatusObj={"QA":"#494949","PC":"#FC5049","FC":"#14BAAD","Ground":"#14BAAD","Surface":"#FC5049","SAFE":"#14BAAD","UN-SAFE":"#FC5049",
 		"SINGAL VILLAGE":"#14BAAD","MULTI VILLAGE":"#FC5049","physicalTestCount":"#14BAAD","bacterialTestCount":"#FC5049",
 		"Completely Satisfied":"#0FBE08","Not Satisfied":"#FF0909","Partially Satisfied":"#FFBA00","SATISFIED":"#0FBE08","PARTIALLY SATISFIED":"#FFBA00","NOT SATISFIED":"#FF0909","TARGET":"#FC5049","COMPLETED":"#14BAAD"}
@@ -3321,7 +3334,7 @@
 					$("#financialYearId").append("<option value="+value[0]+">"+result[i].financialYear+"</option>");
 					
 				}
-				$("#financialYearId").val('2017');
+				$("#financialYearId").val(year);
 			}
 			
 			$("#financialYearId").chosen();
