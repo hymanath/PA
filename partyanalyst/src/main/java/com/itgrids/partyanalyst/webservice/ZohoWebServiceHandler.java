13b7de053a78214821bd33c6fa34af004768fd7c
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-06 12:02:33 +0000

    zoho user otp sent and verification - Nagarjuna Gowd

--- partyanalyst/src/main/java/com/itgrids/partyanalyst/webservice/ZohoWebServiceHandler.java
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/webservice/ZohoWebServiceHandler.java
@@ -2,6 +2,7 @@ package com.itgrids.partyanalyst.webservice;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,6 +12,8 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.itgrids.partyanalyst.dto.AmsVO;
+import com.itgrids.partyanalyst.dto.ResultStatus;
 import com.itgrids.partyanalyst.service.IZohoWebServiceHandlerService;
 
 @Component
@@ -42,4 +45,30 @@ public class ZohoWebServiceHandler {
 		}
 		return null;
 	}
+	
+	@POST 
+	@Path("/otpVerification")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ResultStatus otpVerification(AmsVO vo){
+		try {
+			return zohoWebServiceHandlerService.mobileOtpVerification(vo);
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getMobileNoByMemberShipId() Method in ZohoWebServiceHandler ",e);
+		}
+		return null;
+	}
+	/*
+	@POST 
+	@Path("/sendOTP")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String sendOTPAtLoginTime(){
+		try {
+			return zohoWebServiceHandlerService.sendOTPAtLoginTime(memberShipId);
+		} catch (Exception e) {
+			LOG.error("Exception Occured in getMobileNoByMemberShipId() Method in ZohoWebServiceHandler ",e);
+		}
+		return null;
+	}*/
 }
