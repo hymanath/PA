de977902aecaa695dc252f2656ee4ca08baaea41
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-09 08:12:57 +0000

    jwt token generation  changes - Nagarjuna Gowd

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IZohoAlertService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IZohoAlertService.java
@@ -2,10 +2,12 @@ package com.itgrids.partyanalyst.service;
 
 import org.codehaus.jettison.json.JSONObject;
 
+import com.itgrids.partyanalyst.dto.ResultStatus;
+
 public interface IZohoAlertService {
 	
 	public String getMobileNoByMemberShip(String memberShipId);
 	public String sendOtp(String phoneNumber,Long cadreId,String membershipId);
-	public String checkOTPDetails(JSONObject jObj);
+	public JSONObject checkOTPDetails(JSONObject jObj);
 
 }
