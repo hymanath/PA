e1c45cd864736ab7ebc494210345a5dde431c88b
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-11 07:02:27 +0000

    updated by swapna

--- prrws/src/main/java/com/itgrids/service/ILocationDetailsService.java
+++ prrws/src/main/java/com/itgrids/service/ILocationDetailsService.java
@@ -26,8 +26,8 @@ public interface ILocationDetailsService {
 	
 	public List<KeyValueVO> getPmDesignations(String searchType);
 	public List<KeyValueVO> getDistrictBySearchType(InputVO  inputVO,List<Long> deptIds);
-	public List<KeyValueVO> getConstituenciesBySearchTypeAndDistrictId(InputVO  inputVO,List<Long> districtIds,List<Long> deptIds,List<Long> pmDesignationIds,String type);
-	public List<KeyValueVO> getMandalsBySearchTypeAndConstituencyId(String serchType,List<Long> conIds,List<Long> deptIds,String fromDate,String toDate,List<Long> desigids,String desigType);
+	public List<KeyValueVO> getConstituenciesBySearchTypeAndDistrictId(InputVO  inputVO,List<Long> districtIds,List<Long> deptIds,List<Long> pmDesignationIds,String type,List<Long> statIds);
+	public List<KeyValueVO> getMandalsBySearchTypeAndConstituencyId(String serchType,List<Long> conIds,List<Long> deptIds,String fromDate,String toDate,List<Long> desigids,String desigType,List<Long> statIds);
 	public List<KeyValueVO> getDesignationsBySearchType(String searchType,String fromDate,String toDate,List<Long> deptIds,Long desigId,List<Long> statusId);
 	public List<KeyValueVO> getDepartmentsBySearchType(String searchType,String fromDate,String toDate,List<Long> deptIds,List<Long> statusIds);
 	public List<KeyValueVO> getSubjectsBySearchType(String searchType,String fromDate,String toDate,List<Long> deptIds,List<Long> statusId,String subjectId);
