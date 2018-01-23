b0a881991d346c7bd062a0618f5c720d3e7a36a3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-23 11:16:35 +0000

    Updated By Sanjeev

---
+++ partyanalyst/src/main/java/com/itgrids/partyanalyst/web/action/ImageRenderingAction.java
@@ -0,0 +1,38 @@
+package com.itgrids.partyanalyst.web.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+
+import com.itgrids.partyanalyst.dto.RegistrationVO;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ImageRenderingAction extends ActionSupport implements	ServletRequestAware {
+
+	private HttpServletRequest request;
+	public void setServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public String imageRendering()
+	{
+		try {
+			
+			final HttpSession session = request.getSession();
+			
+		/*	final RegistrationVO user = (RegistrationVO) session.getAttribute("USER");
+			if(user == null || user.getRegistrationID() == null){
+				return ERROR;
+			*///}
+		}catch(Exception e) {
+			LOG.error("Exception raised at execute() in CoreDashBoard Action class", e);
+		}
+		return Action.SUCCESS;
+	
+	}
+	
+}
