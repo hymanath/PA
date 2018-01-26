77219d95a6a2b46c73a945e35014d8788a7f7079
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-01-26 08:37:04 +0000

    Component Wise Comments Summary Implementation -- SRAVANTH.

--- prrws/src/main/java/com/itgrids/dao/impl/ComponentWiseAchievementConfigurationDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/ComponentWiseAchievementConfigurationDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.dao.impl;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import com.itgrids.dao.IComponentWiseAchievementConfigurationDAO;
@@ -13,4 +16,18 @@ public class ComponentWiseAchievementConfigurationDAO extends GenericDaoHibernat
 		super(ComponentWiseAchievementConfiguration.class);
 	}
 
+	public List<Object[]> getComponentWiseMandalAchievementPercentage(String componentName,String locationType,String locationIdStr){
+		StringBuilder sb = new StringBuilder();
+		sb.append("select model.scopeValue,model.achievedPercentage"
+				+ " from ComponentWiseAchievementConfiguration model"
+				+ " where model.isDeleted = 'N' and model.nregaComponent.componentName = :componentName");
+		if(locationType != null && locationType.equalsIgnoreCase("district") && locationIdStr != null)
+			sb.append(" and model.locationAddress.district.prDistrict.districtCode = :locationIdStr");
+		
+		Query query = getSession().createQuery(sb.toString());
+		query.setParameter("componentName", componentName);
+		if(locationType != null && locationType.equalsIgnoreCase("district") && locationIdStr != null)
+			query.setParameter("locationIdStr", locationIdStr);
+		return query.list();
+	}
 }
