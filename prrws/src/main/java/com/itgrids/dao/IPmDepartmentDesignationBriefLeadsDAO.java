84cb6b27966ace16628508b5d30c48823be90265
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-01 14:32:07 +0000

    updated by krishna

---
+++ prrws/src/main/java/com/itgrids/dao/IPmDepartmentDesignationBriefLeadsDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.PmDepartmentDesignationBriefLeads;
+
+public interface IPmDepartmentDesignationBriefLeadsDAO extends GenericDao<PmDepartmentDesignationBriefLeads, Long> {
+
+}
