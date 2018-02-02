4e189f6ba364dc03d04390675909d4f4ef54ec5a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-02 09:53:52 +0000

    WMS model -- sandeep

---
+++ prrws/src/main/java/com/itgrids/model/GovtWorkProgressUpdate.java
@@ -0,0 +1,75 @@
+package com.itgrids.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "govt_work_progress_update")
+public class GovtWorkProgressUpdate {
+	private Long govtWorkProgressUpdateId;
+	private Long govtWorkId;
+	private Long updatedBy;
+	private Date updatedTime;
+	
+	private GovtWork govtWork;
+	private MobileAppUser updatedByUSer;
+	
+	@Column(name="govt_work_progress_update_id")
+	public Long getGovtWorkProgressUpdateId() {
+		return govtWorkProgressUpdateId;
+	}
+	public void setGovtWorkProgressUpdateId(Long govtWorkProgressUpdateId) {
+		this.govtWorkProgressUpdateId = govtWorkProgressUpdateId;
+	}
+	
+	@Column(name="govt_work_id")
+	public Long getGovtWorkId() {
+		return govtWorkId;
+	}
+	public void setGovtWorkId(Long govtWorkId) {
+		this.govtWorkId = govtWorkId;
+	}
+	
+	@Column(name="updated_by")
+	public Long getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Long updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	
+	@Column(name="updated_time")
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "govt_work_id", insertable = false, updatable = false)
+	public GovtWork getGovtWork() {
+		return govtWork;
+	}
+	public void setGovtWork(GovtWork govtWork) {
+		this.govtWork = govtWork;
+	}
+	
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "updated_by", insertable = false, updatable = false)
+	public MobileAppUser getUpdatedByUSer() {
+		return updatedByUSer;
+	}
+	public void setUpdatedByUSer(MobileAppUser updatedByUSer) {
+		this.updatedByUSer = updatedByUSer;
+	}
+	
+	
+}
