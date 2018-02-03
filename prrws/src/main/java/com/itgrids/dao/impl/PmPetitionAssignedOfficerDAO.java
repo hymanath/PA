996b76bab1509f728a7ced5dee358413b4045bff
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-03 03:29:04 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/dao/impl/PmPetitionAssignedOfficerDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PmPetitionAssignedOfficerDAO.java
@@ -1,6 +1,9 @@
 package com.itgrids.dao.impl;
 
+import java.util.List;
+
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -17,5 +20,11 @@ public class PmPetitionAssignedOfficerDAO extends GenericDaoHibernate<PmPetition
 		super(PmPetitionAssignedOfficer.class);
 		
 	}
-
+	public List<Object[]> getAssignedPetitionforPetitionDeptDesignationOfficer(List<Long> pmDeptDesignationOfficerIdsList){
+		StringBuilder str = new StringBuilder();
+		str.append("select distinct model.petitionId,model.pmSubWorkDetailsId from PmPetitionAssignedOfficer model where model.isDeleted='N' and model.pmDepartmentDesignationOfficerId in (:pmDeptDesignationOfficerIdsList)  ");
+		Query query = getSession().createQuery(str.toString());
+		query.setParameterList("pmDeptDesignationOfficerIdsList", pmDeptDesignationOfficerIdsList);
+		return query.list();
+	}
 }
