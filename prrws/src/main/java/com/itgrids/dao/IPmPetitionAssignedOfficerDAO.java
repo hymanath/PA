262eca38a0baf10adb32a5f11f60dac709960328
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-03 03:29:22 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/dao/IPmPetitionAssignedOfficerDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmPetitionAssignedOfficerDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.PmPetitionAssignedOfficer;
 
 public interface IPmPetitionAssignedOfficerDAO extends GenericDao<PmPetitionAssignedOfficer, Long> {
-
+	public List<Object[]> getAssignedPetitionforPetitionDeptDesignationOfficer(List<Long> pmDeptDesignationOfficerIdsList);
 }
--- prrws/src/main/java/com/itgrids/dao/IPmSubWorkDetailsDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmSubWorkDetailsDAO.java
@@ -2,6 +2,7 @@ package com.itgrids.dao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import org.appfuse.dao.GenericDao;
 
@@ -17,13 +18,13 @@ public interface IPmSubWorkDetailsDAO extends GenericDao<PmSubWorkDetails, Long>
 	
 	public List<Long> getPmSubWorkDetailsIds(Long petitionId);
 	public int updatePmsubWorkDetails(List<Long> subWorkDetailsIds,Date updateTime,Long userId);
-	public List<Object[]> getCompleteOrStatusOverviewDetails(List<Long> deptIds ,Date startDate,Date endDate,String type);
-	public List<Object[]> getLeadWiseOverviewDetails(List<Long> deptIds ,Date startDate,Date endDate);
+	public List<Object[]> getCompleteOrStatusOverviewDetails(List<Long> deptIds ,Date startDate,Date endDate,String type,Set<Long> petitionsIdsList);
+	public List<Object[]> getLeadWiseOverviewDetails(List<Long> deptIds ,Date startDate,Date endDate,Set<Long> petitionsIdsList);
 	public List<Long> getPetitionsSubWorksIdsList(List<Long>  petitionIdsList);
 	public int updatePetitionSubWorkStatusdetails(List<Long>  petitionIdsList,Date updatedDate,Long updatedUserId,Long pmStatusId);
 	public List<Object[]> getPetitionsDetailedSubWorksIdsList(List<Long>  petitionIdsList);
 	public List<Object[]> getSubjectsForSearchPage(List<Long> deptIds,Date fromDate,Date toDate,List<Long> statusId,Long subjectId);
-	public List<Object[]> getReferralWiseOverviewDetails(InputVO inputVO,Date startDate,Date endDate);
+	public List<Object[]> getReferralWiseOverviewDetails(InputVO inputVO,Date startDate,Date endDate,Set<Long> petitionsIdsList);
 	public List<Object[]> getPmBriefLeadIds(List<Long> deptIds);
 	public List<Object[]> getAllWorksLatesStatusDetails(Long petitionId);
 	public Object[] getMaxEndorsementAndTempEndorsementNos();
