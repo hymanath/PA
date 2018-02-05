ddd7346c43dda959996d478f2229e53d2e1aaa0a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-05 12:09:59 +0000

    updated by Srishailam

--- prrws/src/main/java/com/itgrids/dao/IPmDeptDesignationPrePostStatusDetailsDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IPmDeptDesignationPrePostStatusDetailsDAO.java
@@ -1,9 +1,11 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.PmDeptDesignationPrePostStatusDetails;
 
 public interface IPmDeptDesignationPrePostStatusDetailsDAO extends GenericDao<PmDeptDesignationPrePostStatusDetails, Long> {
-
+	public List<Long> getItSelfandPoststatusDetail(Long statusId);
 }
