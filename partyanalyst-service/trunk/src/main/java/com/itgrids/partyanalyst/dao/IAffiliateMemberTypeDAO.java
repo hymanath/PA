9707183fee68eddabd9a46cdfb310570c915e8b0
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-05 12:21:24 +0000

    Updated By Sanjeev

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAffiliateMemberTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AffiliateMemberType;
+
+public interface IAffiliateMemberTypeDAO extends GenericDao<AffiliateMemberType, Long> {
+
+}
