73129a39facabb0411dc907f537a91c53d952be5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-06 11:37:51 +0000

    Updated By Sanjeev

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IitdpAppUserDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ITdpAppUser;
+
+public interface IitdpAppUserDAO extends GenericDao<ITdpAppUser, Long> {
+
+}
