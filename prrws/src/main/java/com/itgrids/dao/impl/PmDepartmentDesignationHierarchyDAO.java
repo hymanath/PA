56d0c4f9bf6d94c7a481d17ad624ea885a475068
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-07 13:13:24 +0000

    Updated By Hyma : Forward scenario testing

--- prrws/src/main/java/com/itgrids/dao/impl/PmDepartmentDesignationHierarchyDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/PmDepartmentDesignationHierarchyDAO.java
@@ -21,7 +21,7 @@ public class PmDepartmentDesignationHierarchyDAO extends GenericDaoHibernate<PmD
 		super(PmDepartmentDesignationHierarchy.class);
 	}
 	
-	public List<Object[]> getSubDesignationDetailsForParentDeptDesignations(List<Long> deptDesignationIdsList){
+	public List<Object[]> getSubDesignationDetailsForParentDeptDesignations(List<Long> deptDesignationIdsList,List<Long> deptIdsList){
 		if(deptDesignationIdsList != null && deptDesignationIdsList.size()>0){
 			StringBuilder str = new StringBuilder();
 		
@@ -30,16 +30,22 @@ public class PmDepartmentDesignationHierarchyDAO extends GenericDaoHibernate<PmD
 					"  from PmDepartmentDesignationHierarchy model " );*/
 			str.append(" select distinct model.subPmDepartmentDesignation.pmOfficerDesignation.pmOfficerDesignationId,"
 					  + " model.subPmDepartmentDesignation.pmOfficerDesignation.designation " +
-					"  from PmDepartmentDesignationHierarchy model " );
+					"  from PmDepartmentDesignationHierarchy model where model.isActive='Y' " );
 			
 			if(deptDesignationIdsList != null && deptDesignationIdsList.size() >0){
-				str.append(" where model.pmDepartmentDesignationId in (:deptDesignationIdsList) ");
+				str.append(" and model.pmDepartmentDesignationId in (:deptDesignationIdsList) ");
+			}
+			if(deptIdsList != null && deptIdsList.size() >0){
+				str.append(" and model.subPmDepartmentDesignation.pmDepartmentId in (:deptIdsList)  ");
 			}
 			//str.append(" and model.isActive='Y' and  model.subPmDepartmentDesignation.isDeleted='N' order by  model.subPmDepartmentDesignation.pmOfficerDesignation.designation ");
-			str.append(" and model.isActive='Y' and  model.subPmDepartmentDesignation.isDeleted='N' order by  model.orderNo ");
+			str.append(" and    model.subPmDepartmentDesignation.isDeleted='N' order by  model.orderNo ");
 			Query query = getSession().createQuery(str.toString());
 			if(deptDesignationIdsList != null && deptDesignationIdsList.size() >0){
 				query.setParameterList("deptDesignationIdsList", deptDesignationIdsList);
+			}
+			if(deptIdsList != null && deptIdsList.size() >0){
+				query.setParameterList("deptIdsList", deptIdsList);
 			}
 			return query.list();
 		}
