39c5e2e220db289615a6acb3e86bb7fd0fa72772
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-09 13:41:06 +0000

    Updated By Sanjeev

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityLocationParticipantDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityLocationParticipant;
+
+public interface IActivityLocationParticipantDAO extends GenericDao<ActivityLocationParticipant, Long> {
+
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityParticipantMemberTypeDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityParticipantMemberType;
+
+public interface IActivityParticipantMemberTypeDAO extends GenericDao<ActivityParticipantMemberType, Long> {
+
+}
