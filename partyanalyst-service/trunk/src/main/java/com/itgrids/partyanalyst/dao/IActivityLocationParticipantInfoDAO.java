514e52123e93d033103fa1b2600364c3218e18de
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-09 13:41:06 +0000

    Updated By Sanjeev

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IActivityLocationParticipantInfoDAO.java
@@ -0,0 +1,14 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+import java.util.Set;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.ActivityLocationParticipantInfo;
+
+public interface IActivityLocationParticipantInfoDAO extends GenericDao<ActivityLocationParticipantInfo, Long> {
+	
+	public List<Object[]> getCoveredPeopleOfActivity(Long activityId,Long locationScope, Set<Long> locationValues);
+
+}
