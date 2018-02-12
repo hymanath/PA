46476c06794761bc418b20a8f402d45fa751fc14
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-12 08:32:48 +0000

    updated by swapna   SWM

--- prrws/src/main/java/com/itgrids/service/ISolidWasteManagementService.java
+++ prrws/src/main/java/com/itgrids/service/ISolidWasteManagementService.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import com.itgrids.dto.InputVO;
 import com.itgrids.dto.SolidWasteManagementVO;
+import com.itgrids.dto.WebServiceDataVO;
 
 
 public interface ISolidWasteManagementService 
@@ -10,5 +11,8 @@ public interface ISolidWasteManagementService
 	public List<SolidWasteManagementVO> getSolidInfoLocationWise(InputVO inputVO);	
 	public SolidWasteManagementVO getSolidWasteManagementOverAllCounts(InputVO inputVO);
 	public List<SolidWasteManagementVO> getRfidTrackingOverAllTargets(InputVO inputVO);
+	public  WebServiceDataVO saveRfidTrackingOverAllTargets(); 
+	
+	
 }
 
