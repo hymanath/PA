a0236689048b09f2014a16933400811448d0bbd3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-14 06:41:49 +0000

    Updated by ---Teja

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCallerRelationDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AlertCallerRelationDAO.java
@@ -1,8 +1,10 @@
 package com.itgrids.partyanalyst.dao.hibernate;
 
+import java.util.Date;
 import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
 
 import com.itgrids.partyanalyst.dao.IAlertCallerRelationDAO;
@@ -55,4 +57,29 @@ public class AlertCallerRelationDAO extends GenericDaoHibernate<AlertCallerRelat
 		query.setParameter("alertId", alertId);
 		return (Long) query.uniqueResult();
 	}
+	@SuppressWarnings("unchecked")
+	public Long totalCallCenterCallForRwsDept(Date fromDate,Date toDate){
+		StringBuilder str = new StringBuilder();
+		str.append(" SELECT COUNT(AC.alert_caller_id) as count" +
+				" FROM alert A " +
+				" LEFT JOIN alert_caller_relation ACR ON A.alert_id = ACR.alert_id" +
+				" LEFT  JOIN alert_caller AC ON ACR.alert_caller_id = AC.alert_caller_id" +
+				" LEFT OUTER JOIN alert_category CG ON A.alert_category_id = CG.alert_category_id" +
+				" WHERE" +
+				" A.is_deleted = 'N' AND ACR.is_deleted = 'N' AND A.govt_department_id =:govtDeptId " +
+				" and CG.alert_category_id =:categoryId ");
+	 		
+	 		if(fromDate !=null && toDate !=null){
+	 			str.append(" and date(A.created_time) between :fromDate and :toDate ");
+	 		}
+	 	Query query = getSession().createSQLQuery(str.toString())
+	 			.addScalar("count",Hibernate.LONG);
+	 		query.setParameter("govtDeptId",49l);
+	 		query.setParameter("categoryId",4l);
+	 		if(fromDate !=null && toDate !=null){
+	 			query.setParameter("fromDate",fromDate);
+	 			query.setParameter("toDate",toDate);
+	 		}
+	 	return (Long) query.uniqueResult();
+	 }
 }
