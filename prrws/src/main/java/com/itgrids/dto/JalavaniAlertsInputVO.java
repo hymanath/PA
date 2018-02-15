d4992f5344338129267dbe8bfa10604f9e1a98e3
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-15 07:47:43 +0000

    Updated by ---Teja

---
+++ prrws/src/main/java/com/itgrids/dto/JalavaniAlertsInputVO.java
@@ -0,0 +1,71 @@
+package com.itgrids.dto;
+
+import java.io.Serializable;
+
+public class JalavaniAlertsInputVO implements Serializable{
+	
+	private Long id;
+	private String name;
+	private String fromDateStr;
+	private String toDateStr;
+	private String searchType;
+	private String type;
+	private Long locationTypeId;
+	private Long subLocationId;
+	private Long alertCategoryId;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Long getLocationTypeId() {
+		return locationTypeId;
+	}
+	public void setLocationTypeId(Long locationTypeId) {
+		this.locationTypeId = locationTypeId;
+	}
+	public Long getSubLocationId() {
+		return subLocationId;
+	}
+	public void setSubLocationId(Long subLocationId) {
+		this.subLocationId = subLocationId;
+	}
+	public Long getAlertCategoryId() {
+		return alertCategoryId;
+	}
+	public void setAlertCategoryId(Long alertCategoryId) {
+		this.alertCategoryId = alertCategoryId;
+	}
+	public String getSearchType() {
+		return searchType;
+	}
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFromDateStr() {
+		return fromDateStr;
+	}
+	public void setFromDateStr(String fromDateStr) {
+		this.fromDateStr = fromDateStr;
+	}
+	public String getToDateStr() {
+		return toDateStr;
+	}
+	public void setToDateStr(String toDateStr) {
+		this.toDateStr = toDateStr;
+	}
+}
