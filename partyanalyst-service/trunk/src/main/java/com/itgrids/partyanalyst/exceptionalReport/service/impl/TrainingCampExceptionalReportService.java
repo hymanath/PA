edc9ef7ce6c8c2634bcdd99546cfb5ebffdb7d0d
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-19 09:15:14 +0000

    configured by -> swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/impl/TrainingCampExceptionalReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/impl/TrainingCampExceptionalReportService.java
@@ -1,7 +1,28 @@
 package com.itgrids.partyanalyst.exceptionalReport.service.impl;
 
+import org.apache.log4j.Logger;
+
 import com.itgrids.partyanalyst.exceptionalReport.service.ITrainingCampExceptionalReportService;
+import com.itgrids.partyanalyst.utils.CommonMethodsUtilService;
+import com.itgrids.partyanalyst.utils.DateUtilService;
+import com.itgrids.partyanalyst.utils.SetterAndGetterUtilService;
 
 public class TrainingCampExceptionalReportService implements ITrainingCampExceptionalReportService{
-
+	private final static Logger LOG = Logger.getLogger(TrainingCampExceptionalReportService.class);
+	private CommonMethodsUtilService commonMethodsUtilService;
+	private SetterAndGetterUtilService setterAndGetterUtilService;
+	private DateUtilService dateUtilService;
+	public void setCommonMethodsUtilService(
+			CommonMethodsUtilService commonMethodsUtilService) {
+		this.commonMethodsUtilService = commonMethodsUtilService;
+	}
+	public void setSetterAndGetterUtilService(
+			SetterAndGetterUtilService setterAndGetterUtilService) {
+		this.setterAndGetterUtilService = setterAndGetterUtilService;
+	}
+	public void setDateUtilService(DateUtilService dateUtilService) {
+		this.dateUtilService = dateUtilService;
+	}
+	
+	
 }
