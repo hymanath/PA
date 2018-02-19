21aa5b1bbc502524d4b050b4bda8dcc8730d407a
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-19 08:43:51 +0000

    Updated by santosh

---
+++ partyanalyst/src/main/webapp/exceptionalReport/partyMeetingExceptionalReport.js
@@ -0,0 +1,20 @@
+ getPartyMeetingExceptionReportMeetingLevelWise();
+ function getPartyMeetingExceptionReportMeetingLevelWise()
+	{    
+	 	var jsObj = { 
+					 partyMeetingLevel : "Constituency",
+					 fromDate : "01-01-2018",
+					 toDate : "31-01-2018",
+					 partyMeetingTypeIds:[15],
+					 
+				  }
+		$.ajax({
+			type : 'POST',
+			url : 'getPartyMeetingExceptionReportMeetingLevelWiseAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+		      console.log(result);
+		});
+	}
+	
