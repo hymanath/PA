f468e3a7627dc3169d702dffeb400517ef19ee26
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-20 06:14:21 +0000

    trainingcamp exceptional report -swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/ITrainingCampExceptionalReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/ITrainingCampExceptionalReportService.java
@@ -1,5 +1,12 @@
 package com.itgrids.partyanalyst.exceptionalReport.service;
 
+import java.util.List;
+
+import com.itgrids.partyanalyst.dto.TrainingCampProgramVO;
+
 public interface ITrainingCampExceptionalReportService {
+	public TrainingCampProgramVO getOverallTrainingCampReport(String startDate,String endDate, Long stateId,int size);
+	public List<TrainingCampProgramVO> getListOfParliamentsWithPoorPerformance(String startDate,String endDate, Long stateId,int size);
+	public List<TrainingCampProgramVO> getListOfAssemblyWithPoorPerformance(String startDate,String endDate, Long stateId,int size);
 
 }
