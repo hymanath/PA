e24450eb759bac997e54c3a029c9537d05367346
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-21 07:17:26 +0000

    Updated by santosh

--- partyanalyst/src/main/webapp/exceptionalReport/partyMeetingExceptionalReport.js
+++ partyanalyst/src/main/webapp/exceptionalReport/partyMeetingExceptionalReport.js
@@ -1,5 +1,25 @@
- getPartyMeetingExceptionReportMeetingLevelWise();
- function getPartyMeetingExceptionReportMeetingLevelWise()
+ getConstituencyPartyMeetingExceptionReport();
+ getMandalTownDivisionPartyMeetingExceptionReport();
+ getVillageWardPartyMeetingExceptionReport();
+ function getConstituencyPartyMeetingExceptionReport()
+	{    
+	 	var jsObj = { 
+					 partyMeetingLevel : "Constituency",
+					 fromDate : "01/01/2018",
+					 toDate : "31/01/2018",
+					 partyMeetingTypeIds:[3],
+					 
+				  }
+		$.ajax({
+			type : 'POST',
+			url : 'getPartyMeetingExceptionReportMeetingLevelWiseAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+		      console.log(result);
+		});
+	}
+	function getMandalTownDivisionPartyMeetingExceptionReport()
 	{    
 	 	var jsObj = { 
 					 partyMeetingLevel : "mandalTownDivision",
@@ -17,4 +37,25 @@
 		      console.log(result);
 		});
 	}
+	function getVillageWardPartyMeetingExceptionReport()
+	{    
+	 	var jsObj = { 
+					 partyMeetingLevel : "villageWard",
+					 fromDate : "01/01/2018",
+					 toDate : "31/01/2018",
+					 partyMeetingTypeIds:[14],
+					 
+				  }
+		$.ajax({
+			type : 'POST',
+			url : 'getPartyMeetingExceptionReportMeetingLevelWiseAction.action',
+			dataType : 'json',
+			data : {task:JSON.stringify(jsObj)}
+		}).done(function(result){
+		      console.log(result);
+		});
+	}
+	
+	
+	
 	
