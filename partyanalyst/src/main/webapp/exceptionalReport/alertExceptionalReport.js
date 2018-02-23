b2318a3d58cb2b7a245c574f29f1addd4b376cf4
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-23 15:01:33 +0000

    updated by --- Aravind

--- partyanalyst/src/main/webapp/exceptionalReport/alertExceptionalReport.js
+++ partyanalyst/src/main/webapp/exceptionalReport/alertExceptionalReport.js
@@ -1,5 +1,9 @@
-alert("alert");  
-getAssignedCandidateWisePendingAlerts();
+var spinner = '<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>';
+onloadAlertCalls();
+function onloadAlertCalls(){
+	getAssignedCandidateWisePendingAlerts();
+	getOverAllAlertsDetails();
+}
 function getAssignedCandidateWisePendingAlerts(){  
 	var jsObj = { 
 		fromDateStr : "01/01/2010",
@@ -14,11 +18,53 @@ function getAssignedCandidateWisePendingAlerts(){
 		dataType : 'json',
 		data : {task:JSON.stringify(jsObj)}
 	}).done(function(result){
-		console.log(result);   
+		if(result !=null && result.length>0){
+			buildAssignedCandidateWisePendingAlerts(result);
+		}else{
+			$("#assignedParliamentWiseAlertsDivId").html("No Data Available")
+		}
 	});
 }
-getOverAllAlertsDetails();
-function getOverAllAlertsDetails(){  
+function buildAssignedCandidateWisePendingAlerts(result){
+	var str='';
+	str+='<div class="row">';
+			str+='<div class="col-sm-12 m_top20">';
+				str+='<h5 class="text_bold text-capital" >Parliament Wise Assigned Alerts Pending candidates list</h5>';
+				str+='<div class="table-responsive m_top10">';
+					str+='<table class="table details-overview1">';
+						str+='<thead>';
+							str+='<tr>';
+								str+='<th>Name</th>';
+								str+='<th>Parliament</th>';
+								str+='<th>Constituency</th>';
+								str+='<th>Designation</th>';
+								str+='<th>Total Alerts</th>';
+								str+='<th>Pending Alerts</th>';
+								
+							str+='</tr>';
+						str+='</thead>';
+						str+='<tbody>';
+							for(var i in result){
+								str+='<tr>';
+									str+='<td>'+result[i].name+'</td>';
+									str+='<td>'+result[i].parliament+'</td>';
+									str+='<td>'+result[i].constituency+'</td>';
+									str+='<td>'+result[i].designation+'</td>';
+									str+='<td>'+result[i].totalCount+'</td>';
+									str+='<td>'+result[i].pendingCount+'</td>';
+								str+='</tr>';
+								
+							}
+							
+						str+='</tbody>';
+					str+='</table>';
+			str+='</div>';
+		str+='</div>';
+	str+='</div>';
+	$("#assignedParliamentWiseAlertsDivId").html(str)
+}
+function getOverAllAlertsDetails(){
+	$("#overAllAlertsDivId").html(spinner);	
 	var jsObj = { 
 		fromDateStr : "01/01/2010",
 		toDateStr : "01/01/2050",
@@ -32,7 +78,48 @@ function getOverAllAlertsDetails(){
 		dataType : 'json',
 		data : {task:JSON.stringify(jsObj)}
 	}).done(function(result){
-		console.log(result);   
+		if(result !=null && result.length>0){
+			buildOverAllAlertsDetails(result);
+		}else{
+			$("#overAllAlertsDivId").html("No Data Available");	
+		}
 	});         
+}
+function buildOverAllAlertsDetails(result){
+	var str='';
+	str+='<div class="row">';
+			str+='<div class="col-sm-12 m_top20">';
+				str+='<h5 class="text_bold text-capital" >OverAll Alerts</h5>';
+				str+='<div class="table-responsive m_top10">';
+					str+='<table class="table details-overview">';
+						str+='<thead>';
+							str+='<tr>';
+								str+='<th></th>';
+								str+='<th>Total Alerts</th>';
+								for(var i in result[0].subList){
+									str+='<th>'+result[0].subList[i].name+'</th>';
+								}
+							str+='</tr>';
+						str+='</thead>';
+						str+='<tbody>';
+							for(var i in result){
+								str+='<tr>';
+									if(result[i].name == "Last Month Alerts"){
+										str+='<td>Last&nbsp;Month&nbsp;Alerts</td>';
+									}else{
+										str+='<td>'+result[i].name+'</td>';
+									}
+									str+='<td>'+result[i].totalAlert+'</td>';
+									for(var j in result[i].subList){
+										str+='<td>'+result[i].subList[j].count+'</td>';
+									}
+								str+='</tr>';
+							}
+						str+='</tbody>';
+					str+='</table>';
+			str+='</div>';
+		str+='</div>';
+	str+='</div>';
+	$("#overAllAlertsDivId").html(str);
 }
 
--- partyanalyst/src/main/webapp/exceptionalReport/kaizalaExceptionalReport.js
+++ partyanalyst/src/main/webapp/exceptionalReport/kaizalaExceptionalReport.js
@@ -1,10 +1,17 @@
-alert("kaizala");
-getConstituencyWisePoorPerformance();
-function getConstituencyWisePoorPerformance(){  
+var spinner = '<div class="col-md-12 col-xs-12 col-sm-12"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>';
+onloadKaizalaCalls();
+function onloadKaizalaCalls(){
+	getConstituencyWisePoorPerformance("parliament","performanceKaizalaParliamentWiseDetailsDivId");
+	getConstituencyWisePoorPerformance("constituency","performanceKaizalaConstituencyWiseDetailsDivId");
+	getOverallReport();
+}
+
+function getConstituencyWisePoorPerformance(typeval,id){ 
+ $("#"+id).html(spinner);
 	var jsObj = { 
 		stateId : 1,
 		size : 50,
-		location : "parliament"//constituency
+		location : typeval //"parliament"//constituency
 	}
 	$.ajax({
 		type : 'POST',      
@@ -12,11 +19,107 @@ function getConstituencyWisePoorPerformance(){
 		dataType : 'json',
 		data : {task:JSON.stringify(jsObj)}
 	}).done(function(result){
-		console.log(result);   
+		if(result !=null && result.length>0){
+			buildConstituencyWisePoorPerformance(result,typeval,id);
+		}else{
+			$("#"+id).html("No Data Available");
+		}
 	});
 }
-getOverallReport();
-function getOverallReport(){  
+function buildConstituencyWisePoorPerformance(result,typeval,id){
+	var str='';
+	str+='<div class="row">';
+			str+='<div class="col-sm-12 m_top20">';
+			if(typeval == "parliament"){
+				str+='<h5 class="text_bold text-capital" >Top 7 Parliaments with Poor Performance</h5>';
+			}else{
+				str+='<h5 class="text_bold text-capital" >Top 10 Assembly Constituencies with Poor Performance</h5>';
+			}
+				
+				str+='<div class="table-responsive m_top10">';
+					str+='<table class="table details-overview">';
+						str+='<thead>';
+							str+='<tr>';
+							if(typeval == "parliament"){
+								str+='<th>Parliament</th>';
+								str+='<th>Target</th>';
+								//str+='<th>No Smart Phone</th>';
+								str+='<th>Committee Installed</th>';
+								str+='<th>Committee Not installed</th>';
+								str+='<th>%</th>';
+								str+='<th>Public Installed</th>';
+								str+='<th>Cadre Installed</th>';
+								str+='<th>Total Intalled</th>';
+							}else{
+								str+='<th>Constituency</th>';
+								str+='<th>Parliament</th>';
+								str+='<th>Target</th>';
+								//str+='<th>No Smart Phone</th>';
+								str+='<th>Committee Installed</th>';
+								str+='<th>Committee Not installed</th>';
+								str+='<th>%</th>';
+								str+='<th>Public Installed</th>';
+								str+='<th>Cadre Installed</th>';
+								str+='<th>Total Intalled</th>';
+							}
+							str+='</tr>';
+						str+='</thead>';
+						str+='<tbody>';
+						
+						if(typeval == "parliament"){
+							var countVar1 =0;
+						}else{
+							var countVar2 =0;
+						}
+							for(var i in result){
+								str+='<tr>';
+									if(typeval == "parliament"){
+										str+='<td>'+result[i].parliamentName+'</td>';
+										str+='<td>'+result[i].target+'</td>';
+										str+='<td>'+result[i].committeeInstalled+'</td>';
+										str+='<td>'+result[i].committeeNotInstalled+'</td>';
+										str+='<td>'+result[i].committeeNotInstalledPer+'</td>';
+										str+='<td>'+result[i].publicInstalled+'</td>';
+										str+='<td>'+result[i].cadreInstalled+'</td>';
+										str+='<td>'+result[i].totalInstalled+'</td>';
+									}else{
+										str+='<td>'+result[i].constituencyName+'</td>';
+										str+='<td>'+result[i].parliamentName+'</td>';
+										str+='<td>'+result[i].target+'</td>';
+										str+='<td>'+result[i].committeeInstalled+'</td>';
+										str+='<td>'+result[i].committeeNotInstalled+'</td>';
+										str+='<td>'+result[i].committeeNotInstalledPer+'</td>';
+										str+='<td>'+result[i].publicInstalled+'</td>';
+										str+='<td>'+result[i].cadreInstalled+'</td>';
+										str+='<td>'+result[i].totalInstalled+'</td>';
+									}	
+									
+								
+								str+='</tr>';
+								if(typeval == "parliament"){
+									countVar1 =countVar1+1;
+									if (countVar1 === 7) {
+										break;
+									}
+								}else{
+									countVar2 =countVar2+1;
+									if (countVar2 === 10) {
+										break;
+									}
+								}
+								
+							}
+							
+						str+='</tbody>';
+					str+='</table>';
+			str+='</div>';
+		str+='</div>';
+	str+='</div>';
+	
+	$("#"+id).html(str);
+}
+function getOverallReport(){
+	$("#overAllKaizalaDivId").html(spinner);
 	var jsObj = { 
 		stateId : 1
 	}
@@ -26,6 +129,43 @@ function getOverallReport(){
 		dataType : 'json',
 		data : {task:JSON.stringify(jsObj)}
 	}).done(function(result){
-		console.log(result);   
+		if(result !=null){
+			buildOverallReport(result);
+		}else{
+			$("#overAllKaizalaDivId").html("No Data Available");
+		}  
 	});
 }      
+
+function buildOverallReport(result){
+	var str='';
+	
+	str+='<div class="row">';
+		str+='<div class="col-sm-3 m_top10">';
+			str+='<div class="pad_d1ab66">';
+				str+='<h4 class="">Total Target</h4>';
+				str+='<h3 class="text_bold m_top10">'+result.target+'</h3>';
+			str+='</div>';
+		str+='</div>';
+		str+='<div class="col-sm-3 m_top10">';
+			str+='<div class="pad_d1ab66">';
+				str+='<h4 class="">Installed</h4>';
+				str+='<h3 class="text_bold m_top10">'+result.totalInstalled+'</h3>';
+			str+='</div>';
+		str+='</div>';
+		str+='<div class="col-sm-3 m_top10">';
+			str+='<div class="pad_d1ab66">';
+				str+='<h4 class="">Not Installed</h4>';
+				str+='<h3 class="text_bold m_top10">'+result.totalNotInstalled+'</h3>';
+			str+='</div>';
+		str+='</div>';
+		str+='<div class="col-sm-3 m_top10">';
+			str+='<div class="pad_d1ab66">';
+				str+='<h4 class="">No Smart Phone</h4>';
+				str+='<h3 class="text_bold m_top10">'+result.totalNotHavingSmartphone+'</h3>';
+			str+='</div>';
+		str+='</div>';
+	str+='</div>';
+	
+	$("#overAllKaizalaDivId").html(str);
+}
