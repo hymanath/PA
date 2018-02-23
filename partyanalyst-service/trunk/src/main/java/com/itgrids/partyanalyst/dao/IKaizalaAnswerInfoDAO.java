46054e6562816a67d6f3f74bcaca9d4ad6a5cf77
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-23 04:00:17 +0000

    Kaizala exception report implemented-swadhin

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IKaizalaAnswerInfoDAO.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IKaizalaAnswerInfoDAO.java
@@ -1,10 +1,19 @@
 package com.itgrids.partyanalyst.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.partyanalyst.model.KaizalaAnswerInfo;
 import com.itgrids.partyanalyst.model.KaizalaAnswers;
 
 public interface IKaizalaAnswerInfoDAO extends GenericDao<KaizalaAnswerInfo, Long> {
-	
+	public List<Object[]> getConstituencyWiseTargetList(Long stateId,String location);
+	public List<Object[]> getConstituencyWiseInstalledCommitteeMembers(Long stateId,String location);
+	public List<Object[]> getConstituencyWiseNotHavingSmartPhone(Long stateId,String location);
+	public List<Object[]> getConstituencyWisePublicInstalled(Long stateId,String location);
+	public List<Object[]> getConstituecnyWiseCadreInstalled(Long stateId,String location);
+	public Long getTotalTarget(Long stateId);
+	public Long getTotalInstalled(Long stateId);
+	public Long getNotHavingSmartPhone(Long stateId);
 }
