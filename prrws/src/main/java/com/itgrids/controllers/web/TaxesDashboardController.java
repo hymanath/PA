1a1856648eec16a8f9b9eb17a14291b37bdde6e1
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-02-28 09:17:33 +0000

    Panchayat Taxes Services - Nandhini

---
+++ prrws/src/main/java/com/itgrids/controllers/web/TaxesDashboardController.java
@@ -0,0 +1,54 @@
+package com.itgrids.controllers.web;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itgrids.dto.InputVO;
+import com.itgrids.dto.TaxesVO;
+import com.itgrids.service.ITaxesDashBoardService;
+
+@EnableAutoConfiguration
+@Controller
+@RequestMapping("/")
+public class TaxesDashboardController {
+	
+	private static final Logger LOG = Logger.getLogger(TaxesDashboardController.class);
+	
+	@Autowired
+	private ITaxesDashBoardService taxesDashBoardService;
+	
+	
+	@RequestMapping(value ="/taxesDashboard",method = RequestMethod.GET)
+    public String taxesDashboard(ModelMap model) {
+		return "taxesDashboard";
+    }
+	
+   @RequestMapping(value = "/getTaxesAndCategoryWiseOverViewDetails", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+   public @ResponseBody  TaxesVO  getTaxesAndCategoryWiseOverViewDetails(@RequestBody InputVO inputVo) {
+	TaxesVO  reusltVO = taxesDashBoardService.getTaxesAndCategoryWiseOverViewDetails(inputVo);
+	   return reusltVO;
+   }
+   
+   @RequestMapping(value = "/getTaxesIndicatorDetails", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+   public @ResponseBody  List<TaxesVO>  getTaxesIndicatorDetails(@RequestBody InputVO inputVo) {
+	List<TaxesVO>  reusltList = taxesDashBoardService.getTaxesIndicatorDetails(inputVo);
+	   return reusltList;
+   }
+   
+   @RequestMapping(value = "/getTaxesDefaultOverviewDetails", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+   public @ResponseBody  List<TaxesVO>  getTaxesDefaultOverviewDetails(@RequestBody InputVO inputVo) {
+	List<TaxesVO>  reusltList = taxesDashBoardService.getTaxesDefaultOverviewDetails(inputVo);
+	   return reusltList;
+   }
+
+}
