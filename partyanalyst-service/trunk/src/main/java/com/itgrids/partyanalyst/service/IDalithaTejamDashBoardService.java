6a14d75be6683ef275a2127fe01582ec4ae0b4b8
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-03 15:36:29 +0000

    Updated By Sanjeev

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDalithaTejamDashBoardService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/service/IDalithaTejamDashBoardService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.itgrids.partyanalyst.dto.DalithaTejamInputVo;
 import com.itgrids.partyanalyst.dto.DalithaTejamVO;
+import com.itgrids.partyanalyst.dto.EventLocationVO;
 
 public interface IDalithaTejamDashBoardService {
 
 	public List<DalithaTejamVO> getLatestImages(DalithaTejamInputVo inputVo);
 
 	public List<DalithaTejamVO> getDateWiseCount(DalithaTejamInputVo inputVo);
+	public List<EventLocationVO> DalithTejamLocationWiseData(String fromDate,String toDate,Long locationScopeId,Long activityScopeId,Long locationValue);
 }
