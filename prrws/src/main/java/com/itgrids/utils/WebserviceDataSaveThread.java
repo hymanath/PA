40c29b0fbc0fe56d36264741779314db966b3566
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-06 19:07:31 +0000

    Updated by Dandu Kamalakar

---
+++ prrws/src/main/java/com/itgrids/utils/WebserviceDataSaveThread.java
@@ -0,0 +1,57 @@
+package com.itgrids.utils;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itgrids.dto.WebserviceVO;
+import com.itgrids.service.IWebserviceHandlerService;
+import com.itgrids.service.integration.external.WebServiceUtilService;
+import com.sun.jersey.api.client.ClientResponse;
+
+public class WebserviceDataSaveThread implements Runnable{
+	
+	private static final Logger LOG = Logger.getLogger(WebserviceDataSaveThread.class);
+	
+	@Autowired
+	private IWebserviceHandlerService webserviceHandlerService;
+	
+	@Autowired
+	private WebServiceUtilService webServiceUtilService;
+	
+	private String url;
+	private Object input;
+	private String requestMethod;
+	private Long webserviceId;
+	private String inputData;
+	
+	public WebserviceDataSaveThread(String url,Object input,String requestMethod,Long webserviceId,String inputData)
+	{
+		this.url = url;
+		this.input = input;
+		this.requestMethod = requestMethod;
+		this.webserviceId = webserviceId;
+		this.inputData = inputData;
+	}
+	
+	public void run() 
+	{
+		try{
+			ClientResponse clientResponse = webServiceUtilService.callWebService(url,input,requestMethod);
+			
+			if(clientResponse != null && clientResponse.getStatus() == 200)
+			{
+				WebserviceVO webserviceVO = new WebserviceVO();
+				webserviceVO.setWebserviceId(webserviceId);
+				webserviceVO.setInputData(inputData);
+				String resStr = clientResponse.getEntity(String.class);
+				webserviceVO.setResponseData(resStr != null ? resStr.trim() : null);
+				
+				webserviceHandlerService.saveWebserviceResponseData(webserviceVO);
+			}
+			
+		}catch(Exception e)
+		{
+			LOG.error("Exception Occured in run() Method of WebserviceDataSaveThread Class ",e);
+		}
+	}
+}
