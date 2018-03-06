d497c9e5c40ffd526999ca8595bc11c5b6d8943f
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-06 12:06:31 +0000

    Updated by Dandu Kamalakar

--- prrws/src/main/java/com/itgrids/dao/impl/WebServiceDataDAO.java
+++ prrws/src/main/java/com/itgrids/dao/impl/WebServiceDataDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.dao.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.Query;
@@ -30,15 +31,38 @@ public class WebServiceDataDAO extends GenericDaoHibernate<WebServiceData,Long> 
 		}
 	
 
-	@Override
 	public Long getLatestDataId() {
 		Query query = getSession().createQuery(" select model.webServiceDataId from WebServiceData model where model.webserviceId = 127 ");
 		return (Long)query.uniqueResult();
 	}
 
-	public String getWebserviceResponseData(Long webserviceId, String input) {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings("unchecked")
+	public String getWebserviceResponseData(Long webserviceId,String input) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" SELECT model.responceData FROM WebServiceData model where model.isDeleted ='N' AND model.webserviceId = :webserviceId ");
+		sb.append(" AND model.responceData IS NOT NULL ");
+		
+		if(input != null && input.trim().length() > 0)
+			sb.append(" AND model.inputData = :input ");
+		
+		sb.append(" ORDER BY model.webServiceDataId DESC ");
+		
+		Query query = getSession().createQuery(sb.toString());
+		
+		query.setParameter("webserviceId",webserviceId);
+		
+		if(input != null && input.trim().length() > 0)
+			query.setParameter("input",input);
+		
+		query.setMaxResults(1);
+		
+		List<Object> list = query.list();
+		
+		if(list != null && list.size() > 0)
+			return list.get(0).toString();
+		else
+			return null;
 	}
 
 	
