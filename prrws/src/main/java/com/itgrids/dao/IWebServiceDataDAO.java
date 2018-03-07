f25e7661584b0be05dce7aee07cbb9495ce98f27
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-07 19:37:36 +0000

    Updated by Dandu Kamalakar

--- prrws/src/main/java/com/itgrids/dao/IWebServiceDataDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IWebServiceDataDAO.java
@@ -1,6 +1,7 @@
 package com.itgrids.dao;
 
 import java.util.Date;
+import java.util.List;
 
 import org.appfuse.dao.GenericDao;
 
@@ -9,5 +10,8 @@ import com.itgrids.model.WebServiceData;
 public interface IWebServiceDataDAO extends GenericDao<WebServiceData, Long>{
 
 	public Long getLatestDataId();
+	
 	public String getRfidTrackingOverAllTargetsData(Date fromDate);
+	
+	public List<Object[]> getWebserviceResponseData(Long webserviceId,String input);
 }
