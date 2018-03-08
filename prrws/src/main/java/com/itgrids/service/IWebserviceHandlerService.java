5a33733d3957a2d78eb6ea92a6de5134945160ca
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-08 17:31:41 +0000

    Updated by Dandu Kamalakar

--- prrws/src/main/java/com/itgrids/service/IWebserviceHandlerService.java
+++ prrws/src/main/java/com/itgrids/service/IWebserviceHandlerService.java
@@ -8,4 +8,6 @@ public interface IWebserviceHandlerService {
 	public String callWebService(String url,Object input,String requestMethod);
 	
 	public void saveWebserviceResponseData(WebserviceVO webserviceVO);
+	
+	public WebserviceVO callWebServiceForThread(String url,Object input,String requestMethod);
 }
