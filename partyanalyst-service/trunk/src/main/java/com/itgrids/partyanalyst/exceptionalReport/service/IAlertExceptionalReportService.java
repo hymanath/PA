0b9126f300f218a2d332bb70ad79c5d5a5154d67
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-20 07:41:08 +0000

    Updated By Sanjeev

--- partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/IAlertExceptionalReportService.java
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/exceptionalReport/service/IAlertExceptionalReportService.java
@@ -8,4 +8,5 @@ public interface IAlertExceptionalReportService {
 	public List<AlertCoreDashBoardVO> getAssignedCandidateWisePendingAlerts(String startDate,String endDate, Long stateId,int size,List<Long> alertTypeId);
 	public List<AlertCoreDashBoardVO> getOverAllAlertsDetails(String startDate,String endDate, Long stateId,int size,List<Long> alertTypeIds);
 	public List<AlertCoreDashBoardVO> getDistrictWiseAlertsDetails(String startDate,String endDate, Long stateId,int size,List<Long> alertTypeIds);
+	public List<AlertCoreDashBoardVO> getLocationWiseAlertsDetails(String startDate,String endDate, Long stateId,int size,List<Long> alertTypeIds,String locationType);
 }
