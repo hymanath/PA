b4647e0c4aa6376c33266bde9cdc2093c3e0e259
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-26 11:21:58 +0000

    Updated by -Teja

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IRwsIvrSurveyDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.RwsIvrSurvey;
+
+public interface IRwsIvrSurveyDAO extends GenericDao<RwsIvrSurvey,Long>{
+
+}
