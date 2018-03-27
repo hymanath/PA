85b2eec31123b675207ab7a17d59656e41a7ed0e
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-27 18:29:27 +0000

    updated by sanjeev

---
+++ prrws/src/main/java/com/itgrids/dao/IEncHabLocationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.model.EncHabLocation;
+
+public interface IEncHabLocationDAO extends GenericDao<EncHabLocation,Long>{
+
+}
--- prrws/src/main/java/com/itgrids/dao/IRwsTehsilDAO.java
+++ prrws/src/main/java/com/itgrids/dao/IRwsTehsilDAO.java
@@ -1,5 +1,7 @@
 package com.itgrids.dao;
 
+import java.util.List;
+
 import org.appfuse.dao.GenericDao;
 
 import com.itgrids.model.RwsTehsil;
@@ -7,5 +9,7 @@ import com.itgrids.model.RwsTehsil;
 public interface IRwsTehsilDAO extends GenericDao<RwsTehsil, Long> {
 
 	public String getRwsCode(Long mandalId);
+
+	public List<Object[]> getAllmandals();
 
 }
