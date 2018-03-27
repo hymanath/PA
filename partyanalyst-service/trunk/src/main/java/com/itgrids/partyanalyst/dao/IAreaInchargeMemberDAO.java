69383db7ea2886d135bf5187662a157fd8e4c5b5
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-27 05:37:00 +0000

    updated by srujana : booth  incharge's for modals and dao's

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAreaInchargeMemberDAO.java
@@ -0,0 +1,12 @@
+package com.itgrids.partyanalyst.dao;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AreaInchargeMember;
+
+public interface IAreaInchargeMemberDAO extends GenericDao<AreaInchargeMember, Long>{
+	public String getActiveOrInActiveInchageDetails(Long cadreId);
+	public List<Object[]> getAssignedCadreList(String status);
+}
