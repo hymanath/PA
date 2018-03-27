ec39d45e30feae6a41544bc1e03f37f404ffa395
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-27 05:37:15 +0000

    updated by srujana : booth  incharge's for modals and dao's

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AreaInchargeLocationDAO.java
@@ -0,0 +1,53 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IAreaInchargeLocationDAO;
+import com.itgrids.partyanalyst.model.AreaInchargeLocation;
+
+public class AreaInchargeLocationDAO extends GenericDaoHibernate<AreaInchargeLocation, Long> implements IAreaInchargeLocationDAO {
+
+	public AreaInchargeLocationDAO(){
+		super(AreaInchargeLocation.class);
+	}
+	
+	
+	/*public int getLocationIdsOfBooths(List<Long> boothIds ){
+		Query query = getSession().createQuery(" update AreaInchargeLocation model " +
+				" set model.isAssinged = 'Y',model.isDeleted ='N' where " +
+				"  model.address.booth.boothId in(:boothIds) ");
+		
+		query.setParameterList("boothIds", boothIds);
+		return query.executeUpdate();
+	}*/
+	public Long getLocationIdsOfBooths(Long boothId){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select AIL.AreaInchargeLocationId from  AreaInchargeLocation AIL  where  " );
+		if(boothId != null && boothId.longValue()>0l){
+			sb.append(" AIL.address.booth.boothId =:boothId "); 
+		}
+		Query query = getSession().createQuery(sb.toString());
+		if(boothId != null && boothId.longValue()>0l){
+			query.setParameter("boothId", boothId);
+		}
+		return (Long)query.uniqueResult();
+		
+	}
+	
+	public List<Object[]> getAssignedAndUnAssignedBooths(){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select AIL.areaInchargeLocationId,AIL.isAssinged," +
+				" AIL.address.panchayat.panchayatId,panchayat.panchayatName," +
+				" tehsil.tehsilId,AIL.address.tehsil.tehsilName  " +
+				" from  AreaInchargeLocation AIL " +
+				" left join AIL.address.panchayat panchayat " +
+				" left join AIL.address.tehsil tehsil  " +
+				" " );
+		Query query = getSession().createQuery(sb.toString());
+		return query.list();
+	}
+	
+}
---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/hibernate/AreaInchargeMemberDAO.java
@@ -0,0 +1,78 @@
+package com.itgrids.partyanalyst.dao.hibernate;
+
+import java.util.List;
+
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.Query;
+
+import com.itgrids.partyanalyst.dao.IAreaInchargeMemberDAO;
+import com.itgrids.partyanalyst.model.AreaInchargeMember;
+
+public class AreaInchargeMemberDAO extends GenericDaoHibernate<AreaInchargeMember, Long> implements IAreaInchargeMemberDAO{
+
+	public AreaInchargeMemberDAO() {
+		super(AreaInchargeMember.class);
+		
+	}
+	
+	public List<Object[]> getAreaInchargeDetails(Long voterId,String mobileNo,Long memberShipId){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select tdpCadre.tdpCadreId, tdpCadre.firstname,tdpCadre.relativename," +
+				" tdpCadre.age, tdpCadre.gender, tdpCadre.houseNo," +
+				" address.tehsil.tehsilId,address.tehsil.tehsilName," +
+				" tdpCadre.casteState.caste.casteName,tdpCadre.image,AIM.isActive " +
+				" from  AreaInchargeMember AIM  left join AIM.tdpCadre tdpCadre" +
+				" left join AIM.areaInchargeLocation areaInchargeLocation left join areaInchargeLocation.address address " +
+				"  where AIM.isDeleted ='N' and tdpCadre.isDeleted ='N' ");
+		if(voterId != null && voterId.longValue()>0l){
+			sb.append(" and tdpCadre.voterId =:voterId  ");	
+		}
+        if(mobileNo != null && !mobileNo.isEmpty()){
+        	sb.append(" and tdpCadre.mobileNo =:mobileNo ");	
+        }
+        if(memberShipId != null && memberShipId.longValue()>0l){
+        	sb.append(" and tdpCadre.memberShipNo =:memberShipId ");	
+        }
+        Query query = getSession().createQuery(sb.toString());
+        if(voterId != null && voterId.longValue()>0l){
+        	query.setParameter("voterId", voterId);
+		}
+        if(mobileNo != null && !mobileNo.isEmpty()){
+        	query.setParameter("mobileNo", mobileNo);
+        }
+        if(memberShipId != null && memberShipId.longValue()>0l){
+        	query.setParameter("memberShipId", memberShipId);
+        }
+		return query.list();
+		
+	}
+	
+	public String getActiveOrInActiveInchageDetails(Long cadreId){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select AIM.isActive from  AreaInchargeMember AIM  where AIM.isDeleted ='N' " );
+		if(cadreId != null && cadreId.longValue()>0l){
+			sb.append(" AIM.tdpCadre.tdpCadreId =:cadreId "); 
+		}
+		Query query = getSession().createQuery(sb.toString());
+		if(cadreId != null && cadreId.longValue()>0l){
+			query.setParameter("cadreId", cadreId);
+		}
+		return (String)query.uniqueResult();
+		
+	}
+	
+	public List<Object[]> getAssignedCadreList(String status){
+		StringBuilder sb = new StringBuilder();
+		sb.append(" select AIM.areaInchargeLocationId,tdpCadre.tdpCadreId,tdpCadre.firstname,tdpCadre.houseNo " +
+				"from  AreaInchargeMember AIM left join AIM.tdpCadre tdpCadre  where AIM.isDeleted ='N' " );
+		if(status != null && !status.isEmpty()){
+			sb.append(" AIM.isActive =:status "); 
+		}
+		Query query = getSession().createQuery(sb.toString());
+		if(status != null && !status.isEmpty()){
+			query.setParameter("status", status);
+		}
+		return query.list();
+	}
+	
+}
