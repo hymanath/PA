527e7adb9b804037b0e45b55a6b1b10cfa46ee56
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-03-28 13:55:43 +0000

    Zoho Mobile SAML related Service - balu

---
+++ partyanalyst-service/trunk/src/main/java/com/itgrids/partyanalyst/dao/IAlertCadreTypeRelationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.partyanalyst.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.partyanalyst.model.AlertCadreTypeRelation;
+
+public interface IAlertCadreTypeRelationDAO extends GenericDao<AlertCadreTypeRelation, Long> {
+
+}
