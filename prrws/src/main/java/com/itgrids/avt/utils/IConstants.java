8fe43203ee55f081c0cd526d9a7a5dba698fe270
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-04-02 06:40:07 +0000

    updated by sanjeev for avt

---
+++ prrws/src/main/java/com/itgrids/avt/utils/IConstants.java
@@ -0,0 +1,8 @@
+package com.itgrids.avt.utils;
+
+public interface IConstants {
+
+	public static final String FILE_SEPARATOR = "file.separator";
+	public static final String ADMIN_USERNAME_FOR_SMS = "koffeemedia";
+	public static final String ADMIN_PASSWORD_FOR_SMS = "9885410393";
+}
