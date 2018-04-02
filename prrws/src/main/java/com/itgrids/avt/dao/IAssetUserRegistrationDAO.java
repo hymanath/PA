6afe850042d4526bd44f7c38b4524ede524b6325
itgrids <itgrids@b17b186f-d863-de11-8533-00e0815b4126>
2018-04-02 05:56:41 +0000

    updated by sanjeev for avt

---
+++ prrws/src/main/java/com/itgrids/avt/dao/IAssetUserRegistrationDAO.java
@@ -0,0 +1,9 @@
+package com.itgrids.avt.dao;
+
+import org.appfuse.dao.GenericDao;
+
+import com.itgrids.avt.model.AssetUserRegistration;
+
+public interface IAssetUserRegistrationDAO extends GenericDao<AssetUserRegistration, Long>{
+
+}
